65e8ce6f666291b97be7535133acf54606c72e38
E.Z. Hart <hartez@gmail.com>
2016-03-22 17:47:35 -0600

    Powershell script to create secrets.txt and MapKeys.cs

---
+++ Setup.ps1
@@ -0,0 +1,22 @@
+﻿$createSecrets = Test-Path .\Xamarin.Forms.Controls\secrets.txt
+$createMapKeys = Test-Path .\Xamarin.Forms.ControlGallery.Android\Properties\MapsKey.cs
+
+if(-not $createSecrets){
+    Write-Host "Creating secrets.txt"
+    New-Item -ItemType File .\Xamarin.Forms.Controls\secrets.txt
+}
+
+if(-not $createMapKeys){
+    Write-Host "Creating MapKeys.cs"
+
+    $content = @"
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+[assembly: Android.App.MetaData("com.google.android.maps.v2.API_KEY", Value = "")]
+"@
+
+    Set-Content .\Xamarin.Forms.ControlGallery.Android\Properties\MapsKey.cs $content
+}
