d5c3359070317e57a2cb8e98608d0286071e389d
Samantha Houts <samantha.houts@xamarin.com>
2016-03-23 15:31:38 -0700

    [W] ListView Cells will respect RowHeight

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla39829.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Xamarin.Forms.CustomAttributes;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 39829, "RowHeight of ListView is not working for UWP", PlatformAffected.WinRT)]
+	public class Bugzilla39829 : TestContentPage
+	{
+		protected override void Init()
+		{
+			Title = "Master";
+			var listView = new ListView
+			{
+				RowHeight = 150,
+				AutomationId = "listview",
+				ItemsSource = new string[] { "Test1", "Test2", "Test3", "Test4", "Test5", "Test6", }
+			};
+
+			Content = listView;
+		}
+
+#if UITEST
+		[Test]
+		[Category("ManualReview")]
+		public void Bugzilla39829Test()
+		{
+			RunningApp.WaitForElement(q => q.Marked("listview"));
+			RunningApp.Screenshot("If there isn't substantial space between the list items, this test has failed.");
+		}
+#endif
+	}
+}
