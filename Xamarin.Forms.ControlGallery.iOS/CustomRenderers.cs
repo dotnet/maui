8736a6b6f577ce3d4f24b7d1cf00f1700d183ec2
Rui Marinho <me@ruimarinho.net>
2016-06-22 15:59:54 +0100

    [Controls] Fix use of internal extension methods (#237)
    * [Controls] Fix use of internal extension methods
    
    * [Controls] Update Xamarin UItest on controls
    
    * Update package TestCloud
    
    * Fix
    
    * Revert "Fix"
    
    This reverts commit 283f96a2210322bf37795dff61394938eb909f59.
    
    * Fix reference

--- Xamarin.Forms.ControlGallery.iOS/CustomRenderers.cs
+++ Xamarin.Forms.ControlGallery.iOS/CustomRenderers.cs
@@ -6,6 +6,7 @@ using Xamarin.Forms.ControlGallery.iOS;
 using System.Collections.Generic;
 using System.Linq;
 using System.Drawing;
+using System.Collections;
 
 #if __UNIFIED__
 using UIKit;
@@ -324,12 +325,12 @@ namespace Xamarin.Forms.ControlGallery.iOS
 		public IEnumerable<DataSource> Items
 		{
 			//get{ }
-			set { _tableItems = value.ToList(); }
+			set { _tableItems =  new List<DataSource>(value); }
 		}
 
 		public NativeiOSListViewSource(NativeListView2 view)
 		{
-			_tableItems = view.Items.ToList();
+			_tableItems = new List<DataSource>(view.Items);
 			_listView = view;
 		}
 
@@ -407,12 +408,13 @@ namespace Xamarin.Forms.ControlGallery.iOS
 
 		public IEnumerable<string> Items
 		{
-			set { _tableItems = value.ToList(); }
+			set { _tableItems = new List<string>(value); 
+			}
 		}
 
 		public NativeListViewSource(NativeListView view)
 		{
-			_tableItems = view.Items.ToList();
+			_tableItems = new List<string>(view.Items);
 			_listView = view;
 		}
 
@@ -524,7 +526,10 @@ namespace Xamarin.Forms.ControlGallery.iOS
 
 		public CollectionViewController(UICollectionViewLayout layout, OnItemSelected onItemSelected) : base(layout)
 		{
-			items = Enumerable.Range(0, 20).Select(c => $"#{c}").ToList();
+			items = new List<string>();
+			for (int i = 0; i < 20; i++) {
+				items.Add($"#{i}");
+			}
 			_onItemSelected = onItemSelected;
 		}
 
