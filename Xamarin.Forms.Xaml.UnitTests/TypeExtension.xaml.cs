4226f5b386a83dc9eb2f0c06f4920ac1be19e036
Stephane Delcroix <stephane@delcroix.org>
2016-08-16 20:33:44 +0200

    [Xaml] Clone node tree on DT, allow markup to be evaluated multiple times (#295)

---
+++ Xamarin.Forms.Xaml.UnitTests/TypeExtension.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+using NUnit.Framework;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public enum NavigationOperation
+	{
+		Forward,
+		Back,
+		Replace,
+	}
+
+	[ContentProperty(nameof(Operation))]
+	public class NavigateExtension : IMarkupExtension<ICommand>
+	{
+		public NavigationOperation Operation { get; set; }
+
+		public Type Type { get; set; }
+
+		public ICommand ProvideValue(IServiceProvider serviceProvider)
+		{
+			return new Command(() => { });
+		}
+
+		object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+		{
+			return ProvideValue(serviceProvider);
+		}
+	}
+
+	public partial class TypeExtension : ListView
+	{
+		public TypeExtension()
+		{
+			InitializeComponent();
+		}
+
+		public TypeExtension(bool useCompiledXaml)
+		{
+			//this stub will be replaced at compile time
+		}
+
+		[TestFixture]
+		public class Tests
+		{
+			[TestCase(false)]
+			[TestCase(true)]
+			public void NestedMarkupExtensionInsideDataTemplate(bool useCompiledXaml)
+			{
+				var listView = new TypeExtension(useCompiledXaml);
+				listView.ItemsSource = new string [2];
+
+				var cell = (ViewCell)listView.TemplatedItems [0];
+				var button = (Button)cell.View;
+				Assert.IsNotNull(button.Command);
+
+				cell = (ViewCell)listView.TemplatedItems [1];
+				button = (Button)cell.View;
+				Assert.IsNotNull(button.Command);
+			}
+		}
+	}
+}
