ba6caad23ed7abdb1661cc61114c24eb3ae09da1
Samantha Houts <samantha@teamredwall.com>
2016-10-06 10:17:51 -0700

    [A] Prevent blank page from appearing when MainPage is switched (#421)
    * repro
    
    * [A]Add horrible hack to prevent background flashing when swapping MainPage
    
    * cleanup repro
    
    * Revert "[A]Add horrible hack to prevent background flashing when swapping MainPage"
    
    This reverts commit d0fdc3856d0eaa5ec781f790d524ad7624a26560.
    
    * [A] E.Z.'s less magical but just as effective solution

---
+++ Xamarin.Forms.Controls/Bugzilla44596SplashPage.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.Controls
+{
+	public class Bugzilla44596SplashPage : ContentPage
+	{
+		Action FinishedLoading { get; set; }
+
+
+		public Bugzilla44596SplashPage(Action finishedLoading)
+		{
+			BackgroundColor = Color.Blue;
+			FinishedLoading = finishedLoading;
+		}
+
+
+		protected async override void OnAppearing()
+		{
+			base.OnAppearing();
+			await Task.Delay(2000);
+			FinishedLoading?.Invoke();
+		}
+	}
+}
