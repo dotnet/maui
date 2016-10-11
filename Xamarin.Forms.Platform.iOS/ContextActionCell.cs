9a5dab9424b57a81e4a4132cbf73ddeca1cfd4ef
adrianknight89 <adrianknight89@outlook.com>
2016-10-11 16:39:18 -0500

    [iOS] Tapping on ListView with two fingers should not crash (#379)
    * Tapping on ListView with two fingers should not crash
    
    * Revert "Tapping on ListView with two fingers should not crash"
    
    This reverts commit 0b0752f3cfdbb1c29678d75bd18c5a00e564d77b.
    
    * code without refactoring
    
    * moving up selector check

--- Xamarin.Forms.Platform.iOS/ContextActionCell.cs
+++ Xamarin.Forms.Platform.iOS/ContextActionCell.cs
@@ -661,8 +661,10 @@ namespace Xamarin.Forms.Platform.iOS
 			{
 				var selector = (SelectGestureRecognizer)recognizer;
 
-				var table = (UITableView)recognizer.View;
+				if (selector._lastPath == null)
+					return;
 
+				var table = (UITableView)recognizer.View;
 				if (!selector._lastPath.Equals(table.IndexPathForSelectedRow))
 					table.SelectRow(selector._lastPath, false, UITableViewScrollPosition.None);
 				table.Source.RowSelected(table, selector._lastPath);
