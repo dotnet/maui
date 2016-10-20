ad776f942b0d3ebe625ec3ad2ba928839ee6aeb9
Samantha Houts <samantha@teamredwall.com>
2016-10-20 13:24:09 -0700

    [Android] SoftInputMode works with initial value (#465)

--- Xamarin.Forms.Platform.Android/AppCompat/FormsAppCompatActivity.cs
+++ Xamarin.Forms.Platform.Android/AppCompat/FormsAppCompatActivity.cs
@@ -126,6 +126,8 @@ namespace Xamarin.Forms.Platform.Android
 			(application as IApplicationController)?.SetAppIndexingProvider(new AndroidAppIndexProvider(this));
 			Xamarin.Forms.Application.Current = application;
 
+			SetSoftInputMode();
+
 			CheckForAppLink(Intent);
 
 			application.PropertyChanged += AppOnPropertyChanged;
@@ -166,8 +168,6 @@ namespace Xamarin.Forms.Platform.Android
 				bar = new AToolbar(this);
 
 			SetSupportActionBar(bar);
-
-			SetSoftInputMode();
 
 			_layout = new ARelativeLayout(BaseContext);
 			SetContentView(_layout);
