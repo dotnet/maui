6196407f8924463e526bc96f6855560954d9d2e0
adrianknight89 <adrianknight89@outlook.com>
2016-10-24 16:28:58 -0500

    [Android] Internal keyboard hide/show should handle null views (#418)
    * Check for null input view before trying to hide keyboard
    
    * Adding null check to ShowKeyboard
    
    * change to throw an exception

--- Xamarin.Forms.Platform.Android/KeyboardManager.cs
+++ Xamarin.Forms.Platform.Android/KeyboardManager.cs
@@ -14,14 +14,15 @@ namespace Xamarin.Forms.Platform.Android
 			if (Forms.Context == null)
 				throw new InvalidOperationException("Call Forms.Init() before HideKeyboard");
 
+			if (inputView == null)
+				throw new ArgumentNullException(nameof(inputView) + " must be set before the keyboard can be hidden.");
+
 			using (var inputMethodManager = (InputMethodManager)Forms.Context.GetSystemService(Context.InputMethodService))
 			{
-				IBinder windowToken = null;
-
 				if (!overrideValidation && !(inputView is EditText || inputView is TextView || inputView is SearchView))
 					throw new ArgumentException("inputView should be of type EditText, SearchView, or TextView");
 
-				windowToken = inputView.WindowToken;
+				IBinder windowToken = inputView.WindowToken;
 				if (windowToken != null)
 					inputMethodManager.HideSoftInputFromWindow(windowToken, HideSoftInputFlags.None);
 			}
@@ -31,6 +32,9 @@ namespace Xamarin.Forms.Platform.Android
 		{
 			if (Forms.Context == null)
 				throw new InvalidOperationException("Call Forms.Init() before ShowKeyboard");
+
+			if (inputView == null)
+				throw new ArgumentNullException(nameof(inputView) + " must be set before the keyboard can be shown.");
 
 			using (var inputMethodManager = (InputMethodManager)Forms.Context.GetSystemService(Context.InputMethodService))
 			{
