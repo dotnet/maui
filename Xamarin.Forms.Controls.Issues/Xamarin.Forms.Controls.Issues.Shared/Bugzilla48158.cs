121ddafde56c66f0b720e2cb27c2f3aed13027f2
Rui Marinho <me@ruimarinho.net>
2016-11-30 20:09:57 +0000

    [iOS] Avoid using DrawRect on base VisualElementRenderer (#570)
    * [Controls] Add retro for Bugzilla 48158
    
    * [iOS] Avoid using DrawRect as this will make some properties not being applied to the layer
    
    * [iOS] Make sure BoxRenderer calls base LayoutSubviews
    
    * [iOS] Better fix for adding the Blur effect and avoid override draw’s
    
    * [Controls] Remove extra nunit category

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla48158.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 48158, "Hidden controls become transparent, needs manual verification", PlatformAffected.iOS)]
+	public class Bugzilla48158 : TestContentPage // or TestMasterDetailPage, etc ...
+	{
+		protected override void Init()
+		{
+			var grdInner = new Grid { BackgroundColor = Color.Red, IsVisible = false, Padding = new Thickness(10) };
+			var btn = new Button { Text = "Click and verify background is red" };
+			btn.Clicked += (s, e) =>
+			{
+				grdInner.IsVisible = !grdInner.IsVisible;
+			};
+			var grd = new Grid();
+			grd.Children.Add(grdInner);
+			grd.Children.Add(btn);
+			Content = grd;
+		}
+	}
+}
--- Xamarin.Forms.Platform.iOS/VisualElementRenderer.cs
+++ Xamarin.Forms.Platform.iOS/VisualElementRenderer.cs
@@ -170,17 +170,16 @@ namespace Xamarin.Forms.Platform.iOS
 			return new SizeF(0, 0);
 		}
 
-		public override void Draw(RectangleF rect)
+		public override void LayoutSubviews()
 		{
-			base.Draw(rect);
-			if (_blur != null)
+			base.LayoutSubviews();
+			if (_blur != null && Superview != null)
 			{
-				_blur.Frame = rect;
+				_blur.Frame = Bounds;
 				if (_blur.Superview == null)
 					Superview.Add(_blur);
 			}
 		}
-
 		protected override void Dispose(bool disposing)
 		{
 			if ((_flags & VisualElementRendererFlags.Disposed) != 0)
@@ -289,7 +288,7 @@ namespace Xamarin.Forms.Platform.iOS
 			}
 
 			_blur = new UIVisualEffectView(blurEffect);
-			SetNeedsDisplay();
+			LayoutSubviews();
 		}
 
 		protected virtual void UpdateNativeWidget()
