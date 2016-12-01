3d85653f270854b4aab82e45e6e58afb760feb85
Stephane Delcroix <stephane@delcroix.org>
2016-12-01 22:14:22 +0100

    [XamlC] assigned derived type to generic BP (#566)
    * [XamlC] assigned derived type to generic BP
    
    * [XamlC] fix 48554
    
    * f

---
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Unreported006.xaml.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public partial class Unreported006 : ContentPage
+	{
+		public Unreported006()
+		{
+			InitializeComponent();
+		}
+
+		public Unreported006(bool useCompiledXaml)
+		{
+			//this stub will be replaced at compile time
+		}
+
+		public Layout<View> GenericProperty {
+			get { return (Layout<View>)GetValue(GenericPropertyProperty); }
+			set { SetValue(GenericPropertyProperty, value); }
+		}
+
+		public static readonly BindableProperty GenericPropertyProperty =
+			BindableProperty.Create(nameof(GenericProperty), typeof(Layout<View>), typeof(Unreported006));
+
+		[TestFixture]
+		class Tests
+		{
+			[TestCase(true), TestCase(false)]
+			public void CanAssignGenericBP(bool useCompiledXaml)
+			{
+				var page = new Unreported006();
+				Assert.NotNull(page.GenericProperty);
+				Assert.That(page.GenericProperty, Is.TypeOf<StackLayout>());
+			}
+		}
+	}
+}
