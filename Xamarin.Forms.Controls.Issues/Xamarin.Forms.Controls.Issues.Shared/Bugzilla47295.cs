1ff0db9c6f4ba1167e6b2620066f83088f23c5bf
Stephane Delcroix <stephane@delcroix.org>
2016-12-01 22:39:43 +0100

    Jimmgarrido uwp toolbar (#588)
    * Fixed CommandBar being clipped when expanded by removing explicit Height
    
    * Add UITest

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla47295.cs
@@ -0,0 +1,39 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+    [Preserve(AllMembers = true)]
+    [Issue(IssueTracker.Bugzilla, 47295, "Toolbar in a NavigationPage is clipped when expanded", PlatformAffected.WinRT)]
+    public class Bugzilla47295 : TestNavigationPage
+    {
+        protected override void Init()
+        {
+            PushAsync(new TestPage47295());
+        }
+    }
+
+    [Preserve(AllMembers = true)]
+    public class TestPage47295 : ContentPage
+    {
+        public TestPage47295()
+        {
+            var descLabel = new Label { Text = "Press '...' to expand the CommandBar. The ToolbarItem's text label should not be clipped." };
+            var pressedLabel = new Label { Text = "ToolbarItem pressed.", IsVisible = false };
+            Content = new StackLayout
+            {
+                Children = {
+                    descLabel,
+                    pressedLabel
+                }
+            };
+
+            ToolbarItems.Add(new ToolbarItem("Test", "toolbar_close", () => pressedLabel.IsVisible = true));
+        }
+    }
+}
