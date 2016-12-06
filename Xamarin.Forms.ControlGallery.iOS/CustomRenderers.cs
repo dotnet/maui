3d090a33d4c1fb071ccb2ec822ef7fc94ec3f89a
E.Z. Hart <hartez@gmail.com>
2016-12-06 10:23:22 -0700

    Possible fix for race condition in 21177 UI test

--- Xamarin.Forms.ControlGallery.iOS/CustomRenderers.cs
+++ Xamarin.Forms.ControlGallery.iOS/CustomRenderers.cs
@@ -471,6 +471,8 @@ namespace Xamarin.Forms.ControlGallery.iOS
 			Element.InvokeItemSelected(indexPath.Row);
 		}
 
+		CollectionViewController _controller;
+
 		protected override void OnElementChanged(ElementChangedEventArgs<Bugzilla21177.CollectionView> e)
 		{
 			if (e.NewElement != null)
@@ -482,8 +484,8 @@ namespace Xamarin.Forms.ControlGallery.iOS
 					MinimumInteritemSpacing = 5, // minimum spacing between cells 
 					MinimumLineSpacing = 5 // minimum spacing between rows if ScrollDirection is Vertical or between columns if Horizontal 
 				};
-				var collectionView = new CollectionViewController(flowLayout, ItemSelected);
-				SetNativeControl(collectionView.CollectionView);
+				_controller = new CollectionViewController(flowLayout, ItemSelected);
+				SetNativeControl(_controller.CollectionView);
 			}
 
 			base.OnElementChanged(e);
