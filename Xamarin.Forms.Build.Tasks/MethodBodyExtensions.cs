354e27c09603143cef43d666be5beaed8928c040
Stephane Delcroix <stephane@delcroix.org>
2016-12-12 21:49:05 +0100

    [XamlC] Optimize Stloc/Ldloc (#614)

--- Xamarin.Forms.Build.Tasks/MethodBodyExtensions.cs
+++ Xamarin.Forms.Build.Tasks/MethodBodyExtensions.cs
@@ -12,6 +12,7 @@ namespace Xamarin.Forms.Build.Tasks
 				throw new ArgumentNullException(nameof(self));
 
 			self.OptimizeLongs();
+			self.OptimizeStLdLoc();
 			self.OptimizeMacros();
 		}
 
@@ -34,6 +35,25 @@ namespace Xamarin.Forms.Build.Tasks
 					continue;
 				ExpandMacro(instruction, OpCodes.Ldc_I4, unchecked((int)l));
 				self.Instructions.Insert(++i, Instruction.Create(OpCodes.Conv_I8));
+			}
+		}
+
+		static void OptimizeStLdLoc(this MethodBody self)
+		{
+			var method = self.Method;
+			for (var i = 0; i < self.Instructions.Count; i++) {
+				var instruction = self.Instructions[i];
+				if (instruction.OpCode.Code != Code.Stloc)
+					continue;
+				if (i + 1 >= self.Instructions.Count)
+					continue;
+				var next = self.Instructions[i + 1];
+				int num = ((VariableDefinition)instruction.Operand).Index;
+				var vardef = instruction.Operand;
+				if (next.OpCode.Code != Code.Ldloc || num != ((VariableDefinition)next.Operand).Index)
+					continue;
+				ExpandMacro(instruction, OpCodes.Dup, null);
+				ExpandMacro(next, OpCodes.Stloc, vardef);
 			}
 		}
 	}
