9d2290b2ac6753b037edfc9f0b1cfeaa69c5d815
Stephane Delcroix <stephane@delcroix.org>
2016-12-22 09:34:30 +0100

    Avoid duplicating code in OmPlatform (#591)

--- Xamarin.Forms.Core/OnPlatform.cs
+++ Xamarin.Forms.Core/OnPlatform.cs
@@ -3,29 +3,14 @@ namespace Xamarin.Forms
 	public class OnPlatform<T>
 	{
 		public T Android { get; set; }
-
 		public T iOS { get; set; }
-
 		public T WinPhone { get; set; }
 
 		public T Tizen { get; set; }
 
 		public static implicit operator T(OnPlatform<T> onPlatform)
 		{
-			switch (Device.OS)
-			{
-				case TargetPlatform.iOS:
-					return onPlatform.iOS;
-				case TargetPlatform.Android:
-					return onPlatform.Android;
-				case TargetPlatform.Windows:
-				case TargetPlatform.WinPhone:
-					return onPlatform.WinPhone;
-				case TargetPlatform.Tizen:
-					return onPlatform.Tizen;
-			}
-
-			return onPlatform.iOS;
+			return Device.OnPlatform(iOS: onPlatform.iOS, Android: onPlatform.Android, WinPhone: onPlatform.WinPhone, Tizen: onPlatform.Tizen);
 		}
 	}
 }
