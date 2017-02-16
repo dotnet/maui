1522c0e48d3feacfbaf68a0016e35e21d9c5940c
kingces95 <kingces95@users.noreply.github.com>
2017-02-16 15:01:58 -0500

    Robust doc change detection (#772)
    * Robust doc change detection
    
    * Update docs
    
    * Update all docs before checking for differences

--- update-docs-windows.bat
+++ update-docs-windows.bat
@@ -1,37 +1,36 @@
 @echo off
 PATH="C:\Program Files (x86)\Mono\bin";%PATH%
 
-echo "Updating Xamarin.Forms.Core"
-tools\mdoc\mdoc update --delete Xamarin.Forms.Core\bin\Debug\Xamarin.Forms.Core.dll -L "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETPortable\v4.5\Profile\Profile259" --out docs\Xamarin.Forms.Core | findstr /r "^Members.Added:..," > tmpFile
-set /p RESULT= < tmpFile
-echo "%RESULT%"
-del tmpFile
+IF EXIST docs.xml (erase docs.xml)
+for /r docs %%i in (*.xml) do type %%i >> docs.xml
 
-IF NOT "%RESULT%" == "Members Added: 0, Members Deleted: 0" (exit 1)
+echo "Updating Xamarin.Forms.Core"
+tools\mdoc\mdoc update --delete Xamarin.Forms.Core\bin\Debug\Xamarin.Forms.Core.dll -L "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETPortable\v4.5\Profile\Profile259" --out docs\Xamarin.Forms.Core > nul
+IF %ERRORLEVEL% NEQ 0 (goto fail)
 
 echo "Updating Xamarin.Forms.Xaml"
-tools\mdoc\mdoc update --delete Xamarin.Forms.Xaml\bin\Debug\Xamarin.Forms.Xaml.dll -L "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETPortable\v4.5\Profile\Profile259" --out docs\Xamarin.Forms.Xaml | findstr /r "^Members.Added:..," > tmpFile
-set /p RESULT= < tmpFile
-echo "%RESULT%"
-del tmpFile
-
-IF NOT "%RESULT%" == "Members Added: 0, Members Deleted: 0" (exit 1)
+tools\mdoc\mdoc update --delete Xamarin.Forms.Xaml\bin\Debug\Xamarin.Forms.Xaml.dll -L "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETPortable\v4.5\Profile\Profile259" --out docs\Xamarin.Forms.Xaml > nul
+IF %ERRORLEVEL% NEQ 0 (goto fail)
 
 echo "Updating Xamarin.Forms.Maps"
-tools\mdoc\mdoc update --delete Xamarin.Forms.Maps\bin\Debug\Xamarin.Forms.Maps.dll -L "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETPortable\v4.5\Profile\Profile259" --out docs\Xamarin.Forms.Maps | findstr /r "^Members.Added:..," > tmpFile
-set /p RESULT= < tmpFile
-echo "%RESULT%"
-del tmpFile
-
-IF NOT "%RESULT%" == "Members Added: 0, Members Deleted: 0" (exit 1)
+tools\mdoc\mdoc update --delete Xamarin.Forms.Maps\bin\Debug\Xamarin.Forms.Maps.dll -L "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETPortable\v4.5\Profile\Profile259" --out docs\Xamarin.Forms.Maps > nul
+IF %ERRORLEVEL% NEQ 0 (goto fail)
 
 echo "Updating Xamarin.Forms.Pages"
-tools\mdoc\mdoc update --delete Xamarin.Forms.Pages\bin\Debug\Xamarin.Forms.Pages.dll -L "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETPortable\v4.5\Profile\Profile259" --out docs\Xamarin.Forms.Pages | findstr /r "^Members.Added:..," > tmpFile
-set /p RESULT= < tmpFile
-echo "%RESULT%"
-del tmpFile
+tools\mdoc\mdoc update --delete Xamarin.Forms.Pages\bin\Debug\Xamarin.Forms.Pages.dll -L "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETPortable\v4.5\Profile\Profile259" --out docs\Xamarin.Forms.Pages > nul
+IF %ERRORLEVEL% NEQ 0 (goto fail)
 
-IF NOT "%RESULT%" == "Members Added: 0, Members Deleted: 0" (exit 1)
+IF EXIST _docs.xml (erase _docs.xml)
+for /r docs %%i in (*.xml) do type %%i >> _docs.xml
+fc docs.xml _docs.xml > nul 2> nul
+IF %ERRORLEVEL% NEQ 0 (goto fail)
 
+erase docs.xml _docs.xml
+echo No changes detected.
 exit /B 0
+
+:fail
+erase docs.xml _docs.xml
+echo Changes detected!
+exit /B 1
 
