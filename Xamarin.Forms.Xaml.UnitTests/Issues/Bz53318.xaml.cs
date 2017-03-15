c9589b6065791e79ae30c46589e706b4086cc7fb
Stephane Delcroix <stephane@delcroix.org>
2017-03-15 11:35:44 +0100

    [test] passing test for 53318 (#818)

---
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Bz53318.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Xamarin.Forms;
+using Xamarin.Forms.Core.UnitTests;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public class Bz53318ListView : ListView
+	{
+	}
+
+	[XamlCompilation(XamlCompilationOptions.Skip)]
+	public partial class Bz53318 : ContentPage
+	{
+		public Bz53318()
+		{
+			InitializeComponent();
+		}
+
+		[TestFixture]
+		public class Tests
+		{
+			[SetUp]
+			public void Setup()
+			{
+				Device.PlatformServices = new MockPlatformServices();
+			}
+
+			[TearDown]
+			public void TearDown()
+			{
+				Device.PlatformServices = null;
+			}
+
+			[Test]
+			public void DoesCompilesArgsInsideDataTemplate()
+			{
+				Assert.DoesNotThrow(()=>MockCompiler.Compile(typeof(Bz53318)));
+			}
+		}
+	}
+}
