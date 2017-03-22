8cc913794d1c58bb1961a676387016e6df31b81a
Stephane Delcroix <stephane@delcroix.org>
2017-03-22 17:09:41 +0100

    [C] Make sure all children are parented (#831)
    * unit test
    
    * [C] make sure all children are properly parented

---
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Bz53381App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public partial class Bz53381App : Application
+	{
+		public Bz53381App()
+		{
+			InitializeComponent();
+		}
+	}
+}
