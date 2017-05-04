5518d3fbafc6f3911f0b835a92e2cb5dfb925593
Rui Marinho <me@ruimarinho.net>
2017-05-04 15:27:30 +0100

    Fix bugzilla55542 (#898)
    * [Controls] Add repo for bugzilla 55542
    
    * [Core] Fix caching null renderer for ViewCell view

--- Xamarin.Forms.Controls/App.cs
+++ Xamarin.Forms.Controls/App.cs
@@ -50,7 +50,7 @@ namespace Xamarin.Forms.Controls
 			return new MasterDetailPage
 			{
 				AutomationId = DefaultMainPageId,
-				Master = new ContentPage { Title = "Master", BackgroundColor = Color.Red },
+				Master = new ContentPage { Title = "Master", Content = new View { BackgroundColor = Color.Red } },
 				Detail = CoreGallery.GetMainPage()
 			};
 		}
