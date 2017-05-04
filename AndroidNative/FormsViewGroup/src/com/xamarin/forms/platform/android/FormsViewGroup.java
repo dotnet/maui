40dff0a54cd718747f280b808dc08aa7efb83f4c
E.Z. Hart <hartez@gmail.com>
2017-05-03 20:52:41 -0600

    Correct version of FormsViewGroup.java

--- AndroidNative/FormsViewGroup/src/com/xamarin/forms/platform/android/FormsViewGroup.java
+++ AndroidNative/FormsViewGroup/src/com/xamarin/forms/platform/android/FormsViewGroup.java
@@ -1,106 +1,18 @@
 package com.xamarin.forms.platform.android;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.ViewGroup;
 import android.view.*;
+import android.widget.TextView;
 
-
-public class FormsViewGroup extends ViewGroup {
-
-	public FormsViewGroup(Context context) {
-		super(context);
-		// TODO Auto-generated constructor stub
-	}
-
-	public FormsViewGroup(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		// TODO Auto-generated constructor stub
-	}
-
-	public FormsViewGroup(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
-	}
-
-	public void measureAndLayout (int widthMeasureSpec, int heightMeasureSpec, int l, int t, int r, int b)
-	{
-		measure (widthMeasureSpec, heightMeasureSpec);
-		layout (l, t, r, b);
-	}
-	
-	@Override
-	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-	}
-
-	boolean inputTransparent;
-	
-	protected void setInputTransparent (boolean value)
-	{
-		inputTransparent = value;
-	}
-	
-	protected boolean getInputTransparent ()
-	{
-		return inputTransparent;
-	}
-	
-	@Override
-	public boolean onInterceptTouchEvent (MotionEvent ev)
-	{
-		if (inputTransparent)
-			return false;
-		
-		return super.onInterceptTouchEvent(ev);
-	}
-	
-	@Override
-	public boolean onTouchEvent (MotionEvent ev)
-	{
-		if (inputTransparent)
-			return false;
-		
-		return super.onTouchEvent(ev);
-	}
-	
-	public void sendBatchUpdate (
-			float pivotX,
-			float pivotY,
-			int visibility,
-			boolean enabled,
-			float opacity,
-			float rotation, 
-			float rotationX, 
-			float rotationY,
-			float scale,
-			float translationX,
-			float translationY){
-		setPivotX (pivotX);
-		setPivotY (pivotY);
-		
-		if (getVisibility () != visibility)
-			setVisibility (visibility);
-		
-		if (isEnabled () != enabled)
-			setEnabled (enabled);
-		
-		setAlpha (opacity);
-		setRotation (rotation);
-		setRotationX (rotationX);
-		setRotationY (rotationY);
-		setScaleX (scale);
-		setScaleY (scale);
-		setTranslationX (translationX);
-		setTranslationY (translationY);
-	}
-	
-	package com.xamarin.forms.platform.android;
-
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.ViewGroup;
-import android.view.*;
-
+import static android.R.attr.pivotX;
 
 public class FormsViewGroup extends ViewGroup {
 
@@ -221,6 +133,4 @@ public class FormsViewGroup extends ViewGroup {
 		view.setTranslationX (translationX);
 		view.setTranslationY (translationY);
 	}
-}
-
 }
