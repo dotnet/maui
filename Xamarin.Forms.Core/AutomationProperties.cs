a0801dc345a4cc67cf44004b263eac46b8b89c72
Stephane Delcroix <stephane@delcroix.org>
2017-05-10 20:07:37 +0200

    [C] fix BP declaring Types for A11y (#913)

--- Xamarin.Forms.Core/AutomationProperties.cs
+++ Xamarin.Forms.Core/AutomationProperties.cs
@@ -1,15 +1,14 @@
-﻿﻿
-namespace Xamarin.Forms
+﻿namespace Xamarin.Forms
 {
 	public class AutomationProperties
 	{
-		public static readonly BindableProperty HelpTextProperty = BindableProperty.Create("HelpText", typeof(string), typeof(Element), default(string));
+		public static readonly BindableProperty HelpTextProperty = BindableProperty.Create("HelpText", typeof(string), typeof(AutomationProperties), default(string));
 
-		public static readonly BindableProperty IsInAccessibleTreeProperty = BindableProperty.Create("IsInAccessibleTree", typeof(bool?), typeof(Element), null);
+		public static readonly BindableProperty IsInAccessibleTreeProperty = BindableProperty.Create("IsInAccessibleTree", typeof(bool?), typeof(AutomationProperties), null);
 
-		public static readonly BindableProperty LabeledByProperty = BindableProperty.Create("LabeledBy", typeof(VisualElement), typeof(Element), default(VisualElement));
+		public static readonly BindableProperty LabeledByProperty = BindableProperty.Create("LabeledBy", typeof(VisualElement), typeof(AutomationProperties), default(VisualElement));
 
-		public static readonly BindableProperty NameProperty = BindableProperty.Create("Name", typeof(string), typeof(Element), default(string));
+		public static readonly BindableProperty NameProperty = BindableProperty.Create("Name", typeof(string), typeof(AutomationProperties), default(string));
 
 		public static string GetHelpText(BindableObject bindable)
 		{
