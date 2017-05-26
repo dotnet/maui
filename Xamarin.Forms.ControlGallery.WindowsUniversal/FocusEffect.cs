a9a6a40c67bf52012f46a2809dd98ad0571b48b9
Paul DiPietro <pauldipietro@users.noreply.github.com>
2017-05-26 06:09:00 -0500

    [UWP] Respect TextBox BackgroundFocusBrush Effect (#928)

---
+++ Xamarin.Forms.ControlGallery.WindowsUniversal/FocusEffect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.ControlGallery.WindowsUniversal;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(FocusEffect), "FocusEffect")]
+namespace Xamarin.Forms.ControlGallery.WindowsUniversal
+{
+	public class FocusEffect : PlatformEffect
+	{
+		protected override void OnAttached()
+		{
+			try
+			{
+				(Control as Windows.UI.Xaml.Controls.Control).Background = new SolidColorBrush(Colors.Cyan);
+				(Control as FormsTextBox).BackgroundFocusBrush = new SolidColorBrush(Colors.White);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+			}
+		}
+
+		protected override void OnDetached()
+		{
+		}
+	}
+}
