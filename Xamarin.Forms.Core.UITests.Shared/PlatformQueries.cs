31e17cc1e1122f2327e97095d2eccf1f65f6f7b6
E.Z. Hart <hartez@gmail.com>
2017-08-08 16:34:32 -0600

    Fix ToolBarItemTests for pre-AppCompat

--- Xamarin.Forms.Core.UITests.Shared/PlatformQueries.cs
+++ Xamarin.Forms.Core.UITests.Shared/PlatformQueries.cs
@@ -113,7 +113,7 @@ namespace Xamarin.Forms.Core.UITests
 		public static readonly Func<AppQuery, AppQuery> RootPageListView = q => q.Raw("ListViewRenderer index:0");
 		public static readonly Func<AppQuery, AppQuery> GalleryListView = q => q.Raw("ListViewRenderer index:1");
 		public static readonly Func<AppQuery, AppQuery> PageWithoutNavigationBar = q => q.Raw("* id:'content' index:0");
-		//android.support.v7.widget.
+		
 		public static readonly Func<AppQuery, AppQuery> NavigationBarBackButton =
 			q => q.Class("Toolbar").Child("android.widget.ImageButton");
 #endif
