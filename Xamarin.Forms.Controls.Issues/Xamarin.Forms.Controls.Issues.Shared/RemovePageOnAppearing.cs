a628c46d4549fb50783efe07c0f697f0048b4745
E.Z. Hart <hartez@users.noreply.github.com>
2017-08-14 20:17:21 -0400

    Avoid exception when removing a page from navigation during Appearing handler (#1094)
    * Theoretical repro
    
    * Repro
    
    * Remove unnecessary forced execution of pending transactions
    
    * Update issue data

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/RemovePageOnAppearing.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.Navigation)]
+#endif
+
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.None, 1134, "Removing page during OnAppearing throws exception", PlatformAffected.Android)]
+	public class RemovePageOnAppearing : TestContentPage
+	{
+		const string Success = "Success";
+
+		protected override void Init()
+		{
+			Appearing += async (sender, args) =>
+			{
+				var nav = new NavigationPage(Root());
+				Application.Current.MainPage = nav;
+				await nav.PushAsync(Intermediate());
+				await nav.PushAsync(new PageWhichRemovesAnEarlierPageOnAppearing());	
+			};
+		}
+
+		static ContentPage Root()
+		{
+			return new ContentPage { Content = new Label {Text = "Root"} };
+		}
+
+		static ContentPage Intermediate()
+		{
+			return new ContentPage { Content = new Label {Text = "Intermediate page"} };
+		}
+
+		[Preserve(AllMembers = true)]
+		class PageWhichRemovesAnEarlierPageOnAppearing : ContentPage
+		{
+			public PageWhichRemovesAnEarlierPageOnAppearing()
+			{
+				var instructions = new Label { Text = "If you can see this, the test has passed" };
+
+				Content = new StackLayout { Children = { instructions, new Label { Text = Success } } };
+			}
+
+			protected override void OnAppearing()
+			{
+				var toRemove = Navigation.NavigationStack.Skip(1).First();
+
+				// toRemove should be the IntermediatePage
+				Navigation.RemovePage(toRemove);
+
+				base.OnAppearing();
+			}
+		}
+
+#if UITEST
+		[Test]
+		public void RemovePageOnAppearingDoesNotCrash()
+		{
+			RunningApp.WaitForElement(Success);
+		}
+#endif
+	}
+}
