cc2129395b14a10747b9942582119ca219cc4666
Stephane Delcroix <stephane@delcroix.org>
2017-09-07 09:06:03 +0200

    Better XF .targets errors reporting (#1125)
    * [XamlC] Better XF .targets errors reporting
    
     - throw build errors if multiple xf .targets are imported, or if the
      .targets doesn't match the Tasks.dll ABI. That validation is skipped
      when building in CI, as the build task is not yet present at
      validation time.
     - reuse the main .targets for UnitTest builds, avoid duplication
     - parameterize the .targets
    
    * define defaults before import

---
+++ Xamarin.Forms.Build.Tasks/GetTasksAbi.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.Forms.Build.Tasks
+{
+	public class GetTasksAbi : Task
+	{
+		[Output]
+		public string AbiVersion { get; } = "1";
+
+		public override bool Execute()
+			=> true;
+	}
+}
