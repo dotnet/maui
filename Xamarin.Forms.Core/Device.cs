82325b539eec7628991817a5943e04c7269eda70
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-09-29 19:31:09 +0200

    Changed native cell controls to become internal classes.

--- Xamarin.Forms.Core/Device.cs
+++ Xamarin.Forms.Core/Device.cs
@@ -54,162 +54,153 @@ namespace Xamarin.Forms
         }
 #pragma warning restore 0618
 
-		public static string RuntimePlatform => PlatformServices.RuntimePlatform;
+        public static string RuntimePlatform => PlatformServices.RuntimePlatform;
 
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public static DeviceInfo Info
-		{
-			get
-			{
-				if (info == null)
-					throw new InvalidOperationException("You MUST call Xamarin.Forms.Init(); prior to using it.");
-				return info;
-			}
-			set { info = value; }
-		}
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static DeviceInfo Info
+        {
+            get
+            {
+                if (info == null)
+                    throw new InvalidOperationException("You MUST call Xamarin.Forms.Init(); prior to using it.");
+                return info;
+            }
+            set { info = value; }
+        }
 
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public static bool IsInvokeRequired
-		{
-			get { return PlatformServices.IsInvokeRequired; }
-		}
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static bool IsInvokeRequired
+        {
+            get { return PlatformServices.IsInvokeRequired; }
+        }
 
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public static IPlatformServices PlatformServices
-		{
-			get
-			{
-				if (s_platformServices == null)
-					throw new InvalidOperationException("You MUST call Xamarin.Forms.Init(); prior to using it.");
-				return s_platformServices;
-			}
-			set { s_platformServices = value; }
-		}
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static IPlatformServices PlatformServices
+        {
+            get
+            {
+                if (s_platformServices == null)
+                    throw new InvalidOperationException("You MUST call Xamarin.Forms.Init(); prior to using it.");
+                return s_platformServices;
+            }
+            set { s_platformServices = value; }
+        }
 
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public static IReadOnlyList<string> Flags { get; private set; }
+        public static void BeginInvokeOnMainThread(Action action)
+        {
+            PlatformServices.BeginInvokeOnMainThread(action);
+        }
 
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public static void SetFlags(IReadOnlyList<string> flags)
-		{
-			Flags = flags;
-		}
+        public static double GetNamedSize(NamedSize size, Element targetElement)
+        {
+            return GetNamedSize(size, targetElement.GetType());
+        }
 
-		public static void BeginInvokeOnMainThread(Action action)
-		{
-			PlatformServices.BeginInvokeOnMainThread(action);
-		}
+        public static double GetNamedSize(NamedSize size, Type targetElementType)
+        {
+            return GetNamedSize(size, targetElementType, false);
+        }
 
-		public static double GetNamedSize(NamedSize size, Element targetElement)
-		{
-			return GetNamedSize(size, targetElement.GetType());
-		}
+        [Obsolete("OnPlatform is obsolete as of version 2.3.4. Please use switch(RuntimePlatform) instead.")]
+        public static void OnPlatform(Action iOS = null, Action Android = null, Action WinPhone = null, Action Default = null)
+        {
+            switch (OS)
+            {
+                case TargetPlatform.iOS:
+                    if (iOS != null)
+                        iOS();
+                    else if (Default != null)
+                        Default();
+                    break;
+                case TargetPlatform.Android:
+                    if (Android != null)
+                        Android();
+                    else if (Default != null)
+                        Default();
+                    break;
+                case TargetPlatform.Windows:
+                case TargetPlatform.WinPhone:
+                    if (WinPhone != null)
+                        WinPhone();
+                    else if (Default != null)
+                        Default();
+                    break;
+                case TargetPlatform.Other:
+                    if (Default != null)
+                        Default();
+                    break;
+            }
+        }
 
-		public static double GetNamedSize(NamedSize size, Type targetElementType)
-		{
-			return GetNamedSize(size, targetElementType, false);
-		}
+        [Obsolete("OnPlatform<> (generic) is obsolete as of version 2.3.4. Please use switch(RuntimePlatform) instead.")]
+        public static T OnPlatform<T>(T iOS, T Android, T WinPhone)
+        {
+            switch (OS)
+            {
+                case TargetPlatform.iOS:
+                    return iOS;
+                case TargetPlatform.Android:
+                    return Android;
+                case TargetPlatform.Windows:
+                case TargetPlatform.WinPhone:
+                    return WinPhone;
+            }
 
-		[Obsolete("OnPlatform is obsolete as of version 2.3.4. Please use switch(RuntimePlatform) instead.")]
-		public static void OnPlatform(Action iOS = null, Action Android = null, Action WinPhone = null, Action Default = null)
-		{
-			switch (OS)
-			{
-				case TargetPlatform.iOS:
-					if (iOS != null)
-						iOS();
-					else if (Default != null)
-						Default();
-					break;
-				case TargetPlatform.Android:
-					if (Android != null)
-						Android();
-					else if (Default != null)
-						Default();
-					break;
-				case TargetPlatform.Windows:
-				case TargetPlatform.WinPhone:
-					if (WinPhone != null)
-						WinPhone();
-					else if (Default != null)
-						Default();
-					break;
-				case TargetPlatform.Other:
-					if (Default != null)
-						Default();
-					break;
-			}
-		}
+            return iOS;
+        }
 
-		[Obsolete("OnPlatform<> (generic) is obsolete as of version 2.3.4. Please use switch(RuntimePlatform) instead.")]
-		public static T OnPlatform<T>(T iOS, T Android, T WinPhone)
-		{
-			switch (OS)
-			{
-				case TargetPlatform.iOS:
-					return iOS;
-				case TargetPlatform.Android:
-					return Android;
-				case TargetPlatform.Windows:
-				case TargetPlatform.WinPhone:
-					return WinPhone;
-			}
+        public static void OpenUri(Uri uri)
+        {
+            PlatformServices.OpenUriAction(uri);
+        }
 
-			return iOS;
-		}
+        public static void StartTimer(TimeSpan interval, Func<bool> callback)
+        {
+            PlatformServices.StartTimer(interval, callback);
+        }
 
-		public static void OpenUri(Uri uri)
-		{
-			PlatformServices.OpenUriAction(uri);
-		}
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static Assembly[] GetAssemblies()
+        {
+            return PlatformServices.GetAssemblies();
+        }
 
-		public static void StartTimer(TimeSpan interval, Func<bool> callback)
-		{
-			PlatformServices.StartTimer(interval, callback);
-		}
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static double GetNamedSize(NamedSize size, Type targetElementType, bool useOldSizes)
+        {
+            return PlatformServices.GetNamedSize(size, targetElementType, useOldSizes);
+        }
 
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public static Assembly[] GetAssemblies()
-		{
-			return PlatformServices.GetAssemblies();
-		}
+        internal static Task<Stream> GetStreamAsync(Uri uri, CancellationToken cancellationToken)
+        {
+            return PlatformServices.GetStreamAsync(uri, cancellationToken);
+        }
 
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		public static double GetNamedSize(NamedSize size, Type targetElementType, bool useOldSizes)
-		{
-			return PlatformServices.GetNamedSize(size, targetElementType, useOldSizes);
-		}
+        public static class Styles
+        {
+            public static readonly string TitleStyleKey = "TitleStyle";
 
-		internal static Task<Stream> GetStreamAsync(Uri uri, CancellationToken cancellationToken)
-		{
-			return PlatformServices.GetStreamAsync(uri, cancellationToken);
-		}
+            public static readonly string SubtitleStyleKey = "SubtitleStyle";
 
-		public static class Styles
-		{
-			public static readonly string TitleStyleKey = "TitleStyle";
+            public static readonly string BodyStyleKey = "BodyStyle";
 
-			public static readonly string SubtitleStyleKey = "SubtitleStyle";
+            public static readonly string ListItemTextStyleKey = "ListItemTextStyle";
 
-			public static readonly string BodyStyleKey = "BodyStyle";
+            public static readonly string ListItemDetailTextStyleKey = "ListItemDetailTextStyle";
 
-			public static readonly string ListItemTextStyleKey = "ListItemTextStyle";
+            public static readonly string CaptionStyleKey = "CaptionStyle";
 
-			public static readonly string ListItemDetailTextStyleKey = "ListItemDetailTextStyle";
+            public static readonly Style TitleStyle = new Style(typeof(Label)) { BaseResourceKey = TitleStyleKey };
 
-			public static readonly string CaptionStyleKey = "CaptionStyle";
+            public static readonly Style SubtitleStyle = new Style(typeof(Label)) { BaseResourceKey = SubtitleStyleKey };
 
-			public static readonly Style TitleStyle = new Style(typeof(Label)) { BaseResourceKey = TitleStyleKey };
+            public static readonly Style BodyStyle = new Style(typeof(Label)) { BaseResourceKey = BodyStyleKey };
 
-			public static readonly Style SubtitleStyle = new Style(typeof(Label)) { BaseResourceKey = SubtitleStyleKey };
+            public static readonly Style ListItemTextStyle = new Style(typeof(Label)) { BaseResourceKey = ListItemTextStyleKey };
 
-			public static readonly Style BodyStyle = new Style(typeof(Label)) { BaseResourceKey = BodyStyleKey };
+            public static readonly Style ListItemDetailTextStyle = new Style(typeof(Label)) { BaseResourceKey = ListItemDetailTextStyleKey };
 
-			public static readonly Style ListItemTextStyle = new Style(typeof(Label)) { BaseResourceKey = ListItemTextStyleKey };
-
-			public static readonly Style ListItemDetailTextStyle = new Style(typeof(Label)) { BaseResourceKey = ListItemDetailTextStyleKey };
-
-			public static readonly Style CaptionStyle = new Style(typeof(Label)) { BaseResourceKey = CaptionStyleKey };
-		}
-	}
+            public static readonly Style CaptionStyle = new Style(typeof(Label)) { BaseResourceKey = CaptionStyleKey };
+        }
+    }
 }
