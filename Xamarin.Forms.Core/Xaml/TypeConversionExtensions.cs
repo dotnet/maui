2a5074f7ad74a57fd0b2abc0c4e6d0d5481e1d97
Stephane Delcroix <stephane@delcroix.org>
2017-10-18 09:15:11 +0200

    [Xaml] Chain op_implicit for OnPlatform (if needed) (#1176)
    * [Xaml] Chain op_implicit for OnPlatform (if needed)
    
    For OnPlatform<T>, if a conversion for T to the destination type exists,
    first convert OnPlatform<T> to T, the T to the dest type
    
    Also unify the way we look for op_implicit operators
    
    * [Xaml] only apply the double opImplicit if a flag is set
    
    * [C] allow conversion on parameter

--- Xamarin.Forms.Core/Xaml/TypeConversionExtensions.cs
+++ Xamarin.Forms.Core/Xaml/TypeConversionExtensions.cs
@@ -173,31 +173,9 @@ namespace Xamarin.Forms.Xaml
 
 			//if there's an implicit conversion, convert
 			if (value != null) {
-				MethodInfo opImplicit = null;
-				foreach (var mi in value.GetType().GetRuntimeMethods()) {
-					if (!mi.IsSpecialName) continue;
-					if (mi.Name != "op_Implicit") continue;
-					if (!mi.IsPublic) continue;
-					if (!toType.IsAssignableFrom(mi.ReturnType)) continue;
-					var parameters = mi.GetParameters();
-					if (parameters.Length != 1) continue;
-					if (parameters[0].ParameterType != value.GetType()) continue;
-					opImplicit = mi;
-					break;
-				}
-				if (opImplicit == null) {
-					foreach (var mi in toType.GetRuntimeMethods()) {
-						if (!mi.IsSpecialName) continue;
-						if (mi.Name != "op_Implicit") continue;
-						if (!mi.IsPublic) continue;
-						if (!toType.IsAssignableFrom(mi.ReturnType)) continue;
-						var parameters = mi.GetParameters();
-						if (parameters.Length != 1) continue;
-						if (parameters[0].ParameterType != value.GetType()) continue;
-						opImplicit = mi;
-						break;
-					}
-				}
+				var opImplicit =   value.GetType().GetImplicitConversionOperator(fromType: value.GetType(), toType: toType)
+								?? toType.GetImplicitConversionOperator(fromType: value.GetType(), toType: toType);
+
 				if (opImplicit != null) {
 					value = opImplicit.Invoke(null, new[] { value });
 					return value;
@@ -211,6 +189,18 @@ namespace Xamarin.Forms.Xaml
 				return nativeValue;
 
 			return value;
+		}
+
+		internal static MethodInfo GetImplicitConversionOperator(this Type onType, Type fromType, Type toType)
+		{
+			var mi = onType.GetRuntimeMethod("op_Implicit", new[] { fromType });
+			if (mi == null) return null;
+			if (!mi.IsSpecialName) return null;
+			if (!mi.IsPublic) return null;
+			if (!mi.IsStatic) return null;
+			if (!toType.IsAssignableFrom(mi.ReturnType)) return null;
+
+			return mi;
 		}
 	}
 }
