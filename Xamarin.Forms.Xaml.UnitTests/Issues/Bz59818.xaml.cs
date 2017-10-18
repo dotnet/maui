59d9171a51e5554649720702e4baab603df5601a
Stephane Delcroix <stephane@delcroix.org>
2017-10-18 09:15:11 +0200

    [Xaml] Chain op_implicit for OnPlatform (if needed) (#1176)
    * [Xaml] Chain op_implicit for OnPlatform (if needed)
    
    For OnPlatform<T>, if a conversion for T to the destination type exists,
    first convert OnPlatform<T> to T, the T to the dest type
    
    Also unify the way we look for op_implicit operators
    
    * [Xaml] only apply the double opImplicit if a flag is set
    
    * [C] allow conversion on parameter

---
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Bz59818.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UnitTests;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public partial class Bz59818 : ContentPage
+	{
+		public Bz59818()
+		{
+			InitializeComponent();
+		}
+
+		public Bz59818(bool useCompiledXaml)
+		{
+			//this stub will be replaced at compile time
+		}
+
+		[TestFixture]
+		class Tests
+		{
+			IReadOnlyList<string> _flags;
+
+			[SetUp]
+			public void Setup()
+			{
+				Device.PlatformServices = new MockPlatformServices();
+				_flags = Device.Flags;
+				if (Device.Flags == null)
+					Device.SetFlags(new List<string>().AsReadOnly());
+			}
+
+			[TearDown]
+			public void TearDown()
+			{
+				Device.PlatformServices = null;
+				Device.SetFlags(_flags);
+			}
+
+			[TestCase(true, "xamlDoubleImplicitOpHack")]
+			[TestCase(false, "xamlDoubleImplicitOpHack")]
+			[TestCase(true, null)]
+			[TestCase(false, null)]
+			public void Bz59818(bool useCompiledXaml, string flag)
+			{
+				Device.SetFlags(new List<string>(Device.Flags) {
+					flag
+				}.AsReadOnly());
+
+				((MockPlatformServices)Device.PlatformServices).RuntimePlatform = Device.iOS;
+
+				if (flag != "xamlDoubleImplicitOpHack") {
+					if (useCompiledXaml)
+						Assert.Throws<InvalidCastException>(() => new Bz59818(useCompiledXaml));
+					else
+						Assert.Throws<XamlParseException>(() => new Bz59818(useCompiledXaml));
+					return;
+				}
+				var layout = new Bz59818(useCompiledXaml);
+				Assert.That(layout.grid.ColumnDefinitions[0].Width, Is.EqualTo(new GridLength(100)));
+			}
+		}
+	}
+}
