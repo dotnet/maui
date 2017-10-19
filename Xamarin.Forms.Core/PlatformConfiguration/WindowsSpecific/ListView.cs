a327f471a13f3f39d1805e97411cb13248657a21
Samantha Houts <samhouts@users.noreply.github.com>
2017-10-19 15:17:31 -0700

    [UWP] Fix ListView selection issues **Behavior change** (#1157)
    * Update 44886 repro
    
    * Add 59718 repro
    
    * [UWP] Don't fire a click when SelectedItem changes programmatically
    
    * Update repro for 59718
    
    * [Core] Add Win ListView Selection mode specific
    
    * [Win] Use ListView SelectionMode specific
    
    * Fix test case
    
    * [Win] ListView sets value once when clicked
    
    * Update docs
    
    * Fix specific methods
    
    * Update docs again

---
+++ Xamarin.Forms.Core/PlatformConfiguration/WindowsSpecific/ListView.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Xamarin.Forms.PlatformConfiguration.WindowsSpecific
+{
+	using FormsElement = Forms.ListView;
+
+	public static class ListView
+	{
+		#region SelectionMode
+
+		public static readonly BindableProperty SelectionModeProperty =
+			BindableProperty.CreateAttached("SelectionMode", typeof(ListViewSelectionMode),
+				typeof(ListView), ListViewSelectionMode.Accessible);
+
+		public static ListViewSelectionMode GetSelectionMode(BindableObject element)
+		{
+			return (ListViewSelectionMode)element.GetValue(SelectionModeProperty);
+		}
+
+		public static void SetSelectionMode(BindableObject element, ListViewSelectionMode value)
+		{
+			element.SetValue(SelectionModeProperty, value);
+		}
+
+		public static ListViewSelectionMode GetSelectionMode(this IPlatformElementConfiguration<Windows, FormsElement> config)
+		{
+			return (ListViewSelectionMode)config.Element.GetValue(SelectionModeProperty);
+		}
+
+		public static IPlatformElementConfiguration<Windows, FormsElement> SetSelectionMode(
+			this IPlatformElementConfiguration<Windows, FormsElement> config, ListViewSelectionMode value)
+		{
+			config.Element.SetValue(SelectionModeProperty, value);
+			return config;
+		}
+
+		#endregion
+	}
+
+	public enum ListViewSelectionMode
+	{
+		/// <summary>
+		/// Allows ListItems to have TapGestures. The Enter key and Narrator will not fire the ItemTapped event.
+		/// </summary>
+		Inaccessible,
+		/// <summary>
+		/// Allows the Enter key and Narrator to fire the ItemTapped event. ListItems cannot have TapGestures.
+		/// </summary>
+		Accessible
+	}
+}
