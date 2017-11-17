a26b2102f25b5c449e8e6bf0b5b19717f30543ac
E.Z. Hart <hartez@gmail.com>
2017-11-17 14:52:22 -0700

    Make test 60122 actually test for success

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla60122.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla60122.cs
@@ -49,14 +49,17 @@ namespace Xamarin.Forms.Controls.Issues
 			}
 		}
 
-#if UITEST
+#if UITEST && !__WINDOWS__ 
+
+		// This test won't work on Windows right now because we can only test desktop, so touch events
+		// (like LongPress) don't really work. The test should work manually on a touch screen, though.
+
 		[Test]
 		public void LongClickFiresOnCustomImageRenderer ()
 		{
 			RunningApp.WaitForElement (ImageId);
 			RunningApp.TouchAndHold(ImageId);
-
-			// ???
+			RunningApp.WaitForElement (Success);
 		}
 #endif
 	}
