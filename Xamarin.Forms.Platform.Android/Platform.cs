4591700b1fc42aaec42f968a02834c39a45bdf90
E.Z. Hart <hartez@users.noreply.github.com>
2017-11-23 16:02:56 -0700

    Create renderer with context (#1303)
    * Add new public method CreateRendererWithContext
    
    * Explanatory comment

--- Xamarin.Forms.Platform.Android/Platform.cs
+++ Xamarin.Forms.Platform.Android/Platform.cs
@@ -302,7 +302,7 @@ namespace Xamarin.Forms.Platform.Android
 			throw new InvalidOperationException("RemovePage is not supported globally on Android, please use a NavigationPage.");
 		}
 
-		[Obsolete("CreateRenderer(VisualElement) is obsolete as of version 2.5. Please use CreateRenderer(VisualElement, Context) instead.")]
+		[Obsolete("CreateRenderer(VisualElement) is obsolete as of version 2.5. Please use CreateRendererWithContext(VisualElement, Context) instead.")]
 		public static IVisualElementRenderer CreateRenderer(VisualElement element)
 		{
 			return CreateRenderer(element, Forms.Context);
@@ -315,6 +315,13 @@ namespace Xamarin.Forms.Platform.Android
 			renderer.SetElement(element);
 
 			return renderer;
+		}
+
+		public static IVisualElementRenderer CreateRendererWithContext(VisualElement element, Context context)
+		{
+			// This is an interim method to allow public access to CreateRenderer(element, context), which we 
+			// can't make public yet because it will break the previewer
+			return CreateRenderer(element, context);
 		}
 
 		public static IVisualElementRenderer GetRenderer(VisualElement bindable)
