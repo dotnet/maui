9ebd062f847153f87b6b5e8e7a4e5d02e5bc8155
kingces95 <kingces95@users.noreply.github.com>
2017-11-24 02:46:51 -1000

    Fix 59896; Add empty listView group twice (#1275)

--- Xamarin.Forms.Platform.iOS/Renderers/ListViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/ListViewRenderer.cs
@@ -666,13 +666,16 @@ namespace Xamarin.Forms.Platform.iOS
 				}
 
 				// We're going to base our estimate off of the first cell
-				Cell firstCell;
 				var isGroupingEnabled = List.IsGroupingEnabled;
 
 				if (isGroupingEnabled)
-					firstCell = templatedItems.ActivateContent(0, templatedItems.GetGroup(0)?.ListProxy[0]);
-				else
-					firstCell = templatedItems.ActivateContent(0, templatedItems.ListProxy[0]);
+					templatedItems = templatedItems.GetGroup(0);
+
+				object item = null;
+				if (templatedItems == null || templatedItems.ListProxy.TryGetValue(0, out item) == false)
+					return DefaultRowHeight;
+
+				var firstCell = templatedItems.ActivateContent(0, item);
 
 				// Let's skip this optimization for grouped lists. It will likely cause more trouble than it's worth.
 				if (firstCell?.Height > 0 && !isGroupingEnabled)
