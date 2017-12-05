874677f9b9f1f457389f742a133ba0386a838a03
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-12-05 13:10:42 +0100

    Added Gtk Backend Core Gallery project (#1293)
    * Added Gtk backend Core Gallery Sample
    
    * Updated GTK projects to use Net Framework 4.7 and change the gtk-sharp dependencies path

---
+++ Xamarin.Forms.ControlGallery.GTK/BasicOpenGLView.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL;
+
+namespace Xamarin.Forms.ControlGallery.GTK
+{
+    public class BasicOpenGLApp : Application
+    {
+        public BasicOpenGLApp()
+        {
+            MainPage = new BasicOpenGLView();
+        }
+    }
+
+    public class BasicOpenGLView : ContentPage
+    {
+        private bool _initGl = false;
+        private int _viewportWidth;
+        private int _viewportHeight;
+        private uint _mProgramHandle;
+        private OpenGLView _openGLView = null;
+
+        public BasicOpenGLView()
+        {
+            Title = "Basic OpenGLView Sample";
+
+            var titleLabel = new Label
+            {
+                Text = "OpenGLView",
+                FontSize = 36
+            };
+
+            _openGLView = new OpenGLView
+            {
+                HeightRequest = 300,
+                WidthRequest = 300,
+                HasRenderLoop = true
+            };
+
+            _openGLView.OnDisplay = r =>
+            {
+                if (!_initGl)
+                {
+                    double width_in_pixels = 300;
+                    double height_in_pixels = 300;
+
+                    InitGl((int)width_in_pixels, (int)height_in_pixels);
+                }
+
+                Render();
+            };
+
+            var stack = new StackLayout
+            {
+                Padding = new Size(12, 12),
+                Children = { titleLabel, _openGLView }
+            };
+
+            Content = stack;
+        }
+
+        void InitGl(int width, int height)
+        {
+            _viewportHeight = width;
+            _viewportWidth = height;
+
+            _mProgramHandle = (uint)GL.CreateProgram();
+            if (_mProgramHandle == 0)
+                throw new InvalidOperationException("Unable to create program");
+
+            GL.BindAttribLocation(_mProgramHandle, 0, "vPosition");
+            GL.LinkProgram(_mProgramHandle);
+
+            GL.Viewport(0, 0, _viewportWidth, _viewportHeight);
+
+            GL.UseProgram(_mProgramHandle);
+
+            _initGl = true;
+        }
+
+
+        void Render()
+        {
+            GL.ClearColor(0, 0, 0, 0);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Color3(1.0f, 0.85f, 0.35f);
+            GL.Begin(BeginMode.Triangles);
+
+            GL.Vertex3(0.0, 0.6, 0.0);
+            GL.Vertex3(-0.2, -0.3, 0.0);
+            GL.Vertex3(0.2, -0.3, 0.0);
+
+            GL.End();
+
+            GL.Flush();
+        }
+    }
+}
---
+++ Xamarin.Forms.ControlGallery.GTK/Program.cs
@@ -0,0 +1,66 @@
+﻿using GLib;
+using Xamarin.Forms;
+using Xamarin.Forms.ControlGallery.GTK;
+using System;
+using Xamarin.Forms.Platform.GTK;
+using Xamarin.Forms.Platform.GTK.Renderers;
+using Xamarin.Forms.Controls;
+
+[assembly: ExportRenderer(typeof(DisposePage), typeof(DisposePageRenderer))]
+[assembly: ExportRenderer(typeof(DisposeLabel), typeof(DisposeLabelRenderer))]
+namespace Xamarin.Forms.ControlGallery.GTK
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ExceptionManager.UnhandledException += OnUnhandledException;
+
+            GtkOpenGL.Init();
+            GtkThemes.Init();
+            Gtk.Application.Init();
+            Forms.Init();
+            var app = new App();
+            //var app = new BasicOpenGLApp();
+            //var app = new AdvancedOpenGLApp();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Xamarin.Forms GTK# Backend");
+            window.SetApplicationIcon("xamarinlogo.png");
+            window.Show();
+            Gtk.Application.Run();
+        }
+
+        private static void OnUnhandledException(UnhandledExceptionArgs args)
+        {
+            System.Diagnostics.Debug.WriteLine($"Unhandled GTK# exception: {args.ExceptionObject}");
+        }
+    }
+
+    public class DisposePageRenderer : PageRenderer
+    {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ((DisposePage)Element).SendRendererDisposed();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+
+    public class DisposeLabelRenderer : LabelRenderer
+    {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ((DisposeLabel)Element).SendRendererDisposed();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
