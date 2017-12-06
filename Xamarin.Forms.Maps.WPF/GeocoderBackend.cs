4d92383d1961c421117a3c378b2500c9ce7bb9d4
Mohamed CHOUCHANE <mohamed.chouchane1990@gmail.com>
2017-12-07 00:34:59 +0100

    [WPF] Maps project for WPF backend (#1335)
    * Add Xamarin.Forms.Maps.WPF
    
    * Remove french comment in AssemblyInfo.cs
    
    * Remove private keyword

---
+++ Xamarin.Forms.Maps.WPF/GeocoderBackend.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.Maps.WPF
+{
+	internal class GeocoderBackend
+	{
+		public static void Register()
+		{
+			Geocoder.GetPositionsForAddressAsyncFunc = GetPositionsForAddress;
+			Geocoder.GetAddressesForPositionFuncAsync = GetAddressesForPositionAsync;
+		}
+
+		static async Task<IEnumerable<string>> GetAddressesForPositionAsync(Position position)
+		{
+			return new List<string>();
+		}
+
+		static async Task<IEnumerable<Position>> GetPositionsForAddress(string address)
+		{
+			return new List<Position>();
+		}
+	}
+}
---
+++ Xamarin.Forms.Maps.WPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.WPF;
+using Xamarin.Forms.Platform.WPF;
+
+[assembly: AssemblyTitle("Xamarin.Forms.Maps.WPF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xamarin.Forms.Maps.WPF")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ExportRenderer(typeof(Map), typeof(MapRenderer))]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
