27ea629c41aad068d82e924fe6e8d1bd2f52d0b1
WonYoung Choi <wy80.choi@samsung.com>
2017-12-13 23:29:57 +0900

    Move XF.Build.Tasks to nestandard2.0 (#1370)
    Remove net47 project of XF.Build.Tasks. Xamarin.Forms.nupkg provides
    Xamarin.Forms.Build.Tasks.dll and dependencies built with netstandard2.0 only.

--- Xamarin.Forms.Xaml.Xamlg/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Xaml.Xamlg/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
