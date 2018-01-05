c5afb791530e4d605d7d546ed071208753513690
Samantha Houts <samhouts@users.noreply.github.com>
2018-01-05 01:46:49 -0800

    [UWP] Grouped ListViews respond correctly to Tapped events (#1499)
    * Add repro for issue 1439
    
    * [UWP] Use GlobalIndex to get index of item when there are groups.
    
    * [UWP] Just in case the ListView is disposed before the event fires...
    fixes #1439
    fixes #1401 (duplicate)

--- Xamarin.Forms.Platform.WinRT/ListViewRenderer.cs
+++ Xamarin.Forms.Platform.WinRT/ListViewRenderer.cs
@@ -558,8 +558,13 @@ namespace Xamarin.Forms.Platform.WinRT
 
 		void OnListItemClicked(object sender, ItemClickEventArgs e)
 		{
-			if (e.ClickedItem != null)
-				OnListItemClicked(((WListView)e.OriginalSource).Items.IndexOf(e.ClickedItem));
+			if (e.ClickedItem != null && TemplatedItemsView?.TemplatedItems != null)
+			{
+				var templatedItems = TemplatedItemsView.TemplatedItems;
+				var selectedItemIndex = templatedItems.GetGlobalIndexOfItem(e.ClickedItem);
+				
+				OnListItemClicked(selectedItemIndex);
+			}
 		}
 
 		void OnControlSelectionChanged(object sender, SelectionChangedEventArgs e)
