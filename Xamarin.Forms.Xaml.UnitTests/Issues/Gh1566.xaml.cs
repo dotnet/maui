35ae3847e3041d6e26f906a8fb03327ab0c00aba
Stephane Delcroix <stephane@delcroix.org>
2018-01-12 21:51:45 +0100

    [Xaml] do not check PropertyName on Obsolete BPs (#1571)
    fixes #1566

---
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Gh1566.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UnitTests;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public partial class Gh1566
+	{
+		public Gh1566()
+		{
+			InitializeComponent();
+		}
+
+		public Gh1566(bool useCompiledXaml)
+		{
+			//this stub will be replaced at compile time
+		}
+
+		[TestFixture]
+		class Tests
+		{
+			[SetUp]
+			public void Setup()
+			{
+				Device.PlatformServices = new MockPlatformServices();
+			}
+
+			[TearDown]
+			public void TearDown()
+			{
+				Device.PlatformServices = null;
+			}
+
+			[TestCase(true), TestCase(false)]
+			public void ObsoletePropsDoNotThrow(bool useCompiledXaml)
+			{
+				var layout = new Gh1566(useCompiledXaml);
+				Assert.That(layout.frame.BorderColor, Is.EqualTo(Color.Red));
+			}
+		}
+	}
+}
