933d6809ecc004ed672eef1342dcc73ad97946ca
BradChase2011 <brchase07@yahoo.com>
2018-01-31 13:18:40 -0500

    [UWP] Fixed inconsistency on UWP's DatePicker and the other OS's. (#1183)
    * [UWP] Fixed inconsistency on UWP's DatePicker and the other OS's.
    
    * [UWP] bz60001 Test case.
    
    * * [UWP] Fixed inconsistency on UWP's DatePicker and the other OS's. (Build fix)

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla60001.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using System;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+// Apply the default category of "Issues" to all of the tests in this assembly
+// We use this as a catch-all for tests which haven't been individually categorized
+#if UITEST
+[assembly: NUnit.Framework.Category("Issues")]
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 60001, "[UWP] Inconsistency with DatePicker ", PlatformAffected.UWP)]
+	public class Bugzilla60001 : TestContentPage // or TestMasterDetailPage, etc ...
+	{
+		protected override void Init()
+		{
+			StackLayout layout = new StackLayout() { Orientation = StackOrientation.Vertical };
+			DatePicker picker = new DatePicker();
+			picker.Date = new DateTime(2017, 10, 7, 0, 0, 0, DateTimeKind.Utc);
+			Label label = new Label() { Text = "On Droid this will show as 10/7/2017, on UWP it will show as 10/06/2017.  Local TimeZone for this test was EDT.", LineBreakMode = LineBreakMode.WordWrap };
+			layout.Children.Add(picker);
+			layout.Children.Add(label);
+			// Initialize ui here instead of ctor
+			Content = layout;
+		}
+	}
+}
