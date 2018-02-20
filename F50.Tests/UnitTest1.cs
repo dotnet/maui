e1abb63d7d6d1bcbc75c6550daf70c7d2cfdbd2b
James Montemagno <james.montemagno@gmail.com>
2018-02-20 13:15:08 -0800

    Add base project and test
    This adds base project and test for mult-targeting. Also includes editor config and my gitignore.

---
+++ F50.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace F50.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ F50/Preferences/Preferences.shared.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.F50
+{
+    /// <summary>
+    /// Shared code between preferences
+    /// Contains static methods and shared members
+    /// </summary>
+    public partial class Preferences
+    {
+        public static void DoStuff()
+        {
+
+        }
+    }
+
+}
