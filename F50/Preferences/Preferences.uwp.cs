fdbe2de69640553784ae0df223ca40a41a956b06
Jonathan Dick <jondick@gmail.com>
2018-02-21 10:49:16 -0500

    Added UWP implementation for Preferences

--- F50/Preferences/Preferences.uwp.cs
+++ F50/Preferences/Preferences.uwp.cs
@@ -1,25 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Windows.Storage;
 
 namespace Xamarin.F50
 {
     public partial class Preferences
     {
-		public bool ContainsKey(string key) =>
-			throw new NotImplentedInReferenceAssembly();
+		static readonly object locker = new object();
 
-		public bool Remove(string key) =>
-			throw new NotImplentedInReferenceAssembly();
+		public bool ContainsKey(string key)
+		{
+			lock (locker)
+			{
+				return Settings.Values.ContainsKey(key);
+			}
+		}
 
-		public bool Clear() =>
-			throw new NotImplentedInReferenceAssembly();
+		public bool Remove(string key)
+		{
+			lock (locker)
+			{
+				if (Settings.Values.ContainsKey(key))
+				{
+					Settings.Values.Remove(key);
+					return true;
+				}
+			}
 
-		bool Set<T>(string key, T value) =>
-			throw new NotImplentedInReferenceAssembly();
+			return false;
+		}
 
-		T Get<T>(string key, T defaultValue) =>
-			throw new NotImplentedInReferenceAssembly();
+		public bool Clear()
+		{
+			lock (locker)
+			{
+				Settings.Values.Clear();
+			}
+
+			return true;
+		}
+		
+		bool Set<T>(string key, T value)
+		{
+			lock (locker)
+			{
+				Settings.Values[key] = value;
+			}
+
+			return true;
+		}
+
+		T Get<T>(string key, T defaultValue)
+		{
+			lock (locker)
+			{
+				if (Settings.Values.ContainsKey(key))
+				{
+					var tempValue = settings.Values[key];
+					if (tempValue != null)
+						return (T)tempValue;
+				}
+			}
+
+			return defaultValue;
+		}
+
+		ApplicationDataContainer settings;
+		ApplicationDataContainer Settings
+		{
+			get
+			{
+				if (settings == null)
+				{
+					if (string.IsNullOrWhiteSpace(SharedName))
+						settings = ApplicationData.Current.LocalSettings;
+
+					if (!ApplicationData.Current.LocalSettings.Containers.ContainsKey(SharedName))
+						ApplicationData.Current.LocalSettings.CreateContainer(SharedName, ApplicationDataCreateDisposition.Always);
+					settings = ApplicationData.Current.LocalSettings.Containers[SharedName];
+				}
+
+				return settings;
+			}
+		}
 
 		bool disposedValue = false;
 
