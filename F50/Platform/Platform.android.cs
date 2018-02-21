9c0b83f29d7355c7047761e1bf82ef7e9d6f90d1
Jonathan Dick <jondick@gmail.com>
2018-02-20 21:35:29 -0500

    Added Android lifecycle tracking in Platform
    This also adds platform empty files for future use.

---
+++ F50/Platform/Platform.android.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace Xamarin.F50
+{
+	public partial class Platform
+	{
+		static Application application;
+		static ActivityLifecycleContextListener lifecycleListener;
+
+		internal static Context CurrentContext
+			=> lifecycleListener?.Context ?? application?.ApplicationContext;
+
+		internal static Activity CurrentActivity
+			=> lifecycleListener?.Activity;
+
+		public static void Init(Activity activity, Bundle bundle)
+		{
+			lifecycleListener = new ActivityLifecycleContextListener();
+			application = activity.Application;
+			application.RegisterActivityLifecycleCallbacks(lifecycleListener);
+		}
+	}
+
+	internal class ActivityLifecycleContextListener : Java.Lang.Object, Application.IActivityLifecycleCallbacks
+	{
+		Activity currentActivity = null;
+
+		public Context Context 
+			=> currentActivity ?? Application.Context;
+
+		public Activity Activity
+			=> currentActivity;
+
+		public void OnActivityCreated(Activity activity, Bundle savedInstanceState)
+		{
+		}
+
+		public void OnActivityDestroyed(Activity activity)
+		{
+		}
+
+		public void OnActivityPaused(Activity activity)
+		{
+		}
+
+		public void OnActivityResumed(Activity activity)
+		{
+			currentActivity = activity;
+		}
+
+		public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
+		{
+		}
+
+		public void OnActivityStarted(Activity activity)
+		{
+		}
+
+		public void OnActivityStopped(Activity activity)
+		{
+		}
+	}
+}
