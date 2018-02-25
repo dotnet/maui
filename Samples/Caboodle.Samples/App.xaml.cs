0f91dec3f1c74cce530c99b4f51a0542a73e415d
James Montemagno <james.montemagno@gmail.com>
2018-02-25 13:26:22 -0800

    Add base samples and navigation

---
+++ Samples/Caboodle.Samples/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Caboodle.Samples.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Caboodle.Samples
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new NavigationPage(new HomePage());
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Samples/Caboodle.Samples/ViewModel/HomeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Caboodle.Samples.Model;
+using Caboodle.Samples.View;
+using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caboodle.Samples.ViewModel
+{
+    public class HomeViewModel : BaseViewModel
+    {
+		public ObservableRangeCollection<SampleItem> Items { get; }
+		public HomeViewModel()
+		{
+			Items = new ObservableRangeCollection<SampleItem>()
+			{
+				new SampleItem
+				{
+					Name = "Preferences",
+					Page = typeof(PreferencesPage)
+				}
+			};
+		}
+    }
+}
