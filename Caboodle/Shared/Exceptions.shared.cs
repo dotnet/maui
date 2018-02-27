7adae1a0b5e64dc9bbb3c12fe06600018f36cf07
Matthew Leibowitz <mattleibow@live.com>
2018-02-27 23:00:45 +0200

    Reverting code changes for now

---
+++ Caboodle/Shared/Exceptions.shared.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Caboodle
+{
+	public class NotImplentedInReferenceAssembly : NotImplementedException
+	{
+		public NotImplentedInReferenceAssembly()
+			: base("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.")
+		{
+
+		}
+	}
+}
