039d1ffa4a2a1d47d9d6636c04746fb933385e98
Matthew Leibowitz <mattleibow@live.com>
2018-03-02 06:22:25 +0200

    Added some unit tests

---
+++ DeviceTests/Caboodle.DeviceTests.Shared/DeviceInfo_Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Caboodle;
+using Xunit;
+
+namespace Caboodle.DeviceTests
+{
+    public class DeviceInfo_Tests
+    {
+        [Fact]
+        public void Versions_Are_Correct()
+        {
+#if WINDOWS_UWP
+            Assert.Equal(10, DeviceInfo.Version.Major);
+            Assert.Equal(0, DeviceInfo.Version.Minor);
+            Assert.StartsWith("10.0", DeviceInfo.VersionString);
+#else
+            Assert.True(DeviceInfo.Version.Major > 0);
+#endif
+        }
+
+        [Fact]
+        public void Identifier_Is_Not_Null()
+        {
+            Assert.NotNull(DeviceInfo.Identifier);
+            Assert.NotEmpty(DeviceInfo.Identifier);
+        }
+
+        [Fact]
+        public void Platform_Is_Correct()
+        {
+#if WINDOWS_UWP
+            Assert.Equal(DeviceInfo.Platforms.UWP, DeviceInfo.Platform);
+#elif __IOS__
+            Assert.Equal(DeviceInfo.Platforms.iOS, DeviceInfo.Platform);
+#elif __ANDROID__
+            Assert.Equal(DeviceInfo.Platforms.Android, DeviceInfo.Platform);
+#else
+            throw new PlatformNotSupportedException();
+#endif
+        }
+
+        [Fact]
+        public void App_Build_Is_Correct()
+        {
+            Assert.Equal("1", DeviceInfo.AppBuild);
+        }
+
+        [Fact]
+        public void App_Versions_Are_Correct()
+        {
+            Assert.Equal("1.0.1.0", DeviceInfo.AppVersionString);
+            Assert.Equal(new Version(1, 0, 1, 0), DeviceInfo.AppVersion);
+        }
+
+        [Fact]
+        public Task Screen_Metrics_Are_Not_Null()
+        {
+            return Utils.OnMainThread(() =>
+            {
+                var metrics = DeviceInfo.ScreenMetrics;
+
+                Assert.True(metrics.Width > 0);
+                Assert.True(metrics.Height > 0);
+                Assert.True(metrics.Density > 0);
+            });
+        }
+    }
+}
---
+++ DeviceTests/Caboodle.DeviceTests.Shared/Utils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Caboodle.DeviceTests
+{
+    public class Utils
+    {
+#if WINDOWS_UWP
+        public static async Task OnMainThread(Windows.UI.Core.DispatchedHandler action)
+        {
+            var mainView = Windows.ApplicationModel.Core.CoreApplication.MainView;
+            var normal = Windows.UI.Core.CoreDispatcherPriority.Normal;
+            await mainView.CoreWindow.Dispatcher.RunAsync(normal, action);
+        }
+#elif __ANDROID__
+        public static Task OnMainThread(Action action)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            var looper = Android.OS.Looper.MainLooper;
+            var handler = new Android.OS.Handler(looper);
+            handler.Post(() =>
+            {
+                action();
+
+                tcs.SetResult(true);
+            });
+            return tcs.Task;
+        }
+#elif __IOS__
+       public static Task OnMainThread(Action action)
+        {
+            var obj = new Foundation.NSObject();
+            obj.InvokeOnMainThread(action);
+            return Task.FromResult(true);
+        }
+#endif
+    }
+}
