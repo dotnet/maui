2c76d1ad0e8caa04f1eb0966e3ecd5a9213fdaca
Stephane Delcroix <stephane@delcroix.org>
2018-03-21 15:47:15 +0100

    [C] convenience extension method (#2153)

---
+++ Xamarin.Forms.Core/IServiceProviderExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Xamarin.Forms.Xaml
+{
+	public static class IServiceProviderExtensions
+	{
+		public static T GetService<T>(this System.IServiceProvider provider)
+		{
+			var service = provider.GetService(typeof(T));
+			if (service is T tservice)
+				return tservice;
+			return default(T);
+		}
+	}
+}
