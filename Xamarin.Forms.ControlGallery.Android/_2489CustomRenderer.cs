c4d16c0d4270ee0fa8eb6100ddbfc96ebc4829d4
Samantha Houts <samhouts@users.noreply.github.com>
2018-04-25 14:05:59 -0700

    [Core] Improve Renderer registration order (#2519)
    * Add repro for 2489
    
    - Repo steps are to swap to the TabbedPage root; if the tabs are still visbile, test has passed.
    - Also add Preserve attribute to the core test pages so we can test with the Android linker on.
    
    * [Android] Add note to remind us where the renderers are registered
    
    * [iOS] Switch to loading renderers via RenderWithAttribute
    
    * [Core] Ensure that the most specific renderer is returned by Registrar.GetHandlerType
    
    fixes #2489
    
    * [Core] Prevent insane loop
    
    * implement review feedback

---
+++ Xamarin.Forms.ControlGallery.Android/_2489CustomRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Android.Content;
+using System;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.ControlGallery.Android;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Page), typeof(_2489CustomRenderer))]
+namespace Xamarin.Forms.ControlGallery.Android
+{
+	public class _2489CustomRenderer : PageRenderer
+	{
+		public _2489CustomRenderer(Context context) : base(context)
+		{
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+		{
+			base.OnElementChanged(e);
+
+			System.Diagnostics.Debug.WriteLine($"{e.NewElement.GetType()} is replaced by _2489CustomRenderer");
+		}
+	}
+}
---
+++ Xamarin.Forms.ControlGallery.iOS/_2489CustomRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.ControlGallery.iOS;
+using Xamarin.Forms.Controls;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Page), typeof(_2489CustomRenderer))]
+namespace Xamarin.Forms.ControlGallery.iOS
+{
+	public class _2489CustomRenderer : PageRenderer
+	{
+		protected override void OnElementChanged(VisualElementChangedEventArgs e)
+		{
+			base.OnElementChanged(e);
+
+			System.Diagnostics.Debug.WriteLine($"{e.NewElement.GetType()} is replaced by _2489CustomRenderer");
+		}
+	}
+}
