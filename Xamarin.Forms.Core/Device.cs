c9456d4aa5338843f29f1cef9a4b473cb9519be6
Daniel Cazzulino <daniel@cazzulino.com>
2018-04-25 18:06:21 -0300

    Improve warning message, since the calling code isn't on Device (#2545)
    The existing message is confusing since the `RuntimePlatform` is not a member of the calling code 
    but of `Device`. So use the full member name in the suggested API usage.

--- Xamarin.Forms.Core/Device.cs
+++ Xamarin.Forms.Core/Device.cs
@@ -113,7 +113,7 @@ namespace Xamarin.Forms
             return GetNamedSize(size, targetElementType, false);
         }
 
-        [Obsolete("OnPlatform is obsolete as of version 2.3.4. Please use switch(RuntimePlatform) instead.")]
+        [Obsolete("OnPlatform is obsolete as of version 2.3.4. Please use 'switch (Device.RuntimePlatform)' instead.")]
         public static void OnPlatform(Action iOS = null, Action Android = null, Action WinPhone = null, Action Default = null)
         {
             switch (OS)
@@ -144,7 +144,7 @@ namespace Xamarin.Forms
             }
         }
 
-        [Obsolete("OnPlatform<> (generic) is obsolete as of version 2.3.4. Please use switch(RuntimePlatform) instead.")]
+        [Obsolete("OnPlatform<> (generic) is obsolete as of version 2.3.4. Please use 'switch (Device.RuntimePlatform)' instead.")]
         public static T OnPlatform<T>(T iOS, T Android, T WinPhone)
         {
             switch (OS)
