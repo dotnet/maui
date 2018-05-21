af2d17aab263b95a7bb27e02d450447570e1a7e7
Chris King <kingces95@gmail.com>
2018-05-21 11:14:53 -1000

    refresh .create-nuget.bat

--- .create-nuget.bat
+++ .create-nuget.bat
@@ -1,23 +1,131 @@
 @echo off
 rem stub uncommon targets
 set NUGET_EXE=%NUGET_DIR%NuGet.exe
+
 mkdir Xamarin.Forms.Platform.MacOS\bin\debug\
 mkdir Xamarin.Forms.Platform.Tizen\bin\debug\tizen40\
+mkdir Xamarin.Forms.Platform.MacOS\bin\debug\
+mkdir Xamarin.Forms.Platform.UAP\bin\debug\
+mkdir Xamarin.Forms.Platform.ios\bin\debug\
+mkdir Stubs\Xamarin.Forms.Platform.iOS\bin\iPhone\debug\
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\ar
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\ca
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\cs
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\da
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\de
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\el
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\es
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\fi
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\fr
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\he
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\hi
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\hr
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\hu
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\id
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\it
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\ja
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\ko
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\ms
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\nb
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\nl
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\pl
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\pt-BR
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\pt
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\ro
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\ru
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\sk
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\sv
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\th
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\tr
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\uk
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\vi
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\zh-Hans
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\zh-Hant
+mkdir Xamarin.Forms.Platform.iOS\bin\debug\zh-HK
+
+mkdir Xamarin.Forms.Platform.Android\bin\debug
+echo foo > Xamarin.Forms.Platform.Android\bin\debug\Xamarin.Forms.Platform.Android.dll
+
+mkdir Xamarin.Forms.Platform.Android.FormsViewGroup\bin\debug
+echo foo > Xamarin.Forms.Platform.Android.FormsViewGroup\bin\debug\FormsViewGroup.dll
+
+mkdir Stubs\Xamarin.Forms.Platform.Android\bin\debug
+echo foo > Stubs\Xamarin.Forms.Platform.Android\bin\debug\Xamarin.Forms.Platform.dll
+
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\ar\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\ca\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\cs\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\da\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\de\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\el\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\es\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\fi\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\fr\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\he\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\hi\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\hr\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\hu\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\id\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\it\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\ja\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\ko\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\ms\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\nb\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\nl\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\pl\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\pt-BR\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\pt\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\ro\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\ru\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\sk\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\sv\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\th\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\tr\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\uk\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\vi\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\zh-Hans\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\zh-Hant\Xamarin.Forms.Platform.iOS.resources.dll
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\zh-HK\Xamarin.Forms.Platform.iOS.resources.dll
+
+echo foo > Xamarin.Forms.Platform.iOS\bin\debug\Xamarin.Forms.Platform.iOS.dll
+echo foo > Stubs\Xamarin.Forms.Platform.iOS\bin\iPhone\debug\Xamarin.Forms.Platform.dll
+
 echo foo > Xamarin.Forms.Platform.MacOS\bin\debug\Xamarin.forms.Platform.macOS.dll
 echo foo > Xamarin.Forms.Platform.MacOS\bin\debug\Xamarin.forms.Platform.dll
+
 echo foo > Xamarin.Forms.Platform.Tizen\bin\debug\tizen40\Xamarin.forms.Platform.tizen.dll
 echo foo > Xamarin.Forms.Platform.Tizen\bin\debug\tizen40\Xamarin.forms.Platform.dll
-if "%1" NEQ "" (
+
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\Xamarin.Forms.Platform.UAP.dll
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\Xamarin.Forms.Platform.UAP.pri
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\Xamarin.Forms.Platform.UAP.xr.xml
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\FormsProgressBarStyle.xbf
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\FormsFlyout.xbf
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\FormsCommandBarStyle.xbf
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\Resources.xbf
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\FormsTextBoxStyle.xbf
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\AutoSuggestStyle.xbf
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\SliderStyle.xbf
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\MasterDetailControlStyle.xbf
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\PageControlStyle.xbf
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\TabbedPageStyle.xbf
+echo foo > Xamarin.Forms.Platform.UAP\bin\debug\FormsEmbeddedPageWrapper.xbf
+
+if "%1" == "droid" (
+   %NUGET_EXE% restore .xamarin.forms.android.sln
+   msbuild /v:m /p:platform="any cpu" .xamarin.forms.android.sln
+)
+if "%1" == "ios" (
+   %NUGET_EXE% restore .xamarin.forms.ios.sln
+   msbuild /v:m /p:platform="any cpu" .xamarin.forms.ios.sln
+)
+if "%1" == "build" (
    %NUGET_EXE% restore .xamarin.forms.sln
    msbuild /v:m /p:platform="any cpu" .xamarin.forms.sln
 )
+
 if "%DEBUG_VERSION%"=="" set DEBUG_VERSION=0
 set /a DEBUG_VERSION=%DEBUG_VERSION%+1
-pushd docs
-..\tools\mdoc\mdoc.exe export-msxdoc -o Xamarin.Forms.Core.xml Xamarin.Forms.Core
-..\tools\mdoc\mdoc.exe export-msxdoc -o Xamarin.Forms.Xaml.xml Xamarin.Forms.Xaml
-..\tools\mdoc\mdoc.exe export-msxdoc -o Xamarin.Forms.Maps.xml Xamarin.Forms.Maps
-popd
 pushd .nuspec
 %NUGET_EXE% pack Xamarin.Forms.nuspec -properties configuration=debug;platform=anycpu -Version 9.9.%DEBUG_VERSION%
 if "%CREATE_MAP_NUGET%" NEQ "" (
