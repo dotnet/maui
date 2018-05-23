31c432c5dd8fed909680366c5956b889d3ae9b93
Seungkeun Lee <sngn.lee@samsung.com>
2018-05-23 16:32:08 +0900

    [Tizen] Add CornerRadius implementation on BoxView (#2806)

--- Xamarin.Forms.Platform.Tizen/Renderers/BoxViewRenderer.cs
+++ Xamarin.Forms.Platform.Tizen/Renderers/BoxViewRenderer.cs
@@ -1,20 +1,24 @@
 using System.ComponentModel;
 using EColor = ElmSharp.Color;
-using ERectangle = ElmSharp.Rectangle;
+using Xamarin.Forms.Platform.Tizen.Native;
 
 namespace Xamarin.Forms.Platform.Tizen
 {
-	public class BoxViewRenderer : ViewRenderer<BoxView, ERectangle>
+	public class BoxViewRenderer : ViewRenderer<BoxView, RoundRectangle>
 	{
+		public BoxViewRenderer()
+		{
+			RegisterPropertyHandler(nameof(Element.CornerRadius), OnRadiusUpdate);
+		}
+
 		protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
 		{
 			if (Control == null)
 			{
-				SetNativeControl(new ERectangle(Forms.NativeParent));
+				SetNativeControl(new RoundRectangle(Forms.NativeParent));
 			}
 
 			UpdateColor();
-
 			base.OnElementChanged(e);
 		}
 
@@ -38,12 +42,31 @@ namespace Xamarin.Forms.Platform.Tizen
 			}
 		}
 
+		protected override void UpdateLayout()
+		{
+			base.UpdateLayout();
+			Control.Draw(Control.Geometry);
+		}
+
 		protected override void UpdateOpacity(bool initialize)
 		{
 			if (initialize && Element.Opacity == 1d)
 				return;
 
 			UpdateColor();
+		}
+
+		void OnRadiusUpdate(bool init)
+		{
+			int topLeft = Forms.ConvertToScaledPixel(Element.CornerRadius.TopLeft);
+			int topRight = Forms.ConvertToScaledPixel(Element.CornerRadius.TopRight);
+			int bottomLeft = Forms.ConvertToScaledPixel(Element.CornerRadius.BottomLeft);
+			int bottomRight = Forms.ConvertToScaledPixel(Element.CornerRadius.BottomRight);
+			Control.SetRadius(topLeft, topRight, bottomLeft, bottomRight);
+			if (!init)
+			{
+				Control.Draw();
+			}
 		}
 
 		void UpdateColor()
