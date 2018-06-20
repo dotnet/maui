ad2522354a29c5005d1978684ee8514d5553df8a
Samantha Houts <samhouts@users.noreply.github.com>
2018-06-20 14:14:26 -0700

    [iOS] RecycleElement ListView should not crash when bound to ObservableCollection (ArgumentNullException & ArgumentException) (#3042) fixes #1900 fixes #3026
    * [iOS] Default to TextCell in GetPrototypicalCell
    
    fixes #3026
    
    * [iOS] Don't throw exception when Index doesn't match in RecycleElement
    
    fixes #1900
    
    * Add repros for #1342, #1900, #1927
    
    * errant space
    
    * Remove repros for #1342 and #1927 (tbc in another branch)
    
    * Update Issue1900.cs
    
    * Revert exception type
    
    did not mean to change that! yay for ui tests
    
    * Update Issue1900.cs

--- Xamarin.Forms.Platform.iOS/Renderers/ListViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/ListViewRenderer.cs
@@ -532,7 +532,9 @@ namespace Xamarin.Forms.Platform.iOS
 
 			var groupReset = resetWhenGrouped && Element.IsGroupingEnabled;
 
-			if (!groupReset)
+			// We can't do this check on grouped lists because the index doesn't match the number of rows in a section.
+			// Likewise, we can't do this check on lists using RecycleElement because the number of rows in a section will remain constant because they are reused.
+			if (!groupReset && Element.CachingStrategy == ListViewCachingStrategy.RetainElement)
 			{
 				var lastIndex = Control.NumberOfRowsInSection(section);
 				if (e.NewStartingIndex > lastIndex || e.OldStartingIndex > lastIndex)
@@ -752,9 +754,10 @@ namespace Xamarin.Forms.Platform.iOS
 				else // ListViewCachingStrategy.RetainElement
 					return GetCellForPath(indexPath);
 
+				if (itemTypeOrDataTemplate == null)
+					itemTypeOrDataTemplate = typeof(TextCell);
 
-				Cell protoCell;
-				if (!_prototypicalCellByTypeOrDataTemplate.TryGetValue(itemTypeOrDataTemplate, out protoCell))
+				if (!_prototypicalCellByTypeOrDataTemplate.TryGetValue(itemTypeOrDataTemplate, out Cell protoCell))
 				{
 					// cache prototypical cell by item type; Items of the same Type share
 					// the same DataTemplate (this is enforced by RecycleElementAndDataTemplate)
@@ -988,7 +991,7 @@ namespace Xamarin.Forms.Platform.iOS
 				var renderer = (CellRenderer)Internals.Registrar.Registered.GetHandlerForObject<IRegisterable>(cell);
 				view = new HeaderWrapperView { Cell = cell };
 				view.AddSubview(renderer.GetCell(cell, null, tableView));
-        
+
 				return view;
 			}
 
@@ -1115,8 +1118,8 @@ namespace Xamarin.Forms.Platform.iOS
 
 				if (_isDragging && scrollView.ContentOffset.Y < -10f && _uiTableViewController._usingLargeTitles && Device.Info.CurrentOrientation.IsPortrait())
 				{
-					_uiTableViewController.ForceRefreshing();				
-				}					
+					_uiTableViewController.ForceRefreshing();
+				}
 			}
 
 			public override string[] SectionIndexTitles(UITableView tableView)
@@ -1350,7 +1353,7 @@ namespace Xamarin.Forms.Platform.iOS
 			{
 				if (RefreshControl == null)
 					return;
-				
+
 				_refresh.EndRefreshing();
 
 				UpdateContentOffset(-1);
