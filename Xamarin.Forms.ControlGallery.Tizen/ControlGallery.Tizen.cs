d72e8dd43bde3c57d45f1a95f7a8b52d044d93a5
Kangho <rookiejava+github@gmail.com>
2018-06-21 19:54:00 +0900

    [Controls] Support NativeView to ControlGallery.Tizen (#3079)

--- Xamarin.Forms.ControlGallery.Tizen/ControlGallery.Tizen.cs
+++ Xamarin.Forms.ControlGallery.Tizen/ControlGallery.Tizen.cs
@@ -1,19 +1,22 @@
 using Xamarin.Forms.Platform.Tizen;
 using Xamarin.Forms.Controls;
+using ElmSharp;
 
 namespace Xamarin.Forms.ControlGallery.Tizen
 {
-	class Program : FormsApplication
+	class MainApplication : FormsApplication
 	{
+		internal static EvasObject NativeParent { get; private set; }
 		protected override void OnCreate()
 		{
 			base.OnCreate();
+			NativeParent = MainWindow;
 			LoadApplication(new App());
 		}
 
 		static void Main(string[] args)
 		{
-			var app = new Program();
+			var app = new MainApplication();
 			FormsMaps.Init("HERE", "write-your-API-key-here");
 			global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
 			app.Run(args);
--- Xamarin.Forms.ControlGallery.Tizen/SampleNativeControl.cs
+++ Xamarin.Forms.ControlGallery.Tizen/SampleNativeControl.cs
@@ -2,6 +2,7 @@
 using Xamarin.Forms.ControlGallery.Tizen;
 using Xamarin.Forms.Controls.Issues.Helpers;
 using Xamarin.Forms.Platform.Tizen;
+using ELabel = ElmSharp.Label;
 
 [assembly: Dependency(typeof(SampleNativeControl))]
 namespace Xamarin.Forms.ControlGallery.Tizen
@@ -12,7 +13,11 @@ namespace Xamarin.Forms.ControlGallery.Tizen
 		{
 			get
 			{
-				return new Label { Text = "NativeViews not supported on Tizen" };
+				var label = new ELabel(MainApplication.NativeParent)
+				{
+					Text = "Sample Native Control"
+				};
+				return label.ToView();
 			}
 		}
 	}
