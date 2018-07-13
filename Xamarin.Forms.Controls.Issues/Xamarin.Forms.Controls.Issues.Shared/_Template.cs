0c1127aba0a4025a5df59a47c193752171e565e0
Samantha Houts <samantha.houts@xamarin.com>
2018-07-13 16:02:11 -0700

    Update issue template
    Our most common target is Github again. Also added back the UITestCategories to make it easier to remember to add them.

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/_Template.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/_Template.cs
@@ -2,15 +2,19 @@
 using Xamarin.Forms.Internals;
 
 #if UITEST
+using Xamarin.Forms.Core.UITests;
 using Xamarin.UITest;
 using NUnit.Framework;
 #endif
 
 namespace Xamarin.Forms.Controls.Issues
 {
+#if UITEST
+	[Category(UITestCategories.ManualReview)]
+#endif
 	[Preserve(AllMembers = true)]
-	[Issue(IssueTracker.Bugzilla, 1, "Issue Description", PlatformAffected.Default)]
-	public class Bugzilla1 : TestContentPage // or TestMasterDetailPage, etc ...
+	[Issue(IssueTracker.Github, 1, "Issue Description", PlatformAffected.Default)]
+	public class Issue1 : TestContentPage // or TestMasterDetailPage, etc ...
 	{
 		protected override void Init()
 		{
@@ -24,8 +28,9 @@ namespace Xamarin.Forms.Controls.Issues
 
 #if UITEST
 		[Test]
-		public void Issue1Test ()
+		public void Issue1Test() 
 		{
+			// Delete this and all other UITEST sections if there is no way to automate the test. Otherwise, be sure to rename the test and update the Category attribute on the class. Note that you can add multiple categories.
 			RunningApp.Screenshot ("I am at Issue 1");
 			RunningApp.WaitForElement (q => q.Marked ("IssuePageLabel"));
 			RunningApp.Screenshot ("I see the Label");
