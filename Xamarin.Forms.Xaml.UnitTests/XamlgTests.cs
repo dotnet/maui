7cf3725e9d52d9f8e41dc07078eac321e0ad06d5
Stephane Delcroix <stephane@delcroix.org>
2018-07-26 11:22:39 +0200

    [XamlG] allow x:Name on types in using: xmlns (#3395)
    - fixes #3393

--- Xamarin.Forms.Xaml.UnitTests/XamlgTests.cs
+++ Xamarin.Forms.Xaml.UnitTests/XamlgTests.cs
@@ -74,6 +74,7 @@ namespace Xamarin.Forms.Xaml.UnitTests
     xmlns=""http://xamarin.com/schemas/2014/forms""
     xmlns:x=""http://schemas.microsoft.com/winfx/2009/xaml""
     xmlns:local=""clr-namespace:CustomListViewSample;assembly=CustomListViewSample""
+    xmlns:localusing=""using:CustomListViewSample""
     x:Class=""CustomListViewSample.TestPage"">
     <StackLayout 
         VerticalOptions=""CenterAndExpand""
@@ -81,6 +82,7 @@ namespace Xamarin.Forms.Xaml.UnitTests
         <Label Text=""Hello, Custom Renderer!"" />
         <local:CustomListView x:Name=""listView""
             WidthRequest=""960"" CornerRadius=""50"" OutlineColor=""Blue"" />
+		<localusing:CustomListView x:Name=""listViewusing"" />
     </StackLayout>
 </ContentPage>";
 
@@ -89,9 +91,11 @@ namespace Xamarin.Forms.Xaml.UnitTests
 			var generator = new XamlGenerator();
 			generator.ParseXaml(reader);
 
-			Assert.AreEqual (1, generator.NamedFields.Count());
-			Assert.AreEqual ("listView", generator.NamedFields.First ().Name);
-			Assert.AreEqual ("CustomListViewSample.CustomListView", generator.NamedFields.First ().Type.BaseType);
+			Assert.AreEqual (2, generator.NamedFields.Count());
+			Assert.AreEqual("listView", generator.NamedFields.ToArray()[0].Name);
+			Assert.AreEqual ("CustomListViewSample.CustomListView", generator.NamedFields.ToArray()[0].Type.BaseType);
+			Assert.AreEqual("listViewusing", generator.NamedFields.ToArray()[1].Name);
+			Assert.AreEqual ("CustomListViewSample.CustomListView", generator.NamedFields.ToArray()[1].Type.BaseType);
 		}
 
 		[Test]
