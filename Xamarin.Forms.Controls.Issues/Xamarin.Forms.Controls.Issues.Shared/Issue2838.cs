6031bbfb691033680c877bb7da79fc5d6c717299
Martin Zikmund <martinzikmund@sphereline.com>
2018-07-30 18:54:25 +0200

    [UWP] Fix CornerRadius rendering on Frame (#3239) fixes #2838
    * Updated Frame renderer on UWP to handle CornerRadius properly
    
    * Test page for issue 2838
    
    * Fixed typo and removed UITEST directive
    
    * Label was cut off, fixed

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue2838.cs
@@ -0,0 +1,46 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 2838, "UWP does not render Frame CornerRadius", PlatformAffected.UWP)]
+	public class Issue2838 : TestContentPage
+	{
+		protected override void Init()
+		{
+			// Initialize ui here instead of ctor
+			Content = new StackLayout
+			{
+				Orientation = StackOrientation.Vertical,
+				Children =
+				{
+					new Label()
+					{
+						Text ="The frame below should have its corners rounded and the background should not protrude through them.",
+						TextColor = Color.Black,
+						WidthRequest = 300,
+						HeightRequest = 90,
+						LineBreakMode = LineBreakMode.WordWrap,
+						HorizontalOptions = LayoutOptions.Center,
+						Margin = new Thickness(10)
+					},
+					new Frame
+					{
+						WidthRequest = 300,
+						HeightRequest = 160,
+						HorizontalOptions = LayoutOptions.Center,
+						CornerRadius = 10,
+						BackgroundColor = Color.Red,
+						BorderColor = Color.Blue
+					}
+				}
+			};
+		}
+	}
+}
--- Xamarin.Forms.Platform.UAP/FrameRenderer.cs
+++ Xamarin.Forms.Platform.UAP/FrameRenderer.cs
@@ -56,6 +56,25 @@ namespace Xamarin.Forms.Platform.UWP
 			}
 		}
 
+		protected override void UpdateBackgroundColor()
+		{
+			//background color change must be handled separately
+			//because the background would protrude through the border if the corners are rounded
+			//as the background would be applied to the renderer's FrameworkElement
+			Color backgroundColor = Element.BackgroundColor;
+			if (Control != null)
+			{
+				if (!backgroundColor.IsDefault)
+				{
+					Control.Background = backgroundColor.ToBrush();
+				}
+				else
+				{
+					Control.ClearValue(BackgroundProperty);
+				}
+			}			
+		}
+
 		void PackChild()
 		{
 			if (Element.Content == null)
