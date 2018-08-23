8c53d7075961903336aae1f0e74d058591f7063f
kingces95 <kingces95@users.noreply.github.com>
2018-08-22 21:59:19 -1000

    Fix 3562 Null Reference Exception (#3576)
    - fixes #3562

--- Xamarin.Forms.Platform.Android/AppCompat/Platform.cs
+++ Xamarin.Forms.Platform.Android/AppCompat/Platform.cs
@@ -267,7 +267,9 @@ namespace Xamarin.Forms.Platform.Android.AppCompat
 				_renderer.RemoveAllViews();
 
 				foreach (IVisualElementRenderer rootRenderer in _navModel.Roots.Select(Android.Platform.GetRenderer))
-					rootRenderer.Dispose();
+				{
+					rootRenderer?.Dispose();
+				}
 
 				_navModel = new NavigationModel();
 
