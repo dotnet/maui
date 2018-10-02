0c8294f9499a7c977d5816db67d8f9dbb780660a
adrianknight89 <adrianknight89@outlook.com>
2018-10-02 12:34:12 -0500

    Automate UI test 35127 (#3927)
    * automate test
    - fixes #2262
    * indentation fix
    * indentation fix 2

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla35127.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla35127.cs
@@ -1,8 +1,17 @@
 using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 
-namespace Xamarin.Forms.Controls
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
 {
+#if UITEST
+	[Category(UITestCategories.ScrollView)]
+#endif
 	[Preserve (AllMembers = true)]
 	[Issue (IssueTracker.Bugzilla, 35127, "It is possible to craft a page such that it will never display on Windows")]
 	public class Bugzilla35127 : TestContentPage
@@ -14,10 +23,22 @@ namespace Xamarin.Forms.Controls
 					new Label { Text = "See me?" },
 					new ScrollView {
 						IsVisible = false,
+						AutomationId = "scrollView",
 						Content = new Button { Text = "Click Me?" }
 					}
 				}
 			};
 		}
+
+#if UITEST
+		[Test]
+		public void Issue35127Test() 
+		{
+			RunningApp.WaitForElement (q => q.Marked ("See me?"));
+			var count = RunningApp.Query(q => q.Marked("scrollView")).Length;
+			Assert.IsTrue(count == 0);
+			RunningApp.WaitForNoElement (q => q.Marked ("Click Me?"));
+		}
+#endif
 	}
 }
