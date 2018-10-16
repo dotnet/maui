ce181dc14dc71418979a2f3185e6ec46e46f9472
Stephane Delcroix <stephane@delcroix.org>
2018-10-16 17:10:04 +0200

    [Xaml] fix Namescope being different than tree (#4089)
    As part of #2556, the namescoping tree was assumed to be equiv to the
    object tree, which is obviously wrong. This fixes it, while keeping the
    old behavior as a fallback.
    
    We might go further, but I can't come up with a reasonable scenario
    failing.
    
    - fixes #3821

--- Xamarin.Forms.Build.Tasks/NodeILExtensions.cs
+++ Xamarin.Forms.Build.Tasks/NodeILExtensions.cs
@@ -495,7 +495,12 @@ namespace Xamarin.Forms.Build.Tasks
 				foreach (var instruction in PushTargetProperty(bpRef, propertyRef, declaringTypeReference, module))
 					yield return instruction;
 
-				yield return Create(Newobj, module.ImportCtorReference(("Xamarin.Forms.Xaml", "Xamarin.Forms.Xaml.Internals", "SimpleValueTargetProvider"), paramCount: 2));
+				if (context.Scopes.TryGetValue(node, out var scope))
+					yield return Create(Ldloc, scope.Item1);
+				else
+					yield return Create(Ldnull);
+
+				yield return Create(Newobj, module.ImportCtorReference(("Xamarin.Forms.Xaml", "Xamarin.Forms.Xaml.Internals", "SimpleValueTargetProvider"), paramCount: 3));
 				//store the provider so we can register it again with a different key
 				yield return Create(Dup);
 				var refProvider = new VariableDefinition(module.ImportReference(("mscorlib", "System", "Object")));
--- Xamarin.Forms.Xaml/MarkupExtensions/ReferenceExtension.cs
+++ Xamarin.Forms.Xaml/MarkupExtensions/ReferenceExtension.cs
@@ -14,36 +14,33 @@ namespace Xamarin.Forms.Xaml
 			if (serviceProvider == null)
 				throw new ArgumentNullException(nameof(serviceProvider));
 			var referenceProvider = serviceProvider.GetService<IReferenceProvider>();
-			if (referenceProvider != null)
-				return referenceProvider.FindByName(Name)
-					   ?? throw new XamlParseException($"Can not find the object referenced by `{Name}`", serviceProvider?.GetService<IXmlLineInfoProvider>()?.XmlLineInfo ?? new XmlLineInfo());
+			var value = referenceProvider?.FindByName(Name);
+			if (value != null)
+				return value;
 
-#pragma warning disable CS0612 // Type or member is obsolete
 			//legacy path. could be hit by code processed by previous versions of XamlC
-			var valueProvider = serviceProvider.GetService<IProvideValueTarget>() as IProvideParentValues
-											   ?? throw new ArgumentException("serviceProvider does not provide an IProvideValueTarget");
-			var namescopeprovider = serviceProvider.GetService<INameScopeProvider>();
-			if (namescopeprovider != null && namescopeprovider.NameScope != null) {
-				var value = namescopeprovider.NameScope.FindByName(Name);
-				if (value != null)
-					return value;
-			}
+#pragma warning disable CS0612 // Type or member is obsolete
+			value = serviceProvider.GetService<INameScopeProvider>()?.NameScope?.FindByName(Name);
+			if (value != null)
+				return value;
 
+#pragma warning restore CS0612 // Type or member is obsolete
+
+			//fallback
+			var valueProvider = serviceProvider.GetService<IProvideValueTarget>() as IProvideParentValues
+								   ?? throw new ArgumentException("serviceProvider does not provide an IProvideValueTarget");
 			foreach (var target in valueProvider.ParentObjects) {
-				var bo = target as BindableObject;
-				if (bo == null)
+				if (!(target is BindableObject bo))
 					continue;
-				var ns = NameScope.GetNameScope(bo) as INameScope;
-				if (ns == null)
+				if (!(NameScope.GetNameScope(bo) is INameScope ns))
 					continue;
-				var value = ns.FindByName(Name);
+				value = ns.FindByName(Name);
 				if (value != null)
 					return value;
 			}
 
 			var lineInfo = serviceProvider?.GetService<IXmlLineInfoProvider>()?.XmlLineInfo ?? new XmlLineInfo();
 			throw new XamlParseException($"Can not find the object referenced by `{Name}`", lineInfo);
-#pragma warning restore CS0612 // Type or member is obsolete
 		}
 	}
 }
