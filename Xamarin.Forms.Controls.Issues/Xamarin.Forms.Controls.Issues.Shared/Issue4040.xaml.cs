47b6cdfba51c3e589220d28c3a16a9228aa26e50
Andrei <andrei.misiukevich@gmail.com>
2018-10-17 20:14:52 +0300

    [iOS] Label TextColor has no effect with FormattedString (#4043)
    * https://github.com/xamarin/Xamarin.Forms/issues/4040 fix default font/color for formatted string
    
    * https://github.com/xamarin/Xamarin.Forms/pull/4043 fixed naming, added test case
    
    * Moved field and property to top
    
    - fixes #4040

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue4040.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.CustomAttributes;
+using System.Collections.ObjectModel;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue (IssueTracker.Github, 4040, "[iOS] Label TextColor has no effect with FormattedString", PlatformAffected.iOS)]
+	public partial class Issue4040 : ContentPage
+	{
+		public Issue4040()
+		{
+			#if APP
+			InitializeComponent ();
+			#endif
+		}
+	}
+}
--- Xamarin.Forms.Platform.iOS/Renderers/LabelRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/LabelRenderer.cs
@@ -24,6 +24,10 @@ namespace Xamarin.Forms.Platform.MacOS
 
 		bool _perfectSizeValid;
 
+		FormattedString _formatted;
+
+		bool IsTextFormatted => _formatted != null;
+
 		public override SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint)
 		{
 			if (!_perfectSizeValid)
@@ -294,24 +298,18 @@ namespace Xamarin.Forms.Platform.MacOS
 #endif
 		}
 
-		bool isTextFormatted;
 		void UpdateText()
 		{
 			_perfectSizeValid = false;
-			var values = Element.GetValues(Label.FormattedTextProperty, Label.TextProperty, Label.TextColorProperty);
+			var values = Element.GetValues(Label.FormattedTextProperty, Label.TextProperty);
 
-			var formatted = values[0] as FormattedString;
-			if (formatted == null && Element.LineHeight >= 0)
-				formatted = (string)values[1];
+			_formatted = values[0] as FormattedString;
+			if (_formatted == null && Element.LineHeight >= 0)
+				_formatted = (string)values[1];
 
-			if (formatted != null)
+			if (IsTextFormatted)
 			{
-#if __MOBILE__
-				Control.AttributedText = formatted.ToAttributed(Element, (Color)values[2], Element.LineHeight);
-#else
-				Control.AttributedStringValue = formatted.ToAttributed(Element, (Color)values[2], Element.LineHeight);
-#endif
-				isTextFormatted = true;
+				UpdateFormattedText();
 			}
 			else
 			{
@@ -320,15 +318,26 @@ namespace Xamarin.Forms.Platform.MacOS
 #else
 				Control.StringValue = (string)values[1] ?? "";
 #endif
-				isTextFormatted = false;
 			}
 			UpdateLayout();
 		}
 
+		void UpdateFormattedText()
+		{
+#if __MOBILE__
+			Control.AttributedText = _formatted.ToAttributed(Element, Element.TextColor, Element.LineHeight);
+#else
+			Control.AttributedStringValue = _formatted.ToAttributed(Element, Element.TextColor, Element.LineHeight);
+#endif
+		}
+
 		void UpdateFont()
 		{
-			if (isTextFormatted)
+			if (IsTextFormatted)
+			{
+				UpdateFormattedText();
 				return;
+			}
 			_perfectSizeValid = false;
 
 #if __MOBILE__
@@ -341,8 +350,11 @@ namespace Xamarin.Forms.Platform.MacOS
 
 		void UpdateTextColor()
 		{
-			if (isTextFormatted)
+			if (IsTextFormatted)
+			{
+				UpdateFormattedText();
 				return;
+			}
 
 			_perfectSizeValid = false;
 
