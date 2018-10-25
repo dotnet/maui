5b9ad755a95a1531dca2ffbff56860153cff1b82
mikescandy <michele.scandura@outlook.com>
2018-10-25 17:45:44 +0100

    Adding instructions and UI tests for Bugzilla44461 (#4144)
    * Adding instructions and UI tests for Bugzilla44461
    
    * Update Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla44461.cs
    
    Co-Authored-By: mikescandy <michele.scandura@outlook.com>
    
    * Fix assert

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla44461.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla44461.cs
@@ -1,18 +1,36 @@
-﻿using Xamarin.Forms.CustomAttributes;
+﻿using System;
+using System.Linq;
+using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 
-namespace Xamarin.Forms.Controls
+#if UITEST
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
 {
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Bugzilla, 44461, "ScrollToPosition.Center works differently on Android and iOS", PlatformAffected.iOS)]
 	public class Bugzilla44461 : TestContentPage
 	{
+		const string BtnPrefix= "Button";
+
 		protected override void Init()
 		{
 			var grid = new Grid
 			{
 				RowSpacing = 0,
 			};
+
+			var instructions = new Label
+			{
+				Text = @"Tap the first button (Button0). The button should be aligned with the left side of the screen "
+				+ "and should not move. If it's not, the test failed."
+			};
+
+			grid.Children.Add(instructions);
 
 			var scrollView = new ScrollView
 			{
@@ -33,7 +51,7 @@ namespace Xamarin.Forms.Controls
 			{
 				var button = new Button
 				{
-					Text = "Button" + i
+					Text = $"{BtnPrefix}{i}"
 				};
 				button.Clicked += (sender, args) =>
 				{
@@ -45,5 +63,26 @@ namespace Xamarin.Forms.Controls
 			scrollView.Content = stackLayout;
 			Content = grid;
 		}
+
+#if UITEST && __IOS__
+		[Test]
+		public void Bugzilla44461Test()
+		{
+			var positions = TapButton(0);
+			Assert.AreEqual(positions.initialPosition.X, positions.finalPosition.X);
+			Assert.LessOrEqual(positions.finalPosition.X, 1);
+			RunningApp.Screenshot ("Button0 is aligned with the left side of the screen");
+		}
+
+		(AppRect initialPosition, AppRect finalPosition ) TapButton(int position)
+		{
+			var buttonText = $"{BtnPrefix}{position}";
+			RunningApp.WaitForElement(q => q.Button(buttonText));
+			var initialPosition = RunningApp.Query(buttonText)[0].Rect;
+			RunningApp.Tap(q => q.Button(buttonText));
+			var finalPosition = RunningApp.Query(buttonText)[0].Rect;
+			return (initialPosition, finalPosition);
+		}
+#endif
 	}
 }
