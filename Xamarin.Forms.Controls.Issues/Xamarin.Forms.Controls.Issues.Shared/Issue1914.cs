1a424e6a83cb62e2868dccf0bb67123d245419c9
Rui Marinho <me@ruimarinho.net>
2018-11-16 17:07:12 +0000

    [Controls]Use propotional values on test 1914 (#4434)

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue1914.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue1914.cs
@@ -4,23 +4,23 @@ using Xamarin.Forms.Internals;
 
 namespace Xamarin.Forms.Controls.Issues
 {
-	[Preserve (AllMembers=true)]
-	[Issue (IssueTracker.Github, 1914, "Android rotation ignores anchor", PlatformAffected.Android)]
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 1914, "Android rotation ignores anchor", PlatformAffected.Android)]
 	public class Issue1914 : ContentPage
 	{
-		public Issue1914 ()
+		public Issue1914()
 		{
-			Content = new Rotator ();
+			Content = new Rotator();
 		}
 
 		class Rotator : AbsoluteLayout
 		{
-			public Rotator ()
+			public Rotator()
 			{
-				var image = new Image {Aspect = Aspect.AspectFit, Source = "bank.png"};
-				Children.Add (image, new Rectangle (.5,0,100,100), AbsoluteLayoutFlags.All);
+				var image = new Image { Aspect = Aspect.AspectFit, Source = "bank.png" };
+				Children.Add(image, new Rectangle(.5, .5, .5, .5), AbsoluteLayoutFlags.All);
 				VerticalOptions = HorizontalOptions = LayoutOptions.Center;
-				image.RotateTo (3600, 10000);
+				image.RotateTo(3600, 10000);
 			}
 		}
 	}
