99b85253833593cfd2f565c1ac10a3f1d5686dd7
Stephane Delcroix <stephane@delcroix.org>
2018-11-16 15:40:05 +0100

    [C] ToStringValueConverter shouldn't be public (#4442)

--- Xamarin.Forms.Core/ToStringValueConverter.cs
+++ Xamarin.Forms.Core/ToStringValueConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace Xamarin.Forms
 {
-	public class ToStringValueConverter : IValueConverter
+	class ToStringValueConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
