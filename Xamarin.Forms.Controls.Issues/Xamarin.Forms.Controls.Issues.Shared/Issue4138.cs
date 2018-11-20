7081d5eb043d716bbe96a94d36e1b7502422d614
Shane Neuville <shane94@hotmail.com>
2018-11-20 05:14:24 -0700

    [iOS] center title icon on ios if there is no titleview (#4457) fixes #4138
    * [iOS] center title icon on ios if there is no titleview
    
    * [ios] ui test for centered title icon

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue4138.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+    [Preserve(AllMembers = true)]
+    [Issue(IssueTracker.Github, 4138, "[iOS] NavigationPage.TitleIcon no longer centered",
+        PlatformAffected.iOS)]
+#if UITEST
+    [NUnit.Framework.Category(UITestCategories.Navigation)]
+#endif
+    public class Issue4138 : TestNavigationPage
+    {
+        protected override void Init()
+        {
+            ContentPage contentPage = new ContentPage();
+
+            NavigationPage.SetTitleIcon(contentPage, "coffee.png");
+
+            PushAsync(contentPage);
+        }
+
+
+#if UITEST && __IOS__
+		[Test]
+        public void TitleIconIsCentered()
+        {
+            var element = RunningApp.WaitForElement("coffee.png")[0];
+			var rect = RunningApp.RootViewRect();
+			Assert.AreEqual(element.Rect.CenterX, rect.CenterX);
+        }
+#endif
+    }
+}
