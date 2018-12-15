ebc030b5f42e779d11a3fa91459c4039fa8623f6
Matthew Leibowitz <mattleibow@live.com>
2018-12-15 20:50:28 +0200

    [macos] Added Launcher APIs

--- Xamarin.Essentials/Launcher/Launcher.mac.cs
+++ Xamarin.Essentials/Launcher/Launcher.mac.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Threading.Tasks;
+using AppKit;
+using Foundation;
 
 namespace Xamarin.Essentials
 {
     public static partial class Launcher
     {
-        static Task<bool> PlatformCanOpenAsync(Uri uri) => throw new System.PlatformNotSupportedException();
+        static Task<bool> PlatformCanOpenAsync(Uri uri) =>
+            Task.FromResult(NSWorkspace.SharedWorkspace.UrlForApplication(new NSUrl(uri.AbsoluteUri)) != null);
 
-        static Task PlatformOpenAsync(Uri uri) => throw new System.PlatformNotSupportedException();
+        static Task PlatformOpenAsync(Uri uri) =>
+            Task.FromResult(NSWorkspace.SharedWorkspace.OpenUrl(new NSUrl(uri.AbsoluteUri)));
     }
 }
