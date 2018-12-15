3b5dfa1636823c3cb242d3b21fbac64d900b1279
Matthew Leibowitz <mattleibow@live.com>
2018-12-15 05:04:44 +0200

    [macos] Added the browser API

--- Xamarin.Essentials/Browser/Browser.mac.cs
+++ Xamarin.Essentials/Browser/Browser.mac.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
+using AppKit;
+using Foundation;
 
 namespace Xamarin.Essentials
 {
     public static partial class Browser
     {
         static Task<bool> PlatformOpenAsync(Uri uri, BrowserLaunchMode launchMode) =>
-            throw new System.PlatformNotSupportedException();
+            Task.FromResult(NSWorkspace.SharedWorkspace.OpenUrl(new NSUrl(uri.AbsoluteUri)));
     }
 }
