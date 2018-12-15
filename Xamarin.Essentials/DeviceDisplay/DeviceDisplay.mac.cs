59cc74aaf36d05704c605e1285fa02cdd7406f20
Matthew Leibowitz <mattleibow@live.com>
2018-12-15 20:38:59 +0200

    [macos] Added DeviceDisplay APIs

--- Xamarin.Essentials/DeviceDisplay/DeviceDisplay.mac.cs
+++ Xamarin.Essentials/DeviceDisplay/DeviceDisplay.mac.cs
@@ -1,17 +1,94 @@
-﻿namespace Xamarin.Essentials
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using AppKit;
+using CoreFoundation;
+using Foundation;
+using ObjCRuntime;
+
+namespace Xamarin.Essentials
 {
     public static partial class DeviceDisplay
     {
+        const uint kIOPMAssertionLevelOff = 0;
+        const uint kIOPMAssertionLevelOn = 255;
+        static readonly CFString keepScreenOnName = "PreventUserIdleDisplaySleep";
+        static readonly CFString kIOPMAssertionTypePreventUserIdleDisplaySleep = "PreventUserIdleDisplaySleep";
+
+        [DllImport("/System/Library/Frameworks/IOKit.framework/IOKit")]
+        static extern uint IOPMAssertionCreateWithName(IntPtr type, uint level, IntPtr name, out uint id);
+
+        [DllImport("/System/Library/Frameworks/IOKit.framework/IOKit")]
+        static extern uint IOPMAssertionRelease(uint id);
+
+        static uint keepScreenOnId = 0;
+        static NSObject screenMetricsObserver;
+
         static bool PlatformKeepScreenOn
         {
-            get => throw new System.PlatformNotSupportedException();
-            set => throw new System.PlatformNotSupportedException();
+            get
+            {
+                return keepScreenOnId != 0;
+            }
+
+            set
+            {
+                if (KeepScreenOn == value)
+                    return;
+
+                if (value)
+                {
+                    var result = IOPMAssertionCreateWithName(
+                        kIOPMAssertionTypePreventUserIdleDisplaySleep.Handle,
+                        kIOPMAssertionLevelOn,
+                        keepScreenOnName.Handle,
+                        out keepScreenOnId);
+
+                    // failed to turn on
+                    if (result != 0 && value)
+                        keepScreenOnId = 0;
+                }
+                else
+                {
+                    var result = IOPMAssertionRelease(keepScreenOnId);
+
+                    // successfully turned off
+                    if (result == 0 && !value)
+                        keepScreenOnId = 0;
+                }
+            }
         }
 
-        static DisplayInfo GetMainDisplayInfo() => throw new System.PlatformNotSupportedException();
+        static DisplayInfo GetMainDisplayInfo()
+        {
+            var frame = NSScreen.MainScreen.Frame;
+            var scale = NSScreen.MainScreen.BackingScaleFactor;
 
-        static void StartScreenMetricsListeners() => throw new System.PlatformNotSupportedException();
+            return new DisplayInfo(
+                width: frame.Width,
+                height: frame.Height,
+                density: scale,
+                orientation: DisplayOrientation.Portrait,
+                rotation: DisplayRotation.Rotation0);
+        }
 
-        static void StopScreenMetricsListeners() => throw new System.PlatformNotSupportedException();
+        static void StartScreenMetricsListeners()
+        {
+            if (screenMetricsObserver == null)
+            {
+                screenMetricsObserver = NSNotificationCenter.DefaultCenter.AddObserver(NSApplication.DidChangeScreenParametersNotification, OnDidChangeScreenParameters);
+            }
+        }
+
+        static void StopScreenMetricsListeners()
+        {
+            screenMetricsObserver?.Dispose();
+        }
+
+        static void OnDidChangeScreenParameters(NSNotification notification)
+        {
+            var metrics = GetMainDisplayInfo();
+            OnMainDisplayInfoChanged(metrics);
+        }
     }
 }
