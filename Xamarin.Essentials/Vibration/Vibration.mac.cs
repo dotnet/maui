93f9a11b215d956e717dba148285a16a72492e84
Matthew Leibowitz <mattleibow@live.com>
2018-12-15 20:42:28 +0200

    [macos] Throw in Vibration APIs

--- Xamarin.Essentials/Vibration/Vibration.mac.cs
+++ Xamarin.Essentials/Vibration/Vibration.mac.cs
@@ -4,13 +4,13 @@ namespace Xamarin.Essentials
 {
     public static partial class Vibration
     {
-        internal static bool IsSupported
-            => throw new System.PlatformNotSupportedException();
+        internal static bool IsSupported =>
+            throw new PlatformNotSupportedException();
 
-        static void PlatformVibrate(TimeSpan duration)
-            => throw new System.PlatformNotSupportedException();
+        static void PlatformVibrate(TimeSpan duration) =>
+            throw new PlatformNotSupportedException();
 
-        static void PlatformCancel()
-            => throw new System.PlatformNotSupportedException();
+        static void PlatformCancel() =>
+            throw new PlatformNotSupportedException();
     }
 }
