fab4e5a30c0feececb212a3eae512b3357de8bf6
Matthew Leibowitz <mattleibow@live.com>
2018-12-16 02:39:56 +0200

    [macos] Added Geolocation APIs

--- Xamarin.Essentials/Geolocation/Geolocation.mac.cs
+++ Xamarin.Essentials/Geolocation/Geolocation.mac.cs
@@ -1,15 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace Xamarin.Essentials
+﻿namespace Xamarin.Essentials
 {
     public static partial class Geolocation
     {
-        static Task<Location> PlatformLastKnownLocationAsync() =>
-            throw new System.PlatformNotSupportedException();
-
-        static Task<Location> PlatformLocationAsync(GeolocationRequest request, CancellationToken cancellationToken) =>
-            throw new System.PlatformNotSupportedException();
     }
 }
--- Xamarin.Essentials/Permissions/Permissions.mac.cs
+++ Xamarin.Essentials/Permissions/Permissions.mac.cs
@@ -1,16 +1,105 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using CoreLocation;
+using Foundation;
 
 namespace Xamarin.Essentials
 {
     internal static partial class Permissions
     {
-        static void PlatformEnsureDeclared(PermissionType permission) =>
-            throw new System.PlatformNotSupportedException();
+        static void PlatformEnsureDeclared(PermissionType permission)
+        {
+            var info = NSBundle.MainBundle.InfoDictionary;
 
-        static Task<PermissionStatus> PlatformCheckStatusAsync(PermissionType permission) =>
-            throw new System.PlatformNotSupportedException();
+            if (permission == PermissionType.LocationWhenInUse)
+            {
+                // NOTE: This is not a problem as macOS has a "default" message. But, this is still something
+                //       the developer must do. We use a Console instead of a Debug because we always want to
+                //       print this message.
+                if (!info.ContainsKey(new NSString("NSLocationWhenInUseUsageDescription")))
+                    Console.WriteLine("You must set `NSLocationWhenInUseUsageDescription` in your Info.plist file to enable a user-friendly message.");
+            }
+        }
 
-        static Task<PermissionStatus> PlatformRequestAsync(PermissionType permission) =>
-            throw new System.PlatformNotSupportedException();
+        static Task<PermissionStatus> PlatformCheckStatusAsync(PermissionType permission)
+        {
+            EnsureDeclared(permission);
+
+            switch (permission)
+            {
+                case PermissionType.LocationWhenInUse:
+                    return Task.FromResult(GetLocationStatus());
+            }
+
+            return Task.FromResult(PermissionStatus.Granted);
+        }
+
+        static async Task<PermissionStatus> PlatformRequestAsync(PermissionType permission)
+        {
+            // Check status before requesting first and only request if Unknown
+            var status = await PlatformCheckStatusAsync(permission);
+            if (status != PermissionStatus.Unknown)
+                return status;
+
+            EnsureDeclared(permission);
+
+            switch (permission)
+            {
+                case PermissionType.LocationWhenInUse:
+                    if (!MainThread.IsMainThread)
+                        throw new PermissionException("Permission request must be invoked on main thread.");
+
+                    return await RequestLocationAsync();
+            }
+
+            return PermissionStatus.Granted;
+        }
+
+        static PermissionStatus GetLocationStatus()
+        {
+            if (!CLLocationManager.LocationServicesEnabled)
+                return PermissionStatus.Disabled;
+
+            var status = CLLocationManager.Status;
+
+            switch (status)
+            {
+                case CLAuthorizationStatus.AuthorizedAlways:
+                case CLAuthorizationStatus.AuthorizedWhenInUse:
+                    return PermissionStatus.Granted;
+                case CLAuthorizationStatus.Denied:
+                    return PermissionStatus.Denied;
+                case CLAuthorizationStatus.Restricted:
+                    return PermissionStatus.Restricted;
+                default:
+                    return PermissionStatus.Unknown;
+            }
+        }
+
+        static CLLocationManager locationManager;
+
+        static Task<PermissionStatus> RequestLocationAsync()
+        {
+            locationManager = new CLLocationManager();
+
+            var tcs = new TaskCompletionSource<PermissionStatus>(locationManager);
+
+            locationManager.AuthorizationChanged += LocationAuthCallback;
+            locationManager.StartUpdatingLocation();
+            locationManager.StopUpdatingLocation();
+
+            return tcs.Task;
+
+            void LocationAuthCallback(object sender, CLAuthorizationChangedEventArgs e)
+            {
+                if (e.Status == CLAuthorizationStatus.NotDetermined)
+                    return;
+
+                locationManager.AuthorizationChanged -= LocationAuthCallback;
+                tcs.TrySetResult(GetLocationStatus());
+                locationManager.Dispose();
+                locationManager = null;
+            }
+        }
     }
 }
