05cadb92321d58142e37cab9a0ae29c531aa27f9
James Montemagno <james.montemagno@gmail.com>
2018-12-22 11:28:34 -0800

    GH-182 Color/Point/Rect/Size Extension Converters (#651)
    * Add extension helpers for iOS and Android
    
    * Add UWP colors and adjsut android colors.
    
    * Add UWP Point/Rect/Size
    
    * Rename and optimize!
    
    * Add tests and color helpers!
    
    * Fix unit test and add WithAlpha
    
    * Cleanup iOS as it stores it from 0-1
    
    * Tests are all green!
    
    * Add generate-docs cmd :)
    
    * Add docs config to all :)
    
    * Additional clenaup for docs
    
    * Added some docs
    
    * Updated the mdoc target and regenerated the docs

---
+++ Xamarin.Essentials/Types/PlatformExtensions/ColorExtensions.uwp.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+using WindowsColor = Windows.UI.Color;
+
+namespace Xamarin.Essentials
+{
+    public static partial class ColorExtensions
+    {
+        public static Color ToSystemColor(this WindowsColor color) =>
+            Color.FromArgb(color.A, color.R, color.G, color.B);
+
+        public static WindowsColor ToPlatformColor(this Color color) =>
+               WindowsColor.FromArgb(color.A, color.R, color.G, color.B);
+    }
+}
