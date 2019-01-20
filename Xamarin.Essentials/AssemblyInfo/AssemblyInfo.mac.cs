81f8aacc506a8f2cba8c4796e33cbdd4cf1f30b4
Matthew Leibowitz <mattleibow@live.com>
2019-01-20 03:37:43 +0200

    [macos] Updated from the 1.1.0 branch

---
+++ Xamarin.Essentials/AssemblyInfo/AssemblyInfo.mac.cs
@@ -0,0 +1,3 @@
+﻿using Foundation;
+
+[assembly: LinkerSafe]
---
+++ Xamarin.Essentials/Types/PlatformExtensions/ColorExtensions.mac.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using AppKit;
+
+namespace Xamarin.Essentials
+{
+    public static partial class ColorExtensions
+    {
+        public static Color ToSystemColor(this NSColor color)
+        {
+            color.GetRgba(out var red, out var green, out var blue, out var alpha);
+            return Color.FromArgb((int)(alpha * 255), (int)(red * 255), (int)(green * 255), (int)(blue * 255));
+        }
+
+        public static NSColor ToPlatformColor(this Color color) =>
+            NSColor.FromRgba(color.R, color.G, color.B, color.A);
+    }
+}
