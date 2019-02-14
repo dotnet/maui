55110c8324bd5782938c086ef55a003f956153ab
Stephane Delcroix <stephane@delcroix.org>
2019-02-14 20:08:16 +0100

    [C] Binding null on nullable (#5255)
    * fixes #5274
    
    bindings should allow null for nullable valuetypes!
    
    * [C] Binding null on nullable
    
    @kicsiede's fix for #5242, plus unit test, targetted to 3.6.0
    
    - closes #5242

--- Xamarin.Forms.Core/BindingExpression.cs
+++ Xamarin.Forms.Core/BindingExpression.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -429,7 +429,7 @@ namespace Xamarin.Forms
 		internal static bool TryConvert(ref object value, BindableProperty targetProperty, Type convertTo, bool toTarget)
 		{
 			if (value == null)
-				return !convertTo.GetTypeInfo().IsValueType;
+				return !convertTo.GetTypeInfo().IsValueType || Nullable.GetUnderlyingType(convertTo) != null;
 			if ((toTarget && targetProperty.TryConvert(ref value)) || (!toTarget && convertTo.IsInstanceOfType(value)))
 				return true;
 
