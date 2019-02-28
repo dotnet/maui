a9af5920060e32d9871a9fee0549308b9aa84ea1
Shane Neuville <shane94@hotmail.com>
2019-02-27 18:04:52 -0700

    [Android] move android renderers to separate nuget (#5405)
    * [Android] move renderers to separate nuget
    
    * additional android dependency
    
    * rebase with material changes
    
    * add android to material check
    
    * simplify assembly info
    
    * - add context to Init for future cases

---
+++ Xamarin.Forms.Material.Android/FormsMaterial.cs
@@ -0,0 +1,16 @@
+﻿using Android.Content;
+using Android.OS;
+
+namespace Xamarin.Forms
+{
+    public static class FormsMaterial
+    {
+        public static void Init(Context context, Bundle bundle)
+        {
+			// my only purpose is to exist so when called
+			// this dll doesn't get removed
+
+			VisualMarker.RegisterMaterial();
+        }
+    }
+}
