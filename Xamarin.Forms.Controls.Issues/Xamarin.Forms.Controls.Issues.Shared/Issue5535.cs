d1dc874dabc9fcfc8ef8a6dbb2d3db34e0f403b3
E.Z. Hart <hartez@users.noreply.github.com>
2019-04-03 10:49:05 -0600

    Allow EmptyView swapping in Android CollectionView (#5720) fixes #5535
    * Automated test for issue
    
    * Fix empty view swapping for Android (fixes #5535)
    
    * Remove private modifier
    
    * Account for null Flags when setting flags for test
    
    * Fix rebase issue

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue5535.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.CollectionView)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 5535, "CollectionView: Swapping EmptyViews has no effect",
+		PlatformAffected.iOS | PlatformAffected.Android)]
+	public class Issue5535 : TestNavigationPage
+	{
+		protected override void Init()
+		{
+#if APP
+			Device.SetFlags(new List<string>(Device.Flags ?? new List<string>()) { "CollectionView_Experimental" });
+
+			PushAsync(new GalleryPages.CollectionViewGalleries.EmptyViewGalleries.EmptyViewSwapGallery());
+#endif
+		}
+
+#if UITEST
+		[Test]
+		public void SwappingEmptyViews()
+		{
+			RunningApp.WaitForElement("FilterItems");
+			RunningApp.Tap("FilterItems");	
+			RunningApp.EnterText("abcdef");
+			RunningApp.PressEnter();
+		
+			// Default empty view
+			RunningApp.WaitForElement("Nothing to see here.");
+
+			RunningApp.Tap("ToggleEmptyView");	
+
+			// Other empty view
+			RunningApp.WaitForElement("No results matched your filter.");
+		}
+#endif
+	}
+}
