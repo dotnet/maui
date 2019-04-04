09591bc4e5b83f38d80ff8f33f447c3406796442
Andres G. Aragoneses <knocte@gmail.com>
2019-04-04 19:23:45 +0800

    [Core] Add some Device.InvokeOnMainThread helpers (#5028)
    * [Core] Add some Device.InvokeOnMainThread helpers
    
    These helpers keep popping up on forums/stackoverflow[1][2],
    would be nice to include them out of the box.
    
    [1] https://forums.xamarin.com/discussion/51720/wait-for-device-begininvokeonmainthread
    [2] https://stackoverflow.com/a/47941859
    
    * [Core] Add a helper to get the mainthread's SynchronizationContext
    
    It might be handy to expose it, so that F# devs can use it when
    employing the Async.SwitchToContext API.

--- Xamarin.Forms.Core/Device.cs
+++ Xamarin.Forms.Core/Device.cs
@@ -104,6 +104,66 @@ namespace Xamarin.Forms
 			PlatformServices.BeginInvokeOnMainThread(action);
 		}
 
+		public static Task<T> InvokeOnMainThreadAsync<T>(Func<T> func)
+		{
+			var tcs = new TaskCompletionSource<T>();
+			BeginInvokeOnMainThread(() =>
+			{
+				try
+				{
+					var result = func();
+					tcs.SetResult(result);
+				}
+				catch (Exception ex)
+				{
+					tcs.SetException(ex);
+				}
+			});
+			return tcs.Task;
+		}
+
+		public static Task InvokeOnMainThreadAsync(Action action)
+		{
+			Func<object> dummyFunc = () => { action(); return null; };
+			return InvokeOnMainThreadAsync(dummyFunc);
+		}
+
+		public static Task<T> InvokeOnMainThreadAsync<T>(Func<Task<T>> funcTask)
+		{
+			var tcs = new TaskCompletionSource<T>();
+			BeginInvokeOnMainThread(
+				async() =>
+				{
+					try
+					{
+						var ret = await funcTask().ConfigureAwait(false);
+						tcs.SetResult(ret);
+					}
+					catch (Exception e)
+					{
+						tcs.SetException(e);
+					}
+				}
+			);
+
+			return tcs.Task;
+		}
+
+		public static Task InvokeOnMainThreadAsync(Func<Task> funcTask)
+		{
+			Func<Task<object>> dummyFunc = () => { funcTask(); return null; };
+			return InvokeOnMainThreadAsync(dummyFunc);
+		}
+
+		public static async Task<SynchronizationContext> GetMainThreadSynchronizationContextAsync()
+		{
+			SynchronizationContext ret = null;
+			await InvokeOnMainThreadAsync(() =>
+				ret = SynchronizationContext.Current
+			).ConfigureAwait(false);
+			return ret;
+		}
+
 		public static double GetNamedSize(NamedSize size, Element targetElement)
 		{
 			return GetNamedSize(size, targetElement.GetType());
