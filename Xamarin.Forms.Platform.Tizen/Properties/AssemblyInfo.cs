c013f64e48e18c8cd6e0fb74f7483dd0fc8f97cb
yourina <rina6350.you@gmail.com>
2019-04-09 01:54:41 +0900

    Add the ShellRenderer for Tizen (#5818)

--- Xamarin.Forms.Platform.Tizen/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Platform.Tizen/Properties/AssemblyInfo.cs
@@ -14,6 +14,7 @@ using System.Reflection;
 [assembly: ExportRenderer(typeof(NavigationPage), typeof(NavigationPageRenderer))]
 [assembly: ExportRenderer(typeof(MasterDetailPage), typeof(MasterDetailPageRenderer))]
 [assembly: ExportRenderer(typeof(TabbedPage), typeof(TabbedPageRenderer))]
+[assembly: ExportRenderer(typeof(Shell), typeof(ShellRenderer))]
 
 [assembly: ExportRenderer(typeof(Label), typeof(LabelRenderer))]
 [assembly: ExportRenderer(typeof(Button), typeof(ButtonRenderer))]
