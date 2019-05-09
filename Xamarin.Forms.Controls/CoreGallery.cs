9ffa2b44f378d57a9a240af46d496f035348d9cc
Pavel Yakovlev <v-payako@microsoft.com>
2019-05-09 19:49:01 +0300

    Added memory leak gallery (#5795)
    * Added memory leak gallery
    
    * fixes getting content

--- Xamarin.Forms.Controls/CoreGallery.cs
+++ Xamarin.Forms.Controls/CoreGallery.cs
@@ -282,6 +282,7 @@ namespace Xamarin.Forms.Controls
 		}
 
 		List<GalleryPageFactory> _pages = new List<GalleryPageFactory> {
+				new GalleryPageFactory(() => new MemoryLeakGallery(), "Memory Leak"),
 				new GalleryPageFactory(() => new Issues.A11yTabIndex(), "Accessibility TabIndex"),
 				new GalleryPageFactory(() => new FontImageSourceGallery(), "Font ImageSource"),
 				new GalleryPageFactory(() => new CollectionViewGallery(), "CollectionView Gallery"),
