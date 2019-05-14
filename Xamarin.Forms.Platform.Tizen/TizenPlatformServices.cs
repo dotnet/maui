8f7b8aa8ecdd51ba412199a4b084fda28cb0df76
Kangho Hur <rookiejava@gmail.com>
2019-05-15 04:30:55 +0900

    [Tizen] Support new Named Font Sizes for Tizen (#6113)

--- Xamarin.Forms.Platform.Tizen/TizenPlatformServices.cs
+++ Xamarin.Forms.Platform.Tizen/TizenPlatformServices.cs
@@ -67,7 +67,22 @@ namespace Xamarin.Forms.Platform.Tizen
 					pt = Device.Idiom == TargetIdiom.TV ? 28 : (Device.Idiom == TargetIdiom.Watch ? 32 : 25);
 					break;
 				case NamedSize.Large:
-					pt = Device.Idiom == TargetIdiom.TV ? 84 : (Device.Idiom == TargetIdiom.Watch ? 36 : 31);
+					pt = Device.Idiom == TargetIdiom.TV ? 32 : (Device.Idiom == TargetIdiom.Watch ? 36 : 31);
+					break;
+				case NamedSize.Body:
+					pt = Device.Idiom == TargetIdiom.TV ? 30 : (Device.Idiom == TargetIdiom.Watch ? 32 : 28);
+					break;
+				case NamedSize.Caption:
+					pt = Device.Idiom == TargetIdiom.TV ? 26 : (Device.Idiom == TargetIdiom.Watch ? 24 : 22);
+					break;
+				case NamedSize.Header:
+					pt = Device.Idiom == TargetIdiom.TV ? 84 : (Device.Idiom == TargetIdiom.Watch ? 36 : 138);
+					break;
+				case NamedSize.Subtitle:
+					pt = Device.Idiom == TargetIdiom.TV ? 30 : (Device.Idiom == TargetIdiom.Watch ? 30 : 28);
+					break;
+				case NamedSize.Title:
+					pt = Device.Idiom == TargetIdiom.TV ? 42 : (Device.Idiom == TargetIdiom.Watch ? 36 : 40);
 					break;
 				default:
 					throw new ArgumentOutOfRangeException(nameof(size));
