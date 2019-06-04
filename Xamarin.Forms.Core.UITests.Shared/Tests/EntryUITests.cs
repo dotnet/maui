3bc2e5e94b5be9e7ac89d331021432e6c3979eae
E.Z. Hart <hartez@users.noreply.github.com>
2019-06-04 12:28:21 -0600

    Speed up core UI tests and reduce flakiness (#6395)
    * Speed up ViewUITests and hopefully reduce flakiness
    
    * Make focus tests work even when keyboard pops up
    
    * Move Date/Time picker tests to the end so they can't break the other bubbling tests
    Add delay before trying to dismiss dialogs so they can fully appear
    
    * Fix accidental triggering of Picker when removing focus from Entry
    
    * Bump up number of tests before restart

--- Xamarin.Forms.Core.UITests.Shared/Tests/EntryUITests.cs
+++ Xamarin.Forms.Core.UITests.Shared/Tests/EntryUITests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using Xamarin.Forms.Controls.Issues;
 using Xamarin.Forms.CustomAttributes;
+using Xamarin.UITest.Queries;
 
 namespace Xamarin.Forms.Core.UITests
 {
@@ -25,14 +26,18 @@ namespace Xamarin.Forms.Core.UITests
 		{
 			var remote = new StateViewContainerRemote(App, Test.VisualElement.Focus, PlatformViewType);
 			remote.GoTo();
-			bool isFocused = System.Convert.ToBoolean( App.Query("FocusStateLabel")[0].ReadText());
-			Assert.IsFalse(isFocused);
+			
+			Assert.IsFalse(IsFocused());
 			remote.TapView();
-			isFocused = System.Convert.ToBoolean(App.Query("FocusStateLabel")[0].ReadText());
-			Assert.IsTrue(isFocused);
-			App.Tap("FocusStateLabel");
-			isFocused = System.Convert.ToBoolean(App.Query("FocusStateLabel")[0].ReadText());
-			Assert.IsFalse(isFocused);
+			Assert.IsTrue(IsFocused());
+			App.Tap("Go"); // Won't do anything, we just need to take focus away from the Entry
+			Assert.IsFalse(IsFocused());
+		}
+
+		bool IsFocused()
+		{
+			var focusedText = App.Query(q => q.Marked("FocusStateLabel").All())[0].ReadText();
+			return System.Convert.ToBoolean(focusedText);
 		}
 
 		[UiTestExempt(ExemptReason.CannotTest, "Invalid interaction")]
