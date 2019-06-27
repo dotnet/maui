6c200617357321b862ebffa58820050a55ba616f
adrianknight89 <adrianknight89@outlook.com>
2019-06-27 18:37:25 -0500

    make ShouldChangeText virtual (#6634)

--- Xamarin.Forms.Platform.iOS/Renderers/EditorRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/EditorRenderer.cs
@@ -331,7 +331,7 @@ namespace Xamarin.Forms.Platform.iOS
 				TextView.Text = currentControlText.Substring(0, Element.MaxLength);
 		}
 
-		bool ShouldChangeText(UITextView textView, NSRange range, string text)
+		protected virtual bool ShouldChangeText(UITextView textView, NSRange range, string text)
 		{
 			var newLength = textView.Text.Length + text.Length - range.Length;
 			return newLength <= Element.MaxLength;
@@ -364,10 +364,7 @@ namespace Xamarin.Forms.Platform.iOS
 
 			public override RectangleF Frame
 			{
-				get
-				{
-					return base.Frame;
-				}
+				get => base.Frame;
 				set
 				{
 					base.Frame = value;
