dd8618e14feaab31301322eba64d9b6214eb1d28
adrianknight89 <adrianknight89@outlook.com>
2019-07-03 23:09:02 -0500

    Create UI test template for XAML pages (#5853)
    * create template for XAML pages
    
    * update existing template
    
    * added BindingContext
    
    * removed whitespace
    
    * move BindingContext
    
    * change class name to Issue
    
    * fixed absolute path

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/_TemplateMarkup.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/_TemplateMarkup.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
 
 #if UITEST
 using Xamarin.Forms.Core.UITests;
@@ -12,49 +13,52 @@ namespace Xamarin.Forms.Controls.Issues
 #if UITEST
 	[Category(UITestCategories.ManualReview)]
 #endif
+#if APP
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
 	[Preserve(AllMembers = true)]
-	[Issue(IssueTracker.Github, 1, "Issue Description", PlatformAffected.Default)]
-	public class Issue1 : TestContentPage // or TestMasterDetailPage, etc ...
+	[Issue(IssueTracker.Github, 2, "Issue Description (Markup)", PlatformAffected.Default)]
+	public partial class _TemplateMarkup : TestContentPage
 	{
+		public _TemplateMarkup()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
 		protected override void Init()
 		{
-			// Initialize ui here instead of ctor
-			Content = new Label
-			{
-				AutomationId = "Issue1Label",
-				Text = "See if I'm here"
-			};
-
-			BindingContext = new ViewModelIssue1();
+			BindingContext = new ViewModelIssue2();
 		}
 
 #if UITEST
 		[Test]
-		public void Issue1Test() 
+		public void Issue2Test()
 		{
 			// Delete this and all other UITEST sections if there is no way to automate the test. Otherwise, be sure to rename the test and update the Category attribute on the class. Note that you can add multiple categories.
-			RunningApp.Screenshot("I am at Issue1");
-			RunningApp.WaitForElement(q => q.Marked("Issue1Label"));
+			RunningApp.Screenshot("I am at Issue2");
+			RunningApp.WaitForElement(q => q.Marked("Issue2Label"));
 			RunningApp.Screenshot("I see the Label");
 		}
 #endif
 	}
 
 	[Preserve(AllMembers = true)]
-	public class ViewModelIssue1
+	public class ViewModelIssue2
 	{
-		public ViewModelIssue1()
+		public ViewModelIssue2()
 		{
-
+			
 		}
 	}
 
 	[Preserve(AllMembers = true)]
-	public class ModelIssue1
+	public class ModelIssue2
 	{
-		public ModelIssue1()
+		public ModelIssue2()
 		{
-
+			
 		}
 	}
 }
