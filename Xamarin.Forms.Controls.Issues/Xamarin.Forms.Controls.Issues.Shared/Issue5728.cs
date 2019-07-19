96a4c2471fd001672a2efabd7c461db59b554fc5
Alan Grgic <alan.grgic@gmail.com>
2019-07-19 05:58:59 -0500

    [iOS] Fix initial color of RefreshControl in ListViews (#6473) fixes #5728
    * add reproduction
    
    * make beginrefreshing occur after offset animation and ensure color is set before beginning refresh

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue5728.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 5728, "ListView RefreshControlColor initial", PlatformAffected.iOS)]
+	public class Issue5728 : ContentPage
+	{
+		readonly ListView _listView;
+		public Issue5728()
+		{
+			_listView = new ListView
+			{
+				BackgroundColor = Color.Transparent,
+				IsPullToRefreshEnabled = true,
+				RefreshControlColor = Color.Cyan
+			};
+			_listView.Refreshing += HandleListViewRefreshing;
+			Content = _listView;
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			_listView.BeginRefresh();
+		}
+
+		async void HandleListViewRefreshing(object sender, EventArgs e)
+		{
+			await Task.Delay(1500);
+			_listView.EndRefresh();
+		}
+	}
+}
