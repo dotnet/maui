86650d419f05fff4aff51d239ed3b62acf27d514
Shane Neuville <shneuvil@microsoft.com>
2019-08-15 16:23:12 -0600

    Replace IsDesignModeEnabled check with original Context Type Check (#7143)
    * remove use of DesignMode just store check in static bool
    
    * - fix set
    
    * - fix last check
    
    * - spelling fix

--- Xamarin.Forms.Platform.Android/ContextExtensions.cs
+++ Xamarin.Forms.Platform.Android/ContextExtensions.cs
@@ -115,7 +115,23 @@ namespace Xamarin.Forms.Platform.Android
 			return null;
 		}
 
-		internal static bool IsDesignerContext(this Context context) => DesignMode.IsDesignModeEnabled;
+		static bool? _isDesignerContext;
+		internal static bool IsDesignerContext(this Context context)
+		{
+			if (_isDesignerContext.HasValue)
+				return _isDesignerContext.Value;
+
+			if (context == null)
+				_isDesignerContext = false;
+			else if ($"{context}".Contains("com.android.layoutlib.bridge.android.BridgeContext"))
+				_isDesignerContext = true;
+			else if (context is ContextWrapper contextWrapper)
+				return contextWrapper.BaseContext.IsDesignerContext();
+			else
+				_isDesignerContext = false;
+
+			return _isDesignerContext.Value;
+		}
 
 		public static AFragmentManager GetFragmentManager(this Context context)
 		{
