bbd13a9e6a66e406622da88ac78376028cf1043c
Samantha Houts <samantha.houts@xamarin.com>
2019-08-16 18:22:38 -0700

    Update Forms.cs

--- Xamarin.Forms.Platform.iOS/Forms.cs
+++ Xamarin.Forms.Platform.iOS/Forms.cs
@@ -38,7 +38,6 @@ namespace Xamarin.Forms
 		static bool? s_isiOS11OrNewer;
 		static bool? s_isiOS13OrNewer;
 		static bool? s_respondsTosetNeedsUpdateOfHomeIndicatorAutoHidden;
-		static bool? s_isiOS13OrNewer;
 #endif
 
 #if __MOBILE__
@@ -90,16 +89,6 @@ namespace Xamarin.Forms
 				if (!s_respondsTosetNeedsUpdateOfHomeIndicatorAutoHidden.HasValue)
 					s_respondsTosetNeedsUpdateOfHomeIndicatorAutoHidden = new UIViewController().RespondsToSelector(new ObjCRuntime.Selector("setNeedsUpdateOfHomeIndicatorAutoHidden"));
 				return s_respondsTosetNeedsUpdateOfHomeIndicatorAutoHidden.Value;
-			}
-		}
-
-		internal static bool IsiOS13OrNewer
-		{
-			get
-			{
-				if (!s_isiOS13OrNewer.HasValue)
-					s_isiOS13OrNewer = UIDevice.CurrentDevice.CheckSystemVersion(13, 0);
-				return s_isiOS13OrNewer.Value;
 			}
 		}
 #endif
