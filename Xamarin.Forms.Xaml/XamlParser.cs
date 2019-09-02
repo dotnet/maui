52587223a936df785f8c8bd073915062bc36c813
Stephane Delcroix <stephane@delcroix.org>
2019-09-02 10:49:59 +0200

    [X] handle CDATA explicit properties (#7342)
    - fixes https://github.com/xamarin/Xamarin.Forms/pull/4527#issuecomment-525979139

--- Xamarin.Forms.Xaml/XamlParser.cs
+++ Xamarin.Forms.Xaml/XamlParser.cs
@@ -178,6 +178,7 @@ namespace Xamarin.Forms.Xaml
 							return node;
 						break;
 					case XmlNodeType.Text:
+					case XmlNodeType.CDATA:
 						node = new ValueNode(reader.Value.Trim(), (IXmlNamespaceResolver)reader, ((IXmlLineInfo)reader).LineNumber,
 							((IXmlLineInfo)reader).LinePosition);
 						nodes.Add(node);
