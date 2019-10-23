7055c8975f799e766d4f666d0452e2b2ff58a30c
Mikolaj Kieres <mkieres@users.noreply.github.com>
2019-10-24 05:06:24 +1100

    Adding instructions to the UI Test (issue #38770) (#8062)

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla38770.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla38770.cs
@@ -1,5 +1,8 @@
-﻿using Xamarin.Forms.CustomAttributes;
+﻿using System;
+using System.Linq;
+using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
+using System.Collections.Generic;
 
 #if UITEST
 using Xamarin.UITest;
@@ -12,6 +15,9 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue(IssueTracker.Bugzilla, 38770, "RaiseChild and LowerChild do not work on Windows", PlatformAffected.WinRT)]
 	public class Bugzilla38770 : TestContentPage
 	{
+		StackLayout _boxStack;
+		Label _colorsPositionLabel;
+
 		protected override void Init()
 		{
 			var red = new BoxView
@@ -34,11 +40,11 @@ namespace Xamarin.Forms.Controls.Issues
 				HeightRequest = 50,
 				TranslationX = -25
 			};
-			var boxStack = new StackLayout
+			_boxStack = new StackLayout
 			{
 				Orientation = StackOrientation.Horizontal,
 				Spacing = 0,
-				Margin = new Thickness(0,50,0,0),
+				Margin = new Thickness(0, 50, 0, 0),
 				HorizontalOptions = LayoutOptions.Center,
 				Children =
 				{
@@ -47,6 +53,7 @@ namespace Xamarin.Forms.Controls.Issues
 					blue
 				}
 			};
+			_boxStack.ChildrenReordered += BoxStackOnChildrenReordered;
 
 			var raiseButtons = new StackLayout
 			{
@@ -58,19 +65,19 @@ namespace Xamarin.Forms.Controls.Issues
 					{
 						Text = "Raise Red",
 						WidthRequest = 110,
-						Command = new Command(() => boxStack.RaiseChild(red))
+						Command = new Command(() => _boxStack.RaiseChild(red))
 					},
 					new Button
 					{
 						Text = "Raise Green",
 						WidthRequest = 110,
-						Command = new Command(() => boxStack.RaiseChild(green))
+						Command = new Command(() => _boxStack.RaiseChild(green))
 					},
 					new Button
 					{
 						Text = "Raise Blue",
 						WidthRequest = 110,
-						Command = new Command(() => boxStack.RaiseChild(blue))
+						Command = new Command(() => _boxStack.RaiseChild(blue))
 					}
 				}
 			};
@@ -84,19 +91,62 @@ namespace Xamarin.Forms.Controls.Issues
 					{
 						Text = "Lower Red",
 						WidthRequest = 110,
-						Command = new Command(() => boxStack.LowerChild(red))
+						Command = new Command(() => _boxStack.LowerChild(red))
 					},
 					new Button
 					{
 						Text = "Lower Green",
 						WidthRequest = 110,
-						Command = new Command(() => boxStack.LowerChild(green))
+						Command = new Command(() => _boxStack.LowerChild(green))
 					},
 					new Button
 					{
 						Text = "Lower Blue",
 						WidthRequest = 110,
-						Command = new Command(() => boxStack.LowerChild(blue))
+						Command = new Command(() => _boxStack.LowerChild(blue))
+					}
+				}
+			};
+
+			_colorsPositionLabel = new Label
+			{
+				FormattedText = new FormattedString()
+			};
+			FormatColorsChildrenPositionText();
+
+			var colorsPositionStack = new StackLayout()
+			{
+				Margin = new Thickness(0, 50, 0, 0),
+				Children =
+				{
+					new Label()
+					{
+						Text = "Colors collection order (i.e. z-index)"
+					},
+					_colorsPositionLabel
+				}
+			};
+
+			var instructions = new StackLayout()
+			{
+				Margin = new Thickness(0, 50, 0, 0),
+				HorizontalOptions = LayoutOptions.Center,
+				Children =
+				{
+					new Label()
+					{
+						Text = "When LOWERING an item it's being moved to the START of the collection of children, therefore decreasing it's z-index",
+						FontSize = 15
+					},
+					new Label()
+					{
+						Text = "When RAISING an item it's being moved to the END of the list of children, therefore increasing its z-index",
+						FontSize = 15
+					},
+					new Label()
+					{
+						Text = "For instance, if you decide to press LOWER GREEN button, then the GREEN color should no longer be visible - it will become the first item in the list (lowest z-index) and therefore it will get covered by RED and BLUE.",
+						Margin = new Thickness(0, 10, 0, 0)
 					}
 				}
 			};
@@ -107,9 +157,46 @@ namespace Xamarin.Forms.Controls.Issues
 				{
 					raiseButtons,
 					lowerButtons,
-					boxStack
+					_boxStack,
+					colorsPositionStack,
+					instructions
 				}
 			};
+		}
+
+		void FormatColorsChildrenPositionText()
+		{
+			_colorsPositionLabel.FormattedText.Spans.Clear();
+			for (var i = 0; i < _boxStack.Children.Count; i++)
+			{
+				if (_boxStack.Children[i].BackgroundColor.R > 0)
+				{
+					_colorsPositionLabel.FormattedText.Spans.Add(new Span()
+					{
+						Text = $"{i} Red\n"
+					});
+					continue;
+				}
+
+				if (_boxStack.Children[i].BackgroundColor.G > 0)
+				{
+					_colorsPositionLabel.FormattedText.Spans.Add(new Span()
+					{
+						Text = $"{i} Green\n"
+					});
+					continue;
+				}
+
+				_colorsPositionLabel.FormattedText.Spans.Add(new Span()
+				{
+					Text = $"{i} Blue\n"
+				});
+			}
+		}
+
+		void BoxStackOnChildrenReordered(object sender, EventArgs e)
+		{
+			FormatColorsChildrenPositionText();
 		}
 	}
 }
