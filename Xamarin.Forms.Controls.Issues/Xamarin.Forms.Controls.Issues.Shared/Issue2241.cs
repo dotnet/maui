578685f6595ad111a031bf4c7f5eae9610e23c4e
adrianknight89 <adrianknight89@outlook.com>
2019-10-24 04:56:06 -0500

    Added instructions and changed the test for Issue2241 (#8080) fixes #2274
    * added instructions
    
    * cache old centerY
    
    * Revert "cache old centerY"
    
    This reverts commit cd75d74654d23359cf3ab72d601cb4109a94cda0.

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue2241.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue2241.cs
@@ -3,6 +3,7 @@
 using Xamarin.Forms;
 using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
+using System.Linq;
 
 #if UITEST
 using NUnit.Framework;
@@ -18,15 +19,52 @@ namespace Xamarin.Forms.Controls.Issues
 	{
 		protected override void Init ()
 		{
-			ScrollView scrollView = new ScrollView () {
+			var grid = new Grid
+			{
+				BackgroundColor = Color.Red,
+				HeightRequest = 400,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				AutomationId = "MainGrid"
+			};
+			grid.RowDefinitions.Add(new RowDefinition { Height = 10 });
+			grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+			grid.RowDefinitions.Add(new RowDefinition { Height = 10 });
+
+			var boxView = new BoxView
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				Color = Color.Yellow
+			};
+			Grid.SetRow(boxView, 0);
+
+			var label = new Label
+			{
+				Text = "If the view is scrollable, scroll down to see the yellow line at the bottom of the red box. Scroll back to top. Rotate the device to landscape mode."
+				+ " Scroll down to see the yellow line again. Rotate the device back to portrait while you are still looking at the yellow line at the bottom of the red box."
+				+ " If the view was originally scrollable, it should still be scrollable. You should still be able to see both of the yellow lines.",
+				LineBreakMode = LineBreakMode.WordWrap,
+				Margin = new Thickness(10, 0),
+				VerticalTextAlignment = TextAlignment.Center
+			};
+			Grid.SetRow(label, 1);
+
+			var boxView2 = new BoxView
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				Color = Color.Yellow
+			};
+			Grid.SetRow(boxView2, 2);
+
+			grid.Children.Add(boxView);
+			grid.Children.Add(label);
+			grid.Children.Add(boxView2);
+
+			var scrollView = new ScrollView
+			{
 				HorizontalOptions = LayoutOptions.FillAndExpand,
 				VerticalOptions = LayoutOptions.FillAndExpand,
 				Padding = new Thickness (20),
-				Content = new BoxView () {
-					Color = Color.Red,
-					HeightRequest = 400,
-					HorizontalOptions = LayoutOptions.FillAndExpand
-				}
+				Content = grid
 			};
 
 			Content = scrollView;
@@ -42,22 +80,18 @@ namespace Xamarin.Forms.Controls.Issues
 #else
 			var isAndroid = false;
 #endif
-			var className = "Xamarin_Forms_Platform_iOS_BoxRenderer";
-			if (isAndroid) {
-				className = "BoxRenderer";
-			}
-			var box1 = RunningApp.Query (c => c.Class (className)) [0];
+			var grid1 = RunningApp.Query("MainGrid").Single();
 			RunningApp.SetOrientationLandscape ();
 			RunningApp.ScrollDown ();
 			RunningApp.SetOrientationPortrait ();
-			var box2 = RunningApp.Query (c => c.Class (className)) [0];
+			var grid2 = RunningApp.Query("MainGrid").Single();
 			RunningApp.Screenshot ("Did it resize ok? Do you see some white on the bottom?");
+
 			if (!isAndroid) {
-				Assert.AreEqual (box1.Rect.CenterY, box2.Rect.CenterY);
+				Assert.AreEqual (grid1.Rect.CenterY, grid2.Rect.CenterY);
 			}
 		}
 #endif
 	}
 }
-
 
