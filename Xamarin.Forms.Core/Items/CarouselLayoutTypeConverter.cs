ee1eca51d57a985c2783f110598be3bbf5b5544f
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-12-09 20:57:38 +0100

    Added Converter to simplify the use of Layouts in the CarouselView (#8665)
    fixes #7813

---
+++ Xamarin.Forms.Core/Items/CarouselLayoutTypeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Xamarin.Forms
+{
+	[Xaml.TypeConversion(typeof(LinearItemsLayout))]
+	public class CarouselLayoutTypeConverter : TypeConverter
+	{
+		public override object ConvertFromInvariantString(string value)
+		{
+			if (value == "HorizontalList")
+			{
+				return LinearItemsLayout.CarouselDefault;
+			}
+
+			if (value == "VerticalList")
+			{
+				return LinearItemsLayout.CarouselVertical;
+			}
+
+			throw new InvalidOperationException($"Cannot convert \"{value}\" into {typeof(IItemsLayout)}");
+		}
+	}
+}
