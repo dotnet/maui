7270d912d88b672940a20b76f3d9165473c08392
Janus Weil <janus@gcc.gnu.org>
2020-01-09 18:18:27 +0100

    bugfix for OrientationSensor on iOS (#1050)
    * this is a fix-up for commit 84579595f36a008e172824aa41269596d83d167c
    * quaternions are not commutative, and the original commit had the multiplication in the wrong order
    * it worked for some special cases, but not in general
    * this finally fixes issue #981 (and expands the comments for better explanation)

--- Xamarin.Essentials/OrientationSensor/OrientationSensor.ios.watchos.cs
+++ Xamarin.Essentials/OrientationSensor/OrientationSensor.ios.watchos.cs
@@ -26,12 +26,16 @@ namespace Xamarin.Essentials
 
             var field = data.Attitude.Quaternion;
 
-            // the quaternion returned by the MotionManager refers to a frame where the X axis points north
+            // the quaternion returned by the MotionManager refers to a frame where the X axis points north ("iOS frame")
             var q = new Quaternion((float)field.x, (float)field.y, (float)field.z, (float)field.w);
 
-            // we rotate it by 90 degrees around the Z axis, so that the Y axis points north and the X axis points east
+            // in Xamarin, the reference frame is defined such that Y points north and Z is vertical,
+            // so we first rotate by 90 degrees around the Z axis, in order to get from the Xamarin frame to the iOS frame
             var qz90 = Quaternion.CreateFromAxisAngle(Vector3.UnitZ, (float)(Math.PI / 2.0));
-            q = Quaternion.Multiply(q, qz90);
+
+            // on top of this rotation, we apply the actual quaternion obtained from the MotionManager,
+            // so that the final quaternion will take us from the earth frame in Xamarin convention to the phone frame
+            q = Quaternion.Multiply(qz90, q);
             var rotationData = new OrientationSensorData(q.X, q.Y, q.Z, q.W);
             OnChanged(rotationData);
         }
