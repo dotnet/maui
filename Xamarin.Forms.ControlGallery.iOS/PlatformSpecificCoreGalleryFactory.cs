d6b1aee3af56f8caa5ab82c56112e860303a3964
Shane Neuville <shneuvil@microsoft.com>
2020-01-11 12:19:03 -0700

    Apply cecil fixes to make packages 2017 compatible (#9145)
    * Apply cecil fixes to make packages 2017 compatible
    
    * - version bumps
    
    * - remove extra win builds
    
    * - package fixes
    
    * - conditionally include binding redirect
    
    * - bump type redirector

--- Xamarin.Forms.ControlGallery.iOS/PlatformSpecificCoreGalleryFactory.cs
+++ Xamarin.Forms.ControlGallery.iOS/PlatformSpecificCoreGalleryFactory.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.ControlGallery.iOS;
 using Xamarin.Forms.Controls;
+using Xamarin.Forms.Internals;
 
 [assembly: Dependency(typeof(PlatformSpecificCoreGalleryFactory))]
 
 namespace Xamarin.Forms.ControlGallery.iOS
 {
+	[Preserve(AllMembers = true)]
 	public class PlatformSpecificCoreGalleryFactory : IPlatformSpecificCoreGalleryFactory
 	{
 		public string Title => "iOS Core Gallery";
