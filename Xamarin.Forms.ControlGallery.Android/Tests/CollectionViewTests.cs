436b2f0259f361acc6660a3cca9a688058a9de4e
E.Z. Hart <hartez@users.noreply.github.com>
2020-01-31 18:57:02 -0700

    Rewrite NongreedySnapHelper with older APIs (#9378)
    fixes #9030

---
+++ Xamarin.Forms.ControlGallery.Android/Tests/CollectionViewTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Xamarin.Forms.CustomAttributes;
+
+namespace Xamarin.Forms.ControlGallery.Android.Tests
+{
+	public class CollectionViewTests : PlatformTestFixture 
+	{
+		[Issue(IssueTracker.Github, 9030, "[Bug] ClassNotFoundException when using snap points on API < 23")]
+		[Test(Description = "CollectionView with SnapPointsType set should not crash")]
+		public void SnapPointsDoNotCrashOnOlderAPIs()
+		{
+			var cv = new CollectionView();
+
+			var itemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical)
+			{
+				SnapPointsType = SnapPointsType.Mandatory
+			};
+			cv.ItemsLayout = itemsLayout;
+
+			// Creating the renderer is enough to cause the ClassNotFoundException on older APIs
+			GetRenderer(cv).Dispose();
+		}
+	}
+}
