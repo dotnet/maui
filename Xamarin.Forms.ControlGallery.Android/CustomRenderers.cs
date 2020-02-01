8c3de6fb77bded99c8d1dcb40846ba85613c2a28
Shane Neuville <shneuvil@microsoft.com>
2020-01-31 19:20:30 -0700

    Let users override behavior on Navigation Renderer (#9386)
    - only update toolbaritem specifically changed
    fixes #9360
    fixes #9278

--- Xamarin.Forms.ControlGallery.Android/CustomRenderers.cs
+++ Xamarin.Forms.ControlGallery.Android/CustomRenderers.cs
@@ -23,7 +23,8 @@ using System.Reflection;
 using Android.Text;
 using Android.Text.Method;
 using Xamarin.Forms.Controls.Issues;
-
+using System.IO;
+using AMenuItemCompat = global::Android.Support.V4.View.MenuItemCompat;
 
 [assembly: ExportRenderer(typeof(Issue5461.ScrollbarFadingEnabledFalseScrollView), typeof(ScrollbarFadingEnabledFalseScrollViewRenderer))]
 [assembly: ExportRenderer(typeof(Issue1942.CustomGrid), typeof(Issue1942GridRenderer))]
@@ -46,6 +47,7 @@ using Xamarin.Forms.Controls.Issues;
 [assembly: ExportRenderer(typeof(Xamarin.Forms.Controls.Issues.NoFlashTestNavigationPage), typeof(Xamarin.Forms.ControlGallery.Android.NoFlashTestNavigationPage))]
 [assembly: ExportRenderer(typeof(ShellGestures.TouchTestView), typeof(ShellGesturesTouchTestViewRenderer))]
 [assembly: ExportRenderer(typeof(Issue7249Switch), typeof(Issue7249SwitchRenderer))]
+[assembly: ExportRenderer(typeof(Issue9360.Issue9360NavigationPage), typeof(Issue9360NavigationPageRenderer))]
 
 #if PRE_APPLICATION_CLASS
 #elif FORMS_APPLICATION_ACTIVITY
@@ -54,6 +56,42 @@ using Xamarin.Forms.Controls.Issues;
 #endif
 namespace Xamarin.Forms.ControlGallery.Android
 {
+	public class Issue9360NavigationPageRenderer : Xamarin.Forms.Platform.Android.AppCompat.NavigationPageRenderer
+	{
+		public Issue9360NavigationPageRenderer(Context context) : base(context)
+		{
+		}
+
+		protected override void UpdateMenuItemIcon(Context context, IMenuItem menuItem, ToolbarItem toolBarItem)
+		{
+			if (toolBarItem.Text == "BAD")
+			{
+				toolBarItem = new ToolbarItem
+				{
+					Text = "OK",
+					IconImageSource = ImageSource.FromFile("heart.xml"),
+					Order = toolBarItem.Order,
+					Priority = toolBarItem.Priority
+				};
+
+				if (toolBarItem.IconImageSource is FileImageSource fileImageSource)
+				{
+					var name = Path.GetFileNameWithoutExtension(fileImageSource.File);
+					var id = Xamarin.Forms.Platform.Android.ResourceManager.GetDrawableByName(name);
+					if (id != 0)
+					{
+						var drawable = context.GetDrawable(id);
+						menuItem.SetIcon(drawable);
+						AMenuItemCompat.SetContentDescription(menuItem, new Java.Lang.String("HEART"));
+						return;
+					}
+				}
+			}
+
+			base.UpdateMenuItemIcon(context, menuItem, toolBarItem);
+		}
+	}
+
 	public class NonAppCompatSwitchRenderer : Xamarin.Forms.Platform.Android.SwitchRenderer
 	{
 		public NonAppCompatSwitchRenderer(Context context) : base(context)
