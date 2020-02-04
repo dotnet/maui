f0087cbf5af2b11cca6b5bc6977c1ea75da0dd40
Peter Foot <peter@inthehand.com>
2020-02-04 18:09:49 +0000

    Replacement ActivityIndicator for WPF (#9389) fixes #9372
    * Replaced ActivityIndicator for WPF to use a Windows style progress ring (animated dots).
    
    * Removed private keyword
    
    * Changed foreground color assignment so that Color.Default sets value to Color.Accent to avoid creating a transparent control.

--- Xamarin.Forms.Platform.WPF/Renderers/ActivityIndicatorRenderer.cs
+++ Xamarin.Forms.Platform.WPF/Renderers/ActivityIndicatorRenderer.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WProgressBar = System.Windows.Controls.ProgressBar;
+using Xamarin.Forms.Platform.WPF.Controls;
 
 namespace Xamarin.Forms.Platform.WPF
 {
-	public class ActivityIndicatorRenderer : ViewRenderer<ActivityIndicator, WProgressBar>
+	public class ActivityIndicatorRenderer : ViewRenderer<ActivityIndicator, FormsProgressRing>
 	{
 		protected override void OnElementChanged(ElementChangedEventArgs<ActivityIndicator> e)
 		{
@@ -16,10 +12,10 @@ namespace Xamarin.Forms.Platform.WPF
 			{
 				if (Control == null) // construct and SetNativeControl and suscribe control event
 				{
-					SetNativeControl(new WProgressBar());
+					SetNativeControl(new FormsProgressRing());
 				}
 
-				UpdateIsIndeterminate();
+				UpdateIsActive();
 				UpdateColor();
 			}
 
@@ -28,22 +24,38 @@ namespace Xamarin.Forms.Platform.WPF
 
 		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
 		{
-			base.OnElementPropertyChanged(sender, e);
-
 			if (e.PropertyName == ActivityIndicator.IsRunningProperty.PropertyName)
-				UpdateIsIndeterminate();
+				UpdateIsActive();
 			else if (e.PropertyName == ActivityIndicator.ColorProperty.PropertyName)
 				UpdateColor();
+
+			base.OnElementPropertyChanged(sender, e);
+		}
+
+		public override SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint)
+		{
+			// Restrict control to a square
+			return base.GetDesiredSize(Math.Min(widthConstraint, heightConstraint), Math.Min(widthConstraint, heightConstraint));
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (Element is object)
+			{
+				Element.IsRunning = false;
+			}
+
+			base.Dispose(disposing);
 		}
 
 		void UpdateColor()
 		{
-			Control.UpdateDependencyColor(WProgressBar.ForegroundProperty, Element.Color);
+			Control.UpdateDependencyColor(FormsProgressRing.ForegroundProperty, Element.Color != Color.Default ? Element.Color : Color.Accent);
 		}
 
-		void UpdateIsIndeterminate()
+		void UpdateIsActive()
 		{
-			Control.IsIndeterminate = Element.IsRunning;
+			Control.IsActive = Element.IsRunning;
 		}
 	}
 }
