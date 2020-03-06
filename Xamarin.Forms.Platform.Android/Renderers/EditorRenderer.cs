41343176877e4c7913634675f3b86be673d9f9c7
Andrei <andrei.misiukevich@gmail.com>
2020-03-06 11:25:58 +0300

    fixed typo (#9864)

--- Xamarin.Forms.Platform.Android/Renderers/EditorRenderer.cs
+++ Xamarin.Forms.Platform.Android/Renderers/EditorRenderer.cs
@@ -23,7 +23,7 @@ namespace Xamarin.Forms.Platform.Android
 		{
 		}
 
-		[Obsolete("This constructor is obsolete as of version 2.5. Please use EntryRenderer(Context) instead.")]
+		[Obsolete("This constructor is obsolete as of version 2.5. Please use EditorRenderer(Context) instead.")]
 		[EditorBrowsable(EditorBrowsableState.Never)]
 		public EditorRenderer()
 		{
