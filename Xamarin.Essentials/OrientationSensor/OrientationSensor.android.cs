541445a91d4780260659d88de0d9115ab6c1188d
James Montemagno <james.montemagno@gmail.com>
2020-03-10 14:44:37 -0600

    Update OrientationSensor.android.cs

--- Xamarin.Essentials/OrientationSensor/OrientationSensor.android.cs
+++ Xamarin.Essentials/OrientationSensor/OrientationSensor.android.cs
@@ -43,7 +43,7 @@ namespace Xamarin.Essentials
 
         void ISensorEventListener.OnSensorChanged(SensorEvent e)
         {
-            if (e?.Values?.Count < 4)
+            if ((e?.Values?.Count ?? 0) < 4)
                 return;
 
             var data = new OrientationSensorData(e.Values[0], e.Values[1], e.Values[2], e.Values[3]);
