874383cb4eeef276bd4f60f455b77738a072c17f
James Montemagno <james.montemagno@gmail.com>
2020-03-10 14:42:04 -0600

    Update Accelerometer.android.cs

--- Xamarin.Essentials/Accelerometer/Accelerometer.android.cs
+++ Xamarin.Essentials/Accelerometer/Accelerometer.android.cs
@@ -45,7 +45,7 @@ namespace Xamarin.Essentials
 
         void ISensorEventListener.OnSensorChanged(SensorEvent e)
         {
-            if (e?.Values?.Count < 3)
+            if ((e?.Values?.Count ?? 0) < 3)
                 return;
 
             var data = new AccelerometerData(e.Values[0] / gravity, e.Values[1] / gravity, e.Values[2] / gravity);
