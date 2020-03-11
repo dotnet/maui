1b5e1255b04da808e896e9c95833d3d034704b2f
Matthew Leibowitz <mattleibow@live.com>
2020-03-11 15:52:40 +0200

    Update the permissions

--- Xamarin.Essentials/Permissions/Permissions.mac.cs
+++ Xamarin.Essentials/Permissions/Permissions.mac.cs
@@ -1,109 +1,220 @@
-﻿/*
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using CoreLocation;
 using Foundation;
 
 namespace Xamarin.Essentials
 {
-    internal static partial class Permissions
+    public static partial class Permissions
     {
-        static bool PlatformEnsureDeclared(PermissionType permission, bool throwIfMissing)
-        {
-            var info = NSBundle.MainBundle.InfoDictionary;
+        public static bool IsKeyDeclaredInInfoPlist(string usageKey) =>
+            NSBundle.MainBundle.InfoDictionary.ContainsKey(new NSString(usageKey));
 
-            if (permission == PermissionType.LocationWhenInUse)
+        public static TimeSpan LocationTimeout { get; set; } = TimeSpan.FromSeconds(10);
+
+        public abstract class BasePlatformPermission : BasePermission
+        {
+            protected virtual Func<IEnumerable<string>> RecommendedInfoPlistKeys { get; }
+
+            protected virtual Func<IEnumerable<string>> RequiredInfoPlistKeys { get; }
+
+            public override Task<PermissionStatus> CheckStatusAsync() =>
+                Task.FromResult(PermissionStatus.Granted);
+
+            public override Task<PermissionStatus> RequestAsync() =>
+                Task.FromResult(PermissionStatus.Granted);
+
+            public override void EnsureDeclared()
             {
-                // NOTE: This is not a problem as macOS has a "default" message. But, this is still something
-                //       the developer must do. We use a Console instead of a Debug because we always want to
-                //       print this message.
-                if (!info.ContainsKey(new NSString("NSLocationWhenInUseUsageDescription")))
-                    Console.WriteLine("You must set `NSLocationWhenInUseUsageDescription` in your Info.plist file to enable a user-friendly message.");
+                var plistKeys = RequiredInfoPlistKeys?.Invoke();
+                if (plistKeys != null)
+                {
+                    foreach (var requiredKey in plistKeys)
+                    {
+                        if (!IsKeyDeclaredInInfoPlist(requiredKey))
+                            throw new PermissionException($"You must set `{requiredKey}` in your Info.plist file to use the Permission: {GetType().Name}.");
+                    }
+                }
+
+                plistKeys = RecommendedInfoPlistKeys?.Invoke();
+                if (plistKeys != null)
+                {
+                    foreach (var recommendedKey in plistKeys)
+                    {
+                        // NOTE: This is not a problem as macOS has a "default" message. But, this is still something
+                        //       the developer must do. We use a Console instead of a Debug because we always want to
+                        //       print this message.
+                        if (!IsKeyDeclaredInInfoPlist(recommendedKey))
+                            Console.WriteLine($"You must set `{recommendedKey}` in your Info.plist file to enable a user-friendly message for the Permission: {GetType().Name}.");
+                    }
+                }
             }
 
-            return true;
-        }
-
-        static Task<PermissionStatus> PlatformCheckStatusAsync(PermissionType permission)
-        {
-            EnsureDeclared(permission);
-
-            switch (permission)
+            internal void EnsureMainThread()
             {
-                case PermissionType.LocationWhenInUse:
-                    return Task.FromResult(GetLocationStatus());
-            }
-
-            return Task.FromResult(PermissionStatus.Granted);
-        }
-
-        static async Task<PermissionStatus> PlatformRequestAsync(PermissionType permission)
-        {
-            // Check status before requesting first and only request if Unknown
-            var status = await PlatformCheckStatusAsync(permission);
-            if (status != PermissionStatus.Unknown)
-                return status;
-
-            EnsureDeclared(permission);
-
-            switch (permission)
-            {
-                case PermissionType.LocationWhenInUse:
-                    if (!MainThread.IsMainThread)
-                        throw new PermissionException("Permission request must be invoked on main thread.");
-
-                    return await RequestLocationAsync();
-            }
-
-            return PermissionStatus.Granted;
-        }
-
-        static PermissionStatus GetLocationStatus()
-        {
-            if (!CLLocationManager.LocationServicesEnabled)
-                return PermissionStatus.Disabled;
-
-            var status = CLLocationManager.Status;
-
-            switch (status)
-            {
-                case CLAuthorizationStatus.AuthorizedAlways:
-                case CLAuthorizationStatus.AuthorizedWhenInUse:
-                    return PermissionStatus.Granted;
-                case CLAuthorizationStatus.Denied:
-                    return PermissionStatus.Denied;
-                case CLAuthorizationStatus.Restricted:
-                    return PermissionStatus.Restricted;
-                default:
-                    return PermissionStatus.Unknown;
+                if (!MainThread.IsMainThread)
+                    throw new PermissionException("Permission request must be invoked on main thread.");
             }
         }
 
-        static CLLocationManager locationManager;
-
-        static Task<PermissionStatus> RequestLocationAsync()
+        public partial class EventPermissions : BasePlatformPermission
         {
-            locationManager = new CLLocationManager();
+        }
 
-            var tcs = new TaskCompletionSource<PermissionStatus>(locationManager);
+        public partial class Battery : BasePlatformPermission
+        {
+        }
 
-            locationManager.AuthorizationChanged += LocationAuthCallback;
-            locationManager.StartUpdatingLocation();
-            locationManager.StopUpdatingLocation();
+        public partial class CalendarRead : BasePlatformPermission
+        {
+        }
 
-            return tcs.Task;
+        public partial class CalendarWrite : BasePlatformPermission
+        {
+        }
 
-            void LocationAuthCallback(object sender, CLAuthorizationChangedEventArgs e)
+        public partial class Camera : BasePlatformPermission
+        {
+        }
+
+        public partial class ContactsRead : BasePlatformPermission
+        {
+        }
+
+        public partial class ContactsWrite : BasePlatformPermission
+        {
+        }
+
+        public partial class Flashlight : BasePlatformPermission
+        {
+        }
+
+        public partial class LaunchApp : BasePlatformPermission
+        {
+        }
+
+        public partial class LocationWhenInUse : BasePlatformPermission
+        {
+            protected override Func<IEnumerable<string>> RecommendedInfoPlistKeys =>
+                () => new string[] { "NSLocationWhenInUseUsageDescription" };
+
+            public override Task<PermissionStatus> CheckStatusAsync()
             {
-                if (e.Status == CLAuthorizationStatus.NotDetermined)
-                    return;
+                EnsureDeclared();
 
-                locationManager.AuthorizationChanged -= LocationAuthCallback;
-                tcs.TrySetResult(GetLocationStatus());
-                locationManager.Dispose();
-                locationManager = null;
+                return Task.FromResult(GetLocationStatus());
             }
+
+            public override async Task<PermissionStatus> RequestAsync()
+            {
+                EnsureDeclared();
+
+                var status = GetLocationStatus();
+                if (status == PermissionStatus.Granted)
+                    return status;
+
+                EnsureMainThread();
+
+                return await RequestLocationAsync();
+            }
+
+            internal static PermissionStatus GetLocationStatus()
+            {
+                if (!CLLocationManager.LocationServicesEnabled)
+                    return PermissionStatus.Disabled;
+
+                var status = CLLocationManager.Status;
+
+                return status switch
+                {
+                    CLAuthorizationStatus.AuthorizedAlways => PermissionStatus.Granted,
+                    CLAuthorizationStatus.AuthorizedWhenInUse => PermissionStatus.Granted,
+                    CLAuthorizationStatus.Denied => PermissionStatus.Denied,
+                    CLAuthorizationStatus.Restricted => PermissionStatus.Restricted,
+                    _ => PermissionStatus.Unknown,
+                };
+            }
+
+            static CLLocationManager locationManager;
+
+            internal static Task<PermissionStatus> RequestLocationAsync()
+            {
+                locationManager = new CLLocationManager();
+
+                var tcs = new TaskCompletionSource<PermissionStatus>(locationManager);
+
+                var previousState = CLLocationManager.Status;
+
+                locationManager.AuthorizationChanged += LocationAuthCallback;
+                locationManager.StartUpdatingLocation();
+                locationManager.StopUpdatingLocation();
+
+                return tcs.Task;
+
+                void LocationAuthCallback(object sender, CLAuthorizationChangedEventArgs e)
+                {
+                    if (e.Status == CLAuthorizationStatus.NotDetermined)
+                        return;
+
+                    locationManager.AuthorizationChanged -= LocationAuthCallback;
+                    tcs.TrySetResult(GetLocationStatus());
+                    locationManager.Dispose();
+                    locationManager = null;
+                }
+            }
+        }
+
+        public partial class LocationAlways : BasePlatformPermission
+        {
+        }
+
+        public partial class Maps : BasePlatformPermission
+        {
+        }
+
+        public partial class Media : BasePlatformPermission
+        {
+        }
+
+        public partial class Microphone : BasePlatformPermission
+        {
+        }
+
+        public partial class NetworkState : BasePlatformPermission
+        {
+        }
+
+        public partial class Phone : BasePlatformPermission
+        {
+        }
+
+        public partial class Photos : BasePlatformPermission
+        {
+        }
+
+        public partial class Reminders : BasePlatformPermission
+        {
+        }
+
+        public partial class Sensors : BasePlatformPermission
+        {
+        }
+
+        public partial class Sms : BasePlatformPermission
+        {
+        }
+
+        public partial class StorageRead : BasePlatformPermission
+        {
+        }
+
+        public partial class StorageWrite : BasePlatformPermission
+        {
+        }
+
+        public partial class Vibrate : BasePlatformPermission
+        {
         }
     }
 }
-*/
