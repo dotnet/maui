b00152ba2d82bea673f9dd8a1927632bb46207d5
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-03-11 22:44:03 +0100

    [UWP] Fixed a crash hidden a DatePicker (Xamarin.Forms 4.5-pre4) (#9820)
    * [UWP] Fixed a crash hidden a DatePicker (Xamarin.Forms 4.5-pre4)
    
    * Fixed build

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue9783.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms.CustomAttributes;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Github, 9783, "DatePicker begin to cause Exception in version 4.5.0-282-pre4 and above in some situations",
+		PlatformAffected.UWP)]
+	public partial class Issue9783 : TestContentPage
+	{
+		public Issue9783()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
