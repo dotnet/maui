88db40370a5d51e2219a390445a2dc30019db35f
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-03-18 17:20:59 +0100

    Fixed SwipeView icon size using FontImageSource on Android (#9955)

--- Xamarin.Forms.Platform.Android/Renderers/SwipeViewRenderer.cs
+++ Xamarin.Forms.Platform.Android/Renderers/SwipeViewRenderer.cs
@@ -677,7 +677,22 @@ namespace Xamarin.Forms.Platform.Android
 
 			_ = this.ApplyDrawableAsync(formsSwipeItem, MenuItem.IconImageSourceProperty, Context, drawable =>
 			{
-				drawable.SetBounds(0, 0, iconSize, iconSize);
+				int drawableWidth = drawable.IntrinsicWidth;
+				int drawableHeight = drawable.IntrinsicHeight;
+
+				if(drawableWidth > drawableHeight)
+				{
+					var iconWidth = iconSize;
+					var iconHeight = drawableHeight * iconWidth / drawableWidth;
+					drawable.SetBounds(0, 0, iconWidth, iconHeight);
+				}
+				else
+				{
+					var iconHeight = iconSize;
+					var iconWidth = drawableWidth * iconHeight / drawableHeight;
+					drawable.SetBounds(0, 0, iconWidth, iconHeight);
+				}
+
 				drawable.SetColorFilter(textColor.ToAndroid(), FilterMode.SrcAtop);
 				swipeButton.SetCompoundDrawables(null, drawable, null, null);
 			});
