e7a90f9a31f78f6d4cf9dd3055376f2da624d131
E.Z. Hart <hartez@gmail.com>
2020-03-17 23:40:00 -0600

    Fix button tests for fast renderers

--- Xamarin.Forms.ControlGallery.Android/Tests/PlatformTestFixture.cs
+++ Xamarin.Forms.ControlGallery.Android/Tests/PlatformTestFixture.cs
@@ -379,9 +379,19 @@ namespace Xamarin.Forms.ControlGallery.Android.Tests
 					}
 
 					// Some of the button stuff doesn't work with layout parameters, so we need to parent the control
-					ParentView(control); 
+					var needsParent = control.Parent == null;
+
+					if (needsParent)
+					{
+						ParentView(control);
+					}
+					
 					var result = getProperty(control);
-					UnparentView(control);
+
+					if (needsParent)
+					{
+						UnparentView(control);
+					}
 
 					return result;
 				}
