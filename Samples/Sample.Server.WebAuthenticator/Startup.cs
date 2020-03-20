e5636d3ed521d8f4769d4f75ca9286301b8bb735
Jonathan Dick <jodick@microsoft.com>
2020-03-19 21:21:47 -0400

    Added missing ns

--- Samples/Sample.Server.WebAuthenticator/Startup.cs
+++ Samples/Sample.Server.WebAuthenticator/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Sample.Server.WebAuthenticator
 {
