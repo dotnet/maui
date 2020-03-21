a2f3e4403e06ea4c42811835353f48066bce34b1
James Montemagno <james.montemagno@gmail.com>
2020-03-21 15:39:15 -0600

    Fix ordering for build! (#1186)

--- Samples/Sample.Server.WebAuthenticator/Startup.cs
+++ Samples/Sample.Server.WebAuthenticator/Startup.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Sample.Server.WebAuthenticator
 {
