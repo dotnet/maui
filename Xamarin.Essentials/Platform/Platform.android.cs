9e4fe9780f80aa7bfe86c4db26abf0dbdd7f1c9f
James Montemagno <james.montemagno@gmail.com>
2020-04-06 23:19:18 -0700

    Fixup some dep warnings & update verison code
    Use new compat library, where available

--- Xamarin.Essentials/Platform/Platform.android.cs
+++ Xamarin.Essentials/Platform/Platform.android.cs
@@ -221,7 +221,9 @@ namespace Xamarin.Essentials
                 return config.Locales.Get(0);
 #endif
 
+#pragma warning disable CS0618 // Type or member is obsolete
             return config.Locale;
+#pragma warning restore CS0618 // Type or member is obsolete
         }
 
         internal static void SetLocale(Java.Util.Locale locale)
@@ -231,9 +233,15 @@ namespace Xamarin.Essentials
             var config = resources.Configuration;
 
             if (HasApiLevelN)
+            {
                 config.SetLocale(locale);
+            }
             else
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
                 config.Locale = locale;
+#pragma warning restore CS0618 // Type or member is obsolete
+            }
 
 #pragma warning disable CS0618 // Type or member is obsolete
             resources.UpdateConfiguration(config, resources.DisplayMetrics);
