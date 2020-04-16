8494575e7613787f19272427618440897ab54208
Matthew Leibowitz <mattleibow@live.com>
2020-04-16 21:20:30 +0200

    might be null

--- Xamarin.Essentials/Types/PlatformExtensions/ColorExtensions.mac.cs
+++ Xamarin.Essentials/Types/PlatformExtensions/ColorExtensions.mac.cs
@@ -7,6 +7,9 @@ namespace Xamarin.Essentials
     {
         public static Color ToSystemColor(this NSColor color)
         {
+            if (color == null)
+                throw new ArgumentNullException(nameof(color));
+
             color.GetRgba(out var red, out var green, out var blue, out var alpha);
             return Color.FromArgb((int)(alpha * 255), (int)(red * 255), (int)(green * 255), (int)(blue * 255));
         }
