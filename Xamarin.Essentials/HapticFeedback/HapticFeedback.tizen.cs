857390ba644f4428c6022c7a64867ce1b16fee55
dimonovdd <dimovde@yandex.ru>
2020-04-29 09:05:56 +0300

    added HapticFeedback Tizen

---
+++ Xamarin.Essentials/HapticFeedback/HapticFeedback.tizen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Tizen.System;
+
+namespace Xamarin.Essentials
+{
+    public static partial class HapticFeedback
+    {
+        internal static bool IsSupported => true;
+
+        static void PlatformExecute(HapticFeedbackType type)
+        {
+            Permissions.EnsureDeclared<Permissions.Vibrate>();
+            try
+            {
+                var feedback = new Feedback();
+                var pattern = ConvertType(type);
+                if (feedback.IsSupportedPattern(FeedbackType.Vibration, pattern))
+                    feedback.Play(FeedbackType.Vibration, pattern);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($" -- Tizen -- {ex.Message}");
+            }
+        }
+
+        static string ConvertType(HapticFeedbackType type)
+        {
+            switch (type)
+            {
+                case HapticFeedbackType.LongPress:
+                    return "Hold";
+                default:
+                    return "Tap";
+            }
+        }
+    }
+}
