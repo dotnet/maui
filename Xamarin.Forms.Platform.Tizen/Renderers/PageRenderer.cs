335fdde9b4d144053a8343581fa72f1ae24b8511
Seungkeun Lee <sngn.lee@samsung.com>
2020-05-05 22:56:53 +0900

    [Tize] Provide way to handle when MoreOption was opened/closed (#10553)

--- Xamarin.Forms.Platform.Tizen/Renderers/PageRenderer.cs
+++ Xamarin.Forms.Platform.Tizen/Renderers/PageRenderer.cs
@@ -71,6 +71,8 @@ namespace Xamarin.Forms.Platform.Tizen
 					if (_moreOption.IsValueCreated)
 					{
 						_moreOption.Value.Clicked -= OnMoreOptionItemClicked;
+						_moreOption.Value.Closed -= SendMoreOptionClosed;
+						_moreOption.Value.Opened -= SendMoreOptionOpened;
 						_moreOption.Value.Items.Clear();
 						_moreOption.Value.Unrealize();
 					}
@@ -113,6 +115,14 @@ namespace Xamarin.Forms.Platform.Tizen
 			return moreOptionItem;
 		}
 
+		protected virtual void OnMoreOptionClosed()
+		{
+		}
+
+		protected virtual void OnMoreOptionOpened()
+		{
+		}
+
 		void UpdateBackgroundImage(bool initialize)
 		{
 			if (initialize && Element.BackgroundImageSource.IsNullOrEmpty())
@@ -140,10 +150,22 @@ namespace Xamarin.Forms.Platform.Tizen
 		MoreOption CreateMoreOption()
 		{
 			var moreOption = new MoreOption(_page);
-			moreOption.Clicked += OnMoreOptionItemClicked;
 			_page.Children.Add(moreOption);
 			moreOption.Show();
+			moreOption.Clicked += OnMoreOptionItemClicked;
+			moreOption.Closed += SendMoreOptionClosed;
+			moreOption.Opened += SendMoreOptionOpened;
 			return moreOption;
+		}
+
+		void SendMoreOptionClosed(object sender, EventArgs e)
+		{
+			OnMoreOptionClosed();
+		}
+
+		void SendMoreOptionOpened(object sender, EventArgs e)
+		{
+			OnMoreOptionOpened();
 		}
 
 		void OnToolbarCollectionChanged(object sender, EventArgs eventArgs)
