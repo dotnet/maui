7938a264602156139d7b1c39919099c3bdbab56d
Shane Neuville <shneuvil@microsoft.com>
2020-05-06 05:29:41 -0600

    Remove DualScreen projects from Xamarin.Forms.Sln (#10550)
    * Move Dual Screen Project out of Gallery
    
    * - fix sln
    
    * - remove excess
    
    * - restore
    
    * - fix up the props a bit more
    
    * - fix build settings

---
+++ DualScreen/DualScreen.Mac/Main.cs
@@ -0,0 +1,14 @@
+﻿using AppKit;
+
+namespace DualScreen.Mac
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new AppDelegate();
+			NSApplication.Main(args);
+		}
+	}
+}
