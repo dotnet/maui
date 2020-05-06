261dc7d5ce6b36829895775a74e2354cb2949210
Shane Neuville <shneuvil@microsoft.com>
2020-05-06 11:42:44 -0600

    - fix merge

---
+++ DualScreen/DualScreen.Mac/ViewController.designer.cs
@@ -0,0 +1,18 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+
+namespace DualScreen.Mac
+{
+	[Register("ViewController")]
+	partial class ViewController
+	{
+		void ReleaseDesignerOutlets()
+		{
+		}
+	}
+}
