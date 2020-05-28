a033f11538f2742426a23f30b80977d5af6d0d5b
Stephane Delcroix <stephane@delcroix.org>
2020-05-28 22:33:29 +0200

    [X] TypeConverter on Grid Col/Row Definitions (#10648)
    Simplify Grid Column and Row Definitions by using markup extensions.
    
    - fixes #7302
    - closes #4997

---
+++ Xamarin.Forms.Core/ColumnDefinitionCollectionTypeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Xamarin.Forms
+{
+	[Xaml.TypeConversion(typeof(ColumnDefinitionCollection))]
+	public class ColumnDefinitionCollectionTypeConverter : TypeConverter
+	{
+		public override object ConvertFromInvariantString(string value)
+		{
+			if (value != null) {
+				var lengths = value.Split(',');
+				var coldefs = new ColumnDefinitionCollection();
+				var converter = new GridLengthTypeConverter();
+				foreach (var length in lengths)
+					coldefs.Add(new ColumnDefinition { Width = (GridLength)converter.ConvertFromInvariantString(length) });
+				return coldefs;
+			}
+
+			throw new InvalidOperationException(string.Format("Cannot convert \"{0}\" into {1}", value, typeof(ColumnDefinitionCollection)));
+		}
+	}
+}
---
+++ Xamarin.Forms.Core/RowDefinitionCollectionTypeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Xamarin.Forms
+{
+	[Xaml.TypeConversion(typeof(RowDefinitionCollection))]
+	public class RowDefinitionCollectionTypeConverter : TypeConverter
+	{
+		public override object ConvertFromInvariantString(string value)
+		{
+			if (value != null) {
+				var lengths = value.Split(',');
+				var coldefs = new RowDefinitionCollection();
+				var converter = new GridLengthTypeConverter();
+				foreach (var length in lengths)
+					coldefs.Add(new RowDefinition { Height = (GridLength)converter.ConvertFromInvariantString(length) });
+				return coldefs;
+			}
+
+			throw new InvalidOperationException(string.Format("Cannot convert \"{0}\" into {1}", value, typeof(RowDefinitionCollection)));
+		}
+
+	}
+}
