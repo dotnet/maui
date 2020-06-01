bf6732a36670fb99b770c142740380dfa1dbd70d
Javier Suárez <javiersuarezruiz@hotmail.com>
2020-06-01 20:04:48 +0200

    Use Selected VisualState in CollectionView UWP (#10770)
    Co-authored-by: Samantha Houts <samhouts@users.noreply.github.com>

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue9279.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.UITest.iOS;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 9279, "[Bug] [UWP] CollectionView selected state not working on UWP", PlatformAffected.UWP)]
+	public partial class Issue9279 : TestContentPage
+	{
+		public Issue9279()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+		
+		}
+	}
+}
