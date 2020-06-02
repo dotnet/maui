438940ee1513980400156f4571b93b3c91c1cdcf
Gerald Versluis <gerald.versluis@microsoft.com>
2020-06-02 12:44:07 +0200

    [iOS] Fix Frame issues (#10469) fixes #10422 fixes #10348 fixes #10333 fixes #10166 fixes #10024 fixes #9990 fixes #9774 fixes #9555 fixes #10326 fixes #10906
    * Fix
    
    * Fix UI tests
    
    * Update FrameRenderer.cs
    
    * Revert "Fix UI tests"
    
    This reverts commit e5c16b23fb17f19e6dff1d54f0f3f21febf1f40f.
    
    * Update FrameRenderer.cs
    
    * Update FrameRenderer.cs
    
    * Adding reproductions
    
    * More reproductions
    
    * Update Issue9990.xaml.cs
    
    * Update Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue10024.xaml
    
    * Remove commented code
    
    * provision-monosdk last
    
    * Allow user interaction with Frame content (e.g., Button)
    
    * Allow frame taps to bubble if the frame has no content
    
    Co-authored-by: Samantha Houts <samhouts@users.noreply.github.com>
    Co-authored-by: E.Z. Hart <hartez@gmail.com>
    Co-authored-by: Shane Neuville <shneuvil@microsoft.com>

---
+++ Xamarin.Forms.ControlGallery.iOS/CustomEffects/FooEffect.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(Xamarin.Forms.ControlGallery.iOS.CustomEffects.FooEffect), nameof(Xamarin.Forms.ControlGallery.iOS.CustomEffects.FooEffect))]
+
+namespace Xamarin.Forms.ControlGallery.iOS.CustomEffects
+{
+	public class FooEffect : PlatformEffect
+	{
+		public FooEffect()
+		{
+		}
+
+		protected override void OnAttached()
+		{
+		}
+
+		protected override void OnDetached()
+		{
+
+		}
+	}
+}
---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue9555.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	public class FooEffect : RoutingEffect
+	{
+		public FooEffect() : base("XamControl.FooEffect")
+		{
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 9555, "[iOS] Applying an Effect to Frame adds a shadow", PlatformAffected.iOS)]
+	public partial class Issue9555 : ContentPage
+	{
+		public Issue9555()
+		{
+#if APP
+			InitializeComponent ();
+#endif
+		}
+	}
+}
