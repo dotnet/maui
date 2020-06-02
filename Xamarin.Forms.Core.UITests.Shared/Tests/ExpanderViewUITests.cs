f058a581747553477eae86e8707107a2bdc50791
Rui Marinho <me@ruimarinho.net>
2020-06-02 11:24:31 +0100

    [UITests] Fix Expander UITest API19

--- Xamarin.Forms.Core.UITests.Shared/Tests/ExpanderViewUITests.cs
+++ Xamarin.Forms.Core.UITests.Shared/Tests/ExpanderViewUITests.cs
@@ -18,21 +18,16 @@ namespace Xamarin.Forms.Core.UITests
 			App.WaitForElement("The Second", "");
 			App.Tap("Expander Level 2");
 			App.WaitForElement("Hi, I am Red", "View didn't expand the second level");
+			App.ScrollDownTo("The Fourth");
 			App.Tap("The Fourth");
-
 			App.WaitForNoElement("Hi, I am Red", "View didn't collapse like is should");
-
 			App.WaitForElement("Expander Level 2", "Fourth view didn't expand to show 'Expander level 2'");
 			App.Tap("Expander Level 2");
 			App.WaitForElement("Hi, I am Red", "Expander level 2 of Fourth view didn't expand like it should.");
 			App.Tap("Expander Level 2");
-
 			App.WaitForNoElement("Hi, I am Red", "View didn't collapse like is should");
-
 			App.Tap("The Fourth");
-
 			App.WaitForNoElement("Expander Level 2", "View didn't collapse like is should");
-
 			App.Back();
 		}
 	}
