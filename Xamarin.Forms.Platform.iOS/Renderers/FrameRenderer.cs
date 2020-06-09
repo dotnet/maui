de38e1d79c9d1daf7668f8f566504dbeda46bdd2
Samantha Houts <samhouts@users.noreply.github.com>
2020-06-08 21:25:21 -0700

    Fix merge

--- Xamarin.Forms.Platform.iOS/Renderers/FrameRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/FrameRenderer.cs
@@ -115,14 +115,6 @@ namespace Xamarin.Forms.Platform.iOS
 
 		public override void LayoutSubviews()
 		{
-			if (_shadowView != null)
-			{
-				if (_shadowView.Superview == null && Superview != null)
-					Superview.InsertSubviewBelow(_shadowView, this);
-					
-				_shadowView?.SetNeedsLayout();
-			}
-
 			if (_previousSize != Bounds.Size)
 				SetNeedsDisplay();
 
