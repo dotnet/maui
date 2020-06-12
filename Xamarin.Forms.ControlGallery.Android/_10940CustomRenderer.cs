09585774c32d17f9dc6db6299edcc94fe270c4c6
Javier Suárez <javiersuarezruiz@hotmail.com>
2020-06-13 01:31:18 +0200

    Added repro sample (#11028)
    fixes #10940

---
+++ Xamarin.Forms.ControlGallery.Android/_10940CustomRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.ControlGallery.Android;
+using Xamarin.Forms.Controls.Issues;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+[assembly: ExportRenderer(typeof(CustomSwipeView), typeof(CustomSwipeViewRenderer))]
+namespace Xamarin.Forms.ControlGallery.Android
+{
+	public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+            System.Diagnostics.Debug.WriteLine("Create CustomEntry");
+        }
+    }
+
+    public class CustomSwipeViewRenderer : SwipeViewRenderer
+    {
+
+        public CustomSwipeViewRenderer(Context context) : base(context)
+        {
+
+        }
+    }
+}
