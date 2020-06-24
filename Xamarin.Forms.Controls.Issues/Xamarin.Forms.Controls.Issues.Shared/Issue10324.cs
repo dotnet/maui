ad0e1daf67c34e578f71fd7a00cda666752d9aab
Alex West <18328783+Pandalink@users.noreply.github.com>
2020-06-24 12:37:12 +0100

    [UWP] Physical mouse back button should use OnBackButtonPressed page override (#10346) fixes #10324
    * Send OnBackButtonPressed with XButton1 in NavigationPage
    
    * Remove async await
    
    * Add page to test default back behaviour
    
    * Tidy test page
    
    * Add test file to projitems
    
    Co-authored-by: Alex West <alexwest@nuffieldtechnologies.com>
    Co-authored-by: E.Z. Hart <hartez@gmail.com>
    Co-authored-by: E.Z. Hart <hartez@users.noreply.github.com>

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue10324.cs
@@ -0,0 +1,54 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 10324, "Unable to intercept or disable mouse back button navigation on UWP", PlatformAffected.UWP)]
+	public class Issue10324 : TestNavigationPage
+	{
+		protected override void Init()
+		{
+			Navigation.PushAsync(new IssueFirstPage());
+		}
+
+		class IssueFirstPage : ContentPage
+		{
+			public IssueFirstPage()
+			{
+				Navigation.PushAsync(new IssueTestAlertPage());
+			}
+		}
+
+		class IssueTestAlertPage : ContentPage
+		{
+			public IssueTestAlertPage()
+			{
+				Title = "Hit Mouse BackButton/XButton1. An alert will appear (OnBackButtonPressed overridden)";
+				Content = new StackLayout();
+
+				Navigation.PushAsync(new IssueTestBackPage());
+			}
+
+			protected override bool OnBackButtonPressed()
+			{
+				DisplayAlert("OnBackButtonPressed", "OnBackButtonPressed", "OK");
+				return true;
+			}
+		}
+
+		class IssueTestBackPage : ContentPage
+		{
+			public IssueTestBackPage()
+			{
+				Title = "Hit Mouse BackButton/XButton1. Page will go back (OnBackButtonPressed not overridden)";
+				Content = new StackLayout();
+			}
+
+			protected override bool OnBackButtonPressed()
+			{
+				return base.OnBackButtonPressed();
+			}
+		}
+	}
+}
