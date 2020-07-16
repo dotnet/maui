c77a6d86d053f3bc4b9d8c45775d2c3de267abb9
Jonathan Dick <jondick@gmail.com>
2020-07-16 16:24:44 -0400

    Cleanup

--- Samples/Sample.Server.WebAuthenticator/Startup.cs
+++ Samples/Sample.Server.WebAuthenticator/Startup.cs
@@ -65,7 +65,7 @@ namespace Sample.Server.WebAuthenticator
 
             /*
             * For Apple signin
-            * If you are running the app on Azure you must add the Configuration setting 
+            * If you are running the app on Azure you must add the Configuration setting
             * WEBSITE_LOAD_USER_PROFILE = 1
             * Without this setting you will get a File Not Found exception when AppleAuthenticationHandler tries to generate a certificate using your Auth_{keyId].P8 file.
             */
