8c63ee7917e4b553f5d9e8481cf313837b2641f7
Javier Suárez <javiersuarezruiz@hotmail.com>
2020-07-20 22:32:26 +0200

    [Android] Fix setting the CarouselView position while control isn't visible (#11239)
    * Added repro sample
    
    * Fixed the issue
    
    * Added UITest
    
    fixes #11224

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11224.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+#if UITEST
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.CarouselView)]
+#endif
+#if APP
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 11224, "[Bug] CarouselView Position property fails to update visual while control isn't visible", PlatformAffected.Android)]
+	public partial class Issue11224 : TestContentPage
+	{
+		public Issue11224()
+		{
+#if APP
+			Title = "Issue 11224";
+			InitializeComponent();
+
+			carousel.Scrolled += (sender, args) =>
+			{
+				if (args.CenterItemIndex == 3)
+					ResultLabel.Text = "The test has passed";
+				else
+					ResultLabel.Text = "The test has failed";
+			};
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+
+
+#if APP
+		void Button_Clicked(object sender, EventArgs e)
+		{
+			carousel.IsVisible = true;
+		}
+#endif
+
+#if UITEST
+		[Test]
+		public void CarouselViewPositionFromVisibilityChangeTest()
+		{
+			RunningApp.WaitForElement(q => q.Marked("AppearButton"));
+			RunningApp.Tap(q => q.Marked("AppearButton"));
+			RunningApp.WaitForElement("The test has passed");
+			RunningApp.Screenshot("Success");
+		}
+#endif
+
+	}
+}
