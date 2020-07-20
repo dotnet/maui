aee3603be06b0a2f0a3f2bc54d58c478c6802e77
dimonovdd <dimovde@yandex.ru>
2020-07-20 23:25:08 +0300

    added hapticFeedback macOS

---
+++ Xamarin.Essentials/HapticFeedback/HapticFeedback.macos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using AppKit;
+
+namespace Xamarin.Essentials
+{
+    public static partial class HapticFeedback
+    {
+        internal static bool IsSupported => true;
+
+        static void PlatformPerform(HapticFeedbackType type)
+        {
+            if (type == HapticFeedbackType.LongPress)
+                NSHapticFeedbackManager.DefaultPerformer.PerformFeedback(NSHapticFeedbackPattern.Alignment, NSHapticFeedbackPerformanceTime.Default);
+        }
+    }
+}
