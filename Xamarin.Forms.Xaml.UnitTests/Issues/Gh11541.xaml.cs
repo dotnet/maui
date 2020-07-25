45855f7263fe5a098b1073289288a0e81f04d52c
Stephane Delcroix <stephane@delcroix.org>
2020-07-25 10:12:34 +0200

    [Shapes] Fix RectTypeConverter (#11548)
    Fix inheritance of RectTypeConverter from RectangleTypeConverter, as the
    child isn't precompiled, even if the parent has the attribute...
    
    - fixes #11541

---
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Gh11541.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Core.UnitTests;
+
+using NUnit.Framework;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	public partial class Gh11541 : ContentPage
+	{
+		public Gh11541() => InitializeComponent();
+		public Gh11541(bool useCompiledXaml)
+		{
+			//this stub will be replaced at compile time
+		}
+
+		[TestFixture]
+		class Tests
+		{
+			[SetUp] public void Setup() => Device.PlatformServices = new MockPlatformServices();
+
+			[TearDown] public void TearDown() => Device.PlatformServices = null;
+
+			[Test]
+			public void RectangleGeometryDoesntThrow([Values(false, true)] bool useCompiledXaml)
+			{
+				var layout = new Gh11541(useCompiledXaml);
+			}
+		}
+	}
+}
