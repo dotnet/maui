5754d1a4436e57c225c08526f768ca0f44362372
Stephane Delcroix <stephane@delcroix.org>
2020-07-27 16:06:43 +0200

    [X] throw on mismatching param type for Add() (#11086)
    There was a `TODO: check parameter type` in this code, like if past me
    knew that, at some point in time, what was only a potential edge case
    bug, would explode in my face and ruin my faith in humanity.
    
    Those FIXMEs... they are bottled 'I TOLD YOU SO' little time capsules
    
    - fixes #11061

---
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Gh11061.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Xamarin.Forms;
+using Xamarin.Forms.Build.Tasks;
+using Xamarin.Forms.Core.UnitTests;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	[XamlCompilation(XamlCompilationOptions.Skip)]
+	public partial class Gh11061 : ContentPage
+	{
+		public DateTime MyDateTime { get; set; }
+
+		public Gh11061() => InitializeComponent();
+		public Gh11061(bool useCompiledXaml)
+		{
+			//this stub will be replaced at compile time
+		}
+
+		[TestFixture]
+		class Tests
+		{
+			[SetUp] public void Setup() => Device.PlatformServices = new MockPlatformServices();
+			[TearDown] public void TearDown() => Device.PlatformServices = null;
+
+			[Test]
+			public void XamlCBindingOnNonBP([Values(false, true)] bool useCompiledXaml)
+			{
+				if (useCompiledXaml)
+					Assert.Throws<BuildException>(() => MockCompiler.Compile(typeof(Gh11061)));
+				else
+					Assert.Throws<XamlParseException>(()=> new Gh11061(useCompiledXaml) { BindingContext = new { Date = DateTime.Today} });
+			}
+		}
+	}
+}
+ 
