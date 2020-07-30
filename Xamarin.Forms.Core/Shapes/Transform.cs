5e30265ec37cec9a0538a63cbd4504d9abea7abd
Timothé Larivière <timothe.lariviere@outlook.com>
2020-07-30 02:12:24 +0200

    Mark Transform.ValueProperty as public (#11543)

--- Xamarin.Forms.Core/Shapes/Transform.cs
+++ Xamarin.Forms.Core/Shapes/Transform.cs
@@ -3,7 +3,7 @@
     [TypeConverter(typeof(TransformTypeConverter))]
     public class Transform : BindableObject
     {
-        internal static readonly BindableProperty ValueProperty =
+        public static readonly BindableProperty ValueProperty =
            BindableProperty.Create(nameof(Value), typeof(Matrix), typeof(Transform), new Matrix());
 
         public Matrix Value
