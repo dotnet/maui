daa0589999171cfc153a915ca122b6e80d7948c4
Shane Neuville <shneuvil@microsoft.com>
2020-08-11 05:17:44 -0500

    Set environment variables for UWP test results (#11724)
    * Set environment variables and ignore context
    
    * Update Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Github5623.xaml.cs
    
    * - print vars
    
    * - POW POW POWERSHELL
    
    * - remove print
    
    * - fix invoke call
    
    * - set variables via msbuild
    
    * - delete file if present
    
    * - set all at once and output result
    
    * - messages
    
    * - builid system
    
    * - vars
    
    * - print all the things

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla46363_2.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla46363_2.cs
@@ -15,6 +15,7 @@ namespace Xamarin.Forms.Controls.Issues
 	[Category(UITestCategories.ListView)]
 	[Category(UITestCategories.Cells)]
 	[Category(UITestCategories.ContextActions)]
+	[Category(UITestCategories.UwpIgnore)]
 	[NUnit.Framework.Category(Core.UITests.UITestCategories.Bugzilla)]
 #endif
 
