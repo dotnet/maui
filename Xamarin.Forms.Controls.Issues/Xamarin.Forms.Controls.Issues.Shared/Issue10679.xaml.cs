f5575fb5ca6417eb819bd1370d59bc4d692de54a
Javier Suárez <javiersuarezruiz@hotmail.com>
2020-08-18 18:41:01 +0200

    Fixed Issue10679, avoid NRE accessing a disposed SwipeView (#10682)
    Co-authored-by: Rui Marinho <me@ruimarinho.net>

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue10679.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 10679, "SwipeView crash with NullReferenceException",
+		PlatformAffected.Android)]
+	public partial class Issue10679 : TestShell
+	{
+		public Issue10679()
+		{
+#if APP
+			Device.SetFlags(new List<string>(Device.Flags ?? new List<string>()) { "SwipeView_Experimental" });
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class FirstIssue10679Page : ContentPage
+	{
+		public FirstIssue10679Page()
+		{
+			Title = "Issue 10679";
+
+			var layout = new Grid();
+
+			var button = new Button
+			{
+				VerticalOptions = LayoutOptions.Center,
+				Text = "Go to Next Page"
+			};
+
+			layout.Children.Add(button);
+
+			Content = layout;
+
+			button.Clicked += async (sender, args) =>
+			{
+				await Shell.Current.Navigation.PushAsync(new SecondIssue10679Page()).ConfigureAwait(false);
+			};
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class SecondIssue10679Page : ContentPage
+	{
+		public SecondIssue10679Page()
+		{
+			var layout = new Grid();
+
+			var swipeView = new SwipeView();
+
+			var leftItem = new SwipeItem
+			{
+				BackgroundColor = Color.Red
+			};
+
+			var leftItems = new SwipeItems
+			{
+				Mode = SwipeMode.Execute,
+				SwipeBehaviorOnInvoked = SwipeBehaviorOnInvoked.Close
+			};
+
+			leftItems.Add(leftItem);
+
+			var rightItem = new SwipeItem
+			{
+				BackgroundColor = Color.Green
+			};
+
+			var rightItems = new SwipeItems
+			{
+				Mode = SwipeMode.Execute,
+				SwipeBehaviorOnInvoked = SwipeBehaviorOnInvoked.Close
+			};
+
+			rightItems.Add(rightItem);
+
+			var content = new Grid
+			{
+				BackgroundColor = Color.White
+			};
+
+			var contentLabel = new Label
+			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Text = "Swipe Left or Right"
+			};
+
+			content.Children.Add(contentLabel);
+
+			swipeView.Content = content;
+			swipeView.LeftItems = leftItems;
+			swipeView.RightItems = rightItems;
+
+			layout.Children.Add(swipeView);
+			
+			Content = layout;
+
+			leftItem.Invoked += async (sender, args) =>
+			{
+				Shell.Current.Navigation.InsertPageBefore(new ThirdIssue10679Page(), this);
+				_ = await Shell.Current.Navigation.PopAsync(false);
+			};
+
+			rightItem.Invoked += async (sender, args) =>
+			{
+				Shell.Current.Navigation.InsertPageBefore(new ThirdIssue10679Page(), this);
+				_ = await Shell.Current.Navigation.PopAsync(false);
+			};
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class ThirdIssue10679Page : ContentPage
+	{
+		public ThirdIssue10679Page()
+		{
+			var layout = new StackLayout();
+
+			var infoLabel = new Label
+			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Text = "Without NullReferenceException, the test has passed."
+			};
+
+			layout.Children.Add(infoLabel);
+
+			Content = layout;
+		}
+	}
+}
