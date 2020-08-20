0882a6edb778e974d581c447d6634e752bd6b8e8
Matthew Leibowitz <mattleibow@live.com>
2020-08-21 01:18:44 +0200

    move back

--- Xamarin.Essentials/FileSystem/FileSystem.ios.cs
+++ Xamarin.Essentials/FileSystem/FileSystem.ios.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Foundation;
 using Photos;
@@ -55,6 +56,28 @@ namespace Xamarin.Essentials
                 tcsStream.TrySetResult(data.AsStream())));
 
             return tcsStream.Task;
+        }
+    }
+
+    class UIImageFileResult : FileResult
+    {
+        readonly UIImage uiImage;
+        NSData data;
+
+        internal UIImageFileResult(UIImage image)
+            : base()
+        {
+            uiImage = image;
+
+            FullPath = Guid.NewGuid().ToString() + ".png";
+            FileName = FullPath;
+        }
+
+        internal override Task<Stream> PlatformOpenReadAsync()
+        {
+            data ??= uiImage.AsPNG();
+
+            return Task.FromResult(data.AsStream());
         }
     }
 }
