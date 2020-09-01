5361e0b7fcb4f7e7db2350bb87a46525773ea551
Jonathan Dick <jondick@gmail.com>
2020-09-01 10:50:19 -0400

    Make forms app static

--- Samples/Samples.UWP/MainPage.xaml.cs
+++ Samples/Samples.UWP/MainPage.xaml.cs
@@ -4,13 +4,15 @@ namespace Samples.UWP
 {
     public sealed partial class MainPage : Xamarin.Forms.Platform.UWP.WindowsPage
     {
+        static Samples.App formsApp;
+
         public MainPage()
         {
             InitializeComponent();
 
             Platform.MapServiceToken = "RJHqIE53Onrqons5CNOx~FrDr3XhjDTyEXEjng-CRoA~Aj69MhNManYUKxo6QcwZ0wmXBtyva0zwuHB04rFYAPf7qqGJ5cHb03RCDw1jIW8l";
 
-            LoadApplication(new Samples.App());
+            LoadApplication(formsApp ??= new Samples.App());
         }
     }
 }
