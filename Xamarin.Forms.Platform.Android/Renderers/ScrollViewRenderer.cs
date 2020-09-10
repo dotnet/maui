7483a995854d1f39443541314f34e27bb9fee144
Javier Suárez <javiersuarezruiz@hotmail.com>
2020-09-10 14:05:40 +0200

    Fixed ScrollView Visibility on Android (#10659) fixes #8015 fixes #9103
    Co-authored-by: Samantha Houts <samhouts@users.noreply.github.com>

--- Xamarin.Forms.Platform.Android/Renderers/ScrollViewRenderer.cs
+++ Xamarin.Forms.Platform.Android/Renderers/ScrollViewRenderer.cs
@@ -32,7 +32,9 @@ namespace Xamarin.Forms.Platform.Android
 		LayoutDirection _prevLayoutDirection = LayoutDirection.Ltr;
 		bool _checkedForRtlScroll = false;
 
-		public ScrollViewRenderer(Context context) : base(context)
+		public ScrollViewRenderer(Context context) : base(
+			new ContextThemeWrapper(context, Resource.Style.scrollViewTheme), null,
+			Resource.Attribute.scrollViewStyle)
 		{
 		}
 
