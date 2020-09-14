57ac0a9c62472aed7120053abd972d54ecab6086
Seungkeun Lee <sngn.lee@samsung.com>
2020-09-15 00:43:16 +0900

    [Tizen] Adds the DisplayResolutionUnit (#12074)
    * Apply Device size Scaling and Viewport
    
    * Update InitializationOptions to setup DisplayResolutionUnit
    
    * Fix DisplayResolutionUnit namespace, Fix Init method

---
+++ Xamarin.Forms.Platform.Tizen/DisplayResolutionUnit.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace Xamarin.Forms
+{
+	public class DisplayResolutionUnit
+	{
+		public static DisplayResolutionUnit Pixel(bool useDeviceScale = false)
+		{
+			return new DisplayResolutionUnit()
+			{
+				UseDP = false,
+				UseDeviceScale = useDeviceScale
+			};
+		}
+
+		public static DisplayResolutionUnit DP(bool useDeviceScale = false)
+		{
+			return new DisplayResolutionUnit()
+			{
+				UseDP = true,
+				UseDeviceScale = useDeviceScale
+			};
+		}
+
+		public static DisplayResolutionUnit VP(double width)
+		{
+			if (width <= 0)
+				throw new ArgumentException("width must be bigger than 0", "width");
+
+			return new DisplayResolutionUnit()
+			{
+				UseVP = true,
+				UseDeviceScale = false,
+				ViewportWidth = width
+			};
+		}
+
+		internal static DisplayResolutionUnit FromInit(bool useDP)
+		{
+			return useDP ? DP() : Pixel();
+		}
+
+		DisplayResolutionUnit() { }
+
+		public bool UseDP { get; private set; }
+
+		public bool UseDeviceScale { get; private set; }
+
+		public bool UseVP { get; private set; }
+
+		public double ViewportWidth { get; private set; } = -1;
+	}
+}
