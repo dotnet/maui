10f1043762e91ff3b45cf8ca4ff948ce6bf291aa
KyNam <trankynam@gmail.com>
2020-09-29 01:23:38 +0700

    MacOS - Fix crash when invoking VisualElement.Focus() (#12267)
    NSInternalInconsistencyException: NSWindow: -_oldFirstResponderBeforeBecoming is not a valid message outside of a responder's implementation of -becomeFirstResponder.
    
    https://developer.apple.com/documentation/appkit/nsresponder/1526750-becomefirstresponder?language=objc
    https://developer.apple.com/documentation/appkit/nsresponder/1532115-resignfirstresponder?language=objc
    
    Use the NSWindow makeFirstResponder: method, not this method, to make an object the first responder. Never invoke this method directly.

--- Xamarin.Forms.Platform.iOS/ViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/ViewRenderer.cs
@@ -305,8 +305,12 @@ namespace Xamarin.Forms.Platform.MacOS
 		{
 			if (Control == null)
 				return;
-
+				
+#if __MOBILE__
 			focusRequestArgs.Result = focusRequestArgs.Focus ? Control.BecomeFirstResponder() : Control.ResignFirstResponder();
+#else
+			focusRequestArgs.Result = focusRequestArgs.Focus ? Control.Window.MakeFirstResponder(Control) : Control.Window.MakeFirstResponder(null);
+#endif
 		}
 	}
 }
