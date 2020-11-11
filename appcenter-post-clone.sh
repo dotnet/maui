2e0ec1980830558fc5ad74058c938691d67be89b
Janus Weil <janus@gcc.gnu.org>
2020-11-11 14:49:05 +0100

    Update Samples.iOS app to be compatible with App Store (#1508)

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -5,6 +5,6 @@ echo "Variables:"
 # Updating manifest
 sed -i '' "s/AC_IOS/$AC_IOS/g" $BUILD_REPOSITORY_LOCALPATH/Samples/Samples/Helpers/CommonConstants
 
-sed -i '' "s/APP_SECRET/$APP_SECRET/g" $BUILD_REPOSITORY_LOCALPATH/Samples/Samples.iOS/Info.plist
+sed -i '' "s/APP-SECRET/$APP_SECRET/g" $BUILD_REPOSITORY_LOCALPATH/Samples/Samples.iOS/Info.plist
 
 echo "Manifest updated!"
