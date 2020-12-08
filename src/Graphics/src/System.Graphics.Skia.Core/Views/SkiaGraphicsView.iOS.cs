94d374d63085e3b3ce7f9f8194340951ee51e626
James Clancey <james.clancey@gmail.com>
2020-12-08 12:54:40 -0900

    Working on Skia Cleanup

---
+++ src/Graphics/src/System.Graphics.Skia.Core/Views/SkiaGraphicsView.iOS.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UIKit;
+namespace System.Graphics.Skia.Views {
+    public class SkiaGraphicsView : UIView {
+        public SkiaGraphicsView ()
+        {
+        }
+    }
+}
