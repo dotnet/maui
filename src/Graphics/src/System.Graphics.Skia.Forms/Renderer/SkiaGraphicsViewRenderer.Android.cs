8e9fb8bfc91e7dac229f90701ca42099a4d91457
Jon Lipsky <jon@elevenworks.com>
2020-12-08 21:54:27 -0800

    Added Forms renderers for SkiaGraphicsView

---
+++ src/Graphics/src/System.Graphics.Skia.Forms/Renderer/SkiaGraphicsViewRenderer.Android.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Graphics.Android;
+using System.Graphics.Forms.Android;
+using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(System.Graphics.Forms.SkiaGraphicsView), typeof(SkiaGraphicsViewRenderer))]
+namespace System.Graphics.Forms.Android
+{
+    [Preserve]
+    public class SkiaGraphicsViewRenderer : ViewRenderer<System.Graphics.Forms.SkiaGraphicsView, NativeGraphicsView>
+    {
+        public SkiaGraphicsViewRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SkiaGraphicsView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                // Unsubscribe from event handlers and cleanup any resources
+                SetNativeControl(null);
+            }
+
+            if (e.NewElement != null)
+            {
+                SetNativeControl(new NativeGraphicsView(Context));
+            }
+        }
+
+        protected override void OnElementPropertyChanged(
+            object sender,
+            PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == nameof(SkiaGraphicsView.Drawable))
+                UpdateDrawable();
+        }
+
+        private void UpdateDrawable()
+        {
+            Control.Drawable = Element.Drawable;
+        }
+    }
+}
---
+++ src/Graphics/src/System.Graphics.Skia.Forms/Renderer/SkiaGraphicsViewRenderer.iOS.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Graphics.CoreGraphics;
+using System.Graphics.Forms;
+using System.Graphics.Forms.iOS;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(SkiaGraphicsView), typeof(SkiaGraphicsViewRenderer))]
+namespace System.Graphics.Forms.iOS
+{
+    [Preserve]
+    public class SkiaGraphicsViewRenderer : ViewRenderer<SkiaGraphicsView, NativeGraphicsView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<SkiaGraphicsView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                // Unsubscribe from event handlers and cleanup any resources
+                SetNativeControl(null);
+            }
+
+            if (e.NewElement != null)
+            {
+                SetNativeControl(new NativeGraphicsView());
+            }
+        }
+
+        protected override void OnElementPropertyChanged(
+            object sender,
+            PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == nameof(SkiaGraphicsView.Drawable))
+                UpdateDrawable();
+        }
+
+        private void UpdateDrawable()
+        {
+            Control.Drawable = Element.Drawable;
+        }
+    }
+}
