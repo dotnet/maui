cfd0ea05c29a6bd7c816b57f85957fc3bd6be095
Javier Suárez <javiersuarezruiz@hotmail.com>
2020-12-15 16:07:02 +0100

    [iOS/macOS] Fix NRE in ButtonRenderer (#13137) fixes #13136
    * Fix NRE in iOS ButtonRenderer
    
    * Protected from NRE in more renderers

--- Xamarin.Forms.Platform.iOS/Renderers/ButtonRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/ButtonRenderer.cs
@@ -84,7 +84,7 @@ namespace Xamarin.Forms.Platform.iOS
 
 		public override void LayoutSubviews()
 		{
-			if (_previousSize != Bounds.Size)
+			if (Element != null && _previousSize != Bounds.Size)
 			{
 				SetBackground(Element.Background);
 				SetNeedsDisplay();
