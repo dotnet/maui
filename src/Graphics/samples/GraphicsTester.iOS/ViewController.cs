c00fce22562de801fad66e5526f23f7a7b6679f4
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-01-04 23:35:06 +0100

    Added GraphicsTester.iOS (#2)

---
+++ src/Graphics/samples/GraphicsTester.iOS/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UIKit;
+
+namespace GraphicsTester.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+            View = new TesterView(View.Bounds);
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+            var frame = View.Window.Frame;
+            View.Frame = frame;
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+        }
+    }
+}
