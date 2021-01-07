ec380e4e24b213500a01992b3e10c5e8a1af3b3a
Shane Neuville <shneuvil@microsoft.com>
2021-01-07 07:07:12 -0600

    Make sure all the UIViews is iOS are added at the correct hierarchy (#13300) fixes #13295
    * Fix layout of FlyoutContent
    
    * - fix ui tests

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/TestPages/TestPages.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/TestPages/TestPages.cs
@@ -91,9 +91,9 @@ namespace Xamarin.Forms.Controls
 #if __IOS__
 		static IApp InitializeiOSApp()
 		{
-			string UDID = null;
+			string UDID = "";
 
-			if(TestContext.Parameters.Exists("UDID"))
+			if (TestContext.Parameters.Exists("UDID"))
 			{
 				UDID = TestContext.Parameters["UDID"];
 			}
