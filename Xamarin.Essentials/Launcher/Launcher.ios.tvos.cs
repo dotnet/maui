c541cbe04396033a9caebc5074d95685477c6563
Dimov Dima <dimovde@yandex.ru>
2021-01-16 02:50:40 +0300

    [Launcher][iOS] Fixing save to files bug (#1609)
    * changed creating UIDocumentInteractionController on Launcher
    * changed PlatformOpenAsync on Launcher iOS

--- Xamarin.Essentials/Launcher/Launcher.ios.tvos.cs
+++ Xamarin.Essentials/Launcher/Launcher.ios.tvos.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using CoreGraphics;
 using Foundation;
 using UIKit;
 
@@ -34,43 +35,22 @@ namespace Xamarin.Essentials
 
         static Task PlatformOpenAsync(OpenFileRequest request)
         {
-            var fileUrl = NSUrl.FromFilename(request.File.FullPath);
-
-            documentController = UIDocumentInteractionController.FromUrl(fileUrl);
-            documentController.Delegate = new DocumentControllerDelegate
+            documentController = new UIDocumentInteractionController()
             {
-                DismissHandler = () =>
-                {
-                    documentController?.Dispose();
-                    documentController = null;
-                }
+                Name = request.File.FileName,
+                Url = NSUrl.FromFilename(request.File.FullPath),
+                Uti = request.File.ContentType
             };
-            documentController.Uti = request.File.ContentType;
 
-            var vc = Platform.GetCurrentViewController();
+            var view = Platform.GetCurrentUIViewController().View;
+            var rect = DeviceInfo.Idiom == DeviceIdiom.Tablet
+                ? new CGRect(new CGPoint(view.Bounds.Width / 2, view.Bounds.Height), CGRect.Empty.Size)
+                : view.Bounds;
 
-            CoreGraphics.CGRect? rect = null;
-            if (DeviceInfo.Idiom == DeviceIdiom.Tablet)
-            {
-                rect = new CoreGraphics.CGRect(new CoreGraphics.CGPoint(vc.View.Bounds.Width / 2, vc.View.Bounds.Height), CoreGraphics.CGRect.Empty.Size);
-            }
-            else
-            {
-                rect = vc.View.Bounds;
-            }
-
-            documentController.PresentOpenInMenu(rect.Value, vc.View, true);
-
+            documentController.PresentOpenInMenu(rect, view, true);
             return Task.CompletedTask;
         }
 
-        class DocumentControllerDelegate : UIDocumentInteractionControllerDelegate
-        {
-            public Action DismissHandler { get; set; }
-
-            public override void DidDismissOpenInMenu(UIDocumentInteractionController controller)
-                => DismissHandler?.Invoke();
-        }
 #else
         static Task PlatformOpenAsync(OpenFileRequest request) =>
             throw new FeatureNotSupportedException();
