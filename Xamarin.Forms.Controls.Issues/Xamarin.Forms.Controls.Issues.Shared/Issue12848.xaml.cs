6f9b1f6169219fafe4ef8a0f3d19ecf468146138
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-01-20 12:22:07 +0100

    [Android] CarouselView position resets when visibility toggled (#12864) fixes #12848
    * Fix issue with CarouselView position and visibility on Android
    
    * Updated instructions
    
    * Added UITest
    
    * Fix the issue

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12848.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 12848, "[Bug] CarouselView position resets when visibility toggled",
+		PlatformAffected.Android)]
+	public partial class Issue12848 : TestContentPage
+	{
+		protected override void Init()
+		{
+#if APP
+			InitializeComponent();
+
+			BindingContext = new List<int> { 1, 2, 3 };
+#endif
+		}
+
+#if APP
+		void OnShowButtonClicked(object sender, EventArgs e)
+		{
+			carousel.IsVisible = true;
+		}
+
+		void OnHideButtonClicked(object sender, EventArgs e)
+		{
+			carousel.IsVisible = false;
+		}
+#endif
+
+#if UITEST
+		[Test]
+		public void Issue12848Test()
+		{
+			RunningApp.WaitForElement("TestCarouselView");
+			RunningApp.SwipeRightToLeft();
+			Assert.AreEqual(1, int.Parse(RunningApp.Query(q => q.Marked("CarouselPosition"))[0].Text));
+			RunningApp.Tap("HideButton");
+			RunningApp.Tap("ShowButton");
+			Assert.AreEqual(1, int.Parse(RunningApp.Query(q => q.Marked("CarouselPosition"))[0].Text));
+			RunningApp.Screenshot("Test passed");
+		}
+#endif
+	}
+}
