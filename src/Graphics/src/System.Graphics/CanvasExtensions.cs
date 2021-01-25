52db08cedffff79a18fb243bed5afd1cd483d4d3
Jon Lipsky <jon@elevenworks.com>
2021-01-25 15:47:33 -0800

    Addresses issue #37
    Adds support for double precision primitives.

--- src/Graphics/src/System.Graphics/CanvasExtensions.cs
+++ src/Graphics/src/System.Graphics/CanvasExtensions.cs
@@ -7,14 +7,29 @@ namespace System.Graphics
             target.DrawLine(point1.X, point1.Y, point2.X, point2.Y);
         }
 
+        public static void DrawRectangle(this ICanvas target, Rectangle rect)
+        {
+            target.DrawRectangle((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height);
+        }
+
         public static void DrawRectangle(this ICanvas target, RectangleF rect)
         {
             target.DrawRectangle(rect.X, rect.Y, rect.Width, rect.Height);
         }
 
+        public static void FillRectangle(this ICanvas target, Rectangle rect)
+        {
+            target.FillRectangle((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height);
+        }
+
         public static void FillRectangle(this ICanvas target, RectangleF rect)
         {
             target.FillRectangle(rect.X, rect.Y, rect.Width, rect.Height);
+        }
+
+        public static void DrawRoundedRectangle(this ICanvas target, Rectangle rect, double cornerRadius)
+        {
+            target.DrawRoundedRectangle((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height, (float)cornerRadius);
         }
 
         public static void DrawRoundedRectangle(this ICanvas target, RectangleF rect, float cornerRadius)
@@ -29,11 +44,23 @@ namespace System.Graphics
             target.DrawPath(path);
         }
 
+        public static void DrawRoundedRectangle(this ICanvas target, Rectangle rect, double topLeftCornerRadius, double topRightCornerRadius, double bottomLeftCornerRadius, double bottomRightCornerRadius)
+        {
+            var path = new PathF();
+            path.AppendRoundedRectangle((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height, (float)topLeftCornerRadius, (float)topRightCornerRadius, (float)bottomLeftCornerRadius, (float)bottomRightCornerRadius);
+            target.DrawPath(path);
+        }
+
         public static void DrawRoundedRectangle(this ICanvas target, RectangleF rect, float topLeftCornerRadius, float topRightCornerRadius, float bottomLeftCornerRadius, float bottomRightCornerRadius)
         {
             var path = new PathF();
             path.AppendRoundedRectangle(rect, topLeftCornerRadius, topRightCornerRadius, bottomLeftCornerRadius, bottomRightCornerRadius);
             target.DrawPath(path);
+        }
+
+        public static void FillRoundedRectangle(this ICanvas target, Rectangle rect, double cornerRadius)
+        {
+            target.FillRoundedRectangle((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height, (float)cornerRadius);
         }
 
         public static void FillRoundedRectangle(this ICanvas target, RectangleF rect, float cornerRadius)
@@ -48,6 +75,13 @@ namespace System.Graphics
             target.FillPath(path);
         }
 
+        public static void FillRoundedRectangle(this ICanvas target, Rectangle rect, double topLeftCornerRadius, double topRightCornerRadius, double bottomLeftCornerRadius, double bottomRightCornerRadius)
+        {
+            var path = new PathF();
+            path.AppendRoundedRectangle((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height, (float)topLeftCornerRadius, (float)topRightCornerRadius, (float)bottomLeftCornerRadius, (float)bottomRightCornerRadius);
+            target.FillPath(path);
+        }
+
         public static void FillRoundedRectangle(this ICanvas target, RectangleF rect, float topLeftCornerRadius, float topRightCornerRadius, float bottomLeftCornerRadius, float bottomRightCornerRadius)
         {
             var path = new PathF();
@@ -55,14 +89,30 @@ namespace System.Graphics
             target.FillPath(path);
         }
 
+        public static void DrawEllipse(this ICanvas target, Rectangle rect)
+        {
+            target.DrawEllipse((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height);
+        }
+
         public static void DrawEllipse(this ICanvas target, RectangleF rect)
         {
             target.DrawEllipse(rect.X, rect.Y, rect.Width, rect.Height);
         }
 
+        public static void FillEllipse(this ICanvas target, Rectangle rect)
+        {
+            target.FillEllipse((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height);
+        }
+
         public static void FillEllipse(this ICanvas target, RectangleF rect)
         {
             target.FillEllipse(rect.X, rect.Y, rect.Width, rect.Height);
+        }
+
+        [Obsolete]
+        public static void DrawOval(this ICanvas target, Rectangle rect)
+        {
+            target.DrawEllipse((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height);
         }
 
         [Obsolete]
@@ -75,6 +125,12 @@ namespace System.Graphics
         public static void DrawOval(this ICanvas target, float x, float y, float width, float height)
         {
             target.DrawEllipse(x,y,width, height);
+        }
+
+        [Obsolete]
+        public static void FillOval(this ICanvas target, Rectangle rect)
+        {
+            target.FillEllipse((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height);
         }
 
         [Obsolete]
@@ -109,9 +165,26 @@ namespace System.Graphics
             target.ClipPath(path, windingMode);
         }
 
+        public static void ClipRectangle(this ICanvas target, Rectangle rect)
+        {
+            target.ClipRectangle((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height);
+        }
+
         public static void ClipRectangle(this ICanvas target, RectangleF rect)
         {
             target.ClipRectangle(rect.X, rect.Y, rect.Width, rect.Height);
+        }
+
+        public static void DrawString(
+            this ICanvas target,
+            string value,
+            Rectangle bounds,
+            HorizontalAlignment horizontalAlignment,
+            VerticalAlignment verticalAlignment,
+            TextFlow textFlow = TextFlow.ClipBounds,
+            float lineSpacingAdjustment = 0)
+        {
+            target.DrawString(value, (float)bounds.X, (float)bounds.Y, (float)bounds.Width, (float)bounds.Height, horizontalAlignment, verticalAlignment, textFlow, lineSpacingAdjustment);
         }
 
         public static void DrawString(
@@ -135,6 +208,15 @@ namespace System.Graphics
             target.FillEllipse(x, y, size, size);
         }
 
+        public static void FillCircle(this ICanvas target, Point center, double radius)
+        {
+            var x = center.X - radius;
+            var y = center.Y - radius;
+            var size = radius * 2;
+
+            target.FillEllipse((float)x, (float)y, (float)size, (float)size);
+        }
+
         public static void FillCircle(this ICanvas target, PointF center, float radius)
         {
             var x = center.X - radius;
@@ -151,6 +233,15 @@ namespace System.Graphics
             var size = radius * 2;
 
             target.DrawEllipse(x, y, size, size);
+        }
+
+        public static void DrawCircle(this ICanvas target, Point center, double radius)
+        {
+            var x = center.X - radius;
+            var y = center.Y - radius;
+            var size = radius * 2;
+
+            target.DrawEllipse((float)x, (float)y, (float)size, (float)size);
         }
 
         public static void DrawCircle(this ICanvas target, PointF center, float radius)
@@ -199,6 +290,22 @@ namespace System.Graphics
         }
 
         /// <summary>
+        /// Draws the arc.  This is a helper method to draw an arc when you have a rectangle already defined
+        /// for the ellipse bounds.
+        /// </summary>
+        /// <param name="canvas">canvas</param>
+        /// <param name="bounds">The ellipse bounds.</param>
+        /// <param name="startAngle">The start angle</param>
+        /// <param name="endAngle">The end angle</param>
+        /// <param name="clockwise">The direction to draw the arc</param>
+        /// <param name="closed">If the arc is closed or not</param>
+        public static void DrawArc(this ICanvas canvas, Rectangle bounds, float startAngle, float endAngle, bool clockwise, bool closed)
+        {
+            canvas.DrawArc((float)bounds.X, (float)bounds.Y, (float)bounds.Width, (float)bounds.Height, startAngle, endAngle, clockwise, closed);
+        }
+
+
+        /// <summary>
         /// Fills the arc.  This is a helper method to fill an arc when you have a rectangle already defined
         /// for the ellipse bounds.
         /// </summary>
@@ -210,6 +317,20 @@ namespace System.Graphics
         public static void FillArc(this ICanvas canvas, RectangleF bounds, float startAngle, float endAngle, bool clockwise)
         {
             canvas.FillArc(bounds.X, bounds.Y, bounds.Width, bounds.Height, startAngle, endAngle, clockwise);
+        }
+
+        /// <summary>
+        /// Fills the arc.  This is a helper method to fill an arc when you have a rectangle already defined
+        /// for the ellipse bounds.
+        /// </summary>
+        /// <param name="canvas">canvas</param>
+        /// <param name="bounds">The ellipse bounds.</param>
+        /// <param name="startAngle">The start angle</param>
+        /// <param name="endAngle">The end angle</param>
+        /// <param name="clockwise">The direction to draw the arc</param>
+        public static void FillArc(this ICanvas canvas, Rectangle bounds, float startAngle, float endAngle, bool clockwise)
+        {
+            canvas.FillArc((float)bounds.X, (float)bounds.Y, (float)bounds.Width, (float)bounds.Height, startAngle, endAngle, clockwise);
         }
 
         /// <summary>
@@ -266,14 +387,29 @@ namespace System.Graphics
             }
         }
 
+        public static void SubtractFromClip(this ICanvas target, Rectangle rect)
+        {
+            target.SubtractFromClip((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height);
+        }
+
         public static void SubtractFromClip(this ICanvas target, RectangleF rect)
         {
             target.SubtractFromClip(rect.X, rect.Y, rect.Width, rect.Height);
         }
 
+        public static void SetFillPaint(this ICanvas target, Paint paint, Point point1, Point point2)
+        {
+            target.SetFillPaint(paint, (float)point1.X, (float)point1.Y, (float)point2.X, (float)point2.Y);
+        }
+
         public static void SetFillPaint(this ICanvas target, Paint paint, PointF point1, PointF point2)
         {
             target.SetFillPaint(paint, point1.X, point1.Y, point2.X, point2.Y);
+        }
+
+        public static void SetFillPaint(this ICanvas target, Paint paint, Rectangle rectangle)
+        {
+            target.SetFillPaint(paint, (float)rectangle.Left, (float)rectangle.Top, (float)rectangle.Bottom, (float)rectangle.Right);
         }
 
         public static void SetFillPaint(this ICanvas target, Paint paint, RectangleF rectangle)
