130499628b44caabf6b3d8fc5e02e87c07c0b651
Rachel Kang <rachelkang@microsoft.com>
2021-01-27 11:34:08 -0500

    Fix RTL on Labels with FormattedText and/or LineHeight (#13197) fixes #3311 fixes #12473 fixes #8157
    * Add Issue3311.cs
    
    * Update Issue331.cs test file
    
    * Fix RTL issue on FormattedText
    
    * Add relevant RTL FormattedText test
    
    * Update Issue3311.cs
    
    * Add Issue12473
    
    * Add BackgroundColor to test labels for clarit
    
    * Add LabelTest for RTL Label with LineHeight
    
    * Add missing closing EmbeddedResource tag
    
    * Remove UITest template code
    
    * Update RTL tests to be more thorough
    
    * UpdateHorizontalTextAlignment on UpdateFormattedText
    
    Fix RTL issue that arises when certain text properties are set on a Label w/ RTL and FormattedText (i.e. FontAttributes, FontSize, LineHeight, TextColor, TextTransform, etc)
    
    * Remove TextType from tests
    
    TextType on FormattedText labels causes app crash. This separate issue will be fixed in a follow-up PR

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12473.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.ManualReview)]
+#endif
+#if APP
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 12473, "iOS Label text alignment in RTL with LineHeight", PlatformAffected.iOS)]
+	public partial class Issue12473 : TestContentPage
+	{
+		public Issue12473()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+			
+		}
+	}
+}
