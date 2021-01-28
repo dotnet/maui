d61c3611f2702f35a2bfb2e8b6bf4b5ff0e91924
E.Z. Hart <hartez@users.noreply.github.com>
2021-01-28 15:17:21 -0700

    Move core tests to xUnit (#13567)

--- src/Platform.Handlers/tests/Xamarin.Platform.Handlers.UnitTests/Layouts/HorizontalStackLayoutManagerTests.cs
+++ src/Platform.Handlers/tests/Xamarin.Platform.Handlers.UnitTests/Layouts/HorizontalStackLayoutManagerTests.cs
@@ -1,21 +1,22 @@
 ﻿using System.Collections.Generic;
 using NSubstitute;
-using NUnit.Framework;
 using Xamarin.Forms;
 using Xamarin.Platform.Handlers.Tests;
 using Xamarin.Platform.Layouts;
+using Xunit;
 
 namespace Xamarin.Platform.Handlers.UnitTests.Layouts
 {
-	[TestFixture(Category = TestCategory.Layout)]
+	[Category(TestCategory.Core, TestCategory.Layout)]
 	public class HorizontalStackLayoutManagerTests : StackLayoutManagerTests
 	{
-		[TestCase(0, 100, 0, 0, Description = "No items, width should be zero")]
-		[TestCase(1, 100, 0, 100, Description = "One item, width should match the item")]
-		[TestCase(1, 100, 13, 100, Description = "One item, spacing should have no effect")]
-		[TestCase(2, 100, 13, 213, Description = "Two items, spacing should count once [(100 * 2) + 13 = 213]")]
-		[TestCase(3, 100, 13, 326, Description = "Three items, spacing should count twice [(100 * 3) + (2 * 13) = 326]")]
-		[TestCase(3, 100, -13, 274, Description = "Negative spacing overlaps items [(100 * 3) + (2 * -13) = 274]")]
+		[Theory]
+		[InlineData(0, 100, 0, 0)]
+		[InlineData(1, 100, 0, 100)]
+		[InlineData(1, 100, 13, 100)]
+		[InlineData(2, 100, 13, 213)]
+		[InlineData(3, 100, 13, 326)]
+		[InlineData(3, 100, -13, 274)]
 		public void SpacingMeasurement(int viewCount, double viewWidth, int spacing, double expectedWidth)
 		{
 			var stack = BuildStack(viewCount, viewWidth, 100);
@@ -24,11 +25,11 @@ namespace Xamarin.Platform.Handlers.UnitTests.Layouts
 			var manager = new HorizontalStackLayoutManager(stack);
 			var measuredSize = manager.Measure(double.PositiveInfinity, 100);
 
-			Assert.That(measuredSize.Width, Is.EqualTo(expectedWidth));
+			Assert.Equal(expectedWidth, measuredSize.Width);
 		}
 
-		[Test(Description = "Spacing should not affect arrangement with only one item")]
-		[TestCase(0), TestCase(26), TestCase(-54)]
+		[Theory("Spacing should not affect arrangement with only one item")]
+		[InlineData(0), InlineData(26), InlineData(-54)]
 		public void SpacingArrangementOneItem(int spacing)
 		{
 			var stack = BuildStack(1, 100, 100);
@@ -43,8 +44,8 @@ namespace Xamarin.Platform.Handlers.UnitTests.Layouts
 			stack.Children[0].Received().Arrange(Arg.Is(expectedRectangle));
 		}
 
-		[Test(Description = "Spacing should affect arrangement with more than one item")]
-		[TestCase(26), TestCase(-54)]
+		[Theory("Spacing should affect arrangement with more than one item")]
+		[InlineData(26), InlineData(-54)]
 		public void SpacingArrangementTwoItems(int spacing)
 		{
 			var stack = BuildStack(2, 100, 100);
@@ -62,9 +63,10 @@ namespace Xamarin.Platform.Handlers.UnitTests.Layouts
 			stack.Children[1].Received().Arrange(Arg.Is(expectedRectangle1));
 		}
 
-		[TestCase(150, 100, 100, Description = "The Stack's specified width of 100 should override the child's measured width of 150")]
-		[TestCase(150, 200, 200, Description = "The Stack's specified width of 200 should override the child's measured width of 150")]
-		[TestCase(1250, -1, 1250, Description = "The Stack doesn't specify width, so the child determines the width")]
+		[Theory]
+		[InlineData(150, 100, 100)]
+		[InlineData(150, 200, 200)]
+		[InlineData(1250, -1, 1250)]
 		public void StackAppliesWidth(double viewWidth, double stackWidth, double expectedWidth)
 		{
 			var stack = CreateTestLayout();
@@ -78,11 +80,11 @@ namespace Xamarin.Platform.Handlers.UnitTests.Layouts
 
 			var manager = new HorizontalStackLayoutManager(stack);
 			var measurement = manager.Measure(double.PositiveInfinity, 100);
-			Assert.That(measurement.Width, Is.EqualTo(expectedWidth));
+			Assert.Equal(expectedWidth, measurement.Width);
 		}
 
-		[Test]
-		public void ViewsArrangedWithDesiredHeights() 
+		[Fact]
+		public void ViewsArrangedWithDesiredHeights()
 		{
 			var stack = CreateTestLayout();
 			var manager = new HorizontalStackLayoutManager(stack);
@@ -97,7 +99,7 @@ namespace Xamarin.Platform.Handlers.UnitTests.Layouts
 			manager.Arrange(new Rectangle(Point.Zero, measurement));
 
 			// The tallest IView is 200, so the stack should be that tall
-			Assert.That(measurement.Height, Is.EqualTo(200));
+			Assert.Equal(200, measurement.Height);
 
 			// We expect the first IView to be at 0,0 with a width of 100 and a height of 200
 			var expectedRectangle1 = new Rectangle(0, 0, 100, 200);
--- src/Platform.Handlers/tests/Xamarin.Platform.Handlers.UnitTests/TestClasses/HandlerStub.cs
+++ src/Platform.Handlers/tests/Xamarin.Platform.Handlers.UnitTests/TestClasses/HandlerStub.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using NUnit.Framework;
-
 
 namespace Xamarin.Platform.Handlers.UnitTests
 {
