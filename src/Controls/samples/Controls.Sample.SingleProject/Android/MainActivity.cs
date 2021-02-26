0e94f1401a276fb2f7f9699ddd1f63b4a730fdfc
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-02-26 09:07:26 -0600

    .NET Maui "Single Project" (#13858)
    Context: https://github.com/xamarin/xamarin-android/blob/fcd9c30164d4ac0e88440357b5c96b9fdebccb39/Documentation/guides/OneDotNetSingleProject.md
    
    We are aiming to get a new project type working that would have a
    `.csproj` such as:
    
        <Project Sdk="Microsoft.NET.Sdk">
          <PropertyGroup>
            <TargetFramework>net6.0-android;net6.0-ios</TargetFramework>
            <OutputType>Exe</OutputType>
            <SingleProject>true</SingleProject>
          </PropertyGroup>
        </Project>
    
    Within the project you would have:
    
    * `*.cs` all shared code
    * `iOS/*` - iOS only stuff, including `Info.plist`
    * `Android/*` - Android only stuff, including `AndroidManifest.xml`
    
    I also used some other new settings that results in a smaller
    `Info.plist` and `AndroidManifest.xml`:
    
        <ApplicationTitle>Single Project</ApplicationTitle>
        <ApplicationId>com.microsoft.maui.singleproject</ApplicationId>
        <ApplicationVersion>1.0</ApplicationVersion>
        <AndroidVersionCode>1</AndroidVersionCode>
    
    The eventual goal being we might be able to completely get rid of
    `Info.plist` and `AndroidManifest.xml` for simple apps. You would only
    need them for edge-case, platform-specific features.
    
    To get this working within a NuGet package, we can include two new
    files:
    
    * `Xamarin.Forms.SingleProject.props`
    * `Xamarin.Forms.SingleProject.targets`
    
    They only enable things conditionally, if `$(SingleProject)` is set.
    
    I created a new sample with a single `Label` to demonstrate things
    working.

---
+++ src/Controls/samples/Controls.Sample.SingleProject/Android/MainActivity.cs
@@ -0,0 +1,11 @@
+﻿using Android.App;
+using Microsoft.Maui;
+
+namespace Maui.Controls.Sample.SingleProject
+{
+	[Activity(Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+	public class MainActivity : MauiAppCompatActivity
+	{
+
+	}
+}
---
+++ src/Controls/samples/Controls.Sample.SingleProject/MyApp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace Maui.Controls.Sample.SingleProject
+{
+	public class MyApp : MauiApp
+	{
+		public override IAppHostBuilder CreateBuilder() => 
+			base.CreateBuilder().ConfigureServices((ctx, services) =>
+				{
+					services.AddTransient<MainPage>();
+					services.AddTransient<IWindow, MainWindow>();
+				});
+
+		public override IWindow GetWindowFor(IActivationState state) =>
+			Services.GetService<IWindow>();
+	}
+}
