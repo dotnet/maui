44a6d8c2a890568049aa871845332d1c018a6ba8
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-03-03 14:10:24 -0600

    Make template use .xaml & fix build
    A couple of issues to fix the `Microsoft.Maui.Controls.Build.Tasks`
    NuGet package:
    
    * I deleted `Microsoft.Maui.Controls.nuspec` since is it unused? Its
      existence only confused me.
    * The wrong file was the MSBuild entry point:
      `Microsoft.Maui.Controls.props/targets` should be the one that is
      renamed to be `Microsoft.Maui.Controls.Build.Task.props/targets`.
    * We need `Microsoft.Maui.dll` for the build tasks to load.

--- src/Controls/src/Templates/maui-mobile/MainPage.xaml.cs
+++ src/Controls/src/Templates/maui-mobile/MainPage.xaml.cs
@@ -1,17 +1,15 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
 
 namespace MauiApp1
 {
-	public class MainPage : ContentPage, IPage
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage, IPage
 	{
 		public MainPage()
 		{
-			Content = new Label
-			{
-				Text = "Hello, .NET MAUI Single Project!",
-				BackgroundColor = Color.White
-			};
+			InitializeComponent();
 		}
 
 		public IView View { get => (IView)Content; set => Content = (View)value; }
