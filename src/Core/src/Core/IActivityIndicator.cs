4b664f2181033533363af24b8a5a2b05d76ac1fe
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-04 10:22:09 +0100

    ActivityIndicatorHandler

---
+++ src/Core/src/Core/IActivityIndicator.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.Maui
+{
+	public interface IActivityIndicator : IView
+	{
+		bool IsRunning { get; }
+		Color Color { get; }
+	}
+}
---
+++ src/Core/src/Handlers/ActivityIndicator/ActivityIndicatorHandler.Standard.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Microsoft.Maui.Handlers
+{
+	public partial class ActivityIndicatorHandler : AbstractViewHandler<IActivityIndicator, object>
+	{
+		protected override object CreateNativeView() => throw new NotImplementedException();
+	}
+}
