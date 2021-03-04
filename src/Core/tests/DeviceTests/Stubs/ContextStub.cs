0cd83759cf345187ca0968ec63527016a080c19d
Matthew Leibowitz <mattleibow@live.com>
2021-03-04 04:10:16 +0200

    Fix the Core tests

---
+++ src/Core/tests/DeviceTests/Stubs/ContextStub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Maui.DeviceTests.Stubs
+{
+	class ContextStub : IMauiContext, IDisposable
+	{
+		AppStub _app;
+
+		public ContextStub(AppStub app)
+		{
+			_app = app;
+		}
+
+		public IServiceProvider Services =>
+			_app.Services;
+
+		public IMauiHandlersServiceProvider Handlers =>
+			Services.GetRequiredService<IMauiHandlersServiceProvider>();
+
+#if __ANDROID__
+		public Android.Content.Context Context =>
+			Android.App.Application.Context;
+#endif
+
+		public void Dispose()
+		{
+			_app = null;
+		}
+	}
+}
