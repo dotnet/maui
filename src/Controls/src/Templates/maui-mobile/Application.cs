af679489bd1dda1df29f2954645b580b0ab3b43c
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-03-04 21:41:53 -0600

    .NET 6 maui-mobile template (#371)

---
+++ src/Controls/src/Templates/maui-mobile/Application.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace MauiApp1
+{
+	public class Application : MauiApp
+	{
+		public override IAppHostBuilder CreateBuilder() => 
+			base.CreateBuilder().ConfigureServices((ctx, services) =>
+				{
+					services.AddTransient<MainPage>();
+					services.AddTransient<IWindow, MainWindow>();
+				});
+
+		public override IWindow CreateWindow(IActivationState state)
+		{
+			Microsoft.Maui.Controls.Compatibility.Forms.Init(state);
+			return Services.GetService<IWindow>();
+		}
+	}
+}
