b73b615378fff9bbe4eda4cad197b6cb6034b4c2
Almir Vuk <almir.vuk@outlook.com>
2021-03-05 15:57:28 +0100

    Implement IsTextPredictionEnabled in IEntry Handlers (#423)
    Co-authored-by: E.Z. Hart <hartez@gmail.com>
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>

--- src/Core/src/Handlers/Entry/EntryHandler.cs
+++ src/Core/src/Handlers/Entry/EntryHandler.cs
@@ -6,7 +6,8 @@
 		{
 			[nameof(IEntry.Text)] = MapText,
 			[nameof(IEntry.TextColor)] = MapTextColor,
-			[nameof(IEntry.IsPassword)] = MapIsPassword
+			[nameof(IEntry.IsPassword)] = MapIsPassword,
+			[nameof(IEntry.IsTextPredictionEnabled)] = MapIsTextPredictionEnabled
 		};
 
 		public EntryHandler() : base(EntryMapper)
