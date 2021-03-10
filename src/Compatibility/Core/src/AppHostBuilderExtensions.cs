8170dc19533d1ce2e4e358df55cf34d637cda4a0
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-10 13:33:35 +0100

    Avoid Editor in RegisterCompatibilityRenderers

--- src/Compatibility/Core/src/AppHostBuilderExtensions.cs
+++ src/Compatibility/Core/src/AppHostBuilderExtensions.cs
@@ -1,12 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Compatibility;
 using Microsoft.Maui.Hosting;
-using Microsoft.Maui.Handlers;
 
 namespace Microsoft.Maui.Controls.Compatibility
 {
@@ -17,13 +11,14 @@ namespace Microsoft.Maui.Controls.Compatibility
 			// This won't really be a thing once we have all the handlers built
 			var defaultHandlers = new List<Type>
 			{
-				  typeof(Button) ,
-				  typeof(Entry) ,
-				  typeof(ContentPage) ,
-				  typeof(Page) ,
-				  typeof(Label) ,
-				  typeof(Slider),
-				  typeof(Switch) 
+				typeof(Button),
+				typeof(Editor),
+				typeof(Entry),
+				typeof(ContentPage),
+				typeof(Page),
+				typeof(Label),
+				typeof(Slider),
+				typeof(Switch)
 			};
 
 			Forms.RegisterCompatRenderers(
@@ -58,7 +53,6 @@ namespace Microsoft.Maui.Controls.Compatibility
 			return builder;
 		}
 
-
 		public static IAppHostBuilder RegisterCompatibilityRenderer<TControlType, TMauiType, TRenderer>(this IAppHostBuilder builder)
 			where TMauiType : IFrameworkElement
 		{
@@ -75,6 +69,5 @@ namespace Microsoft.Maui.Controls.Compatibility
 		public static IAppHostBuilder RegisterCompatibilityRenderer<TControlType, TRenderer>(this IAppHostBuilder builder)
 			where TControlType : IFrameworkElement =>		
 				builder.RegisterCompatibilityRenderer<TControlType, TControlType, TRenderer>();
-		
 	}
 }
