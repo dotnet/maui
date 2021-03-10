6a8457a91967a3bdc7333874b812a0c313c3df82
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-10 19:16:47 +0100

    Updated iOS RadioButtonHandler

--- src/Core/src/Handlers/RadioButton/RadioButtonHandler.iOS.cs
+++ src/Core/src/Handlers/RadioButton/RadioButtonHandler.iOS.cs
@@ -5,6 +5,8 @@ namespace Microsoft.Maui.Handlers
 {
 	public partial class RadioButtonHandler : AbstractViewHandler<IRadioButton, UIButton>
 	{
+		static readonly UIControlState[] ControlStates = { UIControlState.Normal, UIControlState.Highlighted, UIControlState.Disabled };
+
 		static CALayer? RadioButtonLayer { get; set; }
 
 		protected override UIButton CreateNativeView()
@@ -12,6 +14,7 @@ namespace Microsoft.Maui.Handlers
 			var nativeRadioButton = new UIButton(UIButtonType.System);
 
 			SetRadioBoxLayer(CreateRadioBoxLayer());
+			SetControlPropertiesFromProxy();
 
 			nativeRadioButton.HorizontalAlignment = UIControlContentHorizontalAlignment.Left;
 
@@ -26,6 +29,19 @@ namespace Microsoft.Maui.Handlers
 		protected virtual CALayer CreateRadioBoxLayer()
 		{
 			return new RadioButtonCALayer(VirtualView, TypedNativeView);
+		}
+
+		void SetControlPropertiesFromProxy()
+		{
+			if (TypedNativeView == null)
+				return;
+
+			foreach (UIControlState uiControlState in ControlStates)
+			{
+				TypedNativeView.SetTitleColor(UIButton.Appearance.TitleColor(uiControlState), uiControlState); // If new values are null, old values are preserved.
+				TypedNativeView.SetTitleShadowColor(UIButton.Appearance.TitleShadowColor(uiControlState), uiControlState);
+				TypedNativeView.SetBackgroundImage(UIButton.Appearance.BackgroundImageForState(uiControlState), uiControlState);
+			}
 		}
 
 		void SetRadioBoxLayer(CALayer layer)
