e94abe6a75a6cda4ac9898f78e4e4aedca114f95
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-10 13:51:22 +0100

    Added Editor handler tests

---
+++ src/Core/tests/DeviceTests/Handlers/Editor/EditorHandlerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Maui.DeviceTests.Stubs;
+using Microsoft.Maui.Handlers;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	[Category(TestCategory.Editor)]
+	public partial class EditorHandlerTests : HandlerTestBase<EditorHandler>
+	{
+		public EditorHandlerTests(HandlerTestFixture fixture) : base(fixture)
+		{
+		}
+
+		[Fact(DisplayName = "Text Initializes Correctly")]
+		public async Task TextInitializesCorrectly()
+		{
+			var editor = new EditorStub()
+			{
+				Text = "Test"
+			};
+
+			await ValidatePropertyInitValue(editor, () => editor.Text, GetNativeText, editor.Text);
+		}
+	}
+}
---
+++ src/Core/tests/DeviceTests/Stubs/EditorStub.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.Maui.DeviceTests.Stubs
+{
+	public partial class EditorStub : StubBase, IEditor
+	{
+		public string Text { get; set; }
+
+		public Color TextColor { get; set; }
+	}
+}
