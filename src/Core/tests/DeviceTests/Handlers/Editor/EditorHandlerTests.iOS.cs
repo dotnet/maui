7dc6f681aab381afa43fcd6dd29629f095a10c58
Rachel Kang <rachelkang@microsoft.com>
2021-03-10 10:27:48 -0500

    Fix iOS handler test to use UITextView instead of UITextField

--- src/Core/tests/DeviceTests/Handlers/Editor/EditorHandlerTests.iOS.cs
+++ src/Core/tests/DeviceTests/Handlers/Editor/EditorHandlerTests.iOS.cs
@@ -5,8 +5,8 @@ namespace Microsoft.Maui.DeviceTests
 {
 	public partial class EditorHandlerTests
 	{
-		UITextField GetNativeEditor(EditorHandler editorHandler) =>
-			(UITextField)editorHandler.View;
+		UITextView GetNativeEditor(EditorHandler editorHandler) =>
+			(UITextView)editorHandler.View;
 
 		string GetNativeText(EditorHandler editorHandler) =>
 			GetNativeEditor(editorHandler).Text;
