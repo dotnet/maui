8ada80424833493a80a0aee601cd637c0615a89d
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-03-10 02:43:42 +0100

    Added XML Comments to Core (#419)
    * Added XML Comments to Core interfaces
    
    * Added more comments
    
    * Cleanup and removal of docs for nonexistent properties
    
    Co-authored-by: E.Z. Hart <hartez@gmail.com>

--- src/Core/src/Core/IText.cs
+++ src/Core/src/Core/IText.cs
@@ -1,9 +1,18 @@
 namespace Microsoft.Maui
 {
+	/// <summary>
+	/// Provides functionality to be able to customize Text.
+	/// </summary>
 	public interface IText
 	{
+		/// <summary>
+		/// Gets the text.
+		/// </summary>
 		string Text { get; }
 
+		/// <summary>
+		/// Gets the text color.
+		/// </summary>
 		Color TextColor { get; }
 	}
 }
--- src/Core/src/Core/ITextInput.cs
+++ src/Core/src/Core/ITextInput.cs
@@ -1,7 +1,13 @@
 namespace Microsoft.Maui
 {
+	/// <summary>
+	/// Represents a View which can take keyboard input.
+	/// </summary>
 	public interface ITextInput : IText
 	{
+		/// <summary>
+		/// Gets the maximum allowed length of input.
+		/// </summary>
 		new string Text { get; set; }
 	}
 }
