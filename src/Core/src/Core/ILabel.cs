9d71e3365b59299f38ce2b1e24e1d1a83e545621
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-11 11:28:51 +0100

    Fixed merge issue

--- src/Core/src/Core/ILabel.cs
+++ src/Core/src/Core/ILabel.cs
@@ -3,7 +3,7 @@ namespace Microsoft.Maui
 	/// <summary>
 	/// Represents a View that displays text.
 	/// </summary>
-	public interface ILabel : IView, IText, IFont, ITextAlignment
+	public interface ILabel : IView, IText, ITextAlignment
 	{
 		/// <summary>
 		/// Gets the space between the text of the Label and it's border.
