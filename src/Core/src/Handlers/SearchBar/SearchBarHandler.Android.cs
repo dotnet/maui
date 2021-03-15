5d7335d6d47922f1fb8efdfa6b11c506d8638bd0
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-15 10:31:43 +0100

    Implement HorizontalTextAlignment in SearchBarHandlers

--- src/Core/src/Handlers/SearchBar/SearchBarHandler.Android.cs
+++ src/Core/src/Handlers/SearchBar/SearchBarHandler.Android.cs
@@ -1,12 +1,20 @@
-﻿using AndroidX.AppCompat.Widget;
+﻿using System.Linq;
+using Android.Widget;
+using SearchView = AndroidX.AppCompat.Widget.SearchView;
 
 namespace Microsoft.Maui.Handlers
 {
 	public partial class SearchBarHandler : AbstractViewHandler<ISearchBar, SearchView>
 	{
+		EditText? _editText;
+
 		protected override SearchView CreateNativeView()
 		{
-			return new SearchView(Context);
+			var searchView = new SearchView(Context);
+
+			_editText ??= searchView.GetChildrenOfType<EditText>().FirstOrDefault();
+
+			return searchView;
 		}
 
 		public static void MapText(SearchBarHandler handler, ISearchBar searchBar)
@@ -17,6 +25,11 @@ namespace Microsoft.Maui.Handlers
 		public static void MapPlaceholder(SearchBarHandler handler, ISearchBar searchBar)
 		{
 			handler.TypedNativeView?.UpdatePlaceholder(searchBar);
+		}
+
+		public static void MapHorizontalTextAlignment(SearchBarHandler handler, ISearchBar searchBar)
+		{
+			handler.TypedNativeView?.UpdateHorizontalTextAlignment(searchBar, handler._editText);
 		}
 	}
 }
