94474ec830d8e4730756aee55c9556e2b17961ac
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-15 11:30:22 +0100

    Moved Picker Handler tests between different classes

--- src/Core/tests/DeviceTests/Handlers/Picker/PickerHandlerTests.Android.cs
+++ src/Core/tests/DeviceTests/Handlers/Picker/PickerHandlerTests.Android.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.Maui.Handlers;
+﻿using System.Threading.Tasks;
+using Microsoft.Maui.DeviceTests.Stubs;
+using Microsoft.Maui.Handlers;
 
 namespace Microsoft.Maui.DeviceTests
 {
 	public partial class PickerHandlerTests
 	{
+		[Fact(DisplayName = "Title Initializes Correctly")]
+		public async Task TitleInitializesCorrectly()
+		{
+			var picker = new PickerStub
+			{
+				Title = "Select an Item"
+			};
+
+			await ValidatePropertyInitValue(picker, () => picker.Title, GetNativeTitle, picker.Title);
+		}
+
 		NativePicker GetNativePicker(PickerHandler pickerHandler) =>
 			(NativePicker)pickerHandler.View;
 
--- src/Core/tests/DeviceTests/Handlers/Picker/PickerHandlerTests.cs
+++ src/Core/tests/DeviceTests/Handlers/Picker/PickerHandlerTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Microsoft.Maui.DeviceTests.Stubs;
+﻿using Microsoft.Maui.DeviceTests.Stubs;
 using Microsoft.Maui.Handlers;
 
 namespace Microsoft.Maui.DeviceTests
@@ -10,18 +9,5 @@ namespace Microsoft.Maui.DeviceTests
 		public PickerHandlerTests(HandlerTestFixture fixture) : base(fixture)
 		{
 		}
-
-#if MONOANDROID
-		[Fact(DisplayName = "Title Initializes Correctly")]
-		public async Task TitleInitializesCorrectly()
-		{
-			var picker = new PickerStub
-			{
-				Title = "Select an Item"
-			};
-
-			await ValidatePropertyInitValue(picker, () => picker.Title, GetNativeTitle, picker.Title);
-		}
-#endif
 	}
 }
