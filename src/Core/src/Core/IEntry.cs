bc5722cf3ea6ccffd7fce10cc6499a75d084c9ea
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-03-16 22:33:54 +0100

    Implement HorizontalTextAlignment property in EntryHandlers (#524)

--- src/Core/src/Core/IEntry.cs
+++ src/Core/src/Core/IEntry.cs
@@ -3,7 +3,7 @@
 	/// <summary>
 	/// Represents a View that is used for single-line text input.
 	/// </summary>
-	public interface IEntry : IView, IText, ITextInput
+	public interface IEntry : IView, IText, ITextInput, ITextAlignment
 	{
 		/// <summary>
 		/// Gets a value that indicates if the entry should visually obscure typed text.
