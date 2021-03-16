3630d42ce3a9155a81c85e1e9eb1addd49bac969
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-03-16 17:31:02 +0100

    TimePicker Handlers (#454)
    * TimePickerHandlers
    
    * Undo changes in sample
    
    * Add some nullability fixes
    
    * Fix build error
    
    * Changes from PR feedback
    
    * Fixed build error
    
    * Updated tests
    
    * Roll time picker and popup into one control
    
    * Move 24 hour view check to handler
    
    * Pull NSLocale -> CultureInfo code out to common property
    
    Co-authored-by: E.Z. Hart <hartez@users.noreply.github.com>
    Co-authored-by: E.Z. Hart <hartez@gmail.com>

---
+++ src/Core/src/Handlers/TimePicker/TimePickerHandler.iOS.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Microsoft.Maui.Handlers
+{
+	public partial class TimePickerHandler : AbstractViewHandler<ITimePicker, MauiTimePicker>
+	{
+		protected override MauiTimePicker CreateNativeView()
+		{
+			return new MauiTimePicker(() => {
+				SetVirtualViewTime();
+				TypedNativeView?.ResignFirstResponder();
+			});
+		}
+
+		protected override void ConnectHandler(MauiTimePicker nativeView)
+		{
+			if (nativeView != null)
+				nativeView.ValueChanged += OnValueChanged;
+		}
+
+		protected override void DisconnectHandler(MauiTimePicker nativeView)
+		{
+			if (nativeView != null)
+			{
+				nativeView.RemoveFromSuperview();
+				nativeView.ValueChanged -= OnValueChanged;
+				nativeView.Dispose();
+			}
+		}
+
+		public static void MapFormat(TimePickerHandler handler, ITimePicker timePicker)
+		{
+			handler.TypedNativeView?.UpdateFormat(timePicker);
+		}
+
+		public static void MapTime(TimePickerHandler handler, ITimePicker timePicker)
+		{
+			handler.TypedNativeView?.UpdateTime(timePicker);
+		}
+
+		void OnValueChanged(object? sender, EventArgs e)
+		{
+			SetVirtualViewTime();
+		}
+
+		void SetVirtualViewTime()
+		{
+			if (VirtualView == null || TypedNativeView == null)
+				return;
+
+			VirtualView.Time = TypedNativeView.Date.ToDateTime() - new DateTime(1, 1, 1);
+		}
+	}
+}
---
+++ src/Core/src/Platform/iOS/TimeExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Microsoft.Maui
+{
+	public static class TimeExtensions
+	{
+		public static string ToFormattedString(this ITimePicker timePicker)
+		{
+			var time = timePicker.Time;
+			var format = timePicker.Format;
+
+			return time.ToFormattedString(format);
+		}
+
+		public static string ToFormattedString(this TimeSpan time, string format)
+		{
+			var cultureInfo = Culture.CurrentCulture;
+
+			return time.ToFormattedString(format, cultureInfo);
+		}
+
+		public static string ToFormattedString(this TimeSpan time, string format, CultureInfo cultureInfo)
+		{
+			if (string.IsNullOrEmpty(format))
+			{
+				string timeformat = cultureInfo.DateTimeFormat.ShortTimePattern;
+				return DateTime.Today.Add(time).ToString(timeformat, cultureInfo);
+			}
+			else
+			{
+				return DateTime.Today.Add(time).ToString(format, cultureInfo);
+			}
+		}
+	}
+}
