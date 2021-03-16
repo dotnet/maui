a736db5ebde79a0be88b7cd8f8be53897a390b92
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-03-16 18:12:28 +0100

    CheckBoxHandlers (#432)

---
+++ src/Core/src/Platform/iOS/CheckBoxExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Maui
+{
+	public static class CheckBoxExtensions
+	{
+		public static void UpdateIsChecked(this NativeCheckBox nativeCheckBox, ICheck check)
+		{
+			nativeCheckBox.IsChecked = check.IsChecked;
+		}
+
+		public static void UpdateColor(this NativeCheckBox nativeCheckBox, ICheck check)
+		{
+			nativeCheckBox.CheckBoxTintColor = check.Color;
+		}
+	}
+}
---
+++ src/Core/tests/DeviceTests/Handlers/CheckBox/CheckBoxHandlerTests.Android.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using AndroidX.AppCompat.Widget;
+using Microsoft.Maui.Handlers;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	public partial class CheckBoxHandlerTests
+	{
+		AppCompatCheckBox GetNativeCheckBox(CheckBoxHandler checkBoxHandler) =>
+			(AppCompatCheckBox)checkBoxHandler.View;
+
+		bool GetNativeIsChecked(CheckBoxHandler checkBoxHandler) =>
+			GetNativeCheckBox(checkBoxHandler).Checked;
+
+		Task ValidateColor(ICheck checkBoxStub, Color color, Action action = null) =>
+			 ValidateHasColor(checkBoxStub, color, action);
+
+		Task ValidateHasColor(ICheck checkBoxStub, Color color, Action action = null)
+		{
+			return InvokeOnMainThreadAsync(() =>
+			{
+				var nativeSwitch = GetNativeCheckBox(CreateHandler(checkBoxStub));
+				action?.Invoke();
+				nativeSwitch.AssertContainsColor(color);
+			});
+		}
+	}
+}
