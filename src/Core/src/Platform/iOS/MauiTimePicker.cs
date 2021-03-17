ff80410701ba2be72ef47c589ec7233861359e53
E.Z. Hart <hartez@gmail.com>
2021-03-17 12:17:58 -0600

    Remove duplicate class

--- src/Core/src/Platform/iOS/MauiTimePicker.cs
+++ src/Core/src/Platform/iOS/MauiTimePicker.cs
@@ -5,21 +5,6 @@ using RectangleF = CoreGraphics.CGRect;
 
 namespace Microsoft.Maui
 {
-	public class NoCaretField : UITextField
-	{
-		public NoCaretField() : base(new RectangleF())
-		{
-			SpellCheckingType = UITextSpellCheckingType.No;
-			AutocorrectionType = UITextAutocorrectionType.No;
-			AutocapitalizationType = UITextAutocapitalizationType.None;
-		}
-
-		public override RectangleF GetCaretRectForPosition(UITextPosition? position)
-		{
-			return new RectangleF();
-		}
-	}
-
 	public class MauiTimePicker : NoCaretField
 	{
 		readonly Action _dateSelected;
