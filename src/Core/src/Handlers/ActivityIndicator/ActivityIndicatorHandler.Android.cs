2a5188076d4207345a342a26e7fc5003201a9c26
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-03-17 19:28:52 +0100

    ActivityIndicator Handlers (#417)
    * ActivityIndicatorHandler
    
    * Register IActivityIndicator
    
    * Add comments to IActivityIndicator
    
    * Fix build error

---
+++ src/Core/src/Handlers/ActivityIndicator/ActivityIndicatorHandler.Android.cs
@@ -0,0 +1,9 @@
+﻿using Android.Widget;
+
+namespace Microsoft.Maui.Handlers
+{
+	public partial class ActivityIndicatorHandler : AbstractViewHandler<IActivityIndicator, ProgressBar>
+	{
+		protected override ProgressBar CreateNativeView() => new ProgressBar(Context) { Indeterminate = true };
+	}
+}
