3fbdcfacf946340be2adfc05c8f99be98a219848
Matthew Leibowitz <mattleibow@live.com>
2021-03-19 19:36:14 +0200

    Fix net6 single project

---
+++ src/Controls/samples/Controls.Sample.SingleProject/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Hosting;
+
+namespace Maui.Controls.Sample.SingleProject
+{
+	public class Startup : IStartup
+	{
+		public void Configure(IAppHostBuilder appBuilder)
+		{
+			appBuilder
+				.RegisterCompatibilityRenderers()
+				.UseMauiApp<MyApp>();
+		}
+	}
+}
