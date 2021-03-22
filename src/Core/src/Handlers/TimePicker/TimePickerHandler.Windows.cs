b2852edf59f5038b944953e0368834418190b674
Shane Neuville <shneuvil@microsoft.com>
2021-03-22 18:24:29 -0500

    - additional handlers

---
+++ src/Core/src/Handlers/TimePicker/TimePickerHandler.Windows.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Maui.Handlers
+{
+	public partial class TimePickerHandler : AbstractViewHandler<ITimePicker, Microsoft.UI.Xaml.Controls.TimePicker>
+	{
+		protected override Microsoft.UI.Xaml.Controls.TimePicker CreateNativeView() => new Microsoft.UI.Xaml.Controls.TimePicker();
+
+		public static void MapFormat(TimePickerHandler handler, ITimePicker view) { }
+		public static void MapTime(TimePickerHandler handler, ITimePicker view) { }
+	}
+}
