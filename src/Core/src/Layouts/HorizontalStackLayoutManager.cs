bb38333ec15460c4abf23db19649e04fb08de307
E.Z. Hart <hartez@gmail.com>
2021-03-24 09:49:45 -0600

    Fix parameter name

--- src/Core/src/Layouts/HorizontalStackLayoutManager.cs
+++ src/Core/src/Layouts/HorizontalStackLayoutManager.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Maui.Layouts
 			return new Size(finalWidth, measure.Height);
 		}
 
-		public override void ArrangeChildren(Rectangle childBounds)
+		public override void ArrangeChildren(Rectangle bounds)
 		{
 			if (Stack.FlowDirection == FlowDirection.LeftToRight)
 			{
