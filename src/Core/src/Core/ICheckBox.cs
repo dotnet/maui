64a445c12111ad327d0f528a3c14f92f47be1671
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-25 18:00:34 +0100

    Added more XML comments to Core interfaces

--- src/Core/src/Core/ICheckBox.cs
+++ src/Core/src/Core/ICheckBox.cs
@@ -1,7 +1,14 @@
 ﻿namespace Microsoft.Maui
 {
+	/// <summary>
+	/// Represents a View that is a type of button that can either be
+	/// checked or empty.
+	/// </summary>
 	public interface ICheckBox : IView
 	{
+		/// <summary>
+		/// Gets a value that indicates whether the CheckBox is checked.
+		/// </summary>
 		bool IsChecked { get; set; }
 	}
 }
