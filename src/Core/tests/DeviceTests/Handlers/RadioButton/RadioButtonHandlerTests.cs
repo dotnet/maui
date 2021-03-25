380804bf95e2566830812f4478e5c7903117844a
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-25 13:36:13 +0100

    Fix build error

--- src/Core/tests/DeviceTests/Handlers/RadioButton/RadioButtonHandlerTests.cs
+++ src/Core/tests/DeviceTests/Handlers/RadioButton/RadioButtonHandlerTests.cs
@@ -6,8 +6,6 @@ namespace Microsoft.Maui.DeviceTests
 	[Category(TestCategory.RadioButton)]
 	public partial class RadioButtonHandlerTests : HandlerTestBase<RadioButtonHandler, RadioButtonStub>
 	{
-		public RadioButtonHandlerTests(HandlerTestFixture fixture) : base(fixture)
-		{
-		}
+		
 	}
 }
