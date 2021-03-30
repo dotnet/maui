ac600176b36a244a0ddcc6c1fe9c8fa728f706f1
James Clancey <james.clancey@gmail.com>
2021-03-30 13:40:52 -0800

    Added HotReload bits to Controls

--- src/Controls/src/Core/HandlerImpl/ContentPage.Impl.cs
+++ src/Controls/src/Core/HandlerImpl/ContentPage.Impl.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Maui.Controls.Internals;
 using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.HotReload;
 
 namespace Microsoft.Maui.Controls
 {
-	public partial class ContentPage : IPage
+	public partial class ContentPage : IPage, HotReload.IHotReloadableView
 	{
 		// TODO ezhart That there's a layout alignment here tells us this hierarchy needs work :) 
 		public Primitives.LayoutAlignment HorizontalLayoutAlignment => Primitives.LayoutAlignment.Fill;
@@ -79,5 +81,31 @@ namespace Microsoft.Maui.Controls
 				frameworkElement.InvalidateMeasure();
 			}
 		}
+
+		#region HotReload
+
+		IView IReplaceableView.ReplacedView => HotReload.MauiHotReloadHelper.GetReplacedView(this) ?? this;
+
+		HotReload.IReloadHandler HotReload.IHotReloadableView.ReloadHandler { get; set; }
+
+		void HotReload.IHotReloadableView.TransferState(IView newView)
+		{
+			//TODO: Let you hot reload the the ViewModel
+			//TODO: Lets do a real state transfer
+			if (newView is View v)
+				v.BindingContext = BindingContext;
+		}
+
+		void HotReload.IHotReloadableView.Reload()
+		{
+			Device.BeginInvokeOnMainThread(() =>
+			{
+				this.CheckHandlers();
+				var reloadHandler = ((IHotReloadableView)this).ReloadHandler;
+				reloadHandler?.Reload();
+				//TODO: if reload handler is null, Do a manual reload?
+			});
+		}
+		#endregion
 	}
 }
