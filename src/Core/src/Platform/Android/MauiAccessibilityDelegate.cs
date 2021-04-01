2393d3979fa6d01f0a19dfc6dbbc68d65f1d22bf
Shane Neuville <shneuvil@microsoft.com>
2021-04-01 10:01:43 -0500

    - move delegate to new type

---
+++ src/Core/src/Platform/Android/MauiAccessibilityDelegate.cs
@@ -0,0 +1,36 @@
+﻿using Android.Views;
+using AView = Android.Views.View;
+
+namespace Microsoft.Maui
+{
+	public class MauiAccessibilityDelegate : View.AccessibilityDelegate
+	{
+		public IView? View { get; set; }
+
+		public MauiAccessibilityDelegate(IView view)
+		{
+			View = view;
+		}
+
+		public override void OnInitializeAccessibilityNodeInfo(AView? host, Android.Views.Accessibility.AccessibilityNodeInfo? info)
+		{
+			if (View == null)
+				return;
+
+			base.OnInitializeAccessibilityNodeInfo(host, info);
+
+			var semantics = View.Semantics;
+			if (semantics == null)
+				return;
+
+			if (info == null)
+				return;
+
+			if (!string.IsNullOrEmpty(semantics.Hint))
+			{
+				info.HintText = semantics.Hint;
+			}
+		}
+	}
+
+}
--- src/Core/src/Platform/Android/ViewExtensions.cs
+++ src/Core/src/Platform/Android/ViewExtensions.cs
@@ -1,5 +1,4 @@
 using AView = Android.Views.View;
-using AAccessibilityDelegate = Android.Views.View.AccessibilityDelegate;
 
 namespace Microsoft.Maui
 {
@@ -41,39 +40,9 @@ namespace Microsoft.Maui
 			nativeView.SetTag(AutomationTagId, view.AutomationId);
 		}
 
-		class MauiAccessibilityDelegate : AAccessibilityDelegate
-		{
-			public IView? View { get; set; }
-
-			public MauiAccessibilityDelegate(IView view)
-			{
-				View = view;
-			}
-
-			public override void OnInitializeAccessibilityNodeInfo(AView? host, Android.Views.Accessibility.AccessibilityNodeInfo? info)
-            {
-				if  (View == null)
-					return;
-
-				base.OnInitializeAccessibilityNodeInfo(host, info);
-
-				var semantics = View.Semantics;
-				if (semantics == null)
-					return;
-
-				if (info == null)
-					return;
-
-				if (!string.IsNullOrEmpty(semantics.Hint))
-				{
-					info.HintText = semantics.Hint;
-				}
-			}
-		}
-
+		
 		public static void UpdateSemantics(this AView nativeView, IView view)
 		{
-			// this code is wrong
 			var semantics = view.Semantics;
 			if (semantics == null)
 				return;
