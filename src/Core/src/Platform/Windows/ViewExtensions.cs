6a71d588248b1b3e8ab0320a273fb29b7f44da5b
Matthew Leibowitz <mattleibow@live.com>
2021-04-06 17:56:33 +0200

    Add the LabelHandler for WinUI (#655)
    * Add the LabelHandler for WinUI
    
    * Windows is beautiful
    
    * ws
    
    * my bad. it does work.
    
    * Revert "my bad. it does work."
    
    This reverts commit 3c985b530dc2a84d53ef7900e7d7db715fb03a18.
    
    * Revert "Revert "my bad. it does work.""
    
    This reverts commit 4b4e0b3be1c5b19255b0ffccad87c11b25ecb0a3.
    
    * not this now
    
    * Don't remove all the xaml on non-Windows
    
    Android and iOS are non-Windows and thus they lose all XAML files. THis only came in with WinUI and is only noticeable since we started using the App.xaml as the main entry point.
    
    * - add build step to winui cake
    
    * Moving toys
    
    Co-authored-by: Shane Neuville <shneuvil@microsoft.com>

--- src/Core/src/Platform/Windows/ViewExtensions.cs
+++ src/Core/src/Platform/Windows/ViewExtensions.cs
@@ -9,10 +9,42 @@ namespace Microsoft.Maui
 		public static void UpdateIsEnabled(this FrameworkElement nativeView, IView view) =>
 			(nativeView as Control)?.UpdateIsEnabled(view.IsEnabled);
 
-		public static void UpdateBackgroundColor(this FrameworkElement nativeView, IView view) =>
-			(nativeView as Control)?.UpdateBackgroundColor(view.BackgroundColor);
+		public static void UpdateBackgroundColor(this FrameworkElement nativeView, IView view)
+		{
+			if (nativeView is Control control)
+				control.UpdateBackgroundColor(view.BackgroundColor);
+			else if (nativeView is Border border)
+				border.UpdateBackgroundColor(view.BackgroundColor);
+			else if (nativeView is Panel panel)
+				panel.UpdateBackgroundColor(view.BackgroundColor);
+		}
+
+		public static void UpdateBackgroundColor(this Control nativeControl, Color color, UI.Xaml.Media.Brush? defaultBrush = null) =>
+			nativeControl.Background = color.IsDefault && defaultBrush != null ? defaultBrush : color.ToNative();
+
+		public static void UpdateBackgroundColor(this Border nativeControl, Color color, UI.Xaml.Media.Brush? defaultBrush = null) =>
+			nativeControl.Background = color.IsDefault && defaultBrush != null ? defaultBrush : color.ToNative();
+
+		public static void UpdateBackgroundColor(this Panel nativeControl, Color color, UI.Xaml.Media.Brush? defaultBrush = null) =>
+			nativeControl.Background = color.IsDefault && defaultBrush != null ? defaultBrush : color.ToNative();
 
 		public static void UpdateAutomationId(this FrameworkElement nativeView, IView view) =>
 			AutomationProperties.SetAutomationId(nativeView, view.AutomationId);
+
+		internal static void UpdateProperty(this FrameworkElement nativeControl, DependencyProperty property, Color color)
+		{
+			if (color.IsDefault)
+				nativeControl.ClearValue(property);
+			else
+				nativeControl.SetValue(property, color.ToNative());
+		}
+
+		internal static void UpdateProperty(this FrameworkElement nativeControl, DependencyProperty property, object? value)
+		{
+			if (value == null)
+				nativeControl.ClearValue(property);
+			else
+				nativeControl.SetValue(property, value);
+		}
 	}
 }
