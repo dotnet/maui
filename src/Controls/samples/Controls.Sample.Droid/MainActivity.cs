b0fa025dffedb1846d711084401d120278091cc4
Rui Marinho <me@ruimarinho.net>
2021-04-08 15:18:02 +0100

    [Sample] Use AdornerService on the sample app

--- src/Controls/samples/Controls.Sample.Droid/MainActivity.cs
+++ src/Controls/samples/Controls.Sample.Droid/MainActivity.cs
@@ -1,5 +1,10 @@
 using Android.App;
+using Android.OS;
+using Android.Widget;
 using Microsoft.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Controls;
+using Android.Views;
 
 namespace Maui.Controls.Sample.Droid
 {
@@ -9,5 +14,68 @@ namespace Maui.Controls.Sample.Droid
 		Categories = new[] { Android.Content.Intent.CategoryDefault })]
 	public class MainActivity : MauiAppCompatActivity
 	{
+		IAdornerService _adornerService;
+		Android.Views.ViewGroupOverlay _parent;
+		Microsoft.Maui.ILayout MainLayout => (CurrentWindow.Page.View as Microsoft.Maui.Controls.ScrollView).Content as Microsoft.Maui.ILayout;
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			_adornerService = MauiApplication.Current.Services.GetService<IAdornerService>();
+			_parent = (ViewGroupOverlay)_adornerService.GetAdornerLayer();
+
+			_adornerService.HandlePoint(point =>
+		   {
+			   _parent.Clear();
+			   AddAdornerAtPoint(MainLayout, point);
+		   });
+			Device.StartTimer(System.TimeSpan.FromSeconds(3), () =>
+			{
+				AddAdornerAtPoint(MainLayout, new Point(2, 2));
+				return false;
+			});
+		}
+
+		void AddAdornerAtPoint(Microsoft.Maui.ILayout layout, Point point)
+		{
+			var view = _adornerService.GetViewAtPoint(layout, point);
+			if (view == null)
+			{
+				System.Diagnostics.Debug.WriteLine("view is null");
+				return;
+			}
+			var rect = view.Frame;
+			System.Diagnostics.Debug.WriteLine($"found view with frame {rect}");
+			if (view is IText textIview)
+			{
+				System.Diagnostics.Debug.WriteLine($"found view Text {textIview.Text}");
+			}
+			Android.Views.View nativeView = AddAdorner((int)this.ToPixels(rect.Location.X),
+										(int)this.ToPixels(rect.Location.Y),
+										(int)this.ToPixels(rect.Size.Width),
+										(int)this.ToPixels(rect.Size.Height));
+			
+			_parent.Add(nativeView);
+		}
+
+		Android.Views.View AddAdorner(int x, int y, int width, int height)
+		{
+			System.Diagnostics.Debug.WriteLine($"Add Adorner {x} {y} {width} {height}");
+
+			Android.Views.View native = new ImageView(this);
+			var shape = new Android.Graphics.Drawables.GradientDrawable();
+			shape.SetStroke(2, Color.Red.ToNative());
+			(native as ImageView).SetImageDrawable(shape);
+
+			//Microsoft.Maui.Controls.Shapes.Rectangle rectShape = new Microsoft.Maui.Controls.Shapes.Rectangle();
+			//rectShape.Stroke = new SolidColorBrush(Color.Red);
+			//rectShape.StrokeThickness = 2;
+			//var native = rectShape.ToNative(this.CurrentWindow.MauiContext);
+
+			native.Measure(Android.Views.View.MeasureSpec.MakeMeasureSpec(width, MeasureSpecMode.Exactly),
+			Android.Views.View.MeasureSpec.MakeMeasureSpec(height, MeasureSpecMode.Exactly));
+			native.Layout(x, y, width, height);
+			return native;
+		}
 	}
 }
