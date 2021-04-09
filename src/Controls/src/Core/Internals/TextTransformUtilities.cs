02e921c81d61bbc75171cf602bff6ec0b3d3eac4
Burak Kaan Köse <bkaankose@outlook.com>
2021-04-09 18:19:25 +0200

    Brought back text transform utilities

---
+++ src/Controls/src/Core/Internals/TextTransformUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using Microsoft.Maui.Controls;
+
+namespace Microsoft.Maui.Controls.Internals
+{
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	public static class TextTransformUtilites
+	{
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static void SetPlainText(InputView inputView, string nativeText)
+		{
+			if (inputView == null)
+				return;
+
+			var textTransform = inputView.TextTransform;
+			var nativeTextWithTransform = inputView.UpdateFormsText(nativeText, textTransform);
+			var formsText = inputView.UpdateFormsText(inputView.Text, textTransform);
+
+			if ((string.IsNullOrEmpty(formsText) && nativeText.Length == 0))
+				return;
+
+			if (formsText == nativeTextWithTransform)
+				return;
+
+			inputView.SetValueFromRenderer(InputView.TextProperty, nativeText);
+		}
+	}
+}
