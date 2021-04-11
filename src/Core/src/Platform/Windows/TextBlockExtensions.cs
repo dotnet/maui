75ccf4525842da51e8b05e93237158d70398bdff
Burak Kaan Köse <bkaankose@outlook.com>
2021-04-11 02:36:02 +0200

    Moved TextBlockExtensions from compat to Windows platform core.

--- src/Core/src/Platform/Windows/TextBlockExtensions.cs
+++ src/Core/src/Platform/Windows/TextBlockExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.Maui
@@ -23,5 +25,46 @@ namespace Microsoft.Maui
 
 		public static void UpdatePadding(this TextBlock nativeControl, ILabel label) =>
 			nativeControl.UpdateProperty(TextBlock.PaddingProperty, label.Padding.ToNative());
+
+		public static void UpdateLineBreakMode(this TextBlock textBlock, LineBreakMode lineBreakMode)
+		{
+			if (textBlock == null)
+				return;
+
+			switch (lineBreakMode)
+			{
+				case LineBreakMode.NoWrap:
+					textBlock.TextTrimming = TextTrimming.Clip;
+					textBlock.TextWrapping = TextWrapping.NoWrap;
+					break;
+				case LineBreakMode.WordWrap:
+					textBlock.TextTrimming = TextTrimming.None;
+					textBlock.TextWrapping = TextWrapping.Wrap;
+					break;
+				case LineBreakMode.CharacterWrap:
+					textBlock.TextTrimming = TextTrimming.WordEllipsis;
+					textBlock.TextWrapping = TextWrapping.Wrap;
+					break;
+				case LineBreakMode.HeadTruncation:
+					// TODO: This truncates at the end.
+					textBlock.TextTrimming = TextTrimming.WordEllipsis;
+					DetermineTruncatedTextWrapping(textBlock);
+					break;
+				case LineBreakMode.TailTruncation:
+					textBlock.TextTrimming = TextTrimming.CharacterEllipsis;
+					DetermineTruncatedTextWrapping(textBlock);
+					break;
+				case LineBreakMode.MiddleTruncation:
+					// TODO: This truncates at the end.
+					textBlock.TextTrimming = TextTrimming.WordEllipsis;
+					DetermineTruncatedTextWrapping(textBlock);
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(lineBreakMode));
+			}
+		}
+
+		static void DetermineTruncatedTextWrapping(TextBlock textBlock) =>
+			textBlock.TextWrapping = textBlock.MaxLines > 1 ? TextWrapping.Wrap : TextWrapping.NoWrap;
 	}
 }
