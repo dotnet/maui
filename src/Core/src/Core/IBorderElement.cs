b354d32d9a2eadb3c1722e0f5de4db21df0b3f0f
Burak Kaan Köse <bkaankose@outlook.com>
2021-04-11 02:35:25 +0200

    Basic core IBorderElement implementation for IButton.

---
+++ src/Core/src/Core/IBorderElement.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Maui
+{
+	public interface IBorderElement
+	{
+		/// <summary>
+		/// Gets the corner radius around the border control element.
+		/// </summary>
+		int CornerRadius { get; }
+	}
+}
--- src/Core/src/Core/IButton.cs
+++ src/Core/src/Core/IButton.cs
@@ -3,7 +3,7 @@ namespace Microsoft.Maui
 	/// <summary>
 	/// Represents a View that reacts to touch events.
 	/// </summary>
-	public interface IButton : IView, IText, IPadding
+	public interface IButton : IView, IText, IPadding , IBorderElement
 	{
 		/// <summary>
 		/// Occurs when the Button is pressed.
