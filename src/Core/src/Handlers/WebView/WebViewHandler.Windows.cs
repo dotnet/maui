61d2358d4a073ef7f3960ffdd98e48e08db5ee1a
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-04-13 14:33:39 +0200

    Created Windows WebViewHandler skeleton

--- src/Core/src/Handlers/WebView/WebViewHandler.Windows.cs
+++ src/Core/src/Handlers/WebView/WebViewHandler.Windows.cs
@@ -1,7 +1,12 @@
-﻿namespace Microsoft.Maui.Handlers
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.Maui.Handlers
 {
-	public partial class WebViewHandler
+	public partial class WebViewHandler : ViewHandler<IWebView, WebView2>
 	{
-		
+		protected override WebView2 CreateNativeView() => new WebView2();
+
+		[MissingMapper]
+		public static void MapSource(WebViewHandler handler, IWebView view) { }
 	}
 }
