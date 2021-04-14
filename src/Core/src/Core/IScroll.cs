86924fa36710dd386b005cd2ea7cf0ef9d6c259a
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-04-14 11:16:47 +0200

    Updated IScroll interface

--- src/Core/src/Core/IScroll.cs
+++ src/Core/src/Core/IScroll.cs
@@ -1,7 +1,35 @@
-﻿namespace Microsoft.Maui
+﻿using Microsoft.Maui.Graphics;
+
+namespace Microsoft.Maui
 {
+	/// <summary>
+	/// A View capable of scrolling if its Content requires.
+	/// </summary>
 	public interface IScroll : IView
 	{
-		Size ContentSize { get; }
+		/// <summary>
+		/// Gets the content of the scroll.
+		/// </summary>
+		IView Content { get; }
+
+		/// <summary>
+		/// Gets the scrolling directions.
+		/// </summary>
+		ScrollOrientation Orientation { get; }
+
+		/// <summary>
+		/// Gets the size of the Content.
+		/// </summary>
+		SizeF ContentSize { get; }
+
+		/// <summary>
+		/// Gets a value that controls when the horizontal scroll bar is visible.
+		/// </summary>
+		ScrollBarVisibility HorizontalScrollBarVisibility { get; }
+
+		/// <summary>
+		/// Gets a value that controls when the vertical scroll bar is visible.
+		/// </summary>
+		ScrollBarVisibility VerticalScrollBarVisibility { get; }
 	}
 }
--- src/Core/src/Primitives/ScrollOrientation.cs
+++ src/Core/src/Primitives/ScrollOrientation.cs
@@ -1,4 +1,4 @@
-namespace Microsoft.Maui.Controls
+﻿namespace Microsoft.Maui
 {
 	public enum ScrollOrientation
 	{
