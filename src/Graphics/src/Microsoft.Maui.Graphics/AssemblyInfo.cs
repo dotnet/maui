32696c5f9b1605bf7429ead595957785c4cfdd24
Jonathan Dick <jondick@gmail.com>
2021-04-14 15:49:00 -0400

    Remove old XF xmlns

--- src/Graphics/src/Microsoft.Maui.Graphics/AssemblyInfo.cs
+++ src/Graphics/src/Microsoft.Maui.Graphics/AssemblyInfo.cs
@@ -1,9 +1,5 @@
 using Microsoft.Maui.Graphics;
 
-[assembly: XmlnsDefinition("http://xamarin.com/schemas/2014/forms", "Microsoft.Maui.Graphics")]
-[assembly: XmlnsPrefix("http://xamarin.com/schemas/2014/forms", "xf")]
-[assembly: XmlnsPrefix("http://xamarin.com/schemas/2014/forms/design", "d")]
-
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/2021/maui", "Microsoft.Maui.Graphics")]
 [assembly: XmlnsPrefix("http://schemas.microsoft.com/dotnet/2021/maui", "maui")]
 [assembly: XmlnsPrefix("http://schemas.microsoft.com/dotnet/2021/maui/design", "d")]
