04989075b0bab448e7d1b2b4f1db562dd80a45ed
Shane Neuville <shneuvil@microsoft.com>
2021-04-15 11:11:20 -0500

    - wire up switch toggle

--- src/Core/src/Handlers/Switch/SwitchHandler.Windows.cs
+++ src/Core/src/Handlers/Switch/SwitchHandler.Windows.cs
@@ -16,5 +16,25 @@ namespace Microsoft.Maui.Handlers
 
 		[MissingMapper]
 		public static void MapThumbColor(SwitchHandler handler, ISwitch view) { }
+
+		protected override void DisconnectHandler(ToggleSwitch nativeView)
+		{
+			base.DisconnectHandler(nativeView);
+			nativeView.Toggled -= OnToggled;
+		}
+
+		protected override void ConnectHandler(ToggleSwitch nativeView)
+		{
+			base.ConnectHandler(nativeView);
+			nativeView.Toggled += OnToggled;
+		}
+
+		void OnToggled(object sender, UI.Xaml.RoutedEventArgs e)
+		{
+			if (VirtualView == null || NativeView == null)
+				return;
+
+			VirtualView.IsOn = NativeView.IsOn;
+		}
 	}
 }
