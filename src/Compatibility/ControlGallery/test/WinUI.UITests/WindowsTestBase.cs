02deff40df1fd26497745479da4d7296811a3a22
Shane Neuville <shneuvil@microsoft.com>
2021-04-16 14:50:44 -0500

    - fix winui tests to start up window and then locate top window manually

--- src/Compatibility/ControlGallery/test/WinUI.UITests/WindowsTestBase.cs
+++ src/Compatibility/ControlGallery/test/WinUI.UITests/WindowsTestBase.cs
@@ -6,6 +6,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
 using Xamarin.UITest;
 
 namespace Microsoft.Maui.Controls.Compatibility.ControlGallery.UITests
@@ -25,12 +26,52 @@ namespace Microsoft.Maui.Controls.Compatibility.ControlGallery.UITests
 			return new WinDriverApp(Session);
 		}
 
-		public static WindowsDriver<WindowsElement> CreateWindowsDriver()
+
+		public static void StartupApplication()
 		{
 			AppiumOptions options = new AppiumOptions();
 			options.AddAdditionalCapability("app", "0d4424f6-1e29-4476-ac00-ba22c3789cb6_ph1m9x8skttmg!App");
-			options.AddAdditionalCapability("appArguments", "RunningAsUITests");
+			
+			try
+			{
+				Session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), options);
+			}
+			catch {
+				// This crashes because it can't find the window but it will at least start the application
+			}
+		}
+
+		public static WindowsElement GetWindowsElement()
+		{
+			AppiumOptions options = new AppiumOptions();
+			options.AddAdditionalCapability("app", "Root");
 			Session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), options);
+
+			new Actions(Session)
+					.SendKeys(Keys.Meta + "s" + Keys.Meta)
+					.Perform();
+
+			try
+			{
+				return Session.FindElementByName("WinUI Desktop");
+			}
+			catch { }
+
+			StartupApplication();
+			return GetWindowsElement();
+		}
+
+		public static WindowsDriver<WindowsElement> CreateWindowsDriver()
+		{
+			var topLevelWindowHandle = GetWindowsElement().GetAttribute("NativeWindowHandle");
+			topLevelWindowHandle = (int.Parse(topLevelWindowHandle)).ToString("x"); // Convert to Hex
+
+			AppiumOptions options = new AppiumOptions();
+			options.AddAdditionalCapability("appTopLevelWindow", topLevelWindowHandle);
+			options.AddAdditionalCapability("appArguments", "RunningAsUITests");
+
+			Session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), options);
+
 			Assert.IsNotNull(Session);
 			Session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
 			Reset();
