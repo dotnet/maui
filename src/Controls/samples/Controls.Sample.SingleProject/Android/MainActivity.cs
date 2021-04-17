693f3ed44716b58769e66630d63734bbabd6d2a9
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-04-16 20:56:47 -0500

    [android] cleanup default styles.xml and themes (#764)
    Going forward, default Maui apps on Android can simply use:
    
        [Activity(Theme = "@style/Maui.MainTheme", MainLauncher = true)]
        public class MainActivity : MauiAppCompatActivity
        {
        }
    
    You could also append `.NoActionBar` to remove the `ActionBar` if needed.
    
    To help with migration from Xamarin.Forms, I left some "compatibility"
    themes intact:
    
        <!-- Themes for Xamarin.Forms backwards compatibility -->
        <style name="MainTheme" parent="Maui.MainTheme" />
        <style name="MainTheme.Base" parent="Maui.MainTheme" />
        <style name="MainTheme.NoActionBar" parent="Maui.MainTheme.NoActionBar" />
        <style name="AppTheme" parent="Maui.MainTheme" />
        <style name="AppTheme.NoActionBar" parent="Maui.MainTheme.NoActionBar" />
    
    These are used in many of the current Xamarin.Forms templates &
    samples, so it will probably be helpful to leave these names in.
    
    I deleted `styles.xml` and `colors.xml` where no longer needed in
    tests, samples, and templates. We can use `Maui.MainTheme` instead.
    
    Other unused things I deleted:
    
    * `strings.xml` -> `action_settings`
    * `strings.xml` -> `app_name`
    * `dimens.xml` -> `fab_margin`
    
    These must have been leftover from old Xamarin.Forms templates or
    something. I couldn't find anything using them.
    
    In a future PR, I will need add a new `Maui.SplashTheme` for splash
    screen support for "single project".

--- src/Controls/samples/Controls.Sample.SingleProject/Android/MainActivity.cs
+++ src/Controls/samples/Controls.Sample.SingleProject/Android/MainActivity.cs
@@ -3,7 +3,7 @@ using Microsoft.Maui;
 
 namespace Maui.Controls.Sample.SingleProject
 {
-	[Activity(Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+	[Activity(Theme = "@style/Maui.MainTheme.NoActionBar", MainLauncher = true)]
 	public class MainActivity : MauiAppCompatActivity
 	{
 	}
