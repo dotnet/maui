6d0f901eb790c1d3ba3b8f54d1330d37dce79437
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-04-19 10:21:03 +0200

    Implement StepperHandler in WinUI

---
+++ src/Core/src/Platform/Windows/StepperExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Microsoft.Maui
+{
+	public static class StepperExtensions
+	{
+		public static void UpdateMinimum(this MauiStepper nativeStepper, IStepper stepper)
+		{
+			nativeStepper.Minimum = stepper.Minimum;
+		}
+
+		public static void UpdateMaximum(this MauiStepper nativeStepper, IStepper stepper)
+		{
+			nativeStepper.Maximum = stepper.Maximum;
+		}
+
+		public static void UpdateInterval(this MauiStepper nativeStepper, IStepper stepper)
+		{
+			nativeStepper.Increment = stepper.Interval;
+		}
+
+		public static void UpdateValue(this MauiStepper nativeStepper, IStepper stepper)
+		{
+			nativeStepper.Value = stepper.Value;
+		}
+	}
+}
