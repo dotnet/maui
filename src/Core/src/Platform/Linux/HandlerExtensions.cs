a7efd1ac014e9482969f332840cd8d32c0e8b4c5
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-04-21 20:11:30 +0200

    Added Linux HandlerExtensions

---
+++ src/Core/src/Platform/Linux/HandlerExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Gtk;
+using System;
+
+namespace Microsoft.Maui
+{
+	public static class HandlerExtensions
+	{
+		public static Widget ToNative(this IView view, IMauiContext context)
+		{
+			_ = view ?? throw new ArgumentNullException(nameof(view));
+			_ = context ?? throw new ArgumentNullException(nameof(context));
+
+			var handler = view.Handler;
+
+			if (handler == null)
+			{
+				handler = context.Handlers.GetHandler(view.GetType());
+
+				if (handler == null)
+					throw new Exception($"Handler not found for view {view}");
+
+				handler.SetMauiContext(context);
+
+				view.Handler = handler;
+			}
+
+			handler.SetVirtualView(view);
+
+			if (handler.NativeView is not Widget result)
+			{
+				throw new InvalidOperationException($"Unable to convert {view} to {typeof(Widget)}");
+			}
+
+			return result;
+		}
+	}
+}
