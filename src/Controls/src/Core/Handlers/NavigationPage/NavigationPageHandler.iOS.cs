bb78fe787df17f3e0d08724ebcf37af75b37ec87
Shane Neuville <shneuvil@microsoft.com>
2021-04-23 11:15:06 -0500

    Android Navigation

---
+++ src/Controls/src/Core/Handlers/NavigationPage/NavigationPageHandler.iOS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Maui.Handlers;
+using UIKit;
+
+namespace Microsoft.Maui.Controls.Handlers
+{
+	public partial class NavigationPageHandler :
+		ViewHandler<NavigationPage, UIView>
+	{
+		protected override UIView CreateNativeView()
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void MapPadding(NavigationPageHandler handler, NavigationPage view) { }
+
+		public static void MapTitleColor(NavigationPageHandler handler, NavigationPage view) { }
+
+		public static void MapNavigationBarBackground(NavigationPageHandler handler, NavigationPage view) { }
+
+		public static void MapTitleIcon(NavigationPageHandler handler, NavigationPage view) { }
+
+		public static void MapTitleView(NavigationPageHandler handler, NavigationPage view) { }
+	}
+}
---
+++ src/Controls/src/Core/Platform/Android/NavigationLayout.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Android.Content;
+using Android.Runtime;
+using Android.Util;
+using AndroidX.CoordinatorLayout.Widget;
+
+namespace Microsoft.Maui.Controls.Platform
+{
+	public class NavigationLayout : CoordinatorLayout
+	{
+		[Preserve(Conditional = true)]
+		public NavigationLayout(Context context) : base(context)
+		{
+		}
+
+		[Preserve(Conditional = true)]
+		public NavigationLayout(Context context, IAttributeSet attrs) : base(context, attrs)
+		{
+		}
+
+		[Preserve(Conditional = true)]
+		public NavigationLayout(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+		{
+		}
+
+		[Preserve(Conditional = true)]
+		protected NavigationLayout(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+		{
+		}
+	}
+}
