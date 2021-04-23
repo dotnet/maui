fb828e944361dd0a1c6d57e0bfee4c16b4c8d87c
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-04-23 18:44:33 +0200

    Implement MaxLength property in WinUI EntryHandler (#817)
    Co-authored-by: Rui Marinho <me@ruimarinho.net>

--- src/Core/src/Platform/Windows/TextBoxExtensions.cs
+++ src/Core/src/Platform/Windows/TextBoxExtensions.cs
@@ -16,5 +16,20 @@ namespace Microsoft.Maui
 			textView.Foreground = brush;
 			textView.ForegroundFocusBrush = brush;
 		}
+
+		public static void UpdateMaxLength(this MauiTextBox textBox, IEntry entry)
+		{
+			var maxLength = entry.MaxLength;
+
+			if (maxLength == -1)
+				maxLength = int.MaxValue;
+
+			textBox.MaxLength = maxLength;
+
+			var currentControlText = textBox.Text;
+
+			if (currentControlText.Length > maxLength)
+				textBox.Text = currentControlText.Substring(0, maxLength);
+		}
 	}
 }
