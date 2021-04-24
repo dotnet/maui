724bc0c8c715f31d975367b456a8aa14637d3b0e
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-04-24 11:12:40 +0200

    Implement basic SliderHandler properties in WinUI (#718)
    * Implement basic Slider properties in WinUI
    
    * Fix build error
    
    * Fix build error
    
    Co-authored-by: Rui Marinho <me@ruimarinho.net>

---
+++ src/Core/src/Platform/Windows/SliderExtensions.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+
+namespace Microsoft.Maui
+{
+	public static class SliderExtensions
+	{
+		public static void UpdateMinimum(this Slider nativeSlider, ISlider slider)
+		{
+			nativeSlider.Minimum = slider.Minimum;
+		}
+
+		public static void UpdateMaximum(this Slider nativeSlider, ISlider slider)
+		{
+			nativeSlider.Maximum = slider.Maximum;
+		}
+
+		public static void UpdateValue(this Slider nativeSlider, ISlider slider)
+		{
+			if (nativeSlider.Value != slider.Value)
+				nativeSlider.Value = slider.Value;
+		}
+
+		public static void UpdateMinimumTrackColor(this Slider nativeSlider, ISlider slider, Brush? defaultForegroundColor)
+		{
+			if (slider.MinimumTrackColor.IsDefault())
+				nativeSlider.Foreground = defaultForegroundColor;
+			else
+				nativeSlider.Foreground = slider.MinimumTrackColor.ToNative();
+		}
+
+		public static void UpdateMaximumTrackColor(this Slider nativeSlider, ISlider slider, Brush? defaultBackgroundColor)
+		{
+			if (slider.MaximumTrackColor.IsDefault())
+				nativeSlider.Background = defaultBackgroundColor;
+			else
+				nativeSlider.Background = slider.MaximumTrackColor.ToNative();
+		}
+	}
+}
