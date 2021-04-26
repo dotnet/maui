8bfbfece93562a9ae6df7dcc079a87cedfbe7362
Jonathan Dick <jondick@gmail.com>
2021-04-26 16:23:34 -0400

    Remove old window

--- src/Controls/samples/Controls.Sample.SingleProject/MyApp.cs
+++ src/Controls/samples/Controls.Sample.SingleProject/MyApp.cs
@@ -6,13 +6,17 @@ namespace Maui.Controls.Sample.SingleProject
 {
 	public class MyApp : IApplication
 	{
+		static bool UseBlazor = false;
+
 		List<IWindow> _windows = new List<IWindow>();
 		public IReadOnlyList<IWindow> Windows => _windows.AsReadOnly();
 
 		public IWindow CreateWindow(IActivationState activationState)
 		{
 			Microsoft.Maui.Controls.Compatibility.Forms.Init(activationState);
-			var window = new MainWindow();
+
+			var window = new Microsoft.Maui.Controls.Window(UseBlazor ? new BlazorPage() : new MainPage());
+
 			_windows.Add(window);			
 			return window;
 		}
