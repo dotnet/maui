65a4499877a87398e5a70798c86539a9ccc1de0a
Shane Neuville <shneuvil@microsoft.com>
2021-04-26 23:04:28 -0500

    iOS NavigationPageHandler (#852)
    * iOS Navigation
    
    * - wire up iOS Navigation
    
    * - remove VET
    
    * - rework with HR
    
    * - fix namespace
    
    * - fix HR
    
    * - fix hr
    
    * - fix SO exception
    
    * - maybe?
    
    * - nullabel fix

--- src/Core/src/Platform/iOS/LayoutView.cs
+++ src/Core/src/Platform/iOS/LayoutView.cs
@@ -57,4 +57,23 @@ namespace Microsoft.Maui
 
 		internal Func<Rectangle, Size>? CrossPlatformArrange { get; set; }
 	}
+
+	public class PageViewController : ContainerViewController
+	{
+		public PageViewController(IPage page,IMauiContext mauiContext)
+		{
+			CurrentView = page;
+			Context = mauiContext;
+			LoadFirstView(page);
+		}
+
+		protected override UIView CreateNativeView(IView view)
+		{
+			return new PageView
+			{
+				CrossPlatformArrange = view.Arrange,
+			};
+		}
+	}
+
 }
