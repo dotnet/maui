a6efd26d90153952065ca083a3e5c948e631cc23
Shane Neuville <shneuvil@microsoft.com>
2021-04-26 19:26:18 -0500

    - fix namespace

--- src/Controls/src/Core/Handlers/NavigationPage/NavigationPageHandler.cs
+++ src/Controls/src/Core/Handlers/NavigationPage/NavigationPageHandler.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Maui.Handlers;
 
+#if WINDOWS
+using static Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific.Page;
+#endif
+
 namespace Microsoft.Maui.Controls.Handlers
 {
 	public partial class NavigationPageHandler
@@ -10,16 +14,16 @@ namespace Microsoft.Maui.Controls.Handlers
 		public static PropertyMapper<NavigationPage, NavigationPageHandler> NavigationPageMapper
 			   = new PropertyMapper<NavigationPage, NavigationPageHandler>(ViewHandler.ViewMapper)
 			   {
-				   [NavigationPage.BarTextColorProperty.PropertyName] = MapTitleColor,
-				   [NavigationPage.BarBackgroundColorProperty.PropertyName] = MapNavigationBarBackground,
-				   [NavigationPage.BarBackgroundProperty.PropertyName] = MapNavigationBarBackground,
-				   [nameof(IPadding.Padding)] = MapPadding,
-				   [nameof(NavigationPage.TitleIconImageSourceProperty.PropertyName)] = MapTitleIcon,
-				   [nameof(NavigationPage.TitleViewProperty.PropertyName)] = MapTitleView,
+					[NavigationPage.BarTextColorProperty.PropertyName] = MapTitleColor,
+					[NavigationPage.BarBackgroundColorProperty.PropertyName] = MapNavigationBarBackground,
+					[NavigationPage.BarBackgroundProperty.PropertyName] = MapNavigationBarBackground,
+					[nameof(IPadding.Padding)] = MapPadding,
+					[nameof(NavigationPage.TitleIconImageSourceProperty.PropertyName)] = MapTitleIcon,
+					[nameof(NavigationPage.TitleViewProperty.PropertyName)] = MapTitleView,
 
 #if WINDOWS
-				[nameof(ToolbarPlacementProperty.PropertyName)] = MapToolbarPlacement,
-				[nameof(ToolbarDynamicOverflowEnabledProperty.PropertyName)] = MapToolbarDynamicOverflowEnabled,
+					[nameof(ToolbarPlacementProperty.PropertyName)] = MapToolbarPlacement,
+					[nameof(ToolbarDynamicOverflowEnabledProperty.PropertyName)] = MapToolbarDynamicOverflowEnabled,
 #endif
 			};
 
