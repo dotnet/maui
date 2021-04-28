aac4daced598754b90d320028502da35a852ff76
Matthew Leibowitz <mattleibow@live.com>
2021-04-29 01:57:42 +0200

    stuffs

--- src/Core/src/Fonts/EmbeddedFontLoader.cs
+++ src/Core/src/Fonts/EmbeddedFontLoader.cs
@@ -8,6 +8,15 @@ namespace Microsoft.Maui
 	{
 		readonly ILogger<EmbeddedFontLoader>? _logger;
 
+#if !NET6_0
+		// The NET6_0 linker won't need this
+		// Make sure to test with full linking on before removing
+#if __ANDROID__
+		[Android.Runtime.Preserve]
+#elif __IOS__
+		[Foundation.Preserve]
+#endif
+#endif
 		public EmbeddedFontLoader()
 			: this(null)
 		{
