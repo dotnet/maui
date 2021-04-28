6926da4f0d98fef36e9c2f97b1efeffa79d4cbea
James Clancey <james.clancey@gmail.com>
2021-04-28 10:24:43 -0800

    Controls now uses the new Interface

--- src/Controls/src/Core/HandlerImpl/Picker.Impl.cs
+++ src/Controls/src/Core/HandlerImpl/Picker.Impl.cs
@@ -5,5 +5,18 @@
 		Font? _font;
 
 		Font ITextStyle.Font => _font ??= Font.OfSize(FontFamily, FontSize).WithAttributes(FontAttributes);
+
+		int IPicker.Count() => Items?.Count ?? ItemsSource?.Count ?? 0;
+
+		string IPicker.DisplayFor(int index)
+		{
+			if (index < 0)
+				return "";
+			if (Items?.Count < index)
+				return Items[index];
+			if (ItemsSource?.Count < index)
+				GetDisplayMember(ItemsSource[index]);
+			return "";
+		}
 	}
 }
