36dc590dd7d6c68f4e1d13b2e4cd4babdd933bb9
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-05-01 20:46:50 +0200

    Implement GraphicsView

---
+++ src/Core/src/Core/IGraphicsView.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace Microsoft.Maui
+{
+	public interface IGraphicsView : IView
+	{
+		IDrawable Drawable { get; }
+	}
+}
---
+++ src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Microsoft.Maui.Handlers
+{
+	public partial class GraphicsViewHandler
+	{
+		public static PropertyMapper<IGraphicsView, GraphicsViewHandler> GraphicsViewMapper = new PropertyMapper<IGraphicsView, GraphicsViewHandler>(ViewHandler.ViewMapper)
+		{
+			[nameof(IGraphicsView.Drawable)] = MapDrawable
+		};
+
+		public GraphicsViewHandler() : base(GraphicsViewMapper)
+		{
+
+		}
+
+		public GraphicsViewHandler(PropertyMapper? mapper = null) : base(mapper ?? GraphicsViewMapper)
+		{
+
+		}
+	}
+}
