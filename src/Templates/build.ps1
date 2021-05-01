50dbcc093fd332aea6653750820ab52983a5bbb3
Jonathan Dick <jond@hey.com>
2021-05-01 09:45:40 -0400

    Merge winui into single project

---
+++ src/Templates/build.ps1
@@ -0,0 +1,9 @@
+$PACKAGEVERSION = "6.0.100-preview.4.472"
+
+& dotnet pack src/Microsoft.Maui.Templates.csproj -p:PackageVersion="$PACKAGEVERSION"
+& dotnet new -u Microsoft.Maui.Templates
+& dotnet new -i "../../artifacts/Microsoft.Maui.Templates.$PACKAGEVERSION.nupkg"
+
+Remove-Item -Force -Recurse ./TestMaui/
+& dotnet new maui -n TestMaui
+& code ./TestMaui/
