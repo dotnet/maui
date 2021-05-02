5b87df2881f95bf6e8feb71c9b7f882c75a2b203
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-05-02 13:13:01 +0200

    Added GraphicsView tests

--- src/Core/tests/DeviceTests/Handlers/GraphicsView/GraphicsViewHandlerTests.iOS.cs
+++ src/Core/tests/DeviceTests/Handlers/GraphicsView/GraphicsViewHandlerTests.iOS.cs
@@ -1,7 +1,24 @@
-﻿namespace Microsoft.Maui.DeviceTests
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics.Native;
+using Microsoft.Maui.Handlers;
+
+namespace Microsoft.Maui.DeviceTests
 {
 	public partial class GraphicsViewHandlerTests
 	{
+		NativeGraphicsView GetNativeGraphicsView(GraphicsViewHandler graphicsViewHandler) =>
+			graphicsViewHandler.NativeView;
 
+		Task ValidateHasColor(IGraphicsView graphicsView, Color color, Action action = null)
+		{
+			return InvokeOnMainThreadAsync(() =>
+			{
+				var nativeGraphicsView = GetNativeGraphicsView(CreateHandler(graphicsView));
+				action?.Invoke();
+				nativeGraphicsView.AssertContainsColor(color);
+			});
+		}
 	}
 }
