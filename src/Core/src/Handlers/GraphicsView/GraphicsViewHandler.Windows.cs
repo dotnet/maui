852ff3fe68f937964ecac428fc8212deba655098
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-05-03 12:37:08 +0200

    Updated Windows GraphicsViewHandler

--- src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.Windows.cs
+++ src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.Windows.cs
@@ -1,7 +1,12 @@
-﻿namespace Microsoft.Maui.Handlers
-{
-	public partial class GraphicsViewHandler
-	{
+﻿using Microsoft.UI.Xaml.Controls;
 
+namespace Microsoft.Maui.Handlers
+{
+	public partial class GraphicsViewHandler : ViewHandler<IGraphicsView, UserControl>
+	{
+		protected override UserControl CreateNativeView() => new UserControl();
+
+		[MissingMapper]
+		public static void MapDrawable(IViewHandler handler, IGraphicsView graphicsView) { }
 	}
 }
