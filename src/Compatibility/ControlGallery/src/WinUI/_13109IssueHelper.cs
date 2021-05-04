6636c930103f06bbe412a637546c1e190a782e7b
Shane Neuville <shneuvil@microsoft.com>
2021-05-04 09:25:10 -0500

    - fix winui build and add winui stage

--- src/Compatibility/ControlGallery/src/WinUI/_13109IssueHelper.cs
+++ src/Compatibility/ControlGallery/src/WinUI/_13109IssueHelper.cs
@@ -4,6 +4,7 @@ using Microsoft.Maui.Controls.Compatibility.Platform.UWP;
 using Microsoft.Maui.Controls.Compatibility.ControlGallery.Issues;
 using Microsoft.Maui.Controls.Compatibility.ControlGallery.WinUI;
 using Bitmap = Microsoft.UI.Xaml.Media.ImageSource;
+using Microsoft.Maui.Controls.Platform;
 
 [assembly: Dependency(typeof(_13109IssueHelper))]
 namespace Microsoft.Maui.Controls.Compatibility.ControlGallery.WinUI
