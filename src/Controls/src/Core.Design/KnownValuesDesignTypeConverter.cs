4b07d4f0ce609e2189fbb427c67ab4e0e3b450bd
Jonathan Dick <jondick@gmail.com>
2021-05-10 18:35:38 -0400

    Migrate design dll's to new designtools extensibility

---
+++ src/Controls/src/Core.Design/KnownValuesDesignTypeConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace Microsoft.Maui.Controls.Design
+{
+	using System;
+	using System.ComponentModel;
+
+	public abstract class KnownValuesDesignTypeConverter : TypeConverter
+	{
+		protected abstract string[] KnownValues { get; }
+
+		// This tells XAML this converter can be used to process strings
+		// Without this the values won't show up as hints
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+			=> sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+
+		public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+			=> new(KnownValues);
+
+		public override bool GetStandardValuesExclusive(ITypeDescriptorContext context)
+			=> false;
+
+		public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+			=> true;
+	}
+}
---
+++ src/Controls/src/Core.Design/NamedSizeDesignTypeConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.Maui.Controls.Design
+{
+	public class NamedSizeDesignTypeConverter : KnownValuesDesignTypeConverter
+	{
+		public NamedSizeDesignTypeConverter()
+		{
+		}
+
+		protected override string[] KnownValues
+			=> new[] { "Default", "Micro", "Small", "Medium", "Large", "Body", "Header", "Title", "Subtitle", "Caption" };
+	}
+}
