d54902aca9fbd019541ea8fb34581459bed68e8a
Rui Marinho <me@ruimarinho.net>
2021-05-10 17:33:40 +0100

    [Build] Build tasks first

--- eng/package.ps1
+++ eng/package.ps1
@@ -7,6 +7,7 @@ Write-Host $msbuild
 
 $artifacts = Join-Path $PSScriptRoot ../artifacts
 $sln = Join-Path $PSScriptRoot ../Microsoft.Maui-net6.sln
+$slnTasks = Join-Path $PSScriptRoot ../Microsoft.Maui.BuildTasks-net6.sln
 
 # Bootstrap .\bin\dotnet\
 $csproj = Join-Path $PSScriptRoot ../src/DotNet/DotNet.csproj
@@ -51,6 +52,14 @@ if ($IsWindows)
 
         # Put our local dotnet.exe on PATH first so Visual Studio knows which one to use
         $env:PATH=($env:DOTNET_ROOT + ";" + $env:PATH)
+
+        # Have to build the solution tasks
+        & $msbuild $slnTasks `
+            /p:configuration=$configuration `
+            /p:SymbolPackageFormat=snupkg `
+            /restore `
+            /t:build `
+            /bl:"$artifacts/maui-build-tasks-$configuration.binlog"
 
         # Have to build the solution first so the xbf files are there for pack
         & $msbuild $sln `
