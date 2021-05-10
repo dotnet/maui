a10dcb2726abe652aff7a3770bea979f2e33164b
Rui Marinho <me@ruimarinho.net>
2021-05-10 15:13:15 +0100

    Update eng/package.ps1
    Co-authored-by: Javier Suárez <javiersuarezruiz@hotmail.com>

--- eng/package.ps1
+++ eng/package.ps1
@@ -66,7 +66,7 @@ if ($IsWindows)
             /t:pack `
             /p:Packing=true `
             /bl:"$artifacts/maui-pack-$configuration.binlog"
-        if (!$?) { throw "Pacl failed." }
+        if (!$?) { throw "Pack failed." }
     }
     finally
     {
