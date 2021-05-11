ebaec027a8917b724ccd1c422483fbb2ed76a3db
E.Z. Hart <hartez@users.noreply.github.com>
2021-05-11 02:43:19 -0600

    Request layout for iOS Label when text change invalidates content size (#1004)
    Fixes #783

--- src/Core/src/Platform/iOS/MauiLabel.cs
+++ src/Core/src/Platform/iOS/MauiLabel.cs
@@ -1,4 +1,5 @@
 ﻿using CoreGraphics;
+using System;
 using UIKit;
 
 namespace Microsoft.Maui.Platform.iOS
@@ -13,6 +14,24 @@ namespace Microsoft.Maui.Platform.iOS
 
 		public MauiLabel()
 		{
+		}
+
+		public override void InvalidateIntrinsicContentSize()
+		{
+			base.InvalidateIntrinsicContentSize();
+
+			if (Frame.Width == 0 && Frame.Height == 0)
+			{
+				// The Label hasn't actually been laid out on screen yet; no reason to request a layout
+				return;
+			}
+
+			if (!Frame.Size.IsCloseTo(AddInsets(IntrinsicContentSize), (nfloat)0.001))
+			{
+				// The text or its attributes have changed enough that the size no longer matches the set Frame. It's possible
+				// that the Label needs to be laid out again at a different size, so we request that the parent do so. 
+				Superview?.SetNeedsLayout();
+			}
 		}
 
 		public override void DrawText(CGRect rect) => base.DrawText(TextInsets.InsetRect(rect));
