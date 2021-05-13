1ba38873a86ece48c36a70a77c2e8b5267d94a46
Matthew Leibowitz <mattleibow@live.com>
2021-05-13 07:38:16 +0200

    Added more iOS tests

--- src/Core/tests/DeviceTests/Handlers/HandlerTestBase.iOS.cs
+++ src/Core/tests/DeviceTests/Handlers/HandlerTestBase.iOS.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using CoreAnimation;
+using CoreGraphics;
 using UIKit;
 using Xunit;
 
@@ -19,12 +20,90 @@ namespace Microsoft.Maui.DeviceTests
 				Rotation = 90
 			};
 
-			var handler = await CreateHandlerAsync(view);
-			var nativeView = (UIView)handler.NativeView;
-
-			var transform = nativeView.Layer.Transform;
+			var transform = await GetValueAsync(view, handler => GetLayerTransform(handler));
 
 			Assert.NotEqual(CATransform3D.Identity, transform);
+		}
+
+		[Fact(DisplayName = "Transformation Calculated Correctly")]
+		public async Task TransformationCalculatedCorrectly()
+		{
+			var view = new TStub()
+			{
+				TranslationX = 10.0,
+				TranslationY = 30.0,
+				Rotation = 248.0,
+				Scale = 2.0,
+				ScaleX = 2.0,
+			};
+
+			var transform = await GetValueAsync(view, handler => GetLayerTransform(handler));
+
+			var expected = new CATransform3D
+			{
+				m11 = -1.4984f,
+				m12 = -3.7087f,
+				m21 = 1.8544f,
+				m22 = -0.7492f,
+				m33 = 2f,
+				m41 = 10f,
+				m42 = 30f,
+				m44 = 1f,
+			};
+
+			expected.AssertEqual(transform);
+		}
+
+		[Theory("Scale initializes correctly")]
+		[InlineData(-10)]
+		[InlineData(-3)]
+		[InlineData(-1.5)]
+		[InlineData(-1)]
+		[InlineData(0)]
+		[InlineData(1)]
+		[InlineData(1.5)]
+		[InlineData(3)]
+		[InlineData(10)]
+		public async Task ScaleInitializeCorrectly(float scale)
+		{
+			var view = new TStub()
+			{
+				Scale = scale,
+			};
+
+			var transform = await GetValueAsync(view, handler => GetLayerTransform(handler));
+
+			var expected = CATransform3D.MakeScale(scale, scale, scale);
+
+			expected.AssertEqual(transform);
+		}
+
+		[Theory("Rotation initializes correctly")]
+		[InlineData(-270)]
+		[InlineData(-180)]
+		[InlineData(-30)]
+		[InlineData(-45)]
+		[InlineData(-1)]
+		[InlineData(0)]
+		[InlineData(1)]
+		[InlineData(45)]
+		[InlineData(30)]
+		[InlineData(180)]
+		[InlineData(270)]
+		public async Task RotationInitializeCorrectly(float rotation)
+		{
+			var view = new TStub()
+			{
+				Rotation = rotation,
+				Width = 100,
+				Height = 100
+			};
+
+			var transform = await GetValueAsync(view, handler => GetLayerTransform(handler));
+
+			var expected = CATransform3D.MakeRotation(rotation, 0, 0, 1);
+
+			expected.AssertEqual(transform);
 		}
 
 		protected THandler CreateHandler(IView view)
@@ -36,6 +115,15 @@ namespace Microsoft.Maui.DeviceTests
 			view.Handler = handler;
 
 			return handler;
+		}
+
+		protected CATransform3D GetLayerTransform(IViewHandler viewHandler)
+		{
+			var view = (UIView)viewHandler.NativeView;
+
+			var transform = view.Layer.Transform;
+
+			return transform;
 		}
 
 		protected string GetAutomationId(IViewHandler viewHandler) =>
