f77c28c5c2896040aa23806822bc36395129ffc4
E.Z. Hart <hartez@gmail.com>
2021-05-14 17:09:23 -0600

    Fix rebase errors

--- src/Core/tests/UnitTests/TestClasses/ViewStub.cs
+++ src/Core/tests/UnitTests/TestClasses/ViewStub.cs
@@ -5,29 +5,28 @@ namespace Microsoft.Maui.UnitTests
 {
 	class ViewStub : IViewStub
 	{
-		public bool IsEnabled => { get; set; }
+		public bool IsEnabled { get; set; }
 
 		public Visibility Visibility { get; set; }
 
 		public double Opacity { get; set; }
 
-
-		public Rectangle Frame => { get; set; }
+		public Rectangle Frame { get; set; }
 
 		public IViewHandler Handler { get; set; }
 
-		public IFrameworkElement Parent => { get; set; }
+		public IFrameworkElement Parent { get; set; }
 
-		public Size DesiredSize => { get; set; }
+		public Size DesiredSize { get; set; }
 
-		public bool IsMeasureValid => { get; set; }
+		public bool IsMeasureValid { get; set; }
 
-		public bool IsArrangeValid => { get; set; }
+		public bool IsArrangeValid { get; set; }
 
-		public double Width => { get; set; }
+		public double Width { get; set; }
 
-		public double Height => { get; set; }
-		public Thickness Margin => { get; set; }
+		public double Height { get; set; }
+		public Thickness Margin { get; set; }
 		public string AutomationId { get; set; }
 
 		public FlowDirection FlowDirection { get; set; }
@@ -38,14 +37,13 @@ namespace Microsoft.Maui.UnitTests
 
 		public Semantics Semantics { get; set; }
 
-		public void Arrange(Rectangle bounds) { }
-			Size.Zero;
+		public Paint Background { get; set; }
+
+		public Size Arrange(Rectangle bounds) => Size.Zero;
+
 		public void InvalidateArrange() { }
 
 		public void InvalidateMeasure() { }
-
-		public Size Arrange(Rectangle bounds) =>
-			Size.Zero;
 
 		public Size Measure(double widthConstraint, double heightConstraint) =>
 			Size.Zero;
