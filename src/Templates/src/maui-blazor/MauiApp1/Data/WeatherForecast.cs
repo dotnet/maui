3e5e72d6ee4630cc7fb62f109498404e84f2fece
Eilon Lipton <Eilon@users.noreply.github.com>
2021-05-19 15:26:43 -0700

    Fix indentation/tabs

--- src/Templates/src/maui-blazor/MauiApp1/Data/WeatherForecast.cs
+++ src/Templates/src/maui-blazor/MauiApp1/Data/WeatherForecast.cs
@@ -2,14 +2,14 @@ using System;
 
 namespace MauiApp1.Data
 {
-    public class WeatherForecast
-    {
-        public DateTime Date { get; set; }
+	public class WeatherForecast
+	{
+		public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+		public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
-    }
+		public string Summary { get; set; }
+	}
 }
--- src/Templates/src/maui-blazor/MauiApp1/Data/WeatherForecastService.cs
+++ src/Templates/src/maui-blazor/MauiApp1/Data/WeatherForecastService.cs
@@ -4,22 +4,22 @@ using System.Threading.Tasks;
 
 namespace MauiApp1.Data
 {
-    public class WeatherForecastService
-    {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+	public class WeatherForecastService
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
-        {
-            var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToArray());
-        }
-    }
+		public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+		{
+			var rng = new Random();
+			return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = startDate.AddDays(index),
+				TemperatureC = rng.Next(-20, 55),
+				Summary = Summaries[rng.Next(Summaries.Length)]
+			}).ToArray());
+		}
+	}
 }
