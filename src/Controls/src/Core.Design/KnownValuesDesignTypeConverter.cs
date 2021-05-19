647877e6b981705b17700bef34b24e3783fac8bf
Jonathan Dick <jondick@gmail.com>
2021-05-19 14:19:43 -0400

    Clean up type comparison

--- src/Controls/src/Core.Design/KnownValuesDesignTypeConverter.cs
+++ src/Controls/src/Core.Design/KnownValuesDesignTypeConverter.cs
@@ -13,7 +13,7 @@
 		// This tells XAML this converter can be used to process strings
 		// Without this the values won't show up as hints
 		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
-			=> sourceType.FullName == typeof(string).FullName || base.CanConvertFrom(context, sourceType);
+			=> sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
 
 		public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
 			=> new(KnownValues);
