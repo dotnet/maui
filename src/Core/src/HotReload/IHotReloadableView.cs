18365d3719e80713fe840fc189a10b3dc597cdc8
E.Z. Hart <hartez@gmail.com>
2021-05-20 10:43:50 -0600

    Update comments

--- src/Core/src/HotReload/IHotReloadableView.cs
+++ src/Core/src/HotReload/IHotReloadableView.cs
@@ -6,8 +6,7 @@ namespace Microsoft.Maui.HotReload
 {
 	// TODO ezhart This interface name is probably wrong because they want it to work for page, and page is not a view
 	// (or FrameworkElement should be called View, and then it's fine)
-	// Alternatively, it's that the Page is _not_ hot-reloadable - the Page's _content_ is. 
-	// But that's probably not right, because I bet they expect Title changes and stuff to be reflected
+
 	public interface IHotReloadableView : IReplaceableView, IFrameworkElement
 	{
 		IReloadHandler ReloadHandler { get; set; }
