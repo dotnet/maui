e4c9f6dfc49a1ecaf020707d9b3c90e6eb67be48
E.Z. Hart <hartez@gmail.com>
2021-05-20 11:54:16 -0600

    Add todo for moving HasContainer

--- src/Core/src/Handlers/IFrameworkElementHandler.cs
+++ src/Core/src/Handlers/IFrameworkElementHandler.cs
@@ -11,7 +11,10 @@ namespace Microsoft.Maui
 		object? NativeView { get; }
 		IFrameworkElement? VirtualView { get; }
 		IMauiContext? MauiContext { get; }
+
+		// TODO ezhart This doesn't make sense for anything above IView, we might need another interface layer to add to ViewHandler
 		bool HasContainer { get; set; }
+
 		void NativeArrange(Rectangle frame);
 		Size GetDesiredSize(double widthConstraint, double heightConstraint);
 	}
