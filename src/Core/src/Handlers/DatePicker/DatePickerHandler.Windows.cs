ca270b687ab2492ffd88167e81489d2c8ea67a84
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-05-26 21:29:07 +0200

    Implement more WinUI DatePickerHandler properties (#859)
    * Implement more properties in WinUI DatePickerHandler
    
    * Mapped native DateChanged event
    
    * Fix broken tests
    
    * Enable nullable in WinUI DatePickerHandler
    
    * Fix build error

--- src/Core/src/Handlers/DatePicker/DatePickerHandler.Windows.cs
+++ src/Core/src/Handlers/DatePicker/DatePickerHandler.Windows.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿#nullable enable
 using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.Maui.Handlers
@@ -7,27 +7,70 @@ namespace Microsoft.Maui.Handlers
 	{
 		protected override DatePicker CreateNativeView() => new DatePicker();
 
-		[MissingMapper]
-		public static void MapFormat(DatePickerHandler handler, IDatePicker datePicker) { }
+		protected override void ConnectHandler(DatePicker nativeView)
+		{
+			nativeView.DateChanged += OnControlDateChanged;
+		}
+
+		protected override void DisconnectHandler(DatePicker nativeView)
+		{
+			nativeView.DateChanged -= OnControlDateChanged;
+		}
+
+		public static void MapFormat(DatePickerHandler handler, IDatePicker datePicker)
+		{
+			handler.NativeView?.UpdateDate(datePicker);
+		}
 
 		public static void MapDate(DatePickerHandler handler, IDatePicker datePicker)
 		{
 			handler.NativeView?.UpdateDate(datePicker);
 		}
 
-		[MissingMapper]
-		public static void MapMinimumDate(DatePickerHandler handler, IDatePicker datePicker) { }
+		public static void MapMinimumDate(DatePickerHandler handler, IDatePicker datePicker)
+		{
+			handler.NativeView?.UpdateMinimumDate(datePicker);
+		}
 
-		[MissingMapper]
-		public static void MapMaximumDate(DatePickerHandler handler, IDatePicker datePicker) { }
+		public static void MapMaximumDate(DatePickerHandler handler, IDatePicker datePicker)
+		{
+			handler.NativeView?.UpdateMaximumDate(datePicker);
+		}
 
-		[MissingMapper]
-		public static void MapCharacterSpacing(DatePickerHandler handler, IDatePicker datePicker) { }
+		public static void MapCharacterSpacing(DatePickerHandler handler, IDatePicker datePicker)
+		{
+			handler.NativeView?.UpdateCharacterSpacing(datePicker);
+		}
 
-		[MissingMapper]
-		public static void MapFont(DatePickerHandler handler, IDatePicker datePicker) { }
+		public static void MapFont(DatePickerHandler handler, IDatePicker datePicker)
+		{
+			var fontManager = handler.GetRequiredService<IFontManager>();
+
+			handler.NativeView?.UpdateFont(datePicker, fontManager);
+		}
 
 		[MissingMapper]
 		public static void MapTextColor(DatePickerHandler handler, IDatePicker datePicker) { }
+
+		void OnControlDateChanged(object? sender, DatePickerValueChangedEventArgs e)
+		{
+			if (VirtualView == null)
+				return;
+
+			if (VirtualView.Date.CompareTo(e.NewDate.Date) != 0)
+			{
+				var date = e.NewDate.Date.Clamp(VirtualView.MinimumDate, VirtualView.MaximumDate);
+				VirtualView.Date = date;
+
+				// Set the control date-time to clamped value, if it exceeded the limits at the time of installation.
+				if (date != e.NewDate.Date)
+				{
+					NativeView?.UpdateDate(date);
+					NativeView?.UpdateLayout();
+				}
+
+				VirtualView.InvalidateMeasure();
+			}
+		}
 	}
 }
