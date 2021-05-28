61fa76fe3227374f6728e5e6f05d461d09c8f0d4
Shane Neuville <shneuvil@microsoft.com>
2021-05-28 03:40:00 -0500

    Fix renderer shim ordering when creating native renderer (#1105)
    * Set Renderer before setting virtual view
    
    * - fix NET6 targets

---
+++ src/Compatibility/Core/src/RendererToHandlerShim.Android.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Android.Views;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+using Microsoft.Maui.Graphics;
+using static Microsoft.Maui.Controls.Compatibility.Platform.Android.AppCompat.Platform;
+using NativeView = Android.Views.View;
+
+namespace Microsoft.Maui.Controls.Compatibility
+{
+    public partial class RendererToHandlerShim
+    {
+		protected override NativeView CreateNativeView()
+		{
+			return VisualElementRenderer.View;
+		}
+
+		IVisualElementRenderer CreateRenderer(IView view)
+		{
+			if (Context != null)
+			{
+				var renderer = Internals.Registrar.Registered.GetHandlerForObject<IVisualElementRenderer>(view, Context)
+										   ?? new DefaultRenderer(Context);
+				return renderer;
+			}
+
+			return null;
+		}
+
+		public override Size GetDesiredSize(double widthConstraint, double heightConstraint)
+		{
+			return GetNativeSize(
+				VisualElementRenderer, widthConstraint, heightConstraint);
+		}
+
+		public override void NativeArrange(Rectangle frame)
+		{
+			// This is a hack to force the shimmed control to actually do layout; without this, some controls won't actually
+			// call OnLayout after SetFrame if their sizes haven't changed (e.g., ScrollView)
+			// Luckily, measuring with MeasureSpecMode.Exactly is pretty fast, since it just returns the value you give it.
+			NativeView?.Measure(MeasureSpecMode.Exactly.MakeMeasureSpec((int)frame.Width),
+				MeasureSpecMode.Exactly.MakeMeasureSpec((int)frame.Height));
+
+			base.NativeArrange(frame);
+		}
+	}
+}
