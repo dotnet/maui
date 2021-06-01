f2b75254a2bad9905ec57d5add5af826fb2c188c
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-06-01 12:09:33 +0200

    Implement on Windows

--- src/Core/src/Handlers/ShapeView/ShapeViewHandler.Windows.cs
+++ src/Core/src/Handlers/ShapeView/ShapeViewHandler.Windows.cs
@@ -1,36 +1,53 @@
 ﻿using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics.Win2D;
 
 namespace Microsoft.Maui.Handlers
 {
-	public partial class ShapeViewHandler : ViewHandler<IShapeView, MauiShapeView>
+	public partial class ShapeViewHandler : ViewHandler<IShapeView, W2DGraphicsView>
 	{
-		protected override MauiShapeView CreateNativeView()
+		protected override W2DGraphicsView CreateNativeView()
 		{
-			return new MauiShapeView();
+			return new W2DGraphicsView();
 		}
 
-		[MissingMapper]
-		public static void MapShape(ShapeViewHandler handler, IShapeView shapeView) { }
-		
-		[MissingMapper]
-		public static void MapFill(ShapeViewHandler handler, IShapeView shapeView) { }
-		
-		[MissingMapper]
-		public static void MapStroke(ShapeViewHandler handler, IShapeView shapeView) { }
-		
-		[MissingMapper]
-		public static void MapStrokeThickness(ShapeViewHandler handler, IShapeView shapeView) { }
-		
-		[MissingMapper]
-		public static void MapStrokeDashPattern(ShapeViewHandler handler, IShapeView shapeView) { }
-	
-		[MissingMapper]
-		public static void MapStrokeLineCap(ShapeViewHandler handler, IShapeView shapeView) { }
-		
-		[MissingMapper]
-		public static void MapStrokeLineJoin(ShapeViewHandler handler, IShapeView shapeView) { }
-		
-		[MissingMapper]
-		public static void MapStrokeMiterLimit(ShapeViewHandler handler, IShapeView shapeView) { }
+		public static void MapShape(ShapeViewHandler handler, IShapeView shapeView)
+		{
+			handler.NativeView?.UpdateShape(shapeView);
+		}
+
+		public static void MapFill(ShapeViewHandler handler, IShapeView shapeView)
+		{
+			handler.NativeView?.InvalidateShape(shapeView);
+		}
+
+		public static void MapStroke(ShapeViewHandler handler, IShapeView shapeView)
+		{
+			handler.NativeView?.InvalidateShape(shapeView);
+		}
+
+		public static void MapStrokeThickness(ShapeViewHandler handler, IShapeView shapeView)
+		{
+			handler.NativeView?.InvalidateShape(shapeView);
+		}
+
+		public static void MapStrokeDashPattern(ShapeViewHandler handler, IShapeView shapeView)
+		{
+			handler.NativeView?.InvalidateShape(shapeView);
+		}
+
+		public static void MapStrokeLineCap(ShapeViewHandler handler, IShapeView shapeView)
+		{
+			handler.NativeView?.InvalidateShape(shapeView);
+		}
+
+		public static void MapStrokeLineJoin(ShapeViewHandler handler, IShapeView shapeView)
+		{
+			handler.NativeView?.InvalidateShape(shapeView);
+		}
+
+		public static void MapStrokeMiterLimit(ShapeViewHandler handler, IShapeView shapeView)
+		{
+			handler.NativeView?.InvalidateShape(shapeView);
+		}
 	}
 }
