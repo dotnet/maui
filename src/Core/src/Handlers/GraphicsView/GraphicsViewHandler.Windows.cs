e2268918562188cfd6d428f469baf058b8a834ae
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-06-01 10:14:03 +0200

    Implement on Windows

--- src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.Windows.cs
+++ src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.Windows.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.Maui.Graphics.Win2D;
 
 namespace Microsoft.Maui.Handlers
 {
-	public partial class GraphicsViewHandler : ViewHandler<IGraphicsView, UserControl>
+	public partial class GraphicsViewHandler : ViewHandler<IGraphicsView, W2DGraphicsView>
 	{
-		protected override UserControl CreateNativeView() => new UserControl();
+		protected override W2DGraphicsView CreateNativeView()
+		{
+			return new W2DGraphicsView();
+		}
 
-		[MissingMapper]
-		public static void MapDrawable(IViewHandler handler, IGraphicsView graphicsView) { }
+		public static void MapDrawable(GraphicsViewHandler handler, IGraphicsView graphicsView)
+		{
+			handler.NativeView?.UpdateDrawable(graphicsView);
+		}
 	}
 }
--- src/Core/src/Platform/Windows/GraphicsViewExtensions.cs
+++ src/Core/src/Platform/Windows/GraphicsViewExtensions.cs
@@ -1,7 +1,12 @@
-﻿namespace Microsoft.Maui
+﻿using Microsoft.Maui.Graphics.Win2D;
+
+namespace Microsoft.Maui
 {
 	public static class GraphicsViewExtensions
 	{
-
+		public static void UpdateDrawable(this W2DGraphicsView nativeGraphicsView, IGraphicsView graphicsView)
+		{
+			nativeGraphicsView.Drawable = graphicsView.Drawable;
+		}
 	}
 }
