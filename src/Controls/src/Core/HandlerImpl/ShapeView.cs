64f5f2436125a58b3b554d8cf02856ce5e189e05
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-06-01 18:47:40 +0200

    Added BindableProperties to ShapeView

--- src/Controls/src/Core/HandlerImpl/ShapeView.cs
+++ src/Controls/src/Core/HandlerImpl/ShapeView.cs
@@ -15,22 +15,85 @@ namespace Microsoft.Maui.Controls.Shapes2
 			Shape = shape;
 		}
 
-		public IShape? Shape { get; set; }
+		public static readonly BindableProperty ShapeProperty =
+			BindableProperty.Create(nameof(Shape), typeof(IShape), typeof(ShapeView), null);
+		
+		public static readonly BindableProperty FillProperty =
+			BindableProperty.Create(nameof(Fill), typeof(Paint), typeof(ShapeView), null);
 
-		public Paint? Fill { get; set; }
+		public static readonly BindableProperty StrokeProperty =
+			BindableProperty.Create(nameof(Stroke), typeof(Color), typeof(ShapeView), null);
 
-		public Color? Stroke { get; set; }
+		public static readonly BindableProperty StrokeThicknessProperty =
+			BindableProperty.Create(nameof(StrokeThickness), typeof(double), typeof(ShapeView), 1.0);
 
-		public double StrokeThickness { get; set; }
+		public static readonly BindableProperty StrokeDashPatternProperty =
+			BindableProperty.Create(nameof(StrokeDashPattern), typeof(float[]), typeof(ShapeView), null);
 
-		public float[]? StrokeDashPattern { get; set; }
+		public static readonly BindableProperty StrokeLineCapProperty =
+			BindableProperty.Create(nameof(StrokeLineCap), typeof(LineCap), typeof(ShapeView), LineCap.Butt);
 
-		public LineCap StrokeLineCap { get; set; }
+		public static readonly BindableProperty StrokeLineJoinProperty =
+			BindableProperty.Create(nameof(StrokeLineJoin), typeof(LineJoin), typeof(ShapeView), LineJoin.Miter);
 
-		public LineJoin StrokeLineJoin { get; set; }
+		public static readonly BindableProperty StrokeMiterLimitProperty =
+			BindableProperty.Create(nameof(StrokeMiterLimit), typeof(float), typeof(ShapeView), 10.0f);
+		
+		public static readonly BindableProperty StretchProperty =	
+			BindableProperty.Create(nameof(Stretch), typeof(Stretch), typeof(ShapeView), Stretch.None);
 
-		public float StrokeMiterLimit { get; set; }
+		public IShape? Shape
+		{
+			set { SetValue(ShapeProperty, value); }
+			get { return (IShape?)GetValue(ShapeProperty); }
+		}
 
-		public Stretch Stretch { get; set; }
+		public Paint? Fill
+		{
+			set { SetValue(FillProperty, value); }
+			get { return (Paint?)GetValue(FillProperty); }
+		}
+
+		public Color? Stroke
+		{
+			set { SetValue(StrokeProperty, value); }
+			get { return (Color?)GetValue(StrokeProperty); }
+		}
+
+		public double StrokeThickness
+		{
+			set { SetValue(StrokeThicknessProperty, value); }
+			get { return (double)GetValue(StrokeThicknessProperty); }
+		}
+
+		public float[] StrokeDashPattern
+		{
+			set { SetValue(StrokeDashPatternProperty, value); }
+			get { return (float[])GetValue(StrokeDashPatternProperty); }
+		}
+
+		public LineCap StrokeLineCap
+		{
+			set { SetValue(StrokeLineCapProperty, value); }
+			get { return (LineCap)GetValue(StrokeLineCapProperty); }
+		}
+
+		public LineJoin StrokeLineJoin
+		{
+			set { SetValue(StrokeLineJoinProperty, value); }
+			get { return (LineJoin)GetValue(StrokeLineJoinProperty); }
+		}
+
+		public float StrokeMiterLimit
+		{
+			set { SetValue(StrokeMiterLimitProperty, value); }
+			get { return (float)GetValue(StrokeMiterLimitProperty); }
+		}
+
+		public Stretch Stretch
+		{
+			set { SetValue(StretchProperty, value); }
+			get { return (Stretch)GetValue(StretchProperty); }
+		}
 	}
 }
