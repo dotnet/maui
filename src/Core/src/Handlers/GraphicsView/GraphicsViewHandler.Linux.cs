e6a5990e18ccc3169a587419fbce8f5d2ba4cd23
lytico <lytico@users.sourceforge.net>
2021-06-07 23:17:45 +0200

    Core.csproj Gtk : implement GraphicsViewHandler.Linux.cs

--- src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.Linux.cs
+++ src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.Linux.cs
@@ -2,10 +2,20 @@
 
 namespace Microsoft.Maui.Handlers
 {
-	public partial class GraphicsViewHandler : ViewHandler<IGraphicsView, Gtk.Widget>
-	{
-		protected override Gtk.Widget CreateNativeView() => throw new NotImplementedException();
 
-		public static void MapDrawable(IViewHandler handler, IGraphicsView graphicsView) { }
+	public partial class GraphicsViewHandler : ViewHandler<IGraphicsView, Microsoft.Maui.Graphics.Native.Gtk.GtkGraphicsView>
+	{
+
+		protected override Microsoft.Maui.Graphics.Native.Gtk.GtkGraphicsView CreateNativeView() => new();
+
+		public static void MapDrawable(GraphicsViewHandler handler, IGraphicsView graphicsView)
+		{
+			if (handler.NativeView is { } nativeView)
+			{
+				nativeView.Drawable = graphicsView.Drawable;
+			}
+		}
+
 	}
+
 }
