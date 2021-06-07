8146fb76d5ab5bde317ee9e336e6f9fed707fd0a
Eilon Lipton <Eilon@users.noreply.github.com>
2021-06-07 09:32:53 -0700

    Use new Blazor WebView2 abstractions (#1181)

---
+++ src/BlazorWebView/src/core/Windows/WinUICoreWebView2SettingsWrapper.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components.WebView.WebView2;
+using Microsoft.Web.WebView2.Core;
+
+namespace Microsoft.AspNetCore.Components.WebView.Maui
+{
+	internal class WinUICoreWebView2SettingsWrapper : ICoreWebView2SettingsWrapper
+	{
+		private readonly CoreWebView2Settings _settings;
+
+		public WinUICoreWebView2SettingsWrapper(CoreWebView2Settings settings)
+		{
+			_settings = settings;
+		}
+
+		public bool IsScriptEnabled
+		{
+			get => _settings.IsScriptEnabled;
+			set => _settings.IsScriptEnabled = value;
+		}
+		public bool IsWebMessageEnabled
+		{
+			get => _settings.IsWebMessageEnabled;
+			set => _settings.IsWebMessageEnabled = value;
+		}
+		public bool AreDefaultScriptDialogsEnabled
+		{
+			get => _settings.AreDefaultScriptDialogsEnabled;
+			set => _settings.AreDefaultScriptDialogsEnabled = value;
+		}
+		public bool IsStatusBarEnabled
+		{
+			get => _settings.IsStatusBarEnabled;
+			set => _settings.IsStatusBarEnabled = value;
+		}
+		public bool AreDevToolsEnabled
+		{
+			get => _settings.AreDevToolsEnabled;
+			set => _settings.AreDevToolsEnabled = value;
+		}
+		public bool AreDefaultContextMenusEnabled
+		{
+			get => _settings.AreDefaultContextMenusEnabled;
+			set => _settings.AreDefaultContextMenusEnabled = value;
+		}
+		public bool AreHostObjectsAllowed
+		{
+			get => _settings.AreHostObjectsAllowed;
+			set => _settings.AreHostObjectsAllowed = value;
+		}
+		public bool IsZoomControlEnabled
+		{
+			get => _settings.IsZoomControlEnabled;
+			set => _settings.IsZoomControlEnabled = value;
+		}
+		public bool IsBuiltInErrorPageEnabled
+		{
+			get => _settings.IsBuiltInErrorPageEnabled;
+			set => _settings.IsBuiltInErrorPageEnabled = value;
+		}
+	}
+}
---
+++ src/BlazorWebView/src/core/Windows/WinUIWebViewManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Components.WebView.WebView2;
+using Microsoft.Extensions.FileProviders;
+using WebView2Control = Microsoft.UI.Xaml.Controls.WebView2;
+
+namespace Microsoft.AspNetCore.Components.WebView.Maui
+{
+	/// <summary>
+	/// An implementation of <see cref="WebViewManager"/> that uses the Edge WebView2 browser control
+	/// to render web content in WinUI applications.
+	/// </summary>
+	public class WinUIWebViewManager : WebView2WebViewManager
+	{
+		readonly WebView2Control _nativeWebView2;
+
+		public WinUIWebViewManager(WebView2Control nativeWebView2, IWebView2Wrapper webview, IServiceProvider services, Dispatcher dispatcher, IFileProvider fileProvider, string hostPageRelativePath) : base(webview, services, dispatcher, fileProvider, hostPageRelativePath)
+		{
+			_nativeWebView2 = nativeWebView2;
+		}
+
+		protected override void QueueBlazorStart()
+		{
+			// In .NET MAUI we use autostart='false' for the Blazor script reference, so we start it up manually in this event
+			_nativeWebView2.CoreWebView2.DOMContentLoaded += async (_, __) =>
+			{
+				await _nativeWebView2.CoreWebView2!.ExecuteScriptAsync(@"
+					Blazor.start();
+					");
+			};
+		}
+	}
+}
