39388e74844d366de52d2c77bdf89273a7b00b51
lytico <lytico@users.sourceforge.net>
2021-06-07 23:01:36 +0200

    Core.csproj Gtk : cleanup PageView.cs

--- src/Core/src/Platform/Linux/PageView.cs
+++ src/Core/src/Platform/Linux/PageView.cs
@@ -36,33 +36,6 @@ namespace Microsoft.Maui
 			}
 		}
 
-		protected override void OnGetPreferredHeightForWidth(int width, out int minimumHeight, out int naturalHeight)
-		{
-			base.OnGetPreferredHeightForWidth(width, out minimumHeight, out naturalHeight);
-
-			if (Content == null) return;
-
-			Content.GetPreferredHeightForWidth(width, out var childMinimumHeight, out var childNaturalHeight);
-			minimumHeight = Math.Max(Math.Max(minimumHeight, childMinimumHeight), hr);
-			naturalHeight = Math.Max(Math.Max(naturalHeight, childNaturalHeight), hr);
-			hr = 0;
-			
-		}
-
-		int hr = 0;
-
-		protected override void OnGetPreferredWidthForHeight(int height, out int minimumWidth, out int naturalWidth)
-		{
-			base.OnGetPreferredWidthForHeight(height, out minimumWidth, out naturalWidth);
-			hr = Math.Max(hr, height);
-
-			if (Content == null) return;
-
-			Content.GetPreferredWidthForHeight(height, out var childMinimumWidth, out var childNaturalWidth);
-			minimumWidth = Math.Max(minimumWidth, childMinimumWidth);
-			naturalWidth = Math.Max(naturalWidth, childNaturalWidth);
-		}
-
 	}
 
 }
