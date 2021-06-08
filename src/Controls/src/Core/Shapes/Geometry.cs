22fdd1b8e5d04c2c91ae43b2419e7360d6f213f6
Jonathan Dick <jondick@gmail.com>
2021-06-07 23:22:31 -0400

    Remove comment

--- src/Controls/src/Core/Shapes/Geometry.cs
+++ src/Controls/src/Core/Shapes/Geometry.cs
@@ -4,7 +4,6 @@ namespace Microsoft.Maui.Controls.Shapes
 {
 	public abstract class Geometry : BindableObject
 	{
-		//public abstract PathF PathForBounds(Graphics.Rectangle rect);
 		public abstract void AppendToPath(PathF path);
 	}
 }
