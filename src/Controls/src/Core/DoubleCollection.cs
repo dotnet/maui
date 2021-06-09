3facec41821616766dcfc00e6b56c9c97318dbd7
Jonathan Dick <jondick@gmail.com>
2021-06-09 14:58:32 -0400

    Add implicit conversion to DoubleCollection

--- src/Controls/src/Core/DoubleCollection.cs
+++ src/Controls/src/Core/DoubleCollection.cs
@@ -1,10 +1,21 @@
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Microsoft.Maui.Controls
 {
 	[TypeConverter(typeof(DoubleCollectionConverter))]
 	public sealed class DoubleCollection : ObservableCollection<double>
 	{
+		public DoubleCollection()
+		{ }
 
+		public DoubleCollection(double[] values)
+			: base(values)
+		{
+		}
+
+		public static implicit operator DoubleCollection(double[] d) => new DoubleCollection(d);
+
+		public static implicit operator DoubleCollection(float[] f) => new(f.Cast<double>().ToArray());
 	}
 }
