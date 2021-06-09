f6082d99419551fedee4ed7582793f381f117b1c
Jonathan Dick <jondick@gmail.com>
2021-06-09 14:57:55 -0400

    Move PointCollection back to Controls

---
+++ src/Controls/src/Core/PointCollection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Maui.Graphics;
+
+namespace Microsoft.Maui.Controls
+{
+	public sealed class PointCollection : ObservableCollection<Point>
+	{
+		public PointCollection() : base()
+		{
+		}
+
+		public PointCollection(Point[] p)
+			: base(p)
+		{
+		}
+
+		public static implicit operator PointCollection(Point[] d) => new(d);
+	}
+}
