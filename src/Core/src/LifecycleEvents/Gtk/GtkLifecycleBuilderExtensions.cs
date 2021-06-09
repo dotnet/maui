6f031c6752be731af847d82652fc6171d135d94a
lytico <lytico@users.sourceforge.net>
2021-06-09 03:34:36 +0200

    Core.csproj Gtk : GtkLifecycleBuilderExtensions.cs: introduce OnShown

--- src/Core/src/LifecycleEvents/Gtk/GtkLifecycleBuilderExtensions.cs
+++ src/Core/src/LifecycleEvents/Gtk/GtkLifecycleBuilderExtensions.cs
@@ -6,5 +6,6 @@ namespace Microsoft.Maui.LifecycleEvents
 		public static IGtkLifecycleBuilder OnClosed(this IGtkLifecycleBuilder lifecycle, GtkLifecycle.OnHidden del) => lifecycle.OnEvent(del);
 		public static IGtkLifecycleBuilder OnLaunched(this IGtkLifecycleBuilder lifecycle, GtkLifecycle.OnStartup del) => lifecycle.OnEvent(del);
 		public static IGtkLifecycleBuilder OnVisibilityChanged(this IGtkLifecycleBuilder lifecycle, GtkLifecycle.OnVisibilityChanged del) => lifecycle.OnEvent(del);
+		public static IGtkLifecycleBuilder OnShown(this IGtkLifecycleBuilder lifecycle, GtkLifecycle.OnShown del) => lifecycle.OnEvent(del);
 	}
 }
