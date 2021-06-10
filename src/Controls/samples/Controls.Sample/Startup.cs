1b012eda50f90a307398954d911eaeef961b49cc
Shane Neuville <shneuvil@microsoft.com>
2021-06-09 19:16:19 -0500

    Update Startup.cs

--- src/Controls/samples/Controls.Sample/Startup.cs
+++ src/Controls/samples/Controls.Sample/Startup.cs
@@ -31,9 +31,8 @@ namespace Maui.Controls.Sample
 
 	public class Startup : IStartup
 	{
-		enum PageType { Xaml, Semantics, Main, Blazor, NavigationPage, Shell }
-		private PageType _pageType = PageType.NavigationPage;
 		enum PageType { Xaml, Semantics, Main, Blazor, NavigationPage, Shell, TabbedPage }
+		private PageType _pageType = PageType.NavigationPage;
 
 		public readonly static bool UseXamlApp = true;
 		public readonly static bool UseFullDI = false;
