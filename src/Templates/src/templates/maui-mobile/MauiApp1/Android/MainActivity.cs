c99e37ea5ef5944ed4e89f6aa7d46707c171c778
David Ortinau <david.ortinau@microsoft.com>
2021-06-11 15:50:18 -0500

    Registering for Android configuration changes

--- src/Templates/src/templates/maui-mobile/MauiApp1/Android/MainActivity.cs
+++ src/Templates/src/templates/maui-mobile/MauiApp1/Android/MainActivity.cs
@@ -3,7 +3,7 @@ using Microsoft.Maui;
 
 namespace MauiApp1
 {
-	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true)]
+	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 	public class MainActivity : MauiAppCompatActivity
 	{
 	}
