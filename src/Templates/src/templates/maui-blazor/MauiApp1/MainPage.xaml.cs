1f84879b2c9585657001a521391a73988b2fa09e
Matthew Leibowitz <mattleibow@live.com>
2021-06-11 23:21:13 +0200

    Clean up the templates a bit

--- src/Templates/src/templates/maui-blazor/MauiApp1/MainPage.xaml.cs
+++ src/Templates/src/templates/maui-blazor/MauiApp1/MainPage.xaml.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using Microsoft.Maui;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Xaml;
 
 namespace MauiApp1
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainPage : ContentPage, IPage
+	public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
