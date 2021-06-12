2dba128ed3dd6ef05dbd810f5b603c209005167d
Matthew Leibowitz <mattleibow@live.com>
2021-06-12 02:39:26 +0200

    Make sure to actually show files in VS

--- src/Templates/src/templates/maui-mobile/MauiApp1/App.xaml.cs
+++ src/Templates/src/templates/maui-mobile/MauiApp1/App.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
-using Microsoft.Maui.Controls.Xaml;
 using Application = Microsoft.Maui.Controls.Application;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 namespace MauiApp1
 {
