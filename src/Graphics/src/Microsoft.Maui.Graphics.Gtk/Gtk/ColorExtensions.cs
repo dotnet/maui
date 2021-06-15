ac4d46952811dd963f74dcbf06e7010bebe45f8f
lytico <lytico@users.sourceforge.net>
2021-06-15 14:22:30 +0200

    Microsoft.Maui.Graphics.Gtk.csproj: remove Gdk.Color - Extensions

--- src/Graphics/src/Microsoft.Maui.Graphics.Gtk/Gtk/ColorExtensions.cs
+++ src/Graphics/src/Microsoft.Maui.Graphics.Gtk/Gtk/ColorExtensions.cs
@@ -19,20 +19,6 @@ namespace Microsoft.Maui.Graphics.Native.Gtk {
 		public static Color ToColor(this Cairo.Color color)
 			=> new Color((float) color.R, (float) color.G, (float) color.B, (float) color.A);
 
-		// https://developer.gnome.org/gdk3/stable/gdk3-Colors.html#GdkColor
-		// When working with cairo, it is often more convenient to use a GdkRGBA instead, and GdkColor has been deprecated in favor of GdkRGBA.
-		[Obsolete("GdkColor has been deprecated in favor of GdkRGBA")]
-		public static Gdk.Color ToGdkColor(this Color color)
-			=> color == default ? default : new Gdk.Color((byte) (color.Red * 255), (byte) (color.Green * 255), (byte) (color.Blue * 255));
-
-		[Obsolete("GdkColor has been deprecated in favor of GdkRGBA")]
-		public static Gdk.Color ToGdkColor(this Gdk.RGBA color)
-			=> new Gdk.Color((byte) (color.Red * 255), (byte) (color.Green * 255), (byte) (color.Blue * 255));
-
-		[Obsolete("GdkColor has been deprecated in favor of GdkRGBA")]
-		public static Color ToColor(this Gdk.Color color)
-			=> new Color(color.Red / (float) ushort.MaxValue, color.Green / (float) ushort.MaxValue, color.Blue / (float) ushort.MaxValue);
-
 	}
 
 }
