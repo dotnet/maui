7a7f3370b3ef89b1f0c1dab224eda4d4607e419e
lytico <lytico@users.sourceforge.net>
2021-06-17 03:04:58 +0200

    Core.csproj Gtk : ViewExtensions.cs: make use of Gtk.Css

--- src/Core/src/Platform/Gtk/ViewExtensions.cs
+++ src/Core/src/Platform/Gtk/ViewExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Gtk;
 using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics.Native.Gtk;
 
 namespace Microsoft.Maui
 {
@@ -14,20 +15,68 @@ namespace Microsoft.Maui
 		[PortHandler("implement drawing of other paints than solidpaint")]
 		public static void UpdateBackground(this Widget nativeView, IView view)
 		{
+			var color = view.Background?.BackgroundColor;
+
 			if (view.Background is SolidPaint solidPaint)
 			{
-				nativeView.SetBackgroundColor(solidPaint.Color);
+				color = solidPaint.Color;
 			}
-			else if (view.Background is Paint paint)
-			{
-				nativeView.SetBackgroundColor(paint.BackgroundColor);
-			}
-			else
-			{
-				;
-			}
-		}
 
+			if (color == null)
+				return;
+
+			switch (nativeView)
+			{
+				case ProgressBar:
+					nativeView.SetColor(color, "background-color", "trough > progress");
+
+					break;
+				case ComboBox box:
+					// no effect: box.SetColor(bkColor, "border-color");
+					box.GetCellRendererText().SetBackground(color);
+
+					break;
+				default:
+					nativeView.SetBackgroundColor(color);
+
+					break;
+			}
+
+		}
+		
+		public static void UpdateForeground(this Widget nativeView, Paint? paint)
+		{
+			var color = paint?.ForegroundColor;
+
+			if (paint is SolidPaint solidPaint)
+			{
+				color = solidPaint.Color;
+			}
+
+			if (color == null)
+				return;
+
+			switch (nativeView)
+			{
+				case ProgressBar:
+					nativeView.SetColor(color, "color", "trough > progress");
+
+					break;
+				case CheckButton:
+					// no effect as check is an icon
+					nativeView.SetColor(color, "color", "check");
+					break;
+				case ComboBox box:
+					 box.GetCellRendererText().SetForeground(color);
+
+					break;
+				default:
+					nativeView.SetForegroundColor(color);
+
+					break;
+			}
+
+		}
 		public static void UpdateIsEnabled(this Widget nativeView, IView view) =>
 			nativeView?.UpdateIsEnabled(view.IsEnabled);
 
@@ -35,10 +84,8 @@ namespace Microsoft.Maui
 			nativeView?.UpdateVisibility(view.Visibility);
 
 		public static void UpdateSemantics(this Widget nativeView, IView view)
-		{
-			
-		}
-		
+		{ }
+
 		public static void UpdateOpacity(this Widget nativeView, IView view) { }
 
 	}
