da44611922ed938eeebff370086fa0d177ceb541
lytico <lytico@users.sourceforge.net>
2021-06-17 03:07:35 +0200

    Core.csproj Gtk : ImageViewExtensions.cs: refactor

--- src/Core/src/Platform/Gtk/ImageViewExtensions.cs
+++ src/Core/src/Platform/Gtk/ImageViewExtensions.cs
@@ -6,16 +6,15 @@ using Gtk;
 
 namespace Microsoft.Maui
 {
+
 	public static class ImageViewExtensions
 	{
+
 		public static void Clear(this ImageView imageView)
-		{
-			
-		}
+		{ }
 
 		public static void UpdateAspect(this ImageView imageView, IImage image)
-		{
-		}
+		{ }
 
 		public static void UpdateIsAnimationPlaying(this ImageView imageView, IImageSourcePart image)
 		{
@@ -33,33 +32,42 @@ namespace Microsoft.Maui
 		{
 			imageView.Clear();
 
+			return await UpdateImageSourceAsync(image, (float)imageView.ScaleFactor, services, p =>
+			{
+				imageView.Image = p;
+				imageView.UpdateIsAnimationPlaying(image);
+			}, cancellationToken);
+
+		}
+
+		public static async Task<IImageSourceServiceResult<Gdk.Pixbuf>?> UpdateImageSourceAsync(this IImageSourcePart? image, float scale, IImageSourceServiceProvider provider, Action<Gdk.Pixbuf?> onResult, CancellationToken cancellationToken = default)
+		{
+			if (image == null)
+				return null;
+
 			image.UpdateIsLoading(false);
 
 			var imageSource = image.Source;
+
 			if (imageSource == null)
 				return null;
 
 			var events = image as IImageSourcePartEvents;
 
-			events?.LoadingStarted();
 			image.UpdateIsLoading(true);
 
 			try
 			{
-				var service = services.GetRequiredImageSourceService(imageSource);
-
-				var scale = imageView.ScaleFactor;
-				var result = await service.GetImageAsync(imageSource, (float)scale, cancellationToken);
-				var uiImage = result?.Value;
-
+				var service = provider.GetRequiredImageSourceService(imageSource);
+				var result = await service.GetImageAsync(imageSource, scale, cancellationToken);
+				var pixbuf = result?.Value;
 				var applied = !cancellationToken.IsCancellationRequested && imageSource == image.Source;
 
 				// only set the image if we are still on the same one
 				if (applied)
 				{
-					imageView.Image = uiImage;
+					onResult(pixbuf);
 
-					imageView.UpdateIsAnimationPlaying(image);
 				}
 
 				events?.LoadingCompleted(applied);
@@ -74,6 +82,7 @@ namespace Microsoft.Maui
 			catch (Exception ex)
 			{
 				events?.LoadingFailed(ex);
+
 			}
 			finally
 			{
@@ -85,6 +94,44 @@ namespace Microsoft.Maui
 			}
 
 			return null;
+
 		}
+
+		public static async Task<IImageSourceServiceResult<Gdk.Pixbuf>?> UpdateImageSourceAsync(this IImageSource? imageSource, float scale, IImageSourceServiceProvider provider, Action<Gdk.Pixbuf?> onResult, CancellationToken cancellationToken = default)
+		{
+
+			if (imageSource == null)
+				return null;
+
+			try
+			{
+				var service = provider.GetRequiredImageSourceService(imageSource);
+				var result = await service.GetImageAsync(imageSource, scale, cancellationToken);
+				var pixbuf = result?.Value;
+				var applied = !cancellationToken.IsCancellationRequested;
+
+				if (applied)
+				{
+					onResult(pixbuf);
+
+				}
+
+				return result;
+			}
+			catch (OperationCanceledException)
+			{
+				// no-op
+			}
+			catch (Exception)
+			{
+				// no-op
+
+			}
+
+			return null;
+
+		}
+
 	}
+
 }
