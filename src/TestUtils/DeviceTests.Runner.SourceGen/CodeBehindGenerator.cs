8c8d94bc3c679808528fedb39a43ed4e0645cfb1
Matthew Leibowitz <mattleibow@live.com>
2021-06-18 10:35:20 +0200

    don't need this

--- src/TestUtils/DeviceTests.Runner.SourceGen/CodeBehindGenerator.cs
+++ src/TestUtils/DeviceTests.Runner.SourceGen/CodeBehindGenerator.cs
@@ -9,26 +9,6 @@ namespace Microsoft.Maui.Controls.SourceGen
 	[Generator]
 	public class CodeBehindGenerator : ISourceGenerator
 	{
-		static string[] _csharpKeywords = new[] {
-			"abstract", "as",
-			"base", "bool", "break", "byte",
-			"case", "catch", "char", "checked", "class", "const", "continue",
-			"decimal", "default", "delegate", "do", "double",
-			"else", "enum", "event", "explicit", "extern",
-			"false", "finally", "fixed", "float", "for", "foreach",
-			"goto",
-			"if", "implicit", "in", "int", "interface", "internal", "is",
-			"lock", "long", "namespace", "new", "null",
-			"object", "operator", "out", "override",
-			"params", "private", "protected", "public",
-			"readonly", "ref", "return",
-			"sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch",
-			"this", "throw", "true", "try", "typeof",
-			"uint", "ulong", "unchecked", "unsafe", "ushort", "using",
-			"virtual", "void", "volatile",
-			"while",
-		};
-
 		public void Initialize(GeneratorInitializationContext context)
 		{
 			//#if DEBUG
