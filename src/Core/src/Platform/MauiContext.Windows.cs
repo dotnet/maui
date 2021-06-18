f75680362e3a683afcbf339b4bc2db7cc8433b8d
Shane Neuville <shneuvil@microsoft.com>
2021-06-18 10:11:04 -0500

    Add UIWindow and UI.XAML.Window to IMauiContext (#1357)
    * Add Platform Windows to IMauiContext
    
    * - window to IMauiContext
    
    * - android fix

---
+++ src/Core/src/Platform/MauiContext.Windows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Maui
+{
+	public partial class MauiContext
+	{
+		public MauiContext(IServiceProvider services, UI.Xaml.Window window) : this(services)
+		{
+			Window = window ?? throw new ArgumentNullException(nameof(window));
+		}
+
+		public UI.Xaml.Window? Window
+		{
+			get;
+			private set;
+		}
+	}
+}
