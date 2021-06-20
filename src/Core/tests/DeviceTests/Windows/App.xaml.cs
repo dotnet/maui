e1616b035375797fda4c5e8c5dbd060ae2bde0af
Matthew Leibowitz <mattleibow@live.com>
2021-06-20 04:21:39 +0200

    This seems to run again :)

--- src/Core/tests/DeviceTests/Windows/App.xaml.cs
+++ src/Core/tests/DeviceTests/Windows/App.xaml.cs
@@ -5,7 +5,7 @@ using Windows.ApplicationModel;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Microsoft.Maui.Core.DeviceTests.WinUI
+namespace Microsoft.Maui.TestUtils.DeviceTests.Sample.WinUI
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
