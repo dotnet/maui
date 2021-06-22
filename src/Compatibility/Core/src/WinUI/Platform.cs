05d28746c2370d4f5857f43b19ff35f107de9e84
E.Z. Hart <hartez@users.noreply.github.com>
2021-06-22 12:32:27 -0600

    Fix winui sample issues (#1374)
    * Setup Xaml Page to look like sample
    
    * - set color
    
    * Make ScrollView/RendererToHandler/HandlerToRenderer work on WinUI
    
    * Clean up unused stuff
    
    * Update TODO
    
    * Reset startup
    
    * Update src/Controls/samples/Controls.Sample/Startup.cs
    
    Co-authored-by: Shane Neuville <shneuvil@microsoft.com>
    
    * More reasonable layout compat tests
    
    Co-authored-by: Shane Neuville <shneuvil@microsoft.com>

--- src/Compatibility/Core/src/WinUI/Platform.cs
+++ src/Compatibility/Core/src/WinUI/Platform.cs
@@ -100,6 +100,7 @@ namespace Microsoft.Maui.Controls.Compatibility.Platform.UWP
 				else if (handler is INativeViewHandler vh)
 				{
 					renderer = new HandlerToRendererShim(vh);
+					SetRenderer(element, renderer);
 				}
 			}
 
