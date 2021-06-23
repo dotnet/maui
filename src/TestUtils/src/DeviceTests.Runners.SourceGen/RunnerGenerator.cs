26f6145a098b5acee653f4c605474fb505b600b1
Matthew Leibowitz <mattleibow@live.com>
2021-06-23 19:09:49 +0200

    new things

--- src/TestUtils/src/DeviceTests.Runners.SourceGen/RunnerGenerator.cs
+++ src/TestUtils/src/DeviceTests.Runners.SourceGen/RunnerGenerator.cs
@@ -178,13 +178,13 @@ namespace " + RootNamespace + @"
 #if !SKIP_HEADLESS_RUNNER_APP_DELEGATE_GENERATION
 			if (global::Microsoft.Maui.TestUtils.DeviceTests.Runners.HeadlessRunner.MauiTestApplicationDelegate.IsHeadlessRunner(args))
 			{
-				global::UIKit.UIApplication.Main(args, null, """ + headlessDelegateName + @""");
+				global::UIKit.UIApplication.Main(args, null, typeof(global::" + RootNamespace + @"." + headlessDelegateName + @"));
 			}
 			else
 #endif
 			{
 #if !SKIP_VISUAL_RUNNER_APP_DELEGATE_GENERATION
-				global::UIKit.UIApplication.Main(args, null, """ + visualDelegateName + @""");
+				global::UIKit.UIApplication.Main(args, null, typeof(global::" + RootNamespace + @"." + visualDelegateName + @"));
 #endif
 			}
 		}
