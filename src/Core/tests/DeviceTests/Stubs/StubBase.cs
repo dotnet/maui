068156606f9c32277bfb8c9d59214de5102ea53d
James Clancey <james.clancey@gmail.com>
2021-06-24 14:11:42 -0800

    Update StubBase.cs

--- src/Core/tests/DeviceTests/Stubs/StubBase.cs
+++ src/Core/tests/DeviceTests/Stubs/StubBase.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Maui.DeviceTests.Stubs
 
 		public IViewHandler Handler { get; set; }
 
-		public IGeometry Clip { get; set; }
+		public IShape Clip { get; set; }
 
 		public IFrameworkElement Parent { get; set; }
 
