70a1fdf60b63c40853626287b0a4df90595acff0
James Clancey <james.clancey@gmail.com>
2021-06-23 16:23:58 -0800

    Updated the Windows Native Ticker

--- src/Core/src/Platform/Windows/NativeTicker.cs
+++ src/Core/src/Platform/Windows/NativeTicker.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Windows.ApplicationModel.Core;
+using System;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.Maui.Controls.Internals;
+
 namespace Microsoft.Maui.Animations
 {
 	public class NativeTicker : Ticker
 	{
 		public NativeTicker(IMauiContext mauiContext)
 		{
+		}
+		public override void Start()
+		{
+
+			CompositionTarget.Rendering += RenderingFrameEventHandler;
+		}
+		public override void Stop()
+		{
+			CompositionTarget.Rendering -= RenderingFrameEventHandler;
+		}
+		void RenderingFrameEventHandler(object sender, object args)
+		{
+			Fire?.Invoke();
 		}
 	}
 }
