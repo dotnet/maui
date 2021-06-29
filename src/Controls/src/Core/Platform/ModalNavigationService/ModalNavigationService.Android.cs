d97d8ab7bdc2c0a6de58aeff92342c5ffa379ca7
Matthew Leibowitz <mattleibow@live.com>
2021-06-29 18:26:27 +0200

    Fix things

--- src/Controls/src/Core/Platform/ModalNavigationService/ModalNavigationService.Android.cs
+++ src/Controls/src/Core/Platform/ModalNavigationService/ModalNavigationService.Android.cs
@@ -1,13 +1,8 @@
 ﻿#nullable enable
-
 using System;
 using System.ComponentModel;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
 using System.Threading.Tasks;
 using Android.Content;
-using Android.Runtime;
 using Android.Views;
 using Android.Views.Animations;
 using AndroidX.Activity;
