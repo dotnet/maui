6760ba9c9d30fe1507f99cb646b538a9eee9e724
Stephane Delcroix <stephane@delcroix.org>
2021-06-29 13:49:47 +0200

    Turn XamlC on by default (#1422)
    * Turn XamlC on by default
    
    * fix tests
    
    * winui
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>

--- src/Compatibility/ControlGallery/src/Core/GalleryPages/SwipeViewGalleries/BasicSwipeGallery.xaml.cs
+++ src/Compatibility/ControlGallery/src/Core/GalleryPages/SwipeViewGalleries/BasicSwipeGallery.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using Microsoft.Maui.Controls.Xaml;
+
 using Microsoft.Maui.Controls.Internals;
 
 namespace Microsoft.Maui.Controls.Compatibility.ControlGallery.GalleryPages.SwipeViewGalleries
 {
 	[Preserve(AllMembers = true)]
+	[XamlCompilation(XamlCompilationOptions.Skip)]
 	public partial class BasicSwipeGallery : ContentPage
 	{
 		public BasicSwipeGallery()
