109ce51946d8c00e58fd7ca358dc84497acb3a57
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-06-29 18:13:11 +0200

    Updated Core comments (#1458)

--- src/Core/src/Core/IContainer.cs
+++ src/Core/src/Core/IContainer.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Microsoft.Maui
 {
+	/// <summary>
+	/// Provides functionality to act as containers for views. 
+	/// </summary>
 	public interface IContainer
 	{
 		/// <summary>
