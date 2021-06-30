f2451831c96c1423360b640d6eb7dbb58a96d468
Shane Neuville <shneuvil@microsoft.com>
2021-06-30 11:39:27 -0700

    Android Gestures (#1489)
    * Android Gestures
    
    * - fix drag and drop pages
    
    * - fix setting virtual view
    
    * - cleanup
    
    * - cleanup
    
    * - fix content
    
    * - cleanup
    
    * - cleanup
    
    * - only show click on catalyst and windows
    
    * - fix unit tests
    
    * - remove editorBrowsable where I could
    
    * - fix iOS
    
    * - fix winui
    
    * - change to interfaces
    
    * - fix validation service check
    
    * - fix ios gallery
    
    * Update GestureManager.cs
    
    * Update GestureManager.cs
    
    * Update GestureManager.cs
    
    * - organize

---
+++ src/Controls/samples/Controls.Sample/Pages/GesturesPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.Controls.Sample.Pages
+{
+	public partial class GesturesPage
+	{
+		public GesturesPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
