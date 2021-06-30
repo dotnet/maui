b6a9663357917f4526ed65e63669df77a16afc7a
Matthew Leibowitz <mattleibow@live.com>
2021-06-30 03:26:26 +0200

    iOS things

--- src/Core/src/Handlers/Window/WindowHandler.iOS.cs
+++ src/Core/src/Handlers/Window/WindowHandler.iOS.cs
@@ -6,5 +6,18 @@ namespace Microsoft.Maui.Handlers
 	public partial class WindowHandler : ElementHandler<IWindow, UIWindow>
 	{
 		public static void MapTitle(WindowHandler handler, IWindow window) { }
+
+		public override void SetVirtualView(IElement view)
+		{
+			base.SetVirtualView(view);
+
+			_ = NativeView ?? throw new InvalidOperationException($"{nameof(NativeView)} should have been set by base class.");
+			_ = VirtualView ?? throw new InvalidOperationException($"{nameof(VirtualView)} should have been set by base class.");
+			_ = MauiContext ?? throw new InvalidOperationException($"{nameof(MauiContext)} should have been set by base class.");
+
+			var nativeContent = VirtualView.Content.ToUIViewController(MauiContext);
+
+			NativeView.RootViewController = nativeContent;
+		}
 	}
 }
