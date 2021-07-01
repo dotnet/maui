1dcd160df89fe2476222330855d646b2dc155e40
Matthew Leibowitz <mattleibow@live.com>
2021-07-01 18:50:19 +0200

    Make the non-generic apps the main type
    The generics are still there, but are just a shortcut to getting the type. This is mainly for Windows which uses xaml, and xaml does not support generics.

--- src/Compatibility/ControlGallery/src/WinUI/App.xaml.cs
+++ src/Compatibility/ControlGallery/src/WinUI/App.xaml.cs
@@ -1,12 +1,53 @@
 ﻿namespace Microsoft.Maui.Controls.Compatibility.ControlGallery.WinUI
 {
-	sealed partial class App : MiddleApp
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	sealed partial class App : MauiWinUIApplication
 	{
-		public static bool RunningAsUITests { get; set; }
+		public static bool RunningAsUITests { get; private set; }
 
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
 		public App()
 		{
 			InitializeComponent();
+			//Suspending += OnSuspending;
+		}
+
+		protected override IStartup OnCreateStartup() => new Startup();
+
+		public override MauiWinUIWindow CreateWindow()
+		{
+			return new MainPage();
+		}
+
+		/// <summary>
+		/// Invoked when the application is launched normally by the end user.  Other entry points
+		/// will be used such as when the application is launched to open a specific file.
+		/// </summary>
+		/// <param name="e">Details about the launch request and process.</param>
+		protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs e)
+		{
+			base.OnLaunched(e);
+			if (!String.IsNullOrWhiteSpace(e.Arguments) &&
+				e.Arguments.Contains("RunningAsUITests"))
+			{
+				RunningAsUITests = true;
+				ControlGallery.App.PreloadTestCasesIssuesList = false;
+			}
+		}
+
+		/// <summary>
+		/// Invoked when Navigation to a certain page fails
+		/// </summary>
+		/// <param name="sender">The Frame which failed navigation</param>
+		/// <param name="e">Details about the navigation failure</param>
+		void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+		{
+			throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
 		}
 	}
 
