923dc035b69ffdae43a30de1b037eb8c1c1c7d97
Matthew Leibowitz <mattleibow@live.com>
2021-07-01 20:16:58 +0200

    Update WinUI to 0.8 stable

--- src/Compatibility/Core/src/WinUI/WindowsPlatformServices.cs
+++ src/Compatibility/Core/src/WinUI/WindowsPlatformServices.cs
@@ -5,7 +5,7 @@ namespace Microsoft.Maui.Controls.Compatibility.Platform.UWP
 	internal class WindowsPlatformServices : WindowsBasePlatformServices
 	{
 #pragma warning disable CS8305 // Type is for evaluation purposes only and is subject to change or removal in future updates.
-		public WindowsPlatformServices(Microsoft.System.DispatcherQueue dispatcher) : base(dispatcher)
+		public WindowsPlatformServices(Microsoft.UI.Dispatching.DispatcherQueue dispatcher) : base(dispatcher)
 #pragma warning restore CS8305 // Type is for evaluation purposes only and is subject to change or removal in future updates.
 		{
 		}
