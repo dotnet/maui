d84c41f5d6bd09b53c7786491ef1798957c3a311
Shane Neuville <shneuvil@microsoft.com>
2021-07-02 15:11:41 -0500

    - fix conflicts

--- src/Compatibility/ControlGallery/src/WinUI/App.xaml.cs
+++ src/Compatibility/ControlGallery/src/WinUI/App.xaml.cs
@@ -5,7 +5,7 @@
 	/// </summary>
 	sealed partial class App : MauiWinUIApplication
 	{
-		public static bool RunningAsUITests { get; private set; }
+		public static bool RunningAsUITests { get; internal set; }
 
 		/// <summary>
 		/// Initializes the singleton application object.  This is the first line of authored code
@@ -14,44 +14,8 @@
 		public App()
 		{
 			InitializeComponent();
-			//Suspending += OnSuspending;
 		}
 
 		protected override IStartup OnCreateStartup() => new Startup();
-
-		public override MauiWinUIWindow CreateWindow()
-		{
-			return new MainPage();
-		}
-
-		/// <summary>
-		/// Invoked when the application is launched normally by the end user.  Other entry points
-		/// will be used such as when the application is launched to open a specific file.
-		/// </summary>
-		/// <param name="e">Details about the launch request and process.</param>
-		protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs e)
-		{
-			base.OnLaunched(e);
-			if (!String.IsNullOrWhiteSpace(e.Arguments) &&
-				e.Arguments.Contains("RunningAsUITests"))
-			{
-				RunningAsUITests = true;
-				ControlGallery.App.PreloadTestCasesIssuesList = false;
-			}
-		}
-
-		/// <summary>
-		/// Invoked when Navigation to a certain page fails
-		/// </summary>
-		/// <param name="sender">The Frame which failed navigation</param>
-		/// <param name="e">Details about the navigation failure</param>
-		void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-		{
-			throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-		}
-	}
-
-	public class MiddleApp : MauiWinUIApplication<WinUIStartup>
-	{
 	}
 }
