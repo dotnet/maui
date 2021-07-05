6e0af9052ba15d902a94ed6a7724813b0ea4283f
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-07-05 11:42:24 +0200

    Update the Gallery adding samples to validate the Wiki Status

---
+++ src/Controls/samples/Controls.Sample/ViewModels/SearchBarViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using Maui.Controls.Sample.ViewModels.Base;
+using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.ViewModels
+{
+	public class SearchBarViewModel : BaseViewModel
+	{
+		public ICommand SearchCommand => new Command<string>(ExecuteSearchCommand);
+
+		void ExecuteSearchCommand(string searchCommandParameter)
+		{
+			Debug.WriteLine($"SearchCommand {searchCommandParameter}");
+		}
+	}
+}
