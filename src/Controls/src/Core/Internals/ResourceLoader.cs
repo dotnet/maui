28c7cd591802645366fb4c4686739b005494e9bc
Stephane Delcroix <stephane@delcroix.org>
2021-07-12 21:21:30 +0200

    fix tests

--- src/Controls/src/Core/Internals/ResourceLoader.cs
+++ src/Controls/src/Core/Internals/ResourceLoader.cs
@@ -57,19 +57,6 @@ namespace Microsoft.Maui.Controls.Internals
 			public bool UseDesignProperties { get; set; }
 		}
 
-		static Action<Exception> exceptionHandler1;
-
-		[Obsolete("2 is better than 1")]
-		internal static Action<Exception> ExceptionHandler
-		{
-			get => exceptionHandler1;
-			set
-			{
-				exceptionHandler1 = value;
-				ExceptionHandler2 = value != null ? ((Exception exception, string filepath) err) => exceptionHandler1(err.exception) : (Action<(Exception, string)>)null;
-			}
-		}
-
 		internal static Action<(Exception exception, string filepath)> ExceptionHandler2 { get; set; }
 	}
 }
