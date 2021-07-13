15ad3e110adce330e99fffd140fdc55831a613f0
lytico <lytico@users.sourceforge.net>
2021-07-13 20:56:56 +0200

    Core.csproj Gtk : FileImageSourceService.Gtk.cs, ImageSourceServiceResult.cs: introduce loading from Resource

--- src/Core/src/ImageSources/FileImageSourceService/FileImageSourceService.Gtk.cs
+++ src/Core/src/ImageSources/FileImageSourceService/FileImageSourceService.Gtk.cs
@@ -1,6 +1,8 @@
 ﻿#nullable enable
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -8,8 +10,10 @@ using NativeImage = Gdk.Pixbuf;
 
 namespace Microsoft.Maui
 {
+
 	public partial class FileImageSourceService
 	{
+
 		public override Task<IImageSourceServiceResult<NativeImage>?> GetImageAsync(IImageSource imageSource, float scale = 1, CancellationToken cancellationToken = default) =>
 			GetImageAsync((IFileImageSource)imageSource, scale, cancellationToken);
 
@@ -20,27 +24,67 @@ namespace Microsoft.Maui
 
 			var filename = imageSource.File;
 
+			NativeImage? TryLoadFile(string file)
+			{
+				if (File.Exists(file))
+					return new NativeImage(filename);
+
+				return null;
+			}
+
+			NativeImage? TryLoadResource(string file)
+			{
+				foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+				{
+					var names = assembly.GetManifestResourceNames();
+					var res = names.FirstOrDefault(r => r.EndsWith($".{file}"));
+
+					if (res != null)
+					{
+						return new(assembly, res);
+					}
+				}
+
+				return default;
+
+			}
+
 			try
 			{
-				var image = File.Exists(filename)
-					? new NativeImage(filename)
-					: NativeImage.LoadFromResource(filename);
+				var imageDirectory = Configuration?.GetImageDirectory();
+				var image = TryLoadFile(filename);
+
+				if (image == null && imageDirectory != null)
+				{
+					image = TryLoadFile(Path.Combine(imageDirectory, filename));
+				}
+
+				var isResource = false;
+
+				if (image == null)
+				{
+					image = TryLoadResource(filename);
+					isResource = true;
+				}
 
 				if (image == null)
 					throw new InvalidOperationException("Unable to load image file.");
 
-				var result = new ImageSourceServiceResult(image, () => image.Dispose());
+				var result = new ImageSourceServiceResult(image, () => image.Dispose()) { IsResource = isResource };
 
 				return FromResult(result);
 			}
 			catch (Exception ex)
 			{
 				Logger?.LogWarning(ex, "Unable to load image file '{File}'.", filename);
+
 				throw;
 			}
 		}
 
 		static Task<IImageSourceServiceResult<NativeImage>?> FromResult(IImageSourceServiceResult<NativeImage>? result) =>
 			Task.FromResult(result);
+
 	}
+
 }
