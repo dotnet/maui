7afb7e560431a31e4da9b4657d3717b4fadd3d7d
lytico <lytico@users.sourceforge.net>
2021-07-13 21:01:03 +0200

    Core.csproj Gtk : GtkCssExtensions.cs: adjust naming / fix image loading / add Gtk.StateFlags - support

--- src/Core/src/Platform/Gtk/GtkCssExtensions.cs
+++ src/Core/src/Platform/Gtk/GtkCssExtensions.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Linq;
+using System.Runtime.CompilerServices;
+using Gtk;
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Graphics.Native.Gtk;
-using Pango;
 
 namespace Microsoft.Maui
 {
@@ -29,65 +29,68 @@ namespace Microsoft.Maui
 			return mainNode;
 		}
 
-		public static string CssImage(this Gdk.Pixbuf nativeImage)
+		/// <summary>
+		/// seems that CssParser doesn't support base64:
+		/// https://github.com/GNOME/gtk/blob/gtk-3-22/gtk/gtkcssparser.c
+		/// _gtk_css_parser_read_url
+		/// </summary>
+		static string CssImage(this Gdk.Pixbuf nativeImage)
 		{
 			var puf = nativeImage.SaveToBuffer(ImageFormat.Png.ToImageExtension());
 
 			return $"url('data:image/png;base64,{Convert.ToBase64String(puf)}')";
 		}
 
-		[PortHandler("implement drawing of other paints than GradientPaint")]
-		public static string? CssImage(this Paint? paint)
+		[MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+		public static void Realize(Gtk.CssProvider p)
 		{
-			if (paint.IsNullOrEmpty())
-				return null;
+			var l = p.ToString().Length;
 
-			string Stops(GradientStop[] sorted)
+			if (l != 0)
 			{
-				var max = sorted[^1].Offset;
-				max = 100 / (max == 0 ? 1 : max);
-				var stops = string.Join(",", sorted.Select(s => $"{s.Color.ToGdkRgba().ToString()} {s.Offset * max}%"));
-
-				return stops;
+				// ReSharper disable once EmptyStatement
+				;
 			}
-
-			switch (paint)
-			{
-				case LinearGradientPaint lg:
-				{
-					var stops = Stops(lg.GetSortedStops());
-					var css = $"linear-gradient( to right, {stops})";
-
-					return css;
-				}
-				case RadialGradientPaint rg:
-				{
-					var stops = Stops(rg.GetSortedStops());
-					var css = $"radial-gradient({stops})";
-
-					return css;
-				}
-
-				default:
-					break;
-			}
-
-			return null;
-
 		}
 
-		public static void SetStyleImageNode(this Gtk.Widget widget, string cssImage, string mainNode, string attr, string? subNode = null)
+		public static void SetStyleValueNode(this Gtk.Widget widget, string value, string mainNode, string attr, string? subNode = null)
 		{
+			if (string.IsNullOrEmpty(value))
+				return;
+
 			using var p = new Gtk.CssProvider();
 
 			subNode = subNode != null ? $" > {subNode} " : subNode;
 
-			p.LoadFromData($"{mainNode}{subNode}{{{attr}:{cssImage}}}");
+			p.LoadFromData($"{mainNode}{subNode}{{{attr}:{value}}}");
 			widget.StyleContext.AddProvider(p, Gtk.StyleProviderPriority.User);
+
+			if (value.StartsWith("url"))
+				Realize(p);
 		}
 
-		public static void SetStyleImage(this Gtk.Widget widget, string cssImage, string attr, string? subNode = null)
-			=> widget.SetStyleImageNode(cssImage, widget.CssMainNode(), attr, subNode);
+		public static void SetStyleValue(this Gtk.Widget widget, string value, string attr, string? subNode = null)
+			=> widget.SetStyleValueNode(value, widget.CssMainNode(), attr, subNode);
+
+		public static string? CssState(this Gtk.StateFlags it) =>
+			it switch
+			{
+				StateFlags.Normal => null,
+				StateFlags.Active => "active",
+				StateFlags.Prelight => "hover",
+				StateFlags.Selected => "selected",
+				StateFlags.Insensitive => "disabled",
+				StateFlags.Inconsistent => "inconsistent",
+				StateFlags.Focused => "focused",
+				StateFlags.Backdrop => "backdrop",
+				StateFlags.DirLtr => "dir(ltr)",
+				StateFlags.DirRtl => "dir(rtl)",
+				StateFlags.Link => "link",
+				StateFlags.Visited => "visited",
+				StateFlags.Checked => "checked",
+				StateFlags.DropActive => "drop(active)",
+				_ => throw new ArgumentOutOfRangeException(nameof(it), it, null)
+			};
 
 	}
 
