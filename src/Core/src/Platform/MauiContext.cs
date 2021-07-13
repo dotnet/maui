44ad80d1f8573cd69d4fb2f2432824a0d88fcf38
Matthew Leibowitz <mattleibow@live.com>
2021-07-13 03:14:44 +0200

    Lazy

--- src/Core/src/Platform/MauiContext.cs
+++ src/Core/src/Platform/MauiContext.cs
@@ -6,27 +6,26 @@ namespace Microsoft.Maui
 {
 	public partial class MauiContext : IMauiContext, IScopedMauiContext
 	{
-		readonly IAnimationManager _animationManager;
+		readonly IAnimationManager? _animationManager;
 
 		public MauiContext()
 		{
 			// Temporary hack until we fully remove Forms.Init
 			Services = null!;
 			Handlers = null!;
-			_animationManager = null!;
 		}
 
 		public MauiContext(IServiceProvider services)
 		{
 			Services = services ?? throw new ArgumentNullException(nameof(services));
 			Handlers = Services.GetRequiredService<IMauiHandlersServiceProvider>();
-			_animationManager = Services.GetRequiredService<IAnimationManager>();
 		}
 
 		public IServiceProvider Services { get; }
 
 		public IMauiHandlersServiceProvider Handlers { get; }
 
-		IAnimationManager IScopedMauiContext.AnimationManager => _animationManager;
+		IAnimationManager IScopedMauiContext.AnimationManager =>
+			_animationManager ?? Services.GetRequiredService<IAnimationManager>();
 	}
 }
