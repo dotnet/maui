7e1155a07a17cb205c3a976c0f928d28942a27d6
Jonathan Dick <jondick@gmail.com>
2021-07-14 12:26:19 -0400

    Fix password NRE and use new WinUI Keyboard state api

--- src/Core/src/Platform/Windows/MauiTextBox.cs
+++ src/Core/src/Platform/Windows/MauiTextBox.cs
@@ -254,21 +254,26 @@ namespace Microsoft.Maui
 
 		static string DetermineTextFromPassword(string realText, int start, string passwordText)
 		{
-			var lengthDifference = passwordText.Length - realText.Length;
+			var rt = realText ?? string.Empty;
+
+			var lengthDifference = passwordText.Length - (rt?.Length ?? 0);
 			if (lengthDifference > 0)
-				realText = realText.Insert(start - lengthDifference, new string(ObfuscationCharacter, lengthDifference));
+				rt = rt.Insert(start - lengthDifference, new string(ObfuscationCharacter, lengthDifference));
 			else if (lengthDifference < 0)
-				realText = realText.Remove(start, -lengthDifference);
+				rt = rt.Remove(start, -lengthDifference);
 
 			var sb = new StringBuilder(passwordText.Length);
 			for (int i = 0; i < passwordText.Length; i++)
-				sb.Append(passwordText[i] == ObfuscationCharacter ? realText[i] : passwordText[i]);
+				sb.Append(passwordText[i] == ObfuscationCharacter ? rt[i] : passwordText[i]);
 
 			return sb.ToString();
 		}
 
 		string Obfuscate(string text, bool leaveLastVisible = false)
 		{
+			if (string.IsNullOrEmpty(text))
+				return string.Empty;
+
 			if (!leaveLastVisible)
 				return new string(ObfuscationCharacter, text.Length);
 
@@ -301,7 +306,7 @@ namespace Microsoft.Maui
 			{
 				// The ctrlDown flag is used to track if the Ctrl key is pressed; if it's actively being used and the most recent
 				// key to trigger OnKeyDown, then treat it as handled.
-				var ctrlDown = app.MainWindow.CoreWindow.GetKeyState(VirtualKey.Control).HasFlag(CoreVirtualKeyStates.Down);
+				var ctrlDown = Microsoft.UI.Input.KeyboardInput.GetKeyStateForCurrentThread(VirtualKey.Control).HasFlag(CoreVirtualKeyStates.Down);
 
 				// The shift, tab, and directional (Home/End/PgUp/PgDown included) keys can be used to select text and should otherwise
 				// be ignored.
