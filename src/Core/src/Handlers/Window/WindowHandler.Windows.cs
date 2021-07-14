7b4aa52a345b79bd99781cd98251a9e575d23d48
E.Z. Hart <hartez@gmail.com>
2021-07-14 10:37:30 -0600

    Measure Page in RootPanel MeasureOverride

--- src/Core/src/Handlers/Window/WindowHandler.Windows.cs
+++ src/Core/src/Handlers/Window/WindowHandler.Windows.cs
@@ -4,45 +4,32 @@ namespace Microsoft.Maui.Handlers
 {
 	public partial class WindowHandler : ElementHandler<IWindow, UI.Xaml.Window>
 	{
-		RootPanel? rootPanel = null;
+		RootPanel? _rootPanel = null;
 
 		protected override void ConnectHandler(UI.Xaml.Window nativeView)
 		{
 			base.ConnectHandler(nativeView);
 
-			if (rootPanel == null)
+			if (_rootPanel == null)
 			{
 				// TODO WINUI should this be some other known constant or via some mechanism? Or done differently?
 				MauiWinUIApplication.Current.Resources.TryGetValue("MauiRootContainerStyle", out object? style);
 
-				rootPanel = new RootPanel
+				_rootPanel = new RootPanel
 				{
 					Style = style as UI.Xaml.Style
 				};
 			}
 
-			nativeView.Content = rootPanel;
-			nativeView.SizeChanged += NativeView_SizeChanged;
+			nativeView.Content = _rootPanel;
 		}
 
 		protected override void DisconnectHandler(UI.Xaml.Window nativeView)
 		{
-			nativeView.SizeChanged -= NativeView_SizeChanged;
-			rootPanel?.Children?.Clear();
+			_rootPanel?.Children?.Clear();
 			nativeView.Content = null;
 
 			base.DisconnectHandler(nativeView);
-		}
-
-		void NativeView_SizeChanged(object sender, UI.Xaml.WindowSizeChangedEventArgs args)
-		{
-			if (rootPanel != null)
-			{
-				foreach (var c in rootPanel.Children)
-				{
-					c.Measure(args.Size);
-				}
-			}
 		}
 
 		public static void MapTitle(WindowHandler handler, IWindow window) =>
@@ -54,8 +41,8 @@ namespace Microsoft.Maui.Handlers
 
 			var nativeContent = window.Content.ToNative(handler.MauiContext);
 
-			handler?.rootPanel?.Children?.Clear();
-			handler?.rootPanel?.Children?.Add(nativeContent);
+			handler?._rootPanel?.Children?.Clear();
+			handler?._rootPanel?.Children?.Add(nativeContent);
 		}
 	}
 }
