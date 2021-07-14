fd31c5f593b1f1203fef319ccce3218906b674d2
Jonathan Dick <jondick@gmail.com>
2021-07-14 11:46:33 -0400

    Resize root panel content on window size changed
    This resizes the root panel's contents when the window size changes

--- src/Core/src/Handlers/Window/WindowHandler.Windows.cs
+++ src/Core/src/Handlers/Window/WindowHandler.Windows.cs
@@ -4,6 +4,47 @@ namespace Microsoft.Maui.Handlers
 {
 	public partial class WindowHandler : ElementHandler<IWindow, UI.Xaml.Window>
 	{
+		RootPanel? rootPanel = null;
+
+		protected override void ConnectHandler(UI.Xaml.Window nativeView)
+		{
+			base.ConnectHandler(nativeView);
+
+			if (rootPanel == null)
+			{
+				// TODO WINUI should this be some other known constant or via some mechanism? Or done differently?
+				MauiWinUIApplication.Current.Resources.TryGetValue("MauiRootContainerStyle", out object? style);
+
+				rootPanel = new RootPanel
+				{
+					Style = style as UI.Xaml.Style
+				};
+			}
+
+			nativeView.Content = rootPanel;
+			nativeView.SizeChanged += NativeView_SizeChanged;
+		}
+
+		protected override void DisconnectHandler(UI.Xaml.Window nativeView)
+		{
+			nativeView.SizeChanged -= NativeView_SizeChanged;
+			rootPanel?.Children?.Clear();
+			nativeView.Content = null;
+
+			base.DisconnectHandler(nativeView);
+		}
+
+		void NativeView_SizeChanged(object sender, UI.Xaml.WindowSizeChangedEventArgs args)
+		{
+			if (rootPanel != null)
+			{
+				foreach (var c in rootPanel.Children)
+				{
+					c.Measure(args.Size);
+				}
+			}
+		}
+
 		public static void MapTitle(WindowHandler handler, IWindow window) =>
 			handler.NativeView?.UpdateTitle(window);
 
@@ -13,19 +54,8 @@ namespace Microsoft.Maui.Handlers
 
 			var nativeContent = window.Content.ToNative(handler.MauiContext);
 
-			// TODO WINUI should this be some other known constant or via some mechanism? Or done differently?
-			MauiWinUIApplication.Current.Resources.TryGetValue("MauiRootContainerStyle", out object? style);
-
-			var root = new RootPanel
-			{
-				Style = style as UI.Xaml.Style,
-				Children =
-				{
-					nativeContent
-				}
-			};
-
-			handler.NativeView.Content = root;
+			handler?.rootPanel?.Children?.Clear();
+			handler?.rootPanel?.Children?.Add(nativeContent);
 		}
 	}
 }
