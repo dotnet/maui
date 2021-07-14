22cc1e532d388ce7ccab7ff2fc3934a6ba415ca2
E.Z. Hart <hartez@gmail.com>
2021-07-14 08:28:29 -0600

    Fix NRE

--- src/Controls/src/Core/ScrollView.cs
+++ src/Controls/src/Core/ScrollView.cs
@@ -278,7 +278,7 @@ namespace Microsoft.Maui.Controls
 
 			SizeRequest contentRequest;
 
-			if (Content is IFrameworkElement fe)
+			if (Content is IFrameworkElement fe && fe.Handler != null)
 			{
 				contentRequest = fe.Handler.GetDesiredSize(widthConstraint, heightConstraint);
 			}
