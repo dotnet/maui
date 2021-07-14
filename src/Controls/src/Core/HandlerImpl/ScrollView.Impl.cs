5c159aabfd85e84cbe9d9291029f34592f788608
E.Z. Hart <hartez@users.noreply.github.com>
2021-07-14 04:30:41 -0600

    WinUI-only ScrollBarHandler (#1671)

---
+++ src/Controls/src/Core/HandlerImpl/ScrollView.Impl.cs
@@ -0,0 +1,33 @@
+﻿namespace Microsoft.Maui.Controls
+{
+	public partial class ScrollView : IScrollView
+	{
+		IView IScrollView.Content => Content;
+
+		double IScrollView.HorizontalOffset
+		{
+			get => ScrollX;
+			set
+			{
+				if (ScrollX != value)
+				{
+					SetScrolledPosition(value, ScrollY);
+				}
+			}
+		}
+
+		double IScrollView.VerticalOffset
+		{
+			get => ScrollY;
+			set
+			{
+				if (ScrollY != value)
+				{
+					SetScrolledPosition(ScrollX, value);
+				}
+			}
+		}
+
+		void IScrollView.ScrollFinished() => SendScrollFinished();
+	}
+}
