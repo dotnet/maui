2272766692e3d0c02b11df41fcd590bda2941d2a
Jonathan Dick <jondick@gmail.com>
2021-07-15 12:27:31 -0400

    Subclass WinUI Window to hook native win32 events

--- src/Core/src/Platform/Windows/MauiWinUIWindow.cs
+++ src/Core/src/Platform/Windows/MauiWinUIWindow.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Maui.LifecycleEvents;
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Maui.LifecycleEvents;
+using WinRT;
 
 namespace Microsoft.Maui
 {
@@ -6,9 +9,17 @@ namespace Microsoft.Maui
 	{
 		public MauiWinUIWindow()
 		{
+			NativeMessage += OnNativeMessage;
 			Activated += OnActivated;
 			Closed += OnClosed;
 			VisibilityChanged += OnVisibilityChanged;
+
+			SubClassingWin32();
+		}
+
+		protected virtual void OnNativeMessage(object? sender, WindowsNativeMessageEventArgs args)
+		{
+			MauiWinUIApplication.Current.Services?.InvokeLifecycleEvents<WindowsLifecycle.OnNativeMessage>(m => m(this, args));
 		}
 
 		protected virtual void OnActivated(object sender, UI.Xaml.WindowActivatedEventArgs args)
@@ -25,5 +36,39 @@ namespace Microsoft.Maui
 		{
 			MauiWinUIApplication.Current.Services?.InvokeLifecycleEvents<WindowsLifecycle.OnVisibilityChanged>(del => del(this, args));
 		}
+
+		public event EventHandler<WindowsNativeMessageEventArgs> NativeMessage;
+
+		#region Native Window
+		IntPtr _hwnd = IntPtr.Zero;
+		delegate IntPtr WinProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+		WinProc? newWndProc = null;
+		IntPtr oldWndProc = IntPtr.Zero;
+
+		[DllImport("user32")]
+		static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, WinProc newProc);
+		[DllImport("user32.dll")]
+		static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+		void SubClassingWin32()
+		{
+			//Get the Window's HWND
+			_hwnd = this.As<IWindowNative>().WindowHandle;
+			if (_hwnd == IntPtr.Zero)
+				throw new NullReferenceException("The Window Handle is null.");
+
+			newWndProc = new WinProc(NewWindowProc);
+			oldWndProc = SetWindowLongPtr(_hwnd, /* GWL_WNDPROC */ -4, newWndProc);
+		}
+
+		IntPtr NewWindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+		{
+			var args = new WindowsNativeMessageEventArgs(hWnd, msg, wParam, lParam);
+
+			NativeMessage?.Invoke(this, args);
+
+			return CallWindowProc(oldWndProc, hWnd, msg, wParam, lParam);
+		}
+		#endregion
 	}
 }
