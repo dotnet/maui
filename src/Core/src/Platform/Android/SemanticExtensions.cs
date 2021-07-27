31648938debc8438afddadf1f9513b66ce625611
Rachel Kang <rachelkang@microsoft.com>
2021-07-27 11:48:05 -0400

    Introduce SetSemanticFocus API

---
+++ src/Core/src/Platform/Android/SemanticExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Android.Text;
+using Android.Views;
+using Android.Views.Accessibility;
+
+namespace Microsoft.Maui
+{
+	public static partial class SemanticExtensions
+	{
+		public static void SetSemanticFocus(IFrameworkElement element)
+		{
+			var view = element.GetViewForAccessibility();
+
+			view?.SendAccessibilityEvent(EventTypes.ViewAccessibilityFocused);
+		}
+
+		internal static global::Android.Views.View? GetViewForAccessibility(this IFrameworkElement element)
+		{
+			var handler = element?.Handler;
+
+			if (element is Layout)
+				return (View?)handler?.NativeView;
+			else if (handler is ViewGroup vg && vg.ChildCount > 0)
+				return vg.GetChildAt(0);
+			else if (handler != null)
+				return (View?)handler?.NativeView;
+
+			return null;
+		}
+
+		internal static global::Android.Views.View? GetViewForAccessibility(this IFrameworkElement element, global::Android.Views.View renderer)
+		{
+			if (renderer == null)
+				return element?.GetViewForAccessibility();
+
+			if (element is Layout)
+				return renderer;
+			else if (renderer is ViewGroup vg && vg.ChildCount > 0)
+				return vg.GetChildAt(0);
+
+			return renderer;
+		}
+	}
+}
---
+++ src/Core/src/Platform/iOS/SemanticExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Foundation;
+using UIKit;
+
+namespace Microsoft.Maui
+{
+	public static partial class SemanticExtensions
+	{
+		public static void SetSemanticFocus(IFrameworkElement element)
+		{
+			if (element?.Handler?.NativeView is not NSObject nativeView)
+				return;
+
+			UIAccessibility.PostNotification(UIAccessibilityPostNotification.LayoutChanged, nativeView);
+		}
+	}
+}
