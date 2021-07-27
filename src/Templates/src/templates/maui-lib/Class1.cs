066c049560b563ccd4578cafd88080f563e10a15
Matthew Leibowitz <mattleibow@live.com>
2021-07-27 18:48:04 +0200

    Add a Maui Class Library template

---
+++ src/Templates/src/templates/maui-lib/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MauiLib1
+{
+	// All the code in this file is shared between all the platforms
+	// including net6.0 shared library.
+	public class Class1
+	{
+	}
+}
---
+++ src/Templates/src/templates/maui-lib/Platforms/Windows/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MauiLib1
+{
+	// All the code in this file is only included on Windows.
+	public class PlatformClass1
+	{
+	}
+}
