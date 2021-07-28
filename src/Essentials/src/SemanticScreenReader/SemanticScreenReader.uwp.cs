835408d457761c0a7836d0c76a01ae3db17a399d
Shane Neuville <shneuvil@microsoft.com>
2021-07-27 19:01:58 -0500

    Initial implementation for semantic reader on winui

---
+++ src/Essentials/src/SemanticScreenReader/SemanticScreenReader.uwp.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Microsoft.UI.Xaml.Automation.Peers;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.Maui.Essentials
+{
+	public static partial class SemanticScreenReader
+	{
+		static void PlatformAnnounce(string text)
+		{
+			if (Platform.CurrentWindow == null)
+				return;
+
+			var peer = FrameworkElementAutomationPeer.FromElement(Platform.CurrentWindow.Content);
+			peer.RaiseNotificationEvent(
+				AutomationNotificationKind.ActionAborted,
+				AutomationNotificationProcessing.ImportantMostRecent,
+				text,
+				"270FA098-C644-40A2-A0BE-A9BEA1222A1E");
+		}
+	}
+}
