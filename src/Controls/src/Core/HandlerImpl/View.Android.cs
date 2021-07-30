4c9f083d856f8eadc8418e584d3b316345b5bd33
Shane Neuville <shneuvil@microsoft.com>
2021-07-30 10:12:00 -0500

    - add as virtual method

--- src/Controls/src/Core/HandlerImpl/View.Android.cs
+++ src/Controls/src/Core/HandlerImpl/View.Android.cs
@@ -10,6 +10,9 @@ namespace Microsoft.Maui.Controls
 	public partial class View : IAccessibilityNodeInfoListener
 	{
 		void IAccessibilityNodeInfoListener.InitializeAccessibilityNodeInfo(AndroidX.Core.View.Accessibility.AccessibilityNodeInfoCompat? nodeInfo) =>
+			InitializeAccessibilityNodeInfo(nodeInfo);
+
+		protected virtual void InitializeAccessibilityNodeInfo(AndroidX.Core.View.Accessibility.AccessibilityNodeInfoCompat? nodeInfo) =>
 			this.UpdateSemanticNodeInfo(nodeInfo);
 	}
 }
