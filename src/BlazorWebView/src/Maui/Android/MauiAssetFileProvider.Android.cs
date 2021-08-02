8df3087397dbc650b662a71c4b50efb5e500ae7c
Eilon Lipton <Eilon@users.noreply.github.com>
2021-08-02 11:12:39 -0700

    Add missing namespaces

--- src/BlazorWebView/src/Maui/Android/MauiAssetFileProvider.Android.cs
+++ src/BlazorWebView/src/Maui/Android/MauiAssetFileProvider.Android.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.AspNetCore.Components.WebView.Maui
 {
