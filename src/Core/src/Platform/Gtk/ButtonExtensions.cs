ce9aa9ad53482cc18131bb3beeb045611bced475
lytico <lytico@users.sourceforge.net>
2021-08-02 15:24:52 +0200

    Core.csproj Gtk : ButtonHandler.Gtk.cs: implement MapCharacterSpacing

--- src/Core/src/Platform/Gtk/ButtonExtensions.cs
+++ src/Core/src/Platform/Gtk/ButtonExtensions.cs
@@ -2,11 +2,25 @@
 
 namespace Microsoft.Maui
 {
+
 	public static class ButtonExtensions
 	{
+
 		public static void UpdateText(this Button nativeButton, IButton button)
 		{
+			// need to attach Attributes after setting text again, so get it ...
+			var attrs = (nativeButton.Child as Label)?.Attributes;
+
+			// cause maybe a new label is created on assiging text:
 			nativeButton.Label = button.Text;
+
+			if (nativeButton.Child is Label lbl)
+			{
+				// and set it again on the new label:
+				lbl.Attributes = attrs;
+			}
 		}
+
 	}
+
 }
