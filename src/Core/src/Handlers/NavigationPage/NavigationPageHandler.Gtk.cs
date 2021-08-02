2aa29c39184ed62c2f9ff5c84f042fb5b3b06711
lytico <lytico@users.sourceforge.net>
2021-08-02 15:30:46 +0200

    Core.csproj Gtk : move NavigationPageHandler from Controls.Core & adopt changes

---
+++ src/Core/src/Handlers/NavigationPage/NavigationPageHandler.Gtk.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using Microsoft.Maui.Handlers;
+
+namespace Microsoft.Maui.Handlers
+{
+
+	internal partial class NavigationPageHandler : ViewHandler<INavigationView, NavigationView>
+	{
+
+		protected override NavigationView CreateNativeView()
+		{
+			return new();
+		}
+
+		protected override void ConnectHandler(NavigationView nativeView)
+		{
+			base.ConnectHandler(nativeView);
+
+			var virtualView = VirtualView;
+
+		}
+
+		protected override void DisconnectHandler(NavigationView nativeView)
+		{
+			base.DisconnectHandler(nativeView);
+
+			var virtualView = VirtualView;
+
+		}
+		
+		
+		private static void PushAsyncTo(NavigationPageHandler arg1, INavigationView arg2, object? arg3)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void PopAsyncTo(NavigationPageHandler arg1, INavigationView arg2, object? arg3)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+
+}
---
+++ src/Core/src/Platform/Gtk/NavigationView.cs
@@ -0,0 +1,11 @@
+namespace Microsoft.Maui
+{
+
+	public class NavigationView : Gtk.Box
+	{
+
+		public NavigationView() : base(Gtk.Orientation.Horizontal, 0) { }
+
+	}
+
+}
