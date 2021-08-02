793303f516968080197ec84066e6cf5c4f1188e1
lytico <lytico@users.sourceforge.net>
2021-08-02 15:48:08 +0200

    Core.csproj Gtk : WindowExtensions.cs: introduce GetWindow / SetWindow & MauiGtkMainWindow.cs: remove obsolete VisibilityNotifyEvent

--- src/Core/src/Platform/Gtk/WindowExtensions.cs
+++ src/Core/src/Platform/Gtk/WindowExtensions.cs
@@ -1,10 +1,47 @@
+using System;
+
 namespace Microsoft.Maui
 {
 
 	public static class WindowExtensions
 	{
+
 		public static void UpdateTitle(this Gtk.Window nativeWindow, IWindow window) =>
 			nativeWindow.Title = window.Title;
+
+		public static IWindow GetWindow(this Gtk.Window nativeWindow)
+		{
+			foreach (var window in MauiGtkApplication.Current.Application.Windows)
+			{
+				if (window?.Handler?.NativeView is Gtk.Window win && win == nativeWindow)
+					return window;
+			}
+
+			throw new InvalidOperationException("Window Not Found");
+		}
+
+		public static void SetWindow(this Gtk.Window nativeWindow, IWindow window, IMauiContext context)
+		{
+			_ = nativeWindow ?? throw new ArgumentNullException(nameof(nativeWindow));
+			_ = window ?? throw new ArgumentNullException(nameof(window));
+			_ = context ?? throw new ArgumentNullException(nameof(context));
+
+			var handler = window.Handler;
+
+			if (handler == null)
+				handler = context.Handlers.GetHandler(window.GetType());
+
+			if (handler == null)
+				throw new Exception($"Handler not found for window {window}.");
+
+			handler.SetMauiContext(context);
+
+			window.Handler = handler;
+
+			if (handler.VirtualView != window)
+				handler.SetVirtualView(window);
+		}
+
 	}
 
 }
