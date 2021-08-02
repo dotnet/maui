a93c45ddbf6c1c8383b995b576e1081acd766e33
E.Z. Hart <hartez@users.noreply.github.com>
2021-08-02 13:12:03 -0600

    Layout indexer parents (#1898)
    * Make sure that update Layout members using the indexer updates parentage
    
    * Also update LayoutHandler

--- src/Controls/src/Core/Layout/Layout.cs
+++ src/Controls/src/Core/Layout/Layout.cs
@@ -26,7 +26,37 @@ namespace Microsoft.Maui.Controls
 
 		public bool IsReadOnly => ((ICollection<IView>)_children).IsReadOnly;
 
-		public IView this[int index] { get => _children[index]; set => _children[index] = value; }
+		public IView this[int index]
+		{
+			get => _children[index]; 
+			set
+			{
+				var old = _children[index];
+
+				if (old == value)
+				{
+					return;
+				}
+
+				if (old is Element oldElement)
+				{
+					oldElement.Parent = null;
+				}
+
+				LayoutHandler?.Remove(old);
+
+				_children[index] = value;
+
+				if (value is Element newElement)
+				{
+					newElement.Parent = this;
+				}
+
+				LayoutHandler?.Add(value);
+
+				InvalidateMeasure();
+			}
+		}
 
 		public static readonly BindableProperty PaddingProperty = PaddingElement.PaddingProperty;
 
