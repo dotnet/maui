a5d299e6168514da26ae8eb83234d167d612f749
Matthew Leibowitz <mattleibow@live.com>
2021-08-06 19:41:41 +0200

    Use new overload of UIApplication.Main (#1974)

--- src/Templates/src/templates/maui-blazor/MauiApp1/Platforms/iOS/Program.cs
+++ src/Templates/src/templates/maui-blazor/MauiApp1/Platforms/iOS/Program.cs
@@ -9,7 +9,7 @@ namespace MauiApp1
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
-			UIApplication.Main(args, null, "AppDelegate");
+			UIApplication.Main(args, null, typeof(AppDelegate));
 		}
 	}
 }
