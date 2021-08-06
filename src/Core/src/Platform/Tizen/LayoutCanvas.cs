51cfa939aca14df8f9d109756d2a764f117727f8
이승근/Common Platform Lab(SR)/Staff Engineer/삼성전자 <sngn.lee@samsung.com>
2021-08-06 11:21:33 +0900

    Fix nested layout issue (#79)
    Co-authored-by: 허강호/Common Platform Lab(SR)/Principal Engineer/삼성전자 <kangho.hur@samsung.com>

---
+++ src/Core/src/Platform/Tizen/LayoutCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ElmSharp;
+using Tizen.UIExtensions.Common;
+using Tizen.UIExtensions.ElmSharp;
+using TSize = Tizen.UIExtensions.Common.Size;
+using Size = Microsoft.Maui.Graphics.Size;
+using Rectangle = Microsoft.Maui.Graphics.Rectangle;
+
+namespace Microsoft.Maui
+{
+	public class LayoutCanvas : Canvas, IMeasurable
+	{
+		public LayoutCanvas(EvasObject parent) : base(parent) { }
+
+		public TSize Measure(double availableWidth, double availableHeight)
+		{
+			return CrossPlatformMeasure?.Invoke(availableWidth.ToScaledDP(), availableHeight.ToScaledDP()).ToPixel() ?? new TSize(0, 0);
+		}
+
+		internal Func<double, double, Size>? CrossPlatformMeasure { get; set; }
+		internal Func<Rectangle, Size>? CrossPlatformArrange { get; set; }
+	}
+}
