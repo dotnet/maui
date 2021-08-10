166f6535ccd2073ad00ae96a8bd2f504aa75fef1
Rui Marinho <me@ruimarinho.net>
2021-08-10 15:33:20 +0100

    Cleanup

--- src/Controls/src/Core/HandlerImpl/IndicatorView.Impl.cs
+++ src/Controls/src/Core/HandlerImpl/IndicatorView.Impl.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Maui.Controls.Internals;
 using Microsoft.Maui.Graphics;
-using Microsoft.Maui.Layouts;
 
 namespace Microsoft.Maui.Controls
 {
@@ -14,22 +8,7 @@ namespace Microsoft.Maui.Controls
 
 		public Paint PositionIndicatorColor => SelectedIndicatorColor.AsPaint();
 
-		public IShapeView IndicatorShape
-		{
-			get
-			{
-				return IndicatorsShape == Controls.IndicatorShape.Square ? new Shapes.Rectangle() { WidthRequest = IndicatorSize, HeightRequest = IndicatorSize,
-																										} : 
-																		   new Shapes.Ellipse() { WidthRequest = IndicatorSize, HeightRequest = IndicatorSize,
-																			   Fill = Brush.Red,
-																			   Stroke = Brush.Red,
-																			   Aspect = Stretch.Uniform,
-																			   HorizontalOptions = LayoutOptions.Center,
-																			   VerticalOptions = LayoutOptions.Center,
-																		   };
-			}
-		}
-
+		public IShapeView IndicatorShape => IndicatorsShape == Controls.IndicatorShape.Square ? new Shapes.Rectangle() : new Shapes.Ellipse();
 		Maui.ILayout ITemplatedIndicatorView.IndicatorsLayoutOverride => (IndicatorTemplate != null) ? IndicatorLayout as Maui.ILayout : null;
 	}
 }
