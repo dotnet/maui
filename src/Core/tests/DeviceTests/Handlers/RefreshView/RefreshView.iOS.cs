66952cd42ae1c1d057f7b497e3a9f4fffd75cef7
Shane Neuville <shneuvil@microsoft.com>
2021-08-10 17:36:54 -0500

    - iOS Tests

--- src/Core/tests/DeviceTests/Handlers/RefreshView/RefreshView.iOS.cs
+++ src/Core/tests/DeviceTests/Handlers/RefreshView/RefreshView.iOS.cs
@@ -5,6 +5,11 @@ namespace Microsoft.Maui.DeviceTests
 {
 	public partial class RefreshViewHandlerTests
 	{
-		
+		MauiRefreshView GetNativeRefreshView(RefreshViewHandler RefreshViewHandler) =>
+			(MauiRefreshView)RefreshViewHandler.NativeView;
+
+		bool GetNativeIsRefreshing(RefreshViewHandler RefreshViewHandler) =>
+			GetNativeRefreshView(RefreshViewHandler).IsRefreshing;
+
 	}
 }
