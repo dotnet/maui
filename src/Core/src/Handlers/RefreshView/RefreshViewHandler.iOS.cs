35662505130df91775d696fca39eaf040af94174
Shane Neuville <shneuvil@microsoft.com>
2021-08-10 15:04:53 -0500

    RefreshViewHandler Android

---
+++ src/Core/src/Handlers/RefreshView/RefreshViewHandler.iOS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+
+namespace Microsoft.Maui.Handlers
+{
+	public partial class RefreshViewHandler : ViewHandler<IRefreshView, UIView>
+	{
+		protected override UIView CreateNativeView()
+		{
+			return new UIView();
+		}
+
+		public static void MapIsRefreshing(RefreshViewHandler handler, IRefreshView refreshView)
+		{
+		}
+
+		public static void MapContent(RefreshViewHandler handler, IRefreshView refreshView)
+		{
+		}
+
+		public static void MapRefreshColor(RefreshViewHandler handler, IRefreshView refreshView)
+		{
+		}
+
+		public static void MapRefreshViewBackground(RefreshViewHandler handler, IView view)
+		{
+		}
+	}
+}
---
+++ src/Core/tests/DeviceTests/Handlers/RefreshView/RefreshViewTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Maui.DeviceTests.Stubs;
+using Microsoft.Maui.Handlers;
+using Xunit;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	[Category("RefreshViewHandler")]
+	public partial class RefreshViewHandlerTests : HandlerTestBase<RefreshViewHandler, RefreshViewStub>
+	{
+#if __ANDROID__
+		[Theory(DisplayName = "Is Refreshing Initializes Correctly")]
+		[InlineData(false)]
+		[InlineData(true)]
+		public async Task IsRefreshingInitializesCorrectly(bool isRefreshing)
+		{
+			var RefreshView = new RefreshViewStub()
+			{
+				IsRefreshing = isRefreshing,
+			};
+			await ValidatePropertyInitValue(RefreshView, () => isRefreshing, GetNativeIsRefreshing, isRefreshing);
+		}
+#endif
+
+	}
+}
