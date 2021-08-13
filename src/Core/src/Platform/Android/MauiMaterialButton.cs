a954305e9930617c9c1499b07326844660d926c8
Shane Neuville <shneuvil@microsoft.com>
2021-08-12 21:25:34 -0500

    - fix up edge cases and add samples

---
+++ src/Core/src/Platform/Android/MauiMaterialButton.cs
@@ -0,0 +1,51 @@
+﻿using Android.Content;
+using AndroidX.Core.Widget;
+using Google.Android.Material.Button;
+
+namespace Microsoft.Maui.Handlers
+{
+	public class MauiMaterialButton : MaterialButton
+	{
+		// Currently Material doesn't have any bottom gravity options
+		// so we just move the layout to the bottom using
+		// SetCompoundDrawablesRelative during Layout
+		internal const int IconGravityBottom = 9999;
+		bool bottomGravity = false;
+		public MauiMaterialButton(Context context) : base(context)
+		{
+		}
+
+		protected override void OnLayout(bool changed, int left, int top, int right, int bottom)
+		{
+			if (bottomGravity)
+			{
+				var drawable = TextViewCompat.GetCompoundDrawablesRelative(this)[3];
+				drawable?.SetBounds(0, 0, drawable.IntrinsicWidth, drawable.IntrinsicHeight);
+			}
+
+			base.OnLayout(changed, left, top, right, bottom);
+		}
+
+		public override int IconGravity
+		{
+			get
+			{
+				return base.IconGravity;
+			}
+
+			set
+			{
+				if (value == IconGravityBottom)
+				{
+					bottomGravity = true;
+					base.IconGravity = MaterialButton.IconGravityTop;
+				}
+				else
+				{
+					bottomGravity = false;
+					base.IconGravity = value;
+				}
+			}
+		}
+	}
+}
