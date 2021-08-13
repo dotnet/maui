1e3a4d3ca6c1e7ca2d8484fe202c0e649c3f97d7
Shane Neuville <shneuvil@microsoft.com>
2021-08-13 10:03:41 -0500

    - fix up device tests

--- src/Core/src/Platform/Android/ButtonExtensions.cs
+++ src/Core/src/Platform/Android/ButtonExtensions.cs
@@ -1,4 +1,9 @@
+using System;
+using Android.Content;
 using AndroidX.AppCompat.Widget;
+using AndroidX.Core.Widget;
+using Google.Android.Material.Button;
+using Microsoft.Maui.Handlers;
 
 namespace Microsoft.Maui
 {
@@ -27,6 +32,58 @@ namespace Microsoft.Maui
 				(int)padding.Top,
 				(int)padding.Right,
 				(int)padding.Bottom);
+		}
+
+		public static void UpdateContentLayout(this MaterialButton materialButton, IButton button)
+		{
+			var context = materialButton.Context;
+			if (context == null)
+				return;
+
+			var icon = materialButton.Icon ??
+						TextViewCompat.GetCompoundDrawablesRelative(materialButton)[3];
+
+			if (icon != null &&
+				!String.IsNullOrEmpty(button.Text) &&
+				button is IButtonContentLayout cl)
+			{
+				var contentLayout = cl.ContentLayout;
+
+				// IconPadding calls materialButton.CompoundDrawablePadding				
+				// Which is why we don't have to worry about calling setCompoundDrawablePadding
+				// ourselves for our custom implemented IconGravityBottom
+				materialButton.IconPadding = (int)context.ToPixels(contentLayout.Spacing);
+
+				switch (contentLayout.Position)
+				{
+					case ButtonContentLayout.ImagePosition.Top:
+						materialButton.Icon = icon;
+						materialButton.IconGravity = MaterialButton.IconGravityTop;
+						break;
+					case ButtonContentLayout.ImagePosition.Bottom:
+						materialButton.Icon = null;
+						TextViewCompat.SetCompoundDrawablesRelative(materialButton, null, null, null, icon);
+						icon?.SetBounds(0, 0, icon.IntrinsicWidth, icon.IntrinsicHeight);
+						materialButton.IconGravity = MauiMaterialButton.IconGravityBottom;
+						break;
+					case ButtonContentLayout.ImagePosition.Left:
+						materialButton.Icon = icon;
+						materialButton.IconGravity = MaterialButton.IconGravityStart;
+						break;
+					case ButtonContentLayout.ImagePosition.Right:
+						materialButton.Icon = icon;
+						materialButton.IconGravity = MaterialButton.IconGravityEnd;
+						break;
+				}
+			}
+			else
+			{
+				// Don't remove this otherwise the button occasionally measures wrong
+				// on first load
+				materialButton.Icon = icon;
+				materialButton.IconPadding = 0;
+				materialButton.IconGravity = MaterialButton.IconGravityTextStart;
+			}
 		}
 	}
 }
