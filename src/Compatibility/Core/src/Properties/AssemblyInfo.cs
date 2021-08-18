967f2263f42be90b011738abbe529b1ee038cba7
Shane Neuville <shneuvil@microsoft.com>
2021-08-18 12:15:38 -0500

    - fix assembly info

--- src/Compatibility/Core/src/Properties/AssemblyInfo.cs
+++ src/Compatibility/Core/src/Properties/AssemblyInfo.cs
@@ -2,4 +2,3 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Compatibility.Windows.UnitTests")]
 [assembly: InternalsVisibleTo("Compatibility.Android.UnitTests")]
-[assembly: InternalsVisibleTo("Microsoft.Maui.TestUtils.DeviceTests.Runners")]
