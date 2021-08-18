be0bbb1317028a2f4b90f54f07659cefa82c0a25
Matthew Leibowitz <mattleibow@live.com>
2021-08-18 02:50:31 +0200

    Merge the maui projects

--- src/Templates/src/templates/maui-mobile/MainPage.xaml.cs
+++ src/Templates/src/templates/maui-mobile/MainPage.xaml.cs
@@ -1,22 +1,26 @@
-﻿using System;
-using Microsoft.Maui.Controls;
+﻿using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 
 namespace MauiApp1
 {
 	public partial class MainPage : ContentPage
 	{
+		int count = 0;
+
 		public MainPage()
 		{
 			InitializeComponent();
 		}
 
-		int count = 0;
 		private void OnCounterClicked(object sender, EventArgs e)
 		{
 			count++;
 			CounterLabel.Text = $"Current count: {count}";
-			SemanticScreenReader.Announce(CounterLabel.Text);
+
+			if (DeviceInfo.Platform == DevicePlatform.Android || DeviceInfo.Platform == DevicePlatform.iOS)
+			{
+				SemanticScreenReader.Announce(CounterLabel.Text);
+			}
 		}
 	}
 }
