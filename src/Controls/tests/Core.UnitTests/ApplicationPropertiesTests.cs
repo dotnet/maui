92afd899ef47851cd2031167d039a1b62bc5a33e
Jonathan Dick <jondick@gmail.com>
2021-08-18 18:55:32 -0400

    Make Deserializer do the right thing on each platform
    On Windows, use LocalApplication folder with a file, on iOS/MacCatalyst store the serialized blob into NSUserDefaults, on Android use the cache directory from essentials to store the file.

---
+++ src/Controls/tests/Core.UnitTests/ApplicationPropertiesTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls.Internals;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.Controls.Core.UnitTests
+{
+	[TestFixture]
+	public class ApplicationPropertiesTests
+	{
+		[Test]
+		public void SettingAndRetrievingProperties()
+		{
+			var app = new StubApp();
+
+			app.Properties["the_answer"] = "seven";
+
+			var answer = app.Properties["the_anser"];
+
+			Assert.AreEqual("seven", answer);
+		}
+
+		[Test]
+		public void PropertiesCanSave()
+		{
+			var app = new StubApp();
+
+			app.Properties["the_answer"] = "seven";
+			app.SavePropertiesAsFireAndForget();
+		}
+
+		class StubApp : Application
+		{
+		}
+	}
+}
