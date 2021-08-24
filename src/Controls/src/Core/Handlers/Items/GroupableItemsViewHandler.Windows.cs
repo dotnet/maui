a4d5b06aa7224ec7fb80c0b0b7d038486e0afa09
Rui Marinho <me@ruimarinho.net>
2021-08-24 17:00:01 +0100

    Add windows handlers

---
+++ src/Controls/src/Core/Handlers/Items/GroupableItemsViewHandler.Windows.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Maui.Handlers;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.Maui.Controls.Handlers.Items
+{
+	public partial class GroupableItemsViewHandler<TItemsView> : SelectableItemsViewHandler<TItemsView> where TItemsView : GroupableItemsView
+	{
+		protected override UserControl CreateNativeView()
+		{
+			throw new NotImplementedException();
+		}
+		public static void MapIsGrouped(GroupableItemsViewHandler<TItemsView> handler, GroupableItemsView itemsView)
+		{
+		}
+	}
+}
