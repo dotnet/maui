639dc3b75de585655f73dc1c46c7d9fc58d56bf9
Javier Suarez <javiersuarezruiz@hotmail.com>
2021-08-25 14:12:55 +0200

    Added more files

---
+++ src/Core/src/Handlers/BoxView/BoxViewHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.Maui.Handlers
+{
+	public partial class BoxViewHandler
+	{
+		public static IPropertyMapper<IBoxView, BoxViewHandler> BoxViewMapper = new PropertyMapper<IBoxView, BoxViewHandler>(ViewHandler.ViewMapper)
+		{
+			[nameof(IBoxView.Color)] = MapColor,
+			[nameof(IBoxView.CornerRadius)] = MapCornerRadius
+		};
+
+		public BoxViewHandler() : base(BoxViewMapper)
+		{
+
+		}
+
+		public BoxViewHandler(IPropertyMapper mapper) : base(mapper ?? BoxViewMapper)
+		{
+
+		}
+	}
+}
