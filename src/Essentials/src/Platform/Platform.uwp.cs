03ea0d6f4a39477cf72cdb07e1f5d9f1d8383616
Shane Neuville <shneuvil@microsoft.com>
2021-08-25 17:04:28 -0500

    ScreenReader.Announce implementation for WINUI (#2251)

--- src/Essentials/src/Platform/Platform.uwp.cs
+++ src/Essentials/src/Platform/Platform.uwp.cs
@@ -8,13 +8,26 @@ namespace Microsoft.Maui.Essentials
 {
 	public static partial class Platform
 	{
+		internal static Window CurrentWindow 
+		{ 
+			get => _currentWindow ?? Window.Current; 
+			set => _currentWindow = value; 
+		}
+
 		internal const string AppManifestFilename = "AppxManifest.xml";
 		internal const string AppManifestXmlns = "http://schemas.microsoft.com/appx/manifest/foundation/windows10";
 		internal const string AppManifestUapXmlns = "http://schemas.microsoft.com/appx/manifest/uap/windows10";
+		private static Window _currentWindow;
 
 		public static string MapServiceToken { get; set; }
 
 		public static async void OnLaunched(LaunchActivatedEventArgs e)
 			=> await AppActions.OnLaunched(e);
+
+		public static void OnActivated(Window window, WindowActivatedEventArgs args)
+		{
+			if (args.WindowActivationState != WindowActivationState.Deactivated)
+				CurrentWindow = window;
+		}
 	}
 }
