316d8d192047d4444611c62cc75e2e90d8ca0896
Matthew Leibowitz <mattleibow@live.com>
2021-08-26 05:19:49 +0200

    Make sure we have a copy of the envvars
    Works around https://github.com/xamarin/xamarin-macios/issues/12555

--- src/TestUtils/src/DeviceTests.Runners/HeadlessRunner/iOS/MauiTestApplicationDelegate.cs
+++ src/TestUtils/src/DeviceTests.Runners/HeadlessRunner/iOS/MauiTestApplicationDelegate.cs
@@ -6,11 +6,54 @@ using Microsoft.DotNet.XHarness.TestRunners.Common;
 using UIKit;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui.Hosting;
+using Microsoft.DotNet.XHarness.iOS.Shared.Execution;
 
 namespace Microsoft.Maui.TestUtils.DeviceTests.Runners.HeadlessRunner
 {
 	public abstract class MauiTestApplicationDelegate : UIApplicationDelegate
 	{
+		// TODO: https://github.com/xamarin/xamarin-macios/issues/12555
+		readonly static string[] EnvVarNames = {
+			"NUNIT_AUTOSTART",
+			"NUNIT_AUTOEXIT",
+			"NUNIT_ENABLE_NETWORK",
+			"DISABLE_SYSTEM_PERMISSION_TESTS",
+			"NUNIT_HOSTNAME",
+			"NUNIT_TRANSPORT",
+			"NUNIT_LOG_FILE",
+			"NUNIT_HOSTPORT",
+			"USE_TCP_TUNNEL",
+			"RUN_END_TAG",
+			"NUNIT_ENABLE_XML_OUTPUT",
+			"NUNIT_ENABLE_XML_MODE",
+			"NUNIT_XML_VERSION",
+			"NUNIT_SORTNAMES",
+			"NUNIT_RUN_ALL",
+			"NUNIT_SKIPPED_METHODS",
+			"NUNIT_SKIPPED_CLASSES",
+		};
+
+		readonly static Dictionary<string, string?> EnvVars = new();
+
+		static MauiTestApplicationDelegate()
+		{
+			// copy into dictionary for later
+			foreach (var envvar in EnvVarNames)
+			{
+				EnvVars[envvar] = Environment.GetEnvironmentVariable(envvar);
+			}
+		}
+
+		static void SetEnvironmentVariables()
+		{
+			// read from dictionary
+			foreach (var envvar in EnvVars)
+			{
+				Console.WriteLine($"  {envvar.Key} = '{envvar.Value}'");
+				Environment.SetEnvironmentVariable(envvar.Key, envvar.Value);
+			}
+		}
+
 		public static bool IsHeadlessRunner(string[] args)
 		{
 			// usually means this is from xharness
@@ -38,6 +81,8 @@ namespace Microsoft.Maui.TestUtils.DeviceTests.Runners.HeadlessRunner
 		{
 			var mauiApp = CreateMauiApp();
 			Services = mauiApp.Services;
+
+			SetEnvironmentVariables();
 
 			Options = Services.GetRequiredService<TestOptions>();
 			RunnerOptions = Services.GetRequiredService<HeadlessRunnerOptions>();
