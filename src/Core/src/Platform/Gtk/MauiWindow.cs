e435af104dc9cc7a07bb9be079637b7c76d2762b
lytico <lytico@users.sourceforge.net>
2021-08-31 03:20:01 +0200

    Core.csproj Gtk : MauiWindow.cs: remove startup-code (it's a complete senseless class now)

--- src/Core/src/Platform/Gtk/MauiWindow.cs
+++ src/Core/src/Platform/Gtk/MauiWindow.cs
@@ -7,41 +7,14 @@ using Microsoft.Maui.LifecycleEvents;
 
 namespace Microsoft.Maui.Native
 {
-	
+
 	[Obsolete("use MauiGtkApplication")]
-	public class MauiWindow<TStartup> : Window
-		where TStartup : IStartup, new()
+	public class MauiWindow : Window
 	{
+
 		public MauiWindow() : base(WindowType.Toplevel)
-		{
-			var startup = new TStartup();
+		{ }
 
-			var host = startup
-				.CreateAppHostBuilder()
-				.ConfigureServices(ConfigureNativeServices)
-				.ConfigureUsing(startup)
-				.Build();
-
-			Services = host.Services;
-			Application = Services.GetRequiredService<IApplication>();
-
-			var mauiContext = new MauiContext(Services);
-
-			var activationState = new ActivationState(mauiContext);
-			var window = Application.CreateWindow(activationState);
-			var content = window.Content;
-
-			Add(content.ToNative(mauiContext));
-			Child.ShowAll();
-		}
-
-		public new IApplication Application { get; protected set; } = null!;
-
-		public IServiceProvider Services { get; protected set; } = null!;
-
-		// Configure native services like HandlersContext, ImageSourceHandlers etc.. 
-		void ConfigureNativeServices(HostBuilderContext ctx, IServiceCollection services)
-		{
-		}
 	}
+
 }
