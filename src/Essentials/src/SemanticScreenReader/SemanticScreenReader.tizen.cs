92a1bd4ab34531af38cf2aefe8216a795f78f63d
김성수/Common Platform Lab(SR)/Staff Engineer/삼성전자 <sung-su.kim@samsung.com>
2021-09-01 18:27:32 +0900

    Fix Essentials (#146)
    * Fix Essentials sample for tizen
    
    * Add SemanticScreenReader for Tizen
    
    * Fix Startup

---
+++ src/Essentials/src/SemanticScreenReader/SemanticScreenReader.tizen.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Maui.Essentials
+{
+	public static partial class SemanticScreenReader
+	{
+		static void PlatformAnnounce(string text)
+		{
+			_ = ElmSharp.Accessible.AccessibleUtil.Say(text, true);
+		}
+	}
+}
