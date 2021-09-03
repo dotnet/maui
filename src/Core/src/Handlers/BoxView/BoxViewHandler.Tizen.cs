9c7286535d581295307eef3e21a5db725207094b
Kangho Hur <kangho.hur@samsung.com>
2021-09-03 15:50:10 +0900

    [Tizen] Adds BoxView Handler

---
+++ src/Core/src/Handlers/BoxView/BoxViewHandler.Tizen.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace Microsoft.Maui.Handlers
+{
+	public partial class BoxViewHandler : ViewHandler<IBoxView, MauiBoxView>
+	{
+		protected override MauiBoxView CreateNativeView()
+		{
+			return new MauiBoxView(NativeParent!)
+			{
+				Drawable = new BoxViewDrawable(VirtualView)
+			};
+		}
+
+		public static void MapColor(BoxViewHandler handler, IBoxView boxView)
+		{
+			handler.NativeView?.InvalidateBoxView(boxView);
+		}
+
+		public static void MapCornerRadius(BoxViewHandler handler, IBoxView boxView)
+		{
+			handler.NativeView?.InvalidateBoxView(boxView);
+		}
+	}
+}
---
+++ src/Core/src/Platform/Tizen/BoxViewExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace Microsoft.Maui
+{
+	public static class BoxViewExtensions
+	{
+		public static void InvalidateBoxView(this MauiBoxView nativeView, IBoxView boxView)
+		{
+			nativeView.Invalidate();
+		}		
+	}
+}
