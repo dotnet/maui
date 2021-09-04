29b08dfbdcd86d58cf20c37868387e958b3d42b3
Jonathan Dick <jond@hey.com>
2021-09-03 21:12:07 -0400

    Delete unnecessary netstandard extensions

--- src/Core/src/Platform/Standard/ProgressBarExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Microsoft.Maui
-{
-	public static class ProgressBarExtensions
-	{
-		public static void UpdateProgress(this object nothing, IProgress progress) { }
-		public static void UpdateProgressColor(this object nothing, IProgress progress) { }
-	}
-}
