da02ea88b10e51d5a91653c79a9108bf1ba87653
E.Z. Hart <hartez@gmail.com>
2021-09-10 16:51:24 -0600

    Add extension method for modifying an existing mapping. Rewrite prepend and append using modify.

--- src/Core/src/PropertyMapperExtensions.cs
+++ src/Core/src/PropertyMapperExtensions.cs
@@ -4,34 +4,38 @@ namespace Microsoft.Maui
 {
 	public static class PropertyMapperExtensions
 	{
-		public static void AppendToMapping<TVirtualView, TViewHandler>(this IPropertyMapper<TVirtualView, TViewHandler> propertyMapper,
-			string key, Action<TViewHandler, TVirtualView> method)
+		public static void ModifyMapping<TVirtualView, TViewHandler>(this IPropertyMapper<TVirtualView, TViewHandler> propertyMapper,
+			string key, Action<TViewHandler, TVirtualView, Action<IElementHandler, IElement>?> method)
 			where TVirtualView : IElement where TViewHandler : IElementHandler
 		{
 			var previousMethod = propertyMapper.GetProperty(key);
 
 			void newMethod(TViewHandler handler, TVirtualView view)
 			{
-				previousMethod?.Invoke(handler, view);
-				method(handler, view);
+				method(handler, view, previousMethod);
 			}
 
 			propertyMapper.Add(key, newMethod);
+		}
+
+		public static void AppendToMapping<TVirtualView, TViewHandler>(this IPropertyMapper<TVirtualView, TViewHandler> propertyMapper,
+			string key, Action<TViewHandler, TVirtualView> method)
+			where TVirtualView : IElement where TViewHandler : IElementHandler
+		{
+			propertyMapper.ModifyMapping(key, (handler, view, action) => { 
+				action?.Invoke(handler, view);
+				method(handler, view);
+			});
 		}
 
 		public static void PrependToMapping<TVirtualView, TViewHandler>(this IPropertyMapper<TVirtualView, TViewHandler> propertyMapper,
 			string key, Action<TViewHandler, TVirtualView> method)
 			where TVirtualView : IElement where TViewHandler : IElementHandler
 		{
-			var previousMethod = propertyMapper.GetProperty(key);
-
-			void newMethod(TViewHandler handler, TVirtualView view)
-			{
+			propertyMapper.ModifyMapping(key, (handler, view, action) => {
 				method(handler, view);
-				previousMethod?.Invoke(handler, view);
-			}
-
-			propertyMapper.Add(key, newMethod);
+				action?.Invoke(handler, view);
+			});
 		}
 	}
 }
