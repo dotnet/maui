abe2d0567ed39cf886b52d71ece76409a4d75e82
E.Z. Hart <hartez@gmail.com>
2021-09-10 17:06:29 -0600

    Add /// comments

--- src/Core/src/PropertyMapperExtensions.cs
+++ src/Core/src/PropertyMapperExtensions.cs
@@ -4,6 +4,14 @@ namespace Microsoft.Maui
 {
 	public static class PropertyMapperExtensions
 	{
+		/// <summary>
+		/// Modify a property mapping in place.
+		/// </summary>
+		/// <typeparam name="TVirtualView">The cross-platform type.</typeparam>
+		/// <typeparam name="TViewHandler">The handler type.</typeparam>
+		/// <param name="propertyMapper">The property mapper in which to change the mapping.</param>
+		/// <param name="key">The name of the property.</param>
+		/// <param name="method">The modified method to call when the property is updated.</param>
 		public static void ModifyMapping<TVirtualView, TViewHandler>(this IPropertyMapper<TVirtualView, TViewHandler> propertyMapper,
 			string key, Action<TViewHandler, TVirtualView, Action<IElementHandler, IElement>?> method)
 			where TVirtualView : IElement where TViewHandler : IElementHandler
@@ -18,6 +26,14 @@ namespace Microsoft.Maui
 			propertyMapper.Add(key, newMethod);
 		}
 
+		/// <summary>
+		/// Specify a method to be run after an existing property mapping.
+		/// </summary>
+		/// <typeparam name="TVirtualView">The cross-platform type.</typeparam>
+		/// <typeparam name="TViewHandler">The handler type.</typeparam>
+		/// <param name="propertyMapper">The property mapper in which to change the mapping.</param>
+		/// <param name="key">The name of the property.</param>
+		/// <param name="method">The method to call after the existing mapping is finished.</param>
 		public static void AppendToMapping<TVirtualView, TViewHandler>(this IPropertyMapper<TVirtualView, TViewHandler> propertyMapper,
 			string key, Action<TViewHandler, TVirtualView> method)
 			where TVirtualView : IElement where TViewHandler : IElementHandler
@@ -28,6 +44,14 @@ namespace Microsoft.Maui
 			});
 		}
 
+		/// <summary>
+		/// Specify a method to be run before an existing property mapping.
+		/// </summary>
+		/// <typeparam name="TVirtualView">The cross-platform type.</typeparam>
+		/// <typeparam name="TViewHandler">The handler type.</typeparam>
+		/// <param name="propertyMapper">The property mapper in which to change the mapping.</param>
+		/// <param name="key">The name of the property.</param>
+		/// <param name="method">The method to call before the existing mapping begins.</param>
 		public static void PrependToMapping<TVirtualView, TViewHandler>(this IPropertyMapper<TVirtualView, TViewHandler> propertyMapper,
 			string key, Action<TViewHandler, TVirtualView> method)
 			where TVirtualView : IElement where TViewHandler : IElementHandler
