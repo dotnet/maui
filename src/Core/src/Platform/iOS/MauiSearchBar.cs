85b56990c6e2d97a1d96d1bf2decc91e4231f7fb
Shane Neuville <shneuvil@microsoft.com>
2021-09-11 07:55:25 -0500

    [Android] Wire up text for SearchBar and Editor (#2454)
    * Wire up text for SearchBar and Editor
    
    * - searchbar
    
    * - fix searchbar text update
    
    * - convert to MauiSearchBar
    
    * Update EditorHandlerTests.cs
    
    * Update EditorHandlerTests.cs
    
    * Update EntryHandlerTests.cs
    
    * Update SearchBarHandler.iOS.cs
    
    * - fix Editor on iOS
    
    * Update EditorHandler.iOS.cs

---
+++ src/Core/src/Platform/iOS/MauiSearchBar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace Microsoft.Maui.Handlers
+{
+	public class MauiSearchBar : UISearchBar
+	{
+		public MauiSearchBar() : this(RectangleF.Empty)
+		{
+		}
+
+		public MauiSearchBar(NSCoder coder) : base(coder)
+		{
+		}
+
+		public MauiSearchBar(CGRect frame) : base(frame)
+		{
+		}
+
+		protected MauiSearchBar(NSObjectFlag t) : base(t)
+		{
+		}
+
+		protected internal MauiSearchBar(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override string? Text
+		{
+			get => base.Text;
+			set
+			{
+				var old = base.Text;
+
+				base.Text = value;
+
+				if (old != value)
+					TextPropertySet?.Invoke(this, EventArgs.Empty);
+			}
+		}
+
+		public event EventHandler? TextPropertySet;
+	}
+}
--- src/Core/tests/DeviceTests/Stubs/SearchBarStub.cs
+++ src/Core/tests/DeviceTests/Stubs/SearchBarStub.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.Maui.Graphics;
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 
 namespace Microsoft.Maui.DeviceTests.Stubs
 {
-	public partial class SearchBarStub : StubBase, ISearchBar
+	public partial class SearchBarStub : StubBase, ISearchBar, ITextInputStub
 	{
 		string _text;
 
-		public string Text { get => _text; set => SetProperty(ref _text, value); }
+		public string Text 
+		{ 
+			get => _text; 
+			set => SetProperty(ref _text, value, onChanged: OnTextChanged); 
+		}
+
+		public event EventHandler<StubPropertyChangedEventArgs<string>> TextChanged;
+
+		void OnTextChanged(string oldValue, string newValue) =>
+			TextChanged?.Invoke(this, new StubPropertyChangedEventArgs<string>(oldValue, newValue));
 
 		public string Placeholder { get; set; }
 		
