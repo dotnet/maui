26eff8cd035a4f70693c59fc8dfa5c30414d7853
Shane Neuville <shneuvil@microsoft.com>
2021-09-14 11:24:19 -0500

    Remove Local Property

--- src/Core/src/Handlers/Page/PageHandler.iOS.cs
+++ src/Core/src/Handlers/Page/PageHandler.iOS.cs
@@ -5,16 +5,15 @@ namespace Microsoft.Maui.Handlers
 {
 	public partial class PageHandler : ContentViewHandler, INativeViewHandler
 	{
-		PageViewController? _pageViewController;
 		protected override ContentView CreateNativeView()
 		{
 			_ = VirtualView ?? throw new InvalidOperationException($"{nameof(VirtualView)} must be set to create a LayoutView");
 			_ = MauiContext ?? throw new InvalidOperationException($"{nameof(MauiContext)} cannot be null");
 
-			_pageViewController ??= new PageViewController(VirtualView, this.MauiContext);
-			ViewController = _pageViewController;
+			if (ViewController == null)
+				ViewController =  new PageViewController(VirtualView, this.MauiContext);
 
-			if (_pageViewController?.CurrentNativeView is ContentView pv)
+			if (ViewController is PageViewController pc && pc.CurrentNativeView is ContentView pv)
 				return pv;
 
 			throw new InvalidOperationException($"PageViewController.View must be a {nameof(ContentView)}");
