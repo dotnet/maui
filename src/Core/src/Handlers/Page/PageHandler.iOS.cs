4f20f64232df34b49a2202fcac25edea1b452e33
Shane Neuville <shneuvil@microsoft.com>
2021-09-14 10:21:50 -0500

    Make VC Public on PageHandler so it's replaceable (#2460)
    * Add option to specify static factory method for creating native views
    
    * Simplify
    
    * Make VC public so it's replaceable
    
    * - coalesce
    
    * - unsealed
    
    * - move VC to base class
    
    Co-authored-by: E.Z. Hart <hartez@gmail.com>
    Co-authored-by: Rui Marinho <me@ruimarinho.net>

--- src/Core/src/Handlers/Page/PageHandler.iOS.cs
+++ src/Core/src/Handlers/Page/PageHandler.iOS.cs
@@ -1,23 +1,20 @@
 ﻿using System;
-using System.Linq;
 using UIKit;
-using NativeView = UIKit.UIView;
 
 namespace Microsoft.Maui.Handlers
 {
 	public partial class PageHandler : ContentViewHandler, INativeViewHandler
 	{
 		PageViewController? _pageViewController;
-		UIViewController? INativeViewHandler.ViewController => _pageViewController;
-
 		protected override ContentView CreateNativeView()
 		{
 			_ = VirtualView ?? throw new InvalidOperationException($"{nameof(VirtualView)} must be set to create a LayoutView");
 			_ = MauiContext ?? throw new InvalidOperationException($"{nameof(MauiContext)} cannot be null");
 
-			_pageViewController = new PageViewController(VirtualView, this.MauiContext);
+			_pageViewController ??= new PageViewController(VirtualView, this.MauiContext);
+			ViewController = _pageViewController;
 
-			if (_pageViewController.CurrentNativeView is ContentView pv)
+			if (_pageViewController?.CurrentNativeView is ContentView pv)
 				return pv;
 
 			throw new InvalidOperationException($"PageViewController.View must be a {nameof(ContentView)}");
