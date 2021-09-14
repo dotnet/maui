a1d030ac277868cf9c40a69be2e659669c24929a
Jonathan Dick <jondick@gmail.com>
2021-09-14 14:46:07 -0400

    Add project for shared sourcegen util classes/extension methods

---
+++ src/SourceGen/src/Util/TypeInfoParts.cs
@@ -0,0 +1,20 @@
+﻿namespace Microsoft.Maui.SourceGen
+{
+	public class TypeInfoParts
+	{
+		public string[] NamespaceParts { get; set; } = new string[0];
+		public string[] TypeNameParts { get; set; } = new string[0];
+
+		public int TypeArgsCount { get; set; } = 0;
+
+		public string GetNamespace(string separator = ".")
+			=> NamespaceParts is null ? string.Empty : string.Join(separator, NamespaceParts);
+
+		public string GetTypeName(string separator = ".")
+			=> TypeNameParts is null ? string.Empty : string.Join(separator, TypeNameParts);
+
+		public string GetFullyQualifiedName(string nsSeparator = ".", string typeSeparator = ".", bool includeGlobalPrefix = true)
+			=> includeGlobalPrefix ? $"global::{GetNamespace(nsSeparator)}.{GetTypeName(typeSeparator)}"
+			: $"{GetNamespace(nsSeparator)}.{GetTypeName(typeSeparator)}";
+	}
+}
---
+++ src/SourceGen/src/Util/TypeSyntaxExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Microsoft.Maui.SourceGen
+{
+	public static class TypeSyntaxExtensions
+	{
+		public static string GetMethodName(this MethodDeclarationSyntax methodDeclarationSyntax)
+			=> methodDeclarationSyntax.Identifier.Text;
+	}
+}
