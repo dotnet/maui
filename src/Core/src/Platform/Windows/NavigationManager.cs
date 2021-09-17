b2e7e21fe1fc9abbfe90e8ca1cf5f6976c738d8b
Shane Neuville <shneuvil@microsoft.com>
2021-09-17 15:47:23 -0500

    - finish wiring up navigation APIs

--- src/Core/src/Platform/Windows/NavigationManager.cs
+++ src/Core/src/Platform/Windows/NavigationManager.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
 
 namespace Microsoft.Maui
 {
@@ -37,20 +38,75 @@ namespace Microsoft.Maui
 			NavigationView = (INavigationView)navigationView;
 		}
 
-		public virtual void NavigateTo(NavigationRequest arg3)
+		public virtual void NavigateTo(NavigationRequest args)
 		{
 			bool push = true;
-			if (NavigationStack.Count > arg3.NavigationStack.Count)
+			if (NavigationStack.Count > args.NavigationStack.Count)
 				push = false;
 
-			var transition = new SlideNavigationTransitionInfo()
-			{
-				Effect = (!push) ? SlideNavigationTransitionEffect.FromLeft : SlideNavigationTransitionEffect.FromRight
-			};
+			IReadOnlyList<IView> newPageStack = args.NavigationStack;
+			bool animated = args.Animated;
+			var previousNavigationStack = NavigationStack;
+			var previousNavigationStackCount = previousNavigationStack.Count;
+			bool initialNavigation = NavigationStack.Count == 0;
 
-			NavigationStack = arg3.NavigationStack;
+			// User has modified navigation stack but not the currently visible page
+			// So we just sync the elements in the stack
+			if (!initialNavigation &&
+				newPageStack[newPageStack.Count - 1] == 
+				previousNavigationStack[previousNavigationStackCount - 1])
+			{
+				SyncBackStackToNavigationStack(newPageStack);
+				NavigationStack = new List<IView>(newPageStack);
+				NavigationView?.NavigationFinished(NavigationStack);
+				return;
+			}
+
+
+			NavigationTransitionInfo? transition = null;
+
+			if (animated)
+			{
+				transition = new SlideNavigationTransitionInfo()
+				{
+					Effect = (!push) ? SlideNavigationTransitionEffect.FromLeft : SlideNavigationTransitionEffect.FromRight
+				};
+			}
+
+
+			NavigationStack = new List<IView>(newPageStack);
 			_currentPage = NavigationStack[NavigationStack.Count - 1];
-			NavigationFrame.Navigate(typeof(NavigationFramePage), null, transition);
+
+			if (push)
+				NavigationFrame.Navigate(typeof(NavigationFramePage), null, transition);
+			else
+				NavigationFrame.GoBack(transition);
+		}
+
+		void SyncBackStackToNavigationStack(IReadOnlyList<IView> pageStack)
+		{
+			// Back stack depth doesn't count the currently visible page
+			var nativeStackCount = NavigationFrame.BackStackDepth + 1;
+
+			// BackStack entries have no hard relationship with a specific IView
+			// Everytime an entry is about to become visible it just grabs whatever
+			// IView is going to be the visible so all we're doing here is syncing
+			// up the number of things on the stack
+			while (nativeStackCount != pageStack.Count)
+			{
+				if (nativeStackCount > pageStack.Count)
+				{
+					NavigationFrame.BackStack.RemoveAt(0);
+				}
+				else
+				{
+					NavigationFrame.BackStack.Insert(
+						0,
+						new PageStackEntry(typeof(NavigationFramePage), null, null));
+				}
+
+				nativeStackCount = NavigationFrame.BackStackDepth + 1;
+			}
 		}
 
 		// This is used to fire NavigationFinished back to the xplat view
@@ -60,6 +116,9 @@ namespace Microsoft.Maui
 		// I haven't found a way to do that
 		void OnNavigated(object sender, UI.Xaml.Navigation.NavigationEventArgs e)
 		{
+			// If the user has inserted or removed any extra pages
+			SyncBackStackToNavigationStack(NavigationStack);
+
 			if (e.Content is not FrameworkElement fe)
 				return;
 
