c50d1f9b27aa78602219abbb9f672947a227b910
Rui Marinho <me@ruimarinho.net>
2021-09-22 22:38:42 +0100

    [Handlers] Add IndicatorView handler for iOS and Android (#2038)
    * Initial work Indicator
    
    * Make it build
    
    * Try again
    
    * Update to a working version
    
    * Remove commented code
    
    * Move native control measure into OnMeasure override
    
    * Fix IndicatorView measure without changing old method
    
    * Add spacing to new Layout
    
    * Add more examples to IndicatorView
    
    * Fix IndicatorView shapes
    
    * Cleanup
    
    * Update IIndicatorView
    
    * Cleanup
    
    * refactor MauiPageControl
    
    * Fix comments
    
    * Fix sizing
    
    * Add carouselview example cleanup startup page
    
    * Fix extra padding
    
    * Fix net6 sln null
    
    * Fix WINUI indicator
    
    * Use IShape
    
    * Fix namespace
    
    * Add comments to IIndicatorVIew interface
    
    * Add Default size
    
    * [iOS] Some refactor indicatorview iOS
    
    * Remove shapes interfaces
    
    * Use extensions methods for Maximum visible and IsCirceShape
    
    * Use extension method.
    
    * try fix extra sizing code
    
    * Remove comment code
    
    Co-authored-by: E.Z. Hart <hartez@gmail.com>

---
+++ src/Core/src/Handlers/IndicatorView/IndicatorViewHandler.Standard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Microsoft.Maui.Handlers
+{
+	public partial class IndicatorViewHandler : ViewHandler<IIndicatorView, object>
+	{
+		protected override object CreateNativeView() => throw new NotImplementedException();
+
+		public static void MapCount(IndicatorViewHandler handler, IIndicatorView indicator) { }
+		public static void MapPosition(IndicatorViewHandler handler, IIndicatorView indicator) { }
+		public static void MapHideSingle(IndicatorViewHandler handler, IIndicatorView indicator) { }
+		public static void MapMaximumVisible(IndicatorViewHandler handler, IIndicatorView indicator) { }
+		public static void MapIndicatorSize(IndicatorViewHandler handler, IIndicatorView indicator) { }
+		public static void MapIndicatorColor(IndicatorViewHandler handler, IIndicatorView indicator) { }
+		public static void MapSelectedIndicatorColor(IndicatorViewHandler handler, IIndicatorView indicator) { }
+		public static void MapIndicatorShape(IndicatorViewHandler handler, IIndicatorView indicator) { }
+	}
+}
