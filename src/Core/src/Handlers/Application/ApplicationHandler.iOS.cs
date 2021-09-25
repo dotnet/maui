fa3c3fb36f4933f9e6bc71cf37bc61b6f963f908
Matthew Leibowitz <mattleibow@live.com>
2021-09-25 02:34:49 +0200

    MacCat can terminate

--- src/Core/src/Handlers/Application/ApplicationHandler.iOS.cs
+++ src/Core/src/Handlers/Application/ApplicationHandler.iOS.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.InteropServices;
+using Foundation;
 using Microsoft.Extensions.Logging;
 using UIKit;
 
@@ -7,7 +10,39 @@ namespace Microsoft.Maui.Handlers
 	{
 		public static void MapTerminate(ApplicationHandler handler, IApplication application, object? args)
 		{
+#if __MACCATALYST__
+			NSApplication.SharedApplication.Terminate();
+#else
 			handler.Logger?.LogWarning("iOS does not support programatically terminating the app.");
+#endif
 		}
+
+#if __MACCATALYST__
+		class NSApplication
+		{
+			static IntPtr ClassHandle => ObjCRuntime.Class.GetHandle("NSApplication");
+			static IntPtr SharedApplicationSelector => ObjCRuntime.Selector.GetHandle("sharedApplication");
+			static IntPtr TerminateSelector => ObjCRuntime.Selector.GetHandle("terminate:");
+
+			readonly IntPtr _handle;
+
+			NSApplication(IntPtr handle)
+			{
+				_handle = handle;
+			}
+
+			public static NSApplication SharedApplication =>
+				new(IntPtr_objc_msgSend(ClassHandle, SharedApplicationSelector));
+
+			public void Terminate() =>
+				void_objc_msgSend_IntPtr(_handle, TerminateSelector, IntPtr.Zero);
+
+			[DllImport(ObjCRuntime.Constants.ObjectiveCLibrary, EntryPoint = "objc_msgSend")]
+			static extern IntPtr IntPtr_objc_msgSend(IntPtr receiver, IntPtr selector);
+
+			[DllImport(ObjCRuntime.Constants.ObjectiveCLibrary, EntryPoint = "objc_msgSend")]
+			static extern void void_objc_msgSend_IntPtr(IntPtr receiver, IntPtr selector, IntPtr arg1);
+		}
+#endif
 	}
 }
