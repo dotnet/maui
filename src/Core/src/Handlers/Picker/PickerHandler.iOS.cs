575bdbed32224d390ee286035e2a00e21dac87cb
Rui Marinho <me@ruimarinho.net>
2021-09-28 10:03:57 +0100

    [Android,IOS] Update picker TextColor (#2708)
    * [Android] Update picker TextColor
    
    * [iOS] Update Picker text color
    
    * Add PortHandler
    
    * [Android] Add TextColorInitializesCorrectly
    
    * [iOS] Test picker TextColorInitializesCorrectly()
    
    Co-authored-by: Jonathan Dick <jodick@microsoft.com>

--- src/Core/src/Handlers/Picker/PickerHandler.iOS.cs
+++ src/Core/src/Handlers/Picker/PickerHandler.iOS.cs
@@ -126,13 +126,15 @@ namespace Microsoft.Maui.Handlers
 			handler.NativeView?.UpdateHorizontalTextAlignment(picker);
 		}
 
+		public static void MapTextColor(PickerHandler handler, IPicker picker)
+		{
+			handler.NativeView?.UpdateTextColor(picker);		
+		}
+
 		public static void MapVerticalTextAlignment(PickerHandler handler, IPicker picker)
 		{
 			handler.NativeView?.UpdateVerticalTextAlignment(picker);
 		}
-
-		[MissingMapper]
-		public static void MapTextColor(PickerHandler handler, IPicker view) { }
 
 		void OnEnded(object? sender, EventArgs eventArgs)
 		{
