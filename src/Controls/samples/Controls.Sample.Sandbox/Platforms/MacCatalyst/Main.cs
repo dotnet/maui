a104e62c36a8c41118e56cfb089386b748b92284
Matthew Leibowitz <mattleibow@live.com>
2021-10-09 01:19:57 +0200

    :)

--- src/Controls/samples/Controls.Sample.Sandbox/Platforms/MacCatalyst/Main.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/Platforms/MacCatalyst/Main.cs
@@ -1,3 +1,9 @@
-namespace Maui.Controls.Sample.MacCatalyst
+using UIKit;
 
-static void Main(string[] args) => UIApplication.Main(args, null, typeof(AppDelegate));
+namespace Maui.Controls.Sample.MacCatalyst
+{
+	public class Application
+	{
+		static void Main(string[] args) => UIApplication.Main(args, null, typeof(AppDelegate));
+	}
+}
