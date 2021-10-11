3df79d8b490c291ceddd6cb398b959766bb04b99
Shane Neuville <shneuvil@microsoft.com>
2021-10-11 16:51:50 -0500

    - fix layout

--- src/Controls/samples/Controls.Sample/Recipes/Views/RecipeSearchPage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Recipes/Views/RecipeSearchPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Controls;
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
 using Recipes.ViewModels;
 using System.Linq;
 
@@ -15,9 +16,23 @@ namespace Recipes.Views
 
 			_viewModel.PropertyChanged += OnPropertedChanged;
 
-			
-			//vListView.SelectedItemsChanged += RecipeSearchPage_SelectedItemsChanged;
+		}
 
+		private void OnImageHandlerChanged(object sender, System.EventArgs e)
+		{
+#if ANDROID
+			if (sender is IView view)
+			{
+				if (view.Handler?.NativeView is Android.Widget.ImageView aView)
+				{
+
+				}
+				else if(view.Handler?.NativeView is Android.Views.ViewGroup vg)
+				{
+					vg.SetClipChildren(true);
+				}
+			}
+#endif
 		}
 
 		private void RecipeSearchPage_Tapped(object sender, System.EventArgs e)
