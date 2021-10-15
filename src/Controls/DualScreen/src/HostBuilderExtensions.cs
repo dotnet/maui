ea707596ea4b1d5f2f4234420781585d80e99e9d
Craig Dunn <craig.dunn@microsoft.com>
2021-10-15 13:44:26 -0700

    basic hostbuilder test

--- src/Controls/DualScreen/src/HostBuilderExtensions.cs
+++ src/Controls/DualScreen/src/HostBuilderExtensions.cs
@@ -15,21 +15,30 @@ namespace Microsoft.Maui.Controls.DualScreen
 		{
 			//builder.Services.AddSingleton<IHostedService>();
 #if ANDROID
+			var consumer = new Consumer();
+			AndroidX.Window.Java.Layout.WindowInfoRepositoryCallbackAdapter wir = null;
+
 			builder.ConfigureLifecycleEvents(lc =>
 			{
 				lc.AddAndroid(android =>
 				{
 					android.OnConfigurationChanged((activity, configuration) =>
 					{
-
+					
 					})
-					.OnResume((activity) =>
+					.OnStart((activity) =>
 					{
-
+						wir.AddWindowLayoutInfoListener(runOnUiThreadExecutor(), consumer); // `consumer` is the IConsumer implementation
+					})
+					.OnStop((activity) =>
+					{
+						wir.RemoveWindowLayoutInfoListener(consumer);
 					})
 					.OnCreate((activity, bundle) =>
 					{
-
+						wir = new AndroidX.Window.Java.Layout.WindowInfoRepositoryCallbackAdapter(
+						AndroidX.Window.Layout.WindowInfoRepository.Companion.GetOrCreate(
+							activity));
 					});
 				});
 			});
@@ -37,5 +46,34 @@ namespace Microsoft.Maui.Controls.DualScreen
 
 			return builder;
 		}
+
+#if ANDROID
+		#region Used by WindowInfoRepository callback
+		static Java.Util.Concurrent.IExecutor runOnUiThreadExecutor()
+		{
+			return new MyExecutor();
+		}
+		class MyExecutor : Java.Lang.Object, Java.Util.Concurrent.IExecutor
+		{
+			global::Android.OS.Handler handler = new global::Android.OS.Handler(global::Android.OS.Looper.MainLooper);
+			public void Execute(Java.Lang.IRunnable r)
+			{
+				handler.Post(r);
+			}
+		}
+		#endregion
+#endif
 	}
+#if ANDROID
+	public class Consumer : Java.Lang.Object, AndroidX.Core.Util.IConsumer
+	{
+		public void Accept(Java.Lang.Object windowLayoutInfo)
+		{
+			var newLayoutInfo = windowLayoutInfo as AndroidX.Window.Layout.WindowLayoutInfo;
+
+			global::Android.Util.Log.Info("JWM2", "%%% LayoutStateChangeCallback.Accept");
+			global::Android.Util.Log.Info("JWM2", newLayoutInfo.ToString());
+		}
+	}
+#endif
 }
