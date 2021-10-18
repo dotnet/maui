1a469de105c2031155f1cf3298c6b52b11fa8e9a
James Montemagno <james.montemagno@gmail.com>
2021-10-18 16:00:25 -0700

    Sort usings

--- src/Templates/src/templates/maui-mobile/GlobalUsings.cs
+++ src/Templates/src/templates/maui-mobile/GlobalUsings.cs
@@ -1,7 +1,7 @@
 ﻿global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Maui;
 global using Microsoft.Maui.Controls;
-global using Microsoft.Maui.Controls.Xaml;
 global using Microsoft.Maui.Controls.Hosting;
+global using Microsoft.Maui.Controls.Xaml;
 global using Microsoft.Maui.Essentials;
 global using Microsoft.Maui.Hosting;
