26db92b5d7c69dd0bc0674b808ddbcb1bf929b1e
Shane Neuville <shneuvil@microsoft.com>
2021-10-21 16:22:59 -0500

    - cleanup

--- src/Controls/src/Core/Platform/Android/Shell/ShellFragmentContainer.cs
+++ src/Controls/src/Core/Platform/Android/Shell/ShellFragmentContainer.cs
@@ -1,14 +1,8 @@
-using System;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
 using Android.Views;
-using AndroidX.Core.Content;
 using AndroidX.Fragment.App;
 using AView = Android.Views.View;
 using LP = Android.Views.ViewGroup.LayoutParams;
-using AColorRes = Android.Resource.Color;
-using AColor = Android.Graphics.Color;
 
 namespace Microsoft.Maui.Controls.Platform
 {
@@ -16,7 +10,6 @@ namespace Microsoft.Maui.Controls.Platform
 	{
 		Page _page;
 		readonly IMauiContext _mauiContext;
-		AView _view;
 
 		public ShellContent ShellContentTab { get; private set; }
 
@@ -29,7 +22,7 @@ namespace Microsoft.Maui.Controls.Platform
 		public override AView OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
 			_page = ((IShellContentController)ShellContentTab).GetOrCreateContent();
-			_view = _page.ToNative(_mauiContext);
+			_ = _page.ToNative(_mauiContext);
 			return new ShellPageContainer(RequireContext(), (INativeViewHandler)_page.Handler, true)
 			{
 				LayoutParameters = new LP(LP.MatchParent, LP.MatchParent)
@@ -41,8 +34,6 @@ namespace Microsoft.Maui.Controls.Platform
 			base.OnDestroyView();			
 			((IShellContentController)ShellContentTab).RecyclePage(_page);
 			_page = null;
-			_view?.RemoveFromParent();
-			_view = null;
 		}
 
 		public override void OnDestroy()
