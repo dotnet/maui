f6ea0998977f0c2cc8686fc170b056ade00210f9
Matthew Leibowitz <mattleibow@live.com>
2021-10-21 16:52:27 +0200

    Clean up the Application extensions (#3042)
    Co-authored-by: redth <jondick@gmail.com>
    Co-authored-by: Shane Neuville <shneuvil@microsoft.com>

--- src/Core/src/Platform/Windows/MauiWinUIApplication.cs
+++ src/Core/src/Platform/Windows/MauiWinUIApplication.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Hosting;
 using Microsoft.Maui.LifecycleEvents;
+using Microsoft.Maui.Platform;
 
 namespace Microsoft.Maui
 {
@@ -17,7 +19,7 @@ namespace Microsoft.Maui
 
 			var applicationContext = new MauiContext(mauiApp.Services, this);
 
-			Services = mauiApp.Services;
+			Services = applicationContext.Services;
 
 			Services.InvokeLifecycleEvents<WindowsLifecycle.OnLaunching>(del => del(this, args));
 
@@ -25,29 +27,9 @@ namespace Microsoft.Maui
 
 			this.SetApplicationHandler(Application, applicationContext);
 
-			var winuiWndow = CreateNativeWindow(args, applicationContext);
-
-			winuiWndow.Activate();
+			this.CreateNativeWindow(Application, args);
 
 			Services.InvokeLifecycleEvents<WindowsLifecycle.OnLaunched>(del => del(this, args));
-		}
-
-		UI.Xaml.Window CreateNativeWindow(UI.Xaml.LaunchActivatedEventArgs args, IMauiContext applicationContext)
-		{
-			var winuiWndow = new MauiWinUIWindow();
-
-			var mauiContext = applicationContext.MakeScoped(winuiWndow);
-
-			Services.InvokeLifecycleEvents<WindowsLifecycle.OnMauiContextCreated>(del => del(mauiContext));
-
-			var activationState = new ActivationState(mauiContext, args);
-			var window = Application.CreateWindow(activationState);
-
-			winuiWndow.SetWindowHandler(window, mauiContext);
-
-			Services.InvokeLifecycleEvents<WindowsLifecycle.OnWindowCreated>(del => del(winuiWndow));
-
-			return winuiWndow;
 		}
 
 		public static new MauiWinUIApplication Current => (MauiWinUIApplication)UI.Xaml.Application.Current;
