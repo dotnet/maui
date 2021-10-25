3674586a131e1a5c753db5f90d3b862243bc26a7
Kangho Hur <kangho.hur@samsung.com>
2021-10-25 13:22:26 +0900

    [Tizen] Initial CollectionViewHandler

---
+++ src/Controls/src/Core/Handlers/Items/GroupableItemsViewHandler.Tizen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Maui.Handlers;
+
+namespace Microsoft.Maui.Controls.Handlers.Items
+{
+	public partial class GroupableItemsViewHandler<TItemsView> : SelectableItemsViewHandler<TItemsView> where TItemsView : GroupableItemsView
+	{
+		
+		protected override Tizen.UIExtensions.ElmSharp.CollectionView CreateNativeView()
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void MapIsGrouped(GroupableItemsViewHandler<TItemsView> handler, GroupableItemsView itemsView)
+		{
+		}
+	}
+}
---
+++ src/Controls/src/Core/Handlers/Items/StructuredItemsViewHandler.Tizen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Maui.Handlers;
+
+namespace Microsoft.Maui.Controls.Handlers.Items
+{
+	public partial class StructuredItemsViewHandler<TItemsView> : ItemsViewHandler<TItemsView> where TItemsView : StructuredItemsView
+	{
+		
+		protected override Tizen.UIExtensions.ElmSharp.CollectionView CreateNativeView()
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void MapHeaderTemplate(StructuredItemsViewHandler<TItemsView> handler, StructuredItemsView itemsView)
+		{
+
+		}
+
+		public static void MapFooterTemplate(StructuredItemsViewHandler<TItemsView> handler, StructuredItemsView itemsView)
+		{
+
+		}
+
+		public static void MapItemsLayout(StructuredItemsViewHandler<TItemsView> handler, StructuredItemsView itemsView)
+		{
+
+		}
+
+		public static void MapItemSizingStrategy(StructuredItemsViewHandler<TItemsView> handler, StructuredItemsView itemsView)
+		{
+
+		}
+	}
+}
