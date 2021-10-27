ae4fb442ebc4286dad033bfa059ed179b103a026
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-10-27 16:20:47 +0200

    Implement Invalidate method in GraphicsView

--- src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.cs
+++ src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.cs
@@ -6,14 +6,20 @@ namespace Microsoft.Maui.Handlers
 		public static IPropertyMapper<IGraphicsView, GraphicsViewHandler> GraphicsViewMapper = new PropertyMapper<IGraphicsView, GraphicsViewHandler>(ViewHandler.ViewMapper)
 		{
 			[nameof(IGraphicsView.Drawable)] = MapDrawable
+		}; 
+		
+		public static CommandMapper<IGraphicsView, GraphicsViewHandler> GraphicsViewCommandMapper = new(ViewCommandMapper)
+		{
+			[nameof(IGraphicsView.Invalidate)] = MapInvalidate
 		};
 
-		public GraphicsViewHandler() : base(GraphicsViewMapper)
+		public GraphicsViewHandler() : base(GraphicsViewMapper, GraphicsViewCommandMapper)
 		{
 
 		}
 
-		public GraphicsViewHandler(IPropertyMapper? mapper = null) : base(mapper ?? GraphicsViewMapper)
+		public GraphicsViewHandler(IPropertyMapper? mapper = null, CommandMapper? commandMapper = null)
+			: base(mapper ?? GraphicsViewMapper, commandMapper ?? GraphicsViewCommandMapper)
 		{
 
 		}
