93d91ab7b0a17115ec9b20779a03a22b1e5f880d
Jonathan Dick <jodick@microsoft.com>
2021-11-02 10:35:22 -0400

    Update src/Core/src/Core/ITextButton.cs
    Co-authored-by: Eilon Lipton <Eilon@users.noreply.github.com>

--- src/Core/src/Core/ITextButton.cs
+++ src/Core/src/Core/ITextButton.cs
@@ -1,7 +1,7 @@
 ﻿namespace Microsoft.Maui
 {
 	/// <summary>
-	/// functionality related with the Button View text.
+	/// Functionality related with the Button View text.
 	/// </summary>
 	public interface ITextButton : IView, IButton, IText
 	{
