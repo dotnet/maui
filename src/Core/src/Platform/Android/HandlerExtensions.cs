4329c624e118afd8a58b25902ceec4bc301ffc25
Shane Neuville <shneuvil@microsoft.com>
2021-11-02 09:33:33 -0500

    Setup first set of Automation Properties

--- src/Core/src/Platform/Android/HandlerExtensions.cs
+++ src/Core/src/Platform/Android/HandlerExtensions.cs
@@ -51,6 +51,10 @@ namespace Microsoft.Maui
 				view = ir.ReplacedView;
 
 			var handler = view.Handler;
+
+			if (handler?.MauiContext != null && handler.MauiContext != context)
+				handler = null;
+
 			if (handler == null)
 				handler = context.Handlers.GetHandler(view.GetType());
 
