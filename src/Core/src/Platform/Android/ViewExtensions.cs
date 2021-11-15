0b3d1854151f8a26d1905e63a2bd81049115ccc9
Tim Miller <innerlogic4321@gmail.com>
2021-11-15 16:03:42 -0500

    Add Android/ViewExtensions.cs

--- src/Core/src/Platform/Android/ViewExtensions.cs
+++ src/Core/src/Platform/Android/ViewExtensions.cs
@@ -210,5 +210,22 @@ namespace Microsoft.Maui
 
 			((ViewGroup)view.Parent).RemoveView(view);
 		}
+
+		internal static Rectangle GetNativeViewBounds(this IView view)
+		{
+			var nativeView = view.GetNative(true);
+			if (nativeView == null || nativeView.Context == null)
+			{
+				return new Rectangle();
+			}
+
+			var location = new int[2];
+			nativeView.GetLocationOnScreen(location);
+			return new Rectangle(
+				location[0],
+				location[1],
+				nativeView.Context.ToPixels(view.Frame.Width),
+				nativeView.Context.ToPixels(view.Frame.Height));
+		}
 	}
 }
