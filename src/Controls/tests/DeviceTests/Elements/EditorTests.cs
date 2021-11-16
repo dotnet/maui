7c68d6b3be8baaa31edfc415f3f70067d9b05127
Shane Neuville <shneuvil@microsoft.com>
2021-11-16 14:15:26 -0600

    - fix unit tests

--- src/Controls/tests/DeviceTests/Elements/EditorTests.cs
+++ src/Controls/tests/DeviceTests/Elements/EditorTests.cs
@@ -1,28 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Handlers;
+using Xunit;
 
-namespace Microsoft.Maui.DeviceTests.Elements
+namespace Microsoft.Maui.DeviceTests
 {
-	class EditorTests
+	[Category(TestCategory.Editor)]
+	public partial class EditorTests : HandlerTestBase
 	{
-
-
-
+#if !IOS
+		// iOS is broken until this point
+		// https://github.com/dotnet/maui/issues/3425
 		[Theory]
 		[InlineData(EditorAutoSizeOption.Disabled)]
 		[InlineData(EditorAutoSizeOption.TextChanges)]
+#endif
 		public async Task AutoSizeInitializesCorrectly(EditorAutoSizeOption option)
 		{
-			var editor = new EditorStub
+			var editor = new Editor
 			{
 				AutoSize = option,
 				Text = "Test"
 			};
 
-			await ValidatePropertyInitValue(editor, () => editor.AutoSize, GetNativeAutoSize, editor.AutoSize);
+			IView layout = new VerticalStackLayout()
+			{
+				Children =
+				{
+					editor
+				}
+			};
+
+			await InvokeOnMainThreadAsync(() =>
+			{
+				_ = CreateHandler<LayoutHandler>(layout);
+
+				layout.Arrange(new Graphics.Rectangle(Graphics.Point.Zero, layout.Measure(1000, 1000)));
+				var initialHeight = editor.Height;
+
+				editor.Text += Environment.NewLine + " Some new text" + Environment.NewLine;
+				layout.Arrange(new Graphics.Rectangle(Graphics.Point.Zero, layout.Measure(1000, 1000)));
+
+				if (option == EditorAutoSizeOption.Disabled)
+					Assert.Equal(initialHeight, editor.Height);
+				else
+					Assert.True(initialHeight < editor.Height);
+			});
 		}
 	}
 }
