0b76ae6fcdcbc5d2a9cd4d35773115c5a667f210
Kangho Hur <kangho.hur@samsung.com>
2021-11-17 10:14:02 +0900

    [Tizen] Move IDispatcher logic out of IPlatformServices

---
+++ src/Core/src/Dispatching/Dispatcher.Tizen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Microsoft.Maui.Dispatching
+{
+	public partial class Dispatcher : IDispatcher
+	{
+		readonly SynchronizationContext _context;
+
+		internal Dispatcher(SynchronizationContext context)
+		{
+			_context = context;
+		}
+
+		bool IsInvokeRequiredImplementation() =>
+			_context != SynchronizationContext.Current;
+
+		void BeginInvokeOnMainThreadImplementation(Action action) =>
+			_context.Post((o) => action(), null);
+	}
+
+	public partial class DispatcherProvider
+	{
+		static IDispatcher? GetForCurrentThreadImplementation()
+		{
+			var context = SynchronizationContext.Current;
+			if (context == null)
+				return null;
+
+			return new Dispatcher(context);
+		}
+	}
+}
