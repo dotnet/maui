a3645b1a6c83b7a4c14c9bfd89b72dfe278b7ac0
Kangho Hur <kangho.hur@samsung.com>
2021-11-17 10:10:41 +0900

    [Tizen] Add VerticalTextAlignment

--- src/Core/src/Handlers/Editor/EditorHandler.Tizen.cs
+++ src/Core/src/Handlers/Editor/EditorHandler.Tizen.cs
@@ -123,6 +123,11 @@ namespace Microsoft.Maui.Handlers
 			handler.NativeView?.UpdateHorizontalTextAlignment(editor);
 		}
 
+		public static void MapVerticalTextAlignment(EditorHandler handler, IEditor editor)
+		{
+			handler.NativeView?.UpdateVerticalTextAlignment(editor);
+		}
+
 		[MissingMapper]
 		public static void MapCharacterSpacing(IEditorHandler handler, IEditor editor) { }
 
