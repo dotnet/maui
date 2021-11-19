3bc377fdded47055eefba53d3e5256f802f5e986
Matthew Leibowitz <mattleibow@live.com>
2021-11-19 03:49:19 +0200

    Fix test code

--- src/Core/tests/DeviceTests/TestBase.cs
+++ src/Core/tests/DeviceTests/TestBase.cs
@@ -8,15 +8,15 @@ namespace Microsoft.Maui.DeviceTests
 	public partial class TestBase
 	{
 		protected Task<T> InvokeOnMainThreadAsync<T>(Func<T> func) =>
-			TestDispatcher.Current.InvokeAsync(func);
+			TestDispatcher.Current.DispatchAsync(func);
 
 		protected Task InvokeOnMainThreadAsync(Action action) =>
-			TestDispatcher.Current.InvokeAsync(action);
+			TestDispatcher.Current.DispatchAsync(action);
 
 		protected Task InvokeOnMainThreadAsync(Func<Task> action) =>
-			TestDispatcher.Current.InvokeAsync(action);
+			TestDispatcher.Current.DispatchAsync(action);
 
 		public Task<T> InvokeOnMainThreadAsync<T>(Func<Task<T>> func) =>
-			TestDispatcher.Current.InvokeAsync(func);
+			TestDispatcher.Current.DispatchAsync(func);
 	}
 }
