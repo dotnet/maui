4901c5bce9ea923caaa1b49fb84f9415d3f6d976
Matthew Leibowitz <mattleibow@live.com>
2021-11-21 18:02:16 +0200

    This is shared

--- src/Core/src/Platform/Windows/SemanticExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Microsoft.Maui.Platform
-{
-	public static partial class SemanticExtensions
-	{
-		public static void SetSemanticFocus(this IView element) { }
-	}
-}
---
+++ src/Core/src/SemanticExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+#if __IOS__ || MACCATALYST
+using NativeView = UIKit.UIView;
+#elif __ANDROID__
+using NativeView = Android.Views.View;
+#elif WINDOWS
+using NativeView = Microsoft.UI.Xaml.FrameworkElement;
+#elif NETSTANDARD
+using NativeView = System.Object;
+#endif
+
+namespace Microsoft.Maui.Platform
+{
+	public static partial class SemanticExtensions
+	{
+		public static void SetSemanticFocus(this IView element)
+		{
+			if (element?.Handler?.NativeView is not NativeView nativeView)
+				throw new NullReferenceException("Can't access view from a null handler");
+
+#if __ANDROID__
+			nativeView.SendAccessibilityEvent(EventTypes.ViewFocused);
+#elif __IOS__ || MACCATALYST
+			UIAccessibility.PostNotification(UIAccessibilityPostNotification.LayoutChanged, nativeView);
+#endif
+		}
+	}
+}
