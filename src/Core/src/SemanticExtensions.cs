fbb11e1c2ebcbc2a7a6e7f071700281550913147
Matthew Leibowitz <mattleibow@live.com>
2021-11-21 18:07:02 +0200

    this

--- src/Core/src/SemanticExtensions.cs
+++ src/Core/src/SemanticExtensions.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 #if __IOS__ || MACCATALYST
+using UIKit;
 using NativeView = UIKit.UIView;
 #elif __ANDROID__
+using Android.Text;
+using Android.Views;
+using Android.Views.Accessibility;
 using NativeView = Android.Views.View;
 #elif WINDOWS
 using NativeView = Microsoft.UI.Xaml.FrameworkElement;
@@ -9,7 +13,7 @@ using NativeView = Microsoft.UI.Xaml.FrameworkElement;
 using NativeView = System.Object;
 #endif
 
-namespace Microsoft.Maui.Platform
+namespace Microsoft.Maui
 {
 	public static partial class SemanticExtensions
 	{
