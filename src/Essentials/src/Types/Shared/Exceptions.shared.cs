e116605a747d4eddf2d1b9aeef67a6f4adf54c6a
김성수/Common Platform Lab(SR)/Staff Engineer/삼성전자 <sung-su.kim@samsung.com>
2021-11-22 13:11:58 +0900

    Fix catch exception for sample (#262)
    * Fix catch exception for control sample
    
    * Fix exception type for Tizen
    
    * Add bracket

--- src/Essentials/src/Types/Shared/Exceptions.shared.cs
+++ src/Essentials/src/Types/Shared/Exceptions.shared.cs
@@ -4,7 +4,7 @@ namespace Microsoft.Maui.ApplicationModel
 {
 	static class ExceptionUtils
 	{
-#if NETSTANDARD || NET6_0
+#if NETSTANDARD || (NET6_0 && !TIZEN)
 		internal static NotImplementedInReferenceAssemblyException NotSupportedOrImplementedException =>
 			new NotImplementedInReferenceAssemblyException();
 #else
