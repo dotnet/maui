d69e4cd6f110c0153c1c56bbd221ddc44632b2e0
Tim Miller <innerlogic4321@gmail.com>
2021-11-23 13:27:23 -0500

    Add netstandard implementation

--- src/Core/src/Platform/Standard/ViewExtensions.cs
+++ src/Core/src/Platform/Standard/ViewExtensions.cs
@@ -49,5 +49,11 @@ namespace Microsoft.Maui
 		public static void UpdateMinimumWidth(this object nativeView, IView view) { }
 
 		public static void UpdateMaximumWidth(this object nativeView, IView view) { }
+
+		internal static System.Threading.Tasks.Task<byte[]?> RenderAsPNG(this IView view)
+			=> System.Threading.Tasks.Task.FromResult<byte[]?>(null);
+
+	    internal static System.Threading.Tasks.Task<byte[]?> RenderAsJPEG(this IView view)
+			=> System.Threading.Tasks.Task.FromResult<byte[]?>(null);
 	}
 }
