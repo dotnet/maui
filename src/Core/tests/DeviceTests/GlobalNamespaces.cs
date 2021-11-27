40ee773c54d6726a6bd3e7607c88150c2ed87430
Matthew Leibowitz <mattleibow@live.com>
2021-11-28 00:10:36 +0200

    Update GlobalNamespaces.cs

--- src/Core/tests/DeviceTests/GlobalNamespaces.cs
+++ src/Core/tests/DeviceTests/GlobalNamespaces.cs
@@ -1,7 +1,4 @@
 ﻿global using Microsoft.Maui;
-global 
-using Microsoft.Maui.Graphics;
-using Microsoft.Maui.Handlers;
-global 
-using Microsoft.Maui.Platform;
-global 
+global using Microsoft.Maui.Graphics;
+global using Microsoft.Maui.Handlers;
+global using Microsoft.Maui.Platform;
