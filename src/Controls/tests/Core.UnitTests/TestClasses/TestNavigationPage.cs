19f6cd22b71e48c485d58bfd5e2d006f60d517fd
Shane Neuville <shneuvil@microsoft.com>
2021-12-01 17:51:11 -0600

    Don't fire native requests if no handler is present (#3610)
    * Don't fire native requests if no handler is present
    
    * - formatting

--- src/Controls/tests/Core.UnitTests/TestClasses/TestNavigationPage.cs
+++ src/Controls/tests/Core.UnitTests/TestClasses/TestNavigationPage.cs
@@ -10,9 +10,9 @@ namespace Microsoft.Maui.Controls.Core.UnitTests
 {
 	public class TestNavigationPage : NavigationPage
 	{
-		internal TestNavigationPage(bool setforMaui, Page root = null) : base(setforMaui, root)
+		internal TestNavigationPage(bool setforMaui, Page root = null, bool setHandler = true) : base(setforMaui, root)
 		{
-			if (setforMaui)
+			if (setforMaui && setHandler)
 			{
 				base.Handler = new TestNavigationHandler();
 			}
