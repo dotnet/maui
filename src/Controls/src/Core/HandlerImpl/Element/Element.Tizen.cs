97f4d4571035732f2312a97d3228966c03c64e35
Kangho Hur <kangho.hur@samsung.com>
2021-12-03 12:35:42 +0900

    [Tizen] Setup first set of Automation Properties

---
+++ src/Controls/src/Core/HandlerImpl/Element/Element.Tizen.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+
+using Microsoft.Maui.Controls.Platform;
+
+namespace Microsoft.Maui.Controls
+{
+	public partial class Element
+	{
+		public static void MapIsInAccessibleTree(IElementHandler handler, Element element)
+		{
+			//TODO : Need to impl
+		}
+
+		public static void MapExcludedWithChildren(IElementHandler handler, Element element)
+		{
+			//TODO : Need to impl
+		}
+	}
+}
