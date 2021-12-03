2894fc53c8ca380c4bdaa3ef6b6c568ad917efa4
redth <jondick@gmail.com>
2021-12-03 14:26:42 -0500

    Remove somewhat duplicate code

--- src/Controls/src/Core/Platform/iOS/Extensions/FontExtensions.cs
+++ src/Controls/src/Core/Platform/iOS/Extensions/FontExtensions.cs
@@ -12,8 +12,5 @@ namespace Microsoft.Maui.Controls.Platform
 
 		internal static UIFont ToUIFont(this IFontElement self)
 			=> CompatServiceProvider.FontManager.GetFont(self.ToFont());
-
-		internal static bool IsDefault(this IFontElement self)
-			=> self.FontFamily == null && self.FontSize == Device.GetNamedSize(NamedSize.Default, typeof(Label), true) && self.FontAttributes == FontAttributes.None;
 	}
 }
