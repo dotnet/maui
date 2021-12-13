80f3e9b528e89015bbc2535508e422a0577e3aa7
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-12-13 12:50:49 +0100

    Fix build error

--- src/Core/src/Handlers/ImageButton/ImageButtonHandler.cs
+++ src/Core/src/Handlers/ImageButton/ImageButtonHandler.cs
@@ -27,7 +27,7 @@ namespace Microsoft.Maui.Handlers
 		{
 			[nameof(IButtonStroke.StrokeThickness)] = MapStrokeThickness,
 			[nameof(IButtonStroke.StrokeColor)] = MapStrokeColor,
-			[nameof(IButtonStroke.CornerRadius)] = MapCornerRadius
+			[nameof(IButtonStroke.CornerRadius)] = MapCornerRadius,
 #if WINDOWS
 			[nameof(IImageButton.Background)] = MapBackground,
 #endif
