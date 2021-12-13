f4bdce3f878b09ccd6f9f25de4fde9320a954bca
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-12-13 23:00:35 +0100

    Remove some pending TODOs (#3746)

--- src/Controls/src/Core/Platform/AlertManager/AlertManager.Android.cs
+++ src/Controls/src/Core/Platform/AlertManager/AlertManager.Android.cs
@@ -130,9 +130,7 @@ namespace Microsoft.Maui.Controls.Platform
 				if (dialog.Window != null)
 				{
 					if (arguments.FlowDirection == FlowDirection.MatchParent && sender is IView view)
-					{
-						// TODO: Update FlowDirection			
-					}
+						dialog.Window.DecorView.UpdateFlowDirection(view);
 					else if (arguments.FlowDirection == FlowDirection.LeftToRight)
 						dialog.Window.DecorView.LayoutDirection = LayoutDirection.Ltr;
 					else if (arguments.FlowDirection == FlowDirection.RightToLeft)
@@ -177,9 +175,7 @@ namespace Microsoft.Maui.Controls.Platform
 				if (alert.Window != null)
 				{
 					if (arguments.FlowDirection == FlowDirection.MatchParent && sender is IView view)
-					{
-						// TODO: Update FlowDirection
-					}
+						alert.Window.DecorView.UpdateFlowDirection(view);
 					else if (arguments.FlowDirection == FlowDirection.LeftToRight)
 						alert.Window.DecorView.LayoutDirection = LayoutDirection.Ltr;
 					else if (arguments.FlowDirection == FlowDirection.RightToLeft)
