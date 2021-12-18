82e4710b662d2124de5d6397b66624de94449976
Redth <jondick@gmail.com>
2021-12-17 21:47:12 -0500

    Added some text transform tests for formattedstring

--- src/Controls/tests/DeviceTests/Elements/LabelTests.cs
+++ src/Controls/tests/DeviceTests/Elements/LabelTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Handlers;
 using Xunit;
 
@@ -24,6 +25,49 @@ namespace Microsoft.Maui.DeviceTests
 			});
 
 			Assert.Equal(expected, nativeText);
+		}
+
+
+		[Theory]
+		[InlineData("There", TextTransform.None, "There")]
+		[InlineData("There", TextTransform.Uppercase, "THERE")]
+		[InlineData("There", TextTransform.Lowercase, "there")]
+		public async Task FormattedStringSpanTextTransformApplied(string text, TextTransform transform, string expected)
+		{
+			var formattedString = new FormattedString();
+			formattedString.Spans.Add(new Span { Text = "Hello" });
+			formattedString.Spans.Add(new Span { Text = text, TextTransform = transform });
+
+			var label = new Label { FormattedText = formattedString };
+
+			var handler = await CreateHandlerAsync<LabelHandler>(label);
+
+			var nativeText = await InvokeOnMainThreadAsync(() =>
+			{
+				return handler.NativeView.AttributedText?.Value;
+			});
+
+			Assert.Equal("Hello" + expected, nativeText);
+		}
+
+		[Fact]
+		public async Task FormattedStringSpanTextTransformOverridesLabelTextTransform()
+		{
+			var formattedString = new FormattedString();
+			formattedString.Spans.Add(new Span { Text = "HELLO" });
+			formattedString.Spans.Add(new Span { Text = "WORLD", TextTransform = TextTransform.None });
+			formattedString.Spans.Add(new Span { Text = "new", TextTransform = TextTransform.Uppercase });
+
+			var label = new Label { FormattedText = formattedString, TextTransform = TextTransform.Lowercase };
+
+			var handler = await CreateHandlerAsync<LabelHandler>(label);
+
+			var nativeText = await InvokeOnMainThreadAsync(() =>
+			{
+				return handler.NativeView.AttributedText?.Value;
+			});
+
+			Assert.Equal("helloWORLDNEW", nativeText);
 		}
 	}
 }
