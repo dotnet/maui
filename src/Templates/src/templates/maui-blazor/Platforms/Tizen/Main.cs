395a3479b90146527daa5dddd19d8e149079211b
조중현/Common Platform Lab(SR)/Engineer/삼성전자 <jh5.cho@samsung.com>
2021-12-20 17:25:42 +0900

    [Templates] Update Tizen Template (#311)

--- src/Templates/src/templates/maui-blazor/Platforms/Tizen/Main.cs
+++ src/Templates/src/templates/maui-blazor/Platforms/Tizen/Main.cs
@@ -2,16 +2,15 @@ using System;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 
-namespace MauiApp._1
-{
-	class Program : MauiApplication
-	{
-		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+namespace MauiApp._1;
 
-		static void Main(string[] args)
-		{
-			var app = new Program();
-			app.Run(args);
-		}
+class Program : MauiApplication
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	static void Main(string[] args)
+	{
+		var app = new Program();
+		app.Run(args);
 	}
 }
