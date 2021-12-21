86eda254a15b4e166af95f07323f8746d88c1b70
Redth <jondick@gmail.com>
2021-12-21 12:18:12 -0500

    Add a test for FormattedString platform conversion

---
+++ src/Controls/tests/DeviceTests/Elements/FormattedStringTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Controls.Platform;
+using Xunit;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	[Category(TestCategory.Label)]
+	public partial class FormattedStringTests : HandlerTestBase
+	{
+		[Fact]
+		public async Task NativeFormattedStringContainsSpan()
+		{
+			var formattedString = new FormattedString();
+			formattedString.Spans.Add(new Span { Text = "HELLO" });
+			formattedString.Spans.Add(new Span { Text = "WORLD" });
+
+			var fontManager = formattedString.GetFontManager();
+
+			var plainText = formattedString.ToString();
+			var actual = await InvokeOnMainThreadAsync(() =>
+			{
+				var result = string.Empty;
+#if __IOS__
+				var attributed = formattedString.ToNSAttributedString(fontManager);
+
+				attributed.EnumerateAttributes(new Foundation.NSRange(0, attributed.Length),
+					 Foundation.NSAttributedStringEnumeration.None, new Foundation.NSAttributedRangeCallback((Foundation.NSDictionary dict, Foundation.NSRange range, ref bool flag) =>
+					 {
+						 result += attributed.Substring(range.Location, range.Length)?.Value;
+					 }));
+#elif __ANDROID__
+				var spannable = formattedString.ToSpannableString(fontManager);
+
+				result = spannable.ToString();
+#elif WINDOWS
+				var runs = formattedString.ToRuns();
+
+				foreach (var r in runs)
+					result += r.Text;
+#endif
+				return result;
+			});
+
+			Assert.Equal("HELLOWORLD", actual);
+		}
+	}
+}
