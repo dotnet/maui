cad00f8c4cdf80d156e4d51cc85f69008ca6ebf2
허강호/Common Platform Lab(SR)/Principal Engineer/삼성전자 <kangho.hur@samsung.com>
2022-01-12 11:43:41 +0900

    [Tizen] Add ImageButtonHandler (#334)

--- src/Core/src/Handlers/ImageButton/ImageButtonHandler.Tizen.cs
+++ src/Core/src/Handlers/ImageButton/ImageButtonHandler.Tizen.cs
@@ -3,13 +3,47 @@ using Tizen.UIExtensions.NUI;
 
 namespace Microsoft.Maui.Handlers
 {
-	//TODO: Need to impl
-	public partial class ImageButtonHandler : ViewHandler<IImageButton, Image>
+	public partial class ImageButtonHandler : ViewHandler<IImageButton, MauiImageButton>
 	{
-		protected override Image CreatePlatformView() => throw new NotImplementedException();
-		
-		void OnSetImageSource(Image? obj)
+		protected override MauiImageButton CreatePlatformView()
 		{
+			return new MauiImageButton();
+		}
+
+		protected override void ConnectHandler(MauiImageButton nativeView)
+		{
+			nativeView.Clicked += OnClicked;
+			nativeView.Pressed += OnPressed;
+			nativeView.Released += OnReleased;
+			base.ConnectHandler(nativeView);
+		}
+
+		protected override void DisconnectHandler(MauiImageButton nativeView)
+		{
+			nativeView.Clicked -= OnClicked;
+			nativeView.Pressed -= OnPressed;
+			nativeView.Released -= OnReleased;
+			base.DisconnectHandler(nativeView);
+		}
+
+		void OnReleased(object? sender, EventArgs e)
+		{
+			VirtualView?.Released();
+		}
+
+		void OnPressed(object? sender, EventArgs e)
+		{
+			VirtualView?.Pressed();
+		}
+
+		void OnClicked(object? sender, EventArgs e)
+		{
+			VirtualView?.Clicked();
+		}
+
+		void OnSetImageSource(Image? img)
+		{
+			//Empty on purpose
 		}
 	}
 }
---
+++ src/Core/src/Platform/Tizen/MauiImageButton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Tizen.NUI;
+using Tizen.UIExtensions.Common;
+using TImage = Tizen.UIExtensions.NUI.Image;
+
+namespace Microsoft.Maui.Platform
+{
+	public class MauiImageButton : TImage
+	{
+		bool _isPressed;
+
+		public event EventHandler? Pressed;
+		public event EventHandler? Released;
+		public event EventHandler? Clicked;
+
+		public MauiImageButton()
+		{
+			TouchEvent += OnTouched;
+		}
+
+		bool OnTouched(object source, TouchEventArgs e)
+		{
+			var state = e.Touch.GetState(0);
+
+			if (state == PointStateType.Down)
+			{
+				_isPressed = true;
+				Pressed?.Invoke(this, EventArgs.Empty);
+				return true;
+			}
+			else if (state == PointStateType.Up)
+			{
+				Released?.Invoke(this, EventArgs.Empty);
+				if (_isPressed)
+				{
+					Clicked?.Invoke(this, EventArgs.Empty);
+				}
+				_isPressed = false;
+				return true;
+			}
+			_isPressed = false;
+			return false;
+		}
+	}
+}
