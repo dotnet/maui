bf718ad3455acfe10bb557a428c797784a8c3e10
Kangho Hur <kangho.hur@samsung.com>
2022-01-14 08:16:27 +0900

    [Tizen] Adds RadioButton Handler

---
+++ src/Core/src/Handlers/RadioButton/RadioButtonHandler.Tizen.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ElmSharp;
+
+namespace Microsoft.Maui.Handlers
+{
+	public partial class RadioButtonHandler : ViewHandler<IRadioButton, MauiRadioButton>
+	{
+		protected override MauiRadioButton CreateNativeView()
+		{
+			_ = NativeParent ?? throw new ArgumentNullException(nameof(NativeParent));
+
+			return new MauiRadioButton(NativeParent)
+			{
+				StateValue = 1
+			};
+		}
+
+		protected override void ConnectHandler(MauiRadioButton nativeView)
+		{
+			NativeView.ValueChanged += OnValueChanged;
+			base.ConnectHandler(nativeView);
+		}
+
+		protected override void DisconnectHandler(MauiRadioButton nativeView)
+		{
+			NativeView.ValueChanged -= OnValueChanged;
+			base.DisconnectHandler(nativeView);
+		}
+
+		public static void MapIsChecked(RadioButtonHandler handler, IRadioButton radioButton)
+		{
+			handler.NativeView?.UpdateIsChecked(radioButton);
+		}
+
+		[MissingMapper]
+		public static void MapContent(RadioButtonHandler handler, IRadioButton radioButton) { }
+
+		public static void MapTextColor(RadioButtonHandler handler, ITextStyle textStyle)
+		{
+			handler.NativeView?.UpdateTextColor(textStyle);
+		}
+
+		[MissingMapper]
+		public static void MapCharacterSpacing(RadioButtonHandler handler, ITextStyle textStyle) { }
+
+		[MissingMapper]
+		public static void MapFont(RadioButtonHandler handler, ITextStyle textStyle) { }
+
+		void OnValueChanged(object? sender, EventArgs e)
+		{
+			VirtualView.IsChecked = NativeView.GroupValue == 1 ? true : false;
+		}
+	}
+}
---
+++ src/Core/src/Platform/Tizen/RadioButtonExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Maui
+{
+	public static class RadioButtonExtensions
+	{
+		public static void UpdateIsChecked(this MauiRadioButton nativeRadioButton, IRadioButton radioButton)
+		{
+			nativeRadioButton.GroupValue = radioButton.IsChecked ? 1 : 0;
+		}
+
+		public static void UpdateTextColor(this MauiRadioButton nativeRadioButton, ITextStyle radioButton)
+		{
+			nativeRadioButton.TextColor = radioButton.TextColor.ToNative();
+		}
+	}
+}
