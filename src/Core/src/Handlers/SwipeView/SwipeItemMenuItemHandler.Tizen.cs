30622a16048da011ddc03435f61252dd4930712d
Kangho Hur <kangho.hur@samsung.com>
2022-01-19 11:52:02 +0900

    [Tizen] Bump to latest

---
+++ src/Core/src/Handlers/SwipeView/SwipeItemMenuItemHandler.Tizen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ElmSharp;
+
+namespace Microsoft.Maui.Handlers
+{
+	//TODO : Need to implement
+	public partial class SwipeItemMenuItemHandler : ElementHandler<ISwipeItemMenuItem, EvasObject>
+	{
+		protected override EvasObject CreateNativeElement()
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void MapTextColor(SwipeItemMenuItemHandler handler, ITextStyle view) { }
+
+		public static void MapCharacterSpacing(SwipeItemMenuItemHandler handler, ITextStyle view) { }
+
+		public static void MapFont(SwipeItemMenuItemHandler handler, ITextStyle view) { }
+
+		public static void MapText(SwipeItemMenuItemHandler handler, ISwipeItemMenuItem view) { }
+
+		public static void MapBackground(SwipeItemMenuItemHandler handler, ISwipeItemMenuItem view) { }
+
+		public static void MapVisibility(SwipeItemMenuItemHandler handler, ISwipeItemMenuItem view) { }
+
+		void OnSetImageSource(EvasObject? obj)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
