ea2ddaf4865d9778f45e51b4fa8f644d4ee65099
Matthew Leibowitz <mattleibow@live.com>
2022-01-21 17:14:48 +0200

    public

--- src/Graphics/src/Microsoft.Maui.Graphics/CanvasDefaults.cs
+++ src/Graphics/src/Microsoft.Maui.Graphics/CanvasDefaults.cs
@@ -1,6 +1,6 @@
 namespace Microsoft.Maui.Graphics
 {
-	internal static class CanvasDefaults
+	public static class CanvasDefaults
 	{
 		public static readonly Color DefaultShadowColor = new Color(0f, 0f, 0f, .5f);
 		public static readonly SizeF DefaultShadowOffset = new SizeF(5, 5);
