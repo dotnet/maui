92563d6ee05d19c5e3f7ed35122c15a2040cc531
Javier Suárez <javiersuarezruiz@hotmail.com>
2022-01-25 20:25:18 +0100

    Add pending Core interface comments (#4289)

--- src/Core/src/Core/ISwipeItemView.cs
+++ src/Core/src/Core/ISwipeItemView.cs
@@ -1,5 +1,8 @@
 ﻿namespace Microsoft.Maui
 {
+	/// <summary>
+	/// Represents a custom individual command in a SwipeView.
+	/// </summary>
 	public interface ISwipeItemView : IContentView, ISwipeItem
 	{
 	}
