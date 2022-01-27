bc3e956122abd5d8d5492625e7afa6db2bf6a7a3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-01-27 13:54:14 +0100

    Fix wrong format

--- src/Core/src/Core/Extensions/IPickerExtension.cs
+++ src/Core/src/Core/Extensions/IPickerExtension.cs
@@ -5,8 +5,8 @@ namespace Microsoft.Maui
 {
 	public static class IPickerExtension
 	{
-		public static string[] GetItemsAsArray(this IPicker picker) 
-			=> Enumerable.Range(0, picker.GetCount()).Select(i => picker.GetItem(i)).ToArray(); 
+		public static string[] GetItemsAsArray(this IPicker picker)
+			=> Enumerable.Range(0, picker.GetCount()).Select(i => picker.GetItem(i)).ToArray();
 
 		public static List<string> GetItemsAsList(this IPicker picker)
 			=> Enumerable.Range(0, picker.GetCount()).Select(i => picker.GetItem(i)).ToList();
