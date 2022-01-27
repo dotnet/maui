4fa24d5ff633b5767aecbf355b18b5e5e6d8269f
Eilon Lipton <Eilon@users.noreply.github.com>
2022-01-27 13:34:00 -0800

    Update MauiAssetsExtensions.iOS.cs

--- src/Core/src/MauiAssetsExtensions.iOS.cs
+++ src/Core/src/MauiAssetsExtensions.iOS.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Foundation;
 
 namespace Microsoft.Maui
 {
 	public static partial class MauiAssetsExtensions
 	{
-		public static Task<Stream?> OpenMauiAsset(this IMauiContext mauiContext!!, string assetPath!!) => throw new NotImplementedException();
+		public static Task<Stream?> OpenMauiAsset(this IMauiContext mauiContext!!, string assetPath!!)
+		{
+			var iOSResourcePath = Path.Combine(NSBundle.MainBundle.ResourcePath, assetPath.Replace('\\', '/'));
+
+			try
+			{
+				return Task.FromResult<Stream?>(File.OpenRead(iOSResourcePath));
+			}
+			catch
+			{
+				return Task.FromResult<Stream?>(null);
+			}
+		}
 	}
 }
