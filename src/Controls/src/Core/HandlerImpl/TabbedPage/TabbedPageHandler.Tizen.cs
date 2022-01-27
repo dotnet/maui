b4a66d3ac62e92c5e8f6b1420d91a4839bce11d3
Kangho Hur <kangho.hur@samsung.com>
2022-01-27 17:31:00 +0900

    [Tizen] Implement IPlatformApplication.Current

---
+++ src/Controls/src/Core/HandlerImpl/TabbedPage/TabbedPageHandler.Tizen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Maui.Handlers;
+using ElmSharp;
+
+namespace Microsoft.Maui.Controls.Handlers
+{
+	public partial class TabbedPageHandler : ViewHandler<TabbedPage, EvasObject>
+	{
+		protected override EvasObject CreateNativeView()
+		{
+			// TODO : Need to impl
+			throw new NotImplementedException();
+		}
+
+		public static void MapBarBackground(TabbedPageHandler handler, TabbedPage view)
+		{
+		}
+		public static void MapBarBackgroundColor(TabbedPageHandler handler, TabbedPage view)
+		{
+		}
+		public static void MapBarTextColor(TabbedPageHandler handler, TabbedPage view)
+		{
+		}
+		public static void MapUnselectedTabColor(TabbedPageHandler handler, TabbedPage view)
+		{
+		}
+		public static void MapSelectedTabColor(TabbedPageHandler handler, TabbedPage view)
+		{
+		}
+
+		public static void MapItemsSource(TabbedPageHandler handler, TabbedPage view)
+		{
+		}
+		public static void MapItemTemplate(TabbedPageHandler handler, TabbedPage view)
+		{
+		}
+		public static void MapSelectedItem(TabbedPageHandler handler, TabbedPage view)
+		{
+		}
+		public static void MapCurrentPage(TabbedPageHandler handler, TabbedPage view)
+		{
+
+		}
+	}
+}
