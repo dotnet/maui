9e46f20af9f2302b15c0baaa0f998a3dd3c10c39
Shane Neuville <shneuvil@microsoft.com>
2022-01-27 20:55:29 -0600

    WinUI TabbedPage Tests/Cleanup/BarBackground (#4360)
    * TabbedPage Handler Fixes
    
    * - Update TabBar Background Color
    
    * - fix up TabbedPage Loading
    
    # Conflicts:
    #	src/Controls/src/Core/Handlers/TabbedPage/TabbedPageHandler.Windows.cs
    #	src/Core/src/Platform/Windows/FrameworkElementExtensions.cs
    #	src/Core/src/Platform/Windows/NavigationRootManager.cs
    #	src/Core/src/Platform/Windows/NavigationViewExtensions.cs
    
    * - fix up sample
    
    * - fixup UI Tests
    
    * - add additional NavigationView tests
    
    * - add BarBackground tests
    
    * - fix loading when nested inside flyout page

--- src/Controls/tests/DeviceTests/HandlerTestBase.cs
+++ src/Controls/tests/DeviceTests/HandlerTestBase.cs
@@ -6,26 +6,63 @@ using Microsoft.Maui.DeviceTests.Stubs;
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Essentials;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace Microsoft.Maui.DeviceTests
 {
 	public partial class HandlerTestBase : TestBase, IDisposable
 	{
+		bool _isCreated;
 		MauiApp _mauiApp;
+		IMauiContext _mauiContext;
 
-		public HandlerTestBase()
+		public void EnsureHandlerCreated(Action<MauiAppBuilder> additionalCreationActions = null)
 		{
-			_mauiApp = MauiApp
+			if (_isCreated)
+			{
+				return;
+			}
+
+			_isCreated = true;
+			var appBuilder = MauiApp
 				.CreateBuilder()
 				.RemapForControls()
+				.ConfigureLifecycleEvents(lifecycle =>
+				{
+#if __IOS__
+					lifecycle
+						.AddiOS(iOS => iOS
+							.OpenUrl((app, url, options) =>
+								Microsoft.Maui.Essentials.Platform.OpenUrl(app, url, options))
+							.ContinueUserActivity((application, userActivity, completionHandler) =>
+								Microsoft.Maui.Essentials.Platform.ContinueUserActivity(application, userActivity, completionHandler))
+							.PerformActionForShortcutItem((application, shortcutItem, completionHandler) =>
+								Microsoft.Maui.Essentials.Platform.PerformActionForShortcutItem(application, shortcutItem, completionHandler)));
+#elif WINDOWS
+					lifecycle
+						.AddWindows(windows =>
+						{
+							windows
+								.OnLaunched((app, e) =>
+									Microsoft.Maui.Essentials.Platform.OnLaunched(e));
+							windows
+								.OnActivated((window, e) =>
+									Microsoft.Maui.Essentials.Platform.OnActivated(window, e));
+						});
+#endif
+				})
 				.ConfigureMauiHandlers(handlers =>
 				{
 					handlers.AddHandler(typeof(Editor), typeof(EditorHandler));
 					handlers.AddHandler(typeof(VerticalStackLayout), typeof(LayoutHandler));
-				})
-				.Build();
+				});
 
-			MauiContext = new ContextStub(_mauiApp.Services);
+			additionalCreationActions?.Invoke(appBuilder);
+
+			_mauiApp = appBuilder.Build();
+
+			_mauiContext = new ContextStub(_mauiApp.Services);
 		}
 
 		public void Dispose()
@@ -33,31 +70,47 @@ namespace Microsoft.Maui.DeviceTests
 			((IDisposable)_mauiApp).Dispose();
 
 			_mauiApp = null;
-			MauiContext = null;
+			_mauiContext = null;
 		}
 
-		protected IMauiContext MauiContext { get; private set; }
+		protected IMauiContext MauiContext
+		{
+			get
+			{
+				EnsureHandlerCreated();
+				return _mauiContext;
+			}
+		}
 
-		protected THandler CreateHandler<THandler>(IView view)
-			where THandler : IViewHandler
+		protected THandler CreateHandler<THandler>(IElement view)
+			where THandler : IElementHandler
+		{
+			return CreateHandler<THandler>(view, MauiContext);
+		}
+
+		protected THandler CreateHandler<THandler>(IElement element, IMauiContext mauiContext)
+			where THandler : IElementHandler
 		{
 			var handler = Activator.CreateInstance<THandler>();
-			handler.SetMauiContext(MauiContext);
+			handler.SetMauiContext(mauiContext);
 
-			handler.SetVirtualView(view);
-			view.Handler = handler;
+			handler.SetVirtualView(element);
+			element.Handler = handler;
 
-			view.Arrange(new Rectangle(0, 0, view.Width, view.Height));
-			handler.NativeArrange(view.Frame);
+			if (element is IView view && handler is IViewHandler viewHandler)
+			{
+				view.Arrange(new Rectangle(0, 0, view.Width, view.Height));
+				viewHandler.NativeArrange(view.Frame);
+			}
 
 			return handler;
 		}
 
-		protected async Task<THandler> CreateHandlerAsync<THandler>(IView view) where THandler : IViewHandler =>
+		protected async Task<THandler> CreateHandlerAsync<THandler>(IElement view) where THandler : IElementHandler =>
 			await InvokeOnMainThreadAsync(() => CreateHandler<THandler>(view));
 
-		protected Task<TValue> GetValueAsync<TValue, THandler>(IView view, Func<THandler, TValue> func)
-			 where THandler : IViewHandler
+		protected Task<TValue> GetValueAsync<TValue, THandler>(IElement view, Func<THandler, TValue> func)
+			 where THandler : IElementHandler
 		{
 			return InvokeOnMainThreadAsync(() =>
 			{
--- src/Controls/tests/DeviceTests/TestCategory.cs
+++ src/Controls/tests/DeviceTests/TestCategory.cs
@@ -5,8 +5,11 @@
 		public const string Button = "Button";
 		public const string Dispatcher = "Dispatcher";
 		public const string Editor = "Editor";
+		public const string FlyoutPage = "FlyoutPage";
 		public const string Gesture = "Gesture";
 		public const string Label = "Label";
+		public const string NavigationPage = "NavigationPage";
+		public const string TabbedPage = "TabbedPage";
 		public const string VisualElement = "VisualElement";
 	}
 }
