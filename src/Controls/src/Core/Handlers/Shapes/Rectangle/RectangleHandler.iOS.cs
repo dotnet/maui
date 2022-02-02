b829815daf7a06d4a0cb4343b017fb2f088c31d5
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-02-02 14:59:41 +0100

    Created specific shapes handlers

---
+++ src/Controls/src/Core/Handlers/Shapes/Rectangle/RectangleHandler.iOS.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace Microsoft.Maui.Controls.Handlers
+{
+	public partial class RectangleHandler : ViewHandler<Rectangle, MauiShapeView>
+	{
+		protected override MauiShapeView CreateNativeView() =>
+			new MauiShapeView();
+
+		public static void MapRadiusX(RectangleHandler handler, Rectangle rectangle)
+		{
+			handler.NativeView?.InvalidateShape(rectangle);
+		}
+
+		public static void MapRadiusY(RectangleHandler handler, Rectangle rectangle)
+		{
+			handler.NativeView?.InvalidateShape(rectangle);
+		}
+	}
+}
