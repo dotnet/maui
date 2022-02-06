97d455372db6cd0e96181c57edfdae3f752775e0
Matthew Leibowitz <mattleibow@live.com>
2022-02-06 05:48:40 +0200

    Refactor DeviceInfo to use an interface (#4465)

--- src/Essentials/src/DeviceInfo/DeviceInfo.uwp.cs
+++ src/Essentials/src/DeviceInfo/DeviceInfo.uwp.cs
@@ -4,16 +4,16 @@ using Windows.Security.ExchangeActiveSyncProvisioning;
 using Windows.System.Profile;
 using Windows.UI.ViewManagement;
 
-namespace Microsoft.Maui.Essentials
+namespace Microsoft.Maui.Essentials.Implementations
 {
-	public static partial class DeviceInfo
+	public class DeviceInfoImplementation : IDeviceInfo
 	{
-		static readonly EasClientDeviceInformation deviceInfo;
-		static DeviceIdiom currentIdiom;
-		static DeviceType currentType = DeviceType.Unknown;
-		static string systemProductName;
+		readonly EasClientDeviceInformation deviceInfo;
+		DeviceIdiom currentIdiom;
+		DeviceType currentType = DeviceType.Unknown;
+		string systemProductName;
 
-		static DeviceInfo()
+		public DeviceInfoImplementation()
 		{
 			deviceInfo = new EasClientDeviceInformation();
 			currentIdiom = DeviceIdiom.Unknown;
@@ -27,85 +27,96 @@ namespace Microsoft.Maui.Essentials
 			}
 		}
 
-		static string GetModel() => deviceInfo.SystemProductName;
+		public string Model => deviceInfo.SystemProductName;
 
-		static string GetManufacturer() => deviceInfo.SystemManufacturer;
+		public string Manufacturer => deviceInfo.SystemManufacturer;
 
-		static string GetDeviceName() => deviceInfo.FriendlyName;
+		public string Name => deviceInfo.FriendlyName;
 
-		static string GetVersionString()
+		public string VersionString
 		{
-			var version = AnalyticsInfo.VersionInfo.DeviceFamilyVersion;
-
-			if (ulong.TryParse(version, out var v))
+			get
 			{
-				var v1 = (v & 0xFFFF000000000000L) >> 48;
-				var v2 = (v & 0x0000FFFF00000000L) >> 32;
-				var v3 = (v & 0x00000000FFFF0000L) >> 16;
-				var v4 = v & 0x000000000000FFFFL;
-				return $"{v1}.{v2}.{v3}.{v4}";
-			}
+				var version = AnalyticsInfo.VersionInfo.DeviceFamilyVersion;
 
-			return version;
+				if (ulong.TryParse(version, out var v))
+				{
+					var v1 = (v & 0xFFFF000000000000L) >> 48;
+					var v2 = (v & 0x0000FFFF00000000L) >> 32;
+					var v3 = (v & 0x00000000FFFF0000L) >> 16;
+					var v4 = v & 0x000000000000FFFFL;
+					return $"{v1}.{v2}.{v3}.{v4}";
+				}
+
+				return version;
+			}
 		}
 
-		static DevicePlatform GetPlatform() => DevicePlatform.UWP;
+		public Version Version => Utils.ParseVersion(VersionString);
 
-		static DeviceIdiom GetIdiom()
+		public DevicePlatform Platform => DevicePlatform.UWP;
+
+		public DeviceIdiom Idiom
 		{
-			switch (AnalyticsInfo.VersionInfo.DeviceFamily)
+			get
 			{
-				case "Windows.Mobile":
-					currentIdiom = DeviceIdiom.Phone;
-					break;
-				case "Windows.Universal":
-				case "Windows.Desktop":
-					{
-						try
+				switch (AnalyticsInfo.VersionInfo.DeviceFamily)
+				{
+					case "Windows.Mobile":
+						currentIdiom = DeviceIdiom.Phone;
+						break;
+					case "Windows.Universal":
+					case "Windows.Desktop":
 						{
-							var currentHandle = Essentials.Platform.CurrentWindowHandle;
-							var settings = UIViewSettingsInterop.GetForWindow(currentHandle);
-							var uiMode = settings.UserInteractionMode;
-							currentIdiom = uiMode == UserInteractionMode.Mouse ? DeviceIdiom.Desktop : DeviceIdiom.Tablet;
+							try
+							{
+								var currentHandle = Essentials.Platform.CurrentWindowHandle;
+								var settings = UIViewSettingsInterop.GetForWindow(currentHandle);
+								var uiMode = settings.UserInteractionMode;
+								currentIdiom = uiMode == UserInteractionMode.Mouse ? DeviceIdiom.Desktop : DeviceIdiom.Tablet;
+							}
+							catch (Exception ex)
+							{
+								Debug.WriteLine($"Unable to get device . {ex.Message}");
+							}
 						}
-						catch (Exception ex)
-						{
-							Debug.WriteLine($"Unable to get device . {ex.Message}");
-						}
-					}
-					break;
-				case "Windows.Xbox":
-				case "Windows.Team":
-					currentIdiom = DeviceIdiom.TV;
-					break;
-				case "Windows.IoT":
-				default:
-					currentIdiom = DeviceIdiom.Unknown;
-					break;
-			}
+						break;
+					case "Windows.Xbox":
+					case "Windows.Team":
+						currentIdiom = DeviceIdiom.TV;
+						break;
+					case "Windows.IoT":
+					default:
+						currentIdiom = DeviceIdiom.Unknown;
+						break;
+				}
 
-			return currentIdiom;
+				return currentIdiom;
+			}
 		}
 
-		static DeviceType GetDeviceType()
+		public DeviceType DeviceType
 		{
-			if (currentType != DeviceType.Unknown)
+			get
+			{
+				if (currentType != DeviceType.Unknown)
+					return currentType;
+
+				try
+				{
+					if (string.IsNullOrWhiteSpace(systemProductName))
+						systemProductName = deviceInfo.SystemProductName;
+
+					var isVirtual = systemProductName.Contains("Virtual") || systemProductName == "HMV domU";
+
+					currentType = isVirtual ? DeviceType.Virtual : DeviceType.Physical;
+				}
+				catch (Exception ex)
+				{
+					Debug.WriteLine($"Unable to get device type. {ex.Message}");
+				}
 				return currentType;
-
-			try
-			{
-				if (string.IsNullOrWhiteSpace(systemProductName))
-					systemProductName = deviceInfo.SystemProductName;
-
-				var isVirtual = systemProductName.Contains("Virtual") || systemProductName == "HMV domU";
-
-				currentType = isVirtual ? DeviceType.Virtual : DeviceType.Physical;
 			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine($"Unable to get device type. {ex.Message}");
-			}
-			return currentType;
 		}
 	}
 }
