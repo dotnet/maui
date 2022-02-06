fe81e5ab4362ec99183791a00a63a263af4fbaf4
James Clancey <james.clancey@gmail.com>
2022-02-06 06:53:05 -0900

    Flyout should always update the details when it changes. (#4519)
    * Always update the details Fragment
    
    If only the Details change, then the details never updates when it's a flyout.
    
    * - added unit test
    
    Co-authored-by: Shane Neuville <shneuvil@microsoft.com>

---
+++ src/Controls/tests/DeviceTests/Elements/FlyoutPage/FlyoutPageTests.Android.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Android.OS;
+using AView = Android.Views.View;
+using Microsoft.Maui.Handlers;
+using Xunit;
+using ATextAlignment = Android.Views.TextAlignment;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Platform;
+using AndroidX.DrawerLayout.Widget;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	public partial class FlyoutPageTests
+	{
+		DrawerLayout FindPlatformFlyoutView(AView aView) =>
+			aView.GetParentOfType<DrawerLayout>();
+	}
+}
---
+++ src/Controls/tests/DeviceTests/Stubs/WindowHandlerStub.Android.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Maui.Platform;
+using Microsoft.Maui.Handlers;
+using AActivity = Android.App.Activity;
+using AViewGroup = Android.Views.ViewGroup;
+using AView = Android.Views.View;
+using AndroidX.AppCompat.Widget;
+using System.Threading.Tasks;
+using Android.Views;
+using Android.OS;
+using AndroidX.AppCompat.App;
+using AndroidX.Fragment.App;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	public class WindowHandlerStub : ElementHandler<IWindow, AActivity>, IWindowHandler
+	{
+		public static IPropertyMapper<IWindow, WindowHandlerStub> WindowMapper = new PropertyMapper<IWindow, WindowHandlerStub>(WindowHandler.Mapper)
+		{
+			[nameof(IWindow.Content)] = MapContent
+		};
+
+		void UpdateContent()
+		{
+			_ = MauiContext ?? throw new InvalidOperationException($"{nameof(MauiContext)} should have been set by base class.");
+
+			var rootManager = MauiContext.GetNavigationRootManager();
+			rootManager.Connect(VirtualView.Content);
+		}
+		public static void MapContent(WindowHandlerStub handler, IWindow window)
+		{
+			handler.UpdateContent();
+		}
+
+		protected override void DisconnectHandler(AActivity nativeView)
+		{
+			base.DisconnectHandler(nativeView);
+			var windowManager = MauiContext.GetNavigationRootManager();
+			windowManager.Disconnect();
+		}
+
+
+
+		public WindowHandlerStub()
+			: base(WindowMapper)
+		{
+		}
+
+		protected override AActivity CreateNativeElement()
+		{
+			return MauiProgram.CurrentContext.GetActivity();
+		}
+	}
+}
