b419fc60ba91f34d1968031fe58f5c64f9b6c350
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-02-09 13:44:55 +0530

    refactored preference into an interface

--- src/Essentials/src/Preferences/Preferences.shared.cs
+++ src/Essentials/src/Preferences/Preferences.shared.cs
@@ -1,9 +1,20 @@
 using System;
+using Microsoft.Maui.Essentials.Implementations;
+using System.ComponentModel;
 
 namespace Microsoft.Maui.Essentials
 {
+	public interface IPreferences
+	{
+		bool ContainsKey(string key, string sharedName);
+		void Remove(string key, string sharedName);
+		void Clear(string sharedName);
+		void Set<T>(string key, T value, string sharedName);
+		T Get<T>(string key, T defaultValue, string sharedName);
+	}
+
 	/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="Type[@FullName='Microsoft.Maui.Essentials.Preferences']/Docs" />
-	public static partial class Preferences
+	public static class Preferences
 	{
 		internal static string GetPrivatePreferencesSharedName(string feature) =>
 			$"{AppInfo.PackageName}.microsoft.maui.essentials.{feature}";
@@ -74,63 +85,63 @@ namespace Microsoft.Maui.Essentials
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='ContainsKey'][1]/Docs" />
 		public static bool ContainsKey(string key, string sharedName) =>
-			PlatformContainsKey(key, sharedName);
+			Current.ContainsKey(key, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Remove'][1]/Docs" />
 		public static void Remove(string key, string sharedName) =>
-			PlatformRemove(key, sharedName);
+			Current.Remove(key, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Clear'][1]/Docs" />
 		public static void Clear(string sharedName) =>
-			PlatformClear(sharedName);
+			Current.Clear(sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Get'][13]/Docs" />
 		public static string Get(string key, string defaultValue, string sharedName) =>
-			PlatformGet<string>(key, defaultValue, sharedName);
+			Current.Get<string>(key, defaultValue, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Get'][7]/Docs" />
 		public static bool Get(string key, bool defaultValue, string sharedName) =>
-			PlatformGet<bool>(key, defaultValue, sharedName);
+			Current.Get<bool>(key, defaultValue, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Get'][10]/Docs" />
 		public static int Get(string key, int defaultValue, string sharedName) =>
-			PlatformGet<int>(key, defaultValue, sharedName);
+			Current.Get<int>(key, defaultValue, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Get'][9]/Docs" />
 		public static double Get(string key, double defaultValue, string sharedName) =>
-			PlatformGet<double>(key, defaultValue, sharedName);
+			Current.Get<double>(key, defaultValue, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Get'][12]/Docs" />
 		public static float Get(string key, float defaultValue, string sharedName) =>
-			PlatformGet<float>(key, defaultValue, sharedName);
+			Current.Get<float>(key, defaultValue, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Get'][11]/Docs" />
 		public static long Get(string key, long defaultValue, string sharedName) =>
-			PlatformGet<long>(key, defaultValue, sharedName);
+			Current.Get<long>(key, defaultValue, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Set'][13]/Docs" />
 		public static void Set(string key, string value, string sharedName) =>
-			PlatformSet<string>(key, value, sharedName);
+			Current.Set<string>(key, value, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Set'][7]/Docs" />
 		public static void Set(string key, bool value, string sharedName) =>
-			PlatformSet<bool>(key, value, sharedName);
+			Current.Set<bool>(key, value, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Set'][10]/Docs" />
 		public static void Set(string key, int value, string sharedName) =>
-			PlatformSet<int>(key, value, sharedName);
+			Current.Set<int>(key, value, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Set'][9]/Docs" />
 		public static void Set(string key, double value, string sharedName) =>
-			PlatformSet<double>(key, value, sharedName);
+			Current.Set<double>(key, value, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Set'][12]/Docs" />
 		public static void Set(string key, float value, string sharedName) =>
-			PlatformSet<float>(key, value, sharedName);
+			Current.Set<float>(key, value, sharedName);
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Set'][11]/Docs" />
 		public static void Set(string key, long value, string sharedName) =>
-			PlatformSet<long>(key, value, sharedName);
+			Current.Set<long>(key, value, sharedName);
 
 		// DateTime
 
@@ -144,10 +155,20 @@ namespace Microsoft.Maui.Essentials
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Get'][8]/Docs" />
 		public static DateTime Get(string key, DateTime defaultValue, string sharedName) =>
-			DateTime.FromBinary(PlatformGet<long>(key, defaultValue.ToBinary(), sharedName));
+			DateTime.FromBinary(Current.Get<long>(key, defaultValue.ToBinary(), sharedName));
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Preferences.xml" path="//Member[@MemberName='Set'][8]/Docs" />
 		public static void Set(string key, DateTime value, string sharedName) =>
-			PlatformSet<long>(key, value.ToBinary(), sharedName);
+			Current.Set<long>(key, value.ToBinary(), sharedName);
+
+		static IPreferences currentImplementation;
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static IPreferences Current =>
+			currentImplementation ??= new PreferencesImplementation();
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static void SetCurrent(IPreferences implementation) =>
+			currentImplementation = implementation;
 	}
 }
