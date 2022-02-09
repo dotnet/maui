7e7c152d5590e72170d5f7db8e12d804dd308e8c
Matthew Leibowitz <mattleibow@live.com>
2022-02-09 16:39:07 +0700

    ns

--- src/Compatibility/ControlGallery/src/Core/Startup.cs
+++ src/Compatibility/ControlGallery/src/Core/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls.Compatibility;
 using Microsoft.Maui.Controls.Hosting;
