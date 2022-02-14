234fc1277b8f4ff17583d22b0b54a24a1bde043d
Rachel Kang <rachelkang@microsoft.com>
2022-02-14 18:23:45 -0500

    Add missing parenthesis

--- src/Core/src/Handlers/View/ViewHandler.Android.cs
+++ src/Core/src/Handlers/View/ViewHandler.Android.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Maui.Handlers
 {
 	public partial class ViewHandler
 	{
-		partial void DisconnectingHandler(PlatformView platformView
+		partial void DisconnectingHandler(PlatformView platformView)
 		{
 			if (platformView.IsAlive()
 				&& ViewCompat.GetAccessibilityDelegate(platformView) is MauiAccessibilityDelegateCompat ad)
