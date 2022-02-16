1d55f4a0ad0f8241992292ee296b83ea990c4f87
moljac <mcvjetko@holisticware.net>
2022-02-16 16:22:32 +0100

    UWP and Tizen ToLIst() fix (using System.Linq;)

--- src/Essentials/src/Email/Email.tizen.cs
+++ src/Essentials/src/Email/Email.tizen.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Tizen.Applications;
 
--- src/Essentials/src/Email/Email.uwp.cs
+++ src/Essentials/src/Email/Email.uwp.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Email;
 using Windows.Foundation.Metadata;
