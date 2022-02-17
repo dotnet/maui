4ebc266393a0035b0418ea6805245bc362e40dc0
Tim Miller <drasticactions@users.noreply.github.com>
2022-02-17 14:01:44 -0500

    [WinUI] Use XamlRoot for GetViewTransform (#4689)
    Using the Parent element isn't correct for getting the correct view transform values.

--- src/Core/src/Platform/Windows/ViewExtensions.cs
+++ src/Core/src/Platform/Windows/ViewExtensions.cs
@@ -287,10 +287,10 @@ namespace Microsoft.Maui.Platform
 
 		internal static Matrix4x4 GetViewTransform(this FrameworkElement element)
 		{
-			var root = element?.Parent as UIElement;
+			var root = element?.XamlRoot;
 			if (root == null)
 				return new Matrix4x4();
-			var offset = element?.TransformToVisual(root) as MatrixTransform;
+			var offset = element?.TransformToVisual(root.Content) as MatrixTransform;
 			if (offset == null)
 				return new Matrix4x4();
 			Matrix matrix = offset.Matrix;
