c64cdfb77ccd01fd1d8270b5da9ee7d4586b0b01
redth <jondick@gmail.com>
2022-02-17 09:58:37 -0500

    Fixes for winui

--- src/Essentials/src/AppActions/AppActions.uwp.cs
+++ src/Essentials/src/AppActions/AppActions.uwp.cs
@@ -11,19 +11,13 @@ using Windows.ApplicationModel.Activation;
 using Microsoft.UI.Xaml;
 #endif
 
-namespace Microsoft.Maui.Essentials.Implementations
+namespace Microsoft.Maui.Essentials
 {
-	public class AppActionsImplementation : IAppActions
+	public static partial class AppActions
 	{
-		public string Type => "XE_APP_ACTION_TYPE";
-		const string appActionPrefix = "XE_APP_ACTIONS-";
-
 		public static string IconDirectory { get; set; } = "";
 
 		public static string IconExtension { get; set; } = "png";
-
-		public bool IsSupported
-		   => true;
 
 		internal static async Task OnLaunched(LaunchActivatedEventArgs e)
 		{
@@ -37,20 +31,31 @@ namespace Microsoft.Maui.Essentials.Implementations
 			}
 #endif
 
-			if (args?.StartsWith(appActionPrefix) ?? false)
+			if (args?.StartsWith(Implementations.AppActionsExtensions.AppActionPrefix) ?? false)
 			{
-				var id = ArgumentsToId(args);
+				var id = Implementations.AppActionsExtensions.ArgumentsToId(args);
 
 				if (!string.IsNullOrEmpty(id))
 				{
-					var actions = await PlatformGetAsync();
+					var actions = await AppActions.GetAsync();
 					var appAction = actions.FirstOrDefault(a => a.Id == id);
 
 					if (appAction != null)
-						AppActions.InvokeOnAppAction(null, appAction);
+						InvokeOnAppAction(null, appAction);
 				}
 			}
 		}
+	}
+}
+
+namespace Microsoft.Maui.Essentials.Implementations
+{
+	public class AppActionsImplementation : IAppActions
+	{
+		public string Type => "XE_APP_ACTION_TYPE";
+
+		public bool IsSupported
+		   => true;
 
 		public async Task<IEnumerable<AppAction>> GetAsync()
 		{
@@ -87,25 +92,26 @@ namespace Microsoft.Maui.Essentials.Implementations
 		{	
 			return SetAsync(actions.AsEnumerable<AppAction>());
 		}
-
-
-		static string ArgumentsToId(string arguments)
-		{
-			if (arguments?.StartsWith(appActionPrefix) ?? false)
-				return Encoding.Default.GetString(Convert.FromBase64String(arguments.Substring(appActionPrefix.Length)));
-
-			return default;
-		}
 	}
 
 	internal static class AppActionsExtensions
 	{
-		static AppAction ToAction(this JumpListItem item)
+		internal const string AppActionPrefix = "XE_APP_ACTIONS-";
+
+		internal static string ArgumentsToId(this string arguments)
+		{
+			if (arguments?.StartsWith(AppActionPrefix) ?? false)
+				return Encoding.Default.GetString(Convert.FromBase64String(arguments.Substring(AppActionPrefix.Length)));
+
+			return default;
+		}
+
+		internal static AppAction ToAction(this JumpListItem item)
 			=> new AppAction(ArgumentsToId(item.Arguments), item.DisplayName, item.Description);
 
-		static JumpListItem ToJumpListItem(this AppAction action)
+		internal static JumpListItem ToJumpListItem(this AppAction action)
 		{
-			var id = appActionPrefix + Convert.ToBase64String(Encoding.Default.GetBytes(action.Id));
+			var id = AppActionPrefix + Convert.ToBase64String(Encoding.Default.GetBytes(action.Id));
 			var item = JumpListItem.CreateWithArguments(id, action.Title);
 
 			if (!string.IsNullOrEmpty(action.Subtitle))
@@ -113,11 +119,11 @@ namespace Microsoft.Maui.Essentials.Implementations
 
 			if (!string.IsNullOrEmpty(action.Icon))
 			{
-				var dir = IconDirectory.Trim('/', '\\').Replace('\\', '/');
+				var dir = AppActions.IconDirectory.Trim('/', '\\').Replace('\\', '/');
 				if (!string.IsNullOrEmpty(dir))
 					dir += "/";
 
-				var ext = IconExtension;
+				var ext = AppActions.IconExtension;
 				if (!string.IsNullOrEmpty(ext) && !ext.StartsWith("."))
 					ext = "." + ext;
 
