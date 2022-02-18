2a494d133cbe161790419f21c6d3a2ad27f3b3e8
moljac <mcvjetko@holisticware.net>
2022-02-18 23:55:57 +0100

    Vibration refactored to interface and singleton (#4727)

--- src/Essentials/src/Vibration/Vibration.shared.cs
+++ src/Essentials/src/Vibration/Vibration.shared.cs
@@ -1,22 +1,38 @@
 using System;
+using System.ComponentModel;
+using Microsoft.Maui.Essentials;
+using Microsoft.Maui.Essentials.Implementations;
 
 namespace Microsoft.Maui.Essentials
 {
+	public interface IVibration
+	{
+		bool IsSupported { get; }
+
+		void Vibrate();
+
+		void Vibrate(double duration);
+
+		void Vibrate(TimeSpan duration);
+
+		void Cancel();
+	}
+
 	/// <include file="../../docs/Microsoft.Maui.Essentials/Vibration.xml" path="Type[@FullName='Microsoft.Maui.Essentials.Vibration']/Docs" />
 	public static partial class Vibration
 	{
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Vibration.xml" path="//Member[@MemberName='Vibrate'][0]/Docs" />
 		public static void Vibrate()
-			=> Vibrate(TimeSpan.FromMilliseconds(500));
+			=> Current.Vibrate(TimeSpan.FromMilliseconds(500));
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Vibration.xml" path="//Member[@MemberName='Vibrate'][1]/Docs" />
 		public static void Vibrate(double duration)
-			=> Vibrate(TimeSpan.FromMilliseconds(duration));
+			=> Current.Vibrate(TimeSpan.FromMilliseconds(duration));
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Vibration.xml" path="//Member[@MemberName='Vibrate'][2]/Docs" />
 		public static void Vibrate(TimeSpan duration)
 		{
-			if (!IsSupported)
+			if (!Current.IsSupported)
 				throw new FeatureNotSupportedException();
 
 			if (duration.TotalMilliseconds < 0)
@@ -24,16 +40,30 @@ namespace Microsoft.Maui.Essentials
 			else if (duration.TotalSeconds > 5)
 				duration = TimeSpan.FromSeconds(5);
 
-			PlatformVibrate(duration);
+			Current.Vibrate(duration);
 		}
 
 		/// <include file="../../docs/Microsoft.Maui.Essentials/Vibration.xml" path="//Member[@MemberName='Cancel']/Docs" />
 		public static void Cancel()
 		{
-			if (!IsSupported)
+			if (!Current.IsSupported)
 				throw new FeatureNotSupportedException();
 
-			PlatformCancel();
+			Current.Cancel();
 		}
+
+#nullable enable
+		static IVibration? currentImplementation;
+#nullable disable
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static IVibration Current =>
+			currentImplementation ??= new VibrationImplementation();
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+#nullable enable
+		public static void SetCurrent(IVibration? implementation) =>
+			currentImplementation = implementation;
+#nullable disable
 	}
 }
