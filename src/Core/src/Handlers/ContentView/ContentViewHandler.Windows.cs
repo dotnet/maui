930baa21062c6800b63bcce26af843bc789efb04
Anton Firszov <antonfir@gmail.com>
2022-02-18 20:31:03 +0100

    Interfaced handlers for ContentView, DatePicker, Editor (#4763)
    * refactor ContentViewHandler
    
    * refactor DatePicker
    
    * refactor EditorHandler

--- src/Core/src/Handlers/ContentView/ContentViewHandler.Windows.cs
+++ src/Core/src/Handlers/ContentView/ContentViewHandler.Windows.cs
@@ -17,16 +17,16 @@ namespace Microsoft.Maui.Handlers
             PlatformView.CrossPlatformArrange = VirtualView.CrossPlatformArrange;
         }
 
-        void UpdateContent()
+        static void UpdateContent(IContentViewHandler handler)
         {
-            _ = PlatformView ?? throw new InvalidOperationException($"{nameof(PlatformView)} should have been set by base class.");
-            _ = VirtualView ?? throw new InvalidOperationException($"{nameof(VirtualView)} should have been set by base class.");
-            _ = MauiContext ?? throw new InvalidOperationException($"{nameof(MauiContext)} should have been set by base class.");
+            _ = handler.PlatformView ?? throw new InvalidOperationException($"{nameof(PlatformView)} should have been set by base class.");
+            _ = handler.VirtualView ?? throw new InvalidOperationException($"{nameof(VirtualView)} should have been set by base class.");
+            _ = handler.MauiContext ?? throw new InvalidOperationException($"{nameof(MauiContext)} should have been set by base class.");
 
-            PlatformView.Children.Clear();
+			handler.PlatformView.Children.Clear();
 
-            if (VirtualView.PresentedContent is IView view)
-                PlatformView.Children.Add(view.ToPlatform(MauiContext));
+            if (handler.VirtualView.PresentedContent is IView view)
+				handler.PlatformView.Children.Add(view.ToPlatform(handler.MauiContext));
         }
 
         protected override ContentPanel CreatePlatformView()
@@ -45,9 +45,9 @@ namespace Microsoft.Maui.Handlers
             return view;
         }
 
-        public static void MapContent(ContentViewHandler handler, IContentView page)
+        public static void MapContent(IContentViewHandler handler, IContentView page)
         {
-            handler.UpdateContent();
+			UpdateContent(handler);
         }
     }
 }
