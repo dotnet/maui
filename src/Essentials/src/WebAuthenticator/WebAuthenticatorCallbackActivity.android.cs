54f6d952fa770d54c0bee97fec29f21875d94bc8
redth <jondick@gmail.com>
2022-02-19 16:28:31 -0500

    Namespace fixes

--- src/Essentials/src/WebAuthenticator/WebAuthenticatorCallbackActivity.android.cs
+++ src/Essentials/src/WebAuthenticator/WebAuthenticatorCallbackActivity.android.cs
@@ -2,7 +2,7 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 
-namespace Microsoft.Maui.Essentials.Implementations
+namespace Microsoft.Maui.Essentials
 {
 	public abstract class WebAuthenticatorCallbackActivity : Activity
 	{
@@ -11,7 +11,7 @@ namespace Microsoft.Maui.Essentials.Implementations
 			base.OnCreate(savedInstanceState);
 
 			// start the intermediate activity again with flags to close the custom tabs
-			var intent = new Intent(this, typeof(WebAuthenticatorIntermediateActivity));
+			var intent = new Intent(this, typeof(Implementations.WebAuthenticatorIntermediateActivity));
 			intent.SetData(Intent.Data);
 			intent.AddFlags(ActivityFlags.ClearTop | ActivityFlags.SingleTop);
 			StartActivity(intent);
