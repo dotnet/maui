9c1baa5d5f13865bd44a6b438f7c3e59bc4012e6
Javier Suárez <javiersuarezruiz@hotmail.com>
2022-02-19 23:33:47 +0100

    Implement Cookies property in WebView (#4419)
    * Implement Cookies in WebView
    
    * Update WebViewHandler.iOS.cs
    
    * Make const internal
    
    * Fix rename
    
    Co-authored-by: redth <jondick@gmail.com>

--- src/Core/src/Handlers/WebView/WebViewHandler.Windows.cs
+++ src/Core/src/Handlers/WebView/WebViewHandler.Windows.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.UI.Xaml.Controls;
-using Microsoft.Web.WebView2.Core;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
+using Windows.Web.Http;
 
 namespace Microsoft.Maui.Handlers
 {
 	public partial class WebViewHandler : ViewHandler<IWebView, WebView2>
 	{
+		readonly HashSet<string> _loadedCookies = new HashSet<string>();
+
 		protected override WebView2 CreatePlatformView() => new MauiWebView();
 
 		protected override void ConnectHandler(WebView2 platformView)
 		{
+			platformView.NavigationStarting += OnNavigationStarted;
 			platformView.NavigationCompleted += OnNavigationCompleted;
 
 			base.ConnectHandler(platformView);
@@ -18,6 +25,7 @@ namespace Microsoft.Maui.Handlers
 
 		protected override void DisconnectHandler(WebView2 platformView)
 		{
+			platformView.NavigationStarting -= OnNavigationStarted;
 			platformView.NavigationCompleted -= OnNavigationCompleted;
 
 			base.DisconnectHandler(platformView);
@@ -45,20 +53,184 @@ namespace Microsoft.Maui.Handlers
 			handler.PlatformView?.UpdateReload(webView);
 		}
 
-		void OnNavigationCompleted(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
-		{
-			if (VirtualView == null)
-				return;
-
-			sender.UpdateCanGoBackForward(VirtualView);
-		}
-
 		public static void MapEval(WebViewHandler handler, IWebView webView, object? arg)
 		{
 			if (arg is not string script)
 				return;
 
 			handler.PlatformView?.Eval(webView, script);
+		}
+
+		void OnNavigationStarted(WebView2 sender, CoreWebView2NavigationStartingEventArgs args)
+		{
+			// TODO: Notify navigation state.
+		}
+		
+		void OnNavigationCompleted(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
+		{
+			if (args.IsSuccess)
+				NavigationSucceeded(sender, args);
+			else
+				NavigationFailed(sender, args);
+		}
+
+		void NavigationSucceeded(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
+		{
+			string? url = sender.Source?.ToString();
+
+			if (url == null)
+				return;
+
+			SendNavigated(url);
+
+			if (VirtualView == null)
+				return;
+
+			sender.UpdateCanGoBackForward(VirtualView);
+		}
+
+		void NavigationFailed(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
+		{
+			string? url = sender.Source?.ToString();
+
+			if (url == null)
+				return;
+
+			SendNavigated(url);
+		}
+		
+		void SendNavigated(string url)
+		{
+			SyncPlatformCookiesToVirtualView(url);
+
+			PlatformView?.UpdateCanGoBackForward(VirtualView);
+		}
+		
+		void SyncPlatformCookiesToVirtualView(string url)
+		{
+			var myCookieJar = VirtualView.Cookies;
+		
+			if (myCookieJar == null)
+				return;
+
+			var uri = CreateUriForCookies(url);
+
+			if (uri == null)
+				return;
+
+			var cookies = myCookieJar.GetCookies(uri);
+			var retrieveCurrentWebCookies = GetCookiesFromPlatformStore(url);
+
+			var filter = new global::Windows.Web.Http.Filters.HttpBaseProtocolFilter();
+			var platformCookies = filter.CookieManager.GetCookies(uri);
+
+			foreach (Cookie cookie in cookies)
+			{
+				var httpCookie = platformCookies
+					.FirstOrDefault(x => x.Name == cookie.Name);
+
+				if (httpCookie == null)
+					cookie.Expired = true;
+				else
+					cookie.Value = httpCookie.Value;
+			}
+
+			SyncPlatformCookies(url);
+		}
+
+		void SyncPlatformCookies(string url)
+		{
+			var uri = CreateUriForCookies(url);
+			
+			if (uri == null)
+				return;
+
+			var myCookieJar = VirtualView.Cookies;
+			
+			if (myCookieJar == null)
+				return;
+
+			InitialCookiePreloadIfNecessary(url);
+			var cookies = myCookieJar.GetCookies(uri);
+			
+			if (cookies == null)
+				return;
+
+			var retrieveCurrentWebCookies = GetCookiesFromPlatformStore(url);
+
+			var filter = new global::Windows.Web.Http.Filters.HttpBaseProtocolFilter();
+			
+			foreach (Cookie cookie in cookies)
+			{
+				HttpCookie httpCookie = new HttpCookie(cookie.Name, cookie.Domain, cookie.Path)
+				{
+					Value = cookie.Value
+				};
+				filter.CookieManager.SetCookie(httpCookie, false);
+			}
+
+			foreach (HttpCookie cookie in retrieveCurrentWebCookies)
+			{
+				if (cookies[cookie.Name] != null)
+					continue;
+
+				filter.CookieManager.DeleteCookie(cookie);
+			}
+		}
+
+		void InitialCookiePreloadIfNecessary(string url)
+		{
+			var myCookieJar = VirtualView.Cookies;
+
+			if (myCookieJar == null)
+				return;
+
+			var uri = new Uri(url);
+
+			if (!_loadedCookies.Add(uri.Host))
+				return;
+
+			var cookies = myCookieJar.GetCookies(uri);
+
+			if (cookies != null)
+			{
+				var existingCookies = GetCookiesFromPlatformStore(url);
+				foreach (HttpCookie cookie in existingCookies)
+				{
+					if (cookies[cookie.Name] == null)
+						myCookieJar.SetCookies(uri, cookie.ToString());
+				}
+			}
+		}
+
+		HttpCookieCollection GetCookiesFromPlatformStore(string url)
+		{
+			var uri = CreateUriForCookies(url);
+			var filter = new global::Windows.Web.Http.Filters.HttpBaseProtocolFilter();
+			var platformCookies = filter.CookieManager.GetCookies(uri);
+			
+			return platformCookies;
+		}
+
+		Uri? CreateUriForCookies(string url)
+		{
+			if (url == null)
+				return null;
+
+			Uri? uri;
+
+			if (url.Length > 2000)
+				url = url.Substring(0, 2000);
+
+			if (Uri.TryCreate(url, UriKind.Absolute, out uri))
+			{
+				if (string.IsNullOrWhiteSpace(uri.Host))
+					return null;
+
+				return uri;
+			}
+
+			return null;
 		}
 
 		public static void MapEvaluateJavaScriptAsync(WebViewHandler handler, IWebView webView, object? arg) 
--- src/Core/src/Platform/Android/MauiWebViewClient.cs
+++ src/Core/src/Platform/Android/MauiWebViewClient.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Maui.Platform
 {
 	public class MauiWebViewClient : WebViewClient
 	{
-		readonly WebViewHandler _handler;
+		WebViewHandler? _handler;
 
 		public MauiWebViewClient(WebViewHandler handler)
 		{
@@ -15,10 +15,23 @@ namespace Microsoft.Maui.Platform
 
 		public override void OnPageStarted(WebView? view, string? url, Bitmap? favicon)
 		{
+			if (_handler?.VirtualView == null || string.IsNullOrWhiteSpace(url) || url == WebViewHandler.AssetBaseUrl)
+				return;
+
+			_handler.SyncPlatformCookiesToVirtualView(url);
+
 			if (_handler != null)
 				_handler.PlatformView.UpdateCanGoBackForward(_handler.VirtualView);
 
 			base.OnPageStarted(view, url, favicon);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+				_handler = null;
+
+			base.Dispose(disposing);
 		}
 	}
 }
