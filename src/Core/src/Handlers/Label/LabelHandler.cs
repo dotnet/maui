014bd8667cbefb247a4bcac0ce16cd818160d0a5
Anton Firszov <antonfir@gmail.com>
2022-02-22 20:13:44 +0100

    wip

--- src/Core/src/Handlers/Label/LabelHandler.cs
+++ src/Core/src/Handlers/Label/LabelHandler.cs
@@ -1,4 +1,14 @@
 #nullable enable
+#if __IOS__ || MACCATALYST
+using PlatformView = Microsoft.Maui.Platform.MauiLabel;
+#elif MONOANDROID
+using PlatformView = AndroidX.AppCompat.Widget.AppCompatTextView;
+#elif WINDOWS
+using PlatformView = Microsoft.UI.Xaml.Controls.TextBlock;
+#elif NETSTANDARD || (NET6_0 && !IOS && !ANDROID)
+using PlatformView = System.Object;
+#endif
+
 namespace Microsoft.Maui.Handlers
 {
 	public partial class LabelHandler
