f602372d5d60b0f09dad39475053cd6afd6d4096
Tanay Parikh <taparik@microsoft.com>
2022-02-23 14:21:19 -0800

    Winforms ExternalNavigationStarting

--- src/BlazorWebView/src/WindowsForms/BlazorWebView.cs
+++ src/BlazorWebView/src/WindowsForms/BlazorWebView.cs
@@ -111,6 +111,10 @@ namespace Microsoft.AspNetCore.Components.WebView.WindowsForms
 			}
 		}
 
+		[Category("Action")]
+		[Description("Allows customizing how external links are opened. Opens external links in the system browser by default.")]
+		public EventHandler<ExternalLinkNavigationEventArgs> ExternalNavigationStarting;
+
 		private void OnHostPagePropertyChanged() => StartWebViewCoreIfPossible();
 
 		private void OnServicesPropertyChanged() => StartWebViewCoreIfPossible();
@@ -154,7 +158,14 @@ namespace Microsoft.AspNetCore.Components.WebView.WindowsForms
 
 			var fileProvider = CreateFileProvider(contentRootDirFullPath);
 
-			_webviewManager = new WebView2WebViewManager(_webview, Services, ComponentsDispatcher, fileProvider, RootComponents.JSComponents, hostPageRelativePath);
+			_webviewManager = new WebView2WebViewManager(
+				_webview,
+				Services,
+				ComponentsDispatcher,
+				fileProvider,
+				RootComponents.JSComponents,
+				hostPageRelativePath,
+				(args) => ExternalNavigationStarting?.Invoke(this, args));
 
 			foreach (var rootComponent in RootComponents)
 			{
