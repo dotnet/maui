f94a0f91ad50abee1ae2c50d95bff19f53bae5b1
Kangho Hur <kangho.hur@samsung.com>
2022-02-23 17:30:41 +0900

    [Tizen] Add handling for TextTransform for SearchBar/Editor/Entry

---
+++ src/Controls/src/Core/Platform/Tizen/Extensions/TextExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Maui.Platform;
+using Microsoft.Maui.Controls.Internals;
+using TEntry = Tizen.UIExtensions.ElmSharp.Entry;
+
+namespace Microsoft.Maui.Controls.Platform
+{
+	public static class TextExtensions
+	{
+		public static void UpdateText(this TEntry entry, InputView inputView)
+		{
+			entry.Text = TextTransformUtilites.GetTransformedText(entry.Text, inputView.TextTransform);
+		}
+	}
+}
