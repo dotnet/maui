020d1d8c8b54b2830b9fce2bafeda4af7f175016
Anton Firszov <antonfir@gmail.com>
2022-02-24 01:15:28 +0100

    interfaced handler for TimePicker

--- src/Core/src/Handlers/TimePicker/TimePickerHandler.Android.cs
+++ src/Core/src/Handlers/TimePicker/TimePickerHandler.Android.cs
@@ -50,34 +50,34 @@ namespace Microsoft.Maui.Handlers
 		}
 
 		// This is a Android-specific mapping
-		public static void MapBackground(TimePickerHandler handler, ITimePicker timePicker)
+		public static void MapBackground(ITimePickerHandler handler, ITimePicker timePicker)
 		{
 			handler.PlatformView?.UpdateBackground(timePicker, DefaultBackground);
 		}
 
-		public static void MapFormat(TimePickerHandler handler, ITimePicker timePicker)
+		public static void MapFormat(ITimePickerHandler handler, ITimePicker timePicker)
 		{
 			handler.PlatformView?.UpdateFormat(timePicker);
 		}
 
-		public static void MapTime(TimePickerHandler handler, ITimePicker timePicker)
+		public static void MapTime(ITimePickerHandler handler, ITimePicker timePicker)
 		{
 			handler.PlatformView?.UpdateTime(timePicker);
 		}
 
-		public static void MapCharacterSpacing(TimePickerHandler handler, ITimePicker timePicker)
+		public static void MapCharacterSpacing(ITimePickerHandler handler, ITimePicker timePicker)
 		{
 			handler.PlatformView?.UpdateCharacterSpacing(timePicker);
 		}
 
-		public static void MapFont(TimePickerHandler handler, ITimePicker timePicker)
+		public static void MapFont(ITimePickerHandler handler, ITimePicker timePicker)
 		{
 			var fontManager = handler.GetRequiredService<IFontManager>();
 
 			handler.PlatformView?.UpdateFont(timePicker, fontManager);
 		}
 
-		public static void MapTextColor(TimePickerHandler handler, ITimePicker timePicker)
+		public static void MapTextColor(ITimePickerHandler handler, ITimePicker timePicker)
 		{
 			handler.PlatformView?.UpdateTextColor(timePicker, DefaultTextColors);
 		}
