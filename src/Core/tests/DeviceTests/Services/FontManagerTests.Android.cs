8c5441ba16984e07ffe5fb9d529a6ff90f6bb898
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-02-24 16:06:28 -0600

    [android] refactor font loading
    Context: https://github.com/microsoft/dotnet-podcasts
    
    After reviewing `dotnet trace` output for the .NET podcast app, we saw
    time spent in `FontManager.CreateTypeface()`:
    
        32.19ms Microsoft.Maui!Microsoft.Maui.FontManager.CreateTypeface(System.ValueTuple`3<string, Microsoft.Maui.FontWeight, bool>)
    
    Reviewing the code, it was doing more work than needed:
    
    1. Saving `AndroidAsset` files to a temp folder.
    
    2. `FontRegistrar` would then return the full path the file.
    
    3. `Typeface.CreateFromFile()` would load the file.
    
    Luckily, this was at least cached between runs?
    
    I refactored this logic so we just load these files directly from
    `Typeface.CreateFromAsset()`. We also shouldn't lose any behavior that
    we had before.
    
    I added new test that also verifies fonts from sub-directories
    work as expected.
    
    After these changes, a `Release` build of the podcast app on a Pixel 5
    device gets:
    
        6.43ms Microsoft.Maui!Microsoft.Maui.FontManager.CreateTypeface(System.ValueTuple`3<string, Microsoft.Maui.FontWeight, bool>)
    
    So I suspect we will save ~25ms on the initial launch of a MAUI app,
    and might save a small amount on subsequent runs.

---
+++ src/Core/tests/DeviceTests/Services/FontManagerTests.Android.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Graphics;
+using Java.Interop;
+using Xunit;
+
+namespace Microsoft.Maui.DeviceTests;
+
+[Category(TestCategory.Fonts)]
+public partial class FontManagerTests : TestBase
+{
+	[Theory]
+	[InlineData("dokdo_regular", "dokdo_regular.ttf")]
+	[InlineData("dokdo_regular.ttf", "dokdo_regular.ttf")]
+	[InlineData("myalias", "dokdo_regular.ttf")]
+	[InlineData("insubfolder", "Fonts/insubfolder.ttf")]
+	[InlineData("insubfolder.ttf", "Fonts/insubfolder.ttf")]
+	public void CanLoadFonts(string fontName, string assetName)
+	{
+		var fontWeight = FontWeight.Regular;
+		var registrar = new FontRegistrar(fontLoader: null);
+		registrar.Register("dokdo_regular.ttf", "myalias");
+		var manager = new FontManager(registrar);
+		var actual = manager.GetTypeface(Font.OfSize(fontName, 12, fontWeight));
+		var expected = Typeface.Create(Typeface.CreateFromAsset(Application.Context.Assets, assetName), (int)fontWeight, italic: false);
+		Assert.True(expected.Equals(actual));
+	}
+}
