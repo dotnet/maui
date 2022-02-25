57e66dedeb4cb13ccd65c2ad0191985550e8d57d
민성현/Common Platform Lab(SR)/삼성전자 <shyun.min@samsung.com>
2022-02-25 13:12:47 +0900

    [NUI] Add TimePickerHandler (#390)
    * [NUI] Add TimePickerHandler
    
    * Update button placement

--- src/Core/src/Handlers/TimePicker/TimePickerHandler.Tizen.cs
+++ src/Core/src/Handlers/TimePicker/TimePickerHandler.Tizen.cs
@@ -1,29 +1,81 @@
-using NView = Tizen.NUI.BaseComponents.View;
+using Tizen.NUI;
+using NEntry = Tizen.UIExtensions.NUI.Entry;
 
 namespace Microsoft.Maui.Handlers
 {
-	public partial class TimePickerHandler : ViewHandler<ITimePicker, NView>
+	public partial class TimePickerHandler : ViewHandler<ITimePicker, NEntry>
 	{
-		protected override NView CreatePlatformView() => new();
+		protected override NEntry CreatePlatformView() => new NEntry
+		{
+			IsReadOnly = true,
+			VerticalAlignment = VerticalAlignment.Center
+		};
+
+		protected override void ConnectHandler(NEntry platformView)
+		{
+			platformView.TouchEvent += OnTouch;
+			base.ConnectHandler(platformView);
+		}
+
+		protected override void DisconnectHandler(NEntry platformView)
+		{
+			platformView.TouchEvent -= OnTouch;
+			base.DisconnectHandler(platformView);
+		}
 
 		public static void MapFormat(ITimePickerHandler handler, ITimePicker timePicker)
 		{
+			handler.PlatformView.UpdateFormat(timePicker);
 		}
 
 		public static void MapTime(ITimePickerHandler handler, ITimePicker timePicker)
 		{
+			handler.PlatformView.UpdateTime(timePicker);
 		}
 
 		public static void MapFont(ITimePickerHandler handler, ITimePicker timePicker)
 		{
+			var fontManager = handler.GetRequiredService<IFontManager>();
+			handler.PlatformView.UpdateFont(timePicker, fontManager);
 		}
 
 		public static void MapTextColor(ITimePickerHandler handler, ITimePicker timePicker)
 		{
+			handler.PlatformView.UpdateTextColor(timePicker);
 		}
 
 		[MissingMapper]
 		public static void MapCharacterSpacing(ITimePickerHandler handler, ITimePicker timePicker) { }
+
+		bool OnTouch(object source, Tizen.NUI.BaseComponents.View.TouchEventArgs e)
+		{
+			if (e.Touch.GetState(0) != Tizen.NUI.PointStateType.Up)
+				return false;
+
+			if (VirtualView == null)
+				return false;
+
+			OpenPopupAsync();
+			return true;
+		}
+
+		async void OpenPopupAsync()
+		{
+			if (VirtualView == null)
+				return;
+
+			using (var popup = new MauiTimePicker(VirtualView.Time))
+			{
+				try
+				{
+					VirtualView.Time = await popup.Open();
+				}
+				catch
+				{
+					// Cancel
+				}
+			}
+		}
 
 	}
 }
--- src/Core/src/Platform/Tizen/TimePickerExtensions.cs
+++ src/Core/src/Platform/Tizen/TimePickerExtensions.cs
@@ -7,7 +7,6 @@ namespace Microsoft.Maui.Platform
 	public static class TimePickerExtensions
 	{
 		static readonly string s_defaultFormat = CultureInfo.CurrentCulture.DateTimeFormat.ShortTimePattern;
-		static TimeSpan Time = DateTime.Now.TimeOfDay;
 
 		public static void UpdateFormat(this Entry platformTimePicker, ITimePicker timePicker)
 		{
@@ -17,7 +16,7 @@ namespace Microsoft.Maui.Platform
 		public static void UpdateTime(this Entry platformTimePicker, ITimePicker timePicker)
 		{
 			// Xamarin using DateTime formatting (https://developer.xamarin.com/api/property/Xamarin.Forms.TimePicker.Format/)
-			platformTimePicker.Text = new DateTime(Time.Ticks).ToString(timePicker.Format ?? s_defaultFormat);
+			platformTimePicker.Text = new DateTime(timePicker.Time.Ticks).ToString(timePicker.Format ?? s_defaultFormat);
 		}
 
 	}
