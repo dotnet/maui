9a6f787a03b3d07fefc06dfeb46150e86cf9b80b
Shane Neuville <shneuvil@microsoft.com>
2022-03-01 16:57:12 -0600

    - fix cursor position with transformed text

--- src/Controls/src/Core/Platform/Android/Extensions/EditTextExtensions.cs
+++ src/Controls/src/Core/Platform/Android/Extensions/EditTextExtensions.cs
@@ -10,11 +10,22 @@ namespace Microsoft.Maui.Controls.Platform
 			var value = TextTransformUtilites.GetTransformedText(inputView.Text, inputView.TextTransform);
 
 			// Setting the text causes the cursor to reset to position zero
-			// Therefore if:
-			// User Types => VirtualView Updated => Triggers Native Update
-			// Then it will cause the cursor to reset to position zero as the user typed
-			if (value != editText.Text)
-				editText.Text = value;
+			// so if we are transforming the text and then setting it to a 
+			// new value then we need to retain the cursor position
+			if (value == inputView.Text)
+				return;
+
+			int selectionStart = 0;
+
+			// This means the user is typing at the end of the string
+			if (selectionStart == value.Length)
+				selectionStart = value.Length;
+			// Users is typing at the start/middle of the string
+			else
+				selectionStart = editText.SelectionStart;
+
+			editText.Text = value;
+			editText.SetSelection(selectionStart);
 		}
 	}
 }
