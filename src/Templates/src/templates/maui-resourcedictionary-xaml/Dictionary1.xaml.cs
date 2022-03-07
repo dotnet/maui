d89515f0d9592a94f2d1a502a886d2334309d6f4
Gerald Versluis <gerald@verslu.is>
2022-03-07 11:14:15 +0100

    Rename NewContent to Dictionary

---
+++ src/Templates/src/templates/maui-resourcedictionary-xaml/Dictionary1.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class Dictionary1 : ResourceDictionary
+{
+	public Dictionary1()
+	{
+		InitializeComponent();
+	}
+}
--- src/Templates/src/templates/maui-resourcedictionary-xaml/NewContent1.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace MauiApp1;
-
-public partial class NewContent1 : ResourceDictionary
-{
-	public NewContent1()
-	{
-		InitializeComponent();
-	}
-}
