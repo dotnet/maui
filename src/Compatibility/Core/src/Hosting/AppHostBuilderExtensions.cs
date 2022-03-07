a9fa34fe7c2059f8cd746448201eabfb78aa841e
Shane Neuville <shneuvil@microsoft.com>
2022-03-07 13:50:27 -0600

    - RegisterVisual

--- src/Compatibility/Core/src/Hosting/AppHostBuilderExtensions.cs
+++ src/Compatibility/Core/src/Hosting/AppHostBuilderExtensions.cs
@@ -9,10 +9,10 @@ namespace Microsoft.Maui.Controls.Compatibility.Hosting
 {
 	public static class MauiAppBuilderExtensions
 	{
-		public static MauiAppBuilder AddVisual<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] TVisualType>(this MauiAppBuilder builder)
-		=> AddVisual(builder, typeof(TVisualType));
+		public static MauiAppBuilder RegisterVisual<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] TVisualType>(this MauiAppBuilder builder)
+		=> RegisterVisual(builder, typeof(TVisualType));
 
-		public static MauiAppBuilder AddVisual(this MauiAppBuilder builder, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] Type TVisualType)
+		public static MauiAppBuilder RegisterVisual(this MauiAppBuilder builder, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] Type TVisualType)
 		{
 			Internals.Registrar.Registered.RegisterVisual(TVisualType);
 			return builder;
