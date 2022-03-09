64d497bdaf6871c60f6ff1b0ed436fec468de0c9
redth <jondick@gmail.com>
2022-03-09 12:42:37 -0500

    Add test for display density on window

---
+++ src/Controls/tests/DeviceTests/Elements/Window/WindowTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Hosting;
+using Xunit;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	[Category(TestCategory.Window)]
+	public partial class WindowTests : HandlerTestBase
+	{
+		[Fact]
+		public async Task WindowHasReasonableDisplayDensity()
+		{
+			var density = 0f;
+
+			var control = new Controls.Window(new Controls.ContentPage());
+			var handler = await CreateHandlerAsync<WindowHandlerStub>(control);
+
+			await InvokeOnMainThreadAsync(() =>
+			{
+				density = handler.VirtualView.DisplayDensity;
+			});
+
+			Assert.InRange(density, 0.1f, 4f);
+		}
+	}
+}
