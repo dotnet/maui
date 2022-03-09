1aba5c4337dc91d06132fa0564c67fc62f6f5496
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-03-09 13:39:50 +0100

    Implement FillRule property in PolygonHandler and PolylineHandler

--- src/Controls/src/Core/Handlers/Shapes/Polygon/PolygonHandler.Standard.cs
+++ src/Controls/src/Core/Handlers/Shapes/Polygon/PolygonHandler.Standard.cs
@@ -3,8 +3,10 @@ using Microsoft.Maui.Controls.Shapes;
 
 namespace Microsoft.Maui.Controls.Handlers
 {
-	public partial class PolygonHandler 
+	public partial class PolygonHandler
 	{
+		public static void MapShape(IViewHandler handler, Polygon polygon) { }
 		public static void MapPoints(IViewHandler handler, Polygon polygon) { }
+		public static void MapFillRule(IViewHandler handler, Polygon polygon) { }
 	}
 }
