a9394c5cb6ad0ea40277d2df4267c2e71d8eb89e
pictos <pedrojesus.cefet@gmail.com>
2022-03-08 23:03:22 -0300

    updated the BaseBehavior implementation

--- src/Controls/src/Core/Interactivity/IPlatformAttachedObject.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Microsoft.Maui.Controls;
-internal interface IPlatformAttachedObject
-{
-	void OnPlatformAttachBehavior(BindableObject bindable);
-	void OnPlatformDeattachBehavior(BindableObject bindable);
-}
--- src/Controls/src/Core/Interactivity/PlatformBehavior.cs
+++ src/Controls/src/Core/Interactivity/PlatformBehavior.cs
@@ -10,6 +10,8 @@ using PlatformView = Android.Views.View;
 using PlatformView = Microsoft.UI.Xaml.FrameworkElement;
 #endif
 
+using System;
+
 namespace Microsoft.Maui.Controls;
 
 /// <summary>
@@ -17,7 +19,7 @@ namespace Microsoft.Maui.Controls;
 /// </summary>
 /// <typeparam name="TView">Virtual View</typeparam>
 /// <typeparam name="TPlatformView">Platform View</typeparam>
-public abstract partial class BasePlatformBehavior<TView, TPlatformView> : Behavior<TView>, IPlatformAttachedObject
+public abstract partial class BasePlatformBehavior<TView, TPlatformView> : Behavior<TView>
 	where TView : VisualElement
 #if __IOS__ || __ANDROID__ || __WINDOWS
 		where TPlatformView : PlatformView
@@ -27,50 +29,56 @@ public abstract partial class BasePlatformBehavior<TView, TPlatformView> : Behav
 {
 	protected TPlatformView? PlatformView => View?.Handler?.PlatformView as TPlatformView;
 	protected TView? View { get; private set; }
-	bool _isRemoved;
 
 	/// <inheritdoc />
-	protected override void OnAttachedTo(TView bindable)
+	protected sealed override void OnAttachedTo(BindableObject bindable)
+	{
+		base.OnAttachedTo(bindable);
+	}
+
+	/// <inheritdoc />
+	protected sealed override void OnDetachingFrom(BindableObject bindable)
+	{
+		base.OnDetachingFrom(bindable);
+	}
+
+	/// <inheritdoc />
+	protected sealed override void OnAttachedTo(TView bindable)
 	{
 		if (bindable.Handler is null)
+		{
+			bindable.HandlerChanged += OnHandlerChanged;
 			return;
-
-		_isRemoved = false;
-		base.OnAttachedTo(bindable);
+		}
 		View = bindable;
-		OnPlatformAttachedBehavior();
+		OnPlatformAttachedBehavior(bindable.Handler);
 	}
 
 	/// <inheritdoc />
-	protected override void OnDetachingFrom(TView bindable)
+	protected sealed override void OnDetachingFrom(TView bindable)
 	{
-		if (_isRemoved)
+		OnPlatformDeattachedBehavior(bindable.Handler);
+		bindable.HandlerChanged -= OnHandlerChanged;
+	}
+
+	private void OnHandlerChanged(object? sender, EventArgs e)
+	{
+		if (sender is not TView visualElement)
 			return;
 
-		base.OnDetachingFrom(bindable);
-		OnPlatformDeattachedBehavior();
-		_isRemoved = true;
+		if (visualElement.Handler is not null)
+			OnAttachedTo(visualElement);
 	}
 
 	/// <summary>
-	/// This method is called when the Handler is attached to the View. Use this method to perform your customizations in the control
+	/// This method is called when the Handler is attached to the View. Use this method to perform your customizations in the control.
 	/// </summary>
-	protected abstract void OnPlatformAttachedBehavior();
+	/// <param name="handler">The <see cref="IViewHandler"/> for the <see cref="VisualElement"/>.</param>
+	protected abstract void OnPlatformAttachedBehavior(IViewHandler handler);
 
 	/// <summary>
-	/// This method is called when the Handler is dettached from the View. Use this method to perform your customizations in the control
+	/// This method is called when the Handler is dettached from the View. Use this method to perform your customizations in the control.
 	/// </summary>
-	protected abstract void OnPlatformDeattachedBehavior();
-
-	void IPlatformAttachedObject.OnPlatformAttachBehavior(BindableObject bindable)
-	{
-		_isRemoved = false;
-		OnAttachedTo(bindable);
-	}
-
-	void IPlatformAttachedObject.OnPlatformDeattachBehavior(BindableObject bindable)
-	{
-		OnDetachingFrom(bindable);
-		_isRemoved = true;
-	}
+	/// <param name="handler">The <see cref="IViewHandler"/> for the <see cref="VisualElement"/>.</param>
+	protected abstract void OnPlatformDeattachedBehavior(IViewHandler? handler);
 }
