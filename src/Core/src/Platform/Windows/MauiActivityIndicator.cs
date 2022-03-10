d1157298fa3a759646a6d215a667b539d66c8ab2
Anton Firszov <antonfir@gmail.com>
2022-03-10 17:45:52 +0100

    ..

--- src/Core/src/Platform/Windows/MauiActivityIndicator.cs
+++ src/Core/src/Platform/Windows/MauiActivityIndicator.cs
@@ -16,11 +16,11 @@ namespace Microsoft.Maui.Platform
 
 		protected override global::Windows.Foundation.Size MeasureOverride(global::Windows.Foundation.Size availableSize)
 		{
-			if (double.IsInfinity(availableSize.Width))
-				availableSize.Width = 32;
+			//if (double.IsInfinity(availableSize.Width))
+			//	availableSize.Width = 32;
 
-			if (double.IsInfinity(availableSize.Height))
-				availableSize.Height = 32;
+			//if (double.IsInfinity(availableSize.Height))
+			//	availableSize.Height = 32;
 
 			var result = base.MeasureOverride(availableSize);
 
