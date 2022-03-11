27b256e8dcc38ef1973ac70179726f1a02806a9f
Matthew Leibowitz <mattleibow@live.com>
2022-03-11 12:54:36 +0800

    no window tests for now

--- src/Core/tests/DeviceTests/Handlers/Window/WindowHandlerTests.cs
+++ src/Core/tests/DeviceTests/Handlers/Window/WindowHandlerTests.cs
@@ -6,18 +6,20 @@ namespace Microsoft.Maui.DeviceTests
 	[Category(TestCategory.Window)]
 	public partial class WindowHandlerTests : HandlerTestBase
 	{
-		[Fact]
-		public async Task WindowHasReasonableDisplayDensity()
-		{
-			var window = MauiProgram.DefaultTestApp.Windows[0];
-			var handler = window.Handler!;
+		// TODO: think of how to set up windows for the headless runner which is not
+		//       a real maui app because we are trying to test a maui app
+		// [Fact]
+		// public async Task WindowHasReasonableDisplayDensity()
+		// {
+		// 	var window = MauiProgram.DefaultTestApp.Windows[0];
+		// 	var handler = window.Handler!;
 
-			var req = new DisplayDensityRequest();
+		// 	var req = new DisplayDensityRequest();
 
-			var density = await InvokeOnMainThreadAsync(() => handler.InvokeWithResult(nameof(IWindow.RequestDisplayDensity), req));
+		// 	var density = await InvokeOnMainThreadAsync(() => handler.InvokeWithResult(nameof(IWindow.RequestDisplayDensity), req));
 
-			Assert.Equal(density, req.Result);
-			Assert.InRange(density, 0.1f, 4f);
-		}
+		// 	Assert.Equal(density, req.Result);
+		// 	Assert.InRange(density, 0.1f, 4f);
+		// }
 	}
 }
