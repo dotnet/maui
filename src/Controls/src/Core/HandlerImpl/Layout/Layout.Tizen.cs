854a98e2d738a42195e4e35ce2012259ef97ec77
이승근/Common Platform Lab(SR)/삼성전자 <sngn.lee@samsung.com>
2022-03-14 16:25:22 +0900

    Fix InputTransparent (#424)

--- src/Controls/src/Core/HandlerImpl/Layout/Layout.Tizen.cs
+++ src/Controls/src/Core/HandlerImpl/Layout/Layout.Tizen.cs
@@ -4,7 +4,18 @@
 	{
 		public static void MapInputTransparent(LayoutHandler handler, Layout layout)
 		{
-			handler.PlatformView?.UpdateInputTransparent(handler, layout);
+			if (layout.CascadeInputTransparent)
+			{
+				handler.PlatformView?.UpdateInputTransparent(handler, layout);
+			}
+			else
+			{
+				// TODO. need to fix
+				// If CascadeInputTransparent is true, child should be got an event
+				// But, in NUI, if Container Sensitive was false, children also can't get an event
+				// So, We can't set Sensitive to false, and we should pass through an event into below view
+				// To pass through an event, on LayoutHandler.tizen I will ovrride OnHitTest method, and returning false when InputTransparent was true
+			}
 			layout.UpdateDescendantInputTransparent();
 		}
 	}
