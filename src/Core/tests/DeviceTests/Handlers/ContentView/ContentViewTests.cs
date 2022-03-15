df2cd93c95897f46a281c46abe3fbfe7fc8967fb
E.Z. Hart <hartez@gmail.com>
2022-03-15 15:45:12 -0600

    Enforce Width/Height for ContentView on Android Fixes #4164

---
+++ src/Core/tests/DeviceTests/Handlers/ContentView/ContentViewTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Maui.DeviceTests.Stubs;
+using Xunit;
+
+namespace Microsoft.Maui.DeviceTests.Handlers.ContentView
+{
+	[Category(TestCategory.ContentView)]
+	public class ContentViewTests : HandlerTestBase<ContentViewHandler, ContentViewStub>
+	{
+		[Fact]
+		public async Task ContentViewDesiredSizeMatchesExplicitValues()
+		{
+			var cv = new ContentViewStub();
+
+			var content = new SliderStub
+			{
+				DesiredSize = new Size(50, 50)
+			};
+
+			cv.Content = content;
+			cv.Width = 100;
+			cv.Height = 150;
+
+			var contentViewHandler = await CreateHandlerAsync(cv);
+
+			var measure = await InvokeOnMainThreadAsync(() => cv.Measure(double.PositiveInfinity, double.PositiveInfinity));
+			
+			Assert.Equal(cv.Width, measure.Width);
+			Assert.Equal(cv.Height, measure.Height);
+		}
+	}
+}
