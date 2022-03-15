1dceb1b77530bb28e2f473907e47e763cef08866
Matthew Leibowitz <mattleibow@live.com>
2022-03-16 00:45:57 +0300

    Update ApplicationExtensions.cs

--- src/Core/src/Platform/iOS/ApplicationExtensions.cs
+++ src/Core/src/Platform/iOS/ApplicationExtensions.cs
@@ -66,14 +66,6 @@ namespace Microsoft.Maui.Platform
 			{
 				sceneDelegate.SetWindow(window);
 				sceneDelegate.GetWindow()?.MakeKeyAndVisible();
-
-				UIScreen.Notifications.ObserveModeDidChange(window.Screen, new System.EventHandler<NSNotificationEventArgs>((sender, args) =>
-				{
-					if (args.Notification.Object is UIScreen screen)
-					{
-						window.GetWindow()?.DisplayDensityUpdated((float)screen.Scale.Value);
-					}
-				}));
 			}
 		}
 
