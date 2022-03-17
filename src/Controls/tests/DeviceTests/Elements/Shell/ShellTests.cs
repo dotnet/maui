d6414f412225f6efb07ae68a72727adebd401dce
Shane Neuville <shneuvil@microsoft.com>
2022-03-17 14:33:14 -0500

    - remove extra code

--- src/Controls/tests/DeviceTests/Elements/Shell/ShellTests.cs
+++ src/Controls/tests/DeviceTests/Elements/Shell/ShellTests.cs
@@ -66,40 +66,6 @@ namespace Microsoft.Maui.DeviceTests
 			});
 		}
 
-		[Fact(DisplayName = "Back Button Executes Command")]
-		public async Task BackButtonExecutesCommand()
-		{
-			SetupBuilder();
-			bool commandExecuted = false;
-			string parameter = String.Empty;
-			var command = new Command((p) =>
-			{
-				parameter = $"{p}";
-				commandExecuted = true;
-			});
-
-			var shell = await CreateShellAsync((shell) =>
-			{
-				shell.Items.Add(new ContentPage());
-			});
-
-			await CreateHandlerAndAddToWindow<ShellHandler>(shell, async (handler) =>
-			{
-				var rootNavView = (handler.PlatformView);
-				Assert.False(rootNavView.IsBackEnabled);
-				await shell.Navigation.PushAsync(new ContentPage());
-				Assert.True(rootNavView.IsBackEnabled);
-				Shell.SetBackButtonBehavior(shell.CurrentPage,
-					new BackButtonBehavior()
-					{
-						Command = command,
-						CommandParameter = "PARAMETER"
-					});
-
-				Assert.False(rootNavView.IsBackEnabled);
-			});
-		}
-
 		[Theory]
 		[ClassData(typeof(ShellBasicNavigationTestCases))]
 		public async Task BasicShellNavigationStructurePermutations(ShellItem[] shellItems)
