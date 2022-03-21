b99914de28857cd17f810f6458494984e47e40c8
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-03-21 13:14:37 -0500

    Unused Java imports

--- src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/ViewHelper.java
+++ src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/ViewHelper.java
@@ -2,12 +2,10 @@ package com.microsoft.maui;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
-import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
