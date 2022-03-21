5f676f2b40b39c5c0aeec0d023145a00b0961b0b
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-03-21 13:26:35 +0100

    Updated Gallery to verify Slider Maximum value can be updated

--- src/Controls/samples/Controls.Sample/Pages/Controls/SliderPage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/Controls/SliderPage.xaml.cs
@@ -8,11 +8,35 @@ namespace Maui.Controls.Sample.Pages
 		public SliderPage()
 		{
 			InitializeComponent();
+
+			UpdateInfo();
 		}
 
 		void OnValueChanged(object sender, ValueChangedEventArgs args)
 		{
 			Debug.WriteLine($"Slider Value: {args.NewValue}");
+		}
+
+		void OnDynamicValueChanged(object sender, ValueChangedEventArgs args)
+		{
+			UpdateInfo();
+		}
+
+		void OnUpdateMinimumButtonClicked(object sender, System.EventArgs e)
+		{
+			DynamicSlider.Minimum = 4;
+			UpdateInfo();
+		}
+		
+		void OnUpdateMaximumButtonClicked(object sender, System.EventArgs e)
+		{
+			DynamicSlider.Maximum = 8;
+			UpdateInfo();
+		}
+
+		void UpdateInfo()
+		{
+			DynamicInfoLabel.Text = $"Minimum: {DynamicSlider.Minimum}, Maximum: {DynamicSlider.Maximum}, Value: {DynamicSlider.Value}";
 		}
 	}
 }
