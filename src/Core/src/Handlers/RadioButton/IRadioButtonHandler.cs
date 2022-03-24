74f22d22665e636cbae3f5f96f7bb7ef32dc0f30
Buyaa Namnan <bunamnan@microsoft.com>
2022-03-24 09:23:25 -0700

    RadioButton - Use default control templates for WinUI3

--- src/Core/src/Handlers/RadioButton/IRadioButtonHandler.cs
+++ src/Core/src/Handlers/RadioButton/IRadioButtonHandler.cs
@@ -3,7 +3,7 @@ using PlatformView = Microsoft.Maui.Platform.ContentView;
 #elif MONOANDROID
 using PlatformView = Android.Views.View;
 #elif WINDOWS
-using PlatformView = Microsoft.Maui.Platform.MauiRadioButton;
+using PlatformView = Microsoft.UI.Xaml.Controls.RadioButton;
 #elif NETSTANDARD || (NET6_0 && !IOS && !ANDROID)
 using PlatformView = System.Object;
 #endif
