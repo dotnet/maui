bfb13f038122554121ef3d973a70dac11a24e65e
Shane Neuville <shneuvil@microsoft.com>
2022-03-28 12:04:08 -0500

    Reproduction for Issue 5616

--- src/Controls/samples/Controls.Sample/Pages/AppShell.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/AppShell.xaml.cs
@@ -8,6 +8,9 @@ namespace Maui.Controls.Sample.Pages
 		{
 			InitializeComponent();
 			SetTabBarBackgroundColor(this, Color.FromRgba(3, 169, 244, 255));
+
+			cv1.ItemsSource = new[] { 1, 2, 3, 4, 5 };
+			cv2.ItemsSource = new[] { 1, 2, 3, 4, 5 };
 		}
 	}
 }
