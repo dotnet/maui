170cde5f849b6b220e7d9248171520ca64862cb6
Matthew Leibowitz <mattleibow@live.com>
2022-03-28 16:32:28 +0200

    fix obsolete

--- src/Controls/src/Build.Tasks/CompiledConverters/FontSizeTypeConverter.cs
+++ src/Controls/src/Build.Tasks/CompiledConverters/FontSizeTypeConverter.cs
@@ -22,8 +22,9 @@ namespace Microsoft.Maui.Controls.XamlC
 				{
 					yield return Instruction.Create(OpCodes.Ldc_R8, size);
 					yield break;
-				}	
-			
+				}
+
+#pragma warning disable CS0612 // Type or member is obsolete
 				if (Enum.TryParse<NamedSize>(value, out NamedSize namedSize))
 				{
 					//Device.GetNamedSize(namedSize, targetObject.GetType())
@@ -31,28 +32,29 @@ namespace Microsoft.Maui.Controls.XamlC
 					var parent = node.Parent as IElementNode;
 					if (parent != null && context.Variables.ContainsKey(parent))
 					{
-					    yield return Instruction.Create(OpCodes.Ldloc, context.Variables[parent]);
-					    yield return Instruction.Create(OpCodes.Callvirt, module.ImportMethodReference(
+						yield return Instruction.Create(OpCodes.Ldloc, context.Variables[parent]);
+						yield return Instruction.Create(OpCodes.Callvirt, module.ImportMethodReference(
 							module.TypeSystem.Object,
 							methodName: "GetType"));
 					}
 					else
 					{
-					    yield return Instruction.Create(OpCodes.Ldtoken, module.ImportReference(("Microsoft.Maui.Controls", "Microsoft.Maui.Controls", "Label")));
-					    yield return Instruction.Create(OpCodes.Call, module.ImportMethodReference(
+						yield return Instruction.Create(OpCodes.Ldtoken, module.ImportReference(("Microsoft.Maui.Controls", "Microsoft.Maui.Controls", "Label")));
+						yield return Instruction.Create(OpCodes.Call, module.ImportMethodReference(
 							("mscorlib", "System", "Type"),
 							methodName: "GetTypeFromHandle",
 							parameterTypes: new[] { ("mscorlib", "System", "RuntimeTypeHandle") },
 							isStatic: true));
 					}
 					yield return Instruction.Create(OpCodes.Call, module.ImportMethodReference(
-						    ("Microsoft.Maui.Controls", "Microsoft.Maui.Controls", "Device"),
-						    methodName: "GetNamedSize",
-						    parameterTypes: new[] {("Microsoft.Maui.Controls", "Microsoft.Maui.Controls", "NamedSize"),("System.Runtime", "System", "Type")},
-						    isStatic: true));
+							("Microsoft.Maui.Controls", "Microsoft.Maui.Controls", "Device"),
+							methodName: "GetNamedSize",
+							parameterTypes: new[] { ("Microsoft.Maui.Controls", "Microsoft.Maui.Controls", "NamedSize"), ("System.Runtime", "System", "Type") },
+							isStatic: true));
 
 					yield break;
 				}
+#pragma warning restore CS0612 // Type or member is obsolete
 			}
 
 			throw new BuildException(BuildExceptionCode.Conversion, node, null, value, typeof(double));
