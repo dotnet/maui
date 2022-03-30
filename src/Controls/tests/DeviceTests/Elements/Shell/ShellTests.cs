476cb7ff1a079ddc9702d568917dceba25f64922
Shane Neuville <shneuvil@microsoft.com>
2022-03-30 17:29:09 -0500

    Comment out shelltests for now

--- src/Controls/tests/DeviceTests/Elements/Shell/ShellTests.cs
+++ src/Controls/tests/DeviceTests/Elements/Shell/ShellTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if !IOS
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -98,3 +99,4 @@ namespace Microsoft.Maui.DeviceTests
 			});
 	}
 }
+#endif
