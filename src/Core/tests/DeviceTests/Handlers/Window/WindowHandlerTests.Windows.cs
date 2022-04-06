a62cc8650dc2245692a8bc9631e0544aeffaae02
Shane Neuville <shneuvil@microsoft.com>
2022-04-05 20:48:25 -0500

    - fix navmanager ctor

--- src/Core/tests/DeviceTests/Handlers/Window/WindowHandlerTests.Windows.cs
+++ src/Core/tests/DeviceTests/Handlers/Window/WindowHandlerTests.Windows.cs
@@ -102,8 +102,6 @@ namespace Microsoft.Maui.DeviceTests
 		{
 			return InvokeOnMainThreadAsync(async () =>
 			{
-				var window = MauiProgram.DefaultWindow;
-
 				FrameworkElement frameworkElement = null;
 				var content = (Panel)window.Content;
 				try
