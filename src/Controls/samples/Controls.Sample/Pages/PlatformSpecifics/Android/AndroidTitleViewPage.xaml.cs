bed944e6fa92c8df85859cbbb901ae9c4ca249ff
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-04-07 11:55:07 +0200

    Added Platform Specifics sample to Core Gallery

---
+++ src/Controls/samples/Controls.Sample/Pages/PlatformSpecifics/Android/AndroidTitleViewPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Windows.Input;
+
+namespace Maui.Controls.Sample.Pages
+{
+    public partial class AndroidTitleViewPage : ContentPage
+    {
+        ICommand _returnToPlatformSpecificsPage;
+
+        public AndroidTitleViewPage(ICommand restore)
+        {
+            InitializeComponent();
+            _returnToPlatformSpecificsPage = restore;
+        }
+
+        void OnReturnButtonClicked(object sender, EventArgs e)
+        {
+            _returnToPlatformSpecificsPage.Execute(null);
+        }
+    }
+}
