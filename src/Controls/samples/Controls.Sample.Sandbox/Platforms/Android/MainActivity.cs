dce97cfa36d1c357a005255c273ac7b703ff022e
Rachel Kang <rachelkang@microsoft.com>
2022-04-08 12:11:40 -0400

    Fix MainActivity

--- src/Controls/samples/Controls.Sample.Sandbox/Platforms/Android/MainActivity.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/Platforms/Android/MainActivity.cs
@@ -9,14 +9,4 @@ namespace Issue4169;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 public class MainActivity : MauiAppCompatActivity
 {
-	[Activity(
-		Theme = "@style/Maui.SplashTheme",
-		MainLauncher = true,
-		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode)]
-	[IntentFilter(
-		new[] { Microsoft.Maui.ApplicationModel.Platform.Intent.ActionAppAction },
-		Categories = new[] { Android.Content.Intent.CategoryDefault })]
-	public class MainActivity : MauiAppCompatActivity
-	{
-	}
 }
