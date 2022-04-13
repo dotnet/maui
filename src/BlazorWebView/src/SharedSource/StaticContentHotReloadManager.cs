e88f34d1892b67bbdd22a338de56c4518c5232dd
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2022-04-13 10:56:43 +0100

    Manager component ready to notify the JS code when update received

--- src/BlazorWebView/src/SharedSource/StaticContentHotReloadManager.cs
+++ src/BlazorWebView/src/SharedSource/StaticContentHotReloadManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Reflection.Metadata;
-using Microsoft.AspNetCore.Components.Rendering;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
 
 [assembly: MetadataUpdateHandler(typeof(Microsoft.AspNetCore.Components.WebView.StaticContentHotReloadManager))]
 
@@ -10,31 +11,48 @@ namespace Microsoft.AspNetCore.Components.WebView
 	{
 		public static readonly StaticContentHotReloadManager Default = new();
 
-		public bool MetadataUpdateSupported => MetadataUpdater.IsSupported;
-
-		public event Action<(string AssemblyName, string RelativePath, byte[] Contents)>? OnContentUpdate;
+		private delegate void ContentUpdateHandler(string assemblyName, string relativePath, byte[] contents);
+		private event ContentUpdateHandler? OnContentUpdate;
 
 		/// <summary>
 		/// MetadataUpdateHandler event. This is invoked by the hot reload host via reflection.
 		/// </summary>
 		public static void UpdateContent(string assemblyName, string relativePath, byte[] contents)
-			=> Default.OnContentUpdate?.Invoke((assemblyName, relativePath, contents));
+			=> Default.OnContentUpdate?.Invoke(assemblyName, relativePath, contents);
 
 		public void AttachToWebViewManagerIfEnabled(WebViewManager manager)
 		{
-			// To provide a consistent way of transporting the data across all platforms,
-			// we can use the existing IJSRuntime. In turn we can get an instance of this
-			// that's always attached to the currently-loaded page (if it's a Blazor page)
-			// by injecting a headless root component.
-			manager.AddRootComponentAsync(typeof(StaticContentUpdater), "body::after", ParameterView.Empty);
+			if (MetadataUpdater.IsSupported)
+			{
+				manager.AddRootComponentAsync(typeof(StaticContentUpdater), "body::after", ParameterView.Empty);
+			}
 		}
 
-		private class StaticContentUpdater : ComponentBase
+		// To provide a consistent way of transporting the data across all platforms,
+		// we can use the existing IJSRuntime. In turn we can get an instance of this
+		// that's always attached to the currently-loaded page (if it's a Blazor page)
+		// by injecting this headless root component.
+		private class StaticContentUpdater : IComponent, IDisposable
 		{
-			protected override void BuildRenderTree(RenderTreeBuilder builder)
+			[Inject] private IJSRuntime JSRuntime { get; set; } = default!;
+
+			public void Attach(RenderHandle renderHandle)
 			{
-				builder.AddContent(0, "HELLO THERE FROM THE INJECTED COMPONENT");
+				Default.OnContentUpdate += HandleContentUpdate;
 			}
+
+			public void Dispose()
+			{
+				Default.OnContentUpdate -= HandleContentUpdate;
+			}
+
+			private void HandleContentUpdate(string assemblyName, string relativePath, byte[] contents)
+			{
+				throw new NotImplementedException();
+			}
+
+			public Task SetParametersAsync(ParameterView parameters)
+				=> Task.CompletedTask;
 		}
 	}
 }
