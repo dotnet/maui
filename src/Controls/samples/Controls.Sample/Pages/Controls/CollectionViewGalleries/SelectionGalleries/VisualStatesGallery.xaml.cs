7e0dd60309ec376aea86fc8d62757268930ab21f
Anton Firszov <antonfir@gmail.com>
2022-04-19 21:12:45 +0200

    extend the samples gallery

---
+++ src/Controls/samples/Controls.Sample/Pages/Controls/CollectionViewGalleries/SelectionGalleries/VisualStatesGallery.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Pages.CollectionViewGalleries.SelectionGalleries
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VisualStatesGallery : ContentPage
+	{
+		public VisualStatesGallery()
+		{
+			InitializeComponent();
+			BindingContext = this;
+		}
+
+		public LineItem[] SingleSelectionItems { get; } = new LineItem[]
+		{
+			new LineItem() { ItemName = "Item 1" },
+			new LineItem() { ItemName = "Item 2" },
+			new LineItem() { ItemName = "Item 3" },
+		};
+
+		public LineItem[] MultiSelectionItems { get; } = new LineItem[]
+		{
+			new LineItem() { ItemName = "Item 1" },
+			new LineItem() { ItemName = "Item 2" },
+			new LineItem() { ItemName = "Item 3" },
+			new LineItem() { ItemName = "Item 4" },
+		};
+
+		public void SelChangedLol(object sender, SelectionChangedEventArgs args)
+		{
+			var collectionView = (CollectionView)sender;
+			System.Diagnostics.Debug.Print($"SEL: {collectionView.SelectedItem}");
+		}
+	}
+
+	public class LineItem
+	{
+		public string ItemName { get; set; }
+
+		public override string ToString() => ItemName;
+	}
+}
