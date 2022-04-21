a7665b58a3be95667292cd1e6e8dcf53a0b2c31b
Matthew Leibowitz <mattleibow@live.com>
2022-04-21 07:12:31 +0200

    Update PageHandler.cs

--- src/Core/src/Handlers/Page/PageHandler.cs
+++ src/Core/src/Handlers/Page/PageHandler.cs
@@ -5,10 +5,10 @@ namespace Microsoft.Maui.Handlers
 	{
 		public static new IPropertyMapper<IContentView, IPageHandler> Mapper = new PropertyMapper<IContentView, IPageHandler>(ContentViewHandler.Mapper)
 		{
-			[nameof(ITitledElement.Title)] = MapTitle,
 #if TIZEN
 			[nameof(IContentView.Background)] = MapBackground,
 #endif
+			[nameof(ITitledElement.Title)] = MapTitle,
 		};
 
 		public static new CommandMapper<IContentView, IPageHandler> CommandMapper = new(ContentViewHandler.CommandMapper)
