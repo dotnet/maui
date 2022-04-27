dae94d4d27cc024af6f44ee14eaacbce93a2d05c
Rachel Kang <rachelkang@microsoft.com>
2022-04-27 10:22:41 -0400

    Update counter text

--- src/Templates/src/templates/maui-mobile/MainPage.xaml.cs
+++ src/Templates/src/templates/maui-mobile/MainPage.xaml.cs
@@ -12,7 +12,11 @@ public partial class MainPage : ContentPage
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
-		CounterBtn.Text = $"Clicked {count} times";
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
