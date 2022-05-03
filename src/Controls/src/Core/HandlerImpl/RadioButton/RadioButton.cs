01f287aa7a1d0d04a2d2400f87db12dc572e3fd3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-05-03 18:01:51 +0200

    Removed unnecessary changes

--- src/Controls/src/Core/HandlerImpl/RadioButton/RadioButton.cs
+++ src/Controls/src/Core/HandlerImpl/RadioButton/RadioButton.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Maui.Controls
 		public static IPropertyMapper<RadioButton, RadioButtonHandler> ControlsRadioButtonMapper =
 			   new PropertyMapper<RadioButton, RadioButtonHandler>(RadioButtonHandler.Mapper)
 			   {
-#if IOS || ANDROID || WINDOWS
+#if IOS || ANDROID
 				   [nameof(IRadioButton.Content)] = MapContent
 #endif
 			   };
