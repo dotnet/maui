013c0ef8c78f2f730928c17abc50db46e893741e
Rachel Kang <rachelkang@microsoft.com>
2022-05-06 12:23:28 -0400

    Update FlyoutBackdrop logic

--- src/Controls/src/Core/Handlers/Shell/Windows/ShellView.cs
+++ src/Controls/src/Core/Handlers/Shell/Windows/ShellView.cs
@@ -40,6 +40,8 @@ namespace Microsoft.Maui.Controls.Platform
 			// If we set it earlier than this then WinUI will transition it back to false
 			if (IsPaneOpen != Element.FlyoutIsPresented)
 				IsPaneOpen = Element.FlyoutIsPresented;
+
+			UpdateFlyoutBackdrop();
 		}
 
 		private protected override void UpdateFlyoutCustomContent()
@@ -67,6 +69,7 @@ namespace Microsoft.Maui.Controls.Platform
 		{
 			_shellSplitView = new ShellSplitView(RootSplitView);
 			_shellSplitView.FlyoutBackdrop = _flyoutBackdrop;
+			UpdateFlyoutBackdrop();
 			TogglePaneButton?.SetAutomationPropertiesAutomationId("OK");
 
 			base.OnApplyTemplateCore();
