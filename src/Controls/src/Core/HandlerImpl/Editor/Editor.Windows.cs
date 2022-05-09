4cd37683ab852b5291f1448e61f4536dd1a35fee
Matthew Leibowitz <mattleibow@live.com>
2022-05-09 18:25:55 +0200

    Update Editor.Windows.cs

--- src/Controls/src/Core/HandlerImpl/Editor/Editor.Windows.cs
+++ src/Controls/src/Core/HandlerImpl/Editor/Editor.Windows.cs
@@ -2,14 +2,14 @@
 {
 	public partial class Editor
 	{
-		public static void MapDetectReadingOrderFromContent(EditorHandler handler, Editor editor)
-		{
-			Platform.InputViewExtensions.UpdateDetectReadingOrderFromContent(handler.PlatformView, editor);
-		}
-
 		public static void MapText(EditorHandler handler, Editor editor)
 		{
 			Platform.TextBoxExtensions.UpdateText(handler.PlatformView, editor);
+		}
+
+		public static void MapDetectReadingOrderFromContent(EditorHandler handler, Editor editor)
+		{
+			Platform.InputViewExtensions.UpdateDetectReadingOrderFromContent(handler.PlatformView, editor);
 		}
 	}
 }
