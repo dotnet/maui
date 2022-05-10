d78ce92966e5e2e1553e0a78b0b4c4ce07eb34ba
Gerald Versluis <gerald@verslu.is>
2022-05-10 16:00:31 +0200

    Update MauiProgram.cs

--- src/Controls/samples/Controls.Sample.Sandbox/MauiProgram.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MauiProgram.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Maui;
+﻿using System;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
 using Microsoft.Maui.Hosting;
 
 namespace Maui.Controls.Sample
@@ -14,9 +17,57 @@ namespace Maui.Controls.Sample
 				.Build();
 	}
 
-	class App : Application
+	class App : Microsoft.Maui.Controls.Application
 	{
-		protected override Window CreateWindow(IActivationState activationState) =>
-			new Window(new MainPage());
+
+		protected override Window CreateWindow(IActivationState activationState)
+		{
+			var button = new Button
+			{
+				Text = "Hello Sandbox!",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+			};
+
+			var contentPage = new ContentPage
+			{
+				Content = button
+			};
+
+			var mainPage = contentPage; // Shell { CurrentItem = contentPage }
+			//mainPage.Navigated += MainPage_Navigated;
+
+			var closeModalButton = new Button
+			{
+				Text = "Close",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+			};
+			closeModalButton.Clicked += (s, a) =>
+			{
+				mainPage.Navigation.PopModalAsync();
+			};
+
+			var popupPage = new ContentPage
+			{
+				Content = closeModalButton
+			};
+
+			popupPage.On<iOS>().SetModalPresentationStyle(UIModalPresentationStyle.FormSheet);
+			
+			button.Clicked += (s, a) =>
+			{
+				mainPage.Navigation.PushModalAsync(popupPage);
+			};
+
+			var window = new Window(mainPage);
+
+			return window;
+		}
+
+		private void MainPage_Navigated(object sender, ShellNavigatedEventArgs e)
+		{
+			Console.WriteLine("--- Navigated");
+		}
 	}
 }
