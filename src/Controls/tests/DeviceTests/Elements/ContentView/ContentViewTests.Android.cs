5ca4baec8220f5fb850d38cc6e6425cff4a150f5
Rui Marinho <me@ruimarinho.net>
2022-05-18 15:47:20 +0100

    [DeviceTests] Add device tests for ControlTemplate updates on ContentView

---
+++ src/Controls/tests/DeviceTests/Elements/ContentView/ContentViewTests.Android.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	public partial class ContentViewTests
+	{
+		static int GetChildCount(ContentViewHandler contentViewHandler)
+		{
+			return contentViewHandler.PlatformView.ChildCount;
+		}
+
+		static int GetContentChildCount(ContentViewHandler contentViewHandler)
+		{
+			return (contentViewHandler.PlatformView.GetChildAt(0) as LayoutViewGroup).ChildCount;
+		}
+	}
+}
---
+++ src/Controls/tests/DeviceTests/Elements/ContentView/ContentViewTests.Windows.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	public partial class ContentViewTests
+	{
+		static int GetChildCount(ContentViewHandler contentViewHandler)
+		{
+			return contentViewHandler.PlatformView.Children.Count;
+		}
+
+		static int GetContentChildCount(ContentViewHandler contentViewHandler)
+		{
+			return (contentViewHandler.PlatformView.Children[0] as LayoutPanel).Children.Count;
+		}
+	}
+}
