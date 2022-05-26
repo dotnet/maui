2e457e58b14db9e8ef464044cc447cc599b03e5d
Shane Neuville <shneuvil@microsoft.com>
2022-05-26 13:49:29 -0500

    Return VER for PlatformView
    - If the VisualElementRenderer doesn't set an internal control then just return the VER for the IElementHandler.PlatformView call

---
+++ src/Controls/tests/DeviceTests/Elements/Frame/FrameTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Handlers.Compatibility;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Platform;
+using Xunit;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	[Category(TestCategory.Frame)]
+	public partial class FrameTests : HandlerTestBase
+	{
+		void SetupBuilder()
+		{
+			EnsureHandlerCreated(builder =>
+			{
+				builder.ConfigureMauiHandlers(handlers =>
+				{
+					handlers.AddHandler<Frame, FrameRenderer>();
+					handlers.AddHandler<Label, LabelHandler>();
+				});
+			});
+		}
+
+		[Fact(DisplayName = "Basic Frame Test")]
+		public async Task BasicFrameTest()
+		{
+			SetupBuilder();
+
+			var frame = new Frame()
+			{
+				HeightRequest = 300,
+				WidthRequest = 300,
+				Content = new Label()
+				{
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+					Text = "Hello Frame"
+				}
+			};
+
+			var labelFrame =
+				await InvokeOnMainThreadAsync(() =>
+					frame.ToHandler(MauiContext).PlatformView.AttachAndRun(async () =>
+					{
+						frame.Measure(300, 300);
+						frame.Arrange(new Graphics.Rect(0, 0, 300, 300));
+
+						// This means the label has been added to the platforms visual tree
+						await OnLoadedAsync(frame.Content);
+						await OnFrameSetToNotEmpty(frame.Content);
+
+						return frame.Content.Frame;
+
+					})
+				);
+
+
+			// validate label is centered in the frame
+			Assert.True(Math.Abs(((300 - labelFrame.Width) / 2) - labelFrame.X) < 1);
+			Assert.True(Math.Abs(((300 - labelFrame.Height) / 2) - labelFrame.Y) < 1);
+		}
+	}
+}
