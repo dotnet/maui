ce94495361752bad78c8a6e029b638c86ebfe478
Matthew Leibowitz <mattleibow@live.com>
2022-06-05 07:41:54 +0200

    Make TemplatedView work as IContentView

--- src/Controls/src/Core/HandlerImpl/ContentView/ContentView.Impl.cs
+++ src/Controls/src/Core/HandlerImpl/ContentView/ContentView.Impl.cs
@@ -3,6 +3,7 @@
 	public partial class ContentView : IContentView
 	{
 		object IContentView.Content => Content;
+
 		IView IContentView.PresentedContent => ((this as IControlTemplated).TemplateRoot as IView) ?? Content;
 
 		protected override void OnApplyTemplate()
