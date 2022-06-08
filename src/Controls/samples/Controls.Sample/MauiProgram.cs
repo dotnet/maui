831298c664f3e7c3bce7fa38f7c1274f6b45d787
E.Z. Hart <hartez@gmail.com>
2022-06-08 10:27:52 -0600

    Revert samples changes

--- src/Controls/samples/Controls.Sample/MauiProgram.cs
+++ src/Controls/samples/Controls.Sample/MauiProgram.cs
@@ -32,7 +32,7 @@ namespace Maui.Controls.Sample
 		static bool UseMauiGraphicsSkia = false;
 
 		enum PageType { Main, Blazor, Shell, Template, FlyoutPage, TabbedPage }
-		readonly static PageType _pageType = PageType.Template;
+		readonly static PageType _pageType = PageType.Main;
 
 		public static MauiApp CreateMauiApp()
 		{
--- src/Controls/samples/Controls.Sample/Pages/Others/TemplatePage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/Others/TemplatePage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.Maui;
 
 namespace Maui.Controls.Sample.Pages
 {
@@ -8,28 +7,13 @@ namespace Maui.Controls.Sample.Pages
 		public TemplatePage()
 		{
 			InitializeComponent();
-
-			TheScrollView.PropertyChanged += (sender, args) => {
-				switch (args.PropertyName)
-				{
-					case "ContentSize":
-						System.Diagnostics.Debug.WriteLine($">>>>>> ContentSize Changed to {(sender as IScrollView).ContentSize}");
-						break;
-				}
-			};
-
-			UpdateButton.Clicked += (sender, args) => 
-			{
-				if (TheLabel.HeightRequest == 50)
-				{
-					TheLabel.HeightRequest = 75;
-				}
-				else
-				{
-					TheLabel.HeightRequest = 50;
-				}
-			};
 		}
 
+		int count = 0;
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			count++;
+			CounterLabel.Text = $"Current count: {count}";
+		}
 	}
 }
