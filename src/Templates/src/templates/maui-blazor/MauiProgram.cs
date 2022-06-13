cc781a14a47e1d402915a565077da63721964374
Tanay Parikh <TanayParikh@users.noreply.github.com>
2022-06-12 17:18:53 -0700

    Fix `maui-blazor` indentation
    Tabs -> spaces consistency.
    
    Fixes: https://github.com/dotnet/maui/issues/7934

--- src/Templates/src/templates/maui-blazor/MauiProgram.cs
+++ src/Templates/src/templates/maui-blazor/MauiProgram.cs
@@ -5,25 +5,25 @@ namespace MauiApp._1;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-		builder.Services.AddMauiBlazorWebView();
-		//-:cnd:noEmit
+        builder.Services.AddMauiBlazorWebView();
+        //-:cnd:noEmit
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
-		//+:cnd:noEmit
+        //+:cnd:noEmit
 
-		builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddSingleton<WeatherForecastService>();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
