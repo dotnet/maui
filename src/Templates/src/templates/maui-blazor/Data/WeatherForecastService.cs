3b124de328ef4194fb9fa1390ab87d889ef24675
Tanay Parikh <TanayParikh@users.noreply.github.com>
2022-06-13 13:35:48 -0700

    Fix `maui-blazor` indentation (#7976)
    Tabs -> spaces consistency.
    
    Fixes: https://github.com/dotnet/maui/issues/7934

--- src/Templates/src/templates/maui-blazor/Data/WeatherForecastService.cs
+++ src/Templates/src/templates/maui-blazor/Data/WeatherForecastService.cs
@@ -2,19 +2,19 @@ namespace MauiApp._1.Data;
 
 public class WeatherForecastService
 {
-	private static readonly string[] Summaries = new[]
-	{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
 
-	public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
-	{
-		return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-		{
-			Date = startDate.AddDays(index),
-			TemperatureC = Random.Shared.Next(-20, 55),
-			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-		}).ToArray());
-	}
+    public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+    {
+        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToArray());
+    }
 }
 
