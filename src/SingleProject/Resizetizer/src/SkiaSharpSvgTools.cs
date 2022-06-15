d88716cb583934b38c856174cfabd3499dc16ea7
Matthew Leibowitz <mattleibow@live.com>
2022-06-15 20:43:52 +0700

    Merge all the chnages from all the PRs

--- src/SingleProject/Resizetizer/src/SkiaSharpSvgTools.cs
+++ src/SingleProject/Resizetizer/src/SkiaSharpSvgTools.cs
@@ -10,12 +10,12 @@ namespace Microsoft.Maui.Resizetizer
 		SKSvg svg;
 
 		public SkiaSharpSvgTools(ResizeImageInfo info, ILogger logger)
-			: this(info.Filename, info.BaseSize, info.TintColor, logger)
+			: this(info.Filename, info.BaseSize, info.Color, info.TintColor, logger)
 		{
 		}
 
-		public SkiaSharpSvgTools(string filename, SKSize? baseSize, SKColor? tintColor, ILogger logger)
-			: base(filename, baseSize, tintColor, logger)
+		public SkiaSharpSvgTools(string filename, SKSize? baseSize, SKColor? backgroundColor, SKColor? tintColor, ILogger logger)
+			: base(filename, baseSize, backgroundColor, tintColor, logger)
 		{
 			var sw = new Stopwatch();
 			sw.Start();
