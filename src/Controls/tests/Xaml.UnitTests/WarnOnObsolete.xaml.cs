20ea3b8c653683720085914c114d00936cc9e32f
Stephane Delcroix <stephane@delcroix.org>
2022-06-16 14:38:08 +0200

    [X] Warn on setting Obsolete properties from Xaml
    Issue compile time warnings when trying to assign a deprecated
    BindableProperty, a deprecated CLR property, or using a deprecated
    setter for a CLR property.
    
    This PR was manually crafted from an experienced master of the art,
    without tools, IDE, code editor, or debugger. This is a unique piece and
    if you break it, a replicate will not be a perfect replicate, but a mere
    reinterpretation. A Certificate of Authenticity has been issued on the
    blockchain, so it must be real.
    
    - fix #8048

---
+++ src/Controls/tests/Xaml.UnitTests/WarnOnObsolete.xaml.cs
@@ -0,0 +1,40 @@
+using System;
+
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls.Core.UnitTests;
+using Microsoft.Maui.Devices;
+using Microsoft.Maui.Controls;
+
+using NUnit.Framework;
+
+namespace Microsoft.Maui.Controls.Xaml.UnitTests
+{
+	public partial class WarnOnObsolete : ContentPage
+	{
+		public WarnOnObsolete() => InitializeComponent();
+		public WarnOnObsolete(bool useCompiledXaml)
+		{
+			//this stub will be replaced at compile time
+		}
+
+		[TestFixture]
+		class Test
+		{
+			[SetUp] public void Setup() => AppInfo.SetCurrent(new MockAppInfo());
+			[TearDown] public void TearDown() => AppInfo.SetCurrent(null);
+		}
+	}
+
+	public class ViewWithObsolete : View
+	{
+		[Obsolete("Can't touch this")]
+		public static BindableProperty ObsoleteBPProperty = BindableProperty.Create("ObsoleteBP", typeof(string), typeof(ViewWithObsolete), null);
+
+		public string ObsoleteBP { get; set; }
+
+		[Obsolete("Can't touch this")]
+		public string ObsoleteProp { get; set; }
+
+		public string ObsoletePropSetter { get; [Obsolete("Can't touch this")]set; }
+	}
+}
