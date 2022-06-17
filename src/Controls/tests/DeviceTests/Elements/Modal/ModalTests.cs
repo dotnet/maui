39d368f69263c10e42bf64bf44a80f482254d2fb
Shane Neuville <shneuvil@microsoft.com>
2022-06-17 16:55:52 -0500

    Update ModalTests.cs

--- src/Controls/tests/DeviceTests/Elements/Modal/ModalTests.cs
+++ src/Controls/tests/DeviceTests/Elements/Modal/ModalTests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Handlers
+using Microsoft.Maui.Controls.Handlers;
 using Microsoft.Maui.DeviceTests.Stubs;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Hosting;
