6ee794b1246fb0fc8036fd9cddb4809f6d6a36d7
Shane Neuville <shneuvil@microsoft.com>
2022-06-22 12:27:11 -0500

    Update src/Controls/tests/DeviceTests/Elements/Shell/ShellTests.Android.cs
    Co-authored-by: Rachel Kang <rachelkang@microsoft.com>

--- src/Controls/tests/DeviceTests/Elements/Shell/ShellTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/Shell/ShellTests.Android.cs
@@ -158,7 +158,7 @@ namespace Microsoft.Maui.DeviceTests
 		}
 
 		[Fact]
-		public async Task FlyoutItemsRendererWhenFlyoutHeaderIsSet()
+		public async Task FlyoutItemsRenderWhenFlyoutHeaderIsSet()
 		{
 			SetupBuilder();
 			VerticalStackLayout header = new VerticalStackLayout()
