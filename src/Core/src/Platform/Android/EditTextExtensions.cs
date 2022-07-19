636767bc686f247edb4c6baddc76ec31996c67ad
E.Z. Hart <hartez@gmail.com>
2022-07-19 11:09:32 -0600

    Update clear button visibility when text changes

--- src/Core/src/Platform/Android/EditTextExtensions.cs
+++ src/Core/src/Platform/Android/EditTextExtensions.cs
@@ -15,6 +15,8 @@ namespace Microsoft.Maui.Platform
 	{
 		public static void UpdateText(this EditText editText, IEntry entry)
 		{
+			var previousTextLength = editText.Length();
+
 			// Setting the text causes the cursor to reset to position zero
 			// Therefore if:
 			// User Types => VirtualView Updated => Triggers Native Update
@@ -171,30 +173,16 @@ namespace Microsoft.Maui.Platform
 			editText.SetCursorVisible(isReadOnly);
 		}
 
+		// TODO: NET7 hartez - Remove this, nothing uses it
 		public static void UpdateClearButtonVisibility(this EditText editText, IEntry entry, Drawable? clearButtonDrawable) =>
 			UpdateClearButtonVisibility(editText, entry, () => clearButtonDrawable);
 
+		// TODO: NET7 hartez - Remove the getClearButtonDrawable parameter, nothing uses it
 		public static void UpdateClearButtonVisibility(this EditText editText, IEntry entry, Func<Drawable?>? getClearButtonDrawable)
 		{
-			// Places clear button drawable at the end or start of the EditText based on FlowDirection.
-			void ShowClearButton()
+			if (entry?.Handler is not EntryHandler entryHandler)
 			{
-				var drawable = getClearButtonDrawable?.Invoke();
-
-				if (entry.GetEffectiveFlowDirection() == FlowDirection.RightToLeft)
-				{
-					editText.SetCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
-				}
-				else
-				{
-					editText.SetCompoundDrawablesWithIntrinsicBounds(null, null, drawable, null);
-				}
-			}
-
-			// Hides clear button drawable from the control.
-			void HideClearButton()
-			{
-				editText.SetCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+				return;
 			}
 
 			bool isFocused = editText.IsFocused;
@@ -206,11 +194,11 @@ namespace Microsoft.Maui.Platform
 
 			if (shouldDisplayClearButton)
 			{
-				ShowClearButton();
+				entryHandler.ShowClearButton();
 			}
 			else
 			{
-				HideClearButton();
+				entryHandler.HideClearButton();
 			}
 		}
 
