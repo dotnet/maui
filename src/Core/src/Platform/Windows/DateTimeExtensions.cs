fe55f196d23d3b9984510c972f376376fac4ea2c
Shane Neuville <shneuvil@microsoft.com>
2022-07-20 10:49:41 -0500

    - remove public API changes

--- src/Core/src/Platform/Windows/DateTimeExtensions.cs
+++ src/Core/src/Platform/Windows/DateTimeExtensions.cs
@@ -2,11 +2,12 @@
 
 namespace Microsoft.Maui.Platform
 {
-	public static class DateTimeExtensions
+	// TODO NET7 MAKE PUBLIC
+	internal static class DateTimeExtensions
 	{
 		public static DateTimeOffset ToDateTimeOffset(this DateTime dateTime)
 		{
-			return dateTime.ToUniversalTime() <= DateTimeOffset.MinValue.UtcDateTime  
+			return dateTime.ToUniversalTime() <= DateTimeOffset.MinValue.UtcDateTime
 				? DateTimeOffset.MinValue
 				: new DateTimeOffset(dateTime);
 		}
