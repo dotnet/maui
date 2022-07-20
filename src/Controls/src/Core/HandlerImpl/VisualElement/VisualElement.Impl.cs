7508f39ba7d1158a9e5b97efa0046a38bb16302d
Javier Suárez <javiersuarezruiz@hotmail.com>
2022-07-20 22:15:03 +0200

    [Windows] Fix crash using animations and changing View sizes (negative values) (#8273)
    * Fix crash using animations and changing View sizes (negative values)
    
    * Renamed ValidatePositive method to EnsurePositive

--- src/Controls/src/Core/HandlerImpl/VisualElement/VisualElement.Impl.cs
+++ src/Controls/src/Core/HandlerImpl/VisualElement/VisualElement.Impl.cs
@@ -180,12 +180,14 @@ namespace Microsoft.Maui.Controls
 		internal Semantics SetupSemantics() =>
 			_semantics ??= new Semantics();
 
-		static void ValidatePositive(double value, string name)
+		static double EnsurePositive(double value)
 		{
 			if (value < 0)
 			{
-				throw new InvalidOperationException($"{name} cannot be less than zero.");
+				return 0;
 			}
+
+			return value;
 		}
 
 		double IView.Width
@@ -205,7 +207,7 @@ namespace Microsoft.Maui.Controls
 					return Primitives.Dimension.Unset;
 				}
 
-				ValidatePositive(value, nameof(IView.Width));
+				value = EnsurePositive(value);
 				return value;
 			}
 		}
@@ -227,7 +229,7 @@ namespace Microsoft.Maui.Controls
 					return Primitives.Dimension.Unset;
 				}
 
-				ValidatePositive(value, nameof(IView.Height));
+				value = EnsurePositive(value);
 				return value;
 			}
 		}
@@ -243,7 +245,7 @@ namespace Microsoft.Maui.Controls
 
 				// Access once up front to avoid multiple GetValue calls
 				var value = MinimumWidthRequest;
-				ValidatePositive(value, nameof(IView.MinimumWidth));
+				value = EnsurePositive(value);
 				return value;
 			}
 		}
@@ -259,7 +261,7 @@ namespace Microsoft.Maui.Controls
 
 				// Access once up front to avoid multiple GetValue calls
 				var value = MinimumHeightRequest;
-				ValidatePositive(value, nameof(IView.MinimumHeight));
+				value = EnsurePositive(value);
 				return value;
 			}
 		}
@@ -270,7 +272,7 @@ namespace Microsoft.Maui.Controls
 			{
 				// Access once up front to avoid multiple GetValue calls
 				var value = MaximumWidthRequest;
-				ValidatePositive(value, nameof(IView.MaximumWidth));
+				value = EnsurePositive(value);
 				return value;
 			}
 		}
@@ -281,7 +283,7 @@ namespace Microsoft.Maui.Controls
 			{
 				// Access once up front to avoid multiple GetValue calls
 				var value = MaximumHeightRequest;
-				ValidatePositive(value, nameof(IView.MaximumHeight));
+				value = EnsurePositive(value);
 				return value;
 			}
 		}
