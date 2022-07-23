766767692949af4dba2fc85c6dbe20e154ada630
Amr Al Sayed <hassaninamr@yahoo.com>
2022-07-23 16:33:10 +0200

    Subscribes to the property chain upto the application for theme binding.

--- src/Controls/src/Core/AppThemeBinding.cs
+++ src/Controls/src/Core/AppThemeBinding.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel;
+
 using Microsoft.Maui.ApplicationModel;
 
 namespace Microsoft.Maui.Controls
@@ -27,8 +29,8 @@ namespace Microsoft.Maui.Controls
 
 		internal override void Apply(object context, BindableObject bindObj, BindableProperty targetProperty, bool fromBindingContextChanged = false)
 		{
-			_weakTarget = new WeakReference<BindableObject>(bindObj);
-			_targetProperty = targetProperty;
+			Target = bindObj;
+			TargetProperty = targetProperty;
 			base.Apply(context, bindObj, targetProperty, fromBindingContextChanged);
 			ApplyCore();
 
@@ -40,33 +42,95 @@ namespace Microsoft.Maui.Controls
 			DetachEvents();
 
 			base.Unapply(fromBindingContextChanged);
-			_weakTarget = null;
-			_targetProperty = null;
+			TargetProperty = null;
+			Target = null;
 		}
 
 		void OnRequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
 			=> ApplyCore(true);
 
+		void OnApplicationChanging(object sender, ParentChangingEventArgs e)
+		{
+			if (Target is VisualElement { Window.Parent: Application app }) app.RequestedThemeChanged -= OnRequestedThemeChanged;
+		}
+
+		void OnApplicationChanged(object sender, EventArgs e)
+		{
+			if (Target is VisualElement { Window.Parent: Application app }) app.RequestedThemeChanged += OnRequestedThemeChanged;
+
+			ApplyCore(true);
+		}
+
+		void OnWindowChanging(object sender, PropertyChangingEventArgs e)
+		{
+			if (string.Equals(e.PropertyName, VisualElement.WindowProperty.PropertyName, StringComparison.Ordinal))
+				if (Target is VisualElement { Window: {} window })
+				{
+					window.ParentChanging -= OnApplicationChanging;
+					window.ParentChanged -= OnApplicationChanged;
+					if (window.Parent is Application app) app.RequestedThemeChanged -= OnRequestedThemeChanged;
+				}
+		}
+
+		void OnWindowChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (string.Equals(e.PropertyName, VisualElement.WindowProperty.PropertyName, StringComparison.Ordinal))
+			{
+				if (Target is VisualElement { Window: {} window })
+				{
+					if (window.Parent is Application app) app.RequestedThemeChanged += OnRequestedThemeChanged;
+
+					window.ParentChanging += OnApplicationChanging;
+					window.ParentChanged += OnApplicationChanged;
+				}
+
+				ApplyCore(true);
+			}
+		}
+
 		void ApplyCore(bool dispatch = false)
 		{
-			if (_weakTarget == null || !_weakTarget.TryGetTarget(out var target))
+			BindableObject target = Target;
+			if (target is null)
 			{
 				DetachEvents();
 				return;
 			}
+
+			BindableProperty targetProperty = TargetProperty;
+			if (targetProperty is null) return;
 
 			if (dispatch)
 				target.Dispatcher.DispatchIfRequired(Set);
 			else
 				Set();
 
-			void Set() => target.SetValueCore(_targetProperty, GetValue());
+			void Set() =>
+			target.SetValueCore(targetProperty, GetValue());
 		}
 
 		object _light;
 		object _dark;
 		bool _isLightSet;
 		bool _isDarkSet;
+
+		BindableObject Target
+		{
+			get =>
+			_weakTarget.TryGetTarget(out BindableObject target)
+			? target
+			: null;
+			set
+			{
+				DetachEvents();
+
+				_weakTarget = new WeakReference<BindableObject>(value);
+
+				AttachEvents();
+			}
+		}
+
+		BindableProperty TargetProperty { get; set; }
 
 		public object Light
 		{
@@ -90,6 +154,15 @@ namespace Microsoft.Maui.Controls
 
 		public object Default { get; set; }
 
+		AppTheme GetCurrentTheme()
+		{
+			return Target is VisualElement { Window.Parent: Application appFromTarget }
+			       ? appFromTarget.RequestedTheme
+			       : Application.Current is {} appFromStatic
+			       ? appFromStatic.RequestedTheme
+			       : AppInfo.RequestedTheme;
+		}
+
 		// Ideally this will get reworked to not use `Application.Current` at all
 		// https://github.com/dotnet/maui/issues/8713
 		// But I'm going with a simple nudge for now so that we can get our 
@@ -97,44 +170,49 @@ namespace Microsoft.Maui.Controls
 		// of the more crashing variety
 		object GetValue()
 		{
-			Application app;
-
-			if (_weakTarget?.TryGetTarget(out var target) == true &&
-				target is VisualElement ve &&
-				ve?.Window?.Parent is Application a)
-			{
-				app = a;
-			}
-			else
-			{
-				app = Application.Current;
-			}
-
-			AppTheme appTheme;
-			if (app == null)
-				appTheme = AppInfo.RequestedTheme;
-			else
-				appTheme = app.RequestedTheme;
-
-			return appTheme switch
-			{
-				AppTheme.Dark => _isDarkSet ? Dark : Default,
-				_ => _isLightSet ? Light : Default,
-			};
+			return GetCurrentTheme() switch
+			       {
+				       AppTheme.Dark => _isDarkSet
+				                        ? Dark
+				                        : Default,
+				       _ => _isLightSet
+				            ? Light
+				            : Default
+			       };
 		}
 
 		void AttachEvents()
 		{
 			DetachEvents();
 
-			if (Application.Current != null)
-				Application.Current.RequestedThemeChanged += OnRequestedThemeChanged;
+			if (Target is VisualElement newVisualElementTarget)
+			{
+				newVisualElementTarget.PropertyChanging += OnWindowChanging;
+				newVisualElementTarget.PropertyChanged += OnWindowChanged;
+				if (newVisualElementTarget.Window is {} newWindow)
+				{
+					newWindow.ParentChanging += OnApplicationChanging;
+					newWindow.ParentChanged += OnApplicationChanged;
+					if (newWindow.Parent is Application newApplication) newApplication.RequestedThemeChanged += OnRequestedThemeChanged;
+				}
+			}
 		}
 
 		void DetachEvents()
 		{
-			if (Application.Current != null)
-				Application.Current.RequestedThemeChanged -= OnRequestedThemeChanged;
+			if (Target is VisualElement oldVisualElementTarget)
+			{
+				if (oldVisualElementTarget.Window is {} oldWindow)
+				{
+					if (oldWindow.Parent is Application oldApplication) oldApplication.RequestedThemeChanged -= OnRequestedThemeChanged;
+
+					oldWindow.ParentChanging -= OnApplicationChanging;
+					oldWindow.ParentChanged -= OnApplicationChanged;
+				}
+
+				oldVisualElementTarget.PropertyChanging -= OnWindowChanging;
+				oldVisualElementTarget.PropertyChanged -= OnWindowChanged;
+			}
 		}
 	}
 }
