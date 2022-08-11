475002aa2ad30f00b18d9d46b80b57e643a78bf2
Shane Neuville <shneuvil@microsoft.com>
2022-08-11 15:08:59 -0500

    Switch to UIAction

--- src/Core/src/Handlers/MenuFlyoutItem/MenuFlyoutItemHandler.iOS.cs
+++ src/Core/src/Handlers/MenuFlyoutItem/MenuFlyoutItemHandler.iOS.cs
@@ -16,36 +16,35 @@ namespace Microsoft.Maui.Handlers
 		{
 			int index = menus.Count;
 			UIImage? uiImage = VirtualView.Source.GetPlatformMenuImage(MauiContext!);
-			var selector = new Selector($"MenuItem{index}:");
 
-			bool selectorFound =
-				MauiUIApplicationDelegate.Current.RespondsToSelector(selector);
-
-			if (!selectorFound)
-			{
-				throw new InvalidOperationException(
-					$"By default we only support 50 MenuItems. You can add more by adding the following code to {MauiUIApplicationDelegate.Current.GetType()}\n\n" +
-					$"[Export(\"MenuItem{index}: \")]\n" +
-					$"internal void MenuItem{index}(UICommand uICommand)\n" +
-					"{\n" +
-					"	uICommand.SendClicked();\n" +
-					"}");
-			}
-
-			var command = UICommand.Create(
+			var command = UIAction.Create(
 				title: VirtualView.Text,
 				uiImage,
-				selector,
-				new NSString($"{index}"));
+				$"M{index}:",
+				OnMenuClicked);
 
 			menus[index] = VirtualView;
 			return command;
+		}
+
+		static void OnMenuClicked(UIAction uIAction)
+		{
+
 		}
 
 		internal static void Execute(UICommand uICommand)
 		{
 			if (uICommand.PropertyList is NSString nsString &&
 				Int32.TryParse(nsString.ToString(), out int index))
+			{
+				menus[index].Clicked();
+			}
+		}
+
+		internal static void Execute(UIAction uICommand)
+		{
+			if (uICommand.Identifier != null &&
+				Int32.TryParse(uICommand.Identifier, out int index))
 			{
 				menus[index].Clicked();
 			}
