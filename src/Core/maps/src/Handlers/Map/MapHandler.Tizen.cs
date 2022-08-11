fda5ca2c6fb55141d713f3d916c47564876cef12
Rui Marinho <me@ruimarinho.net>
2022-08-11 20:52:31 +0100

    [Maps] Add Tizen handlers

---
+++ src/Core/maps/src/Handlers/Map/MapHandler.Tizen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Maui.Handlers;
+
+namespace Microsoft.Maui.Maps.Handlers
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+	public partial class MapHandler : ViewHandler<IMap, object>
+	{
+
+		protected override object CreatePlatformView() => throw new NotImplementedException();
+
+		public static void MapMapType(IMapHandler handler, IMap map) => throw new NotImplementedException();
+
+		public static void MapIsShowingUser(IMapHandler handler, IMap map) => throw new NotImplementedException();
+
+		public static void MapHasScrollEnabled(IMapHandler handler, IMap map) => throw new NotImplementedException();
+
+
+		public static void MapHasTrafficEnabled(IMapHandler handler, IMap map) => throw new NotImplementedException();
+
+		public static void MapHasZoomEnabled(IMapHandler handler, IMap map) => throw new NotImplementedException();
+
+		public static void MapMoveToRegion(IMapHandler handler, IMap map, object? arg) => throw new NotImplementedException();
+
+		public static void MapPins(IMapHandler handler, IMap map) => throw new NotImplementedException();
+
+		public static void MapElements(IMapHandler handler, IMap map) => throw new NotImplementedException();
+	}
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
