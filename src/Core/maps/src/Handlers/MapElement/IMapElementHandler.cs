8abf41ace481f86db085344044141dd1ff349698
Rui Marinho <me@ruimarinho.net>
2022-08-12 11:48:17 +0100

    Fix Tizen

--- src/Core/maps/src/Handlers/MapElement/IMapElementHandler.cs
+++ src/Core/maps/src/Handlers/MapElement/IMapElementHandler.cs
@@ -6,9 +6,9 @@ using Android.OS;
 using Android.Runtime;
 using PlatformView = Java.Lang.Object;
 #elif WINDOWS
-using PlatformView = ElmSharp.EvasObject;
-#elif TIZEN
 using PlatformView = System.Object;
+#elif TIZEN
+using PlatformView = ElmSharp.EvasObject;
 #elif (NETSTANDARD || !PLATFORM) || (NET6_0 && !IOS && !ANDROID && !TIZEN)
 using PlatformView = System.Object;
 #endif
