0a4f6e8032a9ed6c7fd7cdc404872fa424ad8cd1
Shane Neuville <shneuvil@microsoft.com>
2022-08-15 16:09:57 -0500

    - inline creating of args

--- src/Controls/src/Core/ContextFlyout.cs
+++ src/Controls/src/Core/ContextFlyout.cs
@@ -120,8 +120,7 @@ namespace Microsoft.Maui.Controls
 
         void NotifyHandler(string action, int index, IMenuElement view)
         {
-            var args = new Maui.Handlers.ContextFlyoutItemHandlerUpdate(index, view);
-            Handler?.Invoke(action, args);
+            Handler?.Invoke(action, new Maui.Handlers.ContextFlyoutItemHandlerUpdate(index, view));
         }
     }
 }
