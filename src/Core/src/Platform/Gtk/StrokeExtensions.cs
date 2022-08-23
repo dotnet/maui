aa559724564340d9fcf5a5e6784a5f3f105d314a
lytico <lytico@users.sourceforge.net>
2022-08-23 20:32:34 +0200

    [Gtk] Core.csproj: track api changes IV + StrokeExtensions

---
+++ src/Core/src/Platform/Gtk/StrokeExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.Maui.Platform
+{
+	public static class StrokeExtensions
+	{
+		public static void UpdateStrokeShape(this Gtk.Widget platformView, IBorderStroke border) { }
+
+		public static void UpdateStroke(this Gtk.Widget platformView, IBorderStroke border) { }
+
+		public static void UpdateStrokeThickness(this Gtk.Widget platformView, IBorderStroke border) { }
+
+		public static void UpdateStrokeDashPattern(this Gtk.Widget platformView, IBorderStroke border) { }
+
+		public static void UpdateStrokeDashOffset(this Gtk.Widget platformView, IBorderStroke border) { }
+
+		public static void UpdateStrokeMiterLimit(this Gtk.Widget platformView, IBorderStroke border) { }
+
+		public static void UpdateStrokeLineCap(this Gtk.Widget platformView, IBorderStroke border) { }
+
+		public static void UpdateStrokeLineJoin(this Gtk.Widget platformView, IBorderStroke border) { }
+	}
+}
