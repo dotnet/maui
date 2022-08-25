67e3562e7731739fdea9306808526b4632bf0dce
Shane Neuville <shneuvil@microsoft.com>
2022-08-25 09:42:32 -0500

    Secondary ButtonMask (#9589)
    * Add Secondary ButtonMask to TapGesture
    
    * - fix up gallery
    
    * - add right or left click
    
    * - fix unshipped APIs
    
    * - wire up gesture recognizer positions
    
    * - update catalyst and android
    
    * - wire up context menu locations
    
    * - add tests for adding/removing interactions
    
    * - add android checks for secondary button
    
    * - fix accessibility check
    
    - fix send tapped
    
    * - remove additional noise
    
    * - fix gallery and clean up code for pixel conversions
    
    * - fix instantiation calls

--- src/Controls/samples/Controls.Sample/Pages/Core/TapGestureGalleryPage.cs
+++ src/Controls/samples/Controls.Sample/Pages/Core/TapGestureGalleryPage.cs
@@ -8,16 +8,25 @@ namespace Maui.Controls.Sample.Pages
 	{
 		Command TapCommand;
 		Label changeColorBoxView;
+		Label windowPosition = new Label();
+		Label relativeToToggleButtonPosition = new Label();
+		Label relativeToContainerPosition = new Label();
+		Button toggleButton;
 
 		public TapGestureGalleryPage()
 		{
 			TapCommand = new Command<Color>(HandleTapCommand);
+
 			var vertical = new VerticalStackLayout
 			{
 				VerticalOptions = LayoutOptions.Center,
 				HorizontalOptions = LayoutOptions.Center,
 				Spacing = 40
 			};
+
+			vertical.Add(windowPosition);
+			vertical.Add(relativeToToggleButtonPosition);
+			vertical.Add(relativeToContainerPosition);
 
 			var horizontal = new HorizontalStackLayout
 			{
@@ -38,6 +47,7 @@ namespace Maui.Controls.Sample.Pages
 				CommandParameter = Colors.PaleGreen,
 				NumberOfTapsRequired = 1,
 			};
+			singleTapGesture.Tapped += OnTapped;
 			singleTapLabel.GestureRecognizers.Add(singleTapGesture);
 			horizontal.Add(singleTapLabel);
 
@@ -52,6 +62,7 @@ namespace Maui.Controls.Sample.Pages
 				CommandParameter = Colors.Aqua,
 				NumberOfTapsRequired = 2,
 			};
+			doubleTapGesture.Tapped += OnTapped;
 			doubleTapLabel.GestureRecognizers.Add(doubleTapGesture);
 			horizontal.Add(doubleTapLabel);
 
@@ -65,8 +76,7 @@ namespace Maui.Controls.Sample.Pages
 			};
 			vertical.Add(changeColorBoxView);
 
-
-			vertical.Add(new Button()
+			toggleButton = new Button()
 			{
 				Text = "Toggle Single Tap Gesture",
 				Command = new Command(() =>
@@ -76,15 +86,77 @@ namespace Maui.Controls.Sample.Pages
 					else
 						singleTapLabel.GestureRecognizers.Add(singleTapGesture);
 				})
-			});
+			};
+
+			vertical.Add(toggleButton);
+
+			var tripleClicklabel = new Label
+			{
+				Text = "Triple click me!!!",
+				BackgroundColor = Colors.Olive
+			};
+			var tripleClickGesture = new TapGestureRecognizer
+			{
+				Command = TapCommand,
+				CommandParameter = Colors.Olive,
+				NumberOfTapsRequired = 3,
+				Buttons = ButtonsMask.Primary
+			};
+			tripleClickGesture.Tapped += OnTapped;
+			tripleClicklabel.GestureRecognizers.Add(tripleClickGesture);
+			horizontal.Children.Add(tripleClicklabel);
+
+			var rightClickLabel = new Label
+			{
+				Text = "Right click me¡",
+				BackgroundColor = Colors.Coral
+			};
+			var rigthClickGesture = new TapGestureRecognizer
+			{
+				Command = TapCommand,
+				CommandParameter = Colors.Coral,
+				NumberOfTapsRequired = 1,
+				Buttons = ButtonsMask.Secondary
+			};
+			rigthClickGesture.Tapped += OnTapped;
+			rightClickLabel.GestureRecognizers.Add(rigthClickGesture);
+			horizontal.Children.Add(rightClickLabel);
+
+
+			var rightorLeftClickLabel = new Label
+			{
+				Text = "Right or Left click me¡",
+				BackgroundColor = Colors.Green
+			};
+			var rigthOrLeftClickGesture = new TapGestureRecognizer
+			{
+				Command = TapCommand,
+				CommandParameter = Colors.Green,
+				NumberOfTapsRequired = 1,
+				Buttons = ButtonsMask.Secondary | ButtonsMask.Primary
+			};
+
+			rigthOrLeftClickGesture.Tapped += OnTapped;
+			rightorLeftClickLabel.GestureRecognizers.Add(rigthOrLeftClickGesture);
+			horizontal.Children.Add(rightorLeftClickLabel);
+
 
 			Content = vertical;
 		}
 
-		async void HandleTapCommand(Color backgroundColor)
+		void OnTapped(object sender, System.EventArgs e)
+		{
+			var args = (TappedEventArgs)e;
+			var view = (View)sender;
+
+			windowPosition.Text = $"Position inside window: {args.GetPosition(null)}";
+			relativeToToggleButtonPosition.Text = $"Position relative to toggle button: {args.GetPosition(toggleButton)}";
+			relativeToContainerPosition.Text = $"Position inside my view: {args.GetPosition(view)}";
+		}
+
+		void HandleTapCommand(Color backgroundColor)
 		{
 			changeColorBoxView.BackgroundColor = backgroundColor;
-			await DisplayAlert("Tapped", "Tap Command Fired", "Close");
 		}
 	}
 }
