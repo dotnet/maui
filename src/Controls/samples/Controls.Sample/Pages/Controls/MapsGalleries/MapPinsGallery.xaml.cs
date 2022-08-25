39df840a508d8488a1402942be9b3ec7a5c96dda
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-08-25 12:09:24 +0200

    Added more samples to the gallery

--- src/Controls/samples/Controls.Sample/Pages/Controls/MapsGalleries/MapPinsGallery.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/Controls/MapsGalleries/MapPinsGallery.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.Maui.Controls.Maps;
 using Microsoft.Maui.Controls.Xaml;
-using Microsoft.Maui.Devices.Sensors;
+using Position = Microsoft.Maui.Devices.Sensors.Location;
 
 namespace Maui.Controls.Sample.Pages.MapsGalleries
 {
@@ -12,43 +12,43 @@ namespace Maui.Controls.Sample.Pages.MapsGalleries
 		private int _locationIncrement = 0;
 
 		// TODO generate actual random pins
-		private readonly Location[] _randomLocations =
+		private readonly Position[] _randomLocations =
 		{
-			new Location(51.8833333333333, 176.65),
-			new Location(21.3166666666667, 157.833333333333),
-			new Location(71.3, 156.766666666667),
-			new Location(19.7, 155.083333333333),
-			new Location(61.2166666666667, 149.9),
-			new Location(70.2, 148.516666666667),
-			new Location(64.85, 147.716666666667),
-			new Location(57.05, 135.333333333333),
-			new Location(60.7166666666667, 135.05),
-			new Location(58.3, 134.416666666667),
-			new Location(69.45, 133.033333333333),
-			new Location(48.4333333333333, 123.366666666667),
-			new Location(49.25, 123.1),
-			new Location(45.5166666666667, 122.683333333333),
-			new Location(37.7833333333333, 122.416666666667),
-			new Location(47.6166666666667, 122.333333333333),
-			new Location(38.55, 121.466666666667),
-			new Location(50.6833333333333, 120.333333333333),
-			new Location(39.5333333333333, 119.816666666667),
-			new Location(34.4333333333333, 119.716666666667),
-			new Location(49.8833333333333, 119.5),
-			new Location(55.1666666666667, 118.8),
-			new Location(34.05, 118.25),
-			new Location(33.95, 117.4),
-			new Location(32.7166666666667, 117.166666666667),
-			new Location(32.5333333333333, 117.033333333333),
-			new Location(31.85, 116.6),
-			new Location(43.6166666666667, 116.2),
-			new Location(32.6666666666667, 115.466666666667),
-			new Location(36.1833333333333, 115.133333333333),
-			new Location(62.45, 114.4),
-			new Location(51.05, 114.066666666667),
-			new Location(53.5333333333333, 113.5),
-			new Location(33.45, 112.066666666667),
-			new Location(46.6, 112.033333333333),
+			new Position(51.8833333333333, 176.65),
+			new Position(21.3166666666667, 157.833333333333),
+			new Position(71.3, 156.766666666667),
+			new Position(19.7, 155.083333333333),
+			new Position(61.2166666666667, 149.9),
+			new Position(70.2, 148.516666666667),
+			new Position(64.85, 147.716666666667),
+			new Position(57.05, 135.333333333333),
+			new Position(60.7166666666667, 135.05),
+			new Position(58.3, 134.416666666667),
+			new Position(69.45, 133.033333333333),
+			new Position(48.4333333333333, 123.366666666667),
+			new Position(49.25, 123.1),
+			new Position(45.5166666666667, 122.683333333333),
+			new Position(37.7833333333333, 122.416666666667),
+			new Position(47.6166666666667, 122.333333333333),
+			new Position(38.55, 121.466666666667),
+			new Position(50.6833333333333, 120.333333333333),
+			new Position(39.5333333333333, 119.816666666667),
+			new Position(34.4333333333333, 119.716666666667),
+			new Position(49.8833333333333, 119.5),
+			new Position(55.1666666666667, 118.8),
+			new Position(34.05, 118.25),
+			new Position(33.95, 117.4),
+			new Position(32.7166666666667, 117.166666666667),
+			new Position(32.5333333333333, 117.033333333333),
+			new Position(31.85, 116.6),
+			new Position(43.6166666666667, 116.2),
+			new Position(32.6666666666667, 115.466666666667),
+			new Position(36.1833333333333, 115.133333333333),
+			new Position(62.45, 114.4),
+			new Position(51.05, 114.066666666667),
+			new Position(53.5333333333333, 113.5),
+			new Position(33.45, 112.066666666667),
+			new Position(46.6, 112.033333333333),
 		};
 
 		public MapPinsGallery()
@@ -59,7 +59,7 @@ namespace Maui.Controls.Sample.Pages.MapsGalleries
 			{
 				Address = "One Microsoft Way, Redmond, USA",
 				Label = "Microsoft Visitors Center",
-				Location = new Location(47.64232, -122.13684),
+				Location = new Position(47.64232, -122.13684),
 			};
 
 			microsoftPin.MarkerClicked += (s, a) =>
