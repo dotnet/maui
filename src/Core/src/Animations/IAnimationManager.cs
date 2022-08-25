7c05cf8613593b460bfc07b12457f0d2cdc69eb3
Gerald Versluis <gerald.versluis@microsoft.com>
2022-08-25 14:27:27 +0200

    Some Animation classes

--- src/Core/src/Animations/IAnimationManager.cs
+++ src/Core/src/Animations/IAnimationManager.cs
@@ -1,15 +1,34 @@
-﻿namespace Microsoft.Maui.Animations
+﻿namespace Microsoft.Maui.Animations;
+
+/// <summary>
+/// This class is responsible for controlling the animations in .NET MAUI.
+/// </summary>
+public interface IAnimationManager
 {
-	public interface IAnimationManager
-	{
-		ITicker Ticker { get; }
+	/// <summary>
+	/// The ticker that is used by this manager to time the animations.
+	/// </summary>
+	ITicker Ticker { get; }
 
-		double SpeedModifier { get; set; }
+	/// <summary>
+	/// Specifies a factor with which the animations for this manager should be multiplied.
+	/// </summary>
+	double SpeedModifier { get; set; }
 
-		bool AutoStartTicker { get; set; }
+	/// <summary>
+	/// Specifies whether or not to automatically start the animation ticker for this manager.
+	/// </summary>
+	bool AutoStartTicker { get; set; }
 
-		void Add(Animation animation);
+	/// <summary>
+	/// Adds the given animation to this manager. 
+	/// </summary>
+	/// <param name="animation">The animation to be added.</param>
+	void Add(Animation animation);
 
-		void Remove(Animation animation);
-	}
+	/// <summary>
+	/// Removes the given animation from this manager. 
+	/// </summary>
+	/// <param name="animation">The animation to be removed.</param>
+	void Remove(Animation animation);
 }
--- src/Core/src/Animations/Ticker.cs
+++ src/Core/src/Animations/Ticker.cs
@@ -1,48 +1,48 @@
 ﻿using System;
 using System.Timers;
 
-namespace Microsoft.Maui.Animations
+namespace Microsoft.Maui.Animations;
+
+/// <inheritdoc/>
+public class Ticker : ITicker
 {
-	public class Ticker : ITicker
+	Timer? _timer;
+
+	public virtual int MaxFps { get; set; } = 60;
+
+	public Action? Fire { get; set; }
+
+	public virtual bool IsRunning => _timer?.Enabled ?? false;
+
+	public virtual bool SystemEnabled => true;
+
+	public virtual void Start()
 	{
-		Timer? _timer;
+		if (_timer != null)
+			return;
 
-		public virtual int MaxFps { get; set; } = 60;
-
-		public Action? Fire { get; set; }
-
-		public virtual bool IsRunning => _timer?.Enabled ?? false;
-
-		public virtual bool SystemEnabled => true;
-
-		public virtual void Start()
+		_timer = new Timer
 		{
-			if (_timer != null)
-				return;
-
-			_timer = new Timer
-			{
-				AutoReset = true,
-				Interval = 1000 / MaxFps,
-			};
-			_timer.Elapsed += OnTimerElapsed;
-			_timer.AutoReset = true;
-			_timer.Start();
-		}
-
-		public virtual void Stop()
-		{
-			if (_timer == null)
-				return;
-
-			_timer.AutoReset = false;
-			_timer.Stop();
-			_timer.Elapsed -= OnTimerElapsed;
-			_timer.Dispose();
-			_timer = null;
-		}
-
-		void OnTimerElapsed(object? sender, ElapsedEventArgs e) =>
-			Fire?.Invoke();
+			AutoReset = true,
+			Interval = 1000 / MaxFps,
+		};
+		_timer.Elapsed += OnTimerElapsed;
+		_timer.AutoReset = true;
+		_timer.Start();
 	}
+
+	public virtual void Stop()
+	{
+		if (_timer == null)
+			return;
+
+		_timer.AutoReset = false;
+		_timer.Stop();
+		_timer.Elapsed -= OnTimerElapsed;
+		_timer.Dispose();
+		_timer = null;
+	}
+
+	void OnTimerElapsed(object? sender, ElapsedEventArgs e) =>
+		Fire?.Invoke();
 }
