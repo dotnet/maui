2a3c23f5bd781b8e989a33f93e456f9f449a63b3
Gerald Versluis <gerald.versluis@microsoft.com>
2022-08-25 14:27:27 +0200

    Some Animation classes

--- src/Core/src/Animations/AnimationManager.cs
+++ src/Core/src/Animations/AnimationManager.cs
@@ -1,106 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Microsoft.Maui.Animations
+namespace Microsoft.Maui.Animations;
+
+/// <inheritdoc/>
+public class AnimationManager : IAnimationManager, IDisposable
 {
-	public class AnimationManager : IAnimationManager, IDisposable
+	readonly List<Animation> _animations = new();
+	long _lastUpdate;
+	bool _disposedValue;
+
+	/// <inheritdoc/>
+	public AnimationManager(ITicker ticker)
 	{
-		readonly List<Animation> _animations = new();
-		long _lastUpdate;
-		bool _disposedValue;
+		_lastUpdate = GetCurrentTick();
 
-		public AnimationManager(ITicker ticker)
+		Ticker = ticker;
+		Ticker.Fire = OnFire;
+	}
+
+	/// <inheritdoc/>
+	public ITicker Ticker { get; }
+
+	/// <inheritdoc/>
+	public double SpeedModifier { get; set; } = 1;
+
+	/// <inheritdoc/>
+	public bool AutoStartTicker { get; set; } = true;
+
+	/// <inheritdoc/>
+	public void Add(Animation animation)
+	{
+		// If animations are disabled, don't do anything
+		if (!Ticker.SystemEnabled)
+			return;
+
+		if (!_animations.Contains(animation))
+			_animations.Add(animation);
+		if (!Ticker.IsRunning && AutoStartTicker)
+			Start();
+	}
+
+	/// <inheritdoc/>
+	public void Remove(Animation animation)
+	{
+		_animations.TryRemove(animation);
+
+		if (_animations.Count == 0)
+			End();
+	}
+
+	void Start()
+	{
+		_lastUpdate = GetCurrentTick();
+		Ticker.Start();
+	}
+
+	void End() =>
+		Ticker?.Stop();
+
+	long GetCurrentTick() =>
+		Environment.TickCount & int.MaxValue;
+
+	void OnFire()
+	{
+		var now = GetCurrentTick();
+		var milliseconds = TimeSpan.FromMilliseconds(now - _lastUpdate).TotalMilliseconds;
+		_lastUpdate = now;
+
+		var animations = new List<Animation>(_animations);
+		animations.ForEach(OnAnimationTick);
+
+		if (_animations.Count == 0)
+			End();
+
+		void OnAnimationTick(Animation animation)
 		{
-			_lastUpdate = GetCurrentTick();
-
-			Ticker = ticker;
-			Ticker.Fire = OnFire;
-		}
-
-		public ITicker Ticker { get; }
-
-		public double SpeedModifier { get; set; } = 1;
-
-		public bool AutoStartTicker { get; set; } = true;
-
-		public void Add(Animation animation)
-		{
-			// If animations are disabled, don't do anything
-			if (!Ticker.SystemEnabled)
+			if (animation.HasFinished)
+			{
+				_animations.TryRemove(animation);
+				animation.RemoveFromParent();
 				return;
+			}
 
-			if (!_animations.Contains(animation))
-				_animations.Add(animation);
-			if (!Ticker.IsRunning && AutoStartTicker)
-				Start();
-		}
+			animation.Tick(milliseconds * SpeedModifier);
 
-		public void Remove(Animation animation)
-		{
-			_animations.TryRemove(animation);
-
-			if (_animations.Count == 0)
-				End();
-		}
-
-		void Start()
-		{
-			_lastUpdate = GetCurrentTick();
-			Ticker.Start();
-		}
-
-		void End() =>
-			Ticker?.Stop();
-
-		long GetCurrentTick() =>
-			Environment.TickCount & int.MaxValue;
-
-		void OnFire()
-		{
-			var now = GetCurrentTick();
-			var milliseconds = TimeSpan.FromMilliseconds(now - _lastUpdate).TotalMilliseconds;
-			_lastUpdate = now;
-
-			var animations = new List<Animation>(_animations);
-			animations.ForEach(OnAnimationTick);
-
-			if (_animations.Count == 0)
-				End();
-
-			void OnAnimationTick(Animation animation)
+			if (animation.HasFinished)
 			{
-				if (animation.HasFinished)
-				{
-					_animations.TryRemove(animation);
-					animation.RemoveFromParent();
-					return;
-				}
-
-				animation.Tick(milliseconds * SpeedModifier);
-
-				if (animation.HasFinished)
-				{
-					_animations.TryRemove(animation);
-					animation.RemoveFromParent();
-				}
+				_animations.TryRemove(animation);
+				animation.RemoveFromParent();
 			}
 		}
+	}
 
-		protected virtual void Dispose(bool disposing)
+	/// <inheritdoc/>
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!_disposedValue)
 		{
-			if (!_disposedValue)
-			{
-				if (disposing && Ticker is IDisposable disposable)
-					disposable.Dispose();
+			if (disposing && Ticker is IDisposable disposable)
+				disposable.Dispose();
 
-				_disposedValue = true;
-			}
+			_disposedValue = true;
 		}
+	}
 
-		public void Dispose()
-		{
-			Dispose(disposing: true);
-			GC.SuppressFinalize(this);
-		}
+	/// <inheritdoc/>
+	public void Dispose()
+	{
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
 	}
 }
