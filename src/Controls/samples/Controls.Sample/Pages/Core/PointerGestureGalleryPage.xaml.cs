467a2fa75194e1662908aa77f169e81fd9c8756b
Rachel Kang <rachelkang@microsoft.com>
2022-08-25 13:55:27 -0400

    Add sample to GestureGallery

---
+++ src/Controls/samples/Controls.Sample/Pages/Core/PointerGestureGalleryPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Pages
+{
+	public partial class PointerGestureGalleryPage
+	{
+		public PointerGestureGalleryPage()
+		{
+			InitializeComponent();
+		}
+
+		private void HoverBegan(object sender, PointerEventArgs e)
+		{
+			hoverLabel.Text = "Thanks for hovering me! You can hover off now";
+		}
+
+		private void HoverEnded(object sender, PointerEventArgs e)
+		{
+			hoverLabel.Text = "Hover me again!";
+		}
+	}
+}
--- src/Controls/samples/Controls.Sample/ViewModels/GesturesViewModel.cs
+++ src/Controls/samples/Controls.Sample/ViewModels/GesturesViewModel.cs
@@ -19,6 +19,8 @@ namespace Maui.Controls.Sample.ViewModels
 				"Swipe Gesture."),
 			new SectionModel(typeof(TapGestureGalleryPage), "Tap Gesture",
 				"Tap Gesture."),
+			new SectionModel(typeof(PointerGestureGalleryPage), "Pointer Gesture",
+				"Pointer Gesture."),
 		};
 	}
 }
