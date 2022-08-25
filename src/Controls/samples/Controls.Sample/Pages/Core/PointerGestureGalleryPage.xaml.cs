b8d88773678103fd584d2a4d0858c7bb22ca99cd
Rachel Kang <rachelkang@microsoft.com>
2022-08-25 17:28:00 -0400

    Update control gallery sample

--- src/Controls/samples/Controls.Sample/Pages/Core/PointerGestureGalleryPage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/Core/PointerGestureGalleryPage.xaml.cs
@@ -11,12 +11,18 @@ namespace Maui.Controls.Sample.Pages
 
 		private void HoverBegan(object sender, PointerEventArgs e)
 		{
-			hoverLabel.Text = "Thanks for hovering me! You can hover off now";
+			hoverLabel.Text = "Thanks for hovering me!";
 		}
 
 		private void HoverEnded(object sender, PointerEventArgs e)
 		{
 			hoverLabel.Text = "Hover me again!";
+			positionLabel.Text = "Hover above label to reveal pointer position again";
+		}
+
+		private void HoverMoved(object sender, PointerEventArgs e)
+		{
+			positionLabel.Text = $"Pointer position is at: {e.GetPosition((View)sender)}";
 		}
 	}
 }
