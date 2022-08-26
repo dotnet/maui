99d99c130255ec48ddecd3263174f61e5d3bb009
Matthew Leibowitz <mattleibow@live.com>
2022-08-26 21:37:08 +0200

    Fix CI (#9682)

--- src/Core/maps/src/Handlers/MapElement/MapElementHandler.cs
+++ src/Core/maps/src/Handlers/MapElement/MapElementHandler.cs
@@ -5,7 +5,7 @@ using PlatformView = Java.Lang.Object;
 #elif WINDOWS
 using PlatformView = System.Object;
 #elif TIZEN
-using PlatformView = Tizen.NUI.BaseComponents.View;
+using PlatformView = System.Object;
 #elif (NETSTANDARD || !PLATFORM) || (NET6_0 && !IOS && !ANDROID && !TIZEN)
 using PlatformView = System.Object;
 #endif
--- src/Core/maps/src/Handlers/MapPin/MapPinHandler.Tizen.cs
+++ src/Core/maps/src/Handlers/MapPin/MapPinHandler.Tizen.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.Maui.Handlers;
 namespace Microsoft.Maui.Maps.Handlers
 {
-	public partial class MapPinHandler : ElementHandler<IMapPin, Tizen.NUI.BaseComponents.View>
+	public partial class MapPinHandler : ElementHandler<IMapPin, object>
 	{
-		protected override Tizen.NUI.BaseComponents.View CreatePlatformElement() => throw new System.NotImplementedException();
+		protected override object CreatePlatformElement() => throw new System.NotImplementedException();
 		public static void MapLocation(IMapPinHandler handler, IMapPin mapPin) { }
 
 		public static void MapLabel(IMapPinHandler handler, IMapPin mapPin) { }
