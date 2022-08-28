b818a95b355915b793d76b8e0454f5cba81fe716
Matthew Leibowitz <mattleibow@live.com>
2022-08-28 17:13:56 +0200

    Enable Window Sizing (#4942)

--- src/Core/src/Core/IWindow.cs
+++ src/Core/src/Core/IWindow.cs
@@ -1,3 +1,4 @@
+using Microsoft.Maui.Graphics;
 using System.Collections.Generic;
 
 namespace Microsoft.Maui
@@ -21,6 +22,46 @@ namespace Microsoft.Maui
 		/// Gets the read only collection of Window Overlays on top of the Window.
 		/// </summary>
 		IReadOnlyCollection<IWindowOverlay> Overlays { get; }
+
+		/// <summary>
+		/// Gets the specified X coordinate of the IWindow.
+		/// </summary>
+		double X { get; }
+
+		/// <summary>
+		/// Gets the specified Y coordinate of the IWindow.
+		/// </summary>
+		double Y { get; }
+
+		/// <summary>
+		/// Gets the specified width of the IWindow.
+		/// </summary>
+		double Width { get; }
+
+		/// <summary>
+		/// Gets the specified minimum width constraint of the IWindow, between zero and double.PositiveInfinity.
+		/// </summary>
+		double MinimumWidth { get; }
+
+		/// <summary>
+		/// Gets the specified maximum width constraint of the IWindow, between zero and double.PositiveInfinity.
+		/// </summary>
+		double MaximumWidth { get; }
+
+		/// <summary>
+		/// Gets the specified height of the IWindow.
+		/// </summary>
+		double Height { get; }
+
+		/// <summary>
+		/// Gets the specified minimum height constraint of the IWindow, between zero and double.PositiveInfinity.
+		/// </summary>
+		double MinimumHeight { get; }
+
+		/// <summary>
+		/// Gets the specified maximum height constraint of the IWindow, between zero and double.PositiveInfinity.
+		/// </summary>
+		double MaximumHeight { get; }
 
 		/// <summary>
 		/// Adds a Window Overlay to the current Window.
@@ -80,6 +121,8 @@ namespace Microsoft.Maui
 		FlowDirection FlowDirection { get; }
 
 		void DisplayDensityChanged(float displayDensity);
+
+		void FrameChanged(Rect frame);
 
 		float RequestDisplayDensity();
 	}
