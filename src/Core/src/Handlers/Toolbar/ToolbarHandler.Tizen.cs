78bbf06ba97f9f46621b0c70df5c001dcef1f2b7
Sunghyun Min <shyun.min@samsung.com>
2022-08-30 13:35:36 +0900

    [Tizen] Update handling IconPressed event of MauiToolbar

--- src/Core/src/Handlers/Toolbar/ToolbarHandler.Tizen.cs
+++ src/Core/src/Handlers/Toolbar/ToolbarHandler.Tizen.cs
@@ -1,12 +1,39 @@
-﻿namespace Microsoft.Maui.Handlers
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Maui.Handlers
 {
 	public partial class ToolbarHandler : ElementHandler<IToolbar, MauiToolbar>
 	{
 		protected override MauiToolbar CreatePlatformElement() => new();
 
-		public static void MapTitle(IToolbarHandler arg1, IToolbar arg2)
+		protected override void ConnectHandler(MauiToolbar platformView)
 		{
-			arg1.PlatformView.UpdateTitle(arg2);
+			platformView.IconPressed += OnIconPressed;
+			base.ConnectHandler(platformView);
+		}
+
+		protected override void DisconnectHandler(MauiToolbar platformView)
+		{
+			platformView.IconPressed -= OnIconPressed;
+			base.DisconnectHandler(platformView);
+		}
+
+		public static void MapTitle(IToolbarHandler handler, IToolbar toolbar)
+		{
+			handler.PlatformView.UpdateTitle(toolbar);
+		}
+
+		async void OnIconPressed(object? sender, EventArgs args)
+		{
+			if (VirtualView.BackButtonVisible && VirtualView.IsVisible)
+			{
+				// Delays invoking the BackButtonClicked
+				// so the other attached events can be invoked before the pop behavior is done on a FlyoutPage.
+				await Task.Delay(100);
+
+				MauiContext?.GetPlatformWindow().GetWindow()?.BackButtonClicked();
+			}
 		}
 	}
 }
