52bc12987e0aa766d9e65ba16cd541732999de07
Kangho Hur <kangho.hur@samsung.com>
2022-09-01 22:31:30 +0900

    [Tizen] Update SkiaGraphicsView.Tizen.cs (#9767)
    * [Tizen] Update SkiaGraphicsView.Tizen.cs
    
    * Update SkiaSharp version to 2.88.2-preview5
    
    * Update SkiaSharpNativeAssetsVersion
    
    * Use stable skiasharp feed
    
    * Update NuGet.Config
    
    * Update GraphicsTester.Skia.Tizen

--- src/Graphics/src/Graphics.Skia/Views/SkiaGraphicsView.Tizen.cs
+++ src/Graphics/src/Graphics.Skia/Views/SkiaGraphicsView.Tizen.cs
@@ -1,7 +1,8 @@
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Graphics.Skia;
-using SkiaSharp.Views.Tizen;
-using ElmSharp;
+using SkiaSharp.Views.Tizen.NUI;
+using Tizen.NUI;
+using SKPaintSurfaceEventArgs = SkiaSharp.Views.Tizen.SKPaintSurfaceEventArgs;
 
 namespace Microsoft.Maui.Graphics.Skia.Views
 {
@@ -11,7 +12,7 @@ namespace Microsoft.Maui.Graphics.Skia.Views
 		private SkiaCanvas _canvas;
 		private ScalingCanvas _scalingCanvas;
 
-		public SkiaGraphicsView(EvasObject parent, IDrawable drawable = null) : base(parent)
+		public SkiaGraphicsView(IDrawable drawable = null) : base()
 		{
 			_canvas = new SkiaCanvas();
 			_scalingCanvas = new ScalingCanvas(_canvas);
@@ -32,14 +33,13 @@ namespace Microsoft.Maui.Graphics.Skia.Views
 
 		protected virtual void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
 		{
-			if (_drawable == null)
-				return;
+			if (_drawable == null) return;
 
 			var skiaCanvas = e.Surface.Canvas;
 			skiaCanvas.Clear();
 
 			_canvas.Canvas = skiaCanvas;
-			_drawable.Draw(_scalingCanvas, new RectF(0, 0, GetSurfaceSize().Width, GetSurfaceSize().Height));
+			_drawable.Draw(_scalingCanvas, new RectF(0, 0, e.Info.Width , e.Info.Height));
 		}
 	}
 }
