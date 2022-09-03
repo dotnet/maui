707744ab6f3049fe0b3c687e940a58eae85daef9
evolcano <evolcano@gmail.com>
2022-09-03 23:34:20 +0800

    Update GridExtensions.cs
    compatible with add method of Grid  in Xamarin.

--- src/Controls/src/Core/Layout/GridExtensions.cs
+++ src/Controls/src/Core/Layout/GridExtensions.cs
@@ -17,5 +17,25 @@ namespace Microsoft.Maui.Controls
 			grid.SetColumn(view, column);
 			grid.SetRow(view, row);
 		}
+		
+		public static void Add(this Grid grid, IView view, int left, int right, int top, int bottom)
+        {
+            if (view == null)
+                throw new ArgumentNullException(nameof(view));
+            if (left < 0)
+                throw new ArgumentOutOfRangeException(nameof(left));
+            if (right < 1 || right < left + 1)
+                throw new ArgumentOutOfRangeException(nameof(right));
+            if (top < 0)
+                throw new ArgumentOutOfRangeException(nameof(top));
+            if (bottom < 1 || bottom < top + 1)
+                throw new ArgumentOutOfRangeException(nameof(bottom));
+
+            grid.Add(view);
+            grid.SetColumn(view, left);
+            grid.SetRow(view, top);
+            grid.SetColumnSpan(view, right - left);
+            grid.SetRowSpan(view, bottom - top);
+        }
 	}
 }
