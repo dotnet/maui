a3b78f2a0a79402b830cf7f86533920ca935b156
lytico <lytico@users.sourceforge.net>
2022-09-07 16:34:43 +0200

    [Graphics.Gtk] introduce GtkImageLoadingService

--- src/Graphics/src/Graphics.Gtk/Gtk/GtkImage.cs
+++ src/Graphics/src/Graphics.Gtk/Gtk/GtkImage.cs
@@ -5,9 +5,10 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Maui.Graphics.Platform.Gtk;
 
-public class GtkImage : IImage {
-
-	public GtkImage(Gdk.Pixbuf pix) {
+public class GtkImage : IImage
+{
+	public GtkImage(Gdk.Pixbuf pix)
+	{
 		_pixbuf = pix;
 	}
 
@@ -16,11 +17,13 @@ public class GtkImage : IImage {
 	// https://developer.gnome.org/gdk-pixbuf/stable/gdk-pixbuf-The-GdkPixbuf-Structure.html
 	public Gdk.Pixbuf? NativeImage => _pixbuf;
 
-	public void Draw(ICanvas canvas, RectF dirtyRect) {
-		canvas.DrawImage(this, dirtyRect.Left, dirtyRect.Top, (float) Math.Round(dirtyRect.Width), (float) Math.Round(dirtyRect.Height));
+	public void Draw(ICanvas canvas, RectF dirtyRect)
+	{
+		canvas.DrawImage(this, dirtyRect.Left, dirtyRect.Top, (float)Math.Round(dirtyRect.Width), (float)Math.Round(dirtyRect.Height));
 	}
 
-	public void Dispose() {
+	public void Dispose()
+	{
 		var previousValue = Interlocked.Exchange(ref _pixbuf, null);
 		previousValue?.Dispose();
 	}
@@ -30,25 +33,30 @@ public class GtkImage : IImage {
 	public float Height => NativeImage?.Width ?? 0;
 
 	[GtkMissingImplementation]
-	public IImage Downsize(float maxWidthOrHeight, bool disposeOriginal = false) {
+	public IImage Downsize(float maxWidthOrHeight, bool disposeOriginal = false)
+	{
 		return this;
 	}
 
 	[GtkMissingImplementation]
-	public IImage Downsize(float maxWidth, float maxHeight, bool disposeOriginal = false) {
+	public IImage Downsize(float maxWidth, float maxHeight, bool disposeOriginal = false)
+	{
 		return this;
 	}
 
 	[GtkMissingImplementation]
-	public IImage Resize(float width, float height, ResizeMode resizeMode = ResizeMode.Fit, bool disposeOriginal = false) {
+	public IImage Resize(float width, float height, ResizeMode resizeMode = ResizeMode.Fit, bool disposeOriginal = false)
+	{
 		return this;
 	}
 
-	public void Save(Stream stream, ImageFormat format = ImageFormat.Png, float quality = 1) {
+	public void Save(Stream stream, ImageFormat format = ImageFormat.Png, float quality = 1)
+	{
 		NativeImage.SaveToStream(stream, format, quality);
 	}
 
-	public async Task SaveAsync(Stream stream, ImageFormat format = ImageFormat.Png, float quality = 1) {
+	public async Task SaveAsync(Stream stream, ImageFormat format = ImageFormat.Png, float quality = 1)
+	{
 		await Task.Run(() => NativeImage.SaveToStream(stream, format, quality));
 	}
 
@@ -60,5 +68,12 @@ public class GtkImage : IImage {
 		return context.Image;
 	}
 
-	public IImage ToPlatformImage()=> this;
+	public IImage ToPlatformImage() => this;
+
+	public static IImage FromStream(Stream stream, ImageFormat formatHint)
+	{
+		var platformHint = formatHint.ToImageExtension();
+		var pix = new Gdk.Pixbuf(stream);
+		return new GtkImage(pix);
+	}
 }
---
+++ src/Graphics/src/Graphics.Gtk/Gtk/GtkImageLoadingService.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Microsoft.Maui.Graphics.Platform.Gtk;
+
+	public class GtkImageLoadingService : IImageLoadingService
+	{
+		public IImage FromStream(Stream stream, ImageFormat formatHint = ImageFormat.Png)
+		{
+			return GtkImage.FromStream(stream, formatHint);
+		}
+	}
+
