1ce2ecdc8afa8646e4ed5d234e0a45a47dcd46ce
lytico <lytico@users.sourceforge.net>
2022-09-07 15:27:08 +0200

    [Core.Gtk] SemanticExtensions: remove slack

--- src/Core/src/Platform/Gtk/SemanticExtensions.cs
+++ src/Core/src/Platform/Gtk/SemanticExtensions.cs
@@ -1,11 +1,7 @@
 ﻿namespace Microsoft.Maui
 {
-	public static partial class SemanticExtensions_
+	public static partial class SemanticExtensions
 	{
-		/// <summary>
-		/// Force semantic screen reader focus to specified element
-		/// </summary>
-		/// <param name="element"></param>
-		public static void SetSemanticFocus(this IView element) { }
+
 	}
 }
