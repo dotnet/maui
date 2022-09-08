b65f1b4880eb23b2046a7e5a268a1b6d0c97e7a2
lytico <lytico@users.sourceforge.net>
2022-09-08 16:35:45 +0200

    [Controls.Gtk] [Controls.Compatibility.Gtk] move ResourcesProvider & Forms: Register ResourcesProvider

--- src/Compatibility/Core/src/Gtk/Forms.cs
+++ src/Compatibility/Core/src/Gtk/Forms.cs
@@ -1,18 +1,14 @@
 using System;
 using System.Reflection;
+using Microsoft.Maui.Controls.Compatibility.Platform.Gtk;
 
 namespace Microsoft.Maui.Controls.Compatibility
 {
-
 	public static class Forms
 	{
-
 		public static void Init(IActivationState state)
 		{
-			// var gtkServices = new GtkPlatformServices();
-			// Device.PlatformServices = gtkServices;
+			DependencyService.Register<ResourcesProvider>();
 		}
-
 	}
-
 }
