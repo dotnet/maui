eb3137ad7be18e42d91b6003d19901c0840beb03
Gerald Versluis <gerald.versluis@microsoft.com>
2022-09-09 13:51:45 +0200

    Second batch

--- src/Core/src/Fonts/IFontRegistrar.cs
+++ src/Core/src/Fonts/IFontRegistrar.cs
@@ -3,12 +3,31 @@ using System.Reflection;
 
 namespace Microsoft.Maui
 {
+	/// <summary>
+	/// The <see cref="IFontRegistrar"/> keeps track of the fonts that are registered in our application.
+	/// </summary>
 	public interface IFontRegistrar
 	{
+		/// <summary>
+		/// Registers a font in the app font cache.
+		/// </summary>
+		/// <param name="filename">The filename of the font to register.</param>
+		/// <param name="alias">An optional alias with which you can also refer to this font.</param>
+		/// <param name="assembly">The assembly (not used).</param>
 		void Register(string filename, string? alias, Assembly assembly);
 
+		/// <summary>
+		/// Registers a font in the app font cache.
+		/// </summary>
+		/// <param name="filename"></param>
+		/// <param name="alias"></param>
 		void Register(string filename, string? alias);
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="font"></param>
+		/// <returns></returns>
 		// TODO: this should be async as it involves copying files
 		string? GetFont(string font);
 	}
