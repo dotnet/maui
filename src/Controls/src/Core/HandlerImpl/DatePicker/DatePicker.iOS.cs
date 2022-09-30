c438e41754f3796901b36b3f6311c48136d2c7f5
Shane Neuville <shneuvil@microsoft.com>
2022-09-29 23:38:12 -0500

    - fix dph

--- src/Controls/src/Core/HandlerImpl/DatePicker/DatePicker.iOS.cs
+++ src/Controls/src/Core/HandlerImpl/DatePicker/DatePicker.iOS.cs
@@ -7,7 +7,8 @@ namespace Microsoft.Maui.Controls
 	{
 		public static void MapUpdateMode(IDatePickerHandler handler, DatePicker datePicker)
 		{
-			handler.UpdateImmediately = datePicker.OnThisPlatform().UpdateMode() == UpdateMode.Immediately;
+			if (handler is DatePickerHandler dph)
+				dph.UpdateImmediately = datePicker.OnThisPlatform().UpdateMode() == UpdateMode.Immediately;
 		}
 
 		public static void MapUpdateMode(DatePickerHandler handler, DatePicker datePicker) =>
