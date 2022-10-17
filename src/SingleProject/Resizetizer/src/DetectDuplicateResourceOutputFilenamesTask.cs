895ca2b416926ad956f1ec6425452c6bcd9490a6
Matthew Leibowitz <mattleibow@live.com>
2022-10-17 14:51:41 +0200

    Detect duplicate MauiImage items

---
+++ src/SingleProject/Resizetizer/src/DetectDuplicateResourceOutputFilenamesTask.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Microsoft.Maui.Resizetizer
+{
+	public class DetectDuplicateResourceOutputFilenamesTask : Task
+	{
+		public ITaskItem[] Items { get; set; }
+
+		[Required]
+		public string WarningMessage { get; set; }
+
+		[Output]
+		public string[] InvalidItems { get; set; }
+
+		public override bool Execute()
+		{
+			try
+			{
+				if (Items != null)
+				{
+					var infos = ResizeImageInfo.Parse(Items);
+
+					// group the items
+					var filenames = new Dictionary<string, List<ResizeImageInfo>>();
+					foreach (var info in infos)
+					{
+						var filename = info.OutputName;
+						if (!filenames.TryGetValue(filename, out var list))
+						{
+							list = new List<ResizeImageInfo>();
+							filenames[filename] = list;
+						}
+						list.Add(info);
+					}
+
+					// build up the warning
+					var builder = new StringBuilder();
+					builder.Append(WarningMessage);
+
+					// select duplicates
+					var invalidItems = new List<string>();
+					var idupe = 0;
+					foreach (var pair in filenames)
+					{
+						if (pair.Value.Count > 1)
+						{
+							if (idupe > 0)
+								builder.Append("; ");
+							idupe++;
+
+							builder.Append(pair.Key + " -> ");
+
+							var ifname = 0;
+							foreach (var item in pair.Value)
+							{
+								if (ifname > 0)
+									builder.Append(", ");
+								ifname++;
+
+								builder.Append(item.Filename);
+
+								invalidItems.Add(item.Filename);
+							}
+						}
+					}
+
+					if (invalidItems.Count > 0)
+					{
+						InvalidItems = invalidItems.ToArray();
+
+						Log.LogError(builder.ToString());
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.LogErrorFromException(ex);
+			}
+
+			return !Log.HasLoggedErrors;
+		}
+	}
+}
---
+++ src/SingleProject/Resizetizer/test/UnitTests/DetectDuplicateResourceOutputFilenamesTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using SkiaSharp;
+using Xunit;
+
+namespace Microsoft.Maui.Resizetizer.Tests
+{
+	public class DetectDuplicateResourceOutputFilenamesTests
+	{
+		public class ExecuteForApp : MSBuildTaskTestFixture<DetectDuplicateResourceOutputFilenamesTask>
+		{
+			protected DetectDuplicateResourceOutputFilenamesTask GetNewTask(params ITaskItem[] items) =>
+				new DetectDuplicateResourceOutputFilenamesTask
+				{
+					Items = items,
+					WarningMessage = "Duplicate Filenames: ",
+					BuildEngine = this,
+				};
+
+			protected string GetInvalidFilename(DetectDuplicateResourceOutputFilenamesTask task, string path) =>
+				task.InvalidItems.Single(c => c.Replace('\\', '/').EndsWith(path, StringComparison.Ordinal));
+
+			protected void AssertValidFilename(DetectDuplicateResourceOutputFilenamesTask task, ITaskItem item)
+				=> Assert.DoesNotContain(task.InvalidItems ?? Enumerable.Empty<string>(), c => c == item.ItemSpec);
+
+			protected void AssertInvalidFilename(DetectDuplicateResourceOutputFilenamesTask task, ITaskItem item)
+				=> Assert.Contains(task.InvalidItems ?? Enumerable.Empty<string>(), c => c == item.ItemSpec);
+
+			[Fact]
+			public void NoItemsSucceed()
+			{
+				var task = GetNewTask();
+
+				var success = task.Execute();
+
+				Assert.True(success);
+			}
+
+			[Fact]
+			public void NullItemsSucceed()
+			{
+				var task = GetNewTask(null);
+
+				var success = task.Execute();
+
+				Assert.True(success);
+			}
+
+			[Fact]
+			public void NonExistantFileFails()
+			{
+				var items = new[]
+				{
+					new TaskItem("non-existant.png"),
+				};
+
+				var task = GetNewTask(items);
+
+
+				var success = task.Execute();
+
+				Assert.False(success);
+			}
+
+			[Fact]
+			public void ValidFileSucceeds()
+			{
+				var i = new TaskItem("images/camera.png");
+				var task = GetNewTask(i);
+
+				var success = task.Execute();
+
+				AssertValidFilename(task, i);
+				Assert.True(success);
+			}
+
+
+			[Fact]
+			public void InvalidFileFails()
+			{
+				var i = new TaskItem("images/appiconfg-red-512.svg");
+				var task = GetNewTask(i);
+
+				var success = task.Execute();
+
+				AssertInvalidFilename(task, i);
+				Assert.False(success);
+			}
+
+			[Fact]
+			public void SingleInvalidFileFailsWithCorrectErrorMessage()
+			{
+				var i = new TaskItem("images/appiconfg-red-512.svg");
+				var task = GetNewTask(i);
+
+				var success = task.Execute();
+				Assert.False(success);
+
+				Assert.Equal("Invalid Filenames: appiconfg-red-512", LogErrorEvents[0].Message);
+			}
+
+			[Fact]
+			public void MultipleInvalidFileFailsWithCorrectErrorMessage()
+			{
+				var i = new TaskItem("images/appiconfg-red-512.svg");
+				var j = new TaskItem("images/appiconfg-red-512.svg");
+				var task = GetNewTask(i, j);
+
+				var success = task.Execute();
+				Assert.False(success);
+
+				Assert.Equal("Invalid Filenames: appiconfg-red-512, appiconfg-red-512", LogErrorEvents[0].Message);
+			}
+		}
+	}
+}
