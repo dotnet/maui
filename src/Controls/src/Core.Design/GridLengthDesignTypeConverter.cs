832246edcd954ef641de22afb4c02e4f06a86042
ET <48451158+etvorun@users.noreply.github.com>
2022-10-27 16:01:50 -0700

    Design time converters should return TRUE for CanConvertFrom(typeof(string)) (#10969)

--- src/Controls/src/Core.Design/GridLengthDesignTypeConverter.cs
+++ src/Controls/src/Core.Design/GridLengthDesignTypeConverter.cs
@@ -6,6 +6,10 @@ namespace Microsoft.Maui.Controls.Design
 {
 	public class GridLengthDesignTypeConverter : TypeConverter
 	{
+		// This tells XAML this converter can be used to process strings
+		// Without this the values won't show up as hints
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+			=> sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
 
 		public override bool IsValid(ITypeDescriptorContext context, object value)
 		{
