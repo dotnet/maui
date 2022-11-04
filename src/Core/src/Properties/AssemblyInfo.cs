a1a20f41738d670e89c443608e2f4e291ac4f7fe
Shane Neuville <shneuvil@microsoft.com>
2022-11-03 21:38:01 -0500

    - move all core specific tests to new project

--- src/Core/src/Properties/AssemblyInfo.cs
+++ src/Core/src/Properties/AssemblyInfo.cs
@@ -34,3 +34,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("CommunityToolkit.Maui.Markup.UnitTests")]
 [assembly: InternalsVisibleTo("Reloadify-emit")]
 [assembly: InternalsVisibleTo("Microsoft.Maui.TestUtils.DeviceTests.Runners")]
+[assembly: InternalsVisibleTo("Microsoft.Maui.DeviceTests.Shared")]
