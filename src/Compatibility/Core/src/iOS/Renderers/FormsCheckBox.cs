197dfe8ea156830f88c01b5ff0aa68f183141bf5
Rui Marinho <me@ruimarinho.net>
2022-11-09 11:37:49 +0000

    Fix more api warnings

--- src/Compatibility/Core/src/iOS/Renderers/FormsCheckBox.cs
+++ src/Compatibility/Core/src/iOS/Renderers/FormsCheckBox.cs
@@ -22,7 +22,7 @@ namespace Microsoft.Maui.Controls.Compatibility.Platform.iOS
 		public EventHandler CheckedChanged;
 		bool _disposed;
 
-#pragma warning disable CA1416 // TODO: ContentEdgeInsets, AdjustsImageWhenDisabled, AdjustsImageWhenHighlighted unsupported from version 15.0
+#pragma warning disable CA1416, CA1422 // TODO: ContentEdgeInsets, AdjustsImageWhenDisabled, AdjustsImageWhenHighlighted unsupported from version 15.0
 		internal float MinimumViewSize
 		{
 			get { return _minimumViewSize; }
@@ -44,7 +44,7 @@ namespace Microsoft.Maui.Controls.Compatibility.Platform.iOS
 			AdjustsImageWhenDisabled = false;
 			AdjustsImageWhenHighlighted = false;
 		}
-#pragma warning restore CA1416
+#pragma warning restore CA1416, CA1422
 
 		void OnTouchUpInside(object sender, EventArgs e)
 		{
