2f84c0a85d12b40f5727a478d3a87488e45859ce
E.Z. Hart <hartez@gmail.com>
2022-11-11 12:58:29 -0700

    Rebase on updated handlertestbase changes; roll protobuf into Accessibility.Droid

--- src/Core/tests/DeviceTests.Shared/TestCategory.cs
+++ src/Core/tests/DeviceTests.Shared/TestCategory.cs
@@ -1,4 +1,10 @@
-﻿namespace Microsoft.Maui.DeviceTests
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Maui.DeviceTests
 {
 	public static class TestCategory
 	{
@@ -7,18 +13,24 @@
 		public const string Accessibility = "Accessibility";
 		public const string Application = "Application";
 		public const string ActivityIndicator = "ActivityIndicator";
+		public const string Behavior = "Behavior";
 		public const string Border = "Border";
 		public const string BoxView = "BoxView";
 		public const string Button = "Button";
 		public const string CheckBox = "CheckBox";
+		public const string Compatibility = "Compatibility";
 		public const string ContentView = "ContentView";
 		public const string DatePicker = "DatePicker";
 		public const string Dispatcher = "Dispatcher";
 		public const string Editor = "Editor";
+		public const string Element = "Element";
 		public const string Entry = "Entry";
 		public const string FlowDirection = "FlowDirection";
+		public const string FlyoutPage = "FlyoutPage";
 		public const string FlyoutView = "FlyoutView";
 		public const string Fonts = "Fonts";
+		public const string Frame = "Frame";
+		public const string Gesture = "Gesture";
 		public const string GraphicsView = "GraphicsView";
 		public const string Image = "Image";
 		public const string ImageButton = "ImageButton";
@@ -26,6 +38,10 @@
 		public const string IndicatorView = "IndicatorView";
 		public const string Label = "Label";
 		public const string Layout = "Layout";
+		public const string ListView = "ListView";
+		public const string MenuFlyout = "MenuFlyout";
+		public const string Modal = "Modal";
+		public const string NavigationPage = "NavigationPage";
 		public const string NavigationView = "NavigationView";
 		public const string Page = "Page";
 		public const string Picker = "Picker";
@@ -34,12 +50,18 @@
 		public const string ScrollView = "ScrollView";
 		public const string SearchBar = "SearchBar";
 		public const string ShapeView = "ShapeView";
+		public const string Shell = "Shell";
 		public const string Slider = "Slider";
 		public const string Stepper = "Stepper";
 		public const string Switch = "Switch";
+		public const string TabbedPage = "TabbedPage";
+		public const string TemplatedView = "TemplatedView";
 		public const string TextFormatting = "Formatting";
 		public const string TimePicker = "TimePicker";
+		public const string Toolbar = "Toolbar";
 		public const string View = "View";
+		public const string VisualElement = "VisualElement";
+		public const string VisualElementTree = "VisualElementTree";
 		public const string WebView = "WebView";
 		public const string Window = "Window";
 		public const string WindowOverlay = "WindowOverlay";
