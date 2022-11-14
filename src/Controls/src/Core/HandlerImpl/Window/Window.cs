5f2005b9b15a280a97afea5bfe6d5a98a8b58e20
Shane Neuville <shneuvil@microsoft.com>
2022-11-14 16:25:14 -0600

    Set window soft input via window mapper

--- src/Controls/src/Core/HandlerImpl/Window/Window.cs
+++ src/Controls/src/Core/HandlerImpl/Window/Window.cs
@@ -5,7 +5,14 @@ namespace Microsoft.Maui.Controls
 {
 	public partial class Window
 	{
-		public static IPropertyMapper<IWindow, WindowHandler> ControlsWindowMapper = new PropertyMapper<IWindow, WindowHandler>(WindowHandler.Mapper);
+		public static IPropertyMapper<IWindow, WindowHandler> ControlsWindowMapper =
+			new PropertyMapper<IWindow, WindowHandler>(WindowHandler.Mapper)
+			{
+#if ANDROID
+				// This property is also on the Application Mapper since that's where the attached property exists				
+				[PlatformConfiguration.AndroidSpecific.Application.WindowSoftInputModeAdjustProperty.PropertyName] = MapWindowSoftInputModeAdjust,
+#endif
+			};
 
 		internal static void RemapForControls()
 		{
