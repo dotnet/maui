3701176193694dcc404bdcd697f5de17e07ad8a7
Javier Suárez <javiersuarezruiz@hotmail.com>
2022-11-22 12:42:41 +0100

    Fix build errors

--- src/Compatibility/ControlGallery/src/Issues.Shared/Issue15542.cs
+++ src/Compatibility/ControlGallery/src/Issues.Shared/Issue15542.cs
@@ -7,7 +7,7 @@ using Xamarin.UITest;
 using NUnit.Framework;
 #endif
 
-namespace Xamarin.Forms.Controls.Issues
+namespace Microsoft.Maui.Controls.Compatibility.ControlGallery.Issues
 {
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Github, 15542, "[Bug] Shell.TitleView does not render on iOS 16",
--- src/Compatibility/ControlGallery/src/Issues.Shared/Issue15565.cs
+++ src/Compatibility/ControlGallery/src/Issues.Shared/Issue15565.cs
@@ -7,7 +7,7 @@ using Xamarin.UITest;
 using NUnit.Framework;
 #endif
 
-namespace Xamarin.Forms.Controls.Issues
+namespace Microsoft.Maui.Controls.Compatibility.ControlGallery.Issues
 {
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Github, 15565, "[Bug] Shell TitleView and ToolBarItems rendering strange display on iOS 16",
