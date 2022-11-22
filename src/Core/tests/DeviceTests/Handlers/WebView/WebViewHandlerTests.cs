1e4d03f38d7498f2b2709b3234f8c726106e086c
redth <jondick@gmail.com>
2022-11-22 10:56:06 -0500

    Add tests for webview's html5 support
    This queries for elements that show a passing score from the html test site.
    
    In this case we want to know that html5 video works since we enabled it in iOS by default.

--- src/Core/tests/DeviceTests/Handlers/WebView/WebViewHandlerTests.cs
+++ src/Core/tests/DeviceTests/Handlers/WebView/WebViewHandlerTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.DeviceTests.Stubs;
 using Microsoft.Maui.Handlers;
 using Xunit;
@@ -22,6 +25,66 @@ namespace Microsoft.Maui.DeviceTests
 			var url = ((UrlWebViewSourceStub)webView.Source).Url;
 
 			await InvokeOnMainThreadAsync(() => ValidatePropertyInitValue(webView, () => url, GetNativeSource, url));
+		}
+
+		[Theory]
+		[InlineData("row-video.element")]
+		public async Task WebViewIsHtml5Compatible(params string[] elementIds)
+		{
+			var pageLoadTimeout = TimeSpan.FromSeconds(30);
+			var url = "https://html5test.com/";
+
+			await InvokeOnMainThreadAsync(async () =>
+			{
+				var webView = new WebViewStub { 
+					Width = 500,
+					Height = 500,
+				};
+				var handler = CreateHandler(webView);
+				var platformView = handler.PlatformView;
+
+				
+				await platformView.AttachAndRun(async () =>
+				{
+					var tcsLoaded = new TaskCompletionSource<bool>();
+					var ctsTimeout = new CancellationTokenSource(pageLoadTimeout);
+					ctsTimeout.Token.Register(() => tcsLoaded.TrySetException(new TimeoutException($"Failed to load {url}")));
+
+					webView.NavigatedDelegate = (evnt, url, result) =>
+					{
+						if (url.Equals(url, System.StringComparison.OrdinalIgnoreCase))
+							tcsLoaded.TrySetResult(result == WebNavigationResult.Success);
+					};
+
+					// Load page
+					webView.Source = new UrlWebViewSourceStub() { Url = url };
+					handler.UpdateValue(nameof(IWebView.Source));
+					
+					// Ensure it loaded
+					Assert.True(await tcsLoaded.Task, "HTML5Test Page Failed to Load");
+
+					// Wait for the DOM to settle by polling for a known element to exist
+					var waited = TimeSpan.Zero;
+					var waitIncrement = TimeSpan.FromSeconds(1);
+
+					while (waited < pageLoadTimeout)
+					{
+						// This is the element we want to wait to see to know 
+						var scoreTxt = await webView.EvaluateJavaScriptAsync($"document.getElementById('score').innerText");
+						if (!string.IsNullOrWhiteSpace(scoreTxt))
+							break;
+						await Task.Delay(waitIncrement);
+						waited += waitIncrement;
+					}
+
+					// Assert that all the requested elements exist by id and contain a passing check mark
+					foreach (var elementId in elementIds)
+					{
+						var txt = await webView.EvaluateJavaScriptAsync($"document.getElementById('{elementId}').innerText");
+						Assert.Contains("✔", txt, System.StringComparison.InvariantCultureIgnoreCase);
+					}
+				});
+			});
 		}
 	}
 }
