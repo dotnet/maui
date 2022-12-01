ca123235ad5a4deb2603aebc3937a17f8c2912d1
Javier Suárez <javiersuarezruiz@hotmail.com>
2022-12-01 18:57:02 +0100

    Invalidate Button background drawable if size changes (#11604)

--- src/Controls/samples/Controls.Sample/Pages/Controls/ButtonPage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/Controls/ButtonPage.xaml.cs
@@ -72,6 +72,14 @@ namespace Maui.Controls.Sample.Pages
 					return LineBreakMode.WordWrap;
 			}
 		}
+
+		int _backgroundCount;
+
+		void OnBackgroundButtonClicked(object sender, System.EventArgs e)
+		{
+			BackgroundButton.Text = $"Background tapped {_backgroundCount} times";
+			_backgroundCount++;
+		}
 	}
 
 	public class ButtonPageViewModel : BindableObject
