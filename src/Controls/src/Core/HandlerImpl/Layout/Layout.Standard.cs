12cd575092fadbdb5c675817ec21ee09f2936d57
Shane Neuville <shneuvil@microsoft.com>
2022-12-06 19:09:39 -0600

    - fix handlers

--- src/Controls/src/Core/HandlerImpl/Layout/Layout.Standard.cs
+++ src/Controls/src/Core/HandlerImpl/Layout/Layout.Standard.cs
@@ -2,20 +2,10 @@
 {
 	public partial class Layout
 	{
-		public static void MapInputTransparent(LayoutHandler handler, Layout layout) => MapInputTransparent((ILayoutHandler)handler, layout);
+		public static void MapInputTransparent(LayoutHandler handler, Layout layout) { }
 
-		public static void MapInputTransparent(ILayoutHandler handler, Layout layout)
-		{
-			if (handler is LayoutHandler h)
-			{
-				MapInputTransparent(h, layout);
-			}
-		}
+		public static void MapInputTransparent(ILayoutHandler handler, Layout layout) { }
 
-		static void MapInputTransparent(IViewHandler handler, IView layout)
-		{
-			if (handler is ILayoutHandler lh && layout is Layout l)
-				MapInputTransparent(lh, l);
-		}
+		static void MapInputTransparent(IViewHandler handler, IView layout) { }
 	}
 }
--- src/Controls/src/Core/HandlerImpl/Layout/Layout.Windows.cs
+++ src/Controls/src/Core/HandlerImpl/Layout/Layout.Windows.cs
@@ -1,19 +1,27 @@
-﻿namespace Microsoft.Maui.Controls
+﻿using Microsoft.UI.Xaml;
+
+namespace Microsoft.Maui.Controls
 {
 	public partial class Layout
 	{
-		public static void MapInputTransparent(LayoutHandler handler, Layout layout) => MapInputTransparent((ILayoutHandler)handler, layout);
+		public static void MapInputTransparent(LayoutHandler handler, Layout layout) =>
+			UpdateInputTransparent(handler, layout);
 
-		public static void MapInputTransparent(ILayoutHandler handler, Layout layout)
-		{
-			handler.PlatformView?.UpdateInputTransparent(handler, layout);
-			layout.UpdateDescendantInputTransparent();
-		}
+		public static void MapInputTransparent(ILayoutHandler handler, Layout layout) =>
+			UpdateInputTransparent(handler, layout);
 
-		static void MapInputTransparent(IViewHandler handler, IView layout)
+		static void MapInputTransparent(IViewHandler handler, IView layout) =>
+			UpdateInputTransparent(handler, layout);
+
+		static void UpdateInputTransparent(IViewHandler handler, IView layout)
 		{
-			if (handler is ILayoutHandler lh && layout is Layout l)
-				MapInputTransparent(lh, l);
+			if (handler.PlatformView is FrameworkElement fe)
+				fe.UpdateInputTransparent(handler, layout);
+
+			if (layout is Layout l)
+			{
+				l.UpdateDescendantInputTransparent();
+			}
 		}
 	}
 }
