b3120ae9a5f7bc6fd5888645312083d5af260ff7
Shane Neuville <shneuvil@microsoft.com>
2022-12-06 19:12:54 -0600

    - undo

--- src/Core/src/PropertyMapper.cs
+++ src/Core/src/PropertyMapper.cs
@@ -169,9 +169,7 @@ namespace Microsoft.Maui
 			SetPropertyCore(key, (h, v) =>
 			{
 				if (v is TVirtualView vv)
-				{
 					action?.Invoke((TViewHandler)h, vv);
-				}
 				else if (Chained != null)
 				{
 					foreach (var chain in Chained)
