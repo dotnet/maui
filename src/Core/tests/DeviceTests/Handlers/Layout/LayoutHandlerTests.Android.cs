bb50eb1ba8755067a17e6aeaa08edc4d04e51abf
Javier Suárez <javiersuarezruiz@hotmail.com>
2022-12-19 16:58:35 +0100

    Removed duplicated test

--- src/Core/tests/DeviceTests/Handlers/Layout/LayoutHandlerTests.Android.cs
+++ src/Core/tests/DeviceTests/Handlers/Layout/LayoutHandlerTests.Android.cs
@@ -13,32 +13,6 @@ namespace Microsoft.Maui.DeviceTests.Handlers.Layout
 {
 	public partial class LayoutHandlerTests
 	{
-		[Theory(DisplayName = "Shadow Initializes Correctly")]
-		[InlineData(0xFF0000)]
-		[InlineData(0x00FF00)]
-		[InlineData(0x0000FF)]
-		public async Task ShadowInitializesCorrectly(uint color)
-		{
-			var expected = Color.FromUint(color);
-
-			var xPlatShadow = new ShadowStub
-			{
-				Offset = new Point(10, 10),
-				Opacity = 1.0f,
-				Radius = 2.0f
-			};
-
-			var layout = new LayoutStub
-			{
-				Height = 50,
-				Width = 50
-			};
-
-			layout.Shadow = xPlatShadow;
-
-			await ValidateHasColor(layout, expected, () => xPlatShadow.Paint = new SolidPaint(expected));
-		}
-
 		double GetNativeChildCount(IElementHandler layoutHandler)
 		{
 			return GetNativeChildCount(layoutHandler.PlatformView as LayoutViewGroup);
