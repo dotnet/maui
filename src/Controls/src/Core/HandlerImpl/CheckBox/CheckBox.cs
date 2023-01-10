ba98285ae2099697505728cffa86d64ff545d4f2
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-01-10 19:49:10 +0100

    Notify CheckBox Foreground update after Color changed (alternative) (#12102)
    * Notify CheckBox Foreground update after Color changed (alternative)
    
    * Added Device tests

--- src/Controls/src/Core/HandlerImpl/CheckBox/CheckBox.cs
+++ src/Controls/src/Core/HandlerImpl/CheckBox/CheckBox.cs
@@ -11,7 +11,10 @@ namespace Microsoft.Maui.Controls
 		// TODO Make public for NET8
 		internal static IPropertyMapper<ICheckBox, CheckBoxHandler> ControlsCheckBoxMapper = new PropertyMapper<CheckBox, CheckBoxHandler>(CheckBoxHandler.Mapper)
 		{
-			[nameof(CheckBox.Color)] = (handler, _) => handler?.UpdateValue(nameof(ICheckBox.Foreground))
+			[nameof(Color)] = (handler, view) =>
+			{
+				handler?.UpdateValue(nameof(ICheckBox.Foreground));
+			}
 		};
 
 		internal new static void RemapForControls()
