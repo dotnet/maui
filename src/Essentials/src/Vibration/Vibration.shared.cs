83397964a9720c45e33e7b3cc605c765294538a8
Gerald Versluis <gerald@verslu.is>
2023-01-10 14:08:42 +0100

    Update Vibration.shared.cs

--- src/Essentials/src/Vibration/Vibration.shared.cs
+++ src/Essentials/src/Vibration/Vibration.shared.cs
@@ -23,7 +23,7 @@ namespace Microsoft.Maui.Devices
 		/// Vibrates the device for the specified time in the range [0, 5000]ms.
 		/// </summary>
 		/// <remarks>On iOS, the device will only vibrate for 500ms, regardless of the value specified.</remarks>
-		/// <param name="duration">The time to vibrate for.</param>
+		/// <param name="duration">The time to vibrate for. This value will be ignored on iOS as it only supports a vibration of 500ms.</param>
 		void Vibrate(TimeSpan duration);
 
 		/// <summary>
@@ -35,6 +35,7 @@ namespace Microsoft.Maui.Devices
 	/// <summary>
 	/// The Vibration API provides an easy way to make the device vibrate.
 	/// </summary>
+	/// <remarks>On Android make sure that the <c>android.permission.VIBRATE</c> permission is declared in the <c>AndroidManifest.xml</c> file.</remarks>
 	public static class Vibration
 	{
 		/// <summary>
@@ -44,10 +45,10 @@ namespace Microsoft.Maui.Devices
 			Current.Vibrate();
 
 		/// <summary>
-		/// Vibrates the device for the specified number of milliseconds in the range [0, 5000].
+		/// Vibrates the device for the specified number of milliseconds in the range [0, 5000]ms.
 		/// </summary>
 		/// <remarks>On iOS, the device will only vibrate for 500ms, regardless of the value specified.</remarks>
-		/// <param name="duration">The number of milliseconds to vibrate for.</param>
+		/// <param name="duration">The number of milliseconds to vibrate for. This value will be ignored on iOS as it only supports a vibration of 500ms.</param>
 		public static void Vibrate(double duration) =>
 			Current.Vibrate(duration);
 
@@ -55,7 +56,7 @@ namespace Microsoft.Maui.Devices
 		/// Vibrates the device for the specified time in the range [0, 5000]ms.
 		/// </summary>
 		/// <remarks>On iOS, the device will only vibrate for 500ms, regardless of the value specified.</remarks>
-		/// <param name="duration">The time to vibrate for.</param>
+		/// <param name="duration">The time to vibrate for. This value will be ignored on iOS as it only supports a vibration of 500ms.</param>
 		public static void Vibrate(TimeSpan duration) =>
 			Current.Vibrate(duration);
 
@@ -79,8 +80,16 @@ namespace Microsoft.Maui.Devices
 			defaultImplementation = implementation;
 	}
 
+	/// <summary>
+	/// Static class with extension methods for the <see cref="IVibration"/> APIs.
+	/// </summary>
 	public static class VibrationExtensions
 	{
+		/// <summary>
+		/// Vibrates the device for the specified time in the range [0, 5000]ms.
+		/// </summary>
+		/// <param name="vibration">The object this method is invoked on.</param>
+		/// <param name="duration">The time to vibrate for. This value will be ignored on iOS as it only supports a vibration of 500ms.</param>
 		public static void Vibrate(this IVibration vibration, double duration) =>
 			vibration.Vibrate(TimeSpan.FromMilliseconds(duration));
 	}
