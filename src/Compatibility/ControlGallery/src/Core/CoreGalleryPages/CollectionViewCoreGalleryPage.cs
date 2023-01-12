99389f1fefbca1cba4e046d16956484c8eb67fa2
Gerald Versluis <gerald.versluis@microsoft.com>
2023-01-12 13:49:10 +0100

    Restore accidentally deleted files

---
+++ src/Compatibility/ControlGallery/src/Core/CoreGalleryPages/CollectionViewCoreGalleryPage.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Maui.Controls.Compatibility.ControlGallery
+{
+	internal class CollectionViewCoreGalleryPage : CoreGalleryPage<CollectionView>
+	{
+		protected override void InitializeElement(CollectionView element)
+		{
+			base.InitializeElement(element);
+
+			var items = new List<string>();
+
+			for (int n = 0; n < 1000; n++)
+			{
+				items.Add(DateTime.Now.AddDays(n).ToString("D"));
+			}
+
+			element.ItemsSource = items;
+
+			element.HeightRequest = 250;
+
+			element.ItemsLayout = LinearItemsLayout.Vertical;
+		}
+	}
+}
