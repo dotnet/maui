8ab8c8aaac67b9bb20ca1553aa86bb465cc310e2
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-01-18 12:47:47 -0600

    Comments

--- src/Essentials/src/AppInfo/AppInfo.android.cs
+++ src/Essentials/src/AppInfo/AppInfo.android.cs
@@ -13,7 +13,7 @@ namespace Microsoft.Maui.ApplicationModel
 	{
 		static readonly Lazy<string> _name = new Lazy<string>(() => Application.Context.ApplicationInfo.LoadLabel(Application.Context.PackageManager));
 		static readonly Lazy<string> _packageName = new Lazy<string>(() => Application.Context.PackageName);
-#pragma warning disable CA1416, CA1422 // TODO: might be a binding issue in xamarin/xamarin-android
+#pragma warning disable CA1416, CA1422 // Deprecated in API 33: https://developer.android.com/reference/android/content/pm/PackageManager#getPackageInfo(java.lang.String,%20int)
 		static readonly Lazy<PackageInfo> _packageInfo = new Lazy<PackageInfo>(() => Application.Context.PackageManager.GetPackageInfo(_packageName.Value, PackageInfoFlags.MetaData));
 #pragma warning restore CA1416, CA1422
 		static readonly Lazy<AppTheme> _requestedTheme = new Lazy<AppTheme>(GetRequestedTheme);
