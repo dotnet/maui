dd147ec92776f5e9d048f05befdb2a6ea147b799
pawel <pawel@dexus.pl>
2023-01-18 09:42:45 +0100

    ContentPropertyUnitTest update

--- src/Controls/tests/Core.UnitTests/ContentPropertyUnitTest.cs
+++ src/Controls/tests/Core.UnitTests/ContentPropertyUnitTest.cs
@@ -9,89 +9,71 @@ namespace Microsoft.Maui.Controls.Core.UnitTests
 {
 	public class ContentPropertyUnitTest : BaseTestFixture
 	{
-		static (Type ClassType, string ContentPropertyName)[] classesWithContentPropertyNames = new[]
-		{
-			(typeof(Border), "Content"),
-			(typeof(CarouselPage), "Children"),
-			(typeof(Compatibility.AbsoluteLayout), "Children"),
-			(typeof(Compatibility.FlexLayout), "Children"),
-			(typeof(Compatibility.Grid), "Children"),
-			(typeof(Compatibility.Layout<View>), "Children"),
-			(typeof(Compatibility.RelativeLayout), "Children"),
-			(typeof(Compatibility.StackLayout), "Children"),
-			(typeof(ContentPage), "Content"),
-			(typeof(ContentView), "Content"),
-			(typeof(DataTrigger), "Setters"),
-			(typeof(EventTrigger), "Actions"),
-			(typeof(FlexLayout), "Children"),
-			(typeof(FlyoutPage), "Detail"),
-			(typeof(FormattedString), "Spans"),
-			(typeof(Frame), "Content"),
-			(typeof(GradientBrush), "GradientStops"),
-			(typeof(Grid), "Children"),
-			(typeof(HorizontalStackLayout), "Children"),
-			(typeof(ImageBrush), "ImageSource"),
-			(typeof(IndicatorView), "IndicatorLayout"),
-			(typeof(Label), "Text"),
-			(typeof(Layout), "Children"),
-			(typeof(MultiBinding), "Bindings"),
-			(typeof(MultiPage<Page>), "Children"),
-			(typeof(MultiTrigger), "Setters"),
-			(typeof(On), "Value"),
-			(typeof(OnPlatform<object>), "Platforms"),
-			(typeof(RadioButton), "Content"),
-			(typeof(RefreshView), "Content"),
-			(typeof(ScrollView), "Content"),
-			(typeof(Setter), "Value"),
-			(typeof(Shapes.GeometryGroup), "Children"),
-			(typeof(Shapes.Path), "Data"),
-			(typeof(Shapes.PathFigure), "Segments"),
-			(typeof(Shapes.PathGeometry), "Figures"),
-			(typeof(Shapes.Polygon), "Points"),
-			(typeof(Shapes.Polyline), "Points"),
-			(typeof(Shapes.TransformGroup), "Children"),
-			(typeof(Shell), "Items"),
-			(typeof(ShellContent), "Content"),
-			(typeof(ShellItem), "Items"),
-			(typeof(ShellSection), "Items"),
-			(typeof(Span), "Text"),
-			(typeof(Style), "Setters"),
-			(typeof(SwipeItemView), "Content"),
-			(typeof(SwipeView), "Content"),
-			(typeof(TabbedPage), "Children"),
-			(typeof(TableView), "Root"),
-			(typeof(Trigger), "Setters"),
-			(typeof(VerticalStackLayout), "Children"),
-			(typeof(ViewCell), "View"),
-			(typeof(VisualState), "Setters"),
-			(typeof(VisualStateGroup), "States"),
-			(typeof(Window), "Page")
-		};
-
-		static bool IsContentPopertyAttributeValid(Type type, string propertyName)
+		[Theory]
+		[InlineData(typeof(Border), "Content")]
+		[InlineData(typeof(CarouselPage), "Children")]
+		[InlineData(typeof(Compatibility.AbsoluteLayout), "Children")]
+		[InlineData(typeof(Compatibility.FlexLayout), "Children")]
+		[InlineData(typeof(Compatibility.Grid), "Children")]
+		[InlineData(typeof(Compatibility.Layout<View>), "Children")]
+		[InlineData(typeof(Compatibility.RelativeLayout), "Children")]
+		[InlineData(typeof(Compatibility.StackLayout), "Children")]
+		[InlineData(typeof(ContentPage), "Content")]
+		[InlineData(typeof(ContentView), "Content")]
+		[InlineData(typeof(DataTrigger), "Setters")]
+		[InlineData(typeof(EventTrigger), "Actions")]
+		[InlineData(typeof(FlexLayout), "Children")]
+		[InlineData(typeof(FlyoutPage), "Detail")]
+		[InlineData(typeof(FormattedString), "Spans")]
+		[InlineData(typeof(Frame), "Content")]
+		[InlineData(typeof(GradientBrush), "GradientStops")]
+		[InlineData(typeof(Grid), "Children")]
+		[InlineData(typeof(HorizontalStackLayout), "Children")]
+		[InlineData(typeof(IndicatorView), "IndicatorLayout")]
+		[InlineData(typeof(Label), "Text")]
+		[InlineData(typeof(Layout), "Children")]
+		[InlineData(typeof(MultiBinding), "Bindings")]
+		[InlineData(typeof(MultiPage<Page>), "Children")]
+		[InlineData(typeof(MultiTrigger), "Setters")]
+		[InlineData(typeof(On), "Value")]
+		[InlineData(typeof(OnPlatform<object>), "Platforms")]
+		[InlineData(typeof(RadioButton), "Content")]
+		[InlineData(typeof(RefreshView), "Content")]
+		[InlineData(typeof(ScrollView), "Content")]
+		[InlineData(typeof(Setter), "Value")]
+		[InlineData(typeof(Shapes.GeometryGroup), "Children")]
+		[InlineData(typeof(Shapes.Path), "Data")]
+		[InlineData(typeof(Shapes.PathFigure), "Segments")]
+		[InlineData(typeof(Shapes.PathGeometry), "Figures")]
+		[InlineData(typeof(Shapes.Polygon), "Points")]
+		[InlineData(typeof(Shapes.Polyline), "Points")]
+		[InlineData(typeof(Shapes.TransformGroup), "Children")]
+		[InlineData(typeof(Shell), "Items")]
+		[InlineData(typeof(ShellContent), "Content")]
+		[InlineData(typeof(ShellItem), "Items")]
+		[InlineData(typeof(ShellSection), "Items")]
+		[InlineData(typeof(Span), "Text")]
+		[InlineData(typeof(Style), "Setters")]
+		[InlineData(typeof(SwipeItemView), "Content")]
+		[InlineData(typeof(SwipeView), "Content")]
+		[InlineData(typeof(TabbedPage), "Children")]
+		[InlineData(typeof(TableView), "Root")]
+		[InlineData(typeof(Trigger), "Setters")]
+		[InlineData(typeof(VerticalStackLayout), "Children")]
+		[InlineData(typeof(ViewCell), "View")]
+		[InlineData(typeof(VisualState), "Setters")]
+		[InlineData(typeof(VisualStateGroup), "States")]
+		[InlineData(typeof(Window), "Page")]
+		public void CheckContentPropertyAttribute(Type type, string contentPropertyName)
 		{
 			var attributes = Attribute.GetCustomAttributes(type);
 			var attribute = attributes.FirstOrDefault(e => e is ContentPropertyAttribute) as ContentPropertyAttribute;
-			return attribute != null ? attribute.Name.Equals(propertyName, StringComparison.Ordinal) : false;
-		}
+			var hasValidContentProperty = attribute != null && attribute.Name.Equals(contentPropertyName, StringComparison.Ordinal);
 
-		[Fact]
-		public void CheckContentPropertyAttributes()
-		{
-			foreach (var classWithContentProp in classesWithContentPropertyNames)
-			{
-				var classHasValidContentProperty = IsContentPopertyAttributeValid(classWithContentProp.ClassType, classWithContentProp.ContentPropertyName);
-				Assert.True(classHasValidContentProperty, $"The {classWithContentProp.ClassType.FullName} class has an invalid ContentPropertyAttribute. It must be [ContentProperty({classWithContentProp.ContentPropertyName})]");
-			}
-		}
+			Assert.True(hasValidContentProperty, $@"The {type.FullName} class has an invalid ContentPropertyAttribute. It must be [ContentProperty(""{contentPropertyName}"")]");
 
-		[Fact]
-		public void ClassesWithContentPropertyImplementIEnumerable()
-		{
-			foreach (var classWithContentProp in classesWithContentPropertyNames)
-			{
-				Assert.True(typeof(IEnumerable).IsAssignableFrom(classWithContentProp.ClassType), $"Class {classWithContentProp.ClassType.FullName} must implement the IEnumerable interface");
-			}
+			var implementsIEnumerable = typeof(IEnumerable).IsAssignableFrom(type);
+			Assert.True(implementsIEnumerable, $"Class {type.FullName} must implement the IEnumerable interface");
 		}
 	}
 }
