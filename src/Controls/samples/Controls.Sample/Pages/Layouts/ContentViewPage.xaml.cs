ea39fa5a14ffe0bd8c5b2c77b7725aa1c486dc12
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-01-18 11:32:05 +0100

    Updated samples

--- src/Controls/samples/Controls.Sample/Pages/Layouts/ContentViewPage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/Layouts/ContentViewPage.xaml.cs
@@ -5,6 +5,31 @@
 		public ContentViewPage()
 		{
 			InitializeComponent();
+
+			BindingContext = new ContentViewModel();
+		}
+	}
+
+	public class ContentViewModel : BindableObject
+	{
+		private string _text;
+
+		public MainViewModel()
+		{
+			_text = "Content";
+		}
+
+		public string Text
+		{
+			get => _text;
+			set
+			{
+				if (_text != value)
+				{
+					_text = value;
+					OnPropertyChanged();
+				}
+			}
 		}
 	}
 }
