f9be2d1bddf7c6040dc7df0daac92ce50035e076
Gerald Versluis <gerald.versluis@microsoft.com>
2023-01-23 19:49:37 +0100

    Internalize MessagingCenter (#12582)
    * Internalize MessagingCenter
    
    * Remove APIs in unshipped
    
    * Just the classes internal
    
    * Fix up (un)shipped API txt files
    
    * Update PublicAPI.Unshipped.txt
    
    * InternalsVisibleTo for sample app
    
    * Update AssemblyInfo.cs
    
    * Update AssemblyInfo.cs
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>

--- src/Controls/src/Core/Properties/AssemblyInfo.cs
+++ src/Controls/src/Core/Properties/AssemblyInfo.cs
@@ -4,6 +4,13 @@ using Microsoft.Maui.Controls.Internals;
 using Microsoft.Maui.Controls.StyleSheets;
 using Compatibility = Microsoft.Maui.Controls.Compatibility;
 
+[assembly: InternalsVisibleTo("Maui.Controls.Sample")]
+[assembly: InternalsVisibleTo("Microsoft.Maui.Essentials.Sample")]
+[assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.ControlGallery.Android")]
+[assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.ControlGallery.iOS")]
+[assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.ControlGallery.WinUI")]
+[assembly: InternalsVisibleTo("Compatibility.ControlGallery.WinUI")]
+
 [assembly: InternalsVisibleTo("iOSUnitTests")]
 [assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.ControlGallery")]
 [assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility")]
