62041b0310e88fc0ecb0a395ed6f3544b60ead83
Gerald Versluis <gerald.versluis@microsoft.com>
2023-01-23 11:12:22 +0100

    Update AssemblyInfo.cs

--- src/Controls/src/Core/Properties/AssemblyInfo.cs
+++ src/Controls/src/Core/Properties/AssemblyInfo.cs
@@ -9,6 +9,7 @@ using Compatibility = Microsoft.Maui.Controls.Compatibility;
 [assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.ControlGallery.Android")]
 [assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.ControlGallery.iOS")]
 [assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.ControlGallery.WinUI")]
+[assembly: InternalsVisibleTo("Compatibility.ControlGallery.WinUI")]
 
 [assembly: InternalsVisibleTo("iOSUnitTests")]
 [assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.ControlGallery")]
