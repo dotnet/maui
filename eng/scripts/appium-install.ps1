6a7f7930608a6f2acd82dd8c9db968429ab24037
Rui Marinho <me@ruimarinho.net>
2023-01-24 17:54:03 +0000

    Better appium install drivers

---
+++ eng/scripts/appium-install.ps1
@@ -0,0 +1,20 @@
+$x = appium driver list --installed --json  | ConvertFrom-Json
+if(!$x.windows)
+{
+    appium driver install --source=npm appium-windows-driver
+}
+
+if(!$x.uiautomator2)
+{
+    appium driver install uiautomator2
+}
+
+if(!$x.xcuitest)
+{
+    appium driver install xcuitest
+}
+
+if(!$x.mac2)
+{
+    appium driver install mac2
+}
