5cb09251f9326bd3f6fa3e89528bcb44fb51e71a
redth <jondick@gmail.com>
2023-01-25 11:10:57 -0500

    Skip tests on iOS and MacCat

--- src/Core/tests/DeviceTests/Handlers/WebView/WebViewHandlerTests.cs
+++ src/Core/tests/DeviceTests/Handlers/WebView/WebViewHandlerTests.cs
@@ -30,7 +30,7 @@ namespace Microsoft.Maui.DeviceTests
 		}
 
 		[Fact("WebBrowser autoplays HTML5 Video"
-#if ANDROID
+#if ANDROID || IOS || MACCATALYST
 			, Skip = "Capturing a screenshot/image of a WebView on Android does not also capture the video canvas contents required for this test."
 #endif
 			)]
