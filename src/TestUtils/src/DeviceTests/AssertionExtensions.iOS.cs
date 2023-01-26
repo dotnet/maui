bb0ad48d7079f0a4d83eedc3c4788ed58c1fe620
Shane Neuville <shneuvil@microsoft.com>
2023-01-26 17:05:19 -0600

    Fix timing on iOS Toolbar (#12888)
    * Fix timing on iOS Toolbar
    
    This reverts commit 5abffde4a0081c67c244048a4a672d3be82baf2c.
    
    # Conflicts:
    #	src/Controls/src/Core/PublicAPI/net-ios/PublicAPI.Unshipped.txt
    #	src/Controls/src/Core/PublicAPI/net-maccatalyst/PublicAPI.Unshipped.txt
    
    * - fix tests for iOS 14

--- src/TestUtils/src/DeviceTests/AssertionExtensions.iOS.cs
+++ src/TestUtils/src/DeviceTests/AssertionExtensions.iOS.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using CoreAnimation;
 using CoreGraphics;
@@ -553,6 +554,57 @@ namespace Microsoft.Maui.DeviceTests
 			}
 
 			return platformView;
+		}
+
+		static UIView GetBackButton(this UINavigationBar uINavigationBar)
+		{
+			var item = uINavigationBar.FindDescendantView<UIView>(result =>
+			{
+				return result.Class.Name?.Contains("UIButtonBarButton", StringComparison.OrdinalIgnoreCase) == true;
+			});
+
+			return item ?? throw new Exception("Unable to locate back button view");
+		}
+
+		public static void TapBackButton(this UINavigationBar uINavigationBar)
+		{
+			var item = uINavigationBar.GetBackButton();
+
+			var recognizer = item!.GestureRecognizers!.OfType<UITapGestureRecognizer>().FirstOrDefault();
+			_ = recognizer ?? throw new Exception("Unable to Back Button TapGestureRecognizer");
+
+			recognizer.State = UIGestureRecognizerState.Ended;
+		}
+
+		public static string? GetToolbarTitle(this UINavigationBar uINavigationBar)
+		{
+			var item = uINavigationBar.FindDescendantView<UIView>(result =>
+			{
+				return result.Class.Name?.Contains("UINavigationBarTitleControl", StringComparison.OrdinalIgnoreCase) == true;
+			});
+
+			//Pre iOS 15
+			item = item ?? uINavigationBar.FindDescendantView<UIView>(result =>
+			{
+				return result.Class.Name?.Contains("UINavigationBarContentView", StringComparison.OrdinalIgnoreCase) == true;
+			});
+
+			_ = item ?? throw new Exception("Unable to locate TitleBar Control");
+
+			var titleLabel = item.FindDescendantView<UILabel>();
+
+			_ = item ?? throw new Exception("Unable to locate UILabel Inside UINavigationBar");
+			return titleLabel?.Text;
+		}
+
+		public static string? GetBackButtonText(this UINavigationBar uINavigationBar)
+		{
+			var item = uINavigationBar.GetBackButton();
+
+			var titleLabel = item.FindDescendantView<UILabel>();
+
+			_ = item ?? throw new Exception("Unable to locate BackButton UILabel Inside UINavigationBar");
+			return titleLabel?.Text;
 		}
 	}
 }
