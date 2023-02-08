cb5a070d4237b73e16d4442c324dabed7f33d71c
Rui Marinho <me@ruimarinho.net>
2023-02-08 14:41:38 +0000

    [Sample] Clean fields when tapping login button

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -17,6 +17,8 @@ namespace Maui.Controls.Sample
 
 		private void LoginButton_Clicked(object sender, EventArgs e)
 		{
+			UserName.Text = string.Empty;
+			Password.Text = string.Empty;
 			StatusLabel.Text = "Logging in " + DateTime.Now.ToString("HH:mm:ss");
 		}
 	}
