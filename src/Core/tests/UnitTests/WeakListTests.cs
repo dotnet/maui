dotnet/maui

commit f8171b4ad4a910175e2e014a067d888beab7ff53

Occasionally clean up WeakReferences in Styles whose targets have been collected (#8124)

Xamarin.Forms.Core/Interactivity/AttachedCollection.cs
@@ -8,6 +8,9 @@ namespace Xamarin.Forms
 	{
 		readonly List<WeakReference> _associatedObjects = new List<WeakReference>();
 
+		const int CleanupTrigger = 128;
+		int _cleanupThreshold = CleanupTrigger;
+
 		public AttachedCollection()
 		{
 		}
@@ -64,6 +67,7 @@ namespace Xamarin.Forms
 			lock (_associatedObjects)
 			{
 				_associatedObjects.Add(new WeakReference(bindable));
+				CleanUpWeakReferences();
 			}
 			foreach (T item in this)
 				item.AttachTo(bindable);
@@ -122,6 +126,17 @@ namespace Xamarin.Forms
 					continue;
 				item.AttachTo(bindable);
 			}
+		}
+
+		void CleanUpWeakReferences()
+		{
+			if (_associatedObjects.Count < _cleanupThreshold)
+			{
+				return;
+			}
+
+			_associatedObjects.RemoveAll(t => !t.IsAlive);
+			_cleanupThreshold = _associatedObjects.Count + CleanupTrigger;
 		}
 	}
 }

Xamarin.Forms.Core/Style.cs
@@ -10,6 +10,9 @@ namespace Xamarin.Forms
 	{
 		internal const string StyleClassPrefix = "Xamarin.Forms.StyleClass.";
 
+		const int CleanupTrigger = 128;
+		int _cleanupThreshold = CleanupTrigger;
+
 		readonly BindableProperty _basedOnResourceProperty = BindableProperty.CreateAttached("BasedOnResource", typeof(Style), typeof(Style), default(Style),
 			propertyChanged: OnBasedOnResourceChanged);
 
@@ -93,6 +96,8 @@ namespace Xamarin.Forms
 			if (BaseResourceKey != null)
 				bindable.SetDynamicResource(_basedOnResourceProperty, BaseResourceKey);
 			ApplyCore(bindable, BasedOn ?? GetBasedOnResource(bindable));
+
+			CleanUpWeakReferences();
 		}
 
 		public Type TargetType { get; }
@@ -177,6 +182,17 @@ namespace Xamarin.Forms
 			if (value == null)
 				return true;
 			return value.TargetType.IsAssignableFrom(TargetType);
+		}
+
+		void CleanUpWeakReferences()
+		{
+			if (_targets.Count < _cleanupThreshold)
+			{
+				return;
+			}
+
+			_targets.RemoveAll(t => !t.TryGetTarget(out _));
+			_cleanupThreshold = _targets.Count + CleanupTrigger;
 		}
 	}
 }

commit 9e93a778194de52a43b66375cce1fef4662d1bc9

[controls] fix memory leak in Style and AttachedCollection
Context: https://github.com/Vroomer/MAUI-master-detail-memory-leak
Context: https://github.com/symbiogenesis/Maui.DataGrid/tree/memory-leak
Context: https://github.com/dotnet/maui/issues/12039

Using the Visual Studio's `.NET Object Allocation Tracking` profiler
on the sample above I noticed after using the app and taking snapshots:

Snapshot 2:

    WeakReference<Microsoft.Maui.Controls.BindableObject>
    Count: 1,545
    Bytes: 37,080

Snapshot 10:

    WeakReference<Microsoft.Maui.Controls.BindableObject>
    Count: 1,657
    Bytes: 39,768

It appears memory usage is slowly growing by about ~2.45KB each time I
navigate forward a page then return.

f8171b4 introduced a `CleanupTrigger`, but the code has a slight issue:

    void CleanUpWeakReferences()
    {
        if (_targets.Count < _cleanupThreshold)
        {
            return;
        }

        _targets.RemoveAll(t => !t.TryGetTarget(out _));
        _cleanupThreshold = _targets.Count + CleanupTrigger;
    }

Because `_cleanupThreshold` is modified at the bottom of this method,
the `_targets` collection increases by 128 in size over time. The
result being lots of tiny `WeakReference<T>` objects in memory, but
not the actual underlying `T` target.

I did a little research to try to find a better collection than
`List<WeakReference<T>>`:

* https://learn.microsoft.com/dotnet/api/system.runtime.compilerservices.conditionalweaktable-2
* https://source.dot.net/#Microsoft.Extensions.Caching.Memory/MemoryCache.cs
* https://source.dot.net/#PresentationFramework/System/Windows/Input/KeyboardNavigation.cs,08271c29723aa10a

I did not find one that exactly met our needs, so I wrote a very
simple `WeakList<T>`. It cleans up `WeakReference<T>` that are no
longer alive after N operations. I set this threshold to 32 to start,
we can modify this later if needed. This makes the above samples call

src/Controls/src/Core/Interactivity/AttachedCollection.cs
@@ -7,10 +7,7 @@ namespace Microsoft.Maui.Controls
 {
 	internal class AttachedCollection<T> : ObservableCollection<T>, ICollection<T>, IAttachedObject where T : BindableObject, IAttachedObject
 	{
-		readonly List<WeakReference> _associatedObjects = new List<WeakReference>();
-
-		const int CleanupTrigger = 128;
-		int _cleanupThreshold = CleanupTrigger;
+		readonly WeakList<BindableObject> _associatedObjects = new();
 
 		public AttachedCollection()
 		{
@@ -38,13 +35,10 @@ namespace Microsoft.Maui.Controls
 
 		protected override void ClearItems()
 		{
-			foreach (WeakReference weakbindable in _associatedObjects)
+			foreach (var bindable in _associatedObjects)
 			{
 				foreach (T item in this)
 				{
-					var bindable = weakbindable.Target as BindableObject;
-					if (bindable == null)
-						continue;
 					item.DetachFrom(bindable);
 				}
 			}
@@ -54,11 +48,8 @@ namespace Microsoft.Maui.Controls
 		protected override void InsertItem(int index, T item)
 		{
 			base.InsertItem(index, item);
-			foreach (WeakReference weakbindable in _associatedObjects)
+			foreach (var bindable in _associatedObjects)
 			{
-				var bindable = weakbindable.Target as BindableObject;
-				if (bindable == null)
-					continue;
 				item.AttachTo(bindable);
 			}
 		}
@@ -67,8 +58,7 @@ namespace Microsoft.Maui.Controls
 		{
 			lock (_associatedObjects)
 			{
-				_associatedObjects.Add(new WeakReference(bindable));
-				CleanUpWeakReferences();
+				_associatedObjects.Add(bindable);
 			}
 			foreach (T item in this)
 				item.AttachTo(bindable);
@@ -80,27 +70,15 @@ namespace Microsoft.Maui.Controls
 				item.DetachFrom(bindable);
 			lock (_associatedObjects)
 			{
-				for (var i = 0; i < _associatedObjects.Count; i++)
-				{
-					object target = _associatedObjects[i].Target;
-
-					if (target == null || target == bindable)
-					{
-						_associatedObjects.RemoveAt(i);
-						i--;
-					}
-				}
+				_associatedObjects.Remove(bindable);
 			}
 		}
 
 		protected override void RemoveItem(int index)
 		{
 			T item = this[index];
-			foreach (WeakReference weakbindable in _associatedObjects)
+			foreach (var bindable in _associatedObjects)
 			{
-				var bindable = weakbindable.Target as BindableObject;
-				if (bindable == null)
-					continue;
 				item.DetachFrom(bindable);
 			}
 
@@ -110,34 +88,17 @@ namespace Microsoft.Maui.Controls
 		protected override void SetItem(int index, T item)
 		{
 			T old = this[index];
-			foreach (WeakReference weakbindable in _associatedObjects)
+			foreach (var bindable in _associatedObjects)
 			{
-				var bindable = weakbindable.Target as BindableObject;
-				if (bindable == null)
-					continue;
 				old.DetachFrom(bindable);
 			}
 
 			base.SetItem(index, item);
 
-			foreach (WeakReference weakbindable in _associatedObjects)
+			foreach (var bindable in _associatedObjects)
 			{
-				var bindable = weakbindable.Target as BindableObject;
-				if (bindable == null)
-					continue;
 				item.AttachTo(bindable);
 			}
-		}
-
-		void CleanUpWeakReferences()
-		{
-			if (_associatedObjects.Count < _cleanupThreshold)
-			{
-				return;
-			}
-
-			_associatedObjects.RemoveAll(t => t == null || !t.IsAlive);
-			_cleanupThreshold = _associatedObjects.Count + CleanupTrigger;
 		}
 	}
 }

src/Controls/src/Core/Style.cs
@@ -12,13 +12,10 @@ namespace Microsoft.Maui.Controls
 	{
 		internal const string StyleClassPrefix = "Microsoft.Maui.Controls.StyleClass.";
 
-		const int CleanupTrigger = 128;
-		int _cleanupThreshold = CleanupTrigger;
-
 		readonly BindableProperty _basedOnResourceProperty = BindableProperty.CreateAttached("BasedOnResource", typeof(Style), typeof(Style), default(Style),
 			propertyChanged: OnBasedOnResourceChanged);
 
-		readonly List<WeakReference<BindableObject>> _targets = new List<WeakReference<BindableObject>>(4);
+		readonly WeakList<BindableObject> _targets = new();
 
 		Style _basedOnStyle;
 
@@ -66,10 +63,8 @@ namespace Microsoft.Maui.Controls
 					return;
 				_baseResourceKey = value;
 				//update all DynamicResources
-				foreach (WeakReference<BindableObject> bindableWr in _targets)
+				foreach (var target in _targets)
 				{
-					if (!bindableWr.TryGetTarget(out BindableObject target))
-						continue;
 					target.RemoveDynamicResource(_basedOnResourceProperty);
 					if (value != null)
 						target.SetDynamicResource(_basedOnResourceProperty, value);
@@ -98,14 +93,12 @@ namespace Microsoft.Maui.Controls
 		{
 			lock (_targets)
 			{
-				_targets.Add(new WeakReference<BindableObject>(bindable));
+				_targets.Add(bindable);
 			}
 
 			if (BaseResourceKey != null)
 				bindable.SetDynamicResource(_basedOnResourceProperty, BaseResourceKey);
 			ApplyCore(bindable, BasedOn ?? GetBasedOnResource(bindable));
-
-			CleanUpWeakReferences();
 		}
 
 		/// <include file="../../docs/Microsoft.Maui.Controls/Style.xml" path="//Member[@MemberName='TargetType']/Docs/*" />
@@ -117,7 +110,7 @@ namespace Microsoft.Maui.Controls
 			bindable.RemoveDynamicResource(_basedOnResourceProperty);
 			lock (_targets)
 			{
-				_targets.RemoveAll(wr => wr != null && wr.TryGetTarget(out BindableObject target) && target == bindable);
+				_targets.Remove(bindable);
 			}
 		}
 
@@ -149,11 +142,8 @@ namespace Microsoft.Maui.Controls
 
 		void BasedOnChanged(Style oldValue, Style newValue)
 		{
-			foreach (WeakReference<BindableObject> bindableRef in _targets)
+			foreach (var bindable in _targets)
 			{
-				if (!bindableRef.TryGetTarget(out BindableObject bindable))
-					continue;
-
 				UnApplyCore(bindable, oldValue);
 				ApplyCore(bindable, newValue);
 			}
@@ -183,17 +173,5 @@ namespace Microsoft.Maui.Controls
 
 		bool ValidateBasedOn(Style value)
 			=> value is null || value.TargetType.IsAssignableFrom(TargetType);
-
-		void CleanUpWeakReferences()
-		{
-			if (_targets.Count < _cleanupThreshold)
-				return;
-
-			lock (_targets)
-			{
-				_targets.RemoveAll(t => t == null || !t.TryGetTarget(out _));
-				_cleanupThreshold = _targets.Count + CleanupTrigger;
-			}
-		}
 	}
 }

src/Core/src/HotReload/HotReloadHelper.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui.Hosting;
-using Microsoft.Maui.Internal;
 
 namespace Microsoft.Maui.HotReload
 {
@@ -155,7 +154,7 @@ namespace Microsoft.Maui.HotReload
 
 		public static void TriggerReload()
 		{
-			List<IHotReloadableView?>? roots = null;
+			List<IHotReloadableView>? roots = null;
 			while (roots == null)
 			{
 				try

src/Core/src/HotReload/WeakList.cs
@@ -1,106 +1,0 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-
-namespace Microsoft.Maui.Internal
-{
-	class WeakList<T> : IList<T?>
-	{
-		readonly List<WeakReference> _items = new();
-
-		public T? this[int index]
-		{
-			get => (T?)_items[index].Target;
-			set => throw new NotImplementedException();
-		}
-
-		public int Count => CleanseItems().Count;
-
-		public bool IsReadOnly => false;
-
-		public void Add(T? item)
-		{
-			if (item == null)
-				return;
-
-			if (Contains(item))
-				return;
-
-			_items.Add(new WeakReference(item));
-		}
-
-		public void Clear()
-		{
-			CleanseItems();
-
-			//foreach (var item in views)
-			//{
-			//	(item.Target as IDisposable)?.Dispose();
-			//}
-
-			_items.Clear();
-		}
-
-		public bool Contains(T? item)
-		{
-			if (item == null)
-				return false;
-
-			var items = new List<WeakReference>(CleanseItems());
-			foreach (var x in items)
-			{
-				if (x.IsAlive && EqualityComparer<T>.Default.Equals(item, (T)x.Target!))
-					return true;
-			}
-
-			return false;
-		}
-
-		public void CopyTo(T?[] array, int arrayIndex) =>
-			throw new NotImplementedException();
-
-		public IEnumerator<T> GetEnumerator()
-		{
-			var items = new List<WeakReference>(CleanseItems());
-			foreach (var x in items)
-				yield return (T)x.Target!;
-		}
-
-		public int IndexOf(T? item) =>
-			throw new NotImplementedException();
-
-		public void Insert(int index, T? item) =>
-			throw new NotImplementedException();
-
-		public bool Remove(T? item)
-		{
-			if (item == null)
-				return false;
-
-			var removed = _items.RemoveAll(x => EqualityComparer<T>.Default.Equals(item, (T)x.Target!));
-
-			return removed > 0;
-		}
-
-		public void RemoveAt(int index) =>
-			throw new NotImplementedException();
-
-		public void ForEach(Action<T> action)
-		{
-			var items = new List<WeakReference>(CleanseItems());
-			foreach (var item in items)
-			{
-				if (item.IsAlive)
-					action?.Invoke((T)item.Target!);
-			}
-		}
-
-		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-		List<WeakReference> CleanseItems()
-		{
-			_items.RemoveAll(x => !x.IsAlive);
-			return _items;
-		}
-	}
-}

src/Core/src/Platform/Android/EnergySaverListenerManager.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.Maui.Devices;
-using Microsoft.Maui.Internal;
 
 namespace Microsoft.Maui.Platform
 {

src/Core/src/WeakList.cs
@@ -1,0 +1,76 @@
+﻿#nullable enable
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Microsoft.Maui;
+
+/// <summary>
+/// A List type for holding WeakReference's
+/// It clears the underlying List based on a threshold of operations: Add() or GetEnumerator()
+/// </summary>
+class WeakList<T> : IEnumerable<T> where T : class
+{
+	readonly List<WeakReference<T>> _list = new();
+	int _operations = 0;
+
+	public int Count => _list.Count;
+
+	public int CleanupThreshold { get; set; } = 32;
+
+	public void Add(T item)
+	{
+		CleanupIfNeeded();
+		_list.Add(new WeakReference<T>(item));
+	}
+
+	public void Remove(T item)
+	{
+		WeakReference<T> w;
+
+		// A reverse for-loop, means we can call RemoveAt(i) inside the loop
+		for (int i = _list.Count - 1; i >= 0; i--)
+		{
+			w = _list[i];
+			if (w.TryGetTarget(out T? target))
+			{
+				if (target == item)
+				{
+					_list.RemoveAt(i);
+					break;
+				}
+			}
+			else
+			{
+				// Remove if we found one that is not alive
+				_list.RemoveAt(i);
+			}
+		}
+	}
+
+	public void Clear()
+	{
+		_list.Clear();
+		_operations = 0;
+	}
+
+	public IEnumerator<T> GetEnumerator()
+	{
+		CleanupIfNeeded();
+
+		foreach (var w in _list)
+			if (w.TryGetTarget(out T? item))
+				yield return item;
+	}
+
+	void CleanupIfNeeded()
+	{
+		if (++_operations > CleanupThreshold)
+		{
+			_operations = 0;
+			_list.RemoveAll(w => !w.TryGetTarget(out _));
+		}
+	}
+
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}

src/Core/tests/UnitTests/WeakListTests.cs
@@ -1,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Maui.UnitTests
+{
+	public class WeakListTests
+	{
+		[Fact]
+		public async Task ObjectsAreEvicted_GetEnumerator()
+		{
+			var expected = new object();
+			var list = new WeakList<object> { expected, new object() };
+			list.CleanupThreshold = 1;
+
+			await Task.Yield();
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+
+			foreach (var item in list)
+			{
+				// Do nothing
+			}
+
+			Assert.Equal(1, list.Count);
+			Assert.Equal(expected, list.First());
+		}
+
+		[Fact]
+		public async Task ObjectsAreEvicted_Remove()
+		{
+			var expected = new object();
+			var list = new WeakList<object> { expected, new object() };
+			list.CleanupThreshold = 1;
+
+			await Task.Yield();
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+
+			list.Remove(expected);
+
+			Assert.Equal(0, list.Count);
+		}
+
+		[Fact]
+		public async Task ObjectsAreEvicted_Add()
+		{
+			var expected = new object();
+			var list = new WeakList<object> { expected, new object() };
+			list.CleanupThreshold = 1;
+
+			await Task.Yield();
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+
+			list.Add(new object());
+
+			Assert.Equal(2, list.Count);
+			Assert.Equal(expected, list.First());
+		}
+	}
+}