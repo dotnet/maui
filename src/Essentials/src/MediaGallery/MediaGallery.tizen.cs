1e7fe00755cfcabfab452e0842cc38db1cb90dc2
dimonovdd <dimovde@yandex.ru>
2023-02-26 22:51:57 +0300

    upd

---
+++ src/Essentials/src/MediaGallery/MediaGallery.tizen.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Storage;
+
+namespace Microsoft.Maui.Media
+{
+	partial class MediaGalleryImplementation
+	{
+		public bool IsSupported => false;
+
+		public bool CheckCaptureSupport(MediaFileType type) => throw new NotImplementedInReferenceAssemblyException();
+
+		public Task<IEnumerable<MediaFileResult>> PlatformCaptureAsync(MediaFileType type, CancellationToken token = default)
+			=> throw new NotImplementedInReferenceAssemblyException();
+
+		public Task<IEnumerable<MediaFileResult>> PlatformPickAsync(MediaPickRequest request, CancellationToken token = default)
+			=> throw new NotImplementedInReferenceAssemblyException();
+
+		public Task PlatformSaveAsync(MediaFileType type, Stream fileStream, string fileName)
+			=> throw new NotImplementedInReferenceAssemblyException();
+
+		public Task PlatformSaveAsync(MediaFileType type, byte[] data, string fileName)
+			=> throw new NotImplementedInReferenceAssemblyException();
+
+		public Task PlatformSaveAsync(MediaFileType type, string filePath)
+			=> throw new NotImplementedInReferenceAssemblyException();
+	}
+}
