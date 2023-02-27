34965181f225668c91e3358f5a01fa63c8530ee2
Rui Marinho <me@ruimarinho.net>
2023-02-27 16:41:00 +0000

    Update FrameTests.cs

--- src/Controls/tests/DeviceTests/Elements/Frame/FrameTests.cs
+++ src/Controls/tests/DeviceTests/Elements/Frame/FrameTests.cs
@@ -25,7 +25,6 @@ namespace Microsoft.Maui.DeviceTests
 					handlers.AddHandler<StackLayout, LayoutHandler>();
 					handlers.AddHandler<Entry, EntryHandler>();
 					handlers.AddHandler<Frame, FrameRenderer>();
-					handlers.AddHandler<Frame, FrameRenderer>();
 					handlers.AddHandler<Label, LabelHandler>();
 					handlers.AddHandler<Button, ButtonHandler>();
 				});
