81f92d2035a71e8dbfc840f3ab42a223e65c2371
lytico <lytico@users.sourceforge.net>
2023-03-01 00:41:04 +0100

    [Controls.Sample] introduce Main-Gtk

---
+++ src/Controls/samples/Controls.Sample/Platforms/Gtk/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace Maui.Controls.Sample.Platform
+{
+	class Program : MauiGtkApplication
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+		static void Main(string[] args)
+		{
+			var app = new Program();
+			app.Run(args);
+		}
+	}
+}
