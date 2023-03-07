7a51a682182af7c94c40204e13adaf72be5cd721
Manuel de la Pena <mandel@microsoft.com>
2023-03-07 16:24:39 -0500

    Address review comments.

--- src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellPageRendererTracker.cs
+++ src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellPageRendererTracker.cs
@@ -116,7 +116,7 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility
 			{
 				SearchHandler = Shell.GetSearchHandler(Page);
 			}
-			else if (e.PropertyName == Shell.TitleViewProperty.PropertyName || e.PropertyName == VisualElement.HeightProperty.PropertyName || e.PropertyName == VisualElement.WidthProperty.PropertyName)
+			else if (e.PropertyName == Shell.TitleViewProperty.PropertyName || e.IsOneOf (VisualElement.HeightProperty, VisualElement.WidthProperty))
 			{
 				UpdateTitleView();
 			}
