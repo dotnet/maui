1c060b38c81dec2da8a4c5ad49ca79ba5b748086
Gerald Versluis <gerald.versluis@microsoft.com>
2023-03-09 11:15:43 +0100

    Update src/Core/src/Platform/iOS/MauiWKWebView.cs
    Co-authored-by: Manuel de la Pena <mandel@microsoft.com>

--- src/Core/src/Platform/iOS/MauiWKWebView.cs
+++ src/Core/src/Platform/iOS/MauiWKWebView.cs
@@ -100,13 +100,11 @@ namespace Microsoft.Maui.Platform
 				var safeFullUri = new Uri(safeHostUri, safeRelativeUri);
 				NSUrlRequest request = new NSUrlRequest(new NSUrl(safeFullUri.AbsoluteUri));
 
-				_ = _handler.TryGetTarget(out var handler);
-
-				if ((handler?.HasCookiesToLoad(safeFullUri.AbsoluteUri) ?? false) && !(OperatingSystem.IsIOSVersionAtLeast(11) || OperatingSystem.IsTvOSVersionAtLeast(11)))
-					return;
-
-				if (handler is not null)
-					await handler.SyncPlatformCookiesAsync(safeFullUri.AbsoluteUri);
+				if (_handler.TryGetTarget(out var handler)) {
+				    if (handler.HasCookiesToLoad(safeFullUri.AbsoluteUri) && !(OperatingSystem.IsIOSVersionAtLeast(11) || OperatingSystem.IsTvOSVersionAtLeast(11))) 
+				        return;
+				    await handler.SyncPlatformCookiesAsync(safeFullUri.AbsoluteUri);
+				} 
 
 				LoadRequest(request);
 			}
