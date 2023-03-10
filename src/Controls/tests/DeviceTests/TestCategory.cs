9d1b28c5c7475e3b8ec665536f4f1295673713cf
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-03-10 14:09:10 +0100

    Added more tests

--- src/Controls/tests/DeviceTests/TestCategory.cs
+++ src/Controls/tests/DeviceTests/TestCategory.cs
@@ -34,6 +34,7 @@
 		public const string ScrollView = "ScrollView";
 		public const string SearchBar = "SearchBar";
 		public const string Shell = "Shell";
+		public const string SwipeView = "SwipeView";
 		public const string TabbedPage = "TabbedPage";
 		public const string TextInput = "TextInput";
 		public const string Toolbar = "Toolbar";
