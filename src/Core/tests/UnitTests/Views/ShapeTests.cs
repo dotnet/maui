7b6480ab5b80a1fcdbfa6877748492772cf288b6
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-03-14 14:15:40 +0100

    More unit tests

---
+++ src/Core/tests/UnitTests/Views/ShapeTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Shapes;
+using Microsoft.Maui.Graphics;
+using Xunit;
+
+namespace Microsoft.Maui.UnitTests.Views
+{
+	[Category(TestCategory.Core, TestCategory.View)]
+	public class ShapeTests
+	{
+		[Fact]
+		public void ShapeFillSubscribed()
+		{
+			var fill = new SolidColorBrush { Color = Colors.Red };
+			var shape = new Rectangle { Fill = fill };
+
+			bool fired = false;
+			shape.PropertyChanged += (sender, e) =>
+			{
+				if (e.PropertyName == nameof(Shape.Fill))
+					fired = true;
+			};
+
+			fill.Color = Colors.Green;
+			Assert.True(fired, "PropertyChanged did not fire!");
+		}
+
+		[Fact]
+		public void ShapeStrokeSubscribed()
+		{
+			var stroke = new SolidColorBrush { Color = Colors.Red };
+			var shape = new Rectangle { Stroke = stroke };
+
+			bool fired = false;
+			shape.PropertyChanged += (sender, e) =>
+			{
+				if (e.PropertyName == nameof(Shape.Stroke))
+					fired = true;
+			};
+
+			stroke.Color = Colors.Green;
+			Assert.True(fired, "PropertyChanged did not fire!");
+		}
+	}
+}
