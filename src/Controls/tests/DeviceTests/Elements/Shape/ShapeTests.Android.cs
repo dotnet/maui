160527812e4e785f4d97e51012e0b2407611598d
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-03-14 10:56:08 +0100

    Invalidate shapes changing any property inside the brushes (Fill, Stroke)

---
+++ src/Controls/tests/DeviceTests/Elements/Shape/ShapeTests.Android.cs
@@ -0,0 +1,20 @@
+﻿using AndroidX.AppCompat.Widget;
+using Microsoft.Maui.Handlers;
+using System.Threading.Tasks;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	public partial class ShapeTests
+	{
+		Task PerformClick(IButton button)
+		{
+			return InvokeOnMainThreadAsync(() =>
+			{
+				GetNativeButton(CreateHandler<ButtonHandler>(button)).PerformClick();
+			});
+		}
+
+		AppCompatButton GetNativeButton(ButtonHandler buttonHandler) =>
+			buttonHandler.PlatformView;
+	}
+}
