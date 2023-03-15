8b20308023860a26b1be7ab06843342661bba3db
Shane Neuville <shneuvil@microsoft.com>
2023-03-15 10:45:54 -0500

    - fix up the timings on the keyboard a bit more

--- src/Controls/tests/DeviceTests/Elements/TextInput/TextInputTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/TextInput/TextInputTests.Android.cs
@@ -18,7 +18,8 @@ namespace Microsoft.Maui.DeviceTests
 		{
 			SetupBuilder();
 			var textInput = (View)Activator.CreateInstance(controlType);
-
+			textInput.HeightRequest = 100;
+			textInput.WidthRequest = 100;
 			await InvokeOnMainThreadAsync(async () =>
 			{
 				var handler = (IPlatformViewHandler)CreateHandler(textInput);
@@ -29,20 +30,15 @@ namespace Microsoft.Maui.DeviceTests
 					try
 					{
 						await AssertionExtensions.HideKeyboardForView(platformView, message: $"Make sure keyboard starts out closed {controlType}");
-
 						textInput.Focus();
-						await AssertionExtensions.WaitForFocused(platformView, 2000, $"WaitForFocused failed after first focus on {controlType}");
+						await AssertionExtensions.WaitForFocused(platformView, message: $"WaitForFocused failed after first focus on {controlType}");
 						await AssertionExtensions.WaitForKeyboardToShow(platformView, message: $"WaitForKeyboardToShow failed after first focus on {controlType}");
 
 						// Test that keyboard reappears when refocusing on an already focused TextInput control
 						await AssertionExtensions.HideKeyboardForView(platformView, message: $"HideKeyboardForView failed after first keyboard show on {controlType}");
-						await Task.Delay(2000);
 						textInput.Focus();
-						await Task.Delay(2000);
 						await AssertionExtensions.WaitForFocused(platformView, message: $"WaitForFocused failed after second focus on {controlType}");
-						await Task.Delay(2000);
 						await AssertionExtensions.WaitForKeyboardToShow(platformView, message: $"WaitForKeyboardToShow failed after second focus on {controlType}");
-						await Task.Delay(2000);
 					}
 					finally
 					{
