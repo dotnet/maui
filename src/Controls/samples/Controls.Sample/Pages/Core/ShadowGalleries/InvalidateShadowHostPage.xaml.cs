2b87d06d5387bc98eb45e0e125dba6b1e64dbfa5
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-03-15 13:30:50 +0100

    Updated sample

--- src/Controls/samples/Controls.Sample/Pages/Core/ShadowGalleries/InvalidateShadowHostPage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/Core/ShadowGalleries/InvalidateShadowHostPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 
 namespace Maui.Controls.Sample.Pages
 {
@@ -7,12 +9,29 @@ namespace Maui.Controls.Sample.Pages
 		public InvalidateShadowHostPage()
 		{
 			InitializeComponent();
+			UpdateShadowOffset();
 		}
 
 		void OnUpdateHostSizeClicked(object sender, EventArgs args)
 		{
 			var random = new Random();
 			ShadowHost.MinimumHeightRequest = ShadowHost.MinimumWidthRequest = random.Next(100, 300);
+		}
+
+		void OnShadowOffsetXChanged(object sender, ValueChangedEventArgs e)
+		{
+			UpdateShadowOffset();
+		}
+
+		void OnShadowOffsetYChanged(object sender, ValueChangedEventArgs e)
+		{
+			UpdateShadowOffset();
+		}
+
+		void UpdateShadowOffset()
+		{
+			var offset = new Point(ShadowOffsetXSlider.Value, ShadowOffsetYSlider.Value);
+			ShadowHost.Shadow.Offset = offset;
 		}
 	}
 }
