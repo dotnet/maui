d5d63453ae6f1c0562c35b3af9de3cefc374eb92
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-03-15 11:40:15 +0100

    Fix shadow sizing issues on Windows

---
+++ src/Controls/samples/Controls.Sample/Pages/Core/ShadowGalleries/InvalidateShadowHostPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Maui.Controls.Sample.Pages
+{
+	public partial class InvalidateShadowHostPage
+	{
+		public InvalidateShadowHostPage()
+		{
+			InitializeComponent();
+		}
+
+		void OnUpdateHostSizeClicked(object sender, EventArgs args)
+		{
+			var random = new Random();
+			ShadowHost.MinimumHeightRequest = ShadowHost.MinimumWidthRequest = random.Next(100, 300);
+		}
+	}
+}
