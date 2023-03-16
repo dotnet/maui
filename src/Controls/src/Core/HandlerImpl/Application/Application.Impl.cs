c49403b580ee9d1feb8f9aef8bc7272474e3e34b
Matthew Leibowitz <mattleibow@live.com>
2023-03-16 18:30:19 +0200

    Cache the dynamic AppTheme value in Controls (and not Essentials) (#11200) (#13963)
    Previously, the RequestedAppTheme and RequestedLayoutDiection values were cached on first access and this was incorrect as they can change at runtime - both by the OS and the user.
    
    This PR makes a few changes:
    
    Don't cache the theme and layout in Essentials as there is no way to update the cache
    Don't cache the layout anywhere because this is only accessed once when the window is [re]created
    Cache the theme in the Controls Application where the events can actually handle.
    
    ---------
    
    Co-authored-by: Rui Marinho <me@ruimarinho.net>
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    # Conflicts:
    #	src/Essentials/src/AppInfo/AppInfo.android.cs
    
    Co-authored-by: Javier Suárez <javiersuarezruiz@hotmail.com>

--- src/Controls/src/Core/HandlerImpl/Application/Application.Impl.cs
+++ src/Controls/src/Core/HandlerImpl/Application/Application.Impl.cs
@@ -112,10 +112,7 @@ namespace Microsoft.Maui.Controls
 
 		void IApplication.ThemeChanged()
 		{
-			if (UserAppTheme != AppTheme.Unspecified)
-				return;
-
-			TriggerThemeChangedActual();
+			PlatformAppTheme = AppInfo.RequestedTheme;
 		}
 
 		protected virtual Window CreateWindow(IActivationState? activationState)
