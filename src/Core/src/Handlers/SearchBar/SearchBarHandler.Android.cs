cbcc8b7c2fbac42503bb3aa22d542848cfa4f7e1
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-03-17 10:26:57 +0100

    Changes from PR feedabck

--- src/Core/src/Handlers/SearchBar/SearchBarHandler.Android.cs
+++ src/Core/src/Handlers/SearchBar/SearchBarHandler.Android.cs
@@ -119,12 +119,6 @@ namespace Microsoft.Maui.Handlers
 			handler.PlatformView?.UpdateKeyboard(searchBar);
 		}
 
-		static void MapFocus(ISearchBarHandler handler, ISearchBar searchBar, object? args)
-		{
-			if (args is FocusRequest request)
-				handler.QueryEditor?.Focus(request);
-		}
-
 		void OnQueryTextSubmit(object? sender, QueryTextSubmitEventArgs e)
 		{
 			VirtualView.SearchButtonPressed();
