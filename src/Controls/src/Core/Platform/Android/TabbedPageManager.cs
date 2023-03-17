1c1911a7a660adf9319065219a3c1a0a90590cd1
Shane Neuville <shneuvil@microsoft.com>
2023-03-17 13:30:04 -0500

    [net7.0] Cast to a less derived type on TabbedPageManager (#13689) (#13998)
    * Cast to a less derived type on TabbedPageManager (#13689)
    
    # Conflicts:
    #	src/Controls/src/Core/Platform/Android/TabbedPageManager.cs
    #	src/Controls/tests/DeviceTests/Elements/TabbedPage/TabbedPageTests.Android.cs
    #	src/Controls/tests/DeviceTests/Elements/TabbedPage/TabbedPageTests.cs
    
    * Update TabbedPageManager.cs
    
    * - fix namespaces for tests

--- src/Controls/src/Core/Platform/Android/TabbedPageManager.cs
+++ src/Controls/src/Core/Platform/Android/TabbedPageManager.cs
@@ -271,13 +271,17 @@ namespace Microsoft.Maui.Controls.Handlers
 			e.Apply((o, i, c) => SetupPage((Page)o), (o, i) => TeardownPage((Page)o), Reset);
 
 			ViewPager2 pager = _viewPager;
-			var adapter = (MultiPageFragmentStateAdapter<Page>)pager.Adapter;
-			adapter.CountOverride = Element.Children.Count;
+
+			if (pager.Adapter is MultiPageFragmentStateAdapter<Page> adapter)
+			{
+				adapter.CountOverride = Element.Children.Count;
+			}
+
 			if (IsBottomTabPlacement)
 			{
 				BottomNavigationView bottomNavigationView = _bottomNavigationView;
 
-				adapter.NotifyDataSetChanged();
+				pager.Adapter?.NotifyDataSetChanged();
 
 				if (Element.Children.Count == 0)
 				{
@@ -295,7 +299,7 @@ namespace Microsoft.Maui.Controls.Handlers
 			{
 				TabLayout tabs = _tabLayout;
 
-				adapter.NotifyDataSetChanged();
+				pager.Adapter?.NotifyDataSetChanged();
 				if (Element.Children.Count == 0)
 				{
 					tabs.RemoveAllTabs();
--- src/Controls/tests/DeviceTests/Elements/TabbedPage/TabbedPageTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/TabbedPage/TabbedPageTests.Android.cs
@@ -3,6 +3,7 @@ using AndroidX.ViewPager2.Widget;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.DeviceTests.Stubs;
 using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Hosting;
 using Xunit;
 
 namespace Microsoft.Maui.DeviceTests
@@ -22,6 +23,33 @@ namespace Microsoft.Maui.DeviceTests
 				var platformView = tabbedPage.Handler.PlatformView as ViewPager2;
 				Assert.NotNull(platformView);
 				return Task.CompletedTask;
+			});
+		}
+
+		[Fact(DisplayName = "Custom RecyclerView Adapter Doesn't Crash")]
+		public async Task CustomRecyclerViewAdapterDoesNotCrash()
+		{
+			SetupBuilder(builder =>
+			{
+				builder.ConfigureMauiHandlers(handler =>
+				{
+					handler.AddHandler<TabbedPage, CustomTestAdapterHandler>();
+				});
+			});
+
+			var tabbedPage = CreateBasicTabbedPage();
+
+			await CreateHandlerAndAddToWindow<WindowHandlerStub>(new Window(tabbedPage), async (handler) =>
+			{
+				// If you currently try to modify the children too early.
+				// This will sometimes cause `NotifyDataSourceChanged` on the
+				// adapter to get called while it's already processing
+				await Task.Delay(50);
+
+				tabbedPage.Children.Add(new ContentPage());
+
+				// make sure changes have time to propagate
+				await Task.Delay(50);
 			});
 		}
 	}
