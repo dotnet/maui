8da01827cd266b4286b0382b610bc9e43860ed3d
E.Z. Hart <hartez@gmail.com>
2023-03-17 15:59:28 -0600

    Use a layout for MauiButton on Windows which supports LineBreakMode Fixes #9277

--- src/Core/src/Platform/Windows/MauiButton.cs
+++ src/Core/src/Platform/Windows/MauiButton.cs
@@ -2,6 +2,7 @@
 using Microsoft.UI.Xaml.Automation.Peers;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
+using Windows.Security.Credentials.UI;
 using WThickness = Microsoft.UI.Xaml.Thickness;
 
 namespace Microsoft.Maui.Platform
@@ -10,38 +11,143 @@ namespace Microsoft.Maui.Platform
 	{
 		public MauiButton()
 		{
+			Content = new DefaultMauiButtonContent();
+
 			VerticalAlignment = VerticalAlignment.Stretch;
 			HorizontalAlignment = HorizontalAlignment.Stretch;
-			Content = new StackPanel
-			{
-				HorizontalAlignment = HorizontalAlignment.Center,
-				VerticalAlignment = VerticalAlignment.Center,
-				Orientation = Orientation.Horizontal,
-				Margin = new WThickness(0),
-				Children =
-				{
-					new Image
-					{
-						VerticalAlignment = VerticalAlignment.Center,
-						HorizontalAlignment = HorizontalAlignment.Center,
-						Stretch = Stretch.Uniform,
-						Margin = new WThickness(0),
-						Visibility = UI.Xaml.Visibility.Collapsed,
-					},
-					new TextBlock
-					{
-						VerticalAlignment = VerticalAlignment.Center,
-						HorizontalAlignment = HorizontalAlignment.Center,
-						Margin = new WThickness(0),
-						Visibility = UI.Xaml.Visibility.Collapsed,
-					}
-				}
-			};
 		}
 
 		protected override AutomationPeer OnCreateAutomationPeer()
 		{
 			return new MauiButtonAutomationPeer(this);
+		}
+	}
+
+	internal class DefaultMauiButtonContent : Grid
+	{
+		readonly Image _image;
+		readonly TextBlock _textBlock;
+
+		public DefaultMauiButtonContent()
+		{
+			RowDefinitions.Add(new RowDefinition { Height = UI.Xaml.GridLength.Auto });
+			RowDefinitions.Add(new RowDefinition { Height = UI.Xaml.GridLength.Auto });
+
+			ColumnDefinitions.Add(new ColumnDefinition { Width = UI.Xaml.GridLength.Auto });
+			ColumnDefinitions.Add(new ColumnDefinition { Width = UI.Xaml.GridLength.Auto });
+
+			HorizontalAlignment = HorizontalAlignment.Center;
+			VerticalAlignment = VerticalAlignment.Center;
+			Margin = new WThickness(0);
+
+			_image = new Image
+			{
+				VerticalAlignment = VerticalAlignment.Center,
+				HorizontalAlignment = HorizontalAlignment.Center,
+				Stretch = Stretch.None,
+				Margin = new WThickness(0),
+				Visibility = UI.Xaml.Visibility.Collapsed,
+			};
+
+			_textBlock = new TextBlock
+			{
+				VerticalAlignment = VerticalAlignment.Center,
+				HorizontalAlignment = HorizontalAlignment.Center,
+				Margin = new WThickness(0),
+				Visibility = UI.Xaml.Visibility.Collapsed,
+			};
+
+			Children.Add(_image);
+			Children.Add(_textBlock);
+
+			LayoutImageLeft(0);
+		}
+
+		public void LayoutImageLeft(double spacing)
+		{
+			SetupHorizontalLayout(spacing);
+
+			Grid.SetColumn(_image, 0);
+			Grid.SetColumn(_textBlock, 1);
+
+			ColumnDefinitions[0].Width = UI.Xaml.GridLength.Auto;
+			ColumnDefinitions[1].Width = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Star);
+		}
+
+		public void LayoutImageRight(double spacing)
+		{
+			SetupHorizontalLayout(spacing);
+			
+			Grid.SetColumn(_image, 1);
+			Grid.SetColumn(_textBlock, 0);
+
+			ColumnDefinitions[0].Width = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Star);
+			ColumnDefinitions[1].Width = UI.Xaml.GridLength.Auto;
+		}
+
+		public void LayoutImageTop(double spacing) 
+		{
+			SetupVerticalLayout(spacing);
+
+			Grid.SetRow(_image, 0);
+			Grid.SetRow(_textBlock, 1);
+		}
+
+		public void LayoutImageBottom(double spacing) 
+		{
+			SetupVerticalLayout(spacing);
+
+			Grid.SetRow(_image, 1);
+			Grid.SetRow(_textBlock, 0);
+		}
+
+		double AdjustSpacing(double spacing)
+		{
+			if (_image.Visibility == UI.Xaml.Visibility.Collapsed
+				|| _textBlock.Visibility == UI.Xaml.Visibility.Collapsed)
+			{
+				return 0;
+			}
+
+			return spacing;
+		}
+
+		void SetupHorizontalLayout(double spacing)
+		{
+			RowSpacing = 0;
+			ColumnSpacing = AdjustSpacing(spacing);
+
+			RowDefinitions[0].Height = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Star);
+			RowDefinitions[1].Height = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Star);
+
+			Grid.SetRow(_image, 0);
+			Grid.SetRowSpan(_image, 2);
+			Grid.SetColumnSpan(_image, 1);
+
+			Grid.SetRow(_textBlock, 0);
+			Grid.SetRowSpan(_textBlock, 2);
+			Grid.SetColumnSpan(_textBlock, 1);
+
+		}
+
+		void SetupVerticalLayout(double spacing)
+		{
+			ColumnSpacing = 0;
+			RowSpacing = AdjustSpacing(spacing);
+
+			RowDefinitions[0].Height = UI.Xaml.GridLength.Auto;
+			RowDefinitions[1].Height = UI.Xaml.GridLength.Auto;
+
+			ColumnDefinitions[0].Width = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Star);
+			ColumnDefinitions[1].Width = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Star);
+
+			Grid.SetRowSpan(_image, 1);
+			Grid.SetColumn(_image, 0);
+			Grid.SetColumnSpan(_image, 2);
+
+			Grid.SetRowSpan(_textBlock, 1);
+			Grid.SetColumn(_textBlock, 0);
+			Grid.SetColumnSpan(_textBlock, 2);
 		}
 	}
 }
