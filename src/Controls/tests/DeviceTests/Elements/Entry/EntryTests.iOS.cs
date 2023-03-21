9c7d2bd5585e12869f054680d63ff84e8917f19b
tj-devel709 <tjlambert@microsoft.com>
2023-03-21 10:13:08 -0500

    fix tests

--- src/Controls/tests/DeviceTests/Elements/Entry/EntryTests.iOS.cs
+++ src/Controls/tests/DeviceTests/Elements/Entry/EntryTests.iOS.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Handlers.Compatibility;
@@ -44,18 +43,15 @@ namespace Microsoft.Maui.DeviceTests
 			return -1;
 		}
 
-		void SetupBuilder()
+		void SetupNextBuilder()
 		{
 			EnsureHandlerCreated(builder =>
 			{
 				builder.ConfigureMauiHandlers(handlers =>
 				{
-					handlers.AddHandler<ViewCell, ViewCellRenderer>();
-					handlers.AddHandler<TextCell, TextCellRenderer>();
 					handlers.AddHandler<ListView, ListViewRenderer>();
 					handlers.AddHandler<TableView, TableViewRenderer>();
 					handlers.AddHandler<VerticalStackLayout, LayoutHandler>();
-					handlers.AddHandler<Label, LabelHandler>();
 					handlers.AddHandler<Entry, EntryHandler>();
 					handlers.AddHandler<EntryCell, EntryCellRenderer>();
 				});
@@ -65,7 +61,7 @@ namespace Microsoft.Maui.DeviceTests
 		[Fact]
 		public async Task NextListView()
 		{
-			SetupBuilder();
+			SetupNextBuilder();
 
 			var entry1 = new Entry
 			{
@@ -73,71 +69,15 @@ namespace Microsoft.Maui.DeviceTests
 				ReturnType = ReturnType.Next
 			};
 
-			var listView = new ListView(ListViewCachingStrategy.RecycleElement)
+			var listView = new ListView()
 			{
-				ItemsSource = new List<EntryCell>
-				{
-					new EntryCell () {Text="EntryCell1"},
-					new EntryCell () {Text="EntryCell2"},
-					new EntryCell () {Text="EntryCell3"},
-				}
-			};
-
-			var layout = new VerticalStackLayout()
-			{
-				entry1,
-				listView,
-			};
-
-			await CreateHandlerAndAddToWindow<LayoutHandler>(layout, async (handler) =>
-			{
-				KeyboardAutoManager.GoToNextResponderOrResign(entry1.ToPlatform(), customSuperView: entry1.ToPlatform().Superview);
-				var firstResponder = layout.ToPlatform().FindFirstResponder();
-				var field = firstResponder as UITextField;
-				Assert.NotNull(field);
-				Assert.True(field.Text == "EntryCell1");
-				await Task.Delay(10);
-			});
-		}
-
-		[Fact]
-		public async Task NextListView2()
-		{
-			SetupBuilder();
-
-			ObservableCollection<Entry> data = new ObservableCollection<Entry>()
-			{
-				new Entry {Text="cat" },
-				new Entry {Text="dog" },
-				new Entry {Text="catdog" },
-			};
-
-			var entry1 = new Entry
-			{
-				Text = "Entry 1",
-				ReturnType = ReturnType.Next
-			};
-
-			var listView = new ListView(ListViewCachingStrategy.RecycleElement)
-			{
-				//ItemTemplate = new DataTemplate() {
-				//	new ViewCell (),
-				//}
-
-				//listView.DataTemplate = new DataTemplate() {
-				//	new ViewCell() {
-				//		View = new Entry(),
-				//	};
-				//};
-
 				ItemTemplate = new DataTemplate(() =>
 				{
-					return new ViewCell()
-					{
-						View = new Entry ()
-					};
+					var cell = new EntryCell();
+					cell.SetBinding(EntryCell.TextProperty, ".");
+					return cell;
 				}),
-				ItemsSource = data
+				ItemsSource = Enumerable.Range(0, 10).Select(i => $"EntryCell {i}").ToList()
 			};
 
 			var layout = new VerticalStackLayout()
@@ -146,21 +86,27 @@ namespace Microsoft.Maui.DeviceTests
 				listView,
 			};
 
-			await CreateHandlerAndAddToWindow<LayoutHandler>(layout, async (handler) =>
+			layout.HeightRequest = 1000;
+			layout.WidthRequest = 300;
+
+			await InvokeOnMainThreadAsync(async () =>
 			{
-				KeyboardAutoManager.GoToNextResponderOrResign(entry1.ToPlatform(), customSuperView: entry1.ToPlatform().Superview);
-				var firstResponder = layout.ToPlatform().FindFirstResponder();
-				var field = firstResponder as UITextField;
-				Assert.NotNull(field);
-				Assert.True(field.Text == "cat");
-				await Task.Delay(10);
+				var contentViewHandler = CreateHandler<LayoutHandler>(layout);
+				await contentViewHandler.PlatformView.AttachAndRun(() =>
+				{
+					KeyboardAutoManager.GoToNextResponderOrResign(entry1.ToPlatform(), customSuperView: entry1.ToPlatform().Superview);
+					var firstResponder = layout.ToPlatform().FindFirstResponder();
+					var field = firstResponder as UITextField;
+					Assert.NotNull(field);
+					Assert.True(field.Text == "EntryCell 0");
+				});
 			});
 		}
 
 		[Fact]
 		public async Task NextTableView()
 		{
-			SetupBuilder();
+			SetupNextBuilder();
 
 			var entry1 = new Entry
 			{
@@ -191,14 +137,20 @@ namespace Microsoft.Maui.DeviceTests
 				tableView,
 			};
 
-			await CreateHandlerAndAddToWindow<LayoutHandler>(layout, async (handler) =>
+			layout.HeightRequest = 1000;
+			layout.WidthRequest = 300;
+
+			await InvokeOnMainThreadAsync(async () =>
 			{
-				KeyboardAutoManager.GoToNextResponderOrResign(entry1.ToPlatform(), customSuperView: entry1.ToPlatform().Superview);
-				var firstResponder = layout.ToPlatform().FindFirstResponder();
-				var field = firstResponder as UITextField;
-				Assert.NotNull(field);
-				Assert.True(field.Text == "EntryCell1");
-				await Task.Delay(10);
+				var contentViewHandler = CreateHandler<LayoutHandler>(layout);
+				await contentViewHandler.PlatformView.AttachAndRun(() =>
+				{
+					KeyboardAutoManager.GoToNextResponderOrResign(entry1.ToPlatform(), customSuperView: entry1.ToPlatform().Superview);
+					var firstResponder = layout.ToPlatform().FindFirstResponder();
+					var field = firstResponder as UITextField;
+					Assert.NotNull(field);
+					Assert.True(field.Text == "EntryCell1");
+				});
 			});
 		}
 	}
