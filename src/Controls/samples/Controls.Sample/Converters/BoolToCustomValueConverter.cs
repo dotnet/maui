eb58ee775067b7ff4e27c49088bdc185b7999705
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-03-22 11:01:44 +0100

    Added sample to the gallery to validate that colors changes from Converter

---
+++ src/Controls/samples/Controls.Sample/Converters/BoolToCustomValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace Controls.Sample.Converters
+{
+	public class BoolToCustomValueConverter : IValueConverter
+	{
+		public object ValueIfTrue { get; set; }
+		public object ValueIfFalse { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is bool boolValue)
+			{
+				return boolValue ? ValueIfTrue : ValueIfFalse;
+			}
+
+			return ValueIfFalse;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
