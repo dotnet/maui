c44d94f7546608d7ae44e2270c123149a3f47ab3
tj-devel709 <tjlambert@microsoft.com>
2023-03-23 18:12:28 -0500

    add tests and change variable name and usage

--- src/Controls/tests/DeviceTests/Elements/Entry/EntryTests.iOS.cs
+++ src/Controls/tests/DeviceTests/Elements/Entry/EntryTests.iOS.cs
@@ -1,6 +1,10 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Platform;
 using UIKit;
+using Xunit;
 
 namespace Microsoft.Maui.DeviceTests
 {
@@ -35,6 +39,58 @@ namespace Microsoft.Maui.DeviceTests
 				return (int)textField.GetOffsetFromPosition(textField.SelectedTextRange.Start, textField.SelectedTextRange.End);
 
 			return -1;
+		}
+
+		[Fact]
+		public async Task ScrollSkipsAlert()
+		{
+			var entry = new Entry();
+
+			EnsureHandlerCreated(builder =>
+			{
+				builder.ConfigureMauiHandlers(handler =>
+				{
+					handler.AddHandler<VerticalStackLayout, LayoutHandler>();
+					handler.AddHandler<Entry, EntryHandler>();
+				});
+			});
+
+			var layout = new VerticalStackLayout
+			{
+				entry
+			};
+
+			layout.WidthRequest = 300;
+			layout.HeightRequest = 800;
+
+			await InvokeOnMainThreadAsync(async () =>
+			{
+				var contentViewHandler = CreateHandler<LayoutHandler>(layout);
+				await contentViewHandler.PlatformView.AttachAndRun(async () =>
+				{
+					var layoutPlat = layout.ToPlatform();
+					var win = layoutPlat.Window;
+					if (win is UIWindow uIWindow)
+					{
+						var alert = UIAlertController.Create("Popup Alert", "This is a popup", UIAlertControllerStyle.Alert);
+
+						alert.AddTextField((textfield) =>
+						{
+							textfield.Placeholder = "Placeholder Text";
+						});
+
+						_ = uIWindow.RootViewController.PresentViewControllerAsync(alert, true);
+
+						var taskCompletion = new TaskCompletionSource<bool>();
+
+						uIWindow.RootViewController.DismissViewController(true, () => { taskCompletion.SetResult(true); });
+
+						await taskCompletion.Task;
+					}
+
+					Assert.False(KeyboardAutoManagerScroll.IsKeyboardAutoScrollHandling);
+				});
+			});
 		}
 	}
 }
