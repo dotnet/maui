72a70861d8080c7b00ddb71f4d7135e3d9ace95b
James Parsons <japarson@microsoft.com>
2023-03-23 23:59:22 -0700

    Add first draft color test

---
+++ src/Graphics/tests/DeviceTests/Tests/ColorTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Platform;
+using System.Reflection;
+using Xunit;
+
+namespace Microsoft.Maui.Graphics.DeviceTests;
+
+public class ColorTests
+{
+	[Fact]
+	public void TestEquals()
+	{
+		var fields = typeof(Colors).GetFields(BindingFlags.Public | BindingFlags.Static);
+		foreach (FieldInfo field in fields)
+		{
+			Color expected = (Color)field.GetValue(null);
+
+#if WINDOWS
+			var actual = (expected.ToPlatform() as UI.Xaml.Media.SolidColorBrush).ToColor();
+#else
+			var actual = expected.ToPlatform().ToColor();
+#endif
+
+			var isEquals = expected.Equals(actual);
+			Assert.True(isEquals);
+		}
+	}
+}
