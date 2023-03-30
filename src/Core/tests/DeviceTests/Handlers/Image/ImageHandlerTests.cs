b91788136283f55b42464d5d87f44ac9006baa96
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-03-30 16:00:05 +0200

    Device tests

--- src/Core/tests/DeviceTests/Handlers/Image/ImageHandlerTests.cs
+++ src/Core/tests/DeviceTests/Handlers/Image/ImageHandlerTests.cs
@@ -111,8 +111,8 @@ namespace Microsoft.Maui.DeviceTests
 		}
 
 		[Theory(
-#if __IOS__ || __ANDROID__
-			Skip = "Animated GIFs are not yet supported on iOS. Test failing on ANDROID"
+#if __IOS__ 
+			Skip = "Animated GIFs are not yet supported on iOS."
 #endif
 		)]
 		[InlineData("animated_heart.gif", true)]
