3b2061b5c5e041906a507733de32d088fff39801
Rachel Kang <rachelkang@microsoft.com>
2023-03-30 13:06:36 -0400

    Add TODO comment

--- src/Controls/src/Core/VisualElement.cs
+++ src/Controls/src/Core/VisualElement.cs
@@ -1035,6 +1035,7 @@ namespace Microsoft.Maui.Controls
 
 		internal virtual void ComputeConstraintForView(View view) => view.ComputedConstraint = LayoutConstraint.None;
 
+		// TODO: Obsolete in favor of MapFocus https://github.com/dotnet/maui/issues/14299
 		[EditorBrowsable(EditorBrowsableState.Never)]
 		public event EventHandler<FocusRequestArgs> FocusChangeRequested;
 		internal void InvokeFocusChangeRequested(FocusRequestArgs args) =>
