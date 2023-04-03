acc20eecb7cd14c43f0f959764dbca6c35550695
webwarrior-ws <webwarrior-ws@users.noreply.github.com>
2023-04-03 13:35:57 +0200

    Essentials(Gtk): implement Launcher (#15)
    Ported Launcher code from DotNetEssentials.
    
    (cherry picked from commit 8d082edaf73a19bbd474fa598cebfb998db5df2e)

--- src/Essentials/src/Launcher/Launcher.Gtk.cs
+++ src/Essentials/src/Launcher/Launcher.Gtk.cs
@@ -5,16 +5,24 @@ namespace Microsoft.Maui.ApplicationModel
 {
 	partial class LauncherImplementation
 	{
-		Task<bool> PlatformCanOpenAsync(Uri uri) =>
-			throw ExceptionUtils.NotSupportedOrImplementedException;
+		Task<bool> PlatformCanOpenAsync(Uri uri) => Task.FromResult(true);
 
-		Task<bool> PlatformOpenAsync(Uri uri) =>
-			throw ExceptionUtils.NotSupportedOrImplementedException;
+		Task<bool> PlatformOpenAsync(Uri uri) => GTKTryOpenAsync(uri);
 
 		Task<bool> PlatformOpenAsync(OpenFileRequest request) =>
 			throw ExceptionUtils.NotSupportedOrImplementedException;
 
-		Task<bool> PlatformTryOpenAsync(Uri uri) =>
-			throw ExceptionUtils.NotSupportedOrImplementedException;
+		Task<bool> PlatformTryOpenAsync(Uri uri) => GTKTryOpenAsync(uri);
+
+		// ported from https://github.com/nblockchain/DotNetEssentials/blob/master/Xamarin.Essentials/Launcher/Launcher.netstandard.cs
+		static async Task<bool> GTKTryOpenAsync(Uri uri)
+		{
+			string stdout, stderr;
+			int exitCode;
+			var task = Task.Run(
+				() => GLib.Process.SpawnCommandLineSync("xdg-open " + uri.ToString(), out stdout, out stderr, out exitCode));
+			var result = await task;
+			return result;
+		}
 	}
 }
