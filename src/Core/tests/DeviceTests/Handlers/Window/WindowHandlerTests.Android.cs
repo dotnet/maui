ca99981267cc3b4250547959e8cf551c29c070c6
Shane Neuville <shneuvil@microsoft.com>
2023-04-05 09:33:45 -0500

    Propagate Title to android and ios window

---
+++ src/Core/tests/DeviceTests/Handlers/Window/WindowHandlerTests.Android.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Devices;
+using Microsoft.Maui.Graphics;
+using Xunit;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	public partial class WindowHandlerTests : CoreHandlerTestBase
+	{
+		[Fact]
+		public async Task TitleSetsOnWindow()
+		{
+			await InvokeOnMainThreadAsync(() =>
+			{
+				var activity = MauiContext.Context.GetActivity();
+				Assert.NotNull(activity);
+				var testWindow = activity.GetWindow() as Window;
+				Assert.NotNull(testWindow);
+
+				testWindow.Title = "Test Title";
+				Assert.Equal("Test Title", activity.Title);
+				testWindow.Title = null;
+				Assert.Equal(activity.Title, ApplicationModel.AppInfo.Current.Name);
+			});
+		}
+	}
+}
