48d02949c511bd979f061343f90ea63cd227558e
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-04-14 19:27:52 +0200

    [Android] Allow to update Entry Background (once established) (#12227)
    * Added sample
    
    * Fix the issue
    
    * Added device test
    
    * Applied the same change in more controls
    
    * Added more samples (12991)
    
    * Update src/Core/src/Handlers/DatePicker/DatePickerHandler.Android.cs
    
    Co-authored-by: Manuel de la Pena <mandel@microsoft.com>
    
    * Auto-format source code
    
    * Fix merge issue
    
    * Fixd merge issue
    
    * Auto-format source code
    
    * Changes based on PR feedback
    
    * Remove unnecessary changes
    
    * Added platformView and Context null check
    
    * Small change
    
    * Added attribute
    
    * More changes
    
    * Apply more changes
    
    * Changes based on PR feedback
    
    * Auto-format source code
    
    * Merge branch 'main' into fix-12212
    
    ---------
    
    Co-authored-by: Manuel de la Pena <mandel@microsoft.com>
    Co-authored-by: GitHub Actions Autoformatter <autoformat@example.com>

--- src/Controls/samples/Controls.Sample/Pages/Controls/DatePickerPage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/Controls/DatePickerPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+using System;
+using System.Diagnostics;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 
 namespace Maui.Controls.Sample.Pages
 {
@@ -7,6 +10,35 @@ namespace Maui.Controls.Sample.Pages
 		public DatePickerPage()
 		{
 			InitializeComponent();
+
+			UpdateDatePickerBackground();
+		}
+
+		void OnUpdateBackgroundButtonClicked(object sender, System.EventArgs e)
+		{
+			UpdateDatePickerBackground();
+		}
+
+		void OnClearBackgroundButtonClicked(object sender, System.EventArgs e)
+		{
+			BackgroundDatePicker.Background = null;
+		}
+
+		void UpdateDatePickerBackground()
+		{
+			Random rnd = new Random();
+			Color startColor = Color.FromRgba(rnd.Next(256), rnd.Next(256), rnd.Next(256), 255);
+			Color endColor = Color.FromRgba(rnd.Next(256), rnd.Next(256), rnd.Next(256), 255);
+
+			BackgroundDatePicker.Background = new LinearGradientBrush
+			{
+				EndPoint = new Point(1, 0),
+				GradientStops = new GradientStopCollection
+				{
+					new GradientStop { Color = startColor },
+					new GradientStop { Color = endColor, Offset = 1 }
+				}
+			};
 		}
 
 		void OnFocusDatePickerFocused(object sender, Microsoft.Maui.Controls.FocusEventArgs e)
