c3c23015d2a114c41e2356466556170ffdade451
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-04-24 13:42:02 +0200

    More comments

--- src/Core/src/Core/IAccelerator.cs
+++ src/Core/src/Core/IAccelerator.cs
@@ -4,8 +4,14 @@ namespace Microsoft.Maui
 {
 	public interface IAccelerator
 	{
+		/// <summary>
+		/// Specifies the virtual key used to modify another keypress. 
+		/// </summary>
 		public IEnumerable<string> Modifiers { get; }
 
+		/// <summary>
+		/// Specifies the values for each virtual key.
+		/// </summary>
 		public IEnumerable<string> Keys { get; }
 	}
 }
