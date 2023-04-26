8b02815703b05e90c34e257cdb78e87ac61626c6
Shane Neuville <shneuvil@microsoft.com>
2023-04-26 11:06:15 -0500

    Set menuItem show action back to Never (#14623)
    * Set menuItem show action back to Never
    
    * Auto-format source code
    
    * - fix iOS handler registration
    
    ---------
    
    Co-authored-by: GitHub Actions Autoformatter <autoformat@example.com>

--- src/Controls/tests/DeviceTests/Elements/Toolbar/ToolbarTests.cs
+++ src/Controls/tests/DeviceTests/Elements/Toolbar/ToolbarTests.cs
@@ -16,6 +16,7 @@ using Xunit.Sdk;
 
 #if IOS || MACCATALYST
 using FlyoutViewHandler = Microsoft.Maui.Controls.Handlers.Compatibility.PhoneFlyoutPageRenderer;
+using NavigationViewHandler = Microsoft.Maui.Controls.Handlers.Compatibility.NavigationRenderer;
 #endif
 
 namespace Microsoft.Maui.DeviceTests
@@ -39,6 +40,79 @@ namespace Microsoft.Maui.DeviceTests
 
 					SetupShellHandlers(handlers);
 				});
+			});
+		}
+
+
+#if !IOS && !MACCATALYST
+		[Fact(DisplayName = "Toolbar Items Map Correctly")]
+		public async Task ToolbarItemsMapCorrectly()
+		{
+			SetupBuilder();
+			var toolbarItem = new ToolbarItem() { Text = "Toolbar Item 1" };
+			var navPage = new NavigationPage(new ContentPage()
+			{
+				ToolbarItems =
+				{
+					toolbarItem
+				}
+			});
+
+			await CreateHandlerAndAddToWindow<WindowHandlerStub>(new Window(navPage), (handler) =>
+			{
+				ToolbarItemsMatch(handler, toolbarItem);
+				return Task.CompletedTask;
+			});
+		}
+
+		[Fact(DisplayName = "Toolbar Items Order Updates Correctly After Navigation")]
+		public async Task ToolbarItemsOrderUpdatesCorrectlyAfterNavigation()
+		{
+			SetupBuilder();
+			var toolbarItemFirstPage = new ToolbarItem() { Text = "Toolbar Item 1" };
+			var toolbarItemSecondPage = new ToolbarItem() { Text = "Toolbar Item Second Page", Order = ToolbarItemOrder.Secondary };
+
+			var navPage = new NavigationPage(new ContentPage()
+			{
+				ToolbarItems =
+				{
+					toolbarItemFirstPage
+				}
+			});
+
+			await CreateHandlerAndAddToWindow<WindowHandlerStub>(new Window(navPage), async (handler) =>
+			{
+				await navPage.PushAsync(new ContentPage()
+				{
+					ToolbarItems =
+					{
+						toolbarItemSecondPage
+					}
+				});
+
+				ToolbarItemsMatch(handler, GetExpectedToolbarItems(navPage));
+				ToolbarItemsMatch(handler, toolbarItemSecondPage);
+				await navPage.PopAsync();
+				ToolbarItemsMatch(handler, GetExpectedToolbarItems(navPage));
+				ToolbarItemsMatch(handler, toolbarItemFirstPage);
+			});
+		}
+#endif
+
+		[Fact(DisplayName = "Toolbar Title")]
+		public async Task ToolbarTitle()
+		{
+			SetupBuilder();
+			var navPage = new NavigationPage(new ContentPage()
+			{
+				Title = "Page Title"
+			});
+
+			await CreateHandlerAndAddToWindow<WindowHandlerStub>(new Window(navPage), (handler) =>
+			{
+				string title = GetToolbarTitle(handler);
+				Assert.Equal("Page Title", title);
+				return Task.CompletedTask;
 			});
 		}
 
@@ -104,5 +178,10 @@ namespace Microsoft.Maui.DeviceTests
 			}, context2);
 		}
 #endif
+
+		ToolbarItem[] GetExpectedToolbarItems(NavigationPage navPage)
+		{
+			return ((Toolbar)(navPage.Window as IToolbarElement).Toolbar).ToolbarItems.ToArray();
+		}
 	}
 }
