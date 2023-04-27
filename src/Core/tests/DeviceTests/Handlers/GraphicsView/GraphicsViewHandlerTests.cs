54e760de73991d901c198deb5f2990b2bf291552
Matthew Leibowitz <mattleibow@live.com>
2023-04-28 06:34:51 +0800

    no longer needed

--- src/Core/tests/DeviceTests/Handlers/GraphicsView/GraphicsViewHandlerTests.cs
+++ src/Core/tests/DeviceTests/Handlers/GraphicsView/GraphicsViewHandlerTests.cs
@@ -1,16 +1,9 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Maui.DeviceTests.Stubs;
+using Microsoft.Maui.Graphics.Platform;
 using Xunit;
 using Microsoft.Maui.Storage;
 using System.IO;
-
-#if WINDOWS
-using Microsoft.Maui.Graphics.Platform;
-using Microsoft.Maui.Graphics.Win2D;
-using PlatformImageLoadingService = Microsoft.Maui.Graphics.Win2D.W2DImageLoadingService;
-#else
-using Microsoft.Maui.Graphics.Platform;
-#endif
 
 namespace Microsoft.Maui.DeviceTests
 {
