46edcf1e7c575b0a729d4aaf50b3facd5f5ab382
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-05-05 13:33:35 +0200

    Updated sample

--- src/Controls/samples/Controls.Sample/Pages/Core/BorderGalleries/BorderNoLayoutContent.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/Core/BorderGalleries/BorderNoLayoutContent.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 
 namespace Maui.Controls.Sample.Pages
 {
@@ -7,6 +8,18 @@ namespace Maui.Controls.Sample.Pages
 		public BorderNoLayoutContent()
 		{
 			InitializeComponent();
+		}
+
+		void OnContentBackgroundCheckBoxChanged(object sender, CheckedChangedEventArgs e)
+		{
+			UpdateContentBackground();
+		}
+
+		void UpdateContentBackground()
+		{
+			var contentBackground = ContentBackgroundCheckBox.IsChecked ? Color.FromArgb("#99FF0000") : Colors.Transparent;
+
+			CircleLabel.BackgroundColor = PlayLabel.BackgroundColor = StopLabel.BackgroundColor = contentBackground;
 		}
 	}
 }
