f418f9b005c235337abaf5a28c87f70f2769c082
Matthew Leibowitz <mattleibow@live.com>
2023-05-10 19:29:06 +0800

    this

--- src/Controls/src/Core/IInputTransparentContainerElement.cs
+++ src/Controls/src/Core/IInputTransparentContainerElement.cs
@@ -5,6 +5,10 @@ namespace Microsoft.Maui.Controls
 	// There are 2 Layout types: Controls and Compatibility
 	interface IInputTransparentContainerElement
 	{
+		bool InputTransparent { get; }
+
 		bool CascadeInputTransparent { get; }
+
+		Element Parent { get; }
 	}
 }
