259353605b947d372f25584851d28dcc2cfc1c50
E.Z. Hart <hartez@users.noreply.github.com>
2023-05-16 15:55:44 -0600

    Update src/Core/src/Layouts/GridLayoutManager.cs

--- src/Core/src/Layouts/GridLayoutManager.cs
+++ src/Core/src/Layouts/GridLayoutManager.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data.Common;
 using System.Diagnostics;
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Primitives;
