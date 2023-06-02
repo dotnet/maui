59c72cab39b87f541a46fae65239c70ed43a0051
E.Z. Hart <hartez@gmail.com>
2023-06-02 11:20:56 -0600

    [Windows] Fix CollectionView.RemainingItemsThresholdReached (#14391)
    * fix(bug): Fix `RemainingItemsThresholdReached` on Windows
    
    * Fixed an issue where the `CollectionView.RemainingItemsThresholdReached` event wasn't firing on Windows.
    * Added a DeviceTest for the event
    
    * Remove switch statement
    
    * Update src/Controls/src/Core/Handlers/Items/ItemsViewHandler.Windows.cs
    
    Co-authored-by: Rui Marinho <me@ruimarinho.net>
    
    ---------
    
    Co-authored-by: Mike Corsaro <mikecorsaro@microsoft.com>
    Co-authored-by: Rui Marinho <me@ruimarinho.net>

--- src/Controls/tests/DeviceTests/Elements/CollectionView/CollectionViewTests.Windows.cs
+++ src/Controls/tests/DeviceTests/Elements/CollectionView/CollectionViewTests.Windows.cs
@@ -49,5 +49,89 @@ namespace Microsoft.Maui.DeviceTests
 				return Task.CompletedTask;
 			});
 		}
+
+		[Fact]
+		public async Task ValidateItemContainerDefaultHeight()
+		{
+			SetupBuilder();
+			ObservableCollection<string> data = new ObservableCollection<string>()
+			{
+				"Item 1",
+				"Item 2",
+				"Item 3"
+			};
+
+			var collectionView = new CollectionView()
+			{
+				ItemTemplate = new Controls.DataTemplate(() =>
+				{
+					return new VerticalStackLayout()
+					{
+						new Label()
+					};
+				}),
+				ItemsSource = data
+			};
+
+			var layout = new VerticalStackLayout()
+			{
+				collectionView
+			};
+
+			await CreateHandlerAndAddToWindow<LayoutHandler>(layout, async (handler) =>
+			{
+				await Task.Delay(100);
+				ValidateItemContainerStyle(collectionView);
+			});
+		}
+
+		void ValidateItemContainerStyle(CollectionView collectionView)
+		{
+			var handler = (CollectionViewHandler)collectionView.Handler;
+			var control = handler.PlatformView;
+
+			var minHeight = control.ItemContainerStyle.Setters
+				.OfType<WSetter>()
+				.FirstOrDefault(X => X.Property == FrameworkElement.MinHeightProperty).Value;
+
+			Assert.Equal(0d, minHeight);
+		}
+
+		[Fact]
+		public async Task ValidateSendRemainingItemsThresholdReached()
+		{
+			SetupBuilder();
+			ObservableCollection<string> data = new();
+			for (int i = 0; i < 20; i++)
+			{
+				data.Add($"Item {i + 1}");
+			}
+
+			CollectionView collectionView = new();
+			collectionView.ItemsSource = data;
+			collectionView.HeightRequest = 200;
+
+			var layout = new VerticalStackLayout()
+			{
+				collectionView
+			};
+
+			collectionView.RemainingItemsThreshold = 1;
+			collectionView.RemainingItemsThresholdReached += (s, e) =>
+			{
+				for (int i = 20; i < 30; i++)
+				{
+					data.Add($"Item {i + 1}");
+				}
+			};
+
+			await CreateHandlerAndAddToWindow<LayoutHandler>(layout, async (handler) =>
+			{
+				await Task.Delay(200);
+				collectionView.ScrollTo(19, -1, position: ScrollToPosition.End, false);
+				await Task.Delay(200);
+				Assert.True(data.Count == 30);
+			});
+		}
 	}
 }
