eaa66eeab374f1651961fd8410d4668562fda2c1
Rui Marinho <me@ruimarinho.net>
2023-06-05 16:33:43 +0100

    Use variables

--- eng/scripts/appium-install.ps1
+++ eng/scripts/appium-install.ps1
@@ -1,45 +1,52 @@
+$appiumVersion = '2.0.0-beta.64'
+$windowsDriverVersion = 2.7.2
+$androidDriverVersion = 2.25.1
+$iOSDriverVersion = 4.30.2
+$macDriverVersion = 1.6.1
+
 node -v
 npm install -g npm
 node -v
-npm install -g appium@2.0.0-beta.61
+npm install -g appium@$appiumVersion
 appium -v
 $x = appium driver list --installed --json  | ConvertFrom-Json
+
 if(!$x.windows)
 {
-    appium driver install --source=npm appium-windows-driver@2.7.2
+    appium driver install --source=npm appium-windows-driver@$windowsDriverVersion
 }
 else
 {
     appium driver uninstall windows
-    appium driver install --source=npm appium-windows-driver@2.7.2
+    appium driver install --source=npm appium-windows-driver@$windowsDriverVersion
 }
 
 if(!$x.uiautomator2)
 {
-    appium driver install uiautomator2@2.25.1
+    appium driver install uiautomator2@$androidDriverVersion
 }
 else
 {
     appium driver uninstall uiautomator2
-    appium driver install uiautomator2@2.25.1
+    appium driver install uiautomator2@$androidDriverVersion
 }
 
 if(!$x.xcuitest)
 {
-    appium driver install xcuitest@4.30.2
+    appium driver install xcuitest@$iOSDriverVersion
 }
 else
 {
     appium driver uninstall xcuitest
-    appium driver install xcuitest@4.30.2
+    appium driver install xcuitest@$iOSDriverVersion
 }
 
 if(!$x.mac2)
 {
-    appium driver install mac2@1.5.3
+    appium driver install mac2@$macDriverVersion
 }
 else
 {
     appium driver uninstall mac2
-    appium driver install mac2@1.5.3
+    appium driver install mac2@$macDriverVersion
 }
