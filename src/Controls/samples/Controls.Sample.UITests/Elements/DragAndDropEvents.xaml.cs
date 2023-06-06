7d83b4efe069059cb2780e6b871fd5f63ec178bb
Scott Banning <scoban@microsoft.com>
2023-06-06 13:19:18 -0700

    Add the drag and drop gestures back to the appium ui tests for mac/ios targets

--- src/Controls/samples/Controls.Sample.UITests/Elements/DragAndDropEvents.xaml.cs
+++ src/Controls/samples/Controls.Sample.UITests/Elements/DragAndDropEvents.xaml.cs
@@ -6,6 +6,7 @@ namespace Maui.Controls.Sample
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DragAndDropEvents : ContentView
 	{
+		bool _emittedDragOver = false;
 		public DragAndDropEvents()
 		{
 			InitializeComponent();
@@ -18,6 +19,7 @@ namespace Maui.Controls.Sample
 
 		void DragStarting(object sender, DragStartingEventArgs e)
 		{
+			_emittedDragOver = false;
 			AddEvent(nameof(DragStarting));
 		}
 
@@ -33,7 +35,11 @@ namespace Maui.Controls.Sample
 
 		void DragOver(object sender, DragEventArgs e)
 		{
-			AddEvent(nameof(DragOver));
+			if (!_emittedDragOver) // This can generate a lot of noise, only add it once
+			{
+				AddEvent(nameof(DragOver));
+				_emittedDragOver = true;
+			}
 		}
 
 		void Drop(object sender, DropEventArgs e)
