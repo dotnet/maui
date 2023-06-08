525ea4f0e4e5fc049c354747540cacaa97f6f863
Gerald Versluis <gerald.versluis@microsoft.com>
2023-06-08 13:21:46 +0200

    Build & Run Packaged Windows DeviceTests

--- src/TestUtils/src/DeviceTests.Runners/VisualRunner/Pages/HomePage.xaml.cs
+++ src/TestUtils/src/DeviceTests.Runners/VisualRunner/Pages/HomePage.xaml.cs
@@ -1,5 +1,10 @@
 ﻿#nullable enable
+using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.TestUtils.DeviceTests.Runners.HeadlessRunner;
 
 namespace Microsoft.Maui.TestUtils.DeviceTests.Runners.VisualRunner.Pages
 {
@@ -8,6 +13,31 @@ namespace Microsoft.Maui.TestUtils.DeviceTests.Runners.VisualRunner.Pages
 		public HomePage()
 		{
 			InitializeComponent();
+
+			this.Loaded += HomePage_Loaded;
+		}
+
+		bool hasRunHeadless = false;
+
+		private async void HomePage_Loaded(object? sender, System.EventArgs e)
+		{
+			string? testResultsFile = null;
+
+#if WINDOWS
+			var cliArgs = Environment.GetCommandLineArgs();
+			if (cliArgs.Length > 1)
+				testResultsFile = HeadlessTestRunner.TestResultsFile = cliArgs.Skip(1).FirstOrDefault();
+#endif
+
+			if (!string.IsNullOrEmpty(testResultsFile) && !hasRunHeadless)
+			{
+				hasRunHeadless = true;
+
+				var headlessRunner = this.Handler!.MauiContext!.Services.GetRequiredService<HeadlessTestRunner>();
+				await headlessRunner.RunTestsAsync();
+
+				Process.GetCurrentProcess().Kill();
+			}
 		}
 
 		protected override void OnAppearing()
