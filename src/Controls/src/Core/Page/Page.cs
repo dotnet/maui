a259ab9aed1fbc41bce4706343d078016bc49c84
tj-devel709 <tjlambert@microsoft.com>
2023-06-12 09:56:42 -0500

    change the page directory

--- src/Controls/src/Core/Page/Page.cs
+++ src/Controls/src/Core/Page/Page.cs
@@ -14,7 +14,7 @@ using Microsoft.Maui.Graphics;
 namespace Microsoft.Maui.Controls
 {
 	/// <include file="../../docs/Microsoft.Maui.Controls/Page.xml" path="Type[@FullName='Microsoft.Maui.Controls.Page']/Docs/*" />
-	public partial class Page : VisualElement, ILayout, IPageController, IElementConfiguration<Page>, IPaddingElement, ISafeAreaView
+	public partial class Page : VisualElement, ILayout, IPageController, IElementConfiguration<Page>, IPaddingElement, ISafeAreaView, IView, IToolbarElement
 	{
 		/// <include file="../../docs/Microsoft.Maui.Controls/Page.xml" path="//Member[@MemberName='BusySetSignalName']/Docs/*" />
 		public const string BusySetSignalName = "Microsoft.Maui.Controls.BusySet";
@@ -631,5 +631,97 @@ namespace Microsoft.Maui.Controls
 
 			_titleView = newTitleView;
 		}
+
+		internal bool HasNavigatedTo { get; private set; }
+
+		Paint IView.Background
+		{
+			get
+			{
+				if (!Brush.IsNullOrEmpty(Background))
+					return Background;
+				if (!ImageSource.IsNullOrEmpty(BackgroundImageSource))
+					return new ImageSourcePaint(BackgroundImageSource);
+				if (BackgroundColor.IsNotDefault())
+					return new SolidColorBrush(BackgroundColor);
+
+				return null;
+			}
+		}
+
+		Toolbar _toolbar;
+		IToolbar IToolbarElement.Toolbar
+		{
+			get => _toolbar;
+		}
+
+		internal Toolbar Toolbar
+		{
+			get => _toolbar;
+			set
+			{
+				_toolbar = value;
+				Handler?.UpdateValue(nameof(IToolbarElement.Toolbar));
+			}
+		}
+
+		internal void SendNavigatedTo(NavigatedToEventArgs args)
+		{
+			HasNavigatedTo = true;
+			NavigatedTo?.Invoke(this, args);
+			OnNavigatedTo(args);
+			(this as IPageContainer<Page>)?.CurrentPage?.SendNavigatedTo(args);
+		}
+
+		internal void SendNavigatingFrom(NavigatingFromEventArgs args)
+		{
+			NavigatingFrom?.Invoke(this, args);
+			OnNavigatingFrom(args);
+			(this as IPageContainer<Page>)?.CurrentPage?.SendNavigatingFrom(args);
+		}
+
+		internal void SendNavigatedFrom(NavigatedFromEventArgs args)
+		{
+			HasNavigatedTo = false;
+			NavigatedFrom?.Invoke(this, args);
+			OnNavigatedFrom(args);
+			(this as IPageContainer<Page>)?.CurrentPage?.SendNavigatedFrom(args);
+		}
+
+		public event EventHandler<NavigatedToEventArgs> NavigatedTo;
+		public event EventHandler<NavigatingFromEventArgs> NavigatingFrom;
+		public event EventHandler<NavigatedFromEventArgs> NavigatedFrom;
+
+		protected virtual void OnNavigatedTo(NavigatedToEventArgs args) { }
+		protected virtual void OnNavigatingFrom(NavigatingFromEventArgs args) { }
+		protected virtual void OnNavigatedFrom(NavigatedFromEventArgs args) { }
+
+		public virtual Window GetParentWindow()
+			=> this.FindParentOfType<Window>();
+	}
+
+	public sealed class NavigatingFromEventArgs : EventArgs
+	{
+
+	}
+
+	public sealed class NavigatedToEventArgs : EventArgs
+	{
+		internal NavigatedToEventArgs(Page previousPage)
+		{
+			PreviousPage = previousPage;
+		}
+
+		internal Page PreviousPage { get; }
+	}
+
+	public sealed class NavigatedFromEventArgs : EventArgs
+	{
+		internal NavigatedFromEventArgs(Page destinationPage)
+		{
+			DestinationPage = destinationPage;
+		}
+
+		internal Page DestinationPage { get; }
 	}
 }
