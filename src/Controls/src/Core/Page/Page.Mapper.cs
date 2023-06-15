653bf39abe2074d4fda350081ec75eca08b9b1f5
tj-devel709 <tjlambert@microsoft.com>
2023-06-15 12:17:02 -0500

    use Replace mapping instead

--- src/Controls/src/Core/Page/Page.Mapper.cs
+++ src/Controls/src/Core/Page/Page.Mapper.cs
@@ -3,17 +3,10 @@ namespace Microsoft.Maui.Controls;
 
 public partial class Page
 {
-	public static IPropertyMapper<IContentView, IPageHandler> ControlsPageMapper =
-			new PropertyMapper<IContentView, IPageHandler>(ContentViewHandler.Mapper)
-			{
-#if IOS
-				[PlatformConfiguration.iOSSpecific.Page.SafeAreaInsetsProperty.PropertyName] = MapUpdateSafeAreaInsets,
-#endif
-			};
-
 	internal static new void RemapForControls()
 	{
-		// Adjust the mappings to preserve Controls.Page legacy behaviors
-		PageHandler.Mapper = ControlsPageMapper;
+#if IOS
+		PageHandler.Mapper.ReplaceMapping<IContentView, IPageHandler>(PlatformConfiguration.iOSSpecific.Page.SafeAreaInsetsProperty.PropertyName, MapUpdateSafeAreaInsets);
+#endif
 	}
 }
