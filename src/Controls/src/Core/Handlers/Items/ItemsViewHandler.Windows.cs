1620b63598861ad57815ce86747193f5bda4d0a4
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-06-22 11:48:05 +0200

    Unsubscribe before a subscription

--- src/Controls/src/Core/Handlers/Items/ItemsViewHandler.Windows.cs
+++ src/Controls/src/Core/Handlers/Items/ItemsViewHandler.Windows.cs
@@ -116,6 +116,7 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			}
 			else
 			{
+				PlatformView.Items.VectorChanged -= OnItemsVectorChanged;
 				PlatformView.Items.VectorChanged += OnItemsVectorChanged;
 			}
 		}
