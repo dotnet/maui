ffff4e3f17bc1bc2bee8b947e246f22f105c23bd
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-06-27 11:49:58 +0200

    Fix build errors

--- src/Core/src/Handlers/SwipeItemView/SwipeItemViewHandler.Windows.cs
+++ src/Core/src/Handlers/SwipeItemView/SwipeItemViewHandler.Windows.cs
@@ -8,8 +8,7 @@ namespace Microsoft.Maui.Handlers
 		{
 			return new ContentPanel
 			{
-				CrossPlatformMeasure = VirtualView.CrossPlatformMeasure,
-				CrossPlatformArrange = VirtualView.CrossPlatformArrange
+				CrossPlatformLayout = VirtualView
 			};
 		}
 
@@ -19,8 +18,7 @@ namespace Microsoft.Maui.Handlers
 			_ = VirtualView ?? throw new InvalidOperationException($"{nameof(VirtualView)} should have been set by base class.");
 			_ = PlatformView ?? throw new InvalidOperationException($"{nameof(PlatformView)} should have been set by base class.");
 
-			PlatformView.CrossPlatformMeasure = VirtualView.CrossPlatformMeasure;
-			PlatformView.CrossPlatformArrange = VirtualView.CrossPlatformArrange;
+			PlatformView.CrossPlatformLayout = VirtualView;
 		}
 
 		public static void MapContent(ISwipeItemViewHandler handler, ISwipeItemView page)
