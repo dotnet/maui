237f9a4eff9f99c67ace343e07f5a7815a406021
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-06-30 11:09:49 +0200

    Fix the issue

--- src/Graphics/src/Graphics/Font.cs
+++ src/Graphics/src/Graphics/Font.cs
@@ -45,8 +45,11 @@ namespace Microsoft.Maui.Graphics
 		public FontStyleType StyleType { get; private set; }
 
 		public bool Equals(IFont other)
-			=> StyleType == other.StyleType && Weight == other.Weight && Name.Equals(other.Name, StringComparison.Ordinal);
-
+			=> 
+			StyleType == other.StyleType && 
+			Weight == other.Weight &&
+			string.IsNullOrWhiteSpace(Name) ? string.IsNullOrWhiteSpace(other.Name) : Name.Equals(other.Name, StringComparison.OrdinalIgnoreCase);
+	
 		public override bool Equals(object obj)
 			=> obj is IFont font && Equals(font);
 
---
+++ src/Graphics/tests/Graphics.Tests/FontUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Microsoft.Maui.Graphics.Tests
+{
+	public class FontUnitTests
+	{
+		[Fact]
+		public void TestDefault()
+		{
+			Assert.True(Font.Default.Equals(Font.Default));
+		}
+
+		[Fact]
+		public void TestDefaultBold()
+		{
+			Assert.True(Font.DefaultBold.Equals(Font.DefaultBold));
+		}
+	}
+}
