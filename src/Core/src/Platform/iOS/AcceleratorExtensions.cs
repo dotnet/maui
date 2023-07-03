752c7249c29eb403fc15c48ce8282a79b85e9cc9
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-07-03 20:16:42 +0200

    [Windows/macOS] Implement menu key accelerators  (#14740)
    * Implement Meny Accelerators on Windows
    
    * More comments
    
    * Fix build errors
    
    * Updated extension
    
    * More samples
    
    * Small code refactoring
    
    * Updated implementation
    
    * Tweak interface to reflect single key only usage
    
    * Add Mac implementation
    
    * Auto-format source code
    
    * Add samples from XAML and with different key combos
    
    * Small tweaks based on feedback
    
    * Update src/Core/src/Platform/Windows/KeyboardAcceleratorExtensions.cs
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    
    * Update src/Core/src/PublicAPI/net-android/PublicAPI.Unshipped.txt
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    
    * Update src/Core/src/PublicAPI/net-tizen/PublicAPI.Unshipped.txt
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    
    * Update src/Core/src/PublicAPI/net/PublicAPI.Unshipped.txt
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    
    * Update src/Core/src/PublicAPI/netstandard2.0/PublicAPI.Unshipped.txt
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    
    * Update src/Core/src/PublicAPI/netstandard/PublicAPI.Unshipped.txt
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    
    * Update src/Core/src/PublicAPI/net-windows/PublicAPI.Unshipped.txt
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    
    * Update src/Core/src/PublicAPI/net-ios/PublicAPI.Unshipped.txt
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    
    * Update src/Core/src/PublicAPI/net-maccatalyst/PublicAPI.Unshipped.txt
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    
    * Update src/Core/src/Core/IMenuElement.cs
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    
    * Add if/defs for netstandard2.1 DIM support
    
    * Allow to use numbers as key on Windows
    
    * Tweak ifdefs
    
    * Tweaks based on my understanding of feedback
    
    * Remove irrelevant sandbox changes
    
    * Undo more sandbox changes
    
    * Added support to  multiple accelerators on Windows
    
    * Updates based on feedback
    
    * More updates based on feedback
    
    * Small refactoring (from for to foreach)
    
    * More refactoring
    
    * Update based on feedback
    
    * Fix build errors
    
    * Add some API doc comments
    
    * Fix broken tests
    
    * Added more docstrings in the public APIs
    
    * Fix typo in tests and add key only case
    
    * Add more doc comments
    
    * Update src/Core/src/Handlers/MenuFlyoutItem/MenuFlyoutItemHandler.Windows.cs
    
    Co-authored-by: Juan Diego Herrera <juherrera@microsoft.com>
    
    * Fix and update api docs some more
    
    * Changes based on feedback
    
    * Avoid to use Linq in Accelerator class
    
    ---------
    
    Co-authored-by: Rachel Kang <rachelkang@microsoft.com>
    Co-authored-by: GitHub Actions Autoformatter <autoformat@example.com>
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    Co-authored-by: Juan Diego Herrera <juherrera@microsoft.com>

---
+++ src/Core/src/Platform/iOS/AcceleratorExtensions.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace Microsoft.Maui.Platform
+{
+	internal static class AcceleratorExtensions
+	{
+		internal static UIMenuElement CreateMenuItem(this IMenuFlyoutItem virtualView, IMauiContext mauiContext)
+		{
+			int index = MenuFlyoutItemHandler.menus.Count;
+			UIImage? uiImage = virtualView.Source.GetPlatformMenuImage(mauiContext);
+			var selector = new Selector($"MenuItem{index}:");
+
+			bool selectorFound =
+				MauiUIApplicationDelegate.Current.RespondsToSelector(selector);
+
+			if (!selectorFound)
+			{
+				throw new InvalidOperationException(
+					$"By default we only support 50 MenuItems. You can add more by adding the following code to {MauiUIApplicationDelegate.Current.GetType()}\n\n" +
+					$"[Export(\"MenuItem{index}: \")]\n" +
+					$"internal void MenuItem{index}(UICommand uICommand)\n" +
+					"{\n" +
+					"	uICommand.SendClicked();\n" +
+					"}");
+			}
+
+			var accelerator = virtualView.Accelerators?[0];
+			if (accelerator is null)
+				return virtualView.CreateMenuItemCommand(index, uiImage, selector);
+
+			var key = accelerator.Key;
+			var modifiers = accelerator.Modifiers;
+
+			UIKeyModifierFlags modifierFlags = 0;
+
+			// Single key accelerators (no modifier, i.e. "A") and multi-key accelerators are supported (>=1 modifier and 1 key only, i.e. Ctrl+F, Ctrl+Shift+F)
+			if (modifiers is not null && modifiers.Count > 0)
+			{
+				foreach (var modifier in modifiers)
+				{
+					var modifierMask = modifier.ToLowerInvariant();
+
+					switch (modifierMask)
+					{
+						case "ctrl":
+							modifierFlags |= UIKeyModifierFlags.Control;
+							break;
+						case "cmd":
+							modifierFlags |= UIKeyModifierFlags.Command;
+							break;
+						case "alt":
+							modifierFlags |= UIKeyModifierFlags.Alternate;
+							break;
+						case "shift":
+							modifierFlags |= UIKeyModifierFlags.Shift;
+							break;
+					}
+				}
+			}
+
+			return virtualView.CreateMenuItemKeyCommand(index, uiImage, selector, modifierFlags, key);
+		}
+
+		static UIMenuElement CreateMenuItemKeyCommand(this IMenuFlyoutItem virtualView, int index, UIImage? uiImage, Selector selector, UIKeyModifierFlags modifierFlags, string key)
+		{
+			var keyCommand = UIKeyCommand.Create(	
+				title: virtualView.Text,
+				uiImage,
+				selector,
+				key,
+				modifierFlags,
+				new NSString(index.ToString()));
+
+			keyCommand.WantsPriorityOverSystemBehavior = true;
+
+			MenuFlyoutItemHandler.menus[index] = virtualView;
+
+			return keyCommand;
+		}
+
+		static UIMenuElement CreateMenuItemCommand(this IMenuFlyoutItem virtualView, int index, UIImage? uiImage, Selector selector)
+		{
+			var command = UICommand.Create(				
+				title: virtualView.Text,
+				uiImage,
+				selector,
+				new NSString(index.ToString()));
+
+			MenuFlyoutItemHandler.menus[index] = virtualView;
+			return command;
+		}
+	}
+}
