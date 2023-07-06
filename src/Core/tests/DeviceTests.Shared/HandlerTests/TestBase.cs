0504ca2d4b4ce0c91d48bcb5c564093ec0d3d380
Matthew Leibowitz <mattleibow@live.com>
2023-07-06 20:30:35 +0200

    Ensure "text boxes" preserve cursor location (#15099)
    * Ensure iOS "text boxes" preserve cursor location
    
    * Fix the Windows tests
    
    * Attach the controls to the UI for focus tests
    
    * Adding some more "text box" tests
    
    * todo
    
    * tests
    
    * Revert "todo"
    
    This reverts commit 328c6ba789cc40cf28e57b7606037055dbe4c115.
    
    # Conflicts:
    #	src/Controls/tests/DeviceTests/Elements/TextInput/TextInputTests.cs
    
    * this
    
    * move
    
    * Revert "this"
    
    This reverts commit d62b12af5cc999a126d1d333d493999b47891305.
    
    * moar tests
    
    * Revert "Revert "todo""
    
    This reverts commit 6065a41d8a58bfdfdf4990680e0d51fe9936a7e3.
    
    * all working on windows
    
    * Merge the methods
    
    * Update src/Controls/tests/DeviceTests/ControlsHandlerTestBase.cs
    
    Co-authored-by: Rachel Kang <rachelkang@microsoft.com>
    
    * fix the variable names
    
    * fix
    
    * feedback
    
    ---------
    
    Co-authored-by: Rachel Kang <rachelkang@microsoft.com>

--- src/Core/tests/DeviceTests.Shared/HandlerTests/TestBase.cs
+++ src/Core/tests/DeviceTests.Shared/HandlerTests/TestBase.cs
@@ -9,19 +9,19 @@ namespace Microsoft.Maui.DeviceTests
 	{
 		public const int EmCoefficientPrecision = 4;
 
-		protected Task<T> InvokeOnMainThreadAsync<T>(Func<T> func) =>
+		protected static Task<T> InvokeOnMainThreadAsync<T>(Func<T> func) =>
 			TestDispatcher.Current.DispatchAsync(func);
 
-		protected Task InvokeOnMainThreadAsync(Action action) =>
+		protected static Task InvokeOnMainThreadAsync(Action action) =>
 			TestDispatcher.Current.DispatchAsync(action);
 
-		protected Task InvokeOnMainThreadAsync(Func<Task> action) =>
+		protected static Task InvokeOnMainThreadAsync(Func<Task> action) =>
 			TestDispatcher.Current.DispatchAsync(action);
 
-		public Task<T> InvokeOnMainThreadAsync<T>(Func<Task<T>> func) =>
+		protected static Task<T> InvokeOnMainThreadAsync<T>(Func<Task<T>> func) =>
 			TestDispatcher.Current.DispatchAsync(func);
 
-		protected async Task WaitForGC()
+		protected static async Task WaitForGC()
 		{
 			GC.Collect();
 			GC.WaitForPendingFinalizers();
