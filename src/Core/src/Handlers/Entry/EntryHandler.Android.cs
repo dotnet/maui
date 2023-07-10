b312e3a33993101f0babf30350f6b830c75c8f90
Matthew Leibowitz <mattleibow@live.com>
2023-07-10 22:43:38 +0200

    fix things

--- src/Core/src/Handlers/Entry/EntryHandler.Android.cs
+++ src/Core/src/Handlers/Entry/EntryHandler.Android.cs
@@ -142,6 +142,7 @@ namespace Microsoft.Maui.Handlers
 
 			// Let the mapping know that the update is coming from changes to the platform control
 			DataFlowDirection = DataFlowDirection.FromPlatform;
+
 			VirtualView.UpdateText(e);
 
 			// Reset to the default direction
