6686494b7fc59af390c75aac41fc6c2807817434
Lutz Roeder <lutzroeder@users.noreply.github.com>
2023-07-10 09:53:34 -0700

    Fix 'Semibold is not a word' (#16069)

--- src/Templates/src/templates/maui-mobile/MauiProgram.cs
+++ src/Templates/src/templates/maui-mobile/MauiProgram.cs
@@ -12,7 +12,7 @@ public static class MauiProgram
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
 			});
 
 //-:cnd:noEmit
