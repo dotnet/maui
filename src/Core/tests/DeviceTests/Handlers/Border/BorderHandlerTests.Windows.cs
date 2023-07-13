b0b5c1cfe1d4d7c6a257449c2cf55a8abd7be37e
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-07-13 12:49:54 +0200

    Remove unnecessary changes

--- src/Core/tests/DeviceTests/Handlers/Border/BorderHandlerTests.Windows.cs
+++ src/Core/tests/DeviceTests/Handlers/Border/BorderHandlerTests.Windows.cs
@@ -1,42 +1,7 @@
-﻿using System.Threading.Tasks;
-using Microsoft.Maui.DeviceTests.Stubs;
-using Microsoft.UI.Xaml.Hosting;
-using Xunit;
-
-namespace Microsoft.Maui.DeviceTests
+﻿namespace Microsoft.Maui.DeviceTests
 {
 	public partial class BorderHandlerTests
 	{
-		[Theory(DisplayName = "Inner CornerRadius Initializes Correctly")]
-		[InlineData(0)]
-		[InlineData(12)]
-		[InlineData(24)]
-		public async Task InnerCornerRadiusInitializesCorrectly(int cornerRadius)
-		{
-			var expected = cornerRadius;
-
-			var border = new BorderStub()
-			{
-				Content = new LabelStub { Text = "Background", TextColor = Colors.White },
-				Shape = new RoundRectangleShapeStub { CornerRadius = cornerRadius},
-				Background = new SolidPaintStub(Colors.White),
-				Stroke = new SolidPaintStub(Colors.Black),
-				StrokeThickness = 2,
-				Height = 100,
-				Width = 300
-			};
-
-			await AttachAndRun(border, (handler) =>
-			{
-				var contentPanel = GetNativeBorder(handler);
-				var content = contentPanel.Content;
-				var visual = ElementCompositionPreview.GetElementVisual(content);
-
-				var clip = visual.Clip;
-				Assert.NotNull(clip);
-			});
-		}
-
 		ContentPanel GetNativeBorder(BorderHandler borderHandler) =>
 			borderHandler.PlatformView;
 	}
