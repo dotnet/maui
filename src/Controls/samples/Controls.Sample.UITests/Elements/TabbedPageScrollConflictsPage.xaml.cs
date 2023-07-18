a352ca587880f5a2e14989a0dec3e0da58181b03
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-07-18 11:29:42 +0200

    Updated test

--- src/Controls/samples/Controls.Sample.UITests/Elements/TabbedPageScrollConflictsPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.UITests/Elements/TabbedPageScrollConflictsPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
 
 namespace Maui.Controls.Sample
 {
-
+	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class TabbedPageScrollConflictsPage : TabbedPage
 	{
 		public TabbedPageScrollConflictsPage()
