c9ffc41fa255740990f1c3fcd56a25b601118747
cat0363 <kenji.nagano.0363@gmail.com>
2023-07-20 16:27:45 +0900

    Add test case.

--- src/Controls/tests/DeviceTests/Elements/Label/LabelTests.iOS.cs
+++ src/Controls/tests/DeviceTests/Elements/Label/LabelTests.iOS.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Handlers;
 using UIKit;
+using Xunit;
 
 namespace Microsoft.Maui.DeviceTests
 {
@@ -18,5 +20,114 @@ namespace Microsoft.Maui.DeviceTests
 
 		int GetPlatformMaxLines(LabelHandler labelHandler) =>
  			(int)GetPlatformLabel(labelHandler).Lines;
+
+		double GetPlatformCharacterSpacing(LabelHandler labelHandler)
+		{
+			var attributedText = GetPlatformLabel(labelHandler).AttributedText;
+			var characterSpacing = attributedText.GetCharacterSpacing();
+			return characterSpacing;
+		}
+
+		double GetPlatformLineHeight(LabelHandler labelHandler)
+		{
+			var attributedText = GetPlatformLabel(labelHandler).AttributedText;
+			var lineHeight = attributedText.GetLineHeight();
+			return lineHeight;
+		}
+
+		TextDecorations GetPlatformTextDecorations(LabelHandler labelHandler)
+		{
+			var attributedText = GetPlatformLabel(labelHandler).AttributedText;
+			var textDecorations = attributedText.GetTextDecorations();
+			return textDecorations;
+		}
+
+		[Fact(DisplayName = "Using CharacterSpacing with LineHeight and TextDecorations works Correctly")]
+		public async Task CharacterSpacingWithLineHeightWithTextDecorationsWorksCorrectly()
+		{
+			var expectedCharacterSpacing1 = 5;
+			var expectedLineHeight1 = 1.5d;
+			var expectedTextDecorations1 = TextDecorations.Underline;
+
+			var expectedCharacterSpacing2 = 5;
+			var expectedLineHeight2 = 1.5d;
+			var expectedTextDecorations2 = TextDecorations.Strikethrough;
+
+			var expectedCharacterSpacing3 = 0;
+			var expectedLineHeight3 = 0;
+			var expectedTextDecorations3 = TextDecorations.None;
+
+			var expectedCharacterSpacing4 = 0;
+			var expectedLineHeight4 = 0;
+			var expectedTextDecorations4 = TextDecorations.None;
+
+			var label1 = new Label()
+			{
+				Text = "This is label tests.",
+				CharacterSpacing = expectedCharacterSpacing1,
+				LineHeight = expectedLineHeight1,
+				TextDecorations = expectedTextDecorations1
+			};
+
+			var label2 = new Label()
+			{
+				Text = "This is label tests.",
+				CharacterSpacing = expectedCharacterSpacing2,
+				LineHeight = expectedLineHeight2,
+				TextDecorations = expectedTextDecorations2
+			};
+
+			var label3 = new Label()
+			{
+				FormattedText = new FormattedString()
+			};
+			label3.FormattedText.AddLogicalChild(
+				new Span()
+				{
+					Text = "This is label tests.",
+					CharacterSpacing = 5,
+					LineHeight = 1.5d,
+					TextDecorations = TextDecorations.Underline
+				}
+			);
+
+			var label4 = new Label()
+			{
+				TextType = TextType.Html,
+				Text = "This is <strong style=\"color:red\">label</strong> tests."
+			};
+
+			var handler1 = await CreateHandlerAsync<LabelHandler>(label1);
+			var actualCharacterSpacing1 = await InvokeOnMainThreadAsync(() => GetPlatformCharacterSpacing(handler1));
+			Assert.Equal(expectedCharacterSpacing1, actualCharacterSpacing1);
+			var actualLineHeight1 = await InvokeOnMainThreadAsync(() => GetPlatformLineHeight(handler1));
+			Assert.Equal(expectedLineHeight1, actualLineHeight1);
+			var actualTextDecorations1 = await InvokeOnMainThreadAsync(() => GetPlatformTextDecorations(handler1));
+			Assert.Equal(expectedTextDecorations1, actualTextDecorations1);
+
+			var handler2 = await CreateHandlerAsync<LabelHandler>(label2);
+			var actualCharacterSpacing2 = await InvokeOnMainThreadAsync(() => GetPlatformCharacterSpacing(handler2));
+			Assert.Equal(expectedCharacterSpacing2, actualCharacterSpacing2);
+			var actualLineHeight2 = await InvokeOnMainThreadAsync(() => GetPlatformLineHeight(handler2));
+			Assert.Equal(expectedLineHeight2, actualLineHeight2);
+			var actualTextDecorations2 = await InvokeOnMainThreadAsync(() => GetPlatformTextDecorations(handler2));
+			Assert.Equal(expectedTextDecorations2, actualTextDecorations2);
+
+			var handler3 = await CreateHandlerAsync<LabelHandler>(label3);
+			var actualCharacterSpacing3 = await InvokeOnMainThreadAsync(() => GetPlatformCharacterSpacing(handler3));
+			Assert.Equal(expectedCharacterSpacing3, actualCharacterSpacing3);
+			var actualLineHeight3 = await InvokeOnMainThreadAsync(() => GetPlatformLineHeight(handler3));
+			Assert.Equal(expectedLineHeight3, actualLineHeight3);
+			var actualTextDecorations3 = await InvokeOnMainThreadAsync(() => GetPlatformTextDecorations(handler3));
+			Assert.Equal(expectedTextDecorations3, actualTextDecorations3);
+
+			var handler4 = await CreateHandlerAsync<LabelHandler>(label4);
+			var actualCharacterSpacing4 = await InvokeOnMainThreadAsync(() => GetPlatformCharacterSpacing(handler4));
+			Assert.Equal(expectedCharacterSpacing4, actualCharacterSpacing4);
+			var actualLineHeight4 = await InvokeOnMainThreadAsync(() => GetPlatformLineHeight(handler4));
+			Assert.Equal(expectedLineHeight4, actualLineHeight4);
+			var actualTextDecorations4 = await InvokeOnMainThreadAsync(() => GetPlatformTextDecorations(handler4));
+			Assert.Equal(expectedTextDecorations4, actualTextDecorations4);
+		}
 	}
 }
