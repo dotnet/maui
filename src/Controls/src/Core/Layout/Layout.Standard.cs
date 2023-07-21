db9e3a7847499f7f3ed38ff5a31d542b0557c4c4
Matthew Leibowitz <mattleibow@live.com>
2023-07-21 16:05:34 +0200

    Update Layout.Standard.cs

--- src/Controls/src/Core/Layout/Layout.Standard.cs
+++ src/Controls/src/Core/Layout/Layout.Standard.cs
@@ -5,8 +5,10 @@ namespace Microsoft.Maui.Controls
 {
 	public partial class Layout
 	{
+		[Obsolete]
 		public static void MapInputTransparent(LayoutHandler handler, Layout layout) { }
 
+		[Obsolete]
 		public static void MapInputTransparent(ILayoutHandler handler, Layout layout) { }
 
 		[Obsolete]
