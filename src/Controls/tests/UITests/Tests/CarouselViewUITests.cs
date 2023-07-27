e110803fbe3133bf7464d001595c216fef8b5ae1
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-07-27 12:20:38 +0200

    Added UI Tests

---
+++ src/Controls/tests/UITests/Tests/CarouselViewUITests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests
+{
+	public class CarouselViewUITests : UITestBase
+	{
+		const string DragAndDropGallery = "* marked:'CarouselView Gallery'";
+		public CarouselViewUITests(TestDevice device)
+			: base(device)
+		{
+		}
+
+		protected override void FixtureSetup()
+		{
+			base.FixtureSetup();
+			App.NavigateToGallery(DragAndDropGallery);
+		}
+
+		protected override void FixtureTeardown()
+		{
+			base.FixtureTeardown();
+			App.NavigateBack();
+		}
+
+		[Test]
+		public async Task CarouselViewSetPosition()
+		{
+			App.WaitForElement("lblPosition");
+			await Task.Delay(1000);
+			var result = App.Query(c => c.Marked("lblPosition")).First().Text;
+			Assert.AreEqual("3", result);
+		}
+
+		[Test]
+		public void CarouselViewGoToNextCurrentItem()
+		{
+			int indexToTest = 3;
+			var index = indexToTest.ToString();
+			var nextIndex = (indexToTest + 1).ToString();
+
+			CheckLabelValue("lblPosition", index);
+			CheckLabelValue("lblCurrentItem", index);
+
+			App.Tap(x => x.Marked("btnNext"));
+			CheckLabelValue("lblPosition", nextIndex);
+			CheckLabelValue("lblCurrentItem", nextIndex);
+			CheckLabelValue("lblSelected", nextIndex);
+		}
+
+		static void CheckLabelValue(string labelAutomationId, string value)
+		{
+			var result = App.Query(c => c.Marked(labelAutomationId)).First().Text;
+			Assert.AreEqual(value, result);
+		}
+	}
+}
