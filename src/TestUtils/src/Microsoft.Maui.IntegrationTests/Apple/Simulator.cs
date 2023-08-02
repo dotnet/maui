f808ecc68353a1e2eff6c1974edf4ece3ab919ff
Rui Marinho <me@ruimarinho.net>
2023-08-02 18:14:59 +0100

    Try fix integrationtests

--- src/TestUtils/src/Microsoft.Maui.IntegrationTests/Apple/Simulator.cs
+++ src/TestUtils/src/Microsoft.Maui.IntegrationTests/Apple/Simulator.cs
@@ -5,7 +5,7 @@ namespace Microsoft.Maui.IntegrationTests.Apple
 	{
 		readonly string XCRunTool = "xcrun";
 
-		public string XHarnessID { get; set; } = "ios-simulator-64";
+		public string XHarnessID { get; set; } = "ios-simulator-64_15.5";
 
 		string _udid = "";
 		public string GetUDID()
@@ -13,6 +13,7 @@ namespace Microsoft.Maui.IntegrationTests.Apple
 			if (!string.IsNullOrEmpty(_udid))
 				return _udid;
 
+			XHarness.InstallSimulator(XHarnessID);
 			return _udid = XHarness.GetSimulatorUDID(XHarnessID).Trim();
 		}
 
