48182bcb1cf4a0302012fda9fb5e5081a7cc6096
tj-devel709 <tj.devel709@gmail.com>
2023-08-03 09:05:47 -0500

    uitests part 1

---
+++ src/Controls/samples/Controls.Sample.UITests/Elements/GestureRecognizerGallery.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls.Internals;
+
+namespace Maui.Controls.Sample
+{
+	[Preserve(AllMembers = true)]
+	public class GestureRecognizerGallery : ContentViewGalleryPage
+	{
+		public GestureRecognizerGallery()
+		{
+			Add(new GestureRecognizerEvents());
+		}
+	}
+}
+
---
+++ src/Controls/tests/UITests/Tests/GestureRecognizerUITests.cs
@@ -0,0 +1,46 @@
+﻿using Maui.Controls.Sample;
+using Microsoft.Maui.Appium;
+using NUnit.Framework;
+using TestUtils.Appium.UITests;
+using Xamarin.UITest;
+
+namespace Microsoft.Maui.AppiumTests
+{
+	public class GestureRecognizerUITests : UITestBase
+	{
+		const string GestureRecognizerGallery = "* marked:'Gesture Recognizer Gallery'";
+		public GestureRecognizerUITests(TestDevice device)
+			: base(device)
+		{
+		}
+
+		protected override void FixtureSetup()
+		{
+			base.FixtureSetup();
+			App.NavigateToGallery(GestureRecognizerGallery);
+		}
+
+		protected override void FixtureTeardown()
+		{
+			base.FixtureTeardown();
+			App.NavigateBack();
+		}
+
+		[Test]
+		public void PointerGestureTest()
+		{
+			App.WaitForElement("TargetView");
+			App.EnterText("TargetView", "GestureRecognizerEvents");
+			App.Tap("GoButton");
+
+			App.WaitForElement("primaryLabel");
+			// using Tap in place of moving mouse for now
+			App.Tap("primaryLabel");
+			App.Tap("secondaryLabel");
+
+			var secondaryLabelText = App.Query("secondaryLabel").First().Text;
+			Assert.IsNotEmpty(secondaryLabelText);
+		}
+	}
+}
+
