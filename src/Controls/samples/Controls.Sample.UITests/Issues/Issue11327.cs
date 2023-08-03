cb43a494c958da14042da5901e6d1062bfc6aa10
Rui Marinho <me@ruimarinho.net>
2023-08-03 17:47:37 +0100

    [uitests] Add UITest for issue #11327

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue11327.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 11327, "Incorrect layout with GridItemsLayout and RefreshView", PlatformAffected.iOS)]
+	public class Issue11327 : TestContentPage
+	{
+		public List<int> Numbers { get; } = Enumerable.Range(1, 100).ToList();
+
+		protected override void Init()
+		{
+			var itemsLayout = new GridItemsLayout(ItemsLayoutOrientation.Vertical)
+			{
+				Span = 2,
+				VerticalItemSpacing = 5,
+				HorizontalItemSpacing = 5
+			};
+			var collectionView = new CollectionView
+			{
+				ItemsLayout = itemsLayout,
+				BackgroundColor = Colors.DarkBlue
+			};
+			collectionView.ItemTemplate = new DataTemplate(() =>
+			{
+				var layout = new StackLayout
+				{
+					BackgroundColor = Colors.Purple,
+					HeightRequest = 50
+				};
+				var view = new Label
+				{
+					AutomationId = "labeCell"
+				};
+				view.SetBinding(Label.TextProperty, ".");
+				layout.Add(view);
+
+				return layout;
+			});
+			collectionView.SetBinding(CollectionView.ItemsSourceProperty, nameof(Numbers));
+
+			Content = new RefreshView
+			{
+				Content = collectionView
+			};
+
+			BindingContext = this;
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue11327.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue11327 : _IssuesUITest
+	{
+		public Issue11327(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Incorrect layout with GridItemsLayout and RefreshView";
+
+		[Test]
+		public void Issue11327Test()
+		{
+			App.WaitForElement("labeCell", "Timeout wait for labeCell", TimeSpan.FromSeconds(10));
+			VerifyScreenshot();
+		}
+	}
+}
