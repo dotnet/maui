a3913817c86687a03055bbd5ef9d2723a639b791
Rui Marinho <me@ruimarinho.net>
2023-08-04 18:58:49 +0100

    Add test for issue #11363

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue11363.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 11363, "CollectionView inside RefreshView freezes app completely", PlatformAffected.iOS)]
+	public class Issue11363 : TestContentPage
+	{
+		public List<int> Numbers { get; } = Enumerable.Range(1, 100).ToList();
+
+		protected override void Init()
+		{
+			ToolbarItems.Add(new ToolbarItem
+			{
+				Text = "alert",
+				Order =  ToolbarItemOrder.Secondary,
+				Command = new Command(async () => await DisplayAlert("Toolbar action", "Hello, I'm a toolbar action :)", "OK"))
+			});
+			;
+			var collectionView = new CollectionView
+			{
+				
+			};
+			collectionView.ItemTemplate = new DataTemplate(() =>
+			{
+				var layout = new Frame
+				{
+					HeightRequest = 80,
+					HasShadow = false,
+					CornerRadius = 4,
+					Margin = new Microsoft.Maui.Thickness(5, 2, 5, 2)
+
+				};
+				var view = new Label
+				{
+					AutomationId = "labeCell"
+				};
+				view.SetBinding(Label.TextProperty, ".");
+				layout.Content = view;
+
+				return layout;
+			});
+			collectionView.SetBinding(CollectionView.ItemsSourceProperty, nameof(Numbers));
+
+			Content = new RefreshView
+			{
+				Content = collectionView
+			};
+
+			BindingContext = this;
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue11363.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue11363 : _IssuesUITest
+	{
+		public Issue11363(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "CollectionView inside RefreshView freezes app completely";
+
+		[Test]
+		public void Issue11327Test()
+		{
+			App.WaitForElement("labeCell", "Timeout wait for labeCell", TimeSpan.FromSeconds(10));
+			App.ScrollDown("20");
+			App.Tap("alert");
+			App.WaitForElement("OK");
+		}
+	}
+}
