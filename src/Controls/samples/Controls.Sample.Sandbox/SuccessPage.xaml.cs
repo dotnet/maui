5aec0b71583c060a03f49cebf87e4c52b71a8466
Rachel Kang <rachel.j.kang@gmail.com>
2023-08-14 14:15:23 -0400

    Add sample for testing

---
+++ src/Controls/samples/Controls.Sample.Sandbox/SuccessPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample
+{
+	public partial class SuccessPage : ContentPage
+	{
+		public SuccessPage()
+		{
+			InitializeComponent();
+		}
+
+		async private void Button_Clicked(object sender, System.EventArgs e)
+		{
+			await Navigation.PopAsync();
+        }
+    }
+}
