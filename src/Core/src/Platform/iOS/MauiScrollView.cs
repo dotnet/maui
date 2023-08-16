bf19922ac63b117a33b29c1e46eddc384b06d14f
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-08-16 16:49:33 +0200

    Avoid unnecessary event

--- src/Core/src/Platform/iOS/MauiScrollView.cs
+++ src/Core/src/Platform/iOS/MauiScrollView.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using CoreGraphics;
+﻿using CoreGraphics;
 using UIKit;
 
 namespace Microsoft.Maui.Platform
@@ -9,16 +7,6 @@ namespace Microsoft.Maui.Platform
 	{
 		public MauiScrollView()
 		{
-		}
-
-		[UnconditionalSuppressMessage("Memory", "MA0001", Justification = "Proven safe in test: ScrollViewTests.DoesNotleak")]
-		public event EventHandler? LayoutSubviewsChanged;
-
-		public override void LayoutSubviews()
-		{
-			base.LayoutSubviews();
-
-			LayoutSubviewsChanged?.Invoke(this, EventArgs.Empty);	
 		}
 
 		// overriding this method so it does not automatically scroll large UITextFields
