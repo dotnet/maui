e63f357e722ed1f03fb729f2d1df47013f2f5919
Mauro Agnoletti (from Dev Box) <maagno@microsoft.com>
2023-08-17 10:25:49 -0400

    [windows] Added ArrangeOverride override and changed MeasureOverride in ItemContentControl to not call base when the ItemHeight and ItemWidth are defined
    When the height and width of the items are already defined, it's likely that the base.MeasureOverride (WinUI) call reduces the original size, causing issues like the items in a CarouselView not fitting the container, which causes more items to be displayed at once.
    This fix makes sure that we always return the max height and width between the measured override and the existing items height and width.
    Also, adding ArrangeOverride from WinUI base class to control the layout correctly and completely on our side.
    
    Fixes Bug: https://github.com/dotnet/maui/issues/12567

--- src/Controls/src/Core/Platform/Windows/CollectionView/ItemContentControl.cs
+++ src/Controls/src/Core/Platform/Windows/CollectionView/ItemContentControl.cs
@@ -263,6 +263,24 @@ namespace Microsoft.Maui.Controls.Platform
 			this.SetAutomationPropertiesAccessibilityView(_visualElement, defaultAccessibilityView);
 		}
 
+#pragma warning disable RS0016 // Add public types and members to the declared API
+		protected override WSize ArrangeOverride(WSize finalSize)
+#pragma warning restore RS0016 // Add public types and members to the declared API
+		{
+			if (_renderer == null)
+			{
+				return base.ArrangeOverride(finalSize);
+			}
+
+			var width = ItemWidth == default ? finalSize.Width : ItemWidth;
+			var height = ItemHeight == default ? finalSize.Height : ItemHeight;
+			var size = new WSize(width, height);
+
+			base.ArrangeOverride(size);
+
+			return size;
+		}
+
 		protected override WSize MeasureOverride(WSize availableSize)
 		{
 			if (_renderer == null)
@@ -271,50 +289,17 @@ namespace Microsoft.Maui.Controls.Platform
 			}
 
 			var frameworkElement = Content as FrameworkElement;
-			var formsElement = _renderer.VirtualView as VisualElement;
 			var margin = _renderer.VirtualView.Margin;
 
-			if (ItemHeight != default || ItemWidth != default)
-			{
-				formsElement.Layout(new Rect(0, 0, ItemWidth, ItemHeight));
+			frameworkElement.Margin = WinUIHelpers.CreateThickness(margin.Left, margin.Top, margin.Right, margin.Bottom);
 
-				var wsize = new WSize(ItemWidth, ItemHeight);
+			var width = ItemWidth == default ? availableSize.Width : ItemWidth;
+			var height = ItemHeight == default ? availableSize.Height : ItemHeight;
+			var measuredSize = base.MeasureOverride(new WSize(width, height));
+			var finalWidth = Max(measuredSize.Width, ItemWidth);
+			var finalHeight = Max(measuredSize.Height, ItemHeight);
 
-				frameworkElement.Margin =
-					WinUIHelpers.CreateThickness(
-						margin.Left + ItemSpacing.Left,
-						margin.Top + ItemSpacing.Top,
-						margin.Right + ItemSpacing.Right,
-						margin.Bottom + ItemSpacing.Bottom);
-
-				if (CanMeasureContent(frameworkElement))
-					frameworkElement.Measure(wsize);
-
-				return base.MeasureOverride(wsize);
-			}
-			else
-			{
-				var (width, height) = formsElement.Measure(availableSize.Width, availableSize.Height,
-					MeasureFlags.IncludeMargins).Request;
-
-				width = Max(width, availableSize.Width);
-				height = Max(height, availableSize.Height);
-
-				formsElement.Layout(new Rect(0, 0, width, height));
-
-				var wsize = new WSize(width, height);
-
-				frameworkElement.Margin = WinUIHelpers.CreateThickness(
-					margin.Left,
-					margin.Top,
-					margin.Right,
-					margin.Bottom);
-
-				if (CanMeasureContent(frameworkElement))
-					frameworkElement.Measure(wsize);
-
-				return base.MeasureOverride(wsize);
-			}
+			return new WSize(finalWidth, finalHeight);
 		}
 
 		double Max(double requested, double available)
