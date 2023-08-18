1ff66cbd5583f2fcba7170891445f9df82d1cd2e
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-08-18 09:16:19 +0200

    More changes

--- src/Core/src/Handlers/ScrollView/ScrollViewHandler.iOS.cs
+++ src/Core/src/Handlers/ScrollView/ScrollViewHandler.iOS.cs
@@ -51,6 +51,7 @@ namespace Microsoft.Maui.Handlers
 		{
 			base.DisconnectHandler(platformView);
 
+			PendingScrollToRequest = null;
 			_eventProxy.Disconnect(platformView);
 		}
 
--- src/Core/src/Platform/iOS/MauiScrollView.cs
+++ src/Core/src/Platform/iOS/MauiScrollView.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using CoreGraphics;
 using UIKit;
 
