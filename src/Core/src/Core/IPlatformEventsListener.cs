31e896f9b64cc0315eafe5528ca8d9ef5492cdcd
Samantha Houts <samhouts@users.noreply.github.com>
2023-08-21 14:27:39 -0700

    Update src/Core/src/Core/IPlatformEventsListener.cs

--- src/Core/src/Core/IPlatformEventsListener.cs
+++ src/Core/src/Core/IPlatformEventsListener.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Maui
 	/// <summary>
 	/// This lets us wire into areas of a control that are only reachable via
 	/// overriding but we don't want to override those controls.
-	/// TODO: Expose this as a more permanent solution or create a a better one
+	/// TODO: Expose this as a more permanent solution or create a better one
 	/// </summary>
 	internal interface IPlatformEventsListener
 
