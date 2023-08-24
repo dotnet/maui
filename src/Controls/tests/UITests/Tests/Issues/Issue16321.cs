375a373b63a79c00e4ff237983f824ff48d78084
Shane Neuville <shneuvil@microsoft.com>
2023-08-24 11:11:03 -0500

    Present to UIWindow associated with passed in page

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue16321.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue16321 : _IssuesUITest
+	{
+		public Issue16321(TestDevice device) : base(device) { }
+
+		public override string Issue => "Alerts Open on top of current presented view";
+
+		[Test]
+		public void OpenAlertWithModals()
+		{
+			App.Tap("OpenAlertWithModals");
+			App.Tap("Cancel");
+		}
+
+		[Test]
+		public void OpenAlertWithNewUIWindow()
+		{
+			App.Tap("OpenAlertWithNewUIWindow");
+			App.Tap("Cancel");
+		}
+	}
+}
