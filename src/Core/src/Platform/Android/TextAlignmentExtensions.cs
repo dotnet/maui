ac0e77fec9d3d0cb36bde5b13ff5904f62bf8674
Juan Diego Herrera <juherrera@microsoft.com>
2023-08-24 11:29:59 -0700

    Fix Android text alignment in migrated projects

--- src/Core/src/Platform/Android/TextAlignmentExtensions.cs
+++ src/Core/src/Platform/Android/TextAlignmentExtensions.cs
@@ -8,19 +8,27 @@ namespace Microsoft.Maui.Platform
 		internal static void UpdateHorizontalAlignment(this EditText view, TextAlignment alignment, AGravityFlags orMask = AGravityFlags.NoGravity)
 		{
 			if (!Rtl.IsSupported)
-				view.Gravity = alignment.ToHorizontalGravityFlags() | orMask;
+			{
+				// The mask is used to ignore the previous horizontal alignment settings
+				var horizontalGravityMask = AGravityFlags.CenterHorizontal | AGravityFlags.End | AGravityFlags.Start;
+				view.Gravity = (view.Gravity & ~horizontalGravityMask) | alignment.ToHorizontalGravityFlags() | orMask;
+			}
 			else
 				view.TextAlignment = alignment.ToTextAlignment();
 		}
 
 		public static void UpdateVerticalAlignment(this EditText view, TextAlignment alignment, AGravityFlags orMask = AGravityFlags.NoGravity)
 		{
-			view.Gravity = alignment.ToVerticalGravityFlags() | orMask;
+			// The mask is used to ignore the previous vertical alignment settings
+			var verticalGravityMask = AGravityFlags.Top | AGravityFlags.Bottom | AGravityFlags.CenterVertical;
+			view.Gravity = (view.Gravity & ~verticalGravityMask) | alignment.ToVerticalGravityFlags() | orMask;
 		}
 
 		public static void UpdateVerticalAlignment(this TextView view, TextAlignment alignment, AGravityFlags orMask = AGravityFlags.NoGravity)
 		{
-			view.Gravity = alignment.ToVerticalGravityFlags() | orMask;
+			// The mask is used to ignore the previous vertical alignment settings
+			var verticalGravityMask = AGravityFlags.Top | AGravityFlags.Bottom | AGravityFlags.CenterVertical;
+			view.Gravity = (view.Gravity & ~verticalGravityMask) | alignment.ToVerticalGravityFlags() | orMask;
 		}
 
 		public static void UpdateTextAlignment(this EditText view, TextAlignment horizontal, TextAlignment vertical)
