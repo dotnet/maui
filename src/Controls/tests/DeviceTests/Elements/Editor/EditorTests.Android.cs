da81f66cef65b4cdf8fbc7792d8727e2ee24595b
Juan Diego Herrera <juherrera@microsoft.com>
2023-08-24 16:41:35 -0700

    Add (manual) device tests

--- src/Controls/tests/DeviceTests/Elements/Editor/EditorTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/Editor/EditorTests.Android.cs
@@ -1,7 +1,9 @@
 ﻿using System.Threading.Tasks;
+using Android.Views;
 using AndroidX.AppCompat.Widget;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
 using Xunit;
 
 namespace Microsoft.Maui.DeviceTests
@@ -56,6 +58,38 @@ namespace Microsoft.Maui.DeviceTests
 			await SetValueAsync<string, EditorHandler>(editor, "TEsT", SetPlatformText);
 
 			Assert.Equal(2, editor.CursorPosition);
+		}
+
+		[Fact(DisplayName = "Horizontal text aligned when RTL is not supported")]
+		public async Task HorizontalTextAlignedWhenRtlIsFalse()
+		{
+			if (Rtl.IsSupported)
+				return;
+
+			var editor = new Editor { Text = "Foo", HorizontalTextAlignment = TextAlignment.Center };
+
+			var handler = await CreateHandlerAsync<EditorHandler>(editor);
+			var platformEditor = GetPlatformControl(handler);
+
+			Assert.False(platformEditor.Gravity.HasFlag(GravityFlags.Start), "Editor should not have the Start flag.");
+			Assert.False(platformEditor.Gravity.HasFlag(GravityFlags.End), "Editor should not have the End flag.");
+			Assert.True(platformEditor.Gravity.HasFlag(GravityFlags.CenterHorizontal), "Editor should have the CenterHorizontal flag.");
+		}
+
+		[Fact(DisplayName = "Vertical text aligned when RTL is not supported")]
+		public async Task VerticalTextAlignedWhenRtlIsFalse()
+		{
+			if (Rtl.IsSupported)
+				return;
+
+			var editor = new Editor { Text = "Foo", VerticalTextAlignment = TextAlignment.Center };
+
+			var handler = await CreateHandlerAsync<EditorHandler>(editor);
+			var platformEditor = GetPlatformControl(handler);
+
+			Assert.False(platformEditor.Gravity.HasFlag(GravityFlags.Top), "Editor should not have the Top flag.");
+			Assert.False(platformEditor.Gravity.HasFlag(GravityFlags.Bottom), "Editor should not have the Bottom flag.");
+			Assert.True(platformEditor.Gravity.HasFlag(GravityFlags.CenterVertical), "Editor should only have the CenterVertical flag.");
 		}
 	}
 }
--- src/Controls/tests/DeviceTests/Elements/SearchBar/SearchBarTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/SearchBar/SearchBarTests.Android.cs
@@ -1,8 +1,11 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Android.Views;
 using Android.Widget;
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Platform;
+using Xunit;
 using SearchView = AndroidX.AppCompat.Widget.SearchView;
 
 namespace Microsoft.Maui.DeviceTests
@@ -11,6 +14,9 @@ namespace Microsoft.Maui.DeviceTests
 	{
 		static SearchView GetPlatformControl(SearchBarHandler handler) =>
 			handler.PlatformView;
+
+		static EditText GetPlatformQueryEditor(SearchBarHandler handler) =>
+			handler.QueryEditor;
 
 		static Task<string> GetPlatformText(SearchBarHandler handler)
 		{
@@ -29,6 +35,38 @@ namespace Microsoft.Maui.DeviceTests
 			var control = GetPlatformControl(searchBarHandler);
 			var editText = control.GetChildrenOfType<EditText>().FirstOrDefault();
 			return editText.SelectionStart;
+		}
+
+		[Fact(DisplayName = "Horizontal text aligned when RTL is not supported")]
+		public async Task HorizontalTextAlignedWhenRtlIsFalse()
+		{
+			if (Rtl.IsSupported)
+				return;
+
+			var searchBar = new SearchBar { Text = "Foo", HorizontalTextAlignment = TextAlignment.Center };
+
+			var handler = await CreateHandlerAsync<SearchBarHandler>(searchBar);
+			var platformSearchBarEditor = GetPlatformQueryEditor(handler);
+
+			Assert.False(platformSearchBarEditor.Gravity.HasFlag(GravityFlags.Start), "Search bar editor should not have the Start flag.");
+			Assert.False(platformSearchBarEditor.Gravity.HasFlag(GravityFlags.End), "Search bar editor should not have the End flag.");
+			Assert.True(platformSearchBarEditor.Gravity.HasFlag(GravityFlags.CenterHorizontal), "Search bar editor should have the CenterHorizontal flag.");
+		}
+
+		[Fact(DisplayName = "Vertical text aligned when RTL is not supported")]
+		public async Task VerticalTextAlignedWhenRtlIsFalse()
+		{
+			if (Rtl.IsSupported)
+				return;
+
+			var searchBar = new SearchBar { Text = "Foo", VerticalTextAlignment = TextAlignment.Center };
+
+			var handler = await CreateHandlerAsync<SearchBarHandler>(searchBar);
+			var platformSearchBarEditor = GetPlatformQueryEditor(handler);
+
+			Assert.False(platformSearchBarEditor.Gravity.HasFlag(GravityFlags.Top), "Search bar editor should not have the Top flag.");
+			Assert.False(platformSearchBarEditor.Gravity.HasFlag(GravityFlags.Bottom), "Search bar editor should not have the Bottom flag.");
+			Assert.True(platformSearchBarEditor.Gravity.HasFlag(GravityFlags.CenterVertical), "Search bar editor should only have the CenterVertical flag.");
 		}
 	}
 }
