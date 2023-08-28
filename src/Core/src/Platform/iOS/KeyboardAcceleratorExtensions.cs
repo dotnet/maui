eea29ad0a64de5b23afe243af70b31c048b783f6
Rachel Kang <rachel.j.kang@gmail.com>
2023-08-28 18:26:13 -0400

    Add null check

--- src/Core/src/Platform/iOS/KeyboardAcceleratorExtensions.cs
+++ src/Core/src/Platform/iOS/KeyboardAcceleratorExtensions.cs
@@ -15,10 +15,13 @@ namespace Microsoft.Maui.Platform
 			var selector = new Selector(MenuItemSelectedSelector);
 
 			var accelerators = virtualView.KeyboardAccelerators;
-			if (accelerators is null || accelerators.Count == 0 || accelerators[0] is null)
+			if (accelerators is null || accelerators.Count == 0 || accelerators[0] is null || accelerators[0].Key is null)
 				return virtualView.CreateMenuItemCommand(index, uiImage, selector);
 
 			var key = accelerators[0].Key;
+			if (key is null)
+				return virtualView.CreateMenuItemCommand(index, uiImage, selector);
+
 			var modifiers = accelerators[0].Modifiers.ToUIKeyModifierFlags();
 
 			// Single key accelerators (no modifier, i.e. "A") and multi-key accelerators are supported (>=1 modifier and 1 key only, i.e. Ctrl+F, Ctrl+Shift+F)
