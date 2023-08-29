d0dec8e01f63a7865cd0081954f3def4568706a4
Bret Johnson <bret.johnson@microsoft.com>
2023-08-29 10:56:07 -0400

    Fix OnPlatform + Setter when no match for current platform
    When OnPlatforfm is used for a Setter value, and there's no match for the
    current platform, fix so that the property isn't set (same behavior as Xamarin.Forms).
    Previously, XamlC would report a "Missing Value For Setter" error when this happens
    and non-compiled XAML would crash with an NRE. This PR contains fixes for both,
    ensuring the XAML compiler doesn't show an error, then ensuring it doesn't crash
    at runtime.
    
    Fixes #12064

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue12064.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Platform;
+using Microsoft.Maui;
+using System.Linq;
+using System;
+
+#if IOS
+using UIKit;
+using CoreGraphics;
+#endif
+
+namespace Maui.Controls.Sample.Issues
+{
+	//[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 12604, "Setter showing a Build Error when using XAML OnPlatform Markup Extension", PlatformAffected.All)]
+	public partial class Issue12604 : ContentPage
+	{
+		public Issue12604()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue12064.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue12064 : _IssuesUITest
+	{
+		public Issue12064(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Setter showing a Build Error when using XAML OnPlatform Markup Extension";
+
+		[Test]
+		public void Issue12064Test()
+		{
+			App.WaitForElement("TestLabel");
+			VerifyScreenshot();
+		}
+	}
+}
