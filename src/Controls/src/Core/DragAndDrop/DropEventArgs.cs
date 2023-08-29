22c52f079b9b219c978b578ba467a1fbff7f791a
tj-devel709 <tj.devel709@gmail.com>
2023-08-29 17:25:33 -0500

    Nullable datapackage in constructor

--- src/Controls/src/Core/DragAndDrop/DropEventArgs.cs
+++ src/Controls/src/Core/DragAndDrop/DropEventArgs.cs
@@ -26,10 +26,9 @@ namespace Microsoft.Maui.Controls
 		/// <param name="view">The data package associated with the drop.</param>
 		/// <param name="getPosition">Function used to get the position relative a specified <see cref="IElement"/>.</param>
 		/// <param name="platformArgs">The platform-specific data associated with the drag.</param>
-		public DropEventArgs(DataPackageView view, Func<IElement?, Point?>? getPosition, PlatformDropEventArgs platformArgs)
+		public DropEventArgs(DataPackageView? view, Func<IElement?, Point?>? getPosition, PlatformDropEventArgs platformArgs)
 		{
-			_ = view ?? throw new ArgumentNullException(nameof(view));
-			Data = view;
+			Data = view ?? new DataPackageView(new DataPackage());
 			_getPosition = getPosition;
 			PlatformArgs = platformArgs;
 		}
