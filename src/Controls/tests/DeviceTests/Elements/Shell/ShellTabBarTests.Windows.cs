d6514bda9c57a188e77cb6b7770f1f41bd497692
Mike Corsaro <corsarom@gmail.com>
2023-08-29 13:51:58 -0700

    Fixed the tab bar not hiding/showing if property changed (#17069)
    * fix(tabbar): Fixed the tab bar not hiding/showing if property changed
    
    The tabbar on Windows wouldn't toggle visibility if modified, and would only hide/show on current item change
    
    * Apply patch
    
    * Update tab bar vis call to use `UpdateValue`
    
    ---------
    
    Co-authored-by: Mike Corsaro <mikecorsaro@microsoft.com>

--- src/Controls/tests/DeviceTests/Elements/Shell/ShellTabBarTests.Windows.cs
+++ src/Controls/tests/DeviceTests/Elements/Shell/ShellTabBarTests.Windows.cs
@@ -18,6 +18,48 @@ namespace Microsoft.Maui.DeviceTests
 	[Category(TestCategory.Shell)]
 	public partial class ShellTests
 	{
+		[Fact(DisplayName = "Shell TabBar visibility toggle")]
+		public async Task ShellTabBarVisibilityToggleWorks()
+		{
+			await RunShellTabBarTests(shell => { },
+			(shell) =>
+			{
+				var shellItemHandler = shell.CurrentItem.Handler as ShellItemHandler;
+				var navView = shellItemHandler.PlatformView as MauiNavigationView;
+
+				Shell.SetTabBarIsVisible(shell.CurrentPage, false);
+				Assert.False(navView.IsPaneVisible);
+
+				Shell.SetTabBarIsVisible(shell.CurrentPage, true);
+				Assert.True(navView.IsPaneVisible);
+				Assert.True(navView.PaneDisplayMode == UI.Xaml.Controls.NavigationViewPaneDisplayMode.Top);
+
+				return Task.FromResult(true);
+			});
+		}
+
+		[Fact(DisplayName = "Shell TabBar visibility toggles when removing current")]
+		public async Task ShellTabBarVisibilityToggleWorksRemovingCurrentItem()
+		{
+			await RunShellTabBarTests(shell => { },
+			(shell) =>
+			{
+				var shellItemHandler = shell.CurrentItem.Handler as ShellItemHandler;
+				var navView = shellItemHandler.PlatformView as MauiNavigationView;
+
+				Shell.SetTabBarIsVisible(shell.Items[0].Items[1], false);
+				shell.CurrentItem = shell.Items[0].Items[1];
+
+				Assert.False(navView.IsPaneVisible);
+
+				shell.Items[0].Items.RemoveAt(1);
+
+				Assert.True(navView.IsPaneVisible);
+				Assert.True(navView.PaneDisplayMode == UI.Xaml.Controls.NavigationViewPaneDisplayMode.Top);
+				return Task.FromResult(true);
+			});
+		}
+
 		List<WNavigationViewItem> GetTabBarItems(Shell shell)
 		{
 			var shellItemHandler = shell.CurrentItem.Handler as ShellItemHandler;
