9d4fd7848a0ae3eefa801809c0eed0bc2afc3049
E.Z. Hart <hartez@users.noreply.github.com>
2023-08-30 04:43:58 -0600

    [net7.0] [windows] Fixed CarouselView items rendering (#17088)
    * Fix public API record
    
    * Remove accidental add

--- src/Controls/src/Core/Platform/Windows/CollectionView/ItemContentControl.cs
+++ src/Controls/src/Core/Platform/Windows/CollectionView/ItemContentControl.cs
@@ -263,6 +263,22 @@ namespace Microsoft.Maui.Controls.Platform
 			this.SetAutomationPropertiesAccessibilityView(_visualElement, defaultAccessibilityView);
 		}
 
+		/// <inheritdoc/>
+		protected override WSize ArrangeOverride(WSize finalSize)
+		{
+			if (_renderer == null)
+			{
+				return base.ArrangeOverride(finalSize);
+			}
+
+			var width = ItemWidth == default ? finalSize.Width : ItemWidth;
+			var height = ItemHeight == default ? finalSize.Height : ItemHeight;
+			var size = new WSize(width, height);
+
+			return base.ArrangeOverride(size);
+		}
+
+		/// <inheritdoc/>
 		protected override WSize MeasureOverride(WSize availableSize)
 		{
 			if (_renderer == null)
@@ -270,51 +286,25 @@ namespace Microsoft.Maui.Controls.Platform
 				return base.MeasureOverride(availableSize);
 			}
 
+			var width = ItemWidth == default ? availableSize.Width : ItemWidth;
+			var height = ItemHeight == default ? availableSize.Height : ItemHeight;
+			var measuredSize = base.MeasureOverride(new WSize(width, height));
+			var finalWidth = Max(measuredSize.Width, ItemWidth);
+			var finalHeight = Max(measuredSize.Height, ItemHeight);
+			var finalSize = new WSize(finalWidth, finalHeight);
 			var frameworkElement = Content as FrameworkElement;
-			var formsElement = _renderer.VirtualView as VisualElement;
+			var visualElement = _renderer.VirtualView as VisualElement;
 			var margin = _renderer.VirtualView.Margin;
 
-			if (ItemHeight != default || ItemWidth != default)
+			frameworkElement.Margin = WinUIHelpers.CreateThickness(margin.Left, margin.Top, margin.Right, margin.Bottom);
+			visualElement.Layout(new Rect(0, 0, finalWidth, finalHeight));
+
+			if (CanMeasureContent(frameworkElement))
 			{
-				formsElement.Layout(new Rect(0, 0, ItemWidth, ItemHeight));
-
-				var wsize = new WSize(ItemWidth, ItemHeight);
-
-				frameworkElement.Margin =
-					WinUIHelpers.CreateThickness(
-						margin.Left + ItemSpacing.Left,
-						margin.Top + ItemSpacing.Top,
-						margin.Right + ItemSpacing.Right,
-						margin.Bottom + ItemSpacing.Bottom);
-
-				if (CanMeasureContent(frameworkElement))
-					frameworkElement.Measure(wsize);
-
-				return base.MeasureOverride(wsize);
+				frameworkElement.Measure(finalSize);
 			}
-			else
-			{
-				var (width, height) = formsElement.Measure(availableSize.Width, availableSize.Height,
-					MeasureFlags.IncludeMargins).Request;
 
-				width = Max(width, availableSize.Width);
-				height = Max(height, availableSize.Height);
-
-				formsElement.Layout(new Rect(0, 0, width, height));
-
-				var wsize = new WSize(width, height);
-
-				frameworkElement.Margin = WinUIHelpers.CreateThickness(
-					margin.Left,
-					margin.Top,
-					margin.Right,
-					margin.Bottom);
-
-				if (CanMeasureContent(frameworkElement))
-					frameworkElement.Measure(wsize);
-
-				return base.MeasureOverride(wsize);
-			}
+			return finalSize;
 		}
 
 		double Max(double requested, double available)
