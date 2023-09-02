750007d45288b8608a30facb45a3d0d1146a8a6a
Juan Diego Herrera <juherrera@microsoft.com>
2023-09-01 17:35:34 -0700

    Add tests!

--- src/Controls/tests/DeviceTests/Elements/Image/ImageTests.iOS.cs
+++ src/Controls/tests/DeviceTests/Elements/Image/ImageTests.iOS.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Handlers;
 using Xunit;
 
@@ -23,6 +24,75 @@ namespace Microsoft.Maui.DeviceTests
 				Assert.True(platformImage.Value.Size.Height > 0);
 				Assert.True(platformImage.Value.Size.Width > 0);
 			});
+		}
+
+		[Fact("Large images resize correctly")]
+		public async Task LargeImagesResizeCorrectly()
+		{
+			SetupBuilder();
+			var layout = new StackLayout { MaximumWidthRequest = 200 };
+			var image = new Image
+			{
+				Background = Colors.Red,
+				Source = "big_white_horizontal.png",
+				Aspect = Aspect.AspectFit
+			};
+
+			layout.Add(image);
+
+			// big_white_horizontal is 2000x1000px, so it should perfectly fit into a 200px wide layout
+			await InvokeOnMainThreadAsync(async () =>
+			{
+				var handler = CreateHandler<LayoutHandler>(layout);
+
+				await image.Wait();
+
+				await handler.ToPlatform().AssertContainsColor(Colors.White, MauiContext);
+				await handler.ToPlatform().AssertDoesNotContainColor(Colors.Red, MauiContext);
+			});
+
+			// We check for both image and layout to ensure that image is the right size
+			Assert.True(image.Height == 100, "Image should be 100px tall after resize!");
+			Assert.True(image.Width == 200, "Image should be 200px wide after resize!");
+
+			Assert.True(layout.Height == 100, "Layout should be 100px tall!");
+			Assert.True(layout.Width == 200, "Layout should be 200px wide!");
+		}
+
+
+		// big_white_horizontal is 2000x1000px
+		// So when it has to fit a 50x200 space, it should resize to 50x25 (width is the smallest scaled dimension)
+		// When it has to fit in a 200x50 space, it should resize to 100x50 (height is the smallest scaled dimension)
+		[Theory("Aspect Fit resizes on smallest dimension")]
+		[InlineData(50, 200, 50, 25)]
+		[InlineData(200, 50, 100, 50)]
+		public async Task AspectFitResizesOnSmallestDimensions(int widthRequest, int heightRequest, int expectedWidth, int expectedHeight)
+		{
+			SetupBuilder();
+			var layout = new Grid { MaximumHeightRequest = widthRequest, MaximumWidthRequest = heightRequest };
+			var image = new Image
+			{
+				Source = "big_white_horizontal.png",
+				Aspect = Aspect.AspectFit
+			};
+
+			layout.Add(image);
+
+			// With ApsectFit, it should resize to 100x50px in a layout whose height is constrained to 50px
+			await InvokeOnMainThreadAsync(async () =>
+			{
+				var handler = CreateHandler<LayoutHandler>(layout);
+
+				await image.Wait();
+
+				await handler.ToPlatform().AssertContainsColor(Colors.White, MauiContext);
+			});
+
+			Assert.True(image.Height == expectedHeight, "Image should have expected height!");
+			Assert.True(image.Width == expectedWidth, "Image should have expected width!");
+
+			Assert.True(layout.Height == expectedHeight, "Layout should have expected height!");
+			Assert.True(layout.Width == expectedWidth, "Layout should have expected width!");
 		}
 	}
 }
