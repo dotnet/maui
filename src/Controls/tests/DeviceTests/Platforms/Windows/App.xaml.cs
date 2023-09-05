f3e8910495182744e43223bfa063a4874d9c9e02
Shane Neuville <shneuvil@microsoft.com>
2023-09-05 15:08:18 -0500

    Enable Controls Tests

--- src/Controls/tests/DeviceTests/Platforms/Windows/App.xaml.cs
+++ src/Controls/tests/DeviceTests/Platforms/Windows/App.xaml.cs
@@ -4,7 +4,16 @@ namespace Microsoft.Maui.DeviceTests.WinUI
 {
 	public partial class App : MauiWinUIApplication
 	{
-		public App() => InitializeComponent();
+		public App()
+		{
+			InitializeComponent();
+			this.UnhandledException += App_UnhandledException;
+		}
+
+		private void App_UnhandledException(object sender, UI.Xaml.UnhandledExceptionEventArgs e)
+		{
+			e.Handled = true;
+		}
 
 		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 	}
