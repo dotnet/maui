f41bd7c5cc87aadfc7e96a12fabe68169294ad99
Shane Neuville <shneuvil@microsoft.com>
2023-09-07 19:13:14 -0500

    - add s

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -14,5 +14,34 @@ namespace Maui.Controls.Sample
 		{
 			InitializeComponent();
 		}
+
+		void PointerGestureRecognizer_PointerReleased(System.Object sender, Microsoft.Maui.Controls.PointerEventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine("PointerGestureRecognizer_PointerReleased");
+		}
+
+		void PointerGestureRecognizer_PointerEntered(System.Object sender, Microsoft.Maui.Controls.PointerEventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine("PointerGestureRecognizer_PointerEntered");
+		}
+
+		void PointerGestureRecognizer_PointerExited(System.Object sender, Microsoft.Maui.Controls.PointerEventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine("PointerGestureRecognizer_PointerExited");
+		}
+
+		void PointerGestureRecognizer_PointerMoved(System.Object sender, Microsoft.Maui.Controls.PointerEventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine("PointerGestureRecognizer_PointerMoved");
+		}
+
+		void PointerGestureRecognizer_PointerPressed(System.Object sender, Microsoft.Maui.Controls.PointerEventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine("PointerGestureRecognizer_PointerPressed");
+		}
+
+		void ContentView_Unloaded(System.Object sender, System.EventArgs e)
+		{
+		}
 	}
 }
