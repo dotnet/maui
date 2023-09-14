2397a624f90eb3512b6d04b20c39100fdccd4bc0
Gerald Versluis <gerald.versluis@microsoft.com>
2023-09-14 14:26:37 +0200

    Disable failing unpackaged font test

--- src/Core/tests/DeviceTests/Services/FontManagerTests.Windows.cs
+++ src/Core/tests/DeviceTests/Services/FontManagerTests.Windows.cs
@@ -11,11 +11,7 @@ public partial class FontManagerTests : TestBase
 	[InlineData("dokdo_regular.ttf", "ms-appx:///dokdo_regular.ttf#Dokdo")]
 	[InlineData("dokdo_regular#dokdo", "ms-appx:///dokdo_regular.ttf#Dokdo")]
 	[InlineData("dokdo_regular.ttf#dokdo", "ms-appx:///dokdo_regular.ttf#Dokdo")]
-	[InlineData("myalias", "ms-appx:///dokdo_regular.ttf#Dokdo"
-#if WINDOWS
-			, Skip = "Not working for unpackaged"
-#endif
-	)]
+	[InlineData("myalias", "ms-appx:///dokdo_regular.ttf#Dokdo")]
 	public Task CanLoadFonts(string fontName, string actualFontFamily) =>
 		InvokeOnMainThreadAsync(() =>
 		{
@@ -29,7 +25,11 @@ public partial class FontManagerTests : TestBase
 		});
 
 	// TODO: this is not going to work in unpackaged
-	[Fact]
+	[Fact(
+#if WINDOWS
+			Skip = "Not working for unpackaged"
+#endif
+	)]
 	public Task CanLoadEmbeddedFont() =>
 		InvokeOnMainThreadAsync(() =>
 		{
