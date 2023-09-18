fd5e0c9785151130502f714d0a04e3be8908acd2
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-09-18 12:35:22 +0200

    Allow gestures in Windows ContentView

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue15810.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue15810 : _IssuesUITest
+	{
+		readonly string _customViewId = "CustomView";
+
+		public Issue15810(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "TapGestureRecognizer Tapped events not worked in Windows Platform";
+
+		[Test]
+		public void WhenTapCustomViewThenChangesColor()
+		{
+			App.WaitForElement(_customViewId);
+			App.Tap(_customViewId);
+
+			var infoText = App.Query("InfoLabel").First().Text;
+			Assert.IsNotEmpty(infoText);
+		}
+	}
+}
