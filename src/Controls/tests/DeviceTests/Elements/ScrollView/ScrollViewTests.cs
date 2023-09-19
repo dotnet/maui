d4e58d1f5754d5f78713b2750e4855ff5cc01eb9
Shane Neuville <shneuvil@microsoft.com>
2023-09-19 10:09:49 -0500

    - fix compile errors

--- src/Controls/tests/DeviceTests/Elements/ScrollView/ScrollViewTests.cs
+++ src/Controls/tests/DeviceTests/Elements/ScrollView/ScrollViewTests.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Hosting;
 using Xunit;
 
 namespace Microsoft.Maui.DeviceTests
