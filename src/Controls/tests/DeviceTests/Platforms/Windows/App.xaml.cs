6db83f222faa75e24e53228df78d07eb04b4802a
Gerald Versluis <gerald.versluis@microsoft.com>
2023-09-20 13:06:33 +0200

    Remove global exception handler

--- src/Controls/tests/DeviceTests/Platforms/Windows/App.xaml.cs
+++ src/Controls/tests/DeviceTests/Platforms/Windows/App.xaml.cs
@@ -4,16 +4,7 @@ namespace Microsoft.Maui.DeviceTests.WinUI
 {
 	public partial class App : MauiWinUIApplication
 	{
-		public App()
-		{
-			InitializeComponent();
-			this.UnhandledException += App_UnhandledException;
-		}
-
-		private void App_UnhandledException(object sender, UI.Xaml.UnhandledExceptionEventArgs e)
-		{
-			e.Handled = true;
-		}
+		public App() => InitializeComponent();
 
 		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 	}
