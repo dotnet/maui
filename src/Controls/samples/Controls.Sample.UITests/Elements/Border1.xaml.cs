9344c98877a9ddc694a64fd911970c33a5cb6919
Bret Johnson <bret.johnson@microsoft.com>
2023-09-20 03:15:15 -0400

    Add border gallery for UI tests

---
+++ src/Controls/samples/Controls.Sample.UITests/Elements/Border1.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Border1 : ContentView
+	{
+		public Border1()
+		{
+			InitializeComponent();
+		}
+	}
+}
