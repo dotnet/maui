4b446573f889448d1897f9bdf500be9f2b3a13ce
Bret Johnson <bret.johnson@microsoft.com>
2023-09-22 10:57:37 -0400

    Add border gallery ui tests

---
+++ src/Controls/tests/UITests/Tests/BorderUITests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Maui.Appium;
+
+namespace Microsoft.Maui.AppiumTests
+{
+	public class BorderUITests : UITestBase
+	{
+		const string BorderGallery = "* marked:'Border Gallery'";
+
+		public BorderUITests(TestDevice device)
+			: base(device)
+		{
+		}
+
+		protected override void FixtureSetup()
+		{
+			base.FixtureSetup();
+			App.NavigateToGallery(BorderGallery);
+
+			VerifyScreenshot();
+		}
+
+		protected override void FixtureTeardown()
+		{
+			base.FixtureTeardown();
+			App.NavigateBack();
+		}
+	}
+}
