149da969863d5ad36e226e7bf3741b99e8682f11
Lehonti Ramos <lehonti@ramos>
2023-09-26 23:29:32 +0200

    `sealed` some `internal` classes in `Controls.Core` that are not inherited from (2)

--- src/Controls/src/Core/Compatibility/Handlers/ListView/Windows/ListViewRenderer.cs
+++ src/Controls/src/Core/Compatibility/Handlers/ListView/Windows/ListViewRenderer.cs
@@ -57,7 +57,7 @@ namespace Microsoft.Maui.Controls.Handlers.Compatibility
 			AutoPackage = false;
 		}
 
-		internal class ListViewTransparent : WListView
+		internal sealed class ListViewTransparent : WListView
 		{
 			internal ListViewRenderer ListViewRenderer { get; }
 			public ListViewTransparent(ListViewRenderer listViewRenderer) : base()
