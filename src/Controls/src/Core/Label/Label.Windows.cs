57b7f568360c39087f62297c468c8b2101fbd3c9
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-09-29 08:56:29 +0200

    Implement gestures on Windows Spans

--- src/Controls/src/Core/Label/Label.Windows.cs
+++ src/Controls/src/Core/Label/Label.Windows.cs
@@ -1,12 +1,18 @@
 #nullable disable
+using System.Collections.Generic;
 using Microsoft.Maui.Controls.Platform;
+using Microsoft.Maui.Graphics;
+using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.Maui.Controls
 {
 	public partial class Label
 	{
-		public static void MapDetectReadingOrderFromContent(LabelHandler handler, Label label) => MapDetectReadingOrderFromContent((ILabelHandler)handler, label);
-		public static void MapText(LabelHandler handler, Label label) => MapText((ILabelHandler)handler, label);
+		public static void MapDetectReadingOrderFromContent(LabelHandler handler, Label label) =>
+			MapDetectReadingOrderFromContent((ILabelHandler)handler, label);
+
+		public static void MapText(LabelHandler handler, Label label) =>
+			MapText((ILabelHandler)handler, label);
 
 		public static void MapDetectReadingOrderFromContent(ILabelHandler handler, Label label) =>
 			Platform.TextBlockExtensions.UpdateDetectReadingOrderFromContent(handler.PlatformView, label);
@@ -19,5 +25,37 @@ namespace Microsoft.Maui.Controls
 
 		public static void MapMaxLines(ILabelHandler handler, Label label) =>
 			handler.PlatformView?.UpdateMaxLines(label);
+
+		protected override Size ArrangeOverride(Rect bounds)
+		{
+			var size = base.ArrangeOverride(bounds);
+
+			RecalculateSpanPositions();
+
+			return size;
+		}
+
+		void RecalculateSpanPositions()
+		{
+			if (Handler is not ILabelHandler labelHandler)
+			{
+				return;
+			}
+
+			var platformView = labelHandler.PlatformView;
+			var virtualView = labelHandler.VirtualView as Label;
+			if (platformView is null || virtualView is null)
+			{
+				return;
+			}
+
+			var formatted = virtualView.FormattedText;
+			if (formatted is null)
+			{
+				return;
+			}
+
+			platformView.RecalculateSpanPositions(formatted);
+		}
 	}
 }
--- src/Controls/tests/UITests/Tests/Issues/Issue3525.cs
+++ src/Controls/tests/UITests/Tests/Issues/Issue3525.cs
@@ -24,11 +24,6 @@ public class Issue3525 : _IssuesUITest
 			Assert.Ignore("Click (x, y) pointer type mouse is not implemented.");
 		}
 
-		if (Device == TestDevice.Windows)
-		{
-			Assert.Ignore("This test is failing on Windows because the feature is not yet implemented: https://github.com/dotnet/maui/pull/17731");
-		}
-
 		var label = App.WaitForElement(kLabelTestAutomationId);
 		var location = label.GetRect();
 
