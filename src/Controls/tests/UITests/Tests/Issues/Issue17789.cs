af06416cf629087951a940db65ac3df010169b43
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-10-05 16:47:53 +0200

    Adding UI Test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue17789.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue17789 : _IssuesUITest
+	{
+		public Issue17789(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "ContentPage BackgroundImageSource not working";
+
+		[Test]
+		public void ContentPageBackgroundImageSourceWorks()
+		{
+			UITestContext.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows },
+					"The bug only happens on iOS; see https://github.com/dotnet/maui/pull/17789");
+
+			App.WaitForElement("WaitForStubControl");
+			VerifyScreenshot();
+		}
+	}
+}
