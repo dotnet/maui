94d4ac38ba9106a6f78bff8067df14cac06b4d72
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-10-06 10:34:21 +0200

    Added UI Test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issues17366.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Platform;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 17366, "Wrong gray color using transparent in iOS gradients", PlatformAffected.iOS)]
+	public partial class Issue17366 : ContentPage
+	{
+		public Issue17366()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue17366.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue17366 : _IssuesUITest
+	{
+		public Issue17366(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Wrong gray color using transparent in iOS gradients";
+
+		[Test]
+		public void Issue17366Test()
+		{
+			UITestContext.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows },
+					"The bug only happens on iOS; see https://github.com/dotnet/maui/pull/17789");
+
+			App.WaitForElement("WaitForStubControl");
+			VerifyScreenshot();
+		}
+	}
+}
