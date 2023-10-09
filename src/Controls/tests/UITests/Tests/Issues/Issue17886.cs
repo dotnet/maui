732297ca9b52dc968d5685dba339ae8958735085
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-10-09 11:18:04 +0200

    Fix the issue

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue17886.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue17886 : _IssuesUITest
+	{
+		public Issue17886(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Shadow wrong scaling";
+
+		[Test]
+		public void Issue17886Test()
+		{
+			UITestContext.IgnoreIfPlatforms(new TestDevice[] { TestDevice.iOS, TestDevice.Mac, TestDevice.Windows },
+				"See https://github.com/dotnet/maui/issues/17886");
+
+			App.WaitForElement("WaitForStubControl");
+			VerifyScreenshot();
+		}
+	}
+}
