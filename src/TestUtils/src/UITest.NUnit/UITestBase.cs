ea0caf6db75d23516e055b54198849508f6e95e9
Matthew Leibowitz <mattleibow@live.com>
2023-10-13 08:10:01 +0200

    Try and detect the reason for the failures

--- src/TestUtils/src/UITest.NUnit/UITestBase.cs
+++ src/TestUtils/src/UITest.NUnit/UITestBase.cs
@@ -58,6 +58,8 @@ namespace UITest.Appium.NUnit
 
 			if (App.AppState == ApplicationState.Not_Running)
 			{
+				SaveDiagnosticLogs("UITestBaseTearDownNotRunning");
+
 				// Assert.Fail will immediately exit the test which is desirable as the app is not
 				// running anymore so we don't want to log diagnostic data as there is nothing to collect from
 				Reset();
