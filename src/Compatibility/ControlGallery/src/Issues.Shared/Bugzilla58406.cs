cd9fdd75510cecdd298c80eaf18fff47a5e89d7f
Gerald Versluis <gerald.versluis@microsoft.com>
2023-10-15 01:01:26 +0200

    Enable more legacy tests (#17931)
    * Add more tests to iOS
    
    * Ignore failing iOS tests
    
    * Ignore failing iOS tests
    
    ---------
    
    Co-authored-by: Shane Neuville <shneuvil@microsoft.com>

--- src/Compatibility/ControlGallery/src/Issues.Shared/Bugzilla58406.cs
+++ src/Compatibility/ControlGallery/src/Issues.Shared/Bugzilla58406.cs
@@ -49,6 +49,7 @@ namespace Microsoft.Maui.Controls.ControlGallery.Issues
 
 #if UITEST
 [Microsoft.Maui.Controls.Compatibility.UITests.FailsOnMauiAndroid]
+[Microsoft.Maui.Controls.Compatibility.UITests.FailsOnMauiIOS]
 		[Test]
 		public void EffectAppliesToLabel()
 		{
