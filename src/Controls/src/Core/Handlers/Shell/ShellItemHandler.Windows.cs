dotnet/maui

commit d6514bda9c57a188e77cb6b7770f1f41bd497692

Fixed the tab bar not hiding/showing if property changed (#17069)
* fix(tabbar): Fixed the tab bar not hiding/showing if property changed

The tabbar on Windows wouldn't toggle visibility if modified, and would only hide/show on current item change

* Apply patch

* Update tab bar vis call to use `UpdateValue`

---------

Co-authored-by: Mike Corsaro <mikecorsaro@microsoft.com>

src/Controls/src/Core/Handlers/Shell/ShellItemHandler.Windows.cs
@@ -47,7 +47,7 @@ namespace Microsoft.Maui.Controls.Handlers
 				PlatformView.Loaded -= OnNavigationViewLoaded;
 
 			UpdateSearchHandler();
-			MapMenuItems();
+			MapMenuItems(true);
 		}
 
 		protected override void ConnectHandler(FrameworkElement platformView)
@@ -85,6 +85,14 @@ namespace Microsoft.Maui.Controls.Handlers
 
 			if (_currentSearchHandler != null)
 				_currentSearchHandler.PropertyChanged -= OnCurrentSearchHandlerPropertyChanged;
+
+			if (_shellItem?.Parent is IShellController controller)
+			{
+				controller.RemoveAppearanceObserver(this);
+			}
+
+			if (_shellItem is IShellItemController shellItemController)
+				shellItemController.ItemsCollectionChanged -= OnItemsChanged;
 		}
 
 		public override void SetVirtualView(Maui.IElement view)
@@ -92,19 +100,32 @@ namespace Microsoft.Maui.Controls.Handlers
 			if (view.Parent is IShellController controller)
 			{
 				if (_shellItem != null)
+				{
 					controller.RemoveAppearanceObserver(this);
+					((IShellItemController)_shellItem).ItemsCollectionChanged -= OnItemsChanged;
+				}
 
 				_shellItem = (ShellItem)view;
 
 				base.SetVirtualView(view);
 
 				if (_shellItem != null)
+				{
 					controller.AddAppearanceObserver(this, _shellItem);
+					((IShellItemController)_shellItem).ItemsCollectionChanged += OnItemsChanged;
+				}
 			}
 			else
 			{
 				base.SetVirtualView(view);
 			}
+		}
+
+		private void OnItemsChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			// Flag to sync the selected item only after items changed fired, which will happen after modifications
+			// to the items list is finished
+			MapMenuItems(true);
 		}
 
 		private void OnNavigationTabChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
@@ -124,21 +145,17 @@ namespace Microsoft.Maui.Controls.Handlers
 			}
 		}
 
-		void MapMenuItems()
+		void MapMenuItems(bool syncSelectedItem)
 		{
 			IShellItemController shellItemController = VirtualView;
 			var items = new List<BaseShellItem>();
 
-			// only add items if we should be showing the tabs
-			if (shellItemController.ShowTabs)
+			foreach (var item in shellItemController.GetItems())
 			{
-				foreach (var item in shellItemController.GetItems())
-				{
-					if (Routing.IsImplicit(item))
-						items.Add(item.CurrentItem);
-					else
-						items.Add(item);
-				}
+				if (Routing.IsImplicit(item))
+					items.Add(item.CurrentItem);
+				else
+					items.Add(item);
 			}
 
 			object? selectedItem = null;
@@ -205,10 +222,10 @@ namespace Microsoft.Maui.Controls.Handlers
 				}
 			});
 
-			if (ShellItemNavigationView.SelectedItem != selectedItem)
+			if (syncSelectedItem && ShellItemNavigationView.SelectedItem != selectedItem)
 				ShellItemNavigationView.SelectedItem = selectedItem;
 
-			ShellItemNavigationView.PinPaneDisplayModeTo = GetNavigationViewPaneDisplayMode(shellItemController);
+			UpdateValue(Shell.TabBarIsVisibleProperty.PropertyName);
 		}
 
 		void UpdateSearchHandler()
@@ -356,7 +373,7 @@ namespace Microsoft.Maui.Controls.Handlers
 
 		internal void UpdateTitle()
 		{
-			MapMenuItems();
+			MapMenuItems(true);
 		}
 
 		void UpdateCurrentItem()
@@ -383,7 +400,10 @@ namespace Microsoft.Maui.Controls.Handlers
 			}
 
 			UpdateSearchHandler();
-			MapMenuItems();
+
+			// Don't sync the selected item as this function can be called multiple times on item removal
+			// before the list has finished fully updating
+			MapMenuItems(false);
 
 			if (_currentShellSection != null)
 			{
@@ -428,9 +448,17 @@ namespace Microsoft.Maui.Controls.Handlers
 				ShellItemNavigationView.SelectedItem = navigationViewItemViewModel;
 		}
 
+		void UpdateTabBarVisibility(IShellItemController item)
+		{
+			var paneDisplayMode = GetNavigationViewPaneDisplayMode(item);
+			ShellItemNavigationView.PaneDisplayMode = paneDisplayMode;
+			ShellItemNavigationView.PinPaneDisplayModeTo = paneDisplayMode;
+			ShellItemNavigationView.IsPaneVisible = item.ShowTabs;
+		}
+
 		public static void MapTabBarIsVisible(ShellItemHandler handler, ShellItem item)
 		{
-			handler.ShellItemNavigationView.PaneDisplayMode = handler.GetNavigationViewPaneDisplayMode(item);
+			handler.UpdateTabBarVisibility(item);
 		}
 
 		NavigationViewPaneDisplayMode GetNavigationViewPaneDisplayMode(IShellItemController shellItemController)

src/Controls/tests/DeviceTests/Elements/Shell/ShellTabBarTests.Windows.cs
@@ -18,6 +18,48 @@ namespace Microsoft.Maui.DeviceTests
 	[Category(TestCategory.Shell)]
 	public partial class ShellTests
 	{
+		[Fact(DisplayName = "Shell TabBar visibility toggle")]
+		public async Task ShellTabBarVisibilityToggleWorks()
+		{
+			await RunShellTabBarTests(shell => { },
+			(shell) =>
+			{
+				var shellItemHandler = shell.CurrentItem.Handler as ShellItemHandler;
+				var navView = shellItemHandler.PlatformView as MauiNavigationView;
+
+				Shell.SetTabBarIsVisible(shell.CurrentPage, false);
+				Assert.False(navView.IsPaneVisible);
+
+				Shell.SetTabBarIsVisible(shell.CurrentPage, true);
+				Assert.True(navView.IsPaneVisible);
+				Assert.True(navView.PaneDisplayMode == UI.Xaml.Controls.NavigationViewPaneDisplayMode.Top);
+
+				return Task.FromResult(true);
+			});
+		}
+
+		[Fact(DisplayName = "Shell TabBar visibility toggles when removing current")]
+		public async Task ShellTabBarVisibilityToggleWorksRemovingCurrentItem()
+		{
+			await RunShellTabBarTests(shell => { },
+			(shell) =>
+			{
+				var shellItemHandler = shell.CurrentItem.Handler as ShellItemHandler;
+				var navView = shellItemHandler.PlatformView as MauiNavigationView;
+
+				Shell.SetTabBarIsVisible(shell.Items[0].Items[1], false);
+				shell.CurrentItem = shell.Items[0].Items[1];
+
+				Assert.False(navView.IsPaneVisible);
+
+				shell.Items[0].Items.RemoveAt(1);
+
+				Assert.True(navView.IsPaneVisible);
+				Assert.True(navView.PaneDisplayMode == UI.Xaml.Controls.NavigationViewPaneDisplayMode.Top);
+				return Task.FromResult(true);
+			});
+		}
+
 		List<WNavigationViewItem> GetTabBarItems(Shell shell)
 		{
 			var shellItemHandler = shell.CurrentItem.Handler as ShellItemHandler;

commit 371b809143286849b6618b351fec51cd5be6b7b5

[WinUI] Fixes window blinking when resizing
This bug was introduced by https://github.com/dotnet/maui/pull/17069,
but the underlying cause is a bug on WinUI that makes the window blink when
setting `NavigationView.IsPaneVisible = false` (https://microsoft.visualstudio.com/DefaultCollection/OS/_workitems/edit/47131770).

After testing the samples attached on the issues the original commit fixed, this change does not seem to re-introduce those issues.

Fixes https://github.com/dotnet/maui/issues/17724

src/Controls/src/Core/Handlers/Shell/ShellItemHandler.Windows.cs
@@ -476,7 +476,6 @@ namespace Microsoft.Maui.Controls.Handlers
 			var paneDisplayMode = GetNavigationViewPaneDisplayMode(item);
 			mauiNavView.PaneDisplayMode = paneDisplayMode;
 			mauiNavView.PinPaneDisplayModeTo = paneDisplayMode;
-			mauiNavView.IsPaneVisible = item.ShowTabs;
 		}
 
 		public static void MapTabBarIsVisible(ShellItemHandler handler, ShellItem item)