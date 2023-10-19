0fe9f8b8b5f2b6391db364c1eef3a27ae9b09e55
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-10-19 16:57:00 +0200

    More changes

--- src/Essentials/src/Connectivity/Connectivity.uwp.cs
+++ src/Essentials/src/Connectivity/Connectivity.uwp.cs
@@ -20,9 +20,9 @@ namespace Microsoft.Maui.Networking
 		void NetworkStatusChanged(object sender) =>
 			OnConnectivityChanged();
 
-		public NetworkAccess NetworkAccess => 
+		public NetworkAccess NetworkAccess =>
 			HasThreadAccess ?
-			GetNetworkAccess() :			
+			GetNetworkAccess() :
 			DispatchAsync(GetNetworkAccess).GetAwaiter().GetResult();
 
 		bool HasThreadAccess
@@ -89,48 +89,50 @@ namespace Microsoft.Maui.Networking
 			}
 		}
 
-		public IEnumerable<ConnectionProfile> ConnectionProfiles
+		public IEnumerable<ConnectionProfile> ConnectionProfiles =>	
+			HasThreadAccess ?
+			GetConnectionProfiles() :
+			DispatchAsync(GetConnectionProfiles).GetAwaiter().GetResult();
+
+		IEnumerable<ConnectionProfile> GetConnectionProfiles()
 		{
-			get
+			var networkInterfaceList = NetworkInformation.GetConnectionProfiles();
+			foreach (var interfaceInfo in networkInterfaceList.Where(nii => nii.GetNetworkConnectivityLevel() != NetworkConnectivityLevel.None))
 			{
-				var networkInterfaceList = NetworkInformation.GetConnectionProfiles();
-				foreach (var interfaceInfo in networkInterfaceList.Where(nii => nii.GetNetworkConnectivityLevel() != NetworkConnectivityLevel.None))
+				var type = ConnectionProfile.Unknown;
+
+				try
 				{
-					var type = ConnectionProfile.Unknown;
+					var adapter = interfaceInfo.NetworkAdapter;
+					if (adapter == null)
+						continue;
 
-					try
+					// http://www.iana.org/assignments/ianaiftype-mib/ianaiftype-mib
+					switch (adapter.IanaInterfaceType)
 					{
-						var adapter = interfaceInfo.NetworkAdapter;
-						if (adapter == null)
+						case 6:
+							type = ConnectionProfile.Ethernet;
+							break;
+						case 71:
+							type = ConnectionProfile.WiFi;
+							break;
+						case 243:
+						case 244:
+							type = ConnectionProfile.Cellular;
+							break;
+
+						// xbox wireless, can skip
+						case 281:
 							continue;
-
-						// http://www.iana.org/assignments/ianaiftype-mib/ianaiftype-mib
-						switch (adapter.IanaInterfaceType)
-						{
-							case 6:
-								type = ConnectionProfile.Ethernet;
-								break;
-							case 71:
-								type = ConnectionProfile.WiFi;
-								break;
-							case 243:
-							case 244:
-								type = ConnectionProfile.Cellular;
-								break;
-
-							// xbox wireless, can skip
-							case 281:
-								continue;
-						}
 					}
-					catch (Exception ex)
-					{
-						// TODO Add Logging?
-						Debug.WriteLine($"Unable to get Network Adapter, returning Unknown: {ex.Message}");
-					}
-
-					yield return type;
 				}
+				catch (Exception ex)
+				{
+					// TODO Add Logging?
+					Debug.WriteLine($"Unable to get Network Adapter, returning Unknown: {ex.Message}");
+				}
+
+				yield return type;
 			}
 		}
 	}
