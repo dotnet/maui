e3db691c858d9f479a1fe9bdf91879bfefc2b8e9
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-10-20 12:06:31 +0200

    Fix the issue

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issues18131.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Graphics;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 18131, "Color changes are not reflected in the Rectangle shapes",
+		PlatformAffected.Android | PlatformAffected.iOS | PlatformAffected.UWP)]
+	public partial class Issue18131 : ContentPage
+	{
+		public Issue18131()
+		{
+			InitializeComponent();
+		}
+
+		void OnButtonClicked(object sender, System.EventArgs e)
+		{
+			RectangleTest.BackgroundColor = Colors.Green;
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18131.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18131 : _IssuesUITest
+	{
+		public Issue18131(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Color changes are not reflected in the Rectangle shapes";
+
+		[Test]
+		public void Issue15330Test()
+		{
+			App.WaitForElement("ChangeColorButton");
+			App.Click("ChangeColorButton");
+			VerifyScreenshot();
+		}
+	}
+}
