90b14ba209d51307685d1be9046bd0e17d8d216c
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-10-20 10:41:47 +0200

    More tests

---
+++ src/Controls/tests/UITests/Tests/Issues/BorderWithDifferentShapes.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	class BorderWithDifferentShapes : _IssuesUITest
+	{
+		public BorderWithDifferentShapes(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Validate Border using different shapes";
+
+		[Test]
+		public void Issue16094Test()
+		{
+			App.WaitForElement("WaitForStubControl");
+			VerifyScreenshot();
+		}
+	}
+}
