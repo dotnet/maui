2cb1c4cc007d10f4f58dbb8dad79b58b698590bb
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-10-24 12:35:45 +0200

    Added more tests

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18172.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	class Issue18172 : _IssuesUITest
+	{
+		public Issue18172(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Shadows not drawing/updating correctly in Windows & cover entire screen";
+
+		[Test]
+		public void Issue18172Test()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.iOS },
+				"Issue only happens on Windows");
+
+			App.WaitForElement("BorderControl");
+			VerifyScreenshot();
+		}
+	}
+}
