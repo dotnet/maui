5dcb3406afc89d075c61606d85d2b284ff189fa0
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-03 21:04:22 +0100

    [iOS] Fix ContentPage BackgroundImageSource not working  (#17789)
    * Fix ContentPage BackgroundImageSource not working on iOS/Catalyst
    
    * Adding UI Test
    
    * Updated screenshots
    
    * Updated screenshot
    
    * Updated test
    
    * Created iOS PageExtensions
    
    * Enabled test for all the platforms
    
    * Add pending snapshots
    
    * Changed iOS PageExtensions to internal

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue17789.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue17789 : _IssuesUITest
+	{
+		public Issue17789(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "ContentPage BackgroundImageSource not working";
+
+		[Test]
+		public void ContentPageBackgroundImageSourceWorks()
+		{
+			App.WaitForElement("WaitForStubControl");
+			VerifyScreenshot();
+		}
+	}
+}
