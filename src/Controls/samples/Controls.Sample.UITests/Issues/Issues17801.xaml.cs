0f160ca8b773e81b1d1c531f84478437d3136f4f
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-07 13:28:33 +0100

    Added template UI Test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issues17801.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 17801, "ScrollView always has a scroll bar on iOS", PlatformAffected.iOS)]
+	public partial class Issue17801 : ContentPage
+	{
+		int _count = 0;
+
+		public Issue17801()
+		{
+			InitializeComponent();
+		}
+		
+		void OnCounterClicked(object sender, EventArgs e)
+		{
+			_count++;
+
+			if (_count == 1)
+				CounterBtn.Text = $"Clicked {_count} time";
+			else
+				CounterBtn.Text = $"Clicked {_count} times";
+		}
+	}
+}
