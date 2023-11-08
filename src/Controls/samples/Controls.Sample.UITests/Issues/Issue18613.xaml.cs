0124b0540afeb3579243ccf7424ee1f29cc5092b
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-08 10:34:44 +0100

    Created UI Test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue18613.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.ManualTest, "G1", "Disabled Button cannot trigger Command", PlatformAffected.All)]
+	public partial class Issue18613 : ContentPage
+	{
+		public Issue18613()
+		{
+			InitializeComponent();
+		}
+		
+		void OnButtonClicked(object sender, EventArgs e)
+		{
+			Console.WriteLine("Button_Clicked");
+		}
+	}
+
+	public class Issue18613ViewModel : BindableObject
+	{
+		int _count;
+		string _counter;
+
+		public Issue18613ViewModel()
+		{
+			Command = new Command(HandleCommand);
+		}
+
+		public bool IsButtonEnabled { get; set; } = false;
+
+		public ICommand Command { get; set; }
+
+		public string Counter
+		{
+			get { return _counter; }
+			set
+			{
+				_counter = value;
+				OnPropertyChanged();
+			}
+		}
+
+		void HandleCommand()
+		{
+			_count++;
+			Counter = $"Tapped {_count} times";
+			Console.WriteLine("HandleCommand");
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18613.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18613 : _IssuesUITest
+	{
+		public Issue18613(TestDevice device) : base(device) { }
+
+		public override string Issue => "Disabled Button cannot trigger Command";
+
+		[Test]
+		public void CommandDisableButton()
+		{
+			App.WaitForElement("WaitForStubControl");
+			App.Click("ButtonControl");
+
+			// If clicking the Button, we do not have a counter result in
+			// the Label, the command has not been executed.
+			var counter = App.FindElement("CounterLabel").GetText();
+			Assert.True(string.IsNullOrEmpty(counter));
+
+			// Verify the visual status of the disabled Button.
+			VerifyScreenshot();
+		}
+	}
+}
