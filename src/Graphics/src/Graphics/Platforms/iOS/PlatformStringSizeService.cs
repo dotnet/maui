99b70486f90ae7ddc67aef5a6b4213b86629b637
Tim Miller <innerlogic4321@gmail.com>
2023-11-08 19:36:59 +0900

    Remove API check, as the baseline versions of MAUI are newer than the check requires

--- src/Graphics/src/Graphics/Platforms/iOS/PlatformStringSizeService.cs
+++ src/Graphics/src/Graphics/Platforms/iOS/PlatformStringSizeService.cs
@@ -15,21 +15,11 @@ namespace Microsoft.Maui.Graphics.Platform
 			var nsString = new NSString(value);
 			var uiFont = font?.ToPlatformFont(fontSize) ?? FontExtensions.GetDefaultPlatformFont();
 			
-			CGSize size;
-			if (OperatingSystem.IsIOSVersionAtLeast(14) || OperatingSystem.IsMacCatalystVersionAtLeast(13, 1))
-			{
-				size = nsString.GetBoundingRect(
-					CGSize.Empty,
-					NSStringDrawingOptions.UsesLineFragmentOrigin,
-					new UIStringAttributes { Font = uiFont },
-					null).Size;
-			}
-			else
-			{
-#pragma warning disable CA1422 // Validate platform compatibility
-				size = nsString.StringSize(uiFont, CGSize.Empty);
-#pragma warning restore CA1422 // Validate platform compatibility
-			}
+			CGSize size = nsString.GetBoundingRect(
+				CGSize.Empty,
+				NSStringDrawingOptions.UsesLineFragmentOrigin,
+				new UIStringAttributes { Font = uiFont },
+				null).Size;
 
 			uiFont.Dispose();
 			return new SizeF((float)size.Width, (float)size.Height);
