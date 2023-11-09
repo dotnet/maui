8a1d9000cbe808d2c324ca222de1c8f7a461ee8c
lytico <lytico@users.sourceforge.net>
2023-11-09 23:59:48 +0100

    [Gtk] Core.csproj: PageHandler.Gtk: track api changes

--- src/Core/src/Handlers/Page/PageHandler.Gtk.cs
+++ src/Core/src/Handlers/Page/PageHandler.Gtk.cs
@@ -8,40 +8,6 @@ namespace Microsoft.Maui.Handlers
 	public partial class PageHandler : ContentViewHandler
 	{
 
-		public override void SetVirtualView(IView view)
-		{
-			base.SetVirtualView(view);
-
-			_ = PlatformView ?? throw new InvalidOperationException($"{nameof(PlatformView)} should have been set by base class.");
-			_ = VirtualView ?? throw new InvalidOperationException($"{nameof(VirtualView)} should have been set by base class.");
-			_ = MauiContext ?? throw new InvalidOperationException($"{nameof(MauiContext)} should have been set by base class.");
-
-			PlatformView.CrossPlatformMeasure = VirtualView.Measure;
-			PlatformView.CrossPlatformArrange = VirtualView.Arrange;
-
-		}
-
-		protected override ContentView CreatePlatformView()
-		{
-			if (VirtualView == null)
-			{
-				throw new InvalidOperationException($"{nameof(VirtualView)} must be set to create a LayoutView");
-			}
-
-			var pw = new PageView
-			{
-				CrossPlatformMeasure = VirtualView.Measure,
-				CrossPlatformArrange = VirtualView.Arrange
-			};
-
-			return pw;
-		}
-
-		public static void MapContent(PageHandler handler, IView page)
-		{
-			handler.UpdateContent();
-		}
-
 		[MissingMapper]
 		public static void MapTitle(IPageHandler handler, IContentView page) { }
 
