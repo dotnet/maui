9ac15cc2069fcca74b6b34dc882ff94327ed7c4c
lytico <lytico@users.sourceforge.net>
2023-11-10 01:48:21 +0100

    [Gtk] Core.csproj: TabbedViewHandler: use NotImplementedView

--- src/Core/src/Handlers/TabbedView/TabbedViewHandler.cs
+++ src/Core/src/Handlers/TabbedView/TabbedViewHandler.cs
@@ -10,7 +10,7 @@ using PlatformView = Microsoft.UI.Xaml.FrameworkElement;
 #elif TIZEN
 using PlatformView = Tizen.NUI.BaseComponents.View;
 #elif GTK
-using PlatformView = Gtk.Widget;
+using PlatformView = Microsoft.Maui.Platform.NotImplementedView;
 #elif (NETSTANDARD || !PLATFORM) || (NET6_0_OR_GREATER && !IOS && !ANDROID && !TIZEN)
 using PlatformView = System.Object;
 #endif
