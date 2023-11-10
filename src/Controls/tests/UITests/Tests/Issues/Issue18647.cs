f8c13b6132c800c75e0067745e66c4235d32d0dd
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-10 11:53:07 +0100

    Added UI Test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18647.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	class Issue18647 : _IssuesUITest
+	{
+		public Issue18647(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Editor font properties";
+
+		[Test]
+		public void Issue18647Test()
+		{
+			App.WaitForElement("WaitForStubControl");
+
+			VerifyScreenshot();
+		}
+	}
+}
