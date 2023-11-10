f43b695f7325452175bd671245fecd1f9d93f984
Bret Johnson <bret.johnson@microsoft.com>
2023-11-10 13:15:07 -0500

    Include initial TestApp controller

---
+++ src/Controls/tests/UITests/Tests/TestAppController.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using Microsoft.Extensions.Logging;
+using VisualTestUtils.AppConnector.Controller;
+
+public class TestAppController : AppConnectorController
+{
+	public TestAppController(IPAddress ipAddress, int port = 4243, ILogger? logger = null)
+		: base(ipAddress, port, logger)
+	{
+	}
+}
--- src/Controls/tests/UITests/Tests/TestContextSetupFixture.cs
+++ src/Controls/tests/UITests/Tests/TestContextSetupFixture.cs
@@ -1,14 +1,30 @@
-﻿using UITest.Appium;
+﻿using System.Net;
+using UITest.Appium;
 
 // SetupFixture runs once for all tests under the same namespace, if placed outside the namespace it will run once for all tests in the assembly
 public class AssemblySetupFixture : UITestContextSetupFixture
 {
 	AppiumServerContext? _appiumServerContext;
+	TestAppController? _testAppController;
 
 	public override void Initialize()
 	{
 		_appiumServerContext = new AppiumServerContext();
 		_appiumServerContext.CreateAndStartServer();
 		_serverContext = _appiumServerContext;
+
+		// Fire and forget
+		_ = StartControllerAsync();
+	}
+
+	public async Task StartControllerAsync()
+	{
+		if (_testAppController is not null)
+		{
+			throw new InvalidOperationException("Controller is already running.");
+		}
+
+		_testAppController = new TestAppController(IPAddress.Loopback);
+		await _testAppController.InitializeAsync(TimeSpan.FromSeconds(120));
 	}
 }
