55ca87014647d1aec92d343dc8284dc0255f9bd7
lytico <lytico@users.sourceforge.net>
2023-11-10 21:23:42 +0100

    [Gtk] Core.csproj: ViewHandler.Gtk.cs: add MapToolbar [MissingMapper]

--- src/Core/src/Handlers/View/ViewHandler.Gtk.cs
+++ src/Core/src/Handlers/View/ViewHandler.Gtk.cs
@@ -37,6 +37,20 @@
 		[MissingMapper]
 		public static void MapAnchorY(IViewHandler handler, IView view) { }
 
+		public static void MapToolbar(IViewHandler handler, IView view)
+		{
+			if (handler.VirtualView is not IToolbarElement te || te.Toolbar == null)
+				return;
+
+			MapToolbar(handler, te);
+		}
+
+		[MissingMapper]
+		internal static void MapToolbar(IElementHandler handler, IToolbarElement te)
+		{
+			
+		}
+
 	}
 
 }
