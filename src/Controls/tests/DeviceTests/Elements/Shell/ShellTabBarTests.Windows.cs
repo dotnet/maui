b0ffa8c8aaddee4d845fae286f0ac352a000c586
Mike Corsaro <mikecorsaro@microsoft.com>
2023-11-10 10:58:49 -0800

    Modify Shell tabbar vis tests to check for element visibility

--- src/Controls/tests/DeviceTests/Elements/Shell/ShellTabBarTests.Windows.cs
+++ src/Controls/tests/DeviceTests/Elements/Shell/ShellTabBarTests.Windows.cs
@@ -25,12 +25,13 @@ namespace Microsoft.Maui.DeviceTests
 			{
 				var shellItemHandler = shell.CurrentItem.Handler as ShellItemHandler;
 				var navView = shellItemHandler.PlatformView as MauiNavigationView;
+				var navButtonGrid = navView.GetDescendantByName<UI.Xaml.Controls.Grid>("TopNavGrid");
 
 				Shell.SetTabBarIsVisible(shell.CurrentPage, false);
-				Assert.False(navView.IsPaneVisible);
+				Assert.True(navButtonGrid.Visibility == UI.Xaml.Visibility.Collapsed);
 
 				Shell.SetTabBarIsVisible(shell.CurrentPage, true);
-				Assert.True(navView.IsPaneVisible);
+				Assert.True(navButtonGrid.Visibility == UI.Xaml.Visibility.Visible);
 				Assert.True(navView.PaneDisplayMode == UI.Xaml.Controls.NavigationViewPaneDisplayMode.Top);
 
 				return Task.FromResult(true);
@@ -45,18 +46,19 @@ namespace Microsoft.Maui.DeviceTests
 			{
 				var shellItemHandler = shell.CurrentItem.Handler as ShellItemHandler;
 				var navView = shellItemHandler.PlatformView as MauiNavigationView;
+				var navButtonGrid = navView.GetDescendantByName<UI.Xaml.Controls.Grid>("TopNavGrid");
 
 				// Set page to have the tabbar be hidden, then switch to it
 				Shell.SetTabBarIsVisible(shell.Items[0].Items[1], false);
 				shell.CurrentItem = shell.Items[0].Items[1];
 
-				Assert.False(navView.IsPaneVisible);
+				Assert.True(navButtonGrid.Visibility == UI.Xaml.Visibility.Collapsed);
 
 				// Tabbar should now be hidden, remove the current page which should cause
 				// us to switch to another page where the tabbar is not hidden (default)
 				shell.Items[0].Items.RemoveAt(1);
 
-				Assert.True(navView.IsPaneVisible);
+				Assert.True(navButtonGrid.Visibility == UI.Xaml.Visibility.Visible);
 				Assert.True(navView.PaneDisplayMode == UI.Xaml.Controls.NavigationViewPaneDisplayMode.Top);
 				return Task.FromResult(true);
 			});
