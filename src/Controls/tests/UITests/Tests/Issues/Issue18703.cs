63cae8703c299c465262151b581ca89896cd8d28
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-13 09:47:49 +0100

    Added UI Test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18703.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18703 : _IssuesUITest
+	{
+		public Issue18703(TestDevice device) : base(device) { }
+
+		public override string Issue => "Editor TextAlignment properties works";
+
+		[Test]
+		public void EditorTextAlignmentWorks()
+		{
+			App.WaitForElement("WaitForStubControl");
+
+			VerifyScreenshot();
+		}
+	}
+}
