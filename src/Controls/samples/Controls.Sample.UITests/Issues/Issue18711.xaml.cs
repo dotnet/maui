20ae3440f3557f4c7913868bff529f6f4916812e
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-13 12:17:01 +0100

    Added UI Test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue18711.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.ManualTest, "D22", "Editor IsSpellCheckEnabledDisabled works", PlatformAffected.All)]
+	public partial class Issue18711 : ContentPage
+	{
+		public Issue18711()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18711.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18711 : _IssuesUITest
+	{
+		public Issue18711(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Editor IsSpellCheckEnabledDisabled works";
+
+		[Test]
+		public void Issue18711Test()
+		{
+			App.WaitForElement("WaitForStubControl");
+
+			// 1. Enter the text 'qweqw hi hi' in the editor below.
+			App.EnterText("TestEditor", "qweqw hi hi");
+			// 2. The test fails if there is no red underline under the string 'qweqw' to indicate a spelling error.
+			VerifyScreenshot("IsSpellCheckEnabledEnabled");
+			// 3. Uncheck the 'IsSpellCheckEnabled' checkbox to disable spellcheck.
+			App.Click("TestCheckbox");
+			// 4. The test fails if the red underline under the string 'qweqw' is still visible.
+			VerifyScreenshot("IsSpellCheckEnabledDisabled");
+		}
+	}
+}
