0dbbe8f77ea975b879c41b57ae5bc729697ed97f
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-13 12:33:47 +0100

    Added UI Test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue18712.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.ManualTest, "D24", "Editor IsEnabled and IsVisible works", PlatformAffected.All)]
+	public partial class Issue18712 : ContentPage
+	{
+		public Issue18712()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18712.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18712 : _IssuesUITest
+	{
+		public Issue18712(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Editor IsEnabled and IsVisible works";
+
+		[Test]
+		public void Issue18712Test()
+		{
+			App.WaitForElement("WaitForStubControl");
+
+			VerifyScreenshot();
+		}
+	}
+}
