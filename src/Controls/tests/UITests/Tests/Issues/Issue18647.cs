742e45b839aa433faf0058a7fd6def472cbe5a50
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-13 13:13:59 +0100

    Updated test

--- src/Controls/tests/UITests/Tests/Issues/Issue18647.cs
+++ src/Controls/tests/UITests/Tests/Issues/Issue18647.cs
@@ -22,29 +22,30 @@ namespace Microsoft.Maui.AppiumTests.Issues
 			App.EnterText("NoneTextTransformEditor", "aBcDeFg");
 
 			// 2. The test fails if the editor below does not display the string 'aBcDeFg' exactly (case-sensitive).
-			var result2 = App.FindElement("NoneTextTransformEditor").GetText();
+			var result2 = App.FindElement("NoneTextTransformEditor").GetText()?.Trim();
 			Assert.AreEqual("aBcDeFg", result2);
 
 			// 3. Input the string 'aBcDeFg' into the editor below.
 			App.EnterText("LowercaseTextTransformEditor", "aBcDeFg");
 
 			// 4. The test fails if the editor below does not display the string 'abcdefg' exactly (case-sensitive).
-			var result4 = App.FindElement("LowercaseTextTransformEditor").GetText();
+			var result4 = App.FindElement("LowercaseTextTransformEditor").GetText()?.Trim();
 			Assert.AreEqual("abcdefg", result4);
 
 			// 5. Input the string 'aBcDeFg' into the editor below.
 			App.EnterText("UppercaseTextTransformEditor", "aBcDeFg");
 
 			// 6. The test fails if the editor below does not display the string 'ABCDEFG' exactly (case-sensitive).
-			var result6 = App.FindElement("UppercaseTextTransformEditor").GetText();
+			var result6 = App.FindElement("UppercaseTextTransformEditor").GetText()?.Trim();
 			Assert.AreEqual("ABCDEFG", result6);
 
 			// 7. The test fails if the editor below does not display the string 'abcdefg' exactly (case-sensitive).
-			var result7 = App.FindElement("FilledLowercaseTextTransformEditor").GetText();
+			var result7 = App.FindElement("FilledLowercaseTextTransformEditor").GetText()?.Trim();
 			Assert.AreEqual("abcdefg", result7);
 
 			// 8. The test fails if the editor below does not display the string 'ABCDEFG' exactly (case-sensitive).
-			var result8 = App.FindElement("FilledUppercaseTextTransformEditor").GetText();
+			var result8 = App.FindElement("FilledUppercaseTextTransformEditor").GetText()?.Trim();
+
 			Assert.AreEqual("ABCDEFG", result8);
 		}
 	}
