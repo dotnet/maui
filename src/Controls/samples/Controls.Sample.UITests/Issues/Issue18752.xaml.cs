60d0122ce5459d694052fc73821780f143488788
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-15 13:40:49 +0100

    Added UI Test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue18752.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.ManualTest, "D7", "[D7] Editor IsSpellCheckEnabled works", PlatformAffected.All)]
+	public partial class Issue18752 : ContentPage
+	{
+		public Issue18752()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18752.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18752 : _IssuesUITest
+	{
+		public Issue18752(TestDevice device) : base(device) { }
+
+		public override string Issue => "[D7] Editor IsSpellCheckEnabled works";
+
+		[Test]
+		public void Issue18752Test()
+		{
+			App.WaitForElement("WaitForStubControl");
+
+			// 1. Enter the string 'qweqw hi hi' exactly (case-sensitive) in the editor below.
+			App.EnterText("EditorIsSpellCheckDisabled", "qweqw hi hi");
+			// 2. The test fails if there is a red underline under the string 'qweqw' to indicate a spelling error.
+		
+			// 3. Enter the string 'qweqw hi hi' exactly (case-sensitive) in the editor below.
+			App.EnterText("EditorIsSpellCheckEnabled", "qweqw hi hi");
+			// 4. The test fails if there is no red underline under the string 'qweqw' to indicate a spelling error.
+					
+			// 5. The test fails if there is no red underline under the string 'qweqw' in the editor below to indicate a spelling error.
+
+			VerifyScreenshot();
+		}
+	}
+}
