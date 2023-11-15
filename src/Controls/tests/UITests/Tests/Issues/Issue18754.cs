03946f3ee5200ed4c13daf7af1af7897984ef591
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-15 16:30:19 +0100

    Added UI Test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18754.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18754 : _IssuesUITest
+	{
+		public Issue18754(TestDevice device) : base(device) { }
+
+		public override string Issue => "[D9] Editor IsReadOnly works";
+
+		[Test]
+		public void Issue18754Test()
+		{
+			App.WaitForElement("WaitForStubControl");
+
+			// 1. Attempt to enter any text into the editor below.
+			App.Click("ReadOnlyEditor");
+
+			// 2. The test fails if the editor displays the input.
+			Assert.IsFalse(App.IsKeyboardShown());
+
+			// 3. Attempt to edit the text in the editor below.
+			App.Click("FilledReadOnlyEditor");
+
+			// 4. The test fails if the editor displays the input.
+			Assert.IsFalse(App.IsKeyboardShown());
+		}
+	}
+}
