f1adb13b9e2f9926837116d3b330f4c4a6fa1534
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-15 12:55:18 +0100

    Added UITest

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18751.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18751 : _IssuesUITest
+	{
+		public Issue18751(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Can scroll CollectionView inside RefreshView";
+
+		[Test]
+		public void Issue18000Test()
+		{
+			App.WaitForElement("WaitForStubControl");
+
+			// The test passes if you are able to see the image, name, and location of each monkey.
+			VerifyScreenshot();
+		}
+	}
+}
