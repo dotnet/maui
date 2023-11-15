b2e2610ead482dcebd6bb0a0c027f47875c7307d
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-15 14:00:03 +0100

    Added UITest

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue18753.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.ManualTest, "D8", "[D8] Editor IsTextPredictionEnabled works", PlatformAffected.All)]
+	public partial class Issue18753 : ContentPage
+	{
+		public Issue18753()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18753.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18753 : _IssuesUITest
+	{
+		public Issue18753(TestDevice device) : base(device) { }
+
+		public override string Issue => "[D8] Editor IsTextPredictionEnabled works";
+
+		[Test]
+		public void Issue18753Test()
+		{
+			App.WaitForElement("WaitForStubControl");
+
+			// 1. Enter the string 'hi my name is james' exactly (case-sensitive).
+			App.EnterText("EditorIsTextPredictionDisabled", "hi my name is james");
+			// 2. The test fails if the keyboard presents word suggestions.
+
+			if(App.IsKeyboardShown())
+			{
+				VerifyScreenshot("Issue18753IsTextPredictionDisabled");
+				App.DismissKeyboard();
+			}
+
+			// 3. Enter the string 'hi my name is james' exactly (case-sensitive).
+			App.EnterText("EditorIsTextPredictionEnabled", "hi my name is james");
+			// 4. The test fails if the keyboard does not present word suggestions.
+			
+			if (App.IsKeyboardShown())
+			{
+				VerifyScreenshot("Issue18753IsTextPredictionEnabled");
+				App.DismissKeyboard();
+			}
+		}
+	}
+}
