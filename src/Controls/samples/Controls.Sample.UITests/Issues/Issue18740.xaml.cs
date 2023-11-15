cbefd658a83983338994968672ab6e6085ff3c73
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-15 10:37:06 +0100

    Added UI Test for keyboard and focus

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue18740.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 18740, "Virtual keyboard appears with focus on Entry", PlatformAffected.Android)]
+	public partial class Issue18740 : ContentPage
+	{
+		public Issue18740()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18740.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18740 : _IssuesUITest
+	{
+		public Issue18740(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Virtual keyboard appears with focus on Entry";
+
+		[Test]
+		public async Task Issue18740Test()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.iOS, TestDevice.Mac, TestDevice.Windows });
+			
+			App.WaitForElement("WaitForStubControl");
+
+			// 1.Make sure keyboard starts out closed.
+			VerifyScreenshot("Issue18740Unfocused");
+
+			// 2. Focus the Entry.
+			App.Click("TestEntry");
+
+			await Task.Delay(500);
+
+			// 3. Verify that the virtual keyboard appears.
+			VerifyScreenshot("Issue18740Focused");
+		}
+	}
+}
