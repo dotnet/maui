8fa82e78c30b8aa34b4ef390289f22e5ac3f251c
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-20 15:46:01 +0100

    Updated test

--- src/Controls/tests/UITests/Tests/Issues/Issue18896.cs
+++ src/Controls/tests/UITests/Tests/Issues/Issue18896.cs
@@ -15,6 +15,9 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		[Test]
 		public async Task Issue18896Test()
 		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.iOS, TestDevice.Mac },
+				"Currently fails on iOS; see https://github.com/dotnet/maui/issues/18811");
+
 			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Windows },	
 				"Currently fails on Windows; see https://github.com/dotnet/maui/issues/15994");
 
