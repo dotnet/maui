962c91a481d305597a32cc3d3bc8bc7bce79d1bf
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-20 14:14:58 +0100

    Updated test

--- src/Controls/tests/UITests/Tests/Issues/Issue18896.cs
+++ src/Controls/tests/UITests/Tests/Issues/Issue18896.cs
@@ -13,9 +13,15 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		public override string Issue => "Can scroll ListView inside RefreshView";
 
 		[Test]
-		public void Issue18896Test()
+		public async Task Issue18896Test()
 		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Windows },	
+				"Currently fails on Windows; see https://github.com/dotnet/maui/issues/15994");
+
 			App.WaitForElement("WaitForStubControl");
+
+			// Load images
+			await Task.Delay(1000);
 
 			// The test passes if you are able to see the image, name, and location of each monkey.
 			VerifyScreenshot();
