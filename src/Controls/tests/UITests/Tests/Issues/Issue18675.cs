118473386358731d088d82395f88f62865abb54e
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-11-22 17:02:23 +0100

    Updated test

--- src/Controls/tests/UITests/Tests/Issues/Issue18675.cs
+++ src/Controls/tests/UITests/Tests/Issues/Issue18675.cs
@@ -13,12 +13,15 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		public override string Issue => "Editor IsReadOnly property prevent from modifying the text";
 
 		[Test]
-		public void EditorIsReadOnlyPreventModify()
+		public async Task EditorIsReadOnlyPreventModify()
 		{
 			App.WaitForElement("WaitForStubControl");
 
 			// 1.The test fails if the placeholder text in the editor below is missing.
 			App.Click("IsReadOnlyEditor");
+
+			// Wait to remove the selected underline color in the Editor.
+			await Task.Delay(1000);
 
 			// 2. The test fails if the placeholder text in the editor below is not blue.
 			VerifyScreenshot();
