d2f73d097aee1f74a2aa309ae12254120d87362f
Shane Neuville <shneuvil@microsoft.com>
2023-11-22 11:12:43 -0600

    - add test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue18457.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 18457, "Adding/Removing Pages From Removed TabbedPage Causes Crash")]
+public class Issue18457 : TestContentPage
+{
+    bool pagePushed = false;
+    TabbedPage _tabbedPage = new TabbedPage();
+	protected override void Init()
+	{
+        _tabbedPage.Children.Add(new ContentPage() { Title = "tab 1"});
+        _tabbedPage.Children.Add(new ContentPage() { Title = "tab 2"});
+        _tabbedPage.Children.Add(new ContentPage() { Title = "tab 3"});
+	}
+
+	protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+	{
+		base.OnNavigatedTo(args);
+
+        if (!pagePushed)
+        {
+            pagePushed = true;
+            await Navigation.PushAsync(_tabbedPage);
+            await Navigation.PopAsync();
+            _tabbedPage.Children.Add(new ContentPage());
+            await Task.Yield();
+            Content = new VerticalStackLayout(){
+                Children =
+                {
+                    new Label() { Text = "This test pushes and pops a TabbedPage, and then modifies the Children on the popped page."},
+                    new Label() { Text = "If the app doesn't crash, this test has passed.", AutomationId = "Success" }
+                }
+            };
+        }
+	}
+}
