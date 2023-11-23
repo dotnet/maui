30c9a0817a63a9454455524995f58ee65cd45a0c
Mike Parker <mikeparker104>
2023-11-23 16:06:04 +0000

    Fix issue 18111

--- src/Core/src/Platform/iOS/SliderExtensions.cs
+++ src/Core/src/Platform/iOS/SliderExtensions.cs
@@ -32,10 +32,7 @@ namespace Microsoft.Maui.Platform
 		public static void UpdateMaximumTrackColor(this UISlider uiSlider, ISlider slider)
 		{
 			if (slider.MaximumTrackColor != null)
-			{
-				if (uiSlider.TraitCollection.UserInterfaceIdiom != UIUserInterfaceIdiom.Mac)
-					uiSlider.MaximumTrackTintColor = slider.MaximumTrackColor.ToPlatform();
-			}
+				uiSlider.MaximumTrackTintColor = slider.MaximumTrackColor.ToPlatform();
 		}
 
 		public static void UpdateThumbColor(this UISlider uiSlider, ISlider slider)
