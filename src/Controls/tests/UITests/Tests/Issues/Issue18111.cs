7c50444ed9ac0af031883b02062b175cc30b276f
Mike Parker <mikeparker104>
2023-11-30 12:16:44 +0000

    Added UI test for iOS and Mac Catalyst

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18111.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18111 : _IssuesUITest
+	{
+		public Issue18111(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Setting MaximumTrackColor on Slider has no effect";
+
+		[Test]
+		public void SettingMaximumTrackColorOnSliderWorks()
+		{
+			App.WaitForElement("WaitForSliderControl");
+			VerifyScreenshot();
+		}
+	}
+}
