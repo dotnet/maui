5817e95bff6dfda17fda102d390c2ac5bd8989de
lytico <lytico@users.sourceforge.net>
2023-12-01 19:30:41 +0100

    [Gtk] Core.csproj GraphicsViewHandler.Gtk.cs, PlatformTouchGraphicsView.cs: introduce GraphicsViewExtensions

---
+++ src/Core/src/Platform/Gtk/GraphicsViewExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui.Graphics.Platform;
+
+namespace Microsoft.Maui.Platform
+{
+	public static class GraphicsViewExtensions
+	{
+		public static void UpdateDrawable(this PlatformTouchGraphicsView PlatformGraphicsView, IGraphicsView graphicsView)
+		{
+			PlatformGraphicsView.Drawable = graphicsView.Drawable;
+		}
+	}
+}
