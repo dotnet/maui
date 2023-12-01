f45aaaa07f1bec73e41b01862de531cea4ea5218
lytico <lytico@users.sourceforge.net>
2023-12-01 19:29:10 +0100

    [Gtk] Core.csproj GraphicsViewHandler.Gtk.cs, PlatformTouchGraphicsView.cs: implement IGraphicsView - Interactions (first draft)

--- src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.Gtk.cs
+++ src/Core/src/Handlers/GraphicsView/GraphicsViewHandler.Gtk.cs
@@ -5,22 +5,20 @@ namespace Microsoft.Maui.Handlers
 
 	public partial class GraphicsViewHandler : ViewHandler<IGraphicsView, PlatformTouchGraphicsView>
 	{
-
 		protected override PlatformTouchGraphicsView CreatePlatformView() => new();
 
 		public static void MapDrawable(IGraphicsViewHandler handler, IGraphicsView graphicsView)
 		{
-			if (handler.PlatformView is { } nativeView)
-			{
-				nativeView.Drawable = graphicsView.Drawable;
-			}
+			handler.PlatformView?.UpdateDrawable(graphicsView);
 		}
 
 		[MissingMapper]
 		public static void MapFlowDirection(IGraphicsViewHandler handler, IGraphicsView graphicsView) { }
 
-		[MissingMapper]
-		public static void MapInvalidate(IGraphicsViewHandler handler, IGraphicsView graphicsView, object? arg) { }
+		public static void MapInvalidate(IGraphicsViewHandler handler, IGraphicsView graphicsView, object? arg)
+		{
+			handler.PlatformView?.QueueDraw();
+		}
 	}
 
 }
--- src/Core/src/Platform/Gtk/PlatformTouchGraphicsView.cs
+++ src/Core/src/Platform/Gtk/PlatformTouchGraphicsView.cs
@@ -1,10 +1,95 @@
+using Gdk;
+using Gtk;
+using Microsoft.Maui.Graphics;
+
 namespace Microsoft.Maui.Platform;
 
 public class PlatformTouchGraphicsView : Microsoft.Maui.Graphics.Platform.Gtk.GtkGraphicsView
 {
+	IGraphicsView? _graphicsView;
+	bool _isTouching;
+	bool _isInBounds;
 
-	public void Connect(IGraphicsView graphicsView) { }
+	PointF[] GetViewPoints(EventButton e)
+	{
+		return new[] { new PointF((float)e.X, (float)e.Y) };
+	}
 
-	public void Disconnect() { }
+	PointF[] GetViewPoints(EventMotion e)
+	{
+		return new[] { new PointF((float)e.X, (float)e.Y) };
+	}
 
+	PointF[] GetViewPoints(EventCrossing e)
+	{
+		return new[] { new PointF((float)e.X, (float)e.Y) };
+	}
+
+
+	protected override bool OnButtonPressEvent(EventButton e)
+	{
+		_isTouching = true;
+		_graphicsView?.StartInteraction(GetViewPoints(e));
+		return base.OnButtonPressEvent(e);
+	}
+
+	protected override bool OnButtonReleaseEvent(EventButton e)
+	{
+		_isTouching = false;
+		_graphicsView?.EndInteraction(GetViewPoints(e), _isInBounds);
+
+		return base.OnButtonReleaseEvent(e);
+	}
+
+	protected override bool OnTouchEvent(Event evnt)
+	{
+		_isTouching = true;
+		return base.OnTouchEvent(evnt);
+	}
+
+	protected override bool OnEnterNotifyEvent(EventCrossing e)
+	{
+		_isInBounds = true;
+		_graphicsView?.StartHoverInteraction(GetViewPoints(e));
+		return base.OnEnterNotifyEvent(e);
+	}
+
+	protected override bool OnMotionNotifyEvent(EventMotion e)
+	{
+		_graphicsView?.MoveHoverInteraction(GetViewPoints(e));
+		return base.OnMotionNotifyEvent(e);
+	}
+
+	protected override bool OnLeaveNotifyEvent(EventCrossing e)
+	{
+		_isInBounds = false;
+
+		_graphicsView?.EndHoverInteraction();
+
+		if (_isTouching)
+		{
+			_isTouching = false;
+			_graphicsView?.EndInteraction(GetViewPoints(e), _isInBounds);
+		}
+
+		return base.OnLeaveNotifyEvent(e);
+	}
+
+	public void Connect(IGraphicsView graphicsView)
+	{
+		_graphicsView = graphicsView;
+		Events |= EventMask.ButtonPressMask | EventMask.ButtonReleaseMask | EventMask.PointerMotionMask |
+		          EventMask.ButtonMotionMask | EventMask.LeaveNotifyMask | EventMask.EnterNotifyMask
+			;
+	}
+
+	public void Disconnect()
+	{
+		_graphicsView = null;
+	}
+
+	public void UpdateDrawable(IGraphicsView graphicsView)
+	{
+		Drawable = graphicsView.Drawable;
+	}
 }
