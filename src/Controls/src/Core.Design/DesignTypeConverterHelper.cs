8e67e6ca47b55cb7b6ddaebded726057dbd9ab6f
ET <48451158+etvorun@users.noreply.github.com>
2023-12-07 09:30:32 -0800

    Add design time converters for a handful of types special handled by VS XAML intellisense

---
+++ src/Controls/src/Core.Design/DesignTypeConverterHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Linq;
+
+namespace Controls.Core.Design
+{
+	internal static class DesignTypeConverterHelper
+	{
+		/// <summary>
+		/// Returns count of numbers in the string. Returns null if some of the values are invalid or total count exceeds max count.
+		/// </summary>
+		public static int? TryParseNumbers(string numberCollection, char separator, int maxCount)
+		{
+			// Examples: "1,2" or "1 2 3 4". 
+			if (string.IsNullOrEmpty(numberCollection))
+				return null;
+
+			string[] parts = numberCollection.Split(separator);
+			if (parts.Length > maxCount)
+				return null; // too many numbers
+
+			if (parts.All(p => double.TryParse(p, NumberStyles.Number, CultureInfo.InvariantCulture, out _)))
+				return parts.Length; // all numbers are valid
+
+			return null;
+		}
+	}
+}
---
+++ src/Controls/src/Core.Design/VisibilityDesignTypeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Microsoft.Maui.Controls.Design
+{
+	public class VisibilityDesignTypeConverter : StringConverter
+	{
+		private static readonly string[] validValues = new[] { "Collapse", "Hidden", bool.FalseString, bool.TrueString, "Visible" };
+		private static readonly StandardValuesCollection standardValues = new StandardValuesCollection(validValues);
+
+		public override bool GetStandardValuesExclusive(ITypeDescriptorContext context) => true;
+		override public bool GetStandardValuesSupported(ITypeDescriptorContext context) => true;
+		override public StandardValuesCollection GetStandardValues(ITypeDescriptorContext context) => standardValues;
+		public override bool IsValid(ITypeDescriptorContext context, object value)
+		{
+			// MUST MATCH VisibilityConverter.ConvertFrom
+			if (value?.ToString()?.Trim() is string strValue)
+				return validValues.Contains(strValue, StringComparer.OrdinalIgnoreCase);
+
+			return false;
+		}
+	}
+}
