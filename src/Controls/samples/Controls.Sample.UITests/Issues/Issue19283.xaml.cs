a4e38fa1d29e3b8ea506cd3fffb016bbb8cae4fb
Shane Neuville <shneuvil@microsoft.com>
2023-12-07 04:57:23 -0600

    - add test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue19283.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 19283, "PointerOver VSM Breaks Button", PlatformAffected.iOS)]
+	public partial class Issue19283 : ContentPage
+	{
+		public Issue19283()
+		{
+			InitializeComponent();
+
+			btn.Clicked += (sender, args) => 
+			{ 
+				layout.Children.Add(new Label { Text = "Success", AutomationId = "Success" });
+			};
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19283.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19283 : _IssuesUITest
+	{
+		public Issue19283(TestDevice device) : base(device) { }
+
+		public override string Issue => "PointerOver VSM Breaks Button";
+
+		[Test]
+		public void ButtonStillWorksWhenItHasPointerOverVSMSet()
+		{
+			App.Click("btn");
+			App.WaitForElement("Success");
+		}
+	}
+}
