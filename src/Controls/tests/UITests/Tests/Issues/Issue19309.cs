869ceac57842227bef26b53518c011a547bf8370
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-12-11 18:26:37 +0100

    Added UITest

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19309.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19309 : _IssuesUITest
+	{
+		public Issue19309(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Nightly build 9725 broke boxviews do not get their color set anymore";
+
+		[Test]
+		public async Task EditorIsReadOnlyPreventModify()
+		{
+			App.WaitForElement("WaitForStubControl");
+
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			await Task.Yield();
+
+			App.Screenshot("All BoxViews must render.");
+		}
+	}
+}
