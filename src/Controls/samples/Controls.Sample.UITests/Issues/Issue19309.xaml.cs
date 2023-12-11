adfa663cbe063ad3bd0ef2695b76228316f6ec78
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-12-11 13:46:08 +0100

    Added repro sample

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue19309.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 19309, "Nightly build 9725 broke boxviews do not get their color set anymore", PlatformAffected.macOS | PlatformAffected.iOS)]
+	public partial class Issue19309 : ContentPage
+	{
+		public Issue19309()
+		{
+			InitializeComponent();
+		}
+	}
+}
