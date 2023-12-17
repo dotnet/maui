d5faa4d5075c10a1b838295c8d0ae3b0949d300f
Shane Neuville <shneuvil@microsoft.com>
2023-12-16 20:37:14 -0600

    Fix some UI Test timings (#19462)

--- src/Controls/tests/UITests/Tests/DragAndDropUITests.cs
+++ src/Controls/tests/UITests/Tests/DragAndDropUITests.cs
@@ -57,6 +57,7 @@ namespace Microsoft.Maui.AppiumTests
 			App.EnterText("TargetView", "DragAndDropBetweenLayouts");
 			App.Click("GoButton");
 
+			App.WaitForElement("ResetButton");
 			App.Click("ResetButton");
 
 			App.WaitForElement("Red");
