493253193835de114ae983ed25349fcabf87d13a
Javier Suárez Ruiz <jsuarez@microsoft.com>
2023-12-19 19:04:59 +0100

    Fix the issue

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19465.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19465 : _IssuesUITest
+	{
+		public Issue19465(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Double tap gesture NullReferenceException when navigating";
+
+		[Test]
+		public void Issue19465Test()
+		{
+			App.WaitForElement("FirstButton");
+			App.Click("FirstButton");
+
+			App.WaitForElement("SecondLabel");
+			App.DoubleClick("SecondLabel");
+
+			App.WaitForElement("ThirdButton");
+			App.DoubleClick("ThirdButton");
+		}
+	}
+}
