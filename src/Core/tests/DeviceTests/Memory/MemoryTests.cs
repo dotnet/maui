fe8a2e32ca106fb518415dc18ab905f24cfe34c4
E.Z. Hart <hartez@gmail.com>
2023-12-22 09:18:14 -0700

    Simplify handler GC tests

--- src/Core/tests/DeviceTests/Memory/MemoryTests.cs
+++ src/Core/tests/DeviceTests/Memory/MemoryTests.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Maui.DeviceTests;
-using Microsoft.Maui.DeviceTests.Stubs;
-using Microsoft.Maui.Graphics;
-using Microsoft.Maui.Media;
 using Xunit;
-using Xunit.Abstractions;
-using Xunit.Sdk;
 using static Microsoft.Maui.DeviceTests.AssertHelpers;
-
 
 namespace Microsoft.Maui.Handlers.Memory
 {
@@ -35,52 +23,41 @@ namespace Microsoft.Maui.Handlers.Memory
 			_fixture = fixture;
 		}
 
-		[Theory]
-		[ClassData(typeof(MemoryTestTypes))]
-		public async Task Allocate((Type ViewType, Type HandlerType) data)
+		async Task Allocate((Type ViewType, Type HandlerType) data)
 		{
-
-#if ANDROID
-			if (!OperatingSystem.IsAndroidVersionAtLeast(30))
-				return;
-#endif
-
 			var handler = await InvokeOnMainThreadAsync(() => CreateHandler((IElement)Activator.CreateInstance(data.ViewType), data.HandlerType));
-			WeakReference weakHandler = new WeakReference(handler);
-			_fixture.AddReferences(data.HandlerType, (weakHandler, new WeakReference(handler.VirtualView)));
-			handler = null;
+			var view = handler.VirtualView;
 
-			GC.Collect();
-			GC.WaitForPendingFinalizers();
-			GC.Collect();
-			GC.WaitForPendingFinalizers();
+			WeakReference weakHandlerReference = new WeakReference(handler);
+			WeakReference weakViewReference = new WeakReference(view);
+
+			_fixture.AddReferences(data.HandlerType, (weakHandlerReference, weakViewReference));
 		}
 
 		[Theory]
 		[ClassData(typeof(MemoryTestTypes))]
 		public async Task CheckAllocation((Type ViewType, Type HandlerType) data)
 		{
+			// Arrange
+			await Allocate(data);
 
-#if ANDROID
-			if (!OperatingSystem.IsAndroidVersionAtLeast(30))
-				return;
-#endif
-
-			// This is mainly relevant when running inside the visual runner as a single test
-			if (!_fixture.HasType(data.HandlerType))
-				await Allocate(data);
-
-			bool referencesCollected()
+			async Task<bool> referencesCollected()
 			{
-				GC.Collect();
-				GC.WaitForPendingFinalizers();
-				GC.Collect();
-				GC.WaitForPendingFinalizers();
+				// Act
+				await Collect();
 
+				// Assert
 				return !_fixture.DoReferencesStillExist(data.HandlerType);
 			}
 
 			await AssertEventually(referencesCollected, timeout: 5000, message: $"{data.HandlerType} failed to collect.");
+		}
+
+		static async Task Collect()
+		{
+			await Task.Yield();
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
 		}
 	}
 }
--- src/TestUtils/src/DeviceTests/AssertHelpers.cs
+++ src/TestUtils/src/DeviceTests/AssertHelpers.cs
@@ -27,6 +27,27 @@ namespace Microsoft.Maui.DeviceTests
 			}
 		}
 
+		public static async Task AssertEventually(Func<Task<bool>> assertion, int timeout = 1000, int interval = 100, string message = "Assertion timed out")
+		{
+			do
+			{
+				if (await assertion())
+				{
+					return;
+				}
+
+				await Task.Delay(interval);
+				timeout -= interval;
+
+			}
+			while (timeout >= 0);
+
+			if (! (await assertion()))
+			{
+				throw new XunitException(message);
+			}
+		}
+
 		public static async Task<bool> Wait(Func<bool> predicate, int timeout = 1000, int interval = 100) 
 		{
 			do
