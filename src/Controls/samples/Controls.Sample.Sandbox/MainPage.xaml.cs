008627eec845f059cbd4eba4394f59a04e943417
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-01-10 22:03:59 +0100

    Sandbox demo for easy testing

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,18 +1,26 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Maui;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Graphics;
 
 namespace Maui.Controls.Sample
 {
 	public partial class MainPage : ContentPage
 	{
+		public Command ClickCommand { get; }
+
 		public MainPage()
 		{
+			ClickCommand = new(execute: () =>
+			{
+				DisplayAlert("Title", "Button 1 clicked", "Cancel");
+			});
+
 			InitializeComponent();
+			BindingContext = this;
+		}
+
+		private void button2_Clicked(object sender, EventArgs e)
+		{
+			DisplayAlert("Title", "Button 2 clicked", "Cancel");
 		}
 	}
 }
