aeb9612c86c71cbed053596464f60d18c1a701f5
mdh1418 <mitchhwang1418@gmail.com>
2024-01-11 19:29:49 -0500

    Revert "Remove ImageSourceServiceProvider"
    This reverts commit cb0c1158a93c00ab0c9d082773817359c9729e22.

---
+++ src/Core/src/Hosting/ImageSources/ImageSourceServiceProvider.cs
@@ -0,0 +1,61 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Concurrent;
+using Microsoft.Maui.Hosting.Internal;
+
+namespace Microsoft.Maui.Hosting
+{
+	sealed class ImageSourceServiceProvider : MauiFactory, IImageSourceServiceProvider
+	{
+		static readonly string ImageSourceInterface = typeof(IImageSource).FullName!;
+		static readonly Type ImageSourceServiceType = typeof(IImageSourceService<>);
+
+		readonly ConcurrentDictionary<Type, Type> _imageSourceCache = new ConcurrentDictionary<Type, Type>();
+		readonly ConcurrentDictionary<Type, Type> _serviceCache = new ConcurrentDictionary<Type, Type>();
+
+		public ImageSourceServiceProvider(IImageSourceServiceCollection collection, IServiceProvider hostServiceProvider)
+			: base(collection)
+		{
+			HostServiceProvider = hostServiceProvider;
+		}
+
+		public IServiceProvider HostServiceProvider { get; }
+
+		public IImageSourceService? GetImageSourceService(Type imageSource) =>
+			(IImageSourceService?)GetService(GetImageSourceServiceType(imageSource));
+
+		public Type GetImageSourceServiceType(Type imageSource) =>
+			_serviceCache.GetOrAdd(imageSource, type =>
+			{
+				var genericConcreteType = ImageSourceServiceType.MakeGenericType(type);
+
+				if (genericConcreteType != null && GetServiceDescriptor(genericConcreteType) != null)
+					return genericConcreteType;
+
+				return ImageSourceServiceType.MakeGenericType(GetImageSourceType(type));
+			});
+
+		public Type GetImageSourceType(Type imageSource) =>
+			_imageSourceCache.GetOrAdd(imageSource, CreateImageSourceTypeCacheEntry);
+
+		Type CreateImageSourceTypeCacheEntry(Type type)
+		{
+			if (type.IsInterface)
+			{
+				if (type.GetInterface(ImageSourceInterface) != null)
+					return type;
+			}
+			else
+			{
+				foreach (var directInterface in type.GetInterfaces())
+				{
+					if (directInterface.GetInterface(ImageSourceInterface) != null)
+						return directInterface;
+				}
+			}
+
+			throw new InvalidOperationException($"Unable to find the image source type because none of the interfaces on {type.Name} were derived from {nameof(IImageSource)}.");
+		}
+	}
+}
