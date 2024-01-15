9707aa2c0fabad9cc01b83e6e2eb67725901460d
javiersuarezruiz@hotmail.com <javiersuarezruiz@hotmail.com>
2024-01-15 16:42:57 +0100

    Added device test

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue17865.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	internal class Issue17865Model
+	{
+		public Issue17865Model(int itemIndex)
+		{
+			ItemText = $"Item #{itemIndex}";
+		}
+
+		public string ItemText { get; }
+
+	}
+
+	internal class Issue17865ViewModel : BindableObject
+	{
+		ObservableCollection<Issue17865Model> items = new();
+
+		public Issue17865ViewModel()
+		{
+			Populate();
+		}
+
+		void Populate()
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				items.Add(new Issue17865Model(i));
+			}
+		}
+
+		public ObservableCollection<Issue17865Model> Items => items;
+	}
+
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 17865, "CollectionView throws NRE when ScrollTo method is called from a handler of event Window.Created", PlatformAffected.UWP)]
+	public partial class Issue17865 : ContentPage
+	{
+		readonly Issue17865ViewModel _viewModel;
+
+		public Issue17865()
+		{
+			InitializeComponent();
+
+			BindingContext = _viewModel = new Issue17865ViewModel();
+			Instance = this;
+		}
+
+		public void RevealLastItem()
+		{
+			var item = _viewModel.Items.Last();
+			collectionView.ScrollTo(item, null, ScrollToPosition.MakeVisible, false);
+		}
+
+		public static Issue17865 Instance { get; private set; }
+
+		private void OnButtonClicked(object sender, EventArgs e)
+		{
+			RevealLastItem();
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17865.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue17865 : _IssuesUITest
+	{
+		public Issue17865(TestDevice device) : base(device) { }
+
+		public override string Issue => "CollectionView throws NRE when ScrollTo method is called from a handler of event Window.Created";
+
+		[Test]
+		public void Issue17865Test()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.iOS, TestDevice.Mac },
+				"Windows Test.");
+
+			App.WaitForElement("WaitForStubControl");
+
+			App.Click("WaitForStubControl");
+
+			// NOTE: Without crashes the test has passed.
+		}
+	}
+}
