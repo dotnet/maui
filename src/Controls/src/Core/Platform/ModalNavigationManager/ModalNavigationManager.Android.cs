a2526ea2fb86624f1739caafe7acb37c27e6d671
PavloLukianets <109070173+PavloLukianets@users.noreply.github.com>
2024-01-15 16:47:25 +0200

    Fix modal page push in immersive mode

--- src/Controls/src/Core/Platform/ModalNavigationManager/ModalNavigationManager.Android.cs
+++ src/Controls/src/Core/Platform/ModalNavigationManager/ModalNavigationManager.Android.cs
@@ -149,8 +149,16 @@ namespace Microsoft.Maui.Controls.Platform
 			NavAnimationInProgress = true;
 			if (animated)
 			{
-				modalContainer.TranslationY = GetModalParentView().Height;
-				modalContainer?.Animate()?.TranslationY(0)?.SetInterpolator(new DecelerateInterpolator(1))?.SetDuration(300)?.SetListener(new GenericAnimatorListener
+				var translationValue = 0;
+				var windowInsets = ViewCompat.GetRootWindowInsets(parentView);
+				if (windowInsets is not null && !windowInsets.IsVisible(WindowInsetsCompat.Type.StatusBars()))
+				{
+					var statusBarHeight = _window.PlatformActivity.GetStatusBarHeight();
+					translationValue -= statusBarHeight;
+				}
+
+				modalContainer.TranslationY = parentView.Height;
+				modalContainer?.Animate()?.TranslationY(translationValue)?.SetInterpolator(new DecelerateInterpolator(1))?.SetDuration(300)?.SetListener(new GenericAnimatorListener
 				{
 					OnEnd = a =>
 					{
