ea7f3b7ca9c8c992e2d6890358820c0aecd8f280
Ivan Povazan <ivan.povazan@gmail.com>
2024-01-16 13:49:00 +0100

    Add NativeAOT RunOniOS test

--- src/TestUtils/src/Microsoft.Maui.IntegrationTests/AppleTemplateTests.cs
+++ src/TestUtils/src/Microsoft.Maui.IntegrationTests/AppleTemplateTests.cs
@@ -50,5 +50,29 @@ namespace Microsoft.Maui.IntegrationTests
 				$"Project {Path.GetFileName(projectFile)} failed to run. Check test output/attachments for errors.");
 		}
 
+		[Test]
+		[TestCase("maui", "Release", DotNetCurrent, "iossimulator-x64")]
+		public void RunOniOSNativeAOT(string id, string config, string framework, string runtimeIdentifier)
+		{
+			var projectDir = TestDirectory;
+			var projectFile = Path.Combine(projectDir, $"{Path.GetFileName(projectDir)}.csproj");
+
+			Assert.IsTrue(DotnetInternal.New(id, projectDir, framework),
+				$"Unable to create template {id}. Check test output for errors.");
+
+			var extendedBuildProps = BuildProps;
+			extendedBuildProps.Add("PublishAot=true");
+			extendedBuildProps.Add("PublishAotUsingRuntimePack=true"); // TODO: This parameter will become obsolete https://github.com/dotnet/runtime/issues/87060
+			extendedBuildProps.Add("_IsPublishing=true"); // using dotnet build with -p:_IsPublishing=true enables targeting simulators
+			extendedBuildProps.Add($"RuntimeIdentifier={runtimeIdentifier}");
+
+			Assert.IsTrue(DotnetInternal.Build(projectFile, config, framework: $"{framework}-ios", properties: BuildProps),
+				$"Project {Path.GetFileName(projectFile)} failed to build. Check test output/attachments for errors.");
+
+			var appFile = Path.Combine(projectDir, "bin", config, $"{framework}-ios", runtimeIdentifier, $"{Path.GetFileName(projectDir)}.app");
+
+			Assert.IsTrue(XHarness.RunAppleForTimeout(appFile, Path.Combine(projectDir, "xh-results"), TestSimulator.XHarnessID),
+				$"Project {Path.GetFileName(projectFile)} failed to run. Check test output/attachments for errors.");
+		}
 	}
 }
