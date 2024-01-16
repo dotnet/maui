2c9ac46bd163581566b6168f7dd8d28427e2749d
Shane Neuville <shneuvil@microsoft.com>
2024-01-16 13:30:39 -0600

    - fix tests

--- src/Controls/tests/DeviceTests/Elements/Shell/ShellFlyoutTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/Shell/ShellFlyoutTests.Android.cs
@@ -14,24 +14,21 @@ namespace Microsoft.Maui.DeviceTests
 			await RunShellTest(shell =>
 			{
 				shell.FlyoutContent = new VerticalStackLayout() { new Label() { Text = "Flyout Content" } };
-
-				// 1. Set FlyoutIsPresented=true to make the Shell Flyout visible.
-				shell.FlyoutIsPresented = true;
 			},
 			async (shell, handler) =>
 			{
-				await Task.Delay(100);
+				// 1. Set FlyoutIsPresented=true to make the Shell Flyout visible.
+				shell.FlyoutIsPresented = true;
 
 				var dl = GetDrawerLayout(handler) as DrawerLayout;
 				Assert.NotNull(dl);
 
-				// 2. Check that the Flyout has size.
-				var flyoutFrame = GetFlyoutFrame(handler);
-				Assert.True(flyoutFrame.Width > 0);
-				Assert.True(flyoutFrame.Height > 0);
-
-				// 3. Check that the Flyout status. It must be open.
-				Assert.True(dl.IsOpen);
+				await AssertionExtensions.AssertEventually(() =>
+				{
+					// 2. Check that the Flyout has size.
+					var flyoutFrame = GetFlyoutFrame(handler);
+					return flyoutFrame.Width > 0 && flyoutFrame.Height > 0 && dl.IsOpen;
+				});
 			});
 		}
 	}
