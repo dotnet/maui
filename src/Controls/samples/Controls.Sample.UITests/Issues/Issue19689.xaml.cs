05849a1707f86e68ab250ae0cde407e9a65bbaa1
javiersuarezruiz@hotmail.com <javiersuarezruiz@hotmail.com>
2024-01-16 10:00:11 +0100

    Added UITest

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue19689.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 19689, "Java.Lang.IndexOutOfBoundsException: setSpan ( ... ) ends beyond length", PlatformAffected.Android)]
+	public partial class Issue19689 : ContentPage
+	{
+		string _text = string.Empty;
+
+		public Issue19689()
+		{
+			InitializeComponent();
+			
+			BindingContext = this;
+
+			Text = "Bla";
+		}
+
+		public string Text
+		{
+			get => _text;
+			set
+			{
+				if (value != _text)
+				{
+					_text = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19689.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19689 : _IssuesUITest
+	{
+		public Issue19689(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Java.Lang.IndexOutOfBoundsException: setSpan ( ... ) ends beyond length";
+
+		[Test]
+		public void Issue19689Test()
+		{
+			var entry = App.FindElement("TestEntry");
+			Assert.IsNotNull(entry);
+		}
+	}
+}
