8807e1827c0172ab98b40f6066da91f052fbf9c5
Edward Miller <symbiogenisis@outlook.com>
2024-01-18 09:49:23 -0600

    add benchmark

---
+++ src/Core/tests/Benchmarks/Benchmarks/ClampBenchmarker.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Maui.Graphics;
+
+namespace Microsoft.Maui.Benchmarks;
+
+[MemoryDiagnoser]
+public class ClampBenchmarker
+{
+	[Params(3000000)]
+	public int N;
+
+	[Benchmark]
+	public void ClampBenchmark()
+	{
+		int min = 0, max = 100;
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = min; j <= max; j++)
+			{
+				CompareExtensions.Clamp(j, min, max);
+			}
+		}
+	}
+}
