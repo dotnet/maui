a8b035f7005f36b849e3ff665d5e7e2581a3d06c
Edward Miller <symbiogenisis@outlook.com>
2024-01-19 09:12:46 -0600

    add benchmark

---
+++ src/Core/tests/Benchmarks/Benchmarks/ConverterBenchmarker.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Shapes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Microsoft.Maui.Benchmarks
+{
+	[MemoryDiagnoser]
+	public class ConverterBenchmarker
+	{
+		private const int Iterations = 10000;
+
+		[Benchmark]
+		public void CreateLotsOfConverters()
+		{
+			var converters = new List<TypeConverter>();
+
+			for (int i = 0; i < Iterations; i++)
+			{
+				converters.Add(new PointCollectionConverter());
+				converters.Add(new StrokeShapeTypeConverter());
+				converters.Add(new TransformTypeConverter());
+				converters.Add(new ColumnDefinitionCollectionTypeConverter());
+				converters.Add(new RowDefinitionCollectionTypeConverter());
+			}
+		}
+	}
+}
