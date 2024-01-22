11d2e533ac7172bdffb4290a004d9740b5e0f201
Edward Miller <symbiogenisis@outlook.com>
2024-01-22 00:31:35 -0600

    use aggressive inlining

--- src/Core/src/Graphics/CompareExtensions.cs
+++ src/Core/src/Graphics/CompareExtensions.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace Microsoft.Maui.Graphics
 {
 	internal static class CompareExtensions
 	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static T Clamp<T>(this T value, T min, T max) where T : IComparable<T>
 		{
 			if (max.CompareTo(min) < 0)
