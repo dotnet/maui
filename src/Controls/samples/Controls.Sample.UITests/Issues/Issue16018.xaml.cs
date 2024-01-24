1225b963a4bbdcf46e9afad37bf89e8688c244f8
javiersuarezruiz@hotmail.com <javiersuarezruiz@hotmail.com>
2024-01-24 15:49:06 +0100

    Added UITest

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue16018.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 16018, "WordWrap on labels with padding does not work properly on iOS", PlatformAffected.iOS)]
+	public partial class Issue16018 : ContentPage
+	{
+		public Issue16018()
+		{
+			InitializeComponent();
+		}
+
+		void OnTextChanged(object sender, TextChangedEventArgs e)
+		{
+			LabelTest.Text = EntryTest.Text;
+		}
+	}
+}
