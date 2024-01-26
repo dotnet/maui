285d58d2d9e5c761d7d731a645e53b05104c3d67
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-01-26 16:44:50 -0300

    add new keyboards for android

---
+++ src/Core/src/Primitives/DateTimeKeyboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Maui.Primitives;
+sealed class DateKeyboard : Keyboard
+{
+}
+
+
+sealed class PasswordKeyboard : Keyboard
+{
+
+}
+
+sealed class TimeKeyboard : Keyboard
+{
+
+}
--- src/Core/src/Primitives/Keyboard.cs
+++ src/Core/src/Primitives/Keyboard.cs
@@ -1,5 +1,6 @@
 #nullable enable
 using System.ComponentModel;
+using Microsoft.Maui.Primitives;
 
 namespace Microsoft.Maui
 {
@@ -22,58 +23,49 @@ namespace Microsoft.Maui
 		static Keyboard? s_telephone;
 
 		static Keyboard? s_chat;
+#if ANDROID
+		static Keyboard? s_date;
+
+		static Keyboard? s_time;
+
+		static Keyboard? s_password;
+#endif
+
 
 		internal Keyboard()
 		{
 		}
 
 		/// <include file="../../docs/Microsoft.Maui/Keyboard.xml" path="//Member[@MemberName='Plain']/Docs/*" />
-		public static Keyboard Plain
-		{
-			get { return s_plain ??= new CustomKeyboard(KeyboardFlags.None); }
-		}
+		public static Keyboard Plain => s_plain ??= new CustomKeyboard(KeyboardFlags.None);
 
 		/// <include file="../../docs/Microsoft.Maui/Keyboard.xml" path="//Member[@MemberName='Chat']/Docs/*" />
-		public static Keyboard Chat
-		{
-			get { return s_chat ??= new ChatKeyboard(); }
-		}
+		public static Keyboard Chat => s_chat ??= new ChatKeyboard();
 
 		/// <include file="../../docs/Microsoft.Maui/Keyboard.xml" path="//Member[@MemberName='Default']/Docs/*" />
-		public static Keyboard Default
-		{
-			get { return s_def ??= new Keyboard(); }
-		}
+		public static Keyboard Default => s_def ??= new Keyboard();
 
 		/// <include file="../../docs/Microsoft.Maui/Keyboard.xml" path="//Member[@MemberName='Email']/Docs/*" />
-		public static Keyboard Email
-		{
-			get { return s_email ??= new EmailKeyboard(); }
-		}
+		public static Keyboard Email => s_email ??= new EmailKeyboard();
 
 		/// <include file="../../docs/Microsoft.Maui/Keyboard.xml" path="//Member[@MemberName='Numeric']/Docs/*" />
-		public static Keyboard Numeric
-		{
-			get { return s_numeric ??= new NumericKeyboard(); }
-		}
+		public static Keyboard Numeric => s_numeric ??= new NumericKeyboard();
 
 		/// <include file="../../docs/Microsoft.Maui/Keyboard.xml" path="//Member[@MemberName='Telephone']/Docs/*" />
-		public static Keyboard Telephone
-		{
-			get { return s_telephone ??= new TelephoneKeyboard(); }
-		}
+		public static Keyboard Telephone => s_telephone ??= new TelephoneKeyboard();
 
 		/// <include file="../../docs/Microsoft.Maui/Keyboard.xml" path="//Member[@MemberName='Text']/Docs/*" />
-		public static Keyboard Text
-		{
-			get { return s_text ??= new TextKeyboard(); }
-		}
+		public static Keyboard Text => s_text ??= new TextKeyboard();
 
 		/// <include file="../../docs/Microsoft.Maui/Keyboard.xml" path="//Member[@MemberName='Url']/Docs/*" />
-		public static Keyboard Url
-		{
-			get { return s_url ??= new UrlKeyboard(); }
-		}
+		public static Keyboard Url => s_url ??= new UrlKeyboard();
+# if __ANDROID__
+		public static Keyboard Date => s_date ??= new DateKeyboard();
+
+		public static Keyboard Password => s_password ??= new PasswordKeyboard();
+
+		public static Keyboard Time => s_time ??= new TimeKeyboard();
+#endif
 
 		/// <include file="../../docs/Microsoft.Maui/Keyboard.xml" path="//Member[@MemberName='Create']/Docs/*" />
 		public static Keyboard Create(KeyboardFlags flags)
