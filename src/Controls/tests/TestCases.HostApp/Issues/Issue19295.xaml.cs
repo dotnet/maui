2c4e5ac94ebac8fd07bf14bbd8727efcb539007f
Jakub Florkowski <kubaflo123@gmail.com>
2024-01-28 14:14:43 +0100

    [iOS] Fixed auto picker resizing after selection

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue19295.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 19295, "Picker does Not Resize Automatically After Selection", PlatformAffected.iOS)]
+	public partial class Issue19295 : ContentPage
+	{
+		public Issue19295()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19295.cs
@@ -0,0 +1,33 @@
+﻿#if IOS
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using OpenQA.Selenium.Appium;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue19295 : _IssuesUITest
+	{
+		public Issue19295(TestDevice device) : base(device) { }
+
+		public override string Issue => "Picker does Not Resize Automatically After Selection";
+
+		[Test]
+		[Category(UITestCategories.Picker)]
+		public void PickerShouldResizeAfterChangingSelection()
+		{
+			App.WaitForElement("picker");
+
+			var picker = App.FindElement("picker");
+			var sizeWithFirstElementSelected = picker.GetRect().Width;
+
+			picker.Click();
+			((AppiumApp)App)?.Driver.FindElement(MobileBy.ClassName("XCUIElementTypePickerWheel")).SendKeys("Japanese Macaque");
+			var sizeWithSecondElementSelected = picker.GetRect().Width;
+
+			ClassicAssert.AreNotEqual(sizeWithFirstElementSelected, sizeWithSecondElementSelected, "Picker size did not change after selecting an item");
+		}
+	}
+}
+#endif
