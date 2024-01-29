3cf0b7fd09d4c12f32d3535b82f9aeff48e925fb
Simon Rozsival <simon@rozsival.com>
2024-01-29 18:51:38 +0100

    Split concrete and interface type mappings

--- src/Core/src/Hosting/ImageSources/ImageSourceToImageSourceServiceTypeMapping.cs
+++ src/Core/src/Hosting/ImageSources/ImageSourceToImageSourceServiceTypeMapping.cs
@@ -12,28 +12,44 @@ namespace Microsoft.Maui.Hosting
 		internal static ImageSourceToImageSourceServiceTypeMapping GetInstance(IImageSourceServiceCollection collection) =>
 			s_instances.GetOrAdd(collection, static _ => new ImageSourceToImageSourceServiceTypeMapping());
 
-		private readonly Dictionary<Type, Type> _typeMapping = new();
+		private readonly Dictionary<Type, Type> _concreteTypeMapping = new();
+		private readonly Dictionary<Type, Type> _interfaceTypeMapping = new();
 
 		public void Add<TImageSource, TImageSourceService>()
 			where TImageSource : IImageSource
 			where TImageSourceService : class, IImageSourceService<TImageSource>
 		{
-			_typeMapping[typeof(TImageSource)] = typeof(TImageSourceService);
+			if (typeof(TImageSource).IsInterface)
+			{
+				_interfaceTypeMapping[typeof(TImageSource)] = typeof(TImageSourceService);
+			}
+			else
+			{
+				_concreteTypeMapping[typeof(TImageSource)] = typeof(TImageSourceService);
+			}
 		}
 
 		public Type FindImageSourceServiceType(Type type)
 		{
 			Debug.Assert(typeof(IImageSource).IsAssignableFrom(type));
 
-			if (_typeMapping.TryGetValue(type, out var exactImageSourceService))
+			if (_concreteTypeMapping.TryGetValue(type, out var exactImageSourceService)
+				|| _interfaceTypeMapping.TryGetValue(type, out exactImageSourceService))
 			{
 				return exactImageSourceService;
 			}
 
+			return FindImageSourceServiceMapping(type, _concreteTypeMapping)
+				?? FindImageSourceServiceMapping(type, _interfaceTypeMapping)
+				?? throw new InvalidOperationException($"Unable to find a {nameof(IImageSourceService)} corresponding to {type}. Please register a service for {type}.");
+		}
+
+		private static Type? FindImageSourceServiceMapping(Type type, Dictionary<Type, Type> mapping)
+		{
 			Type? bestImageSource = null;
 			Type? bestImageSourceService = null;
 
-			foreach (var (imageSource, imageSourceService) in _typeMapping)
+			foreach (var (imageSource, imageSourceService) in mapping)
 			{
 				if (imageSource.IsAssignableFrom(type))
 				{
@@ -44,14 +60,20 @@ namespace Microsoft.Maui.Hosting
 					}
 					else if (!imageSource.IsAssignableFrom(bestImageSource))
 					{
+						// This exception can be thrown when the image source implements two interfaces that aren't derived from each other
+						// which both have a registered image source service to them.
+						// For example, consider this setup:
+						//    class MyImageSource : IStreamImageSource, IUriImageSource { ... }
+						//    
+						//    services.AddService<IStreamImageSource, StreamImageSourceService>();
+						//    services.AddService<IUriImageSource, UriImageSourceService>();
+						//
+						// 	  var imageSourceService = provider.GetImageSourceService(typeof(MyImageSource));
+						//    ambiguous match: both StreamImageSourceService and UriImageSourceService are registered for MyImageSource
+						
 						throw new InvalidOperationException($"Unable to find a single {nameof(IImageSourceService)} corresponding to {type}. There is an ambiguous match between {bestImageSourceService} ({bestImageSource}) and {imageSourceService} ({imageSource}).");
 					}
 				}
-			}
-
-			if (bestImageSourceService is null)
-			{
-				throw new InvalidOperationException($"Unable to find any configured {nameof(IImageSourceService)} corresponding to {type}.");
 			}
 
 			return bestImageSourceService;
