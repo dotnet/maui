b6da0e8f2c9f8affd61e52f077be067d87ddf0e8
Shane Neuville <shneuvil@microsoft.com>
2024-01-30 17:49:52 -0600

    - add tests for margin and desired size

--- src/Controls/tests/DeviceTests/Elements/View/ViewTests.cs
+++ src/Controls/tests/DeviceTests/Elements/View/ViewTests.cs
@@ -20,5 +20,44 @@ namespace Microsoft.Maui.DeviceTests
 	[Category(TestCategory.View)]
 	public partial class ViewTests : ControlsHandlerTestBase
 	{
+		[Theory]
+		[ClassData(typeof(TestCases.ControlsViewTypesTestCases))]
+		public async Task ViewWithMarginSetsFrameAndDesiredSizeCorrectly(Type controlType)
+		{
+			EnsureHandlerCreated(TestCases.ControlsViewTypesTestCases.Setup);
+
+			var control = Activator.CreateInstance(controlType) as View;
+			control.Margin = new Thickness(5, 5, 5, 5);
+			control.HeightRequest = 50;
+			control.WidthRequest = 50;
+
+			IView layout = new VerticalStackLayout()
+			{
+				Children =
+				{
+					control
+				}
+			};
+
+			await AttachAndRun<LayoutHandler>(layout, async (_) =>
+			{
+				layout.Arrange(new Graphics.Rect(Graphics.Point.Zero, layout.Measure(100, 100)));
+				await Task.Yield();
+				layout.Arrange(new Graphics.Rect(Graphics.Point.Zero, layout.Measure(100, 100)));
+				await Task.Yield();
+
+				var frame = control.Frame;
+				var desiredSize = control.DesiredSize;
+
+				Assert.Equal(5, frame.X, 0.5d);
+				Assert.Equal(5, frame.Y, 0.5d);
+				Assert.Equal(50, frame.Width, 0.5d);
+				Assert.Equal(50, frame.Height, 0.5d);
+				Assert.Equal(55, frame.Right, 0.5d);
+				Assert.Equal(55, frame.Bottom, 0.5d);
+				Assert.Equal(60, desiredSize.Width, 0.5d);
+				Assert.Equal(60, desiredSize.Height, 0.5d);
+			});
+		}
 	}
 }
