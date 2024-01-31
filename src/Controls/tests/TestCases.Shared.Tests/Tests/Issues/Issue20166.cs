3aa222fbb65a8958f6f78f2e8fa31acaf1856f05
Jakub Florkowski <kubaflo123@gmail.com>
2024-01-31 01:26:14 +0100

    Added a UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20166.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue20166 : _IssuesUITest
+	{
+		public Issue20166(TestDevice device) : base(device) { }
+
+		public override string Issue => "Custom FlyoutIcon visible although FlyoutBehavior set to disabled";
+
+		[Test]
+		public void ShouldHideCustomFlyoutIconWhenNavigatingToPageWithDisabledFlyout()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows });
+
+			// Click button 1 to switch to the page with disabled flyout
+			App.WaitForElement("button1");
+			App.Click("button1");
+
+			// 2. Verify that the flyout icon is not rendered.
+			VerifyScreenshot();
+		}
+	}
+}
