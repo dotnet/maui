3137f89ddbb0224da3bab089ac6b6b8e7da9f00e
mdh1418 <mitchhwang1418@gmail.com>
2024-02-01 13:24:29 -0500

    [Core][UnitTests] Add more handler test scenarios

--- src/Core/tests/UnitTests/Hosting/HostBuilderHandlerTests.cs
+++ src/Core/tests/UnitTests/Hosting/HostBuilderHandlerTests.cs
@@ -94,5 +94,161 @@ namespace Microsoft.Maui.UnitTests.Hosting
 
 			Assert.Throws<HandlerNotFoundException>(() => mauiHandlersFactory.GetHandler(typeof(ViewStub)));
 		}
+
+		[Theory]
+		[InlineData(typeof(IViewStub))]
+		[InlineData(typeof(IMyBaseViewStub))]
+		[InlineData(typeof(IMyDerivedViewStub))]
+		public void HostBuilderResolvesHandlerRegisteredUnderBaseInterfaceType(Type baseInterfaceType)
+		{
+			var mauiApp = MauiApp.CreateBuilder()
+				.ConfigureMauiHandlers(handlers => handlers.AddHandler(baseInterfaceType, typeof(ViewHandlerStub)))
+				.Build();
+
+			var mauiHandlersFactory = mauiApp.Services.GetRequiredService<IMauiHandlersFactory>();
+
+			var handlerService = mauiHandlersFactory.GetHandler(typeof(MyDerivedViewStub));
+
+			Assert.NotNull(handlerService);
+			Assert.IsType<ViewHandlerStub>(handlerService);
+		}
+
+		[Fact]
+		public void HostBuilderResolvesToHandlerRegisteredUnderMostDerivedBaseInterfaceType()
+		{
+			var mostDerivedInterfaceHandler = new ViewHandlerStub();
+			var lessDerivedInterfaceHandler = new ViewHandlerStub();
+			var primaryInterfaceHandler = new ViewHandlerStub();
+
+			var mauiApp = MauiApp.CreateBuilder()
+				.ConfigureMauiHandlers(handlers =>
+				{
+					handlers.AddHandler<IMyDerivedViewStub>(_ => mostDerivedInterfaceHandler);
+					handlers.AddHandler<IMyBaseViewStub>(_ => lessDerivedInterfaceHandler);
+					handlers.AddHandler<IViewStub>(_ => primaryInterfaceHandler);
+				})
+				.Build();
+
+			var mauiHandlersFactory = mauiApp.Services.GetRequiredService<IMauiHandlersFactory>();
+
+			var handlerService = mauiHandlersFactory.GetHandler(typeof(MyDerivedViewStub));
+
+			Assert.NotNull(handlerService);
+			Assert.Same(mostDerivedInterfaceHandler, handlerService);
+		}
+
+		[Fact]
+		public void HostBuilderResolvesToHandlerRegisteredUnderConcreteTypeOverInterfaceType()
+		{
+			var interfaceHandler = new ViewHandlerStub();
+			var classHandler = new ViewHandlerStub();
+
+			var mauiApp = MauiApp.CreateBuilder()
+				.ConfigureMauiHandlers(handlers =>
+				{
+					handlers.AddHandler<ViewStub>(_ => classHandler);
+					handlers.AddHandler<IMyDerivedViewStub>(_ => interfaceHandler);
+					handlers.AddHandler<IMyBaseViewStub>(_ => interfaceHandler);
+					handlers.AddHandler<IViewStub>(_ => interfaceHandler);
+				})
+				.Build();
+
+			var mauiHandlersFactory = mauiApp.Services.GetRequiredService<IMauiHandlersFactory>();
+
+			var handlerService = mauiHandlersFactory.GetHandler(typeof(MyDerivedViewStub));
+
+			Assert.NotNull(handlerService);
+			Assert.IsType<ViewHandlerStub>(handlerService);
+			Assert.Same(classHandler, handlerService);
+		}
+
+		[Fact]
+		public void HostBuilderDoesNotResolveHandlersRegisteredUnderMoreDerivedTypes()
+		{
+			var mauiApp = MauiApp.CreateBuilder()
+				.ConfigureMauiHandlers(handlers =>
+				{
+					handlers.AddHandler<MyBaseViewStub, ViewHandlerStub>();
+					handlers.AddHandler<MyDerivedViewStub, ViewHandlerStub>();
+				})
+				.Build();
+
+			var mauiHandlersFactory = mauiApp.Services.GetRequiredService<IMauiHandlersFactory>();
+
+			Assert.Throws<HandlerNotFoundException>(() => mauiHandlersFactory.GetHandler(typeof(ViewStub)));
+		}
+
+		[Theory]
+		[InlineData(typeof(IViewStub), typeof(ViewHandlerStub))]
+		[InlineData(typeof(ViewStub), typeof(ViewHandlerStub))]
+		[InlineData(typeof(MyBaseViewStub), typeof(MyBaseHandlerStub))]
+		[InlineData(typeof(MyDerivedViewStub), typeof(MyBaseHandlerStub))]
+		public void HostBuilderResolvesClosestApplicableServiceType(Type type, Type expectedHandlerType)
+		{
+			var mauiApp = MauiApp.CreateBuilder()
+				.ConfigureMauiHandlers(handlers =>
+				{
+					handlers.AddHandler<IViewStub, ViewHandlerStub>();
+					handlers.AddHandler<MyBaseViewStub, MyBaseHandlerStub>();
+				})
+				.Build();
+
+			var mauiHandlersFactory = mauiApp.Services.GetRequiredService<IMauiHandlersFactory>();
+
+			var handlerService = mauiHandlersFactory.GetHandler(type);
+
+			Assert.NotNull(handlerService);
+			Assert.IsType(expectedHandlerType, handlerService);
+		}
+
+		interface IMyBaseViewStub : IViewStub { }
+		interface IMyDerivedViewStub : IMyBaseViewStub { }
+		class MyBaseViewStub : ViewStub, IMyBaseViewStub { }
+		class MyDerivedViewStub : MyBaseViewStub, IMyDerivedViewStub { }
+		class MyBaseHandlerStub : ViewHandlerStub { }
+
+		[Fact]
+		public void HostBuilderThrowsWhenOnlyInterfacesRelatedByInheritanceAreRegistered()
+		{
+			var mauiApp = MauiApp.CreateBuilder()
+				.ConfigureMauiHandlers(handlers =>
+				{
+					handlers.AddHandler<IParentAViewStub, ViewHandlerStub>();
+					handlers.AddHandler<IParentBViewStub, ViewHandlerStub>();
+				})
+				.Build();
+
+			var mauiHandlersFactory = mauiApp.Services.GetRequiredService<IMauiHandlersFactory>();
+
+			Assert.Throws<InvalidOperationException>(() => mauiHandlersFactory.GetHandler(typeof(ChildViewStub)));
+		}
+
+		[Fact]
+		public void HostBuilderResolvesToHandlerRegisteredUnderConcreteTypeDespiteAmbiguousInterfacesRegistered()
+		{
+			var interfaceHandler = new ViewHandlerStub();
+			var alternateInterfaceHandler = new ViewHandlerStub();
+			var classHandler = new ViewHandlerStub();
+
+			var mauiApp = MauiApp.CreateBuilder()
+				.ConfigureMauiHandlers(handlers =>
+				{
+					handlers.AddHandler<ViewStub>(_ => classHandler);
+					handlers.AddHandler<IParentAViewStub>(_ => alternateInterfaceHandler);
+					handlers.AddHandler<IParentBViewStub>(_ => interfaceHandler);
+				})
+				.Build();
+
+			var mauiHandlersFactory = mauiApp.Services.GetRequiredService<IMauiHandlersFactory>();
+
+			var handlerService = mauiHandlersFactory.GetHandler(typeof(ChildViewStub));
+
+			Assert.NotNull(handlerService);
+			Assert.Same(classHandler, handlerService);
+		}
+
+		class ChildViewStub : ViewStub, IParentAViewStub, IParentBViewStub {}
+		interface IParentAViewStub : IViewStub { }
+		interface IParentBViewStub : IViewStub { }
 	}
 }
