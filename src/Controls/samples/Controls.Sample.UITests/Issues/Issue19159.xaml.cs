fa20b8c6493372ab022dfb548b93f8830c4b2df3
Jakub Florkowski <kubaflo123@gmail.com>
2024-02-03 01:09:53 +0100

    Added a UiTest (#19159)

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue19159.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 19159, "Top tab bar on Shell hides content", PlatformAffected.iOS)]
+	public partial class Issue19159 : Shell
+	{
+		public Issue19159()
+		{
+			InitializeComponent();
+		}
+	}
+
+	public class Issue19159ContentPage : ContentPage
+	{
+		public Issue19159ContentPage()
+		{
+			Content = new StackLayout()
+			{
+				AutomationId = "page",
+				HorizontalOptions = LayoutOptions.Fill,
+				VerticalOptions = LayoutOptions.Fill,
+				BackgroundColor = new Microsoft.Maui.Graphics.Color(255, 0, 0),
+				Margin = new Microsoft.Maui.Thickness(10)
+			};
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19159.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19159 : _IssuesUITest
+	{
+		public Issue19159(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Top tab bar on Shell hides content";
+
+		[Test]
+		public void ContentShouldNotBeOverlaidByTopBar()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Mac, TestDevice.Windows | TestDevice.Android });
+			_ = App.WaitForElement("page1");
+
+			// The content should not be overlaid by top bar
+			App.Screenshot();
+		}
+	}
+}
