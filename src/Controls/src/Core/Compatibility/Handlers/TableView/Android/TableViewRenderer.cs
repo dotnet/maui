500125b0cd6d36cee353d9ee61033c0d17fe7407
Shane Neuville <shneuvil@microsoft.com>
2024-02-05 15:35:56 -0600

    Update TableViewRenderer.cs

--- src/Controls/src/Core/Compatibility/Handlers/TableView/Android/TableViewRenderer.cs
+++ src/Controls/src/Core/Compatibility/Handlers/TableView/Android/TableViewRenderer.cs
@@ -88,24 +88,25 @@ namespace Microsoft.Maui.Controls.Handlers.Compatibility
 
 		protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
 		{
-			// If you do an AtMost (or Unspecified) height measure of ListView, Android will basically create
+			// If you perform an AtMost (or Unspecified) height measure of ListView, Android will essentially create
 			// a scrap copy of all the ListView cells to calculate the height.
 			// https://cs.android.com/android/platform/superproject/main/+/main:frameworks/base/core/java/android/widget/ListView.java;l=1314-1322?q=ListView
-			// This causes issues, because if a TextCell already has a view that's attached to the visual tree then android
-			// will call "GetView" without a convert view and then it just uses that views a scrap view for measuring.
-			// Our problem, is that we don't have a way of knowing if a view we are returning from getView will be the one we
+			// This causes issues because if a TextCell already has a view that's attached to the visual tree, then `OnMeasure(AT_MOST)`
+			// will call "GetView" without a convert view. Android basically creates an in memory copy of the table to calculate the measure.
+   			//
+			// Our problem is that we don't have a way of knowing if a view we are returning from getView will be the one we
 			// should track against our TextCellHandler or not. 
 			// This all worked fine in XF because in XF we didn't really block against just creating as many renderers against a single
-			// VirtualView as you wanted. This led to a whole different set of hard to track issues.
-			// Fundamentally the ListView control on Android is an old control and the TableView should really be converted to
-			// a BindableLayout or just generating xplat views against a VerticalStackLayout.
+			// VirtualView as you wanted. This led to a whole different set of hard-to-track issues.
+			// Fundamentally, the ListView control on Android is an old control and the TableView should really be converted to
+			// a BindableLayout or just generating cross-platform views against a VerticalStackLayout.
 			//
-			// We handle the Unspecified path inside "GetDesiredSize" by calculating the height of the cells ourselves and requesting an exact measure.\\
-			// Because another quirk of ListView is that if you give it an unspecified measure it'll just size itself to the first row
+			// We handle the Unspecified path inside "GetDesiredSize" by calculating the height of the cells ourselves and requesting an exact measure.
+			// Because another quirk of ListView is that if you give it an unspecified measure, it'll just size itself to the first row
 			// https://cs.android.com/android/platform/superproject/main/+/main:frameworks/base/core/java/android/widget/ListView.java;l=1289-1304?q=ListView
 			//
 			// There is a path here where we could make our structures play friendly with the ListView and then just let ListView do its scrapview thing
-			// But, for how we use TableView, converting to an Exactly measure seems good enough for us.	
+			// But, for how we use TableView, converting to an Exactly measure seems good enough for us.
 			if (heightMeasureSpec.GetMode() == MeasureSpecMode.AtMost)
 			{
 				var size = MeasureSpec.GetSize(heightMeasureSpec);
