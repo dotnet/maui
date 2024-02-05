8c681d340fcf92c4714be5667fc66a01502c4038
Jakub Florkowski <kubaflo123@gmail.com>
2024-02-05 19:13:29 +0100

    Addad a UiTest (#19081)

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue19081.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 19081, "[iOS] RadioButton text related properties not working", PlatformAffected.iOS)]
+	public partial class Issue19081 : ContentPage
+	{
+		public Issue19081()
+		{
+			InitializeComponent();
+		}
+	}
+
+}
