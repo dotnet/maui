29a549e5cccd4a583018e660f933b0a9e959a81c
Jakub Florkowski <kubaflo123@gmail.com>
2024-02-07 20:55:16 +0100

    Added a UiTest (#19739)

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue19739.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 19739, "[Android] Pickers reopen when selecting items", PlatformAffected.Android)]
+	public partial class Issue19739 : ContentPage
+	{
+		public List<string> AutomationIds
+			=> new() { "picker1", "picker2", "picker3" };
+
+		public Issue19739()
+		{
+			InitializeComponent();
+			BindingContext = this;
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19739.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19739 : _IssuesUITest
+	{
+		public Issue19739(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "[Android] Pickers reopen when selecting items";
+
+		[Test]
+		public void PickersDialogsShouldAlwaysCloseWhenSelectingItem()
+		{
+			this.IgnoreIfPlatforms(new[]
+			{
+				TestDevice.iOS,
+				TestDevice.Mac,
+				TestDevice.Windows
+			});
+
+			_ = App.WaitForElement("listView");
+
+
+			for (int i=1;i<=3;i++)
+			{
+				App.Click($"picker{i}");
+				((AppiumApp)App).Driver.FindElements(MobileBy.XPath("//android.widget.TextView[@text=\"Item 1\"]")).First().Click();
+			}
+			
+			// Passes when pickers open dialogs and close them after selecting any item
+			VerifyScreenshot();
+		}
+	}
+}
