a1096cc2ade2c0a3d2ec7ecf90c37f57d95e4269
Šimon Rozsíval <simon@rozsival.com>
2024-02-07 17:00:04 +0100

    [Trimming] Add a feature flag to disable XAML loading at runtime (#19310)
    * Add feature flag
    
    * Add rudimentary docs for feature flags
    
    * Add feature flag guards and attributes with warnings
    
    * TMP: Add TODO comment to resolve issue with XamlC calling SetAndLoadSource
    
    * Move feature flags to Core
    
    * Fix typos
    
    * List affected APIs in the docs
    
    * Add summary comment for RuntimeFeature
    
    * Improve ResourceDictionary.SetAndLoadSource
    
    * Fix comment
    
    * Rename feature switch property and name
    
    * Remove comment
    
    * Revisit ResourceDictionary
    
    * Remove annotations from ResourcesLoader
    
    * Remove fixed warnings from tests
    
    * Remove unnecessary changes
    
    * Update docs/design/FeatureSwitches.md
    
    Co-authored-by: Rolf Bjarne Kvinge <rolf@xamarin.com>
    
    * Move feature switch setup code to Controls targets file
    
    * Remove ILC System.Enum.GetValues warning
    
    * Remove unnecessary Debug fallback
    
    * Suppress trimming warnings in source-generated code
    
    * Update docs/design/FeatureSwitches.md
    
    Co-authored-by: MartyIX <203266+MartyIX@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Rolf Bjarne Kvinge <rolf@xamarin.com>
    Co-authored-by: MartyIX <203266+MartyIX@users.noreply.github.com>

--- src/Controls/src/Core/TrimmerConstants.cs
+++ src/Controls/src/Core/TrimmerConstants.cs
@@ -6,4 +6,6 @@ class TrimmerConstants
 	internal const string SerializerTrimmerWarning = "Data Contract Serialization and Deserialization might require types that cannot be statically analyzed. Make sure all of the required types are preserved.";
 
 	internal const string NativeBindingService = "This method properly handles missing properties, and there is not a way to preserve them from this method.";
+
+	internal const string XamlLoadingTrimmerWarning = "Loading XAML at runtime might require types that cannot be statically analyzed. Make sure all of the required types are preserved.";
 }
