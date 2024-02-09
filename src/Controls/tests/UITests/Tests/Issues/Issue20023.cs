b9df3df39c0b53ac618d83b14abd805212d6de69
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-02-09 17:12:12 +0100

    Added test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue20023.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue20023 : _IssuesUITest
+	{
+		public Issue20023(TestDevice device) : base(device) { }
+		public override string Issue => "RadioButton is not currently being Garbage Collected";
+
+		[Test]
+		public void RadioButtonRendersDefaultTemplate()
+		{
+			App.WaitForElement("WaitForStubControl");
+			App.Screenshot("RadioButton is rendered using the default ControlTemplate");
+		}
+	}
+}
