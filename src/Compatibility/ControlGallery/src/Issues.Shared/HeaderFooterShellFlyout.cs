4074cc437b29eeb7a304ece020a8f62dc22618a0
Shane Neuville <shneuvil@microsoft.com>
2024-02-08 18:24:30 -0600

    Update HeaderFooterShellFlyout.cs

--- src/Compatibility/ControlGallery/src/Issues.Shared/HeaderFooterShellFlyout.cs
+++ src/Compatibility/ControlGallery/src/Issues.Shared/HeaderFooterShellFlyout.cs
@@ -193,7 +193,7 @@ namespace Microsoft.Maui.Controls.ControlGallery.Issues
 #if __IOS__
 		[Test]
 		[Ignore("This test fails intermittently, especially on iOS 17; ignore until we can fix it")]
-		public void FlyoutHeaderWithZeroMarginShouldHaveNoY()
+		public async Task FlyoutHeaderWithZeroMarginShouldHaveNoY()
 		{
 			RunningApp.WaitForElement("PageLoaded");
 			this.TapInFlyout("ZeroMarginHeader", makeSureFlyoutStaysOpen: true);
