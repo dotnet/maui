8974e4cd513b0eaa1fd4ad91422ee920bb2a34cc
Matthew Leibowitz <mattleibow@live.com>
2024-02-09 14:58:42 +0200

    woohoo not crashing no more!

---
+++ src/Core/tests/DeviceTests/Handlers/SwipeView/SwipeViewHandlerTests.Windows.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Maui.DeviceTests.Stubs;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	[Category(TestCategory.SwipeView)]
+	public partial class SwipeViewHandlerTests : CoreHandlerTestBase<SwipeViewHandler, SwipeViewStub>
+	{
+		[Fact(DisplayName = "Clip Initializes ContainerView Correctly", Skip = "Failing")]
+		public override Task ContainerViewInitializesCorrectly()
+		{
+			return Task.CompletedTask;
+		}
+
+		[Fact(DisplayName = "ContainerView Remains If Shadow Mapper Runs Again", Skip = "Failing")]
+		public override Task ContainerViewRemainsIfShadowMapperRunsAgain()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
