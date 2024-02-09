a690a843df54e26d1c4a3b7371cd68ce823fa194
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-02-09 14:09:54 +0100

    Added sample

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue20023.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 20023, "RadioButton is not currently being Garbage Collected", PlatformAffected.iOS)]
+	public partial class Issue20023 : ContentPage
+	{
+		public Issue20023()
+		{
+			InitializeComponent();
+		}
+	}
+}
