a67f6a3bd455f8ff13afe6709a2f2980cbc1637e
Jakub Florkowski <kubaflo123@gmail.com>
2024-02-10 14:33:48 +0100

    Added a UiTest (#19803)

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19803.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19803 : _IssuesUITest
+	{
+		public Issue19803(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "[iOS] Setting Binding on Span GridItemsLayout results in NullReferenceException";
+
+		[Test]
+		public void NoNREWhenChangingGridItemsLayout()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows });
+
+			_ = App.WaitForElement("button");
+
+			//The test passes if no NullReferenceException is thrown
+			App.Click("button");
+
+		}
+	}
+}
