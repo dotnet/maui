5d9e03d00ebeb22d3aa07c4a28ffb803bc915e8c
Mike Corsaro <mikecorsaro@microsoft.com>
2024-02-12 15:03:09 -0800

    Added visual regression test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue17694.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue17694 : _IssuesUITest
+	{
+		public Issue17694(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Circle view not rotating from center";
+
+		[Test]
+		public async Task Issue17694Test()
+		{
+			this.IgnoreIfPlatforms(new[]
+			{
+				TestDevice.iOS,
+				TestDevice.Mac,
+				TestDevice.Android
+			});
+
+			App.WaitForElement("Spin");
+
+			// 1. Click button
+			App.Click("Spin");
+
+			await Task.Delay(100);
+
+			VerifyScreenshot();
+		}
+	}
+}
