81635475013fc9bf092bcfb3fbb057a2c2be0648
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-02-13 10:45:29 +0100

    Added UITest to validate the Switch OnColor property

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20535.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue20535 : _IssuesUITest
+	{
+		public Issue20535(TestDevice device) : base(device) { }
+
+		public override string Issue => "Re-enable/move TrackColorInitializesCorrectly/TrackColorUpdatesCorrectly to Appium";
+
+		[Test]
+		public void SwitchTrackColorTest()
+		{
+			App.WaitForElement("WaitForStubControl");
+			App.Click("UpdateOnColorSwitch");
+			VerifyScreenshot();
+		}
+	}
+}
