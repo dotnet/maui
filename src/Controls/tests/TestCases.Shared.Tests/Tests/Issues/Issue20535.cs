f9569d9c52dba2551ce57bd7be12f385ad80972f
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-02-13 12:12:56 +0100

    Added snapshots

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20535.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20535.cs
@@ -11,10 +11,16 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		public override string Issue => "Re-enable/move TrackColorInitializesCorrectly/TrackColorUpdatesCorrectly to Appium";
 
 		[Test]
-		public void SwitchTrackColorTest()
+		public async Task SwitchTrackColorTest()
 		{
 			App.WaitForElement("WaitForStubControl");
+
+			// 1. Update the state of the Switch by updating the OnColor property.
+			// In this way, we validate the initial value but also the update.
 			App.Click("UpdateOnColorSwitch");
+			await Task.Delay(500); // Wait for the Thumb animation to complete
+
+			// 2. Verify the result.
 			VerifyScreenshot();
 		}
 	}
