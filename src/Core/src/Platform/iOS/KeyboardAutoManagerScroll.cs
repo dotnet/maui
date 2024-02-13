70b2dafcd50b66348ce55221bd31c51cef0741e1
tj-devel709 <tj.devel709@gmail.com>
2024-02-13 16:03:12 -0600

    Add UITests

--- src/Core/src/Platform/iOS/KeyboardAutoManagerScroll.cs
+++ src/Core/src/Platform/iOS/KeyboardAutoManagerScroll.cs
@@ -317,7 +317,7 @@ public static class KeyboardAutoManagerScroll
 		nfloat statusBarHeight;
 		nfloat navigationBarAreaHeight;
 
-		if (ContainerView.GetNavigationController() is UINavigationController navigationController)
+		if (ContainerView.FindResponder<UINavigationController>() is UINavigationController navigationController)
 		{
 			navigationBarAreaHeight = navigationController.NavigationBar.Frame.GetMaxY();
 		}
@@ -513,7 +513,7 @@ public static class KeyboardAutoManagerScroll
 
 					// if PrefersLargeTitles is true, we may need additional logic to
 					// handle the collapsable navbar
-					var navController = View?.GetNavigationController();
+					var navController = View?.FindResponder<UINavigationController>();
 					var prefersLargeTitles = navController?.NavigationBar.PrefersLargeTitles ?? false;
 
 					if (prefersLargeTitles)
