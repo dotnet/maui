d6fd5d8af24d01592814acc8e7fb9e0b4f9f1f9c
Jakub Florkowski <kubaflo123@gmail.com>
2024-02-14 15:21:29 +0100

    Added a UiTest (#20439)

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue20439.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue20439 : _IssuesUITest
+	{
+		public override string Issue => "[iOS] Double dash in Entry or Editor crashes the app";
+
+		public Issue20439(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		public void ErrorShouldNotBeThrown()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows });
+
+			_ = App.WaitForElement("entry");
+			App.Click("entry");
+			App.Click("button");
+
+			// The test passes if no crash is observed
+			App.FindElement("editor");
+		}
+	}
+}
