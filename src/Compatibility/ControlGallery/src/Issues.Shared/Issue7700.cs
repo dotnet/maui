d9f0a6beba6f4f0660aef35916aa881e239a4fac
Shane Neuville <shneuvil@microsoft.com>
2024-02-13 18:41:39 -0600

    Update Issue7700.cs

--- src/Compatibility/ControlGallery/src/Issues.Shared/Issue7700.cs
+++ src/Compatibility/ControlGallery/src/Issues.Shared/Issue7700.cs
@@ -142,6 +142,7 @@ namespace Microsoft.Maui.Controls.ControlGallery.Issues
 
 #if UITEST
 		[Test]
+		[FailsOnMauiAndroid]
 		public void AddingItemToUnviewedCollectionViewShouldNotCrash()
 		{
 			RunningApp.WaitForElement(Add1);
