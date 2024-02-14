37a10c4a53c8cb9a86be52df48e2a5305cb55aa3
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-02-14 15:51:39 +0100

    Added test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue19754.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 19754, "Investigate adding coordinate clicking to the Tap feature for catalyst", PlatformAffected.macOS)]
+	public partial class Issue19754 : ContentPage
+	{
+		public Issue19754()
+		{
+			InitializeComponent();
+		}
+
+		void OnTapped(object sender, TappedEventArgs e)
+		{
+			TestLabel.Text = "Tapped";
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19754.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19754 : _IssuesUITest
+	{
+		public Issue19754(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Investigate adding coordinate clicking to the Tap feature for catalyst";
+
+		[Test]
+		public void TapCoordinateTest()
+		{
+			App.WaitForElement("WaitForStubControl");
+			App.TapCoordinates(150, 150);
+			var result = App.FindElement("TestLabel").GetText();
+			Assert.AreEqual("Tapped", result);
+		}
+	}
+}
