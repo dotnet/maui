59c8b652bdf29c9044b282bec9c5e6ebbaa8cb90
Shane Neuville <shneuvil@microsoft.com>
2024-02-17 13:14:40 -0600

    Move keyboard device tests to appium

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue5724.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium.Android;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue5724 : _IssuesUITest
+	{
+		public Issue5724(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Next Moves To Next Entry and Done Closes Input View";
+
+		[Test]
+		public async Task TappingNextMovesToNextElement()
+		{
+			this.IgnoreIfPlatforms(new[]
+			{
+				TestDevice.Mac,
+				TestDevice.iOS,
+				TestDevice.Windows,
+			}, "Send Keys only works on Android which is why we are ignoring these other platforms");
+
+			App.WaitForElement("Entry1");
+			App.Click("Entry1");
+
+			await Task.Yield();
+
+			App.Click("SendNext");
+			await Task.Yield();
+
+			Assert.True(App.IsFocused("Entry2"));
+		}
+
+		[Test]
+		public async Task TappingDoneClosesKeyboard()
+		{
+			this.IgnoreIfPlatforms(new[]
+			{
+				TestDevice.Mac,
+				TestDevice.iOS,
+				TestDevice.Windows,
+			}, "Send Keys only works on Android which is why we are ignoring these other platforms");
+
+			App.WaitForElement("EntryDone");
+			App.Click("EntryDone");
+
+			await Task.Yield();
+			Assert.True(App.IsKeyboardShown());
+			App.Click("SendDone");
+			await Task.Yield();
+			Assert.False(App.IsKeyboardShown());
+		}
+	}
+}
