71fdb4120c183c19fe5ea307de1666367598ca92
Šimon Rozsíval <simon@rozsival.com>
2024-02-20 20:32:33 +0100

    Update src/Controls/src/Core/TrimmerConstants.cs
    Co-authored-by: Jonathan Peppers <jonathan.peppers@gmail.com>

--- src/Controls/src/Core/TrimmerConstants.cs
+++ src/Controls/src/Core/TrimmerConstants.cs
@@ -10,5 +10,5 @@ class TrimmerConstants
 	internal const string XamlRuntimeParsingNotSupportedWarning = "Loading XAML at runtime might require types and members that cannot be statically analyzed. Make sure all of the required types and members are preserved.";
 
 	internal const string QueryPropertyAttributeWarning = "Using QueryPropertyAttribute is not trimming friendly and might not work correctly. Implement the IQueryAttributable interface instead.";
-	internal const string QueryPropertyDocsUrl = "https://learn.microsoft.com/en-us/dotnet/maui/fundamentals/shell/navigation#process-navigation-data-using-a-single-method";
+	internal const string QueryPropertyDocsUrl = "https://learn.microsoft.com/dotnet/maui/fundamentals/shell/navigation#process-navigation-data-using-a-single-method";
 }
