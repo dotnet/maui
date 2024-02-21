3ac67c6f395ef6ba4000c1501ce3c10bc8f5a29f
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-02-21 13:29:53 +0100

    Added UITest

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue7823.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Platform;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 7823, "In a ToolbarItems, if an item has no icon but just text, MAUI uses the icon from the previous page in the Navigation", PlatformAffected.Android)]
+	public partial class Issue7823 : ContentPage
+	{
+		public Issue7823()
+		{
+			InitializeComponent();
+		}
+		
+		void OnToolbarItemClicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new Issue7823Page2());
+		}
+		
+		void OnButtonClicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new Issue7823Page2());
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue7823.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue7823 : _IssuesUITest
+	{
+		public Issue7823(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "In a ToolbarItems, if an item has no icon but just text, MAUI uses the icon from the previous page in the Navigation";
+
+		[Test]
+		public async Task UpdateToolbarItemAfterNavigate()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.iOS, TestDevice.Mac, TestDevice.Windows });
+
+			// 1. Navigate from Page with a TollbarItem using an Icon. 
+			App.WaitForElement("WaitForStubControl");
+			App.Click("WaitForStubControl");
+
+			await Task.Delay(1000); // Wait navigation animation to complete
+
+			// 2. Verify that the second page with a TollbarItem without an icon does not show the icon of the previous page.
+			VerifyScreenshot();
+			App.Back();
+		}
+	}
+}
