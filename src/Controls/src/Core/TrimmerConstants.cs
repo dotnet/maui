e8a4b28f08c426f5038a1628db211ee31396738b
Šimon Rozsíval <simon@rozsival.com>
2024-02-22 12:47:36 +0100

    [Trimming] Disable query property assignment via reflection on NativeAOT (#20731)
    * Add runtime feature
    
    * Annotate the QueryPropertyAttribute with RequiresUnreferencedCode
    
    * Put application of QueryPropertyAttribute behind a feature switch
    
    * Remove fixed warnings
    
    * Update src/Controls/src/Core/TrimmerConstants.cs
    
    Co-authored-by: Jonathan Peppers <jonathan.peppers@gmail.com>
    
    ---------
    
    Co-authored-by: Jonathan Peppers <jonathan.peppers@gmail.com>

--- src/Controls/src/Core/TrimmerConstants.cs
+++ src/Controls/src/Core/TrimmerConstants.cs
@@ -8,4 +8,7 @@ class TrimmerConstants
 	internal const string NativeBindingService = "This method properly handles missing properties, and there is not a way to preserve them from this method.";
 
 	internal const string XamlRuntimeParsingNotSupportedWarning = "Loading XAML at runtime might require types and members that cannot be statically analyzed. Make sure all of the required types and members are preserved.";
+
+	internal const string QueryPropertyAttributeWarning = "Using QueryPropertyAttribute is not trimming friendly and might not work correctly. Implement the IQueryAttributable interface instead.";
+	internal const string QueryPropertyDocsUrl = "https://learn.microsoft.com/dotnet/maui/fundamentals/shell/navigation#process-navigation-data-using-a-single-method";
 }
