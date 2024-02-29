eff0a3c55f523e15d1647e7bbbb3d43aa41e9087
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-02-29 16:57:05 +0100

    More tests

---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Issue6262.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests
+{
+	public class Issue6262 : IssuesUITest
+	{
+		public Issue6262(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "[Bug] Button in Grid gets wrong z-index";
+
+		[Test]
+		public void ImageShouldLayoutOnTopOfButton()
+		{
+			this.IgnoreIfPlatforms([TestDevice.Android, TestDevice.Mac, TestDevice.Windows]);
+
+			App.WaitForElement("ClickMe");
+			App.Click("ClickMe");
+			App.WaitForElement("ClickMe");
+			App.WaitForNoElement("Fail");
+			App.Click("RetryTest");
+			App.WaitForElement("ClickMe");
+			App.Click("ClickMe");
+			App.WaitForElement("ClickMe");
+			App.WaitForNoElement("Fail");
+		}
+	}
+}
