c9934bfad62c14585356d99411aad9f3acc2dffb
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-03-01 12:38:35 +0100

    More tests

---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Issue7338.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests
+{
+	public class Issue7338 : IssuesUITest
+	{
+		const string Success = "success";
+
+		public Issue7338(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "[Bug] CollectionView crash if source is empty in XF 4.2.0.709249";
+
+		[Test]
+		public void Issue3273Test()
+		{
+			this.IgnoreIfPlatforms([TestDevice.Android, TestDevice.Mac, TestDevice.Windows]);
+			
+			// If the instructions are visible at all, then this has succeeded
+			App.WaitForElement(Success);
+		}
+	}
+}
---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Issue7371.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests.Tests.Issues
+{
+	public class Issue7371 : IssuesUITest
+	{
+		public Issue7371(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "iOS race condition(or not checking for null) of refreshing(offset animation) causes NullReferenceException";
+
+		[Test]
+		public async Task RefreshingListViewCrashesWhenDisposedTest()
+		{
+			this.IgnoreIfPlatforms([TestDevice.Android, TestDevice.Mac, TestDevice.Windows]);
+
+			await Task.Delay(500);
+			App.WaitForElement("Success");
+		}
+	}
+}
