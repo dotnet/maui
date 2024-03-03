1d1a51f1367ec84e2c54a1c25c2103f7a287b2eb
Jakub Florkowski <kubaflo123@gmail.com>
2024-03-04 00:58:43 +0100

    Added a UITest (#18526)

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue18526.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues;
+
+public class Issue18526 : _IssuesUITest
+{
+	public override string Issue => "Border not rendering inside a frame";
+
+	public Issue18526(TestDevice device)
+		: base(device)
+	{ }
+
+    [Test]
+	public void BorderShouldRender()
+	{
+		var label = App.WaitForElement("label");
+
+		Assert.True(label.GetText() == ".NET MAUI");
+	}
+}
