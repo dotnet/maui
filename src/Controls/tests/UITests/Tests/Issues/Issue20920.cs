2d7fe691d33785c045f12293271ff8fcb99e5c39
Shane Neuville <shneuvil@microsoft.com>
2024-03-04 17:02:14 -0600

    Don't measure during the layout pass

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue20920.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues;
+
+public class Issue20920 : _IssuesUITest
+{
+	public Issue20920(TestDevice device) : base(device) { }
+
+	public override string Issue => "Nested ScrollView does not work in Android";
+
+	[Test]
+	public void ScrollingBothDirectionsWithNestedScrollViews()
+	{
+		var initialPosition = App.WaitForElement("dotnet_bot").GetRect();
+
+		App.ScrollDown("dotnet_bot");
+		App.ScrollRight("dotnet_bot");
+
+		var afterScrollPosition = App.WaitForElement("dotnet_bot").GetRect();
+
+		Assert.Less(afterScrollPosition.X, initialPosition.X);
+		Assert.Less(afterScrollPosition.Y, initialPosition.Y);
+	}
+}
