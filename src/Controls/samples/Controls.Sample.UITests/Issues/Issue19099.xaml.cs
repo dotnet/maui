eb9bf7f5015c31250dcdd4e36a71d2de01cd113f
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-03-04 13:17:16 +0100

    Sample test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue19099.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 19099, "TapGestureRecognizer no longer works on Button", PlatformAffected.iOS)]
+	public partial class Issue19099 : ContentPage
+	{
+		public Issue19099()
+		{
+			InitializeComponent();
+		}
+
+		void OnTapped(object sender, TappedEventArgs e)
+		{
+			TestLabel.Text = "Success";
+		}
+	}
+}
