d5383c2ce7d8cd60a435285b485ee05bdfe9a81f
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-03-04 13:17:00 +0100

    Fix the issue

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19099.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Maui.AppiumTests;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Controls.AppiumTests.Tests.Issues
+{
+	public class Issue19099 : _IssuesUITest
+	{
+		public Issue19099(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "TapGestureRecognizer no longer works on Button";
+
+		[Test]
+		[Category(UITestCategories.Gestures)]
+		public void TapGestureWorksOnButton()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows }, "This test is only for iOS");
+
+			App.WaitForElement("TestButton").Click();
+
+			var result = App.FindElement("TestLabel").GetText();
+			Assert.AreEqual("Success", result);
+		}
+	}
+}
