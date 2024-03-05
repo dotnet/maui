e52353ff901221d2c79b68e1076cff451535a60e
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-03-05 11:45:06 +0100

    More tests

---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Issue13390.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests
+{
+    internal class Issue13390 : IssuesUITest
+	{
+		public Issue13390(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Custom SlideFlyoutTransition is not working";
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void CustomSlideFlyoutTransitionCausesCrash()
+		{
+			this.IgnoreIfPlatforms([TestDevice.Android, TestDevice.Mac, TestDevice.Windows]);
+
+			// If this hasn't already crashed, the test is passing
+			App.WaitForElement("Success");
+		}
+	}
+}
---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Issue13551.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests
+{
+	public class Issue13551 : IssuesUITest
+	{
+		const string Success1 = "Success1";
+		const string Success2 = "Success2";
+
+		public Issue13551(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "[Bug] [iOS] CollectionView does not display items if `IsVisible` modified via a binding/trigger";
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void CollectionViewWithFooterShouldNotCrashOnDisplay()
+		{
+			this.IgnoreIfPlatforms([TestDevice.Android, TestDevice.Mac, TestDevice.Windows]);
+
+			App.WaitForNoElement(Success1);
+			App.WaitForNoElement(Success2);
+		}
+	}
+}
