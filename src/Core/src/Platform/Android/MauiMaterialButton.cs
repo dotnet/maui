24d16971b03de8c8af0b361f6838294b8cd67367
Matthew Leibowitz <mattleibow@live.com>
2024-03-06 23:34:45 +0200

    this

--- src/Core/src/Platform/Android/MauiMaterialButton.cs
+++ src/Core/src/Platform/Android/MauiMaterialButton.cs
@@ -2,16 +2,23 @@
 using Android.Content;
 using Android.Graphics.Drawables;
 using Android.Views;
+using AndroidX.Core.Widget;
 using Google.Android.Material.Button;
 
 namespace Microsoft.Maui.Platform
 {
 	public class MauiMaterialButton : MaterialButton
 	{
-		// Currently Material doesn't have any bottom gravity options
-		// so we just move the layout to the bottom using
-		// SetCompoundDrawablesRelative during Layout
-		internal const int IconGravityBottom = 128; // needs to be a flags number
+		// The default MaterialButton currently does not have a concept of bottom
+		// gravity which we need for .NET MAUI.
+		// In order to get this feature, we have added a custom gravity option
+		// that serves as a flag to indicate that the icon should be placed at
+		// the bottom.
+		// The real gravity value is IconGravityTop in order to perform all the
+		// normal layout calculations. We then set ForceBottomIconGravity for our
+		// custom layout pass where we simply swap the icon from the top to the
+		// bottom using SetCompoundDrawablesRelative.
+		internal const int IconGravityBottom = 0x1000;
 
 		public MauiMaterialButton(Context context)
 			: base(context)
@@ -19,6 +26,19 @@ namespace Microsoft.Maui.Platform
 		}
 
 		internal MauiResizeableDrawable? ResizeableIcon => Icon as MauiResizeableDrawable;
+
+		public override int IconGravity
+		{
+			get => base.IconGravity;
+			set
+			{
+				// Intercept the grvity value and set the flag if it's bottom.
+				ForceBottomIconGravity = value == IconGravityBottom;
+				base.IconGravity = ForceBottomIconGravity ? IconGravityTop : value;
+			}
+		}
+
+		internal bool ForceBottomIconGravity { get; private set; }
 
 		protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
 		{
@@ -33,6 +53,22 @@ namespace Microsoft.Maui.Platform
 			CalculateIconSize(availableWidth, availableHeight);
 
 			base.OnMeasure(widthMeasureSpec, heightMeasureSpec);
+		}
+
+		protected override void OnLayout(bool changed, int left, int top, int right, int bottom)
+		{
+			base.OnLayout(changed, left, top, right, bottom);
+
+			// After the layout pass, we swap the icon from the top to the bottom.
+			if (ForceBottomIconGravity)
+			{
+				var icons = TextViewCompat.GetCompoundDrawablesRelative(this);
+				if (icons[1] is { } icon)
+				{
+					TextViewCompat.SetCompoundDrawablesRelative(this, null, null, null, icon);
+					icon.SetBounds(0, 0, icon.IntrinsicWidth, icon.IntrinsicHeight);
+				}
+			}
 		}
 
 		internal void CalculateIconSize(int availableWidth, int availableHeight)
@@ -74,10 +110,7 @@ namespace Microsoft.Maui.Platform
 		}
 
 		bool IsIconGravityHorizontal =>
-			IconGravity == IconGravityTextStart ||
-			IconGravity == IconGravityTextEnd ||
-			IconGravity == IconGravityStart ||
-			IconGravity == IconGravityEnd;
+			IconGravity is IconGravityTextStart or IconGravityTextEnd or IconGravityStart or IconGravityEnd;
 
 		int GetTextLayoutWidth()
 		{
