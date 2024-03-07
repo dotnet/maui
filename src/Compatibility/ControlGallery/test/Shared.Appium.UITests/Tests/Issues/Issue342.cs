0b3a3d1a969fee8d1221cc9266a1ec24da554db9
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-03-07 13:58:20 +0100

    More tests

---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Issue342.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace UITests
+{
+	public class Issue342NoSource : IssuesUITest
+	{
+		public Issue342NoSource(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "NRE when Image is not assigned source";
+
+		[Test]
+		[Category(UITestCategories.Page)]
+		public void AppDoesntCrashWhenResettingPage()
+		{
+			this.IgnoreIfPlatforms([TestDevice.Android, TestDevice.iOS, TestDevice.Mac]);
+
+			RunningApp.WaitForNoElement("Uninitialized image", "Cannot see label");
+			RunningApp.Screenshot("All elements present");
+		}
+	}
+
+
+	public class Issue342DelayedSource : IssuesUITest
+	{
+		public Issue342DelayedSource(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "NRE when Image is delayed source";
+
+		[Test]
+		[Category(UITestCategories.Page)]
+		public void AppDoesntCrashWhenResettingPage()
+		{
+			this.IgnoreIfPlatforms([TestDevice.Android, TestDevice.iOS, TestDevice.Mac]);
+
+			RunningApp.WaitForElement("TestReady");
+			RunningApp.Screenshot("Should not crash");
+		}
+	}
+}
