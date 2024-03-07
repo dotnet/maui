bd2620368abf0d3216ce3443215fb8d09b370848
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-03-07 16:56:50 +0100

    More tests

---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Bugzilla26171.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests
+{
+	public class Bugzilla26171 : IssuesUITest
+	{
+		public Bugzilla26171(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Microsoft.Maui.Controls.Maps is not updating VisibleRegion property when layout is changed";
+
+		[Test]
+		[Category(UITestCategories.Maps)]
+		public void Bugzilla26171Test()
+		{
+			this.IgnoreIfPlatforms([TestDevice.Mac, TestDevice.Windows]);
+
+			RunningApp.WaitForElement("lblValue");
+		}
+	}
+}
