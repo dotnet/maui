f9628833bc5719511c1327727198779490f48953
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-03-07 16:40:36 +0100

    More tests

---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Bugzilla25943.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests
+{
+	public class Bugzilla25943 : IssuesUITest
+	{
+		const string InnerLayout = "innerlayout";
+		const string OuterLayout = "outerlayout";
+		const string Success = "Success";
+
+		public Bugzilla25943(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "[Android] TapGestureRecognizer does not work with a nested StackLayout";
+
+		[Test]
+		[Category(UITestCategories.LifeCycle)]
+		public void VerifyNestedStacklayoutTapsBubble()
+		{
+			this.IgnoreIfPlatforms([TestDevice.iOS, TestDevice.Mac, TestDevice.Windows]);
+
+			RunningApp.WaitForElement(InnerLayout);
+			RunningApp.Tap(InnerLayout);
+
+			RunningApp.WaitForElement(OuterLayout);
+			RunningApp.Tap(OuterLayout);
+
+			RunningApp.WaitForElement(Success);
+		}
+	}
+}
