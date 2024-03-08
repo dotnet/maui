3d5539facd5cffc46d324ca7ba46cef900cf01c2
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-03-08 14:01:35 +0100

    More tests

---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Bugzilla39636.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests
+{
+	public class Bugzilla39636 : IssuesUITest
+	{
+		public Bugzilla39636(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Cannot use XamlC with OnPlatform in resources, it throws System.InvalidCastException";
+
+		[Test]
+		[Category(UITestCategories.LifeCycle)]
+		public void DoesNotCrash()
+		{ 
+			RunningApp.WaitForElement("Success");
+		}
+	}
+}
---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Bugzilla39668.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests
+{
+	public class Bugzilla39668 : IssuesUITest
+	{
+		public Bugzilla39668(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Overriding ListView.CreateDefault Does Not Work on Windows";
+
+		[Test]
+		[Category(UITestCategories.ListView)]
+		public void Bugzilla39668Test()
+		{
+			this.IgnoreIfPlatforms([TestDevice.Android, TestDevice.iOS, TestDevice.Mac]);
+
+			RunningApp.WaitForElement("Success");
+		}
+	}
+}
