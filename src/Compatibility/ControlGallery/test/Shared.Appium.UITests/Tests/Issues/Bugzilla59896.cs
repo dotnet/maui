87a2546c8bf44d25288fa269a45503f0d35d74d9
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-03-11 16:18:22 +0100

    More tests

---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Bugzilla59896.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests
+{
+	public class Bugzilla59896 : IssuesUITest
+	{
+		const string BtnAdd = "btnAdd";
+
+		public Bugzilla59896(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "v2.4.0: Adding inserting section to ListView causes crash IF first section is empty ";
+
+		[Test]
+		[Category(UITestCategories.ListView)]
+		public void Bugzilla59896Test()
+		{
+			this.IgnoreIfPlatforms([TestDevice.Android, TestDevice.Mac, TestDevice.Windows]);
+
+			RunningApp.WaitForElement(BtnAdd);
+			RunningApp.Tap(BtnAdd);
+		}
+	}
+}
---
+++ src/Compatibility/ControlGallery/test/Shared.Appium.UITests/Tests/Issues/Bugzilla60123.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+
+namespace UITests
+{
+	public class Bugzilla60123 : IssuesUITest
+	{
+		public Bugzilla60123(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Rui's issue";
+
+		[Test]
+		[Category(UITestCategories.ListView)]
+		public void AppDoesntCrashWhenResettingPage()
+		{
+			RunningApp.WaitForElement("ListView");
+			RunningApp.ScrollDown("ListView");
+			RunningApp.WaitForElement("ListView");
+		}
+	}
+}
