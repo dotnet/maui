21650419ab5093fef40fd950b703b12e576f216b
Rui Marinho <me@ruimarinho.net>
2024-03-12 22:52:41 +0000

    [controls] Add uitest for issue #20294

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue20294.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues;
+
+public class Issue20294 : _IssuesUITest
+{
+	public Issue20294(TestDevice device) : base(device) { }
+
+	public override string Issue => "CollectionView containing a Footer and a Border with StrokeThickness set to decimal value crashes on scroll";
+
+	[Test]
+	public void ScrollToEndDoesntCrash()
+	{
+ 		App.ScrollDown("FOOTER");
+		App.ScrollUp("ONE");
+	}
+}
