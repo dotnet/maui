fe24a31b9bdf0b0240dede5014879245e5883c10
Rui Marinho <me@ruimarinho.net>
2024-03-15 11:33:54 +0000

    [tests] Just run test on iOS

--- src/Controls/tests/UITests/Tests/Issues/Issue20294.cs
+++ src/Controls/tests/UITests/Tests/Issues/Issue20294.cs
@@ -14,6 +14,8 @@ public class Issue20294 : _IssuesUITest
 	[Test]
 	public void ScrollToEndDoesntCrash()
 	{
+		this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows });
+		
 		App.ScrollTo("FOOTER");
 		App.ScrollUp("theCollectionView", ScrollStrategy.Gesture, 0.5);
 		App.ScrollDown("theCollectionView", ScrollStrategy.Gesture, 0.5);
