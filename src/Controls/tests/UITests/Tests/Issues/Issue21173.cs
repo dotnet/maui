9488b8280283e7f85c773970284e8521916ac73f
Jakub Florkowski <kubaflo123@gmail.com>
2024-03-17 02:08:58 +0100

    Added a UiTest (#21173)

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue21173.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues;
+
+public class Issue21173 : _IssuesUITest
+{
+	public override string Issue => "[Android] Border with RoundRectangle Stroke does not correctly round corners of things within it";
+
+	public Issue21173(TestDevice device)
+		: base(device)
+	{ }
+
+    [Test]
+	public void BorderWithRoundRectangleShouldRoundCornersOfContentWithinIt()
+	{
+		_ = App.WaitForElement("border");
+
+		// The test passes if corners of borders' contents' have proper corner radiuses
+		VerifyScreenshot();
+	}
+}
