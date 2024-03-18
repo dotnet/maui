666a7e835917fcda3e7f6c5a0f9ae76d8df29788
lytico <lytico@users.sourceforge.net>
2024-03-18 18:05:35 +0100

    [Gtk] Core.csproj: BorderView as Gtk.Frame

--- src/Core/src/Platform/Gtk/BorderView.cs
+++ src/Core/src/Platform/Gtk/BorderView.cs
@@ -1,4 +1,34 @@
+using Gdk;
+using Gtk;
+
 namespace Microsoft.Maui.Platform;
 
-public class BorderView : ContentView
-{ }
+// https://docs.gtk.org/gtk3/class.Frame.html
+
+public class BorderView : Frame, ICrossPlatformLayoutBacking
+{
+
+	public ICrossPlatformLayout? CrossPlatformLayout { get; set; }
+
+	Widget? _content;
+
+	public Widget? Content
+	{
+		get => _content;
+		set
+		{
+			if (_content != null && value != null)
+			{
+				this.ReplaceChild(_content, value);
+			}
+			else if (value != null)
+			{
+				Child = value;
+				Child.Expand = true;
+			}
+
+			_content = value;
+		}
+	}
+
+}
--- src/Core/src/Platform/Gtk/StrokeExtensions.cs
+++ src/Core/src/Platform/Gtk/StrokeExtensions.cs
@@ -1,29 +1,79 @@
 ﻿namespace Microsoft.Maui.Platform
 {
+
 	public static class StrokeExtensions
 	{
+
+		[MissingMapper]
+		public static void UpdateStroke(this BorderView platformView, IBorderStroke border)
+		{ }
+
 		[MissingMapper]
 		public static void UpdateStrokeShape(this Gtk.Widget platformView, IBorderStroke border) { }
 
 		[MissingMapper]
-		public static void UpdateStroke(this Gtk.Widget platformView, IBorderStroke border) { }
+		public static void UpdateStroke(this Gtk.Widget platformView, IBorderStroke border)
+		{
+			if (platformView is not BorderView borderView)
+				return;
+
+			UpdateStroke(borderView, border);
+		}
 
 		[MissingMapper]
-		public static void UpdateStrokeThickness(this Gtk.Widget platformView, IBorderStroke border) { }
+		public static void UpdateStrokeThickness(this Gtk.Widget platformView, IBorderStroke border)
+		{
+			if (platformView is not BorderView borderView)
+				return;
+
+			UpdateStroke(borderView, border);
+		}
 
 		[MissingMapper]
-		public static void UpdateStrokeDashPattern(this Gtk.Widget platformView, IBorderStroke border) { }
+		public static void UpdateStrokeDashPattern(this Gtk.Widget platformView, IBorderStroke border)
+		{
+			if (platformView is not BorderView borderView)
+				return;
+
+			UpdateStroke(borderView, border);
+		}
 
 		[MissingMapper]
-		public static void UpdateStrokeDashOffset(this Gtk.Widget platformView, IBorderStroke border) { }
+		public static void UpdateStrokeDashOffset(this Gtk.Widget platformView, IBorderStroke border)
+		{
+			if (platformView is not BorderView borderView)
+				return;
+
+			UpdateStroke(borderView, border);
+		}
 
 		[MissingMapper]
-		public static void UpdateStrokeMiterLimit(this Gtk.Widget platformView, IBorderStroke border) { }
+		public static void UpdateStrokeMiterLimit(this Gtk.Widget platformView, IBorderStroke border)
+		{
+			if (platformView is not BorderView borderView)
+				return;
+
+			UpdateStroke(borderView, border);
+		}
 
 		[MissingMapper]
-		public static void UpdateStrokeLineCap(this Gtk.Widget platformView, IBorderStroke border) { }
+		public static void UpdateStrokeLineCap(this Gtk.Widget platformView, IBorderStroke border)
+		{
+			if (platformView is not BorderView borderView)
+				return;
+
+			UpdateStroke(borderView, border);
+		}
 
 		[MissingMapper]
-		public static void UpdateStrokeLineJoin(this Gtk.Widget platformView, IBorderStroke border) { }
+		public static void UpdateStrokeLineJoin(this Gtk.Widget platformView, IBorderStroke border)
+		{
+			if (platformView is not BorderView borderView)
+				return;
+
+			UpdateStroke(borderView, border);
+		}
+
 	}
+
 }
