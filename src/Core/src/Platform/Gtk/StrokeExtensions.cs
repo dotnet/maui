b9c4069f9f704744c268a94e9ec1a3f4f511db1f
lytico <lytico@users.sourceforge.net>
2024-03-18 22:59:19 +0100

    [Gtk ] Core.csproj: StrokeExtensions: minimal implementation for BorderColor, BorderWidth & RoundedRectangle

--- src/Core/src/Platform/Gtk/StrokeExtensions.cs
+++ src/Core/src/Platform/Gtk/StrokeExtensions.cs
@@ -1,15 +1,54 @@
-﻿namespace Microsoft.Maui.Platform
+﻿using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics.Platform.Gtk;
+
+namespace Microsoft.Maui.Platform
 {
 
 	public static class StrokeExtensions
 	{
 
-		[MissingMapper]
-		public static void UpdateStroke(this BorderView platformView, IBorderStroke border)
-		{ }
+		static (string mainNode, string subNode) GetBorderCssNode(this BorderView platformView)
+		{
+			var mainNode = platformView.CssMainNode();
+
+			return (mainNode, $"border,\n.{mainNode}");
+		}
 
 		[MissingMapper]
-		public static void UpdateStrokeShape(this Gtk.Widget platformView, IBorderStroke border) { }
+		public static void UpdateStroke(this BorderView platformView, IBorderStroke border)
+		{
+
+			var (mainNode, subnode) = platformView.GetBorderCssNode();
+			platformView.SetStyleColor(border.Stroke.ToColor(), mainNode, "border-color", subnode);
+			platformView.SetStyleValueNode($"{(int)border.StrokeThickness}px", mainNode, "border-width", subnode);
+
+		}
+
+		[MissingMapper]
+		public static void UpdateStrokeShape(this Gtk.Widget platformView, IBorderStroke border)
+		{
+			if (platformView is not BorderView borderView)
+				return;
+
+			if (border.Shape is { } shape)
+			{
+				var (mainNode, subnode) = borderView.GetBorderCssNode();
+
+				if (border.Shape is IRoundRectangle roundRectangle)
+				{
+
+					// border-top-left-radius, border-top-right-radius, border-bottom-right-radius, border-bottom-left-radius
+					var path = shape.PathForBounds(platformView.Allocation.ToRect());
+					platformView.SetStyleValueNode($"5px", mainNode, "border-radius", subnode);
+					path.Dispose();
+
+				}
+				else
+				{
+					platformView.SetStyleValueNode($"0px", mainNode, "border-radius", subnode);
+				}
+			}
+		}
 
 		[MissingMapper]
 		public static void UpdateStroke(this Gtk.Widget platformView, IBorderStroke border)
