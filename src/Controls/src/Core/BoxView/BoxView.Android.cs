63f9e65827aec1dbf84d42691a85b855d0a6a2be
Jakub Florkowski <kubaflo123@gmail.com>
2024-03-20 02:10:38 +0100

    [Android] box view properties fix (#19926)

---
+++ src/Controls/src/Core/BoxView/BoxView.Android.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.Maui.Controls
+{
+	public partial class BoxView
+	{
+		private protected override void OnHandlerChangedCore()
+		{
+			base.OnHandlerChangedCore();
+
+			if (Handler != null)
+			{
+				foreach (var prop in AndroidBatchPropertyMapper<BoxView, IViewHandler>.SkipList)
+				{
+					Handler.UpdateValue(prop);
+				}
+			}
+		}
+	}
+}
+
