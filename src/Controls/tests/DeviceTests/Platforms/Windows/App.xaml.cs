28c38428be4acf367eebcaebbc112cc483002f25
Mike Corsaro <mikecorsaro@microsoft.com>
2024-03-25 11:41:29 -0700

    Test adding additional logging

--- src/Controls/tests/DeviceTests/Platforms/Windows/App.xaml.cs
+++ src/Controls/tests/DeviceTests/Platforms/Windows/App.xaml.cs
@@ -1,10 +1,53 @@
-﻿using Microsoft.Maui.Hosting;
+﻿using System;
+using System.Reflection.Metadata;
+using Microsoft.Maui.Hosting;
 
 namespace Microsoft.Maui.DeviceTests.WinUI
 {
 	public partial class App : MauiWinUIApplication
 	{
-		public App() => InitializeComponent();
+		private Exception _lastFirstChanceException;
+
+		public App()
+		{
+			InitializeComponent();
+
+			AppDomain.CurrentDomain.FirstChanceException += (_, e) => _lastFirstChanceException = e.Exception;
+			UI.Xaml.Application.Current.UnhandledException += Current_UnhandledException;
+		}
+
+		private void Current_UnhandledException(object sender, UI.Xaml.UnhandledExceptionEventArgs e)
+		{
+			bool handled;
+			Exception exception;
+
+			try
+			{
+				var eventArgsType = e.GetType();
+				handled = (bool)eventArgsType.GetProperty("Handled")!.GetValue(e)!;
+				exception = (Exception)eventArgsType.GetProperty("Exception")!.GetValue(e)!;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(
+					$"Could not get exception details in WinUIUnhandledExceptionHandler: {ex.Message}\n" +
+					$"{ex.StackTrace}");
+
+				return;
+			}
+
+			if (exception.StackTrace is null)
+			{
+				exception = _lastFirstChanceException!;
+			}
+
+			if (exception != null)
+			{
+				Console.WriteLine(
+					$"WinUI crash: {exception.Message}\n" +
+					$"{exception.StackTrace}");
+			}
+		}
 
 		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 	}
