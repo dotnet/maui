00724aa4e3e42b5f217337f2f3621650a9157603
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-04-02 10:09:44 +0200

    Added repro sample

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue18716.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 18716, "Touch events are not working on WebView when a PDF is displayed", PlatformAffected.iOS)]
+	public partial class Issue18716 : ContentPage
+	{
+		public Issue18716()
+		{
+			InitializeComponent();
+		}
+	}
+}
