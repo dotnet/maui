3bccfd6790fd6750f4ff15e1e5ea8f8506719810
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-04-03 11:51:27 +0200

    Added UI Test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue21374.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue21374 : _IssuesUITest
+	{
+		public Issue21374(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Error when adding to ObservableCollection";
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void AddingItemsToObservableCollectionNoCrash()
+		{
+			this.IgnoreIfPlatforms(new[]
+			{
+				TestDevice.Android,
+				TestDevice.Mac,
+				TestDevice.Windows
+			});
+
+			App.WaitForElement("WaitForStubControl");
+			App.Click("WaitForStubControl");
+			App.WaitForNoElement("Success");
+		}
+	}
+}
