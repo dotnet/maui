aebc65b5c2a782d303b27006f2eb511834286bc8
James Parsons <japarson@microsoft.com>
2024-04-04 13:29:48 -0700

    Add ui test

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue17665.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 17665, "Image is not centered in AspectFill mode", PlatformAffected.UWP)]
+	public class Issue17665 : TestContentPage
+	{
+		protected override void Init()
+		{
+			Content =
+				new Grid()
+				{
+					new Image()
+					{
+						AutomationId = "AspectFillImage",
+						Aspect = Microsoft.Maui.Aspect.AspectFill,
+						WidthRequest = 100,
+					}
+				};
+		}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue17665.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue17665 : _IssuesUITest
+	{
+		public Issue17665(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Image is not centered in AspectFill mode";
+
+		[Test]
+		[Category(UITestCategories.ActionSheet)]
+		public void Issue17665Test()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.iOS }, "Only affects Windows.");
+
+			App.WaitForElement("AspectFillImage", timeout: TimeSpan.FromSeconds(4));
+
+			VerifyScreenshot();
+		}
+	}
+}
