147ddd39f357bb89e071c8fb8bb242e7ab7790a2
Mike Corsaro <mikecorsaro@microsoft.com>
2024-04-04 15:34:21 -0700

    * Revert half-width change

--- src/Core/src/Platform/Windows/MauiButton.cs
+++ src/Core/src/Platform/Windows/MauiButton.cs
@@ -2,6 +2,7 @@
 using Microsoft.UI.Xaml.Automation.Peers;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
+using Windows.Security.Credentials.UI;
 using WThickness = Microsoft.UI.Xaml.Thickness;
 
 namespace Microsoft.Maui.Platform
@@ -26,8 +27,6 @@ namespace Microsoft.Maui.Platform
 	{
 		readonly Image _image;
 		readonly TextBlock _textBlock;
-		int _imgColumn = -1;
-		int _imgRow = -1;
 
 		public DefaultMauiButtonContent()
 		{
@@ -61,67 +60,33 @@ namespace Microsoft.Maui.Platform
 			Children.Add(_image);
 			Children.Add(_textBlock);
 
-			SizeChanged += DefaultMauiButtonContent_SizeChanged;
-
 			LayoutImageLeft(0);
-		}
-
-		private void DefaultMauiButtonContent_SizeChanged(object sender, SizeChangedEventArgs e)
-		{
-			// Ensure that images in buttons do not take up more than half the width/height of the
-			// button when we also have text to display. This ensures that we have enough
-			// space to show a reasonable amount of text.
-			if (_imgColumn != -1 &&
-				_textBlock.Visibility == UI.Xaml.Visibility.Visible &&
-				ColumnDefinitions[_imgColumn].Width.IsAuto)
-			{
-				if (_image.ActualWidth > e.NewSize.Width / 2.0)
-				{
-					ColumnDefinitions[_imgColumn].Width = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Star);
-				}
-			}
-
-			if (_imgRow != -1 &&
-				_textBlock.Visibility == UI.Xaml.Visibility.Visible &&
-				RowDefinitions[_imgRow].Height.IsAuto)
-			{
-				if (_image.ActualHeight > e.NewSize.Height / 2.0)
-				{
-					RowDefinitions[_imgRow].Height = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Star);
-				}
-			}
 		}
 
 		public void LayoutImageLeft(double spacing)
 		{
-			_imgColumn = 0;
-
 			SetupHorizontalLayout(spacing);
 
 			Grid.SetColumn(_image, 0);
 			Grid.SetColumn(_textBlock, 1);
 
-			ColumnDefinitions[0].Width = new UI.Xaml.GridLength(0, UI.Xaml.GridUnitType.Auto);
+			ColumnDefinitions[0].Width = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Auto);
 			ColumnDefinitions[1].Width = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Star);
 		}
 
 		public void LayoutImageRight(double spacing)
 		{
-			_imgColumn = 1;
-
 			SetupHorizontalLayout(spacing);
 
 			Grid.SetColumn(_image, 1);
 			Grid.SetColumn(_textBlock, 0);
 
 			ColumnDefinitions[0].Width = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Star);
-			ColumnDefinitions[1].Width = new UI.Xaml.GridLength(0, UI.Xaml.GridUnitType.Auto);
+			ColumnDefinitions[1].Width = new UI.Xaml.GridLength(1, UI.Xaml.GridUnitType.Auto);
 		}
 
 		public void LayoutImageTop(double spacing)
 		{
-			_imgRow = 0;
-
 			SetupVerticalLayout(spacing);
 
 			Grid.SetRow(_image, 0);
@@ -133,8 +98,6 @@ namespace Microsoft.Maui.Platform
 
 		public void LayoutImageBottom(double spacing)
 		{
-			_imgRow = 1;
-
 			SetupVerticalLayout(spacing);
 
 			Grid.SetRow(_image, 1);
@@ -157,8 +120,6 @@ namespace Microsoft.Maui.Platform
 
 		void SetupHorizontalLayout(double spacing)
 		{
-			_imgRow = -1;
-
 			RowSpacing = 0;
 			ColumnSpacing = AdjustSpacing(spacing);
 
@@ -177,8 +138,6 @@ namespace Microsoft.Maui.Platform
 
 		void SetupVerticalLayout(double spacing)
 		{
-			_imgColumn = -1;
-
 			ColumnSpacing = 0;
 			RowSpacing = AdjustSpacing(spacing);
 
