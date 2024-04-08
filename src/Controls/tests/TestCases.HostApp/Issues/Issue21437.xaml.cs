29dc1a2aef803fac59a873a6cf8a84d2100dbaa4
Jakub Florkowski <kubaflo123@gmail.com>
2024-04-09 00:47:59 +0200

    [Android] Added null checks (#21437)

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue21437.xaml.cs
@@ -0,0 +1,24 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using System.Collections.ObjectModel;
+
+namespace Maui.Controls.Sample.Issues;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+[Issue(IssueTracker.Github, 21437, "Removing TapGestureRecognizer with at least 2 taps causes Exception", PlatformAffected.Android)]
+
+public partial class Issue21437 : ContentPage
+{
+	public ObservableCollection<string> Items { get; } = new() { "Item1", "Item2", "Item3" };
+
+	public Command TapCommand => new Command<string>(obj =>
+	{
+		Items.Remove(obj);
+	});
+
+	public Issue21437()
+	{
+		InitializeComponent();
+		BindingContext = this;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21437.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues;
+
+public class Issue21437 : _IssuesUITest
+{
+	public override string Issue => "Removing TapGestureRecognizer with at least 2 taps causes Exception";
+
+	public Issue21437(TestDevice device)
+		: base(device)
+	{ }
+
+    [Test]
+	public void ExceptionShouldNotBeThrown()
+	{
+		_ = App.WaitForElement("Item2");
+		App.DoubleClick("Item2");
+
+        //The test passes if no exception is thrown
+	}
+}
