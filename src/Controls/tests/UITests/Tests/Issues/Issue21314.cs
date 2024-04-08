ad8f8cf2ea5fb7a7d0fc9c0b6ac5ac168a08a341
Stephen Hawley <sthawl@microsoft.com>
2024-04-08 15:15:58 -0400

    add visual test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue21314.cs
@@ -0,0 +1,25 @@
+using Maui.Controls.Sample;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+    public class Issue21314 : _IssuesUITest {
+        public override string Issue => "Image has wrong orientation on iOS";
+
+        public Issue21314(TestDevice device) : base(device)
+        {
+        }
+
+        [Test]
+        public void ImageShouldBePortrait()
+        {
+            this.IgnoreIfPlatforms (new TestDevice[] { TestDevice.Mac, TestDevice.Android, TestDevice.Windows });
+
+            _ = App.WaitForElement ("photo");
+            VerifyScreenshot();
+
+        }
+    }
+}
