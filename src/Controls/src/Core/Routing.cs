2a76d880bad5e74ff9b1d2ab52a00ef7ec05a3ee
Šimon Rozsíval <simon@rozsival.com>
2024-04-09 13:09:21 +0200

    [Trimming] Remove unnecessary regular expressions (#21497)
    * Remove unused usign in Routing
    
    * Remove regex for checking x:Class in XamlLoader
    
    * Remove simple regex from KeyboardAutoManagerScroll in iOS
    
    * Remove simple regex from PathBuilder

--- src/Controls/src/Core/Routing.cs
+++ src/Controls/src/Core/Routing.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace Microsoft.Maui.Controls
 {
