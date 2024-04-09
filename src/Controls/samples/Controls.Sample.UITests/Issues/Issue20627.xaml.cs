9c935aecc8ada3b4e8e0801a58301e68ec7e8b77
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-04-09 09:02:07 +0200

    Added repro sample

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue20627.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using Mopups.Services;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 20627, "WebView error if we leave page while it's navigating", PlatformAffected.iOS)]
+	public partial class Issue20627 : ContentPage
+    {
+        public Issue20627()
+        {
+            InitializeComponent();
+        }
+
+        void OnOpenPopupClicked(object sender, EventArgs e)
+        {
+            MopupService.Instance.PushAsync(new Issue20627Popup());
+        }
+    }
+
+}
