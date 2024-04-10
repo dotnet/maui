bbc8121938455f45451bc38b08c0fca51f7f3b9e
Shane Neuville <shneuvil@microsoft.com>
2024-04-10 17:55:58 -0500

    - make the test better

--- src/Controls/tests/UITests/Tests/Issues/Issue5354.cs
+++ src/Controls/tests/UITests/Tests/Issues/Issue5354.cs
@@ -21,23 +21,28 @@ namespace Maui.Controls.Sample.Issues
 
 			App.WaitForElement("CollectionView5354");
 			App.WaitForElement("Button5354");
-			var colView = App.WaitForElement("CollectionView5354");
-			App.WaitForNoElement("NoElement", timeout: TimeSpan.FromSeconds(3));
-		
-			var linearRect0 = App.WaitForElement("Image0").GetRect();
-			var linearRect1 = App.WaitForElement("Image1").GetRect();
+			
+			for(int i = 0; i < 3; i++)
+			{
+				App.WaitForNoElement("NoElement", timeout: TimeSpan.FromSeconds(3));
+			
+				var linearRect0 = App.WaitForElement("Image0").GetRect();
+				var linearRect1 = App.WaitForElement("Image1").GetRect();
 
-			Assert.AreEqual(linearRect0.X, linearRect1.X);
-			Assert.GreaterOrEqual(linearRect1.Y, linearRect0.Y + linearRect0.Height);
+				Assert.AreEqual(linearRect0.X, linearRect1.X);
+				Assert.GreaterOrEqual(linearRect1.Y, linearRect0.Y + linearRect0.Height);
 
-			App.Click("Button5354");
+				App.Click("Button5354");
 
-			App.WaitForNoElement("NoElement", timeout: TimeSpan.FromSeconds(3));
-			var gridRect0 = App.WaitForElement("Image0").GetRect();
-			var gridRect1 = App.WaitForElement("Image1").GetRect();
+				App.WaitForNoElement("NoElement", timeout: TimeSpan.FromSeconds(3));
+				var gridRect0 = App.WaitForElement("Image0").GetRect();
+				var gridRect1 = App.WaitForElement("Image1").GetRect();
 
-			Assert.AreEqual(gridRect0.Y, gridRect1.Y);
-			Assert.AreEqual(gridRect0.Height, gridRect1.Height);
+				Assert.AreEqual(gridRect0.Y, gridRect1.Y);
+				Assert.AreEqual(gridRect0.Height, gridRect1.Height);
+				
+				App.Click("Button5354");
+			}
 		}
 	}
 }
