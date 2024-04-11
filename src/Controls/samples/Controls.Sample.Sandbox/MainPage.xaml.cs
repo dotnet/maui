85d7cc4be0be004a095ed4fbaa09cf7d51c9ffc9
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-04-11 22:03:27 +0200

    Generate grid

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,18 +1,45 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Maui;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Graphics;
 
-namespace Maui.Controls.Sample
+namespace Maui.Controls.Sample;
+
+public partial class MainPage : ContentPage
 {
-	public partial class MainPage : ContentPage
+	public MainPage()
 	{
-		public MainPage()
+		InitializeComponent();
+	}
+
+	private void ClearGrid_Clicked(object sender, EventArgs e)
+	{
+		Stopwatch sw = Stopwatch.StartNew();
+
+		contentGrid.Clear();
+
+		sw.Stop();
+
+		info.Text = $"Clearing grid took: {sw.ElapsedMilliseconds} ms";
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		const int rowCount = 30;
+		const int columnCount = 30;
+
+		Stopwatch sw = Stopwatch.StartNew();
+		contentGrid.Clear();
+
+		for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
 		{
-			InitializeComponent();
+			for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
+			{
+				Label label = new Label() { Text = $"[{columnIndex}x{rowIndex}]" };
+				contentGrid.Add(label, column: columnIndex, row: rowIndex);
+			}
 		}
+
+		sw.Stop();
+		info.Text = $"Clearing grid took: {sw.ElapsedMilliseconds} ms";
 	}
 }
