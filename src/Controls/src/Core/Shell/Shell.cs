0375f8daec554146c2cbecfcdb0e709ef087e43d
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-04-11 10:34:07 +0200

    Update src/Controls/src/Core/Shell/Shell.cs
    Co-authored-by: Juan Diego Herrera <juherrera@microsoft.com>

--- src/Controls/src/Core/Shell/Shell.cs
+++ src/Controls/src/Core/Shell/Shell.cs
@@ -294,7 +294,7 @@ namespace Microsoft.Maui.Controls
 
 		/// <summary>
 		/// Sets the handler responsible for implementing the integrated search functionality.
-		/// Enabling it results in a search box being added at the top of the page.
+		/// Enabling this property results in a search box being added at the top of the page.
 		/// </summary>
 		/// <param name="obj">The object that modifies the Shell search functionality.</param>
 		/// <param name="handler">Defines the integrated search functionality.</param>
