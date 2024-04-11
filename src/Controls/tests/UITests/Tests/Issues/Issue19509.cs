7a3d90fb7dd33043eb01d915bb2a448e7113483e
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-04-11 13:52:25 +0200

    Fixes the build

--- src/Controls/tests/UITests/Tests/Issues/Issue19509.cs
+++ src/Controls/tests/UITests/Tests/Issues/Issue19509.cs
@@ -13,12 +13,14 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		public override string Issue => "Entry TextColor property not working when the Text value is bound after some time";
 
 		[Test]
-		public void Issue19509Test()
+		public async Task Issue19509Test()
 		{
 			App.WaitForElement("WaitForStubControl");
 
 			// 1. Click a button to update the text
 			App.Click("button");
+
+			await Task.Delay(1000); // Wait to Ripple Effect animation to complete.
 
 			// 2. Verify that the Entry TextColor is correct (Green).
 			VerifyScreenshot();
