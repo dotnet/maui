40b3a74b885ec35ffeba226cd51e90b6d6fe5cae
Stephen Hawley <sthawl@microsoft.com>
2024-04-12 11:09:29 -0400

    make error strings match

--- src/Controls/samples/Controls.Sample.UITests/Issues/Issue21314.xaml.cs
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue21314.xaml.cs
@@ -4,7 +4,7 @@ using Microsoft.Maui.Controls.Xaml;
 namespace Maui.Controls.Sample.Issues
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    [Issue(IssueTracker.Github, 21314, "Image orientation tag ignored on iOS", PlatformAffected.iOS)]
+    [Issue(IssueTracker.Github, 21314, "Image has wrong orientation on iOS", PlatformAffected.iOS)]
     public partial class Issue21314 : ContentPage
     {
         public Issue21314()
