dotnet/maui

commit 5ad30b6544be22ff05b1e0eefbcf86a55509c9cc

[Windows] Fix image button scaling (#20949)
* Cherry pick Windows only changes for fix-18242

* Revert change to csproj

---------

Co-authored-by: Mike Corsaro <mikecorsaro@microsoft.com>

src/Controls/samples/Controls.Sample.UITests/Issues/Issue18242.xaml
@@ -1,0 +1,38 @@
+﻿<?xml version="1.0" encoding="utf-8" ?>
+<ContentPage xmlns="http://schemas.microsoft.com/dotnet/2021/maui"
+			 xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
+			 x:Class="Maui.Controls.Sample.Issues.Issue18242"
+			 Title="Issue 18242">
+	<VerticalStackLayout
+		Padding="12">
+		<Label
+			AutomationId="WaitForStubControl"
+			Text="1. The image must adapt to the size of the Button and be in the position determined by the ContentLayout property."/>
+		<Label 
+			Text="Button with Height"/>
+		<Button
+			HeightRequest="50"
+			ImageSource="dotnet_bot.png"
+			ContentLayout="Left,10"
+			Text="Button"/>
+		<Label 
+			Text="Button without Height"/>
+		<Button
+			ImageSource="dotnet_bot.png"
+			ContentLayout="Left,10"
+			Text="Button"/>
+		<Label 
+			Text="2. The image must adapt to the size of the ImageButton."/>
+		<Label 
+			Text="ImageButton with Height"/>
+		<ImageButton 
+			HeightRequest="30" 
+			WidthRequest="50" 
+			Source="dotnet_bot.png"/>
+		<Label 
+		   Text="ImageButton without Height"/>
+		<ImageButton 
+		   WidthRequest="50" 
+		   Source="dotnet_bot.png"/>
+	</VerticalStackLayout>
+</ContentPage>

src/Controls/samples/Controls.Sample.UITests/Issues/Issue18242.xaml.cs
@@ -1,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 18242, "Button ImageSource not Scaling as expected", PlatformAffected.UWP)]
+	public partial class Issue18242 : ContentPage
+	{
+		public Issue18242()
+		{
+			InitializeComponent();
+		}
+	}
+}

src/Controls/tests/UITests/Tests/Issues/Issue18242.cs
@@ -1,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue18242 : _IssuesUITest
+	{
+		public Issue18242(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Button ImageSource not Scaling as expected";
+
+		[Test]
+		public void Issue18242Test()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.iOS }, "Only Windows for now");
+
+			App.WaitForElement("WaitForStubControl");
+
+			VerifyScreenshot();
+		}
+	}
+}

src/Core/src/Platform/Windows/ButtonExtensions.cs
@@ -159,11 +159,9 @@ namespace Microsoft.Maui.Platform
 		{
 			if (platformButton.GetContent<WImage>() is WImage nativeImage)
 			{
-				nativeImage.Source = nativeImageSource;
-
 				// Stretch to the size of the button
-				nativeImage.HorizontalAlignment = UI.Xaml.HorizontalAlignment.Stretch;
-				nativeImage.VerticalAlignment = UI.Xaml.VerticalAlignment.Stretch;
+				nativeImage.Stretch = UI.Xaml.Media.Stretch.Uniform;
+				nativeImage.Source = nativeImageSource;
 
 				nativeImage.Visibility = nativeImageSource == null
 					? UI.Xaml.Visibility.Collapsed

commit 1f8377cbf1fb88d7df7f33b8de9338f8e2c317b6

Add device test for button images in layouts
Adds a skipped test for #21548 due to a regression caused by #20949

src/Controls/tests/DeviceTests/Elements/Layout/LayoutTests.cs
@@ -240,6 +240,47 @@ namespace Microsoft.Maui.DeviceTests
 			});
 		}
 
+		[Fact(
+#if WINDOWS
+			Skip = "Currently causes a layout cycle due to https://github.com/dotnet/maui/pull/20949"
+#endif
+		)]
+		[Category(TestCategory.Button, TestCategory.FlexLayout)]
+		public async Task ButtonWithImageInFlexLayoutInGridDoesNotCycle()
+		{
+			await ButtonWithImageInFlexLayoutInGridDoesNotCycleCore();
+			// Cycle does not occur on first run
+			await ButtonWithImageInFlexLayoutInGridDoesNotCycleCore();
+		}
+
+		async Task ButtonWithImageInFlexLayoutInGridDoesNotCycleCore()
+		{
+			var grid = new Grid() { MaximumWidthRequest = 150 };
+			grid.AddRowDefinition(new RowDefinition(GridLength.Auto));
+
+			var flexLayout = new FlexLayout() { Wrap = Layouts.FlexWrap.Wrap };
+			grid.Add(flexLayout);
+
+			for (int i = 0; i < 2; i++)
+			{
+				var button = new Button { ImageSource = "black.png" };
+				flexLayout.Add(button);
+			}
+
+			await InvokeOnMainThreadAsync(async () =>
+			{
+				foreach (var button in flexLayout.Children.OfType<Button>())
+					await CreateHandlerAsync<ButtonHandler>(button);
+				var flexLayoutHandler = await CreateHandlerAsync<LayoutHandler>(flexLayout);
+				var layoutHandler = await CreateHandlerAsync<LayoutHandler>(grid);
+
+				// If this can be attached to the hierarchy and make it through a layout
+				// without crashing, then we're good.
+
+				await AttachAndRun(grid, (handler) => { });
+			});
+		}
+
 		[Fact]
 		public async Task GridCellsHonorMaxWidth()
 		{