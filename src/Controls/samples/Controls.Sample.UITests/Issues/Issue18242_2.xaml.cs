ac43167096635b7d252ecb4061c895cfa01f5de6
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-04-19 13:47:50 +0200

    Added manual testing sample

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue18242_2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Platform;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.ManualTest, "18242_2", "Button ImageSource not Scaling as expected", PlatformAffected.iOS)]
+	public partial class Issue18242_2 : ContentPage
+	{
+		readonly Random _random;
+
+		public Issue18242_2()
+		{
+			InitializeComponent();
+
+			_random = new Random();
+		}
+
+		void OnSliderPaddingValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			TopButton.Padding = 
+			BottonButton.Padding = 
+			LeftButton.Padding = 
+			RightButton.Padding = new Thickness(e.NewValue);
+		}
+
+		void OnUpdateSizeButtonClicked(object sender, EventArgs e)
+		{
+			TopButton.HeightRequest = TopButton.WidthRequest =
+			BottonButton.HeightRequest = BottonButton.WidthRequest =
+			LeftButton.HeightRequest = LeftButton.WidthRequest =
+			RightButton.HeightRequest = RightButton.WidthRequest =
+			_random.Next(60, 120);
+		}
+	}
+}
