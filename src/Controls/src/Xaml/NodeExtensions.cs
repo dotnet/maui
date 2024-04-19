6f9b879d856d1ea3a76b13e89548befd95570175
Simon Rozsival <simon@rozsival.com>
2024-04-19 12:36:01 +0200

    Use file-scoped namespace

--- src/Controls/src/Xaml/NodeExtensions.cs
+++ src/Controls/src/Xaml/NodeExtensions.cs
@@ -1,21 +1,20 @@
-namespace Microsoft.Maui.Controls.Xaml
+namespace Microsoft.Maui.Controls.Xaml;
+
+internal static class NodeExtensions
 {
-	internal static class NodeExtensions
+	public static bool TryGetPropertyName(this INode node, INode parentNode, out XmlName name)
 	{
-		public static bool TryGetPropertyName(this INode node, INode parentNode, out XmlName name)
-		{
-			name = default;
-			var parentElement = parentNode as IElementNode;
-			if (parentElement == null)
-				return false;
-			foreach (var kvp in parentElement.Properties)
-			{
-				if (kvp.Value != node)
-					continue;
-				name = kvp.Key;
-				return true;
-			}
+		name = default;
+		var parentElement = parentNode as IElementNode;
+		if (parentElement == null)
 			return false;
+		foreach (var kvp in parentElement.Properties)
+		{
+			if (kvp.Value != node)
+				continue;
+			name = kvp.Key;
+			return true;
 		}
+		return false;
 	}
 }
