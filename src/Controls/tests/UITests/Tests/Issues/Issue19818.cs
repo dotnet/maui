719375bed907a2d45e508c11592ccfc6c03c5a47
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-04-22 13:19:43 +0200

    Added UI Test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19818.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19818 : _IssuesUITest
+	{
+		public Issue19818(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "[MAUI] Stuck when entering/exiting G6 page";
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void Issue19818Test()
+		{
+			this.IgnoreIfPlatforms([TestDevice.iOS, TestDevice.Mac, TestDevice.Windows]);
+
+			App.WaitForElement("TestCollectionView");
+			App.ScrollDown("TestCollectionView", ScrollStrategy.Gesture);
+			App.WaitForNoElement("3");
+		}
+	}
+}
