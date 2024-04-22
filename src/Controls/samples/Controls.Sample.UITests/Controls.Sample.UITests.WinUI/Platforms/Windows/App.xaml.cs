84927bff51b03b9a050ee27a89ba83cf9d1b6835
Matthew Leibowitz <mattleibow@live.com>
2024-04-22 17:12:33 +0200

    undo

---
+++ src/Controls/samples/Controls.Sample.UITests/Controls.Sample.UITests.WinUI/Platforms/Windows/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace Maui.Controls.Sample.Platform
+{
+	public partial class App : MauiWinUIApplication
+	{
+		public App()
+		{
+			InitializeComponent();
+		}
+
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
