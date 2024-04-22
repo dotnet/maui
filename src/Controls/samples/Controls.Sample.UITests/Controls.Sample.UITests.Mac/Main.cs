1082cba336729b023f5176d056b6af5a934090a7
Matthew Leibowitz <mattleibow@live.com>
2024-04-22 17:10:43 +0200

    this

--- src/Controls/samples/Controls.Sample.UITests/Controls.Sample.UITests.Mac/Main.cs
+++
@@ -1,7 +0,0 @@
-using Maui.Controls.Sample.Mac;
-using UIKit;
-
-// This is the main entry point of the application.
-// If you want to use a different Application Delegate class from "AppDelegate"
-// you can specify it here.
-UIApplication.Main(args, null, typeof(AppDelegate));
---
+++ src/Controls/samples/Controls.Sample.UITests/Controls.Sample.UITests.Tizen/MauiProgram.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Hosting;
+
+namespace Maui.Controls.Sample.WinUI
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+
+			builder
+				.UseSharedMauiApp();
+
+			return builder.Build();
+		}
+	}
+}
