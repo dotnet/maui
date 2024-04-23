7feefb35d45c37cfa5e8df53906ef621d6eac16c
Mike Corsaro <corsarom@gmail.com>
2024-04-23 11:53:02 -0700

    [Windows] Use correct build version check
    Windows 11 version isn't actually 11.*, it's 10.0.21996

--- src/Essentials/src/Connectivity/Connectivity.uwp.cs
+++ src/Essentials/src/Connectivity/Connectivity.uwp.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Maui.Networking
 		{
 			get
 			{
-				if (OperatingSystem.IsWindowsVersionAtLeast(11))
+				if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 21996, 0))
 				{
 					var profile = NetworkInformation.GetInternetConnectionProfile();
 					if (profile == null)
