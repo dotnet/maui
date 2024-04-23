bf3a4bbe9ce74b3bade6d026365c98056bc73902
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-04-23 10:04:28 +0200

    Added UI Test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue21747.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue21747 : _IssuesUITest
+	{
+		public Issue21747(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "TemplateBinding with CornerRadius on Border does not work anymore";
+
+		[Test]
+		[Category(UITestCategories.Border)]
+		public void BorderTemplateBindingWorks()
+		{
+			App.WaitForElement("WaitForStubControl");
+		}
+	}
+}
