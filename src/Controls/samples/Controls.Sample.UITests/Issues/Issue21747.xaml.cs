f3b4797616efdcf532dd11f13f749bc5e7408252
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-04-23 10:04:21 +0200

    Repro sample

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue21747.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 21747, "TemplateBinding with CornerRadius on Border does not work anymore", PlatformAffected.All)]
+	public partial class Issue21747 : ContentPage
+	{
+		public Issue21747()
+		{
+			InitializeComponent();
+		}
+	}
+
+	public class MyButton : TemplatedView
+	{
+		public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(
+			nameof(CornerRadius), typeof(CornerRadius), typeof(MyButton), new CornerRadius(6.0),
+			propertyChanged: (bindable, oldValue, newValue) =>
+			{
+				Debug.WriteLine($"CornerRadius changed from '{oldValue}' to '{newValue}'");
+			});
+
+		public static readonly BindableProperty TextProperty = BindableProperty.Create(
+			nameof(Text), typeof(string), typeof(MyButton), "Default",
+			propertyChanged: (bindable, oldValue, newValue) =>
+			{
+				Debug.WriteLine($"Text changed from '{oldValue}' to '{newValue}'");
+			});
+
+		public CornerRadius CornerRadius
+		{
+			get => (CornerRadius)GetValue(CornerRadiusProperty);
+			set => SetValue(CornerRadiusProperty, value);
+		}
+
+		public string Text
+		{
+			get => (string)GetValue(TextProperty);
+			set => SetValue(TextProperty, value);
+		}
+	}
+}
