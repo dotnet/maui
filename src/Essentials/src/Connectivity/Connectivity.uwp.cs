665b646b552406caf5f06a7609bc508475af399b
Mike Corsaro <corsarom@gmail.com>
2024-04-23 14:03:11 -0700

    Update Connectivity.uwp.cs

--- src/Essentials/src/Connectivity/Connectivity.uwp.cs
+++ src/Essentials/src/Connectivity/Connectivity.uwp.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Maui.Networking
 		{
 			get
 			{
-				if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 21996, 0))
+				if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 22000, 0))
 				{
 					var profile = NetworkInformation.GetInternetConnectionProfile();
 					if (profile == null)
