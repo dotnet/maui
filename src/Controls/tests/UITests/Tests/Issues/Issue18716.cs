412424fdf4fea49a96febdbf13358023cbca053a
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-04-26 06:41:58 +0200

    More changes

--- src/Controls/tests/UITests/Tests/Issues/Issue18716.cs
+++ src/Controls/tests/UITests/Tests/Issues/Issue18716.cs
@@ -16,7 +16,7 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		[Category(UITestCategories.WebView)]
 		public void CanScrollWebView()
 		{
-			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Mac, TestDevice.Windows });
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows });
 
 			App.WaitForElement("LoadedControl", timeout: TimeSpan.FromSeconds(1));
 			var result = App.FindElement("LoadedControl").GetText();
