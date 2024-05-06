88be40d08c495d33cda00d4c97b790637ce0cfd1
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-06 13:02:27 +0200

    Added UI Test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue22000.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue22000 : _IssuesUITest
+	{
+		public Issue22000(TestDevice device) : base(device) { }
+
+		public override string Issue => "[Windows] Carousel changes the current position when window is resized";
+
+		[Test]
+		[Category(UITestCategories.CarouselView)]
+		public void ResizeCarouselViewKeepsIndex()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.iOS, TestDevice.Mac });
+
+			App.WaitForElement("WaitForStubControl");
+
+			for (int i = 0; i < 10; i++)
+			{
+				App.Click("UpdateSizeButton");
+			}
+
+			VerifyScreenshot();
+		}
+	}
+}
