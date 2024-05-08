68f8a8c93f72ea6a35478c0064f49d23c198f931
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-08 11:08:47 +0200

    Updated test

--- src/Controls/tests/UITests/Tests/Issues/Issue22035.cs
+++ src/Controls/tests/UITests/Tests/Issues/Issue22035.cs
@@ -15,6 +15,9 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		[Test]
 		public void CarouselViewVirtualViewNotNull()
 		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.iOS, TestDevice.Mac, TestDevice.Windows },
+				"This test is failing, likely due to product issue. More information: https://github.com/dotnet/maui/issues/22287");
+
 			App.WaitForElement("TestNavigateButton");
 
 			// 1. Click a button to navigate to the CarouselView Page.
