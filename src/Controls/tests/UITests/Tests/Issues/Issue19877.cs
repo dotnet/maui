d0df9e5ef93ce6732bf38e2de531e27bed42562a
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-09 15:49:08 +0200

    Added UITest for Issue19877

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19877.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19877 : _IssuesUITest
+	{
+		public Issue19877(TestDevice device) : base(device) { }
+
+		public override string Issue => "RoundRectangle Border is messed up when contains an Image with AspectFill";
+
+		[Test]
+		[Category(UITestCategories.Entry)]
+		public void BorderRoundRectangleWithImage()
+		{
+			this.IgnoreIfPlatforms(new[]
+			{
+				TestDevice.iOS,
+				TestDevice.Mac,
+				TestDevice.Windows
+			});
+
+			App.WaitForElement("TestBorder");
+
+			VerifyScreenshot();
+		}
+	}
+}
