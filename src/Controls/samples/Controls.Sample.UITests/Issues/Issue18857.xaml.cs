9901acc82a3654218a038308c20987c42f6e3336
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-09 08:53:37 +0200

    Added UITest

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue18857.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Graphics;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 18857, "ImageButton Padding & Ripple effect stops working with .NET 8", PlatformAffected.Android)]
+	public partial class Issue18857 : ContentPage
+	{
+		public Issue18857()
+		{
+			InitializeComponent();
+			UpdateImageButtonBackground();
+		}
+
+		void OnUpdateBackgroundButtonClicked(object sender, EventArgs e)
+		{
+			UpdateImageButtonBackground();
+		}
+
+		void OnRemoveBackgroundButtonClicked(object sender, EventArgs e)
+		{
+			BackgroundImageButton.Background = null;
+		}
+
+		void UpdateImageButtonBackground()
+		{
+			Random rnd = new Random();
+			Color startColor = Color.FromRgba(rnd.Next(256), rnd.Next(256), rnd.Next(256), 255);
+			Color endColor = Color.FromRgba(rnd.Next(256), rnd.Next(256), rnd.Next(256), 255);
+
+			BackgroundImageButton.Background = new LinearGradientBrush
+			{
+				EndPoint = new Point(1, 0),
+				GradientStops = new GradientStopCollection
+				{
+					new GradientStop { Color = startColor },
+					new GradientStop { Color = endColor, Offset = 1 }
+				}
+			};
+		}
+	}
+}
