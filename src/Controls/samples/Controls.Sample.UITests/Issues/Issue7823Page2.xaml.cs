bbf7132a0d3391bef4e0d04edc25a4a16fb402f2
Shane Neuville <shneuvil@microsoft.com>
2024-05-10 18:14:16 -0500

    Flakey test fixes

--- src/Controls/samples/Controls.Sample.UITests/Issues/Issue7823Page2.xaml.cs
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue7823Page2.xaml.cs
@@ -13,6 +13,18 @@ namespace Maui.Controls.Sample.Issues
 			InitializeComponent();
 		}
 
+		protected override void OnNavigatedTo(NavigatedToEventArgs args)
+		{
+			base.OnNavigatedTo(args);
+			grid.Add(new Label()
+			{
+				AutomationId = "SecondPageLoaded",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Text = "Issue 7823"
+			});
+		}
+
 		async void OnToolbarItemClicked(object sender, EventArgs e)
 		{
 			await Navigation.PopAsync();
