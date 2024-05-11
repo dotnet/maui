485c641f1658f900e8ac1cb8d559148b5df1fb8a
Jakub Florkowski <kubaflo123@gmail.com>
2024-05-11 16:37:01 +0200

    Added a UI Test (#22289)

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22289.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue22289 : _IssuesUITest
+	{
+		public override string Issue => "InputTransparent=\"true\" on a Layout breaks child controls on Android";
+		public Issue22289(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		public void ButtonsShouldBeVisible()
+		{
+			App.WaitForElement("changeVisibilityButton");
+			App.Click("changeVisibilityButton");
+
+			App.WaitForElement("button1");
+			App.WaitForElement("button2");
+		}
+	}
+}
