b1ade475711533aaa0da4d33f21798849c01c249
Jakub Florkowski <kubaflo123@gmail.com>
2024-05-16 00:39:33 +0200

    Added a UITest (#22032)

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue22032.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 22032, "Shell FlyoutItem Tab Selected Icon Color not changing if using Font icons", PlatformAffected.iOS)]
+	public partial class Issue22032 : TabbedPage
+	{
+		public Issue22032()
+		{
+			InitializeComponent();
+		}
+
+		void Button_Clicked(object sender, EventArgs e)
+		{
+			CurrentPage = tab2;
+		}
+	}
+}
