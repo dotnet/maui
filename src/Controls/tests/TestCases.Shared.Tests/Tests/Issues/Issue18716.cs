2d769c40918d82bd790925792b603df3bfedc4a2
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-16 13:20:07 +0200

    [Testing] Split up current appium tests into separate projects (#21908)
    * Split up current appium tests into separate projects
    
    * Updated samples sln
    
    * More updates on slns
    
    * More changes
    
    * More fixes
    
    * More changes
    
    * More changes
    
    * More changes
    
    * More changes
    
    * Trying to fix Windows project build
    
    * not needed
    
    * revert
    
    * this
    
    * undo
    
    * this
    
    * add it all back
    
    * fixes
    
    * eventually
    
    * huh?
    
    * this
    
    * builds at least
    
    * this
    
    * ui tests
    
    * this
    
    * More changes
    
    * Added uitest project by platform
    
    * Remove IgnoreIfPlatforms
    
    * Changes in the uitests yml
    
    * Updated slnfs
    
    * Fix format errors
    
    * Added samples slnf
    
    * Recover UITest single project
    
    * Updated solutions
    
    * Updated project references in uitests yml
    
    * Added removed UITests comments
    
    * Removed unnecesary changes
    
    * Revert more unneccesary changes
    
    * Fixed build error on macOS
    
    * More changes
    
    * Fix wrong namespace
    
    * Fix deploy issues
    
    * Fix wrong test app path
    
    * Fixed test project paths
    
    * Moved snapshots
    
    * Fix merge issues
    
    * Remove more IgnoreIfPlatforms methods
    
    * Fix merge issue
    
    * Fix build errors
    
    * hmmm
    
    * Maybe this?
    
    * unifi
    
    * Update Microsoft.Maui-vscode.sln
    
    * Updated test
    
    * More fixes
    
    * More fixes
    
    * Updated projects
    
    * Update cake scripts
    
    * More changes
    
    * Fixes in project tests path
    
    * More fixes
    
    * Moved snapshots
    
    * Updated snapshots tests used path
    
    * More changes
    
    * More pending changes
    
    * Fixes the golden tests
    
    * More changes in tests
    
    * Renamed test projects
    
    * More changes
    
    * Updated test
    
    * Fix build error
    
    * More updates
    
    * Renamed projects
    
    * More changes
    
    * Move more projects
    
    * More changes
    
    * Fixed build errors
    
    * Fixed error on mac sln
    
    * More changes
    
    * Updated App path
    
    * - move testcases app into tests folder
    
    * - fix project references
    
    * - add missing targets file
    
    * - fix tizen manifest
    
    * - additional props
    
    * Updated UITesting md
    
    * Updated Development md
    
    * More docs updates
    
    ---------
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>
    Co-authored-by: Shane Neuville <shneuvil@microsoft.com>

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18716.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue18716 : _IssuesUITest
+	{
+		public Issue18716(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "Touch events are not working on WebView when a PDF is displayed";
+
+		/*
+  		There's an issue getting the mouse interactions to work with Appium.
+		[Test]
+		[Category(UITestCategories.WebView)]
+		public async Task CanScrollWebView()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Windows, TestDevice.iOS });
+
+			await Task.Delay(1000); // Wait WebView to load.
+
+			App.WaitForElement("WaitForStubControl");
+			App.ScrollDown("WaitForStubControl", ScrollStrategy.Gesture, 0.75);
+			App.Screenshot("Scrolling has been done correctly.");
+		}*/
+  
+	}
+}
