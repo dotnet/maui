6cd8895b61094578bd5dd04b5b55e7128f8277e7
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-16 12:03:21 +0200

    Added UITest

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue22417.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue22417 : _IssuesUITest
+	{
+		public Issue22417(TestDevice device) : base(device) { }
+
+		public override string Issue => "[MAUI] The list does not show newly added recipes";
+
+		[Test]
+		[Category(UITestCategories.CarouselView)]
+		public async Task AddItemToCarousel()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.iOS, TestDevice.Mac });
+
+			App.WaitForElement("WaitHere");
+
+			App.WaitForElement("AddButton");
+
+			await Task.Delay(500);
+
+			VerifyScreenshot();
+		}
+	}
+}
