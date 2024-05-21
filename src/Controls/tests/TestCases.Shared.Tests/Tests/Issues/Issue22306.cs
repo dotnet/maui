805ea1d4bffb14040a886fe95bbf83ae2027309c
tj-devel709 <tj.devel709@gmail.com>
2024-05-21 00:09:01 -0500

    add additional UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22306.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue22306 : _IssuesUITest
+    {
+        public Issue22306(TestDevice device) : base(device) { }
+
+        public override string Issue => "Resizing buttons' parents resolves layout";
+
+        [Test]
+        [Category(UITestCategories.Button)]
+        public void ButtonsLayoutResolveWhenParentSizeChanges()
+        {
+            this.IgnoreIfPlatforms([TestDevice.Mac, TestDevice.Windows]);
+            try
+            {
+                App.SetOrientationPortrait();
+                WaitForAllElements();
+                var changeBoundsButton = App.WaitForElement("ChangeBoundsButton");
+                VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "Original");
+
+                changeBoundsButton.Click();
+
+                WaitForAllElements();
+                VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "SizeButtonsDownPortrait");
+
+                App.SetOrientationLandscape();
+
+                WaitForAllElements();
+                VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "SizeButtonsDownLandscape");
+                changeBoundsButton.Click();
+                WaitForAllElements();
+
+                App.SetOrientationPortrait();
+                WaitForAllElements();
+                // Cannot use the original screenshot as the black bar on bottom is not as dark after rotation
+                VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "Original2");
+            }
+            finally
+            {
+                App.SetOrientationPortrait();
+            }
+        }
+
+        void WaitForAllElements()
+        {
+            App.WaitForElement("TopGrid");
+            App.WaitForElement("ButtonLeft");
+            App.WaitForElement("ButtonTop");
+            App.WaitForElement("ButtonRight");
+            App.WaitForElement("ButtonBottom");
+            App.WaitForElement("ChangeBoundsButton");
+        }
+    }
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22306_1.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue22306_1 : _IssuesUITest
+    {
+        public Issue22306_1(TestDevice device) : base(device) { }
+
+        public override string Issue => "Button with images measures correctly";
+
+        [Test]
+        [Category(UITestCategories.Button)]
+        public void ButtonMeasuresLargerThanDefault()
+        {
+            this.IgnoreIfPlatforms([TestDevice.Mac, TestDevice.Windows]);
+
+            var rotateButtonRect = App.WaitForElement("RotateButton").GetRect();
+            var button3Rect = App.WaitForElement("Button3").GetRect();
+
+            Assert.That(rotateButtonRect.Height > button3Rect.Height);
+        }
+
+        [Test]
+        [Category(UITestCategories.Button)]
+        public void ButtonLayoutResizesWithImagePosition()
+        {
+            this.IgnoreIfPlatforms([TestDevice.Mac, TestDevice.Windows]);
+
+            App.WaitForElement("RotateButton");
+            VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "Top");
+
+            App.WaitForElement("RotateButton").Tap();
+            VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "Right");
+
+            App.WaitForElement("RotateButton").Tap();
+            VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "Bottom");
+
+            App.WaitForElement("RotateButton").Tap();
+            VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "Left");
+
+
+        }
+    }
+}
