abd1c4740cda1c041dde53cd94132f8335888756
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-21 13:07:40 +0200

    Moved comment

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16561.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16561.cs
@@ -23,8 +23,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 
 		[Test]
 		[Category(UITestCategories.Label)]
-		// https://github.com/dotnet/maui/issues/17435
-		[FailsOnMac]
+		[FailsOnMac("https://github.com/dotnet/maui/issues/17435")]
 		public void TapTwoPlacesQuickly()
 		{
 			if (App is not AppiumApp app2 || app2 is null || app2.Driver is null)
