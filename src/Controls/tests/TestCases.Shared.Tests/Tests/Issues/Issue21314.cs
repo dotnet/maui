347366f4150da50ea4a6de61a3e3704fbbdc17dd
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-22 11:28:04 +0200

    Fix build errors

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21314.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21314.cs
@@ -1,5 +1,4 @@
 #if IOS
-using Maui.Controls.Sample;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 using UITest.Appium;
@@ -7,7 +6,8 @@ using UITest.Core;
 
 namespace Microsoft.Maui.TestCases.Tests.Issues
 {
-    public class Issue21314 : _IssuesUITest {
+    public class Issue21314 : _IssuesUITest 
+	{
         public override string Issue => "Image has wrong orientation on iOS";
 
         public Issue21314(TestDevice device) : base(device)
