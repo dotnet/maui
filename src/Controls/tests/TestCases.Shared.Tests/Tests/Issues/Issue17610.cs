58a6661f7d194c5663807a4aa003f6a6e649f153
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-28 11:08:18 +0200

    Simplified tests

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17610.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17610.cs
@@ -30,14 +30,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			int toX = rect2.CenterX();
 			int toY = rect2.CenterY();
 
-			OpenQA.Selenium.Appium.Interactions.PointerInputDevice touchDevice = new OpenQA.Selenium.Appium.Interactions.PointerInputDevice(PointerKind.Touch);
-			var refreshSequence = new ActionSequence(touchDevice, 0);
-			refreshSequence.AddAction(touchDevice.CreatePointerMove(CoordinateOrigin.Viewport, fromX, fromY, TimeSpan.Zero));
-			refreshSequence.AddAction(touchDevice.CreatePointerDown(PointerButton.TouchContact));
-			refreshSequence.AddAction(touchDevice.CreatePointerMove(CoordinateOrigin.Viewport, toX, toY, TimeSpan.FromMilliseconds(250)));
-			refreshSequence.AddAction(touchDevice.CreatePointerMove(CoordinateOrigin.Viewport, fromX, fromY, TimeSpan.FromMilliseconds(250)));
-			refreshSequence.AddAction(touchDevice.CreatePointerUp(PointerButton.TouchContact));
-			androidApp.Driver.PerformActions([refreshSequence]);
+			App.DragCoordinates(fromX, fromY, toX, toY);
 
 			VerifyScreenshot();
 		}
