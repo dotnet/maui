06872b82d9f0d05a94dedc9d1180129be36dc3ae
Mike Corsaro <mikecorsaro@microsoft.com>
2024-05-30 14:34:26 -0700

    Add test

--- src/Controls/tests/DeviceTests/Elements/ListView/ListViewTests.Windows.cs
+++ src/Controls/tests/DeviceTests/Elements/ListView/ListViewTests.Windows.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Handlers;
 
 namespace Microsoft.Maui.DeviceTests
 {
@@ -13,6 +15,38 @@ namespace Microsoft.Maui.DeviceTests
 		void ValidatePlatformCells(ListView listView)
 		{
 
+		}
+
+		[Fact]
+		public async Task InsertItemWorks()
+		{
+			SetupBuilder();
+
+			var data = new ObservableCollection<string>();
+			var listView = new ListView()
+			{
+				ItemsSource = data
+			};
+
+			var layout = new VerticalStackLayout()
+			{
+				listView
+			};
+
+			await CreateHandlerAndAddToWindow<LayoutHandler>(layout, async (handler) =>
+			{
+				await Task.Delay(100);
+
+				data.Add("Item 1");
+				data.Add("Item 3");
+				data.Insert(1, "Item 2");
+
+				await Task.Delay(100);
+
+				var children = listView.GetVisualTreeDescendants();
+
+				Assert.True(children.Any());
+			});
 		}
 	}
 }
