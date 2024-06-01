42918b233e57d2eb5aace2e63618b19e5439e92a
Jakub Florkowski <kubaflo123@gmail.com>
2024-06-01 18:32:56 +0200

    Implemented ios:Slider.UpdateOnTap

---
+++ src/Controls/src/Core/Slider/Slider.Mapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Maui.Controls.Compatibility;
+
+namespace Microsoft.Maui.Controls
+{
+	public partial class Slider
+	{
+		[Obsolete("Use SliderHandler.Mapper instead.")]
+		public static IPropertyMapper<ISlider, SliderHandler>? ControlsSliderMapper = new ControlsMapper<Slider, SliderHandler>(SliderHandler.Mapper);
+
+		internal static new void RemapForControls()
+		{
+			// Adjust the mappings to preserve Controls.Slider legacy behaviors
+#if IOS
+			SliderHandler.Mapper.ReplaceMapping<Slider, ISliderHandler>(PlatformConfiguration.iOSSpecific.Slider.UpdateOnTapProperty.PropertyName, MapUpdateOnTap);
+#endif
+		}
+	}
+}
