13dc9118eb53f17d311f4cf8642911be746963ba
Jakub Florkowski <kubaflo123@gmail.com>
2024-06-03 00:40:26 +0200

    Update Issue20166.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20166.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20166.cs
@@ -1,27 +1,23 @@
-﻿using System.Drawing;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.AppiumTests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue20166 : _IssuesUITest
 {
-	public class Issue20166 : _IssuesUITest
+	public Issue20166(TestDevice device)
+	: base(device) { }
+
+	public override string Issue => "Custom FlyoutIcon visible although FlyoutBehavior set to disabled";
+
+	[Test]
+	public void ShouldHideCustomFlyoutIconWhenNavigatingToPageWithDisabledFlyout()
 	{
-		public Issue20166(TestDevice device) : base(device) { }
+		// Click button 1 to switch to the page with disabled flyout
+		_ = App.WaitForElement("button1");
+		App.Click("button1");
 
-		public override string Issue => "Custom FlyoutIcon visible although FlyoutBehavior set to disabled";
-
-		[Test]
-		public void ShouldHideCustomFlyoutIconWhenNavigatingToPageWithDisabledFlyout()
-		{
-			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows });
-
-			// Click button 1 to switch to the page with disabled flyout
-			App.WaitForElement("button1");
-			App.Click("button1");
-
-			// 2. Verify that the flyout icon is not rendered.
-			VerifyScreenshot();
-		}
+		// 2. Verify that the flyout icon is not rendered.
+		VerifyScreenshot();
 	}
 }
