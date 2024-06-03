91de95610627ee8d9bccd9639e2f71263ff02462
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-06-03 11:38:12 +0200

    Added new UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22750.cs
@@ -0,0 +1,31 @@
+﻿#if ANDROID
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue22750 : _IssuesUITest
+	{
+		public Issue22750(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "TableView ViewCell vanishes after content is updated";
+		
+		[Test]
+		public void RadioButtonUpdateValueInsideBorder()
+		{
+			App.WaitForElement("WaitForStubControl");
+
+			App.Tap("Yes");
+
+			App.Tap("No");
+			VerifyScreenshot("RadioButtonUpdateValueInsideBorderNo");
+
+			App.Tap("Yes");
+			VerifyScreenshot("RadioButtonUpdateValueInsideBorderYes");
+		}
+	}
+}
+#endif
