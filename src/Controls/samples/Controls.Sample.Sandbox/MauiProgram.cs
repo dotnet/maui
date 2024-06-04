a128c7710cd4f78160bc1b6b659f416627cb76d7
Matthew Leibowitz <mattleibow@live.com>
2024-06-04 18:22:53 +0800

    Added some additional testing assets to sandbox

--- src/Controls/samples/Controls.Sample.Sandbox/MauiProgram.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MauiProgram.cs
@@ -14,6 +14,19 @@ namespace Maui.Controls.Sample
 				.UseMauiMaps()
 #endif
 				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("Dokdo-Regular.ttf", "Dokdo");
+					fonts.AddFont("LobsterTwo-Regular.ttf", "Lobster Two");
+					fonts.AddFont("LobsterTwo-Bold.ttf", "Lobster Two Bold");
+					fonts.AddFont("LobsterTwo-Italic.ttf", "Lobster Two Italic");
+					fonts.AddFont("LobsterTwo-BoldItalic.ttf", "Lobster Two BoldItalic");
+					fonts.AddFont("ionicons.ttf", "Ionicons");
+					fonts.AddFont("SegoeUI.ttf", "Segoe UI");
+					fonts.AddFont("SegoeUI-Bold.ttf", "Segoe UI Bold");
+					fonts.AddFont("SegoeUI-Italic.ttf", "Segoe UI Italic");
+					fonts.AddFont("SegoeUI-Bold-Italic.ttf", "Segoe UI Bold Italic");
+				})
 				.Build();
 	}
 
