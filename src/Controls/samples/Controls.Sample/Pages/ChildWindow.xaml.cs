14eb6ffbaf3ea49ab13fdf7611cf61bc9ef9e29f
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-06-05 13:51:19 +0200

    Added test

---
+++ src/Controls/samples/Controls.Sample/Pages/ChildWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Pages;
+
+public partial class ChildWindow : ContentPage
+{
+    public ChildWindow()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22386.cs
@@ -0,0 +1,24 @@
+﻿#if WINDOWS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue22386 : _IssuesUITest
+{
+	public Issue22386(TestDevice device)
+		: base(device)
+	{ }
+
+	public override string Issue => "[WinUI] Application.Current.CloseWindow Crash When AppWindow.Hide is called in .Net Maui 9.0 Prev 3";
+
+	[Test]
+	public void CloseHiddenWindowNoCrash()
+	{
+		App.WaitForElement("UpdateWindowButton");
+		App.Tap("UpdateWindowButton");
+		App.WaitForElement("Success");
+	}
+}
+#endif
