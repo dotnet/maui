e3763a60258f5949b96fc59cb8cc45ca5aac86e1
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-06-06 22:58:20 +0200

    Revert "Sandbox demo for easy testing"
    This reverts commit 8b9e5d4a62f1d2f792f7d6b53d734edc975e98dd.

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,26 +1,9 @@
-using System;
-using Microsoft.Maui.Controls;
+﻿namespace Maui.Controls.Sample;
 
-namespace Maui.Controls.Sample
+public partial class MainPage : ContentPage
 {
-	public partial class MainPage : ContentPage
+	public MainPage()
 	{
-		public Command ClickCommand { get; }
-
-		public MainPage()
-		{
-			ClickCommand = new(execute: () =>
-			{
-				DisplayAlert("Title", "Button 1 clicked", "Cancel");
-			});
-
-			InitializeComponent();
-			BindingContext = this;
-		}
-
-		private void button2_Clicked(object sender, EventArgs e)
-		{
-			DisplayAlert("Title", "Button 2 clicked", "Cancel");
-		}
+		InitializeComponent();
 	}
 }
