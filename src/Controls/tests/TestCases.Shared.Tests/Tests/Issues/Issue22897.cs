1c4ba3a4ab91fe33cb7c71366cae24815be232f4
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-06-10 11:46:52 +0200

    Added UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22897.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue22897 : _IssuesUITest
+{
+	public Issue22897(TestDevice device) : base(device) { }
+
+	public override string Issue => "Display Bug On iOS about BoxView that under initial InVisible Layout";
+
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxViewRenderChangingVisibility()
+	{
+		App.WaitForElement("WaitForStubControl");
+
+		App.Tap("On");
+		App.Tap("Off");
+		App.Tap("On");
+
+		VerifyScreenshot();
+	}
+}
