819b4cfa9bd610469d3edc1a624725c3e800f6db
Thomas Muller <thomasmuller@me.com>
2024-06-11 14:06:08 +0200

    Flyout test pages added

---
+++ src/Controls/tests/TestCases/Issues/Issue18161.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 18161, "Toggling FlyoutLayoutBehavior on Android causes the app to crash", PlatformAffected.Android)]
+	public partial class Issue18161 : FlyoutPage
+	{
+		public Issue18161()
+		{
+			InitializeComponent();
+		}
+
+		public void ToggleBehaviour_Clicked(object sender, EventArgs e)
+		{
+			FlyoutLayoutBehavior = FlyoutLayoutBehavior == FlyoutLayoutBehavior.Split 
+				? FlyoutLayoutBehavior.Popover 
+				: FlyoutLayoutBehavior.Split;
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases/Issues/Issue20858.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 20858, "FlyoutPage open when rotating test", PlatformAffected.Android)]
+	public partial class Issue20858 : FlyoutPage
+	{
+		public Issue20858()
+		{
+			InitializeComponent();
+		}
+
+		public void OpenFlyout_Clicked(object sender, EventArgs e)
+		{
+			IsPresented = true;
+		}
+	}
+}
