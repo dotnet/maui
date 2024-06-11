a0a0ef5f34fc724ee49cd252a18a65094204dff6
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-06-11 12:21:01 +0200

    Updated test

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19500.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19500.cs
@@ -1,8 +1,9 @@
-﻿using NUnit.Framework;
+﻿#if IOS
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.AppiumTests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues
 {
 	public class Issue19500 : _IssuesUITest
 	{
@@ -15,8 +16,6 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		[Test]
 		public void TextInEditorShouldScroll()
 		{
-			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows });
-
 			_ = App.WaitForElement("editor");
 			App.ScrollDown("editor");
 
@@ -25,3 +24,4 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		}
 	}
 }
+#endif
