b200dcc608d6fb5e4324f0e1f8f0891c43d30e71
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-06-11 13:21:06 +0200

    Updated test

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19997.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19997.cs
@@ -1,8 +1,9 @@
-﻿using NUnit.Framework;
+﻿#if IOS
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.AppiumTests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues
 {
 	public class Issue19997 : _IssuesUITest
 	{
@@ -13,8 +14,6 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		[Test]
 		public void DarkThemeShouldBeApplied()
 		{
-			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.Mac, TestDevice.Windows }, "This is an iOS theme change issue.");
-
 			App.WaitForElement("entry");
 			App.Click("entry");
 
@@ -23,3 +22,4 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		}
 	}
 }
+#endif
