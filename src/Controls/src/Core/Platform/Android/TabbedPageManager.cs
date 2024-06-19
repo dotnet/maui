6f80687d569d62673093766e5e9b9892eadb3229
maonaoda <maonaoda@gmail.com>
2024-06-19 16:17:52 +0900

    add mapper for IsSwipePagingEnabledProperty on Android
    Make MapUpdateOffscreenPageLimit internal
    
    remove unused PublicApi

--- src/Controls/src/Core/Platform/Android/TabbedPageManager.cs
+++ src/Controls/src/Core/Platform/Android/TabbedPageManager.cs
@@ -470,7 +470,7 @@ namespace Microsoft.Maui.Controls.Handlers
 				child.IgnoresContainerArea = child is NavigationPage;
 		}
 
-		void UpdateOffscreenPageLimit()
+		internal void UpdateOffscreenPageLimit()
 		{
 			_viewPager.OffscreenPageLimit = Element.OnThisPlatform().OffscreenPageLimit();
 		}
