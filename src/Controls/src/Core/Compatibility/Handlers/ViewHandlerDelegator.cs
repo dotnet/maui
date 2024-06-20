0321a353ace49ab52dd53f1e1e80f02c6fd4c027
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-06-20 11:00:57 -0500

    Only change ViewHandlerDelegator for iOS/Catalyst

--- src/Controls/src/Core/Compatibility/Handlers/ViewHandlerDelegator.cs
+++ src/Controls/src/Core/Compatibility/Handlers/ViewHandlerDelegator.cs
@@ -20,9 +20,14 @@ namespace Microsoft.Maui.Controls.Handlers.Compatibility
 		internal IPropertyMapper _mapper;
 		internal readonly CommandMapper _commandMapper;
 		IPlatformViewHandler _viewHandler;
+#if IOS || MACCATALYST
 		TElement? _tempElement;
 		WeakReference<TElement>? _element;
 		public TElement? Element => _tempElement ?? _element?.GetTargetOrDefault();
+#else
+		TElement? _element;
+		public TElement? Element => _element;
+#endif
 		bool _disposed;
 
 		public ViewHandlerDelegator(
@@ -81,10 +86,14 @@ namespace Microsoft.Maui.Controls.Handlers.Compatibility
 		{
 #if WINDOWS
 			VisualElementRenderer<TElement, TNativeElement>.SetVirtualView(view, _viewHandler, onElementChanged, ref _element, ref _mapper, _defaultMapper, autoPackage);
-#else
+#elif IOS || MACCATALYST
+			// _tempElement is used here, because the Element property is accessed before SetVirtualView() returns
 			VisualElementRenderer<TElement>.SetVirtualView(view, _viewHandler, onElementChanged, ref _tempElement, ref _mapper, _defaultMapper, autoPackage);
+			// We use _element as a WeakReference, and clear _tempElement
 			_element = _tempElement is null ? null : new(_tempElement);
 			_tempElement = null;
+#else
+			VisualElementRenderer<TElement>.SetVirtualView(view, _viewHandler, onElementChanged, ref _element, ref _mapper, _defaultMapper, autoPackage);
 #endif
 		}
 
