08895cb718f721858d4c4f83287098f923dca257
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-06-20 13:41:39 +0200

    Hide clear button

--- src/Core/src/Platform/Windows/MauiTextBox.cs
+++ src/Core/src/Platform/Windows/MauiTextBox.cs
@@ -1,5 +1,4 @@
-﻿#nullable enable
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.Maui.Platform
@@ -39,14 +38,16 @@ namespace Microsoft.Maui.Platform
 
 			var scrollViewer = element?.GetDescendantByName<ScrollViewer>(ContentElementName);
 			if (scrollViewer is not null)
+			{
 				scrollViewer.VerticalAlignment = verticalAlignment;
+			}
 
 			var placeholder = element?.GetDescendantByName<TextBlock>(PlaceholderTextContentPresenterName);
 			if (placeholder is not null)
+			{
 				placeholder.VerticalAlignment = verticalAlignment;
+			}
 		}
-
-		// IsDeleteButtonEnabled
 
 		public static bool GetIsDeleteButtonEnabled(DependencyObject obj) =>
 			(bool)obj.GetValue(IsDeleteButtonEnabledProperty);
@@ -60,56 +61,24 @@ namespace Microsoft.Maui.Platform
 
 		static void OnIsDeleteButtonEnabledPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs? e = null)
 		{
-			// TODO: cache the buttonStates and buttonVisibleState values on the textBox
-
-			var element = d as FrameworkElement;
-
-			VisualStateGroup? buttonStates = null;
-			VisualState? buttonVisibleState = null;
-			var deleteButton = element?.GetDescendantByName<Button>(DeleteButtonElementName);
-			if (deleteButton?.Parent is Grid rootGrid)
-				(buttonStates, buttonVisibleState) = InterceptDeleteButtonVisualStates(rootGrid);
-
-			var states = buttonStates?.States;
-			if (element is not null && states is not null && buttonVisibleState is not null)
+			if (d is not FrameworkElement element)
 			{
-				var isEnabled = GetIsDeleteButtonEnabled(element);
-				var contains = states.Contains(buttonVisibleState);
-				if (isEnabled && !contains)
-					states.Add(buttonVisibleState);
-				else if (!isEnabled && contains)
-					states.Remove(buttonVisibleState);
-			}
-		}
-
-		static (VisualStateGroup? Group, VisualState? State) InterceptDeleteButtonVisualStates(FrameworkElement? element)
-		{
-			// not the content we expected
-			if (element is null)
-				return (null, null);
-
-			// find "ButtonStates"
-			var visualStateGroups = VisualStateManager.GetVisualStateGroups(element);
-			VisualStateGroup? buttonStates = null;
-			foreach (var group in visualStateGroups)
-			{
-				if (group.Name == ButtonStatesName)
-					buttonStates = group;
+				return;
 			}
 
-			// no button states
-			if (buttonStates is null)
-				return (null, null);
+			Button? deleteButton = element.GetDescendantByName<Button>(DeleteButtonElementName);
 
-			// find and return the "ButtonVisible" state
-			foreach (var state in buttonStates.States)
+			if (deleteButton is not null)
 			{
-				if (state.Name == ButtonVisibleStateName)
-					return (buttonStates, state);
+				if (GetIsDeleteButtonEnabled(element))
+				{
+					deleteButton.RenderTransform = null;
+				}
+				else
+				{
+					deleteButton.RenderTransform = new UI.Xaml.Media.TranslateTransform() { Y = -1000 };
+				}
 			}
-
-			// no button visible state
-			return (null, null);
 		}
 	}
 }
