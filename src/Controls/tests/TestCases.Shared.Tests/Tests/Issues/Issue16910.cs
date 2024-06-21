4c231e8a7bbf2dc6e6a0d4e9c5e845d913bfdf75
Shane Neuville <shneuvil@microsoft.com>
2024-06-21 17:35:20 -0500

    Reenable RefreshView Tests

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16910.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue16910 : _IssuesUITest
+{
+	public override string Issue => "IsRefreshing binding works";
+
+	protected override bool ResetAfterEachTest => true;
+	public Issue16910(TestDevice device)
+		: base(device)
+	{ 
+
+	}
+
+    [Test]
+	public void BindingUpdatesFromProgrammaticRefresh()
+	{
+		_ = App.WaitForElement("StartRefreshing");
+		App.Tap("StartRefreshing");
+		App.WaitForElement("IsRefreshing");
+		App.Tap("StopRefreshing");
+	}
+
+// Windows only works with touch inputs which we don't have running on the test server
+#if !WINDOWS
+    [Test]
+	public void BindingUpdatesFromInteractiveRefresh()
+	{
+		_ = App.WaitForElement("CollectionView");
+		App.ScrollUp("CollectionView");
+		App.WaitForElement("IsRefreshing");
+		App.Tap("StopRefreshing");
+	}
+#endif
+
+}
