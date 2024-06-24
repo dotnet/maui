ba07c08577d6251d199b99b473d41622bc8ed1d4
Matthew Leibowitz <mattleibow@live.com>
2024-06-25 00:52:29 +0800

    Added some comments

--- src/Core/src/ViewExtensions.cs
+++ src/Core/src/ViewExtensions.cs
@@ -56,6 +56,10 @@ namespace Microsoft.Maui
 #endif
 
 #if ANDROID
+			// This is only here for Android because almost all Android views will require
+			// a wrapper when the view is InputTransparent. This is because Android does not
+			// have a concept of "not hit testable" so we have to emulate it intercepting the
+			// the touch events with a parent layout.
 			if (view?.InputTransparent == true && platformView is not IInputTransparentManagingView)
 				return true;
 #endif
