a53c762310b80dc565e9322b1e3b984e9bb27546
redth <jondick@gmail.com>
2024-06-27 12:43:03 -0400

    Update test for expected 1.0.0.0 assembly version

--- src/Controls/tests/Xaml.UnitTests/AssemblyInfoTests.cs
+++ src/Controls/tests/Xaml.UnitTests/AssemblyInfoTests.cs
@@ -37,7 +37,8 @@ namespace Microsoft.Maui.Controls.MSBuild.UnitTests
 		{
 			Assembly testAssembly = System.Reflection.Assembly.Load(assemblyName);
 			Version actual = testAssembly.GetName().Version;
-			Assert.AreEqual(9, actual.Major, actual.ToString());
+			// Currently we keep the assembly verison at 1.0.0.0
+			Assert.AreEqual(1, actual.Major, actual.ToString());
 			Assert.AreEqual(0, actual.Minor, actual.ToString());
 			Assert.AreEqual(0, actual.Build, actual.ToString());
 			Assert.AreEqual(0, actual.Revision, actual.ToString());
