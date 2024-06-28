034062f18d72102ab631e5fb8365608329d217d9
Shane Neuville <shneuvil@microsoft.com>
2024-06-28 14:46:19 -0500

    - fix CarV gallery

--- src/Controls/tests/TestCases.HostApp/Elements/CarouselView/CarouselViewGallery.cs
+++ src/Controls/tests/TestCases.HostApp/Elements/CarouselView/CarouselViewGallery.cs
@@ -5,62 +5,69 @@ using Microsoft.Maui.Controls.Internals;
 
 namespace Maui.Controls.Sample.CollectionViewGalleries.CarouselViewGalleries
 {
-	[Preserve(AllMembers = true)]
-	internal class CarouselViewGallery : ContentPage
+	public class CarouselViewGallery : NavigationPage
 	{
-		public CarouselViewGallery()
+		public CarouselViewGallery() : base(new CarouselViewGalleries())
 		{
-			var descriptionLabel =
-				new Label { Text = "CarouselView Galleries", Margin = new Thickness(2, 2, 2, 2) };
-
-			Title = "CarouselView Galleries";
-
-			Content = new ScrollView
+		}
+		
+		internal class CarouselViewGalleries : ContentPage
+		{
+			public CarouselViewGalleries()
 			{
-				Content = new StackLayout
+				var descriptionLabel =
+					new Label { Text = "CarouselView Galleries", Margin = new Thickness(2, 2, 2, 2) };
+
+				Title = "CarouselView Galleries";
+
+				Content = new ScrollView
 				{
-					Children =
+					Content = new StackLayout
 					{
-						descriptionLabel,
-						TestBuilder.NavButton("CarouselView", () =>
-							new CarouselViewPage(), Navigation),
-						TestBuilder.NavButton("CarouselView With Images", () =>
-							new CarouselViewPageWithImages(), Navigation),
-						TestBuilder.NavButton("CarouselView (Code, Horizontal)", () =>
-							new CarouselCodeGallery(ItemsLayoutOrientation.Horizontal), Navigation),
-						TestBuilder.NavButton("CarouselView (Code, Vertical)", () =>
-							new CarouselCodeGallery(ItemsLayoutOrientation.Vertical), Navigation),
-						TestBuilder.NavButton("CarouselView (XAML, Horizontal)", () =>
-							new CarouselXamlGallery(false), Navigation),
-						TestBuilder.NavButton("CarouselView (XAML, Horizontal, Loop)", () =>
-							new CarouselXamlGallery(true), Navigation),
-						TestBuilder.NavButton("CarouselView (Indicators Forms)", () =>
-							new CarouselItemsGallery(), Navigation),
-						TestBuilder.NavButton("CarouselView (Indicators Default (Native))", () =>
-							new CarouselItemsGallery(useNativeIndicators: true), Navigation),
-						TestBuilder.NavButton("CarouselView Async", () =>
-							new CarouselItemsGallery(setCollectionWithAsync:true, useNativeIndicators: true), Navigation),
-	  					TestBuilder.NavButton("CarouselView Snap", () =>
- 							new CarouselSnapGallery(), Navigation),
-						TestBuilder.NavButton("ObservableCollection and CarouselView", () =>
- 							new CollectionCarouselViewGallery(), Navigation),
-						TestBuilder.NavButton("CarouselView EmptyView", () =>
-  							new EmptyCarouselGallery(), Navigation),
-						TestBuilder.NavButton("IndicatorView", () =>
-  							new IndicatorCodeGallery(), Navigation),
-						TestBuilder.NavButton("CarouselView SetPosition Ctor", () =>
-							new CarouselItemsGallery(useNativeIndicators: true, setPositionOnConstructor: true), Navigation),
-						TestBuilder.NavButton("CarouselView SetPosition Appearing", () =>
-							new CarouselItemsGallery(useNativeIndicators: true, setPositionOnAppearing: true), Navigation),
-						TestBuilder.NavButton("CarouselView loop", () =>
-							new CarouselXamlGallery(true), Navigation),
-						TestBuilder.NavButton("CarouselView Set CurrentItem", () =>
-							new CarouselXamlGallery(false, 3), Navigation),
-						TestBuilder.NavButton("CarouselView Set CurrentItem Loop", () =>
-							new CarouselXamlGallery(true, 3), Navigation),
+						Children =
+						{
+							descriptionLabel,
+							TestBuilder.NavButton("CarouselView", () =>
+								new CarouselViewPage(), Navigation),
+							TestBuilder.NavButton("CarouselView With Images", () =>
+								new CarouselViewPageWithImages(), Navigation),
+							TestBuilder.NavButton("CarouselView (Code, Horizontal)", () =>
+								new CarouselCodeGallery(ItemsLayoutOrientation.Horizontal), Navigation),
+							TestBuilder.NavButton("CarouselView (Code, Vertical)", () =>
+								new CarouselCodeGallery(ItemsLayoutOrientation.Vertical), Navigation),
+							TestBuilder.NavButton("CarouselView (XAML, Horizontal)", () =>
+								new CarouselXamlGallery(false), Navigation),
+							TestBuilder.NavButton("CarouselView (XAML, Horizontal, Loop)", () =>
+								new CarouselXamlGallery(true), Navigation),
+							TestBuilder.NavButton("CarouselView (Indicators Forms)", () =>
+								new CarouselItemsGallery(), Navigation),
+							TestBuilder.NavButton("CarouselView (Indicators Default (Native))", () =>
+								new CarouselItemsGallery(useNativeIndicators: true), Navigation),
+							TestBuilder.NavButton("CarouselView Async", () =>
+								new CarouselItemsGallery(setCollectionWithAsync:true, useNativeIndicators: true), Navigation),
+							TestBuilder.NavButton("CarouselView Snap", () =>
+								new CarouselSnapGallery(), Navigation),
+							TestBuilder.NavButton("ObservableCollection and CarouselView", () =>
+								new CollectionCarouselViewGallery(), Navigation),
+							TestBuilder.NavButton("CarouselView EmptyView", () =>
+								new EmptyCarouselGallery(), Navigation),
+							TestBuilder.NavButton("IndicatorView", () =>
+								new IndicatorCodeGallery(), Navigation),
+							TestBuilder.NavButton("CarouselView SetPosition Ctor", () =>
+								new CarouselItemsGallery(useNativeIndicators: true, setPositionOnConstructor: true), Navigation),
+							TestBuilder.NavButton("CarouselView SetPosition Appearing", () =>
+								new CarouselItemsGallery(useNativeIndicators: true, setPositionOnAppearing: true), Navigation),
+							TestBuilder.NavButton("CarouselView loop", () =>
+								new CarouselXamlGallery(true), Navigation),
+							TestBuilder.NavButton("CarouselView Set CurrentItem", () =>
+								new CarouselXamlGallery(false, 3), Navigation),
+							TestBuilder.NavButton("CarouselView Set CurrentItem Loop", () =>
+								new CarouselXamlGallery(true, 3), Navigation),
+						}
 					}
-				}
-			};
+				};
+			}
 		}
 	}
+	
 }
