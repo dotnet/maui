a5f3689e9fd886d494b8691a5f475f9ca0d7ed83
Lennart Kroneman <lennart.kroneman@daveramsey.com>
2024-07-03 13:19:50 -0500

    add test case for Issue23399

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue23399.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 23399, "Closing Modal While App is Backgrounded Fails", PlatformAffected.Android)]
+	public class Issue23399: NavigationPage
+	{
+		
+		public Issue23399() : base(new TestPage())
+		{
+		}
+		
+		public class TestPage : TestContentPage
+		{
+			protected override void Init()
+			{
+				Content = new VerticalStackLayout()
+				{
+					new Button()
+					{
+						Text = "Open Modal",
+						AutomationId = "OpenModal",
+						Command = new Command(async () =>
+						{
+							await this.Navigation.PushModalAsync(CreateDestinationPage());
+						})
+					}
+				};
+			}
+			
+			ContentPage CreateDestinationPage()
+			{
+				return new ContentPage()
+				{
+					Title = "Test",
+					Content = new VerticalStackLayout()
+					{
+						new Button()
+						{
+							AutomationId = "StartCloseModal",
+							Text = "Close Modal",
+							Command = new Command(()=>
+							{
+								this.Navigation.PopModalAsync();
+							})
+						}
+					}
+				};
+			}
+		}
+	}
+}
