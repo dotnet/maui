72198abd00091846d5ce8089b25914286e2ecfe2
Jakub Florkowski <kubaflo123@gmail.com>
2024-07-03 19:33:50 +0200

    Update Issue22289.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22289.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22289.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.AppiumTests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues
 {
 	public class Issue22289 : _IssuesUITest
 	{
