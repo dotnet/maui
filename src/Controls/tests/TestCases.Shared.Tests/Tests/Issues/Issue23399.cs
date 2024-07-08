a964d40c15626a76cfbc7738b18122de818b33a9
Lennart Kroneman <lennart.kroneman@daveramsey.com>
2024-07-08 07:52:13 -0500

    add category on the test and remove catch block

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue23399.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue23399.cs
@@ -10,28 +10,17 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		public override string Issue => "Closing Modal While App is Backgrounded Fails"; 
 
 		[Test]
+		[Category(UITestCategories.Navigation)]
 		public void MakingFragmentRelatedChangesWhileAppIsBackgroundedFails()
 		{
-			try
-			{
-				App.WaitForElement("OpenModal");
-				App.Tap("OpenModal");
-				App.WaitForElement("StartCloseModal");
-				App.Tap("StartCloseModal");
-				App.BackgroundApp();
-				App.WaitForNoElement("StartCloseModal");
-				App.ForegroundApp();
-				App.WaitForElement("OpenModal");
-			}
-			catch
-			{
-				SaveUIDiagnosticInfo();
-
-				// Just in case these tests leave the app in an unreliable state
-				App.ResetApp();
-				FixtureSetup();
-				throw;
-			}
+			App.WaitForElement("OpenModal");
+			App.Tap("OpenModal");
+			App.WaitForElement("StartCloseModal");
+			App.Tap("StartCloseModal");
+			App.BackgroundApp();
+			App.WaitForNoElement("StartCloseModal");
+			App.ForegroundApp();
+			App.WaitForElement("OpenModal");
 		}
 	}
 }
