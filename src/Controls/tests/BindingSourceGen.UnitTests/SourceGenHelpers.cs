6ccd241f6df1a3dc670136e2f1ab679acaac4144
Jeremi Kurdek <jkurdek@gmail.com>
2024-07-16 16:42:46 +0200

    Add warning when referencing results of other source generators

--- src/Controls/tests/BindingSourceGen.UnitTests/SourceGenHelpers.cs
+++ src/Controls/tests/BindingSourceGen.UnitTests/SourceGenHelpers.cs
@@ -4,6 +4,7 @@ using System.Runtime.Loader;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.Maui.Controls.BindingSourceGen;
+using Xunit;
 
 
 internal record CodeGeneratorResult(
@@ -20,28 +21,32 @@ internal static class SourceGenHelpers
 
     internal static List<string> StepsForComparison = [TrackingNames.Bindings, TrackingNames.BindingsWithDiagnostics];
 
-    internal static CSharpGeneratorDriver CreateDriver()
+    internal static CSharpGeneratorDriver CreateDriver(IEnumerable<ISourceGenerator> generators)
     {
-        var generator = new BindingSourceGenerator();
-        var sourceGenerator = generator.AsSourceGenerator();
         return CSharpGeneratorDriver.Create(
-            [sourceGenerator],
+            generators: generators,
             driverOptions: new GeneratorDriverOptions(disabledOutputs: IncrementalGeneratorOutputKind.None, trackIncrementalGeneratorSteps: true),
             parseOptions: ParseOptions);
     }
 
     internal static CodeGeneratorResult Run(string source)
     {
+        return Run(source, new[] { new BindingSourceGenerator() });
+    }
+
+    internal static CodeGeneratorResult Run(string source, IEnumerable<IIncrementalGenerator> generators)
+    {
+        // Function assumes the first generator in a list is BindingSourceGenerator
+        Assert.NotEmpty(generators);
+        Assert.IsType<BindingSourceGenerator>(generators.First());
+
         var inputCompilation = CreateCompilation(source);
-        var driver = CreateDriver();
+        var driver = CreateDriver(generators.Select(g => g.AsSourceGenerator()));
 
-        var result = driver.RunGeneratorsAndUpdateCompilation(inputCompilation, out Compilation compilation, out _).GetRunResult().Results.Single();
-
-        var generatedCodeDiagnostic = compilation.GetDiagnostics();
-        var generatedCode = result.GeneratedSources.Length == 1 ? result.GeneratedSources.Single().SourceText.ToString() : "";
+        var result = driver.RunGeneratorsAndUpdateCompilation(inputCompilation, out Compilation compilation, out _).GetRunResult().Results.FirstOrDefault();
+        var generatedCode = result.GeneratedSources.FirstOrDefault().SourceText.ToString();
 
         var trackedSteps = result.TrackedSteps;
-
         var resultBinding = trackedSteps.TryGetValue("Bindings", out ImmutableArray<IncrementalGeneratorRunStep> value)
             ? (BindingInvocationDescription)value[0].Outputs[0].Value
             : null;
@@ -50,7 +55,7 @@ internal static class SourceGenHelpers
             GeneratedFiles: result.GeneratedSources.ToDictionary(source => source.HintName, source => source.SourceText.ToString()),
             SourceCompilationDiagnostics: inputCompilation.GetDiagnostics(),
             SourceGeneratorDiagnostics: result.Diagnostics,
-            GeneratedCodeCompilationDiagnostics: generatedCodeDiagnostic,
+            GeneratedCodeCompilationDiagnostics: compilation.GetDiagnostics(),
             Binding: resultBinding);
     }
 
@@ -73,7 +78,7 @@ internal static class SourceGenHelpers
 
     internal static Compilation CreateCompilation(Dictionary<string, string> sources)
     {
-        var syntaxTrees = sources.Select(s => CSharpSyntaxTree.ParseText(s.Value, ParseOptions, path: s.Key)).ToList(); 
+        var syntaxTrees = sources.Select(s => CSharpSyntaxTree.ParseText(s.Value, ParseOptions, path: s.Key)).ToList();
         return CreateCompilationFromSyntaxTrees(syntaxTrees);
     }
 }
