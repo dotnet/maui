cb0a332557641bf01de5d10d08ce188b5f3a5989
Stephane Delcroix <stephane@delcroix.org>
2024-07-18 01:47:13 +0200

    [X] do not apply Bindings if DataType doesnt match (#22056)
    * [X] do not apply Bindings if DataType doesnt match
    
    Make sure the behavior of bindings is consistent accross implementations
    (compiled/not compiled)
    
    fixes a bunch of issues
    
    * fixes
    
    * send binding diagnostic

--- src/Controls/src/Core/Binding.cs
+++ src/Controls/src/Core/Binding.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.Reflection;
+using Microsoft.Maui.Controls.Xaml.Diagnostics;
 
 namespace Microsoft.Maui.Controls
 {
@@ -105,6 +106,8 @@ namespace Microsoft.Maui.Controls
 			}
 		}
 
+		internal Type DataType { get; set; }
+
 		internal override void Apply(bool fromTarget)
 		{
 			base.Apply(fromTarget);
@@ -120,7 +123,14 @@ namespace Microsoft.Maui.Controls
 			object src = _source;
 			var isApplied = IsApplied;
 
-			base.Apply(src ?? context, bindObj, targetProperty, fromBindingContextChanged, specificity);
+			var bindingContext = src ?? Context ?? context;
+			if (DataType != null && bindingContext != null && !DataType.IsAssignableFrom(bindingContext.GetType()))
+			{
+				BindingDiagnostics.SendBindingFailure(this, "Binding", "Mismatch between the specified x:DataType and the current binding context");
+				bindingContext = null;
+			}
+
+			base.Apply(bindingContext, bindObj, targetProperty, fromBindingContextChanged, specificity);
 
 			if (src != null && isApplied && fromBindingContextChanged)
 				return;
@@ -131,7 +141,6 @@ namespace Microsoft.Maui.Controls
 			}
 			else
 			{
-				object bindingContext = src ?? Context ?? context;
 				if (_expression == null)
 					_expression = new BindingExpression(this, SelfPath);
 				_expression.Apply(bindingContext, bindObj, targetProperty, specificity);
