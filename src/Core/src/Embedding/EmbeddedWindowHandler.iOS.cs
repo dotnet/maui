d3efab83ca3090608fbfddfc732145855eb3b9b3
Matthew Leibowitz <mattleibow@live.com>
2024-07-20 01:32:10 +0800

    Update EmbeddedWindowHandler.iOS.cs

--- src/Core/src/Embedding/EmbeddedWindowHandler.iOS.cs
+++ src/Core/src/Embedding/EmbeddedWindowHandler.iOS.cs
@@ -1,21 +1,19 @@
-﻿//using Microsoft.Maui.Graphics;
-//using Microsoft.UI.Windowing;
+﻿using Microsoft.Maui.Graphics;
+using Microsoft.UI.Windowing;
 
-//using PlatformWindow = Microsoft.UI.Xaml.Window;
+using PlatformWindow = Microsoft.UI.Xaml.Window;
 
-//namespace Microsoft.Maui.Embedding;
+namespace Microsoft.Maui.Embedding;
 
-//internal partial class EmbeddedWindowHandler
-//{
-//	protected override void ConnectHandler(PlatformWindow platformView)
-//	{
-//		base.ConnectHandler(platformView);
+internal partial class EmbeddedWindowHandler
+{
+	protected override void ConnectHandler(PlatformWindow platformView)
+	{
+		base.ConnectHandler(platformView);
+	}
 
-//	}
-
-//	protected override void DisconnectHandler(PlatformWindow platformView)
-//	{
-
-//		base.DisconnectHandler(platformView);
-//	}
-//}
+	protected override void DisconnectHandler(PlatformWindow platformView)
+	{
+		base.DisconnectHandler(platformView);
+	}
+}
