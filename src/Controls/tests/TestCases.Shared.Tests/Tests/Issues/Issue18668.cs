5b0c10398baee341fc798ef3c1bf2a7a867227b0
Jakub Florkowski <kubaflo123@gmail.com>
2024-07-20 01:25:13 +0200

    Added a UI Test (#18668)

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18668.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue18668 : _IssuesUITest
+	{
+		public override string Issue => "Visual state change for disabled RadioButton";
+
+		public Issue18668(TestDevice device) : base(device){ }
+
+		[Test]
+		[Category(UITestCategories.RadioButton)]
+		public void TestIssue18668()
+		{
+			App.WaitForElement("button");
+			App.Click("button");
+
+			// The test passes if the radio button is visually disabled
+			VerifyScreenshot();
+		}
+	}
+}
