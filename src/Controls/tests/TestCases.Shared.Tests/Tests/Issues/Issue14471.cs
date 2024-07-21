6f232805f451a460e36411271eaef7b54d93b65d
Jakub Florkowski <kubaflo123@gmail.com>
2024-07-21 03:05:51 +0200

    Added a UI Test (#14471)

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue14471.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue14471 : _IssuesUITest
+{
+	public Issue14471(TestDevice device) : base(device){ }
+
+	public override string Issue => "Image can disappear when going back to the page";
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void Issue14471Test()
+	{
+		App.WaitForElement("image");
+		App.Click("switchToTab2Button");
+		App.WaitForElement("switchToTab1Button");
+		App.Click("switchToTab1Button");
+		App.WaitForElement("image");
+
+		// The test passes if image is loaded
+		VerifyScreenshot();
+	}
+}
