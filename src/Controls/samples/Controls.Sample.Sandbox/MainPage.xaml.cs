737d001170c2b78c92ef5f2afe2acf8fc10aa5f8
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-07-27 12:23:44 +0200

    WIP

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,9 +1,44 @@
-﻿namespace Maui.Controls.Sample;
+﻿using Microsoft.Maui.Graphics.Platform;
+
+namespace Maui.Controls.Sample;
 
 public partial class MainPage : ContentPage
 {
+	private int counter;
+
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+
+	private void NewWindowButton_Clicked(object sender, EventArgs e)
+	{
+		counter++;
+		TestPage page = new()
+		{
+			Description = $"#{counter}"
+		};
+
+		Window secondWindow = new Window(page);
+		Application.Current!.OpenWindow(secondWindow);
+	}
+
+	private void ActivateWindow2_Clicked(object sender, EventArgs e)
+	{
+		IReadOnlyList<Window> windows = Application.Current!.Windows;
+
+		debugInfo.Text += "#0 ";
+
+		int windowNumber = int.Parse(windowToActivate.Text);
+		int windowIndex = windowNumber - 1;
+
+		debugInfo.Text += $"windows.count {windows.Count} ?? {windowNumber}";	
+
+		if (windows.Count >= windowNumber)
+		{
+			Window windowToActivate = windows[windowIndex];
+			Application.Current!.ActivateWindow(windowToActivate);
+		}
+	}
+
 }
