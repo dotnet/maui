3c6021fc2b6eb55759f307bd7d8d96d8ec365b81
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-07-27 12:23:44 +0200

    WIP

---
+++ src/Controls/samples/Controls.Sample.Sandbox/Platforms/MacCatalyst/SceneDelegate.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
+using Foundation;
+using Microsoft.Maui;
+using UIKit;
+
+namespace Maui.Controls.Sample.Platform;
+
+[Register("SceneDelegate")]
+public class SceneDelegate : MauiUISceneDelegate
+{
+}
---
+++ src/Controls/samples/Controls.Sample.Sandbox/TestPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Maui.Controls.Sample;
+
+public partial class TestPage : ContentPage
+{
+	public string? Description { get; set; } = "N/A";
+
+	public TestPage()
+	{
+		BindingContext = this;
+		InitializeComponent();
+	}
+
+}
