42db6a22de23c3f387af46ffd9041386e85b39f9
Alberto Aldegheri <albyrock87@gmail.com>
2024-08-02 19:14:37 +0200

    Simplify logging and freeze only if there's a drawable

--- src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/glide/GlideLogging.java
+++ src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/glide/GlideLogging.java
@@ -6,25 +6,13 @@ public class GlideLogging {
     private static final String TAG = "Glide";
     private static final boolean IS_VERBOSE_LOGGABLE = Log.isLoggable(TAG, Log.VERBOSE);
 
-    private final String context;
-
-    public GlideLogging(String context) {
-        this.context = context;
-    }
-
-    public void v(String message) {
-        if (IS_VERBOSE_LOGGABLE) {
-            Log.v(TAG, context + " => " + message);
-        }
-    }
-
     public static boolean isVerboseLoggable() {
         return IS_VERBOSE_LOGGABLE;
     }
 
-    public static void v(String context, String message) {
+    public static void v(String message) {
         if (IS_VERBOSE_LOGGABLE) {
-            Log.v(TAG, context + " => " + message);
+            Log.v(TAG, message);
         }
     }
 }
--- src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/glide/MauiCustomViewTarget.java
+++ src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/glide/MauiCustomViewTarget.java
@@ -22,12 +22,10 @@ public class MauiCustomViewTarget extends CustomViewTarget<ImageView, Drawable> 
     private final PlatformAppCompatImageView platformView;
     private boolean destroyed;
     private boolean failed;
-    private GlideLogging logger;
 
     public MauiCustomViewTarget(@NonNull ImageView view, ImageLoaderCallback callback) {
         super(view);
 
-        this.logger = new GlideLogging("MauiCustomViewTarget");
         this.callback = callback;
         this.platformView = view instanceof PlatformAppCompatImageView ? (PlatformAppCompatImageView) view : null;
     }
@@ -35,10 +33,10 @@ public class MauiCustomViewTarget extends CustomViewTarget<ImageView, Drawable> 
     @Override
     protected void onResourceCleared(@Nullable Drawable placeholder) {
         if (destroyed || platformView == null) {
-            logger.v("onResourceCleared: setImageDrawable(placeholder)");
+            GlideLogging.v("onResourceCleared: setImageDrawable(placeholder)");
             view.setImageDrawable(placeholder);
-        } else if (platformView != null) {
-            logger.v("onResourceCleared: freeze()");
+        } else if (platformView != null && platformView.getDrawable() != null) {
+            GlideLogging.v("onResourceCleared: freeze()");
             // if we're switching the image with another one, don't set the empty placeholder (null)
             // and simply freeze the view to prevent it from accessing the bitmap which is about to be recycled
             // See more: https://bumptech.github.io/glide/javadocs/4140/library/com.bumptech.glide.request.target/-target/on-load-cleared.html
@@ -46,7 +44,7 @@ public class MauiCustomViewTarget extends CustomViewTarget<ImageView, Drawable> 
         }
         
         if (this.destroyed && !this.failed) {
-            logger.v("onResourceCleared: setIsImageRecycled(true)");
+            GlideLogging.v("onResourceCleared: setIsImageRecycled(true)");
             PlatformInterop.setGlideClearedTag(this.view, true);
         }
     }
@@ -58,28 +56,28 @@ public class MauiCustomViewTarget extends CustomViewTarget<ImageView, Drawable> 
 
     @Override
     public void onLoadFailed(@Nullable Drawable errorDrawable) {
-        logger.v("onLoadFailed: errorDrawable");
+        GlideLogging.v("onLoadFailed: errorDrawable");
         this.failed = true;
         view.setImageDrawable(errorDrawable);
         PlatformInterop.setGlideClearedTag(view, false);
 
         // trigger the callback out of this target
         post(() -> {
-            logger.v("onLoadFailed: callback.onComplete(false)");
+            GlideLogging.v("onLoadFailed: callback.onComplete(false)");
             callback.onComplete(false, errorDrawable, this::clear);
         });
     }
 
     @Override
     public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
-        logger.v("onResourceReady: " + resource);
+        GlideLogging.v("onResourceReady(resource)");
 
         // set the image
         view.setImageDrawable(resource);
         PlatformInterop.setGlideClearedTag(this.view, false);
 
         post(() -> {
-            logger.v("onResourceReady: callback.onComplete(true)");
+            GlideLogging.v("onResourceReady: callback.onComplete(true)");
             callback.onComplete(true, resource, this::clear);
         });
     }
