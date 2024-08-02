63cfcce79f26b82fdaed0b153276fc1e0674b149
Rui Marinho <me@ruimarinho.net>
2024-08-02 14:40:49 +0100

    Try more build

--- build.cmd
+++ build.cmd
@@ -1,6 +1,2 @@
-@ECHO OFF
-SETLOCAL
-PowerShell -NoProfile -NoLogo -ExecutionPolicy Bypass -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = ''; try { & '%~dp0build.ps1' %*; exit $LASTEXITCODE } catch { write-host $_; exit 1 }"
-SET exit_code=%ERRORLEVEL%
-ECHO build.cmd completed
-EXIT /b %exit_code%
+@echo off
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build %*"
