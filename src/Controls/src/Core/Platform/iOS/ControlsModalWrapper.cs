c50cc25f678ba83f3fddfb01afb9cf55cee01aad
Piers Deseilligny <42496508+piersdeseilligny@users.noreply.github.com>
2024-08-02 17:05:24 +0100

    Cleanup PR

--- src/Controls/src/Core/Platform/iOS/ControlsModalWrapper.cs
+++ src/Controls/src/Core/Platform/iOS/ControlsModalWrapper.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
 using Foundation;
 using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
 using Microsoft.Maui.Graphics;
-using Microsoft.Maui.Graphics.Platform;
 using UIKit;
 
 namespace Microsoft.Maui.Controls.Platform
