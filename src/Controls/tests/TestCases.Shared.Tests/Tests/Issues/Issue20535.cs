ebac3393fd5bd41b95f6a35bd702e079c613e942
Gerald Versluis <gerald.versluis@microsoft.com>
2024-08-08 20:32:42 +0200

    Update Issue20535.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20535.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20535.cs
@@ -2,26 +2,25 @@
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.AppiumTests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue20535 : _IssuesUITest
 {
-	public class Issue20535 : _IssuesUITest
+	public Issue20535(TestDevice device) : base(device) { }
+
+	public override string Issue => "Re-enable/move TrackColorInitializesCorrectly/TrackColorUpdatesCorrectly to Appium";
+
+	[Test]
+	public async Task SwitchTrackColorTest()
 	{
-		public Issue20535(TestDevice device) : base(device) { }
+		App.WaitForElement("WaitForStubControl");
 
-		public override string Issue => "Re-enable/move TrackColorInitializesCorrectly/TrackColorUpdatesCorrectly to Appium";
+		// 1. Update the state of the Switch by updating the OnColor property.
+		// In this way, we validate the initial value but also the update.
+		App.Click("UpdateOnColorSwitch");
+		await Task.Delay(500); // Wait for the Thumb animation to complete
 
-		[Test]
-		public async Task SwitchTrackColorTest()
-		{
-			App.WaitForElement("WaitForStubControl");
-
-			// 1. Update the state of the Switch by updating the OnColor property.
-			// In this way, we validate the initial value but also the update.
-			App.Click("UpdateOnColorSwitch");
-			await Task.Delay(500); // Wait for the Thumb animation to complete
-
-			// 2. Verify the result.
-			VerifyScreenshot();
-		}
+		// 2. Verify the result.
+		VerifyScreenshot();
 	}
 }
