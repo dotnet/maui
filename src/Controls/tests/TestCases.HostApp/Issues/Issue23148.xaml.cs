caba738e1ea9ab3e5452237f9e0c2db844b13c1a
Suthi <suthi.yuvaraj@syncfusion.com>
2024-08-08 18:01:25 +0530

    Commit for UItest 23148

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue23148.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Controls.TestCases.HostApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 23148, "Incorrect height of CollectionView when ItemsSource is empty", PlatformAffected.Android)]
+
+	public partial class Issue23148 : ContentPage
+	{
+		public ObservableCollection<string> Items { get; set; } = [];
+
+		public ICommand AddCommand => new Command(() => Items.Add("Item"));
+
+		public Issue23148()
+		{
+			InitializeComponent();
+			BindingContext = this;
+		}
+	}
+}
