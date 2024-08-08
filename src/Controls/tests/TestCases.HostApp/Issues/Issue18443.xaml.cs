6b7d48198d2144ee974f395690fc5709dff20f79
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2024-08-08 16:47:41 +0530

    Added UI test for 18443

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue18443.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 18443, "SelectionLength Property Not Applied to Entry at Runtime", PlatformAffected.Android)]
+	public partial class Issue18443 : ContentPage
+	{
+		public Issue18443()
+		{
+			InitializeComponent();
+			entry.Focused += (sender, e) =>
+			{
+				entry.SelectionLength = 3;
+			};
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18443.cs
@@ -0,0 +1,22 @@
+﻿#if !MACCATALYST && !IOS
+using NUnit.Framework;
+using UITest.Core;
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue18443 : _IssuesUITest
+	{
+		public Issue18443(TestDevice device) : base(device) { }
+
+		public override string Issue => "[Android] SelectionLength Property Not Applied to Entry at Runtime";
+
+		[Test]
+		[Category(UITestCategories.Entry)]
+		public async Task EntrySelectionLengthRuntimeUpdate()
+		{
+			await Task.Delay(500);
+			VerifyScreenshot();
+		}
+
+	}
+}
+#endif
