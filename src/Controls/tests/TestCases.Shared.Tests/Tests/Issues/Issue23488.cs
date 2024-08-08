c2694d4ad9a49e2f778d024ec21bb4a5f83d1254
Vignesh-SF3580 <vignesh.nagarajan@syncfusion.com>
2024-08-08 16:37:12 +0530

    added UITest - 23488

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue23488.cs
@@ -0,0 +1,24 @@
+﻿#if !MACCATALYST
+using NUnit.Framework;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Tests.Issues
+{
+    public class Issue23488 : _IssuesUITest
+    {
+		public Issue23488(TestDevice device): base(device)
+		{
+		}
+
+		public override string Issue => "Span text-decoration is incorrect whereas the Label behaves properly";
+
+		[Test]
+		[Category(UITestCategories.Label)]
+		public async Task LabelHyperlinkUnderlineColor()
+		{
+			await Task.Delay(500);
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
