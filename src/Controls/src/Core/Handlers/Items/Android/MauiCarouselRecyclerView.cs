9abce61f94749e1b3d7b8246042a070a81c5b8b5
AhamedAliNishad <ahamedalinishad.j@syncfusion.com>
2024-08-12 20:02:29 +0530

    Fix for the Carousel Position not updated properly with loop enabled in Android.

--- src/Controls/src/Core/Handlers/Items/Android/MauiCarouselRecyclerView.cs
+++ src/Controls/src/Core/Handlers/Items/Android/MauiCarouselRecyclerView.cs
@@ -17,6 +17,7 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 		bool _initialized;
 		bool _isVisible;
 		bool _disposed;
+		bool _isCurrentItemNull;
 
 		List<View> _oldViews;
 		CarouselViewOnGlobalLayoutListener _carouselViewLayoutListener;
@@ -133,12 +134,25 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			if (oldItemViewAdapter != null && _initialized)
 			{
 				ItemsView.SetValueFromRenderer(CarouselView.PositionProperty, 0);
-				ItemsView.SetValueFromRenderer(CarouselView.CurrentItemProperty, null);
 			}
 
 			_gotoPosition = -1;
 
 			base.UpdateAdapter();
+
+			// we can set the current item here, if suppose dynamically set itemsource as null and reload again with some items.
+			if (oldItemViewAdapter != null && _initialized && Carousel.Loop && _isCurrentItemNull)
+			{
+				SetCurrentItem(Carousel.Position);
+				_isCurrentItemNull = false;
+			}
+
+			//we can null the CurrentItemProperty only if we don't have any items
+			if (ItemsViewAdapter == null || ItemsViewAdapter?.ItemsSource?.Count == 0)
+			{
+				ItemsView.SetValueFromRenderer(CarouselView.CurrentItemProperty, null);
+				_isCurrentItemNull = true;
+			}
 
 			UpdateInitialPosition();
 
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17283.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    internal class Issue17283 : _IssuesUITest
+	{
+		public override string Issue => "[Android] CarouselView doesn't scroll to the right Position after changing the ItemSource with Loop enabled";
+
+		public Issue17283(TestDevice testDevice) : base(testDevice) { }
+
+		[Test]
+		[Category(UITestCategories.CarouselView)]
+		public void Issue17283Test()
+		{
+			_ = App.WaitForElement("goToLastItemButton");
+			App.Click("goToLastItemButton");
+			App.WaitForElement("5");
+			App.Click("reloadItemsButton");
+			App.WaitForElement("5last");
+		}
+	}
+}
