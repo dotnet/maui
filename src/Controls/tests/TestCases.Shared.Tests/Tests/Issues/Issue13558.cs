dd98ab220c7bd2fbff06283bd679619207c200a8
KarthikRajaKalaimani <karthikraja.kalaimani@syncfusion.com>
2024-08-13 17:52:07 +0530

    fix-13558 Picker in ListView works on Windows but not on Android

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13558.cs
@@ -0,0 +1,25 @@
+﻿#if ANDROID
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue13558 : _IssuesUITest
+	{
+		public Issue13558(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Picker in ListView works on Windows but not on Android";
+
+		[Test]
+		[Category(UITestCategories.ListView)]
+		public void Issue13558Test()
+		{
+			App.WaitForElement("ListViewId");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
