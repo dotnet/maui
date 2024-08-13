69935596077556529b32cb926ca5f143a6c8602c
Nanthini Mahalingam <nanthini.mahalingam@syncfusion.com>
2024-08-13 15:32:31 +0530

    Added the UI test for collection view group footer template crash issue.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18702.cs
@@ -0,0 +1,29 @@
+﻿#if WINDOWS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue18702 : _IssuesUITest
+	{
+		const string element = "collectionView";
+
+		public Issue18702(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "The CollectionView has a grouped footer template that crashes the application";
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		[Category(UITestCategories.Compatibility)]
+		[FailsOnIOS]
+		public void CollectionViewGroupedFooterTemplateShouldNotCrash()
+		{
+			App.WaitForElement(element);
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
