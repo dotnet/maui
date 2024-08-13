4eec2443cd747936f2b7be7160271b3dc5648f3d
Nanthini Mahalingam <nanthini.mahalingam@syncfusion.com>
2024-08-13 13:02:28 +0530

    Added the UI test file.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue14708.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 14708, "[Bug][Android] Investigate SearchBar presentation in horizontal screen orientation",
+		PlatformAffected.Android)]
+	public partial class Issue14708 : ContentPage
+	{
+		public Issue14708()
+		{
+			InitializeComponent();
+
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue14708.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue14708 : _IssuesUITest
+	{
+		public override string Issue => "Search bar taken whole screen space when it aligns in landscape orientation";
+
+		public Issue14708(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.SearchBar)]
+		public void SearchBarScreenSpaceUpdatingAtLandscape()
+		{
+			App.WaitForElement("WaitForSearchBarControl");
+			App.SetOrientationLandscape();
+			Task.Delay(1000); // Wait to complete the device rotation animation.
+			var rtlEntryRect = App.FindElement("WaitForSearchBarControl").GetRect();
+			// Set focus
+			App.Click(rtlEntryRect.X, rtlEntryRect.Y);
+
+			// Tap Search Button at right side of search bar
+			var margin = 30;
+			App.Click(rtlEntryRect.Width + margin, rtlEntryRect.Y + margin);
+			VerifyScreenshot();
+
+		}
+	}
+}
