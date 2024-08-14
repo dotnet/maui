c9099a8ba3bda4e1eb40c290e3ec610a70bd0736
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2024-08-14 18:55:07 +0530

    Fix Stepper doesn't change increment value when being bound to a double in MVVM context (Windows)

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20706.cs
@@ -0,0 +1,30 @@
+﻿#if WINDOWS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue20706 : _IssuesUITest
+	{
+		public override string Issue => "Stepper doesn't change increment value when being bound to a double in MVVM context (Windows)";
+		public Issue20706(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.Stepper)]
+		public void ChangeIncrementValue()
+		{
+			App.WaitForElement("stepper");
+			App.Tap("label1");
+			App.TapCoordinates(34, 20);
+			App.WaitForElement("entry");
+			VerifyScreenshot();
+			App.Tap("label10");
+			App.WaitForElement("entry");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
