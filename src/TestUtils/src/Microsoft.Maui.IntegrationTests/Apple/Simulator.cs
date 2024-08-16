7b28251d618ed434b4c0378d7fb2e46fac527d95
Shane Neuville <shneuvil@microsoft.com>
2024-08-15 20:07:00 -0500

    - logs

--- src/TestUtils/src/Microsoft.Maui.IntegrationTests/Apple/Simulator.cs
+++ src/TestUtils/src/Microsoft.Maui.IntegrationTests/Apple/Simulator.cs
@@ -41,6 +41,7 @@ namespace Microsoft.Maui.IntegrationTests.Apple
 			var simUDID = GetUDID();
 
 			var timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+			Console.WriteLine($"Logging simulator {simUDID} to {logDirectory}");
 			StartProcess("zip", $"-9r \"{logDirectory}/DiagnosticReports_{simUDID}_{timestamp}.zip\" \"{homeDirectory}/Library/Logs/DiagnosticReports/\"");
 			StartProcess("zip", $"-9r \"{logDirectory}/CoreSimulator_{simUDID}_{timestamp}.zip\" \"{homeDirectory}/Library/Logs/CoreSimulator/{simUDID}\"");
 			StartProcess("xcrun", $"simctl spawn {simUDID} log collect --output {homeDirectory}/{simUDID}_log_{timestamp}.logarchive");
