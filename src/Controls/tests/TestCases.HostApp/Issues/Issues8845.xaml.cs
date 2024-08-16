aa719471f6cb49fd118cbf99f5969f85d28c28ca
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2024-08-16 15:05:39 +0530

    fix-8845-Picker on windows shows "Microsoft.Maui.Controls.Picker" if ItemsSource has an empty string

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issues8845.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 8845, "Picker on windows shows \"Microsoft.Maui.Controls.Picker\" if ItemsSource has an empty string", PlatformAffected.UWP)]
+	public partial class Issue8845 : ContentPage
+	{
+		public Issue8845()
+		{
+			InitializeComponent();
+			picker.ItemsSource =
+				new List<object>
+				{
+					new { DisplayName = (string)null!},
+					new { DisplayName = "Not null"},
+				};
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			picker.SelectedIndex = 0;
+		}
+
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue8845.cs
@@ -0,0 +1,26 @@
+﻿#if WINDOWS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue8845 : _IssuesUITest
+	{
+		public Issue8845(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Picker on windows shows \"Microsoft.Maui.Controls.Picker\" if ItemsSource has an empty string"; 
+		
+		[Test]
+		[Category(UITestCategories.Picker)]
+		public void PickerShouldDisplayValueFromItemDisplayBinding()
+		{
+			App.WaitForElement("MauiPicker");
+			App.Tap("UpdateButton");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
