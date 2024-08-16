c1f9772975d4b98b1c52753bd37f9cc77b11c4db
Gerald Versluis <gerald.versluis@microsoft.com>
2024-08-16 15:30:54 +0200

    Fix UI test

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17865.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17865.cs
@@ -1,8 +1,9 @@
-﻿using NUnit.Framework;
+﻿#if WINDOWS
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.AppiumTests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues
 {
 	public class Issue17865 : _IssuesUITest
 	{
@@ -13,11 +14,9 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		public override string Issue => "CollectionView throws NRE when ScrollTo method is called from a handler of event Window.Created";
 
 		[Test]
+		[Category(UITestCategories.CollectionView)]
 		public void Issue17865Test()
 		{
-			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Android, TestDevice.iOS, TestDevice.Mac },
-				"Windows Test.");
-
 			App.WaitForElement(ButtonId);
 
 			App.Click(ButtonId);
@@ -26,3 +25,4 @@ namespace Microsoft.Maui.AppiumTests.Issues
 		}
 	}
 }
+#endif
