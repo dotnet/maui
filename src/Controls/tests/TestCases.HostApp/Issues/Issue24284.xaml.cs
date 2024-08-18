774afdc5474310969a6a2ef060281597b8bd4ecf
Jakub Florkowski <kubaflo123@gmail.com>
2024-08-18 14:42:00 +0200

    [Testing] moved FlyoutHeaderAdaptsToMinimumHeight to appium

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue24284.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+[Issue(IssueTracker.Github, 24284, "FlyoutHeaderAdaptsToMinimumHeight", PlatformAffected.All)]
+public partial class Issue24284 : Shell
+{
+	public Issue24284()
+	{
+		InitializeComponent();
+		var layout = new VerticalStackLayout()
+		{
+			AutomationId = "FlyoutHeader",
+			BackgroundColor = Colors.Red,
+			Children = {
+				new Label() { Text = "Flyout Header" }
+			},
+			MinimumHeightRequest = 30
+		};
+		FlyoutHeader = layout;
+		FlyoutHeaderBehavior = FlyoutHeaderBehavior.CollapseOnScroll;
+		FlyoutIsPresented = true;
+	}
+}
