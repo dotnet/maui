8a31ab936624744cb87b1781cabec1171a2c2a25
Diana Soltani <dianasoltani@microsoft.com>
2024-08-20 14:01:46 -0400

    move other platform api docs into ExtensionPoints

--- src/Controls/src/Core/PlatformConfiguration/ExtensionPoints.cs
+++ src/Controls/src/Core/PlatformConfiguration/ExtensionPoints.cs
@@ -1,4 +1,3 @@
-
 namespace Microsoft.Maui.Controls.PlatformConfiguration
 {
 	/// <summary>
@@ -8,14 +7,36 @@ namespace Microsoft.Maui.Controls.PlatformConfiguration
 	/// Developers specifiy the type name of this marker class to the <see cref="M:Microsoft.Maui.Controls.IElementConfiguration`1.On``1" /> method to specify the underlying Android control on which to run a platform-specific effect.
 	/// </remarks>
 	public sealed class Android : IConfigPlatform { }
-	/// <include file="../../../docs/Microsoft.Maui.Controls.PlatformConfiguration/iOS.xml" path="Type[@FullName='Microsoft.Maui.Controls.PlatformConfiguration.iOS']/Docs/*" />
+
+	/// <summary>
+	/// Marker class that identifies the iOS platform.
+	/// </summary>
+	/// <remarks>
+	/// Developers specify the type name of this marker class to the <see cref = "M:Microsoft.Maui.Controls.IElementConfiguration`1.On``1" /> method to specify the underlying iOS control on which to run a platform-specific effect.
+	/// </remarks>
 	public sealed class iOS : IConfigPlatform { }
-	/// <include file="../../../docs/Microsoft.Maui.Controls.PlatformConfiguration/Windows.xml" path="Type[@FullName='Microsoft.Maui.Controls.PlatformConfiguration.Windows']/Docs/*" />
+
+	/// <summary>
+	/// Marker class that identifies the Windows platform.
+	/// </summary>
+	/// <remarks>
+	/// Developers specify the type name of this marker class to the <see cref = "M:Microsoft.Maui.Controls.IElementConfiguration`1.On``1" /> method to specify the underlying Windows control on which to run a platform-specific effect.
+	/// </remarks>
 	public sealed class Windows : IConfigPlatform { }
-	/// <include file="../../../docs/Microsoft.Maui.Controls.PlatformConfiguration/Tizen.xml" path="Type[@FullName='Microsoft.Maui.Controls.PlatformConfiguration.Tizen']/Docs/*" />
+
+	/// <summary>
+	/// Marker class that identifies the Tizen platform.
+	/// </summary>
+	/// <remarks>
+	/// Developers specify the type name of this marker class to the <see cref = "M:Microsoft.Maui.Controls.IElementConfiguration`1.On``1" /> method to specify the underlying Tizen control on which to run a platform-specific effect.
+	/// </remarks>
 	public sealed class Tizen : IConfigPlatform { }
-	/// <include file="../../../docs/Microsoft.Maui.Controls.PlatformConfiguration/macOS.xml" path="Type[@FullName='Microsoft.Maui.Controls.PlatformConfiguration.macOS']/Docs/*" />
+	/// <summary>Marker class that identifies the macOS platform.</summary>
+	/// <remarks>
+	/// Developers specify the type name of this marker class to the <see cref = "M:Microsoft.Maui.Controls.IElementConfiguration`1.On``1" /> method to specify the underlying iOS control on which to run a platform-specific effect.
+	/// </remarks>
 	public sealed class macOS : IConfigPlatform { }
+
 	/// <include file="../../../docs/Microsoft.Maui.Controls.PlatformConfiguration/GTK.xml" path="Type[@FullName='Microsoft.Maui.Controls.PlatformConfiguration.GTK']/Docs/*" />
 	public sealed class GTK : IConfigPlatform { }
 }
