a76812ea288c3f74ac21e6b72059a7fd6db0477c
Nanthini Mahalingam <nanthini.mahalingam@syncfusion.com>
2024-08-23 18:14:13 +0530

    Fixed the binding context issue on ListView

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13558.cs
@@ -0,0 +1,25 @@
+﻿#if ANDROID
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue13558 : _IssuesUITest
+	{
+		public Issue13558(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Picker values are not displaying when loaded within a ListView";
+
+		[Test]
+		[Category(UITestCategories.ListView)]
+		public void PickerValuesShouldBeDisplayedInListView()
+		{
+			App.WaitForElement("ListViewId");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
