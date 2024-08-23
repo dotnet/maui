3dc9f4de18a672be6c1cdb4a67f7269aa361b6ca
Matthew Leibowitz <mattleibow@live.com>
2024-08-23 20:11:33 +0800

    Update UITest.cs

--- src/Controls/tests/TestCases.Shared.Tests/UITest.cs
+++ src/Controls/tests/TestCases.Shared.Tests/UITest.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Maui.TestCases.Tests
 			string projectRootDirectory = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)!;
 
 			_visualRegressionTester = new VisualRegressionTester(testRootDirectory: projectRootDirectory,
-				visualComparer: new MagickNetVisualComparer(differenceThreshold: 0.0035),
+				visualComparer: new MagickNetVisualComparer(differenceThreshold: 0.0045),
 				visualDiffGenerator: new MagickNetVisualDiffGenerator(),
 				ciArtifactsDirectory: ciArtifactsDirectory);
 
