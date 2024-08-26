40100c312dcdf1c78355dd62fd8d2cca18728ec5
Jeremi Kurdek <jkurdek@gmail.com>
2024-08-26 17:27:35 +0200

    Fix formatting

--- src/Controls/src/BindingSourceGen/BindingSourceGeneratorUtilities.cs
+++ src/Controls/src/BindingSourceGen/BindingSourceGeneratorUtilities.cs
@@ -4,47 +4,47 @@ namespace Microsoft.Maui.Controls.BindingSourceGen;
 
 public static class BindingGenerationUtilities
 {
-    internal static bool IsTypeNullable(ITypeSymbol typeInfo, bool enabledNullable)
-    {
-        if (!enabledNullable && typeInfo.IsReferenceType)
-        {
-            return true;
-        }
+	internal static bool IsTypeNullable(ITypeSymbol typeInfo, bool enabledNullable)
+	{
+		if (!enabledNullable && typeInfo.IsReferenceType)
+		{
+			return true;
+		}
 
-        return IsNullableValueType(typeInfo) || IsNullableReferenceType(typeInfo);
-    }
+		return IsNullableValueType(typeInfo) || IsNullableReferenceType(typeInfo);
+	}
 
-    private static bool IsNullableValueType(ITypeSymbol typeInfo) =>
-        typeInfo is INamedTypeSymbol namedTypeSymbol
-            && namedTypeSymbol.IsGenericType
-            && namedTypeSymbol.ConstructedFrom.SpecialType == SpecialType.System_Nullable_T;
+	private static bool IsNullableValueType(ITypeSymbol typeInfo) =>
+		typeInfo is INamedTypeSymbol namedTypeSymbol
+			&& namedTypeSymbol.IsGenericType
+			&& namedTypeSymbol.ConstructedFrom.SpecialType == SpecialType.System_Nullable_T;
 
-    private static bool IsNullableReferenceType(ITypeSymbol typeInfo) =>
-        typeInfo.IsReferenceType && typeInfo.NullableAnnotation == NullableAnnotation.Annotated;
+	private static bool IsNullableReferenceType(ITypeSymbol typeInfo) =>
+		typeInfo.IsReferenceType && typeInfo.NullableAnnotation == NullableAnnotation.Annotated;
 
-    internal static TypeDescription CreateTypeDescription(ITypeSymbol typeSymbol, bool enabledNullable)
-    {
-        var isNullable = IsTypeNullable(typeSymbol, enabledNullable);
-        return new TypeDescription(
-            GlobalName: GetGlobalName(typeSymbol, isNullable, typeSymbol.IsValueType),
-            IsNullable: isNullable,
-            IsGenericParameter: typeSymbol.Kind == SymbolKind.TypeParameter, //TODO: Add support for generic parameters
-            IsValueType: typeSymbol.IsValueType);
-    }
+	internal static TypeDescription CreateTypeDescription(ITypeSymbol typeSymbol, bool enabledNullable)
+	{
+		var isNullable = IsTypeNullable(typeSymbol, enabledNullable);
+		return new TypeDescription(
+			GlobalName: GetGlobalName(typeSymbol, isNullable, typeSymbol.IsValueType),
+			IsNullable: isNullable,
+			IsGenericParameter: typeSymbol.Kind == SymbolKind.TypeParameter, //TODO: Add support for generic parameters
+			IsValueType: typeSymbol.IsValueType);
+	}
 
-    internal static string GetGlobalName(ITypeSymbol typeSymbol, bool isNullable, bool isValueType)
-    {
-        if (isNullable && isValueType)
-        {
-            // Strips the "?" from the type name
-            return ((INamedTypeSymbol)typeSymbol).TypeArguments[0].ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
-        }
+	internal static string GetGlobalName(ITypeSymbol typeSymbol, bool isNullable, bool isValueType)
+	{
+		if (isNullable && isValueType)
+		{
+			// Strips the "?" from the type name
+			return ((INamedTypeSymbol)typeSymbol).TypeArguments[0].ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+		}
 
-        return typeSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
-    }
+		return typeSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+	}
 
-    internal static bool IsAccessible(Accessibility declaredAccessibility) =>
-        declaredAccessibility == Accessibility.Public
-            || declaredAccessibility == Accessibility.Internal
-            || declaredAccessibility == Accessibility.ProtectedOrInternal;
+	internal static bool IsAccessible(Accessibility declaredAccessibility) =>
+		declaredAccessibility == Accessibility.Public
+			|| declaredAccessibility == Accessibility.Internal
+			|| declaredAccessibility == Accessibility.ProtectedOrInternal;
 }
--- src/Controls/src/BindingSourceGen/BindingTransformer.cs
+++ src/Controls/src/BindingSourceGen/BindingTransformer.cs
@@ -3,48 +3,48 @@ namespace Microsoft.Maui.Controls.BindingSourceGen;
 
 public interface IBindingInvocationTransformer
 {
-    BindingInvocationDescription Transform(BindingInvocationDescription BindingInvocationDescription);
+	BindingInvocationDescription Transform(BindingInvocationDescription BindingInvocationDescription);
 }
 
 public class ReferenceTypesConditionalAccessTransformer : IBindingInvocationTransformer
 {
-    public BindingInvocationDescription Transform(BindingInvocationDescription BindingInvocationDescription)
-    {
-        var path = TransformPath(BindingInvocationDescription);
-        return BindingInvocationDescription with { Path = path };
-    }
+	public BindingInvocationDescription Transform(BindingInvocationDescription BindingInvocationDescription)
+	{
+		var path = TransformPath(BindingInvocationDescription);
+		return BindingInvocationDescription with { Path = path };
+	}
 
-    private static EquatableArray<IPathPart> TransformPath(BindingInvocationDescription BindingInvocationDescription)
-    {
-        var newPath = new List<IPathPart>();
-        foreach (var pathPart in BindingInvocationDescription.Path)
-        {
-            var sourceIsReferenceType = newPath.Count == 0 && !BindingInvocationDescription.SourceType.IsValueType;
-            var previousPartIsReferenceType = newPath.Count > 0 && PreviousPartIsReferenceType(newPath.Last());
+	private static EquatableArray<IPathPart> TransformPath(BindingInvocationDescription BindingInvocationDescription)
+	{
+		var newPath = new List<IPathPart>();
+		foreach (var pathPart in BindingInvocationDescription.Path)
+		{
+			var sourceIsReferenceType = newPath.Count == 0 && !BindingInvocationDescription.SourceType.IsValueType;
+			var previousPartIsReferenceType = newPath.Count > 0 && PreviousPartIsReferenceType(newPath.Last());
 
-            if (pathPart is not MemberAccess && pathPart is not IndexAccess)
-            {
-                newPath.Add(pathPart);
-            }
-            else if (sourceIsReferenceType || previousPartIsReferenceType)
-            {
-                newPath.Add(new ConditionalAccess(pathPart));
-            }
-            else
-            {
-                newPath.Add(pathPart);
-            }
-        }
+			if (pathPart is not MemberAccess && pathPart is not IndexAccess)
+			{
+				newPath.Add(pathPart);
+			}
+			else if (sourceIsReferenceType || previousPartIsReferenceType)
+			{
+				newPath.Add(new ConditionalAccess(pathPart));
+			}
+			else
+			{
+				newPath.Add(pathPart);
+			}
+		}
 
-        return new EquatableArray<IPathPart>(newPath.ToArray());
+		return new EquatableArray<IPathPart>(newPath.ToArray());
 
-        static bool PreviousPartIsReferenceType(IPathPart previousPathPart) =>
-            previousPathPart switch
-            {
-                MemberAccess memberAccess => !memberAccess.IsValueType,
-                IndexAccess indexAccess => !indexAccess.IsValueType,
-                ConditionalAccess { Part: var inner } => PreviousPartIsReferenceType(inner),
-                _ => false,
-            };
-    }
+		static bool PreviousPartIsReferenceType(IPathPart previousPathPart) =>
+			previousPathPart switch
+			{
+				MemberAccess memberAccess => !memberAccess.IsValueType,
+				IndexAccess indexAccess => !indexAccess.IsValueType,
+				ConditionalAccess { Part: var inner } => PreviousPartIsReferenceType(inner),
+				_ => false,
+			};
+	}
 }
