033b01abb201ae276d01393f2a787af56d4acf89
dustin-wojciechowski <dustin.wojciechowski@microsoft.com>
2024-08-27 09:24:16 -0700

    [Testing] CollectionViewInfiniteScroll Conversion to UI Test (#24411)
    * Initial Commit
    
    * Solved issue of automation id not picking up
    
    * Renamed files, set correct issue text
    
    * Removed unnecessary variables, comments
    
    * Disabled test for Catalyst as well
    
    * Fixed previous
    
    * Added MovedToAppium tag for original test

--- src/Controls/tests/TestCases.HostApp/Issues/Github5623.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Github5623.cs
@@ -1,75 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+
 using System.Collections.ObjectModel;
-using System.Security.Cryptography;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Maui.Controls.CustomAttributes;
 using Microsoft.Maui.Controls.Internals;
-using Microsoft.Maui.Controls.Xaml;
-using Microsoft.Maui.Graphics;
 
-#if UITEST
-using Xamarin.UITest;
-using Xamarin.UITest.Queries;
-using NUnit.Framework;
-using Microsoft.Maui.Controls.Compatibility.UITests;
-using System.Linq;
-#endif
-
-namespace Microsoft.Maui.Controls.ControlGallery.Issues
+namespace Maui.Controls.Sample.Issues
 {
-#if UITEST
-	[Category(UITestCategories.CollectionView)]
-	[Category(UITestCategories.UwpIgnore)]
-	[NUnit.Framework.Category(Compatibility.UITests.UITestCategories.Github5000)]
-#endif
-#if APP
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-#endif
-	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.Github, 5623, "CollectionView with Incremental Collection (RemainingItemsThreshold)", PlatformAffected.All)]
-	public partial class Github5623 : TestContentPage
+	public partial class Github5623 : ContentPage
 	{
-#if APP
 		int _itemCount = 10;
 		const int MaximumItemCount = 100;
 		const int PageSize = 10;
 		static readonly SemaphoreSlim SemaphoreSlim = new SemaphoreSlim(1, 1);
-
 		public Github5623()
 		{
 			InitializeComponent();
-
 			BindingContext = new ViewModel5623();
-		}
-
-		async void CollectionView_RemainingItemsThresholdReached(object sender, System.EventArgs e)
-		{
-			await SemaphoreSlim.WaitAsync();
-			try
-			{
-				var itemsSource = (sender as CollectionView).ItemsSource as ObservableCollection<Model5623>;
-				var nextSet = await GetNextSetAsync();
-
-				// nothing to add
-				if (nextSet.Count == 0)
-					return;
-
-				Device.BeginInvokeOnMainThread(() =>
-				{
-					foreach (var item in nextSet)
-					{
-						itemsSource.Add(item);
-					}
-				});
-
-				System.Diagnostics.Debug.WriteLine("Count: " + itemsSource.Count);
-			}
-			finally
-			{
-				SemaphoreSlim.Release();
-			}
 		}
 
 		void CollectionView_OnScrolled(object sender, ItemsViewScrolledEventArgs e)
@@ -98,7 +43,8 @@ namespace Microsoft.Maui.Controls.ControlGallery.Issues
 					collection.Add(new Model5623((BindingContext as ViewModel5623).ItemSizingStrategy == ItemSizingStrategy.MeasureAllItems)
 					{
 						Text = i.ToString(),
-						BackgroundColor = i % 2 == 0 ? Colors.AntiqueWhite : Colors.Lavender
+						BackgroundColor = i % 2 == 0 ? Colors.AntiqueWhite : Colors.Lavender,
+						AutomationId = i.ToString()
 					});
 				}
 
@@ -107,37 +53,34 @@ namespace Microsoft.Maui.Controls.ControlGallery.Issues
 				return collection;
 			});
 		}
-#endif
 
-		protected override void Init()
+		async void CollectionView_RemainingItemsThresholdReached(object sender, System.EventArgs e)
 		{
+			await SemaphoreSlim.WaitAsync();
+			try
+			{
+				var itemsSource = (sender as CollectionView).ItemsSource as ObservableCollection<Model5623>;
+				var nextSet = await GetNextSetAsync();
 
+				// nothing to add
+				if (nextSet.Count == 0)
+					return;
+
+				Dispatcher.Dispatch(() =>
+				{
+					foreach (var item in nextSet)
+					{
+						itemsSource.Add(item);
+					}
+				});
+
+				System.Diagnostics.Debug.WriteLine("Count: " + itemsSource.Count);
+			}
+			finally
+			{
+				SemaphoreSlim.Release();
+			}
 		}
-
-#if UITEST
-		[MovedToAppium]
-		[Test]
-		[Compatibility.UITests.FailsOnMauiIOS]
-		public void CollectionViewInfiniteScroll()
-		{
-			RunningApp.WaitForElement("CollectionView5623");
-
-			var colView = RunningApp.Query("CollectionView5623").Single();
-
-			AppResult[] lastCellResults = null;
-
-			RunningApp.QueryUntilPresent(() =>
-			 {
-				 RunningApp.DragCoordinates(colView.Rect.CenterX, colView.Rect.Y + colView.Rect.Height - 50, colView.Rect.CenterX, colView.Rect.Y + 5);
-
-				 lastCellResults = RunningApp.Query("99");
-
-				 return lastCellResults;
-			 }, 100, 1);
-
-			Assert.IsTrue(lastCellResults?.Any() ?? false);
-		}
-#endif
 	}
 
 	[Preserve(AllMembers = true)]
@@ -159,7 +102,8 @@ namespace Microsoft.Maui.Controls.ControlGallery.Issues
 				collection.Add(new Model5623(ItemSizingStrategy == ItemSizingStrategy.MeasureAllItems)
 				{
 					Text = i.ToString(),
-					BackgroundColor = i % 2 == 0 ? Colors.AntiqueWhite : Colors.Lavender
+					BackgroundColor = i % 2 == 0 ? Colors.AntiqueWhite : Colors.Lavender,
+					AutomationId = i.ToString()
 				});
 			}
 
@@ -172,7 +116,7 @@ namespace Microsoft.Maui.Controls.ControlGallery.Issues
 		}
 	}
 
-	[Preserve(AllMembers = true)]
+	[Preserve(AllMembers = true)] 
 	public class Model5623
 	{
 		Random random = new Random();
@@ -185,13 +129,15 @@ namespace Microsoft.Maui.Controls.ControlGallery.Issues
 
 		public string HeightText { get; private set; }
 
+		public string AutomationId {get; set;}
+
 		public Model5623(bool isUneven)
 		{
 			var byteArray = new byte[4];
 			random.NextBytes(byteArray);
 
 			if (isUneven)
-				Height = 100 + (BitConverter.ToInt32(byteArray, 0) % 300 + 300) % 300;
+				Height = 20 + (BitConverter.ToInt32(byteArray, 0) % 300 + 300) % 300;
 
 			HeightText = "(Height: " + Height + ")";
 		}
