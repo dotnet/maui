588eeceea19fa7f34abe494fc47c4141cd19bde7
TJ Lambert <50846373+tj-devel709@users.noreply.github.com>
2024-08-27 14:54:17 -0500

    [iOS] Fix Button image sizing and layout (#22476)

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue22306_2.xaml.cs
@@ -0,0 +1,26 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 22306_2, "Button sizes content with respect to the BorderWidth", PlatformAffected.iOS)]
+	public partial class Issue22306_2 : ContentPage
+	{
+		bool toggleBorderWidth = false;
+
+		public Issue22306_2()
+		{
+			InitializeComponent();
+		}
+
+		void Change_BorderWidth_Button_Clicked (object sender, System.EventArgs e)
+		{
+			toggleBorderWidth = !toggleBorderWidth;
+
+			TextButton.BorderWidth = toggleBorderWidth ? 5 : 0;
+			ImageButton.BorderWidth = toggleBorderWidth ? 20 : 0;
+			TextImageButton.BorderWidth = toggleBorderWidth ? 20 : 0;
+		}
+	}
+}
