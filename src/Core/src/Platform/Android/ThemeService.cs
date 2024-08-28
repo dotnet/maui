5efad4085347a0fdb23c89a42512173a47503b1c
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-08-28 12:38:10 +0530

    ThemeService class added

---
+++ src/Core/src/Platform/Android/ThemeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Android.Content.Res;
+
+namespace Microsoft.Maui.Platform
+{
+	internal static class ThemeService
+	{
+		public static event Action<UiMode>? ThemeChanged;
+
+		public static void NotifyThemeChange(UiMode uiMode)
+		{
+			ThemeChanged?.Invoke(uiMode);
+		}
+	}
+}
