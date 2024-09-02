a8c5fab728e210974284097e50ad24e23c5dfd79
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2024-09-02 19:06:39 +0530

    Added Test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue22452.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 22452, "Fix error when running new template maui app on iOS", PlatformAffected.iOS)]
+	public partial class Issue22452 : Shell
+	{
+		public Issue22452()
+		{
+			InitializeComponent();
+		}
+	}
+
+	public class Issue22452Tab1Content : ContentPage
+	{
+		public Issue22452Tab1Content()
+		{
+			Content = new Label()
+			{
+				AutomationId= "TapOnePage",
+				Text = "Tap one page",
+			};
+		}
+	}
+
+	public class Issue22452Tab2Content : ContentPage
+	{
+		public Issue22452Tab2Content()
+		{
+			Content = new Label()
+			{
+				AutomationId = "TapTwoPage",
+				Text = "Tap two page",
+			};
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22452.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue22452 : _IssuesUITest
+{
+	public Issue22452(TestDevice device) : base(device) { }
+
+	public override string Issue => "Fix error when running new template maui app on iOS";
+
+	[Test]
+	[Category(UITestCategories.Shell)]
+	[FailsOnMac]
+	public void NavigatingBetweenFlyoutItems()
+	{
+		App.WaitForElement("TapTwoPage");
+		App.Tap("FlyoutItem1");
+		App.Tap("FlyoutItem2");
+		VerifyScreenshot();
+	}
+}
