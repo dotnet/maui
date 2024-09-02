ebae157bcee4583e0c14a9a78a60df0e5636e1ee
Rui Marinho <me@ruimarinho.net>
2024-09-02 19:25:45 +0100

    Try new changes

--- src/Controls/src/Core/Handlers/Items2/iOS/LoopObservableItemsSource2.cs
+++ src/Controls/src/Core/Handlers/Items2/iOS/LoopObservableItemsSource2.cs
@@ -4,12 +4,13 @@ using System.Collections;
 using Foundation;
 using ObjCRuntime;
 using UIKit;
+using Microsoft.Maui.Controls.Handlers.Items;
 
 namespace Microsoft.Maui.Controls.Handlers.Items2
 {
 	internal class LoopObservableItemsSource2 : Items.ObservableItemsSource, Items.ILoopItemsViewSource
 	{
-		//const int LoopBy = 3;
+		int _section = 0;
 
 		public LoopObservableItemsSource2(IEnumerable itemSource, UICollectionViewController collectionViewController, bool loop, int group = -1) : base(itemSource, collectionViewController, group)
 		{
@@ -18,7 +19,29 @@ namespace Microsoft.Maui.Controls.Handlers.Items2
 
 		public bool Loop { get; set; }
 
+		protected override NSIndexPath[] CreateIndexesFrom(int startIndex, int count)
+		{
+			if (ItemCount == 0)
+			{
+				count += 2;
+				startIndex = 0;
+			}
+			return IndexPathHelpers.GenerateIndexPathRange(_section, startIndex, count);
+		}
+
 		//We are going to add 2 items since we are inserting 1 item at the beginning and 1 item at the end
-		public int LoopCount => Loop ? Count + 2 : Count;
+		public int LoopCount
+		{
+			get
+			{
+				var newCount = ItemCount;
+				if (newCount > 0)
+				{
+					newCount = ItemCount + 2;
+				}
+				return newCount;
+			}
+		}
+
 	}
 }
