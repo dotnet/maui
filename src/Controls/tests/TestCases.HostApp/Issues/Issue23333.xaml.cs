85e421db6a98cc8cd71ff85a95ed4439421b3efb
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-09-04 14:40:20 +0530

    script file method name updated

--- src/Controls/tests/TestCases.HostApp/Issues/Issue23333.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue23333.xaml.cs
@@ -4,7 +4,7 @@ using Microsoft.Maui.Controls.Xaml;
 namespace Maui.Controls.Sample.Issues
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	[Issue(IssueTracker.Github, 23333, "Frame offsets inner content view by 1pt",  PlatformAffected.Android | PlatformAffected.iOS | PlatformAffected.macOS)]
+	[Issue(IssueTracker.Github, 23333, "Frame offsets inner content view by 1pt", PlatformAffected.Android | PlatformAffected.iOS | PlatformAffected.macOS)]
 	public partial class Issue23333 : ContentPage
 	{
 		public Issue23333()
