ea8c57ffaeee70958bd4ef6edb3b22c2eff09fe4
Rui Marinho <me@ruimarinho.net>
2024-09-05 17:10:53 +0100

    Again

--- build.cmd
+++ build.cmd
@@ -1,2 +1,9 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build %*"
+setlocal
+
+set _args=%*
+if "%~1"=="-?" set _args=-help
+if "%~1"=="/?" set _args=-help
+
+powershell -ExecutionPolicy ByPass -NoProfile -Command "& '%~dp0eng\build.ps1'" %_args%
+exit /b %ERRORLEVEL%
