e090051cab00644c8820aa6f22de4fae477e6517
Rui Marinho <me@ruimarinho.net>
2024-09-06 23:51:07 +0100

    Revert "try use the latest tooling"
    This reverts commit 1b337f8b735b03b4403950feff480ba85e891b49.

--- eng/common/native/CommonLibrary.psm1
+++ eng/common/native/CommonLibrary.psm1
@@ -277,8 +277,7 @@ function Get-MachineArchitecture {
   if (($ProcessorArchitecture -Eq "AMD64") -Or
       ($ProcessorArchitecture -Eq "IA64") -Or
       ($ProcessorArchitecture -Eq "ARM64") -Or
-      ($ProcessorArchitecture -Eq "LOONGARCH64") -Or
-      ($ProcessorArchitecture -Eq "RISCV64")) {
+      ($ProcessorArchitecture -Eq "LOONGARCH64")) {
     return "x64"
   }
   return "x86"
