8184e18c176225be789ff673ce3eb4bdec1162be
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2024-09-10 12:32:44 +0530

    Added the test case.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue20736.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 20736, "Editor control does not scroll properly on iOS when enclosed in a Border control", PlatformAffected.iOS)]
+public partial class Issue20736 : ContentPage
+{
+	public Issue20736()
+    {
+        InitializeComponent();
+	}
+}
