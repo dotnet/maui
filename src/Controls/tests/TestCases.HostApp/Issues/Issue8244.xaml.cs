10a39ddca62095e914856f472f7ce7f2c834d31b
Suthi <suthi.yuvaraj@syncfusion.com>
2024-09-11 19:54:38 +0530

    [iOS] Custom FontImage icon is not render in Tabbar in Shell

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue8244.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.Controls.Sample.Issues
+{
+	public partial class Issue8244 : Shell
+	{
+		public Issue8244()
+		{
+			InitializeComponent();
+		}
+	}
+	public partial class MainTabPage : ContentPage
+	{
+		public MainTabPage()
+		{
+			Content = new VerticalStackLayout()
+			{
+				new Label(){
+					Text = "Page Loaded in first Tab",
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+					TextDecorations = TextDecorations.Underline,
+
+				},
+			};
+		}
+	}
+
+	public partial class SecondTabPage : ContentPage
+	{
+		public SecondTabPage()
+		{
+			Content = new VerticalStackLayout()
+			{
+				new Label(){
+						Text = "Page Loaded in Second Tab",
+						VerticalOptions = LayoutOptions.Center,
+						HorizontalOptions = LayoutOptions.Center,
+						TextDecorations = TextDecorations.Underline,
+
+					},
+			};
+		}
+	}
+	public partial class ThirdTabPage : ContentPage
+	{
+		public ThirdTabPage()
+		{
+			Content = new VerticalStackLayout()
+			{
+				new Label(){
+						Text = "Page Loaded in Third Tab",
+						VerticalOptions = LayoutOptions.Center,
+						HorizontalOptions = LayoutOptions.Center,
+						TextDecorations = TextDecorations.Underline,
+
+					},
+			};
+		}
+	}
+}
+}
