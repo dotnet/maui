2596c411ee301c92497d6eb0311cfee88a8b05a7
Nanthini Mahalingam <nanthini.mahalingam@syncfusion.com>
2024-09-12 12:02:19 +0530

    Updated the collection of images in view model class.

--- src/Controls/tests/TestCases.HostApp/Issues/Issue21980.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue21980.cs
@@ -18,20 +18,20 @@ namespace Maui.Controls.Sample.Issues
 	{
 		private readonly IReadOnlyList<string> _imageSource1 = new List<string>
 		{
-			"https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemZoom/440022s.jpg",
-			"https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemZoom/440022s2.jpg",
-			"https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemZoom/440022s3.jpg",
-			"https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemZoom/440022s4.jpg",
-			"https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemZoom/440022s5.jpg"
+			"dotnet_bot.png",
+			"dotnet_bot.png",
+			"dotnet_bot.png",
+			"dotnet_bot.png",
+			"dotnet_bot.png",
 		};
 		private readonly IReadOnlyList<string> _imageSource2 = new List<string>()
 		{
-			"https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemZoom/K92259s.jpg",
-			"https://xcdn.next.co.uk/common/Items/Default/Default/ItemImages/AltItemZoom/K92259s2.jpg",
-			"https://xcdn.next.co.uk/common/Items/Default/Default/ItemImages/AltItemZoom/K92259s3.jpg",
-			"https://xcdn.next.co.uk/common/Items/Default/Default/ItemImages/AltItemZoom/K92259s4.jpg",
-			"https://xcdn.next.co.uk/common/Items/Default/Default/ItemImages/AltItemZoom/K92259s5.jpg",
-			"https://xcdn.next.co.uk/common/Items/Default/Default/ItemImages/AltItemZoom/K92259s6.jpg"
+			"dotnet_bot.png",
+			"dotnet_bot.png",
+			"dotnet_bot.png",
+			"dotnet_bot.png",
+			"dotnet_bot.png",
+			"dotnet_bot.png",
 		};
 
 		private IReadOnlyList<string> _images = [];
