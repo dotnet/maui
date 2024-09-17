f3a35c77a144d258ff638f26706ab212d5c1969e
BagavathiPerumal <bagavathiperumal.a@syncfusion.com>
2024-09-17 20:55:16 +0530

    fix-24701--Removed-Parent-from-PlatformView-Before-Adding-to-View.

--- src/Controls/src/Core/Compatibility/Handlers/ListView/Android/ViewCellRenderer.cs
+++ src/Controls/src/Core/Compatibility/Handlers/ListView/Android/ViewCellRenderer.cs
@@ -244,6 +244,12 @@ namespace Microsoft.Maui.Controls.Handlers.Compatibility
 
 				var platformView = _viewCell.View.ToPlatform(Element.FindMauiContext());
 				_viewHandler = (IPlatformViewHandler)_viewCell.View.Handler;
+
+				if (platformView.Parent != null && platformView.Parent is ViewGroup parentViewGroup)
+				{
+					parentViewGroup.RemoveView(platformView);
+				}
+
 				AddView(platformView);
 
 				UpdateIsEnabled();
