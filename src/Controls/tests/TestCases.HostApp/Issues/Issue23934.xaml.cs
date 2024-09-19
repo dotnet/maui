55a0fc423ae4d4b9832dbbfb6996b14bd883d524
Nanthini Mahalingam <nanthini.mahalingam@syncfusion.com>
2024-09-19 22:21:27 +0530

    Added the UI test cases files

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue23934.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 23934, "RelativeLayout content disappears when it has a border with a border stroke", PlatformAffected.All)]
+	public partial class Issue23934 : ContentPage
+	{
+		public Issue23934()
+		{
+			InitializeComponent();
+		}
+	}
+}
