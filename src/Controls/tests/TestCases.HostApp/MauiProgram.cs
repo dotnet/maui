aaae0818b77aeff44027d60bd33f7f359cb5d53d
Nanthini Mahalingam <nanthini.mahalingam@syncfusion.com>
2024-09-20 14:28:18 +0530

    Added the .UseMauiCompatibility in maui program class to test the relative layout content.

--- src/Controls/tests/TestCases.HostApp/MauiProgram.cs
+++ src/Controls/tests/TestCases.HostApp/MauiProgram.cs
@@ -2,6 +2,7 @@ using System;
 using Maui.Controls.Sample.Issues;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
 
@@ -16,6 +17,7 @@ namespace Maui.Controls.Sample
 #if IOS || ANDROID
 			appBuilder.UseMauiMaps();
 #endif
+			appBuilder.UseMauiCompatibility();
 			appBuilder.UseMauiApp<App>()
 				.ConfigureFonts(fonts =>
 				{
