383671908a74c2c1d74fd0a92a3d7d760fbe1e4f
Jakub Florkowski <kubaflo123@gmail.com>
2024-09-21 03:06:06 +0200

    [Android] SearchHandler - added focus/unfocus support

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue24670.cs
@@ -0,0 +1,27 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.ManualTest, "24670", "SearchHandler.Focused event never fires", PlatformAffected.All)]
+	public partial class Issue24670 : Shell
+	{
+		public Issue24670()
+		{
+			InitializeComponent();
+		}
+
+		private void SearchHandler_Focused(object sender, EventArgs e)
+		{
+			focusedLabel.Text = "Focused: True";
+		}
+
+		private void SearchHandler_Unfocused(object sender, EventArgs e)
+		{
+			unfocusedLabel.Text = "Unfocused: True";
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			searchHandler.SetIsFocused(!searchHandler.IsFocused);
+		}
+	}
+}
