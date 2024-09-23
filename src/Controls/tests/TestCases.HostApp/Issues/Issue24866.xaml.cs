688ef5226743cfd16817c30f4b1050c53fce4943
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2024-09-23 14:49:05 +0530

    Fixed - 24866- [Windows] CollectionView with grouping fails to add items when a footer template is present or crashes when removing data.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue24866.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 24866, "[Windows] CollectionView with grouping fails to add items when a footer template is present or crashes when removing data.", PlatformAffected.UWP)]
+	public partial class Issue24866 : ContentPage
+	{
+		GroupedCollectionViewModel _viewModel;
+		public Issue24866()
+		{
+			InitializeComponent();
+			_viewModel = new GroupedCollectionViewModel();
+			BindingContext = _viewModel;
+		}
+
+		private void OnAddClicked(object sender, EventArgs e)
+		{
+			_viewModel.AddAnimal();
+		}
+
+		private void OnClearClicked(object sender, EventArgs e)
+		{
+			_viewModel.Reset();
+		}
+
+	}
+
+	public class Animal
+	{
+		public string Name { get; set; }
+	}
+
+	public class AnimalGroup : ObservableCollection<Animal>
+	{
+		public string Name { get; private set; }
+
+		public AnimalGroup(string name, List<Animal> animals) : base(animals)
+		{
+			Name = name;
+		}
+	}
+
+	public class GroupedCollectionViewModel
+	{
+		public GroupedCollectionViewModel()
+		{
+			Init();
+
+		}
+
+		private void Init()
+		{
+			AnimalBaseList.Clear();
+			AnimalBaseList.Add(new AnimalGroup("Bears", new List<Animal>
+			{
+				new Animal
+				{
+					Name = "American Black Bear",
+				},
+				new Animal
+				{
+					Name = "Asian Black Bear",					
+				},
+				new Animal
+				{
+					Name = "Brown Bear",
+				},
+				new Animal
+				{
+					Name = "Grizzly-Polar Bear Hybrid",
+				},
+				new Animal
+				{
+					Name = "Sloth Bear",
+				},
+				new Animal
+				{
+					Name = "Sun Bear",
+				},
+				new Animal
+				{
+					Name = "Polar Bear",
+				},
+				new Animal
+				{
+					Name = "Spectacled Bear",
+				},
+				new Animal
+				{
+					Name = "Short-faced Bear",
+				},
+				new Animal
+				{
+					Name = "California Grizzly Bear",
+				}
+			}));
+
+			AnimalBaseList.Add(new AnimalGroup("Cats", new List<Animal>
+			{
+				new Animal
+				{
+					Name = "Abyssinian",
+				},
+				new Animal
+				{
+					Name = "Arabian Mau",
+				},
+				new Animal
+				{
+					Name = "Bengal",
+				},
+				new Animal
+				{
+					Name = "Burmese",
+				},
+				new Animal
+				{
+					Name = "Cyprus",
+				},
+				new Animal
+				{
+					Name = "German Rex",
+				},
+				new Animal
+				{
+					Name = "Highlander",
+				},
+				new Animal
+				{
+					Name = "Peterbald",
+				},
+				new Animal
+				{
+					Name = "Scottish Fold",
+				},
+				new Animal
+				{
+					Name = "Sphynx",
+				}
+			}));
+
+			Animals.Add(new AnimalGroup(AnimalBaseList[0].Name, new List<Animal>()));
+
+			// Add one to the first group
+			AddAnimal();
+
+			Animals.Add(new AnimalGroup(AnimalBaseList[1].Name, AnimalBaseList[1].ToList()));
+
+		}
+
+
+		public bool AddAnimal()
+		{
+			// Add animal from first group
+			var sourceGroup = AnimalBaseList.First();
+			var targetGroup = Animals.First();
+
+			if (sourceGroup.Count == 0)
+				return false;
+
+			var animal = sourceGroup.First();
+			targetGroup.Add(animal);
+			sourceGroup.Remove(animal);
+
+			return true;
+		}
+
+		public void Reset()
+		{
+			Animals.Clear();
+			Init();
+
+		}
+
+		private List<AnimalGroup> AnimalBaseList { get; set; } = new List<AnimalGroup>();
+
+
+		public ObservableCollection<AnimalGroup> Animals { get; private set; } = new ObservableCollection<AnimalGroup>();
+
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue24866.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue24866 : _IssuesUITest
+	{
+		public Issue24866(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "[Windows] CollectionView with grouping fails to add items when a footer template is present or crashes when removing data.";
+		
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		[FailsOnMac]
+		public void CollectionViewShouldAddItemsWithFooterWhenGrouped()
+		{
+			App.WaitForElement("CollectionView");
+			App.Tap("add");
+			VerifyScreenshot();
+
+		}
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		[FailsOnMac]
+		public void CollectionViewShouldClearAndAddItemsOnGrouping()
+		{
+			App.WaitForElement("CollectionView");
+			App.Tap("add");
+			App.Tap("clear");
+			VerifyScreenshot();
+		}
+	}
+}
