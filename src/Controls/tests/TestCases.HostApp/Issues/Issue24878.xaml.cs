5e3d789764ed991a7cb35525f712a21fb1a84a5c
Jakub Florkowski <kubaflo123@gmail.com>
2024-09-24 02:43:35 +0200

    Added a UI test

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue24878.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls.Internals;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 24878, "AppThemeBinding does not work on ToolbarItems", PlatformAffected.Android)]
+	public partial class Issue24878 : Shell
+	{
+		public Issue24878()
+		{
+			InitializeComponent();
+		}
+	}
+}
