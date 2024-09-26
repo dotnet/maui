047b8e7c9b1fb03f1fd3b5f1074654ebb28b8eae
Gerald Versluis <gerald.versluis@microsoft.com>
2024-09-26 16:03:46 +0200

    Update Bugzilla30324.cs

--- src/Controls/tests/TestCases.HostApp/Issues/Bugzilla30324.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Bugzilla30324.cs
@@ -1,28 +1,88 @@
-﻿using NUnit.Framework;
-using UITest.Appium;
-using UITest.Core;
+using System;
+using Microsoft.Maui.Controls.Internals;
+using Microsoft.Maui.Graphics;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues;
+namespace Maui.Controls.Sample.Issues;
 
-public class Bugzilla30324 : _IssuesUITest
+[Preserve(AllMembers = true)]
+[Issue(IssueTracker.Bugzilla, 30324, "Detail view of FlyoutPage does not get appearance events on Android when whole FlyoutPage disappears/reappears")]
+public class Bugzilla30324 : TestNavigationPage
 {
-	public Bugzilla30324(TestDevice testDevice) : base(testDevice)
+	Label _lbl;
+	int _count;
+
+	protected override void Init()
 	{
+		FlyoutPage page = new FlyoutPage();
+		page.Flyout = new Page() { Title = "Flyout", BackgroundColor = Colors.Red };
+		_lbl = new Label();
+
+		var otherPage = new ContentPage()
+		{
+			Title = "Other",
+			Content = new StackLayout
+			{
+				Children = {
+					new Button () {
+						AutomationId = "navigateback",
+						Text = "navigate back",
+						HorizontalOptions = LayoutOptions.Center,
+						VerticalOptions = LayoutOptions.Center,
+
+						Command = new Command (() => Navigation.PopAsync())
+					}
+				}
+			}
+		};
+
+		page.Detail = new ContentPage()
+		{
+			Title = "Detail",
+			Content = new StackLayout
+			{
+				Children = {
+					_lbl,
+					new Button () {
+						AutomationId = "navigate",
+						Text = "navigate",
+						HorizontalOptions = LayoutOptions.Center,
+						VerticalOptions = LayoutOptions.Center,
+
+						Command = new Command (() => Navigation.PushAsync (otherPage))
+					}
+				}
+			}
+		};
+
+		page.Appearing += (sender, e) =>
+		{
+			System.Diagnostics.Debug.WriteLine("Appear MDP");
+		};
+		page.Disappearing += (sender, e) =>
+		{
+			System.Diagnostics.Debug.WriteLine("Disappear MDP");
+		};
+		page.Detail.Appearing += (sender, args) =>
+		{
+			if (_count == 2)
+			{
+				_lbl.AutomationId = "Appeardetail";
+				_lbl.Text = "Appear detail";
+			}
+			System.Diagnostics.Debug.WriteLine("Appear detail");
+		};
+		page.Detail.Disappearing += (sender, args) =>
+		{
+			System.Diagnostics.Debug.WriteLine("Disappear detail");
+			_lbl.AutomationId = "Disappeardetail";
+			_lbl.Text = "Disappear detail";
+			page.Detail.BackgroundColor = Colors.Green;
+			_count++;
+		};
+		page.Flyout.Appearing += (sender, e) =>
+		{
+			System.Diagnostics.Debug.WriteLine("Appear flyout");
+		};
+		Navigation.PushAsync(page);
 	}
-
-	public override string Issue => "Detail view of FlyoutPage does not get appearance events on Android when whole FlyoutPage disappears/reappears";
-
-	// [Test]
-	// [Category(UITestCategories.FlyoutPage)]
-	// public void Bugzilla30324Test ()
-	// {
-	// 	App.WaitForElement("navigate");
-
-	// 	App.Tap("navigate");
-	// 	App.Tap("navigateback");
-	// 	App.WaitForElement("Disappeardetail");
-	// 	App.Tap("navigate");
-	// 	App.Tap("navigateback");
-	// 	App.WaitForElement("Appeardetail");
-	// }
 }
