dd9b214f837829ab22cc51688d451d3e456c2bba
Gerald Versluis <gerald.versluis@microsoft.com>
2024-09-26 16:25:56 +0200

    Update Bugzilla32902.cs

--- src/Controls/tests/TestCases.HostApp/Issues/Bugzilla32902.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Bugzilla32902.cs
@@ -1,27 +1,108 @@
-﻿using NUnit.Framework;
-using UITest.Appium;
-using UITest.Core;
+using System;
+using Microsoft.Maui.Controls.CustomAttributes;
+using Microsoft.Maui.Controls.Internals;
+using Microsoft.Maui.Graphics;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues;
+namespace Maui.Controls.Sample.Issues;
 
-public class Bugzilla32902 : _IssuesUITest
+[Preserve(AllMembers = true)]
+[Issue(IssueTracker.Bugzilla, 32902, "[iOS | iPad] App Crashes (without debug log) when Flyout Detail isPresented and navigation being popped")]
+public class Bugzilla32902 : TestContentPage
 {
-	public Bugzilla32902(TestDevice testDevice) : base(testDevice)
+	ContentPage FirstContentPage { get; set; }
+
+	FlyoutPage HomePage { get; set; }
+
+	NavigationPage DetailPage { get; set; }
+
+	ContentPage RootPage { get; set; }
+
+	protected override void Init()
 	{
+		var rootContentPageLayout = new StackLayout();
+		var rootContentPageButton = new Button()
+		{
+			Text = "PushAsync to next page",
+			AutomationId = "btnNext",
+			BackgroundColor = Color.FromArgb("#ecf0f1"),
+			TextColor = Colors.Black
+		};
+		rootContentPageButton.Clicked += async (sender, args) =>
+		{
+			await Navigation.PushAsync(FirstContentPage);
+		};
+
+		rootContentPageLayout.Children.Add(rootContentPageButton);
+		Content = rootContentPageLayout;
+
+		Title = "RootPage";
+		BackgroundColor = Color.FromArgb("#2c3e50");
+
+		//ROOT PAGE
+		RootPage = new ContentPage()
+		{
+			Title = "Flyout",
+			BackgroundColor = Color.FromArgb("#1abc9c")
+		};
+		var rootPageLayout = new StackLayout();
+		var rootPageButton = new Button()
+		{
+			Text = "Pop Modal and Pop Root",
+			AutomationId = "btnPop",
+			BackgroundColor = Color.FromArgb("#ecf0f1"),
+			TextColor = Colors.Black
+		};
+		rootPageButton.Clicked += async (sender, args) =>
+		{
+			await Navigation.PopModalAsync();
+			await Navigation.PopToRootAsync();
+		};
+		rootPageLayout.Children.Add(rootPageButton);
+		RootPage.Content = rootPageLayout;
+
+
+		//DETAIL PAGE
+		DetailPage = new NavigationPage(new ContentPage()
+		{
+			Title = "RootNavigationDetailPage",
+			BackgroundColor = Color.FromArgb("#2980b9"),
+			Content = new Button
+			{
+				Text = "PopModal",
+				TextColor = Colors.White,
+				Command = new Command(async () =>
+				{
+					await Navigation.PopModalAsync();
+				})
+			}
+		});
+
+		//FLYOUTPAGE PAGE
+		HomePage = new FlyoutPage()
+		{
+			Flyout = RootPage,
+			Detail = DetailPage
+		};
+
+		//FIRST CONTENT PAGE
+		FirstContentPage = new ContentPage()
+		{
+			Title = "First Content Page",
+			BackgroundColor = Color.FromArgb("#e74c3c")
+		};
+		var firstContentPageLayout = new StackLayout();
+		var firstContentPageButton = new Button()
+		{
+			Text = "Push Modal To Flyout-Detail Page",
+			AutomationId = "btnPushModal",
+			BackgroundColor = Color.FromArgb("#ecf0f1"),
+			TextColor = Colors.Black
+		};
+		firstContentPageButton.Clicked += async (sender, args) =>
+		{
+			await Navigation.PushModalAsync(HomePage);
+		};
+		firstContentPageLayout.Children.Add(firstContentPageButton);
+		FirstContentPage.Content = firstContentPageLayout;
 	}
-
-	public override string Issue => "[iOS | iPad] App Crashes (without debug log) when Flyout Detail isPresented and navigation being popped";
-
-	// [Test]
-	// [Category(UITestCategories.FlyoutPage)]
-	// public void Bugzilla32902Test()
-	// {
-	// 	if (Devices.DeviceInfo.Idiom == Devices.DeviceIdiom.Tablet)
-	// 	{
-	// 		App.Tap("btnNext");
-	// 		App.Tap("btnPushModal");
-	// 		App.Tap("Flyout");
-	// 		App.Tap("btnPop");
-	// 	}
-	// }
 }
