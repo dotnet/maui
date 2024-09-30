f3c24349e7e51deb3dfdf76e122d4b7fe74e0ed7
Suthi <suthi.yuvaraj@syncfusion.com>
2024-09-30 19:44:50 +0530

    Testcase

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue17390/InnerTabbedPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+public partial class InnerTabbedPage : ContentPage
+{
+	public InnerTabbedPage()
+	{
+		InitializeComponent();
+	}
+
+    async void OpenNonTabbedPage(object sender, EventArgs args)
+    {
+        await Shell.Current.GoToAsync("nontabbedpage");
+    }
+}
---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue17390/Issue17390.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 17390, "Shell where the bottom padding is not calculated properly when navigating from a tabbed page to a non-tabbed page and returning back to the tabbed page.",
+		PlatformAffected.Android)]
+	public partial class Issue17390 : Shell
+	{
+		public Issue17390()
+		{
+			InitializeComponent();
+			Routing.RegisterRoute("nontabbedpage", typeof(NonTabbedPage));
+			Routing.RegisterRoute("innertabbedpage", typeof(InnerTabbedPage));
+		}
+		async void OpenNonTabbedPage(object sender, EventArgs args)
+		{
+			await Shell.Current.GoToAsync("nontabbedpage");
+		}
+
+		async void OpenInnerTabbedPage(object sender, EventArgs args)
+		{
+			await Shell.Current.GoToAsync("innertabbedpage");
+		}
+	}
+}
