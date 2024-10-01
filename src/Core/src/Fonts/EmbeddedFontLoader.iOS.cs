11168df3cda1005759da18689417938b2b7cd078
Tim Miller <innerlogic4321@gmail.com>
2024-10-01 21:14:47 +0900

    Fix deprecated API usage for embedded fonts on iOS/Catalyst

--- src/Core/src/Fonts/EmbeddedFontLoader.iOS.cs
+++ src/Core/src/Fonts/EmbeddedFontLoader.iOS.cs
@@ -1,5 +1,6 @@
 #nullable enable
 using System;
+using System.IO;
 using CoreGraphics;
 using CoreText;
 using Foundation;
@@ -18,32 +19,26 @@ namespace Microsoft.Maui
 		{
 			try
 			{
-				CGFont? cgFont;
-
-				if (font.ResourceStream == null)
-				{
-					if (!System.IO.File.Exists(font.FontName))
-						throw new InvalidOperationException("ResourceStream was null.");
-
-					var provider = new CGDataProvider(font.FontName);
-					cgFont = CGFont.CreateFromProvider(provider);
-				}
-				else
-				{
-					var data = NSData.FromStream(font.ResourceStream);
-					if (data == null)
-						throw new InvalidOperationException("Unable to load font stream data.");
-					var provider = new CGDataProvider(data);
-					cgFont = CGFont.CreateFromProvider(provider);
-				}
-
+				var provider = GetCGDataProviderFromEmbeddedFont(font);
+				CGFont? cgFont = CGFont.CreateFromProvider(provider);
+				
 				if (cgFont == null)
 					throw new InvalidOperationException("Unable to load font from the stream.");
 
 				var name = cgFont.PostScriptName;
 
-#pragma warning disable CA1416  // TODO:  'RegisterGraphicsFont' is obsolete on: 'ios' 15.0 and later
-#pragma warning disable CA1422
+				if (OperatingSystem.IsIOSVersionAtLeast(18, 0) || OperatingSystem.IsMacCatalystVersionAtLeast(18, 0))
+				{
+					var fontFilePath = SaveCGFontToFile(provider, name);
+					if (string.IsNullOrEmpty(fontFilePath))
+						return null;
+					var fontUrl = NSUrl.FromFilename(fontFilePath);
+					var nsError = CTFontManager.RegisterFontsForUrl(fontUrl, CTFontManagerScope.Process);
+					if (nsError != null)
+						throw new NSErrorException(nsError);
+					return name;
+				}
+
 				if (CTFontManager.RegisterGraphicsFont(cgFont, out var error))
 					return name;
 #pragma warning restore CA1422 
@@ -64,6 +59,45 @@ namespace Microsoft.Maui
 			}
 
 			return null;
+		}
+
+		private static CGDataProvider GetCGDataProviderFromEmbeddedFont(EmbeddedFont font)
+		{
+			if (font.ResourceStream == null)
+			{
+				if (!System.IO.File.Exists(font.FontName))
+					throw new InvalidOperationException("ResourceStream was null.");
+
+				return new CGDataProvider(font.FontName);
+			}
+			else
+			{
+				var data = NSData.FromStream(font.ResourceStream);
+				if (data == null)
+					throw new InvalidOperationException("Unable to load font stream data.");
+				return new CGDataProvider(data);
+			}
+		}
+
+		private static string? SaveCGFontToFile(CGDataProvider dataProvider, string? fontName)
+		{
+			if (string.IsNullOrEmpty(fontName))
+				return null;
+
+			using (var fontData = dataProvider.CopyData())
+			{
+				if (fontData == null)
+					return null;
+
+				var tempFolderPath = NSFileManager.DefaultManager.GetTemporaryDirectory().Path;
+				if (string.IsNullOrEmpty(tempFolderPath))
+					return null;
+
+				var fontFilePath = System.IO.Path.Combine(tempFolderPath, $"{fontName}.ttf");
+
+				System.IO.File.WriteAllBytes(fontFilePath, fontData.ToArray());
+				return fontFilePath;
+			}
 		}
 	}
 }
