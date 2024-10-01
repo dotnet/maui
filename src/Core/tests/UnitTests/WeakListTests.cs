d8a14f68ac4e057c70115ab7973f71ab0658e7c6
Shane Neuville <shneuvil@microsoft.com>
2024-10-01 17:00:01 -0500

    - collect harder

--- src/Core/tests/UnitTests/WeakListTests.cs
+++ src/Core/tests/UnitTests/WeakListTests.cs
@@ -11,12 +11,13 @@ namespace Microsoft.Maui.UnitTests
 		public async Task ObjectsAreEvicted_GetEnumerator()
 		{
 			var expected = new object();
-			var list = new WeakList<object> { expected, new object() };
+			var evict = new object();
+			WeakReference weakReference = new WeakReference(evict);
+			var list = new WeakList<object> { expected, evict };
+			evict = null;
 			list.CleanupThreshold = 1;
-
-			await Task.Yield();
-			GC.Collect();
-			GC.WaitForPendingFinalizers();
+			
+			Assert.False(await weakReference.WaitForCollect(), "Object failed to be collected");
 
 			foreach (var item in list)
 			{
@@ -31,12 +32,13 @@ namespace Microsoft.Maui.UnitTests
 		public async Task ObjectsAreEvicted_Remove()
 		{
 			var expected = new object();
-			var list = new WeakList<object> { expected, new object() };
+			var evict = new object();
+			WeakReference weakReference = new WeakReference(evict);
+			var list = new WeakList<object> { expected, evict };
+			evict = null;
 			list.CleanupThreshold = 1;
 
-			await Task.Yield();
-			GC.Collect();
-			GC.WaitForPendingFinalizers();
+			Assert.False(await weakReference.WaitForCollect(), "Object failed to be collected");
 
 			list.Remove(expected);
 
@@ -47,14 +49,16 @@ namespace Microsoft.Maui.UnitTests
 		public async Task ObjectsAreEvicted_Add()
 		{
 			var expected = new object();
-			var list = new WeakList<object> { expected, new object() };
+			var evict = new object();
+			WeakReference weakReference = new WeakReference(evict);
+			var list = new WeakList<object> { expected, evict };
+			evict = null;
 			list.CleanupThreshold = 1;
+			
+			Assert.False(await weakReference.WaitForCollect(), "Object failed to be collected");
 
-			await Task.Yield();
-			GC.Collect();
-			GC.WaitForPendingFinalizers();
-
-			list.Add(new object());
+			var triggerCleanup = new object();
+			list.Add(triggerCleanup);
 
 			Assert.Equal(2, list.Count);
 			Assert.Equal(expected, list.First());
