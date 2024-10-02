4f8ea3ec44a476f7fe24b3c2291af1da82bd96b4
Alexander Heubusch <aheubusch@users.noreply.github.com>
2024-10-02 21:53:43 +0200

    [android] Fallback to default icons in SearchHandler
    If no QueryIcon / ClearIcon is specified use default icon.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25067.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 25067, "Render default icons in SearchHandler", PlatformAffected.Android)]
+	public partial class Issue25067 : Shell
+	{
+		public Issue25067()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25067.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue25067 : _IssuesUITest
+{
+	public Issue25067(TestDevice device) : base(device) { }
+
+	public override string Issue => "Render default icons in SearchHandler";
+
+	[Test]
+	[Category(UITestCategories.Shell)]
+	public void SearchHandlerRendersDefaultSearchIconAndClearIcon()
+	{
+		// App.WaitForElement("searchHandler"); doesnt work
+		App.WaitForElement("WaitForStubControl");
+
+		VerifyScreenshot();
+	}
+}
