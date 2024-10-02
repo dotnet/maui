a258d032d8dd0eb2a303faec6a7b8a1423cf03df
Shane Neuville <shneuvil@microsoft.com>
2024-10-02 11:57:42 -0500

    Update EmbeddedFontLoader.iOS.cs

--- src/Core/src/Fonts/EmbeddedFontLoader.iOS.cs
+++ src/Core/src/Fonts/EmbeddedFontLoader.iOS.cs
@@ -41,8 +41,6 @@ namespace Microsoft.Maui
 
 				if (CTFontManager.RegisterGraphicsFont(cgFont, out var error))
 					return name;
-#pragma warning restore CA1422 
-#pragma warning restore CA1416
 
 				var uiFont = UIFont.FromName(name, 10);
 				if (uiFont is not null)
