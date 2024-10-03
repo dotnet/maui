1f21c8824995f863d38bd78037443a93344b017f
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-10-03 19:16:57 +0530

    fix added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue21368.xaml.cs
@@ -0,0 +1,9 @@
+namespace Test;
+
+public partial class Issue21368 : ContentPage
+{
+	public Issue21368()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21368.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue21368 : _IssuesUITest
+    {
+        public override string Issue => "Image has wrong orientation on iOS";
+
+        public Issue21368(TestDevice device) : base(device)
+        {
+        }
+
+        public void VerifyImageAspects()
+        {
+            App.WaitForElement("LabelNames");
+            VerifyScreenshot();
+        }
+    }
+}
