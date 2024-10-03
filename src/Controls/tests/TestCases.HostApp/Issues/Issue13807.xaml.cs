f00bc096ce80bf10a2e0a83ca71f2914dbe75aac
Vignesh-SF3580 <vignesh.nagarajan@syncfusion.com>
2024-10-03 12:13:44 +0530

    Added testcases.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue13807.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 13807, "NavigationPage TitleBar Persists if child pages have a title", PlatformAffected.All)]
+	public partial class Issue13807 : TabbedPage
+    {
+        public Issue13807()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13807.cs
@@ -0,0 +1,25 @@
+﻿#if !MACCATALYST
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue13807 : _IssuesUITest
+	{
+		public Issue13807(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "NavigationPage TitleBar Persists if child pages have a title";
+
+		[Test]
+		[Category(UITestCategories.Navigation)]
+		public void NavigationBarVisibility()
+		{
+			App.WaitForElement("label");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
