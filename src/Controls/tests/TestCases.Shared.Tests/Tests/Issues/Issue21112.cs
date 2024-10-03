06c84b52e483e31cb162c1f782b386a4388d47d0
Vignesh-SF3580 <vignesh.nagarajan@syncfusion.com>
2024-10-03 17:44:32 +0530

    added test cases.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21112.cs
@@ -0,0 +1,27 @@
+﻿#if !MACCATALYST
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue21112 : _IssuesUITest
+    {
+		public Issue21112(TestDevice testDevice) : base(testDevice)
+		{
+		}
+		public override string Issue => "TableView TextCell command executes only once";
+
+		[Test]
+		[Category(UITestCategories.TableView)]
+		public void TableViewTextCellCommand()
+		{
+			App.WaitForElement("TextCell");
+			App.Tap("TextCell");
+			App.Tap("Button");
+			App.Tap("TextCell");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
