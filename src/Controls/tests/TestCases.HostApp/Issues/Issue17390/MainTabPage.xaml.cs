45a58348d84c904253db05a1213a0bad5468b786
Suthi <suthi.yuvaraj@syncfusion.com>
2024-10-04 13:06:13 +0530

    fix for test case fail

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue17390/MainTabPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	public partial class MainTabPage : ContentPage
+	{
+		public MainTabPage()
+		{
+			InitializeComponent();
+		}
+
+		async void OpenNonTabbedPage(object sender, EventArgs args)
+		{
+			await Shell.Current.GoToAsync("nontabbedpage");
+		}
+
+		async void OpenInnerTabbedPage(object sender, EventArgs args)
+		{
+			await Shell.Current.GoToAsync("innertabbedpage");
+		}
+	}
+}
