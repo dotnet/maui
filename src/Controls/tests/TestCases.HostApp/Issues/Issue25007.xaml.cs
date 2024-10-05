4342c1e4e60d14742e9269d9787dcab32de4e917
Jakub Florkowski <kubaflo123@gmail.com>
2024-10-05 16:15:31 +0200

    [Android] Input transparent for a content page

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25007.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 25007, "ContentPage Property InputTransparent = true, causes blank page", PlatformAffected.Android)]
+	public partial class Issue25007 : ContentPage
+	{
+		int _clicksCount;
+		public Issue25007()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonClicked(object sender, EventArgs e)
+		{
+			this.InputTransparent = true;
+		}
+
+		private void UpdateCountClicked(object sender, EventArgs e)
+		{
+			ActionButton.Text = $"Count: {++_clicksCount}";
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25007.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue25007 : _IssuesUITest
+	{
+		public Issue25007(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "ContentPage Property InputTransparent = true, causes blank page";
+
+		[Test]
+		[Category(UITestCategories.InputTransparent)]
+		public void InputTransparentShouldWorkOnContentPage()
+		{
+			App.WaitForElement("ActionButton");
+			App.Click("ActionButton");
+			App.Click("EnableInputTransparentButton");
+			App.Click("ActionButton");
+			
+			var countNumberText = App.FindElement("ActionButton").GetText();
+			Assert.That(countNumberText,Is.EqualTo("Count: 1"));
+		}
+	}
+}
