ec64aaddfe0d73a8a1f5af127808755716659a21
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-10-07 18:25:56 +0530

    UI test files name changed

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue15695.xaml.cs
@@ -0,0 +1,16 @@
+namespace Maui.Controls.Sample.Issues;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+[Issue(IssueTracker.Github, 15695, "Support for Switch OFF State color", PlatformAffected.All)]
+public partial class Issue15695 : ContentPage
+{
+	public Issue15695()
+	{
+		InitializeComponent();
+	}
+
+	private void OnButtonClicked(object? sender, EventArgs e)
+	{
+		switch1.IsToggled = !switch1.IsToggled;
+	}
+}
