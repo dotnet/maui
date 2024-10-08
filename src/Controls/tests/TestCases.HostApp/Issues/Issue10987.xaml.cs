8a67e9bf3b27067b5570a10c7621ce292385af6f
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2024-10-08 14:24:04 +0530

    Fixed - 10987 - Editor HorizontalTextAlignment Does not Works.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue10987.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 10987, "Editor HorizontalTextAlignment Does not Works.", PlatformAffected.All)]
+	public partial class Issue10987 : ContentPage
+	{
+		public Issue10987()
+		{
+			InitializeComponent();
+		}		
+
+		private void UpdateButtonClicked(object sender, EventArgs e)
+		{
+			editor.VerticalTextAlignment = TextAlignment.End;
+			editor.HorizontalTextAlignment = TextAlignment.End;
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10987.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue10987 : _IssuesUITest
+	{
+		public Issue10987(TestDevice device) : base(device) { }
+
+		public override string Issue => "Editor HorizontalTextAlignment Does not Works.";
+
+		[Test]
+		[Category(UITestCategories.Editor)]
+		[FailsOnMac]
+  		public void EditorPlaceholderRuntimeTextAlignmentChanged()
+		{
+			App.WaitForElement("button");
+			App.Tap("button");
+			VerifyScreenshot();
+		}
+
+		[Test]
+		[Category(UITestCategories.Editor)]
+		[FailsOnMac]
+		public void EditorRuntimeTextAlignmentChanged()
+		{
+			App.EnterText("MauiEditor", "Editor");
+			App.WaitForElement("button");
+			App.Tap("button");
+			VerifyScreenshot();
+			VerifyScreenshot();
+		}		
+	}
+}
