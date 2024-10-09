11a183fe6629f4ac41ea7e6f49e0bb08d8050230
Rui Marinho <me@ruimarinho.net>
2024-10-09 19:59:33 +0100

    [iOS] Make sure to distinguish between group header and global header on CV2

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/CollectionView/CollectionViewUITests.Grouping.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests
+{
+    public class CollectionViewGroupingTests : CollectionViewUITests
+    {
+
+        protected override bool ResetAfterEachTest => true;
+
+        public CollectionViewGroupingTests(TestDevice device)
+            : base(device)
+        {
+        }
+
+        [Test]
+        [Category(UITestCategories.CollectionView)]
+        public void GroupingAndHeaderWorks()
+        {
+            VisitInitialGallery("Grouping");
+
+            VisitSubGallery("Basic Grouping");
+
+            // header
+            App.WaitForElement("This is a header");
+            // group header
+            App.WaitForElement("Avengers");
+            // group footer
+            App.WaitForElement("Total members: 12");
+        }
+    }
+}
