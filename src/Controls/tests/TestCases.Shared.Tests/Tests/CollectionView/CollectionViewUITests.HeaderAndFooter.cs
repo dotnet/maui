c6e3dadd44fdb6fa7e715035d6b56af4760becfe
Rui Marinho <me@ruimarinho.net>
2024-10-09 19:03:58 +0100

    Update more tests

--- src/Controls/tests/TestCases.Shared.Tests/Tests/CollectionView/CollectionViewUITests.HeaderAndFooter.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/CollectionView/CollectionViewUITests.HeaderAndFooter.cs
@@ -4,55 +4,89 @@ using UITest.Core;
 
 namespace Microsoft.Maui.TestCases.Tests
 {
-	public class CollectionViewHeaderAndFooterTests : CollectionViewUITests
-	{
+    public class CollectionViewHeaderAndFooterTests : CollectionViewUITests
+    {
 
-		protected override bool ResetAfterEachTest => true;
+        protected override bool ResetAfterEachTest => true;
 
-		public CollectionViewHeaderAndFooterTests(TestDevice device)
-			: base(device)
-		{
-		}
+        public CollectionViewHeaderAndFooterTests(TestDevice device)
+            : base(device)
+        {
+        }
 
-		[Test]
-		[Category(UITestCategories.CollectionView)]
-		public void HeaderFooterStringWorks()
-		{
+        [Test]
+        [Category(UITestCategories.CollectionView)]
+        public void HeaderFooterStringWorks()
+        {
             // Navigate to the selection galleries
-			VisitInitialGallery("Header Footer");
-			
-			// Navigate to the specific sample inside selection galleries
-			VisitSubGallery("Header/Footer (String)");
+            VisitInitialGallery("Header Footer");
+
+            // Navigate to the specific sample inside selection galleries
+            VisitSubGallery("Header/Footer (String)");
 
             App.WaitForElement("Just a string as a header");
             App.WaitForElement("This footer is also a string");
         }
 
         [Test]
-		[Category(UITestCategories.CollectionView)]
-		public void HeaderFooterViewWorks()
-		{
+        [Category(UITestCategories.CollectionView)]
+        public void HeaderFooterViewWorks()
+        {
             // Navigate to the selection galleries
-			VisitInitialGallery("Header Footer");
-			
-			// Navigate to the specific sample inside selection galleries
-			VisitSubGallery("Header/Footer (Forms View)");
+            VisitInitialGallery("Header Footer");
+
+            // Navigate to the specific sample inside selection galleries
+            VisitSubGallery("Header/Footer (Forms View)");
 
             App.WaitForElement("This Is A Header");
             App.WaitForElement("This Is A Footer");
         }
 
         [Test]
-		[Category(UITestCategories.CollectionView)]
-		public void HeaderFooterTemplateViewWorks()
-		{
+        [Category(UITestCategories.CollectionView)]
+        public void HeaderFooterTemplatewWorks()
+        {
             // Navigate to the selection galleries
-			VisitInitialGallery("Header Footer");
-			
-			// Navigate to the specific sample inside selection galleries
-			VisitSubGallery("Header/Footer (Template)");
+            VisitInitialGallery("Header Footer");
 
-			VerifyScreenshot();
+            // Navigate to the specific sample inside selection galleries
+            VisitSubGallery("Header/Footer (Template)");
+
+            VerifyScreenshot();
+        }
+
+        [Test]
+        [Category(UITestCategories.CollectionView)]
+        public void HeaderFooterGridWorks()
+        {
+            // Navigate to the selection galleries
+            VisitInitialGallery("Header Footer");
+
+            // Navigate to the specific sample inside selection galleries
+            VisitSubGallery("Header/Footer (Grid)");
+
+            App.WaitForElement("This Is A Header");
+            App.WaitForElement("This Is A Footer");
+
+            VerifyScreenshot();
+        }
+
+        [Test]
+        [Category(UITestCategories.CollectionView)]
+        public void HeaderFooterGridHorizontalWorks()
+        {
+            // Navigate to the selection galleries
+            VisitInitialGallery("Header Footer");
+
+            // Navigate to the specific sample inside selection galleries
+            VisitSubGallery("Header/Footer (Grid Horizontal)");
+
+            App.WaitForElement("This Is A Header");
+            
+            // This is a bug in the test, the footer is not being found
+            //App.WaitForElement("This Is A Footer");
+
+            VerifyScreenshot();
         }
     }
 }
