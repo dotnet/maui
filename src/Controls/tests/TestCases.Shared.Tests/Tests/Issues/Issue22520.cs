1fbac8a0cf30d5a125a6e91ef1119d3323e1cc9f
Jakub Florkowski <kubaflo123@gmail.com>
2024-10-09 17:47:05 +0200

    ImageButton border (BorderWidth) overlaps the image

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22520.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue22520 : _IssuesUITest
+{
+	public override string Issue => "ImageButton border (BorderWidth) overlaps the image";
+
+	public Issue22520(TestDevice device)
+		: base(device)
+	{ }
+
+    [Test]
+	public void CornersShouldBeProperlyRounded()
+	{
+		_ = App.WaitForElement("imageButton");
+		VerifyScreenshot();
+	}
+}
