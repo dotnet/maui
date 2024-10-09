a59da6e5877037e8360540ea371576c43bbed7c0
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-10-09 12:05:30 +0100

    More changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla32801.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla32801.cs
@@ -1,36 +1,36 @@
-﻿// #if IOS
-// using NUnit.Framework;
-// using UITest.Appium;
-// using UITest.Core;
+﻿#if IOS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
 
-// namespace Microsoft.Maui.TestCases.Tests.Issues;
+namespace Microsoft.Maui.TestCases.Tests.Issues;
 
-// public class Bugzilla32801 : _IssuesUITest
-// {
-// 	public Bugzilla32801(TestDevice testDevice) : base(testDevice)
-// 	{
-// 	}
+public class Bugzilla32801 : _IssuesUITest
+{
+	public Bugzilla32801(TestDevice testDevice) : base(testDevice)
+	{
+	}
 
-// 	public override string Issue => "Memory Leak in TabbedPage + NavigationPage";
+	public override string Issue => "Memory Leak in TabbedPage + NavigationPage";
 
-// 	[Test]
-// 	[Category(UITestCategories.TabbedPage)]
-// 	[FailsOnIOS]
-// 	public void Bugzilla32801Test()
-// 	{
-// 		App.Tap("btnAdd");
-// 		App.Tap("btnAdd");
-// 		App.Tap("btnStack");
-// 		App.WaitForElement("Stack 3");
-// 		App.Tap("Tab");
-// 		App.Tap("btnStack");
-// 		App.WaitForElement("Stack 1");
-// 	}
+	// [Test]
+	// [Category(UITestCategories.TabbedPage)]
+	// [FailsOnIOS]
+	// public void Bugzilla32801Test()
+	// {
+	// 	App.Tap("btnAdd");
+	// 	App.Tap("btnAdd");
+	// 	App.Tap("btnStack");
+	// 	App.WaitForElement("Stack 3");
+	// 	App.Tap("Tab");
+	// 	App.Tap("btnStack");
+	// 	App.WaitForElement("Stack 1");
+	// }
 
-// 	[TearDown]
-// 	public void TearDown()
-// 	{
-// 		App.SetOrientationPortrait();
-// 	}
-// }
-// #endif
+	// [TearDown]
+	// public void TearDown()
+	// {
+	// 	App.SetOrientationPortrait();
+	// }
+}
+#endif
