58cead3e45a4f5b24f844850e9c40318ec04b270
Vignesh-SF3580 <vignesh.nagarajan@syncfusion.com>
2024-10-10 15:43:12 +0530

    updated testcases.

--- src/Controls/tests/TestCases.HostApp/Issues/Issue18235.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue18235.xaml.cs
@@ -13,22 +13,12 @@ namespace Maui.Controls.Sample.Issues
 		public Issue18235()
 		{
 			InitializeComponent();
-			PrintButtonTexts();
 		}
 
 		private void Button_Clicked(object sender, EventArgs e)
 		{
-
 			if (sender is Button buttonClicked)
 				buttonClicked.Text = String.Empty; // Changing this to any other string doesn't reproduce the problem
-
-			PrintButtonTexts();
-		}
-
-		private void PrintButtonTexts()
-		{
-			ButtonText.Text = $"Button1.Text = '{Button1.Text}'{Environment.NewLine}"
-				+ $"Button2.Text = '{Button2.Text}'{Environment.NewLine}";
 		}
 	}
 }
