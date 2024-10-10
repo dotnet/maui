1594fd2e5960e21a6a23ab2d43f3f4e7556943b4
Simon Rozsival <simon@rozsival.com>
2024-10-10 17:43:48 +0200

    Set root element type arguments if needed

--- src/Controls/src/Xaml/XamlParser.cs
+++ src/Controls/src/Xaml/XamlParser.cs
@@ -43,6 +43,15 @@ namespace Microsoft.Maui.Controls.Xaml
 		public static void ParseXaml(RootNode rootNode, XmlReader reader)
 		{
 			var attributes = ParseXamlAttributes(reader, out IList<KeyValuePair<string, string>> xmlns);
+			foreach (var kvp in attributes)
+			{
+				if (kvp.Key == XmlName.xTypeArguments && kvp.Value is ValueNode { Value: IList<XmlType> typeArguments })
+				{
+					rootNode.XmlType.TypeArguments = typeArguments;
+					break;
+				}
+			}
+
 			var prefixes = PrefixesToIgnore(xmlns);
 			(rootNode.IgnorablePrefixes ?? (rootNode.IgnorablePrefixes = new List<string>())).AddRange(prefixes);
 			rootNode.Properties.AddRange(attributes);
