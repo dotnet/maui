ad90669cbe84887a27518e1a7b2a9691708b2b9a
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2024-10-10 16:01:35 +0530

    Fixed [Android] Label's HorizontalTextAlignment property is not updated properly at runtime

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue11655.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue11655 : _IssuesUITest
+	{
+		public Issue11655(TestDevice device) : base(device) { }
+
+		public override string Issue => "Label's HorizontalTextAlignment property is not updated properly at runtime";
+
+		[Test]
+		[Category(UITestCategories.Label)]
+		public void EntrySelectionLengthRuntimeUpdate()
+		{
+			App.WaitForElement("Button");
+			App.Tap("Button");
+			VerifyScreenshot();
+		}
+
+	}
+}
