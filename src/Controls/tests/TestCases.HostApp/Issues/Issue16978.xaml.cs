7ffc9c22a21c7804f00e032a0080609b29e1c624
Suthi <suthi.yuvaraj@syncfusion.com>
2024-10-10 20:49:17 +0530

    Commit for test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue16978.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.Controls.Sample.Issues
+{
+	public partial class Issue16978 : ContentPage
+	{
+		public Issue16978()
+		{
+			InitializeComponent();
+		}
+		private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+		{
+			(sender as Grid).BackgroundColor = Colors.GreenYellow;
+		}
+	}
+}
