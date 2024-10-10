d46dbf8fb081d857358327c8f10b97a234bb5f62
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-10-10 13:32:36 +0100

    Implement current UITests extension methods using an identifier using a Query

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla32148.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla32148.cs
@@ -12,14 +12,14 @@ public class Bugzilla32148 : _IssuesUITest
 
 	public override string Issue => " Pull to refresh hides the first item on a list view";
 
-	// [Test]
-	// [Category(UITestCategories.ListView)]
-	// [FailsOnIOS]
-	// public void Bugzilla32148Test()
-	// {
-	// 	App.WaitForElement("Contact0 LastName");
-	// 	App.Tap("Search");
-	// 	App.WaitForElement("Contact0 LastName");
-	// 	App.Screenshot("For manual review, is the first cell visible?");
-	// }
+	[Test]
+	[Category(UITestCategories.ListView)]
+	[FailsOnIOS]
+	public void Bugzilla32148Test()
+	{
+		App.WaitForNoElement("Contact0 LastName");
+		App.Tap(AppiumQuery.ByXPath("//*[@text='" + "Search" + "']"));
+		App.WaitForNoElement("Contact0 LastName");
+		App.Screenshot("For manual review, verify that the first cell is visible");
+	}
 }
