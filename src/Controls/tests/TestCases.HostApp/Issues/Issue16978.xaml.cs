7c127c1f0b3555d356de70bf3a4488f43b8c0287
Suthi <suthi.yuvaraj@syncfusion.com>
2024-10-11 19:00:12 +0530

    commit for Gesture on iOS

--- src/Controls/tests/TestCases.HostApp/Issues/Issue16978.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue16978.xaml.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Maui.Controls.Sample.Issues
 {
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 16978, "[Android]PanGestureRecognizer is not updated for Frame",
+	PlatformAffected.Android)]
 	public partial class Issue16978 : ContentPage
 	{
 		public Issue16978()
