d87adc36942f7d6b90caef1a9d99efac07130835
Jakub Florkowski <kubaflo123@gmail.com>
2024-10-12 13:06:49 +0200

    Removed an xaml file for ui test

--- src/Controls/tests/TestCases.HostApp/Issues/Issue25114.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25114.xaml.cs
@@ -1,8 +1,5 @@
 namespace Maui.Controls.Sample.Issues;
-using System;
-using Microsoft.Maui;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Xaml;
 
 [Issue(IssueTracker.Github, 25114, "NullReferenceException when setting BarBackgroundColor for a NavigationPage", PlatformAffected.All)]
 public class Issue25114Flyout : FlyoutPage
@@ -19,22 +16,25 @@ public class Issue25114Flyout : FlyoutPage
 	}
 }
 
-
-public partial class Issue25114 : ContentPage
+public class Issue25114 : ContentPage
 {
 	public Issue25114()
 	{
-		InitializeComponent();
+		Content = new Button()
+		{
+			AutomationId = "button",
+			HeightRequest = 100,
+			Text = "Change the Navigation Bar's Color",
+			Command = new Command(() =>
+			{
+				Application.Current.Resources["Primary"] = "#0000FF";
+			})
+		};
 	}
 
 	protected override void OnAppearing()
 	{
 		base.OnAppearing();
 		Application.Current.Resources["Primary"] = "#00FF00";
-	}
-
-	private void OnButtonClicked(object sender, EventArgs e)
-	{
-		Application.Current.Resources["Primary"] = "#0000FF";
 	}
 }
