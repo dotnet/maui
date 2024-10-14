b1b92f856baaf8f64076b831c957bb62d0e58d55
Suthi <suthi.yuvaraj@syncfusion.com>
2024-10-14 13:08:38 +0530

    Commit for windows test case

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17390.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17390.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 {
 	internal class Issue17390 : _IssuesUITest
 	{
-		public override string Issue => "Shell where the bottom padding is not calculated properly when navigating between the tabs";
+		public override string Issue => "Shell bottom padding when navigating between tabs";
 
 		public Issue17390(TestDevice device) : base(device)
 		{
@@ -19,10 +19,12 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		public void ShellBottomPaddingWhenNavigatingBetweenTabs()
 		{
 			// Is a iOS issue; see https://github.com/dotnet/maui/issues/17390
-			App.WaitForElement("MainTabPage");
-			App.Click("InnerTabbedPageButton");
-			App.Click("OpenNonTabbedPage");
-			App.Click("BackToTabbedPageButton");
+			App.WaitForElement("InnerTabbedPageButton");
+			App.Tap("InnerTabbedPageButton");
+			App.WaitForElement("OpenNonTabbedPage");
+			App.Tap("OpenNonTabbedPage");
+			App.WaitForElement("BackToTabbedPageButton");
+			App.Tap("BackToTabbedPageButton");
 			VerifyScreenshot();
 		}
 	}
