27ed7f56babe4deb7df660a59e875f791e9711c0
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2024-10-14 16:45:38 +0530

    Added test sample and script.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue23801.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 23801, "Span GestureRecognizers don't work when the span is wrapped over two lines", PlatformAffected.Android)]
+public partial class Issue23801 : ContentPage
+{
+	public Issue23801()
+	{
+		InitializeComponent();
+	}
+
+	private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+	{
+		testLabel.Text = "Label Span tapped at end of first line";
+	}
+}
