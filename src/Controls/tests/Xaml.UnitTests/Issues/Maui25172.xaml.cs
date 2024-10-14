18c62e7fd61d0b68c8ce7a03027aecad4a028b1c
Simon Rozsival <simon@rozsival.com>
2024-10-14 13:34:40 +0200

    Update test

--- src/Controls/tests/Xaml.UnitTests/Issues/Maui25172.xaml.cs
+++ src/Controls/tests/Xaml.UnitTests/Issues/Maui25172.xaml.cs
@@ -6,13 +6,17 @@ using NUnit.Framework;
 
 namespace Microsoft.Maui.Controls.Xaml.UnitTests;
 
-[XamlCompilation(XamlCompilationOptions.Skip)]
 public partial class Maui25172 : CoreContentPage<VM25172>
 {
     public Maui25172()
     {
         InitializeComponent();
     }
+
+	public Maui25172(bool useCompiledXaml)
+	{
+		//this stub will be replaced at compile time
+	}
 
     [TestFixture]
     class Test
@@ -30,9 +34,15 @@ public partial class Maui25172 : CoreContentPage<VM25172>
         }
 
         [Test]
-        public void CompilationWithGenericBaseClassSucceeds()
+        public void CompilationWithGenericBaseClassSucceeds([Values(true, false)] bool useCompiledXaml)
         {
-			MockCompiler.Compile(typeof(Maui25172));
+			if (useCompiledXaml)
+			{
+				MockCompiler.Compile(typeof(Maui25172));
+			}
+
+			var page = new Maui25172(useCompiledXaml);
+			Assert.IsTrue(typeof(CoreContentPage<VM25172>).IsAssignableFrom(page.GetType()));
         }
     }
 }
