302063ab021a10958667945b52d65c3fd8780b07
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-10-15 18:56:40 +0530

    new test case and snapshot added for android & iOS

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20706.cs
@@ -0,0 +1,28 @@
+﻿#if !MACCATALYST
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue20706 : _IssuesUITest
+	{
+		public override string Issue => "Stepper doesn't change increment value when being bound to a double in MVVM context (Windows)";
+		public Issue20706(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.Stepper)]
+		public void ChangeIncrementValue()
+		{
+			App.WaitForElement("incrementButton");
+			App.IncreaseStepper("myStepper");
+			App.Click("incrementButton");
+			App.IncreaseStepper("myStepper");
+			App.DecreaseStepper("myStepper");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
