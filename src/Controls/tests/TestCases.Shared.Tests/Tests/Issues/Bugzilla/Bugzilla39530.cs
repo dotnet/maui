595205946f5ec74adf47064597a20593be3d9d8a
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-10-17 01:50:23 +0200

    More changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla39530.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla39530.cs
@@ -18,6 +18,7 @@ public class Bugzilla39530 : _IssuesUITest
 	[Test]
 	[FailsOnIOS]
 	[FailsOnMac]
+	[FailsOnWindows("Fails finding the frame element. Investigate the cause.")]
 	public void Bugzilla39530PanTest()
 	{
 		if (App is not AppiumApp app2 || app2 is null || app2.Driver is null)
