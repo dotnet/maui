f9fe93713d4f5cfa701a1796f08bb20532843852
Karthik Raja <karthikraja.kalaimani@syncfusion.com>
2024-10-16 14:41:56 +0530

    Test case committed

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue12213.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue12213 : _IssuesUITest
+	{
+		public Issue12213(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "[Windows] TapGestureRecognizer not working on Entry";
+
+		[Test]
+		[Category(UITestCategories.Entry)]
+		[Category(UITestCategories.Gestures)]
+		[FailsOnMac]
+		public void TapGestureRecognizerNotWorkingOnEntry()
+		{
+			App.WaitForElement("EntryId");
+			App.Tap("EntryId");
+			VerifyScreenshot();
+		}
+	}
+}
