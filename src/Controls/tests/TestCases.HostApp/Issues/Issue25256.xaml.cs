b3ffcca084097591de0df3a1a15b982c9c6983dc
Jakub Florkowski <kubaflo123@gmail.com>
2024-10-18 01:15:50 +0200

    [Android] Reset ListView ScrolledEventArgs.ScrollY when ItemsSource changes

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25256.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, "25256", "ListView ScrolledEventArgs.ScrollY does not reset when ItemsSource changes", PlatformAffected.Android)]
+	public partial class Issue25256 : ContentPage
+	{
+
+		public Issue25256()
+		{
+			InitializeComponent();
+			list.ItemsSource = Enumerable.Range(0, 100).Select(i => i.ToString()).ToList();
+		}
+
+		private void ListView_Scrolled(object sender, ScrolledEventArgs e)
+		{
+			scrollYLabel.Text = e.ScrollY.ToString();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			list.ItemsSource = Enumerable.Range(100, 100).Select(i => i.ToString()).ToList();
+		}
+	}
+}
