deaf345cc74c448ea2ab377b6681f298eb04b70c
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2024-10-18 14:51:37 +0530

    Fixed 25181.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25181.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 25181, "CollectionView item alignment issue in HorizontalGrid layout when only one item is present", PlatformAffected.iOS)]
+public partial class Issue25181 : ContentPage
+{
+	public IList<TestItem> Items { get; set; }
+
+	public Issue25181()
+    {
+        InitializeComponent();
+		BindingContext = this;
+		Items = new List<TestItem>();
+		Items.Add(new TestItem() { Name = "Test Item 1" });
+		collectionview.ItemsSource = Items;
+	}
+	
+	public class TestItem
+	{
+		public string Name { get; set; }
+	}
+}
