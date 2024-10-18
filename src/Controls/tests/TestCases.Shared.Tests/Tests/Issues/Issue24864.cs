14d499e485ea5c1af8d4c23f507a096e328d6b63
Jakub Florkowski <kubaflo123@gmail.com>
2024-10-18 12:18:13 +0200

    [iOS] Transparent button border color when border color is not set

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue24864.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue24864 : _IssuesUITest
+	{
+		public override string Issue => "Button control: inconsistent border visibility behaviour iOS vs Android when BackgroundColor is Transparent";
+
+		public Issue24864(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.Button)]
+		public void ButtonShouldHaveTransparentBorder()
+		{
+			App.WaitForElement("button");
+
+			VerifyScreenshot();
+		}
+	}
+}
