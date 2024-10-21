34bbe3fd3544542784017e21edadd177553d53b3
Shalini-Ashokan <shalini.ashokan@syncfusion.com>
2024-10-21 17:37:35 +0530

    Added test cases

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17123.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue17123 : _IssuesUITest 
+	{
+        public override string Issue => "TableSection and TableRoot Title properties are displayed as lower case";
+
+        public Issue17123(TestDevice device) : base(device)
+        {
+        }
+
+        [Test]
+		[Category(UITestCategories.Frame)]
+		public void ValidateTableViewTitles()
+        {
+			App.WaitForElement("TableView");
+			App.WaitForElement("TableRootLabel");
+			var tableViewRootText = App.FindElement("TableRootLabel").GetText();
+			if (string.IsNullOrEmpty(tableViewRootText))
+			{
+				Assert.Fail("Table root text is does not match expected");
+			}
+			else
+			{
+				Assert.That(tableViewRootText, Is.EqualTo("TableRoot Title Text"));
+			}
+
+			App.WaitForElement("TableSectionLabel");
+			var tableViewSelectionText = App.FindElement("TableSectionLabel").GetText();
+			if (string.IsNullOrEmpty(tableViewSelectionText))
+			{
+				Assert.Fail("Table selection text is does not match expected");
+			}
+			else
+			{
+				Assert.That(tableViewSelectionText, Is.EqualTo("TableSection Title Text"));
+			}
+
+			VerifyScreenshot();
+        }
+    }
+}
