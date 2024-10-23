6e623558fe5f70a9faed3ebf30e23447134e9f6e
Vishnu Menon <vishnumenon.krishnadithyan@syncfusion.com>
2024-10-23 19:32:53 +0530

    changes removed

--- src/Controls/src/Core/Compatibility/Handlers/ListView/Windows/ListViewRenderer.cs
+++ src/Controls/src/Core/Compatibility/Handlers/ListView/Windows/ListViewRenderer.cs
@@ -812,8 +812,6 @@ namespace Microsoft.Maui.Controls.Handlers.Compatibility
 
 		void OnControlSelectionChanged(object sender, WSelectionChangedEventArgs e)
 		{
-			if (Element is null)
-				return;
 			bool areEqual = false;
 
 			if (Element.SelectedItem != null && Element.SelectedItem.GetType().IsValueType)
