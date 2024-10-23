7d5e245d6de24de74a52962713aead0699806060
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-10-23 17:45:31 +0530

    review changes added

--- src/Controls/src/Core/Editor/Editor.iOS.cs
+++ src/Controls/src/Core/Editor/Editor.iOS.cs
@@ -11,10 +11,7 @@ namespace Microsoft.Maui.Controls
 			Platform.TextExtensions.UpdateText(handler.PlatformView, editor);
 
 			// Any text changes in the editor field require recalculating the CharacterSpacing by regenerating the attributed string to properly apply the spacing and override the current text formatting.
-			if (editor.CharacterSpacing > 0)
-			{
-				handler.PlatformView?.UpdateCharacterSpacing(editor);
-			}
+			handler?.UpdateValue(nameof(CharacterSpacing));
 		}
 	}
 }
