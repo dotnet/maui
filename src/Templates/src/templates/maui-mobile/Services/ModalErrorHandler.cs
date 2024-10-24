4d6b87a813d46a71208241ffcf3f9c1fba371546
Shane Neuville <shneuvil@microsoft.com>
2024-10-24 14:35:34 -0500

    - remove lock

--- src/Templates/src/templates/maui-mobile/Services/ModalErrorHandler.cs
+++ src/Templates/src/templates/maui-mobile/Services/ModalErrorHandler.cs
@@ -5,15 +5,27 @@ namespace MauiApp._1.Services;
 /// </summary>
 public class ModalErrorHandler : IErrorHandler
 {
+	Task? _currentTask;
 	/// <summary>
 	/// Handle error in UI.
 	/// </summary>
 	/// <param name="ex">Exception.</param>
 	public void HandleError(Exception ex)
 	{
-		lock (this)
+		DisplayAlert(ex).FireAndForgetSafeAsync();
+	}
+
+	async Task DisplayAlert(Exception ex)
+	{
+		// Only display one error at a time
+		if (_currentTask is null)
+			return;
+
+		if (Shell.Current is Shell shell)
 		{
-			Shell.Current?.DisplayAlert("Error", ex.Message, "OK");
+			_currentTask = shell.DisplayAlert("Error", ex.Message, "OK");
+			await (_currentTask ?? Task.CompletedTask);
+			_currentTask = null;
 		}
 	}
 }
