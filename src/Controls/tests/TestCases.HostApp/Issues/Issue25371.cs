974836b762e2b2da0720fcdfb192a976f48c9c77
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2024-10-28 19:15:41 +0530

    Added test cases

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25371.cs
@@ -0,0 +1,54 @@
+using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 25371, "OnNavigatedTo not called when navigating back to a specific page", PlatformAffected.iOS | PlatformAffected.macOS)]
+	public class Issue25371 : TestContentPage
+	{
+		Label label;
+		protected override void Init()
+		{
+			var stackLayout = new StackLayout();
+		    label = new Label()
+			{
+				Text = "Welcome to Main page",
+				AutomationId ="MainPageLabel"
+			};
+
+			var button = new Button() { Text = "MoveToNextPage", AutomationId="MoveToNextPage" };
+			button.Clicked += Button_Clicked;
+			stackLayout.Children.Add(label);
+			stackLayout.Children.Add(button);
+			Content = stackLayout;
+			
+		}
+		protected override void OnNavigatedTo(NavigatedToEventArgs args)
+		{
+			label.Text = "OnNavigationTo method is called";
+			base.OnNavigatedTo(args);
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new SecondPage());
+		}
+	}
+
+	public class SecondPage : ContentPage
+	{
+		public SecondPage()
+		{
+			var label = new Label()
+			{
+				Text = "Welcome to Second Page",
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center
+			};
+
+			Content = label;
+		}
+	}
+}
+
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25371.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue25371 : _IssuesUITest
+	{
+		public override string Issue => "OnNavigatedTo not called when navigating back to a specific page";
+
+		public Issue25371(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.TableView)]
+		public void ValidateTableViewTitles()
+		{
+			App.WaitForElement("MainPageLabel");
+			App.Tap("MoveToNextPage");
+			App.WaitForElement("SecondPageLabel");
+
+			App.LongPress(BackButtonAutomationId);
+			var navigationToLabel = App.FindElement("MainPageLabel").GetText();
+			if (string.IsNullOrEmpty(navigationToLabel))
+			{
+				Assert.Fail("OnNavigationTo method is not called");
+			}
+			else
+			{
+				Assert.That(navigationToLabel, Is.EqualTo("OnNavigationTo method is called"));
+			}
+		}
+	}
+}
