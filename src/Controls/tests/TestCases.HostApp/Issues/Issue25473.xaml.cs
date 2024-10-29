ed3242aa79365e76743156fa5e16626e6e3dee3a
Prakash Kannan <prakash.kannan@syncfusion.com>
2024-10-29 13:18:29 +0530

    Fixed-Entry-ClearButtonVisibility-Issue

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25473.xaml.cs
@@ -0,0 +1,12 @@
+namespace Maui.Controls.Sample.Issues
+{
+
+	[Issue(IssueTracker.Github, 25473, "MAUI Entry in Windows always shows ClearButton despite ClearButtonVisibility set to 'Never'", PlatformAffected.UWP)]
+	public partial class Issue25473 : ContentPage
+	{
+		public Issue25473()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25473.cs
@@ -0,0 +1,28 @@
+﻿#if !MACCATALYST
+using Microsoft.Maui.Platform;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue25473 : _IssuesUITest
+	{
+		public Issue25473(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "MAUI Entry in Windows always shows ClearButton despite ClearButtonVisibility set to 'Never'";
+
+		[Test]
+		[Category(UITestCategories.Entry)]
+		public void VerifyEntryClearButtonVisibilitySetToNever()
+		{
+			App.WaitForElement("EntryField");
+			App.Tap("EntryField");
+			App.EnterText("EntryField", "Entry");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
