9f3568f3566296d56d022ff7a1cdc77d508c770d
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2024-10-29 18:48:29 +0530

    Automation sample for 25453.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25504.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 25504, "ListView crashes when changing the root page inside the ItemSelected event", PlatformAffected.UWP)]
+	public partial class Issue25504 : ContentPage
+	{
+		public Issue25504()
+		{
+			InitializeComponent();
+			this.BindingContext = this;
+
+			var chapters = new List<Chapter>
+		{
+			new Chapter { Title = "1. Introduction to .NET MAUI"},
+		};
+
+			listView.ItemsSource = chapters;
+		}
+
+		private void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+		{
+			if (Application.Current?.Windows.Count > 0)
+			{
+				Application.Current.Windows[0].Page = new DetailsPage();
+			}
+		}
+
+		public class DetailsPage : ContentPage
+		{
+			public DetailsPage()
+			{
+				Content = new Label { Text = "Details Page", AutomationId = "DetailsPage" };
+			}
+		}
+	}
+	public class Chapter
+	{
+		public string Title { get; set; }
+	}
+}
