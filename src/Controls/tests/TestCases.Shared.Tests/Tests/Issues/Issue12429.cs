42f825d61216b589c64b2acdbc5e6fb7f34631a5
Jakub Florkowski <kubaflo123@gmail.com>
2024-10-31 13:42:34 +0100

    Added UiTests (#12429)

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue12429.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue12429 : _IssuesUITest
+{
+	public override string Issue => "[iOS] CollectionView Items display issue when Header is resized";
+
+	public Issue12429(TestDevice device) : base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.CollectionView)]
+	public void HeaderShouldNotCollapseWithItems()
+	{
+		App.WaitForElement("button");
+		App.Click("button");
+		App.Click("button");
+		App.Click("button");
+		
+		App.WaitForElement("HeaderItem4");
+		
+		//The test passes of header has 4 elements that don't overlap with the collection view's items
+		VerifyScreenshot();
+	}
+}
