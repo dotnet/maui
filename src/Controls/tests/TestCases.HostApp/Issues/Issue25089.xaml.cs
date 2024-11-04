a568b57c90a32236fa9259c5cfb42515d88164f8
BagavathiPerumal <bagavathiperumal.a@syncfusion.com>
2024-11-04 15:47:04 +0530

    fix-25089-Testcase added.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25089.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 25089, "OnAppearing of Page called again, although this page was on already replaced NavigationStack", PlatformAffected.UWP | PlatformAffected.Android)]
+	public partial class Issue25089 : Shell
+	{
+		public static SharedViewModel SharedViewModel { get; } = new SharedViewModel();
+		public Issue25089()
+		{
+			InitializeComponent();
+			Routing.RegisterRoute("FirstPage", typeof(FirstPage));
+			Routing.RegisterRoute("SecondPage", typeof(SecondPage));
+		}
+	}
+
+	public class SharedViewModel : INotifyPropertyChanged
+	{
+		string _statusText = "Page Appearing Sequence: Initial";
+
+		public string StatusText
+		{
+			get => _statusText;
+			set
+			{
+				if (_statusText != value)
+				{
+					_statusText = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			Appearing += MainPage_Appearing;	
+			BindingContext = Issue25089.SharedViewModel;
+
+			var statusLabel = new Label
+			{
+				AutomationId = "StatusLabel",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center
+			};
+			statusLabel.SetBinding(Label.TextProperty, nameof(SharedViewModel.StatusText));
+
+			var mainButton = new Button
+			{
+				Text = "Go to First Page",
+				AutomationId = "MainButton",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+			};
+
+			mainButton.Clicked += async (sender, e) =>
+			{
+				await Shell.Current.GoToAsync("FirstPage", true);
+			};
+
+			var stackLayout = new VerticalStackLayout
+			{
+				Children = { statusLabel, mainButton }
+			};
+
+			Content = stackLayout;
+		}
+
+		void MainPage_Appearing(object sender, EventArgs e)
+		{
+			Issue25089.SharedViewModel.StatusText += " | MainPage appeared";
+		}
+	}
+
+	public class FirstPage : ContentPage
+	{
+		public FirstPage()
+		{
+			Title = "First Page";
+			Appearing += FirstPage_Appearing;
+
+			var firstPageButton = new Button
+			{
+				Text = "Go to Second Page",
+				AutomationId = "FirstPageButton",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			firstPageButton.Clicked += async (sender, e) =>
+			{
+				await Shell.Current.GoToAsync("//SecondPage", true);
+			};
+
+			var stackLayout = new VerticalStackLayout
+			{
+				Children = { firstPageButton }
+			};
+
+			Content = stackLayout;
+		}
+
+		void FirstPage_Appearing(object sender, EventArgs e)
+		{
+			Issue25089.SharedViewModel.StatusText += " | FirstPage appeared";
+		}
+	}
+
+	public class SecondPage : ContentPage
+	{
+		public SecondPage()
+		{
+			Title = "Second Page";
+			Appearing += SecondPage_Appearing;
+
+			var secondPageButton = new Button
+			{
+				Text = "Go to Main Page",
+				AutomationId = "SecondPageButton",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			secondPageButton.Clicked += async (sender, e) =>
+			{
+				await Shell.Current.GoToAsync("//MainPage", true);
+			};
+
+			var stackLayout = new VerticalStackLayout
+			{
+				Children = { secondPageButton }
+			};
+
+			Content = stackLayout;
+		}
+
+		void SecondPage_Appearing(object sender, EventArgs e)
+		{
+			Issue25089.SharedViewModel.StatusText += " | SecondPage appeared";
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25089.cs
@@ -0,0 +1,31 @@
+﻿#if !MACCATALYST
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue25089 : _IssuesUITest
+    {
+		public Issue25089(TestDevice device): base(device)
+		{
+		}
+
+		public override string Issue => "OnAppearing of Page called again, although this page was on already replaced NavigationStack";
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void VerifyPageAppearingSequence()
+		{
+			App.WaitForElement("MainButton");
+			App.Tap("MainButton");
+			App.WaitForElement("FirstPageButton");
+			App.Tap("FirstPageButton");
+			App.WaitForElement("SecondPageButton");
+			App.Tap("SecondPageButton");
+			var label = App.FindElement("StatusLabel");
+			Assert.That(label.GetText(), Is.EqualTo("Page Appearing Sequence: Initial | MainPage appeared | FirstPage appeared | SecondPage appeared | MainPage appeared"));
+		}
+	}
+}
+#endif
