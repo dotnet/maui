f149944785ef24b660893f99c679da4b5edee835
Suthi <suthi.yuvaraj@syncfusion.com>
2024-11-04 10:04:00 +0530

    Testcases commit

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13537.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue13537 : _IssuesUITest
+		{
+		const string BackButtonAutomationId = "Back";
+		public Issue13537(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+			public override string Issue => "ApplyQueryAttributes should Trigger for all navigations";
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void ApplyQueryAttributeShouldTriggerforPushAndPopButton()
+		{
+			App.WaitForElement("TestLabel");
+			App.Tap("PushAsyncButton");
+			App.WaitForElement("TestLabel3");
+			App.Tap("PopAsyncButton");
+			VerifyScreenshot();
+		}
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void ApplyQueryAttributeShouldTriggerforNavigatingBetweenTabs()
+		{
+			App.WaitForElement("TestLabel");
+			App.Tap("Favourite");
+			VerifyScreenshot();
+		}
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void ApplyQueryAttributeShouldTriggerforBackButton()
+		{
+			App.WaitForElement("TestLabel");
+			App.Tap("PushAsyncButton");
+			App.Tap(BackButtonAutomationId);
+			VerifyScreenshot();
+		}
+	}
+
+}
