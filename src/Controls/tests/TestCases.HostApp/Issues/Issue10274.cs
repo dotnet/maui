4632a2d29d49484f1dad9b944b0b7e3fb830a4ef
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2024-11-05 16:51:42 +0530

    UI Testcases added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue10274.cs
@@ -0,0 +1,80 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 10274, "MAUI Flyout does not work on Android when not using Shell", PlatformAffected.Android)]
+	public class Issue10274 : NavigationPage
+	{
+		public Issue10274() : base(new MainPage())
+		{
+
+		}
+
+		public class MainPage : ContentPage
+		{
+			public MainPage()
+			{
+				var button = new Button
+				{
+					Text = "Navigate to Flyout Page",
+					AutomationId = "button"
+				};
+
+				button.Clicked += OnNavigateToFlyoutPageClicked;
+
+				Content = new StackLayout
+				{
+					Padding = new Thickness(20),
+					Children = { button }
+				};
+			}
+			private async void OnNavigateToFlyoutPageClicked(object sender, EventArgs e)
+			{
+				await Navigation.PushAsync(new CustomFlyoutPage());
+			}
+		}
+
+		public class CustomFlyoutPage : FlyoutPage
+		{
+			public CustomFlyoutPage()
+			{
+				Flyout = new ContentPage
+				{
+					Title = "Flyout",
+					Content = new StackLayout
+					{
+						Padding = new Thickness(20),
+						Children =
+						{
+							new Label { Text = "This is the Flyout page." }
+						}
+					}
+				};
+
+				var button = new Button
+				{
+					Text = "Go Back",
+					AutomationId = "flyoutPageButton"
+				};
+				button.Clicked += OnGoBackClicked;
+
+				var detailPage = new ContentPage
+				{
+					Title = "Detail",
+					Content = new StackLayout
+					{
+						Padding = new Thickness(20),
+
+						Children = { button }
+					}
+				};
+
+				Detail = new NavigationPage(detailPage);
+			}
+
+			private async void OnGoBackClicked(object sender, EventArgs e)
+			{
+				await Navigation.PopAsync();
+			}
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10274.cs
@@ -0,0 +1,31 @@
+﻿#if !MACCATALYST
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue10274 : _IssuesUITest
+    {
+		public Issue10274(TestDevice device): base(device)
+		{
+		}
+
+		public override string Issue => "MAUI Flyout does not work on Android when not using Shell";
+
+		[Test]
+		[Category(UITestCategories.FlyoutPage)]
+		public void FlyoutPageNavigation()
+		{
+			App.WaitForElement("button");
+			App.Tap("button");
+
+			App.WaitForElement("flyoutPageButton");
+			App.Tap("flyoutPageButton");
+
+			App.WaitForElement("button");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
