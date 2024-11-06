194163a1a2bccf427fb097d2fbb6ea89d782bcaf
Stephane Delcroix <stephane@delcroix.org>
2024-11-06 17:51:27 +0100

    wip

--- src/Controls/src/Xaml/XamlCompilationAttribute.cs
+++ src/Controls/src/Xaml/XamlCompilationAttribute.cs
@@ -4,52 +4,44 @@ using System.Runtime.CompilerServices;
 
 namespace Microsoft.Maui.Controls.Xaml
 {
+	[Obsolete("use XamlProcessingOptions instead.")]
+	[Flags]
 	public enum XamlCompilationOptions
 	{
-		/// <summary>
-		/// Picks the best Inflator available, don't change it unless you know what you're doing.
-		/// </summary>
-		Default = 0,
-		RuntimeInflator = 1 << 0,
-		[Obsolete("use Default. Or use RuntimeInflator to force XAML inflation at runtime.")]
-		Skip = RuntimeInflator,
-		XamlCInflator = 1 << 1,
-		[Obsolete("use Default. Or use XamlCInflator to force XAML inflation using XamlC at compile time.")]
-		Compile = XamlCInflator,
-		SourceGenInflator = 1 << 2,
+		Skip = XamlInflator.Runtime,
+		Compile = XamlInflator.XamlC,
 	}
 
+	[Obsolete("use XamlProcessingAttribute instead.")]
 	[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Module | AttributeTargets.Class, Inherited = false)]
 	public sealed class XamlCompilationAttribute : Attribute
 	{
-		public XamlCompilationAttribute(XamlCompilationOptions xamlCompilationOptions) : this(xamlCompilationOptions, false)
-		{
-		}
-		internal XamlCompilationAttribute(XamlCompilationOptions inflators, bool generateDebugSwitch)
+		public XamlCompilationAttribute(XamlCompilationOptions xamlCompilationOptions) 
 		{ 
-			GenerateDebugSwitch = generateDebugSwitch;
-			XamlCompilationOptions = inflators;
+			XamlCompilationOptions = xamlCompilationOptions;
 		}
 
-		public XamlCompilationOptions XamlCompilationOptions { get; }
-		internal bool GenerateDebugSwitch { get; } = false;
+		public XamlCompilationOptions XamlCompilationOptions { get; set;}
 	}
 
 	static class XamlCExtensions
 	{
 		public static bool IsCompiled(this Type type)
 		{
+#pragma warning disable CS0618 // Type or member is obsolete
 			var attr = type.GetCustomAttribute<XamlCompilationAttribute>();
 			if (attr != null)
-				return attr.XamlCompilationOptions == XamlCompilationOptions.XamlCInflator;
+				return attr.XamlCompilationOptions == XamlCompilationOptions.Compile;
 			attr = type.Module.GetCustomAttribute<XamlCompilationAttribute>();
 			if (attr != null)
-				return attr.XamlCompilationOptions == XamlCompilationOptions.XamlCInflator;
+				return attr.XamlCompilationOptions == XamlCompilationOptions.Compile;
 			attr = type.Assembly.GetCustomAttribute<XamlCompilationAttribute>();
 			if (attr != null)
-				return attr.XamlCompilationOptions == XamlCompilationOptions.XamlCInflator;
+				return attr.XamlCompilationOptions == XamlCompilationOptions.Compile;
 
 			return false;
+#pragma warning restore CS0618 // Type or member is obsolete
+
 		}
 	}
 }
