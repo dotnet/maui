284cebd2c9012868f7dcfb4aaed44f806bf0afa3
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2024-11-07 19:02:40 +0530

    Added the test case for layout and label element.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue15559.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue15559 : _IssuesUITest
+	{
+		public override string Issue => "[iOS] Vertical layout content of label is truncated when a width request is set in the layout";
+
+		public Issue15559(TestDevice device) : base(device)
+		{ }
+
+		[Test]
+		[Category(UITestCategories.Layout)]
+		public void LabelDisplayWithoutCroppingInsideVerticalLayout()
+		{
+			App.WaitForElement("LayoutWidth");
+			App.WaitForElement("LabelWidth");
+			VerifyScreenshot();
+		}
+	}
+}
