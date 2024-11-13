ed1f9740b6b6638c903774d947aac8be94632204
Stephane Delcroix <stephane@delcroix.org>
2024-11-13 15:43:16 -0800

    expand markup part x

--- src/Controls/src/SourceGen/ExpandMarkupsVisitor.cs
+++ src/Controls/src/SourceGen/ExpandMarkupsVisitor.cs
@@ -42,11 +42,11 @@ class ExpandMarkupsVisitor : IXamlNodeVisitor
 			return;
 
 		var markupString = markupnode.MarkupString;
-		// if (ParseExpression(ref markupString, markupnode.NamespaceResolver, markupnode, markupnode, parentNode) is IElementNode node)
-		// {
-		// 	((IElementNode)parentNode).Properties[propertyName] = node;
-		// 	node.Parent = parentNode;
-		// }
+		if (ParseExpression(ref markupString, markupnode.NamespaceResolver, markupnode, markupnode, parentNode) is IElementNode node)
+		{
+			((IElementNode)parentNode).Properties[propertyName] = node;
+			node.Parent = parentNode;
+		}
 	}
 
 	public void Visit(ElementNode node, INode parentNode)
@@ -61,170 +61,170 @@ class ExpandMarkupsVisitor : IXamlNodeVisitor
 	{
 	}
 
-	// INode? ParseExpression(ref string expression, IXmlNamespaceResolver nsResolver, IXmlLineInfo xmlLineInfo, INode node, INode parentNode)
-	// {
-	// 	if (expression.StartsWith("{}", StringComparison.Ordinal))
-	// 		return new ValueNode(expression.Substring(2), null);
+	INode? ParseExpression(ref string expression, IXmlNamespaceResolver nsResolver, IXmlLineInfo xmlLineInfo, INode node, INode parentNode)
+	{
+		if (expression.StartsWith("{}", StringComparison.Ordinal))
+			return new ValueNode(expression.Substring(2), null);
 
-	// 	if (expression[expression.Length - 1] != '}')
-	// 	{
-	// 		//FIXME fix location
-	// 		var location = Context.FilePath is not null ? Location.Create(Context.FilePath, new TextSpan(), new LinePositionSpan()) : null;
-	// 		Context.ReportDiagnostic(Diagnostic.Create(Descriptors.ExpressionNotClosed, location));
-	// 		return null;
-	// 	}
+		if (expression[expression.Length - 1] != '}')
+		{
+			//FIXME fix location
+			var location = Context.FilePath is not null ? Location.Create(Context.FilePath, new TextSpan(), new LinePositionSpan()) : null;
+			Context.ReportDiagnostic(Diagnostic.Create(Descriptors.ExpressionNotClosed, location));
+			return null;
+		}
 
-	// 	if (!MarkupExpressionParser.MatchMarkup(out var match, expression, out var len))
-	// 	{
-	// 		//FIXME fix location
-	// 		var location = Context.FilePath is not null ? Location.Create(Context.FilePath, new TextSpan(), new LinePositionSpan()) : null;
-	// 		Context.ReportDiagnostic(Diagnostic.Create(Descriptors.XamlParserError, location));
-	// 		return null;
-	// 	}
+		if (!MarkupExpressionParser.MatchMarkup(out var match, expression, out var len))
+		{
+			//FIXME fix location
+			var location = Context.FilePath is not null ? Location.Create(Context.FilePath, new TextSpan(), new LinePositionSpan()) : null;
+			Context.ReportDiagnostic(Diagnostic.Create(Descriptors.XamlParserError, location));
+			return null;
+		}
 
-	// 	expression = expression.Substring(len).TrimStart();
-	// 	if (expression.Length == 0)
-	// 	{
-	// 		//FIXME fix location
-	// 		var location = Context.FilePath is not null ? Location.Create(Context.FilePath, new TextSpan(), new LinePositionSpan()) : null;
-	// 		Context.ReportDiagnostic(Diagnostic.Create(Descriptors.ExpressionNotClosed, location));
-	// 		return null;
-	// 	}
-	// 	var serviceProvider = new XamlServiceProvider(node, Context);
-	// 	//serviceProvider.Add(typeof(IXmlNamespaceResolver), nsResolver);
+		expression = expression.Substring(len).TrimStart();
+		if (expression.Length == 0)
+		{
+			//FIXME fix location
+			var location = Context.FilePath is not null ? Location.Create(Context.FilePath, new TextSpan(), new LinePositionSpan()) : null;
+			Context.ReportDiagnostic(Diagnostic.Create(Descriptors.ExpressionNotClosed, location));
+			return null;
+		}
+		var serviceProvider = new XamlServiceProvider(node, Context);
+		//serviceProvider.Add(typeof(IXmlNamespaceResolver), nsResolver);
 
-	// 	return new MarkupExpansionParser().Parse(match!, ref expression, serviceProvider);
-	// }
+		return new MarkupExpansionParser().Parse(match!, ref expression, serviceProvider);
+	}
 
-	// class SGContextProvider
-	// {
-	// 	public SGContextProvider(SourceGenContext context) => Context = context;
+	class SGContextProvider
+	{
+		public SGContextProvider(SourceGenContext context) => Context = context;
 
-	// 	public SourceGenContext Context { get; }
-	// }
+		public SourceGenContext Context { get; }
+	}
 
-	// public class MarkupExpansionParser : MarkupExpressionParser, IExpressionParser<INode>
-	// {
-	// 	IElementNode _node;
-	// 	object IExpressionParser.Parse(string match, ref string remaining, IServiceProvider serviceProvider) => Parse(match, ref remaining, serviceProvider);
+	public class MarkupExpansionParser : MarkupExpressionParser, IExpressionParser<INode>
+	{
+		IElementNode? _node;
+		object IExpressionParser.Parse(string match, ref string remaining, IServiceProvider serviceProvider) => Parse(match, ref remaining, serviceProvider);
 
-	// 	public INode Parse(string match, ref string remaining, IServiceProvider serviceProvider)
-	// 	{
-	// 		if (!(serviceProvider.GetService(typeof(IXmlNamespaceResolver)) is IXmlNamespaceResolver nsResolver))
-	// 			throw new ArgumentException();
-	// 		IXmlLineInfo? xmlLineInfo = null;
-	// 		if (serviceProvider.GetService(typeof(IXmlLineInfoProvider)) is IXmlLineInfoProvider xmlLineInfoProvider)
-	// 			xmlLineInfo = xmlLineInfoProvider.XmlLineInfo;
-	// 		var contextProvider = serviceProvider.GetService(typeof(SGContextProvider)) as SGContextProvider;
+		public INode Parse(string match, ref string remaining, IServiceProvider serviceProvider)
+		{
+			if (!(serviceProvider.GetService(typeof(IXmlNamespaceResolver)) is IXmlNamespaceResolver nsResolver))
+				throw new ArgumentException();
+			IXmlLineInfo? xmlLineInfo = null;
+			if (serviceProvider.GetService(typeof(IXmlLineInfoProvider)) is IXmlLineInfoProvider xmlLineInfoProvider)
+				xmlLineInfo = xmlLineInfoProvider.XmlLineInfo;
+			var contextProvider = serviceProvider.GetService(typeof(SGContextProvider)) as SGContextProvider;
 
-	// 		var split = match.Split(':');
-	// 		if (split.Length > 2)
-	// 			throw new ArgumentException();
+			var split = match.Split(':');
+			if (split.Length > 2)
+				throw new ArgumentException();
 
-	// 		var (prefix, name) = ParseName(match);
+			var (prefix, name) = ParseName(match);
 
-	// 		var namespaceuri = nsResolver.LookupNamespace(prefix) ?? "";
-	// 		if (!string.IsNullOrEmpty(prefix) && string.IsNullOrEmpty(namespaceuri))
-	// 			//FIXME report error properly
-	// 			throw new Exception();
-	// 			//throw new BuildException(BuildExceptionCode.XmlnsUndeclared, xmlLineInfo, null, prefix);
+			var namespaceuri = nsResolver.LookupNamespace(prefix) ?? "";
+			if (!string.IsNullOrEmpty(prefix) && string.IsNullOrEmpty(namespaceuri))
+				//FIXME report error properly
+				throw new Exception();
+				//throw new BuildException(BuildExceptionCode.XmlnsUndeclared, xmlLineInfo, null, prefix);
 
-	// 		IList<XmlType>? typeArguments = null;
-	// 		var childnodes = new List<(XmlName, INode)>();
-	// 		var contentname = new XmlName(null, null);
+			IList<XmlType>? typeArguments = null;
+			var childnodes = new List<(XmlName, INode)>();
+			var contentname = new XmlName(null, null);
 
-	// 		if (remaining.StartsWith("}", StringComparison.Ordinal))
-	// 		{
-	// 			remaining = remaining.Substring(1);
-	// 		}
-	// 		else
-	// 		{
-	// 			Property parsed;
-	// 			do
-	// 			{
-	// 				try {
-	// 					parsed = ParseProperty(serviceProvider, ref remaining);
-	// 				}
-	// 				catch (XamlParseException xpe)
-	// 				{
-	// 					//
-	// 				}
-	// 				XmlName childname;
+			if (remaining.StartsWith("}", StringComparison.Ordinal))
+			{
+				remaining = remaining.Substring(1);
+			}
+			else
+			{
+				Property parsed = new Property();
+				do
+				{
+					try {
+						parsed = ParseProperty(serviceProvider, ref remaining);
+					}
+					catch (XamlParseException )
+					{
+						//
+					}
+					XmlName childname;
 
-	// 				if (parsed.name == null)
-	// 				{
-	// 					childname = contentname;
-	// 				}
-	// 				else
-	// 				{
-	// 					var (propertyPrefix, propertyName) = ParseName(parsed.name);
+					if (parsed.name == null)
+					{
+						childname = contentname;
+					}
+					else
+					{
+						var (propertyPrefix, propertyName) = ParseName(parsed.name);
 
-	// 					childname = XamlParser.ParsePropertyName(new XmlName(
-	// 						propertyPrefix == "" ? null : nsResolver.LookupNamespace(propertyPrefix),
-	// 						propertyName));
+						childname = XamlParser.ParsePropertyName(new XmlName(
+							propertyPrefix == "" ? null : nsResolver.LookupNamespace(propertyPrefix),
+							propertyName));
 
-	// 					if (childname.NamespaceURI == null && childname.LocalName == null)
-	// 						continue;
-	// 				}
+						if (childname.NamespaceURI == null && childname.LocalName == null)
+							continue;
+					}
 
-	// 				if (childname == XmlName.xTypeArguments)
-	// 				{
-	// 					typeArguments = TypeArgumentsParser.ParseExpression(parsed.strValue, nsResolver, xmlLineInfo);
-	// 					childnodes.Add((childname, new ValueNode(typeArguments, nsResolver)));
-	// 				}
-	// 				else
-	// 				{
-	// 					var childnode = parsed.value as INode ?? new ValueNode(parsed.strValue, nsResolver);
-	// 					childnodes.Add((childname, childnode));
-	// 				}
-	// 			}
-	// 			while (!parsed.last);
-	// 		}
+					if (childname == XmlName.xTypeArguments)
+					{
+						typeArguments = TypeArgumentsParser.ParseExpression(parsed.strValue, nsResolver, xmlLineInfo);
+						childnodes.Add((childname, new ValueNode(typeArguments, nsResolver)));
+					}
+					else
+					{
+						var childnode = parsed.value as INode ?? new ValueNode(parsed.strValue, nsResolver);
+						childnodes.Add((childname, childnode));
+					}
+				}
+				while (!parsed.last);
+			}
 
 
-	// 		if (!(serviceProvider.GetService(typeof(IXamlTypeResolver)) is XamlTypeResolver typeResolver))
-	// 			throw new NotSupportedException();
+				// if (!(serviceProvider.GetService(typeof(IXamlTypeResolver)) is XamlTypeResolver typeResolver))
+				// 	throw new NotSupportedException();
 
-	// 		var xmltype = new XmlType(namespaceuri, name + "Extension", typeArguments);
+				var xmltype = new XmlType(namespaceuri, name + "Extension", typeArguments);
 
-	// 		//The order of lookup is to look for the Extension-suffixed class name first and then look for the class name without the Extension suffix.
-	// 		if (!typeResolver.TryResolve(xmltype, out _))
-	// 		{
-	// 			xmltype = new XmlType(namespaceuri, name, typeArguments);
-	// 			if (!typeResolver.TryResolve(xmltype, out _))
-	// 			{
-	// 				var ex = new XamlParseException($"MarkupExtension not found for {match}", serviceProvider);
-	// 				if (ExceptionHandler != null)
-	// 				{
-	// 					ExceptionHandler(ex);
-	// 					return null;
-	// 				}
-	// 				throw ex;
-	// 			}
-	// 		}
+				// //The order of lookup is to look for the Extension-suffixed class name first and then look for the class name without the Extension suffix.
+				// if (!typeResolver.TryResolve(xmltype, out _))
+				// {
+				// 	xmltype = new XmlType(namespaceuri, name, typeArguments);
+				// 	if (!typeResolver.TryResolve(xmltype, out _))
+				// 	{
+				// 		var ex = new XamlParseException($"MarkupExtension not found for {match}", serviceProvider);
+				// 		if (ExceptionHandler != null)
+				// 		{
+				// 			ExceptionHandler(ex);
+				// 			return null;
+				// 		}
+				// 		throw ex;
+				// 	}
+				// }
 
-	// 		_node = xmlLineInfo == null
-	// 			? new ElementNode(xmltype, null, nsResolver)
-	// 			: new ElementNode(xmltype, null, nsResolver, xmlLineInfo.LineNumber, xmlLineInfo.LinePosition);
+			_node = xmlLineInfo == null
+				? new ElementNode(xmltype, null, nsResolver)
+				: new ElementNode(xmltype, null, nsResolver, xmlLineInfo.LineNumber, xmlLineInfo.LinePosition);
 
-	// 		foreach (var (childname, childnode) in childnodes)
-	// 		{
-	// 			childnode.Parent = _node;
+			foreach (var (childname, childnode) in childnodes)
+			{
+				childnode.Parent = _node;
 
-	// 			if (childname == contentname)
-	// 			{
-	// 				//ContentProperty
-	// 				_node.CollectionItems.Add(childnode);
-	// 			}
-	// 			else
-	// 			{
-	// 				_node.Properties[childname] = childnode;
-	// 			}
-	// 		}
+				if (childname == contentname)
+				{
+					//ContentProperty
+					_node.CollectionItems.Add(childnode);
+				}
+				else
+				{
+					_node.Properties[childname] = childnode;
+				}
+			}
 
-	// 		return _node;
-	// 	}
-	// }
+			return _node;
+		}
+	}
 }
 
 internal class XamlServiceProvider : IServiceProvider
