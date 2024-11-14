42c5ef36318f2ac35f73a6fc02673e6ea05e81aa
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2024-11-14 15:57:20 +0530

    Fixed-ToolBarVisible-Issue

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue24547.cs
@@ -0,0 +1,109 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 24547, "[Windows] FlyoutPage ShouldShowToolbarButton when overridden to return false, still shows button in title bar", PlatformAffected.UWP)]
+	public class Issue24547 : NavigationPage
+	{
+	
+		public Issue24547() : base(new Issue24547PopoverPage())
+		{
+
+		}
+		public partial class Issue24547PopoverPage : FlyoutPage
+		{
+			public Issue24547PopoverPage()
+			{
+				FlyoutLayoutBehavior = FlyoutLayoutBehavior.Popover;
+
+				Flyout = new ContentPage
+				{
+					Title = "Flyout",
+					BackgroundColor = Colors.Red,
+					Content = new StackLayout
+					{
+						Children = {
+							new Label { Text = "Flyout" }
+						}
+					}
+				};
+
+				ContentPage contentPage = new ContentPage
+				{
+					BackgroundColor = Colors.Green,
+					Title = "Detail",
+					Content = new StackLayout
+					{
+						Children = 
+						{
+							CreateDetailButton()
+						}
+					}
+				};
+
+				Detail = new NavigationPage(contentPage);
+				Button button = new Button() { Text = "Menu", AutomationId = "MenuButton" };
+				button.Clicked += (s, e) => IsPresented = true;
+				NavigationPage.SetTitleView(contentPage, button);
+			}
+
+			private Button CreateDetailButton()
+			{
+				Button button = new Button
+				{
+					Text = "Detail",
+					AutomationId = "DetailButton"
+				};
+
+				button.Clicked += OnDetailButtonClicked;
+				return button;
+			}
+
+			private async void OnDetailButtonClicked(object sender, EventArgs e)
+			{
+				await this.Window.Page.Navigation.PushAsync(new NavigationPage(new Issue24547Page1()));
+			}
+
+			public override bool ShouldShowToolbarButton()
+			{
+				return false;
+			}
+		}
+
+		public partial class Issue24547Page1 : ContentPage
+		{
+			public Issue24547Page1()
+			{
+				Title = "Content Page";
+				VerticalStackLayout layout = new VerticalStackLayout
+				{
+					Children =
+					{
+						CreatePopButton()
+					}
+				};
+				Content = layout;
+
+				ToolbarItems.Add(new ToolbarItem
+				{
+					Text = "Item One",
+				});
+			}
+
+			private Button CreatePopButton()
+			{
+				Button button = new Button
+				{
+					Text = "Pop Button",
+					AutomationId = "PopButton",
+				};
+
+				button.Clicked += OnPopButtonClicked;
+				return button;
+			}
+
+			private async void OnPopButtonClicked(object sender, EventArgs e)
+			{
+				await this.Window.Page.Navigation.PopAsync();
+			}
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue24547.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue24547: _IssuesUITest
+{
+    public Issue24547(TestDevice device) : base(device) { }
+
+    public override string Issue => "[Windows] FlyoutPage ShouldShowToolbarButton when overridden to return false, still shows button in title bar";
+
+	[Test]
+	[Category(UITestCategories.FlyoutPage)]
+	public void VerifyInitialToolbarButtonHidden()
+	{
+		App.WaitForElement("DetailButton");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.FlyoutPage)]
+	public void VerifyToolbarButtonHiddenOnNavigateBack()
+    {
+		App.WaitForElement("DetailButton");
+		App.Tap("DetailButton");
+		App.WaitForElement("PopButton");
+		App.Tap("PopButton");
+		App.WaitForElement("DetailButton");
+		VerifyScreenshot();
+    }
+}
