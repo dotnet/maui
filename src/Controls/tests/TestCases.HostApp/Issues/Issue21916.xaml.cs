dff5dbd7211e0d482b8b7a959047fe06e39cc221
Karthik Raja <karthikraja.kalaimani@syncfusion.com>
2024-11-14 10:58:30 +0530

    Fix included and UI test case added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue21916.xaml.cs
@@ -0,0 +1,74 @@
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 21916, "Page and control Unloaded events firing on iOS when navigating to another page", PlatformAffected.iOS)]
+	public partial class Issue21916 : Shell
+	{
+        public Issue21916()
+        {
+            InitializeComponent();
+             Routing.RegisterRoute("Issue21916_MainPage", typeof(Issue21916_MainPage));
+            Routing.RegisterRoute("Issue21916_NewPage", typeof(Issue21916_NewPage));
+        }
+    }
+
+    public class Issue21916_MainPage : ContentPage
+    {
+          int mainPageLoadedCount = 0;
+            int mainPageUnloadedCount = 0;
+             Label label;
+             bool isNavigated;
+        public Issue21916_MainPage()
+        {
+            var stack = new StackLayout();
+            label = new Label();
+            var Button = new Button();
+            Button.Text = "Click to navigate new page";
+            Button.AutomationId = "ButtonId";
+            Button.Clicked += Clicked;
+            stack.Children.Add(Button);
+            stack.Children.Add(label);
+            this.Content = stack;
+            this.Loaded += MainPage_Loaded;
+            this.Unloaded += MainPage_UnLoaded;
+        }
+
+        private void Clicked(object sender,EventArgs e)
+        {
+            isNavigated =true;
+            Shell.Current.GoToAsync(nameof(Issue21916_NewPage));     
+        }
+
+        private void MainPage_UnLoaded(object sender, EventArgs e)
+		{
+			mainPageUnloadedCount++;
+        }
+
+        private void MainPage_Loaded(object sender, EventArgs e)
+		{
+            if(isNavigated)
+            {
+			 mainPageLoadedCount++;
+             label.Text = $"Unloaded event triggered {mainPageUnloadedCount} times\nLoaded event triggered {mainPageLoadedCount} times";		
+            }
+        }
+    }
+
+    public class Issue21916_NewPage : ContentPage
+    {
+        public Issue21916_NewPage()
+        {
+            var stack = new StackLayout();
+            var Button = new Button();
+            Button.Text = "Click to navigate main page";
+            Button.AutomationId = "ButtonId";
+            Button.Clicked += Clicked;
+            stack.Children.Add(Button);
+            this.Content = stack;
+        }
+
+         private void Clicked(object sender,EventArgs e)
+        {
+            Shell.Current.GoToAsync("..");
+        }
+    }
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21916.cs
@@ -0,0 +1,28 @@
+#if !ANDROID && !MACCATALYST
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue21916 : _IssuesUITest
+	{
+		public Issue21916(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Page and control Unloaded events firing on iOS when navigating to another page"; 
+		
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void Shell_Issue21916()
+		{
+			App.WaitForElement("ButtonId");
+			App.Click("ButtonId");
+			App.WaitForElement("ButtonId");
+			App.Click("ButtonId");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
