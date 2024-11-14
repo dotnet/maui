9df9caca64b21f5402d90bd9ebe0182389e80de0
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-11-14 12:28:19 +0100

    Implement change themes in Appium actions on Windows

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/ThemeChange.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/ThemeChange.cs
@@ -1,4 +1,4 @@
-#if ANDROID || IOS
+#if ANDROID || IOS || TEST_FAILS_ON_WINDOWS // Using AppThemeBinding and changing theme not working on Windows
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
