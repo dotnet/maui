cfe48bf4a428e755f12244f0ae3a213407a57adb
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2024-11-14 14:43:22 +0530

    Modified changes

--- src/Core/src/Platform/iOS/MauiTextView.cs
+++ src/Core/src/Platform/iOS/MauiTextView.cs
@@ -164,7 +164,7 @@ namespace Microsoft.Maui.Platform
 		{
 			var contentHeight = ContentSize.Height;
 			var availableSpace = Bounds.Height - contentHeight * ZoomScale;
-			if (availableSpace <= 0 )
+			if (availableSpace <= 0)
 				return;
 			ContentOffset = VerticalTextAlignment switch
 			{
