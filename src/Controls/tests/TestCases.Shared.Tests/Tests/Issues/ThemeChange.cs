518f261a656866cd2c10d682cb6fbef77a5ae00c
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-11-18 02:25:42 +0100

    More changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/ThemeChange.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/ThemeChange.cs
@@ -1,4 +1,4 @@
-#if ANDROID || IOS || TEST_FAILS_ON_WINDOWS // Using AppThemeBinding and changing theme not working on Windows
+#if ANDROID || IOS // Using AppThemeBinding and changing theme not working on Windows
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
