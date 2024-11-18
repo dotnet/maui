c9cd12da74a7a02f57c5759890f943fe218ea67a
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2024-11-18 07:23:40 +0530

    Updated the fix for back bar button item.

--- src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellPageRendererTracker.cs
+++ src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellPageRendererTracker.cs
@@ -400,14 +400,10 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility
 					var previousNavItem = viewControllers[count - 2].NavigationItem;
 					if (previousNavItem != null)
 					{
-						if (text is not null)
+						if (previousNavItem.BackBarButtonItem is null)
 						{
-							var barButtonItem = (previousNavItem.BackBarButtonItem ??= new UIBarButtonItem());
-							barButtonItem.Title = text;
-						}
-						else if (previousNavItem.BackBarButtonItem != null)
-						{
-							previousNavItem.BackBarButtonItem = null;
+							previousNavItem.BackBarButtonItem ??= new UIBarButtonItem();
+							previousNavItem.BackBarButtonItem.Title = string.IsNullOrEmpty(text) ? string.Empty : text;
 						}
 					}
 				}
