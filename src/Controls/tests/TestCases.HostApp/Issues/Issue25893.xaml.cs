f4126e713396fc514d96be91672e3cf575a16293
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-11-20 15:59:42 +0100

    Added repro sample

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25893.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+
+	[Issue(IssueTracker.Github, 25893, "Setting MenuFlyoutSubItem IconImageSource throws a NullReferenceException", PlatformAffected.UWP)]
+	public partial class Issue25893 : TestContentPage
+	{
+		public Issue25893()
+		{
+			InitializeComponent();	
+		}
+
+		protected override void Init()
+		{
+		
+		}
+	}
+}
