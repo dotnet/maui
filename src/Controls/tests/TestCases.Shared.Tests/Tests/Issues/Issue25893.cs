c9661232e10bb438ff033f4824f00be830ac1e6f
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-11-21 12:56:47 +0100

    Added UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25893.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue25893 : _IssuesUITest
+{
+	public override string Issue => "Setting MenuFlyoutSubItem IconImageSource throws a NullReferenceException";
+
+	public Issue25893(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	[Test]
+	[Category(UITestCategories.ContextActions)]
+	public void MenuFlyoutSubItemWithIconNoCrash()
+	{
+		App.WaitForElement("WaitForStubControl");
+
+		// Without crashing, the test has passed.
+	}
+}
