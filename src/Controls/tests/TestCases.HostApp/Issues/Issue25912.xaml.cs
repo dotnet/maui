d2fa0e7665d92dc78628339e8b293fdeaef3127d
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-11-21 19:10:12 +0530

    UITest added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25912.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+[Issue(IssueTracker.Github, 25912, "ToolbarItem color when used with IconImageSource is always white", PlatformAffected.iOS | PlatformAffected.macOS)]
+	public partial class Issue25912 : ContentPage
+	{
+
+		public Issue25912()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25912.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using OpenQA.Selenium;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue25912 : _IssuesUITest
+	{
+		public Issue25912(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "ToolbarItem color when used with IconImageSource is always white";
+
+		[Test]
+		[Category(UITestCategories.ToolbarItem)]
+		public void VerifyToolbarItemIconColor()
+		{
+			App.WaitForElement("Label");
+			VerifyScreenshot();
+		}
+
+	}
+}
