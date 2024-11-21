30e0c67eea2a9e303aaa862fb234cc47f4c82b55
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2024-11-21 10:44:54 +0530

    Renamed class name

--- src/Controls/src/Core/Stepper/Stepper.Android.cs
+++ src/Controls/src/Core/Stepper/Stepper.Android.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Microsoft.Maui.Controls;
 
-public class Stepper.Android
+public partial class Stepper
 {
     internal static void MapInterval(IStepperHandler handler, IStepper view)
     {
