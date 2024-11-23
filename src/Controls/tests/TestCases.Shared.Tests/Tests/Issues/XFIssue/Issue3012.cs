0dbdb9be3660c3a780dde38e249fd2a89f33eecd
Anandhan Rajagopal <97146406+anandhan-rajagopal@users.noreply.github.com>
2024-11-23 22:18:55 +0530

    Addressed the review changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue3012.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue3012.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues;
 
 public class Issue3012 : _IssuesUITest
 {
-	const string DumbyEntry = "DumbyEntry";
+	const string OtherEntry = "OtherEntry";
 	const string FocusTargetEntry = "FocusTargetEntry";
 
 	public Issue3012(TestDevice testDevice) : base(testDevice)
@@ -19,11 +19,11 @@ public class Issue3012 : _IssuesUITest
 	[Category(UITestCategories.Entry)]
 	public void Issue3012Test()
 	{
-		App.WaitForElement(DumbyEntry);
-		App.Tap(DumbyEntry);
+		App.WaitForElement(OtherEntry);
+		App.Tap(OtherEntry);
 		App.Tap(FocusTargetEntry);
 		Assert.That(App.FindElement("UnfocusedCountLabel").GetText(), Is.EqualTo($"Unfocused count: {0}"));
-		App.Tap(DumbyEntry);
+		App.Tap(OtherEntry);
 		Assert.That(App.FindElement("UnfocusedCountLabel").GetText(), Is.EqualTo($"Unfocused count: {1}"));
 	}
 }
