559c9ebe20a8824105f4f019f542b67b3d2a10ff
Stephane Delcroix <stephane@delcroix.org>
2024-11-25 13:51:26 +0100

    datetime

--- src/Controls/tests/Xaml.UnitTests/BindablePropertiesAccessModifiers.xaml.cs
+++ src/Controls/tests/Xaml.UnitTests/BindablePropertiesAccessModifiers.xaml.cs
@@ -1,59 +1,57 @@
 using Microsoft.Maui.Controls.Core.UnitTests;
 using NUnit.Framework;
 
-namespace Microsoft.Maui.Controls.Xaml.UnitTests
+namespace Microsoft.Maui.Controls.Xaml.UnitTests;
+
+public class AccessModifiersControl : View
 {
-	public class AccessModifiersControl : View
+	public static BindableProperty PublicFooProperty = BindableProperty.Create(nameof(PublicFoo),
+		typeof(string),
+		typeof(AccessModifiersControl),
+		"");
+
+	public string PublicFoo
 	{
-		public static BindableProperty PublicFooProperty = BindableProperty.Create(nameof(PublicFoo),
-			typeof(string),
-			typeof(AccessModifiersControl),
-			"");
-
-		public string PublicFoo
-		{
-			get => (string)GetValue(PublicFooProperty);
-			set => SetValue(PublicFooProperty, value);
-		}
-
-		internal static BindableProperty InternalBarProperty = BindableProperty.Create(nameof(InternalBar),
-			typeof(string),
-			typeof(AccessModifiersControl),
-			"");
-
-		public string InternalBar
-		{
-			get => (string)GetValue(InternalBarProperty);
-			set => SetValue(InternalBarProperty, value);
-		}
+		get => (string)GetValue(PublicFooProperty);
+		set => SetValue(PublicFooProperty, value);
 	}
 
-	public class BindablePropertiesAccessModifiersVM
+	internal static BindableProperty InternalBarProperty = BindableProperty.Create(nameof(InternalBar),
+		typeof(string),
+		typeof(AccessModifiersControl),
+		"");
+
+	public string InternalBar
 	{
-		public string Foo => "Foo";
-		public string Bar => "Bar";
+		get => (string)GetValue(InternalBarProperty);
+		set => SetValue(InternalBarProperty, value);
 	}
+}
 
-	[XamlProcessing(XamlInflator.Default, true)]
-	public partial class BindablePropertiesAccessModifiers : ContentPage
+public class BindablePropertiesAccessModifiersVM
+{
+	public string Foo => "Foo";
+	public string Bar => "Bar";
+}
+
+
+[XamlProcessing(XamlInflator.Default, true)]
+public partial class BindablePropertiesAccessModifiers : ContentPage
+{
+
+	public BindablePropertiesAccessModifiers() => InitializeComponent();
+
+	[TestFixture]
+	public class Tests
 	{
+		[SetUp] public void Setup() => Application.Current = new MockApplication();
+		[TearDown] public void TearDown() => Application.Current = null;
 
-
-		public BindablePropertiesAccessModifiers() => InitializeComponent();
-
-		[TestFixture]
-		public class Tests
+		[Test] public void BindProperties([Values]XamlInflator inflator)
 		{
-			[SetUp] public void Setup() => Application.Current = new MockApplication();
-			[TearDown] public void TearDown() => Application.Current = null;
-
-			[Test] public void BindProperties([Values]XamlInflator inflator)
-			{
-				var page = new BindablePropertiesAccessModifiers(useCompiledXaml);
-				page.BindingContext = new BindablePropertiesAccessModifiersVM();
-				Assert.AreEqual("Bar", page.AMC.GetValue(AccessModifiersControl.InternalBarProperty));
-				Assert.AreEqual("Foo", page.AMC.GetValue(AccessModifiersControl.PublicFooProperty));
-			}
+			var page = new BindablePropertiesAccessModifiers(inflator) { BindingContext = new BindablePropertiesAccessModifiersVM() };
+			Assert.AreEqual("Bar", page.AMC.GetValue(AccessModifiersControl.InternalBarProperty));
+			Assert.AreEqual("Foo", page.AMC.GetValue(AccessModifiersControl.PublicFooProperty));
 		}
 	}
 }
