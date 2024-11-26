440e3904d813d758ed869034a9a49ef1584c3e9f
Rui Marinho <me@ruimarinho.net>
2024-11-26 12:36:40 +0000

    [uitest] Update test for 19609

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19609.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19609.cs
@@ -17,8 +17,18 @@ public class Issue19609 : _IssuesUITest
 	public void CanTapButtonOnEmptyView()
 	{
 		var btnElementId = "btnClick";
+		var btnElementCVId = "cv";
+		App.WaitForElement(btnElementCVId, $"Can t find the CollectionView {btnElementCVId}", timeout: TimeSpan.FromSeconds(10));
+#if WINDOWS
+		// on Windows, the button is not on Appium hirearchy, so we need to tap on the center of the CollectionView where the empty view is
+		var cv = App.FindElement(btnElementCVId);
+		var cvLocation = cv.GetRect();
+		var cvCenter = new Point(cvLocation.X + cvLocation.Width / 2, cvLocation.Y + cvLocation.Height / 2);
+		App.TapCoordinates(cvCenter.X, cvCenter.Y);
+#else
 		App.WaitForElement(btnElementId, $"Can t find the button {btnElementId}", timeout: TimeSpan.FromSeconds(10));
 		App.Tap(btnElementId);
+#endif
 		App.WaitForTextToBePresentInElement(btnElementId, "Clicked", timeout: TimeSpan.FromSeconds(10));
 	}
 }
