8eee38537830380bfa2556bfb2638f5c209b8d5b
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2024-11-26 12:38:17 +0530

    Update Issue13537.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13537.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13537.cs
@@ -6,6 +6,11 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 {
     public class Issue13537 : _IssuesUITest
 		{
+#if ANDROID
+		const string backButtonIdentifier = "";
+#else
+        const string backButtonIdentifier = "Home";
+#endif
 		public Issue13537(TestDevice testDevice) : base(testDevice)
 		{
 		}
@@ -31,7 +36,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement("HomePageTestLabel");
 			App.Tap("PushAsyncButton");
 			App.WaitForElement("InnerPageTestLabel");
-			App.TapBackArrow("Home");
+			App.TapBackArrow(backButtonIdentifier);
 			var result = App.WaitForElement("HomePageTestLabel").GetText();
 			Assert.That(result , Is.EqualTo("Issue13537HomePage QueryAttribute is triggered"));
 		}
