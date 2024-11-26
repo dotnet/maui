a743c254b15f266af44a48cd3add01f77b9f1408
Shane Neuville <shneuvil@microsoft.com>
2024-11-25 16:30:21 -0800

    Remove code that enables/disables Entry (#26101)

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25829.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+using System.Threading.Tasks;
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue25829 : _IssuesUITest
+	{
+		public Issue25829(TestDevice device) : base(device) { }
+
+		public override string Issue => "ScrollView starts at the position of first Entry control on the bottom rather than at 0";
+
+		[Test]
+		[Category(UITestCategories.Entry)]
+		public void ScrollViewStartsOccasionallyStartsAtTheFirstEntry()
+		{
+			App.WaitForElement("PushModal");
+			App.Tap("PushModal");
+			App.WaitForElement("Success");
+			App.Tap("Success");
+		}
+	}
+}
