d85bdaf3e87e06de35ee7b2f1e284d615c5bb192
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-11-27 13:20:19 +0100

    Added test

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25991.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue25991 : _IssuesUITest
+	{
+		public Issue25991(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "CarouselView reverts to displaying 1st item in collection when collection modified";
+
+		[Test]
+		[Category(UITestCategories.CarouselView)]
+		public void Issue25991Test()
+		{
+			App.WaitForElement("WaitForStubControl");
+			App.Click("ScrollToPerson2Button");
+			App.Click("AddItemButton");
+			VerifyScreenshot();
+		}
+	}
+}
