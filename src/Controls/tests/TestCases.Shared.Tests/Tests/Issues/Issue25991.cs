fa1e345c9e58f977e776d4e42f1c772fab19d878
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-11-28 15:28:49 +0100

    More changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25991.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25991.cs
@@ -7,6 +7,9 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 {
 	public class Issue25991 : _IssuesUITest
 	{
+		const string InfoLabel = "InfoLabel";
+		const string AddItemButton = "AddItemButton";
+
 		public Issue25991(TestDevice device)
 			: base(device)
 		{ }
@@ -19,29 +22,29 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		{
 			App.WaitForElement("WaitForStubControl");
 
-			App.WaitForElement("InfoLabel");
-			var result1 = App.FindElement("InfoLabel").GetText();
+			App.WaitForElement(InfoLabel);
+			var result1 = App.FindElement(InfoLabel).GetText();
 			ClassicAssert.AreEqual("0", result1);
 
 			App.Click("ScrollToPerson2Button");
-			App.Click("AddItemButton");
+			App.Click(AddItemButton);
 
-			App.WaitForElement("InfoLabel");
-			var result2 = App.FindElement("InfoLabel").GetText();
+			App.WaitForElement(InfoLabel);
+			var result2 = App.FindElement(InfoLabel).GetText();
 			ClassicAssert.AreEqual("1", result2);
 
 			App.Click("KeepItemsInViewButton");
-			App.Click("AddItemButton");
+			App.Click(AddItemButton);
 
-			App.WaitForElement("InfoLabel");
-			var result3 = App.FindElement("InfoLabel").GetText();
+			App.WaitForElement(InfoLabel);
+			var result3 = App.FindElement(InfoLabel).GetText();
 			ClassicAssert.AreEqual("0", result3);
 
 			App.Click("KeepLastItemInViewButton");
-			App.Click("AddItemButton");
+			App.Click(AddItemButton);
 
-			App.WaitForElement("InfoLabel");
-			var result4 = App.FindElement("InfoLabel").GetText();
+			App.WaitForElement(InfoLabel);
+			var result4 = App.FindElement(InfoLabel).GetText();
 			ClassicAssert.AreEqual("4", result4);
 
 		}
