d4c99607e513fdeb8991da37b9192bc4d0e58259
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-11-28 13:10:06 +0100

    More changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue6258.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue6258.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if ANDROID
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -21,3 +22,4 @@ public class Issue6258 : _IssuesUITest
 		VerifyScreenshot();
 	}
 }
+#endif
