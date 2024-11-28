41661fa624f0ee94e1888c7d96e73bfa5b3c2319
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-11-28 13:35:20 +0100

    More tests

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla58875.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla58875.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_WINDOWS // Using ActivateContextMenu internally does a right click and is not working correctly.
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -6,31 +7,35 @@ namespace Microsoft.Maui.TestCases.Tests.Issues;
 
 public class Bugzilla58875 : _IssuesUITest
 {
+	const string Button1Id = "Button1Id";
+	const string ContextAction = "More";
+	const string Target = "Swipe me";
+
 	public Bugzilla58875(TestDevice testDevice) : base(testDevice)
 	{
 	}
 
 	public override string Issue => "Back navigation disables Context Action in whole app, if Context Action left open";
 
-	// 	[Test]
-	// 	[Category(UITestCategories.ContextActions)]
-	// 	public void Bugzilla58875Test()
-	// 	{
-	// 		App.WaitForElement(q => q.Marked(Button1Id));
-	// 		App.Tap(q => q.Marked(Button1Id));
-	// 		App.WaitForElement(q => q.Marked(Target));
-	// 		App.ActivateContextMenu(Target);
-	// 		App.WaitForElement(q => q.Marked(ContextAction));
-	// 		App.Back();
+	[Test]
+	[Category(UITestCategories.ContextActions)]
+	public void Bugzilla58875Test()
+	{
+		App.WaitForElement(Button1Id);
+		App.Tap(Button1Id);
+		App.WaitForElement(Target);
+		App.ActivateContextMenu(Target);
+		App.WaitForElement(ContextAction);
+		App.Back();
 
-	// #if ANDROID
-	// 		App.Back(); // back button dismisses the ContextAction first, so we need to hit back one more time to get to previous page
-	// #endif
+#if ANDROID
+		App.Back(); // back button dismisses the ContextAction first, so we need to hit back one more time to get to previous page
+#endif
 
-	// 		App.WaitForElement(q => q.Marked(Button1Id));
-	// 		App.Tap(q => q.Marked(Button1Id));
-	// 		App.WaitForElement(q => q.Marked(Target));
-	// 		App.ActivateContextMenu(Target);
-	// 		App.WaitForElement(q => q.Marked(ContextAction));
-	// 	}
+		App.WaitForElement(Button1Id);
+		App.Tap(Button1Id);
+		App.WaitForElement(Target);
+		App.ActivateContextMenu(Target);
+		App.WaitForElement(ContextAction);
+	}
 }
