2eb59690aa7aefe55837826a97f669a94dc2d7fd
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2024-11-28 15:30:32 +0530

    Fixed-VerticalTextAlignment-Issue

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25973.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	internal class Issue25973 : _IssuesUITest
+	{
+		public override string Issue => "Editor vertical text alignment not working after toggling IsVisible";
+
+		public Issue25973(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.Editor)]
+		public void VerifyEditorVerticalTextAlignmentWhenVisibilityToggled()
+		{
+			App.WaitForElement("ToggleButton");
+			App.Click("ToggleButton");
+			VerifyScreenshot();
+		}
+	}
+}
