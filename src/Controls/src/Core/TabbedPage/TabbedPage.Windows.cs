27f438bf2ff9d5cf055b999b39bb3c415b097fe0
BagavathiPerumal <bagavathiperumal.a@syncfusion.com>
2024-11-28 18:39:23 +0530

    fix-26103-Added measure invalidation before arranging NavigationView to fix scrolling and layout issues.

--- src/Controls/src/Core/TabbedPage/TabbedPage.Windows.cs
+++ src/Controls/src/Core/TabbedPage/TabbedPage.Windows.cs
@@ -169,7 +169,10 @@ namespace Microsoft.Maui.Controls
 		void OnNavigationViewSizeChanged(object sender, SizeChangedEventArgs e)
 		{
 			if (_navigationView != null)
+			{
+				this.InvalidateMeasure();
 				this.Arrange(_navigationView);
+			}
 		}
 
 		void SetupNavigationView()
