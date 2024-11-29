e94ead0ca02fec3f72d35f296a5b80262fc3fabf
Anandhan Rajagopal <97146406+anandhan-rajagopal@users.noreply.github.com>
2024-11-29 17:24:47 +0530

    Enabled 25th set of XamarinUITest into Appium (#23)
    * Migrated Xamarin to Appium for Issue15542 and Issue15565
    
    * Updated migrated code changes
    
    * fixed the failure on windows platform
    
    * Updated the comments
    
    ---------
    
    Co-authored-by: Harish <harish.kumar@syncfusion.com>

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue15542.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue15542.cs
@@ -6,22 +6,32 @@ namespace Microsoft.Maui.TestCases.Tests.Issues;
 
 public class Issue15542 : _IssuesUITest
 {
+
+#if ANDROID
+	const string Page1 = "PAGE 1";
+#else
+	const string Page1 = "page 1";
+#endif
+
 	public Issue15542(TestDevice testDevice) : base(testDevice)
 	{
 	}
 
 	public override string Issue => "[Bug] Shell.TitleView does not render on iOS 16";
 
-	// [Test]
-	// [Category(UITestCategories.TitleView)]
-	// public void TitleViewHeightDoesntOverflow()
-	// {
-	// 	var titleView = App.WaitForElement("TitleViewId")[0].Rect;
-	// 	var topTab = App.WaitForElement("page 1")[0].Rect;
+	[Test]
+	[Category(UITestCategories.TitleView)]
+	public void TitleViewHeightDoesntOverflow()
+	{
+		var titleView = App.WaitForElement("title 1").GetRect();
+#if WINDOWS // In Windows the Page 1 items are inside the root navViewItem which in popup we neet top it once.
+		App.Tap("navViewItem");
+#endif
+		var topTab = App.WaitForElement(Page1).GetRect();
 
-	// 	var titleViewBottom = titleView.Y + titleView.Height;
-	// 	var topTabTop = topTab.Y;
+		var titleViewBottom = titleView.Y + titleView.Height;
+		var topTabTop = topTab.Y;
 
-	// 	Assert.GreaterOrEqual(topTabTop, titleViewBottom, "Title View is incorrectly positioned in iOS 16");
-	// }
+		Assert.That(topTabTop, Is.GreaterThanOrEqualTo(titleViewBottom), "Title View is incorrectly positioned in iOS 16");
+	}
 }
