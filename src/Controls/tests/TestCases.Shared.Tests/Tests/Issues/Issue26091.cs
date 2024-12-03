8e1740ffd3cb3fa0dbab243bd0eb171e9a884776
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2024-12-03 11:50:55 +0530

    Added test and snapshots

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26091.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue26091 : _IssuesUITest
+	{
+		public override string Issue => "TableView cells become empty after adding a new cell with context actions";
+
+		public Issue26091(TestDevice device)
+		: base(device)
+		{ }
+
+		string item = "Add new";
+
+		[Test]
+		[Category(UITestCategories.TableView)]
+		public void DynamicallyAddingTableViewCells()
+		{
+			App.WaitForElement("TableView");
+			App.Tap(item);
+			App.Tap(item);
+			App.Tap(item);
+
+			VerifyScreenshot();
+		}
+	}
+}
