a12492cc58ade15690ba66a8875844582c822255
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2024-12-03 11:50:55 +0530

    Added test and snapshots

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue26091.cs
@@ -0,0 +1,39 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 26091, "TableView cells become empty after adding a new cell with context actions", PlatformAffected.iOS)]
+public partial class Issue26091 : ContentPage
+{
+	TableSection dataSection;
+	TableView tableView;
+	int count = 0;
+	public Issue26091()
+	{
+
+		dataSection = new TableSection {
+			new TextCell{ Text = "Cell1" },
+			new TextCell{ Text = "Cell2", ContextActions = { new MenuItem{ Text = "Delete" } } },
+			new TextCell{ Text = "Add new", AutomationId="AddTextCell", Command = new Command (AddNew) }
+		};
+
+		tableView = new TableView
+		{
+			AutomationId = "TableView",
+			Root = new TableRoot
+			{
+				dataSection
+			}
+		};
+
+		Content = tableView;
+	}
+
+	void AddNew(object parameters)
+	{
+		count++;
+		dataSection.Insert(0, new TextCell
+		{
+			Text = "Fresh cell " + count,
+			ContextActions = { new MenuItem { Text = "Delete" } }
+		});
+	}
+}
