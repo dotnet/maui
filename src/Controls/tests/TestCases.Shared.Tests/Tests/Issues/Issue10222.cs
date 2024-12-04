9e16e68a2da9f95c07cd357ca126cab31ac12435
Anandhan Rajagopal <97146406+anandhan-rajagopal@users.noreply.github.com>
2024-12-04 15:09:37 +0530

    Resolved build error

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10222.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10222.cs
@@ -9,19 +9,15 @@ public class Issue10222 : _IssuesUITest
     public Issue10222(TestDevice device) : base(device)
     {
     }
+	public override string Issue => "[CollectionView] ObjectDisposedException if the page is closed during scrolling";
 
-
-		public override string Issue => "[CollectionView] ObjectDisposedException if the page is closed during scrolling";
-	
-		[Test]
-		[Category(UITestCategories.LifeCycle)]
-		public void Issue10222Test()
-		{
-			App.WaitForElement("goTo");
-			App.Tap("goTo");
-			App.WaitForElement("items1");
-			App.WaitForElement("goTo");
-		}
-		
+	[Test]
+	[Category(UITestCategories.LifeCycle)]
+	public void Issue10222Test()
+	{
+		App.WaitForElement("goTo");
+		App.Tap("goTo");
+		App.WaitForElement("items1");
+		App.WaitForElement("goTo");
 	}
 }
