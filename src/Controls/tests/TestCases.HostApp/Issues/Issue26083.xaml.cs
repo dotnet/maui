bb79e1b7d2657db4c4a24b4aab4dbcedd3db98a2
BagavathiPerumal <bagavathiperumal.a@syncfusion.com>
2024-12-04 21:06:46 +0530

    fix-26083-Testcase added.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue26083.xaml.cs
@@ -0,0 +1,52 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 26083, "UI not updating GridItemsLayout when Span becomes 1", PlatformAffected.UWP)]
+public partial class Issue26083 : ContentPage
+{
+	public Issue26083()
+	{
+		InitializeComponent();
+	}
+
+	private void CollectionView_SizeChanged(object sender, EventArgs e)
+	{
+		CollectionView collectionView = sender as CollectionView;
+		if (collectionView != null)
+		{
+			GridItemsLayout gridItemsLayout = (GridItemsLayout)collectionView.ItemsLayout;
+			if (collectionView.Width < 1000)
+			{
+				gridItemsLayout.Span = 1;
+			}
+			else
+			{
+				gridItemsLayout.Span = 2;
+			}
+
+		}
+
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		customGrid.WidthRequest = 100;
+	}
+
+	public class MainViewModel
+	{
+		public List<string> Items { get; set; }
+		public MainViewModel()
+		{
+			Items = new List<string>()
+			{
+				"Item 1",
+				"Item 2",
+				"Item 3",
+				"Item 4",
+				"Item 5"
+			};
+		}
+	}
+}
+
+
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26083.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue26083 : _IssuesUITest
+	{
+		public Issue26083(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "UI not updating GridItemsLayout when Span becomes 1";
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void ItemsWrapGridShouldUpdateBasedOnCollectionViewSize()
+		{
+			App.WaitForElement("Button");
+			App.Tap("Button");
+			VerifyScreenshot();
+		}
+	}
+}
