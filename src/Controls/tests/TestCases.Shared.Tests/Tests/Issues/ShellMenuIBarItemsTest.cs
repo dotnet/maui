0b2abeb9a7e98f3a492e3dcd45000388760463c8
Mike Corsaro <mikecorsaro@microsoft.com>
2024-12-04 15:18:49 -0800

    Add correct menu bar item foreground resource override

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/ShellMenuIBarItemsTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class ShellMenuIBarItemsTest : _IssuesUITest
+	{
+		public ShellMenuIBarItemsTest(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Shell MenuBarItems Test";
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void SettingMenuBarItemColorsWork()
+		{
+			Task.Delay(millisecondsDelay: 100).Wait();
+
+			App.Screenshot("VerifyMenuBarItemColorsWork");
+		}
+	}
+}
