63d6ef9c2e05bfb64972eda8d3ac2af294603df6
Rui Marinho <me@ruimarinho.net>
2024-12-04 12:39:09 +0000

    [testing] Add test for issue #26066

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26066.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue26066 : _IssuesUITest
+{
+    public Issue26066(TestDevice testDevice) : base(testDevice)
+    {
+    }
+
+    public override string Issue => "CollectionViewHandler2 RelativeSource binding to AncestorType not working";
+
+    [Test]
+    [Category(UITestCategories.CollectionView)]
+    public void CollectionView2ShouldFindAncestorType()
+    {
+        var text = "CV2";
+        var i = 1;
+        var automationId = $"{text} - Item {i}".Replace(" ", "", StringComparison.Ordinal);
+        App.WaitForElement(automationId);
+        App.Click(automationId);
+        App.WaitForElement("OK");
+        App.Click("OK");
+    }
+}
