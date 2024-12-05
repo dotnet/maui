fe839888874f773e6d2211b757473ad56dfeb52c
Jakub Florkowski <kubaflo123@gmail.com>
2024-12-05 12:19:12 +0100

    Added a UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26366.cs
@@ -0,0 +1,28 @@
+﻿#if ANDROID || IOS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue26366 : _IssuesUITest
+	{
+		public Issue26366(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "CollectionView ScrollOffset does not reset when the ItemSource is changed";
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void ScrollOffsetShouldReset()
+		{
+			App.WaitForElement("Button");
+			App.ScrollDown("CollectionView");
+			App.Click("Button");
+			var text = App.FindElement("Label").GetText();
+			Assert.That(text, Is.EqualTo("0"));
+		}
+	}
+}
+#endif
