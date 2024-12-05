0354fb0efd02298a8cee1a5ee530f9b93c674d51
Jakub Florkowski <kubaflo123@gmail.com>
2024-12-05 03:52:19 +0100

    [iOS] CV - Reset ScrollOffset when item source changes

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26366.cs
@@ -0,0 +1,28 @@
+﻿#if ANDROID || IOS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue26366 : _IssuesUITest
+	{
+		public Issue26366(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "CollectionView ScrollOffset does not reset when the ItemSource is changed";
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void ScrollOffsetShouldReset()
+		{
+			App.WaitForElement("Button");
+			App.ScrollDown("CollectionView");
+			Assert.That(App.FindElement("Label").GetText(), Is.Not.EqualTo("0"));
+			App.Click("Button");
+			Assert.That(App.FindElement("Label").GetText(), Is.EqualTo("0"));
+		}
+	}
+}
+#endif
