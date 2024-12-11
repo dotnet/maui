64666c91edb515d8cd2488a9cbf949fdfe0fc929
Anandhan Rajagopal <97146406+anandhan-rajagopal@users.noreply.github.com>
2024-12-11 21:19:08 +0530

    Enabled 41st set of UITests from XamarinUITest into Appium (#39) (#26492)
    * Migrated the Issues 2499 and 2948
    
    * Addressed the feedbacks
    
    * Updated migrated test cases
    
    ---------
    
    Co-authored-by: nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue2499.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue2499.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_IOS && TEST_FAILS_ON_CATALYST //The test fails on iOS and macOS because Appium is unable to locate the Picker control elements (such as "cat"), resulting in a TimeoutException.
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -12,27 +13,16 @@ public class Issue2499 : _IssuesUITest
 
 	public override string Issue => "Binding Context set to Null in Picker";
 
-	// [Test]
-	// [Category(UITestCategories.Picker)]
-	// [FailsOnIOSWhenRunningOnXamarinUITest]
-	// 	public void Issue2499Test()
-	// 	{
-	// 		App.WaitForElement("picker");
-	// 		App.Tap("picker");
-	// 		App.WaitForElement("cat");
-
-	// 		AppResult[] items = App.Query("cat");
-	// 		Assert.AreNotEqual(items.Length, 0);
-	// 		App.WaitForElement(q => q.Marked("mouse"));
-	// 		App.Tap("mouse");
-	// #if __IOS__
-	// 		System.Threading.Tasks.Task.Delay(500).Wait();
-	// 		var cancelButtonText = "Done";
-	// 		App.WaitForElement(q => q.Marked(cancelButtonText));
-	// 		App.Tap(q => q.Marked(cancelButtonText));
-	// 		System.Threading.Tasks.Task.Delay(1000).Wait();
-	// #endif
-	// 		items = App.Query("cat");
-	// 		Assert.AreEqual(items.Length, 0);
-	// 	}
+	[Test]
+	[Category(UITestCategories.Picker)]
+	public void Issue2499Test()
+	{
+		App.WaitForElement("picker");
+		App.Tap("picker");
+		App.WaitForElement("cat");
+		App.WaitForElement("mouse");
+		App.Tap("mouse");
+		App.WaitForNoElement("cat");
+	}
 }
+#endif
