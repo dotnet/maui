488927f2d7fecb043cb5877a8ea31e46dd54b696
Rui Marinho <me@ruimarinho.net>
2024-12-11 13:50:56 +0000

    [iOS] Add Test for issue #20443

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20443.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue20443 : _IssuesUITest
+	{
+		public override string Issue => "CollectionView item sizing wrong after refresh";
+
+		public Issue20443(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void RefreshShouldNotChangeSize()
+		{
+			_ = App.WaitForElement("lblSmall");
+			_ = App.WaitForElement("lblBig");
+
+			App.DragCoordinates(100, 100, 100, 500);
+
+			Task.Delay(5000).Wait();
+			VerifyScreenshot();
+
+		}
+	}
+}
