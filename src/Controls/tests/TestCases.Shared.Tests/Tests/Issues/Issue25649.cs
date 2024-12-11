ba4eea9ab9023f24fca95a93436eb42d8326e582
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-12-11 11:38:13 +0530

    Modified a test case

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25649.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25649.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿#if !MACCATALYST
+
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -12,13 +14,26 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 
 		public override string Issue => "CollectionView OnCollectionViewScrolled Calls and parameters are inconsistent or incorrect";
 
+
+#if IOS
+		private const string firstVisibleIndex = "5";
+#elif ANDROID
+		private const string firstVisibleIndex = "6";
+#elif WINDOWS
+		private const string firstVisibleIndex = "14";
+#endif
+
 		[Test]
 		[Category(UITestCategories.CollectionView)]
 		public void Issue25649Test()
 		{
 			App.WaitForElement("collectionView");
-			App.ScrollDown("collectionView");
-			VerifyScreenshot();
+			App.ScrollDown("collectionView", ScrollStrategy.Gesture, 0.99);
+			var firstVisibleItemIndex = App.FindElement("FirstVisibleItemIndex").GetText();
+			Assert.That(firstVisibleItemIndex, Is.EqualTo(firstVisibleIndex));
+			var lastVisibleItemIndex = App.FindElement("LastVisibleItemIndex").GetText();
+			Assert.That(lastVisibleItemIndex, Is.EqualTo("30"));
 		}
 	}
 }
+#endif
