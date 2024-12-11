80a75116f558e52c28fc2182cf6f9dc3de77c005
Jakub Florkowski <kubaflo123@gmail.com>
2024-12-11 02:47:06 +0100

    [Android] Set SetClipChildren to false

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issues18831.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 18831, "Border Shadow in ListView not showing", PlatformAffected.Android)]
+	public partial class Issue18831 : ContentPage
+	{
+		public Issue18831()
+		{
+			InitializeComponent();
+		}
+	}
+}
