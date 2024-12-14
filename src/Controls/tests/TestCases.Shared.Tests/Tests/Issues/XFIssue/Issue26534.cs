0d5884ada5614187d1415ea98ab116b48cddf2fa
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2024-12-14 15:25:15 +0530

    Added test sample.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue26534.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue26534 : _IssuesUITest
+{
+	public Issue26534(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Exception occurred when using GroupShortNameBinding in Grouped ListView";
+
+	[Test]
+	[Category(UITestCategories.ListView)]
+	public void VerifyListViewWithGroupShortNameBinding()
+	{
+		App.WaitForElement("listview");
+	}
+}
