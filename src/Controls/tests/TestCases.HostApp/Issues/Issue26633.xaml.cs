021049e17cc591e940e5b68847e18cf53f82fb43
maonaoda <maonaoda@gmail.com>
2024-12-15 14:18:01 +0800

    test: add UITest for Issue26633

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue26633.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 26633, "Label height in Grid with ColumnSpacing > 0 incorrect in certain cases", PlatformAffected.All)]
+	public partial class Issue26633 : ContentPage
+	{
+		public Issue26633()
+		{
+			InitializeComponent();
+		}
+	}
+}
