ff9368d27d8005edbedae19b93b03379a1a4fbf2
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2024-12-16 12:28:28 +0530

    Modified the logics based on the border width

--- src/Controls/src/Core/Button/Button.iOS.cs
+++ src/Controls/src/Core/Button/Button.iOS.cs
@@ -213,8 +213,8 @@ namespace Microsoft.Maui.Controls
 				{
 					imageInsets.Top -= (titleHeight / 2) + sharedSpacing;
 					imageInsets.Bottom += (titleHeight / 2) + sharedSpacing;
-					imageInsets.Left += (float)(size.Width - imageWidth) / 2;
-					imageInsets.Right += (float)(size.Width - imageWidth) / 2;
+					imageInsets.Left += (float)(((size.Width - imageWidth) / 2) - borderWidth);
+					imageInsets.Right += (float)(((size.Width - imageWidth) / 2) - borderWidth);
 
 					titleInsets.Top += (imageHeight / 2) + sharedSpacing;
 					titleInsets.Bottom -= (imageHeight / 2) + sharedSpacing;
@@ -225,8 +225,8 @@ namespace Microsoft.Maui.Controls
 				{
 					imageInsets.Top += (titleHeight / 2) + sharedSpacing;
 					imageInsets.Bottom -= (titleHeight / 2) + sharedSpacing;
-					imageInsets.Left += (float)(size.Width - imageWidth) / 2;
-					imageInsets.Right += (float)(size.Width - imageWidth) / 2;
+					imageInsets.Left += (float)(((size.Width - imageWidth) / 2) - borderWidth);
+					imageInsets.Right += (float)(((size.Width - imageWidth) / 2) - borderWidth);
 
 					titleInsets.Top -= (imageHeight / 2) + sharedSpacing;
 					titleInsets.Bottom += (imageHeight / 2) + sharedSpacing;
