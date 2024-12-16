0a7dc5856629872a2c9e609c7d33d0e2e18416da
Jakub Florkowski <kubaflo123@gmail.com>
2024-12-16 01:28:23 +0100

    [iOS] Respect explicitly set size constraints

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26644.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue26644 : _IssuesUITest
+	{
+		public Issue26644(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "[iOS] Label with a fixed WidthRequest has wrong height";
+
+		[Test]
+		[Category(UITestCategories.Layout)]
+		[Category(UITestCategories.Label)]
+		public void LabelHeightShouldBeCorrect()
+		{
+			App.WaitForElement("Label1");
+			var text1 = App.FindElement("Label1").GetText();
+			var text2 = App.FindElement("Label2").GetText();
+			
+			Assert.That(text1, Is.EqualTo(text2));
+		}
+	}
+}
