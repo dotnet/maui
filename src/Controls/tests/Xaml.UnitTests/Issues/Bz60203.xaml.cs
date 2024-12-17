cfef65383914f7467e231ef353fde9d720f3ac93
Stephane Delcroix <stephane@delcroix.org>
2024-12-17 09:57:18 +0100

    tests

--- src/Controls/tests/Xaml.UnitTests/Issues/Bz60203.xaml.cs
+++ src/Controls/tests/Xaml.UnitTests/Issues/Bz60203.xaml.cs
@@ -1,36 +1,30 @@
-using System;
-using Microsoft.Maui.Controls.Core.UnitTests;
 using Microsoft.Maui.Graphics;
 using NUnit.Framework;
 
-namespace Microsoft.Maui.Controls.Xaml.UnitTests
+namespace Microsoft.Maui.Controls.Xaml.UnitTests;
+
+[XamlProcessing(XamlInflator.Default, true)]
+public partial class Bz60203 : ContentPage
 {
-	public partial class Bz60203 : ContentPage
+	public Bz60203()
 	{
-		public Bz60203()
+		InitializeComponent();
+	}
+
+
+	[TestFixture]
+	class Tests
+	{
+
+		[Test]
+		public void CanCompileMultiTriggersWithDifferentConditions([Values]XamlInflator inflator)
 		{
-			InitializeComponent();
+			var layout = new Bz60203(inflator);
+			Assert.That(layout.label.BackgroundColor, Is.EqualTo(BackgroundColorProperty.DefaultValue));
+			layout.BindingContext = new { Text = "Foo" };
+			layout.label.TextColor = Colors.Blue;
+			Assert.That(layout.label.BackgroundColor, Is.EqualTo(Colors.Pink));
 		}
 
-		public Bz60203(bool useCompiledXaml)
-		{
-			//this stub will be replaced at compile time
-		}
-
-		[TestFixture]
-		class Tests
-		{
-
-			[TestCase(true), TestCase(false)]
-			public void CanCompileMultiTriggersWithDifferentConditions(bool useCompiledXaml)
-			{
-				var layout = new Bz60203(useCompiledXaml);
-				Assert.That(layout.label.BackgroundColor, Is.EqualTo(BackgroundColorProperty.DefaultValue));
-				layout.BindingContext = new { Text = "Foo" };
-				layout.label.TextColor = Colors.Blue;
-				Assert.That(layout.label.BackgroundColor, Is.EqualTo(Colors.Pink));
-			}
-
-		}
 	}
 }
