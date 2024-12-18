2f52c895c4fa353dcfaa6ecaacc443f90ce63c33
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2024-12-18 10:16:24 +0530

    Fixed-ToolBar-Visibility-FlyoutPage

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21645.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue21645 : _IssuesUITest
+{
+    public Issue21645(TestDevice device) : base(device) { }
+
+    public override string Issue => "Android - Navigation Title disappears if Flyout is set to a new window";
+
+	[Test]
+	[Category(UITestCategories.FlyoutPage)]
+	public void VerifyToolbarButtonVisibilityWhenFlyoutReset()
+	{
+		App.WaitForElement("ResetFlyoutButton");
+        App.Click("ResetFlyoutButton");
+        VerifyScreenshot();
+	}
+}
