b7e376e9e16e332002a67444ea2b5891f7cf2bda
Vignesh-SF3580 <vignesh.nagarajan@syncfusion.com>
2024-12-18 18:39:55 +0530

    added testcases.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue16053.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 16053, "ListView SelectedItem retains its value after ListView is cleared", PlatformAffected.All)]
+	public partial class Issue16053 : ContentPage
+	{
+		public Issue16053()
+		{
+			InitializeComponent();
+			BindingContext = new _16053ViewModel();
+
+			Loaded += (s, e) =>
+			{
+				if (BindingContext is _16053ViewModel viewModel)
+				{
+					viewModel.SelectedItem = viewModel.Items[2];
+				}
+			};
+		}
+
+		private void Button_Click(object sender, EventArgs e)
+		{
+			if (ListView1.SelectedItem is string selectedItem)
+			{
+				lbl.Text = $"Selected Item: {selectedItem}";
+			}
+			else
+			{
+				lbl.Text = "No item selected";
+			}
+		}
+
+		private void Button_Click_1(object sender, EventArgs e)
+		{
+			if (BindingContext is _16053ViewModel viewModel)
+			{
+				viewModel.Items.Clear();
+			}
+		}
+	}
+
+	public class _16053ViewModel : INotifyPropertyChanged
+	{
+		private string _selectedItem;
+
+		public ObservableCollection<string> Items { get; set; }
+
+		public string SelectedItem
+		{
+			get => _selectedItem;
+			set
+			{
+				if (_selectedItem != value)
+				{
+					_selectedItem = value;
+					OnPropertyChanged(nameof(SelectedItem));
+				}
+			}
+		}
+
+		public _16053ViewModel()
+		{
+			Items = new ObservableCollection<string>
+			{
+				"Coffee",
+				"Tea",
+				"Orange Juice",
+				"Milk",
+				"Iced Tea",
+				"Mango Shake"
+			};
+		}
+		public event PropertyChangedEventHandler PropertyChanged;
+
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16053.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue16053: _IssuesUITest
+	{
+		public Issue16053(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "ListView SelectedItem retains its value after ListView is cleared";
+
+		[Test]
+		[Category(UITestCategories.ListView)]
+		public void ListViewSelectedItemUpdated()
+		{
+			App.WaitForElement("ClearButton");
+			App.Tap("DisplayItem");
+			App.Tap("ClearButton");
+			App.Tap("DisplayItem");
+			var label = App.WaitForElement("SelectedItemLabel");
+			Assert.That(label.GetText(), Is.EqualTo("No item selected"));
+		}
+	}
+}
