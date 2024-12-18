777696a38050591c58b6fef9c9e2c8fe70cca0fb
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2024-12-18 11:50:31 +0530

    Added UI tests.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/ContextActionCellTest.cs
@@ -0,0 +1,29 @@
+﻿#if IOS || ANDROID
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class ContextActionCellTest : _IssuesUITest
+{
+	public ContextActionCellTest(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Table View ContextActionCell Test";
+
+	[Test]
+	[Category(UITestCategories.ActivityIndicator)]
+	public void VerifyContextActionCell()
+	{
+		App.WaitForElement("Text Cell");
+#if IOS
+		App.SwipeRightToLeft("Text Cell", 25, 500);
+#elif ANDROID
+		App.LongPress("Text Cell");
+#endif
+		VerifyScreenshot();
+	}
+}
+#endif
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/FlyoutTest.cs
@@ -0,0 +1,39 @@
+﻿#if !MACCATALYST
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class HamburgerIconTest : _IssuesUITest
+	{
+		public override string Issue => "FlyoutTest";
+
+		public HamburgerIconTest(TestDevice device)
+		: base(device)
+		{ }
+
+		[Test]
+		[Category(UITestCategories.ActivityIndicator), Order(1)]
+		public void VerifyHamburgerIcon()
+		{
+			App.WaitForElement("Tab1Page");
+			VerifyScreenshot();
+		}
+
+		[Test]
+		[Category(UITestCategories.ActivityIndicator), Order(2)]
+		public void VerifyFlyoutBackgroundColor()
+		{
+			App.WaitForElement("Tab1Page");
+			App.Tap("ChangeFlyoutBackground");
+#if ANDROID
+			App.Tap(AppiumQuery.ByXPath("//android.widget.ImageButton[@content-desc='Open navigation drawer']"));
+#else
+			App.Tap(FlyoutIconAutomationId);
+#endif
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
