c5b100e5c30db9881532a8410dfd36a4d181f52b
Rui Marinho <me@ruimarinho.net>
2024-12-19 18:09:04 +0000

    Revert "[ci] Always install .net (#26729)"
    This reverts commit 5a5817e06fcd2a8b6606e4226c7e85395fdea681.

--- eng/configure-toolset.ps1
+++
@@ -1,8 +0,0 @@
-# We can't use already installed dotnet cli since we need to install additional workloads.
-# We could potentially try to find an existing installation that has all the required workloads,
-# but it's unlikely one will be available.
-
-useInstalledDotNetCli="false"
-
-# Working around issue https://github.com/dotnet/arcade/issues/7327
-# DisableNativeToolsetInstalls=true
