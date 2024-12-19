a4182b570f47eed850537d079008a58b3c3cae51
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2024-12-19 19:27:34 +0530

    Fixed IsEnabled Property is not works for Tab

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue5161.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue5161:_IssuesUITest
+{
+public Issue5161(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "ShellContent IsEnabledProperty does not work"; 
+
+        [Test]
+		[Category(UITestCategories.Shell)]
+		public void CheckIsEnabled()
+		{
+			App.WaitForElement("ThirdPage");
+            App.Tap("ThirdPage");
+			App.Tap("SecondPage");
+            VerifyScreenshot();
+		}
+}
