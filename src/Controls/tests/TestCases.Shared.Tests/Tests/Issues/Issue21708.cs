6852946e914fe372ea9baed0d6a67a22fc130e77
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-12-23 19:16:37 +0530

    test cases added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21708.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue21708 : _IssuesUITest
+{
+	public Issue21708(TestDevice device) : base(device) { }
+
+	public override string Issue => "CollectionView.Scrolled event offset isn't correctly reset when items change on Android";
+
+	[Test]
+	[Category(UITestCategories.CollectionView)]
+	public void VerifyCollectionViewVerticalOffset()
+	{
+		App.WaitForElement("Fill");
+		App.Tap("Fill");
+		App.Tap("Scroll");
+		App.Tap("Empty");
+		App.Tap("Fill");
+		App.Tap("Scroll");
+		VerifyScreenshot();
+	}
+}
