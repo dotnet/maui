11f1b506db3bdf03b5c61b93cf27a0cc728a9f5f
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2024-12-24 13:06:35 +0530

    Updated-Test

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21645.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21645.cs
@@ -1,3 +1,4 @@
+#if !MACCATALYST
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
@@ -6,16 +7,17 @@ namespace Microsoft.Maui.TestCases.Tests.Issues;
 
 public class Issue21645 : _IssuesUITest
 {
-    public Issue21645(TestDevice device) : base(device) { }
+	public Issue21645(TestDevice device) : base(device) { }
 
-    public override string Issue => "Android - Navigation Title disappears if Flyout is set to a new window";
+	public override string Issue => "Android - Navigation Title disappears if Flyout is set to a new window";
 
 	[Test]
 	[Category(UITestCategories.FlyoutPage)]
 	public void VerifyToolbarButtonVisibilityWhenFlyoutReset()
 	{
 		App.WaitForElement("ResetFlyoutButton");
-        App.Click("ResetFlyoutButton");
-        VerifyScreenshot();
+		App.Click("ResetFlyoutButton");
+		VerifyScreenshot();
 	}
 }
+#endif
