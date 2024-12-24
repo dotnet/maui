e1ed19cc6f02eebeb6b3a287cf3967a937526a55
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-12-24 15:17:12 +0530

    issue link added

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21708.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21708.cs
@@ -18,7 +18,9 @@ public class Issue21708 : _IssuesUITest
 		App.ScrollDown("CollectionView");
 		Assert.That(App.FindElement("Label").GetText(), Is.GreaterThan("0"));
 		App.Tap("Empty");
-		#if !MACCATALYST || !IOS   //When ItemSource is cleared, the VerticalOffset does not reset to zero on Mac and iOS.The offset updates normally after a new source is added.
+		#if !MACCATALYST || !IOS   
+		//When ItemSource is cleared, the VerticalOffset does not reset to zero on Mac and iOS.The offset updates normally after a new source is added
+		//https://github.com/dotnet/maui/issues/26798
 		Assert.That(App.FindElement("Label").GetText(), Is.EqualTo("0"));
 		#endif
 		App.Tap("Fill");
