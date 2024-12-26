3cb6aa47aef5a9b30cc7a24a86d55bf146ffb169
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-12-26 11:39:11 +0530

    test case added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue21708.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 21708, "CollectionView.Scrolled event offset isn't correctly reset when items change on Android", PlatformAffected.All)]
+	public partial class Issue21708 : ContentPage
+	{
+		ObservableCollection<int> _items;
+        double _verticalOffset;
+
+        public Issue21708()
+        {
+            InitializeComponent();
+            _items = new ObservableCollection<int>();
+            AddItemsToCollectionView();
+            CollectionView.ItemsSource = _items;
+			BindingContext = this;
+        }
+
+        public double VerticalOffset
+        {
+            get => _verticalOffset;
+            set
+            {
+                _verticalOffset = value;
+                OnPropertyChanged(nameof(VerticalOffset));
+            }
+        }
+
+        void CollectionView_OnScrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            VerticalOffset = e.VerticalOffset;
+        }
+
+        void EmptyButton_OnClicked(object sender, EventArgs e)
+        {
+            _items.Clear();
+        }
+
+        void FillButton_OnClicked(object sender, EventArgs e)
+		{
+			AddItemsToCollectionView();
+		}
+
+		void AddItemsToCollectionView()
+		{
+			foreach (var i in Enumerable.Range(0, 50))
+				_items.Add(i);
+		}
+	}
+}
