4ecf5007ffb822ae91edae18f1f09a0650e1343a
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2024-12-31 16:15:58 +0530

    Added test case and image

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla56298.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla56298.cs
@@ -1,8 +1,3 @@
-﻿#if TEST_FAILS_ON_ANDROID && TEST_FAILS_ON_WINDOWS && TEST_FAILS_ON_CATALYST
-// ListView.HasUnevenRows Property Changes Not Reflected in UI on Android and Windows Platforms. Issue Link: https://github.com/dotnet/maui/issues/26780
-// The CI-generated image on Catalyst doesn't match expectations, seems to be uneven rows aren't working correctly.
-// However, the locally generated image appears to function as expected.
-
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
@@ -19,8 +14,6 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 
 		[Test]
 		[Category(UITestCategories.ListView)]
-		[FailsOnAndroidWhenRunningOnXamarinUITest("This test is failing, likely due to product issue")]
-		[FailsOnWindowsWhenRunningOnXamarinUITest("This test is failing, likely due to product issue")]
 		public void Bugzilla56298Test()
 		{
 			App.WaitForElement("btnAdd");
@@ -31,4 +24,3 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		}
 	}
 }
-#endif
