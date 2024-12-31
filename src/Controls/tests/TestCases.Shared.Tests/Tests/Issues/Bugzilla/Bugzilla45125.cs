2f51dd957086c706404cdecb56c9ede28f58a896
Anandhan Rajagopal <97146406+anandhan-rajagopal@users.noreply.github.com>
2024-12-31 18:56:02 +0530

    [Testing] Enabling ported UITests from Xamarin.UITests to Appium - 65 (#26873)
    * migrated issue fron xamarian uitest into appium
    
    * migrated issue
    
    * Migrated the Bugzilla37625 (#56)
    
    Co-authored-by: nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>
    
    * Updated Enabled UITests
    
    * Fixed flaky tests Issue1414
    
    * Update Issue7290.cs
    
    * Update Issue5412.cs
    
    * Update Bugzilla30317.cs
    
    ---------
    
    Co-authored-by: LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
    Co-authored-by: nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla45125.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla45125.cs
@@ -7,7 +7,7 @@ public class Bugzilla45125 : _IssuesUITest
 {
 	const string AppearingLabelId = "appearing";
 	const string DisappearingLabelId = "disappearing";
-	const string TestButtonId = "TestButtonId";
+	const string TestButtonId = "Next";
 
 	public Bugzilla45125(TestDevice testDevice) : base(testDevice)
 	{
@@ -15,34 +15,30 @@ public class Bugzilla45125 : _IssuesUITest
 
 	public override string Issue => "ListView lacks a way to get information about visible elements (such as FirstVisibleItem) to restore visual positions of elements";
 
-	// TODO From Xamarin.UITest migration: test references variable in UI, needs to be refactored
-	// [FailsOnAndroidWhenRunningOnXamarinUITest]
-	// [FailsOnIOSWhenRunningOnXamarinUITest]
-	// [Test]
-	// public void Bugzilla45125Test()
-	// {
-	// 	RunTest();
+	[Test]
+	[Category(UITestCategories.ListView)]
+	public void Bugzilla45125Test()
+	{
+		RunTest();
 
-	// 	App.Tap(TestButtonId);
-	// 	RunTest();
+		App.Tap(TestButtonId);
+		RunTest();
 
-	// 	App.Tap(TestButtonId);
-	// 	RunTest();
+		App.Tap(TestButtonId);
+		RunTest();
 
-	// 	App.Tap(TestButtonId);
-	// 	RunTest();
-	// }
+		App.Tap(TestButtonId);
+		RunTest();
+	}
 
-	// void RunTest()
-	// {
-	// 	App.WaitForElement(AppearingLabelId);
-	// 	App.WaitForElement(DisappearingLabelId);
+	void RunTest()
+	{
+		App.WaitForElementTillPageNavigationSettled(AppearingLabelId);
+		App.WaitForElementTillPageNavigationSettled(DisappearingLabelId);
+		var appearing = int.Parse(App.FindElement(AppearingLabelId)?.GetText() ?? "0");
+		var disappearing = int.Parse(App.FindElement(DisappearingLabelId)?.GetText() ?? "0");
 
-	// 	App.Screenshot("There should be appearing and disappearing events for the Groups and Items.");
-	// 	var appearing = int.Parse(App.FindElement(AppearingLabelId)?.GetText() ?? "0");
-	// 	var disappearing = int.Parse(App.FindElement(DisappearingLabelId)?.GetText() ?? "0");
-
-	// 	Assert.That(appearing, Is.GreaterThan(0), $"Test {_TestNumber}: No appearing events for groups found.");
-	// 	Assert.That(disappearing, Is.GreaterThan(0), $"Test {_TestNumber}: No disappearing events for groups found.");
-	// }
+		Assert.That(appearing, Is.GreaterThan(0));
+		Assert.That(disappearing, Is.GreaterThan(0));
+	}
 }
