95a0aa027fa464fdae0b618325da00752999841d
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-01-02 13:01:49 +0530

    iOS Offset fix added

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21708.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21708.cs
@@ -18,10 +18,6 @@ public class Issue21708 : _IssuesUITest
 		App.ScrollDown("CollectionView");
 		Assert.That(App.FindElement("Label").GetText(), Is.GreaterThan("0"));
 		App.Tap("Empty");
-		#if ANDROID || WINDOWS
-		//When ItemSource is cleared, the VerticalOffset does not reset to zero on Mac and iOS.The offset updates normally after a new source is added
-		//https://github.com/dotnet/maui/issues/26798
 		Assert.That(App.FindElement("Label").GetText(), Is.EqualTo("0"));
-		#endif
 	}
 }
