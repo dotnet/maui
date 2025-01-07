f9eb1e338e37e03936cf9c85dcc45add77ee3cd5
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-01-07 17:51:37 +0530

    Updated the test case for CV1 alone

--- src/Controls/src/Core/Handlers/Items/iOS/StructuredItemsViewController.cs
+++ src/Controls/src/Core/Handlers/Items/iOS/StructuredItemsViewController.cs
@@ -206,6 +206,8 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 					_headerUIView.Frame = new CoreGraphics.CGRect(-headerWidth, 0, headerWidth, CollectionView.Frame.Height);
 				}
 
+				// The FooterView frame is updated when the CollectionView does not contain any items.
+				// Previously, the FooterView frame was updated only for a CollectionView with an ItemSource.
 				if (_footerUIView != null && (_footerUIView.Frame.X != ItemsViewLayout.CollectionViewContentSize.Width || _footerUIView.Frame.X != footerWidth || emptyWidth > 0))
 					_footerUIView.Frame = new CoreGraphics.CGRect(ItemsViewLayout.CollectionViewContentSize.Width + emptyWidth, 0, footerWidth, CollectionView.Frame.Height);
 
@@ -259,6 +261,8 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 					height = ItemsViewLayout.CollectionViewContentSize.Height;
 				}
 
+				// The FooterView frame is updated when the CollectionView does not contain any items.
+				// Previously, the FooterView frame was updated only for a CollectionView with an ItemSource.
 				if (_footerUIView != null && (_footerUIView.Frame.Y != height || _footerUIView.Frame.Y != footerHeight || emptyHeight > 0))
 				{
 					_footerUIView.Frame = new CoreGraphics.CGRect(0, height + emptyHeight, CollectionView.Frame.Width, footerHeight);
