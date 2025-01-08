1f2106d12dd7f8df04892c9be462db5ceb10cf1f
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-01-08 14:20:21 +0530

    Fixed SearchBar IsEnabled not working

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue14566.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue14566 : _IssuesUITest
+	{
+		public Issue14566(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		const string SearchBar = "SearchBar";
+		const string ResultText = "ResultText";
+		const string CheckResultButton = "CheckResultButton";
+		const string Success = "Success";
+		const string Failure = "Failure";
+		public override string Issue => "SearchBar IsEnabled property not functioning";
+
+		[Test]
+		[Category(UITestCategories.SearchBar)]
+		public void SearchBarShouldRespectIsEnabled()
+		{
+			App.WaitForElement(CheckResultButton);
+			App.Tap(SearchBar);
+			App.EnterText(SearchBar, "Hello");
+			App.Tap(CheckResultButton);
+			var resultText = App.WaitForElement(ResultText).GetText();
+			Assert.That(resultText, Is.EqualTo(Success));
+
+
+		}
+	}
+}
