78768d732c936b5a2d4b65bfe6fef2c7a9318739
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-01-08 14:20:21 +0530

    Fixed SearchBar IsEnabled not working

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue14566.cs
@@ -0,0 +1,54 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+
+	[Issue(IssueTracker.Github, 14566, "SearchBar IsEnabled property not functioning", PlatformAffected.Android | PlatformAffected.iOS | PlatformAffected.macOS)]
+	public class Issue14566 : TestContentPage
+	{
+		const string SearchBar = "SearchBar";
+		const string ResultText = "ResultText";
+		const string CheckResultButton = "CheckResultButton";
+		const string Success = "Success";
+		const string Failure = "Failure";
+
+		protected override void Init()
+		{
+			var layout = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.Center,
+				Padding = new Thickness(30, 0),
+				Spacing = 25
+			};
+
+			var searchBar = new SearchBar
+			{
+				AutomationId = SearchBar,
+				Placeholder = "Search Placeholder",
+				IsEnabled = false
+			};
+			var searchText = new Label
+			{
+				Text = Failure,
+
+			};
+			searchBar.TextChanged += (sender, e) =>
+			{
+				// Handle text changed event
+				searchText.Text = $"{e.NewTextValue}";
+			};
+
+			var result = new Label() { AutomationId = ResultText };
+			var checkResultButton = new Button() { AutomationId = CheckResultButton, Text = "Check Result" };
+			checkResultButton.Clicked += (sender, e) =>
+			{
+				result.Text = string.IsNullOrEmpty(searchBar.Text) ? Success : Failure;
+			};
+
+
+			layout.Children.Add(searchBar);
+			layout.Children.Add(checkResultButton);
+			layout.Children.Add(result);
+
+			Content = layout;
+		}
+	}
+}
