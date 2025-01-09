1ed16c7d33fbd13861d9fced7259478f0a1969a3
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-01-09 16:04:01 +0530

    Updated code changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10987.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10987.cs
@@ -1,11 +1,12 @@
-﻿#if !MACCATALYST
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 namespace Microsoft.Maui.TestCases.Tests.Issues
 {
 	public class Issue10987 : _IssuesUITest
 	{
+		const string Editor = "Editor";
+		const string RTLEditor = "RTLEditor";
 		public Issue10987(TestDevice device) : base(device) { }
 
 		public override string Issue => "Editor HorizontalTextAlignment Does not Works.";
@@ -14,7 +15,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		[Category(UITestCategories.Editor)]
 		public void EditorPlaceholderRuntimeTextAlignmentChanged()
 		{
-			App.WaitForElement("RTLEditor");
+			App.WaitForElement(RTLEditor);
 			App.Tap("Button");
 			VerifyScreenshot();
 		}
@@ -23,9 +24,11 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		[Category(UITestCategories.Editor)]
 		public void EditorRuntimeTextAlignmentChanged()
 		{
-			App.WaitForElement("RTLEditor");
-			App.EnterText("Editor", "Editor Text");
-			App.EnterText("RTLEditor", "RTL Editor");
+			//To make sure editor is focused in mac platform
+			App.Tap(Editor);
+			App.EnterText(Editor, "Editor Text");
+			App.WaitForElement(RTLEditor);
+			App.EnterText(RTLEditor, "RTL Editor");
 #if IOS
 			var app = App as AppiumApp;
 			KeyboardScrolling.HideKeyboard(app!, app!.Driver, true);
@@ -35,4 +38,3 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		}
 	}
 }
-#endif
