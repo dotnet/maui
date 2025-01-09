f9eae34116fa2c6cb7f83b28fd28475cc1f042cc
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-01-09 16:52:25 +0530

    Updated code changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue11655.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue11655.cs
@@ -1,5 +1,4 @@
-﻿#if !MACCATALYST
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 namespace Microsoft.Maui.TestCases.Tests.Issues
@@ -21,4 +20,3 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 
 	}
 }
-#endif
