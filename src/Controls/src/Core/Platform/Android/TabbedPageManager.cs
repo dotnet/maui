5bf0d113419f42e95d505b275b9fa2c66ca43606
Jakub Florkowski <kubaflo123@gmail.com>
2025-01-10 18:54:14 +0100

    [Android] Top bar item colors - fix

--- src/Controls/src/Core/Platform/Android/TabbedPageManager.cs
+++ src/Controls/src/Core/Platform/Android/TabbedPageManager.cs
@@ -890,6 +890,7 @@ namespace Microsoft.Maui.Controls.Handlers
 
 					icon.Mutate();
 					icon.SetState(_stateSet);
+					_tabLayout.TabIconTint = colors;
 					ADrawableCompat.SetTintList(icon, colors);
 				}
 			}
