70cc3dda1053d2418865439f7e73fe82553a3c77
nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>
2025-01-10 08:02:51 -0800

    [Testing] Enabling CV related UITests in Appium (#27012)
    * Verified the failures on iOS that were associated with the CV-related issues
    
    * Verified the five issues failures on iOS that were associated with the CV-related
    
    * Feedback addressed
    
    * Updated the CollectionViewItemsUpdatingScrollModeUITests
    
    * Updated the modified tests
    
    * removed unwanted changes
    
    * Added iOS and android images
    
    * Added the snapshots and updated the CollectionViewTabbedPageUITests
    
    ---------
    
    Co-authored-by: Anandhan Rajagopal <97146406+anandhan-rajagopal@users.noreply.github.com>

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/CollectionViewUITests.CollectionViewTabbedPage.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/CollectionViewUITests.CollectionViewTabbedPage.cs
@@ -9,15 +9,8 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		const string Add1 = "Add1";
 		const string Add2 = "Add2";
 		const string Success = "Success";
-#if ANDROID
-		const string FirstPage = "7700 FIRST PAGE";
-		const string Tab2 = "TAB2";
-		const string Tab3 = "TAB3";
-#else
-		const string FirstPage = "7700 First Page";
 		const string Tab2 = "Tab2";
 		const string Tab3 = "Tab3";
-#endif
 		protected override bool ResetAfterEachTest => true;
 
 		public CollectionViewTabbedPageUITests(TestDevice device)
@@ -32,19 +25,17 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		{
 			App.WaitForElement(Add1);
 			App.Tap(Add1);
-			App.WaitForElement(Tab2);
-			App.Tap(Tab2);
+			App.TapTab(Tab2);
 			App.WaitForElementTillPageNavigationSettled(Success);		
 		}
 
 		[Test]
 		[Category(UITestCategories.CollectionView)]
-		public void AddingGroupToUnviewedGroupedCollectionViewShouldNotCrash()
+		public  void AddingGroupToUnviewedGroupedCollectionViewShouldNotCrash()
 		{
 			App.WaitForElement(Add2);
 			App.Tap(Add2);
-			App.WaitForElement(Tab3);
-			App.Tap(Tab3);
+			App.TapTab(Tab3);
 			App.WaitForElementTillPageNavigationSettled(Success);
 		}		
 	}
