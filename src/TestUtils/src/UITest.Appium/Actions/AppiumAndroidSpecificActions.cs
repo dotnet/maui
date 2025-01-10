597b147080aa42b0150dee0cef3b023181805753
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2025-01-10 22:46:30 +0530

    Resolved build conflicts.

--- src/TestUtils/src/UITest.Appium/Actions/AppiumAndroidSpecificActions.cs
+++ src/TestUtils/src/UITest.Appium/Actions/AppiumAndroidSpecificActions.cs
@@ -139,21 +139,6 @@ namespace UITest.Appium
 				return CommandResponse.FailedEmptyResponse;
 			}
 		}
-		
-		CommandResponse ToggleSecondaryToolbarItems(IDictionary<string, object> parameters)
-		{
-			try
-			{
-				_appiumApp.WaitForElement(MoreButton);
-				_appiumApp.Tap(MoreButton);
-
-				return CommandResponse.SuccessEmptyResponse;
-			}
-			catch
-			{
-				return CommandResponse.FailedEmptyResponse;
-			}
-		}
     
 		CommandResponse GetSystemBars(IDictionary<string, object> parameters)
 		{
