b7be2b83984090176ecf5eac9c97fc940f225561
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2025-01-13 10:05:51 +0530

    Update ItemsViewLayout.cs

--- src/Controls/src/Core/Handlers/Items/iOS/ItemsViewLayout.cs
+++ src/Controls/src/Core/Handlers/Items/iOS/ItemsViewLayout.cs
@@ -114,7 +114,6 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			ClearCellSizeCache();
 
 			EstimatedItemSize = CGSize.Empty;
-
 			_currentSize = size;
 
 			var newSize = new CGSize(Math.Floor(size.Width), Math.Floor(size.Height));
