df5e094a404bad686c04b15c3c622f70689a0dd6
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-01-14 13:49:04 +0100

    Include no caret controls for UITests comparing snapshots

---
+++ src/Controls/tests/TestCases.HostApp/Controls/NoCaretSearchBarHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Maui.Controls.Sample
+{
+	public class NoCaretSearchBarHandler
+	{
+		public static void RemoveCaret()
+		{
+			Microsoft.Maui.Handlers.SearchBarHandler.Mapper.AppendToMapping(nameof(NoCaretSearchBar), (handler, view) =>
+			{
+				if (view is NoCaretSearchBar)
+				{
+#if ANDROID
+					var editText = Microsoft.Maui.Platform.ViewGroupExtensions.GetFirstChildOfType<Android.Widget.EditText>(handler.PlatformView);
+					editText.SetCursorVisible(false);
+#elif IOS || MACCATALYST
+					handler.PlatformView.TintColor = UIKit.UIColor.Clear;
+#endif
+				}
+			});
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26968.cs
@@ -0,0 +1,50 @@
+﻿#if TEST_FAILS_ON_WINDOWS // NoCaret Controls not implemented on Windows.
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	internal class Issue26968 : _IssuesUITest
+	{
+		public override string Issue => "Need reliable validation support to ensure the KeyboardType in Appium";
+
+		public Issue26968(TestDevice device)
+		: base(device)
+		{ }
+
+		[Test]
+		[Category(UITestCategories.Editor)]
+		public void VerifyNoCaretEditor()
+		{
+			App.WaitForElement("TestEditor");
+			App.Tap("TestEditor");
+			App.EnterText("TestEditor", "Test");
+
+			VerifyScreenshot();
+		}
+
+		[Test]
+		[Category(UITestCategories.Entry)]
+		public void VerifyNoCaretEntry()
+		{
+			App.WaitForElement("TestEntry");
+			App.Tap("TestEntry");
+			App.EnterText("TestEntry", "Test");
+
+			VerifyScreenshot();
+		}
+
+		[Test]
+		[Category(UITestCategories.SearchBar)]
+		public void VerifyNoCaretSearchBar()
+		{
+			App.WaitForElement("TestSearchBar");
+			App.Tap("TestSearchBar");
+			App.EnterText("TestSearchBar", "Test");
+
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
