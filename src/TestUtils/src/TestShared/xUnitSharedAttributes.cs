28f8581e3b0617434e4ad2d9546cbe7aef2c06fc
Jonathan Peppers <jonathan.peppers@microsoft.com>
2025-01-14 08:47:50 -0600

    Put trimmer warnings back

--- src/TestUtils/src/TestShared/xUnitSharedAttributes.cs
+++ src/TestUtils/src/TestShared/xUnitSharedAttributes.cs
@@ -1,7 +1,6 @@
 ﻿#nullable enable
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Reflection;
 using Xunit;
@@ -72,12 +71,7 @@ namespace Microsoft.Maui
 		/// Initializes a new instance of the <see cref="ClassDataAttribute"/> class.
 		/// </summary>
 		/// <param name="class">The class that provides the data.</param>
-#if NETSTANDARD
 		public ClassDataAttribute(Type @class)
-#else
-		public ClassDataAttribute([DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] Type @class)
-#endif
-
 		{
 			Class = @class;
 		}
@@ -85,9 +79,6 @@ namespace Microsoft.Maui
 		/// <summary>
 		/// Gets the type of the class that provides the data.
 		/// </summary>
-#if !NETSTANDARD
-		[DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]
-#endif
 		public Type Class { get; private set; }
 
 		/// <inheritdoc/>
