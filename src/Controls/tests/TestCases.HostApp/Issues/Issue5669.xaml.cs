82a67ea1c920793e793c3c9474fbd7239cc8ec01
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-01-15 19:19:14 +0530

    Modified code changes

--- src/Controls/tests/TestCases.HostApp/Issues/Issue5669.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue5669.xaml.cs
@@ -1,8 +1,4 @@
-﻿#if IOS
-using UIKit;
-#endif
-
-namespace Maui.Controls.Sample.Issues
+﻿namespace Maui.Controls.Sample.Issues
 {
 	[Issue(IssueTracker.Github, 5669, "Windows SearchBar MaxLength > 0 not working properly", PlatformAffected.UWP)]
 	public partial class Issue5669 : ContentPage
@@ -15,20 +11,6 @@ namespace Maui.Controls.Sample.Issues
 		private void Button_Clicked(object sender, EventArgs e)
 		{
 			searchbar.MaxLength = 4;
-		}
-
-		private void HideKeyboardButton_Clicked(object sender, EventArgs e)
-		{
-#if IOS
-			foreach (var scene in UIApplication.SharedApplication.ConnectedScenes)
-			{
-				if (scene is UIWindowScene windowScene)
-				{
-					var window = windowScene.Windows.FirstOrDefault();
-					window?.EndEditing(true);
-				}
-			}
-#endif
 		}
 	}
 }
