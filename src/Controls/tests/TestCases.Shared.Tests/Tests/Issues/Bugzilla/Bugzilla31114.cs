1627edb869b8dc4450e81660ba8b0af6de046bb6
KarthikRajaKalaimani <92777139+KarthikRajaKalaimani@users.noreply.github.com>
2025-01-15 16:28:13 +0530

    removed comments

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla31114.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla31114.cs
@@ -1,7 +1,6 @@
 ﻿#if TEST_FAILS_ON_CATALYST && TEST_FAILS_ON_IOS
 // DragCoordinates not supported on Catalyst. 
 // On IOS the cells are disapper randomly https://github.com/dotnet/maui/issues/25656, 
-// On Android scroll positions not reseted on Android Issue: https://github.com/dotnet/maui/issues/26661.
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
