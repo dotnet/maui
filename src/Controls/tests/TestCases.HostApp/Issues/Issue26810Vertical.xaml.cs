c8ed42357871b534574958c682d65932e9bea8ca
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2025-01-17 15:33:54 +0530

    Commit for HorizontalChanges

--- src/Controls/tests/TestCases.HostApp/Issues/Issue26810Vertical.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue26810Vertical.xaml.cs
@@ -1,12 +1,15 @@
 using System.Collections.ObjectModel;
 
 namespace Maui.Controls.Sample.Issues;
-[Issue(IssueTracker.Github, 26810, "Scroll To first item in CollectionView when updating the collection with KeepItemsInView",
+
+[Issue(IssueTracker.None, 26810, "Scroll To first item in CollectionView with vertical orientation when updating the collection with KeepItemsInView",
 		PlatformAffected.Android)]
-public partial class Issue26810 : ContentPage
+
+public partial class Issue26810Vertical : ContentPage
 {
+
 	private ObservableCollection<Issue26810ItemModel> Items { get; set; } = new ObservableCollection<Issue26810ItemModel>();
-	public Issue26810()
+	public Issue26810Vertical()
 	{
 		InitializeComponent();
 		for (int i = 1; i <= 30; i++)
@@ -16,7 +19,6 @@ public partial class Issue26810 : ContentPage
 		CollectionView.ItemsSource = Items;
 		this.BindingContext = this;
 	}
-
 	private void ItemsUpdatingScrollMode_Clicked(object sender, EventArgs e)
 	{
 		var button = (Button)sender;
@@ -42,10 +44,4 @@ public partial class Issue26810 : ContentPage
 			CollectionView.ScrollTo(19, position: ScrollToPosition.End, animate: true);
 		}
 	}
-}
-
-public class Issue26810ItemModel
-{
-	public string Name { get; set; }
-	public string AutomationId { get; set; }
 }
