009740cb65bc71bcb9f52b98e38b83b82b7c13ee
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-01-17 14:05:25 +0530

    Fixed slider thumb image scaling issue

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13258.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue13258 : _IssuesUITest
+	{
+		public Issue13258(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "MAUI Slider thumb image is big on android";
+
+		[Test]
+		[Category(UITestCategories.Slider)]
+		public void SliderThumbImageShouldBeScaled()
+		{
+			App.WaitForElement("slider");
+			VerifyScreenshot();
+		}
+	}
+}
