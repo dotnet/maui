36dcf775c36d921a054e10b4047f18e048ad8dc3
praveenkumarkarunanithi <praveenkumar.karunanithi@syncfusion.com>
2025-01-22 12:39:38 +0530

    updated the new class access specifier.

--- src/Core/src/Platform/WebViewHelper.cs
+++ src/Core/src/Platform/WebViewHelper.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace Microsoft.Maui.Platform;
 
-public static class WebViewHelper
+internal static class WebViewHelper
 {
-    public static bool IsRelativeUrl(string url)
+    internal static bool IsRelativeUrl(string url)
     {
         return !url.StartsWith("/") && !Uri.TryCreate(url, UriKind.Absolute, out _);
     }
