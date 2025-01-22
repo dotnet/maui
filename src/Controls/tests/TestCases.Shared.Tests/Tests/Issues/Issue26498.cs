aee4046dad6e2f51346785bf894a793692b61aed
SubhikshaSf4851 <subhiksha.c@syncfusion.com>
2025-01-22 15:36:58 +0530

    Fix for Issue 26498 - Null Exception in listview

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26498.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    public class Issue26498 : _IssuesUITest
+	{
+		public override string Issue => "Null Exception on clearing collection in list view after navigation";
+		public Issue26498(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.ListView)]
+		public void Issue26498TestNullExeption()
+		{
+			App.WaitForElement("OpenListViewPage");
+			App.Tap("OpenListViewPage");
+			App.WaitForElement("BackButton");
+			App.Click("BackButton");
+			App.WaitForElement("OpenListViewPage");
+			App.Click("OpenListViewPage");
+			App.WaitForElement("ClearButton");
+			App.Click("ClearButton");
+			VerifyScreenshot();
+											
+		}
+	}
+}
