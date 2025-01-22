988675327292570a1b50333206a2c0993bbb6385
praveenkumarkarunanithi <praveenkumar.karunanithi@syncfusion.com>
2025-01-22 15:53:22 +0530

    Fix updated

--- src/Core/src/Platform/WebViewHelper.cs
+++ src/Core/src/Platform/WebViewHelper.cs
@@ -6,6 +6,6 @@ internal static class WebViewHelper
 {
     internal static bool IsRelativeUrl(string url)
     {
-        return !url.StartsWith("/") && !Uri.TryCreate(url, UriKind.Absolute, out _);
+        return !url.StartsWith('/') && !Uri.TryCreate(url, UriKind.Absolute, out _);
     }
 }
