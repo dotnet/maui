2d57c03188ef827639ce162475739fd74e418b86
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-01-22 10:17:46 +0100

    Added repro samples

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue26838.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 26838, "[MAUI]After entering the string, ‘any records’ is not displayed.", PlatformAffected.iOS)]
+	public partial class Issue26838 : TestContentPage
+	{
+		public Issue26838()
+		{
+			InitializeComponent();
+		}
+
+		protected override void Init()
+		{
+			BindingContext = new MonkeysViewModel();
+		}
+	}
+
+	public class FilterData : BindableObject
+	{
+		public static readonly BindableProperty FilterProperty = BindableProperty.Create(nameof(Filter), typeof(string), typeof(FilterData), null);
+
+		public string Filter
+		{
+			get { return (string)GetValue(FilterProperty); }
+			set { SetValue(FilterProperty, value); }
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue27138.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 27138, "[MAUI]I5_EmptyView - When input some string in the filtering bar and click the \"search\" during project debugging, app will hang", PlatformAffected.iOS)]
+	public partial class Issue27138 : TestContentPage
+	{
+		public Issue27138()
+		{
+			InitializeComponent();
+		}
+
+		protected override void Init()
+		{
+			BindingContext = new MonkeysViewModel();
+		}
+	}
+}
