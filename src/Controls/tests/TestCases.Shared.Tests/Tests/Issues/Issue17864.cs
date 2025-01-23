20878ecc6b16675f454bb91b1bbfd99f1b412017
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-01-23 13:12:04 +0530

    Fixed NRE when grouping changed

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17864.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue17864 : _IssuesUITest
+	{
+		const string CollectionView = "CollectionView";
+		public Issue17864(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "[Windows] CollectionView throws NRE when value of IsGrouped property is changed to false";
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void CollectionViewShouldNotCrashWhenIsGroupedChanges()
+		{
+			App.WaitForElement(CollectionView);
+			App.Tap("Button");
+			App.WaitForElement(CollectionView);
+		}
+	}
+}
