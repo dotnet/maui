750afe8b4c57ebcecc227d4d63c03c77cab2ae3d
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-01-23 14:10:45 +0530

    Removed test images

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20706.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20706.cs
@@ -1,5 +1,4 @@
-﻿#if TEST_FAILS_ON_CATALYST // Stepper interaction is not implemented on catalyst
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using NUnit.Framework.Legacy;
 using UITest.Appium;
 using UITest.Core;
@@ -20,14 +19,14 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement("entry");
 			// check the current value.
 			var initialValue = App.FindElement("entry").GetText();
-			ClassicAssert.AreEqual("0", initialValue);
+		    Assert.That("0", Is.EqualTo(initialValue));
 
 			// Increase the value.
 			App.IncreaseStepper("myStepper");
 
 			// Verify that the value has been increased.
 			var step1Value = App.FindElement("entry").GetText();
-			ClassicAssert.AreEqual("2", step1Value);
+			Assert.That("2", Is.EqualTo(step1Value));
 
 			// Change the Stepper increment value.
 			App.Click("incrementButton");
@@ -35,15 +34,14 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			// Increase the value.
 			App.IncreaseStepper("myStepper");
 			var step2Value = App.FindElement("entry").GetText();
-			ClassicAssert.AreEqual("12", step2Value);
+			Assert.That("12", Is.EqualTo(step2Value));
 
 			// Decrease the value.
 			App.DecreaseStepper("myStepper");
 
 			// Verify that the value has decreased.
 			var step3Value = App.FindElement("entry").GetText();
-			ClassicAssert.AreEqual("2", step3Value);
+			Assert.That("2", Is.EqualTo(step3Value));
 		}
 	}
 }
-#endif
