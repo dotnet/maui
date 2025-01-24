9e45b7577787563a05895d51d35121afd8df4143
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-01-24 16:50:37 +0530

    Fixed-EditorAndEntrykeyboardVisiblity-Issue

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue27236.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue27236 : _IssuesUITest
+	{
+		public Issue27236(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "android allows type into hidden Entry control";
+
+		[Test]
+		[Category(UITestCategories.Entry)]
+		public void VerifyEntryKeyboardVisibilityToggle()
+		{
+			App.WaitForElement("ToggleEntryVisibilityButton");
+			App.Tap("ToggleEntryVisibilityButton");
+			var element = App.WaitForElement("ToggleableEntry");
+			element.SendKeys("Hello, Entry");
+			App.Tap("ToggleEntryVisibilityButton");
+			var keyboardVisible = App.IsKeyboardShown();
+			Assert.That(keyboardVisible, Is.False);
+		}
+
+		[Test]
+		[Category(UITestCategories.Editor)]
+		public void VerifyEditorKeyboardVisibilityToggle()
+		{
+			App.WaitForElement("ToggleEditorVisibilityButton");
+			App.Tap("ToggleEditorVisibilityButton");
+			var element = App.WaitForElement("ToggleableEditor");
+			element.SendKeys("Hello, Editor");
+			App.Tap("ToggleEditorVisibilityButton");
+			var keyboardVisible = App.IsKeyboardShown();
+			Assert.That(keyboardVisible, Is.False);
+		}
+	}
+}
