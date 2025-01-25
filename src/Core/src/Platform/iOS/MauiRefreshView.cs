1b1aa6902994371700f5563562863485f41e2b66
Jakub Florkowski <kubaflo123@gmail.com>
2025-01-25 13:47:14 +0100

    RefreshView Loader Freeze - fix

--- src/Core/src/Platform/iOS/MauiRefreshView.cs
+++ src/Core/src/Platform/iOS/MauiRefreshView.cs
@@ -29,6 +29,26 @@ namespace Microsoft.Maui.Platform
 			_refreshControlParent = this;
 		}
 
+		public override void MovedToWindow()
+		{
+			base.MovedToWindow();
+			if (IsRefreshing)
+			{
+				if (Window is not null)
+				{
+					CoreFoundation.DispatchQueue.MainQueue.DispatchAsync(() =>
+					{
+						_refreshControl.BeginRefreshing();
+						TryOffsetRefresh(this, true);
+					});
+				}
+				else if (Window is null)
+				{
+					_refreshControl.EndRefreshing();
+				}
+			}
+		}
+
 		public bool IsRefreshing
 		{
 			get { return _isRefreshing; }
