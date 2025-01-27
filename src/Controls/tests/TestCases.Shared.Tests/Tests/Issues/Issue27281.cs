db94358552e904d34518c5c19ec6a78b371cc9a7
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-01-27 13:30:49 +0100

    Added UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue27281.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+    internal class Issue27281 : _IssuesUITest
+	{
+		public Issue27281(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Application freezes when Background property is set to a DynamicResource of type OnPlatform Color";
+
+		[Test]
+		[Category(UITestCategories.Brush)]
+		public void UpdateDynamicResourceOnPlatformColor()
+		{
+			App.WaitForElement("WaitForStubControl");
+			App.Tap("WaitForStubControl");
+			App.Tap("TestButton");
+			var result = App.WaitForElement("ResultLabel").GetText();
+
+			Assert.That(result, Is.EqualTo("Passed"));
+		}
+	}
+}
