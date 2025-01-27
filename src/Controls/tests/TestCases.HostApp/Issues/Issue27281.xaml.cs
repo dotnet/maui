5bc2d019e74542020c58668a8add33cfee77b755
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-01-27 13:30:40 +0100

    Added repro sample

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue27281.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 27281, "Application freezes when Background property is set to a DynamicResource of type OnPlatform Color", PlatformAffected.All)]
+	public partial class Issue27281 : ContentPage
+	{
+		public Issue27281()
+		{
+			InitializeComponent();
+		}
+
+		void OnAddDictionaryButtonClicked(object sender, EventArgs e)
+		{
+			Resources.MergedDictionaries.Add(new Issue27281Colors());
+		}
+
+		void OnTestButtonClicked(object sender, EventArgs e)
+		{
+			ResultLabel.Text = "Passed";
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue27281Colors.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	public partial class Issue27281Colors : ResourceDictionary
+	{
+		public Issue27281Colors()
+		{
+			InitializeComponent();
+		}
+	}
+}
