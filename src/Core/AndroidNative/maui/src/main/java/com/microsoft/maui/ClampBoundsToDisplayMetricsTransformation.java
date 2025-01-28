014d07b14ffc076c8f1ffe3249e150d55828998e
Matthew Leibowitz <mattleibow@live.com>
2025-01-28 23:22:39 +0800

    whitespace for some reason

--- src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/ClampBoundsToDisplayMetricsTransformation.java
+++ src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/ClampBoundsToDisplayMetricsTransformation.java
@@ -10,50 +10,55 @@ import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 
 class ClampBoundsToDisplayMetricsTransformation extends BitmapTransformation {
-    private static final String ID = "com.microsoft.maui.ClampBoundsToDisplayMetricsTransformation";
-    private DisplayMetrics displayMetrics;
-    private String DisplayMetricsID;
+  private static final String ID = "com.microsoft.maui.ClampBoundsToDisplayMetricsTransformation";
 
-    public ClampBoundsToDisplayMetricsTransformation(DisplayMetrics display) {
-        this.displayMetrics = display;
-        DisplayMetricsID = displayMetrics.widthPixels + "x" + displayMetrics.heightPixels;
+  private DisplayMetrics displayMetrics;
+  private String displayMetricsID;
+
+  public ClampBoundsToDisplayMetricsTransformation(DisplayMetrics display) {
+    displayMetrics = display;
+    displayMetricsID = displayMetrics.widthPixels + "x" + displayMetrics.heightPixels;
+  }
+
+  @Override
+  protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
+    int width = toTransform.getWidth();
+    int height = toTransform.getHeight();
+
+    if (width <= displayMetrics.widthPixels && height <= displayMetrics.heightPixels) {
+      return toTransform;
     }
 
-    @Override
-    protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
-        int width = toTransform.getWidth();
-        int height = toTransform.getHeight();
+    float aspectRatio = (float) width / (float) height;
 
-        if (width <= displayMetrics.widthPixels && height <= displayMetrics.heightPixels) {
-            return toTransform;
-        }
-
-        float aspectRatio = (float) width / (float) height;
-
-        if (width > height) {
-            outWidth = displayMetrics.widthPixels;
-            outHeight = Math.round(displayMetrics.widthPixels / aspectRatio);
-        } else {
-            outHeight = displayMetrics.heightPixels;
-            outWidth = Math.round(displayMetrics.heightPixels * aspectRatio);
-        }
-
-        return Bitmap.createScaledBitmap(toTransform, outWidth, outHeight, false);
+    if (width > height) {
+      outWidth = displayMetrics.widthPixels;
+      outHeight = Math.round(displayMetrics.widthPixels / aspectRatio);
+    } else {
+      outHeight = displayMetrics.heightPixels;
+      outWidth = Math.round(displayMetrics.heightPixels * aspectRatio);
     }
 
-    @Override
-     public boolean equals(Object o) {
-       return o instanceof ClampBoundsToDisplayMetricsTransformation;
-     }
+    return Bitmap.createScaledBitmap(toTransform, outWidth, outHeight, false);
+  }
 
-      @Override
-     public int hashCode() {
-       return ID.hashCode();
-     }
+  @Override
+  public boolean equals(Object o) {
+    if (o instanceof ClampBoundsToDisplayMetricsTransformation) {
+      ClampBoundsToDisplayMetricsTransformation transform = (ClampBoundsToDisplayMetricsTransformation) o;
+      return displayMetricsID.equals(transform.displayMetricsID);
+    }
+    return false;
+  }
 
-      @Override
-     public void updateDiskCacheKey(MessageDigest messageDigest) {
-       byte[] bytes = (ID + DisplayMetricsID).getBytes(Key.CHARSET);
-       messageDigest.update(bytes);
-     }
+  @Override
+  public int hashCode() {
+    return ID.hashCode();
+  }
+
+  @Override
+  public void updateDiskCacheKey(MessageDigest messageDigest) {
+    byte[] bytes = (ID + displayMetricsID).getBytes(Key.CHARSET);
+    messageDigest.update(bytes);
+  }
 }
