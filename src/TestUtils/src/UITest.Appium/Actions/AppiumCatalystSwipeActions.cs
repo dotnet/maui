3d4ae31601ffeec7f62d07a1de9fb4b9a2f1336e
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-01-28 20:27:25 +0100

    Implement Appium swipe actions using Mac Driver on Catalyst

---
+++ src/TestUtils/src/UITest.Appium/Actions/AppiumCatalystSwipeActions.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium.Appium;
+
+namespace UITest.Appium
+{
+	public class AppiumCatalystSwipeActions : AppiumSwipeActions
+	{
+		public AppiumCatalystSwipeActions(AppiumApp appiumApp) : base(appiumApp) { }
+
+		protected override void SwipeToRight(AppiumDriver driver, AppiumElement? element, double swipePercentage, int swipeSpeed, bool withInertia = true)
+		{
+			var position = element is not null ? element.Location : System.Drawing.Point.Empty;
+			var size = element is not null ? element.Size : driver.Manage().Window.Size;
+
+			int y = (int)(position.X + (size.Width * swipePercentage));
+			int x = position.Y + size.Height / 2;
+
+			var parameters = new Dictionary<string, object>
+			{
+				{ "x" , x },
+				{ "y" , y },
+				{ "direction" , "right" },
+				{ "velocity" , swipeSpeed },
+
+			};
+
+			if (element is not null)
+			{
+				// The internal element identifier to swipe on.
+				// If both are set then x and y are considered as relative element coordinates.
+				// If only x and y are set then these are parsed as absolute coordinates.
+				parameters.Add("elementId", element.Id);
+			}
+
+			driver.ExecuteScript("mobile: swipe", parameters);
+		}
+
+		protected override void SwipeToLeft(AppiumDriver driver, AppiumElement? element, double swipePercentage, int swipeSpeed, bool withInertia = true)
+		{
+			var position = element is not null ? element.Location : System.Drawing.Point.Empty;
+			var size = element is not null ? element.Size : driver.Manage().Window.Size;
+
+			int x = (int)(position.X + (size.Width * 0.05));
+			int y = position.Y + size.Height / 2;
+
+			var parameters = new Dictionary<string, object>
+			{
+				{ "x" , x },
+				{ "y" , y },
+				{ "direction" , "left" },
+				{ "velocity" , swipeSpeed },
+
+			};
+
+			if (element is not null)
+			{
+				// The internal element identifier to swipe on.
+				// If both are set then x and y are considered as relative element coordinates.
+				// If only x and y are set then these are parsed as absolute coordinates.
+				parameters.Add("elementId", element.Id);
+			}
+
+			driver.ExecuteScript("mobile: swipe", parameters);
+		}
+	}
+}
