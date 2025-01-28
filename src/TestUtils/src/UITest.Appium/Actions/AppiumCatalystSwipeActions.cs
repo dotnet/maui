b5d6180299a51618125e8a6f9fb6dc9f77569133
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-01-28 20:58:07 +0100

    Updated impl

--- src/TestUtils/src/UITest.Appium/Actions/AppiumCatalystSwipeActions.cs
+++ src/TestUtils/src/UITest.Appium/Actions/AppiumCatalystSwipeActions.cs
@@ -11,27 +11,25 @@ namespace UITest.Appium
 			var position = element is not null ? element.Location : System.Drawing.Point.Empty;
 			var size = element is not null ? element.Size : driver.Manage().Window.Size;
 
-			int y = (int)(position.X + (size.Width * swipePercentage));
-			int x = position.Y + size.Height / 2;
+			int startX = (int)(position.X + (size.Width * 0.05));
+			int startY = position.Y + size.Height / 2;
+
+			int endX = (int)(position.X + (size.Width * swipePercentage));
+			int endY = startY;
 
 			var parameters = new Dictionary<string, object>
 			{
-				{ "x" , x },
-				{ "y" , y },
-				{ "direction" , "right" },
-				{ "velocity" , swipeSpeed },
-
+				{ "startX" , startX },
+				{ "startY" , startY },
+				{ "endX" , endX },
+				{ "endY" , endY },
+				{ "duration" , swipeSpeed },
 			};
 
 			if (element is not null)
-			{
-				// The internal element identifier to swipe on.
-				// If both are set then x and y are considered as relative element coordinates.
-				// If only x and y are set then these are parsed as absolute coordinates.
-				parameters.Add("elementId", element.Id);
-			}
-
-			driver.ExecuteScript("mobile: swipe", parameters);
+				parameters.Add("sourceElementId", element.Id);
+			
+			driver.ExecuteScript("macos: clickAndDrag", parameters);
 		}
 
 		protected override void SwipeToLeft(AppiumDriver driver, AppiumElement? element, double swipePercentage, int swipeSpeed, bool withInertia = true)
@@ -39,27 +37,25 @@ namespace UITest.Appium
 			var position = element is not null ? element.Location : System.Drawing.Point.Empty;
 			var size = element is not null ? element.Size : driver.Manage().Window.Size;
 
-			int x = (int)(position.X + (size.Width * 0.05));
-			int y = position.Y + size.Height / 2;
+			int startX = (int)(position.X + (size.Width * swipePercentage));
+			int startY = position.Y + size.Height / 2;
+
+			int endX = (int)(position.X + (size.Width * 0.05));
+			int endY = startY;
 
 			var parameters = new Dictionary<string, object>
 			{
-				{ "x" , x },
-				{ "y" , y },
-				{ "direction" , "left" },
-				{ "velocity" , swipeSpeed },
-
+				{ "startX" , startX },
+				{ "startY" , startY },
+				{ "endX" , endX },
+				{ "endY" , endY },
+				{ "duration" , swipeSpeed },
 			};
 
 			if (element is not null)
-			{
-				// The internal element identifier to swipe on.
-				// If both are set then x and y are considered as relative element coordinates.
-				// If only x and y are set then these are parsed as absolute coordinates.
-				parameters.Add("elementId", element.Id);
-			}
-
-			driver.ExecuteScript("mobile: swipe", parameters);
+				parameters.Add("sourceElementId", element.Id);
+			
+			driver.ExecuteScript("macos: pressAndDrag", parameters);
 		}
 	}
 }
