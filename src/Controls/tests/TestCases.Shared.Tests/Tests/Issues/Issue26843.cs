ac7c27198a2046db855a5d9db082f588bf938a20
praveenkumarkarunanithi <praveenkumar.karunanithi@syncfusion.com>
2025-01-29 12:01:54 +0530

    updating ui test case with ignore windows comment

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26843.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26843.cs
@@ -1,14 +1,14 @@
-using System;
+#if TEST_FAILS_ON_WINDOWS //for more information:https://github.com/dotnet/maui/issues/24968
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
-
+ 
 namespace Microsoft.Maui.TestCases.Tests.Issues
 {
     internal class Issue26843 : _IssuesUITest
     {
         public Issue26843(TestDevice device) : base(device) { }
-
+ 
         public override string Issue => "WebView Fails to Load URLs with Certain Encoded Characters on Android";
         
         /// <summary>
@@ -17,9 +17,9 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		/// </summary>
         [Test]
         [Category(UITestCategories.WebView)]
-        [TestCase("https://example.com/test-Ağ-Sistem%20Bilgi%20Güvenliği%20Md/Guide.pdf", "TurkishChars")]
-        [TestCase("https://google.com/[]", "SquareBrackets")]
-        public async Task WebViewShouldLoadEncodedUrl(string url, string screenshotName)
+        [TestCase("https://example.com/test-Ağ-Sistem%20Bilgi%20Güvenliği%20Md/Guide.pdf")]
+        [TestCase("https://google.com/[]")]
+        public void WebViewShouldLoadEncodedUrl(string url)
         {
             App.WaitForElement("WebViewSourceEntry");
             App.ClearText("WebViewSourceEntry");
@@ -27,8 +27,25 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
             App.EnterText("WebViewSourceEntry", url);
             App.WaitForElement("LoadUrlButton");
             App.Tap("LoadUrlButton");
-            await Task.Delay(1000);
-            VerifyScreenshot($"{TestContext.CurrentContext.Test.MethodName}_with_{screenshotName}");
+            var label = App.WaitForElement("NavigationResultLabel");
+            Assert.That(label.GetText(), Is.EqualTo("Successfully navigated to the encoded URL"));
+        }
+ 
+        public override void TestSetup()
+        {
+            base.TestSetup();
+ 
+            try
+            {
+                App.WaitForElement("NoInternetAccessLabel", timeout: TimeSpan.FromSeconds(1));
+                Assert.Inconclusive("This device doesn't have internet access");
+            }
+            catch (TimeoutException)
+            {
+                // Element not found within timeout, assume internet is available
+                // Continue with the test
+            }
         }
     }
 }
+#endif
