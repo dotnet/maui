53f72f12982f5a719103287e3ebb16e223ac2736
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-01-29 15:18:09 +0530

    Modified the test case

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue27367.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue27367.cs
@@ -1,6 +1,5 @@
 #if TEST_FAILS_ON_CATALYST && TEST_FAILS_ON_WINDOWS
 // On Catalyst, Swipe actions not supported in Appium.
-//Unable to capture a screenshot with the swipe open in windows, as SwipeRightToLeft does not fully open the SwipeItems and instead closes them.
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
@@ -9,17 +8,27 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 {
 	public class Issue27367 : _IssuesUITest
 	{
-		const string SwipeViewId = "SwipeView";
+		const string SwipeItem = "SwipeItem";
+		const string SwipeItemView = "SwipeItemView";
 		public Issue27367(TestDevice device) : base(device) { }
 
 		public override string Issue => "[Android] Right SwipeView items are not visible in the SwipeView";
 
-		[Test]
+		[Test, Order(1)]
 		[Category(UITestCategories.SwipeView)]
 		public void TextInRightSwipeItemIsVisibleOnRightSwipe()
 		{
-			App.WaitForElement(SwipeViewId);
-			App.SwipeRightToLeft(SwipeViewId);
+			App.WaitForElement(SwipeItem);
+			App.SwipeRightToLeft(SwipeItem);
+			VerifyScreenshot();
+		}
+
+		[Test, Order(2)]
+		[Category(UITestCategories.SwipeView)]
+		public void CustomViewInRightSwipeItemViewIsVisibleOnRightSwipe()
+		{
+			App.WaitForElement(SwipeItemView);
+			App.SwipeRightToLeft(SwipeItemView);
 			VerifyScreenshot();
 		}
 	}
