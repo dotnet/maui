ab6344ae0d663263b019ea96d5aa9810be917f29
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-02-03 15:45:31 +0530

    updated changes

--- src/Core/src/Platform/iOS/MauiTextView.cs
+++ src/Core/src/Platform/iOS/MauiTextView.cs
@@ -162,17 +162,27 @@ namespace Microsoft.Maui.Platform
 
 		void ShouldCenterVertically()
 		{
-			var contentHeight = ContentSize.Height - TextContainerInset.Top - TextContainerInset.Bottom;
+			var contentHeight = ContentSize.Height;
 			var availableSpace = Bounds.Height - contentHeight * ZoomScale;
 			if (availableSpace <= 0)
 				return;
-
-			TextContainerInset = VerticalTextAlignment switch
+			ContentOffset = VerticalTextAlignment switch
 			{
-				Maui.TextAlignment.Center => new UIEdgeInsets((nfloat)Math.Max(1, availableSpace / 2), TextContainerInset.Left, TextContainerInset.Bottom, TextContainerInset.Right),
-				Maui.TextAlignment.End => new UIEdgeInsets((nfloat)Math.Max(1, availableSpace), TextContainerInset.Left, TextContainerInset.Bottom, TextContainerInset.Right),
-				_ => TextContainerInset,
+				Maui.TextAlignment.Center => new CGPoint(0, -Math.Max(1, availableSpace / 2)),
+				Maui.TextAlignment.End => new CGPoint(0, -Math.Max(1, availableSpace)),
+				_ => ContentOffset,
 			};
+
+			// Scroll the content to the cursor position if it is hidden by the keyboard
+			if (KeyboardAutoManagerScroll.IsKeyboardShowing)
+			{
+				var cursorRect = KeyboardAutoManagerScroll.FindCursorPosition();
+				if (cursorRect.HasValue && cursorRect.Value.Bottom > KeyboardAutoManagerScroll.KeyboardFrame.Top)
+				{
+					var offset = cursorRect.Value.Bottom - KeyboardAutoManagerScroll.KeyboardFrame.Top;
+					ContentOffset = new CGPoint(ContentOffset.X, ContentOffset.Y + offset);
+				}
+			}
 		}
 
 		void UpdatePlaceholderFont(UIFont? value)
