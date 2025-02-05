b474b6c5a393707e0dc376ad793b4ed199a63b00
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-02-05 20:07:16 +0530

    Fixed-Custom-FlyoutIcon-Color

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/IssueShell6738.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class IssueShell6738 : _IssuesUITest
+	{
+		public override string Issue => "The color of the custom icon in Shell always resets to the default blue";
+
+		public IssueShell6738(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void EnsureCustomFlyoutIconColor()
+		{
+			App.WaitForElement("Label");
+			VerifyScreenshot();
+		}
+	}
+}
