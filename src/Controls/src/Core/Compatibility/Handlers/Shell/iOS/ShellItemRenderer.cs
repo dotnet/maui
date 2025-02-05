b69ed1a25ddcd4d7279c3862d29719da0c34e5ad
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-02-05 19:00:12 +0530

    Removed unwanted keyword

--- src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellItemRenderer.cs
+++ src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellItemRenderer.cs
@@ -323,7 +323,7 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility
 			UpdateMoreCellsEnabled();
 		}
 
-		private void UpdateCellsEnabled()
+		void UpdateCellsEnabled()
 		{
 			for (int i = 0; i < TabBar.Items.Length; i++)
 			{
