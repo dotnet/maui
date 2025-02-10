25cb8bc38359b2684440d0050bdc7d493be16137
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-02-10 15:50:51 +0530

    Modified-Code

--- src/Controls/src/Core/NavigationPage/NavigationPageToolbar.cs
+++ src/Controls/src/Core/NavigationPage/NavigationPageToolbar.cs
@@ -71,9 +71,11 @@ namespace Microsoft.Maui.Controls
 				return;
 
 			_toolbarTracker.PagePropertyChanged -= OnPagePropertyChanged;
-			FlyoutPage currentFlyoutPage = cp.FindParentOfType<FlyoutPage>();
-			if (currentFlyoutPage?.Flyout == cp || currentFlyoutPage?.Flyout?.InternalChildren.Contains(cp) is true)
+			var currentParent = cp.Parent;
+			if (currentParent is FlyoutPage flyoutPage && flyoutPage.Flyout == cp)
+			{
 				return;
+			}
 
 			_currentPage = cp;
 			_currentNavigationPage = _currentPage.FindParentOfType<NavigationPage>();
@@ -87,15 +89,6 @@ namespace Microsoft.Maui.Controls
 			_navigationPagesStack.Clear();
 			if (_currentNavigationPage == null)
 			{
-				FlyoutPage isFlyoutPage = _currentPage.FindParentOfType<FlyoutPage>();
-
-				// In a FlyoutPage, The Detail page manages navigation and overrides the toolbar visibility.
-				// Therefore,return early without modifying the toolbar visibility.
-				if (isFlyoutPage?.Flyout == _currentPage)
-				{
-					return;
-				}
-
 				IsVisible = false;
 				return;
 			}
