bd0044cdc06e555f5c13be434501988551804680
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2025-02-11 19:57:11 +0530

    Implemented automation peer for Border

---
+++ src/Core/src/Platform/Windows/MauiBorderAutomationPeer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Automation.Peers;
+using Microsoft.UI.Xaml.Controls;
+
+public class MauiBorderAutomationPeer : FrameworkElementAutomationPeer
+{
+    public MauiBorderAutomationPeer(Panel owner) : base(owner) { }
+
+    protected override AutomationControlType GetAutomationControlTypeCore()
+    {
+        return AutomationControlType.Pane;
+    }
+
+    protected override string GetClassNameCore()
+    {
+        return nameof(Panel);
+    }
+
+    protected override bool IsControlElementCore() => true;
+
+    protected override bool IsContentElementCore() => true;
+
+}
