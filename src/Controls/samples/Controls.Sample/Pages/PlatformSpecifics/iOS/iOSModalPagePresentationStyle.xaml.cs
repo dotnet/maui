3961740c46ea4bbc5726015e16340c20492485dd
Gerald Versluis <gerald.versluis@microsoft.com>
2025-02-11 13:06:45 +0100

    Minor fixes

--- src/Controls/samples/Controls.Sample/Pages/PlatformSpecifics/iOS/iOSModalPagePresentationStyle.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/PlatformSpecifics/iOS/iOSModalPagePresentationStyle.xaml.cs
@@ -8,22 +8,34 @@ namespace Maui.Controls.Sample.Pages
 {
 	public partial class iOSModalPagePresentationStyle : ContentPage
 	{
-		//bool childPage = false;
-		public Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific.UIModalPresentationStyle presentationStyle = Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific.UIModalPresentationStyle.Automatic;
+		bool isChildPage;
+
+		public Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific.UIModalPresentationStyle presentationStyle =
+			Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific.UIModalPresentationStyle.Automatic;
+
 		public iOSModalPagePresentationStyle()
 		{
 			InitializeComponent();
 		}
-		public iOSModalPagePresentationStyle(UIModalPresentationStyle presentationStyle, View? modalOrigin=null, Rectangle? rectangle = null)
+
+		public iOSModalPagePresentationStyle(UIModalPresentationStyle presentationStyle, bool isChildPage = true,
+			View? modalOrigin = null, Rectangle? rectangle = null)
 		{
 			InitializeComponent();
-			if(modalOrigin != null){
+
+			if(modalOrigin is not null)
+			{
 				On<iOS>().SetModalPopoverView(modalOrigin);
 			}
-			if(rectangle != null){
+
+			if(rectangle is not null)
+			{
 				On<iOS>().SetModalPopoverRect(rectangle.Value);
 			}
+
 			On<iOS>().SetModalPresentationStyle(presentationStyle);
+
+			this.isChildPage = isChildPage;
 		}
 
 		async void OnPushFormSheetClicked(object sender, EventArgs e)
@@ -31,21 +43,30 @@ namespace Maui.Controls.Sample.Pages
 			Microsoft.Maui.Controls.Page pushMe = new iOSModalPagePresentationStyle(UIModalPresentationStyle.FormSheet);
 			await Navigation.PushModalAsync(pushMe);
 		}
+
 		async void OnPushPopoverClicked(object sender, EventArgs e)
 		{
 			Microsoft.Maui.Controls.Page pushMe = new iOSModalPagePresentationStyle(UIModalPresentationStyle.Popover, originButton);
 			await Navigation.PushModalAsync(pushMe);
 		}
+
 		async void OnPushPopoverOffsetClicked(object sender, EventArgs e)
 		{
-			var offset = new System.Drawing.Rectangle(0,0,100,10);
+			var offset = new System.Drawing.Rectangle(0, 0, 100, 10);
 			Microsoft.Maui.Controls.Page pushMe = new iOSModalPagePresentationStyle(UIModalPresentationStyle.Popover, originButton2, offset);
 			await Navigation.PushModalAsync(pushMe);
 		}
 
 		async void OnReturnButtonClicked(object sender, EventArgs e)
 		{
-			await Navigation.PopModalAsync();
+			if (isChildPage)
+			{
+				await Navigation.PopModalAsync();
+			}
+			else
+			{
+				await Navigation.PopAsync();
+			}
 		}
 	}
 }
