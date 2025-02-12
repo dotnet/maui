18511ba226a8eb9cd69e39b6ae071ce16cd34131
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-02-12 13:00:53 +0530

    Added test case.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue27627.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue27627 : _IssuesUITest
+	{
+		public override string Issue => "[Windows] Border Automation Peer";
+
+		public Issue27627(TestDevice device)
+		: base(device)
+		{ }
+
+		[Test]
+		[Category(UITestCategories.Border)]
+		public void VerifyBorderAutomationPeer()
+		{
+			App.WaitForElement("TestBorder");
+		}
+	}
+}
