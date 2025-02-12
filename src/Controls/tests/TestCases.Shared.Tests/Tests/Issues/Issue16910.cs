564150ff947f2be9f56e5496590b2cd52441eebd
LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
2025-02-12 18:55:23 +0530

    enable test case for all platforms

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16910.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16910.cs
@@ -17,30 +17,25 @@ public class Issue16910 : _IssuesUITest
 
 	}
 
-#if !MACCATALYST
 	[Test]
-	[FailsOnMacWhenRunningOnXamarinUITest("When the refreshview appears on catalyst. Appium starts to have a really hard time finding elements")]
-	public void BindingUpdatesFromProgrammaticRefresh()
+public void BindingUpdatesFromProgrammaticRefresh()
 	{
 		_ = App.WaitForElement("StartRefreshing");
 		App.Tap("StartRefreshing");
 		App.WaitForElement("IsRefreshing");
-		App.Click("StopRefreshing");
+		App.Tap("StopRefreshing");
 		App.WaitForElement("IsNotRefreshing");
 	}
-#endif
 
 	// Windows only works with touch inputs which we don't have running on the test server
-#if !WINDOWS && !MACCATALYST
-	[Test]
+[Test]
 	public void BindingUpdatesFromInteractiveRefresh()
 	{
 		_ = App.WaitForElement("CollectionView");
-		App.ScrollUp("CollectionView");
+		App.ScrollUp("CollectionView", ScrollStrategy.Gesture, swipeSpeed: 7000);
 		App.WaitForElement("IsRefreshing");
 		App.Tap("StopRefreshing");
 		App.WaitForElement("IsNotRefreshing");
 	}
-#endif
 
 }
