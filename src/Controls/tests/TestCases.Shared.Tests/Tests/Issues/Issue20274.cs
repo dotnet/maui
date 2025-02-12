00384fb9e9977e485149d37354ab709856b930e0
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-02-12 19:54:07 +0530

    Test case added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20274.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue20274 : _IssuesUITest
+	{
+		public override string Issue => "IsEnabled=False on CollectionView not working";
+
+		public Issue20274(TestDevice device) : base(device)
+		{
+		}
+
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void VerifyCollectionViewIsEnableState()
+		{
+			App.WaitForElement("Button");
+			App.Tap("ItemsLabel");
+			Assert.That(App.FindElement("Label").GetText(), Is.EqualTo("Item 1"));
+			App.Tap("Button");
+			App.Tap("ItemsLabel");
+			Assert.That(App.FindElement("Label").GetText(), Is.EqualTo("Item 1 - Selected"));
+		}
+	}
+}
