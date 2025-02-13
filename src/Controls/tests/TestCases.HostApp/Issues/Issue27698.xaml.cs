413b44d26168e6379fd600449c74814b579db0ed
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-02-13 12:23:26 +0100

    Added repro sample

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue27698.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 27698, "Editor with AutoSize=EditorAutoSizeOption.TextChanges bad performance on iOS", PlatformAffected.iOS)]
+	public partial class Issue27698 : TestContentPage
+	{
+		int _fontSize;
+		int _characterSpacing;
+		
+		public Issue27698()
+		{
+			InitializeComponent();
+
+			_fontSize = 16;
+			_characterSpacing = 2;
+		}
+		
+		protected override void Init()
+		{
+
+		}
+
+		void OnUpdateFontSizeButtonClicked(object sender, EventArgs e)
+		{
+			Test001.FontSize = Test002.FontSize =Test003.FontSize = _fontSize;
+			_fontSize++;
+		}
+
+		void OnUpdateCharacterSpacingButtonClicked(object sender, EventArgs e)
+		{
+			Test001.CharacterSpacing = Test002.CharacterSpacing =Test003.CharacterSpacing = _characterSpacing;
+			_characterSpacing++;
+		}
+	}
+}
