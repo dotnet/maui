0891706fedce7cdac1e961d61adf83e095494bed
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2025-02-14 18:20:55 +0530

    Added test sample.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue27563.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue27563 : _IssuesUITest
+	{
+		public override string Issue => "[Windows] CarouselView Scrolling Issue";
+
+		public Issue27563(TestDevice device)
+		: base(device)
+		{ }
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void VerifyCollectionViewScrolling()
+		{
+			App.WaitForElement("PositionButton");
+			App.Tap("PositionButton");
+
+			VerifyScreenshot();
+		}
+	}
+}
