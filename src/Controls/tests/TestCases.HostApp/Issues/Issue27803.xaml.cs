8bf389bbccab9416d1b3d7287c09f5a8e6138ec3
bhavanesh2001 <bhavanesh001@gmail.com>
2025-02-15 10:31:09 +0530

    adding Ui test

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue27803.xaml.cs
@@ -0,0 +1,40 @@
+namespace Controls.TestCases.HostApp.Issues;
+
+[Issue(IssueTracker.Github,27803,"DatePicker default format on iOS",PlatformAffected.iOS)]
+public partial class Issue27803 : ContentPage
+{
+	private DateTime workDate =  DateTime.Now;
+	public DateTime WorkDate 
+	{
+		get => workDate;
+		set
+		{
+			workDate = value;
+			OnPropertyChanged();
+		}
+	}
+
+	private Color datePickerTextColor = Color.FromArgb("#0000FF");
+	public Color DatePickerTextColor
+	{
+		get => datePickerTextColor;
+		set
+		{
+			datePickerTextColor = value;
+			OnPropertyChanged();
+		}
+	}
+	public Issue27803()
+	{
+		InitializeComponent();
+		BindingContext = this;
+		colorLabel.Text = DatePickerTextColor.ToArgbHex();
+		AutomationProperties.SetHelpText(datePicker,datePicker.TextColor.ToArgbHex());
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		DatePickerTextColor = Color.FromArgb("#FF0000");
+		colorLabel.Text = datePicker.TextColor.ToArgbHex();
+	}
+}
