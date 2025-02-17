30b2c6cabeefc44abb773b9e4c8ea2f0f005e9db
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-02-17 12:54:28 +0530

    Removed UI test and added the device test

--- src/Controls/tests/DeviceTests/Elements/CollectionView/CollectionViewTests.cs
+++ src/Controls/tests/DeviceTests/Elements/CollectionView/CollectionViewTests.cs
@@ -10,6 +10,7 @@ using System.Xml.Linq;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Handlers.Compatibility;
 using Microsoft.Maui.Controls.Handlers.Items;
+using Microsoft.Maui.Controls.Handlers.Items2;
 using Microsoft.Maui.Controls.Platform;
 using Microsoft.Maui.DeviceTests.Stubs;
 using Microsoft.Maui.Graphics;
@@ -44,8 +45,10 @@ namespace Microsoft.Maui.DeviceTests
 					handlers.AddHandler<Button, ButtonHandler>();
 					handlers.AddHandler<SwipeView, SwipeViewHandler>();
 					handlers.AddHandler<SwipeItem, SwipeItemMenuItemHandler>();
+					handlers.AddHandler<Border, BorderHandler>();
 #if IOS || MACCATALYST
 					handlers.AddHandler(typeof(NavigationPage), typeof(NavigationRenderer));
+					handlers.AddHandler<CollectionView, CollectionViewHandler2>();
 #else
 					handlers.AddHandler(typeof(NavigationPage), typeof(NavigationViewHandler));
 #endif
