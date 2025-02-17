a6ed4b2cdd5653c313dd3c6c90d383ff04e1088a
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-02-17 13:44:44 +0530

    Fixed ScrollView inside a Grid expands width past device screen when rotated

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue27715.cs
@@ -0,0 +1,27 @@
+﻿#if ANDROID || IOS //Issue is only reproducible on Android and iOS platforms.
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue27715 : _IssuesUITest
+	{
+
+		public Issue27715(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "ScrollView inside a Grid expands width past device screen when rotated";
+
+		[Test]
+		[Category(UITestCategories.ScrollView)]
+		public void ScrollViewShouldRenderWithinBounds()
+		{
+			App.WaitForElement("label");
+			App.SetOrientationLandscape();
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
