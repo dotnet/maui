9a45b3d0b8f6afa5d4abfed0eb8e75ff619d999d
nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>
2025-02-18 11:30:32 +0530

    Added the images for mac platform

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22306.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22306.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 				App.SetOrientationLandscape();
 
 				WaitForAllElements();
-				VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "SizeButtonsDownLandscape");
+				VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "SizeButtonsDownLandscape");
 				changeBoundsButton.Click();
 				WaitForAllElements();
 
