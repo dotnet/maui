7256a77f2b610b1fcaf82f1cf5ca5f899c069279
Gerald Versluis <gerald.versluis@microsoft.com>
2025-02-18 19:40:28 +0100

    Internalize/remove MessagingCenter (#27842)
    * Internalize MessagingCenter
    
    * Oops

--- src/Controls/src/Core/Properties/AssemblyInfo.cs
+++ src/Controls/src/Core/Properties/AssemblyInfo.cs
@@ -5,7 +5,6 @@ using Microsoft.Maui.Controls.StyleSheets;
 using Compatibility = Microsoft.Maui.Controls.Compatibility;
 
 [assembly: InternalsVisibleTo("iOSUnitTests")]
-[assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.ControlGallery")]
 [assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility")]
 [assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.Android")]
 [assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.iOS")]
