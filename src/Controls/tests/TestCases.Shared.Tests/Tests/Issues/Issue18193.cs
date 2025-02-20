e77006bf6393f14c496e3ac9893882ddc0af2a02
LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
2025-02-20 11:17:09 +0530

    modified changes for issue18193

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18193.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18193.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_CATALYST //related issue: https://github.com/dotnet/maui/issues/27206
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -31,3 +32,4 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		}
 	}
 }
+#endif
