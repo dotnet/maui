a98101f23ceb198fce83b0f7e5c4d662b1c80b10
LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
2025-02-20 19:48:34 +0530

    enable ScrollViewObjectDisposed.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/ScrollViewObjectDisposed.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/ScrollViewObjectDisposed.cs
@@ -1,5 +1,4 @@
-﻿#if !ANDROID && !WINDOWS
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -17,8 +16,6 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		// ScrollViewObjectDisposedTest (src\Compatibility\ControlGallery\src\Issues.Shared\ScrollViewObjectDisposed.cs)
 		[Test]
 		[Description("Tapping a button inside the ScrollView does not cause an exception.")]
-		[FailsOnAndroidWhenRunningOnXamarinUITest("This test is failing, likely due to product issue")]
-		[FailsOnWindowsWhenRunningOnXamarinUITest("This test is failing, likely due to product issue")]
 		public void ScrollViewObjectDisposedTest()
 		{
 			// 1. Tap the button.
@@ -29,4 +26,3 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		}
 	}
 }
-#endif
