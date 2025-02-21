5594050f2857d7628939f7f9ccb15aae2b14a654
Suthi <suthi.yuvaraj@syncfusion.com>
2025-02-21 14:50:44 +0530

    commit for testcase changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25991.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25991.cs
@@ -22,30 +22,23 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		{
 			App.WaitForElement("WaitForStubControl");
 
-			App.WaitForElement(InfoLabel);
-			var result1 = App.FindElement(InfoLabel).GetText();
-			ClassicAssert.AreEqual("0", result1);
+			App.WaitForElement("0");
 
 			App.Click("ScrollToPerson2Button");
 			App.Click(AddItemButton);
 
-			App.WaitForElement(InfoLabel);
-			var result2 = App.FindElement(InfoLabel).GetText();
-			ClassicAssert.AreEqual("1", result2);
+			App.WaitForElement("1");
 
 			App.Click("KeepItemsInViewButton");
 			App.Click(AddItemButton);
 
-			App.WaitForElement(InfoLabel);
-			var result3 = App.FindElement(InfoLabel).GetText();
-			ClassicAssert.AreEqual("0", result3);
+
+			App.WaitForElement("0");
 
 			App.Click("KeepLastItemInViewButton");
 			App.Click(AddItemButton);
 
-			App.WaitForElement(InfoLabel);
-			var result4 = App.FindElement(InfoLabel).GetText();
-			ClassicAssert.AreEqual("4", result4);
+			App.WaitForElement("4");
 
 		}
 	}
