26ebe7a8a01a7b7c7de3bb29168875471abff5c7
Jakub Florkowski <kubaflo123@gmail.com>
2025-02-23 15:51:21 +0100

    Added a UiTest

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25606.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, "25606", "Empty View Overlapping Header in CollectionView with CollectionViewHandler2", PlatformAffected.iOS)]
+	public partial class Issue25606 : ContentPage
+	{
+		public Issue25606()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25606.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue25606 : _IssuesUITest
+	{
+		public Issue25606(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Empty View Overlapping Header in CollectionView with CollectionViewHandler2";
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void HeaderShouldNotBeCoveredByEmptyView()
+		{
+			App.WaitForElement("Header");
+		}
+	}
+}
