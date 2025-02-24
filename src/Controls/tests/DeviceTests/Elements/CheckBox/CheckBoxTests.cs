1f54d06902ae31d6961cdc1b136ca8222500bd35
nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>
2025-02-24 11:30:57 +0530

    Added the CornerRadius and IsEnabled property for DeviceTest

--- src/Controls/tests/DeviceTests/Elements/CheckBox/CheckBoxTests.cs
+++ src/Controls/tests/DeviceTests/Elements/CheckBox/CheckBoxTests.cs
@@ -62,5 +62,21 @@ namespace Microsoft.Maui.DeviceTests
 				Assert.Equal(expectedValue, nativeOpacityValue);
 			});
 		}
+		
+		[Fact("The IsEnabled of a CheckBox should match with native IsEnabled")]
+		public async Task CheckBoxIsEnabled()
+		{
+			var checkBox = new CheckBox();
+			checkBox.IsEnabled = false;
+			var expectedValue = checkBox.IsEnabled;
+
+			var handler = await CreateHandlerAsync<CheckBoxHandler>(checkBox);
+			var nativeView = GetNativeCheckBox(handler);
+			await InvokeOnMainThreadAsync(() =>
+			{
+				var isEnabled = nativeView.Enabled;
+				Assert.Equal(expectedValue, isEnabled);
+			});		
+		}
 	}
 }
