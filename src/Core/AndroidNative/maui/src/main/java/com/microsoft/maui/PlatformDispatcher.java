7b40077dd413d2c9a13798092a0becfbdf8ce091
Jonathan Peppers <jonathan.peppers@microsoft.com>
2025-02-24 12:53:32 -0600

    Update src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/PlatformDispatcher.java

--- src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/PlatformDispatcher.java
+++ src/Core/AndroidNative/maui/src/main/java/com/microsoft/maui/PlatformDispatcher.java
@@ -19,6 +19,6 @@ public class PlatformDispatcher extends Handler {
     }
 
     public boolean isDispatchRequired() {
-        return Looper.getMainLooper() != getLooper();
+        return Looper.myLooper() != getLooper();
     }
 }
