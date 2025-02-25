7c34e5a8cc7fa8f625e6bfe76d21644cc3ec0ad7
LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
2025-02-25 19:04:50 +0530

    enable mac for stepper

--- src/TestUtils/src/UITest.Appium/AppiumCatalystApp.cs
+++ src/TestUtils/src/UITest.Appium/AppiumCatalystApp.cs
@@ -11,12 +11,12 @@ namespace UITest.Appium
 			: base(new MacDriver(remoteAddress, GetOptions(config)), config)
 		{
 			_commandExecutor.AddCommandGroup(new AppiumAppleContextMenuActions(this));
+			_commandExecutor.AddCommandGroup(new AppiumAppleStepperActions(this));
 			_commandExecutor.AddCommandGroup(new AppiumCatalystMouseActions(this));
 			_commandExecutor.AddCommandGroup(new AppiumCatalystTouchActions(this));
 			_commandExecutor.AddCommandGroup(new AppiumCatalystAlertActions(this));
 			_commandExecutor.AddCommandGroup(new AppiumCatalystSpecificActions(this));
 			_commandExecutor.AddCommandGroup(new AppiumCatalystSwipeActions(this));
-			_commandExecutor.AddCommandGroup(new AppiumAppleStepperActions(this));
 			_commandExecutor.AddCommandGroup(new AppiumCatalystVirtualKeyboardActions(this));
 			_commandExecutor.AddCommandGroup(new AppiumCatalystScrollActions(this));
 		}
