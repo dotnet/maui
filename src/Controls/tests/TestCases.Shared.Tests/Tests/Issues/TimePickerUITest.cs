da4b7a532005b16b675a54e5833f5312348f3311
NafeelaNazhir <nafeela.nazhirhussain@syncfusion.com>
2025-02-25 13:30:18 +0530

    [Testing] Fix for flaky UITests in CI that occasionally fail - 3 (#27905)
    * enabled flaky UITest cases
    
    * updated changes
    
    * updated changes
    
    * Updated Image
    
    * updated Android image

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/TimePickerUITest.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/TimePickerUITest.cs
@@ -1,4 +1,5 @@
-﻿#if TEST_FAILS_ON_CATALYST // TimePicker not opens the dialog, issue: https://github.com/dotnet/maui/issues/10827 
+﻿#if TEST_FAILS_ON_CATALYST && TEST_FAILS_ON_IOS // TimePicker not opens the dialog, issue: https://github.com/dotnet/maui/issues/10827 
+// Picker popup layout is inconsistent on IOS , issue: https://github.com/dotnet/maui/issues/27898
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
