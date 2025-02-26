46d46174cb6998325f5544cb7ea3f56b4a31a5e8
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-02-26 17:11:12 +0530

    updated code changes in ui test file

--- src/Controls/tests/TestCases.Shared.Tests/UITest.cs
+++ src/Controls/tests/TestCases.Shared.Tests/UITest.cs
@@ -18,7 +18,6 @@ namespace Microsoft.Maui.TestCases.Tests
 #elif MACUITEST
 		[TestFixture(TestDevice.Mac)]
 #elif WINTEST
-		[TestFixture(TestDevice.Windows)]
 	[TestFixture(TestDevice.Windows)]
 #endif
 	public abstract class UITest : UITestBase
