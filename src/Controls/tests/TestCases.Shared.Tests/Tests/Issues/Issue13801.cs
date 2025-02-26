41607fd3292c3018cf2888331afce6e9e0b33437
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2025-02-26 20:05:13 +0530

    Added test cases

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13801.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue13801 : _IssuesUITest
+	{
+		public override string Issue => "Path does not render if it has Margin";
+
+		public Issue13801(TestDevice testDevice) : base(testDevice) { }
+
+		[Test]
+		[Category(UITestCategories.Shape)]
+		public void Issue13801VerifyPathMargin()
+		{
+			App.WaitForElement("Path");
+			VerifyScreenshot();
+		}
+	}
+}
