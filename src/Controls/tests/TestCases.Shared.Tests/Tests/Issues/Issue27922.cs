b5791e582352509d5206bafc773b0ca3ae32f035
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2025-02-27 16:18:59 +0530

    Commit for testcases

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue27922.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue27922 : _IssuesUITest
+{
+	public override string Issue => "[WinUI]CollectionView with GroupHeader enabled should scroll properly with groupheader";
+
+	public Issue27922(TestDevice device) : base(device)
+	{
+	}
+
+	[Test]
+	[Category(UITestCategories.Shell)]
+	public void ItemShouldbeScrolledbasedOnGroupHeader()
+	{
+		App.WaitForElement("27922GroupedCollection");
+		App.WaitForElement("27922ScrollToButton");
+		App.Tap("27922ScrollToButton");
+		App.WaitForElement("Coffee");
+	}
+}
+
