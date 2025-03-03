1126cbc850c0124a5f23fe03d138f6c964d57873
HarishKumarSF4517 <harish.kumar@syncfusion.com>
2025-03-03 18:54:00 +0530

    Increase stability for CI failure Issues

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25889.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25889.cs
@@ -18,7 +18,8 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		{
 			App.WaitForElement("collectionView");
 			App.ScrollDown("collectionView", ScrollStrategy.Gesture, 0.99);
-
+			// In CI, the CommandFired text does not update when Using ScrollDown action from Appium. To ensure the command is triggered, first scroll up and then attempt to scrolling down again.
+            App.ScrollUp("collectionView", ScrollStrategy.Gesture, swipePercentage:0.99);
 			App.WaitForElement("collectionView");
 			App.ScrollDown("collectionView", ScrollStrategy.Gesture, 0.99);
 
