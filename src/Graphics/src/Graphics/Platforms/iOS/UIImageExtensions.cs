88671386a0c0efba706decd85939da32c6f46a63
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-03-04 19:10:36 +0530

    Removed the old approach

--- src/Graphics/src/Graphics/Platforms/iOS/UIImageExtensions.cs
+++ src/Graphics/src/Graphics/Platforms/iOS/UIImageExtensions.cs
@@ -29,21 +29,6 @@ namespace Microsoft.Maui.Graphics.Platform
 
 		public static UIImage ScaleImage(this UIImage target, CGSize size, bool disposeOriginal = false)
 		{
-			if (!OperatingSystem.IsIOSVersionAtLeast(17))
-			{
-				UIGraphics.BeginImageContext(size);
-				target.Draw(new CGRect(CGPoint.Empty, size));
-				var image = UIGraphics.GetImageFromCurrentImageContext();
-				UIGraphics.EndImageContext();
-
-				if (disposeOriginal)
-				{
-					target.Dispose();
-				}
-
-				return image;
-			}
-
 			using (var renderer = new UIGraphicsImageRenderer(target.Size))
 			{
 				var resultImage = renderer.CreateImage((UIGraphicsImageRendererContext imageContext) =>
