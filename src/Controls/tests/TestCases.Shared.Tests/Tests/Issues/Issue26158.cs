12a9cb18a4043b8de10bb132da8ccb8a31a7bf7f
Jakub Florkowski <kubaflo123@gmail.com>
2025-03-04 01:02:37 +0100

    Update Issue26158.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26158.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26158.cs
@@ -1,5 +1,4 @@
-﻿#if ANDROID || IOS
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 namespace Microsoft.Maui.TestCases.Tests.Issues
@@ -20,4 +19,3 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		}
 	}
 }
-#endif
