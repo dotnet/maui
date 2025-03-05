d932b4977bc2e624ed07eee4d06f963b186a9f52
Vignesh-SF3580 <vignesh.nagarajan@syncfusion.com>
2025-03-05 18:59:40 +0530

    added testcases.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28078.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue28078: _IssuesUITest
+	{
+		public Issue28078(TestDevice testDevice) : base(testDevice)
+		{
+		}
+		public override string Issue => "Crash occurs when click on flyout menu";
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void ShellFlyoutMenuCrash()
+		{
+			App.WaitForElement("button");
+			App.Tap("button");
+			App.WaitForElement("label");
+			App.TapShellFlyoutIcon();
+			VerifyScreenshot();
+		}
+	}
+}
