401f741acf92ab2b8c4c6bc6c9dcfa769b60b7de
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-03-05 13:17:26 +0530

    [Windows] Fix for 28153 (The border color of the RadioButton is visible in Windows only)

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue28153.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controls.TestCases.HostApp.Issues;
+
+[Issue(IssueTracker.Github, 28153, "The border color of the RadioButton is visible in Windows only", PlatformAffected.UWP)]
+class Issue28153 : ContentPage
+{
+	public Issue28153()
+	{
+		var stackLayout = new StackLayout();
+		var radioButton = new RadioButton
+		{
+			AutomationId = "RadioButton",
+			Content = "RadioButton",         
+			BorderColor = Colors.Red          
+		};
+		stackLayout.Children.Add(radioButton);
+		Content = stackLayout;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28153.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Tests.Issues
+{
+	internal class Issue28153 : _IssuesUITest
+	{
+		public Issue28153(TestDevice device) : base(device)
+		{
+		}
+		public override string Issue => "The border color of the RadioButton is visible in Windows only";
+		[Test]
+		[Category(UITestCategories.RadioButton)]
+		public void RadioButtonBorderColorWithoutBorderWidth()
+		{
+			App.WaitForElement("RadioButton");
+			VerifyScreenshot();
+		}
+	}
+}
