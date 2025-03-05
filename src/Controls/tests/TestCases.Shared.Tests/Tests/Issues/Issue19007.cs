0ad0e7007a6b2805f39a878adc06862f25932575
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-03-05 10:48:41 +0530

    Added snapshot for winUI and macOS, updated test sample

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19007.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19007.cs
@@ -1,24 +1,21 @@
-using System;
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue19007 : _IssuesUITest
 {
-    public class Issue19007 : _IssuesUITest
+    public Issue19007(TestDevice testDevice) : base(testDevice)
     {
-        public Issue19007(TestDevice testDevice) : base(testDevice)
-        {
-        }
+    }
 
-        public override string Issue => "Incomplete Label Display on macOS and IOS When Padding is Applied";
+    public override string Issue => "Incomplete Label Display on macOS and IOS When Padding is Applied";
 
-        [Test]
-        [Category(UITestCategories.Label)]
-        public void LabelWithPadding()
-        {
-            App.WaitForElement("Label");
-            VerifyScreenshot();
-        }
+    [Test]
+    [Category(UITestCategories.Label)]
+    public void LabelWithPadding()
+    {
+        App.WaitForElement("Label");
+        VerifyScreenshot();
     }
 }
