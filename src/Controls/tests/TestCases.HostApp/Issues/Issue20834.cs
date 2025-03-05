992fa5c04b802d967eb357ecf98aa44c1dacc4aa
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-03-05 10:33:53 +0530

    Fixed andoird canvas drawing outside bounds

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue20834.cs
@@ -0,0 +1,83 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 20834, "[Android] GraphicsView can also be visible outside the canvas", PlatformAffected.Android)]
+	public class Issue20834 : TestContentPage
+	{
+		readonly Issue20834_Drawable drawable = new();
+		float oldX, oldY;
+		StackLayout rootLayout;
+		Grid grid;
+		GraphicsView graphicsView;
+
+		protected override void Init()
+		{
+			rootLayout = new StackLayout();
+			grid = new Grid()
+			{
+				BackgroundColor = Color.FromArgb("#1a2033"),
+				HeightRequest = 300,
+				VerticalOptions = LayoutOptions.Center
+			};
+			graphicsView = new GraphicsView() { AutomationId = "graphicsView" };
+			Label label = new Label()
+			{
+				AutomationId = "DropTarget",
+				Text = "Drop the circle here",
+				HeightRequest = 100,
+				BackgroundColor = Color.FromArgb("#1a2033"),
+				TextColor = Colors.White,
+			};
+
+			graphicsView.Drawable = drawable;
+			graphicsView.StartInteraction += GraphicsView_StartInteraction;
+			graphicsView.DragInteraction += GraphicsView_DragInteraction;
+
+			grid.Add(graphicsView);
+
+			rootLayout.Add(grid);
+			rootLayout.Add(new StackLayout()
+			{
+				BackgroundColor = Colors.CornflowerBlue,
+				WidthRequest = 100,
+				HeightRequest = 100,
+			});
+			rootLayout.Add(label);
+
+			Content = rootLayout;
+
+		}
+
+		void GraphicsView_DragInteraction(object sender, TouchEventArgs e)
+		{
+			var x = e.Touches[0].X;
+			var y = e.Touches[0].Y;
+			x -= oldX;
+			y -= oldY;
+			drawable.X += x;
+			drawable.Y += y;
+			oldX = e.Touches[0].X;
+			oldY = e.Touches[0].Y;
+			graphicsView.Invalidate();
+		}
+
+		void GraphicsView_StartInteraction(object sender, TouchEventArgs e)
+		{
+			oldX = e.Touches[0].X;
+			oldY = e.Touches[0].Y;
+		}
+	}
+}
+
+internal class Issue20834_Drawable : IDrawable
+{
+	public float X { get; set; } = 20f;
+	public float Y { get; set; } = 20f;
+
+	public void Draw(ICanvas canvas, RectF dirtyRect)
+	{
+		canvas.SaveState();
+		canvas.FillColor = Colors.Coral;
+		canvas.FillEllipse(X, Y, 100, 100);
+		canvas.ResetState();
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20834.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue20834 : _IssuesUITest
+{
+	const string graphicsView = "graphicsView";
+	public Issue20834(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "[Android] GraphicsView can also be visible outside the canvas";
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void GraphicsViewShouldNotDrawOutsideCanvasBounds()
+	{
+		App.WaitForElement(graphicsView);
+		App.DragAndDrop(graphicsView, "DropTarget");
+		VerifyScreenshot();
+	}
+}
