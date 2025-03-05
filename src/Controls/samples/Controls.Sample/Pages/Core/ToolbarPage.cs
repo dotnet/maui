6f67a733018471f1be7050463cc2e5ac37fa479b
Gerald Versluis <gerald.versluis@microsoft.com>
2025-03-05 14:15:25 +0100

    Implement iOS secondary ToolbarItem

---
+++ src/Controls/samples/Controls.Sample/Pages/Core/ToolbarPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Pages;
+
+public partial class ToolbarPage
+{
+	public ToolbarPage()
+	{
+		InitializeComponent();
+	}
+
+	void ItemClicked(object sender, EventArgs e)
+	{
+		if (sender is ToolbarItem tbi)
+		{
+			menuLabel.Text = $"You clicked on ToolbarItem: {tbi.Text}";
+		}
+	}
+}
