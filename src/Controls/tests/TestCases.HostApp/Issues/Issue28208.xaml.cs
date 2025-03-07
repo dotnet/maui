82536a69cb12fc137093bea20597bfcdbb20016e
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-03-07 10:50:09 +0530

    Change file namespace scope

--- src/Controls/tests/TestCases.HostApp/Issues/Issue28208.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue28208.xaml.cs
@@ -1,62 +1,60 @@
 ﻿using System.ComponentModel;
 using System.Globalization;
 
-namespace Maui.Controls.Sample.Issues
+namespace Maui.Controls.Sample.Issues;
+[Issue(IssueTracker.Github, 28208, "[Windows] The Slider and Stepper control does not work in One-Way binding mode with a MultiBinding Converter", PlatformAffected.UWP)]
+public partial class Issue28208 : ContentPage
 {
-	[Issue(IssueTracker.Github, 28208, "[Windows] The Slider and Stepper control does not work in One-Way binding mode with a MultiBinding Converter", PlatformAffected.UWP)]
-	public partial class Issue28208 : ContentPage
+	Issue28208ViewModel _vm;
+	public Issue28208()
 	{
-		Issue28208ViewModel _vm;
-		public Issue28208()
-		{
-			InitializeComponent();
-			this.BindingContext = _vm = new Issue28208ViewModel();
-		}
-
-		private void OnCounterClicked(object sender, EventArgs e)
-		{
-			if (_vm is not null)
-			{
-				_vm.Price++;
-			}
-		}
+		InitializeComponent();
+		this.BindingContext = _vm = new Issue28208ViewModel();
 	}
 
-	public class Issue28208ViewModel : INotifyPropertyChanged
+	private void OnCounterClicked(object sender, EventArgs e)
 	{
-		private double _price = 2;
-
-		public double Price
+		if (_vm is not null)
 		{
-			get => _price;
-			set
-			{
-				_price = value;
-				OnPropertyChanged(nameof(Price));
-			}
-		}
-		public event PropertyChangedEventHandler PropertyChanged;
-
-		private void OnPropertyChanged(string propertyName)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			_vm.Price++;
 		}
 	}
+}
 
-	public class Issue28208OneWayMultiBindingValueConverter : IMultiValueConverter
+public class Issue28208ViewModel : INotifyPropertyChanged
+{
+	private double _price = 2;
+
+	public double Price
 	{
-		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		get => _price;
+		set
 		{
-			if (values[0] is double price)
-			{
-				return price;
-			}
+			_price = value;
+			OnPropertyChanged(nameof(Price));
+		}
+	}
+	public event PropertyChangedEventHandler PropertyChanged;
 
-			return 0.0;
-		}
-		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+	private void OnPropertyChanged(string propertyName)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
+
+public class Issue28208OneWayMultiBindingValueConverter : IMultiValueConverter
+{
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (values[0] is double price)
 		{
-			throw new NotImplementedException();
+			return price;
 		}
+
+		return 0.0;
+	}
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
 	}
 }
