239aea83280e28ba65f1d56066f3812ae94ef63b
Bhavanesh N <bhavanesh001@gmail.com>
2025-03-07 08:35:03 +0530

    set CancelsTouchesInView to false

--- src/Controls/src/Core/Platform/GestureManager/GesturePlatformManager.iOS.cs
+++ src/Controls/src/Core/Platform/GestureManager/GesturePlatformManager.iOS.cs
@@ -490,7 +490,8 @@ namespace Microsoft.Maui.Controls.Platform
 			var result = new MauiUITapGestureRecognizer(action)
 			{
 				NumberOfTapsRequired = (uint)tapGesture.NumberOfTapsRequired,
-				ShouldRecognizeSimultaneously = ShouldRecognizeTapsTogether
+				ShouldRecognizeSimultaneously = ShouldRecognizeTapsTogether,
+				CancelsTouchesInView = false
 			};
 
 			// For whatever reason the secondary mask doesn't work on catalyst
