76216d16fd29a35cd7e5b13c9c89f6de91e586ea
Vignesh-SF3580 <vignesh.nagarajan@syncfusion.com>
2025-03-07 17:17:07 +0530

    Updated testcases.

--- src/Controls/tests/TestCases.HostApp/Issues/Issue28078.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue28078.cs
@@ -1,72 +1,71 @@
-﻿namespace Maui.Controls.Sample.Issues
-{
-	[Issue(IssueTracker.Github, 28078, "Crash occurs when click on flyout menu", PlatformAffected.Android)]
-	public class Issue28078 : Shell
-	{
-		public Issue28078() 
-		{
-			Items.Add(new _28078MainPage(this));
-		}
+﻿namespace Maui.Controls.Sample.Issues;
 
-		public void LoggedIn()
-        {
-            Items.Clear();
-            Items.Add(new _28708Page1());
-            Items.Add(new _28708Page2());
-        }
+[Issue(IssueTracker.Github, 28078, "Crash occurs when click on flyout menu", PlatformAffected.Android)]
+public class Issue28078 : Shell
+{
+	public Issue28078()
+	{
+		Items.Add(new _28078MainPage(this));
 	}
 
-	public class _28078MainPage : ContentPage
-    {
-        public _28078MainPage(Issue28078 shell)
-        {
-            Title = "Main Page";
+	public void LoggedIn()
+	{
+		Items.Clear();
+		Items.Add(new _28708Page1());
+		Items.Add(new _28708Page2());
+	}
+}
 
-            var button = new Button 
-			{ 
-    			Text = "Click",
-    			AutomationId = "button"
-			};
-            button.Clicked += (_, _) => 
-            { 
-                shell.LoggedIn();
-            };
+public class _28078MainPage : ContentPage
+{
+	public _28078MainPage(Issue28078 shell)
+	{
+		Title = "Main Page";
 
-            Content = new VerticalStackLayout
-            {
-                Children = { button }
-            };
-        }
-    }
+		var button = new Button
+		{
+			Text = "Click",
+			AutomationId = "button"
+		};
+		button.Clicked += (_, _) =>
+		{
+			shell.LoggedIn();
+		};
 
-	public class _28708Page1 : ContentPage
-    {
-        public _28708Page1()
-        {
-            Title = "Page 1";
-            Content = new VerticalStackLayout
-            {
-                Children = 
-        		{ 
-            		new Label 
-            		{ 
-                		Text = "First Page",
-                		AutomationId = "label"
-            		} 
-        		}
-            };
-        }
-    }
+		Content = new VerticalStackLayout
+		{
+			Children = { button }
+		};
+	}
+}
 
-    public class _28708Page2 : ContentPage
-    {
-        public _28708Page2()
-        {
-            Title = "Page 2";
-            Content = new VerticalStackLayout
-            {
-                Children = { new Label { Text = "Second Page" } }
-            };
-        }
+public class _28708Page1 : ContentPage
+{
+	public _28708Page1()
+	{
+		Title = "Page 1";
+		Content = new VerticalStackLayout
+		{
+			Children =
+				{
+					new Label
+					{
+						Text = "First Page",
+						AutomationId = "label"
+					}
+				}
+		};
+	}
+}
+
+public class _28708Page2 : ContentPage
+{
+	public _28708Page2()
+	{
+		Title = "Page 2";
+		Content = new VerticalStackLayout
+		{
+			Children = { new Label { Text = "Second Page" } }
+		};
 	}
 }
