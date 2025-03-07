786d497c9b65986f7fefc8b5fa23edd8c6d5321e
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-03-07 10:51:03 +0530

    Updated  test file namespace

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28098.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28098.cs
@@ -2,25 +2,22 @@ using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue28098 : _IssuesUITest
 {
-    public class Issue28098 : _IssuesUITest
+    public override string Issue => "Returning back from navigation to MainPage would result in a blank screen";
+    public Issue28098(TestDevice device) : base(device)
     {
-        public override string Issue => "Returning back from navigation to MainPage would result in a blank screen";
-        public Issue28098(TestDevice device) : base(device)
-        {
-        }
+    }
 
-        [Test]
-        [Category(UITestCategories.CarouselView)]
-        [Category(UITestCategories.CollectionView)]
-        public void BlankScreenOnNavigationBack()
-        {
-            App.WaitForElement("Button");
-            App.Tap("Button");
-            App.WaitForElement("BackButton");
-            App.Tap("BackButton");
-            VerifyScreenshot();
-        }
+    [Test]
+    [Category(UITestCategories.Picker)]
+    public void BlankScreenOnNavigationBack()
+    {
+        App.WaitForElement("Button");
+        App.Tap("Button");
+        App.WaitForElement("BackButton");
+        App.Tap("BackButton");
+        VerifyScreenshot();
     }
 }
