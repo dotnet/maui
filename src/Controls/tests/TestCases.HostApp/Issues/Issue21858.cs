334cad12856ec648477b17afe2acf094c2510671
Jakub Florkowski <kubaflo123@gmail.com>
2025-03-09 02:54:57 +0100

    DatePicker not respecting UserAppTheme - fix

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue21858.cs
@@ -0,0 +1,27 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 21858, "DatePicker not respecting UserAppTheme", PlatformAffected.Android)]
+	public class Issue21858 : ContentPage
+	{
+		public Issue21858()
+		{
+			Content = new VerticalStackLayout()
+			{
+				Children =
+				{
+					new DatePicker() {
+						AutomationId = "DatePicker",
+						Format = "D",
+						Date = DateTime.Now
+					},
+					new Button()
+					{
+						Text = "Toggle Theme",
+						AutomationId = "ToggleThemeButton",
+						Command = new Command(() => Application.Current!.UserAppTheme = Application.Current.UserAppTheme == AppTheme.Dark ? AppTheme.Light : AppTheme.Dark)
+					}
+				},
+			};
+		}
+	}
+}
