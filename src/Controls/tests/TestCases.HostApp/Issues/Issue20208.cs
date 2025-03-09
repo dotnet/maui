7a1f9acc958133c1e644ad76068d15af26584710
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-03-09 14:54:59 +0530

    [Windows] Fix for 20208 ( When minimumheightrequest is set on label, the label is not vertically centered in Windows )

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue20208.cs
@@ -0,0 +1,30 @@
+﻿using Maui.Controls.Sample.Issues;
+
+namespace Controls.TestCases.HostApp.Issues;
+[Issue(IssueTracker.Github, 20208, "When minimumheightrequest is set on label, the label is not vertically centered in Windows", PlatformAffected.UWP)]
+public class Issue20208 : TestContentPage
+{
+	public Issue20208()
+	{
+	}
+	protected override void Init()
+	{
+		FlexLayout flexLayout = new FlexLayout
+		{
+			VerticalOptions = LayoutOptions.Center,
+			HorizontalOptions = LayoutOptions.Center,
+			BackgroundColor = Colors.Red,
+			HeightRequest = 50,
+		};
+		Label labelWithMinimumHeight = new Label
+		{
+			AutomationId = "LabelWithMinimumHeight",
+			VerticalOptions = LayoutOptions.Fill,
+			VerticalTextAlignment = TextAlignment.Center,
+			MinimumHeightRequest = 39,
+			Text = "Hello, World!"
+		};
+		flexLayout.Children.Add(labelWithMinimumHeight);
+		Content = flexLayout;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20208.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+internal class Issue20208 : _IssuesUITest
+{
+	public Issue20208(TestDevice device) : base(device) { }
+	public override string Issue => "When minimumheightrequest is set on label, the label is not vertically centered in Windows";
+	[Test]
+	[Category(UITestCategories.Label)]
+	public void LabelWithMinimumHeightVerticallyCentered()
+	{
+		App.WaitForElement("LabelWithMinimumHeight");
+		VerifyScreenshot();
+	}
+}
+
