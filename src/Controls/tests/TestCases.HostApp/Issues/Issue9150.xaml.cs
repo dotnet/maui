a229147465131603365b4552d3f9ec9593b5fd06
Jakub Florkowski <kubaflo123@gmail.com>
2025-03-09 20:32:26 +0100

    Fix #9150

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue9150.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 9150, "Picker Attribute \"SelectedIndex\" Not being respected on page load", PlatformAffected.All)]
+	public partial class Issue9150 : ContentPage
+	{
+		public Issue9150()
+		{
+			InitializeComponent();
+		}
+	}
+}
