caa21683d9644224dadb24be1a18e6a7bb0d7e14
Brayan Khosravian <Brayan@live.at>
2025-03-09 11:42:25 +0100

    -fix-27998: added UI Tests

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue27998.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Maui.Controls.Sample.Issues;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(	IssueTracker.Github,
+		27998,
+		"[Windows] ScrollView is not scrolling to the bottom if in grid with *,auto Width",
+		PlatformAffected.UWP)]
+public partial class Issue27998 : TestContentPage
+{
+	public Issue27998()
+	{
+		InitializeComponent();
+	}
+
+	protected override void Init() { }
+
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue27998.cs
@@ -0,0 +1,39 @@
+﻿//#if WINDOWS
+using NUnit.Framework;
+using OpenQA.Selenium.Internal;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue27998 : _IssuesUITest
+	{
+		private const string _itemsId = "ItemsId";
+		private const string _lastItemId = "LastItemId";
+		private const string _firstItemId = "FirstItemId";
+		private const string _lastItemText = "INSIDE SCROLL 27";
+
+		public Issue27998(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "[Windows] ScrollView is not scrolling to the bottom if in grid with *,auto Width";
+
+		[Test]
+		[Category(UITestCategories.Cells)]
+		[Category(UITestCategories.ScrollView)]
+		[Category(UITestCategories.Border)]
+		public void GridAutosStarsScollToEndDisplaysLastItem()
+		{
+			var firstItem = App.WaitForElement(_firstItemId);
+			var lastItem = App.WaitForElement(_lastItemId);
+			Assert.That(lastItem.IsDisplayed().Equals(false));
+
+			App.ScrollDown(_itemsId);
+			App.WaitForTextToBePresentInElement(_lastItemId, _lastItemText);
+			
+			Assert.That(lastItem.IsDisplayed().Equals(true));
+		}
+	}
+}
+//#endif
