6578c775ecdb40c1289327ffe037bd59782fa45b
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-03-10 15:59:56 +0530

    Update PlatformGraphicsView.cs

--- src/Graphics/src/Graphics/Platforms/Android/PlatformGraphicsView.cs
+++ src/Graphics/src/Graphics/Platforms/Android/PlatformGraphicsView.cs
@@ -79,6 +79,8 @@ namespace Microsoft.Maui.Graphics.Platform
 
 			androidCanvas.Restore();
 			_canvas.Canvas = null;
+
+			androidCanvas.Restore();
 		}
 
 		protected override void OnSizeChanged(int width, int height, int oldWidth, int oldHeight)
