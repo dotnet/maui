57ba6c7af063dcc5aa8c8c7614710dd494ad8d60
Jakub Florkowski <kubaflo123@gmail.com>
2025-03-10 15:29:09 +0100

    TabbedPage in FlyoutPage

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22755.cs
@@ -0,0 +1,24 @@
+﻿#if ANDROID
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue22755 : _IssuesUITest
+	{
+		public Issue22755(TestDevice device) : base(device) { }
+
+		public override string Issue => "[Android] TabbedPage in FlyoutPage in navigationPage does not fit all screen";
+
+		[Test]
+		public void Issue22755Test()
+		{
+			App.WaitForElement("label");
+
+			// The test passes if navigation bar is correctly placed
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
