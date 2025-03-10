5bf2c5689028031d9dc19521c3e0812445f9815c
Rui Marinho <me@ruimarinho.net>
2025-03-10 15:10:42 +0000

    [iOS] Update nullability on iOS api

--- src/Core/src/Platform/iOS/ApplicationExtensions.cs
+++ src/Core/src/Platform/iOS/ApplicationExtensions.cs
@@ -43,7 +43,7 @@ namespace Microsoft.Maui.Platform
 			}
 		}
 
-		public static void CreatePlatformWindow(this IUIApplicationDelegate platformApplication, IApplication application, UIApplication uiApplication, NSDictionary launchOptions)
+		public static void CreatePlatformWindow(this IUIApplicationDelegate platformApplication, IApplication application, UIApplication uiApplication, NSDictionary? launchOptions)
 		{
 			// Find any userinfo/dictionaries we might pass into the activation state
 			var dicts = new List<NSDictionary>();
