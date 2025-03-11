ef8a3f29f9ae1e172df1a92195a54dbd05724eef
SubhikshaSf4851 <subhiksha.c@syncfusion.com>
2025-03-11 14:32:45 +0530

    Updated testcase sample

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue23377.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue23377.cs
@@ -5,18 +5,18 @@ using UITest.Core;
 namespace Microsoft.Maui.TestCases.Tests.Issues;
 public class Issue23377 : _IssuesUITest
 {
-	public override string Issue => "Item Spacing misbehaviour for horizontal list";
-	public Issue23377(TestDevice device) : base(device){ }
+	public Issue23377(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "Horizontal Item spacing in collectionView";
 
 	[Test]
 	[Category(UITestCategories.CollectionView)]
 	public void Issue23377ItemSpacing()
 	{
-		App.WaitForElement("EntryControl");
-		App.EnterText("EntryControl", "100");
-		App.Tap("ChangeItemSpace");
 		App.WaitForElement("ChangeItemSpace");
+		App.Tap("ChangeItemSpace");
 		VerifyScreenshot();
 	}
 }
-
