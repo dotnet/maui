18a7dc3b7b4277c3dedb21275688c49e95b3f4c1
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2025-03-11 15:48:54 +0530

    Added test cases and committed the images

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25558.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue25558 : _IssuesUITest
+	{
+		public override string Issue => "ImageButton dosen't scale Image correctly";
+
+		public Issue25558(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.ImageButton)]
+		public void Issue25558VerifyImageButtonAspects()
+		{
+			App.WaitForElement("imageButton");
+			VerifyScreenshot();
+		}
+	}
+}
