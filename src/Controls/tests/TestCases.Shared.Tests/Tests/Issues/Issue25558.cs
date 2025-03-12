344dd40a7ba775d49074c1c158460df97b9f17ae
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2025-03-12 10:18:54 +0530

    Addressed a review concerns

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25558.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25558.cs
@@ -2,22 +2,21 @@
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue25558 : _IssuesUITest
 {
-	public class Issue25558 : _IssuesUITest
+	public override string Issue => "ImageButton dosen't scale Image correctly";
+
+	public Issue25558(TestDevice device) : base(device)
 	{
-		public override string Issue => "ImageButton dosen't scale Image correctly";
+	}
 
-		public Issue25558(TestDevice device) : base(device)
-		{
-		}
-
-		[Test]
-		[Category(UITestCategories.ImageButton)]
-		public void Issue25558VerifyImageButtonAspects()
-		{
-			App.WaitForElement("imageButton");
-			VerifyScreenshot();
-		}
+	[Test]
+	[Category(UITestCategories.ImageButton)]
+	public void Issue25558VerifyImageButtonAspects()
+	{
+		App.WaitForElement("imageButton");
+		VerifyScreenshot();
 	}
 }
