cdb84e465774a3bd64fd9065e2125741132036b7
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-03-12 19:06:39 +0530

    updated namespace

--- src/Controls/tests/TestCases.HostApp/Issues/Issue20834.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue20834.cs
@@ -1,70 +1,69 @@
-﻿namespace Maui.Controls.Sample.Issues
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 20834, "[Android] GraphicsView can also be visible outside the canvas", PlatformAffected.Android)]
+public class Issue20834 : TestContentPage
 {
-	[Issue(IssueTracker.Github, 20834, "[Android] GraphicsView can also be visible outside the canvas", PlatformAffected.Android)]
-	public class Issue20834 : TestContentPage
+	readonly Issue20834_Drawable drawable = new();
+	float oldX, oldY;
+	StackLayout rootLayout;
+	Grid grid;
+	GraphicsView graphicsView;
+
+	protected override void Init()
 	{
-		readonly Issue20834_Drawable drawable = new();
-		float oldX, oldY;
-		StackLayout rootLayout;
-		Grid grid;
-		GraphicsView graphicsView;
-
-		protected override void Init()
+		rootLayout = new StackLayout();
+		grid = new Grid()
 		{
-			rootLayout = new StackLayout();
-			grid = new Grid()
-			{
-				BackgroundColor = Color.FromArgb("#1a2033"),
-				HeightRequest = 300,
-				VerticalOptions = LayoutOptions.Center
-			};
-			graphicsView = new GraphicsView() { AutomationId = "graphicsView" };
-			Label label = new Label()
-			{
-				AutomationId = "DropTarget",
-				Text = "Drop the circle here",
-				HeightRequest = 100,
-				BackgroundColor = Color.FromArgb("#1a2033"),
-				TextColor = Colors.White,
-			};
-
-			graphicsView.Drawable = drawable;
-			graphicsView.StartInteraction += GraphicsView_StartInteraction;
-			graphicsView.DragInteraction += GraphicsView_DragInteraction;
-
-			grid.Add(graphicsView);
-
-			rootLayout.Add(grid);
-			rootLayout.Add(new BoxView()
-			{
-				BackgroundColor = Colors.CornflowerBlue,
-				WidthRequest = 100,
-				HeightRequest = 100,
-			});
-			rootLayout.Add(label);
-
-			Content = rootLayout;
-
-		}
-
-		void GraphicsView_DragInteraction(object sender, TouchEventArgs e)
+			BackgroundColor = Color.FromArgb("#1a2033"),
+			HeightRequest = 300,
+			VerticalOptions = LayoutOptions.Center
+		};
+		graphicsView = new GraphicsView() { AutomationId = "graphicsView" };
+		Label label = new Label()
 		{
-			var x = e.Touches[0].X;
-			var y = e.Touches[0].Y;
-			x -= oldX;
-			y -= oldY;
-			drawable.X += x;
-			drawable.Y += y;
-			oldX = e.Touches[0].X;
-			oldY = e.Touches[0].Y;
-			graphicsView.Invalidate();
-		}
+			AutomationId = "DropTarget",
+			Text = "Drop the circle here",
+			HeightRequest = 100,
+			BackgroundColor = Color.FromArgb("#1a2033"),
+			TextColor = Colors.White,
+		};
 
-		void GraphicsView_StartInteraction(object sender, TouchEventArgs e)
+		graphicsView.Drawable = drawable;
+		graphicsView.StartInteraction += GraphicsView_StartInteraction;
+		graphicsView.DragInteraction += GraphicsView_DragInteraction;
+
+		grid.Add(graphicsView);
+
+		rootLayout.Add(grid);
+		rootLayout.Add(new BoxView()
 		{
-			oldX = e.Touches[0].X;
-			oldY = e.Touches[0].Y;
-		}
+			BackgroundColor = Colors.CornflowerBlue,
+			WidthRequest = 100,
+			HeightRequest = 100,
+		});
+		rootLayout.Add(label);
+
+		Content = rootLayout;
+
+	}
+
+	void GraphicsView_DragInteraction(object sender, TouchEventArgs e)
+	{
+		var x = e.Touches[0].X;
+		var y = e.Touches[0].Y;
+		x -= oldX;
+		y -= oldY;
+		drawable.X += x;
+		drawable.Y += y;
+		oldX = e.Touches[0].X;
+		oldY = e.Touches[0].Y;
+		graphicsView.Invalidate();
+	}
+
+	void GraphicsView_StartInteraction(object sender, TouchEventArgs e)
+	{
+		oldX = e.Touches[0].X;
+		oldY = e.Touches[0].Y;
 	}
 }
 
