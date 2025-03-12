60d1c244cae302f14c5d59082a371dadcba53113
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2025-03-12 17:35:54 +0530

    Commited the image and modified the test case

--- src/Controls/tests/TestCases.HostApp/Issues/Issue25558.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25558.xaml.cs
@@ -1,7 +1,6 @@
 namespace Maui.Controls.Sample.Issues;
 
-[XamlCompilation(XamlCompilationOptions.Compile)]
-[Issue(IssueTracker.Github, 25558, "ImageButton dosen't scale Image correctly", PlatformAffected.Android)]
+[Issue(IssueTracker.Github, 25558, "ImageButton does not scale Image correctly", PlatformAffected.Android)]
 public partial class Issue25558 : ContentPage
 {
 	public Issue25558()
