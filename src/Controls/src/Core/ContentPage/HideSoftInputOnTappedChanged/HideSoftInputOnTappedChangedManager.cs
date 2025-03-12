1f95508e5b312dfa39f037b4738645cca74ea0af
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2025-03-12 21:02:16 +0530

    fix for 26792

--- src/Controls/src/Core/ContentPage/HideSoftInputOnTappedChanged/HideSoftInputOnTappedChangedManager.cs
+++ src/Controls/src/Core/ContentPage/HideSoftInputOnTappedChanged/HideSoftInputOnTappedChangedManager.cs
@@ -13,7 +13,7 @@ namespace Microsoft.Maui.Controls
 			{
 				foreach (var page in _contentPages)
 				{
-					if (page.HideSoftInputOnTapped && page.HasNavigatedTo)
+					if (page.HideSoftInputOnTapped && (page.HasNavigatedTo || (page is ContentPage && _contentPages.Count == 1)))
 						return true;
 				}
 				return false;
