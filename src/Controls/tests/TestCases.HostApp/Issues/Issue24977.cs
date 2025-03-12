dae3894a039fb832cfc0bab62078f111708332d7
Matthew Leibowitz <mattleibow@live.com>
2025-03-12 19:41:46 +0800

    Added a button for Start

--- src/Controls/tests/TestCases.HostApp/Issues/Issue24977.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue24977.cs
@@ -5,37 +5,47 @@ public class Issue24977 : TestNavigationPage
 {
 	protected override void Init()
 	{
-		var root = CreateRootContentPage();
-		PushAsync(root);
+		PushAsync(new ContentPage
+		{
+			Content = new VerticalStackLayout
+			{
+				Spacing = 12,
+				Padding = new Thickness(12, 24),
+				Children =
+				{
+					CreateButton(TextAlignment.Start),
+					CreateButton(TextAlignment.Center),
+					CreateButton(TextAlignment.End),
+				}
+			}
+		});
 	}
 
-	ContentPage CreateRootContentPage()
+	Button CreateButton(TextAlignment textAlignment)
 	{
-		var ContentPage = new ContentPage();
-		VerticalStackLayout rootLayout = new VerticalStackLayout
+		var btn = new Button
 		{
-			Spacing = 10,
-			Padding = new Thickness(10),
+			Text = textAlignment.ToString(),
+			AutomationId = $"{textAlignment}Button"
 		};
-
-		Button centerButton = new Button() { Text = "Center", AutomationId = "CenterButton" };
-		centerButton.Clicked += (s, e) => Navigation.PushAsync(new Issue24977_1());
-		Button endButton = new Button() { Text = "End", AutomationId = "EndButton" };
-		endButton.Clicked += (s, e) => Navigation.PushAsync(new Issue24977_2());
-		rootLayout.Add(centerButton);
-		rootLayout.Add(endButton);
-		ContentPage.Content = rootLayout;
-		return ContentPage;
+		btn.Clicked += (s, e) => Navigation.PushAsync(new Issue24977_1(textAlignment));
+		return btn;
 	}
 }
 
 public class Issue24977_1 : TestContentPage
 {
-	Label CursorHeightTracker;
-	internal Editor editor;
+	Editor editor;
+	Label cursorHeightTracker;
+
+	public Issue24977_1(TextAlignment textAlignment)
+	{
+		editor.VerticalTextAlignment = textAlignment;
+	}
+
 	protected override void Init()
 	{
-		Grid rootGrid = new Grid
+		var rootGrid = new Grid
 		{
 			RowDefinitions =
 			{
@@ -47,7 +57,7 @@ public class Issue24977_1 : TestContentPage
 			Margin = new Thickness(30)
 		};
 
-		Entry entry = new Entry()
+		var entry = new Entry
 		{
 			Text = "Content before",
 			AutomationId = "EntryBefore",
@@ -55,31 +65,29 @@ public class Issue24977_1 : TestContentPage
 			BackgroundColor = Colors.Aquamarine,
 		};
 
-		CursorHeightTracker = new Label()
+		cursorHeightTracker = new Label
 		{
 			Text = "0",
 			AutomationId = "CursorHeightTracker",
 			BackgroundColor = Colors.Aquamarine,
 		};
 
-		editor = new Editor()
+		editor = new Editor
 		{
 			Text = "Hello World!",
 			AutomationId = "IssueEditor",
 			BackgroundColor = Colors.Orange,
-			VerticalTextAlignment = TextAlignment.Center,
 		};
 		editor.TextChanged += Editor_TextChanged;
 
-		Button button = new Button() { Text = "Erase" };
+		var button = new Button { Text = "Erase" };
 		button.Clicked += (s, e) => editor.Text = string.Empty;
 
 		rootGrid.Add(entry, 0, 0);
-		rootGrid.Add(CursorHeightTracker, 0, 1);
+		rootGrid.Add(cursorHeightTracker, 0, 1);
 		rootGrid.Add(editor, 0, 2);
 
 		rootGrid.Add(button, 0, 3);
-
 
 		Content = rootGrid;
 	}
@@ -105,44 +113,34 @@ public class Issue24977_1 : TestContentPage
 			var cursorInContainer = container.ConvertRectFromView(local, textInput);
 			var cursorInWindow = container.ConvertRectToView(cursorInContainer, null);
 
-			CursorHeightTracker.Text = cursorInWindow.Y.ToString();
-
+			cursorHeightTracker.Text = cursorInWindow.Y.ToString();
 		}
 
-	}
-
-	UIKit.UIView GetContainerView(UIKit.UIView startingPoint)
-	{
-		var rootView = FindResponder<Microsoft.Maui.Platform.ContainerViewController>(startingPoint)?.View;
-
-		if (rootView is not null)
+		UIKit.UIView GetContainerView(UIKit.UIView startingPoint)
 		{
-			return rootView;
+			var rootView = FindResponder<Microsoft.Maui.Platform.ContainerViewController>(startingPoint)?.View;
+
+			if (rootView is not null)
+			{
+				return rootView;
+			}
+
+			return null;
 		}
 
-		return null;
-	}
-
-	T FindResponder<T>(UIKit.UIView view) where T : UIKit.UIResponder
-	{
-		var nextResponder = view as UIKit.UIResponder;
-		while (nextResponder is not null)
+		T FindResponder<T>(UIKit.UIView view)
+			where T : UIKit.UIResponder
 		{
-			nextResponder = nextResponder.NextResponder;
+			var nextResponder = view as UIKit.UIResponder;
+			while (nextResponder is not null)
+			{
+				nextResponder = nextResponder.NextResponder;
 
-			if (nextResponder is T responder)
-				return responder;
+				if (nextResponder is T responder)
+					return responder;
+			}
+			return null;
 		}
-		return null;
 #endif
-	}
-}
-
-public class Issue24977_2 : Issue24977_1
-{
-	protected override void Init()
-	{
-		base.Init();
-		editor.VerticalTextAlignment = TextAlignment.End;
 	}
 }
