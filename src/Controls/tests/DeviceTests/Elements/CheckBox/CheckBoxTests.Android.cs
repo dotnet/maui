ec16fc8d01a115329b5627d72b8503fa8cd280d0
nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>
2025-03-14 10:35:54 +0530

    Enabled the translation property for device tests

--- src/Controls/tests/DeviceTests/Elements/CheckBox/CheckBoxTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/CheckBox/CheckBoxTests.Android.cs
@@ -5,6 +5,7 @@ using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Handlers;
 using Xunit;
+using System.ComponentModel;
 
 namespace Microsoft.Maui.DeviceTests
 {
@@ -22,13 +23,31 @@ namespace Microsoft.Maui.DeviceTests
 				return nativeView.Alpha;
 			});
 		}
-		
-		Task<bool> GetPlatformIsVisible(CheckBoxHandler checkBoxHandler)
+
+		//src/Compatibility/Core/tests/Android/TranslationTests.cs
+		[Fact]
+		[Description("The Translation property of a CheckBox should match with native Translation")]
+		public async Task CheckBoxTranslationConsistent()
 		{
-			return InvokeOnMainThreadAsync(() =>
+			var checkBox = new CheckBox()
 			{
-				var nativeView = GetNativeCheckBox(checkBoxHandler);
-				return nativeView.Visibility == Android.Views.ViewStates.Visible;
+				TranslationX = 50,
+				TranslationY = -20
+			};
+
+			var handler = await CreateHandlerAsync<CheckBoxHandler>(checkBox);
+			var nativeView = GetNativeCheckBox(handler);
+			await InvokeOnMainThreadAsync(() =>
+			{
+				var translation = nativeView.TranslationX;
+				var density = Microsoft.Maui.Devices.DeviceDisplay.Current.MainDisplayInfo.Density;
+				var expectedInPixels = density * checkBox.TranslationX;
+
+				Assert.Equal(expectedInPixels, translation, 1.0);
+
+				var translationY = nativeView.TranslationY;
+				var expectedYInPixels = density * checkBox.TranslationY;
+				Assert.Equal(expectedYInPixels, translationY, 1.0);
 			});
 		}
 	}
--- src/Controls/tests/DeviceTests/Elements/SearchBar/SearchBarTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/SearchBar/SearchBarTests.Android.cs
@@ -1,9 +1,12 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Android.Widget;
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Platform;
 using SearchView = AndroidX.AppCompat.Widget.SearchView;
+using Xunit;
+using System.ComponentModel;
 
 namespace Microsoft.Maui.DeviceTests
 {
@@ -40,12 +43,31 @@ namespace Microsoft.Maui.DeviceTests
 			});
 		}
 
-		Task<bool> GetPlatformIsVisible(SearchBarHandler searchBarHandler)
+		//src/Compatibility/Core/tests/Android/TranslationTests.cs
+		[Fact]
+		[Description("The Translation property of a SearchBar should match with native Translation")]
+		public async Task SearchBarTranslationConsistent()
 		{
-			return InvokeOnMainThreadAsync(() =>
+			var searchBar = new SearchBar()
 			{
-				var nativeView = GetPlatformControl(searchBarHandler);
-				return nativeView.Visibility == Android.Views.ViewStates.Visible;
+				Text = "SearchBar Test",
+				TranslationX = 50,
+				TranslationY = -20
+			};
+
+			var handler = await CreateHandlerAsync<SearchBarHandler>(searchBar);
+			var nativeView = GetPlatformControl(handler);
+			await InvokeOnMainThreadAsync(() =>
+			{
+				var translation = nativeView.TranslationX;
+				var density = Microsoft.Maui.Devices.DeviceDisplay.Current.MainDisplayInfo.Density;
+				var expectedInPixels = density * searchBar.TranslationX;
+
+				Assert.Equal(expectedInPixels, translation, 1.0);
+
+				var translationY = nativeView.TranslationY;
+				var expectedYInPixels = density * searchBar.TranslationY;
+				Assert.Equal(expectedYInPixels, translationY, 1.0);
 			});
 		}
 	}
