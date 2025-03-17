a6ad49840de70a9616cb4c76fd704081bdf973de
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-03-17 12:15:28 +0530

    [Windows] Fix for 15806 ( RadioButton Border color not working for focused visual state )

--- src/Controls/src/Core/RadioButton/RadioButton.cs
+++ src/Controls/src/Core/RadioButton/RadioButton.cs
@@ -1,6 +1,7 @@
 #nullable disable
 using System;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Logging;
 using Microsoft.Maui.Controls.Internals;
 using Microsoft.Maui.Controls.Shapes;
@@ -276,7 +277,7 @@ namespace Microsoft.Maui.Controls
 		{
 		}
 
-		protected override void OnPropertyChanged(string propertyName = null)
+		protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
 		{
 			base.OnPropertyChanged(propertyName);
 			if (propertyName == BorderColorProperty.PropertyName)
@@ -614,7 +615,7 @@ namespace Microsoft.Maui.Controls
 		Font ITextStyle.Font => this.ToFont();
 
 #if ANDROID
-		object IContentView.Content 
+		object IContentView.Content
 		{
 			get
 			{
