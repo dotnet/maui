fc2f90c6ea0fb9143ccba51adadfdda205e8908d
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-03-18 19:12:41 +0530

    alignment updated

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28440.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28440.cs
@@ -2,30 +2,28 @@
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue28440 : _IssuesUITest
 {
-	public class Issue28440 : _IssuesUITest
+	public override string Issue => "FlyoutPage IsPresented not updated properly in windows";
+
+	public Issue28440(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.FlyoutPage)]
+	public void CheckFlyoutPageIsPresentedState()
 	{
-		public override string Issue => "FlyoutPage IsPresented not updated properly in windows";
-
-		public Issue28440(TestDevice device)
-		: base(device)
-		{ }
-
-		[Test]
-		[Category(UITestCategories.FlyoutPage)]
-		public void CheckFlyoutPageIsPresentedState()
-		{
-			var buttonElement = App.WaitForElement("Button");
-			var buttonRect = buttonElement.GetRect();
-			App.Tap("Button");
-			App.WaitForElement("flyoutMenu");
-			float tapX = buttonRect.X + buttonRect.Width - 10;
-			float tapY = buttonRect.CenterY();
-			App.TapCoordinates(tapX, tapY);
-			App.WaitForElement("Button");
-			App.Tap("Button");
-			App.WaitForElement("flyoutMenu");
-		}
+		var buttonElement = App.WaitForElement("Button");
+		var buttonRect = buttonElement.GetRect();
+		App.Tap("Button");
+		App.WaitForElement("flyoutMenu");
+		float tapX = buttonRect.X + buttonRect.Width - 10;
+		float tapY = buttonRect.CenterY();
+		App.TapCoordinates(tapX, tapY);
+		App.WaitForElement("Button");
+		App.Tap("Button");
+		App.WaitForElement("flyoutMenu");
 	}
 }
