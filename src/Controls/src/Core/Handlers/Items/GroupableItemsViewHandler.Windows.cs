ecd64bb7c4aab039736e3e4049c968a9a2f1f06e
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-03-18 19:43:04 +0530

    Fixed the EmptyView and EmptyViewTemplate issues in CollectionView

--- src/Controls/src/Core/Handlers/Items/GroupableItemsViewHandler.Windows.cs
+++ src/Controls/src/Core/Handlers/Items/GroupableItemsViewHandler.Windows.cs
@@ -18,14 +18,12 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 
 		protected override CollectionViewSource CreateCollectionViewSource()
 		{
-			if (ItemsView != null && ItemsView.IsGrouped)
+			var itemsSource = Element.ItemsSource;
+			if (ItemsView is not null && ItemsView.IsGrouped && itemsSource is not null)
 			{
-				var itemTemplate = Element.ItemTemplate;
-				var itemsSource = Element.ItemsSource;
-
 				return new CollectionViewSource
 				{
-					Source = TemplatedItemSourceFactory.CreateGrouped(itemsSource, itemTemplate,
+					Source = TemplatedItemSourceFactory.CreateGrouped(itemsSource, Element.ItemTemplate,
 					ItemsView.GroupHeaderTemplate, ItemsView.GroupFooterTemplate, Element, mauiContext: MauiContext),
 					IsSourceGrouped = true,
 					ItemsPath = new Microsoft.UI.Xaml.PropertyPath(nameof(GroupTemplateContext.Items))
