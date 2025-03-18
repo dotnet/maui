7002874421d86fca31c6ec046c69f4165fb15486
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-03-18 13:26:36 +0530

    Fixed BoxView placement inside Border

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue19668.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 19668, "BoxView Placement inside Border.", PlatformAffected.UWP)]
+
+public class Issue19668 : ContentPage
+{
+	public Issue19668()
+	{
+		var horizontalStackLayout = new HorizontalStackLayout
+		{
+			Spacing = 25,
+			HorizontalOptions = LayoutOptions.Center
+		};
+
+		var rightBorder = new Border
+		{
+			WidthRequest = 55,
+			HeightRequest = 35,
+			StrokeThickness = 3,
+			Background = Colors.Green,
+			StrokeShape = new RoundRectangle
+			{
+				CornerRadius = 6
+			}
+		};
+
+		horizontalStackLayout.Add(rightBorder);
+
+		var plusLabel = new Label
+		{
+			Text = "+",
+			VerticalOptions = LayoutOptions.Center
+		};
+		horizontalStackLayout.Add(plusLabel);
+
+		var boxView = new BoxView
+		{
+			WidthRequest = 55,
+			HeightRequest = 35,
+			Color = Colors.LightBlue,
+			CornerRadius = 6,
+		};
+		horizontalStackLayout.Add(boxView);
+
+		var equalLabel = new Label
+		{
+			Text = "=",
+			VerticalOptions = LayoutOptions.Center
+		};
+		horizontalStackLayout.Add(equalLabel);
+
+		var parentBorder = new Border
+		{
+			WidthRequest = 55,
+			HeightRequest = 35,
+			StrokeThickness = 3,
+			Background = Colors.Green,
+			AutomationId = "Border",
+			StrokeShape = new RoundRectangle
+			{
+				CornerRadius = 6
+			}
+		};
+
+		// BoxView inside the Border
+		var innerBoxView = new BoxView
+		{
+			WidthRequest = 55,
+			HeightRequest = 35,
+			Color = Colors.LightBlue,
+			CornerRadius = 6,
+			BackgroundColor = Colors.Transparent
+		};
+
+		parentBorder.Content = innerBoxView;
+		horizontalStackLayout.Add(parentBorder);
+
+		Content = horizontalStackLayout;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19668.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue19668:_IssuesUITest
+{
+	public Issue19668(TestDevice testDevice) : base(testDevice)
+	{
+	}
+	public override string Issue => "BoxView Placement inside Border.";
+
+	[Test]
+	[Category(UITestCategories.Border)]
+	public void BoxViewPlacementInsideBorder()
+	{
+		App.WaitForElement("Border");
+		VerifyScreenshot();
+	}
+}
