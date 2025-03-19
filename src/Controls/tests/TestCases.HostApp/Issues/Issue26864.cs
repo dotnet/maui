5a6c1954e9b8cd71913e14e425276dfa2e180f19
KarthikRajaKalaimani <92777139+KarthikRajaKalaimani@users.noreply.github.com>
2025-03-19 11:41:01 +0530

    Moved to file scope namespace

--- src/Controls/tests/TestCases.HostApp/Issues/Issue26864.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue26864.cs
@@ -1,41 +1,39 @@
-namespace Maui.Controls.Sample.Issues
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 26864, "Shell Content Title Not Rendering in Full-Screen Mode on Mac Catalyst", PlatformAffected.macOS)]
+public class Issue26864 : Shell
 {
-
-	[Issue(IssueTracker.Github, 26864, "Shell Content Title Not Rendering in Full-Screen Mode on Mac Catalyst", PlatformAffected.macOS)]
-	public class Issue26864 : Shell
+	public Issue26864()
 	{
-		public Issue26864()
+		var tabBar = new TabBar
 		{
-			var tabBar = new TabBar
+			AutomationId = "TabBar"
+		};
+		var tab = new Tab
+		{
+			Title = "Nested Tabs",
+			AutomationId = "tabbar"
+		};
+		var homeShellContent = new ShellContent
+		{
+			Title = "Home",
+			Content = new ContentPage
 			{
-				AutomationId = "TabBar"
-			};
-			var tab = new Tab
+				BackgroundColor = Microsoft.Maui.Graphics.Colors.White
+			}
+		};
+		var settingsShellContent = new ShellContent
+		{
+			Title = "Settings",
+			Content = new ContentPage
 			{
-				Title = "Nested Tabs",
-				AutomationId = "tabbar"
-			};
-			var homeShellContent = new ShellContent
-			{
-				Title = "Home",
-				Content = new ContentPage
-				{
-					BackgroundColor = Microsoft.Maui.Graphics.Colors.White
-				}
-			};
-			var settingsShellContent = new ShellContent
-			{
-				Title = "Settings",
-				Content = new ContentPage
-				{
-					BackgroundColor = Microsoft.Maui.Graphics.Colors.White
-				}
-			};
-			tab.Items.Add(homeShellContent);
-			tab.Items.Add(settingsShellContent);
-			tabBar.Items.Add(tab);
-			this.Items.Add(tabBar);
-		}
-
+				BackgroundColor = Microsoft.Maui.Graphics.Colors.White
+			}
+		};
+		tab.Items.Add(homeShellContent);
+		tab.Items.Add(settingsShellContent);
+		tabBar.Items.Add(tab);
+		this.Items.Add(tabBar);
 	}
+
 }
