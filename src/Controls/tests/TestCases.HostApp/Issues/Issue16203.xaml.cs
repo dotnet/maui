8d22812d9d49f1f76924e8c88f0487b1dd9fc517
Bhavanesh N <bhavanesh001@gmail.com>
2025-03-19 21:00:37 +0530

    lot of tests

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue16203.xaml.cs
@@ -0,0 +1,20 @@
+namespace Controls.TestCases.HostApp.Issues;
+
+[Issue(IssueTracker.Github, 16203, "SwipeGestureRecognizer does not raise Swiped event", PlatformAffected.Android)]
+public partial class Issue16203 : ContentPage
+{
+	public Issue16203()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		clickedLabel.Text = "Clicked";
+	}
+
+	private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+	{
+		swipedLabel.Text = "Swiped";
+	}
+}
