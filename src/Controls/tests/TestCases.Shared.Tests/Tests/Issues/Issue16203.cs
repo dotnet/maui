e918d1dc8bca9fcbc6473b2c1c4471322d53965c
Bhavanesh N <bhavanesh001@gmail.com>
2025-03-19 21:00:37 +0530

    lot of tests

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16203.cs
@@ -0,0 +1,30 @@
+using System;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Tests.Issues;
+
+public class Issue16203 : _IssuesUITest
+{
+	public Issue16203(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "SwipeGestureRecognizer does not raise Swiped event";
+
+    [Test]
+	[Category(UITestCategories.Button)]
+	public void ButtonWithSwipeGestureShouldWork()
+	{
+		App.WaitForElement("MauiButton");
+		App.Tap("MauiButton");
+		App.SwipeRightToLeft("MauiButton");
+
+        var text1 = App.WaitForElement("ClickedLabel").GetText();
+        var text2 = App.WaitForElement("SwipedLabel").GetText();
+
+        Assert.That(text1,Is.EqualTo("Clicked"));
+        Assert.That(text2,Is.EqualTo("Swiped"));
+	}
+}
