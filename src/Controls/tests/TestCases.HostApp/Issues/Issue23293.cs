1e29bd95ddd141607f9c4f946c905fe23b0b3466
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-03-20 16:23:06 +0530

    [Windows] Fix for 23293 ( 'Grouping for Vertical list without DataTemplates' page loading exception ).

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue23293.cs
@@ -0,0 +1,89 @@
+﻿namespace Controls.TestCases.HostApp.Issues;
+
+[Issue(IssueTracker.Github, 23293, "'Grouping for Vertical list without DataTemplates' page loading exception", PlatformAffected.UWP)]
+class Issue23293 : ContentPage
+{
+	public List<AnimalGroup> Animals { get; private set; } = new List<AnimalGroup>();
+	public Issue23293()
+	{
+		Animals.Add(new AnimalGroup("Bears", new List<Animal>
+		{
+			new Animal { Name = "American Black Bear" },	
+			new Animal { Name = "Asian Black Bear" },
+			new Animal { Name = "Brown Bear" },
+			new Animal { Name = "Grizzly-Polar Bear Hybrid" },
+			new Animal { Name = "Sloth Bear" },
+		}));
+
+		Animals.Add(new AnimalGroup("Cats", new List<Animal>
+		{
+			new Animal { Name = "Abyssinian" },
+			new Animal { Name = "Arabian Mau" },
+			new Animal { Name = "Bengal" },
+			new Animal { Name = "Burmese" },
+			new Animal { Name = "Cyprus" },
+		}));
+
+		Animals.Add(new AnimalGroup("Dogs", new List<Animal>
+		{
+			new Animal { Name = "Afghan Hound" },
+			new Animal { Name = "Alpine Dachsbracke" },
+			new Animal { Name = "American Bulldog" },
+			new Animal { Name = "Bearded Collie" },
+			new Animal { Name = "Boston Terrier" },
+		}));
+
+		Animals.Add(new AnimalGroup("Elephants", new List<Animal>
+		{
+			new Animal { Name = "African Bush Elephant" },
+			new Animal { Name = "African Forest Elephant" },
+			new Animal { Name = "Desert Elephant" },
+			new Animal { Name = "Borneo Elephant" },
+			new Animal { Name = "Indian Elephant" },
+		}));
+
+		Animals.Add(new AnimalGroup("Monkeys", new List<Animal>
+		{
+			new Animal { Name = "Baboon" },
+			new Animal { Name = "Capuchin Monkey" },
+			new Animal { Name = "Blue Monkey" },
+			new Animal { Name = "Squirrel Monkey" },
+			new Animal { Name = "Golden Lion Tamarin" },
+		}));
+	
+		var collectionViewWithoutDataTemplate = new CollectionView
+		{
+			AutomationId = "CollectionViewWithoutDataTemplate",
+			ItemsSource = Animals,
+			IsGrouped = true,
+		};
+
+		var grid = new Grid();
+		grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+		grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+
+		grid.Add(collectionViewWithoutDataTemplate, 0, 1);
+		
+		Content = grid;
+	}
+}
+
+public class AnimalGroup : List<Animal>
+{
+	public string Name { get; }
+
+	public AnimalGroup(string name, List<Animal> animals) : base(animals)
+	{
+		Name = name;
+	}
+}
+
+public class Animal
+{
+	public string Name { get; set; }
+
+	public override string ToString()
+	{
+		return Name;  
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue23293.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+class Issue23293 : _IssuesUITest
+{
+	public Issue23293(TestDevice device) : base(device) { }
+
+	public override string Issue => "'Grouping for Vertical list without DataTemplates' page loading exception";
+
+	[Test]
+	[Category(UITestCategories.CollectionView)]
+	public void GroupedCollectionViewWithoutDataTemplate()
+	{
+		App.WaitForElement("CollectionViewWithoutDataTemplate");
+		VerifyScreenshot();
+	}
+}
+
