d606713ad9de89c06024fc4bc3a8dea1b5f313b6
Vignesh-SF3580 <vignesh.nagarajan@syncfusion.com>
2025-03-20 19:49:52 +0530

    added testcases.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue28201.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 28201, "TiteView disposed when Page is disposed", PlatformAffected.iOS)]
+public class Issue28201 : NavigationPage
+{
+	public Issue28201()
+	{
+		Navigation.PushAsync(new Issue28201MainPage());
+	}
+}
+
+public partial class Issue28201MainPage : TestContentPage
+{
+	public static bool isPageDestroyed = false;
+	public Issue28201MainPage()
+	{
+		InitializeComponent();
+	}
+
+	protected override void Init()
+	{
+
+	}
+
+	private void OnPushClicked(object sender, EventArgs e)
+	{
+		isPageDestroyed = false;
+		Navigation.PushAsync(new Issue28201MainPage(), true);
+	}
+
+	private void OnPopToRootClicked(object sender, EventArgs e)
+	{
+		Navigation.PopToRootAsync(true);
+		GC.Collect();
+	}
+
+	private void OnCheckStatusClicked(object sender, EventArgs e)
+	{
+		StatusLabel.Text = isPageDestroyed ? "Page Destroyed" : "Page Active";
+	}
+
+	~Issue28201MainPage()
+	{
+		isPageDestroyed = true;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28201.cs
@@ -0,0 +1,31 @@
+﻿#if IOS || MACCATALYST
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue28201 : _IssuesUITest
+{
+	public Issue28201(TestDevice testDevice) : base(testDevice)
+	{
+	}
+	public override string Issue => "TiteView disposed when Page is disposed";
+
+	[Test]
+	[Category(UITestCategories.TitleView)]
+	public void TitleViewDisposed()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			App.WaitForElement("pushButton");
+			App.Tap("pushButton");
+			App.WaitForElement("popButton");
+			App.Tap("popButton");
+		}
+		var label = App.WaitForElement("label");
+		App.Tap("checkStatusButton");
+		Assert.That(label.GetText(), Is.EqualTo("Page Destroyed"));
+	}
+}
+#endif
