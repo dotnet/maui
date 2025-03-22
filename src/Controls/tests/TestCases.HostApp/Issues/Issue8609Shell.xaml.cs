0179e29719aef2e4dc599757703220bc350a7968
Jakub Florkowski <kubaflo123@gmail.com>
2025-03-22 17:04:23 +0100

    Added a UITest

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue8609Shell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.Controls.Sample.Issues;
+[Issue(IssueTracker.Github, "8609Shell", "Shell.AutoResizeTabBarIcons", PlatformAffected.iOS)]
+public partial class Issue8609Shell : Shell
+{
+	public Issue8609Shell()
+	{
+		InitializeComponent();
+	}
+}
