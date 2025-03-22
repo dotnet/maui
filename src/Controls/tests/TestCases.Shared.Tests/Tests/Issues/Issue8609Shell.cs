dae2432a20e0d3469f83448e004d8afd3ca82237
Jakub Florkowski <kubaflo123@gmail.com>
2025-03-22 17:04:23 +0100

    Added a UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue8609Shell.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue8609Shell : _IssuesUITest
+	{
+		public Issue8609Shell(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Shell.AutoResizeTabBarIcons";
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void TabBarIconsShouldAutoscale()
+		{
+			App.WaitForElement("Tab1");
+			VerifyScreenshot();
+		}
+	}
+}
