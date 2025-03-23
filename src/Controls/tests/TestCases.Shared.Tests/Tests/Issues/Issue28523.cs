43dd64fba542138185e6647ce57714a1f0bc1392
Jakub Florkowski <42434498+kubaflo@users.noreply.github.com>
2025-03-24 00:56:25 +0100

    [iOS] Changing carousel view orientation with disabled loop - fix (#28545)
    * [iOS] Changing carousel view orientation with disabled loop - fix
    
    * Added snapshots

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28523.cs
@@ -0,0 +1,25 @@
+﻿#if ANDROID || IOS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue28523 : _IssuesUITest
+{
+	public Issue28523(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Different behavior on iOS and Android when Loop = False";
+
+	[Test]
+	[Category(UITestCategories.CarouselView)]
+	public void CarouselViewItemShouldScaleProperly()
+	{
+		App.WaitForElement("Baboon");
+		App.SetOrientationLandscape();
+		App.WaitForElement("Baboon");
+		VerifyScreenshot();
+	}
+}
+#endif
