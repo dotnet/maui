52afafb91d328053ad28d092b9e5ed43ca026d66
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-03-25 20:12:18 +0530

    Fixed Test case failure in PR 28560 (#28584)
    * Resaved Image For RadioButtonUpdateValueInserBorder
    
    * updated test case code
    
    ---------
    
    Co-authored-by: praveenkumarkarunanithi <praveenkumar.karunanithi@syncfusion.com>

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19109.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19109.cs
@@ -1,3 +1,4 @@
+#if IOS
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
@@ -17,7 +18,9 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement("EntryControl");
 			App.WaitForElement("FocusButton");
 			App.Tap("FocusButton");
-			VerifyScreenshot();
+			var rangeText = App.WaitForElement("RangeLabel").GetText();
+			Assert.That(rangeText, Is.EqualTo("Start=5, End=5"), "Cursor position not applied correctly");
 		}
 	}
 }
+#endif
