9b86fd3cc2d2c355306f52d4e0beb21417095cc5
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-03-25 14:58:23 +0530

    SearchHandler ClearPlaceholderIcon color fix

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20250.cs
@@ -0,0 +1,25 @@
+﻿#if !WINDOWS // It seems the ClearPlaceholderIcon is not supported on Windows
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue20250 : _IssuesUITest
+	{
+		public override string Issue => "[iOS] SearchHandler ClearPlaceholderIcon color";
+
+		public Issue20250(TestDevice device)
+		: base(device)
+		{ }
+
+		[Test]
+		[Category(UITestCategories.ActivityIndicator)]
+		public void VerifySearchHandlerClearPlaceholderIconColor()
+		{
+			App.WaitForElement("label");
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
