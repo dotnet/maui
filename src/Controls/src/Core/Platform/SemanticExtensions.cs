6d9f980ee3e7e3792063cac83b1ed6d216ab879d
tj-devel709 <tj.devel709@gmail.com>
2025-03-25 10:34:30 -0500

    add platform semanticExtensions

--- src/Controls/src/Core/Platform/SemanticExtensions.cs
+++ src/Controls/src/Core/Platform/SemanticExtensions.cs
@@ -6,21 +6,42 @@
 			=> virtualView.TapGestureRecognizerNeedsDelegate();
 
 		internal static bool TapGestureRecognizerNeedsDelegate(this View virtualView)
-			=> virtualView.TapGestureRecognizerNeedsButtonAnnouncement();
+			=> virtualView.TapGestureRecognizerNeedsActionClick();
 
 		internal static bool TapGestureRecognizerNeedsActionClick(this View virtualView)
 		{
 			foreach (var gesture in virtualView.GestureRecognizers)
 			{
+#if MACCATALYST || ANDROID
+				// On Catalyst, will appear as "button, you are currently on a button, to click this button, press control + Option + space".
+				// On Android, will appear as "double tap to activate" in TalkBack.
+				// You are able to click or activate these TGR multiple times on Android and Catalyst, but we cannot secondary click with Catalyst or Android using the provided prompt.
+				// Hence, we should not mark as button for the secondary click.
+				if (gesture is TapGestureRecognizer tgr)
+				{
+					return (tgr.Buttons & ButtonsMask.Primary) == ButtonsMask.Primary;
+				}
+#elif IOS
+				// On iOS, will appear as "Button" in VoiceOver and can tap Secondary button
+				if (gesture is TapGestureRecognizer tgr)
+				{
+					return true;
+				}
+#else
 				//Accessibility can't handle Tap Recognizers with > 1 tap
 				if (gesture is TapGestureRecognizer tgr && tgr.NumberOfTapsRequired == 1)
 				{
 					return (tgr.Buttons & ButtonsMask.Primary) == ButtonsMask.Primary;
 				}
+#endif
 			}
 			return false;
 		}
 
+		/// <summary>
+		/// On Android, we can have a separate prompt that will label the TapGestureRecognizer as "button".
+		/// This method is used to determine if we need to add that prompt.
+		/// </summary>
 		internal static bool TapGestureRecognizerNeedsButtonAnnouncement(this View virtualView)
 		{
 			foreach (var gesture in virtualView.GestureRecognizers)
