bb8406b2b0c8ad8c537de6daebb866f19ca6db43
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-03-26 20:20:50 +0530

    Fixed-Header-Footer-Issues-In-CV2

--- src/Controls/src/Core/Handlers/Items2/iOS/TemplatedCell2.cs
+++ src/Controls/src/Core/Handlers/Items2/iOS/TemplatedCell2.cs
@@ -134,8 +134,16 @@ namespace Microsoft.Maui.Controls.Handlers.Items2
 
 		public void Bind(DataTemplate template, object bindingContext, ItemsView itemsView)
 		{
-			var virtualView = PlatformHandler?.VirtualView as View ?? 
-			                  template.CreateContent(bindingContext, itemsView) as View;
+			View virtualView = null;
+			if (CurrentTemplate != template)
+			{
+				CurrentTemplate = template;
+				virtualView = template.CreateContent(bindingContext, itemsView) as View;
+			}
+			else if (PlatformHandler?.VirtualView is View existingView)
+			{
+				virtualView = existingView;
+			}
 
 			BindVirtualView(virtualView, bindingContext, itemsView, false);
 		}
@@ -147,6 +155,16 @@ namespace Microsoft.Maui.Controls.Handlers.Items2
 
 		void BindVirtualView(View virtualView, object bindingContext, ItemsView itemsView, bool needsContainer)
 		{
+			var oldElement = PlatformHandler?.VirtualView as View;
+
+			if (oldElement is not null && oldElement != virtualView)
+			{
+				oldElement.BindingContext = null;
+				itemsView.RemoveLogicalChild(oldElement);
+				PlatformHandler = null;
+				PlatformView?.RemoveFromSuperview();
+			}
+
 			if (PlatformHandler is null && virtualView is not null)
 			{
 				var mauiContext = itemsView.FindMauiContext()!;
