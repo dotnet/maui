c7a8f20714a22bd472b31e93ecaf969a4e79df59
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-03-28 18:22:50 +0530

    Test case added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28497.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue28497 : _IssuesUITest
+	{
+		public override string Issue => "[iOS] Fix Crash When Using ShellContent with DataTemplate and Binding";
+
+		public Issue28497(TestDevice device)
+		: base(device)
+		{ }
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void FlyoutItemTextShouldDisplayProperly()
+		{
+			var buttonElement = App.WaitForElement("Button");
+			var buttonRect = buttonElement.GetRect();
+			App.WaitForElement("Button");
+			App.TapShellFlyoutIcon();
+			App.WaitForElement("ShellItemLabel");
+			float tapX = buttonRect.X + buttonRect.Width - 10;
+			float tapY = buttonRect.CenterY();
+			App.TapCoordinates(tapX, tapY);
+			App.TapShellFlyoutIcon();
+			App.WaitForElement("ShellItemLabel");
+		}
+	}
+}
