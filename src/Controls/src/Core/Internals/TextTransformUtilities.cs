2d3a51990ad7e689ad1ad10e04ac0b79f486ec7f
Gerald Versluis <gerald.versluis@microsoft.com>
2025-04-01 21:00:17 +0200

    Add back TextTransformUtilites

--- src/Controls/src/Core/Internals/TextTransformUtilities.cs
+++ src/Controls/src/Core/Internals/TextTransformUtilities.cs
@@ -1,23 +1,31 @@
 #nullable disable
+using System;
 using System.ComponentModel;
 
 namespace Microsoft.Maui.Controls.Internals;
 
+[Obsolete("Class name contains a typo. Please use TextTransformUtilities instead, this will be removed in a future version.")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class TextTransformUtilites
+{
+	public static string GetTransformedText(string source, TextTransform textTransform)
+		=> TextTransformUtilities.GetTransformedText(source, textTransform);
+
+	public static void SetPlainText(InputView inputView, string platformText)
+		=> TextTransformUtilities.SetPlainText(inputView, platformText);
+}
+
 /// <summary>
 /// A utilities class for text transformations.
 /// </summary>
-/// <remarks>For internal use by the .NET MAUI platform.</remarks>
-[EditorBrowsable(EditorBrowsableState.Never)]
 public static class TextTransformUtilities
 {
 	/// <summary>
 	/// Applies the <paramref name="textTransform"/> to <paramref name="source"/>.
 	/// </summary>
-	/// <remarks>For internal use by the .NET MAUI platform mostly.</remarks>
 	/// <param name="source">The text to transform.</param>
 	/// <param name="textTransform">The transform to apply to <paramref name="source"/>.</param>
-	/// <returns>The transformed text.</returns>
-	[EditorBrowsable(EditorBrowsableState.Never)]
+	/// <returns>The transformed text.</returns>]
 	public static string GetTransformedText(string source, TextTransform textTransform)
 	{
 		if (string.IsNullOrEmpty(source))
@@ -40,10 +48,8 @@ public static class TextTransformUtilities
 	/// <summary>
 	/// Sets the plain text value to the specified input view.
 	/// </summary>
-	/// <remarks>For internal use by the .NET MAUI platform.</remarks>
 	/// <param name="inputView">The view that will receive the text value.</param>
 	/// <param name="platformText">The text that will be applied to the view.</param>
-	[EditorBrowsable(EditorBrowsableState.Never)]
 	public static void SetPlainText(InputView inputView, string platformText)
 	{
 		if (inputView is null)
