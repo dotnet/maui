9be9822ca91e45927fc196c56618160e95939810
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-04-02 14:46:12 +0530

    Added TestCase

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue18011.xaml.cs
@@ -0,0 +1,10 @@
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 18011, "RadioButton TextColor for plain Content not working on iOS", PlatformAffected.iOS | PlatformAffected.macOS)]
+public partial class Issue18011 : ContentPage
+{
+    public Issue18011()
+    {
+        InitializeComponent();
+    }
+}
