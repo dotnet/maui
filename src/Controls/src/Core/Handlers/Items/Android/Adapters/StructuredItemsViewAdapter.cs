d6dba814955750671123cac155f175d870cd6a8b
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-04-02 10:08:32 +0530

    Fixed the OnPlatform issue on all the platform

--- src/Controls/src/Core/Handlers/Items/Android/Adapters/StructuredItemsViewAdapter.cs
+++ src/Controls/src/Core/Handlers/Items/Android/Adapters/StructuredItemsViewAdapter.cs
@@ -3,6 +3,7 @@ using System;
 using System.ComponentModel;
 using Android.Content;
 using AndroidX.RecyclerView.Widget;
+using Microsoft.Maui.Devices;
 using Microsoft.Maui.Graphics;
 using ViewGroup = Android.Views.ViewGroup;
 
@@ -18,6 +19,8 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 		// I don't want this action to get GC'd
 		Action<Size> _reportMeasure;
 		Func<Size?> _retrieveStaticSize;
+
+		object view;
 
 		protected internal StructuredItemsViewAdapter(TItemsView itemsView,
 			Func<View, Context, ItemContentView> createItemContentView = null) : base(itemsView, createItemContentView)
@@ -66,12 +69,14 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 
 			if (viewType == ItemViewType.Header)
 			{
-				return CreateHeaderFooterViewHolder(ItemsView.Header, ItemsView.HeaderTemplate, context);
+				GetViewFromOnPlatformView(ItemsView.Header);
+				return CreateHeaderFooterViewHolder(view, ItemsView.HeaderTemplate, context);
 			}
 
 			if (viewType == ItemViewType.Footer)
 			{
-				return CreateHeaderFooterViewHolder(ItemsView.Footer, ItemsView.FooterTemplate, context);
+				GetViewFromOnPlatformView(ItemsView.Footer);
+				return CreateHeaderFooterViewHolder(view, ItemsView.FooterTemplate, context);
 			}
 
 			return base.OnCreateViewHolder(parent, viewType);
@@ -167,6 +172,27 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 		void SetStaticSize(Size size)
 		{
 			_size = size;
+		}
+
+		void GetViewFromOnPlatformView(object value)
+		{
+			if (value is OnPlatform<View> onPlatformView && onPlatformView.Platforms.Count > 0)
+			{
+				var platform = DeviceInfo.Current.Platform;
+				foreach (var platformView in onPlatformView.Platforms)
+				{
+					if (platformView.Platform.Contains(platform.ToString()))
+					{
+						// We have a platform-specific view; use that
+						view = platformView.Value;
+						break;
+					}
+				}
+			}
+			else
+			{
+				view = value;
+			}
 		}
 	}
 }
