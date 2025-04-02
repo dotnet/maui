dff76fbb7efd8eca9cdea4cdb9c78bf6c7ab7826
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-04-02 14:46:12 +0530

    Added TestCase

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18011.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue18011 : _IssuesUITest
+{
+    public Issue18011(TestDevice device) : base(device) { }
+
+    public override string Issue => "RadioButton TextColor for plain Content not working on iOS";
+
+    [Test]
+    [Category(UITestCategories.RadioButton)]
+    public void VerifyRadioButtonTextColor()
+    {
+        App.WaitForElement("RadioButton");
+        VerifyScreenshot();
+    }
+}
