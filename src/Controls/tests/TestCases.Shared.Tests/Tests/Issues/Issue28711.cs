0aa057c469438eb74e57be19388147932819bdd1
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-04-03 16:11:52 +0530

    Fixed-28711 : NRE crash fixed when using Name in SolidColorBrush

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28711.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue28711 : _IssuesUITest
+{
+	public Issue28711(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "NRE occurs when using Name for SolidColorBrush";
+
+	[Test]
+	[Category(UITestCategories.TitleView)]
+	public void BrushWithNameDoesNotCrash()
+	{
+		App.WaitForElement("label");
+	}
+}
