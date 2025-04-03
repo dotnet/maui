29842d6d212ce4deb286688968b74c6e9e5f3631
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-04-03 10:33:58 +0530

    fixed webview backgroundcolor issue

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue28714.cs
@@ -0,0 +1,42 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 28714, "[iOS] WebView BackgroundColor is not setting correctly", PlatformAffected.iOS, isInternetRequired: true)]
+public partial class Issue28714 : ContentPage
+{
+	public Issue28714()
+	{
+		BackgroundColor = Colors.YellowGreen;
+		var verticalStackLayout = new VerticalStackLayout();
+		verticalStackLayout.Spacing = 20;
+
+		var webView = new WebView()
+		{
+			HeightRequest = 300,
+			WidthRequest = 400,
+			BackgroundColor = Colors.Transparent,
+			Source = new HtmlWebViewSource
+			{
+				Html = @"
+    <!DOCTYPE html>
+    <html lang='en'>
+    <body>
+        <h1>Welcome to WebView</h1>
+        <p id='message'></p>
+    </body>
+    </html>"
+			}
+
+		};
+
+		var label = new Label
+		{
+			Text = "WebView BackgroundColor",
+			AutomationId = "label"
+		};
+
+		verticalStackLayout.Add(label);
+		verticalStackLayout.Add(webView);
+
+		Content = verticalStackLayout;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28714.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue28714 : _IssuesUITest
+{
+	public override string Issue => "[iOS] WebView BackgroundColor is not setting correctly";
+
+	public Issue28714(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.WebView)]
+	public void VerifyWebViewBackgroundColor()
+	{
+		VerifyInternetConnectivity();
+		App.WaitForElement("label");
+		VerifyScreenshot();
+	}
+}
