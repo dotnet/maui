a5931674781c9484aa557ef030c98b5dcb03c038
TamilarasanSF4853 <tamilarasan.velu@syncfusion.com>
2025-04-03 20:51:44 +0530

    [Testing] Migration of Compatibility.Core platform-specific unit tests into device tests - 8 (#28496)
    * code changes added
    
    * updated code

---
+++ src/Controls/tests/DeviceTests/Elements/Page/PageTests.Android.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Dispatching;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using Xunit;
+using AView = Android.Views.View;
+using AndroidX.Fragment.App;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	public partial class PageTests : ControlsHandlerTestBase
+	{
+		//src/Compatibility/Core/tests/Android/EmbeddingTests.cs
+		[Fact(DisplayName = "Can Create Platform View From ContentPage")]
+		public async Task CanCreatePlatformViewFromContentPage()
+		{
+
+			var contentPage = new ContentPage { Title = "Embedded Page" };
+			var handler = CreateHandler<PageHandler>(contentPage);
+			var mauiContext = handler.MauiContext;
+
+			await contentPage.Dispatcher.DispatchAsync(() =>
+			{
+
+				AView platformView = contentPage.ToPlatform(mauiContext);
+				if (platformView is FragmentContainerView containerView)
+				{
+					var activity = mauiContext.Context as AndroidX.Fragment.App.FragmentActivity;
+					var fragmentManager = activity.SupportFragmentManager;
+					var fragment = fragmentManager.FindFragmentById(containerView.Id);
+					Assert.NotNull(fragment);
+				}
+			});
+		}
+	}
+}
