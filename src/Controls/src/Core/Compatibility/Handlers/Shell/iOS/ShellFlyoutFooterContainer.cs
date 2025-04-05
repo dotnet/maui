abbd66325c9e8d379ecc529ecb6dd2a3bb327209
Jakub Florkowski <kubaflo123@gmail.com>
2025-04-05 15:07:05 +0200

    Update ShellFlyoutFooterContainer.cs

--- src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellFlyoutFooterContainer.cs
+++ src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellFlyoutFooterContainer.cs
@@ -4,13 +4,31 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility;
 
 internal class ShellFlyoutFooterContainer : UIContainerView, IPlatformMeasureInvalidationController
 {
+	bool _invalidateParentWhenMovedToWindow;
+
 	public ShellFlyoutFooterContainer(View view) : base(view) { }
 
-	void IPlatformMeasureInvalidationController.InvalidateAncestorsMeasuresWhenMovedToWindow() { }
+	void IPlatformMeasureInvalidationController.InvalidateAncestorsMeasuresWhenMovedToWindow()
+	{
+		_invalidateParentWhenMovedToWindow = true;
+	}
 
 	void IPlatformMeasureInvalidationController.InvalidateMeasure(bool isPropagating)
 	{
-		var size = SizeThatFits(new CGSize(View.Frame.Width, double.PositiveInfinity));
-		Frame = new CGRect(0, Superview.Frame.Height - size.Height, Superview.Frame.Width, size.Height);
+		if (Superview is not null)
+		{
+			var size = SizeThatFits(new CGSize(Superview.Frame.Width, double.PositiveInfinity));
+			Frame = new CGRect(0, Superview.Frame.Height - size.Height, Superview.Frame.Width, size.Height);
+		}
+	}
+
+	public override void MovedToWindow()
+	{
+		base.MovedToWindow();
+		if (_invalidateParentWhenMovedToWindow)
+		{
+			_invalidateParentWhenMovedToWindow = false;
+			this.InvalidateAncestorsMeasures();
+		}
 	}
 }
