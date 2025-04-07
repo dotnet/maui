6fc393387f8fe0b2c25fd4659eed35efdbc6eb92
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-04-07 15:50:53 +0530

    [Windows] Fix for 7144 ( IndicatorView using templated icons not working )

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue7144.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue7144.cs
@@ -6,7 +6,9 @@ namespace Microsoft.Maui.TestCases.Tests.Issues;
 
 public class Issue7144 : _IssuesUITest
 {
-	public Issue7144(TestDevice device) : base(device) { }
+	public Issue7144(TestDevice device) : base(device)
+	{
+	}
 
 	public override string Issue => "IndicatorView using templated icons not working";
 
