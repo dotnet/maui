a79de0da87fdd3bcbf1092dbf71ff4449b9f7bee
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-04-08 21:36:54 +0530

    Added test case

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19642.cs
@@ -0,0 +1,24 @@
+﻿#if TEST_FAILS_ON_MACCATALYST || TEST_FAILS_ON_IOS //Issue link - https://github.com/dotnet/maui/issues/19642
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue19642 : _IssuesUITest
+{
+	public Issue19642(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "NullReferenceException when using ImagePaint on Mac/iOS";
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void ImagePaintShouldBeDrawn()
+	{
+		App.WaitForElement("TestLabel");
+		VerifyScreenshot();
+	}
+}
+#endif
