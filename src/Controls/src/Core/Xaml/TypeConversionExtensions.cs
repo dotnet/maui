84df6a214ac886e5e4bac88c6f71efb50acc14b8
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-04-08 12:48:41 +0530

    Simplified the fix

--- src/Controls/src/Core/Xaml/TypeConversionExtensions.cs
+++ src/Controls/src/Core/Xaml/TypeConversionExtensions.cs
@@ -216,7 +216,7 @@ namespace Microsoft.Maui.Controls.Xaml
 				}
 			}
 
-			if ((value != null && !toType.IsAssignableFrom(value.GetType())) || (value != null && toType.IsAssignableFrom(typeof(View))))
+			if (value != null && (!toType.IsAssignableFrom(value.GetType()) || toType.IsAssignableFrom(typeof(View))))
 			{
 				if (TypeConversionHelper.TryConvert(value, toType, out var convertedValue))
 				{
