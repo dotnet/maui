c2448f8ed8070785d08253dab093a09a5e6eeeed
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-04-09 17:11:06 +0530

    Included the test case and sample for the fix

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28678.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue28678 : _IssuesUITest
+{
+	public Issue28678(TestDevice device) : base(device) { }
+
+	public override string Issue => "TargetInvocationException Occurs When Selecting Header/Footer After Changing ItemsLayout in CV2";
+
+	[Test]
+	[Category(UITestCategories.CollectionView)]
+	public void CollectionView2ShouldNotCrash()
+	{
+		App.WaitForElement("Button");
+		App.Tap("Button");
+		App.Tap("HeaderButton");
+		App.WaitForElement("Button");
+	}
+}
