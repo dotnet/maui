5c8bc5559e315a7b8398e0a2a1bd4b064137f999
tj-devel709 <tj.devel709@gmail.com>
2025-04-11 16:37:51 -0500

    sample code to remove later

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,9 +1,44 @@
-﻿namespace Maui.Controls.Sample;
+﻿using System.Collections.ObjectModel;
+namespace Maui.Controls.Sample;
 
 public partial class MainPage : ContentPage
 {
+	public ObservableCollection<Item> Items { get; set; }
+
 	public MainPage()
 	{
 		InitializeComponent();
+
+		Items = new ObservableCollection<Item>
+        {
+            new Item { Name = "Item 1", Description = "Description for item 1" },
+            new Item { Name = "Item 2", Description = "Description for item 2" },
+            new Item { Name = "Item 3", Description = "Description for item 3" },
+            new Item { Name = "Item 4", Description = "Description for item 4" },
+            new Item { Name = "Item 5", Description = "Description for item 5" }
+        };
+
+        BindingContext = this;
 	}
+
+    void NoneSelectionMode(object sender, EventArgs e)
+    {
+        collectionView.SelectionMode = SelectionMode.None;
+    }
+
+    void SingleSelectionMode(object sender, EventArgs e)
+    {
+        collectionView.SelectionMode = SelectionMode.Single;
+    }
+
+    void MultipleSelectionMode(object sender, EventArgs e)
+    {
+        collectionView.SelectionMode = SelectionMode.Multiple;
+    }
+
+	public class Item
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
 }
