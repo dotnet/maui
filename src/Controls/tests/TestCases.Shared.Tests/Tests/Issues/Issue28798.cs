3e38ad76f69034db9fdb06cff1f64a9b5a72e9b4
praveenkumarkarunanithi <praveenkumar.karunanithi@syncfusion.com>
2025-04-11 18:59:34 +0530

    updated test case codes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28798.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28798.cs
@@ -3,21 +3,20 @@ using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+internal class Issue28798 : _IssuesUITest
 {
-	internal class Issue28798 : _IssuesUITest
+	public Issue28798(TestDevice device) : base(device) { }
+
+	public override string Issue => "Controls Disappear When WebView is Used with Hardware Acceleration Disabled in Android";
+
+	[Test]
+	[Category(UITestCategories.WebView)]
+	public void ControlsShouldRemainVisibleWithWebViewWhenHardwareAccelerationIsDisabled()
 	{
-		public Issue28798(TestDevice device) : base(device) { }
-
-		public override string Issue => "Controls Disappear When WebView is Used with Hardware Acceleration Disabled in Android";
-
-		[Test]
-		[Category(UITestCategories.WebView)]
-		public void ControlsShouldRemainVisibleWithWebViewWhenHardwareAccelerationIsDisabled()
-		{
-			App.WaitForElement("TestLabel");
-			VerifyScreenshot();
-		}
+		App.WaitForElement("TestLabel");
+		VerifyScreenshot();
 	}
 }
 #endif
