403355c3139b79b56c023863add36666679080d4
Bhavanesh N <bhavanesh001@gmail.com>
2025-04-13 10:47:07 +0530

    Fix some Flaky UI tests that fails on android

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue2809.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue2809.cs
@@ -16,6 +16,9 @@ public class Issue2809 : _IssuesUITest
 	[Category(UITestCategories.ToolbarItem)]
 	public void TestPageDoesntCrash()
 	{
+#if ANDROID
+		App.WaitForElement(AppiumQuery.ByXPath("//android.widget.ImageView[@content-desc=\"More options\"]"));
+#endif
 		App.TapMoreButton();
 		App.Tap("Item 1");
 	}
