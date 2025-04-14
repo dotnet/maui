8952f6c33fe733ee69326d57d2c919c79c811005
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-04-14 15:04:47 +0530

    updated snaps

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13258.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13258.cs
@@ -3,24 +3,23 @@ using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue13258 : _IssuesUITest
 {
-	public class Issue13258 : _IssuesUITest
+	public Issue13258(TestDevice testDevice) : base(testDevice)
 	{
-		public Issue13258(TestDevice testDevice) : base(testDevice)
-		{
-		}
+	}
 
-		public override string Issue => "MAUI Slider thumb image is big on android";
+	public override string Issue => "MAUI Slider thumb image is big on android";
 
-		[Test]
-		[Category(UITestCategories.Slider)]
-		public void SliderThumbImageShouldBeScaled()
-		{
-			App.WaitForElement("ToggleImageBtn");
-			App.Tap("ToggleImageBtn");
-			VerifyScreenshot();
-		}
+	[Test]
+	[Category(UITestCategories.Slider)]
+	public void SliderThumbImageShouldBeScaled()
+	{
+		App.WaitForElement("ToggleImageBtn");
+		App.Tap("ToggleImageBtn");
+		VerifyScreenshot();
 	}
 }
 #endif
