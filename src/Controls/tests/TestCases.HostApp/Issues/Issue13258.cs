e0416179f33e2c1100a5bafbaaa35b6e86edd66d
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-04-14 15:04:47 +0530

    updated snaps

--- src/Controls/tests/TestCases.HostApp/Issues/Issue13258.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue13258.cs
@@ -1,53 +1,51 @@
-﻿namespace Maui.Controls.Sample.Issues
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 13258, "MAUI Slider thumb image is big on android", PlatformAffected.Android | PlatformAffected.iOS | PlatformAffected.macOS)]
+public class Issue13258 : TestContentPage
 {
-
-	[Issue(IssueTracker.Github, 13258, "MAUI Slider thumb image is big on android", PlatformAffected.Android | PlatformAffected.iOS | PlatformAffected.macOS)]
-	public class Issue13258 : TestContentPage
+	protected override void Init()
 	{
-		protected override void Init()
-		{
-			StackLayout rootLayout = new StackLayout() { Spacing = 10, Padding = 10 };
+		StackLayout rootLayout = new StackLayout() { Spacing = 10, Padding = 10 };
 
-			Label slider1DescriptionLabel = CreateLabel("Slider with Thumb Image");
-			Slider slider1 = CreateSlider("avatar.png");
+		Label slider1DescriptionLabel = CreateLabel("Slider with Thumb Image");
+		Slider slider1 = CreateSlider("avatar.png");
 
-			Label slider2DescriptionLabel = CreateLabel("Thumb Image will be set to coffee.png at run time");
-			Slider slider2 = CreateSlider();
+		Label slider2DescriptionLabel = CreateLabel("Thumb Image will be set to coffee.png at run time");
+		Slider slider2 = CreateSlider();
 
-			Label slider3DescriptionLabel = CreateLabel("Thumb Image will be set to null at run time");
-			Slider slider3 = CreateSlider("shopping_cart.png");
+		Label slider3DescriptionLabel = CreateLabel("Thumb Image will be set to null at run time");
+		Slider slider3 = CreateSlider("shopping_cart.png");
 
-			Button button = new Button() { Text = "Change Thumb Image", AutomationId = "ToggleImageBtn" };
-			button.Clicked += (s, e) => ToggleThumbImages(slider2, slider3);
+		Button button = new Button() { Text = "Change Thumb Image", AutomationId = "ToggleImageBtn" };
+		button.Clicked += (s, e) => ToggleThumbImages(slider2, slider3);
 
-			rootLayout.Children.Add(slider1DescriptionLabel);
-			rootLayout.Children.Add(slider1);
+		rootLayout.Children.Add(slider1DescriptionLabel);
+		rootLayout.Children.Add(slider1);
 
-			rootLayout.Children.Add(slider2DescriptionLabel);
-			rootLayout.Children.Add(slider2);
+		rootLayout.Children.Add(slider2DescriptionLabel);
+		rootLayout.Children.Add(slider2);
 
-			rootLayout.Children.Add(slider3DescriptionLabel);
-			rootLayout.Children.Add(slider3);
+		rootLayout.Children.Add(slider3DescriptionLabel);
+		rootLayout.Children.Add(slider3);
 
-			rootLayout.Children.Add(button);
+		rootLayout.Children.Add(button);
 
-			Content = rootLayout;
-		}
+		Content = rootLayout;
+	}
 
-		Label CreateLabel(string text)
-		{
-			return new Label { Text = text };
-		}
+	Label CreateLabel(string text)
+	{
+		return new Label { Text = text };
+	}
 
-		Slider CreateSlider(string thumbImageSource = null)
-		{
-			return new Slider { ThumbImageSource = thumbImageSource };
-		}
+	Slider CreateSlider(string thumbImageSource = null)
+	{
+		return new Slider { ThumbImageSource = thumbImageSource };
+	}
 
-		private void ToggleThumbImages(Slider slider2, Slider slider3)
-		{
-			slider2.ThumbImageSource = "coffee.png";
-			slider3.ThumbImageSource = null;
-		}
+	private void ToggleThumbImages(Slider slider2, Slider slider3)
+	{
+		slider2.ThumbImageSource = "coffee.png";
+		slider3.ThumbImageSource = null;
 	}
 }
