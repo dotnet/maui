5f5e1f5d05a1a4c900af0f2c490988e2dbaf7f4c
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-04-15 20:16:30 +0530

    Included the test sample and test case

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue26795.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue26795 : _IssuesUITest
+{
+    public Issue26795(TestDevice device) : base(device) { }
+
+    public override string Issue => "Specifying HeightRequest in Webview when wrapped by ScrollView set invisible causes crash in iOS";
+
+    [Test]
+    [Category(UITestCategories.WebView)]
+    public void WebViewShouldNotCrash()
+    {
+        App.WaitForElement("Label");
+    }
+}
