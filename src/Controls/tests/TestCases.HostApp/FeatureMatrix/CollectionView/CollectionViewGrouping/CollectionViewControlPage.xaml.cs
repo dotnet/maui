6a3d0c785f21456d16aeac1a4f25554044901e3b
NafeelaNazhir <nafeela.nazhirhussain@syncfusion.com>
2025-04-16 10:02:58 +0530

    Changes updated

--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/CollectionView/CollectionViewGrouping/CollectionViewControlPage.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/CollectionView/CollectionViewGrouping/CollectionViewControlPage.xaml.cs
@@ -2,25 +2,24 @@ using System;
 using Microsoft.Maui.Controls;
 using System.Collections.ObjectModel;
 
-namespace Maui.Controls.Sample
+namespace Maui.Controls.Sample;
+
+public partial class CollectionViewControlPage : ContentPage
 {
-	public partial class CollectionViewControlPage : ContentPage
+	private CollectionViewViewModel _viewModel;
+
+	public CollectionViewControlPage()
 	{
-		private CollectionViewViewModel _viewModel;
+		InitializeComponent();
+		_viewModel = new CollectionViewViewModel();
+		_viewModel.ItemsSourceType = ItemsSourceType.ObservableCollectionT;
+		BindingContext = _viewModel;
+	}
 
-		public CollectionViewControlPage()
-		{
-			InitializeComponent();
-			_viewModel = new CollectionViewViewModel();
-			_viewModel.ItemsSourceType = ItemsSourceType.ObservableCollectionT;
-			BindingContext = _viewModel;
-		}
-
-		private async void NavigateToOptionsPage_Clicked(object sender, EventArgs e)
-		{
-			BindingContext = _viewModel = new CollectionViewViewModel();
-			_viewModel.ItemsSourceType = ItemsSourceType.ObservableCollectionT;
-			await Navigation.PushAsync(new GroupingOptionsPage(_viewModel));
-		}
+	private async void NavigateToOptionsPage_Clicked(object sender, EventArgs e)
+	{
+		BindingContext = _viewModel = new CollectionViewViewModel();
+		_viewModel.ItemsSourceType = ItemsSourceType.ObservableCollectionT;
+		await Navigation.PushAsync(new GroupingOptionsPage(_viewModel));
 	}
 }
