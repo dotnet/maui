5f39c02447cb1ed0a6eedf8da9eceee91bd79a32
Gerald Versluis <gerald.versluis@microsoft.com>
2025-04-16 11:03:16 +0200

    Obsolete UseMauiCompatibility for .NET 10

--- src/Compatibility/Core/src/AppHostBuilderExtensions.cs
+++ src/Compatibility/Core/src/AppHostBuilderExtensions.cs
@@ -57,6 +57,9 @@ namespace Microsoft.Maui.Controls.Compatibility.Hosting
 			Controls.Hosting.CompatibilityCheck.ResetCompatibilityCheck();
 		}
 
+		[Obsolete("The compatibility functionality is deprecated. This will be removed in the future, please make sure to migrate to handlers. " +
+			"You might already not be using any of the compatibility functionality anymore, in that case please remove this call " +
+			"(and the reference to the Compatibility NuGet package) as it will introduce unnecessary overhead that impacts performance.")]
 		public static MauiAppBuilder UseMauiCompatibility(this MauiAppBuilder builder)
 		{
 			Controls.Hosting.CompatibilityCheck.UseCompatibility();
