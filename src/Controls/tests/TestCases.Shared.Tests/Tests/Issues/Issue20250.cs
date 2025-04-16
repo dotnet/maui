e38ace89017b03bb695539c69fea1ad9b652b237
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-04-16 17:36:18 +0530

    Test script changes.

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20250.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20250.cs
@@ -4,23 +4,23 @@ using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue20250 : _IssuesUITest
 {
-	public class Issue20250 : _IssuesUITest
+	public override string Issue => "[iOS] SearchHandler ClearPlaceholderIcon color";
+
+	public Issue20250(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.ActivityIndicator)]
+	public void VerifySearchHandlerClearPlaceholderIconColor()
 	{
-		public override string Issue => "[iOS] SearchHandler ClearPlaceholderIcon color";
-
-		public Issue20250(TestDevice device)
-		: base(device)
-		{ }
-
-		[Test]
-		[Category(UITestCategories.Shell)]
-		public void VerifySearchHandlerClearPlaceholderIconColor()
-		{
-			App.WaitForElement("label");
-			VerifyScreenshot();
-		}
+		App.WaitForElement("label");
+		VerifyScreenshot();
 	}
 }
+
 #endif
