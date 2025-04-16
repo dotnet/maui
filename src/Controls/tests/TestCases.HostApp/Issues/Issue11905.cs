238e8372473ab896c1b2f063bfaedd725463ecf2
praveenkumarkarunanithi <praveenkumar.karunanithi@syncfusion.com>
2025-04-16 16:57:49 +0530

    added UI test

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue11905.cs
@@ -0,0 +1,50 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+[Issue(IssueTracker.Github, 11905, "WebView.EvaluateJavaScriptAsync fails to execute JavaScript containing newline characters", PlatformAffected.iOS | PlatformAffected.UWP)]
+public partial class Issue11905 : ContentPage
+{
+	private WebView Browser;
+    public Issue11905()
+    {
+        Browser = new WebView
+        {
+            Source = "https://example.com",
+			AutomationId = "WebView",
+            VerticalOptions = LayoutOptions.Fill,
+            HorizontalOptions = LayoutOptions.Fill
+        };
+        
+        Browser.Navigated += Browser_Navigated;
+        
+        Content = Browser;
+    }
+    
+    private async void Browser_Navigated(object sender, WebNavigatedEventArgs e)
+	{
+		string jsCode = "document.body.style.backgroundColor = 'blue';\r\n" +
+					"var box = document.createElement('div');\r" +
+					"box.style.position = 'fixed';\n" +
+					"box.style.cssText = 'top:40%; left:20%; width:60%; background-color:red; border:5px solid black; padding:20px;';" +
+					"box.innerHTML = '<h2 id=\"newline-test-header\" style=\"color:white\">Newline Test</h2>';" +
+					"document.body.appendChild(box);";
+		
+		await Browser.EvaluateJavaScriptAsync(jsCode);
+		
+		var result = await Browser.EvaluateJavaScriptAsync(
+			"document.getElementById('newline-test-header') !== null");
+		
+		if (result.ToLowerInvariant() == "true")
+		{
+			var testLabel = new Label { 
+				Text = "TestLabel", 
+				AutomationId = "TestLabel",
+			};
+			
+			var grid = new Grid();
+			grid.Children.Add(Browser);
+			grid.Children.Add(testLabel);
+			Content = grid;
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue11905.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+internal class Issue11905 : _IssuesUITest
+{
+	public Issue11905(TestDevice device) : base(device) { }
+
+	public override string Issue => "WebView.EvaluateJavaScriptAsync fails to execute JavaScript containing newline characters";
+
+	[Test]
+	[Category(UITestCategories.WebView)]
+	public void JavaScriptWithNewlinesShouldExecuteCorrectly()
+	{
+		App.WaitForElement("WebView");
+		App.WaitForElement("TestLabel");
+	}
+}
