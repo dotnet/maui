e136498854c1ce3d5982e5070b04e8ab59005b06
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-04-17 17:02:56 +0530

    namespace changes added

--- src/Controls/tests/TestCases.HostApp/Issues/Issue10805.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue10805.cs
@@ -1,30 +1,29 @@
-﻿namespace Maui.Controls.Sample.Issues
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 10805, "Date Format property in DatePicker not working on Windows with date Format", PlatformAffected.UWP)]
+public class Issue10805 : ContentPage
 {
-	[Issue(IssueTracker.Github, 10805, "Date Format property in DatePicker not working on Windows with date Format", PlatformAffected.UWP)]
-	public class Issue10805 : ContentPage
+	public Issue10805()
 	{
-		public Issue10805()
+		var label = new Label
 		{
-			var label = new Label
-			{
-				Text = "Verify DatePicker DateFormat",
-				AutomationId = "DatePickerLabel",
-			};
+			Text = "Verify DatePicker DateFormat",
+			AutomationId = "DatePickerLabel",
+		};
 
-			var datePicker = new DatePicker
-			{
-				Format = "dd.MM.yyyy",
-				Date = new DateTime(2022, 2, 25),
-			};
+		var datePicker = new DatePicker
+		{
+			Format = "dd.MM.yyyy",
+			Date = new DateTime(2022, 2, 25),
+		};
 
-			Content = new StackLayout
+		Content = new StackLayout
+		{
+			Children =
 			{
-				Children =
-				{
-					label,
-					datePicker
-				},
-			};
-		}
+				label,
+				datePicker
+			},
+		};
 	}
 }
--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10805.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10805.cs
@@ -2,22 +2,20 @@
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue10805 : _IssuesUITest
 {
-	public class Issue10805 : _IssuesUITest
+	public Issue10805(TestDevice device) : base(device)
 	{
-		public Issue10805(TestDevice device) : base(device)
-		{
-		}
+	}
 
-		public override string Issue => "Date Format property in DatePicker not working on Windows with date Format";
+	public override string Issue => "Date Format property in DatePicker not working on Windows with date Format";
 
-		[Test]
-		[Category(UITestCategories.DatePicker)]
-		public void VerifyDatePickerDateFormat()
-		{
-			App.WaitForElement("DatePickerLabel");
-			VerifyScreenshot();
-		}
+	[Test]
+	[Category(UITestCategories.DatePicker)]
+	public void VerifyDatePickerDateFormat()
+	{
+		App.WaitForElement("DatePickerLabel");
+		VerifyScreenshot();
 	}
 }
