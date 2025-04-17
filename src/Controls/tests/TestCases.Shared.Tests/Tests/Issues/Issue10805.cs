a7351c1039b4f9644db9ad3041fbe4f5f7c46d30
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-04-17 16:54:12 +0530

    Test added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10805.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue10805 : _IssuesUITest
+	{
+		public Issue10805(TestDevice device) : base(device)
+		{
+		}
+
+		public override string Issue => "Date Format property in DatePicker not working on Windows with date Format";
+
+		[Test]
+		[Category(UITestCategories.DatePicker)]
+		public void VerifyDatePickerDateFormat()
+		{
+			App.WaitForElement("DatePickerLabel");
+			VerifyScreenshot();
+		}
+	}
+}
