2507dd717b2a785f4db2bd17274d4c1fe3162a13
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-04-17 12:16:25 +0530

    Fixed the group header footer template issue

--- src/Controls/src/Core/Handlers/Items2/CollectionViewHandler2.iOS.cs
+++ src/Controls/src/Core/Handlers/Items2/CollectionViewHandler2.iOS.cs
@@ -56,6 +56,8 @@ namespace Microsoft.Maui.Controls.Handlers.Items2
 			[StructuredItemsView.FooterTemplateProperty.PropertyName] = MapFooterTemplate,
 			[StructuredItemsView.HeaderProperty.PropertyName] = MapHeaderTemplate,
 			[StructuredItemsView.FooterProperty.PropertyName] = MapFooterTemplate,
+			[GroupableItemsView.GroupHeaderTemplateProperty.PropertyName] = MapHeaderTemplate,
+			[GroupableItemsView.GroupFooterTemplateProperty.PropertyName] = MapFooterTemplate,
 		};
 	}
 
