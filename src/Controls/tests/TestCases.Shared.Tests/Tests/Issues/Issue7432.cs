50f888c770050bf640beebca3aec230a84d46e51
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-04-22 20:01:40 +0530

    Included the test sample and test case for the issue

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue7432.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue7432 : _IssuesUITest
+{
+    public Issue7432(TestDevice device) : base(device) { }
+
+    public override string Issue => "Android Image.Scale produces wrong layout";
+
+    [Test]
+    [Category(UITestCategories.Image)]
+    public void ImageShouldScaleProperly()
+    {
+        App.WaitForElement("Image");
+    }
+}
