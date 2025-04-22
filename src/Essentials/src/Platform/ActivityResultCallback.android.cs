f9709caca5a0adced57abcfa815dfc6295e1e5d1
phunkeler <69320109+phunkeler@users.noreply.github.com>
2025-04-22 08:12:16 -0400

    [Android] MediaPicker: Use New Photo Picker (#24027)
    * init
    
    * refactor + prior behavior

---
+++ src/Essentials/src/Platform/ActivityResultCallback.android.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AndroidX.Activity.Result;
+using JavaObject = Java.Lang.Object;
+
+namespace Microsoft.Maui.ApplicationModel;
+
+class ActivityResultCallback<T>(Action<T> onActivityResult) : JavaObject, IActivityResultCallback
+	where T : JavaObject
+{
+	readonly Action<T> _onActivityResult = onActivityResult;
+
+	public void OnActivityResult(JavaObject result)
+	{
+		_onActivityResult.Invoke(result as T);
+	}
+}
