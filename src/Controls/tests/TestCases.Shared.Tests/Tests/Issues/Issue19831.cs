9749b0cc3619969e722ed290447f85af7d1d8905
Bhavanesh N <bhavanesh001@gmail.com>
2025-04-24 00:22:02 +0530

    add test category for ActionModeMenuShouldNotBeVisibleAfterSwitchingTab

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19831.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19831.cs
@@ -14,6 +14,7 @@ public class Issue19831 : _IssuesUITest
 	{ }
 
     [Test]
+	[Category(UITestCategories.ListView)]
 	public void ActionModeMenuShouldNotBeVisibleAfterSwitchingTab()
 	{
 		_ = App.WaitForElement("Item1");
