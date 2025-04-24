70e5518acb969d94db1dbb7aabcf653f1884b5ef
tj-devel709 <tj.devel709@gmail.com>
2025-04-24 08:26:48 -0500

    screenshots 6

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21375.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21375.cs
@@ -14,11 +14,7 @@ public class Issue21375 : _IssuesUITest
 
     [Test]
     [Category(UITestCategories.CollectionView)]
-#if WINDOWS // Windows screenshots are a little behind at times. Adding small delays to help with that
-    public async Task SelectedItemsShowSelected ()
-#else
     public void SelectedItemsShowSelected ()
-#endif
     {
         var collectionView = App.WaitForElement("collectionView");
         var centerX = collectionView.GetRect().X + (collectionView.GetRect().Width / 2);
@@ -28,38 +24,31 @@ public class Issue21375 : _IssuesUITest
 
         App.TapCoordinates(centerX, firstItemY);
         App.WaitForElement("calculateButton").Tap();
-#if WINDOWS // Windows has a little bit of delay
-        await Task.Delay(10);
-#endif
+
         VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "_single");
 
         App.WaitForElement("multipleButton").Tap();
         App.TapCoordinates(centerX, secondItemY);
         App.TapCoordinates(centerX, thirdItemY);
         App.WaitForElement("calculateButton").Tap();
-#if WINDOWS // Windows has a little bit of delay
-        await Task.Delay(10);
-#endif
+
         VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "_multiple");
 
         App.WaitForElement("noneButton").Tap();
         App.WaitForElement("calculateButton").Tap();
-#if WINDOWS // Windows has a little bit of delay
-        await Task.Delay(10);
-#endif
         VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "_none");
 
         App.WaitForElement("singleButton").Tap();
         App.WaitForElement("calculateButton").Tap();
-#if !WINDOWS // Windows clears out the selected items when we switch selection mode back to single
+
+#if !WINDOWS // Windows clears out the selected items when we switch back
         VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "_single");
-#endif
 
         App.WaitForElement("multipleButton").Tap();
         App.WaitForElement("calculateButton").Tap();
-#if WINDOWS // Windows has a little bit of delay
-        await Task.Delay(10);
-#endif
+
         VerifyScreenshot(TestContext.CurrentContext.Test.MethodName + "_multiple");
+#endif
+
     }
 }
