e05ce1441586549175846036e01e5e94db798293
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-04-24 16:57:50 +0530

    Modify TestCase

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16767_Downsize.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue16767_Downsize : _IssuesUITest
+{
+	public Issue16767_Downsize(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Downsize function in W2DImage class";
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void ImagePaintWithDownsize()
+	{
+		App.WaitForElement("descriptionLabel");
+		VerifyScreenshot();
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16767_Resize.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue16767_Resize : _IssuesUITest
+{
+	public Issue16767_Resize(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Resize function in W2DImage class";
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void ImagePaintWithResizeModeFit()
+	{
+		App.WaitForElement("ResizeModeFit");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void ImagePaintWithResizeModeBleed()
+	{
+		App.WaitForElement("ResizeModeBleed");
+		App.Tap("ResizeModeBleed");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void ImagePaintWithResizeModeStretch()
+	{
+		App.WaitForElement("ResizeModeStretch");
+		App.Tap("ResizeModeStretch");
+		VerifyScreenshot();
+	}
+}
