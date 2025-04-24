0927ff273fd4b6cb0acdc55b1e55e69b3708a7bf
NafeelaNazhir <nafeela.nazhirhussain@syncfusion.com>
2025-04-24 22:00:41 +0530

    [Testing] Feature Matrix UITest Cases for CollectionView Grouping Feature (#29002)
    * Added GroupingFeature tests
    
    * updated changes
    
    * updated changes
    
    * Added test fails attribute
    
    * Corrected Mistakes
    
    * Updated Images
    
    * Corrected mistakes
    
    * Changes updated
    
    * Modified changes
    
    * Resolved Conflicts

--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/CollectionView/CollectionViewGrouping/CollectionViewGroupingPage.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/CollectionView/CollectionViewGrouping/CollectionViewGroupingPage.xaml.cs
@@ -3,22 +3,23 @@ using Microsoft.Maui.Controls;
 using System.Collections.ObjectModel;
 
 namespace Maui.Controls.Sample;
-public partial class CollectionViewHeaderPage : ContentPage
+
+public partial class CollectionViewGroupingPage : ContentPage
 {
 	private CollectionViewViewModel _viewModel;
 
-	public CollectionViewHeaderPage()
+	public CollectionViewGroupingPage()
 	{
 		InitializeComponent();
 		_viewModel = new CollectionViewViewModel();
-		_viewModel.ItemsSourceType = ItemsSourceType.ObservableCollection5T;
+		_viewModel.ItemsSourceType = ItemsSourceType.ObservableCollectionT;
 		BindingContext = _viewModel;
 	}
 
 	private async void NavigateToOptionsPage_Clicked(object sender, EventArgs e)
 	{
 		BindingContext = _viewModel = new CollectionViewViewModel();
-		_viewModel.ItemsSourceType = ItemsSourceType.ObservableCollection5T;
-		await Navigation.PushAsync(new HeaderFooterOptionsPage(_viewModel));
+		_viewModel.ItemsSourceType = ItemsSourceType.ObservableCollectionT;
+		await Navigation.PushAsync(new GroupingOptionsPage(_viewModel));
 	}
 }
