0bc72cfabe77a723152117ebbf4cb034dfaae1b6
NafeelaNazhir <nafeela.nazhirhussain@syncfusion.com>
2025-04-28 18:09:52 +0530

    Added Scrolling behavior

--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/CollectionView/CollectionViewFeaturePage.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/CollectionView/CollectionViewFeaturePage.xaml.cs
@@ -27,5 +27,10 @@ namespace Maui.Controls.Sample
 		{
 			await Navigation.PushAsync(new CollectionViewHeaderPage());
 		}
+
+		private async void OnScrollingButtonClicked(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new CollectionViewScrollPage());
+		}
 	}
 }
