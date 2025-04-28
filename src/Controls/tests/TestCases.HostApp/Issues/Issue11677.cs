b8b2c43fd420e4010e88e752ef3ff06a3dcf9f55
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-04-28 15:07:42 +0530

    review changes added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue11677.cs
@@ -0,0 +1,31 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 11677, "[iOS][maccatalyst] SearchBar BackgroundColor is black when set to transparent", PlatformAffected.iOS)]
+public class Issue11677 : ContentPage
+{
+	public Issue11677()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children =
+			{
+				new SearchBar
+				{
+					BackgroundColor = Colors.Transparent,
+					Placeholder = "Search"
+				},
+				new Label
+				{
+					Text = "Test SearchBar with Transparent Background",
+					FontSize = 16,
+					TextColor = Colors.Black,
+					AutomationId = "Label"
+				}
+			}
+		};
+	}
+}
+
+
+
+	
