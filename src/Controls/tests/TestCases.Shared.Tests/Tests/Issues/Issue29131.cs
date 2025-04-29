41219014e1cb1bd72e07b0965b0466bce3e0107e
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-04-29 22:18:10 +0530

    Fixed-KeepScrollOffset-Issue

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29131.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue29131 : _IssuesUITest
+{
+	public Issue29131(TestDevice device) : base(device) { }
+
+	public override string Issue => "Android - KeepScrollOffset doesn't not works as expected when new items are added in CollectionView";
+	const string AddNewItem = "AddNewItem";
+	const string ScrollButton = "ScrollButton";
+
+	[Test]
+	[Category(UITestCategories.CollectionView)]
+	public void KeepScrollOffSetShouldWork()
+	{
+		App.WaitForElement("CollectionView");
+		App.Click("KeepScrollOffsetButton");
+		App.Click(ScrollButton);
+		App.Click(AddNewItem);
+		App.WaitForElement("Item 30");
+		App.Click(ScrollButton);
+		App.Click(AddNewItem);
+		App.WaitForElement("Item 32");
+		App.Click(ScrollButton);
+		App.Click(AddNewItem);
+		App.WaitForElement("Item 30");
+	}
+}
