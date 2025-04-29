c31a16cefcdb2d20b6f3f2bc84c1fde81a660664
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-04-29 10:34:43 +0530

    Committed the test case and sample

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue20612.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Maui.Maps;
+
+namespace Maui.Controls.Sample.Issues;
+
+
+[Issue(IssueTracker.Github, 20612, "Disconnecting Map Handler causes Map to crash on second page entrance and moving to region.", PlatformAffected.Android)]
+public class Issue20612 : TestNavigationPage
+{
+    protected override void Init()
+    {
+        PushAsync(new Issue20612page1());
+    }
+}
+
+public class Issue20612page1 : ContentPage
+{
+    public Issue20612page1()
+    {
+        var openMapButton = new Button
+        {
+            Text = "Navigate to Map page",
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+            AutomationId = "MapButton"
+        };
+
+        openMapButton.Clicked += (s, e) =>
+        {
+            Navigation.PushAsync(new Issue20612page2());
+        };
+        Content = openMapButton;
+    }
+}
+
+public class Issue20612page2 : ContentPage
+{
+    private Microsoft.Maui.Controls.Maps.Map _map;
+
+    public Issue20612page2()
+    {
+        _map = new Microsoft.Maui.Controls.Maps.Map
+        {
+            HorizontalOptions = LayoutOptions.Fill,
+            VerticalOptions = LayoutOptions.Fill,
+            BackgroundColor = Colors.Red
+        };
+
+        var goBackButton = new Button
+        {
+            Text = "Go back",
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+            AutomationId = "GoBackButton"
+        };
+        goBackButton.Clicked += GoBack;
+
+        var grid = new Grid();
+        grid.Children.Add(_map);
+        grid.Children.Add(goBackButton);
+
+        Content = grid;
+
+        MoveMap();
+    }
+
+    private async void MoveMap()
+    {
+        await Task.Delay(1000).ConfigureAwait(false);
+
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            var mapSpan = MapSpan.FromCenterAndRadius(
+                new Location(5, 5),
+                Distance.FromMeters(10000));
+
+            _map.MoveToRegion(mapSpan);
+        });
+    }
+
+    void GoBack(object sender, EventArgs e)
+    {
+        _map.Handler?.DisconnectHandler();
+        Navigation.PopAsync();
+    }
+}
