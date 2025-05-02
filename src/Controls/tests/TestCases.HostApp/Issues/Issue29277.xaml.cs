6508bb090c8a23893b6b2d91264626c1b4339d1a
Bhavanesh N <bhavanesh001@gmail.com>
2025-05-02 09:33:14 +0530

    Update UI Test

--- src/Controls/tests/TestCases.HostApp/Issues/Issue29277.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue29277.xaml.cs
@@ -10,13 +10,21 @@ public partial class Issue29277 : Shell
 
 	private void Focus_Clicked(object sender, EventArgs e)
 	{
-		searchHandler.Focus();
-		label.Text="Focused";
+		searchHandler.Focus();	
 	}
 
 	private void Unfocus_Clicked(object sender, EventArgs e)
 	{
 		searchHandler.Unfocus();
+	}
+
+	private void searchHandler_Focused(object sender, EventArgs e)
+	{
+		label.Text="Focused";
+	}
+
+	private void searchHandler_Unfocused(object sender, EventArgs e)
+	{
 		label.Text = "Unfocused";
 	}
 }
