0fdb27576392ba5bf6fdb947f655efcec8d52187
Jakub Florkowski <kubaflo123@gmail.com>
2025-05-05 00:06:11 +0200

    [a11y] support fot the button role to spans with GestureRecognizer

--- src/Controls/src/Core/Platform/SemanticExtensions.cs
+++ src/Controls/src/Core/Platform/SemanticExtensions.cs
@@ -15,6 +15,16 @@
 					return (tgr.Buttons & ButtonsMask.Primary) == ButtonsMask.Primary;
 				}
 			}
+
+			foreach (var gesture in virtualView.GestureController.CompositeGestureRecognizers)
+			{
+				//Accessibility can't handle Tap Recognizers with > 1 tap
+				if (gesture is ChildGestureRecognizer cgr && cgr.GestureRecognizer is TapGestureRecognizer tgr && tgr.NumberOfTapsRequired == 1)
+				{
+					return (tgr.Buttons & ButtonsMask.Primary) == ButtonsMask.Primary;
+				}
+			}
+
 			return false;
 		}
 	}
