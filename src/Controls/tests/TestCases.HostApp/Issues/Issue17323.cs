5a3623009a58a7117886fe97c080f711f1194e61
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-05-05 17:46:27 +0530

    Position Update

--- src/Controls/tests/TestCases.HostApp/Issues/Issue17323.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue17323.cs
@@ -34,7 +34,7 @@ public class Issue17323_Drawable : IDrawable
 		canvas.StrokeSize = 5;
 		canvas.FontSize = 18;
 		canvas.DrawRectangle(dirtyRect);
-		canvas.DrawString("Hello World", dirtyRect.X + 5, dirtyRect.Y + 5, dirtyRect.Width - 10, dirtyRect.Height - 10 - 150, HorizontalAlignment.Left, VerticalAlignment.Center);
-		canvas.DrawString("مرحبا بالعالم", dirtyRect.X + 5, dirtyRect.Y + 5 + 150, dirtyRect.Width - 10, dirtyRect.Height - 10 - 150, HorizontalAlignment.Left, VerticalAlignment.Center);
+		canvas.DrawString("Hello World", dirtyRect.X + 5, dirtyRect.Y + 5 , dirtyRect.Width - 10, dirtyRect.Height - 160, HorizontalAlignment.Left, VerticalAlignment.Center);
+		canvas.DrawString("مرحبا بالعالم", dirtyRect.X + 5, dirtyRect.Y + 20, dirtyRect.Width - 10, dirtyRect.Height - 160, HorizontalAlignment.Left, VerticalAlignment.Center);
 	}
 }
