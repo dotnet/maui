758553b493ed27e8b1e2eba9bf263135d09fd46c
Jakub Florkowski <kubaflo123@gmail.com>
2025-05-06 01:48:21 +0200

    [Android] Button with corner radius shadow broken on Android device

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20596.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue20596 : _IssuesUITest
+{
+	public Issue20596(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "[Android] Button with corner radius shadow broken on Android device";
+
+	[Test]
+	[Category(UITestCategories.Button)]
+	public void ShadowShouldUpdateOnCornerRadiusChange()
+	{
+		App.WaitForElement("UpdateCornerRadiusButton");
+		App.Click("UpdateCornerRadiusButton");
+		VerifyScreenshot();
+	}
+}
