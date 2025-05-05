f83e9582b02c5009f1c3fd9eb05214c67b5839ef
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-05-05 14:25:42 +0530

    Code Changes and TestCase Update

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue17323.cs
@@ -0,0 +1,40 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 17323, "Automatic Flow Direction Change for Arabic Strings in When Drawing the String in MAUI on Android and Opposite Behavior in Windows.", PlatformAffected.UWP)]
+public class Issue17323 : TestContentPage
+{
+	protected override void Init()
+	{
+		VerticalStackLayout _verticalStackLayout = new VerticalStackLayout();
+		GraphicsView _graphicsView = new GraphicsView
+		{
+			Drawable = new Issue17323_Drawable(),
+			HeightRequest = 300,
+			WidthRequest = 350,
+		};
+
+		Label label = new Label
+		{
+			AutomationId = "Label",
+			Text = "The Arabic text should be drawn from left to right on Android for consistent behavior with Windows, iOS, and macOS."
+		};
+
+		_verticalStackLayout.Add(_graphicsView);
+		_verticalStackLayout.Add(label);
+		Content = _verticalStackLayout;
+	}
+}
+
+
+public class Issue17323_Drawable : IDrawable
+{
+	public void Draw(ICanvas canvas, RectF dirtyRect)
+	{
+		canvas.StrokeColor = Colors.Red;
+		canvas.StrokeSize = 5;
+		canvas.FontSize = 18;
+		canvas.DrawRectangle(dirtyRect);
+		canvas.DrawString("Hello World", dirtyRect.X + 5, dirtyRect.Y + 5, dirtyRect.Width - 10, dirtyRect.Height - 10 - 150, HorizontalAlignment.Left, VerticalAlignment.Center);
+		canvas.DrawString("مرحبا بالعالم", dirtyRect.X + 5, dirtyRect.Y + 5 + 150, dirtyRect.Width - 10, dirtyRect.Height - 10 - 150, HorizontalAlignment.Left, VerticalAlignment.Center);
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17323.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue17323 : _IssuesUITest
+{
+	public Issue17323(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Automatic Flow Direction Change for Arabic Strings in When Drawing the String in MAUI on Android and Opposite Behavior in Windows.";
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void ArabicStringShouldBeLeftToRight()
+	{
+		App.WaitForElement("Label");
+		VerifyScreenshot();
+	}
+}
