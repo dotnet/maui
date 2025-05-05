936eccc1cf4aa22d63138749fc93bb0efee9b33a
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-05-05 05:08:33 +0200

    Added UITest

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue8945.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+
+	[Issue(IssueTracker.Github, 8945, "Add Open/Close API to picker controls", PlatformAffected.All)]
+	public partial class Issue8945 : TestContentPage
+	{
+		public Issue8945()
+		{
+			InitializeComponent();
+		}
+
+		protected override void Init()
+		{
+
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			IsOpenPicker.Opened += IsOpenPickerOpened;
+			IsOpenPicker.Closed += IsOpenPickerClosed;
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+
+			IsOpenPicker.Opened -= IsOpenPickerOpened;
+			IsOpenPicker.Closed -= IsOpenPickerClosed;
+		}
+
+		void OnOpenClicked(object sender, EventArgs e)
+		{
+			IsOpenPicker.IsOpen = true;
+		}
+
+		void OnCloseClicked(object sender, EventArgs e)
+		{
+			IsOpenPicker.IsOpen = false;
+		}
+
+		void IsOpenPickerOpened(object sender, PickerOpenedEventArgs e)
+		{
+			StatusLabel.Text = "Picker Opened";
+		}
+
+		void IsOpenPickerClosed(object sender, PickerClosedEventArgs e)
+		{
+			StatusLabel.Text = "Picker Closed";
+		}
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue8945.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue8945 : _IssuesUITest
+	{
+		public Issue8945(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Add Open/Close API to picker controls";
+
+		[Test]
+		[Category(UITestCategories.Picker)]
+		public void OpenClosePicker()
+		{
+			App.WaitForElement("WaitForStubControl");
+			App.Tap("OpenPickerButton");
+			Assert.That(App.WaitForElement("StatusLabel")?.GetText(), Is.EqualTo("Picker Opened"));
+			App.Tap("ClosePickerButton");
+			Assert.That(App.WaitForElement("StatusLabel")?.GetText(), Is.EqualTo("Picker Closed"));
+		}
+	}
+}
