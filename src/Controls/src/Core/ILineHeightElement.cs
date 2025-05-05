bd997fce46331a15a09d90c3851700817647e117
Gerald Versluis <gerald@verslu.is>
2025-05-05 16:48:28 +0300

    Revert "Remove InternalsVisibleTo for Toolkit (#28994)"
    This reverts commit 504aa864f7ceab08da44ce185029c3a026440c9c.

--- src/Controls/src/Core/ILineHeightElement.cs
+++ src/Controls/src/Core/ILineHeightElement.cs
@@ -1,25 +1,13 @@
-namespace Microsoft.Maui.Controls;
+#nullable disable
+using System.ComponentModel;
 
-/// <summary>
-/// Defines properties and methods for elements that support line height customization.
-/// </summary>
-/// <remarks>
-/// This interface is implemented by UI elements that need to control the spacing between lines of text,
-/// providing consistent line height behavior across different text-based controls.
-/// </remarks>
-public interface ILineHeightElement
+namespace Microsoft.Maui.Controls.Internals
 {
-	/// <summary>
-	/// Gets the line height for text displayed by this element.
-	/// </summary>
-	/// <value>A multiplier that determines the spacing between lines of text. A value of 1.0 represents standard line height.</value>
-	double LineHeight { get; }
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	interface ILineHeightElement
+	{
+		double LineHeight { get; }
 
-	/// <summary>
-	/// Called when the <see cref="LineHeight"/> property changes.
-	/// </summary>
-	/// <param name="oldValue">The old value of the property.</param>
-	/// <param name="newValue">The new value of the property.</param>
-	/// <remarks>Implementors should implement this method explicitly.</remarks>
-	void OnLineHeightChanged(double oldValue, double newValue);
+		void OnLineHeightChanged(double oldValue, double newValue);
+	}
 }
--- src/Controls/src/Core/ITextAlignmentElement.cs
+++ src/Controls/src/Core/ITextAlignmentElement.cs
@@ -1,33 +1,14 @@
-namespace Microsoft.Maui.Controls;
-
-/// <summary>
-/// Defines properties and methods for elements that support text alignment.
-/// </summary>
-/// <remarks>
-/// This interface is implemented by UI elements that need to control the horizontal and vertical 
-/// alignment of displayed text.
-/// </remarks>
-public interface ITextAlignmentElement
+#nullable disable
+namespace Microsoft.Maui.Controls
 {
-	/// <summary>
-	/// Gets the horizontal alignment of the text.
-	/// </summary>
-	/// <value>A <see cref="TextAlignment"/> value that specifies how the text is horizontally aligned. The default value depends on the implementing control.</value>
-	/// <remarks>Implementors should implement this property publicly.</remarks>
-	TextAlignment HorizontalTextAlignment { get; }
+	interface ITextAlignmentElement
+	{
+		//note to implementor: implement the properties publicly
+		TextAlignment HorizontalTextAlignment { get; }
 
-	/// <summary>
-	/// Gets the vertical alignment of the text.
-	/// </summary>
-	/// <value>A <see cref="TextAlignment"/> value that specifies how the text is vertically aligned. The default value depends on the implementing control.</value>
-	/// <remarks>Implementors should implement this property publicly.</remarks>
-	TextAlignment VerticalTextAlignment { get; }
+		TextAlignment VerticalTextAlignment { get; }
 
-	/// <summary>
-	/// Called when the <see cref="HorizontalTextAlignment"/> property changes.
-	/// </summary>
-	/// <param name="oldValue">The old value of the property.</param>
-	/// <param name="newValue">The new value of the property.</param>
-	/// <remarks>Implementors should implement this method explicitly.</remarks>
-	void OnHorizontalTextAlignmentPropertyChanged(TextAlignment oldValue, TextAlignment newValue);
+		//note to implementor: but implement the methods explicitly
+		void OnHorizontalTextAlignmentPropertyChanged(TextAlignment oldValue, TextAlignment newValue);
+	}
 }
