7265614633dcc759065ffd72808703e1d2e609a8
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-05-06 12:03:12 +0530

    Have added the test case

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28975.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue28975 : _IssuesUITest
+{
+	public Issue28975(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "Setting the Entry keyboard type to Date results in it behaving like a password entry";
+
+	[Test]
+	[Category(UITestCategories.Entry)]
+	public void EnsureEntryWithDateKeyboardIsNotPassword()
+	{
+		App.WaitForElement("DateEntry");
+		App.EnterText("DateEntry", "2023-10-01");
+		VerifyScreenshot();
+	}
+}
+
+
+
+
