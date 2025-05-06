12c793d40ba1a5ee7d6bea6bb8f0d6d1f91e65fe
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-05-06 11:53:21 +0200

    Updated tests

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue8945.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue8945.cs
@@ -16,11 +16,23 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		[Category(UITestCategories.DatePicker)]
 		public void OpenCloseDatePicker()
 		{
+			Exception? exception = null;
+			
 			App.WaitForElement("WaitForStubControl");
+			App.WaitForElement("OpenDatePickerButton");
 			App.Tap("OpenDatePickerButton");
-			Assert.That(App.WaitForElement("DatePickerStatusLabel")?.GetText(), Is.EqualTo("DatePicker Opened"));
+			Assert.That(App.WaitForElement("DatePickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("DatePicker Opened"));
+			VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Opened");
+			
+			App.WaitForElement("CloseDatePickerButton");
 			App.Tap("CloseDatePickerButton");
-			Assert.That(App.WaitForElement("DatePickerStatusLabel")?.GetText(), Is.EqualTo("DatePicker Closed"));
+			Assert.That(App.WaitForElement("DatePickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("DatePicker Closed"));
+			VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Closed");
+
+			if (exception != null)
+			{
+				throw exception;
+			}
 		}
 
 #if TEST_FAILS_ON_WINDOWS // Pending implementation of Open/Close API for TimePicker on Windows
@@ -28,11 +40,23 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		[Category(UITestCategories.TimePicker)]
 		public void OpenCloseTimePicker()
 		{
+			Exception? exception = null;
+
 			App.WaitForElement("WaitForStubControl");
+			App.WaitForElement("OpenTimePickerButton");
 			App.Tap("OpenTimePickerButton");
-			Assert.That(App.WaitForElement("TimePickerStatusLabel")?.GetText(), Is.EqualTo("TimePicker Opened"));
+			Assert.That(App.WaitForElement("TimePickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("TimePicker Opened"));
+			VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Opened");
+
+			App.WaitForElement("CloseTimePickerButton");
 			App.Tap("CloseTimePickerButton");
-			Assert.That(App.WaitForElement("TimePickerStatusLabel")?.GetText(), Is.EqualTo("TimePicker Closed"));
+			Assert.That(App.WaitForElement("TimePickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("TimePicker Closed"));
+			VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Closed");
+
+			if (exception != null)
+			{
+				throw exception;
+			}
 		}
 #endif
 
@@ -40,11 +64,23 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		[Category(UITestCategories.Picker)]
 		public void OpenClosePicker()
 		{
+			Exception? exception = null;
+
 			App.WaitForElement("WaitForStubControl");
+			App.WaitForElement("OpenPickerButton");
 			App.Tap("OpenPickerButton");
-			Assert.That(App.WaitForElement("PickerStatusLabel")?.GetText(), Is.EqualTo("Picker Opened"));
+			Assert.That(App.WaitForElement("PickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("Picker Opened"));
+			VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Opened");
+
+			App.WaitForElement("ClosePickerButton");
 			App.Tap("ClosePickerButton");
-			Assert.That(App.WaitForElement("PickerStatusLabel")?.GetText(), Is.EqualTo("Picker Closed"));
+			Assert.That(App.WaitForElement("PickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("Picker Closed"));
+			VerifyScreenshotOrSetException(ref exception, TestContext.CurrentContext.Test.MethodName + "_Closed");
+
+			if (exception != null)
+			{
+				throw exception;
+			}
 		}
 	}
 }
