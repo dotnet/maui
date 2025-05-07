86bdf24d07137ce7886e81730eca7e245ed9f4f7
nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>
2025-05-07 09:33:12 +0530

    Added the CarouselView FeatureMatrix to the hostapp

---
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/CarouselView/CarouselViewOptionsPage.xaml.cs
@@ -0,0 +1,286 @@
+namespace Maui.Controls.Sample;
+
+public partial class CarouselViewOptionsPage : ContentPage
+{
+    private CarouselViewViewModel _viewModel;
+
+    public CarouselViewOptionsPage(CarouselViewViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+
+    private async void ApplyButton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+
+    private void OnEmptyViewChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (EmptyViewNone.IsChecked)
+        {
+            _viewModel.EmptyView = null; // No empty view
+        }
+        else if (EmptyViewString.IsChecked)
+        {
+            _viewModel.EmptyView = "No items available"; // String
+        }
+        else if (EmptyViewCustomView.IsChecked)
+        {
+            _viewModel.EmptyView = new StackLayout
+            {
+                Children =
+                    {
+                        new Label
+                        {
+                            Text = "No items available",
+                            FontSize = 18,
+                            HorizontalOptions = LayoutOptions.Center,
+                            VerticalOptions = LayoutOptions.Center,
+                            TextColor = Colors.Red
+                        },
+                        new Image
+                        {
+                            Source = "dotnet_bot.png",
+                            HeightRequest = 120,
+                            WidthRequest = 120,
+                            HorizontalOptions = LayoutOptions.Center
+                        }
+                    }
+            };
+            _viewModel.EmptyViewTemplate = null;
+
+        }
+
+        else if (EmptyViewDataTemplate.IsChecked)
+        {
+            _viewModel.EmptyView = null;
+            _viewModel.EmptyViewTemplate = new DataTemplate(() =>
+            {
+                return new StackLayout
+                {
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                    new Label
+                    {
+                        Text = "No items available (DataTemplate)",
+                        TextColor = Colors.Red,
+                        FontSize = 16,
+                        HorizontalOptions = LayoutOptions.Center
+                    },
+                    new Image
+                    {
+                        Source = "dotnet_bot.png",
+                        HeightRequest = 100,
+                        WidthRequest = 100,
+                        HorizontalOptions = LayoutOptions.Center
+                    }
+                    }
+                };
+            });
+        }
+    }
+
+
+    private void OnItemTemplateChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (ItemTemplateNone.IsChecked)
+        {
+            _viewModel.ItemTemplate = null; 
+        }
+        else if (ItemTemplateString.IsChecked)
+        {
+            _viewModel.ItemTemplate = new DataTemplate(() =>
+            {
+                var label = new Label
+                {
+                    Margin = new Thickness(10),
+                    FontSize = 18,
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center
+                };
+                label.SetBinding(Label.TextProperty, ".");
+                return label;
+            });
+        }
+        else if (ItemTemplateGrid.IsChecked)
+        {
+            _viewModel.ItemTemplate = new DataTemplate(() =>
+            {
+                Grid grid = new Grid
+                {
+                    BackgroundColor = Colors.LightGray,
+                    HeightRequest = 200,
+                    WidthRequest = 300,
+                    Margin = new Thickness(10)
+                };
+
+                var label = new Label
+                {
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                    FontSize = 24,
+                    TextColor = Colors.DarkBlue
+                };
+                label.SetBinding(Label.TextProperty, ".");
+
+                grid.Children.Add(label);
+                return grid;
+            });
+        }
+        else if (ItemTemplateCustomView.IsChecked)
+        {
+            _viewModel.ItemTemplate = new DataTemplate(() =>
+            {
+                var frame = new Frame
+                {
+                    BorderColor = Colors.Blue,
+                    CornerRadius = 10,
+                    Margin = new Thickness(20),
+                    HeightRequest = 300,
+                    WidthRequest = 300,
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center
+                };
+
+                var stackLayout = new StackLayout
+                {
+                    Spacing = 10
+                };
+
+                var label = new Label
+                {
+                    FontSize = 24,
+                    TextColor = Colors.DarkBlue,
+                    HorizontalOptions = LayoutOptions.Center
+                };
+                label.SetBinding(Label.TextProperty, ".");
+
+                var image = new Image
+                {
+                    Source = "dotnet_bot.png",
+                    HeightRequest = 150,
+                    WidthRequest = 150,
+                    HorizontalOptions = LayoutOptions.Center
+                };
+
+                stackLayout.Children.Add(label);
+                stackLayout.Children.Add(image);
+                frame.Content = stackLayout;
+
+                return frame;
+            });
+        }
+    }
+
+    private void OnItemsSourceChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (ItemsSourceNone.IsChecked)
+        {
+            _viewModel.ItemsSourceType = CarouselViewItemsSourceType.None;
+        }
+        else if (ObservableCollection.IsChecked)
+        {
+            _viewModel.ItemsSourceType = CarouselViewItemsSourceType.ObservableCollection;
+        }
+    }
+
+    private void OnItemsLayoutChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (ItemsLayoutVertical.IsChecked)
+        {
+            _viewModel.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical);
+        }
+        else if (ItemsLayoutHorizontal.IsChecked)
+        {
+            _viewModel.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Horizontal);
+        }
+    }
+
+
+    private void OnHorizontalScrollBarVisibilityChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (HScrollDefault.IsChecked)
+        {
+            _viewModel.HorizontalScrollBarVisibility = ScrollBarVisibility.Default;
+        }
+        else if (HScrollAlways.IsChecked)
+        {
+            _viewModel.HorizontalScrollBarVisibility = ScrollBarVisibility.Always;
+        }
+        else if (HScrollNever.IsChecked)
+        {
+            _viewModel.HorizontalScrollBarVisibility = ScrollBarVisibility.Never;
+        }
+    }
+
+    private void OnVerticalScrollBarVisibilityChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (VScrollDefault.IsChecked)
+        {
+            _viewModel.VerticalScrollBarVisibility = ScrollBarVisibility.Default;
+        }
+        else if (VScrollAlways.IsChecked)
+        {
+            _viewModel.VerticalScrollBarVisibility = ScrollBarVisibility.Always;
+        }
+        else if (VScrollNever.IsChecked)
+        {
+            _viewModel.VerticalScrollBarVisibility = ScrollBarVisibility.Never;
+        }
+    }
+
+    private void OnItemsUpdatingScrollModeChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (KeepItemsInView.IsChecked)
+        {
+            _viewModel.ItemsUpdatingScrollMode = ItemsUpdatingScrollMode.KeepItemsInView;
+        }
+        else if (KeepScrollOffset.IsChecked)
+        {
+            _viewModel.ItemsUpdatingScrollMode = ItemsUpdatingScrollMode.KeepScrollOffset;
+        }
+        else if (KeepLastItemInView.IsChecked)
+        {
+            _viewModel.ItemsUpdatingScrollMode = ItemsUpdatingScrollMode.KeepLastItemInView;
+        }
+    }
+
+    private void OnPeekAreaInsetsEntryCompleted(object sender, EventArgs e)
+    {
+        if (sender is Entry entry)
+        {
+            if (double.TryParse(entry.Text, out double value))
+            {
+                value = Math.Clamp(value, 50, 200);
+
+                if (sender == LeftInset)
+                {
+                    _viewModel.LeftPeekInset = value;
+                    entry.Text = value.ToString();
+                }
+                else if (sender == RightInset)
+                {
+                    _viewModel.RightPeekInset = value;
+                    entry.Text = value.ToString();
+                }
+            }
+        }
+    }
+
+    private void OnPositionEntryCompleted(object sender, EventArgs e)
+    {
+        if (sender is Entry entry)
+        {
+            if (int.TryParse(entry.Text, out int value))
+            {
+                value = Math.Clamp(value, 0, 4);
+                _viewModel.Position = value;
+                entry.Text = value.ToString();
+            }
+        }
+    }
+}
---
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/CarouselView/CarouselViewViewModel.cs
@@ -0,0 +1,290 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq;
+
+namespace Maui.Controls.Sample
+{
+    public enum CarouselViewItemsSourceType
+    {
+        None,
+        ObservableCollection,
+    }
+
+    public class CarouselViewViewModel : INotifyPropertyChanged
+    {
+        private object _emptyView;
+        private DataTemplate _emptyViewTemplate;
+        private DataTemplate _itemTemplate;
+        private CarouselViewItemsSourceType _itemsSourceType = CarouselViewItemsSourceType.ObservableCollection;
+        private bool _isLoopEnabled = true;
+        private bool _isSwipeEnabled = true;
+        private bool _isBounceEnabled = true;
+        private bool _isScrollAnimated = true;
+        private Thickness _peekAreaInsets;
+        private int _position;
+        private object _currentItem;
+        private IItemsLayout _itemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical);
+
+        private ScrollBarVisibility _horizontalScrollBarVisibility = ScrollBarVisibility.Default;
+        private ScrollBarVisibility _verticalScrollBarVisibility = ScrollBarVisibility.Default;
+        private ItemsUpdatingScrollMode _itemsUpdatingScrollMode;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CarouselViewViewModel()
+        {
+            LoadItems();
+        }
+
+        public ObservableCollection<string> Items { get; private set; }
+
+        private void LoadItems()
+        {
+            Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5"
+            };
+
+            OnPropertyChanged(nameof(Items));
+            OnPropertyChanged(nameof(ItemsSource));
+        }
+
+        public object EmptyView
+        {
+            get => _emptyView;
+            set { _emptyView = value; OnPropertyChanged(); }
+        }
+
+        public DataTemplate EmptyViewTemplate
+        {
+            get => _emptyViewTemplate;
+            set { _emptyViewTemplate = value; OnPropertyChanged(); }
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get => _itemTemplate;
+            set { _itemTemplate = value; OnPropertyChanged(); }
+        }
+
+        public CarouselViewItemsSourceType ItemsSourceType
+        {
+            get => _itemsSourceType;
+            set
+            {
+                if (_itemsSourceType != value)
+                {
+                    _itemsSourceType = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(ItemsSource));
+                }
+            }
+        }
+
+        public bool Loop
+        {
+            get => _isLoopEnabled;
+            set
+            {
+                if (_isLoopEnabled != value)
+                {
+                    _isLoopEnabled = value;
+                    OnPropertyChanged();
+                                OnPropertyChanged(nameof(ItemsSource));
+
+                }
+            }
+        }
+
+        public bool IsSwipeEnabled
+        {
+            get => _isSwipeEnabled;
+            set
+            {
+                if (_isSwipeEnabled != value)
+                {
+                    _isSwipeEnabled = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsBounceEnabled
+        {
+            get => _isBounceEnabled;
+            set
+            {
+                if (_isBounceEnabled != value)
+                {
+                    _isBounceEnabled = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsScrollAnimated
+        {
+            get => _isScrollAnimated;
+            set
+            {
+                if (_isScrollAnimated != value)
+                {
+                    _isScrollAnimated = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Thickness PeekAreaInsets
+        {
+            get => _peekAreaInsets;
+            set
+            {
+                if (_peekAreaInsets != value)
+                {
+                    _peekAreaInsets = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private double _leftPeekInset;
+        public double LeftPeekInset
+        {
+            get => _leftPeekInset;
+            set
+            {
+                if (_leftPeekInset != value)
+                {
+                    _leftPeekInset = value;
+                    PeekAreaInsets = new Thickness(_leftPeekInset, 0, _rightPeekInset, 0);
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private double _rightPeekInset;
+        public double RightPeekInset
+        {
+            get => _rightPeekInset;
+            set
+            {
+                if (_rightPeekInset != value)
+                {
+                    _rightPeekInset = value;
+                    PeekAreaInsets = new Thickness(_leftPeekInset, 0, _rightPeekInset, 0);
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int Position
+        {
+            get => _position;
+            set
+            {
+                if (_position != value)
+                {
+                    _position = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public object CurrentItem
+        {
+            get => _currentItem;
+            set
+            {
+                if (!object.Equals(_currentItem, value))
+                {
+                    _currentItem = value;
+                    if (_currentItem != null && Items != null)
+                    {
+                        int index = Items.IndexOf(_currentItem.ToString());
+                        if (index != -1 && index != _position)
+                        {
+                            _position = index;
+                            OnPropertyChanged(nameof(Position));
+                        }
+                    }
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ScrollBarVisibility HorizontalScrollBarVisibility
+        {
+            get => _horizontalScrollBarVisibility;
+            set
+            {
+                if (_horizontalScrollBarVisibility != value)
+                {
+                    _horizontalScrollBarVisibility = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ScrollBarVisibility VerticalScrollBarVisibility
+        {
+            get => _verticalScrollBarVisibility;
+            set
+            {
+                if (_verticalScrollBarVisibility != value)
+                {
+                    _verticalScrollBarVisibility = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ItemsUpdatingScrollMode ItemsUpdatingScrollMode
+        {
+            get => _itemsUpdatingScrollMode;
+            set
+            {
+                if (_itemsUpdatingScrollMode != value)
+                {
+                    _itemsUpdatingScrollMode = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public IItemsLayout ItemsLayout
+        {
+            get => _itemsLayout;
+            set
+            {
+                if (_itemsLayout != value)
+                {
+                    _itemsLayout = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public object ItemsSource
+        {
+            get
+            {
+                return ItemsSourceType switch
+                {
+                    CarouselViewItemsSourceType.ObservableCollection => Items,
+                    CarouselViewItemsSourceType.None => null,
+                    _ => Items
+                };
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
