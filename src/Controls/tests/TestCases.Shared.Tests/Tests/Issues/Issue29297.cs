1cf997557e312fc8c2775223bb7781d8834f5291
KarthikRajaKalaimani <92777139+KarthikRajaKalaimani@users.noreply.github.com>
2025-05-07 12:12:51 +0530

    Test case added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29297.cs
@@ -0,0 +1,27 @@
+#if !ANDROID
+//Skipping the test case to run in android because the loaded event is not triggered when navigating back to previous page.
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue29297 : _IssuesUITest
+{
+	public Issue29297(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Crash on shell navigation for Mac Catalyst";
+
+	[Test]
+	[Category(UITestCategories.Shell)]
+	public void Shell_Issue21916()
+	{
+		App.WaitForElement("Button");
+		App.Click("Button");
+		App.WaitForElement("Button");
+		App.Click("Button");
+		App.WaitForElement("Successfully navigated back");
+	}
+}
+#endif
