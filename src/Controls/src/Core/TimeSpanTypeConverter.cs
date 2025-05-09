af6a95e7898209ff2c16319a1feca2d4905c9ac2
Edward Miller <symbiogenesis@outlook.com>
2025-05-09 16:48:33 -0500

    handle more destination types

--- src/Controls/src/Core/TimeSpanTypeConverter.cs
+++ src/Controls/src/Core/TimeSpanTypeConverter.cs
@@ -8,10 +8,10 @@ namespace Microsoft.Maui.Controls;
 public class TimeSpanTypeConverter : TypeConverter, IExtendedTypeConverter
 {
     public override bool CanConvertFrom(ITypeDescriptorContext? context, Type? sourceType)
-        => sourceType == typeof(string) || sourceType == typeof(TimeOnly);
+        => sourceType == typeof(string) || sourceType == typeof(TimeOnly) || sourceType == typeof(TimeSpan);
 
     public override bool CanConvertTo(ITypeDescriptorContext? context, Type? destinationType)
-        => destinationType == typeof(string) || destinationType == typeof(TimeSpan);
+        => destinationType == typeof(string) || destinationType == typeof(TimeSpan) || destinationType == typeof(TimeOnly);
 
     object IExtendedTypeConverter.ConvertFromInvariantString(string value, IServiceProvider serviceProvider)
     {
@@ -39,16 +39,65 @@ public class TimeSpanTypeConverter : TypeConverter, IExtendedTypeConverter
     {
         if (value is TimeSpan timeSpan)
         {
-            if (destinationType == typeof(string))
-            {
-                return timeSpan.ToString();
-            }
-            if (destinationType == typeof(TimeOnly))
-            {
-                return TimeOnly.FromTimeSpan(timeSpan);
-            }
+            return ConvertToDestinationType(timeSpan, destinationType, culture);
         }
+		else if (value is TimeOnly timeOnly)
+		{
+			return ConvertToDestinationType(timeOnly, destinationType, culture);
+		}
+		else if (value is string stringValue)
+		{
+			if (destinationType == typeof(string))
+			{
+				return stringValue;
+			}
+			if (TimeOnly.TryParse(stringValue, culture, out timeOnly))
+			{
+				return ConvertToDestinationType(timeOnly, destinationType, culture);
+			}
+			else if (TimeSpan.TryParse(stringValue, culture, out var timeSpan))
+			{
+				return ConvertToDestinationType(timeSpan, destinationType, culture);
+			}
+		}
+
         throw new NotSupportedException($"Cannot convert \"{value}\" into {destinationType}");
     }
+
+	private static object ConvertToDestinationType(TimeOnly timeOnly, Type destinationType, CultureInfo? culture)
+	{
+		if (destinationType == typeof(string))
+		{
+			return timeOnly.ToString(culture);
+		}
+		if (destinationType == typeof(TimeSpan))
+		{
+			return timeOnly.ToTimeSpan();
+		}
+		if (destinationType == typeof(TimeOnly))
+		{
+			return timeOnly;
+		}
+
+		throw new NotSupportedException($"Cannot convert \"{value}\" into {destinationType}");
+	}
+
+	private static object ConvertToDestinationType(TimeSpan timeSpan, Type destinationType, CultureInfo? culture)
+	{
+		if (destinationType == typeof(string))
+		{
+			return timeSpan.ToString(culture);
+		}
+		if (destinationType == typeof(TimeSpan))
+		{
+			return timeSpan;
+		}
+		if (destinationType == typeof(TimeOnly))
+		{
+			return TimeOnly.FromTimeSpan(timeSpan);
+		}
+
+		throw new NotSupportedException($"Cannot convert \"{value}\" into {destinationType}");
+	}
 }
 #endif
