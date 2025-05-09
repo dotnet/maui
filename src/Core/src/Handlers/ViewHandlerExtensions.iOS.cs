722e73d6a68aca88780f69bc8f8cc80f3d98a502
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-05-09 15:25:01 +0530

    Updated the fix.

--- src/Core/src/Handlers/ViewHandlerExtensions.iOS.cs
+++ src/Core/src/Handlers/ViewHandlerExtensions.iOS.cs
@@ -102,8 +102,8 @@ namespace Microsoft.Maui
 			}
 			else if (platformView is MauiLabel)
 			{
-				widthConstraint = IsExplicitSet(virtualView.Width) ? virtualView.Width : widthConstraint;
-				heightConstraint = IsExplicitSet(virtualView.Height) ? virtualView.Height : heightConstraint;
+				widthConstraint = IsExplicitSet(virtualView.Width) ? virtualView.Width : widthConstraint >= 0 ? widthConstraint : float.PositiveInfinity;
+				heightConstraint = IsExplicitSet(virtualView.Height) ? virtualView.Height : heightConstraint >= 0 ? heightConstraint : float.PositiveInfinity;
 				sizeThatFits = platformView.SizeThatFits(new CGSize((float)widthConstraint, (float)heightConstraint));
 			}
 			else
