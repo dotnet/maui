26584cd801cd018299d0155f445b66d75b7a58fd
Jakub Florkowski <kubaflo123@gmail.com>
2025-05-11 18:05:50 +0200

    Added a UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue605.cs
@@ -0,0 +1,23 @@
+﻿#if ANDROID
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue605 : _IssuesUITest
+{
+	public Issue605(TestDevice testDevice) : base(testDevice) { }
+
+	public override string Issue => "Allow text selection and copy in readonly Entry";
+
+	[Test]
+	[Category(UITestCategories.Entry)]
+	public void TextSelectionShouldBeEnabledInReadonlyEditor()
+	{
+		App.WaitForElement("Entry");
+		App.LongPress("Entry");
+		VerifyScreenshot();
+	}
+}
+#endif
