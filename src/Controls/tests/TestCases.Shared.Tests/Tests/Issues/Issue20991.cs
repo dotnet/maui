0203231a1a8ccbe008c811891d7b4e4ad1d77478
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-05-12 17:09:29 +0530

    Fixed Custom Drawable does not support binding

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20991.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue20991 : _IssuesUITest
+{
+	public Issue20991(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Custom IDrawable control does not support binding";
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void GraphicsViewDrawableShouldSupportBinding()
+	{
+		App.WaitForElement("descriptionLabel");
+		VerifyScreenshot();
+	}
+}
