903a651769fb91b00cd46f0764dbe93704b32dd8
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-05-13 20:16:25 +0530

    Updated the test case.

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29192.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29192.cs
@@ -1,3 +1,4 @@
+#if TEST_FAILS_ON_WINDOWS && TEST_FAILS_ON_CATALYST && TEST_FAILS_ON_IOS // CollectionView MeasureFirstItem sizing not applied on Windows, iOS and macOS) https://github.com/dotnet/maui/issues/29130
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
@@ -18,3 +19,4 @@ public class Issue29192 : _IssuesUITest
 		VerifyScreenshot();
 	}
 }
+#endif
