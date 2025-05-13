b0b23a594ef9e2ea308259e9e206e0c700121079
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-05-13 16:01:13 +0530

    test case added

--- src/Controls/src/Core/Handlers/Items/CarouselViewHandler.cs
+++ src/Controls/src/Core/Handlers/Items/CarouselViewHandler.cs
@@ -21,9 +21,7 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			[Controls.CarouselView.PeekAreaInsetsProperty.PropertyName] = MapPeekAreaInsets,
 			[Controls.CarouselView.IsBounceEnabledProperty.PropertyName] = MapIsBounceEnabled,
 			[Controls.CarouselView.PositionProperty.PropertyName] = MapPosition,
-			#if ANDROID || IOS
 			[Controls.CarouselView.LoopProperty.PropertyName] = MapLoop,
-			#endif
 			[Controls.CarouselView.CurrentItemProperty.PropertyName] = MapCurrentItem
 		};
 	}
