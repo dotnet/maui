d467d1c3973dcc25aec005d88d5875b51627609b
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-05-13 11:42:39 +0530

    Refactored-conditional-block

--- src/Controls/src/Core/Handlers/Items/Android/Adapters/EmptyViewAdapter.cs
+++ src/Controls/src/Core/Handlers/Items/Android/Adapters/EmptyViewAdapter.cs
@@ -332,9 +332,13 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 				var itemHeight = size.Height;
 
 				if (isHeader)
+				{
 					_headerHeight = itemHeight;
+				}
 				else
+				{
 					_footerHeight = itemHeight;
+				}
 			}
 		}
 
@@ -345,13 +349,13 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 				switch (collectionView.ItemsLayout)
 				{
 					case LinearItemsLayout linearLayout:
-						{
-							return linearLayout.Orientation == ItemsLayoutOrientation.Vertical;
-						}
+				    {
+						return linearLayout.Orientation == ItemsLayoutOrientation.Vertical;
+					}
 					case GridItemsLayout gridItemsLayout:
-						{
-							return gridItemsLayout.Orientation == ItemsLayoutOrientation.Vertical;
-						}
+					{
+						return gridItemsLayout.Orientation == ItemsLayoutOrientation.Vertical;
+					}
 				}
 			}
 			// Default
