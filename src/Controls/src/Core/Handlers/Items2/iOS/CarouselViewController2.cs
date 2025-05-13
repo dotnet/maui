8a48675d4f546ac4b2c19fd2d7ed33b14d7f83db
Bhavanesh N <bhavanesh001@gmail.com>
2025-05-13 22:32:34 +0530

    Fixi failing UI tests

--- src/Controls/src/Core/Handlers/Items2/iOS/CarouselViewController2.cs
+++ src/Controls/src/Core/Handlers/Items2/iOS/CarouselViewController2.cs
@@ -368,6 +368,11 @@ namespace Microsoft.Maui.Controls.Handlers.Items2
 		}
 		internal void UpdateLoop()
 		{
+			if (!InitialPositionSet)
+			{
+				return;
+			}
+			
 			if (ItemsView is not CarouselView carousel)
 			{
 				return;
