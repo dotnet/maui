a71c2378fd76c8beb0c9d8e84c04f2bfe9419968
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-05-13 12:39:06 +0530

    Adding TestCase

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10041.cs
@@ -0,0 +1,32 @@
+﻿#nullable disable
+using System.Drawing;
+using System.Globalization;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue10041 : _IssuesUITest
+	{
+		public Issue10041(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Throws exception when shell item is null";
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void ShouldThrowExceptionWhenShellItemIsNull()
+		{
+
+			Assert.Throws<InvalidOperationException>(() =>
+			{
+				// App.WaitForElement("CloseButton");
+			});
+			
+
+		}
+	}
+}
