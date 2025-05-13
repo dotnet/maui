ac758847d9fc1e292bb791189e4106fb26ee5c37
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-05-13 16:01:13 +0530

    test case added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue29411.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 29411, "[Android] CarouselView.Loop = false causes crash on Android when changed at runtime", PlatformAffected.Android)]
+public partial class Issue29411 : ContentPage
+{
+	ObservableCollection<string> _items;
+
+    public Issue29411()
+    {
+        InitializeComponent();
+
+        _items = new ObservableCollection<string>
+        {
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 4",
+            "Item 5",
+        };
+
+        carouselView.ItemsSource = _items;
+
+        carouselView.ItemTemplate = new DataTemplate(() =>
+        {
+            var label = new Label
+            {
+                FontSize = 18,
+                Margin = new Thickness(10),
+                BackgroundColor = Colors.LightGray
+            };
+            label.SetBinding(Label.TextProperty, "."); // Proper binding
+
+            return label;
+        });
+    }
+
+    void OnDisabledButtonClicked(object sender, EventArgs e)
+    {
+        carouselView.Loop = !carouselView.Loop;
+    }
+
+    void OnPositionChangeClicked(object sender, EventArgs e)
+    {
+        carouselView.Position = 2;
+    }
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29411.cs
@@ -0,0 +1,25 @@
+﻿#if TEST_FAILS_ON_CATALYST				// On Catalyst, Swipe actions not supported in Appium.
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue29411 : _IssuesUITest
+{
+	public Issue29411(TestDevice device) : base(device) { }
+
+	public override string Issue => "[Android] CarouselView.Loop = false causes crash on Android when changed at runtime";
+
+	[Test]
+	[Category(UITestCategories.CarouselView)]
+	public void VerifyLoopChangeAtRuntimeShouldNotCrash()
+	{
+		App.WaitForElement("CarouselView");
+		App.Tap("Button");
+		App.SwipeLeftToRight("CarouselView");
+		App.WaitForElement("CarouselView");
+	}
+}
+
+#endif
