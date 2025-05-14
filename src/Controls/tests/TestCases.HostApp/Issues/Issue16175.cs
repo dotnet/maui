1a0f0e19afc75b3d592a0cb0a237403029fe6111
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-05-14 18:26:22 +0530

    Added testcases.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue16175.cs
@@ -0,0 +1,46 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 16175, "OnNavigatedTo was not triggered when tabs in More section", PlatformAffected.iOS)]
+public class Issue16175 : NavigationPage
+{
+	public Issue16175() : base(new Issue16175MainPage())
+	{
+
+	}
+}
+
+public class Issue16175MainPage : TabbedPage
+{
+	public Issue16175MainPage()
+	{
+		for (var i = 0; i < 10; i++)
+		{
+			Children.Add(new Issue16175Page(i.ToString()));
+		}
+	}
+}
+
+public class Issue16175Page : ContentPage
+{
+	private readonly Label _navigatedToLabel;
+
+	public Issue16175Page(string title)
+	{
+		Title = title;
+		_navigatedToLabel = new Label { Text = "NavigatedTo: Not triggered", AutomationId = "navigatedToLabel" };
+
+		Content = new VerticalStackLayout
+		{
+			Children = { _navigatedToLabel }
+		};
+	}
+
+	protected override void OnNavigatedTo(NavigatedToEventArgs args)
+	{
+		if (Parent is TabbedPage tabbed && tabbed.CurrentPage?.Title.Equals("8", StringComparison.Ordinal) == true)
+		{
+			_navigatedToLabel.Text = "NavigatedTo: Triggered";
+		}
+		base.OnNavigatedTo(args);
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16175.cs
@@ -0,0 +1,28 @@
+﻿#if IOS || MACCATALYST // More tab is available in MAC and iOS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue16175 : _IssuesUITest
+	{
+		public Issue16175(TestDevice testDevice) : base(testDevice)
+		{
+		}
+		public override string Issue => "OnNavigatedTo was not triggered when tabs in More section";
+
+		[Test]
+		[Category(UITestCategories.TabbedPage)]
+		public void TabbedPageTabEventsTriggeredInMoreSection()
+		{
+			App.WaitForElement("More");
+			App.Tap("More");
+			App.WaitForElement("8");
+			App.Tap("8");
+			var navigatedToLabel = App.WaitForElement("navigatedToLabel");
+			Assert.That(navigatedToLabel.GetText(), Is.EqualTo("NavigatedTo: Triggered"));
+		}
+	}
+}
+#endif
