ef52a88b25df92efa7a3dbb2cb2fe36047d44b82
Simon Rozsival <simon@rozsival.com>
2025-05-14 11:02:14 +0200

    Migrate NavigationPage to the new handler registration pattern

--- src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
+++ src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
@@ -95,7 +95,6 @@ public static partial class AppHostBuilderExtensions
 #endif
 
 #if IOS || MACCATALYST
-		handlersCollection.AddHandler(typeof(NavigationPage), typeof(Handlers.Compatibility.NavigationRenderer));
 		handlersCollection.AddHandler(typeof(TabbedPage), typeof(Handlers.Compatibility.TabbedRenderer));
 		handlersCollection.AddHandler(typeof(FlyoutPage), typeof(Handlers.Compatibility.PhoneFlyoutPageRenderer));
 #endif
