fb2a5c8a3260f30534b8a16f52732f7099d15ab4
Simon Rozsival <simon@rozsival.com>
2025-05-14 09:38:19 +0200

    Migrate Stepper to the new handler registration pattern

--- src/Controls/src/Core/Stepper/Stepper.Mapper.cs
+++ src/Controls/src/Core/Stepper/Stepper.Mapper.cs
@@ -3,8 +3,12 @@ using System;
 namespace Microsoft.Maui.Controls;
 public partial class Stepper
 {
-	internal static new void RemapForControls()
+	static Stepper() => RemapForControls();
+
+	private static new void RemapForControls()
 	{
+		VisualElement.RemapIfNeeded();
+
 		StepperHandler.Mapper.AppendToMapping(nameof(Stepper.Increment), MapInterval);
 	}
 
