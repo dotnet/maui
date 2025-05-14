90e58dd94db69aefef62afe4d4620909bb367fe1
Simon Rozsival <simon@rozsival.com>
2025-05-14 16:02:16 +0200

    Pass Android context to the ctor

--- src/Core/src/Handlers/Element/ElementHandlerAttribute.cs
+++ src/Core/src/Handlers/Element/ElementHandlerAttribute.cs
@@ -5,13 +5,24 @@ namespace Microsoft.Maui.Handlers;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, Inherited = true, AllowMultiple = false)]
 internal abstract class ElementHandlerAttribute : Attribute
 {
-	public abstract IElementHandler CreateHandler();
+	public abstract IElementHandler CreateHandler(IMauiContext context);
 	public abstract Type HandlerType { get; }
 }
 
 internal sealed class ElementHandlerAttribute<THandler> : ElementHandlerAttribute
 	where THandler : IElementHandler, new()
 {
-	public override IElementHandler CreateHandler() => new THandler();
+	public override IElementHandler CreateHandler(IMauiContext context) => new THandler();
 	public override Type HandlerType => typeof(THandler);
 }
+
+#if ANDROID
+internal sealed class ElementHandlerWithAndroidContextAttribute<THandler> : ElementHandlerAttribute
+	where THandler : IElementHandler, IElementHandlerWithAndroidContext<THandler>
+{
+	public override IElementHandler CreateHandler(IMauiContext context)
+		=> THandler.CreateHandler(context.Context);
+
+	public override Type HandlerType => typeof(THandler);
+}
+#endif
