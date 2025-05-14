9b057461202ac9c86e4e95fba2515f1cff0abcbc
Simon Rozsival <simon@rozsival.com>
2025-05-14 16:04:03 +0200

    Migrate Android's Frame to the new handler registration pattern

--- src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
+++ src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
@@ -66,6 +66,7 @@ public static partial class AppHostBuilderExtensions
 #if ANDROID
 #pragma warning disable CS0618 // Type or member is obsolete
 		handlersCollection.AddHandler(typeof(Frame), typeof(Handlers.Compatibility.FrameRenderer));
+		handlersCollection.AddHandler(typeof(TableView), typeof(Handlers.Compatibility.TableViewRenderer));
 #pragma warning restore CS0618 // Type or member is obsolete
 #endif
 
