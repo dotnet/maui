3a4f5335870622bb91d8359a0cfdc92e39652568
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-05-14 13:08:15 +0530

    Modified the test case

--- src/Controls/tests/TestCases.HostApp/Issues/Issue29411.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue29411.xaml.cs
@@ -5,13 +5,34 @@ namespace Maui.Controls.Sample.Issues;
 [Issue(IssueTracker.Github, 29411, "[Android] CarouselView.Loop = false causes crash on Android when changed at runtime", PlatformAffected.Android)]
 public partial class Issue29411 : ContentPage
 {
-	ObservableCollection<string> _items;
-
+    CarouselViewViewModel _viewModel;
     public Issue29411()
     {
         InitializeComponent();
+		_viewModel = new CarouselViewViewModel();
+        BindingContext = _viewModel;
+	}
 
-        _items = new ObservableCollection<string>
+    void OnButtonClicked(object sender, EventArgs e)
+    {
+        if (carouselView.Loop)
+        {
+            carouselView.Loop = false;
+             
+        }
+    }
+}
+
+
+public class CarouselViewViewModel : BindableObject
+{
+    public ObservableCollection<string> ItemsSource { get; set; }
+ 
+	public DataTemplate ItemTemplate { get; set; }
+   
+    public CarouselViewViewModel()
+    {
+        ItemsSource = new ObservableCollection<string>
         {
             "Item 1",
             "Item 2",
@@ -19,25 +40,19 @@ public partial class Issue29411 : ContentPage
             "Item 4",
             "Item 5",
         };
-
-        carouselView.ItemsSource = _items;
-
-        carouselView.ItemTemplate = new DataTemplate(() =>
+ 
+       ItemTemplate = new DataTemplate(() =>
         {
             var label = new Label
             {
-                FontSize = 18,
                 Margin = new Thickness(10),
+                FontSize = 18,
+                AutomationId = "Label", 
                 BackgroundColor = Colors.LightGray
+                 
             };
-            label.SetBinding(Label.TextProperty, "."); // Proper binding
-
+            label.SetBinding(Label.TextProperty, ".");
             return label;
         });
-    }
-
-    void OnDisabledButtonClicked(object sender, EventArgs e)
-    {
-        carouselView.Loop = !carouselView.Loop;
     }
 }
