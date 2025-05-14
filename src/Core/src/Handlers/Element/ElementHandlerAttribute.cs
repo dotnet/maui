4100252b463d5c91252f84d2bfd73d0c14ec26e4
Simon Rozsival <simon@rozsival.com>
2025-05-14 09:56:41 +0200

    Migrate IContentView to the new handler registration pattern

--- src/Core/src/Handlers/Element/ElementHandlerAttribute.cs
+++ src/Core/src/Handlers/Element/ElementHandlerAttribute.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Microsoft.Maui.Handlers;
 
-[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, Inherited = true, AllowMultiple = false)]
 internal abstract class ElementHandlerAttribute : Attribute
 {
 	public abstract IElementHandler CreateHandler();
