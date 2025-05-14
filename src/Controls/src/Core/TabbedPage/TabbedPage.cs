cded9f38b9ff389330b3eae16223fe8eda36a5e9
Simon Rozsival <simon@rozsival.com>
2025-05-14 11:06:46 +0200

    Migrate TabbedPage to the new handler registration pattern

--- src/Controls/src/Core/TabbedPage/TabbedPage.cs
+++ src/Controls/src/Core/TabbedPage/TabbedPage.cs
@@ -6,6 +6,11 @@ namespace Microsoft.Maui.Controls
 {
 	/// <summary>A <see cref="MultiPage{T}"/> that displays an array of tabs across the top of the screen, each of which loads content onto the screen.</summary>
 	[ContentProperty(nameof(Children))]
+#if IOS || MACCATALYST
+	[ElementHandler<Handlers.Compatibility.NavigationRenderer>]
+#elif WINDOWS || ANDROID || TIZEN
+	[ElementHandler<TabbedViewHandler>]
+#endif
 	public partial class TabbedPage : MultiPage<Page>, IBarElement, IElementConfiguration<TabbedPage>, ITabbedView
 	{
 		/// <summary>Bindable property for <see cref="BarBackgroundColor"/>.</summary>
