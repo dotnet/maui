a85527722a605a6ad0ee963fcf1dfee8f3f4876e
Simon Rozsival <simon@rozsival.com>
2025-05-14 09:41:25 +0200

    Migrate TimePicker to the new handler registration pattern

--- src/Controls/src/Core/TimePicker/TimePicker.Mapper.cs
+++ src/Controls/src/Core/TimePicker/TimePicker.Mapper.cs
@@ -5,8 +5,12 @@ namespace Microsoft.Maui.Controls
 {
 	public partial class TimePicker
 	{
-		internal static new void RemapForControls()
+		static TimePicker() => RemapForControls();
+
+		private static new void RemapForControls()
 		{
+			VisualElement.RemapIfNeeded();
+
 			// Adjust the mappings to preserve Controls.TimePicker legacy behaviors
 #if IOS
 			TimePickerHandler.Mapper.ReplaceMapping<TimePicker, ITimePickerHandler>(PlatformConfiguration.iOSSpecific.TimePicker.UpdateModeProperty.PropertyName, MapUpdateMode);
