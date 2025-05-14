d606981e4838b84426bd45f60dd6fcceee92a559
Simon Rozsival <simon@rozsival.com>
2025-05-14 09:29:52 +0200

    Migrate Picker to the new handler registration pattern

--- src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
+++ src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
@@ -62,7 +62,6 @@ public static partial class AppHostBuilderExtensions
 
 	internal static IMauiHandlersCollection AddControlsHandlers(this IMauiHandlersCollection handlersCollection)
 	{
-		handlersCollection.AddHandler<Picker, PickerHandler>();
 		handlersCollection.AddHandler<ProgressBar, ProgressBarHandler>();
 		handlersCollection.AddHandler<ScrollView, ScrollViewHandler>();
 		handlersCollection.AddHandler<SearchBar, SearchBarHandler>();
@@ -251,7 +250,6 @@ public static partial class AppHostBuilderExtensions
 		Toolbar.RemapForControls();
 		Window.RemapForControls();
 		SwipeView.RemapForControls();
-		Picker.RemapForControls();
 		SearchBar.RemapForControls();
 		Stepper.RemapForControls();
 		TabbedPage.RemapForControls();
