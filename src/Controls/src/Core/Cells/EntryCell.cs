849ae0b784c8ea1f4e9521f6be0bee945cbcfccf
Simon Rozsival <simon@rozsival.com>
2025-05-14 11:39:57 +0200

    Migrate ImageCell and EntryCell to the new registration pattern

--- src/Controls/src/Core/Cells/EntryCell.cs
+++ src/Controls/src/Core/Cells/EntryCell.cs
@@ -7,6 +7,11 @@ namespace Microsoft.Maui.Controls
 {
 	/// <include file="../../../docs/Microsoft.Maui.Controls/EntryCell.xml" path="Type[@FullName='Microsoft.Maui.Controls.EntryCell']/Docs/*" />
 	[Obsolete("The controls which use EntryCell (ListView and TableView) are obsolete. Please use CollectionView instead.")]
+#if WINDOWS || ANDROID || IOS || MACCATALYST
+#pragma warning disable CS0618 // Type or member is obsolete
+	[ElementHandler<Handlers.Compatibility.EntryCellRenderer>]
+#pragma warning restore CS0618 // Type or member is obsolete
+#endif
 	public class EntryCell : Cell, ITextAlignmentElement, IEntryCellController, ITextAlignment
 	{
 		/// <summary>Bindable property for <see cref="Text"/>.</summary>
