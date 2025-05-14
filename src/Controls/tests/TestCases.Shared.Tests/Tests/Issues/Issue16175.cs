ca5c3096384e534521f6909232b6371c1623b27e
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-05-14 23:21:59 +0530

    updated testcase.

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16175.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16175.cs
@@ -3,26 +3,25 @@ using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
-{
-	public class Issue16175 : _IssuesUITest
-	{
-		public Issue16175(TestDevice testDevice) : base(testDevice)
-		{
-		}
-		public override string Issue => "OnNavigatedTo was not triggered when tabs in More section";
+namespace Microsoft.Maui.TestCases.Tests.Issues;
 
-		[Test]
-		[Category(UITestCategories.TabbedPage)]
-		public void TabbedPageTabEventsTriggeredInMoreSection()
-		{
-			App.WaitForElement("More");
-			App.Tap("More");
-			App.WaitForElement("8");
-			App.Tap("8");
-			var navigatedToLabel = App.WaitForElement("navigatedToLabel");
-			Assert.That(navigatedToLabel.GetText(), Is.EqualTo("NavigatedTo: Triggered"));
-		}
+public class Issue16175 : _IssuesUITest
+{
+	public Issue16175(TestDevice testDevice) : base(testDevice)
+	{
+	}
+	public override string Issue => "OnNavigatedTo was not triggered when tabs in More section";
+
+	[Test]
+	[Category(UITestCategories.TabbedPage)]
+	public void TabbedPageTabEventsTriggeredInMoreSection()
+	{
+		App.WaitForElement("More");
+		App.Tap("More");
+		App.WaitForElement("8");
+		App.Tap("8");
+		var navigatedToLabel = App.WaitForElement("navigatedToLabel");
+		Assert.That(navigatedToLabel.GetText(), Is.EqualTo("NavigatedTo: Triggered"));
 	}
 }
 #endif
