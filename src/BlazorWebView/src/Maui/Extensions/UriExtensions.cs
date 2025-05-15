0baadbd37c96799812abf254a0848b85b90360d8
Matthew Leibowitz <mattleibow@live.com>
2025-05-15 22:21:33 +0200

    Update UriExtensions.cs

--- src/BlazorWebView/src/Maui/Extensions/UriExtensions.cs
+++ src/BlazorWebView/src/Maui/Extensions/UriExtensions.cs
@@ -1,25 +1,24 @@
 ﻿using System;
 using System.IO;
 
-namespace Microsoft.AspNetCore.Components.WebView.Maui
+namespace Microsoft.AspNetCore.Components.WebView.Maui;
+
+internal static class UriExtensions      
 {
-	internal static class UriExtensions
+	internal static bool IsBaseOfPage(this Uri baseUri, string? uriString)
 	{
-	    internal static bool IsBaseOfPage(this Uri baseUri, string? uriString)
-	    {
-	        if (string.IsNullOrWhiteSpace(uriString))
-	            return false;
-	
-	        if (!Uri.TryCreate(uriString, UriKind.Absolute, out var uri))
-	            return false;
-	
-	        if (uri.Scheme is not ("http" or "https"))
-	            return false;
-	
-	        if (Path.HasExtension(uri.AbsolutePath))
-	            return false;
-	
-	        return baseUri.IsBaseOf(uri);
-	    }
+		if (string.IsNullOrWhiteSpace(uriString))
+			return false;
+
+		if (!Uri.TryCreate(uriString, UriKind.Absolute, out var uri))
+			return false;
+
+		if (uri.Scheme is not ("http" or "https"))
+			return false;
+
+		if (Path.HasExtension(uri.AbsolutePath))
+			return false;
+
+		return baseUri.IsBaseOf(uri);
 	}
 }
