5561f093b02a60f5a826295348418393e200b45b
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-05-15 16:22:46 +0530

    Fixed the Issue with the TitleBar TrailingContent not being properly aligned on macOS

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29516.cs
@@ -0,0 +1,23 @@
+﻿#if MACCATALYST || WINDOWS
+// TitleBar is only available on Mac Catalyst and Windows. https://learn.microsoft.com/en-us/dotnet/maui/user-interface/controls/titlebar?view=net-maui-9.0
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue29516 : _IssuesUITest
+{
+    public Issue29516(TestDevice device) : base(device) { }
+
+    public override string Issue => "Issue with the TitleBar TrailingContent not being properly aligned on macOS";
+
+    [Test]
+    [Category(UITestCategories.Window)]
+    public void TitleBarTrailingContentShouldRenderProperly()
+    {
+        App.WaitForElement("ContentLabel");
+        VerifyScreenshot();
+    }
+}
+#endif
