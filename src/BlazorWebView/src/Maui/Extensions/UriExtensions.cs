74bb09d6f1f83a03fb2198631f1279afe7957b29
tw4 <88425310+tw4@users.noreply.github.com>
2025-05-16 21:45:42 +0300

    fix: Allow force reload for URLs with dots in query parameters

--- src/BlazorWebView/src/Maui/Extensions/UriExtensions.cs
+++ src/BlazorWebView/src/Maui/Extensions/UriExtensions.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.IO;
 
-namespace Microsoft.AspNetCore.Components.WebView.Maui
-{
-	internal static class UriExtensions
-	{
-		internal static bool IsBaseOfPage(this Uri baseUri, string? uriString)
-		{
-			if (Path.HasExtension(uriString))
-			{
-				// If the path ends in a file extension, it's not referring to a page.
-				return false;
-			}
+namespace Microsoft.AspNetCore.Components.WebView.Maui;
 
-			var uri = new Uri(uriString!);
-			return baseUri.IsBaseOf(uri);
-		}
+internal static class UriExtensions
+{
+	internal static bool IsBaseOfPage(this Uri baseUri, string? uriString)
+	{
+		if (string.IsNullOrWhiteSpace(uriString))
+			return false;
+
+		if (!Uri.TryCreate(uriString, UriKind.Absolute, out var uri))
+			return false;
+
+		if (uri.Scheme is not ("http" or "https"))
+			return false;
+
+		if (Path.HasExtension(uri.AbsolutePath))
+			return false;
+
+		return baseUri.IsBaseOf(uri);
 	}
 }
