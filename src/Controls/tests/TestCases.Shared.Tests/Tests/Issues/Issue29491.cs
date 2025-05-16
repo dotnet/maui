7f5a7d9ab0f50422f2e6b102552c746406961256
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-05-16 16:38:11 +0530

    test added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29491.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue29491 : _IssuesUITest
+{
+	public override string Issue => "[CV2][CollectionView] Changing CollectionView's ItemsSource in runtime removes elements' parent seemingly random";
+
+	public Issue29491(TestDevice device)
+	: base(device)
+	{ }
+
+	[Category(UITestCategories.CollectionView)]
+	public void VerifyDataTemplateParentisNotNull()
+	{
+		App.WaitForElement("Button");
+		App.Tap("Button");
+		App.WaitForElement("Micorosoft.Maui.Controls.CollectionView");
+	}
+}
