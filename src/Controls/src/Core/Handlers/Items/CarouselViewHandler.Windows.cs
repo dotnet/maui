af0a81cdf9d29361ab1e58ef81e8e1e1464ecdc7
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-05-16 19:45:02 +0530

    Updated the fix

--- src/Controls/src/Core/Handlers/Items/CarouselViewHandler.Windows.cs
+++ src/Controls/src/Core/Handlers/Items/CarouselViewHandler.Windows.cs
@@ -382,7 +382,9 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			var currentItemPosition = GetItemPositionInCarousel(ItemsView.CurrentItem);
 
 			if (currentItemPosition < 0 || currentItemPosition >= ItemCount || ItemsView.Position == currentItemPosition)
+			{
 				return;
+			}
 
 			ItemsView.ScrollTo(currentItemPosition, position: ScrollToPosition.Center, animate: ItemsView.AnimateCurrentItemChanges);
 		}
