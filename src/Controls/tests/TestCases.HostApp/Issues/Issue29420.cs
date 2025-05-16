4f07eb7d9b10ae5db28b7e6d2f860ee96b6ab26a
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-05-16 19:05:04 +0530

    Added the test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue29420.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Maui.Controls.Sample.Issues;
+[Issue(IssueTracker.Github, 29420, "KeepLastInView Not Working as Expected in CarouselView", PlatformAffected.UWP)]
+public class Issue29420 : ContentPage
+{
+	public Issue29420()
+	{
+		var count = 0;
+		var verticalStackLayout = new VerticalStackLayout();
+		var carouselItems = new ObservableCollection<string>
+		{
+			"Item 0", "Item 1","Item 2", "Item 3", "Item 4", "Item 5",
+
+		};
+
+		CarouselView carouselView = new CarouselView
+		{
+			ItemsSource = carouselItems,
+			AutomationId = "CarouselView",
+			Loop = true,
+			ItemsUpdatingScrollMode = ItemsUpdatingScrollMode.KeepLastItemInView,
+
+			ItemTemplate = new DataTemplate(() =>
+			{
+				var grid = new Grid
+				{
+					Padding = 10
+				};
+
+				var label = new Label
+				{
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+					FontSize = 18,
+					Background = Colors.Pink,
+				};
+				label.SetBinding(Label.TextProperty, ".");
+				label.SetBinding(Label.AutomationIdProperty, ".");
+
+				grid.Children.Add(label);
+				return grid;
+			}),
+		};
+
+		var addButton = new Button
+		{
+			Text = "Insert item at 0th index",
+			AutomationId = "Button",
+			Margin = new Thickness(20),
+		};
+		
+		addButton.Clicked += (sender, e) =>
+		{
+			carouselItems.Insert(0, "NewItem" + count.ToString());
+			count++;
+		};
+
+		verticalStackLayout.Children.Add(addButton);
+		verticalStackLayout.Children.Add(carouselView);
+		Content = verticalStackLayout;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29420.cs
@@ -0,0 +1,27 @@
+﻿#if TEST_FAILS_ON_IOS || TEST_FAILS_ON_CATALYST // CarouselView Fails to Keep Last Item in View on iOS and macOS https://github.com/dotnet/maui/issues/18029
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue29420 : _IssuesUITest
+{
+	public override string Issue => "KeepLastInView Not Working as Expected in CarouselView";
+
+	public Issue29420(TestDevice device) : base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.CarouselView)]
+	public void VerifyCarouselViewKeepLastInViewOnItemAdd()
+	{
+		App.WaitForElement("CarouselView");
+		for (var i = 0; i < 5; i++)
+		{
+			App.Tap("Button");
+		}
+		
+		App.WaitForElement("Item 5");
+	}
+}
+#endif
