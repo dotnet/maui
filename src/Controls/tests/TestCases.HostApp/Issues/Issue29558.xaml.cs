ca36e04b797f7d7f4938d85c417a59e5c639658a
Jakub Florkowski <kubaflo123@gmail.com>
2025-05-18 17:42:16 +0200

    [Android] Input controls should not change keyboard visibility - test

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue29558.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 29558, "Input controls should not change keyboard visibility", PlatformAffected.Android)]
+public partial class Issue29558 : Shell
+{
+	int _action = 0;
+
+	public Issue29558()
+	{
+		InitializeComponent();
+	}
+
+	private void UnfocusButton_Clicked(object sender, EventArgs e)
+	{
+		if (_action == 0)
+		{
+			searchHandler.Unfocus();
+		}
+		else if (_action == 1)
+		{
+			entry.Unfocus();
+		}
+		else if (_action == 2)
+		{
+			editor.Unfocus();
+		}
+		_action++;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29558.cs
@@ -0,0 +1,36 @@
+﻿#if ANDROID
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue29558 : _IssuesUITest
+{
+	public Issue29558(TestDevice testDevice) : base(testDevice) { }
+
+	public override string Issue => "Input controls should not change keyboard visibility";
+
+	[Test]
+	[Category(UITestCategories.SoftInput)]
+	public void Issue29558InputViewShouldNotHideOnUnfocusCall()
+	{
+		bool isKeyboardVisible;
+		App.WaitForElement("unfocusButton");
+
+		List<string> inputControls = ["entry", "editor", "searchHandler"];
+		foreach (var control in inputControls)
+		{
+			App.Click(control);
+			isKeyboardVisible = App.IsKeyboardShown();
+			Assert.That(isKeyboardVisible, Is.True);
+			App.Click("unfocusButton");
+			isKeyboardVisible = App.IsKeyboardShown();
+			Assert.That(isKeyboardVisible, Is.True);
+			App.DismissKeyboard();
+			isKeyboardVisible = App.IsKeyboardShown();
+			Assert.That(isKeyboardVisible, Is.False);
+		}
+	}
+}
+#endif
