f9587a116655163a3474bec103558faf42e1e833
Jakub Florkowski <kubaflo123@gmail.com>
2025-05-18 14:11:29 +0200

    [.NET10] Added An API to SearchHandler so users can hide or show the softkeyboard
    Added a UI Test

--- src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/SearchHandlerAppearanceTracker.cs
+++ src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/SearchHandlerAppearanceTracker.cs
@@ -33,6 +33,8 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility
 			_fontManager = fontManager;
 			_searchHandler = searchHandler;
 			_searchHandler.PropertyChanged += SearchHandlerPropertyChanged;
+			_searchHandler.ShowKeyboardRequested += OnShowKeyboardRequested;
+			_searchHandler.HideKeyboardRequested += OnHideKeyboardRequested;
 			_searchHandler.FocusChangeRequested += SearchHandlerFocusChangeRequested;
 			_uiSearchBar = searchBar;
 			_uiSearchBar.OnEditingStarted += OnEditingStarted;
@@ -341,6 +343,17 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility
 			_uiSearchBar.ResignFirstResponder();
 		}
 
+		void OnShowKeyboardRequested(object sender, EventArgs e)
+		{
+			_uiSearchBar?.BecomeFirstResponder();
+		}
+
+		void OnHideKeyboardRequested(object sender, EventArgs e)
+		{
+			_uiSearchBar?.ResignFirstResponder();
+		}
+
+
 		UIToolbar CreateNumericKeyboardAccessoryView()
 		{
 			var keyboardWidth = UIScreen.MainScreen.Bounds.Width;
@@ -398,6 +411,8 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility
 				{
 					_searchHandler.FocusChangeRequested -= SearchHandlerFocusChangeRequested;
 					_searchHandler.PropertyChanged -= SearchHandlerPropertyChanged;
+					_searchHandler.ShowKeyboardRequested -= OnShowKeyboardRequested;
+					_searchHandler.HideKeyboardRequested -= OnHideKeyboardRequested;
 				}
 				_searchHandler = null;
 				_uiSearchBar = null;
