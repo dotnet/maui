17392438ba4d3e0b1961571230ed8f9c3b265532
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2025-05-19 15:52:29 +0530

    Update CarouselViewHandler.Windows.cs

--- src/Controls/src/Core/Handlers/Items/CarouselViewHandler.Windows.cs
+++ src/Controls/src/Core/Handlers/Items/CarouselViewHandler.Windows.cs
@@ -196,7 +196,6 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			// that will handle this so we want to skip this mapper call. We need to wait for the LIstView to be ready
 			if (handler.InitialPositionSet)
 			{
-				handler.IsProgrammaticScroll = true;
 				handler.UpdatePosition();
 			}
 
@@ -222,7 +221,6 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			handler.UpdateLoop();
 		}
 
-		internal bool IsProgrammaticScroll { get; private set; }
 		internal bool InitialPositionSet { get; private set; }
 
 
@@ -502,15 +500,10 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 
 			if (position == Element.Position)
 			{
-				IsProgrammaticScroll = false;
 				return;
 			}
 
-			if (!IsProgrammaticScroll)
-			{
-				IsProgrammaticScroll = false;
-				SetCarouselViewPosition(position);
-			}
+			SetCarouselViewPosition(position);
 		}
 
 		void OnScrollViewChanging(object sender, ScrollViewerViewChangingEventArgs e)
