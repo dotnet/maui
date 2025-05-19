c69bd8c951776253d3cdca90d03de336946367f4
tw4 <88425310+tw4@users.noreply.github.com>
2025-05-19 10:42:55 +0300

    style: use curly braces for single-line statements

--- src/BlazorWebView/src/Maui/Extensions/UriExtensions.cs
+++ src/BlazorWebView/src/Maui/Extensions/UriExtensions.cs
@@ -8,16 +8,25 @@ internal static class UriExtensions
 	internal static bool IsBaseOfPage(this Uri baseUri, string? uriString)
 	{
 		if (string.IsNullOrWhiteSpace(uriString))
+		{
 			return false;
+		}
 
 		if (!Uri.TryCreate(uriString, UriKind.Absolute, out var uri))
+		{
 			return false;
+		}
 
 		if (uri.Scheme is not ("http" or "https"))
+		{
+
 			return false;
+		}
 
 		if (Path.HasExtension(uri.AbsolutePath))
+		{
 			return false;
+		}
 
 		if (!string.Equals(baseUri.Host, uri.Host, StringComparison.OrdinalIgnoreCase) &&
 			!uri.Host.EndsWith("." + baseUri.Host, StringComparison.OrdinalIgnoreCase))
