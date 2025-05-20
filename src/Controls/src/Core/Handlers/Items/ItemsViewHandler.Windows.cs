f60c374586864057fcd4851b30fdbf77dcc0b825
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2025-05-20 18:56:01 +0530

    Update ItemsViewHandler.Windows.cs

--- src/Controls/src/Core/Handlers/Items/ItemsViewHandler.Windows.cs
+++ src/Controls/src/Core/Handlers/Items/ItemsViewHandler.Windows.cs
@@ -451,7 +451,9 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			if (Element is CarouselView)
 			{
 				if (!e.IsIntermediate)
+				{
 					HandleScroll(_scrollViewer);
+				}
 			}
 			else
 			{
