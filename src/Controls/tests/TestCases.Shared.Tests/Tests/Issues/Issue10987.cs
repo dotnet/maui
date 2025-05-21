487a6fd64a5987232736a0fef9132a63ec9b0f67
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-05-21 10:45:46 +0530

    modified test case

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10987.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10987.cs
@@ -1,40 +1,39 @@
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue10987 : _IssuesUITest
 {
-	public class Issue10987 : _IssuesUITest
+	const string LTREditor = "LTREditor";
+	const string RTLEditor = "RTLEditor";
+	public Issue10987(TestDevice device) : base(device) { }
+
+	public override string Issue => "Editor HorizontalTextAlignment Does not Works.";
+
+	[Test]
+	[Category(UITestCategories.Editor)]
+	public void EditorPlaceholderRuntimeTextAlignmentChanged()
 	{
-		const string Editor = "Editor";
-		const string RTLEditor = "RTLEditor";
-		public Issue10987(TestDevice device) : base(device) { }
+		App.WaitForElement(RTLEditor);
+		App.Tap("UpdateAlignmentButton");
+		VerifyScreenshot();
+	}
 
-		public override string Issue => "Editor HorizontalTextAlignment Does not Works.";
-
-		[Test]
-		[Category(UITestCategories.Editor)]
-		public void EditorPlaceholderRuntimeTextAlignmentChanged()
-		{
-			App.WaitForElement(RTLEditor);
-			App.Tap("Button");
-			VerifyScreenshot();
-		}
-
-		[Test]
-		[Category(UITestCategories.Editor)]
-		public void EditorRuntimeTextAlignmentChanged()
-		{
-			//To make sure editor is focused in mac platform
-			App.Tap(Editor);
-			App.EnterText(Editor, "Editor Text");
-			App.WaitForElement(RTLEditor);
-			App.EnterText(RTLEditor, "RTL Editor");
+	[Test]
+	[Category(UITestCategories.Editor)]
+	public void EditorRuntimeTextAlignmentChanged()
+	{
+		//To make sure editor is focused in mac platform
+		App.Tap(LTREditor);
+		App.EnterText(LTREditor, "Editor Text");
+		App.WaitForElement(RTLEditor);
+		App.EnterText(RTLEditor, "RTL Editor");
 #if IOS
-			var app = App as AppiumApp;
-			KeyboardScrolling.HideKeyboard(app!, app!.Driver, true);
+		var app = App as AppiumApp;
+		KeyboardScrolling.HideKeyboard(app!, app!.Driver, true);
 #endif
-			App.Tap("ResetButton");
-			VerifyScreenshot();
-		}
+		App.Tap("ResetAlignmentButton");
+		VerifyScreenshot();
 	}
 }
