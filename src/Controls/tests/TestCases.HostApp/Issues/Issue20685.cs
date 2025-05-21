255cedcb1a5c4767f9f7bc1f81da0613d6e5f9ab
Anandhan Rajagopal <97146406+anandhan-rajagopal@users.noreply.github.com>
2025-05-21 11:28:44 +0530

    code optimization

--- src/Controls/tests/TestCases.HostApp/Issues/Issue20685.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue20685.cs
@@ -1,111 +1,79 @@
-using System.Windows.Input;
+namespace Maui.Controls.Sample.Issues;
 
-namespace Maui.Controls.Sample.Issues
+[Issue(IssueTracker.Github, 20685, "MenuBarItem Commands not working on Mac Catalyst",
+    PlatformAffected.All)]
+public class Issue20685 : TestShell
 {
-    [Issue(IssueTracker.Github, 20685, "MenuBarItem Commands not working on Mac Catalyst",
-        PlatformAffected.All)]
-    public class Issue20685 : TestShell
+    protected override void Init()
     {
-        protected override void Init()
+        var resultLabel = new Label
         {
-            // Create a label to display results
-            var resultLabel = new Label
+            Text = "No action performed yet",
+            FontSize = 18,
+            HorizontalOptions = LayoutOptions.Center,
+            AutomationId = "ResultLabel"
+        };
+
+        var menuItems = new MenuBarItem { Text = "MenuItems" };
+       
+
+        // 1. Menu item using Clicked event
+        var clickedItem = new MenuFlyoutItem
+        {
+            Text = "Test Clicked Event",
+            AutomationId = "ClickedEventItem"
+        };
+        clickedItem.Clicked += (s, e) => resultLabel.Text = "Clicked event handler executed";
+        menuItems.Add(clickedItem);
+
+        // 2. Menu item using Command
+        var commandItem = new MenuFlyoutItem
+        {
+            Text = "Test Command",
+            AutomationId = "CommandItem",
+            Command = new Command(() => resultLabel.Text = "Command executed")
+        };
+        menuItems.Add(commandItem);
+
+        // 3. Menu item using Command with parameter
+        var parameterItem = new MenuFlyoutItem
+        {
+            Text = "Test Command Parameter",
+            AutomationId = "CommandWithParamItem",
+            Command = new Command<string>(param => resultLabel.Text = $"Command executed with parameter: {param}"),
+            CommandParameter = "Test Parameter"
+        };
+        menuItems.Add(parameterItem);
+
+        MenuBarItems.Add(menuItems);
+
+        var contentPage = new ContentPage
+        {
+            Content = new VerticalStackLayout
             {
-                Text = "No action performed yet",
-                FontSize = 18,
-                HorizontalOptions = LayoutOptions.Center,
+                Padding = new Thickness(20),
+                Spacing = 20,
+                HorizontalOptions = LayoutOptions.Fill,
                 VerticalOptions = LayoutOptions.Center,
-                AutomationId = "ResultLabel"
-            };
-
-            // Set up MenuBarItems with both Clicked and Command handlers
-            var menuBarItem1 = new MenuBarItem { Text = "Clicked Events" };
-            var menuBarItem2 = new MenuBarItem { Text = "Commands" };
-            var menuBarItem3 = new MenuBarItem { Text = "Command With Param" };
-            var menuBarItem4 = new MenuBarItem { Text = "Disabled" };
-
-            // Add an item that uses the Clicked event
-            var clickedItem = new MenuFlyoutItem
-            {
-                Text = "Clicked Event Item",
-                AutomationId = "ClickedEventItem"
-            };
-            clickedItem.Clicked += (s, e) =>
-            {
-                resultLabel.Text = "Clicked event handler executed";
-            };
-            menuBarItem1.Add(clickedItem);
-
-            // Add an item that uses a Command
-            var commandItem = new MenuFlyoutItem
-            {
-                Text = "Command Item",
-                AutomationId = "CommandItem",
-                Command = new Command(() =>
-                {
-                    resultLabel.Text = "Command executed";
-                })
-            };
-            menuBarItem2.Add(commandItem);
-
-            // Add an item that uses a Command with parameter
-            var commandWithParamItem = new MenuFlyoutItem
-            {
-                Text = "Command with Parameter",
-                AutomationId = "CommandWithParamItem",
-                Command = new Command<string>((param) =>
-                {
-                    resultLabel.Text = $"Command executed with parameter: {param}";
-                }),
-                CommandParameter = "Test Parameter"
-            };
-            menuBarItem3.Add(commandWithParamItem);
-
-            // Add a disabled item to test the disabled state
-            var disabledItem = new MenuFlyoutItem
-            {
-                Text = "Disabled Item",
-                AutomationId = "DisabledItem",
-                IsEnabled = false
-            };
-            menuBarItem4.Add(disabledItem);
-
-            // Add the menu bar items to the Shell
-            MenuBarItems.Add(menuBarItem1);
-            MenuBarItems.Add(menuBarItem2);
-            MenuBarItems.Add(menuBarItem3);
-            MenuBarItems.Add(menuBarItem4);
-
-            // Create a content page with a simple layout
-            var contentPage = new ContentPage
-            {
-                Content = new VerticalStackLayout
-                {
-                    Padding = new Thickness(20),
-                    Spacing = 20,
-                    HorizontalOptions = LayoutOptions.Fill,
-                    VerticalOptions = LayoutOptions.Center,
-                    Children =
+                Children =
                     {
-                        new Label 
+                        new Label
                         {
-                            Text = "MenuFlyoutItem Delegate Test",
+                            Text = "MenuFlyoutItem Test",
                             FontSize = 24,
                             HorizontalOptions = LayoutOptions.Center
                         },
                         resultLabel,
                         new Label
                         {
-                            Text = "Use the menu bar items at the top of the screen to test delegate implementations",
+                            Text = "Use the menu bar at the top to test the commands",
                             HorizontalOptions = LayoutOptions.Center,
                             HorizontalTextAlignment = TextAlignment.Center
                         }
                     }
-                }
-            };
-            
-            // Add the content page to the Shell
-            AddContentPage(contentPage);
-        }
+            }
+        };
+
+        AddContentPage(contentPage);
     }
 }
--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20685.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20685.cs
@@ -1,65 +1,58 @@
-#if MACCATALYST || WINDOWS// MenuBarItems are only supported on desktop platforms
-using System;
-using System.Threading.Tasks;
+#if MACCATALYST || WINDOWS // MenuBarItems are only supported on desktop platforms
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+[Category(UITestCategories.Shell)]
+public class Issue20685 : _IssuesUITest
 {
-    public class Issue20685 : _IssuesUITest
+
+    public const string ResultLabel = "ResultLabel";
+    public const string MenuItems = "MenuItems";
+    public const string ClickedEventItem = "Test Clicked Event";
+    public const string CommandItem = "Test Command";
+    public const string CommandWithParamItem = "Test Command Parameter";
+
+    public Issue20685(TestDevice testDevice) : base(testDevice)
     {
-        public Issue20685(TestDevice testDevice) : base(testDevice)
-        {
-        }
+    }
 
-        public override string Issue => "MenuBarItem Commands not working on Mac Catalyst";
+    public override string Issue => "MenuBarItem Commands not working on Mac Catalyst";
 
-        [Test]
-        [Category(UITestCategories.Shell)]
-        public void MenuFlyoutItem_ClickedEventWorks()
-        {
-            // Click on the clicked events menu item
-            App.WaitForElement("Clicked Events");
-            App.Tap("Clicked Events");
-            App.WaitForElement("Clicked Event Item");
-            App.Tap("Clicked Event Item");
+    [Test]
+    public void MenuFlyoutItem_ClickedEventWorks()
+    {
+        App.WaitForElement(MenuItems);
+        App.Tap(MenuItems);
+        App.WaitForElement(ClickedEventItem);
+        App.Tap(ClickedEventItem);
 
-            // Verify the result is displayed correctly
-            Assert.That(App.WaitForElement("ResultLabel")?.GetText(), Is.EqualTo("Clicked event handler executed"));
-        }
+        Assert.That(App.WaitForElement(ResultLabel)?.GetText(), Is.EqualTo("Clicked event handler executed"));
+    }
 
-        [Test]
-        [Category(UITestCategories.Shell)]
-        public void MenuFlyoutItem_CommandWorks()
-        {
-            // Click on the commands menu item
-            App.WaitForElement("Commands");
-            App.Tap("Commands");
-            //App.TapMenuBarItem("Commands");
-            App.WaitForElement("Command Item");
-            App.Tap("Command Item");
+    [Test]
+    public void MenuFlyoutItem_CommandWorks()
+    {
+        App.WaitForElement(MenuItems);
+        App.Tap(MenuItems);
+        App.WaitForElement(CommandItem);
+        App.Tap(CommandItem);
 
-            // Verify the result is displayed correctly
-            Assert.That(App.WaitForElement("ResultLabel")?.GetText(), Is.EqualTo("Command executed"));
-        }
+        Assert.That(App.WaitForElement(ResultLabel)?.GetText(), Is.EqualTo("Command executed"));
+    }
 
-        [Test]
-        [Category(UITestCategories.Shell)]
-        public void MenuFlyoutItem_CommandWithParameterWorks()
-        {
-            // Click on the commands with parameter menu item
-            App.WaitForElement("Command With Param");
-            App.Tap("Command With Param");
-            //App.TapMenuBarItem("Command With Param");
-            App.WaitForElement("Command with Parameter");
-            App.Tap("Command with Parameter");
+    [Test]
+    public void MenuFlyoutItem_CommandWithParameterWorks()
+    {
+        App.WaitForElement(MenuItems);
+        App.Tap(MenuItems);
+        App.WaitForElement(CommandWithParamItem);
+        App.Tap(CommandWithParamItem);
 
-            // Verify the result is displayed correctly
-            Assert.That(App.WaitForElement("ResultLabel")?.GetText(), Is.EqualTo("Command executed with parameter: Test Parameter"));
-        }
-
-       
+        Assert.That(App.WaitForElement(ResultLabel)?.GetText(), Is.EqualTo("Command executed with parameter: Test Parameter"));
     }
 }
+
 #endif
