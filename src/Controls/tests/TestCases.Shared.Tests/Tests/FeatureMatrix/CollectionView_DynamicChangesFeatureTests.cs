ddccf576fb96dad253a82809e1448f1421d343d4
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-05-23 13:08:58 +0530

    Revert unwanted changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/CollectionView_DynamicChangesFeatureTests.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/CollectionView_DynamicChangesFeatureTests.cs
@@ -3,7 +3,6 @@ using UITest.Appium;
 using UITest.Core;
 
 namespace Microsoft.Maui.TestCases.Tests;
-
 public class CollectionView_DynamicChangesFeatureTests : UITest
 {
 	public const string DynamicChangesFeatureMatrix = "CollectionView Feature Matrix";
