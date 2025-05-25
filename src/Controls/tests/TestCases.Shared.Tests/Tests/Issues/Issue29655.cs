6657f9febd2c9c4432aedfc4f0c2e9883a37674c
Jakub Florkowski <kubaflo123@gmail.com>
2025-05-25 17:55:33 +0200

    Implemented BackButtonBehavior Clicked Event

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29655.cs
@@ -0,0 +1,26 @@
+﻿#if ANDROID || IOS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue29655 : _IssuesUITest
+{
+	public Issue29655(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "BackButtonBehavior Clicked event does not exist";
+
+	[Test]
+	[Category(UITestCategories.Navigation)]
+	public void BackButtonBehaviorClickEventShouldWork()
+	{
+		App.WaitForElement("GoToDetailPage");
+		App.Tap("GoToDetailPage");
+		App.TapBackArrow("Click");
+		App.WaitForElement("SuccessLabel");
+	}
+}
+#endif
