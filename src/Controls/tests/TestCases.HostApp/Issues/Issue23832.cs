f1fa26fa4c28a7ac39cba4c7d27f4d46381a0f87
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-05-26 17:59:11 +0530

    Added TestCase

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue23832.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Maui.Graphics.Platform;
+using System.Reflection;
+using IImage = Microsoft.Maui.Graphics.IImage;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 23832, "Some HEIC photos is upside down after using PlatformImage.Resize", PlatformAffected.Android | PlatformAffected.iOS)]
+public class Issue23832 : ContentPage
+{
+    public Issue23832()
+    {
+        VerticalStackLayout _stackLayout = new VerticalStackLayout
+        {
+            Padding = new Thickness(20),
+            Spacing = 25,
+        };
+
+        Label _labelResize = new Label
+        {
+            AutomationId = "ResizeLabel",
+            Text = "Resize image"
+        };
+
+        GraphicsView _graphicsViewResize = new GraphicsView
+        {
+            Drawable = new Issue23832_ResizeDrawable(),
+            HeightRequest = 300,
+            WidthRequest = 400,
+        };
+
+        Label _labelDownsize = new Label
+        {
+            Text = "Resize image"
+        };
+
+        GraphicsView _graphicsViewDownsize = new GraphicsView
+        {
+            Drawable = new Issue23832_ResizeDrawable(),
+            HeightRequest = 300,
+            WidthRequest = 400,
+        };
+
+        _stackLayout.Children.Add(_labelResize);
+        _stackLayout.Children.Add(_graphicsViewResize);
+        _stackLayout.Children.Add(_labelDownsize);
+        _stackLayout.Children.Add(_graphicsViewDownsize);
+        Content = _stackLayout;
+    }
+
+}
+
+internal class Issue23832_ResizeDrawable : IDrawable
+{
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        IImage image;
+        Assembly assembly = GetType().GetTypeInfo().Assembly;
+        using (Stream stream = assembly.GetManifestResourceStream("Controls.TestCases.HostApp.Resources.Images.img_0111.heic"))
+        {
+            image = PlatformImage.FromStream(stream);
+        }
+
+        if (image != null)
+        {
+            IImage newImage = image.Resize(400, 300, ResizeMode.Fit, false);
+            canvas.DrawImage(newImage, 0, 0, newImage.Width, newImage.Height);
+        }
+    }
+}
+
+internal class Issue23832_DownsizeDrawable : IDrawable
+{
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        IImage image;
+        Assembly assembly = GetType().GetTypeInfo().Assembly;
+        using (Stream stream = assembly.GetManifestResourceStream("Controls.TestCases.HostApp.Resources.Images.img_0111.heic"))
+        {
+            image = PlatformImage.FromStream(stream);
+        }
+
+        if (image != null)
+        {
+            IImage newImage = image.Downsize(400, 300);
+            canvas.DrawImage(newImage, 0, 0, newImage.Width, newImage.Height);
+        }
+    }
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue23832.cs
@@ -0,0 +1,24 @@
+﻿#if TEST_FAILS_ON_WINDOWS //If this PR https://github.com/dotnet/maui/pull/29138 is merged, then we can test the behavior on Windows as well.
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue23832 : _IssuesUITest
+{
+	public Issue23832(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Some HEIC photos is upside down after using PlatformImage.Resize";
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void ImageShouldNotRenderUpsideDown()
+	{
+		App.WaitForElement("ResizeLabel");
+		VerifyScreenshot();
+	}
+}
+#endif
