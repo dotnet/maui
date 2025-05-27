2a3717b69ac7e688c101d6fb63a0ed8807213486
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-05-27 17:59:37 +0530

    Added test sample

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue29547.cs
@@ -0,0 +1,21 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 29547, "The default native colors are not displayed when the Switch on color is not explicitly set", PlatformAffected.UWP)]
+public class Issue1 : ContentPage
+{
+	public Issue1()
+	{
+		var switchControl = new Switch
+		{
+			IsToggled = true,
+			VerticalOptions = LayoutOptions.Center,
+			HorizontalOptions = LayoutOptions.Center,
+			AutomationId = "switchControl"
+		};
+
+		var grid = new Grid();
+		grid.Children.Add(switchControl);
+
+		Content = grid;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29547.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue1 : _IssuesUITest
+{
+	public override string Issue => "The default native colors are not displayed when the Switch on color is not explicitly set";
+
+	public Issue1(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.Switch)]
+	public void VerifySwitchDefaultColors()
+	{
+		App.WaitForElement("switchControl");
+		VerifyScreenshot();
+	}
+}
