d17804ef5d86baea98cc69febc1d676fb0fc1094
Alberto Aldegheri <albyrock87@gmail.com>
2025-05-29 12:57:19 +0200

    Fix iOS Label being truncated when using Padding (#29031)

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue28180.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 28180, "Labels with Padding are truncated on iOS", PlatformAffected.iOS | PlatformAffected.macOS)]
+public partial class Issue28180 : ContentPage
+{
+	public Issue28180()
+	{
+		InitializeComponent();
+	}
+
+	private void OnToggleClicked(object sender, EventArgs e)
+	{
+		LongTextLabel.Padding = LongTextLabel.Padding == 50 ? 0 : 50;
+	}
+}
