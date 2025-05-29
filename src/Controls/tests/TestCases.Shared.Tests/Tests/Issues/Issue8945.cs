38d921a1503c3b930f4eb31d2ba48954cf3ab23c
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-05-29 15:53:36 +0200

    More changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue8945.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue8945.cs
@@ -23,7 +23,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement("WaitForStubControl");
 			App.WaitForElement("OpenDatePickerButton");
 			App.Tap("OpenDatePickerButton");
-			Assert.That(App.WaitForElement("DatePickerStatusLabel")?.GetText(), Is.EqualTo("DatePicker Opened"));
+			Assert.That(App.WaitForElement("DatePickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("DatePicker Opened"));
 #if ANDROID
 			App.TapDisplayAlertButton(CancelBtn);
 #else
@@ -31,7 +31,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 #endif
 			App.WaitForElement("CloseDatePickerButton");
 			App.Tap("CloseDatePickerButton");
-			Assert.That(App.WaitForElement("DatePickerStatusLabel")?.GetText(), Is.EqualTo("DatePicker Closed"));
+			Assert.That(App.WaitForElement("DatePickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("DatePicker Closed"));
 		}
 
 		[Test, Order(2)]
@@ -41,7 +41,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement("WaitForStubControl");
 			App.WaitForElement("OpenTimePickerButton");
 			App.Tap("OpenTimePickerButton");
-			Assert.That(App.WaitForElement("TimePickerStatusLabel")?.GetText(), Is.EqualTo("TimePicker Opened"));
+			Assert.That(App.WaitForElement("TimePickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("TimePicker Opened"));
 #if ANDROID
 			App.TapDisplayAlertButton(CancelBtn);
 #else
@@ -49,7 +49,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 #endif
 			App.WaitForElement("CloseTimePickerButton");
 			App.Tap("CloseTimePickerButton");
-			Assert.That(App.WaitForElement("TimePickerStatusLabel")?.GetText(), Is.EqualTo("TimePicker Closed"));
+			Assert.That(App.WaitForElement("TimePickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("TimePicker Closed"));
 		}
 
 		[Test, Order(3)]
@@ -59,7 +59,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement("WaitForStubControl");
 			App.WaitForElement("OpenPickerButton");
 			App.Tap("OpenPickerButton");
-			Assert.That(App.WaitForElement("PickerStatusLabel")?.GetText(), Is.EqualTo("Picker Opened"));
+			Assert.That(App.WaitForElement("PickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("Picker Opened"));
 #if ANDROID
 			App.TapDisplayAlertButton(CancelBtn);
 #else
@@ -67,7 +67,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 #endif
 			App.WaitForElement("ClosePickerButton");
 			App.Tap("ClosePickerButton");
-			Assert.That(App.WaitForElement("PickerStatusLabel")?.GetText(), Is.EqualTo("Picker Closed"));
+			Assert.That(App.WaitForElement("PickerStatusLabel", timeout: TimeSpan.FromSeconds(2))?.GetText(), Is.EqualTo("Picker Closed"));
 		}
 	}
 }
