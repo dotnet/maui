9646d1930e2235849b100541bf4a90733a1bbbb4
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-05-29 16:16:41 +0530

    test case added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue29999.cs
@@ -0,0 +1,39 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 29999, "RadioButton TextTransform Property Does Not Apply on Android and Windows Platforms", PlatformAffected.All)]
+public class Issue29999 : ContentPage
+{
+	RadioButton radioButton;
+
+    public Issue29999()
+    {
+        radioButton = new RadioButton
+		{
+			Content = "HelloWorld",
+			AutomationId = "radioButton"
+        };
+
+        var button = new Button
+		{
+			Text = "Lower",
+			AutomationId = "LowerCaseButton"
+        };
+        button.Clicked += OnButtonClicked;
+
+        var layout = new VerticalStackLayout
+        {
+            Children =
+            {
+                radioButton,
+                button
+            }
+        };
+
+        Content = layout;
+    }
+
+    void OnButtonClicked(object sender, EventArgs e)
+    {
+        radioButton.TextTransform = TextTransform.Lowercase;
+    }
+}
