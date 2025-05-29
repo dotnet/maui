a6628816bd785b5ff8821485b9703e8d9ae6539e
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-05-29 16:16:41 +0530

    test case added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29999.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue29999 : _IssuesUITest
+{
+	public override string Issue => "RadioButton TextTransform Property Does Not Apply on Android and Windows Platforms";
+
+	public Issue29999(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.RadioButton)]
+	public void VerifyCarouselLayoutOrientationChange()
+	{
+		App.WaitForElement("radioButton");
+		App.Tap("LowerCaseButton");
+		VerifyScreenshot();
+	}
+}
