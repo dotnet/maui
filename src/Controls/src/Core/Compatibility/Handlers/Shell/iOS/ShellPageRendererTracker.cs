39a6e482e6bcdc462243a7a5b9778a6bb5ea2605
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-05-30 22:41:18 +0530

    [iOS] Fixed Shell.SearchHandler ClearPlaceholderIcon Color Issue (#28601)
    * SearchHandler ClearPlaceholderIcon color fix
    
    * Added Mac snapshot
    
    * Updated SearchBar Icon changes
    
    * Test script changes.
    
    * Revert unwanted changes
    
    * Updated custom icon changes

--- src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellPageRendererTracker.cs
+++ src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellPageRendererTracker.cs
@@ -807,7 +807,14 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility
 
 		void SetSearchBarIcon(UISearchBar searchBar, ImageSource source, UISearchBarIcon icon)
 		{
-			source.LoadImage(source.FindMauiContext(), image =>
+			var mauiContext = source.FindMauiContext(true);
+
+			if (mauiContext is null)
+			{
+				return;
+			}
+
+			source.LoadImage(mauiContext, image =>
 			{
 				var result = image?.Value;
 				if (result != null)
@@ -833,6 +840,11 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility
 			}
 
 			UpdateToolbarItemsInternal();
+
+			//UIKIt will try to override our colors when the SearchController is inside the NavigationBar
+			//Best way was to force them to be set again when page is loaded / ViewDidLoad
+			_searchHandlerAppearanceTracker?.UpdateSearchBarColors();
+
 			CheckAppeared();
 		}
 
@@ -854,9 +866,6 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility
 				return;
 
 			_isVisiblePage = true;
-			//UIKIt will try to override our colors when the SearchController is inside the NavigationBar
-			//Best way was to force them to be set again when page is Appearing / ViewDidLoad
-			_searchHandlerAppearanceTracker?.UpdateSearchBarColors();
 			UpdateShellToMyPage();
 
 			if (_context.Shell.Toolbar != null)
