c58e269112e39fc8f502c6e56bc744ecd254eb3d
NafeelaNazhir <nafeela.nazhirhussain@syncfusion.com>
2025-06-02 15:11:57 +0530

    Added Label Feature Tests

---
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/Image/ImageViewModel.cs
@@ -0,0 +1,129 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Controls;
+
+public class ImageViewModel : INotifyPropertyChanged
+{
+	private Aspect _aspect;
+	private bool _isAnimationPlaying;
+	private bool _isOpaque;
+	private ImageSource _source = ImageSource.FromFile("animated_heart.gif");
+	private string _sourcePath = "animated_heart.gif";
+
+	public Aspect Aspect
+	{
+		get => _aspect;
+		set { if (_aspect != value) { _aspect = value; OnPropertyChanged(); } }
+	}
+
+	public bool IsAnimationPlaying
+	{
+		get => _isAnimationPlaying;
+		set { if (_isAnimationPlaying != value) { _isAnimationPlaying = value; OnPropertyChanged(); } }
+	}
+
+	public bool IsOpaque
+	{
+		get => _isOpaque;
+		set { if (_isOpaque != value) { _isOpaque = value; OnPropertyChanged(); } }
+	}
+
+	public ImageSource Source
+	{
+		get => _source!;
+		set { if (_source != value) { _source = value; OnPropertyChanged(); } }
+	}
+
+	private Color _fontColor = Colors.Blue;
+	public Color FontColor
+	{
+		get => _fontColor;
+		set
+		{
+			if (_fontColor != value)
+			{
+				_fontColor = value;
+				OnPropertyChanged();
+				UpdateFontImageSource();
+			}
+		}
+	}
+
+	private double _fontSize = 250;
+	public double FontSize
+	{
+		get => _fontSize;
+		set
+		{
+			if (_fontSize != value)
+			{
+				_fontSize = value;
+				OnPropertyChanged();
+				UpdateFontImageSource();
+			}
+		}
+	}
+
+	private double _containerHeight = 550;
+	public double ContainerHeight
+	{
+		get => _containerHeight;
+		set
+		{
+			if (_containerHeight != value)
+			{
+				_containerHeight = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	private double _containerWidth = 350; // initial value as in XAML
+	public double ContainerWidth
+	{
+		get => _containerWidth;
+		set
+		{
+			if (_containerWidth != value)
+			{
+				_containerWidth = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	private void UpdateFontImageSource()
+	{
+		if (Source is FontImageSource fontImage)
+		{
+			fontImage.Color = FontColor;
+			fontImage.Size = FontSize;
+			OnPropertyChanged(nameof(Source));
+		}
+	}
+	public string SourcePath
+	{
+		get => _sourcePath!;
+		set
+		{
+			if (_sourcePath != value)
+			{
+				_sourcePath = value;
+				if (!string.IsNullOrWhiteSpace(_sourcePath))
+				{
+					if (_sourcePath.StartsWith("http", System.StringComparison.OrdinalIgnoreCase))
+						Source = ImageSource.FromUri(new System.Uri(_sourcePath));
+					else
+						Source = ImageSource.FromFile(_sourcePath);
+				}
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	protected void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/ImageFeatureTests.cs
@@ -0,0 +1,339 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+
+namespace Microsoft.Maui.TestCases.Tests;
+
+public class ImageFeatureTests : UITest
+{
+	public const string ImageFeatureMatrix = "Image Feature Matrix";
+
+	public const string Options = "Options";
+	public const string Apply = "Apply";
+	public const string ImageAspectFit = "ImageAspectFit";
+	public const string ImageAspectFill = "ImageAspectFill";
+	public const string ImageFill = "ImageFill";
+	public const string ImageCenter = "ImageCenter";
+	public const string SourceTypeFile = "SourceTypeFile";
+	public const string SourceTypeFontImage = "SourceTypeFontImage";
+	public const string SourceTypeStream = "SourceTypeStream";
+	public const string SourceTypeUri = "SourceTypeUri";
+
+
+	public ImageFeatureTests(TestDevice device)
+		: base(device)
+	{
+	}
+
+	protected override void FixtureSetup()
+	{
+		base.FixtureSetup();
+		App.NavigateToGallery(ImageFeatureMatrix);
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_AspectFitWithImageSourceFromFile()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageAspectFit);
+		App.Tap(ImageAspectFit);
+		App.WaitForElement(SourceTypeFile);
+		App.Tap(SourceTypeFile);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_AspectFitWithImageSourceFromUri()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageAspectFit);
+		App.Tap(ImageAspectFit);
+		App.WaitForElement(SourceTypeUri);
+		App.Tap(SourceTypeUri);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_AspectFitWithImageSourceFromStream()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageAspectFit);
+		App.Tap(ImageAspectFit);
+		App.WaitForElement(SourceTypeStream);
+		App.Tap(SourceTypeStream);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_AspectFitWithImageSourceFromFontImage()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageAspectFit);
+		App.Tap(ImageAspectFit);
+		App.WaitForElement(SourceTypeFontImage);
+		App.Tap(SourceTypeFontImage);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_AspectFillWithImageSourceFromFile()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageAspectFill);
+		App.Tap(ImageAspectFill);
+		App.WaitForElement(SourceTypeFile);
+		App.Tap(SourceTypeFile);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_AspectFillWithImageSourceFromUri()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageAspectFill);
+		App.Tap(ImageAspectFill);
+		App.WaitForElement(SourceTypeUri);
+		App.Tap(SourceTypeUri);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_AspectFillWithImageSourceFromStream()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageAspectFill);
+		App.Tap(ImageAspectFill);
+		App.WaitForElement(SourceTypeStream);
+		App.Tap(SourceTypeStream);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_AspectFillWithImageSourceFromFontImage()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageAspectFill);
+		App.Tap(ImageAspectFill);
+		App.WaitForElement(SourceTypeFontImage);
+		App.Tap(SourceTypeFontImage);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_FillWithImageSourceFromFile()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageFill);
+		App.Tap(ImageFill);
+		App.WaitForElement(SourceTypeFile);
+		App.Tap(SourceTypeFile);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_FillWithImageSourceFromUri()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageFill);
+		App.Tap(ImageFill);
+		App.WaitForElement(SourceTypeUri);
+		App.Tap(SourceTypeUri);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_FillWithImageSourceFromStream()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageFill);
+		App.Tap(ImageFill);
+		App.WaitForElement(SourceTypeStream);
+		App.Tap(SourceTypeStream);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_FillWithImageSourceFromFontImage()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageFill);
+		App.Tap(ImageFill);
+		App.WaitForElement(SourceTypeFontImage);
+		App.Tap(SourceTypeFontImage);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_CenterWithImageSourceFromFile()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageCenter);
+		App.Tap(ImageCenter);
+		App.WaitForElement(SourceTypeFile);
+		App.Tap(SourceTypeFile);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_CenterWithImageSourceFromUri()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageCenter);
+		App.Tap(ImageCenter);
+		App.WaitForElement(SourceTypeUri);
+		App.Tap(SourceTypeUri);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_CenterWithImageSourceFromStream()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageCenter);
+		App.Tap(ImageCenter);
+		App.WaitForElement(SourceTypeStream);
+		App.Tap(SourceTypeStream);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyImageAspect_CenterWithImageSourceFromFontImage()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(ImageCenter);
+		App.Tap(ImageCenter);
+		App.WaitForElement(SourceTypeFontImage);
+		App.Tap(SourceTypeFontImage);
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyFontImageWithFontColorRed()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(SourceTypeFontImage);
+		App.Tap(SourceTypeFontImage);
+		App.WaitForElement("FontColorRed");
+		App.Tap("FontColorRed");
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyFontImageWithFontColorGreen()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(SourceTypeFontImage);
+		App.Tap(SourceTypeFontImage);
+		App.WaitForElement("FontColorGreen");
+		App.Tap("FontColorGreen");
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void VerifyFontImageWithFontSize()
+	{
+		App.WaitForElement(Options);
+		App.Tap(Options);
+		App.WaitForElement(SourceTypeFontImage);
+		App.Tap(SourceTypeFontImage);
+		App.WaitForElement("EntryFontSize");
+		App.ClearText("EntryFontSize");
+		App.EnterText("EntryFontSize", "50");
+		App.WaitForElement(Apply);
+		App.Tap(Apply);
+		App.WaitForElement("ImageControl");
+		VerifyScreenshot();
+	}
+}
