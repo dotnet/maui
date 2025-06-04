98e4e8b305c09a7ca5644f1c4a5a8d1c26158274
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-06-04 13:05:32 +0530

    [Android] Fix for disabled Picker view intercepts GestureRecognizer of parent container

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22565.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue22565 : _IssuesUITest
+{
+	public Issue22565(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "A disabled Picker prevents the parent container's GestureRecognizer from being triggered";
+
+	[Test]
+	[Category(UITestCategories.Picker)]
+	public void VerifyGesturePropagationWithDisabledPicker()
+	{
+		App.WaitForElement("22565DescriptionLabel");
+		App.Tap("DisabledPicker");
+		Assert.That(App.FindElement("22565DescriptionLabel").GetText(), Is.EqualTo("Parent Gesture recognizer triggered"));
+	}
+}
