fa7be51f0e4ac8cf13a353bd57ab8281c7d1d7d2
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-06-05 19:07:56 +0530

    Updated the test case

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29633.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29633.cs
@@ -1,3 +1,4 @@
+#if TEST_FAILS_ON_CATALYST // Highlighting Not Working Properly on Mac Catalyst, https://github.com/dotnet/maui/issues/27519
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
@@ -21,3 +22,4 @@ public class Issue29633 : _IssuesUITest
 		VerifyScreenshot();
 	}
 }
+#endif
