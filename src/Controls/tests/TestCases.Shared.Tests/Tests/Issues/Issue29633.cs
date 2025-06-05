bf42130e3a64370ef996021df19556630bd7ca9e
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-06-05 19:02:32 +0530

    Added the test case

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29633.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue29633 : _IssuesUITest
+{
+	public Issue29633(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "[Android] Picker Does Not Show Selected Item Highlight";
+
+	[Test]
+	[Category(UITestCategories.Picker)]
+	public void PickerShouldShowSelectedItemHighlight()
+	{
+		App.WaitForElement("HighlightPickerItem");
+		App.Click("HighlightPickerItem");
+		VerifyScreenshot();
+	}
+}
