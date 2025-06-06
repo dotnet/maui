77b68a8546fa37495ba533da6e995732a7825162
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-06-06 10:53:36 +0200

    More FlyoutPage tests

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21814FlyoutPage.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21814FlyoutPage.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using NUnit.Framework.Legacy;
 using UITest.Appium;
 using UITest.Core;
 
@@ -7,21 +6,83 @@ namespace Microsoft.Maui.TestCases.Tests.Issues;
 
 public class Issue21814FlyoutPage : _IssuesUITest
 {
+	const string FlyoutContent1 = "FlyoutContent1";
+	const string FlyoutContent2 = "FlyoutContent2";
+	const string FlyoutContent3 = "FlyoutContent3";
+
+	const string FlyoutItem1OnNavigatedToLabel = "FlyoutItem1OnNavigatedToLabel";
+	const string FlyoutItem1OnNavigatedFromLabel = "FlyoutItem1OnNavigatedFromLabel";
+
+	const string FlyoutItem2OnNavigatedToLabel = "FlyoutItem2OnNavigatedToLabel";
+	const string FlyoutItem2OnNavigatedFromLabel = "FlyoutItem2OnNavigatedFromLabel";
+
+	const string FlyoutItem3OnNavigatedToLabel = "FlyoutItem3OnNavigatedToLabel";
+	const string FlyoutItem3OnNavigatedFromLabel = "FlyoutItem3OnNavigatedFromLabel";
+
+	const string Item1MenuItem = "Item 1";
+	const string Item2MenuItem = "Item 2";
+	const string Item3MenuItem = "Item 3";
+
 	public Issue21814FlyoutPage(TestDevice device) : base(device)
 	{
 	}
 
 	public override string Issue => "Add better parameters for navigation args (FlyoutPage)";
 
-	[Test]
-	[Category(UITestCategories.Navigation)]
+	[Test, Order(1)]
 	[Category(UITestCategories.FlyoutPage)]
-	public void VerifyFlyoutPageInitialNavigationEventArgs()
+	[Category(UITestCategories.Navigation)]
+	public void InitialPageLoadShowsCorrectNavigationInfo()
 	{
+		// Verify Item 1 is initially loaded and shows correct navigation info
 		App.WaitForElement("FlyoutContent1");
-		
-		App.WaitForElement("FlyoutItem1OnNavigatedToLabel");
-		var navigatedTo = App.FindElement("FlyoutItem1OnNavigatedToLabel").GetText();
-		Assert.That(navigatedTo, Is.EqualTo("PreviousPage: Null, NavigationType: Push"));
+
+		var onNavigatedToText = App.FindElement(FlyoutItem1OnNavigatedToLabel).GetText();
+		Assert.That(onNavigatedToText, Does.Contain("PreviousPage: Null"));
+		Assert.That(onNavigatedToText, Does.Contain("NavigationType: Push"));
+
+		// Initially, OnNavigatingFrom and OnNavigatedFrom should show "-"
+		var onNavigatedFromText = App.FindElement(FlyoutItem1OnNavigatedFromLabel).GetText();
+		Assert.That(onNavigatedFromText, Is.EqualTo("-"));
+	}
+
+	[Test, Order(2)]
+	[Category(UITestCategories.FlyoutPage)]
+	[Category(UITestCategories.Navigation)]
+	public void NavigationFromItem1ToItem2ShowsCorrectParameters()
+	{
+		// Start on Item 1
+		App.WaitForElement(FlyoutContent1);
+
+		// Open flyout menu and navigate to Item 2
+		OpenFlyoutMenu();
+		App.Tap(Item2MenuItem);
+		App.WaitForElement(FlyoutContent2);
+
+		// Verify Item 2 navigation parameters
+		var item2OnNavigatedToText = App.FindElement(FlyoutItem2OnNavigatedToLabel).GetText();
+		Assert.That(item2OnNavigatedToText, Does.Contain("PreviousPage: Issue21814FlyoutItem1"));
+		Assert.That(item2OnNavigatedToText, Does.Contain("NavigationType: Push"));
+	}
+
+	[Test, Order(3)]
+	[Category(UITestCategories.FlyoutPage)]
+	[Category(UITestCategories.Navigation)]
+	public void NavigationFromItem2ToItem3ShowsCorrectParameters()
+	{
+		// Navigate to Item 3
+		OpenFlyoutMenu();
+		App.Tap(Item3MenuItem);
+		App.WaitForElement(FlyoutContent3);
+            
+		// Verify Item 3 navigation parameters
+		var item3OnNavigatedToText = App.FindElement(FlyoutItem3OnNavigatedToLabel).GetText();
+		Assert.That(item3OnNavigatedToText, Does.Contain("PreviousPage: Issue21814FlyoutItem1"));
+		Assert.That(item3OnNavigatedToText, Does.Contain("NavigationType: Push"));
+	}
+	
+	void OpenFlyoutMenu()
+	{
+		App.TapFlyoutPageIcon();
 	}
 }
