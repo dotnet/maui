013b61456d1f1fe0289b33f1662a43ef29888e85
Stephane Delcroix <stephane@delcroix.org>
2025-06-07 12:00:45 +0200

    various fixes

--- src/Controls/tests/Xaml.UnitTests/BindingDiagnosticsTests.xaml.cs
+++ src/Controls/tests/Xaml.UnitTests/BindingDiagnosticsTests.xaml.cs
@@ -30,13 +30,13 @@ namespace Microsoft.Maui.Controls.Xaml.UnitTests
 			public void Setup()
 			{
 				enableDiagnosticsInitialState = RuntimeFeature.EnableDiagnostics;
-				RuntimeFeature.EnableDiagnostics = true;
+				RuntimeFeature.EnableMauiDiagnostics = true;
 			}
 
 			[TearDown]
 			public void TearDown()
 			{
-				RuntimeFeature.EnableDiagnostics = enableDiagnosticsInitialState;			
+				RuntimeFeature.EnableMauiDiagnostics = enableDiagnosticsInitialState;			
 			}
 
 			[TestCase(false)]
