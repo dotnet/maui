2b56ab29023e0f6bca6c8082c186dce07ddb3a8c
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-06-09 13:44:47 +0530

    Have updated the test case and have added the iOS snapshot

--- src/Controls/tests/TestCases.HostApp/Issues/Issue23293.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue23293.cs
@@ -29,46 +29,6 @@ public class Issue23293 : ContentPage
 		Animals.Add(new Issue23293AnimalGroup("Bears", new List<string>
 		{
 			"American Black Bear",
-			"Asian Black Bear",
-			"Brown Bear",
-			"Grizzly-Polar Bear Hybrid",
-			"Sloth Bear",
-		}));
-
-		Animals.Add(new Issue23293AnimalGroup("Cats", new List<string>
-		{
-			"Abyssinian",
-			"Arabian Mau",
-			"Bengal",
-			"Burmese",
-			"Cyprus",
-		}));
-
-		Animals.Add(new Issue23293AnimalGroup("Dogs", new List<string>
-		{
-			"Afghan Hound",
-			"Alpine Dachsbracke",
-			"American Bulldog",
-			"Bearded Collie",
-			"Boston Terrier",
-		}));
-
-		Animals.Add(new Issue23293AnimalGroup("Elephants", new List<string>
-		{
-			"African Bush Elephant",
-			"African Forest Elephant",
-			"Desert Elephant",
-			"Borneo Elephant",
-			"Indian Elephant",
-		}));
-
-		Animals.Add(new Issue23293AnimalGroup("Monkeys", new List<string>
-		{
-			"Baboon",
-			"Capuchin Monkey",
-			"Blue Monkey",
-			"Squirrel Monkey",
-			"Golden Lion Tamarin",
 		}));
 	}
 }
