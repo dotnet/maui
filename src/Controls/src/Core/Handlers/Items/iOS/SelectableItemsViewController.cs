2e635786bc49b7812372f21dbe0adc77e2b765e3
Bhavanesh N <bhavanesh001@gmail.com>
2025-06-10 22:12:54 +0530

    Fix Crash when ItemsSource is set to null in the SelectionChanged handler (#29887)
    * Add null check for ItemsSource before Selection/ Deselection
    
    * Add an UI test
    
    * Update null check
    
    * more null checks
    
    * Update namespace
    
    * update namespace again

--- src/Controls/src/Core/Handlers/Items/iOS/SelectableItemsViewController.cs
+++ src/Controls/src/Core/Handlers/Items/iOS/SelectableItemsViewController.cs
@@ -25,18 +25,31 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 		// _Only_ called if the user initiates the selection change; will not be called for programmatic selection
 		public override void ItemSelected(UICollectionView collectionView, NSIndexPath indexPath)
 		{
+			if (ItemsView?.ItemsSource is null)
+			{
+				return;
+			}
 			FormsSelectItem(indexPath);
 		}
 
 		// _Only_ called if the user initiates the selection change; will not be called for programmatic selection
 		public override void ItemDeselected(UICollectionView collectionView, NSIndexPath indexPath)
 		{
+			if (ItemsView?.ItemsSource is null)
+			{
+				return;
+			}
 			FormsDeselectItem(indexPath);
 		}
 
 		// Called by Forms to mark an item selected 
 		internal void SelectItem(object selectedItem)
 		{
+			if (ItemsView?.ItemsSource is null)
+			{
+				return;
+			}
+
 			var index = GetIndexForItem(selectedItem);
 
 			if (index.Section > -1 && index.Item > -1)
@@ -52,6 +65,11 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 		// Called by Forms to clear the native selection
 		internal void ClearSelection()
 		{
+			if (ItemsView?.ItemsSource is null)
+			{
+				return;
+			}
+	
 			var selectedItemIndexes = CollectionView.GetIndexPathsForSelectedItems();
 
 			foreach (var index in selectedItemIndexes)
@@ -95,7 +113,7 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 
 		internal void UpdatePlatformSelection()
 		{
-			if (ItemsView == null)
+			if (ItemsView?.ItemsSource is null)
 			{
 				return;
 			}
