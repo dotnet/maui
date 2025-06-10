6ffdc7c6b1a51b614ced9175d75fa86dcb0c2811
HarishKumarSF4517 <harish.kumar@syncfusion.com>
2025-06-10 20:53:07 +0530

    feature_matrix_twopaneview

---
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/TwoPaneView/TwoPaneViewControlPage.xaml.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample;
+
+public class TwoPaneViewControlPage : NavigationPage
+{
+	private TwoPaneViewViewModel _viewModel;
+
+	public TwoPaneViewControlPage()
+	{
+		_viewModel = new TwoPaneViewViewModel();
+		PushAsync(new TwoPaneViewControlMainPage(_viewModel));
+	}
+}
+public partial class TwoPaneViewControlMainPage : ContentPage
+{
+	private TwoPaneViewViewModel _viewModel;
+
+	public TwoPaneViewControlMainPage(TwoPaneViewViewModel viewModel)
+	{
+		InitializeComponent();
+		_viewModel = viewModel;
+		BindingContext = _viewModel;
+	}
+
+	private async void NavigateToOptionsPage_Clicked(object sender, EventArgs e)
+	{
+		BindingContext = _viewModel = new TwoPaneViewViewModel();
+		await Navigation.PushAsync(new TwoPaneViewOptionsPage(_viewModel));
+	}
+}
