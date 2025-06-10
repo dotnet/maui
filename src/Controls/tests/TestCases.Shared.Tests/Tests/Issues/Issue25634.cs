778f5028c740feb17bf86894704f154151aa610e
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-06-10 20:03:41 +0530

    test case added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25634.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue25634 : _IssuesUITest
+	{
+		public Issue25634(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "Picker ItemDisplayBinding doesn't support MVVM properly";
+
+		public void VerifyPickerItemDisplayBindingValue()
+		{
+			App.WaitForElement("PickerButton");
+			App.Tap("PickerButton");
+			VerifyScreenshot();
+		}
+	}
+}
