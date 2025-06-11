4abfab7a9b126343f5499e16d80e9f89eaeed4d3
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-06-11 12:06:53 +0530

    Added testcases.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/IssueDummy.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class IssueDummy : _IssuesUITest
+{
+	public IssueDummy(TestDevice testDevice) : base(testDevice)
+	{
+	}
+	public override string Issue => "Flyout icon not replaced after root page change";
+
+	[Test]
+	[Category(UITestCategories.FlyoutPage)]
+	public void FlyoutIconUpdatedAfterInsertPageBefore()
+	{
+		App.WaitForElement("InsertPageButton");
+		App.Tap("InsertPageButton");
+		VerifyScreenshot();
+	}
+}
