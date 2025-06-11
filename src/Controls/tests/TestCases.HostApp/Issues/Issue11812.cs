933668bd01aa80411d014964016ff4354ed31011
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-06-11 15:05:15 +0530

    Added the test sample and test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue11812.cs
@@ -0,0 +1,127 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 11812, "Setting Content of ContentView through style would crash on parent change", PlatformAffected.Android)]
+public partial class Issue11812 : ContentPage
+{
+	Label _countLabel;
+	ContentView _mainContentView;
+	Issue11812ViewModel _viewModel;
+	public Issue11812()
+	{
+		this.BindingContext = _viewModel = new Issue11812ViewModel();
+
+		var button = new Button
+		{
+			Text = "Add ContentView",
+			HorizontalOptions = LayoutOptions.Center,
+			AutomationId = "TestButton"
+		};
+		button.Clicked += OnButtonClicked;
+
+		_countLabel = new Label();
+		_countLabel.AutomationId = "TestLabel";
+		_countLabel.SetBinding(Label.TextProperty, "Count");
+
+		_mainContentView = new ContentView();
+		_mainContentView.SetBinding(ContentView.ContentProperty, "Content.InnerContentView");
+
+		var layout = new VerticalStackLayout
+		{
+			Spacing = 25,
+			Padding = new Thickness(30, 0),
+			VerticalOptions = LayoutOptions.Center,
+			Children =
+			{
+				button,
+				_countLabel,
+				_mainContentView
+			}
+		};
+
+		Content = layout;
+	}
+	private void OnButtonClicked(object sender, EventArgs e)
+	{
+		_viewModel.Count++;
+		_viewModel.Content = new Issue11812Model()
+		{
+			InnerContentView = new Issue11812InnerContentView() { BackgroundColor = Colors.Red }
+		};
+	}
+}
+
+public class Issue11812InnerContentView : ContentView
+{
+	public Issue11812InnerContentView()
+	{
+		Application.Current.Resources ??= new ResourceDictionary();
+
+		if (!Application.Current.Resources.ContainsKey("SubContentStyle"))
+		{
+			Application.Current.Resources["SubContentStyle"] = new Style(typeof(ContentView))
+			{
+				Setters =
+			{
+				new Setter
+				{
+					Property = ContentView.ContentProperty,
+					Value = new Label { Text = "SubContent" }
+				}
+			}
+			};
+		}
+		Style = (Style)Application.Current.Resources["SubContentStyle"];
+	}
+}
+
+public class Issue11812Model
+{
+	public Issue11812InnerContentView InnerContentView { get; set; }
+}
+
+public class Issue11812ViewModel : INotifyPropertyChanged
+{
+	public Issue11812ViewModel()
+	{
+		Content = new Issue11812Model()
+		{
+			InnerContentView = new Issue11812InnerContentView()
+		};
+	}
+
+	private Issue11812Model _content;
+	public Issue11812Model Content
+	{
+		get => _content;
+		set
+		{
+			if (_content != value)
+			{
+				_content = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+	private int _count;
+	public int Count
+	{
+		get => _count;
+		set
+		{
+			if (_count != value)
+			{
+				_count = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue11812.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue11812 : _IssuesUITest
+{
+	public Issue11812(TestDevice device) : base(device) { }
+
+	public override string Issue => "Setting Content of ContentView through style would crash on parent change";
+
+	[Test]
+	[Category(UITestCategories.Border)]
+	public void InnerContentViewShouldNotCrash()
+	{
+		App.WaitForElement("TestButton");
+		App.Tap("TestButton");
+		App.WaitForElement("TestLabel");
+	}
+}
