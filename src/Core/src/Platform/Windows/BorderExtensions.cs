bb27bd338c0c5e9e7c6af013ca8a86457c814329
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-06-11 13:56:17 +0530

    Windows fix added

--- src/Core/src/Platform/Windows/BorderExtensions.cs
+++ src/Core/src/Platform/Windows/BorderExtensions.cs
@@ -75,6 +75,10 @@ namespace Microsoft.Maui.Platform
 					borderPath.StrokeDashArray.Add(value);
 				}
 			}
+			else if (borderDashArray is null)
+			{
+				borderPath.StrokeDashArray = null;
+			}
 		}
 
 		public static void UpdateBorderDashOffset(this Path borderPath, double borderDashOffset)
