9252e3b024c5064e37a6a756544ccf5b04bfc2db
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-06-11 12:46:37 +0530

    addressed the review comments

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25634.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25634.cs
@@ -2,23 +2,21 @@
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue25634 : _IssuesUITest
 {
-	public class Issue25634 : _IssuesUITest
+	public Issue25634(TestDevice testDevice) : base(testDevice)
 	{
-		public Issue25634(TestDevice testDevice) : base(testDevice)
-		{
-		}
+	}
 
-		public override string Issue => "Picker ItemDisplayBinding doesn't support MVVM properly";
+	public override string Issue => "Picker ItemDisplayBinding doesn't support MVVM properly";
 
-		[Test]
-		[Category(UITestCategories.Picker)]
-		public void VerifyPickerItemDisplayBindingValue()
-		{
-			App.WaitForElement("PickerButton");
-			App.Tap("PickerButton");
-			VerifyScreenshot();
-		}
+	[Test]
+	[Category(UITestCategories.Picker)]
+	public void VerifyPickerItemDisplayBindingValue()
+	{
+		App.WaitForElement("PickerButton");
+		App.Tap("PickerButton");
+		VerifyScreenshot();
 	}
 }
