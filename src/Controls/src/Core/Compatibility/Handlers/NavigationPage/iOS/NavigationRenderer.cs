c5bbac245da3e36a4df5fca112f99fc26f91d36a
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-06-12 12:24:16 +0530

    Updated NavigationRenderer.cs

--- src/Controls/src/Core/Compatibility/Handlers/NavigationPage/iOS/NavigationRenderer.cs
+++ src/Controls/src/Core/Compatibility/Handlers/NavigationPage/iOS/NavigationRenderer.cs
@@ -650,7 +650,7 @@ namespace Microsoft.Maui.Controls.Handlers.Compatibility
 			// Update the flyout icon when the root page changes
 			if (index == 0)
 			{
-				GetParentingViewController()?.UpdateLeftBarButtonItem();
+				(target as ParentingViewController)?.UpdateLeftBarButtonItem();
 			}
 		}
 
