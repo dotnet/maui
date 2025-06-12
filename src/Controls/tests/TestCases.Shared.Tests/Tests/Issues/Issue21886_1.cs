beb3f5f15ac28f2c73fd63a126cab1005adf7dd9
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-06-12 16:29:23 +0530

    [Android] Fix for resize method returns a disposed image

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue21886_1.cs
@@ -0,0 +1,27 @@
+#if TEST_FAILS_ON_WINDOWS // Issue Link - https://github.com/dotnet/maui/issues/16767
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue21886_1 : _IssuesUITest
+{
+	public Issue21886_1(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "The resize method returns an image that has already been disposed";
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void VerifyResizeMethodReturnsValidImage()
+	{
+		App.WaitForElement("ConvertedImageStatusLabel");
+		App.Tap("Issue21886_1ResizeBtn");
+
+		var resizeLabelText = App.FindElement("ConvertedImageStatusLabel").GetText();
+		Assert.That(resizeLabelText, Is.EqualTo("Success"));
+	}
+}
+#endif
