fbf7bab44819222e6d81e9891eed54f2842c9c65
bhavanesh001 <bhavanesh001@gmail.com>
2025-06-12 13:11:21 +0530

    Use valid namespaces in uitest files

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19289.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19289.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Tests.Issues;
+namespace Microsoft.Maui.TestCases.Tests.Issues;
 
 public class Issue19289 : _IssuesUITest
 {
