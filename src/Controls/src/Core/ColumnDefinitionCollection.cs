214c60aca83b7e53d8aeaca9378add80521b3d9b
Edward Miller <symbiogenesis@outlook.com>
2025-06-13 12:11:21 -0500

    Optimize converters for GridLength, ColumnDefinition, and RowDefinition (#20048)
    * add benchmarks
    
    * set CanConvertTo to be consistent with the ColumnDefinitionTypeConverter
    
    * optimize GridLength conversion
    
    * optimize grid definition conversion
    
    * enable nullable types
    
    * fix build
    
    * reduce diff
    
    * Slightly better
    
    * Update RowDefinition.cs
    
    * Even better
    
    ---------
    
    Co-authored-by: Edward Miller <symbiogenisis@outlook.com>
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>

--- src/Controls/src/Core/ColumnDefinitionCollection.cs
+++ src/Controls/src/Core/ColumnDefinitionCollection.cs
@@ -1,4 +1,5 @@
 #nullable disable
+using System.Collections.Generic;
 namespace Microsoft.Maui.Controls
 {
 	/// <include file="../../docs/Microsoft.Maui.Controls/ColumnDefinitionCollection.xml" path="Type[@FullName='Microsoft.Maui.Controls.ColumnDefinitionCollection']/Docs/*" />
@@ -9,6 +10,10 @@ namespace Microsoft.Maui.Controls
 		}
 
 		public ColumnDefinitionCollection(params ColumnDefinition[] definitions) : base(definitions)
+		{
+		}
+
+		internal ColumnDefinitionCollection(List<ColumnDefinition> definitions, bool copy) : base(definitions, copy)
 		{
 		}
 	}
