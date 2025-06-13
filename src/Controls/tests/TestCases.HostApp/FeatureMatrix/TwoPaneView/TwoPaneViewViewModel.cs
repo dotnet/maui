6f938707502d38db5bc478cdf3d2a46271872617
HarishKumarSF4517 <harish.kumar@syncfusion.com>
2025-06-13 16:21:46 +0530

    Updated TwoPaneViewFeatureTest

--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/TwoPaneView/TwoPaneViewViewModel.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/TwoPaneView/TwoPaneViewViewModel.cs
@@ -1,73 +1,75 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Foldable;
 
 namespace Maui.Controls.Sample
 {
-	public class TwoPaneViewViewModel : INotifyPropertyChanged
-	{
-		private double _minTallModeHeight = 300;
-		private double _minWideModeWidth = 500;
-		private GridLength _pane1Length = new GridLength(1, GridUnitType.Star);
-		private GridLength _pane2Length = new GridLength(1, GridUnitType.Star);
+    public class TwoPaneViewViewModel : INotifyPropertyChanged
+    {
+        private GridLength _pane1Length = new GridLength(1, GridUnitType.Star);
+        private GridLength _pane2Length = new GridLength(1, GridUnitType.Star);
+        private double _minTallModeHeight = 500;
+        private double _minWideModeWidth = 700;
+        private bool _isShadowEnabled = true;
 
-		public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
 
-		public double MinTallModeHeight
-		{
-			get => _minTallModeHeight;
-			set
-			{
-				if (_minTallModeHeight != value)
-				{
-					_minTallModeHeight = value;
-					OnPropertyChanged();
-				}
-			}
-		}
+        public bool IsShadowEnabled
+        {
+            get => _isShadowEnabled;
+            set
+            {
+                if (_isShadowEnabled != value)
+                {
+                    _isShadowEnabled = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-		public double MinWideModeWidth
-		{
-			get => _minWideModeWidth;
-			set
-			{
-				if (_minWideModeWidth != value)
-				{
-					_minWideModeWidth = value;
-					OnPropertyChanged();
-				}
-			}
-		}
+        public GridLength Pane1Length
+        {
+            get => _pane1Length;
+            set
+            {
+                _pane1Length = value;
+                OnPropertyChanged();
+            }
+        }
 
-		public GridLength Pane1Length
-		{
-			get => _pane1Length;
-			set
-			{
-				if (_pane1Length != value)
-				{
-					_pane1Length = value;
-					OnPropertyChanged();
-				}
-			}
-		}
+        public GridLength Pane2Length
+        {
+            get => _pane2Length;
+            set
+            {
+                _pane2Length = value;
+                OnPropertyChanged();
+            }
+        }
 
-		public GridLength Pane2Length
-		{
-			get => _pane2Length;
-			set
-			{
-				if (_pane2Length != value)
-				{
-					_pane2Length = value;
-					OnPropertyChanged();
-				}
-			}
-		}
+        public double MinTallModeHeight
+        {
+            get => _minTallModeHeight;
+            set
+            {
+                _minTallModeHeight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double MinWideModeWidth
+        {
+            get => _minWideModeWidth;
+            set
+            {
+                _minWideModeWidth = value;
+                OnPropertyChanged();
+            }
+        }
 
 		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-	}
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/TwoPaneViewFeatureTests.cs
@@ -0,0 +1,133 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+
+namespace Microsoft.Maui.TestCases.Tests
+{
+    public class TwoPaneViewFeatureTests : UITest
+    {
+        public const string TwoPaneViewFeatureMatrix = "TwoPaneView Feature Matrix";
+
+        public TwoPaneViewFeatureTests(TestDevice device)
+            : base(device)
+        {
+        }
+
+        protected override void FixtureSetup()
+        {
+            base.FixtureSetup();
+            App.NavigateToGallery(TwoPaneViewFeatureMatrix);
+        }
+
+        [Test, Order(1)]
+        [Category("TwoPaneView")]
+        public void TwoPaneView()
+        {
+            App.WaitForElement("Options");
+            App.Tap("Options");
+
+            App.WaitForElement("Apply");
+            App.Tap("Apply");
+
+            VerifyScreenshot();
+        }
+
+        [Test]
+        [Category("TwoPaneView")]
+        public void TwoPaneView_Pane1Size()
+        {
+            App.WaitForElement("Options");
+            App.Tap("Options");
+
+            App.IncreaseStepper("Pane1LengthStepper");
+
+            App.WaitForElement("Apply");
+            App.Tap("Apply");
+
+            VerifyScreenshot();
+        }
+
+        [Test]
+        [Category("TwoPaneView")]
+        public void TwoPaneView_Pane2Size()
+        {
+            App.WaitForElement("Options");
+            App.Tap("Options");
+
+            App.IncreaseStepper("Pane2LengthStepper");
+
+            App.WaitForElement("Apply");
+            App.Tap("Apply");
+
+            VerifyScreenshot();
+        }
+        
+        [Test]
+        [Category("TwoPaneView")]
+        public void TwoPaneView_Pane1SmallerThanPane2()
+        {
+            App.WaitForElement("Options");
+            App.Tap("Options");
+
+            App.IncreaseStepper("Pane2LengthStepper");
+            App.DecreaseStepper("Pane1LengthStepper");
+
+            App.WaitForElement("Apply");
+            App.Tap("Apply");
+
+            VerifyScreenshot();
+        }
+
+        [Test]
+        [Category("TwoPaneView")]
+        public void TwoPaneView_Pane2SmallerThanPane1()
+        {
+            App.WaitForElement("Options");
+            App.Tap("Options");
+
+            App.IncreaseStepper("Pane1LengthStepper");
+            App.DecreaseStepper("Pane2LengthStepper");
+
+            App.WaitForElement("Apply");
+            App.Tap("Apply");
+
+            VerifyScreenshot();
+        }
+
+#if ANDROID || IOS
+        [Test]
+        [Category("TwoPaneView")]
+        public void TwoPaneView_Height()
+        {
+            App.WaitForElement("Options");
+            App.Tap("Options");
+
+            App.IncreaseStepper("TallModeStepper");
+
+            App.WaitForElement("Apply");
+            App.Tap("Apply");
+
+            VerifyScreenshot();
+        }
+#endif
+
+#if MACCATALYST || WINDOWS
+        [Test]
+        [Category("TwoPaneView")]
+        public void TwoPaneView_Width()
+        {
+            App.WaitForElement("Options");
+            App.Tap("Options");
+
+            App.IncreaseStepper("WideModeStepper");
+            App.IncreaseStepper("WideModeStepper");
+
+            App.WaitForElement("Apply");
+            App.Tap("Apply");
+
+            VerifyScreenshot();
+        }
+#endif
+    }
+}
