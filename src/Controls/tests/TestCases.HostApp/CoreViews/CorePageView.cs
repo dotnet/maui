b0a8249b6550542d5affc8f491312fc09da8cc94
HarishKumarSF4517 <harish.kumar@syncfusion.com>
2025-06-13 17:38:58 +0530

    [Testing] Feature matrix UITest Cases for BoxView Control (#29808)
    * harish_feature_matrix_boxview
    
    * Added the Snapshots
    
    * Updated the Snapshots
    
    * Renamed the file
    
    * Added property and Updated Snaps
    
    * Resaved Snapshots from latest build

--- src/Controls/tests/TestCases.HostApp/CoreViews/CorePageView.cs
+++ src/Controls/tests/TestCases.HostApp/CoreViews/CorePageView.cs
@@ -86,6 +86,7 @@ namespace Maui.Controls.Sample
 			new GalleryPageFactory(() => new CollectionViewFeaturePage(), "CollectionView Feature Matrix"),
 			new GalleryPageFactory(() => new LabelControlPage(), "Label Feature Matrix"),
 			new GalleryPageFactory(() => new CarouselViewFeaturePage(), "CarouselView Feature Matrix"),
+			new GalleryPageFactory(() => new BoxViewControlPage(), "BoxView Feature Matrix"),
 		};
 
 		public CorePageView(Page rootPage)
