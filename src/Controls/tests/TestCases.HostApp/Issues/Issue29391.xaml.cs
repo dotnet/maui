c258a36a46788c70842de19494df6826bbc3f3d6
Jakub Florkowski <kubaflo123@gmail.com>
2025-06-15 12:24:31 +0200

    [iOS] Support for IsSwipeEnabled on CarouselView2

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue29391.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 29391, "[iOS] IsSwipeEnabled Not Working on CarouselView (CV2)", PlatformAffected.iOS)]
+	public partial class Issue29391 : ContentPage
+	{
+		public Issue29391()
+		{
+			InitializeComponent();
+		}
+	}
+}
