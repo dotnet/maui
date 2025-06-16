45e5f1acf15f07dfd749e3babe627393e4fa6b7d
Gerald Versluis <gerald.versluis@microsoft.com>
2025-06-16 08:51:30 +0200

    Fix muliselect result Android

--- src/Essentials/src/MediaPicker/MediaPicker.android.cs
+++ src/Essentials/src/MediaPicker/MediaPicker.android.cs
@@ -6,7 +6,6 @@ using Android.App;
 using Android.Content;
 using Android.Content.PM;
 using Android.Provider;
-using Android.Widget;
 using AndroidX.Activity.Result;
 using AndroidX.Activity.Result.Contract;
 using Microsoft.Maui.ApplicationModel;
@@ -134,15 +133,9 @@ namespace Microsoft.Maui.Media
 
 		async Task<FileResult> PickUsingPhotoPicker(MediaPickerOptions options, bool photo)
 		{
-			var pickVisualMediaRequestBuilder = new PickVisualMediaRequest.Builder()
-				.SetMediaType(photo ? ActivityResultContracts.PickVisualMedia.ImageOnly.Instance : ActivityResultContracts.PickVisualMedia.VideoOnly.Instance);
-
-			if (options.SelectionLimit > 1)
-			{
-				pickVisualMediaRequestBuilder.SetMaxItems(options.SelectionLimit);
-			}
-			
-			var pickVisualMediaRequest = pickVisualMediaRequestBuilder.Build();
+			var pickVisualMediaRequest = new PickVisualMediaRequest.Builder()
+				.SetMediaType(photo ? ActivityResultContracts.PickVisualMedia.ImageOnly.Instance : ActivityResultContracts.PickVisualMedia.VideoOnly.Instance)
+				.Build();
 
 			var androidUri = await PickVisualMediaForResult.Instance.Launch(pickVisualMediaRequest);
 
@@ -180,7 +173,7 @@ namespace Microsoft.Maui.Media
 
 			var androidUris = await PickMultipleVisualMediaForResult.Instance.Launch(pickVisualMediaRequest);
 
-			if (androidUris?.IsEmpty ?? true)
+			if (androidUris?.IsEmpty  ?? true)
 			{
 				return [];
 			}
