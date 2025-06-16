7fe933c2e29eda6ef0d2f00f4ca67054d56ae8de
Jakub Florkowski <42434498+kubaflo@users.noreply.github.com>
2025-06-16 19:09:53 +0200

    ScrollView's Background on iOS (#25541)

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue24061.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue24061 : _IssuesUITest
+	{
+		public Issue24061(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "ScrollView's gradient background doesn't work with ScrollView";
+
+		[Test]
+		[Category(UITestCategories.ScrollView)]
+		public void ScrollViewGradientBackgroundShouldWorkWithScrollView()
+		{
+			App.WaitForElement("Item2");
+			App.SwipeRightToLeft("Item2", 0.99);
+
+			VerifyScreenshot("ScrollViewGradientBackground");
+			App.Click("button");
+			VerifyScreenshot("ScrollViewWithoutGradientBackground");
+			App.Click("button");
+			VerifyScreenshot("ScrollViewGradientBackground");
+		}
+	}
+}
