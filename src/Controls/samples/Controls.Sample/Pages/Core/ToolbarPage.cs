89599b79a95136169476762fc37735d5ca26e156
Gerald Versluis <gerald.versluis@microsoft.com>
2025-06-17 12:35:37 +0200

    Improved sample

--- src/Controls/samples/Controls.Sample/Pages/Core/ToolbarPage.cs
+++ src/Controls/samples/Controls.Sample/Pages/Core/ToolbarPage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
 
 namespace Maui.Controls.Sample.Pages;
@@ -18,13 +19,24 @@ public partial class ToolbarPage
 		}
 	}
 
-	private void Button_Clicked(object sender, EventArgs e)
+	void Button_Clicked(object sender, EventArgs e)
 	{
 		secondary4.IsEnabled = !secondary4.IsEnabled;
 	}
 
-	private void Button_Clicked1(object sender, EventArgs e)
+	void Button_Clicked1(object sender, EventArgs e)
 	{
 		primary1.IsEnabled = !primary1.IsEnabled;
+	}
+
+	void Button_Clicked2(object sender, EventArgs e)
+	{
+		Task.Delay(5000).ContinueWith(t =>
+		{
+			Dispatcher.Dispatch(() =>
+			{
+				secondary2.IsEnabled = !secondary2.IsEnabled;
+			});
+		});
 	}
 }
