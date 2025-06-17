0ec22ee567867caf985d300493408c5317661496
Shane Neuville <shneuvil@microsoft.com>
2025-06-17 15:38:16 -0500

    Weak subscription to CanExecuteChange events (#29837)
    * Weak subscription to CanExecuteChange events
    
    * - add tests
    
    * - fix lifecycle of CanExecute subscription
    
    * - cleanup code based on Pictos recommendations
    
    * - add a netstandard_20 version of DependentHandle
    
    * - add more
    
    * - fix missing null check on textcell

--- src/Controls/src/Core/Cells/TextCell.cs
+++ src/Controls/src/Core/Cells/TextCell.cs
@@ -1,42 +1,27 @@
 #nullable disable
 using System;
 using System.Windows.Input;
+using Microsoft.Maui.Controls.Internals;
 using Microsoft.Maui.Graphics;
 
 namespace Microsoft.Maui.Controls
 {
 	/// <include file="../../../docs/Microsoft.Maui.Controls/TextCell.xml" path="Type[@FullName='Microsoft.Maui.Controls.TextCell']/Docs/*" />
-	public class TextCell : Cell
+	public class TextCell : Cell, ICommandElement
 	{
 		/// <summary>Bindable property for <see cref="Command"/>.</summary>
-		public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(TextCell), default(ICommand),
-			propertyChanging: (bindable, oldvalue, newvalue) =>
-			{
-				var textCell = (TextCell)bindable;
-				var oldcommand = (ICommand)oldvalue;
-				if (oldcommand != null)
-					oldcommand.CanExecuteChanged -= textCell.OnCommandCanExecuteChanged;
-			}, propertyChanged: (bindable, oldvalue, newvalue) =>
-			{
-				var textCell = (TextCell)bindable;
-				var newcommand = (ICommand)newvalue;
-				if (newcommand != null)
-				{
-					textCell.IsEnabled = newcommand.CanExecute(textCell.CommandParameter);
-					newcommand.CanExecuteChanged += textCell.OnCommandCanExecuteChanged;
-				}
-			});
+		public static readonly BindableProperty CommandProperty =
+			BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(TextCell),
+			propertyChanging: CommandElement.OnCommandChanging,
+			propertyChanged: CommandElement.OnCommandChanged);
 
 		/// <summary>Bindable property for <see cref="CommandParameter"/>.</summary>
-		public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(TextCell), default(object),
-			propertyChanged: (bindable, oldvalue, newvalue) =>
-			{
-				var textCell = (TextCell)bindable;
-				if (textCell.Command != null)
-				{
-					textCell.IsEnabled = textCell.Command.CanExecute(newvalue);
-				}
-			});
+		public static readonly BindableProperty CommandParameterProperty =
+			BindableProperty.Create(nameof(CommandParameter),
+				typeof(object),
+				typeof(TextCell),
+				null,
+				propertyChanged: CommandElement.OnCommandParameterChanged);
 
 		/// <summary>Bindable property for <see cref="Text"/>.</summary>
 		public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(TextCell), default(string));
@@ -104,9 +89,14 @@ namespace Microsoft.Maui.Controls
 			Command?.Execute(CommandParameter);
 		}
 
-		void OnCommandCanExecuteChanged(object sender, EventArgs eventArgs)
+		void ICommandElement.CanExecuteChanged(object sender, EventArgs eventArgs)
 		{
+			if (Command is null)
+				return;
+
 			IsEnabled = Command.CanExecute(CommandParameter);
 		}
+
+		WeakCommandSubscription ICommandElement.CleanupTracker { get; set; }
 	}
 }
