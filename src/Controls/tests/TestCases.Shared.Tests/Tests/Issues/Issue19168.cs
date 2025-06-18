41963189ff9b49190a74a42dad90f52e14207632
KarthikRajaKalaimani <92777139+KarthikRajaKalaimani@users.noreply.github.com>
2025-06-18 12:06:42 +0530

    Test case added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19168.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue19168 : _IssuesUITest
+{
+	public Issue19168(TestDevice testDevice) : base(testDevice)
+	{
+	}
+	public override string Issue => "iOS Picker dismiss does not work when clicking outside of the Picker";
+
+	[Test]
+	[Category(UITestCategories.Picker)]
+	public void PickerShouldDismissWhenClickOnOutside()
+	{
+		App.WaitForElement("Picker");
+		App.Tap("Picker");
+#if MACCATALYST
+		App.TapCoordinates(600, 200);
+		App.WaitForElement("Button");
+#elif ANDROID
+		App.Back();
+		App.WaitForElement("Button");
+#elif IOS
+		App.Tap("Button");
+#endif
+		VerifyScreenshot();
+	}
+}
