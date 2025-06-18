d0d72670611fad9940e189db8e1e9a12695b8ef0
Shane Neuville <shneuvil@microsoft.com>
2025-06-18 14:10:24 -0500

    - fix spelling

--- src/Controls/src/Core/ScrollView/ScrollView.cs
+++ src/Controls/src/Core/ScrollView/ScrollView.cs
@@ -428,7 +428,7 @@ namespace Microsoft.Maui.Controls
 		void IScrollView.ScrollFinished() => SendScrollFinished();
 	
 
-		// Don't delete this override. At somepoint in the future we'd like to delete Compatibility.Layout
+		// Don't delete this override. At some point in the future we'd like to delete Compatibility.Layout
 		// and this is the only way to ensure binary compatibility with code that's already compiled against MAUI
 		// and is overriding OnSizeAllocated.
 		protected override Size MeasureOverride(double widthConstraint, double heightConstraint)
@@ -465,7 +465,7 @@ namespace Microsoft.Maui.Controls
 		}
 	
 
-		// Don't delete this override. At somepoint in the future we'd like to delete Compatibility.Layout
+		// Don't delete this override. At some point in the future we'd like to delete Compatibility.Layout
 		// and this is the only way to ensure binary compatibility with code that's already compiled against MAUI
 		// and is overriding OnSizeAllocated.
 		protected override Size ArrangeOverride(Rect bounds)
@@ -476,7 +476,7 @@ namespace Microsoft.Maui.Controls
 			return Frame.Size;
 		}		
 
-		// Don't delete this override. At somepoint in the future we'd like to delete Compatibility.Layout
+		// Don't delete this override. At some point in the future we'd like to delete Compatibility.Layout
 		// and this is the only way to ensure binary compatibility with code that's already compiled against MAUI
 		// and is overriding OnSizeAllocated.
 		protected override void OnSizeAllocated(double width, double height)
