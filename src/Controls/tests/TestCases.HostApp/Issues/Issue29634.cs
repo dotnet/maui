c20d463225ee5b4ac63a302536e276a954bcfee2
Shane Neuville <shneuvil@microsoft.com>
2025-06-19 15:03:15 -0500

    - rework test

--- src/Controls/tests/TestCases.HostApp/Issues/Issue29634.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue29634.cs
@@ -3,38 +3,46 @@
 [Issue(IssueTracker.Github, 29634, "iOS CV: Empty view not resizing when bounds change", PlatformAffected.iOS)]
 public class Issue29634 : ContentPage
 {
+	CollectionView _collectionView;
 	public Issue29634()
 	{
-		var cv = new CollectionView
+		var button = new Button
+		{
+			Margin = new Thickness(0, 0, 0, 5),
+			BackgroundColor = Colors.LightSeaGreen,
+			FontSize = 12,
+			TextColor = Colors.DarkSlateGray,
+			Text = "Button text",
+			Command = new Command(() => Content.WidthRequest = 200),
+			AutomationId = "RunTest"
+		};
+
+		button.SizeChanged += (sender, e) =>
+		{
+			if (sender is Button b && b.Width == 200)
+			{
+				((Grid)Content).Add(new Label(){Text = "Button Successfully resized", AutomationId = "SuccessLabel"});
+			}
+		};
+
+		_collectionView = new CollectionView
 		{
 			ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Horizontal) { ItemSpacing = 10 },
 			ItemTemplate = new DataTemplate(),
-			EmptyView = new Button
-			{
-				Margin=new Thickness(0,0,0,5),
-				BackgroundColor=Colors.LightSeaGreen,
-				FontSize = 12,
-				TextColor = Colors.DarkSlateGray,
-				Text="Button text"
-			}
+			EmptyView = button
 		};
-		Grid.SetColumn(cv, 1);
+
+		Grid.SetColumn(_collectionView, 1);
 
 		Content = new Grid
 		{
+			WidthRequest = 400,
 			ColumnDefinitions =
 			[
 				new ColumnDefinition(GridLength.Auto),
 				new ColumnDefinition(GridLength.Star)
 			],
-			Children = { cv }
+			Children = { _collectionView }
 		};
-	}
-
-	protected override async void OnAppearing()
-	{
-		base.OnAppearing();
-		await Task.Delay(300);
-		((Grid)Content).Add(new Label { Text = "StubLabel", AutomationId = "StubLabel" });
 	}
 }
