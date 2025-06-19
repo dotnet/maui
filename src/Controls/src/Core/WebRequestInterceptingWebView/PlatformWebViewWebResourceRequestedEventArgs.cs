b75e1027233a4d8ba7e09d514b422adba2d59979
Matthew Leibowitz <mattleibow@live.com>
2025-06-20 01:57:08 +0200

    Update PlatformWebViewWebResourceRequestedEventArgs.cs

--- src/Controls/src/Core/WebRequestInterceptingWebView/PlatformWebViewWebResourceRequestedEventArgs.cs
+++ src/Controls/src/Core/WebRequestInterceptingWebView/PlatformWebViewWebResourceRequestedEventArgs.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Microsoft.Maui.Controls;
