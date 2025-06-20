598cd56d966bb7aac03bcdfe1621cbcca27d6a93
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-06-20 11:27:47 +0530

    Added test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue25921.cs
@@ -0,0 +1,25 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 25921, "[Windows] Setting BackgroundColor for Slider updates the Maximum Track Color", PlatformAffected.UWP)]
+public class Issue25921 : ContentPage
+{
+	public Issue25921()
+	{
+		var layout = new VerticalStackLayout();
+		layout.VerticalOptions = LayoutOptions.Center;
+		var slider = new Slider()
+		{
+			AutomationId = "testSlider",
+			WidthRequest = 300,
+			Maximum = 100,
+			Minimum = 0,
+			Value = 50,
+			BackgroundColor = Colors.Yellow,
+			MaximumTrackColor = Colors.Red,
+			MinimumTrackColor = Colors.Fuchsia,
+		};
+
+		layout.Children.Add(slider);
+		Content = layout;
+	}
+}
