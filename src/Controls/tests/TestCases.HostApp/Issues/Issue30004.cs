eb655f32b4238a92e64f159794ecb89c974ba67e
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2025-06-20 11:42:52 +0530

    Removed the external ttf file

--- src/Controls/tests/TestCases.HostApp/Issues/Issue30004.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue30004.cs
@@ -10,13 +10,10 @@ public class Issue30004 : ContentPage
 			Children =
 				{
 					new Label(){Text = "FontAwesome", AutomationId="FontImage", HorizontalOptions = LayoutOptions.Center, FontSize = 20, Margin = new Thickness(0, 20, 0, 20)},
-					new Image() { Source = new FontImageSource() {  FontFamily = "FontAwesome", Glyph = "\xf7a4", Color = Colors.Black, Size = 50}, Margin = 4, Background= Colors.Red, WidthRequest=100, HeightRequest=100, HorizontalOptions = LayoutOptions.Center},
+					new Image() { Source = new FontImageSource() {  FontFamily = "FA", Glyph = "\xf7a4", Color = Colors.Black, Size = 50}, Margin = 4, Background= Colors.Red, WidthRequest=100, HeightRequest=100, HorizontalOptions = LayoutOptions.Center},
 
 					new Label(){Text = "ionicons", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, FontSize = 20, Margin = new Thickness(0, 20, 0, 20)},
 					new Image() { Source = new FontImageSource() { FontFamily = "Ion",Glyph = "\uf47e", Color = Colors.Black, Size = 50},  Background= Colors.Red,WidthRequest=100, HeightRequest=100, HorizontalOptions = LayoutOptions.Center},
-
-					new Label(){Text = "CustomerIcon", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, FontSize = 20, Margin = new Thickness(0, 20, 0, 20)},
-					new Image() { Source = new FontImageSource{Glyph = "\xe776",FontFamily = "MauiSampleFontIcon", Size = 50, Color = Colors.Green},  Background= Colors.Red,WidthRequest=100, HeightRequest=100, HorizontalOptions = LayoutOptions.Center },
 				}
 		};
 	}
