6d24650c124d43a5053a3f8b55ff663a2348f670
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-06-20 11:27:47 +0530

    Added test case

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25921.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue25921 : _IssuesUITest
+{
+	public override string Issue => "[Windows] Setting BackgroundColor for Slider updates the Maximum Track Color";
+
+	public Issue25921(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.Slider)]
+	public void VerifyTabChevronIconColor()
+	{
+		App.WaitForElement("testSlider");
+		VerifyScreenshot();
+	}
+}
