1fda037f871854de5625889cc34d5791df1a4cd2
Jakub Florkowski <kubaflo123@gmail.com>
2025-06-22 15:56:55 +0200

    IndicatorView does not visually update dot indicators when the ItemsSource count changes - fix

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue30118.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue30118 : _IssuesUITest
+{
+	public Issue30118(TestDevice testDevice) : base(testDevice) { }
+
+	public override string Issue => "IndicatorView does not visually update dot indicators when the ItemsSource count changes";
+
+	[Test]
+	[Category(UITestCategories.IndicatorView)]
+	public void IndicatorViewShouldUpdate()
+	{
+		App.WaitForElement("AddButton");
+		App.Tap("AddButton");
+		App.Tap("AddButton");
+		App.Tap("AddButton");
+		App.Tap("AddButton");
+		App.Tap("AddButton");
+		VerifyScreenshot();
+	}
+}
