2f8fe52fce10e34768e4c221670acfd71482c0bb
Rui Marinho <me@ruimarinho.net>
2025-06-23 15:57:53 +0100

    [testint] Don t skip BindingUnsubscribesForDeadTarget

--- src/Controls/tests/Core.UnitTests/TypedBindingUnitTests.cs
+++ src/Controls/tests/Core.UnitTests/TypedBindingUnitTests.cs
@@ -1406,7 +1406,7 @@ namespace Microsoft.Maui.Controls.Core.UnitTests
 			}
 		}
 
-		[Fact(Skip = "Failing on dnceng"), Category(TestCategory.Memory)]
+		[Fact, Category(TestCategory.Memory)]
 		public async Task BindingUnsubscribesForDeadTarget()
 		{
 			var viewmodel = new TestViewModel();
