342265ba49585aef0dbc103604612a25cd930fb9
AhamedAliNishad <ahamedalinishad.j@syncfusion.com>
2025-06-24 17:16:30 +0530

    Included the test sample and test case for the fix

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue18420.cs
@@ -0,0 +1,35 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 18420, "[Windows]ViewExtensions RotateYTo and RotateXTo with length 0 crashes on Windows", PlatformAffected.UWP)]
+public class Issue18420 : ContentPage
+{
+	int count = 0;
+	Button rotateButton;
+	public Issue18420()
+	{
+		var layout = new VerticalStackLayout
+		{
+			Spacing = 25,
+			Padding = new Thickness(30, 0),
+			VerticalOptions = LayoutOptions.Center
+		};
+
+		rotateButton = new Button
+		{
+			Text = "Click me to rotate",
+			BackgroundColor= Colors.Red,
+			HorizontalOptions = LayoutOptions.Center,
+			AutomationId= "RotateButton"
+		};
+
+		rotateButton.Clicked += OnRotateButtonClicked;
+		layout.Children.Add(rotateButton);
+		this.Content = layout;
+	}
+
+	private void OnRotateButtonClicked(object sender, EventArgs e)
+	{
+		count++;
+		rotateButton.RotateYTo(10 + count, 0);
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18420.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue18420 : _IssuesUITest
+{
+	public Issue18420(TestDevice device) : base(device) { }
+
+	public override string Issue => "[Windows]ViewExtensions RotateYTo and RotateXTo with length 0 crashes on Windows";
+
+	[Test]
+	[Category(UITestCategories.ViewBaseTests)]
+	public void ApplyingRotationWithZeroDurationShouldNotCrash()
+	{
+		App.WaitForElement("RotateButton");
+		App.Tap("RotateButton");
+		App.Tap("RotateButton");
+		App.Tap("RotateButton");
+		App.WaitForElement("RotateButton");
+	}
+}
