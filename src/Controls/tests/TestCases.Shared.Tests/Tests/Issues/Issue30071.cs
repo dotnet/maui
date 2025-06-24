a8cf0268d2fa95634ed6518fd9bdef1b0ef8cfbf
SubhikshaSf4851 <subhiksha.c@syncfusion.com>
2025-06-24 17:08:51 +0530

    [Windows] Fixed Entry Editor CharacterSpacing Apply to placeholder

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue30071.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue30071 : _IssuesUITest
+{
+	public Issue30071(TestDevice device)
+		: base(device)
+	{
+	}
+
+	public override string Issue => "Entry Editor Placeholder CharacterSpacing Property Not Working on Windows";
+
+	[Test]
+	[Category(UITestCategories.Entry)]
+	[Category(UITestCategories.Editor)]
+	public void Issue30071PlaceholderCharacterSpacingShouldApply()
+	{
+		App.WaitForElement("label");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Entry)]
+	[Category(UITestCategories.Editor)]
+	public void Issue30071PlaceholderCharacterSpacingShouldChange()
+	{
+		App.WaitForElement("entry");
+		App.WaitForElement("button");
+		App.Tap("button");
+		VerifyScreenshot();
+	}
+}
