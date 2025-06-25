29a37559cabd23329907a2f2c7e1098f412260e5
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-06-25 16:19:00 +0530

    Fixed Map pin not updating

--- src/Core/maps/src/Handlers/MapPin/MapPinHandler.Android.cs
+++ src/Core/maps/src/Handlers/MapPin/MapPinHandler.Android.cs
@@ -6,22 +6,50 @@ namespace Microsoft.Maui.Maps.Handlers
 {
 	public partial class MapPinHandler : ElementHandler<IMapPin, MarkerOptions>
 	{
+		// Keep track of the actual marker associated with this handler
+		internal Marker? Marker { get; set; }
+
 		protected override MarkerOptions CreatePlatformElement() => new MarkerOptions();
 
 		public static void MapLocation(IMapPinHandler handler, IMapPin mapPin)
 		{
+			var mapPinHandler = handler as MapPinHandler;
 			if (mapPin.Location != null)
-				handler.PlatformView.SetPosition(new LatLng(mapPin.Location.Latitude, mapPin.Location.Longitude));
+			{
+				// Always update the MarkerOptions
+				var position = new LatLng(mapPin.Location.Latitude, mapPin.Location.Longitude);
+				handler.PlatformView.SetPosition(position);
+
+				// If we have a reference to the actual Marker on the map, update it too
+				if (mapPinHandler?.Marker != null)
+				{
+					mapPinHandler.Marker.Position = position;
+				}
+			}
 		}
 
 		public static void MapLabel(IMapPinHandler handler, IMapPin mapPin)
 		{
 			handler.PlatformView.SetTitle(mapPin.Label);
+
+			// Update the actual marker if available
+			var mapPinHandler = handler as MapPinHandler;
+			if (mapPinHandler?.Marker != null)
+			{
+				mapPinHandler.Marker.Title = mapPin.Label;
+			}
 		}
 
 		public static void MapAddress(IMapPinHandler handler, IMapPin mapPin)
 		{
 			handler.PlatformView.SetSnippet(mapPin.Address);
+
+			// Update the actual marker if available
+			var mapPinHandler = handler as MapPinHandler;
+			if (mapPinHandler?.Marker != null)
+			{
+				mapPinHandler.Marker.Snippet = mapPin.Address;
+			}
 		}
 	}
 }
