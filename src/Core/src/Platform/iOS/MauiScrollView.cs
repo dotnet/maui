df9aebc15bd2c77d6519609c1b62d35f2ddb2fb7
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-06-26 12:19:32 +0530

    fix updated

--- src/Core/src/Platform/iOS/MauiScrollView.cs
+++ src/Core/src/Platform/iOS/MauiScrollView.cs
@@ -58,8 +58,9 @@ namespace Microsoft.Maui.Platform
 				// arrangement with an adjusted starting point and recalculating the content offset.
 				if (_previousEffectiveUserInterfaceLayoutDirection != EffectiveUserInterfaceLayoutDirection)
 				{
-					// In mac platform, Scrollbar is not updated based on FlowDirection, So resetting the scroll indicators
-					if (OperatingSystem.IsMacCatalyst())
+					// In mac platform, Scrollbar is not updated based on FlowDirection, so resetting the scroll indicators
+					// It's a native limitation; to maintain platform consistency, a hack fix is applied to show the Scrollbar based on the FlowDirection.
+					if (OperatingSystem.IsMacCatalyst() && _previousEffectiveUserInterfaceLayoutDirection is not null)
 					{
 						bool showsVertical = ShowsVerticalScrollIndicator;
 						bool showsHorizontal = ShowsHorizontalScrollIndicator;
