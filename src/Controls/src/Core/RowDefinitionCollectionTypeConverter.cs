4fb89b7eb3a35b62c61d3e1afdeb3142efd35967
Rui Marinho <me@ruimarinho.net>
2025-06-30 10:53:17 +0100

    Fix merge

--- src/Controls/src/Core/RowDefinitionCollectionTypeConverter.cs
+++ src/Controls/src/Core/RowDefinitionCollectionTypeConverter.cs
@@ -3,6 +3,7 @@ using System.Buffers;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
+using System.Linq;
 using Microsoft.Maui.Controls.Xaml;
 
 namespace Microsoft.Maui.Controls
@@ -14,7 +15,7 @@ namespace Microsoft.Maui.Controls
 		public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
 			=> sourceType == typeof(string);
 
-		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		public override bool CanConvertTo(ITypeDescriptorContext? context, Type? destinationType)
 			=> destinationType == typeof(string);
 
 		public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
@@ -49,7 +50,7 @@ namespace Microsoft.Maui.Controls
 			return new RowDefinitionCollection(definitions, copy: false);
 		}
 
-		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
 		{
 			if (value is not RowDefinitionCollection definitions)
 				throw new NotSupportedException();
