8a81288e219f39a6691133a529a6b86139067e27
LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
2025-07-01 21:45:41 +0530

    Fixed Test case failure in PR 30306 - [6/30/2025] Candidate (#30315)
    * updated changes
    
    * modified changes
    
    * added snapshot

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29086.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29086.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_WINDOWS //The AutomationId for SwipeView items does not function as expected on the Windows platform. Additionally, programmatic swiping is currently not working. For reference:  https://github.com/dotnet/maui/issues/14777.
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -11,7 +12,7 @@ public class Issue29086 : _IssuesUITest
 	{
 	}
 
-	public override string Issue => "SwipeView Closes when Content Changes even with SwipeBehaviorOnInvoked='RemainOpen'";
+	public override string Issue => "SwipeView Closes when Content Changes even with SwipeBehaviorOnInvoked is set to RemainOpen";
 
 	[Test]
 	[Category(UITestCategories.SwipeView)]
@@ -19,8 +20,11 @@ public class Issue29086 : _IssuesUITest
 	{
 		App.WaitForElement("SwipeItem");
 		App.SwipeLeftToRight("SwipeItem");
+		App.WaitForElement("AddButton");
 		App.Click("AddButton");
+		App.WaitForElement("AddButton");
 		App.Click("AddButton");
 		VerifyScreenshot();
 	}
 }
+#endif
