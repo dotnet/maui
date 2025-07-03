74741ef4a15756c0f64ff118334004f103f5472e
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-07-03 13:13:04 +0530

    Updated fix

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue13356.cs
@@ -0,0 +1,68 @@
+﻿#if ANDROID
+using Android.Content;
+using Android.OS;
+using View = Android.Views.View;
+using Google.Android.Material.BottomSheet;
+#endif
+using Microsoft.Maui.Platform;
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 13356, "Java.Lang.IllegalArgumentException: The style on this component requires your app theme to be Theme.MaterialComponent.", PlatformAffected.Android)]
+public class Issue13356 : TestContentPage
+{
+	protected override void Init()
+	{
+		var button = new Button
+		{
+			Text = "Show me",
+			AutomationId = "showButton",
+			HorizontalOptions = LayoutOptions.Center
+		};
+		button.Clicked += Button_Clicked;
+
+		var stack = new VerticalStackLayout
+		{
+			Spacing = 25,
+			Padding = new Thickness(30, 0),
+			VerticalOptions = LayoutOptions.Center,
+			Children =
+			{
+				button
+			}
+		};
+
+		Content = new ScrollView
+		{
+			Content = stack
+		};
+	}
+
+	void Button_Clicked(object sender, EventArgs e)
+	{
+#if ANDROID
+		var droidContext = Handler.MauiContext.Context;
+		var myDialog = new Issue13356Dialog(droidContext);
+		var dialogContent = new Button { Text = "Dialog Button", Padding = new Thickness(30) };
+		myDialog.Content = dialogContent.ToPlatform(Application.Current.Handler.MauiContext);
+		myDialog.ShowDialog();
+#endif
+	}
+
+#if ANDROID
+public class Issue13356Dialog : BottomSheetDialog {
+    public View Content { get; set; }
+
+    public Issue13356Dialog(Context context) : base(context) {}
+
+    protected override void OnCreate(Bundle savedInstanceState) {
+        base.OnCreate(savedInstanceState);
+        SetContentView(Content);
+    }
+
+    public void ShowDialog() {
+        if (!IsShowing)
+            Show();
+    }
+}
+#endif
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13356.cs
@@ -0,0 +1,25 @@
+﻿#if ANDROID // This Issue is only reprodcued in Android platform
+using NUnit.Framework;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue13356 : _IssuesUITest
+{
+
+	public Issue13356(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Java.Lang.IllegalArgumentException: The style on this component requires your app theme to be Theme.MaterialComponent.";
+
+	[Test]
+	[Category(UITestCategories.Button)]
+	public void MaterialButtonShouldHaveMaterialThemeComponent()
+	{
+		App.waitForElement("showButton");
+		App.Tap("showButton");
+		App.WaitForElement("Dialog Button");
+	}
+}
+#endif
