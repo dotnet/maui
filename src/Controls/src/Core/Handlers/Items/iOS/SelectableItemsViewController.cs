cbbcacb4538a26c44cd02aa10db2e7c90faecb01
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-07-03 18:49:44 +0530

    Fixed CV selection issue

--- src/Controls/src/Core/Handlers/Items/iOS/SelectableItemsViewController.cs
+++ src/Controls/src/Core/Handlers/Items/iOS/SelectableItemsViewController.cs
@@ -55,10 +55,17 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			if (index.Section > -1 && index.Item > -1)
 			{
 				// Ensure the selected index is updated after the collection view's items generation is completed
-				CollectionView.PerformBatchUpdates(null, _ =>
+				if (!CollectionView.IsLoaded())
+				{
+					CollectionView.PerformBatchUpdates(null, _ =>
+					{
+						CollectionView.SelectItem(index, true, UICollectionViewScrollPosition.None);
+					});
+				}
+				else
 				{
 					CollectionView.SelectItem(index, true, UICollectionViewScrollPosition.None);
-				});
+				}
 			}
 		}
 
