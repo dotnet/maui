e2a3b31f9124553e45822f0c891edcbe55b85a76
Subhiksha Chandrasekaran <subhiksha.c@syncfusion.com>
2025-07-03 22:44:52 +0530

    [Windows] Fixed CanvasDrawingSession Exception on Clipping Image (#30028)
    * [Windows] Fixed CanvasDrawingSession Exception
    
    * Update in description label
    
    * Updated Suggested Changes

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18430.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue18430 : _IssuesUITest
+{
+	public Issue18430(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "CanvasDrawingSession Exception caused on Windows";
+
+	[Test]
+	[Category(UITestCategories.GraphicsView)]
+	public void Issue18430ExceptionShouldNotThrown()
+	{
+		App.WaitForElement("Issue18430DescriptionLabel");
+	}
+}
