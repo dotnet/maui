ef395b41577c9d0f2c338a447667628a4754495b
LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
2025-07-05 01:30:01 +0530

    Fixed Test case(Issue28798) failure in PR 30306 - [6/30/2025] Candidate (#30413)
    * Added test_fails and issue link
    
    * updated

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28798.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28798.cs
@@ -1,4 +1,4 @@
-#if ANDROID
+#if ANDROID && TEST_FAILS_ON_ANDROID //for more information : https://github.com/dotnet/maui/issues/30411
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
