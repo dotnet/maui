2ee1fdaa338a30b616df5681e6aaa176bb41d9b6
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2025-07-07 19:00:15 +0530

    testcases changes

--- src/Controls/tests/TestCases.HostApp/Issues/Issue21963.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue21963.xaml.cs
@@ -1,14 +1,81 @@
 namespace Maui.Controls.Sample.Issues;
 
 [Issue(IssueTracker.Github, 21963, "Shell.MenuItemTemplate Sometime Does Not Work", PlatformAffected.Android)]
-public partial class Issue21963 : Shell
+public partial class Issue21963 : TestShell
 {
-	public Issue21963()
+
+	protected override void Init()
 	{
-		InitializeComponent();
+		this.FlyoutIsPresented = true;
+		this.ItemTemplate = new DataTemplate(() =>
+	{
+		var content = new ContentView();
+		var label = new Label();
+		label.SetBinding(Label.TextProperty, "Title");
+		content.Content = label;
+
+		return content;
+	});
+
+		// MenuItemTemplate
+		this.MenuItemTemplate = new DataTemplate(() =>
+		{
+			var content = new ContentView();
+			var label = new Label();
+			label.SetBinding(Label.TextProperty, "Title");
+			content.Content = label;
+
+			return content;
+		});
+
+		// ShellContent 1
+		var shellContent1 = new ShellContent
+		{
+			Title = "ShellContent 1",
+			AutomationId = "Issue21963ShellContent1",
+			ContentTemplate = new DataTemplate(typeof(Issues.Issue21963InnerPage))
+		};
+
+		// ShellContent 2
+		var shellContent2 = new ShellContent
+		{
+			Title = "ShellContent 2",
+			AutomationId = "Issue21963ShellContent2",
+			ContentTemplate = new DataTemplate(typeof(Issues.Issue21963MenuPage))
+		};
+
+		// ShellContent 3
+		var shellContent3 = new ShellContent
+		{
+			Title = "ShellContent 3",
+			AutomationId = "Issue21963ShellContent3",
+			ContentTemplate = new DataTemplate(typeof(Issues.Issue21963FlyoutPage))
+		};
+
+		// MenuItems
+		var menuItem1 = new MenuItem
+		{
+			Text = "MenuItem 1",
+			AutomationId = "Issue21963MenuItem1"
+		};
+
+		var menuItem2 = new MenuItem
+		{
+			Text = "MenuItem 2",
+			AutomationId = "Issue21963MenuItem2"
+		};
+
+		// Add items to Shell
+		Items.Add(shellContent1);
+		Items.Add(shellContent2);
+		Items.Add(shellContent3);
+
+		Items.Add(menuItem1);
+		Items.Add(menuItem2);
 	}
 }
-public partial class Issue21963InnerPage : ContentPage
+
+public class Issue21963InnerPage : ContentPage
 {
 	public Issue21963InnerPage()
 	{
@@ -25,7 +92,7 @@ public partial class Issue21963InnerPage : ContentPage
 	}
 }
 
-public partial class Issue21963MenuPage : ContentPage
+public class Issue21963MenuPage : ContentPage
 {
 	public Issue21963MenuPage()
 	{
@@ -41,7 +108,7 @@ public partial class Issue21963MenuPage : ContentPage
 		};
 	}
 }
-public partial class Issue21963FlyoutPage : ContentPage
+public class Issue21963FlyoutPage : ContentPage
 {
 	public Issue21963FlyoutPage()
 	{
