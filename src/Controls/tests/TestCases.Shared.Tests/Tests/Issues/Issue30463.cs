bdd390979aba454c336d8633279a1b73a885a5c9
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-07-07 17:36:24 +0530

    Modified test

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue30463.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue30463.cs
@@ -3,6 +3,7 @@ using UITest.Appium;
 using UITest.Core;
 
 namespace Microsoft.Maui.TestCases.Tests.Issues;
+
 public class Issue30463 : _IssuesUITest
 {
 	public Issue30463(TestDevice device) : base(device) { }
@@ -16,6 +17,10 @@ public class Issue30463 : _IssuesUITest
 		App.WaitForElement("ChangeSelectedIndexToMinusOne");
 		App.Tap("ChangeSelectedIndexToMinusOne");
 		var picker = App.WaitForElement("RegainingPickerTitle").GetText();
+#if WINDOWS || IOS
+			Assert.That(picker, Is.Empty);
+#else
 		Assert.That(picker, Is.EqualTo("Select an item"));
+#endif
 	}
 }
