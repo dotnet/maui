ec2654ddd2f4c1979c367ebd9eaef49178df1ace
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-07-07 15:50:20 +0530

    Fix for Picker title not restoring after deselection on iOS

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue30463.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue30463 : _IssuesUITest
+{
+	public Issue30463(TestDevice device) : base(device) { }
+
+	public override string Issue => "Picker title is not displayed again";
+
+	[Test]
+	[Category(UITestCategories.Picker)]
+	public void PickerShouldRegainTitle()
+	{
+		App.WaitForElement("ChangeSelectedIndexToMinusOne");
+		App.Tap("ChangeSelectedIndexToMinusOne");
+		var picker = App.WaitForElement("RegainingPickerTitle").GetText();
+		Assert.That(picker, Is.EqualTo("Select an item"));
+	}
+}
