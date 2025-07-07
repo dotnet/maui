4c1544144ade33cdfb8b4f8c9a653b27b492c1d5
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-07-07 19:19:57 +0530

    Added the test sample and case

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue14164.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue14164 : _IssuesUITest
+{
+	public Issue14164(TestDevice device) : base(device) { }
+
+	public override string Issue => "[iOS ,MAC]Border overlapped with content view label when setting clip";
+
+	[Test, Order(1)]
+	[Category(UITestCategories.Border)]
+	public void BorderContentShouldNotDisappearWhenClippingApplied()
+	{
+		App.WaitForElement("AddClipButton");
+		App.Tap("AddClipButton");
+		VerifyScreenshot();
+	}
+
+	[Test, Order(2)]
+	[Category(UITestCategories.Border)]
+	public void BorderContentShouldNotDisappearWhenClippingRemoved()
+	{
+		App.WaitForElement("RemoveClipButton");
+		App.Tap("RemoveClipButton");
+		VerifyScreenshot();
+	}
+}
