a192b5514191c7248831abd4c09ef1e8e04b34ba
Shane Neuville <shneuvil@microsoft.com>
2025-07-07 21:02:06 -0500

    Update ShellSectionRenderer.cs

--- src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellSectionRenderer.cs
+++ src/Controls/src/Core/Compatibility/Handlers/Shell/iOS/ShellSectionRenderer.cs
@@ -19,8 +19,6 @@ namespace Microsoft.Maui.Controls.Platform.Compatibility
 
 		public bool IsInMoreTab { get; set; }
 
-		internal bool PopRequested { get; set; }
-
 		public ShellSection ShellSection
 		{
 			get { return _shellSection; }
