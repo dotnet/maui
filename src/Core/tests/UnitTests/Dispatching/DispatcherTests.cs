66c9bf43fa5252657c919f445c8966ca90ebb168
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-07-08 16:05:06 +0530

    Added UnitTest

--- src/Core/tests/UnitTests/Dispatching/DispatcherTests.cs
+++ src/Core/tests/UnitTests/Dispatching/DispatcherTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.Maui.Dispatching;
+using NSubstitute;
 using Xunit;
 
 namespace Microsoft.Maui.UnitTests.Dispatching
@@ -188,5 +189,44 @@ namespace Microsoft.Maui.UnitTests.Dispatching
 				// If it's repeating, ticks will be greater than 1
 				Assert.True(ticks > 1);
 			});
+
+		[Fact]
+		public void DispatchIfRequired_ShouldCallDispatch_WhenDispatchIsRequired()
+		{
+			// Arrange
+			var dispatcher = Substitute.For<IDispatcher>();
+			dispatcher.IsDispatchRequired.Returns(true);
+
+			int executionCount = 0;
+			Action testAction = () => executionCount++;
+
+			// Configure the mock to actually execute the action when Dispatch is called
+			dispatcher.Dispatch(Arg.Do<Action>(action => action()));
+
+			// Act
+			dispatcher.DispatchIfRequired(testAction);
+
+			// Assert
+			dispatcher.Received(1).Dispatch(Arg.Any<Action>());
+			Assert.Equal(1, executionCount);
+		}
+
+		[Fact]
+		public void DispatchIfRequired_ShouldExecuteAction_WhenDispatchIsNotRequired()
+		{
+			// Arrange
+			var dispatcher = Substitute.For<IDispatcher>();
+			dispatcher.IsDispatchRequired.Returns(false);
+
+			int executionCount = 0;
+			Action testAction = () => executionCount++;
+
+			// Act
+			dispatcher.DispatchIfRequired(testAction);
+
+			// Assert
+			dispatcher.DidNotReceive().Dispatch(Arg.Any<Action>());
+			Assert.Equal(1, executionCount);
+		}
 	}
 }
