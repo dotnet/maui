d4bc1b17c36a6a1ac1e062cb1ddcd80d8c37fcfe
Shane Neuville <shneuvil@microsoft.com>
2025-07-10 09:46:12 -0500

    Fix SafeArea management on iOS
    # Conflicts:
    #	src/Controls/src/Core/PublicAPI/net-ios/PublicAPI.Unshipped.txt
    #	src/Controls/src/Core/PublicAPI/net-maccatalyst/PublicAPI.Unshipped.txt
    #	src/Core/src/PublicAPI/net-ios/PublicAPI.Unshipped.txt
    #	src/Core/src/PublicAPI/net-maccatalyst/PublicAPI.Unshipped.txt

---
+++ src/Core/src/Platform/iOS/SafeAreaPadding.cs
@@ -0,0 +1,33 @@
+﻿using CoreGraphics;
+using UIKit;
+
+namespace Microsoft.Maui.Platform;
+
+internal readonly record struct SafeAreaPadding(double Left, double Right, double Top, double Bottom)
+{
+	public static SafeAreaPadding Empty { get; } = new(0, 0, 0, 0);
+
+	public bool IsEmpty { get; } = Left == 0 && Right == 0 && Top == 0 && Bottom == 0;
+	public double HorizontalThickness { get; } = Left + Right;
+	public double VerticalThickness { get; } = Top + Bottom;
+
+	public CGRect InsetRect(CGRect bounds)
+	{
+		if (IsEmpty)
+		{
+			return bounds;
+		}
+
+		return new CGRect(
+			bounds.Left + Left,
+			bounds.Top + Top,
+			bounds.Width - HorizontalThickness,
+			bounds.Height - VerticalThickness);
+	}
+}
+
+internal static class SafeAreaInsetsExtensions
+{
+	public static SafeAreaPadding ToSafeAreaInsets(this UIEdgeInsets insets) =>
+		new(insets.Left, insets.Right, insets.Top, insets.Bottom);
+}
