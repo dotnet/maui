aae14204a12dfc6d9bb1ccdaf56857867d821186
LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
2025-07-10 20:06:13 +0530

    Added sample and test files

--- src/Controls/tests/TestCases.HostApp/CoreViews/CorePageView.cs
+++ src/Controls/tests/TestCases.HostApp/CoreViews/CorePageView.cs
@@ -93,6 +93,7 @@ namespace Maui.Controls.Sample
 			new GalleryPageFactory(() => new ImageButtonControlPage(), "ImageButton Feature Matrix"),
 			new GalleryPageFactory(() => new BoxViewControlPage(), "BoxView Feature Matrix"),
 			new GalleryPageFactory(() => new ToolbarFeaturePage(), "Toolbar Feature Matrix"),
+			new GalleryPageFactory(() => new EditorControlPage(), "Editor Feature Matrix"),
 		};
 
 		public CorePageView(Page rootPage)
