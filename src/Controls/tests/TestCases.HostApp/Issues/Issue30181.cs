53b8cd47984e4d4ab75ac09e1294ecfa662dbda6
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-07-10 19:16:43 +0530

    Fixed Entry bound to a double casts values too early, preventing small negative decimal entries

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue30181.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel;
+
+namespace Controls.TestCases.HostApp.Issues;
+
+[Issue(IssueTracker.Github, 30181, "Entry bound to a double casts values too early, preventing small negative decimal entries", PlatformAffected.All)]
+public class Issue30181 : ContentPage
+{
+    public Issue30181()
+    {
+        var viewModel = new Issue30181_ViewModel();
+        BindingContext = viewModel;
+
+        var numericEntry = new Entry
+        {
+            Keyboard = Keyboard.Numeric,
+            AutomationId = "Issue30181Entry"
+        };
+        numericEntry.SetBinding(Entry.TextProperty, nameof(viewModel.NumericValue));
+
+        var stackLayout = new StackLayout
+        {
+            Padding = 20,
+            Children = { numericEntry }
+        };
+
+        Content = stackLayout;
+    }
+}
+
+public partial class Issue30181_ViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    double? _numericValue;
+    public double? NumericValue
+    {
+        get => _numericValue;
+        set
+        {
+            if (_numericValue != value)
+            {
+                _numericValue = value;
+                OnPropertyChanged(nameof(NumericValue));
+            }
+        }
+    }
+
+    protected void OnPropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
