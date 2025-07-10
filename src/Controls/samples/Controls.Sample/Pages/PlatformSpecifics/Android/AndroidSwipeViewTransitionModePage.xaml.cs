99f8025c0069d6f85019318dce485fd4734dd819
Simon Cropp <simon.cropp@gmail.com>
2025-07-11 07:47:54 +1000

    fix some incorrect casting using `as` (#30459)
    if the type is known then a direct cast should be used instead of an as.
    since, in the case where the assumption is wrong, then it is better to
    get a cast exception instead of a null ref exception.
    
    basically if code uses `as` and does not check for null when that
    variable is used, then it is likely problematic code.

--- src/Controls/samples/Controls.Sample/Pages/PlatformSpecifics/Android/AndroidSwipeViewTransitionModePage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/PlatformSpecifics/Android/AndroidSwipeViewTransitionModePage.xaml.cs
@@ -16,7 +16,7 @@ namespace Maui.Controls.Sample.Pages
 
 		void OnSwipeViewTransitionModeChanged(object sender, EventArgs e)
 		{
-			SwipeTransitionMode transitionMode = (SwipeTransitionMode)(sender as EnumPicker)!.SelectedItem;
+			SwipeTransitionMode transitionMode = (SwipeTransitionMode)((EnumPicker)sender).SelectedItem;
 			swipeView.On<Microsoft.Maui.Controls.PlatformConfiguration.Android>().SetSwipeTransitionMode(transitionMode);
 		}
 
