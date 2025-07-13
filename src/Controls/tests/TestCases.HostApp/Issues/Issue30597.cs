1c71d629432876a65feb422314f25b46b4f45f3c
Jakub Florkowski <kubaflo123@gmail.com>
2025-07-13 17:41:22 +0200

    Added a UI Test

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue30597.cs
@@ -0,0 +1,22 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 30597, "[iOS] SearchBar placeholder color is not updating on theme change", PlatformAffected.iOS)]
+public class Issue30597 : ContentPage
+{
+	public Issue30597()
+	{
+		Content = new StackLayout
+		{
+			Children =
+				{
+					new SearchBar { Placeholder = "Placeholder - system's default" },
+					new Button
+					{
+						Text = "Change Theme",
+						AutomationId = "changeThemeButton",
+						Command= new Command(() => Application.Current!.UserAppTheme = Application.Current!.UserAppTheme != AppTheme.Dark ? AppTheme.Dark : AppTheme.Light)
+					}
+				}
+		};
+	}
+}
