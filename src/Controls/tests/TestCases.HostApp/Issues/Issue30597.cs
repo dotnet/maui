481b4562452b03260a9c17d3b7843cec11724b8e
Jakub Florkowski <kubaflo123@gmail.com>
2025-07-13 17:53:32 +0200

    Update Issue30597.cs

--- src/Controls/tests/TestCases.HostApp/Issues/Issue30597.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue30597.cs
@@ -5,16 +5,34 @@ public class Issue30597 : ContentPage
 {
 	public Issue30597()
 	{
+		var searchBar3 = new SearchBar
+		{
+			Placeholder = "Placeholder with AppThemeBinding - red/green"
+		};
+		searchBar3.SetAppThemeColor(SearchBar.PlaceholderColorProperty, Colors.Red, Colors.Green);
+
 		Content = new StackLayout
 		{
 			Children =
 				{
-					new SearchBar { Placeholder = "Placeholder - system's default" },
+					new SearchBar
+					{
+						Placeholder = "Placeholder - system's default"
+					},
+					new SearchBar
+					{
+						PlaceholderColor = Colors.Red,
+						Placeholder = "Placeholder - red"
+					},
+					searchBar3,
 					new Button
 					{
 						Text = "Change Theme",
 						AutomationId = "changeThemeButton",
-						Command= new Command(() => Application.Current!.UserAppTheme = Application.Current!.UserAppTheme != AppTheme.Dark ? AppTheme.Dark : AppTheme.Light)
+						Command= new Command(() =>
+						{
+							Application.Current!.UserAppTheme = Application.Current!.UserAppTheme != AppTheme.Dark ? AppTheme.Dark : AppTheme.Light;
+						})
 					}
 				}
 		};
