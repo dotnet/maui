49855df4aeaf6ebad6022e2105862d06f39e6eb1
Rui Marinho <me@ruimarinho.net>
2025-07-14 14:39:12 +0100

    Try update other properties

--- src/Core/AndroidNative/settings.gradle
+++ src/Core/AndroidNative/settings.gradle
@@ -7,7 +7,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
