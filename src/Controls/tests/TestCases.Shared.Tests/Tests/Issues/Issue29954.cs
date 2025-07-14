ddcc651f362a63be2e380b6089596a4767581a3d
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-07-14 13:53:39 +0200

    Added UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29954.cs
@@ -0,0 +1,34 @@
+using System;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue29954: _IssuesUITest
+{
+	public Issue29882(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "The status bar is blank when ManualMAUITests sample project debugging on the Android API 36 emulator";
+
+	[Test]
+	[Category(UITestCategories.Navigation)]
+	public void StatusBarShouldBeVisibleOnAndroidAPI36()
+	{
+		App.WaitForElement("StatusBarDescriptionLabel");
+		
+		App.WaitForElement("ApiLevelLabel");
+
+		// Test opening modal which should also have proper status bar handling
+		App.WaitForElement("OpenModalButton");
+		App.Click("OpenModalButton");
+		App.WaitForElement("ModalStatusBarTestLabel");
+
+		// The modal content should not overlap with the status bar
+		// Close modal and verify main page is still correct
+		App.Click("CloseModalButton");
+		App.WaitForElement("StatusBarDescriptionLabel");
+	}
+}
