d18135d48c4a3566e46ead2bf84c77400999e0e7
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-07-15 18:37:22 +0530

    Revert "Fix updated"
    This reverts commit 78ac95e1b3780d6a57a5411ddc894d4e86ed37da.

--- src/Core/src/Platform/Windows/ViewExtensions.cs
+++ src/Core/src/Platform/Windows/ViewExtensions.cs
@@ -123,10 +123,6 @@ namespace Microsoft.Maui.Platform
 
 		public static void UpdateFlowDirection(this FrameworkElement platformView, IView view)
 		{
-			if(platformView is WebView2)
-			{
-				return;
-			}
 			var flowDirection = view.FlowDirection;
 			switch (flowDirection)
 			{
