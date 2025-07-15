bb509b13ccaa2f049a2e1230e91796054e520db7
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-07-15 16:09:18 +0530

    test case added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue30515.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue30515 : _IssuesUITest
+{
+	public override string Issue => "[iOS] WebView.Reload() with HtmlWebViewSource returns WebNavigationResult.Failure in Navigated event";
+
+	public Issue30515(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.WebView)]
+	public void VerifyWebViewHTMLSourceReloadStatus()
+	{
+		App.WaitForElement("NavigationStatusLabel");
+		App.Tap("ReloadButton");
+		Assert.That(App.FindElement("NavigationStatusLabel").GetText(), Is.EqualTo("Success"));
+	}
+}
