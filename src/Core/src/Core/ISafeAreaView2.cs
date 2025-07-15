e012b4f767d1a82d88ba985e02885d686c298ef7
Shane Neuville <shneuvil@microsoft.com>
2025-07-15 13:20:34 -0500

    - add images and fix naming

--- src/Core/src/Core/ISafeAreaView2.cs
+++ src/Core/src/Core/ISafeAreaView2.cs
@@ -6,7 +6,7 @@
 	/// <remarks>
 	/// This interface is only recognized on the iOS/Mac Catalyst platforms; other platforms will ignore it.
 	/// </remarks>
-	internal interface ISafeAreaPage
+	internal interface ISafeAreaView2
 	{
 		/// <summary>
 		/// Internal property for the Page's SafeAreaInsets Thickness that may be changed in the future.
