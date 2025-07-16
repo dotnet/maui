7a6e107db6cb02d0f99f5f5ea81bd5049269a4f4
Jakub Florkowski <kubaflo123@gmail.com>
2025-07-17 01:02:33 +0200

    Added snapshots

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue30601.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue30601.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_CATALYST || TEST_FAILS_ON_IOS // There's a separate PR for this issue on iOS and Catalyst https://github.com/dotnet/maui/pull/30597
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -19,3 +20,4 @@ public class Issue30601 : _IssuesUITest
 		VerifyScreenshot();
 	}
 }
+#endif
