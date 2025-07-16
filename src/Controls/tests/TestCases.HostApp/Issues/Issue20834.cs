e0140c985e517b41135ceaf0eca41581e53352a1
Matthew Leibowitz <mattleibow@live.com>
2025-07-16 02:34:53 +0200

    Use buttons to make windows work

--- src/Controls/tests/TestCases.HostApp/Issues/Issue20834.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue20834.cs
@@ -5,65 +5,83 @@ public class Issue20834 : TestContentPage
 {
 	readonly Issue20834_Drawable drawable = new();
 	float oldX, oldY;
-	Grid rootLayout;
-	Grid grid;
 	GraphicsView graphicsView;
 
 	protected override void Init()
 	{
-		rootLayout = new Grid()
+		var rootLayout = new Grid()
 		{
-			RowDefinitions = new RowDefinitionCollection()
-		{
-			new RowDefinition() { Height = 300 },
-			new RowDefinition() { Height = 100 },
-			new RowDefinition() { Height = 100 }
+			RowDefinitions =
+			{
+				new RowDefinition() { Height = 300 },
+				new RowDefinition() { Height = 100 },
+				new RowDefinition() { Height = 100 }
 			}
 		};
 
-		grid = new Grid()
+		var grid = new Grid()
 		{
 			BackgroundColor = Color.FromArgb("#1a2033"),
 			HeightRequest = 300,
 			VerticalOptions = LayoutOptions.Center
 		};
-		graphicsView = new GraphicsView() { AutomationId = "graphicsView" };
-		Label label = new Label()
+
+		graphicsView = new GraphicsView();
+		var dragTarget = new Button()
 		{
-			AutomationId = "DropTarget",
-			Text = "Drop the circle here",
-			HeightRequest = 100,
-			BackgroundColor = Color.FromArgb("#1a2033"),
-			TextColor = Colors.White,
+			AutomationId = "DragTarget",
+			Background = Colors.Transparent,
+			BorderWidth = 0,
+			CornerRadius = 0,
 		};
 
-		Label invalidDroplabel = new Label()
+		var dropTarget1 = new Button()
 		{
-			AutomationId = "DropTarget_2",
+			AutomationId = "DropTarget1",
+			Text = "Drop the circle here",
+			HeightRequest = 100,
+			TextColor = Colors.White,
+			Background = Color.FromArgb("#1a2033"),
+			BorderWidth = 0,
+			CornerRadius = 0,
+			VerticalOptions = LayoutOptions.Start,
+			InputTransparent = true,
+		};
+
+		var dropTarget2 = new Button()
+		{
+			AutomationId = "DropTarget2",
 			Text = "Circle should be visible here",
 			HeightRequest = 100,
 			TextColor = Colors.White,
+			Background = Colors.Transparent,
+			BorderWidth = 0,
+			CornerRadius = 0,
 			VerticalOptions = LayoutOptions.Start,
+			HorizontalOptions = LayoutOptions.Start,
+			InputTransparent = true,
+		};
+
+		var box = new BoxView()
+		{
+			BackgroundColor = Colors.CornflowerBlue,
+			WidthRequest = 100,
+			HeightRequest = 100,
 		};
 
 		graphicsView.Drawable = drawable;
 		graphicsView.StartInteraction += GraphicsView_StartInteraction;
 		graphicsView.DragInteraction += GraphicsView_DragInteraction;
 
+		grid.Add(dragTarget);
 		grid.Add(graphicsView);
 
 		rootLayout.Add(grid, 0, 0);
-		rootLayout.Add(invalidDroplabel, 0, 0);
-		rootLayout.Add(new BoxView()
-		{
-			BackgroundColor = Colors.CornflowerBlue,
-			WidthRequest = 100,
-			HeightRequest = 100,
-		}, 0, 1);
-		rootLayout.Add(label, 0, 2);
+		rootLayout.Add(dropTarget2, 0, 0);
+		rootLayout.Add(box, 0, 1);
+		rootLayout.Add(dropTarget1, 0, 2);
 
 		Content = rootLayout;
-
 	}
 
 	void GraphicsView_DragInteraction(object sender, TouchEventArgs e)
@@ -84,18 +102,19 @@ public class Issue20834 : TestContentPage
 		oldX = e.Touches[0].X;
 		oldY = e.Touches[0].Y;
 	}
-}
 
-internal class Issue20834_Drawable : IDrawable
-{
-	public float X { get; set; } = 20f;
-	public float Y { get; set; } = 20f;
-
-	public void Draw(ICanvas canvas, RectF dirtyRect)
+	class Issue20834_Drawable : IDrawable
 	{
-		canvas.SaveState();
-		canvas.FillColor = Colors.Blue;
-		canvas.FillEllipse(X, Y, 100, 100);
-		canvas.ResetState();
+		public float X { get; set; } = 20f;
+
+		public float Y { get; set; } = 20f;
+
+		public void Draw(ICanvas canvas, RectF dirtyRect)
+		{
+			canvas.SaveState();
+			canvas.FillColor = Colors.Blue;
+			canvas.FillEllipse(X, Y, 100, 100);
+			canvas.ResetState();
+		}
 	}
 }
