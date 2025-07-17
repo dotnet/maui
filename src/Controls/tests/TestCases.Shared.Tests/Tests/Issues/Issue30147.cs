3e361555abd9b479e8cc28c842e4cceb81f46f84
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-07-18 03:45:08 +0530

    [Testing] Add UITest for Issue30147 on iOS (#30506)
    * fix added
    
    * Revert "fix added"
    
    This reverts commit 138797f06454b88d91cc6e868bace6e15a2101ed.
    
    * test case added
    
    * test sample modified
    
    * test sample updated
    
    * affected platform changed
    
    * sample changes added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue30147.cs
@@ -0,0 +1,24 @@
+﻿#if IOS  // The issue is specific to iOS, so restricting other platforms
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue30147 : _IssuesUITest
+{
+	public override string Issue => "MauiScrollView resets ContentOffset on first layout pass";
+
+	public Issue30147(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.ScrollView)]
+	public void VerifyScrollViewContentOffsetValue()
+	{
+		App.WaitForElement("OffsetLabel");
+		Assert.That(App.FindElement("OffsetLabel").GetText(), Is.EqualTo("500"));
+	}
+}
+#endif
