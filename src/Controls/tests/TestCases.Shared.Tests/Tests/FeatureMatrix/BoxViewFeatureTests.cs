702d1558373cf70b1963034fb063432b1957d52a
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-07-21 14:08:00 +0530

    added FIll to boxview

--- src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/BoxViewFeatureTests.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/BoxViewFeatureTests.cs
@@ -2,163 +2,199 @@ using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
+namespace Microsoft.Maui.TestCases.Tests;
 
-namespace Microsoft.Maui.TestCases.Tests
+public class BoxViewFeatureTests : UITest
 {
-	public class BoxViewFeatureTests : UITest
+	const string ResetButton = "ResetButton";
+	const string CornerRadiusEntry = "CornerRadiusEntry";
+	const string CornerRadiusLabel = "CornerRadiusLabel";
+	const string RedRadioButton = "RedRadioButton";
+	const string OpacityEntry = "OpacityEntry";
+	const string OpacityLabel = "OpacityLabel";
+	const string ShadowCheckBox = "ShadowCheckBox";
+
+	public const string BoxViewFeatureMatrix = "BoxView Feature Matrix";
+
+
+	public BoxViewFeatureTests(TestDevice device)
+		: base(device)
 	{
-		public const string BoxViewFeatureMatrix = "BoxView Feature Matrix";
+	}
 
-		public BoxViewFeatureTests(TestDevice device)
-			: base(device)
-		{
-		}
-
-		protected override void FixtureSetup()
-		{
-			base.FixtureSetup();
-			App.NavigateToGallery(BoxViewFeatureMatrix);
-		}
+	protected override void FixtureSetup()
+	{
+		base.FixtureSetup();
+		App.NavigateToGallery(BoxViewFeatureMatrix);
+	}
 
 
-		[Test]
-		[Category(UITestCategories.BoxView)]
-		public void BoxView_IsVisible()
-		{
-			App.WaitForElement("ResetButton");
-			App.Tap("ResetButton");
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxView_IsVisible()
+	{
+		App.WaitForElement(ResetButton);
+		App.Tap(ResetButton);
 
-			App.WaitForElement("VisibilityCheckBox");
-			App.Tap("VisibilityCheckBox");
+		App.WaitForElement("VisibilityCheckBox");
+		App.Tap("VisibilityCheckBox");
 
-			VerifyScreenshot();
-		}
+		VerifyScreenshot();
+	}
 
-		[Test]
-		[Category(UITestCategories.BoxView)]
-		public void BoxView_CornerRadiusWithColor()
-		{
-			App.WaitForElement("ResetButton");
-			App.Tap("ResetButton");
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxView_CornerRadiusWithColor()
+	{
+		App.WaitForElement(ResetButton);
+		App.Tap(ResetButton);
 
-			App.WaitForElement("CornerRadiusEntry");
-			App.EnterText("CornerRadiusEntry", "60,10,20,40");
+		App.WaitForElement(CornerRadiusEntry);
+		App.EnterText(CornerRadiusEntry, "60,10,20,40");
 
-			App.WaitForElement("CornerRadiusLabel");
-			App.Tap("CornerRadiusLabel");
+		App.WaitForElement(CornerRadiusLabel);
+		App.Tap(CornerRadiusLabel);
 
-			App.WaitForElement("RedRadioButton");
-			App.Tap("RedRadioButton");
+		App.WaitForElement(RedRadioButton);
+		App.Tap(RedRadioButton);
 
-			VerifyScreenshot();
-		}
+		VerifyScreenshot();
+	}
 
-		[Test]
-		[Category(UITestCategories.BoxView)]
-		public void BoxView_ColorWithOpacity()
-		{
-			App.WaitForElement("ResetButton");
-			App.Tap("ResetButton");
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxView_ColorWithOpacity()
+	{
+		App.WaitForElement(ResetButton);
+		App.Tap(ResetButton);
 
-			App.WaitForElement("RedRadioButton");
-			App.Tap("RedRadioButton");
+		App.WaitForElement(RedRadioButton);
+		App.Tap(RedRadioButton);
 
-			App.WaitForElement("OpacityEntry");
-			App.EnterText("OpacityEntry", "0.5");
+		App.WaitForElement(OpacityEntry);
+		App.EnterText(OpacityEntry, "0.5");
 
-			App.WaitForElement("OpacityLabel");
-			App.Tap("OpacityLabel");
+		App.WaitForElement(OpacityLabel);
+		App.Tap(OpacityLabel);
 
-			VerifyScreenshot();
-		}
+		VerifyScreenshot();
+	}
 
-		[Test]
-		[Category(UITestCategories.BoxView)]
-		public void BoxView_CornerRadiusWithFlowDirection()
-		{
-			App.WaitForElement("ResetButton");
-			App.Tap("ResetButton");
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxView_CornerRadiusWithFlowDirection()
+	{
+		App.WaitForElement(ResetButton);
+		App.Tap(ResetButton);
 
-			App.WaitForElement("CornerRadiusEntry");
-			App.EnterText("CornerRadiusEntry", "60,10,20,40");
+		App.WaitForElement(CornerRadiusEntry);
+		App.EnterText(CornerRadiusEntry, "60,10,20,40");
 
-			App.WaitForElement("CornerRadiusLabel");
-			App.Tap("CornerRadiusLabel");
+		App.WaitForElement(CornerRadiusLabel);
+		App.Tap(CornerRadiusLabel);
 
-			App.WaitForElement("FlowDirectionRTLCheckBox");
-			App.Tap("FlowDirectionRTLCheckBox");
+		App.WaitForElement("FlowDirectionRTLCheckBox");
+		App.Tap("FlowDirectionRTLCheckBox");
 
-			VerifyScreenshot();
-		}
-#if TEST_FAILS_ON_WINDOWS // For more information see: https://github.com/dotnet/maui/issues/27732
-		[Test]
-		[Category(UITestCategories.BoxView)]
-		public void BoxView_CornerRadiusWithOpacityAndShadow()
-		{
-			App.WaitForElement("ResetButton");
-			App.Tap("ResetButton");
+		VerifyScreenshot();
+	}
 
-			App.WaitForElement("CornerRadiusEntry");
-			App.EnterText("CornerRadiusEntry", "60,10,20,40");
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxView_CornerRadiusWithOpacityAndShadow()
+	{
+		App.WaitForElement(ResetButton);
+		App.Tap(ResetButton);
 
-			App.WaitForElement("CornerRadiusLabel");
-			App.Tap("CornerRadiusLabel");
+		App.WaitForElement(CornerRadiusEntry);
+		App.EnterText(CornerRadiusEntry, "60,10,20,40");
 
-			App.WaitForElement("OpacityEntry");
-			App.EnterText("OpacityEntry", "0.5");
+		App.WaitForElement(CornerRadiusLabel);
+		App.Tap(CornerRadiusLabel);
 
-			App.WaitForElement("OpacityLabel");
-			App.Tap("OpacityLabel");
+		App.WaitForElement(OpacityEntry);
+		App.EnterText(OpacityEntry, "0.5");
 
-			App.WaitForElement("ShadowCheckBox");
-			App.Tap("ShadowCheckBox");
+		App.WaitForElement(OpacityLabel);
+		App.Tap(OpacityLabel);
 
-			VerifyScreenshot();
-		}
+		App.WaitForElement(ShadowCheckBox);
+		App.Tap(ShadowCheckBox);
 
-		[Test]
-		[Category(UITestCategories.BoxView)]
-		public void BoxView_CornerRadiusWithColorAndShadow()
-		{
-			App.WaitForElement("ResetButton");
-			App.Tap("ResetButton");
+		VerifyScreenshot();
+	}
 
-			App.WaitForElement("CornerRadiusEntry");
-			App.EnterText("CornerRadiusEntry", "60,10,20,40");
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxView_CornerRadiusWithColorAndShadow()
+	{
+		App.WaitForElement(ResetButton);
+		App.Tap(ResetButton);
 
-			App.WaitForElement("CornerRadiusLabel");
-			App.Tap("CornerRadiusLabel");
+		App.WaitForElement(CornerRadiusEntry);
+		App.EnterText(CornerRadiusEntry, "60,10,20,40");
 
-			App.WaitForElement("RedRadioButton");
-			App.Tap("RedRadioButton");
+		App.WaitForElement(CornerRadiusLabel);
+		App.Tap(CornerRadiusLabel);
 
-			App.WaitForElement("ShadowCheckBox");
-			App.Tap("ShadowCheckBox");
+		App.WaitForElement(RedRadioButton);
+		App.Tap(RedRadioButton);
 
-			VerifyScreenshot();
-		}
+		App.WaitForElement(ShadowCheckBox);
+		App.Tap(ShadowCheckBox);
 
-		[Test]
-		[Category(UITestCategories.BoxView)]
-		public void BoxView_ColorWithOpacityAndShadow()
-		{
-			App.WaitForElement("ResetButton");
-			App.Tap("ResetButton");
+		VerifyScreenshot();
+	}
 
-			App.WaitForElement("RedRadioButton");
-			App.Tap("RedRadioButton");
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxView_ColorWithOpacityAndShadow()
+	{
+		App.WaitForElement(ResetButton);
+		App.Tap(ResetButton);
 
-			App.WaitForElement("OpacityEntry");
-			App.EnterText("OpacityEntry", "0.5");
+		App.WaitForElement(RedRadioButton);
+		App.Tap(RedRadioButton);
 
-			App.WaitForElement("OpacityLabel");
-			App.Tap("OpacityLabel");
+		App.WaitForElement(OpacityEntry);
+		App.EnterText(OpacityEntry, "0.5");
 
-			App.WaitForElement("ShadowCheckBox");
-			App.Tap("ShadowCheckBox");
+		App.WaitForElement(OpacityLabel);
+		App.Tap(OpacityLabel);
 
-			VerifyScreenshot();
-		}
-#endif
+		App.WaitForElement(ShadowCheckBox);
+		App.Tap(ShadowCheckBox);
+
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxView_FillWithSolidColor()
+	{
+		App.WaitForElement("SolidRadioButton");
+		App.Tap("SolidRadioButton");
+
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxView_FillWithLinearGradient()
+	{
+		App.WaitForElement("LinearRadioButton");
+		App.Tap("LinearRadioButton");
+
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.BoxView)]
+	public void BoxView_FillWithRadialGradient()
+	{
+		App.WaitForElement("RadialRadioButton");
+		App.Tap("RadialRadioButton");
+
+		VerifyScreenshot();
 	}
 }
