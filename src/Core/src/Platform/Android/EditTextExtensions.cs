e4e804be12fa054064566f8cab855f86d520d4d0
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-07-23 18:00:01 +0530

    Updated the fix

--- src/Core/src/Platform/Android/EditTextExtensions.cs
+++ src/Core/src/Platform/Android/EditTextExtensions.cs
@@ -218,6 +218,14 @@ namespace Microsoft.Maui.Platform
 			}
 		}
 
+		internal static void UpdateClearButtonColor(this EditText editText, Graphics.Color textColor, Drawable? clearButtonDrawable)
+		{
+			if (textColor is not null)
+				clearButtonDrawable?.SetColorFilter(textColor.ToPlatform(), FilterMode.SrcIn);
+			else
+				clearButtonDrawable?.ClearColorFilter();
+		}
+
 		public static void UpdateReturnType(this EditText editText, IEntry entry)
 		{
 			editText.ImeOptions = entry.ReturnType.ToPlatform();
