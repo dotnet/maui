7b8bc8ec75795eb7fc508b0e50cdf59d4fc09a61
Matthew Leibowitz <mattleibow@live.com>
2025-07-23 23:45:03 +0200

    windows is done

--- src/Core/src/Primitives/WebViewInitializationCompletedEventArgs.cs
+++ src/Core/src/Primitives/WebViewInitializationCompletedEventArgs.cs
@@ -2,26 +2,39 @@ using System;
 #if IOS || MACCATALYST
 using PlatformWebView = WebKit.WKWebView;
 #elif ANDROID
-using PlatformWebView = Microsoft.Maui.Platform.MauiHybridWebView;
+using PlatformWebView = Android.Webkit.WebView;
 #elif WINDOWS
 using PlatformWebView = Microsoft.Web.WebView2.Core.CoreWebView2;
 #endif
 
 namespace Microsoft.Maui;
 
+/// <summary>
+/// Provides platform-specific information for the <see cref="IInitializationAwareWebView.WebViewInitializationCompleted(WebViewInitializationCompletedEventArgs)"/> event.
+/// </summary>
 public class WebViewInitializationCompletedEventArgs
 {
 #if IOS || MACCATALYST || ANDROID || WINDOWS
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="WebViewInitializationCompletedEventArgs"/> class.
+	/// </summary>
+	/// <param name="sender">The native view that is being initialized.</param>
 	internal WebViewInitializationCompletedEventArgs(PlatformWebView? sender)
 	{
 		Sender = sender;
 	}
 
+	/// <summary>
+	/// Gets the native view attached to the event.
+	/// </summary>
 	public PlatformWebView? Sender { get; }
 
 #else
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="WebViewInitializationCompletedEventArgs"/> class.
+	/// </summary>
 	internal WebViewInitializationCompletedEventArgs()
 	{
 	}
