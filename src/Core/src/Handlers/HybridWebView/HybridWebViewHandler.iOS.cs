00345103e471d3083e808f157700eabf0469f69c
Matthew Leibowitz <mattleibow@live.com>
2025-07-24 20:13:57 +0200

    Rework windows

--- src/Core/src/Handlers/HybridWebView/HybridWebViewHandler.iOS.cs
+++ src/Core/src/Handlers/HybridWebView/HybridWebViewHandler.iOS.cs
@@ -66,7 +66,7 @@ namespace Microsoft.Maui.Handlers
 			}
 
 			// Invoke the WebViewInitialized event to signal that the web view has been initialized
-			var initializedArgs = new WebViewInitializationCompletedEventArgs(webview);
+			var initializedArgs = new WebViewInitializationCompletedEventArgs(webview, config);
 			VirtualView?.WebViewInitializationCompleted(initializedArgs);
 
 			return webview;
