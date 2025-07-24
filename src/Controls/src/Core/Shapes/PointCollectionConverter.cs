5521734a3706a30ee1354df06fe18c5eaadeb3d2
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-07-24 22:14:15 +0530

    [Net 10] - Annotated remaining Converter classes for nullability - 2 (#30777)
    * Annotated converters
    
    * fixed missing API
    
    * more changes
    
    * further refactoring
    
    * more changes

--- src/Controls/src/Core/Shapes/PointCollectionConverter.cs
+++ src/Controls/src/Core/Shapes/PointCollectionConverter.cs
@@ -1,4 +1,3 @@
-#nullable disable
 using System;
 using System.ComponentModel;
 using System.Globalization;
@@ -11,13 +10,13 @@ namespace Microsoft.Maui.Controls.Shapes
 	/// <include file="../../../docs/Microsoft.Maui.Controls.Shapes/PointCollectionConverter.xml" path="Type[@FullName='Microsoft.Maui.Controls.Shapes.PointCollectionConverter']/Docs/*" />
 	public class PointCollectionConverter : TypeConverter
 	{
-		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
 			=> sourceType == typeof(string) || sourceType == typeof(Point[]);
 
-		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		public override bool CanConvertTo(ITypeDescriptorContext? context, Type? destinationType)
 			=> destinationType == typeof(string);
 
-		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
 		{
 			if (value is Point[] pointArray)
 			{
@@ -25,7 +24,12 @@ namespace Microsoft.Maui.Controls.Shapes
 			}
 
 			var strValue = value?.ToString();
-			string[] points = strValue.Split(new char[] { ' ', ',' });
+			if (string.IsNullOrWhiteSpace(strValue))
+			{
+				throw new InvalidOperationException("Cannot convert null or empty string into PointCollection.");
+			}
+
+			string[] points = strValue!.Split(new char[] { ' ', ',' });
 			var pointCollection = new PointCollection();
 			double x = 0;
 			bool hasX = false;
@@ -33,7 +37,9 @@ namespace Microsoft.Maui.Controls.Shapes
 			foreach (string point in points)
 			{
 				if (string.IsNullOrWhiteSpace(point))
+				{
 					continue;
+				}
 
 				if (double.TryParse(point, NumberStyles.Number, CultureInfo.InvariantCulture, out double number))
 				{
@@ -49,19 +55,25 @@ namespace Microsoft.Maui.Controls.Shapes
 					}
 				}
 				else
+				{
 					throw new InvalidOperationException(string.Format("Cannot convert \"{0}\" into {1}", point, typeof(double)));
+				}
 			}
 
 			if (hasX)
+			{
 				throw new InvalidOperationException(string.Format("Cannot convert string into PointCollection"));
+			}
 
 			return pointCollection;
 		}
 
-		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
 		{
 			if (value is not PointCollection pc)
+			{
 				throw new NotSupportedException();
+			}
 
 			var converter = new PointTypeConverter();
 			return string.Join(", ", pc.Select(p => converter.ConvertToInvariantString(p)));
