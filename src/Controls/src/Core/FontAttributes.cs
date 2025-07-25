e99fea7d899ada0f0e34729296e926a0b16c62f4
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-07-25 11:55:47 +0530

    enabled more converters for nullable

--- src/Controls/src/Core/FontAttributes.cs
+++ src/Controls/src/Core/FontAttributes.cs
@@ -4,77 +4,101 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 
-namespace Microsoft.Maui.Controls
+namespace Microsoft.Maui.Controls;
+
+/// <include file="../../docs/Microsoft.Maui.Controls/FontAttributes.xml" path="Type[@FullName='Microsoft.Maui.Controls.FontAttributes']/Docs/*" />
+[Flags]
+public enum FontAttributes
 {
-	/// <include file="../../docs/Microsoft.Maui.Controls/FontAttributes.xml" path="Type[@FullName='Microsoft.Maui.Controls.FontAttributes']/Docs/*" />
-	[Flags]
-	public enum FontAttributes
+	/// <include file="../../docs/Microsoft.Maui.Controls/FontAttributes.xml" path="//Member[@MemberName='None']/Docs/*" />
+	None = 0,
+	/// <include file="../../docs/Microsoft.Maui.Controls/FontAttributes.xml" path="//Member[@MemberName='Bold']/Docs/*" />
+	Bold = 1 << 0,
+	/// <include file="../../docs/Microsoft.Maui.Controls/FontAttributes.xml" path="//Member[@MemberName='Italic']/Docs/*" />
+	Italic = 1 << 1
+}
+#nullable enable
+/// <include file="../../docs/Microsoft.Maui.Controls/FontAttributesConverter.xml" path="Type[@FullName='Microsoft.Maui.Controls.FontAttributesConverter']/Docs/*" />
+public sealed class FontAttributesConverter : TypeConverter
+{
+	public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+		=> sourceType == typeof(string);
+
+	public override bool CanConvertTo(ITypeDescriptorContext? context, Type? destinationType)
+		=> destinationType == typeof(string);
+
+	public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
 	{
-		/// <include file="../../docs/Microsoft.Maui.Controls/FontAttributes.xml" path="//Member[@MemberName='None']/Docs/*" />
-		None = 0,
-		/// <include file="../../docs/Microsoft.Maui.Controls/FontAttributes.xml" path="//Member[@MemberName='Bold']/Docs/*" />
-		Bold = 1 << 0,
-		/// <include file="../../docs/Microsoft.Maui.Controls/FontAttributes.xml" path="//Member[@MemberName='Italic']/Docs/*" />
-		Italic = 1 << 1
+		var strValue = value?.ToString() ?? string.Empty;
+
+		if (string.IsNullOrEmpty(strValue) || string.IsNullOrWhiteSpace(strValue))
+		{
+			return FontAttributes.None;
+		}
+
+		FontAttributes attributes = FontAttributes.None;
+		strValue = strValue.Trim();
+		if (strValue.IndexOf(",", StringComparison.Ordinal) != -1)
+		{ //Xaml
+			foreach (var part in strValue.Split(','))
+			{
+				attributes |= ParseSingleAttribute(part, strValue);
+			}
+		}
+		else
+		{ //CSS or single value
+			foreach (var part in strValue.Split(' '))
+			{
+				attributes |= ParseSingleAttribute(part, strValue);
+			}
+		}
+		return attributes;
 	}
 
-	/// <include file="../../docs/Microsoft.Maui.Controls/FontAttributesConverter.xml" path="Type[@FullName='Microsoft.Maui.Controls.FontAttributesConverter']/Docs/*" />
-	public sealed class FontAttributesConverter : TypeConverter
+	static FontAttributes ParseSingleAttribute(string part, string originalvalue)
 	{
-		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
-			=> sourceType == typeof(string);
-
-		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
-			=> destinationType == typeof(string);
-
-		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		part = part.Trim();
+		if (Enum.TryParse(part, true, out FontAttributes attr))
 		{
-			var strValue = value?.ToString();
-
-			if (string.IsNullOrEmpty(strValue))
-				return FontAttributes.None;
-
-			FontAttributes attributes = FontAttributes.None;
-			strValue = strValue.Trim();
-			if (strValue.IndexOf(",", StringComparison.Ordinal) != -1)
-			{ //Xaml
-				foreach (var part in strValue.Split(','))
-					attributes |= ParseSingleAttribute(part, strValue);
-
-			}
-			else
-			{ //CSS or single value
-				foreach (var part in strValue.Split(' '))
-					attributes |= ParseSingleAttribute(part, strValue);
-			}
-			return attributes;
+			return attr;
 		}
 
-		static FontAttributes ParseSingleAttribute(string part, string originalvalue)
+		if (part.Equals("normal", StringComparison.OrdinalIgnoreCase))
 		{
-			part = part.Trim();
-			if (Enum.TryParse(part, true, out FontAttributes attr))
-				return attr;
-			if (part.Equals("normal", StringComparison.OrdinalIgnoreCase))
-				return FontAttributes.None;
-			if (part.Equals("oblique", StringComparison.OrdinalIgnoreCase))
-				return FontAttributes.Italic;
-
-			throw new InvalidOperationException(string.Format("Cannot convert \"{0}\" into {1}", originalvalue, typeof(FontAttributes)));
+			return FontAttributes.None;
 		}
 
-		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		if (part.Equals("oblique", StringComparison.OrdinalIgnoreCase))
 		{
-			if (value is not FontAttributes attr)
-				throw new NotSupportedException();
-			if (attr == FontAttributes.None)
-				return "";
-			var parts = new List<string>();
-			if ((attr & FontAttributes.Bold) == FontAttributes.Bold)
-				parts.Add(nameof(FontAttributes.Bold));
-			if ((attr & FontAttributes.Italic) == FontAttributes.Italic)
-				parts.Add(nameof(FontAttributes.Italic));
-			return string.Join("' ", parts);
+			return FontAttributes.Italic;
 		}
+
+		throw new InvalidOperationException(string.Format("Cannot convert \"{0}\" into {1}", originalvalue, typeof(FontAttributes)));
+	}
+
+	public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
+	{
+		if (value is not FontAttributes attr)
+		{
+			throw new NotSupportedException();
+		}
+
+		if (attr == FontAttributes.None)
+		{
+			return "";
+		}
+
+		var parts = new List<string>();
+		if ((attr & FontAttributes.Bold) == FontAttributes.Bold)
+		{
+			parts.Add(nameof(FontAttributes.Bold));
+		}
+
+		if ((attr & FontAttributes.Italic) == FontAttributes.Italic)
+		{
+			parts.Add(nameof(FontAttributes.Italic));
+		}
+
+		return string.Join("' ", parts);
 	}
 }
