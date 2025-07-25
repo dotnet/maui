17e2928ccb21e70616f70d6fef87cecdb527ca1f
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2025-07-25 15:17:20 +0530

    Added the test for the issue

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28557.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue28557 : _IssuesUITest
+{
+	public Issue28557(TestDevice device) : base(device) { }
+
+	public override string Issue => "NRE in CarouselViewController on iOS 15.5 & 16.4";
+
+	[Test]
+	[Category(UITestCategories.CarouselView)]
+	public void CarouselViewShouldNotCrashOnSourceUpdateWithPageNavigation()
+	{
+		App.WaitForElement("TestCarouselView");
+		App.Tap("NavigateToButton");
+		App.WaitForElement("SourceUpdateAndNavigateBackButton");
+		App.Tap("SourceUpdateAndNavigateBackButton");
+		App.WaitForElement("TestCarouselView");
+	}
+}
