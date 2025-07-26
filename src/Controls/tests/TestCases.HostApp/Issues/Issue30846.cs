c040533052f1699f42743517a0896052734db6c6
Matthew Leibowitz <mattleibow@live.com>
2025-07-26 03:08:44 +0200

    did this thing

--- src/Controls/tests/TestCases.HostApp/Issues/Issue30846.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue30846.cs
@@ -3,6 +3,9 @@ namespace TestCases.HostApp.Issues;
 [Issue(IssueTracker.Github, 30846, "Media Playback Customization in HybridWebView", PlatformAffected.All)]
 public partial class Issue30846 : ContentPage
 {
+	Label _videoStatusLabel;
+	Label _audioStatusLabel;
+
 	public Issue30846()
 	{
 		Title = "HybridWebView Autoplay Test";
@@ -15,7 +18,9 @@ public partial class Issue30846 : ContentPage
 			RowDefinitions =
 			{
 				new RowDefinition(GridLength.Auto), // First row: switch and label
-				new RowDefinition(GridLength.Star)  // Second row: webview container fills remaining height
+				new RowDefinition(GridLength.Auto), // Second row: video status
+				new RowDefinition(GridLength.Auto), // Third row: audio status
+				new RowDefinition(GridLength.Star)  // Forth row: webview container fills remaining height
 			},
 			ColumnDefinitions =
 			{
@@ -32,7 +37,7 @@ public partial class Issue30846 : ContentPage
 			VerticalOptions = LayoutOptions.Fill,
 		};
 		grid.Add(webViewContainer);
-		Grid.SetRow(webViewContainer, 1);
+		Grid.SetRow(webViewContainer, 3);
 		Grid.SetColumnSpan(webViewContainer, 2);
 
 		var switchControl = new Switch
@@ -57,10 +62,32 @@ public partial class Issue30846 : ContentPage
 		Grid.SetColumn(switchLabel, 1);
 		grid.Add(switchLabel);
 
+		_videoStatusLabel = new Label
+		{
+			AutomationId = "VideoStatusLabel",
+			Text = "",
+			HorizontalOptions = LayoutOptions.Start,
+			VerticalOptions = LayoutOptions.Center,
+		};
+		Grid.SetRow(_videoStatusLabel, 1);
+		Grid.SetColumnSpan(_videoStatusLabel, 2);
+		grid.Add(_videoStatusLabel);
+
+		_audioStatusLabel = new Label
+		{
+			AutomationId = "AudioStatusLabel",
+			Text = "",
+			HorizontalOptions = LayoutOptions.Start,
+			VerticalOptions = LayoutOptions.Center,
+		};
+		Grid.SetRow(_audioStatusLabel, 2);
+		Grid.SetColumnSpan(_audioStatusLabel, 2);
+		grid.Add(_audioStatusLabel);
+
 		Content = grid;
 	}
 
-	static HybridWebView CreateHybridWebView(bool requireUserGesture)
+	HybridWebView CreateHybridWebView(bool requireUserGesture)
 	{
 		var hybridWebView = new HybridWebView
 		{
@@ -69,6 +96,14 @@ public partial class Issue30846 : ContentPage
 			HybridRoot = "hybridroot",
 			HorizontalOptions = LayoutOptions.Fill,
 			VerticalOptions = LayoutOptions.Fill,
+		};
+
+		hybridWebView.RawMessageReceived += (s, e) =>
+		{
+			if (e.Message.StartsWith("Video"))
+				_videoStatusLabel.Text = e.Message;
+			else if (e.Message.StartsWith("Audio"))
+				_audioStatusLabel.Text = e.Message;
 		};
 
 		hybridWebView.WebViewInitializing += (s, e) =>
@@ -87,10 +122,18 @@ public partial class Issue30846 : ContentPage
 			// Set media playback requirements based on the switch state
 			e.PlatformArgs.Settings.MediaPlaybackRequiresUserGesture = requireUserGesture;
 #elif WINDOWS
+			// WebView2 requires that different environments have different UDF to support multiple simultaneous instances.
+			// Technically we are swapping out the instances, but the first instance takes time to shut down.
+			var lad = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+			e.PlatformArgs.UserDataFolder = Path.Combine(lad, "Controls.TestCases.HostApp", $"UserDataFolder-{requireUserGesture}");
+
 			// Set media playback requirements based on the switch state
-			e.PlatformArgs.EnvironmentOptions.AdditionalBrowserArguments = requireUserGesture
-				? "--autoplay-policy=no-user-gesture-required"
-				: "";
+			e.PlatformArgs.EnvironmentOptions = new Microsoft.Web.WebView2.Core.CoreWebView2EnvironmentOptions
+			{
+				AdditionalBrowserArguments = requireUserGesture
+					? ""
+					: "--autoplay-policy=no-user-gesture-required"
+			};
 #endif
 		};
 
