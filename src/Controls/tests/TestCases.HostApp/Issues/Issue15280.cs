3dc2b9b5c206493de9f49d91a3723682d8fb2b7e
BagavathiPerumal <bagavathiperumal.a@syncfusion.com>
2025-07-28 18:43:01 +0530

    fix-15280-Testcase Updated.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue15280.cs
@@ -0,0 +1,68 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 15280, "Swipe gestures attached to rotated controls are rotated on Android", PlatformAffected.Android)]
+public partial class Issue15280 : ContentPage
+{
+    Image _botImage;
+    Label _directionLabel;
+    public Issue15280()
+    {
+        Title = "Swipe Gesture Rotation Test";
+        Padding = 20;
+
+        _botImage = new Image
+        {
+            AutomationId = "RotatedImage",
+            Source = "dotnet_bot.png",
+            HeightRequest = 300,
+            WidthRequest = 300,
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+            BackgroundColor = Colors.LightBlue
+        };
+
+        var upSwipe = new SwipeGestureRecognizer { Direction = SwipeDirection.Up, Threshold = 20 };
+        var downSwipe = new SwipeGestureRecognizer { Direction = SwipeDirection.Down, Threshold = 20 };
+        var leftSwipe = new SwipeGestureRecognizer { Direction = SwipeDirection.Left, Threshold = 20 };
+        var rightSwipe = new SwipeGestureRecognizer { Direction = SwipeDirection.Right, Threshold = 20 };
+
+        upSwipe.Swiped += SwipeGestureRecognizer_Swiped;
+        downSwipe.Swiped += SwipeGestureRecognizer_Swiped;
+        leftSwipe.Swiped += SwipeGestureRecognizer_Swiped;
+        rightSwipe.Swiped += SwipeGestureRecognizer_Swiped;
+
+        _botImage.GestureRecognizers.Add(upSwipe);
+        _botImage.GestureRecognizers.Add(downSwipe);
+        _botImage.GestureRecognizers.Add(leftSwipe);
+        _botImage.GestureRecognizers.Add(rightSwipe);
+
+        _directionLabel = new Label
+        {
+            AutomationId = "DirectionLabel",
+            Text = "Try swiping Left on the image",
+            FontSize = 18,
+            HorizontalOptions = LayoutOptions.Center,
+            HorizontalTextAlignment = TextAlignment.Center,
+            TextColor = Colors.Blue
+        };
+
+        var mainLayout = new VerticalStackLayout
+        {
+            Spacing = 20,
+            Children = 
+            {
+                _botImage,
+                _directionLabel
+            }
+        };
+
+        Content = new ScrollView { Content = mainLayout };
+    }
+
+    void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+    {
+        _directionLabel.Text = $"Swiped: {e.Direction.ToString().ToUpperInvariant()}";
+        
+        _botImage.Rotation += 90;
+    }
+}
