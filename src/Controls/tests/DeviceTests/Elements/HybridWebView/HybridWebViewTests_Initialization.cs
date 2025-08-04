73feb799b256621d213dd2b592ddc82d1760562c
Matthew Leibowitz <mattleibow@live.com>
2025-08-04 20:42:51 +0200

    WebView2 has restrictions
    WebView2 does not allow different envvars and some settings in a
    single process simultaneously without also configuring different user
    data folders.
    
    Also, better to run the webview tests in series as they can confict
    with other things we do.

--- src/Controls/tests/DeviceTests/Elements/HybridWebView/HybridWebViewTests_Initialization.cs
+++ src/Controls/tests/DeviceTests/Elements/HybridWebView/HybridWebViewTests_Initialization.cs
@@ -8,6 +8,9 @@ using Xunit;
 namespace Microsoft.Maui.DeviceTests;
 
 [Category(TestCategory.HybridWebView)]
+#if WINDOWS
+[Collection(WebViewsCollection)]
+#endif
 public partial class HybridWebViewTests_Initialization : HybridWebViewTestsBase
 {
 	const string UserAgent = "HybridWebViewTests User Agent";
