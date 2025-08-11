e1e1acfbcfd079e0340ead1b395286a4ce97d14e
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-08-11 16:34:38 +0530

    Fixed IOS image not loaded from sub dir

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22887.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue22887 : _IssuesUITest
+{
+
+	public Issue22887(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Microsoft.Maui.Controls.ImageSource.FromFile fails in iOS when image in subfolder";
+
+
+	[Test]
+	[Category(UITestCategories.Image)]
+	public void ImageShouldLoadFromSubfolder()
+	{
+		App.WaitForElement("ImageView", timeoutMessage: "Image is not rendered in the view");
+
+	}
+}
