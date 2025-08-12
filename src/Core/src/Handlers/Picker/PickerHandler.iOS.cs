c9313f04a2f15b8e336b1067b6262bf86e1b632e
Jakub Florkowski <42434498+kubaflo@users.noreply.github.com>
2025-08-12 12:41:36 +0200

    MacOS picker dialog improvement (#27525)

--- src/Core/src/Handlers/Picker/PickerHandler.iOS.cs
+++ src/Core/src/Handlers/Picker/PickerHandler.iOS.cs
@@ -38,7 +38,7 @@ namespace Microsoft.Maui.Handlers
 		protected override MauiPicker CreatePlatformView() =>
 			new MauiPicker(null) { BorderStyle = UITextBorderStyle.RoundedRect };
 
-		void DisplayAlert(MauiPicker uITextField)
+		void DisplayAlert(MauiPicker uITextField, int selectedIndex)
 		{
 			var paddingTitle = 0;
 			if (!string.IsNullOrEmpty(VirtualView.Title))
@@ -49,6 +49,12 @@ namespace Microsoft.Maui.Handlers
 			var pickerView = new UIPickerView(frame);
 			pickerView.Model = new PickerSource(this);
 			pickerView?.ReloadAllComponents();
+
+			var source = (PickerSource)pickerView.Model;
+			source.SelectedIndex = selectedIndex;
+			pickerView.Select(Math.Max(selectedIndex, 0), 0, true);
+			pickerView.ReloadAllComponents();
+
 			// The UIPickerView is displayed as a subview of the UIAlertController when an empty string is provided as the title, instead of using the VirtualView title. 
 			// This behavior deviates from the expected native macOS behavior.
 			var pickerController = UIAlertController.Create("", "", UIAlertControllerStyle.ActionSheet);
@@ -247,7 +253,8 @@ namespace Microsoft.Maui.Handlers
 				if (Handler is not PickerHandler handler)
 					return;
 
-				handler.DisplayAlert(handler.PlatformView);
+				int selectedIndex = handler.VirtualView?.SelectedIndex ?? 0;
+				handler.DisplayAlert(handler.PlatformView, selectedIndex);
 #endif
 			}
 
