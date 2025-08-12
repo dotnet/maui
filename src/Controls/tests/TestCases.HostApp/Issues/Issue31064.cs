3a59c3c251ff256fc88ebc7d81772d9d7dda3bd0
Subhiksha Chandrasekaran <subhiksha.c@syncfusion.com>
2025-08-12 13:37:43 +0530

    Test sample for indicator view

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue31064.cs
@@ -0,0 +1,54 @@
+namespace Controls.TestCases.HostApp.Issues;
+
+[Issue(IssueTracker.Github, 31064, "Indicator view size doesnt update dynamically", PlatformAffected.iOS)]
+
+public class Issue31064 : ContentPage
+{
+	IndicatorView _indicator;
+
+	public Issue31064()
+	{
+		var carousel = new CarouselView
+		{
+			ItemsSource = new[] { "Item 1", "Item 2", "Item 3" },
+			HeightRequest = 250,
+			HorizontalScrollBarVisibility = ScrollBarVisibility.Never,
+		};
+
+		_indicator = new IndicatorView
+		{
+			HorizontalOptions = LayoutOptions.Center
+		};
+
+		carousel.IndicatorView = _indicator;
+
+		var button = new Button
+		{
+			AutomationId = "Issue31064Button",
+			Text = "Increase Indicator Size",
+			HorizontalOptions = LayoutOptions.Center
+		};
+		button.Clicked += (s, e) =>
+		{
+			_indicator.IndicatorSize = 20;
+		};
+
+		Content = new VerticalStackLayout
+		{
+			Spacing = 20,
+			Padding = 20,
+			Children =
+			{
+				new Label
+				{
+					HorizontalOptions = LayoutOptions.Center,
+					VerticalOptions = LayoutOptions.Center,
+					Text = "Test passes if IndicatorView size updates dynamically"
+				},
+				carousel,
+				_indicator,
+				button
+			}
+		};
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31064.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue31064 : _IssuesUITest
+{
+	public Issue31064(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Indicator view size doesnt update dynamically";
+
+	[Test]
+	[Category(UITestCategories.IndicatorView)]
+	public void Issue31064IndicatorViewSizeUpdatesDynamically()
+	{
+		App.WaitForElement("Issue31064Button");
+		App.Tap("Issue31064Button");
+		VerifyScreenshot();
+	}
+}
