c539d79e463b832532787dbe9a698a23f0b47f53
Evin Ballantyne <EBallantyne@acceleratedsystems.com>
2025-08-14 14:19:59 -0400

    new file to file scoped namespace

--- src/Controls/samples/Controls.Sample/ViewModels/RadioButtonNullSelectionViewModel.cs
+++ src/Controls/samples/Controls.Sample/ViewModels/RadioButtonNullSelectionViewModel.cs
@@ -2,27 +2,26 @@
 using Maui.Controls.Sample.ViewModels.Base;
 using Microsoft.Maui.Controls;
 
-namespace Maui.Controls.Sample.ViewModels
+namespace Maui.Controls.Sample.ViewModels;
+
+public class RadioButtonNullSelectionViewModel : BaseViewModel
 {
-    public class RadioButtonNullSelectionViewModel : BaseViewModel
-    {
-	    object? _selectedItem = null;
+	object? _selectedItem = null;
 
-	    public object? SelectedItem
-	    {
-		    get => _selectedItem;
-		    set
-		    {
-			    _selectedItem = value;
-			    OnPropertyChanged();
-		    }
-	    }
+	public object? SelectedItem
+	{
+		get => _selectedItem;
+		set
+		{
+			_selectedItem = value;
+			OnPropertyChanged();
+		}
+	}
 
-	    public ICommand ClearSelectionCommand => new Command(ExecuteClearSelectionCommand);
+	public ICommand ClearSelectionCommand => new Command(ExecuteClearSelectionCommand);
 
-	    void ExecuteClearSelectionCommand()
-	    {
-		    SelectedItem = null;
-	    }
+	void ExecuteClearSelectionCommand()
+	{
+		SelectedItem = null;
 	}
 }
