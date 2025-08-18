b53bdf3a2243f0ee0071a5a312cab827dc7a4fbb
Anandhan Rajagopal <97146406+anandhan-rajagopal@users.noreply.github.com>
2025-08-18 18:02:09 +0530

    Added test for Issue31139

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31139.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue31139 : _IssuesUITest
+	{
+		public Issue31139(TestDevice device) : base(device) { }
+
+		public override string Issue => "Issue31139";
+
+		[Test]
+		[Category(UITestCategories.Dispatcher)]
+		public void BindableUpdatesFromBackgroundThreadDoNotCrashAndPropagate()
+		{
+			App.WaitForElement("Success", timeout: TimeSpan.FromSeconds(3));
+		}
+	}
+}
