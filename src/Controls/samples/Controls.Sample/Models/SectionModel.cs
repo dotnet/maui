fc8c842c87c10ef5ef6fe247d6fb77c557eaa6d8
Rui Marinho <me@ruimarinho.net>
2025-08-19 13:32:01 +0100

    Fixes for trimming

--- src/Controls/samples/Controls.Sample/Models/SectionModel.cs
+++ src/Controls/samples/Controls.Sample/Models/SectionModel.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Maui.Controls.Sample.Models
 {
 	public sealed class SectionModel
 	{
-		public SectionModel(Type type, string title, string description, object? viewModel = null)
+		public SectionModel(
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] Type type, 
+			string title, 
+			string description, 
+			object? viewModel = null)
 		{
 			Type = type;
 			Title = title;
@@ -12,6 +17,8 @@ namespace Maui.Controls.Sample.Models
 			ViewModel = viewModel;
 		}
 
+		// Add DynamicallyAccessedMembers attribute to Type property to fix IL2072 error
+		[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]
 		public Type Type { get; }
 
 		public string Title { get; }
