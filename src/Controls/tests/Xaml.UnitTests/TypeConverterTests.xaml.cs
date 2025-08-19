f7356f1a433c41ca8b29c00989a4a90f87ba124b
Gerald Versluis <gerald.versluis@microsoft.com>
2025-08-19 15:22:11 +0200

    Add tests

--- src/Controls/tests/Xaml.UnitTests/TypeConverterTests.xaml.cs
+++ src/Controls/tests/Xaml.UnitTests/TypeConverterTests.xaml.cs
@@ -10,6 +10,40 @@ public partial class TypeConverterTests : ContentPage
 	class Tests
 	{
 		[Test]
+		public void GridLengthsAreConverted([Values] XamlInflator inflator,
+			[Values("en-US", "pt-PT")] string culture)
+		{
+			System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(culture);
+
+			if (inflator == XamlInflator.SourceGen)
+			{
+				var result = MockSourceGenerator.CreateMauiCompilation()
+						.WithAdditionalSource(
+							"""
+							using System;
+							using NUnit.Framework;
+
+							namespace Microsoft.Maui.Controls.Xaml.UnitTests;
+
+							[XamlProcessing(XamlInflator.Default, true)]
+							public partial class TypeConverterTests : ContentPage
+							{
+								public TypeConverterTests() => InitializeComponent();
+							}
+							""").RunMauiSourceGenerator(typeof(TypeConverterTests));
+
+				Assert.That(result.Diagnostics, Is.Empty);
+			}
+			var page = new TypeConverterTests(inflator);
+			Assert.That(page.grid, Is.TypeOf<Grid>());
+			Assert.AreEqual(4, page.grid.RowDefinitions.Count);
+			Assert.AreEqual(GridLength.Star, page.grid.RowDefinitions[0].Height);
+			Assert.AreEqual(10, page.grid.RowDefinitions[1].Height.Value);
+			Assert.AreEqual(GridLength.Auto, page.grid.RowDefinitions[2].Height);
+			Assert.AreEqual(1.25, page.grid.RowDefinitions[3].Height.Value);
+		}
+
+		[Test]
 		public void UriAreConverted([Values] XamlInflator inflator)
 		{
 			var layout = new TypeConverterTests(inflator);
