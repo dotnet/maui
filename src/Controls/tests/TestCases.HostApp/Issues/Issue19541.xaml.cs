15aa2b73cecd13b6496e3fa7ba384537150549e5
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-08-19 19:11:17 +0530

    test case added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue19541.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 19541, "[iOS] - Swipeview with collectionview issue", PlatformAffected.iOS)]
+public partial class Issue19541 : ContentPage
+{
+    SwipeView _swipeView;
+	public Issue19541()
+	{
+		InitializeComponent();
+		LoadInitialList();
+	}
+
+	void LoadInitialList()
+	{
+		var list = new List<Issue19541Model>();
+		for (int i = 0; i < 3; i++)
+		{
+			list.Add(new Issue19541Model
+			{
+				Name = $"Name {i}",
+			});
+		}
+
+		collectionView.ItemsSource = list;
+	}
+
+	void OnRefreshClicked(object sender, EventArgs e)
+	{
+		var list = new List<Issue19541Model>();
+		for (int i = 0; i < 3; i++)
+		{
+			list.Add(new Issue19541Model
+			{
+				Name = $"Name {i}",
+			});
+		}
+
+		collectionView.ItemsSource = list;
+	}
+
+	void swipeView_Loaded(object sender, EventArgs e)
+	{
+		if (_swipeView is null)
+			_swipeView = (SwipeView)sender;
+	}
+
+	void Button_Clicked(object sender, EventArgs e)
+	{
+		if (_swipeView == null)
+			return;
+		
+		_swipeView.Open(OpenSwipeItem.RightItems);
+	}
+}
+
+
+public class Issue19541Model
+{
+    public string Name { get; set; }
+}
+
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue19541.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue19541 : _IssuesUITest
+{
+    public Issue19541(TestDevice device) : base(device)
+    {
+    }
+
+    public override string Issue => "[iOS] - Swipeview with collectionview issue";
+
+    [Test]
+    [Category(UITestCategories.SwipeView)]
+    public void SwipeItemShouldBeCloseWhenUpdateTheCollectionView()
+    {
+        App.WaitForElement("RefreshButton");
+        App.Tap("OpenButton");
+        App.Tap("RefreshButton");
+        VerifyScreenshot();
+    }
+}
