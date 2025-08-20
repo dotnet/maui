da15f74f4bf0e8877becafba9c692aa04eb079ec
praveenkumarkarunanithi <praveenkumar.karunanithi@syncfusion.com>
2025-08-20 18:17:04 +0530

    Create Issue29529.cs

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29529.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue29529 : _IssuesUITest
+{
+	public override string Issue => "CurrentItemChangedEventArgs and PositionChangedEventArgs Not Updating Correctly in CarouselView";
+
+	public Issue29529(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.CarouselView)]
+	public void Issue29529VerifyPreviousPositionOnInsert()
+	{
+		App.WaitForElement("carouselview");
+		App.Tap("ScrollToButton");
+		App.Tap("InsertButton");
+		var text = App.FindElement("positionLabel").GetText();
+		Assert.That(text, Is.EqualTo("Current Position: 0, Previous Position: 3"));
+		text = App.FindElement("itemLabel").GetText();
+		Assert.That(text, Is.EqualTo("Current Item: Item 1, Previous Item: Item 4"));
+		text = App.FindElement("eventCountLabel").GetText();
+		Assert.That(text, Is.EqualTo("PositionChanged: 1, CurrentItemChanged: 1"));
+	}
+}
