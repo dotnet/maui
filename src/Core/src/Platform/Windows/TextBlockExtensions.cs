3566861900071a7e38d4913bbb8d44ba0137c774
MartyIX <203266+MartyIX@users.noreply.github.com>
2025-08-22 13:54:12 +0200

    Actual change
    Co-authored-by: PureWeen <5375137+PureWeen@users.noreply.github.com>

--- src/Core/src/Platform/Windows/TextBlockExtensions.cs
+++ src/Core/src/Platform/Windows/TextBlockExtensions.cs
@@ -4,6 +4,7 @@ using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Resolvers;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Documents;
 using WThickness = Microsoft.UI.Xaml.Thickness;
@@ -70,6 +71,7 @@ namespace Microsoft.Maui.Platform
 			if (label.LineHeight >= 0)
 			{
 				platformControl.LineHeight = label.LineHeight * platformControl.FontSize;
+				platformControl.LineStackingStrategy = LineStackingStrategy.BlockLineHeight;
 			}
 		}
 
