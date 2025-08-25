40895bee666da3c51a9f021271cd8b3f8bf2b4e0
MartyIX <203266+MartyIX@users.noreply.github.com>
2025-08-25 08:54:23 +0200

    Disable test on Android

--- src/Controls/tests/TestCases.HostApp/Issues/Issue24520.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue24520.xaml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Internals;
-using Microsoft.Maui.Controls.Xaml;
-
-namespace Maui.Controls.Sample.Issues;
+﻿namespace Maui.Controls.Sample.Issues;
 
 [Issue(IssueTracker.Github, 24520, "Change the LineStackingStrategy to BlockLineHeight for Labels on Windows", PlatformAffected.UWP)]
 public partial class Issue24520 : ContentPage
