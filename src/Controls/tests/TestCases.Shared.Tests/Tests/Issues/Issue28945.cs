4eab1b5462bfddaed379997ce52b5908abfe1f40
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-08-29 10:37:11 +0530

    Modified Changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28945.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28945.cs
@@ -7,7 +7,6 @@ namespace Microsoft.Maui.TestCases.Tests.Issues;
 
 public class Issue28945 : _IssuesUITest
 {
-	const string toggleFocusButtonId = "TglFocusButton";
 	const string statusLabelId = "statusLabel";
 	const string FocusSuccessMessage = "ContentView Focused";
 	const string UnfocusSuccessMessage = "ContentView UnFocused";
@@ -37,19 +36,10 @@ public class Issue28945 : _IssuesUITest
 			{ "keys", keys },
 		});
 
-		App.WaitForElement(toggleFocusButtonId);
-		App.Tap(toggleFocusButtonId);
-
 		// Verify that the content view is focused
 		var focusText = App.WaitForElement(statusLabelId).GetText();
 		Assert.That(focusText, Is.EqualTo(FocusSuccessMessage));
 
-		App.WaitForElement(toggleFocusButtonId);
-		App.Tap(toggleFocusButtonId);
-
-		// Verify that the content view is unfocused
-		var unfocusText = App.WaitForElement(statusLabelId).GetText();
-		Assert.That(unfocusText, Is.EqualTo(UnfocusSuccessMessage));
 	}
 }
 #endif
