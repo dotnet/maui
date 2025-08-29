8c3c9994b8f968468718342ea38c5ee02b4da2aa
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2025-08-29 14:11:00 +0530

    Added test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue31361.cs
@@ -0,0 +1,70 @@
+using System.Collections.ObjectModel;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 31361, "CarouselView content is scrolling vertically", PlatformAffected.iOS)]
+public class Issue31361 : ContentPage
+{
+	bool isBounce;
+	public Issue31361()
+	{
+		var verticalStackLayout = new VerticalStackLayout();
+		var carouselItems = new ObservableCollection<string>
+		{
+			"Item 1",
+			"Item 2",
+			"Item 3",
+			"Item 4",
+		};
+
+		CarouselView2 carouselView = new CarouselView2
+		{
+			ItemsSource = carouselItems,
+			AutomationId = "carouselview",
+			ItemTemplate = new DataTemplate(() =>
+			{
+				var grid = new Grid
+				{
+					Margin = 50,
+					BackgroundColor = Colors.LightGray
+				};
+
+				var label = new Label
+				{
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+					FontSize = 18,
+				};
+				label.SetBinding(Label.TextProperty, ".");
+				grid.Children.Add(label);
+				return grid;
+			}),
+			HorizontalOptions = LayoutOptions.Fill,
+		};
+
+		var contentLabel = new Label
+		{
+			AutomationId = "contentLabel",
+			Text = "Test Content",
+			HorizontalOptions = LayoutOptions.Center,
+			Padding = new Thickness(20),
+		};
+
+		carouselView.Scrolled += (s, e) =>
+		{
+			isBounce = e.VerticalDelta == 0;
+			if (isBounce)
+			{
+				contentLabel.Text = "The content is not scrollable";
+			}
+			else
+			{
+				contentLabel.Text = "The content is scrollable";
+			}
+		};
+
+		verticalStackLayout.Children.Add(contentLabel);
+		verticalStackLayout.Children.Add(carouselView);
+		Content = verticalStackLayout;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31361.cs
@@ -0,0 +1,26 @@
+#if TEST_FAILS_ON_CATALYST  //In Catalyst, `ScrollDown` isn't functioning correctly with Appium.
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue31361 : _IssuesUITest
+{
+    public override string Issue => "CarouselView content is scrolling vertically";
+
+    public Issue31361(TestDevice device)
+    : base(device)
+    { }
+
+    [Test]
+    [Category(UITestCategories.CarouselView)]
+    public void VerifyHorizontalCarouselViewPreventsVerticalScrolling()
+    {
+        App.WaitForElement("carouselview");
+        App.ScrollDown("carouselview");
+        var text = App.FindElement("contentLabel").GetText();
+        Assert.That(text, Is.EqualTo("The content is not scrollable"));
+    }
+}
+#endif
