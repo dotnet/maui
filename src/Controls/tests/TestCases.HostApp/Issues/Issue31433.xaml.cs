a079baca9dfdd229758dfedd3a2a53ac1a8dac15
Jakub Florkowski <kubaflo123@gmail.com>
2025-09-02 01:25:02 +0200

    Added a UITest

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue31433.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 31433, "HorizontalOptions for FlexLayout does not work", PlatformAffected.Android)]
+public partial class Issue31433 : ContentPage
+{
+	public Issue31433()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31433.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue31433 : _IssuesUITest
+{
+	public Issue31433(TestDevice testDevice) : base(testDevice) { }
+
+	public override string Issue => "HorizontalOptions for FlexLayout does not work";
+
+	[Test]
+	[Category(UITestCategories.ScrollView)]
+	public void HorizontalOptionsForFlexLayoutShouldWork()
+	{
+		App.WaitForElement("button");
+		VerifyScreenshot();
+	}
+}
