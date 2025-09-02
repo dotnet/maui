69dc3dea36fb8b2c400d2c6a002c4c020fa97f71
BagavathiPerumal <bagavathiperumal.a@syncfusion.com>
2025-09-02 11:52:24 +0530

    fix-31121-Resolved the TabbedPage FlowDirection runtime updating issue by updating the FlowDirection of the View (UILayoutContainerView) using UpdateFlowDirection() when the TabbedPage FlowDirection property changes.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31121.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue31121 : _IssuesUITest
+	{
+		public Issue31121(TestDevice testDevice) : base(testDevice) { }
+
+		public override string Issue => "[iOS, Mac] TabbedPage FlowDirection Property Renders Opposite Layout Direction When Set via ViewModel Binding";
+
+		[Test]
+		[Category(UITestCategories.TabbedPage)]
+		public void TabbedPageFlowDirectionUpdatesOnRuntimeChange()
+		{
+			App.WaitForElement("RightToLeftButton");
+			App.Tap("RightToLeftButton");
+			VerifyScreenshot();
+		}
+	}
+}
