4cf314da41e1b98a5da2b90dd3980c997c9d7947
BagavathiPerumal <bagavathiperumal.a@syncfusion.com>
2025-09-02 11:57:59 +0530

    fix-31121-Changes committed.

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31121.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31121.cs
@@ -2,21 +2,19 @@
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue31121 : _IssuesUITest
 {
-	public class Issue31121 : _IssuesUITest
+	public Issue31121(TestDevice testDevice) : base(testDevice) { }
+
+	public override string Issue => "[iOS, Mac] TabbedPage FlowDirection Property Renders Opposite Layout Direction When Set via ViewModel Binding";
+
+	[Test]
+	[Category(UITestCategories.TabbedPage)]
+	public void TabbedPageFlowDirectionUpdatesOnRuntimeChange()
 	{
-		public Issue31121(TestDevice testDevice) : base(testDevice) { }
-
-		public override string Issue => "[iOS, Mac] TabbedPage FlowDirection Property Renders Opposite Layout Direction When Set via ViewModel Binding";
-
-		[Test]
-		[Category(UITestCategories.TabbedPage)]
-		public void TabbedPageFlowDirectionUpdatesOnRuntimeChange()
-		{
-			App.WaitForElement("RightToLeftButton");
-			App.Tap("RightToLeftButton");
-			VerifyScreenshot();
-		}
+		App.WaitForElement("RightToLeftButton");
+		App.Tap("RightToLeftButton");
+		VerifyScreenshot();
 	}
 }
