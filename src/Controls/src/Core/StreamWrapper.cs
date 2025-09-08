0e1971bc4f0139e9f9b46cb4aa357f8a90ebe12c
AhamedAliNishad <ahamedalinishad.j@syncfusion.com>
2025-09-08 15:21:02 +0530

    Fixed the UriImage not showing on windows

--- src/Controls/src/Core/StreamWrapper.cs
+++ src/Controls/src/Core/StreamWrapper.cs
@@ -1,4 +1,4 @@
-#nullable disable
+﻿#nullable disable
 using System;
 using System.IO;
 using System.Net.Http;
@@ -92,6 +92,12 @@ namespace Microsoft.Maui.Controls
 
 		public static async Task<Stream> GetStreamAsync(Uri uri, CancellationToken cancellationToken, HttpClient client)
 		{
+#if WINDOWS
+			if (client.DefaultRequestHeaders.UserAgent.Count == 0)
+			{
+				client.DefaultRequestHeaders.UserAgent.ParseAdd("MauiApp/1.1");
+			}
+#endif
 			var response = await client.GetAsync(uri, cancellationToken).ConfigureAwait(false);
 			if (!response.IsSuccessStatusCode)
 			{
