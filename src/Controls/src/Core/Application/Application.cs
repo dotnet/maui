5fd91a8da0f9584b9fdcf1661b9b06b6834fd6af
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-09-09 16:24:32 +0530

    Windows UserAppTheme Changes

--- src/Controls/src/Core/Application/Application.cs
+++ src/Controls/src/Core/Application/Application.cs
@@ -262,6 +262,11 @@ namespace Microsoft.Maui.Controls
 				_lastAppTheme = newTheme;
 
 				OnPropertyChanged(nameof(UserAppTheme));
+#if WINDOWS
+				// Notify platform so it can apply the correct UI theme
+				OnRequestedThemeChangedPlatform(newTheme);
+#endif
+
 				OnParentResourcesChanged([new KeyValuePair<string, object>(AppThemeBinding.AppThemeResource, newTheme)]);
 				_weakEventManager.HandleEvent(this, new AppThemeChangedEventArgs(newTheme), nameof(RequestedThemeChanged));
 			}
@@ -270,6 +275,10 @@ namespace Microsoft.Maui.Controls
 				_themeChangedFiring = false;
 			}
 		}
+
+#if WINDOWS
+		partial void OnRequestedThemeChangedPlatform(AppTheme newTheme);
+#endif
 
 		public event EventHandler<ModalPoppedEventArgs>? ModalPopped;
 
