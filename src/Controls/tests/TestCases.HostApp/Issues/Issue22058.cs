212f5387e0dd612ad485c1526396c95fcb33fcbd
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-09-11 16:57:01 +0530

    Added test

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue22058.cs
@@ -0,0 +1,49 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 22058, "[Windows] OS system components ignore app theme", PlatformAffected.UWP)]
+public class Issue22058 : ContentPage
+{
+	public Issue22058()
+	{
+		var button = new Button
+		{
+			Text = "Change User App Theme",
+			AutomationId = "ThemeChangeButton",
+			VerticalOptions = LayoutOptions.Center,
+			HorizontalOptions = LayoutOptions.Center
+		};
+
+		button.Clicked += (sender, e) =>
+		{
+			if(Application.Current is not null)
+			{
+				if (Application.Current.UserAppTheme == AppTheme.Dark)
+				{
+					Application.Current.UserAppTheme = AppTheme.Unspecified;
+				}
+				else
+				{
+					Application.Current.UserAppTheme = AppTheme.Dark;
+				}
+			}
+		};
+
+		var timePicker = new TimePicker
+		{
+			VerticalOptions = LayoutOptions.Center,
+			AutomationId = "timePicker",
+			HorizontalOptions = LayoutOptions.End,
+		};
+
+		var verticalStackLayout = new VerticalStackLayout()
+		{
+			Spacing = 20,
+			Padding = new Thickness(20),
+		};
+
+		verticalStackLayout.Add(button);
+		verticalStackLayout.Add(timePicker);
+
+		Content = verticalStackLayout;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue22058.cs
@@ -0,0 +1,30 @@
+﻿#if WINDOWS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue22058 : _IssuesUITest
+{
+	public override string Issue => "[Windows] OS system components ignore app theme";
+
+	public Issue22058(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.TimePicker)]
+	public void VerifyTimePickerTheme()
+	{
+		App.WaitForElement("ThemeChangeButton");
+		App.Tap("ThemeChangeButton");
+		App.Tap("timePicker");
+
+		Thread.Sleep(500); // Slight delay to allow the TimePicker to open
+		VerifyScreenshot();
+		App.TapCoordinates(10, 10);
+		App.Tap("ThemeChangeButton");
+	}
+}
+#endif
