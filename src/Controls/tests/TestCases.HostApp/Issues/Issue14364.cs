79a103eab2145ea4889f75662d0adee7495db759
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-09-12 12:05:16 +0530

    Added testcase.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue14364.cs
@@ -0,0 +1,50 @@
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 14364, "Control size properties are not available during Loaded event", PlatformAffected.Android)]
+public class Issue14364 : Shell
+{
+    public Issue14364()
+    {
+        ShellContent shellContent = new ShellContent
+        {
+            ContentTemplate = new DataTemplate(typeof(Issue14364Page)),
+            Route = "Issue14364Page"
+        };
+
+        Items.Add(shellContent);
+    }
+}
+
+public class Issue14364Page : ContentPage
+{
+    public Issue14364Page()
+    {
+        var grid = new Grid
+        {
+            HeightRequest = 300,
+            WidthRequest = 200
+        };
+
+        var label = new Label
+        {
+            Text = "Size",
+            AutomationId = "labelSize"
+        };
+
+        label.Loaded += Label_Loaded;
+
+        grid.Children.Add(label);
+        Content = grid;
+    }
+
+    private void Label_Loaded(object sender, EventArgs e)
+    {
+        if (sender is Label label)
+        {
+            var h1 = label.Height;
+            var h2 = label.Width;
+
+            label.Text = $"Height: {h1}, Width: {h2}";
+        }
+    }
+}
