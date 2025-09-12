85860e0de447f13437f732dd8e7e8432a52d86a7
KarthikRajaKalaimani <92777139+KarthikRajaKalaimani@users.noreply.github.com>
2025-09-12 17:34:36 +0530

    Included automation Id for Tapping

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20062.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20062.cs
@@ -16,18 +16,8 @@ public class Issue20062 : _IssuesUITest
 	public void CollectionViewSelectionChangesVisualState()
 	{
 		App.WaitForElement("CollectionView");
-#if ANDROID
-		App.TapCoordinates(80, 180);
-		App.TapCoordinates(80, 450);
-#elif IOS
-App.TapCoordinates(50, 100);
-		App.TapCoordinates(50, 280);
-#elif MACCATALYST
-var firstItem = App.FindElement("a").GetRect();
-		var X = firstItem.X + 5;
-		App.TapCoordinates(X, 150);
-		App.TapCoordinates(X, 250);
-#endif
+		App.Tap("FirstItem");
+		App.Tap("ThirdItem");
 		VerifyScreenshot();
 	}
 }
