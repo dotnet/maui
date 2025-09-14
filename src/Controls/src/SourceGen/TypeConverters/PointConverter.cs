782f48f64603ba469b0cecff0389c2083a1e2bdd
Gerald Versluis <gerald.versluis@microsoft.com>
2025-09-14 18:31:09 +0200

    Refactor XAML SourceGen TypeConverters (#31467)
    * Refactor SG TypeConverters
    
    * Remove some dead code
    
    * Rename ITypeConverter to ISGTypeConverter
    
    Renamed the ITypeConverter interface and its usages to ISGTypeConverter for clarity and consistency in source generator type converters. Updated references in BaseTypeConverter and TypeConverterRegistry accordingly.
    
    * Remove BaseTypeConverter
    
    * Lazy loading
    
    * Build error
    
    * Fix test
    
    * Small optimization
    
    * Update ColorConverter.cs
    
    * Apply latest context.ProjectItem changes
    
    * Make KnownNamedColors case insensitive
    
    * Use GetTypeByMetadataName and ToFQDisplayString
    
    * Rgba Argb whatever

---
+++ src/Controls/src/SourceGen/TypeConverters/PointConverter.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Xml;
+using Microsoft.CodeAnalysis;
+using Microsoft.Maui.Controls.Xaml;
+using static Microsoft.Maui.Controls.SourceGen.GeneratorHelpers;
+
+namespace Microsoft.Maui.Controls.SourceGen.TypeConverters;
+
+internal class PointConverter : ISGTypeConverter
+{
+	public IEnumerable<string> SupportedTypes => new[] { "Point", "Microsoft.Maui.Graphics.Point" };
+
+	public string Convert(string value, BaseNode node, ITypeSymbol toType, SourceGenContext context, LocalVariable? parentVar = null)
+	{
+		var xmlLineInfo = (IXmlLineInfo)node;
+		// IMPORTANT! Update RectTypeDesignConverter.IsValid if making changes here
+		if (!string.IsNullOrEmpty(value))
+		{
+			string[] xy = value.Split([',']);
+			if (xy.Length == 2 && double.TryParse(xy[0], NumberStyles.Number, CultureInfo.InvariantCulture, out var x)
+				&& double.TryParse(xy[1], NumberStyles.Number, CultureInfo.InvariantCulture, out var y))
+			{
+				var pointType = context.Compilation.GetTypeByMetadataName("Microsoft.Maui.Graphics.Point")!;
+				return $"new {pointType.ToFQDisplayString()}({FormatInvariant(x)}, {FormatInvariant(y)})";
+			}
+		}
+
+		context.ReportConversionFailed( xmlLineInfo, value, Descriptors.PointConversionFailed);
+		return "default";
+	}
+}
