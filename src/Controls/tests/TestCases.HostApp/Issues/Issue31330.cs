0c1384e07d161435bbed2a5261b8e7b953946bea
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-09-18 18:12:41 +0530

    Updated test sample

--- src/Controls/tests/TestCases.HostApp/Issues/Issue31330.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue31330.cs
@@ -7,9 +7,18 @@ public class Issue31330 : ContentPage
 {
     public Issue31330()
     {
-        var grid = new Grid();
+        Grid grid = new Grid();
+        grid.RowDefinitions = new RowDefinitionCollection
+        {
+            new RowDefinition { Height = GridLength.Auto },
+            new RowDefinition { Height = GridLength.Auto },
+            new RowDefinition { Height = GridLength.Star },
+            new RowDefinition { Height = GridLength.Star }
+        };
 
-        var boxView = new BoxView
+        Label labelText = new Label { Text = "The test passes if the edges of the circle touch the BoxView.", FontAttributes = FontAttributes.Bold };
+
+        BoxView boxView = new BoxView
         {
             Color = Colors.Green,
             HeightRequest = 100,
@@ -17,16 +26,57 @@ public class Issue31330 : ContentPage
             AutomationId = "Issue31330BoxView"
         };
 
-        var ellipse = new Ellipse
+        Ellipse ellipse = new Ellipse
         {
-            Fill = new SolidColorBrush(Colors.Red),
-            StrokeThickness = 20,
+            Fill = Colors.Yellow,
+            StrokeThickness = 0,
             WidthRequest = 100,
             HeightRequest = 100
         };
 
+        Button button = new Button { Text = "Update StrokeThickness" };
+        button.Clicked += (s, e) =>
+        {
+            ellipse.StrokeThickness = 20;
+        };
+
+        Label label = new Label
+        {
+            Text = "Test passes if the Rectangle renders as filled shape for small height",
+            FontAttributes = FontAttributes.Bold
+        };
+
+        Rectangle rectangle = new Rectangle
+        {
+            WidthRequest = 50,
+            HeightRequest = 1.2,
+            Fill = Colors.Blue
+        };
+
+        Grid bottomGrid = new Grid { Background = Colors.AliceBlue };
+        bottomGrid.Children.Add(label);
+        bottomGrid.Children.Add(rectangle);
+
+        bottomGrid.RowDefinitions = new RowDefinitionCollection
+        {
+            new RowDefinition { Height = GridLength.Auto },
+            new RowDefinition { Height = GridLength.Star },
+        };
+
+        bottomGrid.SetRow(label, 0);
+        bottomGrid.SetRow(rectangle, 1);
+
+        grid.Children.Add(labelText);
+        grid.SetRow(labelText, 0);
+        grid.Children.Add(button);
+        grid.SetRow(button, 1);
         grid.Children.Add(boxView);
+        grid.SetRow(boxView, 2);
         grid.Children.Add(ellipse);
+        grid.SetRow(ellipse, 2);
+        grid.SetRow(bottomGrid, 3);
+        grid.Children.Add(bottomGrid);
+
         Content = grid;
     }
 }
