072a07cd97a03a862356c9a557debeedabf68936
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-09-18 15:26:45 +0530

    test case added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31534.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue31534 : _IssuesUITest
+{
+	public Issue31534(TestDevice testDevice) : base(testDevice) { }
+
+	public override string Issue => "ScrollView height was increased after the application closes";
+
+	[Test]
+	[Category(UITestCategories.ScrollView)]
+	public void VerifyScrollViewHeightWhenAppClose()
+	{
+		App.WaitForElement("MauiButton");
+		App.Tap("MauiButton");
+		App.Tap("MauiButton");
+		App.Tap("MauiButton");
+		App.WaitForElement("MauiButton");
+	}
+}
