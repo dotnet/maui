c135d3edeb64b03ef807b5d426a6070164284dd9
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-09-22 18:43:12 +0530

    Test added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31680.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue31680 : _IssuesUITest
+{
+	public Issue31680(TestDevice testDevice) : base(testDevice) { }
+
+	public override string Issue => "System.IndexOutOfRangeException when scrolling CollectionView with image CarouselView";
+
+    [Test]
+    [Category(UITestCategories.CarouselView)]
+    public void CollectionViewInsideCarouselViewShouldNotThrowIndexOutOfRangeException()
+    {
+        App.WaitForElement("TitleLabel");
+        
+        // Note: This issue occurs only during manual scrolling. The test may fail randomly without the fix, 
+        // but the issue can be consistently reproduced manually with each scroll.
+        App.ScrollDown("MainCollectionView", ScrollStrategy.Gesture, 0.99, swipeSpeed: 50, true);
+        App.ScrollDown("MainCollectionView", ScrollStrategy.Gesture, 0.99, swipeSpeed: 50, true);
+        App.ScrollTo("Footer");
+        App.WaitForElement("Footer");
+	}
+}
