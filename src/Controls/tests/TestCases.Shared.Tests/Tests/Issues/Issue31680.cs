455f7c7d3e611571b739832b69db5f1f00d5952c
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-09-23 11:44:01 +0530

    Update Issue31680.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31680.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31680.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_WINDOWS   // Scrolling the CollectionView using App.ScrollDown is not working on Windows platform.  
+                            // Also, the issue itself is not reproducible on Windows.
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -6,21 +8,22 @@ namespace Microsoft.Maui.TestCases.Tests.Issues;
 
 public class Issue31680 : _IssuesUITest
 {
-	public Issue31680(TestDevice testDevice) : base(testDevice) { }
+    public Issue31680(TestDevice testDevice) : base(testDevice) { }
 
-	public override string Issue => "System.IndexOutOfRangeException when scrolling CollectionView with image CarouselView";
+    public override string Issue => "System.IndexOutOfRangeException when scrolling CollectionView with image CarouselView";
 
     [Test]
     [Category(UITestCategories.CarouselView)]
     public void CollectionViewInsideCarouselViewShouldNotThrowIndexOutOfRangeException()
     {
         App.WaitForElement("TitleLabel");
-        
+
         // Note: This issue occurs only during manual scrolling. The test may fail randomly without the fix, 
         // but the issue can be consistently reproduced manually with each scroll.
         App.ScrollDown("MainCollectionView", ScrollStrategy.Gesture, 0.99, swipeSpeed: 50, true);
         App.ScrollDown("MainCollectionView", ScrollStrategy.Gesture, 0.99, swipeSpeed: 50, true);
         App.ScrollTo("Footer");
         App.WaitForElement("Footer");
-	}
+    }
 }
+#endif
