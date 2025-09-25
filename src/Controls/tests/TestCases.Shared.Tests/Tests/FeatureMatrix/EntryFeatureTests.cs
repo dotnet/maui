7d5c171915ffa4c52ab622322cff1e7e45ea7f3f
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-09-25 20:27:11 +0530

    Updated the images from CI

--- src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/EntryFeatureTests.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/EntryFeatureTests.cs
@@ -41,8 +41,6 @@ public class EntryFeatureTests : _GalleryUITest
 	{
 		App.WaitForElement("TestEntry");
 		App.Tap("TestEntry");
-		App.Tap(MainLabel);
-		App.Tap("TestEntry");
 		App.PressEnter();
 		App.DismissKeyboard();
 		Assert.That(App.WaitForElement("CompletedLabel").GetText(), Is.EqualTo("Completed: Event Triggered"));
@@ -53,8 +51,6 @@ public class EntryFeatureTests : _GalleryUITest
 	{
 
 		App.WaitForElement("TestEntry");
-		App.ClearText("TestEntry");
-		App.Tap(MainLabel);
 		App.ClearText("TestEntry");
 		App.EnterText("TestEntry", "New Text");
 
