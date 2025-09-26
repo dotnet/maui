4247fb5f9313932d00e7070451ea2fc3402558f3
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-09-26 11:40:48 +0530

    Revert "test case removed"
    This reverts commit 5c3d9c150f0b8a2c20efda615d3353a755c21d25.

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue31534.cs
@@ -0,0 +1,65 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 31534, "[WinUI] ScrollView height was increased after the application closes", PlatformAffected.UWP)]
+public class Issue31534 : ContentPage
+{
+	public Issue31534()
+	{
+		// Create ScrollView
+		var scrollView = new ScrollView
+		{
+			BackgroundColor = Colors.LightGray
+		};
+
+		// Create layout inside ScrollView
+		var stackLayout = new VerticalStackLayout
+		{
+			Padding = 20,
+			Spacing = 10
+		};
+
+		var HorizontalLayout = new HorizontalStackLayout
+		{
+			Spacing = 10
+		};
+
+		var heightLabel = new Label
+		{
+			Text = "ScrollView Height:",
+			FontSize = 16,
+			TextColor = Colors.Red
+		};
+
+		var valueLabel = new Label
+		{
+			Text = "0",
+			FontSize = 16,
+			TextColor = Colors.Red,
+			AutomationId = "HeightLabel"
+		};
+
+		HorizontalLayout.Add(heightLabel);
+		HorizontalLayout.Add(valueLabel);
+		stackLayout.Add(HorizontalLayout);
+
+		for (int i = 1; i <= 20; i++)
+		{
+			stackLayout.Add(new Label
+			{
+				Text = $"Item {i}"
+			});
+		}
+
+		// Put layout inside ScrollView
+		scrollView.Content = stackLayout;
+
+		// Set page content
+		Content = scrollView;
+
+		// Attach SizeChanged handler
+		scrollView.SizeChanged += (s, e) =>
+		{
+			valueLabel.Text = scrollView.Height.ToString();
+		};
+	}
+}
