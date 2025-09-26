b56da873067d76829a1925470c89227b4172b8c6
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-09-26 17:47:22 +0530

    Added test

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue24627.cs
@@ -0,0 +1,33 @@
+﻿#if WINDOWS || MACCATALYST // TitleBar is only supported on Windows and MacCatalyst
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue24627 : _IssuesUITest
+{
+	public override string Issue => "[Windows] TitleBar Title FontAttributes";
+
+	public Issue24627(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test, Order(1)]
+	[Category(UITestCategories.TitleView)]
+	public void VerifyTitleBarTitleFontAttributesBold()
+	{
+		App.WaitForElement("ChangeFAButton");
+		VerifyScreenshot(includeTitleBar: true);
+	}
+
+	[Test, Order(2)]
+	[Category(UITestCategories.TitleView)]
+	public void ChangeTitleBarTitleFontAttributesToNone()
+	{
+		App.WaitForElement("ChangeFAButton");
+		App.Tap("ChangeFAButton");
+		VerifyScreenshot(includeTitleBar: true);
+	}
+}
+#endif
