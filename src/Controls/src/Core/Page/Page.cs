6f8e3804d26d154bc6c88fd28724b48866e1f052
KarthikRajaKalaimani <92777139+KarthikRajaKalaimani@users.noreply.github.com>
2025-09-30 18:16:31 +0530

    Fixed NavigationPage and FlyoutPage both call OnNavigatedTo, so it is called twice

--- src/Controls/src/Core/Page/Page.cs
+++ src/Controls/src/Core/Page/Page.cs
@@ -824,6 +824,8 @@ namespace Microsoft.Maui.Controls
 
 		internal void SendNavigatedTo(NavigatedToEventArgs args)
 		{
+			if(HasNavigatedTo)
+				return;
 			HasNavigatedTo = true;
 			NavigatedTo?.Invoke(this, args);
 			OnNavigatedTo(args);
