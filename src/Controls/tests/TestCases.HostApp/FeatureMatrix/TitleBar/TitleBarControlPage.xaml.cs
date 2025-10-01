2cf89fe7043397ec626f37555c78f465c228241a
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-10-01 15:50:41 +0530

    test sample changes

--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/TitleBar/TitleBarControlPage.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/TitleBar/TitleBarControlPage.xaml.cs
@@ -37,6 +37,7 @@ public partial class TitleBarControlPage : ContentPage
 				ForegroundColor = _viewModel.ForegroundColor,
 				Icon = _viewModel.Icon,
 				IsVisible = _viewModel.IsVisible,
+				TitleFontAttributes = FontAttributes.None,
 				HeightRequest = 60
 			};
 
