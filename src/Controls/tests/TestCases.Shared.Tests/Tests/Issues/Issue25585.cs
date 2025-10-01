110a7034b5d0f9946d1c56f5a2a699d1582dc5fc
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-10-01 10:42:49 +0530

    Update Issue25585.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25585.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25585.cs
@@ -2,25 +2,24 @@ using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
-{
-	public class Issue25585 : _IssuesUITest
-	{
-		public Issue25585(TestDevice device) : base(device) { }
+namespace Microsoft.Maui.TestCases.Tests.Issues;
 
-		public override string Issue => "App Unresponsive when prompting the user from a new page";
-		[Test]
-		[Category(UITestCategories.DisplayAlert)]
-		public void VerifyDisplayAlertIsShown()
-		{
-			App.WaitForElement("GoToSecondPage");
-			App.TapShellFlyoutIcon();
-			App.Tap("Second Page");
+public class Issue25585 : _IssuesUITest
+{
+	public Issue25585(TestDevice device) : base(device) { }
+
+	public override string Issue => "App Unresponsive when prompting the user from a new page";
+	[Test]
+	[Category(UITestCategories.DisplayAlert)]
+	public void VerifyDisplayAlertIsShown()
+	{
+		App.WaitForElement("GoToSecondPage");
+		App.TapShellFlyoutIcon();
+		App.Tap("Second Page");
 #if MACCATALYST
 			App.WaitForElement(AppiumQuery.ById($"action-button--{999 - 0}"));
 #else
-			App.WaitForElement("OK");
+		App.WaitForElement("OK");
 #endif
-		}
 	}
 }
