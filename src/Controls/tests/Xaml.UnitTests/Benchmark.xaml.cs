cab5948a85028b1bba3560d5c998618e625637d3
Simon Rozsival <simon@rozsival.com>
2025-10-02 12:10:19 +0200

    wip

--- src/Controls/tests/Xaml.UnitTests/Benchmark.xaml.cs
+++ src/Controls/tests/Xaml.UnitTests/Benchmark.xaml.cs
@@ -1,9 +1,23 @@
+using System;
 namespace Microsoft.Maui.Controls.Xaml.UnitTests;
 
 public partial class Benchmark : ContentPage
 {
-	public Benchmark()
+	public Benchmark(string inflator)
 	{
-		InitializeComponent();
+		switch (inflator)
+		{
+			case "Runtime":
+				InitializeComponentRuntime();
+				break;
+			case "XamlC":
+				InitializeComponentXamlC();
+				break;
+			case "SourceGen":
+				InitializeComponentSourceGen();
+				break;
+			default:
+				throw new NotSupportedException($"no code for {inflator} generated. check the [XamlProcessing] attribute.");
+		}
 	}
 }
