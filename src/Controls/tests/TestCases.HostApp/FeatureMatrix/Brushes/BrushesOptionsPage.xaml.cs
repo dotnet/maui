14ec8e67b01a0d4371cc95db7951ecbec79631cc
LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
2025-10-03 18:21:52 +0530

    update the dynamically add and remove the gradient stops

--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/Brushes/BrushesOptionsPage.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/Brushes/BrushesOptionsPage.xaml.cs
@@ -1,130 +1,129 @@
 using Microsoft.Maui.Controls;
 
-namespace Maui.Controls.Sample
+namespace Maui.Controls.Sample;
+
+public partial class BrushesOptionsPage : ContentPage
 {
-	public partial class BrushesOptionsPage : ContentPage
+	public BrushesOptionsPage(BrushesViewModel vm)
 	{
-		public BrushesOptionsPage(BrushesViewModel vm)
-		{
-			InitializeComponent();
-			BindingContext = vm;
-		}
+		InitializeComponent();
+		BindingContext = vm;
+	}
 
-		async void OnMainClicked(object sender, EventArgs e)
-		{
-			await Navigation.PopAsync();
-		}
+	async void OnMainClicked(object sender, EventArgs e)
+	{
+		await Navigation.PopAsync();
+	}
 
-		void OnBrushTypeChanged(object sender, CheckedChangedEventArgs e)
+	void OnBrushTypeChanged(object sender, CheckedChangedEventArgs e)
+	{
+		if (!e.Value)
+			return;
+		if (BindingContext is not BrushesViewModel vm)
+			return;
+		if (sender == SolidRadio)
 		{
-			if (!e.Value)
-				return;
-			if (BindingContext is not BrushesViewModel vm)
-				return;
-			if (sender == SolidRadio)
-			{
-				vm.ApplySolidBrushCommand.Execute(null);
-				return;
-			}
-			if (sender == LinearRadio)
-			{
-				vm.ApplyLinearGradientCommand.Execute(null);
-				return;
-			}
-			if (sender == RadialRadio)
-			{
-				vm.ApplyRadialGradientCommand.Execute(null);
-				return;
-			}
-			if (sender == NoneRadio)
-			{
-				vm.ApplyNullBrushCommand.Execute(null);
-				return;
-			}
+			vm.ApplySolidBrushCommand.Execute(null);
+			return;
 		}
-
-		public void OnColor1Changed(object sender, CheckedChangedEventArgs e)
+		if (sender == LinearRadio)
 		{
-			if (!e.Value)
-				return;
-			if (BindingContext is not BrushesViewModel vm)
-				return;
-			if (sender is RadioButton rb && rb.Content is string color)
-			{
-				vm.SelectedColorName1 = color;
-			}
+			vm.ApplyLinearGradientCommand.Execute(null);
+			return;
 		}
-
-		public void OnColor2Changed(object sender, CheckedChangedEventArgs e)
+		if (sender == RadialRadio)
 		{
-			if (!e.Value)
-				return;
-			if (BindingContext is not BrushesViewModel vm)
-				return;
-			if (sender is RadioButton rb && rb.Content is string color)
-			{
-				vm.SelectedColorName2 = color;
-			}
+			vm.ApplyRadialGradientCommand.Execute(null);
+			return;
 		}
-
-		void OnShadowTypeChanged(object sender, CheckedChangedEventArgs e)
+		if (sender == NoneRadio)
 		{
-			if (!e.Value)
-				return;
-			if (BindingContext is not BrushesViewModel vm)
-				return;
-			if (sender == ShadowSolidRadio)
-			{
-				vm.ApplySolidShadowCommand?.Execute(null);
-				return;
-			}
-			if (sender == ShadowLinearRadio)
-			{
-				vm.ApplyLinearShadowCommand?.Execute(null);
-				return;
-			}
-			if (sender == ShadowRadialRadio)
-			{
-				vm.ApplyRadialShadowCommand?.Execute(null);
-				return;
-			}
+			vm.ApplyNullBrushCommand.Execute(null);
+			return;
 		}
+	}
 
-		void OnStrokeTypeChanged(object sender, CheckedChangedEventArgs e)
+	public void OnColor1Changed(object sender, CheckedChangedEventArgs e)
+	{
+		if (!e.Value)
+			return;
+		if (BindingContext is not BrushesViewModel vm)
+			return;
+		if (sender is RadioButton rb && rb.Content is string color)
 		{
-			if (!e.Value)
-				return;
-			if (BindingContext is not BrushesViewModel vm)
-				return;
-			SelectAndExecute(sender, vm,
-				Solid: () => vm.ApplySolidStrokeCommand?.Execute(null),
-				Linear: () => vm.ApplyLinearStrokeCommand?.Execute(null),
-				Radial: () => vm.ApplyRadialStrokeCommand?.Execute(null),
-				None: () => vm.ApplyNullStrokeCommand?.Execute(null));
+			vm.SelectedColorName1 = color;
 		}
+	}
 
-		void SelectAndExecute(object sender, BrushesViewModel vm, Action Solid, Action Linear, Action Radial, Action None)
+	public void OnColor2Changed(object sender, CheckedChangedEventArgs e)
+	{
+		if (!e.Value)
+			return;
+		if (BindingContext is not BrushesViewModel vm)
+			return;
+		if (sender is RadioButton rb && rb.Content is string color)
 		{
-			if (sender == SolidRadio || sender == ShadowSolidRadio || sender == StrokeSolidRadio)
-			{
-				Solid?.Invoke();
-				return;
-			}
-			if (sender == LinearRadio || sender == ShadowLinearRadio || sender == StrokeLinearRadio)
-			{
-				Linear?.Invoke();
-				return;
-			}
-			if (sender == RadialRadio || sender == ShadowRadialRadio || sender == StrokeRadialRadio)
-			{
-				Radial?.Invoke();
-				return;
-			}
-			if (sender == NoneRadio || sender == StrokeNoneRadio)
-			{
-				None?.Invoke();
-				return;
-			}
+			vm.SelectedColorName2 = color;
+		}
+	}
+
+	void OnShadowTypeChanged(object sender, CheckedChangedEventArgs e)
+	{
+		if (!e.Value)
+			return;
+		if (BindingContext is not BrushesViewModel vm)
+			return;
+		if (sender == ShadowSolidRadio)
+		{
+			vm.ApplySolidShadowCommand?.Execute(null);
+			return;
+		}
+		if (sender == ShadowLinearRadio)
+		{
+			vm.ApplyLinearShadowCommand?.Execute(null);
+			return;
+		}
+		if (sender == ShadowRadialRadio)
+		{
+			vm.ApplyRadialShadowCommand?.Execute(null);
+			return;
+		}
+	}
+
+	void OnStrokeTypeChanged(object sender, CheckedChangedEventArgs e)
+	{
+		if (!e.Value)
+			return;
+		if (BindingContext is not BrushesViewModel vm)
+			return;
+		SelectAndExecute(sender, vm,
+			Solid: () => vm.ApplySolidStrokeCommand?.Execute(null),
+			Linear: () => vm.ApplyLinearStrokeCommand?.Execute(null),
+			Radial: () => vm.ApplyRadialStrokeCommand?.Execute(null),
+			None: () => vm.ApplyNullStrokeCommand?.Execute(null));
+	}
+
+	void SelectAndExecute(object sender, BrushesViewModel vm, Action Solid, Action Linear, Action Radial, Action None)
+	{
+		if (sender == SolidRadio || sender == ShadowSolidRadio || sender == StrokeSolidRadio)
+		{
+			Solid?.Invoke();
+			return;
+		}
+		if (sender == LinearRadio || sender == ShadowLinearRadio || sender == StrokeLinearRadio)
+		{
+			Linear?.Invoke();
+			return;
+		}
+		if (sender == RadialRadio || sender == ShadowRadialRadio || sender == StrokeRadialRadio)
+		{
+			Radial?.Invoke();
+			return;
+		}
+		if (sender == NoneRadio || sender == StrokeNoneRadio)
+		{
+			None?.Invoke();
+			return;
 		}
 	}
 }
