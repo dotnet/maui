e2d85b79850e9e583974835cc39971267573796b
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2025-10-03 15:05:11 +0530

    Set a RootSplitView  corner radius to Zero.

--- src/Core/src/Platform/Windows/MauiNavigationView.cs
+++ src/Core/src/Platform/Windows/MauiNavigationView.cs
@@ -134,6 +134,7 @@ namespace Microsoft.Maui.Platform
 			// It causes the content to not be flush up against the title bar
 			PaneContentGrid.Margin = new WThickness(0, 0, 0, 0);
 			UpdateMenuItemsContainerHeight();
+			RootSplitView.CornerRadius = new UI.Xaml.CornerRadius(0);
 
 			// On WinAppSDK 1.7, it seems that if the PaneContentGrid width is not explicitly set,
 			// it takes on the desired width of its child.
