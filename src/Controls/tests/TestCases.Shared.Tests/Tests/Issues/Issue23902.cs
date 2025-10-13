ad30a55c357f40ba898d70c1024ed2d215363b75
KarthikRajaKalaimani <92777139+KarthikRajaKalaimani@users.noreply.github.com>
2025-10-13 12:21:53 +0530

    Added additional test case

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue23902.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue23902.cs
@@ -19,7 +19,36 @@ public class Issue23902 : _IssuesUITest
 	{
 		App.WaitForElement("Go to Third Page");
 		App.Tap("ThirdPageButton");
-		await Task.Delay(300);
+		await Task.Delay(300); // Wait for navigation animations to complete.
 		Assert.That(App.WaitForElement("HeaderLabel").GetText(), Is.EqualTo("NavigatedTo called 1 times"));
 	}
+
+	[Test]
+	[Category(UITestCategories.Navigation)]
+	public async Task Issue23902NavigationTest2()
+	{
+		App.WaitForElement("Go to Third Page");
+		App.Tap("ThirdPageButton");
+		await Task.Delay(300); // Wait for navigation animations to complete.
+		App.Tap("SecondPageButton");
+		App.WaitForElement("Go to Third Page");
+		App.Tap("ThirdPageButton");
+		await Task.Delay(300); // Wait for navigation animations to complete.
+		Assert.That(App.WaitForElement("HeaderLabel").GetText(), Is.EqualTo("NavigatedTo called 2 times"));
+	}
+
+	[Test]
+	[Category(UITestCategories.Navigation)]
+	public async Task Issue23902NavigationTest3()
+    {
+        App.WaitForElement("Go to Third Page");
+		App.Tap("ThirdPageButton");
+		await Task.Delay(300); // Wait for navigation animations to complete.
+		App.WaitForElement("Go to 4th Page");
+		App.Tap("FourthPageButton");
+		await Task.Delay(300); // Wait for navigation animations to complete. 
+		App.WaitForElement("Go Back to 3rd Page");
+		App.Tap("ThirdPageButton");
+		Assert.That(App.WaitForElement("HeaderLabel").GetText(), Is.EqualTo("NavigatedTo called 2 times"));
+    }
 }
