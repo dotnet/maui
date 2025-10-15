5a7d26e512078e8cf3e261b50c017054b4cc5682
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-10-15 15:47:00 +0200

    Updated test

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31917.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31917.cs
@@ -14,24 +14,28 @@ public class Issue31917 : _IssuesUITest
 	[Category(UITestCategories.SwipeView)]
 	public void SwipeItemBackgroundShouldUpdateOnAppThemeChange()
 	{
+		Exception? exception = null;
+		
 		App.WaitForElement("changeThemeButton");
 
 		// Reveal the swipe items by swiping left on the SwipeView
 		App.SwipeLeftToRight("TestSwipeView");
-		
-		App.WaitForElement("TestSwipeItem");
-		App.WaitForElement("TestSwipeItemView");
 
 		// Take screenshot before theme change
-		VerifyScreenshot("BeforeThemeChange");
+		VerifyScreenshotOrSetException(ref exception, "Issue31917BeforeThemeChange");
 
 		// Change theme
 		App.Tap("changeThemeButton");
 
 		// Wait a moment for theme change to apply
-		System.Threading.Thread.Sleep(500);
+		Thread.Sleep(500);
 
 		// Take screenshot after theme change to verify background colors updated
-		VerifyScreenshot("AfterThemeChange");
+		VerifyScreenshotOrSetException(ref exception, "Issue31917AfterThemeChange");
+		
+		if (exception != null)
+		{
+			throw exception;
+		}
 	}
 }
