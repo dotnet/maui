d61c0e9c291ff310bd0a4a2d8e8ff4d416763a87
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-10-22 13:45:18 +0530

    Border AutomationPeer changes

--- src/Core/src/Platform/Windows/MauiBorderAutomationPeer.cs
+++ src/Core/src/Platform/Windows/MauiBorderAutomationPeer.cs
@@ -3,8 +3,8 @@ using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.Maui.Platform
 {
-	// TODO: Make this class public in .NET 10.0. Issue Link: https://github.com/dotnet/maui/issues/30205
-	internal class MauiBorderAutomationPeer : FrameworkElementAutomationPeer
+	// TODO: Make this class public in .NET11.0. Issue Link: https://github.com/dotnet/maui/issues/30205
+	internal partial class MauiBorderAutomationPeer : FrameworkElementAutomationPeer
 	{
 		internal MauiBorderAutomationPeer(Panel owner) : base(owner) { }
 
@@ -17,6 +17,8 @@ namespace Microsoft.Maui.Platform
 		{
 			return nameof(Panel);
 		}
+
+		protected override bool IsKeyboardFocusableCore() => true;
 
 		protected override bool IsControlElementCore() => true;
 
