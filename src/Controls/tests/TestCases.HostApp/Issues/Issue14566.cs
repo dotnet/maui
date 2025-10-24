9536e0df780c64fbcca6b77762d3d371562aaf44
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-10-24 11:09:09 +0530

    Revert "test case modified"
    This reverts commit 49f0d6f9d21458eb6a1f046cc5e955135ede1434.

--- src/Controls/tests/TestCases.HostApp/Issues/Issue14566.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue14566.cs
@@ -4,6 +4,10 @@
 	public class Issue14566 : TestContentPage
 	{
 		const string SearchBar = "SearchBar";
+		const string ResultText = "ResultText";
+		const string CheckResultButton = "CheckResultButton";
+		const string Success = "Success";
+		const string Failure = "Failure";
 
 		protected override void Init()
 		{
@@ -20,8 +24,24 @@
 				Placeholder = "Search Placeholder",
 				IsEnabled = false
 			};
+			var searchText = new Label();
+
+			searchBar.TextChanged += (sender, e) =>
+			{
+				// Handle text changed event
+				searchText.Text = $"{e.NewTextValue}";
+			};
+
+			var result = new Label() { AutomationId = ResultText };
+			var checkResultButton = new Button() { AutomationId = CheckResultButton, Text = "Check Result" };
+			checkResultButton.Clicked += (sender, e) =>
+			{
+				result.Text = string.IsNullOrWhiteSpace(searchBar.Text) ? Success : Failure;
+			};
 
 			layout.Children.Add(searchBar);
+			layout.Children.Add(checkResultButton);
+			layout.Children.Add(result);
 
 			Content = layout;
 		}
