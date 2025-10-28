72b3d1a92477121e5a11f8e5a877d0fe453ef3fa
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-10-28 13:55:02 +0530

    Added the comment

--- src/Controls/src/Core/Handlers/Items2/ItemsViewHandler2.iOS.cs
+++ src/Controls/src/Core/Handlers/Items2/ItemsViewHandler2.iOS.cs
@@ -128,6 +128,7 @@ namespace Microsoft.Maui.Controls.Handlers.Items2
 				itemsLayout.ItemsUpdatingScrollMode = itemsView.ItemsUpdatingScrollMode;
 				if (handler.Controller?.CollectionView?.CollectionViewLayout is LayoutFactory2.CustomUICollectionViewCompositionalLayout customLayout)
 				{
+					// When the CollectionView’s ItemsUpdatingScrollMode value changes dynamically, update the items layout’s ItemsUpdatingScrollMode property accordingly.
 					customLayout.UpdateItemsUpdatingScrollMode(itemsView.ItemsUpdatingScrollMode);
 				}
 			}
