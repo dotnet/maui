7c1a01955cdbc6b1f2466c1f0f0266f529e5ceea
Jakub Florkowski <kubaflo123@gmail.com>
2025-10-31 00:56:19 +0100

    Added a UITest

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32200.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue32200 : _IssuesUITest
+{
+	public override string Issue => "NavigationPage TitleView iOS 26";
+	public Issue32200(TestDevice device) : base(device) { }
+
+	[Test]
+	[Category(UITestCategories.Navigation)]
+	public void NavagionPageTitleViewShouldRespectMargins()
+	{
+		App.WaitForElement("Label");
+		VerifyScreenshot();
+	}
+}
