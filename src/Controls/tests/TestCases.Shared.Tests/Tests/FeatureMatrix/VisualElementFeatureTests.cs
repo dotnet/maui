698ceb015b646a0f3aa81fc81ee145b5ce998ec4
HarishKumarSF4517 <harish.kumar@syncfusion.com>
2025-10-31 18:26:52 +0530

    Update VisualElementFeatureTests.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/VisualElementFeatureTests.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/VisualElementFeatureTests.cs
@@ -19,4 +19,112 @@ public class VisualElementFeatureTests : UITest
 		base.FixtureSetup();
 		App.NavigateToGallery(VisualElementFeatureMatrix);
 	}
+
+	[Test]
+	public void VisualElement_IsVisible()
+	{
+		App.WaitForElement("Reset");
+		App.Tap("Reset");
+
+		App.WaitForElement("Options");
+		App.Tap("Options");
+
+		App.WaitForElement("VisibilityCheckBox");
+		App.Tap("VisibilityCheckBox");
+
+		App.WaitForElement("Apply");
+		App.Tap("Apply");
+
+		VerifyScreenshot();
+	}
+
+	[Test]
+	public void VisualElement_IsShadow()
+	{
+		App.WaitForElement("Reset");
+		App.Tap("Reset");
+
+		App.WaitForElement("Options");
+		App.Tap("Options");
+
+		App.WaitForElement("ShadowCheckBox");
+		App.Tap("ShadowCheckBox");
+
+		App.WaitForElement("Apply");
+		App.Tap("Apply");
+
+		VerifyScreenshot();
+	}
+	[Test]
+	public void VisualElement_Rotation()
+	{
+		App.WaitForElement("Reset");
+		App.Tap("Reset");
+
+		App.WaitForElement("Options");
+		App.Tap("Options");
+
+		App.WaitForElement("Preset45RotateButton");
+		App.Tap("Preset45RotateButton");
+
+		App.WaitForElement("Apply");
+		App.Tap("Apply");
+
+		VerifyScreenshot();
+	}
+	[Test]
+	public void VisualElement_Scale()
+	{
+		App.WaitForElement("Reset");
+		App.Tap("Reset");
+
+		App.WaitForElement("Options");
+		App.Tap("Options");
+
+		App.WaitForElement("Preset2xScaleButton");
+		App.Tap("Preset2xScaleButton");
+
+		App.WaitForElement("Apply");
+		App.Tap("Apply");
+
+		VerifyScreenshot();
+	}
+
+	[Test]
+	public void VisualElement_ScaleX()
+	{
+		App.WaitForElement("Reset");
+		App.Tap("Reset");
+
+		App.WaitForElement("Options");
+		App.Tap("Options");
+
+		App.WaitForElement("ScaleXEntry");
+		App.ClearText("ScaleXEntry");
+		App.EnterText("ScaleXEntry", "2");
+
+		App.WaitForElement("Apply");
+		App.Tap("Apply");
+
+		VerifyScreenshot();
+	}
+
+	[Test]
+	public void VisualElement_Scaley()
+	{
+		App.WaitForElement("Reset");
+		App.Tap("Reset");
+
+		App.WaitForElement("Options");
+		App.Tap("Options");
+
+		App.WaitForElement("ScaleYEntry");
+		App.ClearText("ScaleYEntry");
+		App.EnterText("ScaleYEntry", "2");
+
+		App.WaitForElement("Apply");
+		App.Tap("Apply");
+
+		VerifyScreenshot();
+	}
 }
