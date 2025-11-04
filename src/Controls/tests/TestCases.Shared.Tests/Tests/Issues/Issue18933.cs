d1cc47f2b6e28d5b8979ba1cb75cfb550bef84cb
SyedAbdulAzeem <syedabdulazeem.a@syncfusion.com>
2025-11-04 17:19:02 +0530

    Added a test to verify background reset after clearing, and included snapshots for Android and iOS platforms

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18933.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18933.cs
@@ -12,12 +12,21 @@ public class Issue18933 : _IssuesUITest
 
 	public override string Issue => "ContentView Background Color Not Cleared When Set to Null";
 
-	[Test]
+	[Test, Order(1)]
 	[Category(UITestCategories.ViewBaseTests)]
 	public void VerifyBackgroundColorCleared()
 	{
 		App.WaitForElement("clearBgBtn");
 		App.Tap("clearBgBtn");
+		VerifyScreenshot();
+	}
+
+	[Test, Order(2)]
+	[Category(UITestCategories.ViewBaseTests)]
+	public void VerifyBackgroundColorSet()
+	{
+		App.WaitForElement("setBgBtn");
+		App.Tap("setBgBtn");
 		VerifyScreenshot();
 	}
 }
