d1286cc873ca5045f3f82e02d8535a92125add9b
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-11-04 10:12:40 +0530

    Addressed review comment

--- src/Controls/src/Core/Handlers/Items/Android/MauiCarouselRecyclerView.cs
+++ src/Controls/src/Core/Handlers/Items/Android/MauiCarouselRecyclerView.cs
@@ -18,12 +18,11 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 		bool _initialized;
 		bool _isVisible;
 		bool _disposed;
-
+		readonly float _touchSlop;
+		volatile float _initialTouchX;
+		volatile float _initialTouchY;
 		List<View> _oldViews;
 		CarouselViewOnGlobalLayoutListener _carouselViewLayoutListener;
-		float _touchSlop;
-		float _initialTouchX;
-		float _initialTouchY;
 
 		protected CarouselView Carousel => ItemsView as CarouselView;
 
@@ -34,6 +33,7 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			_touchSlop = ViewConfiguration.Get(context).ScaledTouchSlop;
 		}
 
+		// Gets or sets a value indicating whether swipe gestures are enabled for the carousel.
 		public bool IsSwipeEnabled { get; set; }
 
 		public override bool OnInterceptTouchEvent(MotionEvent ev)
