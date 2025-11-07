90f1b698c75fffd909899393a7b7be89a2221776
Rui Marinho <me@ruimarinho.net>
2025-11-07 19:19:37 +0000

    Fix build

--- src/Controls/tests/Xaml.UnitTests/BindingDiagnosticsTests.rt.xaml.cs
+++ src/Controls/tests/Xaml.UnitTests/BindingDiagnosticsTests.rt.xaml.cs
@@ -14,7 +14,7 @@ public partial class BindingDiagnosticsTests : ContentPage
 
 #if !DEBUG
 	[Fact(Skip = "This test runs only in debug")]
-#else
+#endif
 	public class Tests : IDisposable
 	{
 		bool enableDiagnosticsInitialState;
