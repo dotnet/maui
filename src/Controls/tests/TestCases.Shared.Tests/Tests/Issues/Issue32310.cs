db4261eb7091cb6c52a1840402e1593e411e3647
BagavathiPerumal <bagavathiperumal.a@syncfusion.com>
2025-11-10 17:25:37 +0530

    fix-32310-Made changes to fix Android modal navigation race condition by adding PresentationCompleted event and lifecycle-aware completion tracking for non-animated modals.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32310.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue32310 : _IssuesUITest
+{
+	public Issue32310(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "App hangs if PopModalAsync is called after PushModalAsync with single await Task.Yield()";
+
+	[Test]
+	[Category(UITestCategories.Navigation)]
+	public void ModalNavigationShouldNotHang()
+	{
+		App.WaitForElement("NavigateButton");
+		App.Tap("NavigateButton");
+		App.WaitForElement("NavigateButton");
+		VerifyScreenshot();
+	}
+}
