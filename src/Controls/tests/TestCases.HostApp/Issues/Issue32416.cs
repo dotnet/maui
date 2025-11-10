dff321aa7f9264591c7d263c6ff908964f035840
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-11-10 16:50:33 +0530

    Fix added for windows and added test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue32416.cs
@@ -0,0 +1,51 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 32416, "Shell.FlyoutVerticalScrollMode Disabled does not disable scrolling", PlatformAffected.UWP)]
+public class Issue32416 : TestShell
+{
+	protected override void Init()
+	{
+		FlyoutVerticalScrollMode = ScrollMode.Disabled;
+		var flyoutItem = new FlyoutItem
+		{
+			Title = "Menu"
+		};
+
+		// Add a ShellContent
+		flyoutItem.Items.Add(new ShellContent
+		{
+			Title = "Home",
+			ContentTemplate = new DataTemplate(typeof(Issue32416_ContentPage))
+		});
+
+		// Add FlyoutItem to the Shell
+		Items.Add(flyoutItem);
+
+		// Add MenuItems (static links in flyout)
+		for (int i = 1; i <= 20; i++)
+		{
+			Items.Add(new MenuItem
+			{
+				Text = $"Item {i}"
+			});
+		}
+	}
+}
+
+public class Issue32416_ContentPage : ContentPage
+{
+	public Issue32416_ContentPage()
+	{
+		Content = new StackLayout
+		{
+			Children =
+			{
+				new Label
+				{
+					AutomationId = "Issue32416_Label",
+					Text = "This is the Sample page."
+				}
+			}
+		};
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32416.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue32416 : _IssuesUITest
+{
+	public Issue32416(TestDevice testDevice) : base(testDevice)
+	{
+	}
+	public override string Issue => "Shell.FlyoutVerticalScrollMode Disabled does not disable scrolling";
+
+	[Test]
+	[Category(UITestCategories.FlyoutPage)]
+	public void VerifyFlyoutVerticalScrollModeDisabled()
+	{
+		App.WaitForElement("Issue32416_Label");
+		App.ShowFlyout();
+		App.WaitForElement("Item 1");
+		App.ScrollDown("Item 1", ScrollStrategy.Gesture, swipePercentage: 0.95, swipeSpeed: 100);
+		VerifyScreenshot();
+	}
+}
