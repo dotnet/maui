57f44ea0cbe3377b8541be0a090fc0caceab2c34
HarishKumarSF4517 <harish.kumar@syncfusion.com>
2025-11-10 10:30:03 +0530

    Added snapshots for ios and Android

--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/VisualTransform/VisualTransformOptionsPage.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/VisualTransform/VisualTransformOptionsPage.xaml.cs
@@ -34,36 +34,6 @@ namespace Maui.Controls.Sample
 			Navigation.PopAsync();
 		}
 
-		// Preset button handlers
-		private void Preset45Rotate_Clicked(object sender, EventArgs e)
-		{
-			_viewModel.Rotation = 45.0;
-		}
 
-		private void Preset2xScale_Clicked(object sender, EventArgs e)
-		{
-			_viewModel.Scale = 2.0;
-		}
-
-		private void PresetFlipX_Clicked(object sender, EventArgs e)
-		{
-			_viewModel.ScaleX = -1.0;
-			_viewModel.ScaleY = 1.0;
-		}
-
-		private void PresetSkew_Clicked(object sender, EventArgs e)
-		{
-			_viewModel.ScaleX = 1.2;
-			_viewModel.ScaleY = 0.8;
-			_viewModel.Rotation = 15.0;
-		}
-
-		private void Preset3DCube_Clicked(object sender, EventArgs e)
-		{
-			_viewModel.RotationX = 25.0;
-			_viewModel.RotationY = 35.0;
-			_viewModel.Rotation = 10.0;
-			_viewModel.Scale = 1.2;
-		}
 	}
 }
