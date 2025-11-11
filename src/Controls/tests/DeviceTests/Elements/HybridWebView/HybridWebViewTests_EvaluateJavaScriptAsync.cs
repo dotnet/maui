ed4c62637795bd6461dfbf56d54f3f242c2d3968
Matthew Leibowitz <mattleibow@live.com>
2025-11-11 03:56:42 +0200

    Add some more tests

--- src/Controls/tests/DeviceTests/Elements/HybridWebView/HybridWebViewTests_EvaluateJavaScriptAsync.cs
+++ src/Controls/tests/DeviceTests/Elements/HybridWebView/HybridWebViewTests_EvaluateJavaScriptAsync.cs
@@ -11,15 +11,29 @@ namespace Microsoft.Maui.DeviceTests;
 public partial class HybridWebViewTests_EvaluateJavaScriptAsync : HybridWebViewTestsBase
 {
 	[Fact]
-	public Task EvaluateJavaScriptAndGetResult() =>
+	public Task EvaluateJavaScriptAndGetResultFromFunction() =>
 		RunTest(async (hybridWebView) =>
 		{
 			// Run some JavaScript to call a method and get result
 			var result1 = await hybridWebView.EvaluateJavaScriptAsync("EvaluateMeWithParamsAndReturn('abc', 'def')");
 			Assert.Equal("abcdef", result1);
+		});
 
+	[Fact]
+	public Task EvaluateJavaScriptAndGetResultFromProperty() =>
+		RunTest(async (hybridWebView) =>
+		{
 			// Run some JavaScript to get an arbitrary result by running JavaScript
 			var result2 = await hybridWebView.EvaluateJavaScriptAsync("window.TestKey");
 			Assert.Equal("test_value", result2);
+		});
+		
+	[Fact]
+	public Task EvaluateJavaScriptAndPassStrings() =>
+		RunTest(async (hybridWebView) =>
+		{
+			// Run some JavaScript to call a method and get result
+			var result1 = await hybridWebView.EvaluateJavaScriptAsync("EvaluateMeWithParamsAndReturn('\"Hel', 'lo!\"')");
+			Assert.Equal("\"Hello!\"", result1);
 		});
 }
