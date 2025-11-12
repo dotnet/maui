ba53597907dc0a1990936b6bb4db1330d6ba3bf8
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-11-12 13:16:31 +0530

    Update Issue14572.cs

--- src/Controls/tests/TestCases.HostApp/Issues/Issue14572.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue14572.cs
@@ -20,7 +20,7 @@ public class Issue14572 : TabbedPage
 			if (item is Vm1)
 				return new DataTemplate(() => new ContentPage() { Title = "Type 1" });
 			if (item is Vm2)
-				return new DataTemplate(() => new ContentPage() { Title = "Type 2", Content = new Label { Text = "Type 2 Content" }, AutomationId="Type2Content" });
+				return new DataTemplate(() => new ContentPage() { Title = "Type 2", Content = new Label { Text = "Type 2 Content", AutomationId = "Type2Content" } });
 			if (item is Vm3)
 				return new DataTemplate(() => new ContentPage() { Title = "Type 3" });
 
