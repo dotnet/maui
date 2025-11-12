6b9a66be4daad32a8c1badc6c7c8a37a3d349936
Matthew Leibowitz <mattleibow@live.com>
2025-11-12 05:18:31 +0200

    Add some code

---
+++ src/AI/src/Essentials.AI/Platform/Android/MLKitGenAIChatClient.cs
@@ -0,0 +1,75 @@
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Maui.Essentials.AI;
+
+/// <summary>
+/// Provides an <see cref="IChatClient"/> implementation based on ML Kit GenAI APIs
+/// </summary>
+public sealed class MLKitGenAIChatClient : IChatClient
+{
+	/// <summary>
+	/// Lazily-initialized metadata describing the implementation.
+	/// </summary>
+	private ChatClientMetadata? _metadata;
+
+	/// <summary>
+	/// Initializes a new <see cref="MLKitGenAIChatClient"/> instance.
+	/// </summary>
+	public MLKitGenAIChatClient()
+	{
+	}
+
+	/// <inheritdoc />
+	public async Task<ChatResponse> GetResponseAsync(
+		IEnumerable<ChatMessage> chatMessages,
+		ChatOptions? options = null,
+		CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <inheritdoc />
+	public async IAsyncEnumerable<ChatResponseUpdate> GetStreamingResponseAsync(
+		IEnumerable<ChatMessage> chatMessages,
+		ChatOptions? options = null,
+		[EnumeratorCancellation] CancellationToken cancellationToken = default)
+	{
+		yield break;
+		throw new NotImplementedException();
+	}
+
+	/// <inheritdoc />
+	object? IChatClient.GetService(Type serviceType, object? serviceKey)
+	{
+		ArgumentNullException.ThrowIfNull(serviceType);
+
+		// If there's a service key, we don't support keyed services.
+		if (serviceKey is not null)
+		{
+			return null;
+		}
+
+		// If there's a request for metadata, lazily-initialize it and return it. We don't need to worry about race conditions,
+		// as there's no requirement that the same instance be returned each time, and creation is idempotent.
+		if (serviceType == typeof(ChatClientMetadata))
+		{
+			return _metadata ??= new ChatClientMetadata(
+				providerName: "ml-kit",
+				defaultModelId: "gemini-nano");
+		}
+
+		if (serviceType.IsInstanceOfType(this))
+		{
+			return this;
+		}
+
+		return null;
+	}
+
+	/// <inheritdoc />
+	void IDisposable.Dispose()
+	{
+		// Nothing to dispose. Implementation required for the IChatClient interface.
+	}
+}
---
+++ src/AI/src/Essentials.AI/Platform/Windows/WindowsAIChatClient.cs
@@ -0,0 +1,76 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Maui.Essentials.AI;
+
+/// <summary>
+/// Provides an <see cref="IChatClient"/> implementation based on native Windows AI APIs
+/// </summary>
+public sealed class WindowsAIChatClient : IChatClient
+{
+	/// <summary>
+	/// Lazily-initialized metadata describing the implementation.
+	/// </summary>
+	private ChatClientMetadata? _metadata;
+
+	/// <summary>
+	/// Initializes a new <see cref="WindowsAIChatClient"/> instance.
+	/// </summary>
+	public WindowsAIChatClient()
+	{
+	}
+
+	/// <inheritdoc />
+	public async Task<ChatResponse> GetResponseAsync(
+		IEnumerable<ChatMessage> chatMessages,
+		ChatOptions? options = null,
+		CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <inheritdoc />
+	public async IAsyncEnumerable<ChatResponseUpdate> GetStreamingResponseAsync(
+		IEnumerable<ChatMessage> chatMessages,
+		ChatOptions? options = null,
+		[EnumeratorCancellation] CancellationToken cancellationToken = default)
+	{
+		yield break;
+		throw new NotImplementedException();
+	}
+
+	/// <inheritdoc />
+	object? IChatClient.GetService(Type serviceType, object? serviceKey)
+	{
+		ArgumentNullException.ThrowIfNull(serviceType);
+
+		// If there's a service key, we don't support keyed services.
+		if (serviceKey is not null)
+		{
+			return null;
+		}
+
+		// If there's a request for metadata, lazily-initialize it and return it. We don't need to worry about race conditions,
+		// as there's no requirement that the same instance be returned each time, and creation is idempotent.
+		if (serviceType == typeof(ChatClientMetadata))
+		{
+			return _metadata ??= new ChatClientMetadata(
+				providerName: "windows-ai",
+				defaultModelId: "phi-silica");
+		}
+
+		if (serviceType.IsInstanceOfType(this))
+		{
+			return this;
+		}
+
+		return null;
+	}
+
+	/// <inheritdoc />
+	void IDisposable.Dispose()
+	{
+		// Nothing to dispose. Implementation required for the IChatClient interface.
+	}
+}
