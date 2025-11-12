4702d51ec5196c18828d2b407740c588710d010d
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-11-12 12:11:58 +0530

    Added a test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue16119.cs
@@ -0,0 +1,39 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 16119, "MenuFlyoutItem with FileImageSource displays icon in monochrome instead of original colors", PlatformAffected.UWP)]
+public class Issue16119 : TestShell
+{
+	protected override void Init()
+	{
+		MenuBarItem menuBarItem = new MenuBarItem {  Text = "Menu Flyout Item" };
+
+		MenuFlyoutItem menuFlyoutItem = new MenuFlyoutItem
+		{
+			Text = "Hello World",
+			IconImageSource = new FileImageSource { File = "small_dotnet_bot.png" }
+		};
+		menuBarItem.Add(menuFlyoutItem);
+
+		MenuBarItems.Add(menuBarItem);
+
+		VerticalStackLayout layout = new VerticalStackLayout
+		{
+			Padding = new Thickness(30, 0),
+			Spacing = 25,
+			VerticalOptions = LayoutOptions.Center,
+			Children =
+			{
+				new Label
+				{
+					AutomationId = "Issue16119DescriptionLabel",
+					Text = "The test passes if the MenuFlyoutItem's icon is in original color instead of monochrome",
+					HorizontalOptions = LayoutOptions.Center,
+					FontSize = 12
+				},
+			}
+		};
+
+		AddContentPage(new ContentPage { Content = layout });
+		FlyoutBehavior = FlyoutBehavior.Disabled;
+	}
+}
