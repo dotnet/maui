bf85d8fa577304eef74fcb9679a76ae727b4c75f
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-11-12 18:53:00 +0530

    Update Issue14572.cs

--- src/Controls/tests/TestCases.HostApp/Issues/Issue14572.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue14572.cs
@@ -6,22 +6,22 @@ public class Issue14572 : TabbedPage
 {
 	public Issue14572()
 	{
-		ItemsSource = new List<object> { new Vm1(), new Vm2(), new Vm3() };
-		ItemTemplate = new TabbedItemTemplateSelector();
+		ItemsSource = new List<object> { new Issue14572Vm1(), new Issue14572Vm2(), new Issue14572Vm3() };
+		ItemTemplate = new Issue14572TabbedItemTemplateSelector();
 	}
 	
-	public class Vm1 { }
-	public class Vm2 { }
-	public class Vm3 { }
-	class TabbedItemTemplateSelector : DataTemplateSelector
+	public class Issue14572Vm1 { }
+	public class Issue14572Vm2 { }
+	public class Issue14572Vm3 { }
+	class Issue14572TabbedItemTemplateSelector : DataTemplateSelector
 	{
 		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
 		{
-			if (item is Vm1)
+			if (item is Issue14572Vm1)
 				return new DataTemplate(() => new ContentPage() { Title = "Type 1" });
-			if (item is Vm2)
+			if (item is Issue14572Vm2)
 				return new DataTemplate(() => new ContentPage() { Title = "Type 2", Content = new Label { Text = "Type 2 Content", AutomationId = "Type2Content" } });
-			if (item is Vm3)
+			if (item is Issue14572Vm3)
 				return new DataTemplate(() => new ContentPage() { Title = "Type 3" });
 
 			return null;
