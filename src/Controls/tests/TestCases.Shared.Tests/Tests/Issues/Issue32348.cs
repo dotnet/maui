e545eec0ab9e65d1e5fdb0fe0ab397dfdf697de1
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-11-14 19:16:27 +0530

    added testcase.

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32348.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue32348 : _IssuesUITest
+{
+	public Issue32348(TestDevice testDevice) : base(testDevice)
+	{
+	}
+	public override string Issue => "CollectionView VSM Background and BackgroundColor work similarly";
+
+	[Test]
+	[Category(UITestCategories.CollectionView)]
+	public void TableViewTextCellCommand()
+	{
+		App.WaitForElement("Item 2");
+		App.Tap("Item 2");
+		App.WaitForElement("Item 5");
+		App.Tap("Item 5");
+		VerifyScreenshot();
+	}
+}
