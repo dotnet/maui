9e5f655742b2cfa9f987857407c827fe5e4402d8
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2025-11-19 16:17:28 +0530

    Added test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue31911.cs
@@ -0,0 +1,84 @@
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 31911, "CollectionView Header Footer not removed when set to null on Android", PlatformAffected.Android)]
+public class Issue31911 : ContentPage
+{
+    public Issue31911()
+    {
+        var grid = new Grid
+        {
+            Margin = new Thickness(20),
+            RowDefinitions =
+                {
+                    new RowDefinition { Height = GridLength.Auto },
+                    new RowDefinition { Height = GridLength.Star }
+                }
+        };
+        CollectionView collectionView = new CollectionView
+        {
+            AutomationId = "CollectionView",
+            ItemsSource = System.Array.Empty<string>(),
+        };
+        collectionView.EmptyView = new Label
+        {
+            BackgroundColor = Color.FromArgb("#FFE40606"),
+            Text = "EmptyView: This should show when no data.",
+        };
+
+        Label headerContent = new Label
+        {
+            BackgroundColor = Colors.LightBlue,
+            Text = "Header: Click me to verify EmptyView position",
+        };
+        collectionView.Header = headerContent;
+
+        Label footerContent = new Label
+        {
+            BackgroundColor = Colors.LightCoral,
+            Text = "Footer: Click me to verify EmptyView position",
+        };
+        collectionView.Footer = footerContent;
+
+        var buttonGrid = new Grid
+        {
+            ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = GridLength.Star },
+                    new ColumnDefinition { Width = GridLength.Star }
+                },
+            Margin = new Thickness(0, 10)
+        };
+
+        var toggleHeaderButton = new Button
+        {
+            AutomationId = "ToggleHeaderButton",
+            Text = "Remove Header",
+        };
+        toggleHeaderButton.Clicked += (s, e) =>
+        {
+            collectionView.Header = null;
+            toggleHeaderButton.Text = "Add Header";
+        };
+        Grid.SetColumn(toggleHeaderButton, 0);
+        buttonGrid.Children.Add(toggleHeaderButton);
+
+        var toggleFooterButton = new Button
+        {
+            AutomationId = "ToggleFooterButton",
+            Text = "Remove Footer",
+        };
+        toggleFooterButton.Clicked += (s, e) =>
+        {
+            collectionView.Footer = null;
+            toggleFooterButton.Text = "Add Footer";
+        };
+        Grid.SetColumn(toggleFooterButton, 1);
+        buttonGrid.Children.Add(toggleFooterButton);
+
+        Grid.SetRow(buttonGrid, 0);
+        grid.Children.Add(buttonGrid);
+        Grid.SetRow(collectionView, 1);
+        grid.Children.Add(collectionView);
+        Content = grid;
+    }
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31911.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue31911 : _IssuesUITest
+{
+    public Issue31911(TestDevice device) : base(device)
+    {
+    }
+
+    public override string Issue => "CollectionView Header Footer not removed when set to null on Android";
+
+    [Test, Order(1)]
+    [Category(UITestCategories.CollectionView)]
+    public void HeaderShouldBeRemovedWhenSetToNull()
+    {
+        App.WaitForElement("ToggleHeaderButton");
+        App.Tap("ToggleHeaderButton");
+        VerifyScreenshot();
+    }
+
+    [Test, Order(2)]
+    [Category(UITestCategories.CollectionView)]
+    public void FooterShouldBeRemovedWhenSetToNull()
+    {
+        App.WaitForElement("ToggleFooterButton");
+        App.Tap("ToggleFooterButton");
+        VerifyScreenshot();
+    }
+}
