dc8900f25c6520da1ee52ae68131241fe671d57c
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-11-21 17:57:15 +0530

    added UI test sample

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32586.cs
@@ -0,0 +1,31 @@
+﻿#if iOS || ANDROID
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue32586 : _IssuesUITest
+{
+	public override string Issue => "[iOS] Layout issue using TranslateToAsync causes infinite property changed cycle";
+
+	public Issue32586(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.SafeAreaEdges)]
+	public async Task VerifyLayoutWithTranslateToAsync()
+	{
+		var label = App.WaitForElement("TestLabel");
+		App.Tap("FooterButton");
+		await Task.Delay(500); // Wait for the animation to complete
+		App.Tap("FooterButton");
+		await Task.Delay(500); // Wait for the animation to complete
+
+		var labelText = label.GetText();
+		Assert.That(labelText, Is.EqualTo("Footer is now hidden"));
+	}
+}
+#endif
