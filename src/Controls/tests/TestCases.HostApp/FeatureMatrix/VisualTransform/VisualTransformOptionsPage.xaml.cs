a8c9582e6ae5f060181867f3f6795fb0a5894000
HarishKumarSF4517 <harish.kumar@syncfusion.com>
2025-11-21 18:38:42 +0530

    Resolved_review_correction

--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/VisualTransform/VisualTransformOptionsPage.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/VisualTransform/VisualTransformOptionsPage.xaml.cs
@@ -2,27 +2,9 @@ namespace Maui.Controls.Sample
 {
 	public partial class VisualTransformOptionsPage : ContentPage
 	{
-		private VisualTransformViewModal _viewModel;
+		private VisualTransformViewModel _viewModel;
 
-		private void OnAnchorXChanged(object sender, TextChangedEventArgs e)
-		{
-			if (double.TryParse(e.NewTextValue, out var value))
-			{
-				_viewModel.AnchorX = value;
-				AnchorXLabel.Text = $"AnchorX: {value:F2}";
-			}
-		}
-
-		private void OnAnchorYChanged(object sender, TextChangedEventArgs e)
-		{
-			if (double.TryParse(e.NewTextValue, out var value))
-			{
-				_viewModel.AnchorY = value;
-				AnchorYLabel.Text = $"AnchorY: {value:F2}";
-			}
-		}
-
-		public VisualTransformOptionsPage(VisualTransformViewModal viewModel)
+		public VisualTransformOptionsPage(VisualTransformViewModel viewModel)
 		{
 			InitializeComponent();
 			_viewModel = viewModel;
