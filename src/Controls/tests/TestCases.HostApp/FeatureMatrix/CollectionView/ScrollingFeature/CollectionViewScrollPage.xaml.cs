3b1dad61d077eb67eb39b1f2d3a4bdc6128f356a
TamilarasanSF4853 <tamilarasan.velu@syncfusion.com>
2025-11-23 21:35:17 +0530

    added events

--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/CollectionView/ScrollingFeature/CollectionViewScrollPage.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/CollectionView/ScrollingFeature/CollectionViewScrollPage.xaml.cs
@@ -17,6 +17,30 @@ public partial class CollectionViewScrollPage : ContentPage
 	{
 		BindingContext = _viewModel = new CollectionViewViewModel();
 		_viewModel.ItemsSourceType = ItemsSourceType.ObservableCollectionT3;
+		_viewModel.ScrollToPosition = ScrollToPosition.MakeVisible;
+		scrolledEventLabel.Text = "";
+		scrollToRequestedLabel.Text = "";
+		remainingItemsThresholdLabel.Text = "";
 		await Navigation.PushAsync(new ScrollBehaviorOptionsPage(_viewModel));
+	}
+
+	private void OnCollectionViewScrolled(object sender, ItemsViewScrolledEventArgs e)
+	{
+		scrolledEventLabel.Text = "Scrolled Event Fired";
+	}
+
+	private void OnCollectionViewScrollToRequested(object sender, ScrollToRequestEventArgs e)
+	{
+		scrollToRequestedLabel.Text = "ScrollToRequested Event Fired";
+	}
+
+	private void OnScrollToButtonClicked(object sender, EventArgs e)
+	{
+		collectionView.ScrollTo(_viewModel.ScrollToIndex, position: _viewModel.ScrollToPosition, animate: true);
+	}
+
+	private void OnRemainingItemsThresholdReached(object sender, EventArgs e)
+	{
+		remainingItemsThresholdLabel.Text = "RemainingItemsThresholdReached Event Fired";
 	}
 }
--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/CollectionView/ScrollingFeature/ScrollBehaviorOptionsPage.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/CollectionView/ScrollingFeature/ScrollBehaviorOptionsPage.xaml.cs
@@ -98,4 +98,26 @@ public partial class ScrollBehaviorOptionsPage : ContentPage
 			_viewModel.FlowDirection = FlowDirection.RightToLeft;
 		}
 	}
+	private void OnScrollToPositionChanged(object sender, CheckedChangedEventArgs e)
+	{
+		if (!(sender is RadioButton radioButton) || !e.Value)
+			return;
+
+		if (radioButton == ScrollToPositionMakeVisible)
+			_viewModel.ScrollToPosition = ScrollToPosition.MakeVisible;
+		else if (radioButton == ScrollToPositionStart)
+			_viewModel.ScrollToPosition = ScrollToPosition.Start;
+		else if (radioButton == ScrollToPositionCenter)
+			_viewModel.ScrollToPosition = ScrollToPosition.Center;
+		else if (radioButton == ScrollToPositionEnd)
+			_viewModel.ScrollToPosition = ScrollToPosition.End;
+	}
+
+	private void ScrollToIndexEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		if (int.TryParse(ScrollToIndexEntry.Text, out int index))
+		{
+			_viewModel.ScrollToIndex = index;
+		}
+    }
 }
