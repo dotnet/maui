ff12a1076f98183103448f30b1e67dfbbc48378e
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-11-24 13:50:56 +0530

    Test case added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32724.cs
@@ -0,0 +1,79 @@
+# if TEST_FAILS_ON_ANDROID
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+public class Issue32724 : _IssuesUITest
+{
+	public Issue32724(TestDevice device) : base(device) { }
+
+	public override string Issue => "Applying Shadow property affects the properties in Visual Transform Matrix";
+	[Test]
+	[Category(UITestCategories.Border)]
+	public void VerifyScaleAndShadow()
+	{
+		App.WaitForElement("ScaleButton");
+		App.Tap("ScaleButton");
+		App.Tap("ToggleShadowButton");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Border)]
+	public void VerifyRotationAndShadow()
+	{
+		App.Tap("RotationButton");
+		App.Tap("ToggleShadowButton");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Border)]
+	public void VerifyRotationXAndShadow()
+	{
+		App.Tap("RotationXButton");
+		App.Tap("ToggleShadowButton");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Border)]
+	public void VerifyRotationYAndShadow()
+	{
+		App.Tap("RotationYButton");
+		App.Tap("ToggleShadowButton");
+		VerifyScreenshot();
+	}
+
+	[Test]
+	[Category(UITestCategories.Border)]
+	public void VerifyAnchorXAndShadow()
+	{
+		App.Tap("AnchorXButton");
+		App.Tap("RotationButton");
+		App.Tap("ToggleShadowButton");
+		VerifyScreenshot();
+	}
+	[Test]
+	[Category(UITestCategories.Border)]
+	public void VerifyAnchorYAndShadow()
+    {
+        App.Tap("AnchorYButton");
+		App.Tap("RotationButton");
+		App.Tap("ToggleShadowButton");
+		VerifyScreenshot();
+    }
+
+	[Test]
+	[Category(UITestCategories.Border)]
+	public void VerifyAnchorXAndAnchorYShadow()
+	{
+		App.Tap("AnchorXButton");
+		App.Tap("AnchorYButton");
+		App.Tap("RotationButton");
+		App.Tap("ToggleShadowButton");
+		VerifyScreenshot();
+	}
+}
+#endif
