dfaec79af81192215f7f1ddddf7851a44a5c2b44
Shane Neuville <shneuvil@microsoft.com>
2025-11-24 18:38:49 -0600

    - revert sandbox changes

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -5,16 +5,5 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-		Console.WriteLine("=== SANDBOX APP: MainPage loaded ===");
-	}
-
-	private void OnCrashClicked(object sender, EventArgs e)
-	{
-		Console.WriteLine("=== SANDBOX APP: Crash button clicked! About to crash... ===");
-		StatusLabel.Text = "About to crash...";
-		
-		// Intentional crash - null reference exception
-		string? nullString = null;
-		Console.WriteLine($"Length: {nullString!.Length}");
 	}
 }
