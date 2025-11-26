b9646b721ab60c21652002f940aba7fb38b5b213
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-11-26 15:52:42 +0530

    Added fix and test case

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue29824.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue29824 : _IssuesUITest
+{
+	public Issue29824(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Shell Colors Do Not Update Correctly When Switching Between TabBar Items";
+
+
+	[Test, Order(1)]
+	[Category(UITestCategories.Shell)]
+	public void ShellAppearanceUpdatesWhenChangingShellSectionToTab_1()
+	{
+		App.WaitForElement("HomePageButton");
+		App.Tap("HomePageButton");
+		App.Click("Settings");
+		App.Tap("Home");
+		VerifyScreenshot();
+	}
+
+	[Test, Order(2)]
+	[Category(UITestCategories.Shell)]
+	public void ShellAppearanceUpdatesWhenChangingShellSectionToTab_2()
+	{
+		App.WaitForElement("Settings");
+		App.Tap("Settings");
+		App.Tap("SettingsPageButton");
+		App.Tap("Home");
+		App.Back();
+		App.WaitForElement("HomePageButton");
+		App.Click("Settings");
+		VerifyScreenshot();
+	}
+
+	[Test, Order(3)]
+	[Category(UITestCategories.Shell)]
+	public void ShellAppearanceUpdatesWhenChangingBetweenTabs()
+	{
+		App.WaitForElement("SettingsPageLabel");
+		App.Back();
+		App.WaitForElement("SettingsPageButton");
+		App.Click("Home");
+		App.WaitForElement("HomePageLabel");
+		App.Back();
+		App.WaitForElement("HomePageButton");
+		App.Tap("Settings");
+		App.Tap("Home");
+		VerifyScreenshot();
+	}
+
+}
