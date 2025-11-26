38ce94a14e1e8dd579e10fe127d2d40b468a7f84
Simon Rozsival <simon@rozsival.com>
2025-11-26 09:31:28 +0100

    Improve ElementHandelrAttribute error handling

--- src/Core/src/Handlers/Element/ElementHandlerAttribute.cs
+++ src/Core/src/Handlers/Element/ElementHandlerAttribute.cs
@@ -4,15 +4,33 @@ using System.Diagnostics.CodeAnalysis;
 namespace Microsoft.Maui.Handlers;
 
 [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-internal class ElementHandlerAttribute(
-	[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] Type handlerType)
-	: Attribute
+internal class ElementHandlerAttribute : Attribute
 {
-	[UnconditionalSuppressMessage("Trimming", "IL2026", Justification = "The DynamicallyAccessedMembers annotation ensures constructor availability.")]
-	public virtual IElementHandler CreateHandler(IMauiContext context)
-		=> (IElementHandler)Activator.CreateInstance(handlerType)!;
+	public ElementHandlerAttribute([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] Type handlerType)
+	{
+		HandlerType = handlerType;
+	}
 
-	public virtual Type HandlerType => handlerType;
+	public virtual IElementHandler CreateHandler(IMauiContext context)
+	{
+		object? handler = Activator.CreateInstance(HandlerType);
+		if (handler is IElementHandler elementHandler)
+		{
+			return elementHandler;
+		}
+
+		if (typeof(IElementHandler).IsAssignableFrom(HandlerType))
+		{
+			throw new InvalidOperationException($"Could not create an instance of handler type {HandlerType} for element handler. Ensure it has a public parameterless constructor.");
+		}
+		else
+		{
+			throw new InvalidOperationException($"The specified handler type {HandlerType} does not implement {nameof(IElementHandler)}.");
+		}
+	}
+
+	[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]
+	public virtual Type HandlerType { get; }
 }
 
 #if ANDROID
