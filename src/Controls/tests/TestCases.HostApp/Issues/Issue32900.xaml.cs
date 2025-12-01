36798406b35f1438b6acdc3c13c95c4b79a10237
Jakub Florkowski <42434498+kubaflo@users.noreply.github.com>
2025-12-01 11:46:55 +0100

    [Issue-Resolver] Fix TabBar visibility on macOS 26 (Tahoe) (#32910)
    * Fix TabBar visibility on macOS 26 by excluding Mac from TabSidebar mode
    
    Fixes #32900
    
    The TabSidebar mode introduced for iOS/Catalyst 18+ was causing tabs
    to be hidden on macOS. This change excludes macOS from using TabSidebar
    mode by checking UIDevice.CurrentDevice.UserInterfaceIdiom != UIUserInterfaceIdiom.Mac.
    
    Changes:
    - Modified DisableiOS18ToolbarTabs to skip TabSidebar mode on macOS
    - Added UI test case Issue32900 to verify tabs are visible on macOS
    
    * Improve on agent's solution

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue32900.xaml.cs
@@ -0,0 +1,10 @@
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 32900, "'Search Recipe' and 'My Recipe' tab are missing on MacOS Tahoe 26.1", PlatformAffected.macOS)]
+public partial class Issue32900 : Shell
+{
+	public Issue32900()
+	{
+		InitializeComponent();
+	}
+}
