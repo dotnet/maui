50eb58dd318b37705a9f2949a3de74f6b3c760e5
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-12-03 12:09:52 +0530

    sandbox sample

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -5,5 +5,17 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		System.Diagnostics.Debug.WriteLine("[SafeArea] MainPage initialized");
+	}
+
+	private async void OnGoToSignOutClicked(object sender, EventArgs e)
+	{
+		// await MainThread.InvokeOnMainThreadAsync(async () =>
+		// {
+		//     await Shell.Current.GoToAsync("//signout", true);
+		// });
+		System.Diagnostics.Debug.WriteLine("[SafeArea] Navigation to SignOut page using shell");
+		await Shell.Current.GoToAsync("//signout", false);
+
 	}
 }
