4f6ee262e6101f980b7b0260163c0b5f82623dec
Stephane Delcroix <stephane@delcroix.org>
2025-12-03 15:56:12 +0100

    Move duplicate routes test from DeviceTests to Core.UnitTests
    The test ShouldThrowsArgumentExceptionOnDuplicateRoutes doesn't require
    any platform handlers - it's testing pure exception throwing logic.
    This should be a unit test, not a device test.
    
    Moved to: src/Controls/tests/Core.UnitTests/ShellTests.cs
    Removed from: src/Controls/tests/DeviceTests/Elements/Shell/ShellTests.cs

--- src/Controls/tests/DeviceTests/Elements/Shell/ShellTests.cs
+++ src/Controls/tests/DeviceTests/Elements/Shell/ShellTests.cs
@@ -1157,36 +1157,5 @@ namespace Microsoft.Maui.DeviceTests
 				action?.Invoke(value);
 				return value;
 			});
-
-		[Fact(DisplayName = "Duplicate Routes Should Throws Exception")]
-		public void ShouldThrowsArgumentExceptionOnDuplicateRoutes()
-		{
-			var shell = new Shell();
-			var sameRoute = $"DuplicateRoute";
-
-			var flyoutItem1 = new FlyoutItem();
-			var shellContent1 = new ShellContent
-			{
-				Title = "Home"
-			};
-			flyoutItem1.Items.Add(shellContent1);
-			shell.Items.Add(flyoutItem1);
-
-			var flyoutItem2 = new FlyoutItem();
-			var shellContent2 = new ShellContent
-			{
-				Title = "Home"
-			};
-			flyoutItem2.Items.Add(shellContent2);
-			shell.Items.Add(flyoutItem2);
-
-			shellContent1.Route = sameRoute;
-			var exception = Assert.Throws<ArgumentException>(() =>
-			{
-				shellContent2.Route = sameRoute;
-			});
-
-			Assert.Equal($"Duplicated Route: \"{sameRoute}\" is already registered to another element of type ShellContent. Routes must be unique across the Shell hierarchy to avoid navigation conflicts. (Parameter 'route')", exception.Message);
-		}
 	}
 }
