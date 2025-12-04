f6a7b5c23fd1b0d35eae62749cacfd31553e37d6
SuthiYuvaraj <suthi.yuvaraj@syncfusion.com>
2025-12-04 21:38:04 +0530

    Fix for TabBar Navigation does not invoke its IQueryAttributable.ApplyQueryAttributes(query) (#25663)
    * Fix - 13573 fix changes
    
    * Testcases commit
    
    * Update Shell.cs
    
    * commit for testcases
    
    * commit for review changes
    
    * Commit for class name changes
    
    * Commit class name change
    
    * Commit for review
    
    * commit for testcase changes
    
    * Commit for images
    
    * Test case changes
    
    * Update Issue13537.cs
    
    * Update Issue13537.cs
    
    * Update Issue13537.cs
    
    * Update Shell.cs
    
    * Update Shell.cs
    
    * changes for initial triggering
    
    * comments updated
    
    * Update Issue13537.cs
    
    * Update Issue13537.cs
    
    commit testcase failure
    
    * Changes for indentation
    
    * Review changes
    
    * Update Issue13537.cs

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue13537.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue13537 : _IssuesUITest
+	{
+#if ANDROID
+		const string backButtonIdentifier = "";
+#else
+		const string backButtonIdentifier = "Home";
+#endif
+		public Issue13537(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "ApplyQueryAttributes should Trigger for all navigations";
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void ApplyQueryAttributeShouldTriggerforPushAndPopButton()
+		{
+			App.WaitForElement("HomePageTestLabel");
+			App.Tap("PushAsyncButton");
+			App.WaitForElement("InnerPageTestLabel");
+			App.Tap("PopAsyncButton");
+			var result = App.WaitForElement("HomePageTestLabel").GetText();
+			Assert.That(result, Is.EqualTo("Issue13537HomePage QueryAttribute is triggered"));
+		}
+
+#if TEST_FAILS_ON_CATALYST
+		//App.TapBackArrow doesnot actually tab the back arrow button instead it taps the first tab element on the screen as both have the same access identifier.
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void ApplyQueryAttributeShouldTriggerforPushAndBackButton()
+		{
+			App.WaitForElement("HomePageTestLabel");
+			App.Tap("PushAsyncButton");
+			App.WaitForElement("InnerPageTestLabel");
+#if IOS
+			App.Back();
+#else
+			App.TapBackArrow(backButtonIdentifier);
+#endif
+			var result = App.WaitForElement("HomePageTestLabel").GetText();
+			Assert.That(result, Is.EqualTo("Issue13537HomePage QueryAttribute is triggered"));
+		}
+#endif
+
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void ApplyQueryAttributeShouldTriggerforTab()
+		{
+			App.WaitForElement("Favorite");
+			App.Tap("Favorite");
+			App.WaitForElement("FavouritePageTestLabel");
+			App.Tap("GoToAsyncButton");
+			var result = App.WaitForElement("InnerPageTestLabel").GetText();
+			Assert.That(result, Is.EqualTo("Parameter From Favorite Page to New Page"));
+		}
+	}
+
+}
