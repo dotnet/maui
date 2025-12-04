1e4fe791d76c15feb0a7792e6a6d545a8a94c3ac
SubhikshaSf4851 <subhiksha.c@syncfusion.com>
2025-12-04 20:03:38 +0530

    Updated TestSample

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue32984.cs
@@ -0,0 +1,35 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, "32984", "Picker on resize not working on Windows", PlatformAffected.UWP)]
+public class Issue32984 : ContentPage
+{
+	public Issue32984()
+	{
+		VerticalStackLayout layout = new VerticalStackLayout
+		{
+			WidthRequest = 600
+		};
+
+		Picker issue32984Picker = new Picker
+		{
+			AutomationId= "issue32984Picker",	
+			Title = "Select an item",
+			HorizontalOptions = LayoutOptions.Fill,
+			ItemsSource = new List<string> { "Item1", "Item2", "Item3"}
+		};
+
+		Button button = new Button
+		{
+			AutomationId= "issue32984Button",
+			Text = "Click me",
+			HorizontalOptions = LayoutOptions.Fill
+		};
+
+		button.Clicked += (s, e) => { layout.WidthRequest = 200; };
+
+		layout.Children.Add(issue32984Picker);
+		layout.Children.Add(button);
+
+		Content = layout;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32984.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue32984 : _IssuesUITest
+{
+	public Issue32984(TestDevice device)
+		: base(device)
+	{
+	}
+
+	public override string Issue => "Picker on resize not working on Windows";
+
+	[Test]
+	[Category(UITestCategories.Picker)]
+	public void Issue32984PickerShouldResize()
+	{
+		App.WaitForElement("issue32984Button");
+		App.Tap("issue32984Picker");
+		App.TapCoordinates(10, 10);
+		App.Tap("issue32984Button");
+		App.Tap("issue32984Picker");
+		VerifyScreenshot();
+	}
+}
