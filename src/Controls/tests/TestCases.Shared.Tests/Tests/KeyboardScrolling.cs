e38eeec8b46574ca230faa6701274704898eec82
HarishKumarSF4517 <harish.kumar@syncfusion.com>
2025-12-05 18:45:04 +0530

    Update KeyboardScrolling.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/KeyboardScrolling.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/KeyboardScrolling.cs
@@ -120,7 +120,7 @@ namespace Microsoft.Maui.TestCases.Tests
 
 		internal static void CloseiOSEditorKeyboard(AppiumDriver? driver)
 		{
-			var keyboardDoneButton = driver?.FindElement(MobileBy.Name("Microsoft.Maui.Platform.MauiDoneAccessory.DoneButton"));
+			var keyboardDoneButton = driver?.FindElement(MobileBy.Name("Done"));
 			keyboardDoneButton?.Click();
 		}
 
