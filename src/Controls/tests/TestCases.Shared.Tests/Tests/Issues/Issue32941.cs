b1c0da6eb5268c3bd7fa662f61ec587372af593e
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-12-05 15:12:35 +0530

    added snapshots

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32941.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32941.cs
@@ -1,25 +1,26 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_CATALYST && TEST_FAILS_ON_WINDOWS // SafeAreaEdges not supported on Catalyst and Windows
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue32941 : _IssuesUITest
 {
-	public class Issue32941 : _IssuesUITest
+	public Issue32941(TestDevice testDevice) : base(testDevice)
 	{
-		public Issue32941(TestDevice testDevice) : base(testDevice)
-		{
-		}
+	}
 
-		public override string Issue => "Label Overlapped by Android Status Bar When Using SafeAreaEdges=Container in .NET MAUI";
+	public override string Issue => "Label Overlapped by Android Status Bar When Using SafeAreaEdges=Container in .NET MAUI";
 
-		[Test]
-		[Category(UITestCategories.Shell)]
-		[Category(UITestCategories.SafeAreaEdges)]
-		public void ShellContentShouldRespectSafeAreaEdges_After_Navigation()
-		{
-			App.WaitForElement("NavigateToNextPageBtn");
-			App.Tap("NavigateToNextPageBtn");
-			VerifyScreenshot();
-		}
+	[Test]
+	[Category(UITestCategories.Shell)]
+	[Category(UITestCategories.SafeAreaEdges)]
+	public void ShellContentShouldRespectSafeAreaEdges_After_Navigation()
+	{
+		App.WaitForElement("NavigateToNextPageBtn");
+		App.Tap("NavigateToNextPageBtn");
+		VerifyScreenshot();
 	}
 }
+#endif
