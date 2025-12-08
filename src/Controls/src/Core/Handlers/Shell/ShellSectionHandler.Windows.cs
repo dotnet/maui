8d031f6fdd42f1a37701cbde617e93748340e538
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-12-08 13:42:01 +0530

    co pilot concern addressed

--- src/Controls/src/Core/Handlers/Shell/ShellSectionHandler.Windows.cs
+++ src/Controls/src/Core/Handlers/Shell/ShellSectionHandler.Windows.cs
@@ -250,7 +250,7 @@ namespace Microsoft.Maui.Controls.Handlers
 
 		void OnShellContentPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
 		{
-			if(sender is not ShellContent shellContent)
+			if (sender is not ShellContent shellContent)
 				return;
 
 			if (e.PropertyName == nameof(ShellContent.Title))
