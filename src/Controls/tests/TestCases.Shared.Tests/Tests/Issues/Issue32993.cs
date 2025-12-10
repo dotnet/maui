d570732ea15bd4a14ec1ea50b5e155b678281c7b
Subhiksha Chandrasekaran <subhiksha.c@syncfusion.com>
2025-12-10 16:18:33 +0530

    Fix and TestSample updated

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32993.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue32993 : _IssuesUITest
+{
+	public override string Issue => "TabBar Should update correctly in RTL mode";
+
+	public Issue32993(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.Shell)]
+	public void Issue32993TabBarUpdatesCorrectlyInRTLMode()
+	{
+		App.WaitForElement("Issue32993SetRTL");
+		App.Tap("Issue32993SetRTL");
+		VerifyScreenshot();
+	}
+}
