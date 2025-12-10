1e47874ef4329a59160424b4070e9789095f0ffb
James Alickolli <jalickolli@gmail.com>
2025-12-11 00:04:05 +1100

    [Android] Added BorderThickness and Color to Picker

--- src/Controls/src/Core/Picker/Picker.Android.cs
+++ src/Controls/src/Core/Picker/Picker.Android.cs
@@ -1,7 +1,17 @@
-﻿namespace Microsoft.Maui.Controls
+﻿using Microsoft.Maui.Controls.Platform;
+
+namespace Microsoft.Maui.Controls
 {
 	public partial class Picker
 	{
+		public static void MapBorderColor(IPickerHandler handler, Picker picker)
+		{
+			handler.PlatformView?.CreateBorder(picker);
+		}
 
+		public static void MapBorderThickness(IPickerHandler handler, Picker picker)
+		{
+			handler.PlatformView?.CreateBorder(picker);
+		}
 	}
 }
