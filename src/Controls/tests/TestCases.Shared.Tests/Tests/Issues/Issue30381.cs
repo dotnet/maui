ea45c82a57b6acebf2198a553806c08f3dc043ca
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-12-11 12:23:36 +0530

    test added

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue30381.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue30381 : _IssuesUITest
+{
+    public override string Issue => "WebView GoBack/GoForward not working for HtmlWebViewSource on iOS";
+
+    public Issue30381(TestDevice device) : base(device)
+    {
+    }
+
+    [Test]
+    [Category(UITestCategories.WebView)]
+    public void WebViewCanGoForwardShouldHaveValueAfterNavigation()
+    {
+        // Wait for WebView to load
+        App.WaitForElement("TestWebView");
+        App.WaitForElement("ClickLinkButton");
+
+        // Click the link via JavaScript to navigate to external URL
+        App.Tap("ClickLinkButton");
+
+        // Wait for navigation to complete
+        // Add a delay to allow navigation to process
+        Thread.Sleep(3000);
+        App.Tap("UpdateStatusButton");
+        Assert.That(App.FindElement("CanGoBackLabel").GetText(), Is.EqualTo("CanGoBack: True"));
+        Assert.That(App.FindElement("CanGoForwardLabel").GetText(), Is.EqualTo("CanGoForward: False"));
+
+        App.Tap("GoBackButton");
+
+        // Wait for back navigation to complete
+        Thread.Sleep(2000);
+
+        Assert.That(App.FindElement("CanGoBackLabel").GetText(), Is.EqualTo("CanGoBack: False"));
+        Assert.That(App.FindElement("CanGoForwardLabel").GetText(), Is.EqualTo("CanGoForward: True"));
+    }
+}
