d819fb7da835242d72671ff9afef7b3b475d46be
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-12-11 15:14:07 +0530

    Added the test case

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32932.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue32932 : _IssuesUITest
+{
+	public Issue32932(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "[Android] EmptyView doesn’t display when CollectionView is placed inside a VerticalStackLayout";
+
+	[Test]
+	[Category(UITestCategories.CollectionView)]
+	public void EmptyViewShouldDisplayWhenCollectionViewIsInsideVerticalStackLayout()
+	{
+		App.WaitForElement("EmptyView");
+		VerifyScreenshot();
+	}
+}
