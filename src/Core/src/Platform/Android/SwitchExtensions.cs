d7cde7e5446ced97b2dc6f8fd79b833a488ab868
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-12-12 14:31:56 +0530

    Updated SwitchExtension.cs

--- src/Core/src/Platform/Android/SwitchExtensions.cs
+++ src/Core/src/Platform/Android/SwitchExtensions.cs
@@ -7,6 +7,9 @@ namespace Microsoft.Maui.Platform
 {
 	public static class SwitchExtensions
 	{
+		static ColorStateList? _defaultTrackTintList;
+		static ColorStateList? _defaultThumbTintList;
+
 		public static void UpdateIsOn(this ASwitch aSwitch, ISwitch view)
 		{
 			aSwitch.Checked = view.IsOn;
@@ -34,15 +37,31 @@ namespace Microsoft.Maui.Platform
 		internal static void UpdateTrackColor(this MSwitch materialSwitch, ISwitch view)
 		{
 			var trackColor = view.TrackColor;
+			_defaultTrackTintList ??= materialSwitch.TrackTintList;
 
 			if (trackColor is not null)
 			{
 				materialSwitch.TrackTintList = ColorStateList.ValueOf(trackColor.ToPlatform());
 			}
-			// else
-			// {
-			// 	materialSwitch.TrackTintList = null;
-			// }
+			else
+			{
+				materialSwitch.TrackTintList = _defaultTrackTintList;
+			}
+		}
+
+		internal static void UpdateThumbColor(this MSwitch materialSwitch, ISwitch view)
+		{
+			var thumbColor = view.ThumbColor;
+			_defaultThumbTintList ??= materialSwitch.ThumbTintList;
+			if (thumbColor is not null)
+			{
+				materialSwitch.ThumbTintList = ColorStateList.ValueOf(thumbColor.ToPlatform());
+			}
+			else if (thumbColor is null && materialSwitch is MauiMaterialSwitch mauiSwitch)
+			{
+				materialSwitch.ThumbTintList = _defaultThumbTintList;
+
+			}
 		}
 
 		public static void UpdateThumbColor(this ASwitch aSwitch, ISwitch view)
