e674c818dc09bb278fad8ed8ced936beceaffd4a
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-12-17 02:21:34 +0530

    [iOS] Fixed memory leak with PopToRootAsync when using TitleView (#28547)
    ### Issue Detail
    When navigating using PushAsync and PopToRootAsync, the destructor was
    not invoked when using PopToRootAsync.
     
    ### Root Cause
    The TitleView was not properly disposed when using PopToRootAsync.
     
    ### Description of Change
    Resolved the issue by disposing the TitleView using Disconnect, similar
    to PopAsync.
    
    Reference:
    https://github.com/dotnet/maui/blob/main/src/Controls/src/Core/Compatibility/Handlers/NavigationPage/iOS/NavigationRenderer.cs#L345
    
    ### Issues Fixed
    
    Fixes https://github.com/dotnet/maui/issues/28201
    
    ### Screenshots
    
    | Before Issue Fix | After Issue Fix |
    |----------|----------|
    | <video width="300" height="600"
    src="https://github.com/user-attachments/assets/23f571a4-f6f9-4978-b6d8-0a334b2dd593">
    | <video width="300" height="600"
    src="https://github.com/user-attachments/assets/0c7e5202-bf49-4fbd-98b1-3028c1cbc8c8">)
    |

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue28201.cs
@@ -0,0 +1,33 @@
+﻿#if TEST_FAILS_ON_ANDROID // Destructor not triggered on Android: https://github.com/dotnet/maui/issues/28549
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue28201 : _IssuesUITest
+{
+	public Issue28201(TestDevice testDevice) : base(testDevice)
+	{
+	}
+	public override string Issue => "TitleView disposed when Page is disposed";
+
+	[Test]
+	[Category(UITestCategories.TitleView)]
+	public void TitleViewDisposed()
+	{
+		string pushButton = "pushButton";
+		string popButton = "popButton";
+		for (int i = 0; i < 3; i++)
+		{
+			App.WaitForElement(pushButton);
+			App.Tap(pushButton);
+			App.WaitForElement(popButton);
+			App.Tap(popButton);
+		}
+		var label = App.WaitForElement("label");
+		App.Tap("checkStatusButton");
+		Assert.That(label.GetText(), Is.EqualTo("Page Destroyed"));
+	}
+}
+#endif
