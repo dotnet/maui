9f61d473a547f263471d8f5c8c7640435207597e
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-12-17 02:24:51 +0530

    Fixed the FlyoutPage.Flyout Disappearing When Maximizing the Window on Mac Platform (#26701)
    ### Issue Details
    
    - FlyoutPage.Flyout disappeared When the FlyoutPage is maximized
    
    ### Root Cause
    
    - When the FlyoutPage is maximized, the flyout is removed even when
    IsPresented is true. This causes the FlyoutPage's flyout to disappear.
    
    ### Description of Change
    
    - When maximizing the Mac window, the FlyoutPage is removed in the
    ViewWillTransitionToSize(CoreGraphics.CGSize toSize,
    IUIViewControllerTransitionCoordinator coordinator) method using the
    UpdatePresented(bool value) method. Here, there is no need to update the
    FlyoutPage when maximizing the Mac window.
    - Therefore, I have ignored the FlyoutPage update in this scenario.
    
    ### Issues Fixed
    Fixes https://github.com/dotnet/maui/issues/22719
    
    Validated the behaviour in the following platforms
    - [x] Android
    - [x] Windows
    - [x] iOS
    - [x] Mac
    
    ### Test Case:
    This issue occurs only when resizing the Mac window, and we could not
    find a way to automate the dynamic resizing of Mac windows. Therefore,
    test cases for this scenario are not included in this PR.
    
    ### Output
    
    |Before|After|
    |--|--|
    | <video
    src="https://github.com/user-attachments/assets/079fceb7-02cd-430c-be9c-014838a1dc4f"
    >| <video
    src="https://github.com/user-attachments/assets/e13d7b26-5090-409b-aaf6-e375791854c2">|
    
    ---------
    
    Co-authored-by: Shane Neuville <shneuvil@microsoft.com>
    Co-authored-by: Javier Suárez <javiersuarezruiz@hotmail.com>

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue22719.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 22719, "FlyoutPage flyout disappears when maximizing the window on the mac platform", PlatformAffected.macOS)]
+	public partial class Issue22719 : FlyoutPage, IFlyoutPageController
+	{
+		public Issue22719()
+		{
+			InitializeComponent();
+#if WINDOWS
+			FlyoutLayoutBehavior = FlyoutLayoutBehavior.SplitOnLandscape;
+#else
+			FlyoutLayoutBehavior = FlyoutLayoutBehavior.SplitOnPortrait;
+#endif
+		}
+
+		//  iPhone by default doesn't support split modes so we set this to validate split mode behaviors 
+		bool IFlyoutPageController.ShouldShowSplitMode
+		{
+			get
+			{
+				var orientation = DeviceDisplay.Current.MainDisplayInfo.Orientation;
+#if WINDOWS
+				return orientation == DisplayOrientation.Landscape;
+#else
+				return orientation == DisplayOrientation.Portrait;
+#endif
+			}
+		}
+	}
+}
