75f97d7f82fcd2ea0e5409f284d9bac04d80019e
Gerald Versluis <gerald.versluis@microsoft.com>
2025-12-19 14:55:37 +0100

    Remove CheckBox and Editor XML docs and update controls
    Deleted the XML documentation files for CheckBox and Editor. Updated related core control source files, possibly to remove or refactor references to these documentation files or to align with changes in documentation strategy.

--- src/Controls/src/Core/Slider/Slider.cs
+++ src/Controls/src/Core/Slider/Slider.cs
@@ -6,7 +6,14 @@ using Microsoft.Maui.Graphics;
 
 namespace Microsoft.Maui.Controls
 {
-	/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="Type[@FullName='Microsoft.Maui.Controls.Slider']/Docs/*" />
+	/// <summary>
+	/// Represents a horizontal bar that a user can slide to select a value from a continuous range.
+	/// </summary>
+	/// <remarks>
+	/// The <see cref="Slider"/> control allows users to select a numeric value by moving a thumb along a track.
+	/// Use the <see cref="Minimum"/> and <see cref="Maximum"/> properties to define the range,
+	/// and the <see cref="Value"/> property to get or set the current selection.
+	/// </remarks>
 	[DebuggerDisplay("{GetDebuggerDisplay(), nq}")]
 	public partial class Slider : View, ISliderController, IElementConfiguration<Slider>, ISlider
 	{
@@ -16,7 +23,7 @@ namespace Microsoft.Maui.Controls
 		bool _userSetValue = false;
 		bool _isRecoercing = false;
 
-		/// <summary>Bindable property for <see cref="Minimum"/>.</summary>
+		/// <summary>Bindable property for <see cref="Minimum"/>. This is a bindable property.</summary>
 		public static readonly BindableProperty MinimumProperty = BindableProperty.Create(
 			nameof(Minimum), typeof(double), typeof(Slider), 0d,
 			propertyChanged: (bindable, oldValue, newValue) =>
@@ -25,7 +32,7 @@ namespace Microsoft.Maui.Controls
 				slider.RecoerceValue();
 			});
 
-		/// <summary>Bindable property for <see cref="Maximum"/>.</summary>
+		/// <summary>Bindable property for <see cref="Maximum"/>. This is a bindable property.</summary>
 		public static readonly BindableProperty MaximumProperty = BindableProperty.Create(
 			nameof(Maximum), typeof(double), typeof(Slider), 1d,
 			propertyChanged: (bindable, oldValue, newValue) =>
@@ -34,7 +41,7 @@ namespace Microsoft.Maui.Controls
 				slider.RecoerceValue();
 			});
 
-		/// <summary>Bindable property for <see cref="Value"/>.</summary>
+		/// <summary>Bindable property for <see cref="Value"/>. This is a bindable property.</summary>
 		public static readonly BindableProperty ValueProperty = BindableProperty.Create(nameof(Value), typeof(double), typeof(Slider), 0d, BindingMode.TwoWay, coerceValue: (bindable, value) =>
 		{
 			var slider = (Slider)bindable;
@@ -68,33 +75,41 @@ namespace Microsoft.Maui.Controls
 			}
 		}
 
-		/// <summary>Bindable property for <see cref="MinimumTrackColor"/>.</summary>
+		/// <summary>Bindable property for <see cref="MinimumTrackColor"/>. This is a bindable property.</summary>
 		public static readonly BindableProperty MinimumTrackColorProperty = BindableProperty.Create(nameof(MinimumTrackColor), typeof(Color), typeof(Slider), null);
 
-		/// <summary>Bindable property for <see cref="MaximumTrackColor"/>.</summary>
+		/// <summary>Bindable property for <see cref="MaximumTrackColor"/>. This is a bindable property.</summary>
 		public static readonly BindableProperty MaximumTrackColorProperty = BindableProperty.Create(nameof(MaximumTrackColor), typeof(Color), typeof(Slider), null);
 
-		/// <summary>Bindable property for <see cref="ThumbColor"/>.</summary>
+		/// <summary>Bindable property for <see cref="ThumbColor"/>. This is a bindable property.</summary>
 		public static readonly BindableProperty ThumbColorProperty = BindableProperty.Create(nameof(ThumbColor), typeof(Color), typeof(Slider), null);
 
-		/// <summary>Bindable property for <see cref="ThumbImageSource"/>.</summary>
+		/// <summary>Bindable property for <see cref="ThumbImageSource"/>. This is a bindable property.</summary>
 		public static readonly BindableProperty ThumbImageSourceProperty = BindableProperty.Create(nameof(ThumbImageSource), typeof(ImageSource), typeof(Slider), default(ImageSource));
 
-		/// <summary>Bindable property for <see cref="DragStartedCommand"/>.</summary>
+		/// <summary>Bindable property for <see cref="DragStartedCommand"/>. This is a bindable property.</summary>
 		public static readonly BindableProperty DragStartedCommandProperty = BindableProperty.Create(nameof(DragStartedCommand), typeof(ICommand), typeof(Slider), default(ICommand));
 
-		/// <summary>Bindable property for <see cref="DragCompletedCommand"/>.</summary>
+		/// <summary>Bindable property for <see cref="DragCompletedCommand"/>. This is a bindable property.</summary>
 		public static readonly BindableProperty DragCompletedCommandProperty = BindableProperty.Create(nameof(DragCompletedCommand), typeof(ICommand), typeof(Slider), default(ICommand));
 
 		readonly Lazy<PlatformConfigurationRegistry<Slider>> _platformConfigurationRegistry;
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='.ctor'][1]/Docs/*" />
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Slider"/> class with default minimum (0) and maximum (1) values.
+		/// </summary>
 		public Slider()
 		{
 			_platformConfigurationRegistry = new Lazy<PlatformConfigurationRegistry<Slider>>(() => new PlatformConfigurationRegistry<Slider>(this));
 		}
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='.ctor'][2]/Docs/*" />
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Slider"/> class with specified minimum, maximum, and initial values.
+		/// </summary>
+		/// <param name="min">The minimum value of the slider.</param>
+		/// <param name="max">The maximum value of the slider.</param>
+		/// <param name="val">The initial value of the slider, clamped between <paramref name="min"/> and <paramref name="max"/>.</param>
+		/// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="min"/> is greater than or equal to <paramref name="max"/>.</exception>
 		public Slider(double min, double max, double val) : this()
 		{
 			if (min >= max)
@@ -113,71 +128,120 @@ namespace Microsoft.Maui.Controls
 			Value = val.Clamp(min, max);
 		}
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='MinimumTrackColor']/Docs/*" />
+		/// <summary>
+		/// Gets or sets the color of the filled portion of the slider track (from minimum to current value).
+		/// This is a bindable property.
+		/// </summary>
+		/// <value>The <see cref="Color"/> of the minimum track. The default is <see langword="null"/>, which uses the platform default.</value>
 		public Color MinimumTrackColor
 		{
 			get { return (Color)GetValue(MinimumTrackColorProperty); }
 			set { SetValue(MinimumTrackColorProperty, value); }
 		}
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='MaximumTrackColor']/Docs/*" />
+		/// <summary>
+		/// Gets or sets the color of the unfilled portion of the slider track (from current value to maximum).
+		/// This is a bindable property.
+		/// </summary>
+		/// <value>The <see cref="Color"/> of the maximum track. The default is <see langword="null"/>, which uses the platform default.</value>
 		public Color MaximumTrackColor
 		{
 			get { return (Color)GetValue(MaximumTrackColorProperty); }
 			set { SetValue(MaximumTrackColorProperty, value); }
 		}
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='ThumbColor']/Docs/*" />
+		/// <summary>
+		/// Gets or sets the color of the slider thumb (the draggable element).
+		/// This is a bindable property.
+		/// </summary>
+		/// <value>The <see cref="Color"/> of the thumb. The default is <see langword="null"/>, which uses the platform default.</value>
 		public Color ThumbColor
 		{
 			get { return (Color)GetValue(ThumbColorProperty); }
 			set { SetValue(ThumbColorProperty, value); }
 		}
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='ThumbImageSource']/Docs/*" />
+		/// <summary>
+		/// Gets or sets an <see cref="ImageSource"/> to use as the slider thumb instead of the platform default.
+		/// This is a bindable property.
+		/// </summary>
+		/// <value>The <see cref="ImageSource"/> for the thumb. The default is <see langword="null"/>.</value>
 		public ImageSource ThumbImageSource
 		{
 			get { return (ImageSource)GetValue(ThumbImageSourceProperty); }
 			set { SetValue(ThumbImageSourceProperty, value); }
 		}
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='DragStartedCommand']/Docs/*" />
+		/// <summary>
+		/// Gets or sets the command to execute when the user starts dragging the slider thumb.
+		/// This is a bindable property.
+		/// </summary>
+		/// <value>The <see cref="ICommand"/> to execute. The default is <see langword="null"/>.</value>
 		public ICommand DragStartedCommand
 		{
 			get { return (ICommand)GetValue(DragStartedCommandProperty); }
 			set { SetValue(DragStartedCommandProperty, value); }
 		}
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='DragCompletedCommand']/Docs/*" />
+		/// <summary>
+		/// Gets or sets the command to execute when the user completes dragging the slider thumb.
+		/// This is a bindable property.
+		/// </summary>
+		/// <value>The <see cref="ICommand"/> to execute. The default is <see langword="null"/>.</value>
 		public ICommand DragCompletedCommand
 		{
 			get { return (ICommand)GetValue(DragCompletedCommandProperty); }
 			set { SetValue(DragCompletedCommandProperty, value); }
 		}
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='Maximum']/Docs/*" />
+		/// <summary>
+		/// Gets or sets the maximum value of the slider.
+		/// This is a bindable property.
+		/// </summary>
+		/// <value>The maximum value. The default is 1.</value>
+		/// <remarks>Changing this value will automatically clamp the <see cref="Value"/> to be within the new range.</remarks>
 		public double Maximum
 		{
 			get { return (double)GetValue(MaximumProperty); }
 			set { SetValue(MaximumProperty, value); }
 		}
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='Minimum']/Docs/*" />
+		/// <summary>
+		/// Gets or sets the minimum value of the slider.
+		/// This is a bindable property.
+		/// </summary>
+		/// <value>The minimum value. The default is 0.</value>
+		/// <remarks>Changing this value will automatically clamp the <see cref="Value"/> to be within the new range.</remarks>
 		public double Minimum
 		{
 			get { return (double)GetValue(MinimumProperty); }
 			set { SetValue(MinimumProperty, value); }
 		}
 
-		/// <include file="../../docs/Microsoft.Maui.Controls/Slider.xml" path="//Member[@MemberName='Value']/Docs/*" />
+		/// <summary>
+		/// Gets or sets the current value of the slider.
+		/// This is a bindable property.
+		/// </summary>
+		/// <value>The current value, clamped between <see cref="Minimum"/> and <see cref="Maximum"/>. The default is 0.</value>
 		public double Value
 		{
 			get { return (double)GetValue(ValueProperty); }
 			set { SetValue(ValueProperty, value); }
 		}
 
+		/// <summary>
+		/// Occurs when the <see cref="Value"/> property changes.
+		/// </summary>
 		public event EventHandler<ValueChangedEventArgs> ValueChanged;
+		
+		/// <summary>
+		/// Occurs when the user starts dragging the slider thumb.
+		/// </summary>
 		public event EventHandler DragStarted;
+		
+		/// <summary>
+		/// Occurs when the user completes dragging the slider thumb.
+		/// </summary>
 		public event EventHandler DragCompleted;
 
 		void ISliderController.SendDragStarted()
