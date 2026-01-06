bd7a6d677ee885eb299cb34adf6784a437910cf2
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2026-01-06 17:55:51 +0530

    Material 3 for entry control

--- src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
+++ src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
@@ -70,13 +70,24 @@ public static partial class AppHostBuilderExtensions
 		handlersCollection.AddHandler<CollectionView, CollectionViewHandler>();
 		handlersCollection.AddHandler<CarouselView, CarouselViewHandler>();
 #endif
+#if ANDROID
+		if (RuntimeFeature.IsMaterial3Enabled)
+		{
+			handlersCollection.AddHandler<Entry, MaterialEntryHandler>();
+		}
+		else
+		{
+			handlersCollection.AddHandler<Entry, EntryHandler>();
+		}
+#else
+		handlersCollection.AddHandler<Entry, EntryHandler>();
+#endif
 		handlersCollection.AddHandler<Application, ApplicationHandler>();
 		handlersCollection.AddHandler<ActivityIndicator, ActivityIndicatorHandler>();
 		handlersCollection.AddHandler<BoxView, BoxViewHandler>();
 		handlersCollection.AddHandler<Button, ButtonHandler>();
 		handlersCollection.AddHandler<DatePicker, DatePickerHandler>();
 		handlersCollection.AddHandler<Editor, EditorHandler>();
-		handlersCollection.AddHandler<Entry, EntryHandler>();
 		handlersCollection.AddHandler<GraphicsView, GraphicsViewHandler>();
 		handlersCollection.AddHandler<Image, ImageHandler>();
 		handlersCollection.AddHandler<Layout, LayoutHandler>();
