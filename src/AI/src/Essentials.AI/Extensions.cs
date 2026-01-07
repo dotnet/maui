83d7f33165c4f199501bc1002912923d3f5077a7
Matthew Leibowitz <mattleibow@live.com>
2026-01-08 00:53:05 +0200

    xcode

--- src/AI/src/Essentials.AI/Extensions.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.Extensions.AI;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-namespace Microsoft.Maui.Essentials.AI;
