d343277fd5ffdb8c2f3a1a7b19f4d757d50632a4
Stephane Delcroix <stephane@delcroix.org>
2026-01-14 10:58:52 +0100

    Fix sample projects to use Runtime inflator for deprecated APIs
    - Controls.Sample: Already fixed (uses Runtime inflator)
    - Controls.Sample.Embedding: Set Runtime inflator (nullability issues)
    - Controls.Sample.Profiling: Set Runtime inflator + pragma for XamlCompilation
    - Essentials.Sample: Set Runtime inflator + pragma for XamlCompilation
    - DeviceTests.Runners: Set Runtime inflator (deprecated Frame)
    - DeviceRunner: Add pragma for deprecated XamlCompilation

--- src/Controls/samples/Controls.Sample.Profiling/Startup.cs
+++ src/Controls/samples/Controls.Sample.Profiling/Startup.cs
@@ -3,7 +3,9 @@ using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Controls.Xaml;
 using Microsoft.Maui.Hosting;
 
+#pragma warning disable CS0618 // XamlCompilationAttribute is deprecated, remove this in .NET 12
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+#pragma warning restore CS0618
 
 namespace Maui.Controls.Sample.Profiling
 {
