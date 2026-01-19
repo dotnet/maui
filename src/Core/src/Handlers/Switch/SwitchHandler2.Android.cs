1ac0c3f11a369733d257896f5ed5c98c1e4c41f5
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2026-01-19 19:36:22 +0530

    Updated SwitchHandler and SwitchHandler2

--- src/Core/src/Handlers/Switch/SwitchHandler2.Android.cs
+++ src/Core/src/Handlers/Switch/SwitchHandler2.Android.cs
@@ -1,25 +1,20 @@
-using System;
-using Android.Widget;
 using Google.Android.Material.MaterialSwitch;
-using Microsoft.Maui.Graphics;
 
 namespace Microsoft.Maui.Handlers;
 
 // TODO: material3 - make it public in .net 11
-internal partial class SwitchHandler2 : ViewHandler<ISwitch, MaterialSwitch>
+internal class SwitchHandler2 : SwitchHandler
 {
-	public static PropertyMapper<ISwitch, SwitchHandler2> Mapper =
-		new(ViewMapper)
-		{
-			[nameof(ISwitch.IsOn)] = MapIsOn,
-			[nameof(ISwitch.TrackColor)] = MapTrackColor,
-			[nameof(ISwitch.ThumbColor)] = MapThumbColor,
-		};
+	public static new PropertyMapper<ISwitch, SwitchHandler2> Mapper =
+  new(ViewMapper)
+  {
+	  [nameof(ISwitch.IsOn)] = MapIsOn,
+	  [nameof(ISwitch.TrackColor)] = MapTrackColor,
+	  [nameof(ISwitch.ThumbColor)] = MapThumbColor,
+  };
 
-	public static CommandMapper<ISwitch, SwitchHandler2> CommandMapper =
-		new(ViewCommandMapper);
-
-	MaterialSwitchCheckedChangeListener? _changeListener;
+	public static new CommandMapper<ISwitch, SwitchHandler2> CommandMapper =
+	 new(ViewCommandMapper);
 
 	public SwitchHandler2() : base(Mapper, CommandMapper)
 	{
@@ -30,78 +25,18 @@ internal partial class SwitchHandler2 : ViewHandler<ISwitch, MaterialSwitch>
 		return new MaterialSwitch(MauiMaterialContextThemeWrapper.Create(Context));
 	}
 
-	protected override void ConnectHandler(MaterialSwitch platformView)
+	public static new void MapIsOn(ISwitchHandler handler, ISwitch view)
 	{
-		_changeListener = new MaterialSwitchCheckedChangeListener(this);
-		platformView.SetOnCheckedChangeListener(_changeListener);
-
-		base.ConnectHandler(platformView);
+		(handler.PlatformView as MaterialSwitch)?.UpdateIsOn(view);
 	}
 
-	protected override void DisconnectHandler(MaterialSwitch platformView)
+	public static new void MapTrackColor(ISwitchHandler handler, ISwitch view)
 	{
-		platformView.SetOnCheckedChangeListener(null);
-		_changeListener?.Dispose();
-		_changeListener = null;
-
-		base.DisconnectHandler(platformView);
+		(handler.PlatformView as MaterialSwitch)?.UpdateTrackColor(view);
 	}
 
-	public override Size GetDesiredSize(double widthConstraint, double heightConstraint)
+	public static new void MapThumbColor(ISwitchHandler handler, ISwitch view)
 	{
-		Size size = base.GetDesiredSize(widthConstraint, heightConstraint);
-
-		if (size.Width == 0)
-		{
-			int width = (int)widthConstraint;
-
-			if (widthConstraint <= 0)
-				width = (int)(Context?.GetThemeAttributeDp(global::Android.Resource.Attribute.SwitchMinWidth) ?? 0);
-
-			size = new Size(width, size.Height);
-		}
-
-		return size;
-	}
-
-	public static void MapIsOn(SwitchHandler2 handler, ISwitch view)
-	{
-		handler.PlatformView?.UpdateIsOn(view);
-	}
-
-	public static void MapTrackColor(SwitchHandler2 handler, ISwitch view)
-	{
-		handler.PlatformView?.UpdateTrackColor(view);
-	}
-
-	public static void MapThumbColor(SwitchHandler2 handler, ISwitch view)
-	{
-		handler.PlatformView?.UpdateThumbColor(view);
-	}
-
-	void OnCheckedChanged(bool isOn)
-	{
-		if (VirtualView is null || VirtualView.IsOn == isOn)
-			return;
-
-		VirtualView.IsOn = isOn;
-	}
-
-	sealed class MaterialSwitchCheckedChangeListener : Java.Lang.Object, CompoundButton.IOnCheckedChangeListener
-	{
-		readonly WeakReference<SwitchHandler2> _handler;
-
-		public MaterialSwitchCheckedChangeListener(SwitchHandler2 handler)
-		{
-			_handler = new WeakReference<SwitchHandler2>(handler);
-		}
-
-		void CompoundButton.IOnCheckedChangeListener.OnCheckedChanged(CompoundButton? buttonView, bool isToggled)
-		{
-			if (_handler.TryGetTarget(out var handler))
-			{
-				handler.OnCheckedChanged(isToggled);
-			}
-		}
+		(handler.PlatformView as MaterialSwitch)?.UpdateThumbColor(view);
 	}
 }
