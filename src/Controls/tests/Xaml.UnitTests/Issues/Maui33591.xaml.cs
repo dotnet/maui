6adda8d75156a65d311bb627a3a6c220aa538120
Simon Rozsival <simon@rozsival.com>
2026-01-20 13:30:22 +0100

    Fix build errors: add missing EventTrigger resource strings and analyzer release tracking

--- src/Controls/tests/Xaml.UnitTests/Issues/Maui33591.xaml.cs
+++ src/Controls/tests/Xaml.UnitTests/Issues/Maui33591.xaml.cs
@@ -1,12 +1,10 @@
-using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Maui.Graphics;
 using NUnit.Framework;
 
 namespace Microsoft.Maui.Controls.Xaml.UnitTests;
 
 /// <summary>
-/// Simple test TriggerAction for use in Maui33591 tests.
+/// Simple test TriggerAction that tracks invocation count.
 /// </summary>
 public class Maui33591TestTriggerAction : TriggerAction<VisualElement>
 {
@@ -28,63 +26,33 @@ public partial class Maui33591 : ContentPage
 	[TestFixture]
 	class Tests
 	{
-		/// <summary>
-		/// Test that XAML page creates elements correctly across all inflators.
-		/// </summary>
 		[Test]
-		public void PageCreatesSuccessfully([Values] XamlInflator inflator)
+		public void EventTriggerHasEventPropertySet([Values] XamlInflator inflator)
 		{
 			var page = new Maui33591(inflator);
-			Assert.That(page, Is.Not.Null);
-			Assert.That(page.Title, Is.EqualTo("EventTrigger AOT-Safe Test"));
+
+			var trigger = page.TestButton!.Triggers.OfType<EventTrigger>().Single();
+			Assert.That(trigger.Event, Is.EqualTo("Clicked"));
+			Assert.That(trigger.Actions.Count, Is.EqualTo(1));
 		}
 
-		/// <summary>
-		/// Test that all named controls are created.
-		/// </summary>
 		[Test]
-		public void ControlsAreCreated([Values] XamlInflator inflator)
-		{
-			var page = new Maui33591(inflator);
-			
-			Assert.That(page.ClickButton, Is.Not.Null);
-			Assert.That(page.ClickButton!.Text, Is.EqualTo("Click Me"));
-			Assert.That(page.ClickButton.BackgroundColor, Is.EqualTo(Colors.Blue));
-
-			Assert.That(page.TextEntry, Is.Not.Null);
-			Assert.That(page.TextEntry!.Placeholder, Is.EqualTo("Type something"));
-
-			Assert.That(page.TestSwitch, Is.Not.Null);
-			Assert.That(page.TestSwitch!.IsToggled, Is.False);
-
-			Assert.That(page.StatusLabel, Is.Not.Null);
-			Assert.That(page.StatusLabel!.Text, Is.EqualTo("Ready"));
-		}
-
-		/// <summary>
-		/// Test that EventTriggers are created with correct Event property for all inflators.
-		/// This validates backward compatibility - the Event property should be set regardless
-		/// of whether the inflator uses reflection or AOT-safe factory methods.
-		/// </summary>
-		[Test]
-		public void EventTriggersHaveEventPropertySet([Values] XamlInflator inflator)
+		public void EventTriggerFiresWhenEventOccurs([Values] XamlInflator inflator)
 		{
 			var page = new Maui33591(inflator);
 
-			// Button with Clicked EventTrigger
-			var buttonTrigger = page.ClickButton!.Triggers.OfType<EventTrigger>().Single();
-			Assert.That(buttonTrigger.Event, Is.EqualTo("Clicked"), "Button EventTrigger.Event should be 'Clicked'");
-			Assert.That(buttonTrigger.Actions.Count, Is.EqualTo(1), "Button EventTrigger should have 1 action");
+			var trigger = page.TestButton!.Triggers.OfType<EventTrigger>().Single();
+			var action = (Maui33591TestTriggerAction)trigger.Actions[0];
 
-			// Entry with TextChanged EventTrigger
-			var entryTrigger = page.TextEntry!.Triggers.OfType<EventTrigger>().Single();
-			Assert.That(entryTrigger.Event, Is.EqualTo("TextChanged"), "Entry EventTrigger.Event should be 'TextChanged'");
-			Assert.That(entryTrigger.Actions.Count, Is.EqualTo(1), "Entry EventTrigger should have 1 action");
+			Assert.That(action.InvokeCount, Is.EqualTo(0), "Action should not be invoked yet");
 
-			// Switch with Toggled EventTrigger
-			var switchTrigger = page.TestSwitch!.Triggers.OfType<EventTrigger>().Single();
-			Assert.That(switchTrigger.Event, Is.EqualTo("Toggled"), "Switch EventTrigger.Event should be 'Toggled'");
-			Assert.That(switchTrigger.Actions.Count, Is.EqualTo(1), "Switch EventTrigger should have 1 action");
+			page.TestButton.SendClicked();
+
+			Assert.That(action.InvokeCount, Is.EqualTo(1), "Action should be invoked once after click");
+
+			page.TestButton.SendClicked();
+
+			Assert.That(action.InvokeCount, Is.EqualTo(2), "Action should be invoked twice after second click");
 		}
 	}
 }
