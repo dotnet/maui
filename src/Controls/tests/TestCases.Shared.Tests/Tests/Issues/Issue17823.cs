4f191bbdf8f0e48c7532067d1a32adca97952122
Jakub Florkowski <kubaflo123@gmail.com>
2026-01-22 12:09:06 +0100

    Improved the UiTest

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17823.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue17823.cs
@@ -1,4 +1,3 @@
-using System;
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
@@ -15,16 +14,50 @@ public class Issue17823 : _IssuesUITest
 
 	[Test]
 	[Category(UITestCategories.CollectionView)]
-	public void ReorderLastItemWithHeaderPlacesItemAtStart()
+	public void ReorderingItemToEndWithHeaderDoesNotCrash()
 	{
-		Assert.That(App.WaitForTextToBePresentInElement("ReorderStatusLabel", "Item 1, Item 2, Item 3, Item 4", TimeSpan.FromSeconds(3)), Is.True);
-
+		// Verify header is present
 		App.WaitForElement("HeaderLabel");
-		App.WaitForElement("ReorderItem3");
-		App.WaitForElement("ReorderItem0");
 
+		// Verify all items are present
+		App.WaitForElement("ReorderItem0");
+		App.WaitForElement("ReorderItem3");
+
+		// Verify initial state
+		var initialText = App.FindElement("ReorderStatusLabel").GetText();
+		Assert.That(initialText, Is.EqualTo("Item 1, Item 2, Item 3, Item 4"));
+
+		// The bug: dragging first item to the end would crash with header present
+		// This is because adapter indices include header (0=header, 1=item0, 2=item1, etc.)
+		// but item source indices don't (0=item0, 1=item1, etc.)
+		App.DragAndDrop("ReorderItem0", "ReorderItem3");
+
+		// Verify reorder succeeded without crash by checking status label updated
+		var afterDrag = App.FindElement("ReorderStatusLabel").GetText();
+		Assert.That(afterDrag, Is.EqualTo("Item 2, Item 3, Item 4, Item 1"));
+	}
+
+	[Test]
+	[Category(UITestCategories.CollectionView)]
+	public void ReorderingLastItemWithHeaderDoesNotCrash()
+	{
+		// Verify header is present
+		App.WaitForElement("HeaderLabel");
+
+		// Verify items are present
+		App.WaitForElement("ReorderItem0");
+		App.WaitForElement("ReorderItem3");
+
+		// Verify initial state
+		var initialText = App.FindElement("ReorderStatusLabel").GetText();
+		Assert.That(initialText, Is.EqualTo("Item 1, Item 2, Item 3, Item 4"));
+
+		// Test dragging last item to first position
+		// This tests the opposite direction
 		App.DragAndDrop("ReorderItem3", "ReorderItem0");
 
-		Assert.That(App.WaitForTextToBePresentInElement("ReorderStatusLabel", "Item 4, Item 1, Item 2, Item 3", TimeSpan.FromSeconds(5)), Is.True);
+		// Verify reorder succeeded without crash
+		var afterDrag = App.FindElement("ReorderStatusLabel").GetText();
+		Assert.That(afterDrag, Is.EqualTo("Item 4, Item 1, Item 2, Item 3"));
 	}
 }
