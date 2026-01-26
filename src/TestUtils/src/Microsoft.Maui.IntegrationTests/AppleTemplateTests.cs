dfea72a2a943c14b40afbf466c93adaca25de584
Milos Kotlar <kotlarmilos@gmail.com>
2026-01-26 16:15:27 +0100

    Update test attributes from [Test] to [Fact]

--- src/TestUtils/src/Microsoft.Maui.IntegrationTests/AppleTemplateTests.cs
+++ src/TestUtils/src/Microsoft.Maui.IntegrationTests/AppleTemplateTests.cs
@@ -77,19 +77,19 @@ namespace Microsoft.Maui.IntegrationTests
 		public void RunOniOS_BlazorRelease() => RunOniOS("maui-blazor", "Release", DotNetCurrent, RuntimeVariant.Mono, null);
 
 		// CoreCLR test variants
-		[Test]
+		[Fact]
 		public void RunOniOS_MauiDebug_CoreCLR() => RunOniOS("maui", "Debug", DotNetCurrent, RuntimeVariant.CoreCLR, null);
 
-		[Test]
+		[Fact]
 		public void RunOniOS_MauiRelease_CoreCLR() => RunOniOS("maui", "Release", DotNetCurrent, RuntimeVariant.CoreCLR, null);
 
-		[Test]
+		[Fact]
 		public void RunOniOS_MauiReleaseTrimFull_CoreCLR() => RunOniOS("maui", "Release", DotNetCurrent, RuntimeVariant.CoreCLR, "full");
 
-		[Test]
+		[Fact]
 		public void RunOniOS_BlazorDebug_CoreCLR() => RunOniOS("maui-blazor", "Debug", DotNetCurrent, RuntimeVariant.CoreCLR, null);
 
-		[Test]
+		[Fact]
 		public void RunOniOS_BlazorRelease_CoreCLR() => RunOniOS("maui-blazor", "Release", DotNetCurrent, RuntimeVariant.CoreCLR, null);
 
 		// TODO: Re-enable once ASP.NET Core fixes trimmer warning IL2111 with Blazor Router.NotFoundPage
