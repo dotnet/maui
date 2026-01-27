d94c8f92348328cecb043a4639c408a00d8fe10e
Shane Neuville <shneuvil@microsoft.com>
2026-01-27 14:30:17 -0600

    Add retryTimeout to Issue10563 SwipeView screenshot tests

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10563.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10563.cs
@@ -31,7 +31,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.Tap(OpenLeftId);
 			// Wait for swipe animation to complete - the SwipeItem text becomes visible
 			App.WaitForElement("Issue 10563");
-			VerifyScreenshotOrSetException(ref exception, "Left_SwipeItems");
+			VerifyScreenshotOrSetException(ref exception, "Left_SwipeItems", retryTimeout: TimeSpan.FromSeconds(2));
 			App.Tap(CloseId);
 			// Wait for close animation to complete - the SwipeItem text disappears
 			App.WaitForNoElement("Issue 10563");
@@ -40,7 +40,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement(OpenRightId);
 			App.Tap(OpenRightId);
 			App.WaitForElement("Issue 10563");
-			VerifyScreenshotOrSetException(ref exception, "Right_SwipeItems");
+			VerifyScreenshotOrSetException(ref exception, "Right_SwipeItems", retryTimeout: TimeSpan.FromSeconds(2));
 			App.Tap(CloseId);
 			App.WaitForNoElement("Issue 10563");
 
@@ -48,7 +48,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement(OpenTopId);
 			App.Tap(OpenTopId);
 			App.WaitForElement("Issue 10563");
-			VerifyScreenshotOrSetException(ref exception, "Top_SwipeItems");
+			VerifyScreenshotOrSetException(ref exception, "Top_SwipeItems", retryTimeout: TimeSpan.FromSeconds(2));
 			App.Tap(CloseId);
 			App.WaitForNoElement("Issue 10563");
 
@@ -56,7 +56,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement(OpenBottomId);
 			App.Tap(OpenBottomId);
 			App.WaitForElement("Issue 10563");
-			VerifyScreenshotOrSetException(ref exception, "Bottom_SwipeItems");
+			VerifyScreenshotOrSetException(ref exception, "Bottom_SwipeItems", retryTimeout: TimeSpan.FromSeconds(2));
 			App.Tap(CloseId);
 			App.WaitForNoElement("Issue 10563");
 
