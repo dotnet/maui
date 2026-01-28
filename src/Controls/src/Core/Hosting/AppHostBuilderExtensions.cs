c7d1b377e2b0ecfdd90454acb47ce2296ac7e1ca
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2026-01-28 18:13:10 +0530

    Addressed review concern.

--- src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
+++ src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
@@ -130,7 +130,7 @@ public static partial class AppHostBuilderExtensions
 		}
 
 #if ANDROID
-		if(RuntimeFeature.IsMaterial3Enabled)
+		if (RuntimeFeature.IsMaterial3Enabled)
 		{
 			handlersCollection.AddHandler<DatePicker, DatePickerHandler2>();
 		}
