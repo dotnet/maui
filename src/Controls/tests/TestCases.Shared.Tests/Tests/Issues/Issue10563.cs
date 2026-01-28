845706a3beaef6adcb70b50813aeeb45934aab2a
Shane Neuville <shneuvil@microsoft.com>
2026-01-28 16:26:41 -0600

    Issue10563: Use flat button background to prevent animation artifacts
    Set Background = SolidColorBrush(Colors.LightGray) on all buttons
    to prevent ripple animation that causes visual differences on CI.
    
    Note: Baselines will need regeneration on CI with this change.

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10563.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10563.cs
@@ -31,8 +31,8 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.Tap(OpenLeftId);
 			// Wait for swipe animation to complete - the SwipeItem text becomes visible
 			App.WaitForElement("Issue 10563");
-			// SwipeView animations can have timing variance - use retryTimeout and small tolerance
-			VerifyScreenshotOrSetException(ref exception, "Left_SwipeItems", tolerance: 1.0, retryTimeout: TimeSpan.FromSeconds(3));
+			// SwipeView animations can have timing variance - use retryTimeout
+			VerifyScreenshotOrSetException(ref exception, "Left_SwipeItems", retryTimeout: TimeSpan.FromSeconds(3));
 			App.Tap(CloseId);
 			// Wait for close animation to complete - the SwipeItem text disappears
 			App.WaitForNoElement("Issue 10563");
@@ -41,7 +41,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement(OpenRightId);
 			App.Tap(OpenRightId);
 			App.WaitForElement("Issue 10563");
-			VerifyScreenshotOrSetException(ref exception, "Right_SwipeItems", tolerance: 1.0, retryTimeout: TimeSpan.FromSeconds(3));
+			VerifyScreenshotOrSetException(ref exception, "Right_SwipeItems", retryTimeout: TimeSpan.FromSeconds(3));
 			App.Tap(CloseId);
 			App.WaitForNoElement("Issue 10563");
 
@@ -49,7 +49,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement(OpenTopId);
 			App.Tap(OpenTopId);
 			App.WaitForElement("Issue 10563");
-			VerifyScreenshotOrSetException(ref exception, "Top_SwipeItems", tolerance: 1.0, retryTimeout: TimeSpan.FromSeconds(3));
+			VerifyScreenshotOrSetException(ref exception, "Top_SwipeItems", retryTimeout: TimeSpan.FromSeconds(3));
 			App.Tap(CloseId);
 			App.WaitForNoElement("Issue 10563");
 
@@ -57,7 +57,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 			App.WaitForElement(OpenBottomId);
 			App.Tap(OpenBottomId);
 			App.WaitForElement("Issue 10563");
-			VerifyScreenshotOrSetException(ref exception, "Bottom_SwipeItems", tolerance: 1.0, retryTimeout: TimeSpan.FromSeconds(3));
+			VerifyScreenshotOrSetException(ref exception, "Bottom_SwipeItems", retryTimeout: TimeSpan.FromSeconds(3));
 			App.Tap(CloseId);
 			App.WaitForNoElement("Issue 10563");
 
