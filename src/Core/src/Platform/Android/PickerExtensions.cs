ec2aa4db92934d20cde05daf5fb23519e3790e76
SyedAbdulAzeem <syedabdulazeem.a@syncfusion.com>
2026-01-29 13:11:23 +0530

    Introduced a common base method for Material 2 and Material 3; cleaned up pragma warnings

--- src/Core/src/Platform/Android/PickerExtensions.cs
+++ src/Core/src/Platform/Android/PickerExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content.Res;
+using AndroidX.AppCompat.Widget;
 using AppCompatAlertDialog = AndroidX.AppCompat.App.AlertDialog;
 
 namespace Microsoft.Maui.Platform
@@ -11,15 +12,7 @@ namespace Microsoft.Maui.Platform
 
 		public static void UpdateTitleColor(this MauiPicker platformPicker, IPicker picker)
 		{
-			var titleColor = picker.TitleColor;
-
-			if (titleColor != null)
-			{
-				if (PlatformInterop.CreateEditTextColorStateList(platformPicker.TextColors, titleColor.ToPlatform()) is ColorStateList c)
-					platformPicker.SetHintTextColor(c);
-				else if (picker.TextColor == picker.TitleColor)
-					platformPicker.SetHintTextColor(titleColor.ToPlatform());
-			}
+			platformPicker.UpdateTitleColorCore(picker);
 		}
 
 		public static void UpdateTextColor(this MauiPicker platformPicker, IPicker picker, ColorStateList? defaultColor)
@@ -42,30 +35,12 @@ namespace Microsoft.Maui.Platform
 
 		internal static void UpdatePicker(this MauiPicker platformPicker, IPicker picker)
 		{
-			platformPicker.Hint = picker.Title;
-
-			if (picker.SelectedIndex == -1 || picker.SelectedIndex >= picker.GetCount())
-				platformPicker.Text = null;
-			else
-				platformPicker.Text = picker.GetItem(picker.SelectedIndex);
+			platformPicker.UpdatePickerCore(picker);
 		}
 
 		internal static void UpdateFlowDirection(this AppCompatAlertDialog alertDialog, MauiPicker platformPicker)
 		{
-			var platformLayoutDirection = platformPicker.LayoutDirection;
-
-			// Propagate the MauiPicker LayoutDirection to the AlertDialog
-			var dv = alertDialog.Window?.DecorView;
-
-			dv?.LayoutDirection = platformLayoutDirection;
-
-			var lv = alertDialog?.ListView;
-
-			if (lv is not null)
-			{
-				lv.LayoutDirection = platformLayoutDirection;
-				lv.TextDirection = platformLayoutDirection.ToTextDirection();
-			}
+			alertDialog.UpdateFlowDirectionCore(platformPicker);
 		}
 
 		// TODO: Material3 - make it public in .net 11
@@ -76,6 +51,28 @@ namespace Microsoft.Maui.Platform
 
 		// TODO: Material3 - make it public in .net 11
 		internal static void UpdateTitleColor(this MauiMaterialPicker platformPicker, IPicker picker)
+		{
+			platformPicker.UpdateTitleColorCore(picker);
+		}
+
+		// TODO: Material3 - make it public in .net 11
+		internal static void UpdateSelectedIndex(this MauiMaterialPicker platformPicker, IPicker picker)
+		{
+			UpdatePicker(platformPicker, picker);
+		}
+
+		internal static void UpdatePicker(this MauiMaterialPicker platformPicker, IPicker picker)
+		{
+			platformPicker.UpdatePickerCore(picker);
+		}
+
+		internal static void UpdateFlowDirection(this AppCompatAlertDialog alertDialog, MauiMaterialPicker platformPicker)
+		{
+			alertDialog.UpdateFlowDirectionCore(platformPicker);
+		}
+
+		// TODO: Material3 - make it public in .net 11
+		internal static void UpdateTitleColorCore(this AppCompatEditText platformPicker, IPicker picker)
 		{
 			var titleColor = picker.TitleColor;
 
@@ -93,12 +90,7 @@ namespace Microsoft.Maui.Platform
 		}
 
 		// TODO: Material3 - make it public in .net 11
-		internal static void UpdateSelectedIndex(this MauiMaterialPicker platformPicker, IPicker picker)
-		{
-			UpdatePicker(platformPicker, picker);
-		}
-
-		internal static void UpdatePicker(this MauiMaterialPicker platformPicker, IPicker picker)
+		internal static void UpdatePickerCore(this AppCompatEditText platformPicker, IPicker picker)
 		{
 			platformPicker.Hint = picker.Title;
 
@@ -112,11 +104,12 @@ namespace Microsoft.Maui.Platform
 			}
 		}
 
-		internal static void UpdateFlowDirection(this AppCompatAlertDialog alertDialog, MauiMaterialPicker platformPicker)
+		// TODO: Material3 - make it public in .net 11
+		internal static void UpdateFlowDirectionCore(this AppCompatAlertDialog alertDialog, AppCompatEditText platformPicker)
 		{
 			var platformLayoutDirection = platformPicker.LayoutDirection;
 
-			// Propagate the MauiMaterialPicker LayoutDirection to the AlertDialog
+			// Propagate the LayoutDirection to the AlertDialog
 			var dv = alertDialog.Window?.DecorView;
 
 			dv?.LayoutDirection = platformLayoutDirection;
