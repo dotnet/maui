8de0481e4b86882ac5e146ef31c48099f42e1b9d
Shane Neuville <shneuvil@microsoft.com>
2026-01-28 21:56:29 -0600

    Remove changes covered by other PRs (#33737, #33689)
    - Issue18751/18896 local images: covered by #33737
    - Issue12134 timeout: covered by #33689 (uses native CookieManager)

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18896.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue18896.cs
@@ -1,31 +1,40 @@
+#if TEST_FAILS_ON_ANDROID && TEST_FAILS_ON_IOS && TEST_FAILS_ON_CATALYST && TEST_FAILS_ON_WINDOWS//Related issues : https://github.com/dotnet/maui/issues/33507, https://github.com/dotnet/maui/issues/18811, https://github.com/dotnet/maui/issues/15994
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues;
-
-public class Issue18896 : _IssuesUITest
+namespace Microsoft.Maui.TestCases.Tests.Issues
 {
-	const string ListView = "TestListView";
-
-	public Issue18896(TestDevice device) : base(device)
+	public class Issue18896 : _IssuesUITest
 	{
-	}
+		const string ListView = "TestListView";
 
-	public override string Issue => "Can scroll ListView inside RefreshView";
+		public Issue18896(TestDevice device) : base(device)
+		{
+		}
 
-	[Test]
-	[Category(UITestCategories.ListView)]
-	public void Issue18896Test()
-	{
-		App.WaitForElement("WaitForStubControl");
+		public override string Issue => "Can scroll ListView inside RefreshView";
 
-		App.ScrollDown(ListView);
+		[Test]
+		[Category(UITestCategories.ListView)]
+		[FailsOnIOSWhenRunningOnXamarinUITest("Currently fails on iOS; see https://github.com/dotnet/maui/issues/18811")]
+		[FailsOnMacWhenRunningOnXamarinUITest("Currently fails on Catalyst; see https://github.com/dotnet/maui/issues/18811")]
+		[FailsOnWindowsWhenRunningOnXamarinUITest("Currently fails on Windows; see https://github.com/dotnet/maui/issues/15994")]
+		public void Issue18896Test()
+		{
+			VerifyInternetConnectivity();
+			App.WaitForElement("WaitForStubControl");
 
-		App.ScrollUp(ListView);
+			App.ScrollDown(ListView);
 
-		// The test passes if you are able to see the image, name, and location of each monkey.
-		// Use retryTimeout to wait for images to load and scrollbar to hide
-		VerifyScreenshot(retryTimeout: TimeSpan.FromSeconds(2));
+			App.ScrollUp(ListView);
+
+			// Load images and hide scrollbar.
+			Thread.Sleep(2000);
+
+			// The test passes if you are able to see the image, name, and location of each monkey.
+			VerifyScreenshot(retryDelay: TimeSpan.FromSeconds(2));
+		}
 	}
 }
+#endif
