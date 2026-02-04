bcf585ab8772428f2cbb273d2f75cc429169e0c4
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2026-02-04 12:30:15 +0530

    Added test cases

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue33783.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue33783 : _IssuesUITest
+{
+	public Issue33783(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Switch ThumbColor not applied correctly when theme changes on iOS";
+
+	[Test, Order(0)]
+	[Category(UITestCategories.Switch)]
+	public void VerifySwitchThumbColorOnDarkThemeChange()
+	{
+		App.WaitForElement("DarkThemeButton");
+		App.Tap("DarkThemeButton");
+		VerifyScreenshot("Issue33783_SwitchThumbColor_DarkTheme");
+	}
+
+	[Test, Order(1)]
+	[Category(UITestCategories.Switch)]
+	public void VerifySwitchThumbColorOnLightThemeChange()
+	{
+		App.WaitForElement("LightThemeButton");
+		App.Tap("LightThemeButton");
+		VerifyScreenshot("Issue33783_SwitchThumbColor_LightTheme");
+	}
+}
