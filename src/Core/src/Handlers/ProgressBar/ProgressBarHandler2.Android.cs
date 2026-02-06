889481ca937c4f29ca51301c4072f2b4c49e544f
SyedAbdulAzeem <syedabdulazeem.a@syncfusion.com>
2026-02-06 15:46:56 +0530

    Simplified handler and managed progress color separately for Material2 and Material3

--- src/Core/src/Handlers/ProgressBar/ProgressBarHandler2.Android.cs
+++ src/Core/src/Handlers/ProgressBar/ProgressBarHandler2.Android.cs
@@ -1,48 +1,16 @@
+using Google.Android.Material.ProgressIndicator;
+
 namespace Microsoft.Maui.Handlers;
 
 // TODO: Material3 - make it public in .net 11
-internal class ProgressBarHandler2 : ViewHandler<IProgress, MaterialProgressBar>
+internal class ProgressBarHandler2 : ProgressBarHandler
 {
-	public static PropertyMapper<IProgress, ProgressBarHandler2> Mapper =
-		new(ViewMapper)
-		{
-			[nameof(IProgress.Progress)] = MapProgress,
-			[nameof(IProgress.ProgressColor)] = MapProgressColor,
-		};
-
-	public static CommandMapper<IProgress, ProgressBarHandler2> CommandMapper =
-		new(ViewCommandMapper);
-
-	public ProgressBarHandler2() : base(Mapper, CommandMapper)
+	protected override LinearProgressIndicator CreatePlatformView()
 	{
-	}
-
-	protected override MaterialProgressBar CreatePlatformView()
-	{
-		return new MaterialProgressBar(Context)
+		return new LinearProgressIndicator(MauiMaterialContextThemeWrapper.Create(Context))
 		{
 			Indeterminate = false,
 			Max = ProgressBarExtensions.Maximum
 		};
-	}
-
-	protected override void ConnectHandler(MaterialProgressBar platformView)
-	{
-		base.ConnectHandler(platformView);
-	}
-
-	protected override void DisconnectHandler(MaterialProgressBar platformView)
-	{
-		base.DisconnectHandler(platformView);
-	}
-
-	public static void MapProgress(ProgressBarHandler2 handler, IProgress progress)
-	{
-		handler.PlatformView?.UpdateProgress(progress);
-	}
-
-	public static void MapProgressColor(ProgressBarHandler2 handler, IProgress progress)
-	{
-		handler.PlatformView?.UpdateProgressColor(progress);
 	}
 }
