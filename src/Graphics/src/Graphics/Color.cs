ec6c23c36be99f007bf15cffefa766c24804535e
Stephane Delcroix <stephane@delcroix.org>
2026-02-09 12:06:00 +0100

    Remove 'virtual' from Color.Equals(Color)

--- src/Graphics/src/Graphics/Color.cs
+++ src/Graphics/src/Graphics/Color.cs
@@ -128,7 +128,7 @@ namespace Microsoft.Maui.Graphics
 		/// <summary>
 		/// Determines whether the specified <see cref="Color"/> is equal to the current color using byte-precision comparison.
 		/// </summary>
-		public virtual bool Equals(Color other)
+		public bool Equals(Color other)
 		{
 			if (other is null)
 				return false;
