d0a06f7a9ffa182459b48b84aef8c08e7069f739
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2026-02-11 16:39:34 +0530

    removed unwanted changes

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -2,22 +2,8 @@
 
 public partial class MainPage : ContentPage
 {
-	public List<string> Pages { get; set; }
-	public string SelectedPage { get; set; }
-
 	public MainPage()
 	{
-		BindingContext = this;
-
-		Pages = new List<string>
-		{
-			"Page 1",
-			"Page 2",
-			"Page 3"
-		};
-
-		SelectedPage = Pages[0];
-
 		InitializeComponent();
 	}
 }
