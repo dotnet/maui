156ebc90e42d8a9750e83a47381b03484e95182f
Subhiksha Chandrasekaran <subhiksha.c@syncfusion.com>
2026-02-12 16:19:22 +0530

    Fix for  progress bar 26 version

--- src/Core/src/Handlers/ProgressBar/ProgressBarHandler.iOS.cs
+++ src/Core/src/Handlers/ProgressBar/ProgressBarHandler.iOS.cs
@@ -1,4 +1,6 @@
-﻿using ObjCRuntime;
+﻿using System;
+using CoreGraphics;
+using ObjCRuntime;
 using UIKit;
 
 namespace Microsoft.Maui.Handlers
@@ -18,6 +20,49 @@ namespace Microsoft.Maui.Handlers
 		public static void MapProgressColor(IProgressBarHandler handler, IProgress progress)
 		{
 			handler.PlatformView?.UpdateProgressColor(progress);
+		}
+
+		internal static void MapFlowDirection(IProgressBarHandler handler, IProgress progress)
+		{
+			var progressbar = handler.PlatformView;
+			UISemanticContentAttribute contentAttribute = GetSemanticContentAttribute(progress);
+			progressbar.SemanticContentAttribute = contentAttribute;
+
+			// On iOS 26, UIProgressView no longer applies the SemanticContentAttribute to its internal subviews, so update
+			// each subview explicitly to keep flow direction consistent.
+			if (OperatingSystem.IsIOSVersionAtLeast(26))
+			{
+				foreach (var subview in progressbar.Subviews)
+				{
+					subview.SemanticContentAttribute = contentAttribute;
+				}
+			}
+		}
+
+		static UISemanticContentAttribute GetSemanticContentAttribute(IProgress progress)
+		{
+			return progress.FlowDirection switch
+			{
+				FlowDirection.RightToLeft => UISemanticContentAttribute.ForceRightToLeft,
+				FlowDirection.LeftToRight => UISemanticContentAttribute.ForceLeftToRight,
+				_ => GetParentSemanticContentAttribute(progress)
+			};
+		}
+
+		static UISemanticContentAttribute GetParentSemanticContentAttribute(IProgress progress)
+		{
+			var parentView = (progress as IView)?.Parent as IView;
+			if (parentView is null)
+			{
+				return UISemanticContentAttribute.Unspecified;
+			}
+
+			return parentView.FlowDirection switch
+			{
+				FlowDirection.LeftToRight => UISemanticContentAttribute.ForceLeftToRight,
+				FlowDirection.RightToLeft => UISemanticContentAttribute.ForceRightToLeft,
+				_ => UISemanticContentAttribute.Unspecified,
+			};
 		}
 	}
 }
