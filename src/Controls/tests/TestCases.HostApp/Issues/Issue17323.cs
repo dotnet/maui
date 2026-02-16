3eba71ba14f8bb10949a354ca4f9b815652fc1d8
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2026-02-16 12:57:49 +0530

    Modified the test

--- src/Controls/tests/TestCases.HostApp/Issues/Issue17323.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue17323.cs
@@ -1,6 +1,6 @@
 ﻿namespace Maui.Controls.Sample.Issues;
 
-[Issue(IssueTracker.Github, 17323, "Arabic text flows RTL on Android in MAUI, but flows LTR on Windows", PlatformAffected.UWP)]
+[Issue(IssueTracker.Github, 17323, "Arabic text flows RTL on Android in MAUI, but flows LTR on Windows", PlatformAffected.Android)]
 public class Issue17323 : TestContentPage
 {
 	protected override void Init()
@@ -34,7 +34,7 @@ public class Issue17323_Drawable : IDrawable
 		canvas.StrokeSize = 5;
 		canvas.FontSize = 18;
 		canvas.DrawRectangle(dirtyRect);
-		canvas.DrawString("Hello World", dirtyRect.X + 5, dirtyRect.Y + 5 , dirtyRect.Width - 10, dirtyRect.Height - 160, HorizontalAlignment.Left, VerticalAlignment.Center);
+		canvas.DrawString("Hello World", dirtyRect.X + 5, dirtyRect.Y + 5, dirtyRect.Width - 10, dirtyRect.Height - 160, HorizontalAlignment.Left, VerticalAlignment.Center);
 		canvas.DrawString("مرحبا بالعالم", dirtyRect.X + 5, dirtyRect.Y + 20, dirtyRect.Width - 10, dirtyRect.Height - 160, HorizontalAlignment.Left, VerticalAlignment.Center);
 	}
 }
