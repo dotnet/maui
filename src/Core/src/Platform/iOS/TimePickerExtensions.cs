089c14343d82ea5039a0d7fb64b25f6f7ab842b3
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2026-02-16 11:54:35 +0530

    unwanted changes removed

--- src/Core/src/Platform/iOS/TimePickerExtensions.cs
+++ src/Core/src/Platform/iOS/TimePickerExtensions.cs
@@ -32,32 +32,11 @@ public static class TimePickerExtensions
 	{
 		if (picker is not null)
 		{
-			mauiTimePicker.UpdateTime(timePicker, null);
+			picker.Date = new DateTime(1, 1, 1).Add(timePicker?.Time ?? TimeSpan.Zero).ToNSDate();
 		}
 	}
 
-	public static void UpdateFormat(this UIDatePicker picker, ITimePicker timePicker)
-	{
-		picker.UpdateTime(timePicker);
-	}
-
-	public static void UpdateFormat(this MauiTimePicker mauiTimePicker, ITimePicker timePicker, UIDatePicker? picker)
-	{
-		mauiTimePicker.UpdateTime(timePicker, picker);
-	}
-
-	public static void UpdateTime(this MauiTimePicker mauiTimePicker, ITimePicker timePicker)
-	{
-		mauiTimePicker.UpdateTime(timePicker, null);
-	}
-
-	public static void UpdateTime(this UIDatePicker picker, ITimePicker timePicker)
-	{
-		if (picker != null)
-			picker.Date = new DateTime(1, 1, 1).Add(timePicker.Time).ToNSDate();
-	}
-
-	public static void UpdateTime(this MauiTimePicker mauiTimePicker, ITimePicker timePicker, UIDatePicker? picker)
+		public static void UpdateTime(this MauiTimePicker mauiTimePicker, ITimePicker timePicker, UIDatePicker? picker)
 	{
 		picker?.UpdateTime(timePicker);
 
@@ -125,59 +104,6 @@ public static class TimePickerExtensions
 		if (picker != null && format != null)
 		{
 			picker.Locale = new NSLocale(formattingCulture.TwoLetterISOLanguageName);
-		}
-
-		mauiTimePicker.UpdateCharacterSpacing(timePicker);
-	}
-
-	public static void UpdateTextAlignment(this MauiTimePicker textField, ITimePicker timePicker)
-	{
-		// TODO: Update TextAlignment based on the EffectiveFlowDirection property.
-	}
-
-	public static void UpdateTime(this MauiTimePicker mauiTimePicker, ITimePicker timePicker, UIDatePicker? picker)
-	{
-		picker?.UpdateTime(timePicker);
-
-		var cultureInfo = Culture.CurrentCulture;
-
-		if (string.IsNullOrEmpty(timePicker.Format))
-		{
-			NSLocale locale = new NSLocale(cultureInfo.TwoLetterISOLanguageName);
-
-			if (picker is not null)
-			{
-				picker.Locale = locale;
-			}
-		}
-
-		var time = timePicker.Time;
-		var format = timePicker.Format;
-
-		mauiTimePicker.Text = time?.ToFormattedString(format, cultureInfo);
-
-		if (format is not null)
-		{
-			if (format.Contains('H', StringComparison.Ordinal))
-			{
-				var ci = new CultureInfo("de-DE");
-				NSLocale locale = new NSLocale(ci.TwoLetterISOLanguageName);
-
-				if (picker is not null)
-				{
-					picker.Locale = locale;
-				}
-			}
-			else if (format.Contains('h', StringComparison.Ordinal))
-			{
-				var ci = new CultureInfo("en-US");
-				NSLocale locale = new NSLocale(ci.TwoLetterISOLanguageName);
-
-				if (picker is not null)
-				{
-					picker.Locale = locale;
-				}
-			}
 		}
 
 		mauiTimePicker.UpdateCharacterSpacing(timePicker);
