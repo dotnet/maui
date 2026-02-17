2650d18c7849b474236e6c8331c0918664e5f467
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2026-02-17 19:48:51 +0530

    Removed unwanted codes

--- src/Controls/src/Core/Handlers/Items/iOS/ObservableGroupedSource.cs
+++ src/Controls/src/Core/Handlers/Items/iOS/ObservableGroupedSource.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using Foundation;
 using UIKit;
