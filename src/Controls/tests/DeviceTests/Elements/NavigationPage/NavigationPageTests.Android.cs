bb72ecc9b9b152ab8a18070a7930fae39f4545e4
Brian Runck <brunck@intermatic.com>
2026-02-17 16:59:26 -0500

    Create new device test

--- src/Controls/tests/DeviceTests/Elements/NavigationPage/NavigationPageTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/NavigationPage/NavigationPageTests.Android.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Google.Android.Material.AppBar;
@@ -8,6 +9,7 @@ using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Handlers;
 using Microsoft.Maui.DeviceTests.Stubs;
+using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Platform;
 using Xunit;
 
@@ -45,6 +47,62 @@ namespace Microsoft.Maui.DeviceTests
 				Assert.False(failed);
 				await navPage.Navigation.PopAsync();
 				Assert.False(failed);
+			});
+		}
+
+		[Fact(DisplayName = "StackNavigationManager Clears References On Disconnect (Issue 33918)")]
+		public async Task StackNavigationManagerClearsReferencesOnDisconnect()
+		{
+			SetupBuilder();
+
+			var mainPage = new ContentPage { Title = "Main Page" };
+			var baseNavPage = new NavigationPage(mainPage);
+
+			await CreateHandlerAndAddToWindow<WindowHandlerStub>(new Window(baseNavPage), async (handler) =>
+			{
+				var tab1Content = new ContentPage { Title = "Tab 1", Content = new Label { Text = "Tab 1 Content" } };
+				var tab2Content = new ContentPage { Title = "Tab 2", Content = new Label { Text = "Tab 2 Content" } };
+
+				var tab1Nav = new NavigationPage(tab1Content) { Title = "Tab 1" };
+				var tab2Nav = new NavigationPage(tab2Content) { Title = "Tab 2" };
+
+				var tabbedPage = new TabbedPage
+				{
+					Title = "Tabbed Modal",
+					Children = { tab1Nav, tab2Nav }
+				};
+
+				await baseNavPage.Navigation.PushModalAsync(tabbedPage, animated: false);
+				await OnLoadedAsync(tab1Content);
+
+				var tab1Handler = tab1Nav.Handler as NavigationViewHandler;
+				var tab2Handler = tab2Nav.Handler as NavigationViewHandler;
+				Assert.NotNull(tab1Handler);
+				Assert.NotNull(tab2Handler);
+
+				var tab1SnManager = tab1Handler.StackNavigationManager;
+				var tab2SnManager = tab2Handler.StackNavigationManager;
+				Assert.NotNull(tab1SnManager);
+				Assert.NotNull(tab2SnManager);
+
+				tabbedPage.CurrentPage = tab2Nav;
+				await Task.Delay(100);
+
+				await baseNavPage.Navigation.PopModalAsync(animated: false);
+				await Task.Delay(100);
+
+				var flags = BindingFlags.NonPublic | BindingFlags.Instance;
+				var currentPageField = typeof(StackNavigationManager).GetField("_currentPage", flags);
+				var fragmentContainerViewField = typeof(StackNavigationManager).GetField("_fragmentContainerView", flags);
+				var fragmentManagerField = typeof(StackNavigationManager).GetField("_fragmentManager", flags);
+
+				Assert.Null(currentPageField.GetValue(tab1SnManager));
+				Assert.Null(fragmentContainerViewField.GetValue(tab1SnManager));
+				Assert.Null(fragmentManagerField.GetValue(tab1SnManager));
+
+				Assert.Null(currentPageField.GetValue(tab2SnManager));
+				Assert.Null(fragmentContainerViewField.GetValue(tab2SnManager));
+				Assert.Null(fragmentManagerField.GetValue(tab2SnManager));
 			});
 		}
 	}
