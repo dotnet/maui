3a107548d01fba956975fd21f4503cf2a484c647
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2026-02-23 15:25:06 +0530

    Fixed ios slider image

--- src/Core/src/Platform/iOS/SliderExtensions.cs
+++ src/Core/src/Platform/iOS/SliderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Graphics.Platform;
 using ObjCRuntime;
 using UIKit;
 
@@ -21,19 +22,25 @@ namespace Microsoft.Maui.Platform
 		public static void UpdateValue(this UISlider uiSlider, ISlider slider)
 		{
 			if ((float)slider.Value != uiSlider.Value)
+			{
 				uiSlider.Value = (float)slider.Value;
+			}
 		}
 
 		public static void UpdateMinimumTrackColor(this UISlider uiSlider, ISlider slider)
 		{
-			if (slider.MinimumTrackColor != null)
+			if (slider.MinimumTrackColor is not null)
+			{
 				uiSlider.MinimumTrackTintColor = slider.MinimumTrackColor.ToPlatform();
+			}
 		}
 
 		public static void UpdateMaximumTrackColor(this UISlider uiSlider, ISlider slider)
 		{
-			if (slider.MaximumTrackColor != null)
+			if (slider.MaximumTrackColor is not null)
+			{
 				uiSlider.MaximumTrackTintColor = slider.MaximumTrackColor.ToPlatform();
+			}
 		}
 
 		public static void UpdateThumbColor(this UISlider uiSlider, ISlider slider)
@@ -52,15 +59,26 @@ namespace Microsoft.Maui.Platform
 		public static async Task UpdateThumbImageSourceAsync(this UISlider uiSlider, ISlider slider, IImageSourceServiceProvider provider)
 		{
 			var thumbImageSource = slider.ThumbImageSource;
-
-			if (thumbImageSource != null)
+			if (thumbImageSource is not null)
 			{
 				// Clear the thumb color if we have a thumb image, so that slider doesn't clear image when sliding
 				uiSlider.ThumbTintColor = null;
 				var service = provider.GetRequiredImageSourceService(thumbImageSource);
 				var scale = uiSlider.GetDisplayDensity();
 				var result = await service.GetImageAsync(thumbImageSource, scale);
-				var thumbImage = result?.Value;
+				var thumbImageSize = result?.Value.Size ?? CGSize.Empty;
+				var defaultThumbSize = CalculateDefaultThumbSize(uiSlider);
+
+				UIImage? thumbImage;
+				if (thumbImageSize.IsEmpty)
+				{
+					thumbImage = result?.Value;
+				}
+				else
+				{
+					// Resize the image if the size is valid
+					thumbImage = result?.Value?.ResizeImageSource(defaultThumbSize.Width, defaultThumbSize.Height, thumbImageSize);
+				}
 
 				if (thumbImage is not null && slider.ThumbColor is not null)
 				{
@@ -81,6 +99,13 @@ namespace Microsoft.Maui.Platform
 			{
 				uiSlider.SetNeedsLayout();
 			}
+		}
+
+		static CGSize CalculateDefaultThumbSize(UISlider uiSlider)
+		{
+			var trackRect = uiSlider.TrackRectForBounds(uiSlider.Bounds);
+			var thumbRect = uiSlider.ThumbRectForBounds(uiSlider.Bounds, trackRect, 0);
+			return thumbRect.Size;
 		}
 	}
 }
