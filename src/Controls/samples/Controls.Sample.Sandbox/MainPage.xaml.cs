4662a34baeb18134c6e96b01e099852f4cc1a84f
Stephane Delcroix <stephane@delcroix.org>
2026-03-01 16:17:05 +0100

    Revert "Add CarPlay + Widget demo to Sandbox app"
    This reverts commit 27a6fbf5cf662723b75820cce79b041d5388cc4d.

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -6,18 +6,4 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
-	void OnUpdateWidgetClicked(object? sender, EventArgs e)
-	{
-#if IOS || MACCATALYST
-#pragma warning disable CA1416
-		var emojis = new[] { "🟣", "🔵", "🟢", "🟠", "🔴", "⭐", "🎵", "🚀" };
-		var emoji = emojis[Random.Shared.Next(emojis.Length)];
-		var title = WidgetTitle.Text ?? "MAUI Sandbox";
-		var message = WidgetMessage.Text ?? "Hello!";
-		MauiProgram.UpdateWidgetData(title, message, emoji);
-		WidgetStatus.Text = $"✅ Updated: {emoji} {title}";
-#pragma warning restore CA1416
-#endif
-	}
 }
