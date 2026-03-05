980fa21fc85d376f9cd9310930cf49a5578e9447
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2026-03-05 11:43:32 +0530

    Added MaterialShapeableImageView for Material3 ImageButton

--- src/Core/src/Platform/Android/MauiShapeableImageView.cs
+++ src/Core/src/Platform/Android/MauiShapeableImageView.cs
@@ -7,20 +7,52 @@ namespace Microsoft.Maui.Platform
 {
 	public class MauiShapeableImageView : ShapeableImageView
 	{
-
-		public MauiShapeableImageView(Context context) : base(MauiMaterialContextThemeWrapper.Create(context))
+		public MauiShapeableImageView(Context? context) : base(context)
 		{
 		}
 
-		public MauiShapeableImageView(Context context, IAttributeSet? attrs) : base(MauiMaterialContextThemeWrapper.Create(context), attrs)
+		public MauiShapeableImageView(Context? context, IAttributeSet? attrs) : base(context, attrs)
 		{
 		}
 
-		public MauiShapeableImageView(Context context, IAttributeSet? attrs, int defStyle) : base(MauiMaterialContextThemeWrapper.Create(context), attrs, defStyle)
+		public MauiShapeableImageView(Context? context, IAttributeSet? attrs, int defStyle) : base(context, attrs, defStyle)
 		{
 		}
 
 		protected MauiShapeableImageView(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+		{
+		}
+
+		protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
+		{
+			// The padding has a few issues. This is a workaround for the following issue:
+			// https://github.com/material-components/material-components-android/issues/2063
+
+			// ShapeableImageView combines ContentPadding with Padding and updates
+			// Padding with the result.
+			base.OnMeasure(widthMeasureSpec, heightMeasureSpec);
+
+			// We need to reset the padding to 0 to avoid a double padding.
+			SetPadding(0, 0, 0, 0);
+		}
+	}
+
+	// TODO: net11 - Remove this class and update MauiShapeableImageView to public API with the same changes.
+	internal class MaterialShapeableImageView : ShapeableImageView
+	{
+		internal MaterialShapeableImageView(Context context) : base(MauiMaterialContextThemeWrapper.Create(context))
+		{
+		}
+
+		internal MaterialShapeableImageView(Context context, IAttributeSet? attrs) : base(MauiMaterialContextThemeWrapper.Create(context), attrs)
+		{
+		}
+
+		internal MaterialShapeableImageView(Context context, IAttributeSet? attrs, int defStyle) : base(MauiMaterialContextThemeWrapper.Create(context), attrs, defStyle)
+		{
+		}
+
+		protected MaterialShapeableImageView(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
 		{
 		}
 
