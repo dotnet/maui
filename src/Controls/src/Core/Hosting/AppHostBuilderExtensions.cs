c06d266225fc40701f579ceb86b3bb6987627f13
Sheik Syed <sheiksyedm@syncfusion.com>
2026-03-18 20:40:52 +0530

    Fix formatting for Entry handler registration

--- src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
+++ src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
@@ -85,7 +85,7 @@ public static partial class AppHostBuilderExtensions
 			handlersCollection.AddHandler<SearchBar, SearchBarHandler2>();
 			handlersCollection.AddHandler<Slider, SliderHandler2>();
 			handlersCollection.AddHandler<DatePicker, DatePickerHandler2>();
-      handlersCollection.AddHandler<Entry, EntryHandler2>();
+            handlersCollection.AddHandler<Entry, EntryHandler2>();
 		}
 		else
 		{
@@ -101,7 +101,7 @@ public static partial class AppHostBuilderExtensions
 			handlersCollection.AddHandler<SearchBar, SearchBarHandler>();
 			handlersCollection.AddHandler<Slider, SliderHandler>();
 			handlersCollection.AddHandler<DatePicker, DatePickerHandler>();
-      handlersCollection.AddHandler<Entry, EntryHandler>();
+            handlersCollection.AddHandler<Entry, EntryHandler>();
 		}
 #else
 		handlersCollection.AddHandler<Label, LabelHandler>();
@@ -116,7 +116,7 @@ public static partial class AppHostBuilderExtensions
 		handlersCollection.AddHandler<SearchBar, SearchBarHandler>();
 		handlersCollection.AddHandler<Slider, SliderHandler>();
 		handlersCollection.AddHandler<DatePicker, DatePickerHandler>();
-    handlersCollection.AddHandler<Entry, EntryHandler>();
+        handlersCollection.AddHandler<Entry, EntryHandler>();
 #endif
 		handlersCollection.AddHandler<Application, ApplicationHandler>();
 		handlersCollection.AddHandler<BoxView, BoxViewHandler>();
