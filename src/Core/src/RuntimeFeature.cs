f730d5d9b2bef26375b2159113191eb201ed8eaf
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2026-03-23 17:25:05 +0530

    BuildProperties added

--- src/Core/src/RuntimeFeature.cs
+++ src/Core/src/RuntimeFeature.cs
@@ -28,6 +28,7 @@ namespace Microsoft.Maui
 		const bool IsMeterSupportedByDefault = true;
 		const bool EnableAspireByDefault = true;
 		const bool IsMaterial3EnabledByDefault = false;
+		const bool IsWindowsCollectionView2HandlerEnabledByDefault = false;
 
 #pragma warning disable IL4000 // Return value does not match FeatureGuardAttribute 'System.Diagnostics.CodeAnalysis.RequiresUnreferencedCodeAttribute'. 
 #if NET9_0_OR_GREATER
@@ -155,6 +156,14 @@ namespace Microsoft.Maui
 			AppContext.TryGetSwitch($"{FeatureSwitchPrefix}.{nameof(IsMaterial3Enabled)}", out bool isEnabled)
 				? isEnabled
 				: IsMaterial3EnabledByDefault;
+
+#if NET10_0_OR_GREATER
+		[FeatureSwitchDefinition($"{FeatureSwitchPrefix}.{nameof(IsWindowsCollectionView2HandlerEnabled)}")]
+#endif
+		public static bool IsWindowsCollectionView2HandlerEnabled =>
+			AppContext.TryGetSwitch($"{FeatureSwitchPrefix}.{nameof(IsWindowsCollectionView2HandlerEnabled)}", out bool isEnabled)
+				? isEnabled
+				: IsWindowsCollectionView2HandlerEnabledByDefault;
 
 #pragma warning restore IL4000
 	}
