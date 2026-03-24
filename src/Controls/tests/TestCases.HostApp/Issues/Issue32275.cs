f8034a8f401f2198fc4447fa612bb1d3353cd95d
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2026-03-24 17:24:34 +0530

    test case added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue32275.cs
@@ -0,0 +1,6 @@
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 32275, "Shell Flyout SafeArea Rendering", PlatformAffected.iOS)]
+public class Issue32275 : ShellFlyoutContentBase
+{
+}
