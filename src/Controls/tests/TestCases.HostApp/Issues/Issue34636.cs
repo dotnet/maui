7a181f1cd0f70585e8a0925acfbf2b63dfab1f93
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2026-03-25 19:22:57 +0530

    committed the test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue34636.cs
@@ -0,0 +1,110 @@
+using System.Collections.ObjectModel;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 34636, "CollectionView ItemSpacing - First and last item on the list is truncated after changing Spacing value", PlatformAffected.Android)]
+public class Issue34636 : ContentPage
+{
+    static readonly IReadOnlyList<MonkeyItem> MonkeyData = new[]
+    {
+        new MonkeyItem("Baboon", "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/baboon.jpg", "Africa & Asia"),
+        new MonkeyItem("Capuchin Monkey", "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/capuchin.jpg", "Central & South America"),
+        new MonkeyItem("Blue Monkey", "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/bluemonkey.jpg", "Central and East Africa"),
+        new MonkeyItem("Squirrel Monkey", "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/saimiri.jpg", "Central & South America"),
+        new MonkeyItem("Golden Lion Tamarin", "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/tamarin.jpg", "Brazil"),
+        new MonkeyItem("Howler Monkey", "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/alouatta.jpg", "South America"),
+        new MonkeyItem("Japanese Macaque", "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/macasa.jpg", "Japan"),
+        new MonkeyItem("Mandrill", "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/mandrill.jpg", "Southern Cameroon, Gabon, Equatorial Guinea, and Congo"),
+    };
+
+    readonly ObservableCollection<MonkeyItem> _items = new(MonkeyData);
+    readonly LinearItemsLayout _itemsLayout = new(ItemsLayoutOrientation.Vertical) { ItemSpacing = 0 };
+    CollectionView _collectionView;
+
+    public Issue34636()
+    {
+        Title = "CollectionView ItemSpacing regression";
+
+        _collectionView = new CollectionView
+        {
+            AutomationId = "MonkeyCollectionView",
+            BackgroundColor = Colors.LightGray,
+            VerticalOptions = LayoutOptions.Fill,
+            ItemsSource = _items,
+            ItemsLayout = _itemsLayout,
+            ItemTemplate = new DataTemplate(() =>
+            {
+                var image = new Image
+                {
+                    HeightRequest = 60,
+                    WidthRequest = 60,
+                    Aspect = Aspect.AspectFill
+                };
+                image.SetBinding(Image.SourceProperty, nameof(MonkeyItem.ImageUrl));
+                Grid.SetRowSpan(image, 2);
+
+                var nameLabel = new Label
+                {
+                    FontAttributes = FontAttributes.Bold
+                };
+                nameLabel.SetBinding(Label.TextProperty, nameof(MonkeyItem.Name));
+                Grid.SetColumn(nameLabel, 1);
+
+                var locationLabel = new Label
+                {
+                    FontAttributes = FontAttributes.Italic,
+                    VerticalOptions = LayoutOptions.End
+                };
+                locationLabel.SetBinding(Label.TextProperty, nameof(MonkeyItem.Location));
+                Grid.SetRow(locationLabel, 1);
+                Grid.SetColumn(locationLabel, 1);
+
+                return new Grid
+                {
+                    Padding = new Thickness(10),
+                    RowDefinitions =
+                    {
+                        new RowDefinition { Height = GridLength.Auto },
+                        new RowDefinition { Height = GridLength.Auto }
+                    },
+                    ColumnDefinitions =
+                    {
+                        new ColumnDefinition { Width = GridLength.Auto },
+                        new ColumnDefinition { Width = GridLength.Star }
+                    },
+                    Children = { image, nameLabel, locationLabel }
+                };
+            })
+        };
+
+        var changeSpacingButton = new Button
+        {
+            AutomationId = "ChangeSpacingButton",
+            Text = "Change spacing to 70"
+        };
+        changeSpacingButton.Clicked += (_, _) =>
+        {
+            _itemsLayout.ItemSpacing = 70;
+            changeSpacingButton.Text = "Spacing changed to 70";
+        };
+
+        Content = new Grid
+        {
+            RowDefinitions =
+            {
+                new RowDefinition { Height = GridLength.Auto },
+                new RowDefinition { Height = GridLength.Star }
+            },
+            Children =
+            {
+                changeSpacingButton,
+                _collectionView
+            }
+        };
+
+        Grid.SetRow(changeSpacingButton, 0);
+        Grid.SetRow(_collectionView, 1);
+    }
+
+    record MonkeyItem(string Name, string ImageUrl, string Location);
+}
