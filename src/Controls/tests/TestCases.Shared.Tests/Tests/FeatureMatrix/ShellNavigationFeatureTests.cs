e667a44aca25baefe7ac8491c27458a56c57e30c
TamilarasanSF4853 <tamilarasan.velu@syncfusion.com>
2026-04-01 20:53:22 +0530

    added changes

--- src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/ShellNavigationFeatureTests.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/FeatureMatrix/ShellNavigationFeatureTests.cs
@@ -883,6 +883,10 @@ public class ShellNavigationFeatureTests : _GalleryUITest
 	[Test, Order(50)]
 	public void BackButtonBehavior_IsEnabled_False_BackButtonDoesNotNavigate()
 	{
+		if (iOS26OrHigher)
+        {
+            Assert.Ignore("Fails on iOS 26 due to bug issue: https://github.com/dotnet/maui/issues/34771");
+        }
 		App.WaitForElement("MainPageIdentityLabel");
 		App.WaitForElement("IsEnabledButton");
 		App.Tap("IsEnabledButton");
@@ -905,6 +909,10 @@ public class ShellNavigationFeatureTests : _GalleryUITest
 	[Test, Order(51)]
 	public void BackButtonBehavior_IsVisible_False_ProgrammaticNavStillWorks()
 	{
+		if (iOS26OrHigher)
+        {
+            Assert.Ignore("Fails on iOS 26 due to bug issue: https://github.com/dotnet/maui/issues/34771");
+        }
 		App.WaitForElement("MainPageIdentityLabel");
 		App.WaitForElement("IsVisibleButton");
 		App.Tap("IsVisibleButton");
@@ -917,6 +925,10 @@ public class ShellNavigationFeatureTests : _GalleryUITest
 	[Test, Order(52)]
 	public void BackButtonBehavior_IconOverride_CustomIconShownOnBackButton()
 	{
+		if (iOS26OrHigher)
+        {
+            NavigateToDetail1AndWait();
+        }
 		App.WaitForElement("Detail1GoBackButton");
 		App.Tap("Detail1GoBackButton");
 		App.WaitForElement("MainPageIdentityLabel");
