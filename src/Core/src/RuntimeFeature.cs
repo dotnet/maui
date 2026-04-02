4b1eafd44f37a5d61d6f05dc12ac7259a40dc6e2
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2026-04-02 10:54:15 +0530

    Update RuntimeFeature.cs

--- src/Core/src/RuntimeFeature.cs
+++ src/Core/src/RuntimeFeature.cs
@@ -158,7 +158,7 @@ namespace Microsoft.Maui
 				? isEnabled
 				: IsMaterial3EnabledByDefault;
 
-#if NET10_0_OR_GREATER
+#if NET11_0_OR_GREATER
 		[FeatureSwitchDefinition($"{FeatureSwitchPrefix}.{nameof(IsWindowsCollectionView2HandlerEnabled)}")]
 #endif
 		public static bool IsWindowsCollectionView2HandlerEnabled =>
