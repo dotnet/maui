3d7444e9e2a7bd697920d201326209229849cba2
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2026-04-15 17:39:34 +0530

    Update Issue34823.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue34823.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue34823.cs
@@ -1,3 +1,4 @@
+#if TEST_FAILS_ON_ANDROID // This test fails on Android because of user app theme is not responsive in Hostapp.
 using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
@@ -33,3 +34,4 @@ public class Issue34823 : _IssuesUITest
 		VerifyScreenshot();
 	}
 }
+#endif
