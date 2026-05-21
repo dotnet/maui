43fc6016c386fdbda61ca596758af8fefec9be53
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2026-05-21 18:41:46 +0530

    Updated test and MauiBorderAutomationPeer.

--- src/Core/src/Platform/Windows/MauiBorderAutomationPeer.cs
+++ src/Core/src/Platform/Windows/MauiBorderAutomationPeer.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.UI.Xaml.Automation.Peers;
+﻿using Microsoft.UI.Xaml.Automation;
+using Microsoft.UI.Xaml.Automation.Peers;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.Maui.Platform
 {
-	// TODO: Make this class public in .NET 10.0. Issue Link: https://github.com/dotnet/maui/issues/30205
-	internal class MauiBorderAutomationPeer : FrameworkElementAutomationPeer
+	// TODO: Make this class public in .NET11.0. Issue Link: https://github.com/dotnet/maui/issues/30205
+	internal partial class MauiBorderAutomationPeer : FrameworkElementAutomationPeer
 	{
 		internal MauiBorderAutomationPeer(Panel owner) : base(owner) { }
 
@@ -20,6 +21,16 @@ namespace Microsoft.Maui.Platform
 
 		protected override bool IsControlElementCore() => true;
 
-		protected override bool IsContentElementCore() => true;
+		protected override bool IsContentElementCore() => HasAutomationId();
+
+		bool HasAutomationId()
+		{
+			if (Owner is not ContentPanel contentPanel)
+			{
+				return false;
+			}
+
+			return !string.IsNullOrEmpty(AutomationProperties.GetAutomationId(contentPanel));
+		}
 	}
 }
