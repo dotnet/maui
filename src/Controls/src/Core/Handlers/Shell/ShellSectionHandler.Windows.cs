db9dc0b5254f26e510c1f84f475aadbe6556bc84
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2026-06-02 19:15:05 +0530

    Update ShellSectionHandler.Windows.cs

--- src/Controls/src/Core/Handlers/Shell/ShellSectionHandler.Windows.cs
+++ src/Controls/src/Core/Handlers/Shell/ShellSectionHandler.Windows.cs
@@ -120,6 +120,18 @@ namespace Microsoft.Maui.Controls.Handlers
 				shellItemHandler.MapMenuItems();
 			}
 
+			if (e.Action == NotifyCollectionChangedAction.Reset)
+			{
+				UnsubscribeAllShellContent();
+
+				foreach (var item in ((IShellSectionController)_shellSection).GetItems())
+				{
+					SubscribeToShellContent(item);
+				}
+
+				return;
+			}
+
 			if (e.OldItems is not null)
 			{
 				foreach (var item in e.OldItems)
