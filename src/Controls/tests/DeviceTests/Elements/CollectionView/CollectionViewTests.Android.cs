a8a77fb16374b4fb1bd6f2439a88ae37aa41e3cc
praveenkumarkarunanithi <praveenkumar.karunanithi@syncfusion.com>
2026-06-02 13:10:22 +0530

    Resolving build error in device test.

--- src/Controls/tests/DeviceTests/Elements/CollectionView/CollectionViewTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/CollectionView/CollectionViewTests.Android.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Android.Content;
