e1551b34449de0e07347350b80fa03d7130d54fa
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2026-06-12 14:31:23 +0530

    Add windows fix

--- src/Core/src/Animations/PlatformTicker.Windows.cs
+++ src/Core/src/Animations/PlatformTicker.Windows.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.UI.Xaml.Media;
+using ViewManagement = Windows.UI.ViewManagement;
 
 namespace Microsoft.Maui.Animations
 {
 	/// <inheritdoc/>
 	public class PlatformTicker : Ticker
 	{
+		readonly ViewManagement.UISettings _uiSettings = new();
+
+		// Always read the live system value so AnimationManager.Add() and OnFire() both
+		// see the current "Show animations in Windows" (Ease of Access) setting without caching.
+		/// <inheritdoc/>
+		public override bool SystemEnabled => _uiSettings.AnimationsEnabled;
+
 		/// <inheritdoc/>
 		public override void Start()
 		{
