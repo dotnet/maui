c80d03bfdc71794eb49d5f3d91a5ae0a50ee87e2
James Alickolli <jalickolli@gmail.com>
2026-06-16 21:31:27 +1000

    Update Picker.Windows.cs

--- src/Controls/src/Core/Picker/Picker.Windows.cs
+++ src/Controls/src/Core/Picker/Picker.Windows.cs
@@ -1,32 +1,50 @@
 ﻿using System;
 using Microsoft.Maui.Controls.Platform;
+using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.Maui.Controls
 {
-	public partial class Picker
-	{
-		public static void MapHorizontalOptions(IPickerHandler handler, Picker picker)
-		{
-			handler.PlatformView?.UpdateHorizontalOptions(picker);
-		}
+    public partial class Picker
+    {
+        public static void MapHorizontalOptions(IPickerHandler handler, Picker picker)
+        {
+            handler.PlatformView?.UpdateHorizontalOptions(picker);
+        }
 
-		public static void MapVerticalOptions(IPickerHandler handler, Picker picker)
-		{
-			handler.PlatformView?.UpdateVerticalOptions(picker);
-		}
+        public static void MapVerticalOptions(IPickerHandler handler, Picker picker)
+        {
+            handler.PlatformView?.UpdateVerticalOptions(picker);
+        }
 
-		public static void MapBorderColor(IPickerHandler handler, Picker picker)
+        public static void MapBorderColor(IPickerHandler handler, Picker picker)
+        {
+            if (handler.PlatformView is null)
+                return;
+
+            if (picker.BorderColor is null)
+            {
+                handler.PlatformView.ClearValue(Control.BorderBrushProperty);
+                return;
+            }
+
+            handler.PlatformView.BorderBrush = picker.BorderColor.ToPlatform();
+        }
+
+        public static void MapBorderThickness(IPickerHandler handler, Picker picker)
 		{
-			if (picker.BorderColor is null)
+			if (handler.PlatformView is null)
+				return;
+
+			if (!picker.IsSet(BorderThicknessProperty))
+				return;
+
+			if (picker.BorderThickness == default)
 			{
+				handler.PlatformView.ClearValue(Control.BorderThicknessProperty);
 				return;
 			}
-			handler.PlatformView.BorderBrush = picker.BorderColor.ToPlatform();
-		}
-
-		public static void MapBorderThickness(IPickerHandler handler, Picker picker)
-		{
+			
 			handler.PlatformView.BorderThickness = picker.BorderThickness.ToPlatform();
 		}
-	}
+    }
 }
