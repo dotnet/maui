cc4a4284132f2addebb35b4af941361c921949d8
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2026-06-29 20:02:05 +0530

    [iOS] Fix RefreshView causes CollectionView scroll position to reset (#31376)
    <!-- Please let the below note in for people that find this PR -->
    > [!NOTE]
    > Are you waiting for the changes in this PR to be merged?
    > It would be very helpful if you could [test the resulting
    artifacts](https://github.com/dotnet/maui/wiki/Testing-PR-Builds) from
    this PR and let us know in a comment if this change resolves your issue.
    Thank you!
    
    <!--
    !!!!!!! MAIN IS THE ONLY ACTIVE BRANCH. MAKE SURE THIS PR IS TARGETING
    MAIN. !!!!!!!
    -->
    
    ### Root Cause
    When refresh starts or ends, the scroll view's content offset gets reset
    to (0,0) instead of maintaining the user's current scroll position.
    
    ### Description of Change
    
    <!-- Enter description of the fix in this section -->
    Fixed by storing the original scroll position before refresh starts and
    restoring it after refresh completes, preventing the view from jumping
    back to the top during pull-to-refresh operations.
    
    
    ### Issues Fixed
    
    <!-- Please make sure that there is a bug logged for the issue being
    fixed. The bug should describe the problem and how to reproduce it. -->
    
    Fixes #24533 
    
    <!--
    Are you targeting main? All PRs should target the main branch unless
    otherwise noted.
    -->
    
    **Tested the behavior in the following platforms.**
    - [x] Android
    - [x] Windows
    - [x] iOS
    - [x] Mac
    
    | Before  | After  |
    |---------|--------|
    | **iOS**<br> <video
    src="https://github.com/user-attachments/assets/041c5713-8f9f-4e28-973a-1242384e2b57"
    width="300" height="600"> | **iOS**<br> <video
    src="https://github.com/user-attachments/assets/7fcc79ae-d153-4013-840c-98277b7f01da"
    width="300" height="600"> |

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue24533.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 24533, "[iOS] RefreshView causes CollectionView scroll position to reset", PlatformAffected.iOS)]
+public class Issue24533 : ContentPage
+{
+	ObservableCollection<string> _items;
+	public ObservableCollection<string> Items
+	{
+		get => _items;
+		set
+		{
+			_items = value;
+			OnPropertyChanged(nameof(Items));
+		}
+	}
+
+	bool _isLoading;
+	public bool IsLoading
+	{
+		get => _isLoading;
+		set
+		{
+			_isLoading = value;
+			OnPropertyChanged(nameof(IsLoading));
+		}
+	}
+
+	ICommand _refreshCommand;
+	public ICommand RefreshCommand
+	{
+		get => _refreshCommand;
+		set
+		{
+			_refreshCommand = value;
+			OnPropertyChanged(nameof(RefreshCommand));
+		}
+	}
+
+	string _verticalOffsetText = "VerticalOffset: 0.00";
+	public string VerticalOffsetText
+	{
+		get => _verticalOffsetText;
+		set
+		{
+			_verticalOffsetText = value;
+			OnPropertyChanged(nameof(VerticalOffsetText));
+		}
+	}
+
+		int count;
+
+	public Issue24533()
+	{
+		// Initialize properties
+		RefreshCommand = new Command(AddItems);
+		Items = new ObservableCollection<string>();
+		this.On<Microsoft.Maui.Controls.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+		// Create CollectionView
+		var collectionView = new CollectionView
+		{
+			SelectionMode = SelectionMode.Single,
+			ItemTemplate = new DataTemplate(() =>
+			{
+				var stack = new VerticalStackLayout();
+				var label = new Label();
+				label.SetBinding(Label.TextProperty, ".");
+				stack.Children.Add(label);
+				return stack;
+			}),
+		};
+		collectionView.Scrolled += CollectionView_Scrolled;
+
+		var btn = new Button { Text = "Load More", AutomationId="Footer", HeightRequest = 60 };
+		btn.Clicked += Button_Clicked;
+		collectionView.Footer =	 btn;
+			
+
+		// Bind ItemsSource
+		collectionView.SetBinding(ItemsView.ItemsSourceProperty, nameof(Items));
+
+		// Create RefreshView
+		var refreshView = new RefreshView
+		{
+			Margin = 16
+		};
+		refreshView.SetBinding(RefreshView.IsRefreshingProperty, nameof(IsLoading));
+		refreshView.SetBinding(RefreshView.CommandProperty, nameof(RefreshCommand));
+		refreshView.Content = collectionView;
+
+		var verticalOffsetLabel = new Label
+		{
+			AutomationId = "VerticalOffsetLabel"
+		};
+		verticalOffsetLabel.SetBinding(Label.TextProperty, nameof(VerticalOffsetText));
+
+		Content = new StackLayout
+		{
+			Children =
+			{
+				verticalOffsetLabel,
+				refreshView
+			}
+		};
+
+		// Set BindingContext to itself (so bindings work)
+		BindingContext = this;
+
+		// Load initial items
+		AddItems();
+	}
+
+	void Button_Clicked(object sender, EventArgs e)
+	{
+		IsLoading = true;
+	}
+
+	void AddItems()
+	{
+		// To simulate the reset behavior: Reassign Items collection like XAML scenario
+		foreach (int value in Enumerable.Range(count, 25))
+		{
+			Items.Add(value.ToString());
+		}
+		count += 20;
+
+		IsLoading = false;
+	}
+
+	void CollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+	{
+		VerticalOffsetText = $"VerticalOffset: {e.VerticalOffset:F2}";
+	}
+}
