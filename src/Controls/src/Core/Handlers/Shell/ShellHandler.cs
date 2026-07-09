5521552520c81230881c5c4d2f2546257c066183
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2026-07-09 15:35:59 +0530

    Updated changes

--- src/Controls/src/Core/Handlers/Shell/ShellHandler.cs
+++ src/Controls/src/Core/Handlers/Shell/ShellHandler.cs
@@ -15,32 +15,32 @@ namespace Microsoft.Maui.Controls.Handlers
 				new PropertyMapper<Shell, ShellHandler>(ElementMapper)
 				{
 #if WINDOWS || TIZEN
-					[nameof(IToolbarElement.Toolbar)] = (handler, view) => ViewHandler.MapToolbar(handler, view),
-					[nameof(IFlyoutView.Flyout)] = MapFlyout,
-					[nameof(Shell.Items)] = MapItems,
-					[nameof(Shell.FlyoutItems)] = MapFlyoutItems,
-					[nameof(Shell.FlyoutIcon)] = MapFlyoutIcon,
+				[nameof(IToolbarElement.Toolbar)] = (handler, view) => ViewHandler.MapToolbar(handler, view),
+				[nameof(IFlyoutView.Flyout)] = MapFlyout,
 #endif
-
-					[nameof(Shell.CurrentItem)] = MapCurrentItem,
-					[nameof(Shell.FlyoutBackground)] = MapFlyoutBackground,
-					[nameof(Shell.FlyoutBackgroundColor)] = MapFlyoutBackground,
-					[nameof(Shell.FlyoutBackdrop)] = MapFlyoutBackdrop,
-					[nameof(Shell.FlyoutHeader)] = MapFlyoutHeader,
-					[nameof(Shell.FlyoutHeaderTemplate)] = MapFlyoutHeader,
-					[nameof(Shell.FlyoutFooter)] = MapFlyoutFooter,
-					[nameof(Shell.FlyoutFooterTemplate)] = MapFlyoutFooter,
-					[nameof(Shell.FlyoutHeaderBehavior)] = MapFlyoutHeaderBehavior,
-					[nameof(IFlyoutView.FlyoutBehavior)] = MapFlyoutBehavior,
-					[nameof(IFlyoutView.FlyoutWidth)] = MapFlyoutWidth,
-					[nameof(IFlyoutView.IsPresented)] = MapIsPresented,
-					[nameof(Shell.FlyoutContent)] = MapFlyout,
-					[nameof(Shell.FlyoutContentTemplate)] = MapFlyout,
-					[nameof(Shell.FlowDirection)] = MapFlowDirection,
-					[nameof(Shell.FlyoutBackgroundImage)] = MapFlyoutBackgroundImage,
-					[nameof(Shell.FlyoutBackgroundImageAspect)] = MapFlyoutBackgroundImage,
-					[nameof(Shell.FlyoutVerticalScrollMode)] = MapFlyoutVerticalScrollMode,
-
+				[nameof(IFlyoutView.IsPresented)] = MapIsPresented,
+				[nameof(IFlyoutView.FlyoutBehavior)] = MapFlyoutBehavior,
+				[nameof(IFlyoutView.FlyoutWidth)] = MapFlyoutWidth,
+				[nameof(Shell.FlyoutBackground)] = MapFlyoutBackground,
+				[nameof(Shell.FlyoutBackgroundColor)] = MapFlyoutBackground,
+				[nameof(Shell.FlyoutContent)] = MapFlyout,
+				[nameof(Shell.CurrentItem)] = MapCurrentItem,
+				[nameof(Shell.FlyoutBackdrop)] = MapFlyoutBackdrop,
+				[nameof(Shell.FlyoutFooter)] = MapFlyoutFooter,
+				[nameof(Shell.FlyoutFooterTemplate)] = MapFlyoutFooter,
+				[nameof(Shell.FlyoutHeader)] = MapFlyoutHeader,
+				[nameof(Shell.FlyoutHeaderTemplate)] = MapFlyoutHeader,
+				[nameof(Shell.FlyoutHeaderBehavior)] = MapFlyoutHeaderBehavior,
+#if WINDOWS || TIZEN
+				[nameof(Shell.Items)] = MapItems,
+				[nameof(Shell.FlyoutItems)] = MapFlyoutItems,
+				[nameof(Shell.FlyoutIcon)] = MapFlyoutIcon,
+#endif
+				[nameof(Shell.FlyoutContentTemplate)] = MapFlyout,
+				[nameof(Shell.FlowDirection)] = MapFlowDirection,
+				[nameof(Shell.FlyoutBackgroundImage)] = MapFlyoutBackgroundImage,
+				[nameof(Shell.FlyoutBackgroundImageAspect)] = MapFlyoutBackgroundImage,
+				[nameof(Shell.FlyoutVerticalScrollMode)] = MapFlyoutVerticalScrollMode,
 #if ANDROID
 					[nameof(Shell.FlyoutHeight)] = MapFlyoutHeight,
 #endif
