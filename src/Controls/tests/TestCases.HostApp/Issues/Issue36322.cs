b04061e7c60ad80ccb51a15d2f40d722b2abe4e4
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2026-07-09 16:18:35 +0530

    Test

--- src/Controls/tests/TestCases.HostApp/Issues/Issue36322.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue36322.cs
@@ -1,10 +1,37 @@
 namespace Maui.Controls.Sample.Issues;
 
 [Issue(IssueTracker.Github, 36322, "Shell.TitleView is not centered on initial load on Windows", PlatformAffected.UWP)]
-public partial class Issue36322 : Shell
+public class Issue36322 : TestShell
 {
-	public Issue36322()
+	protected override void Init()
 	{
-		InitializeComponent();
+		var page = new ContentPage
+		{
+			Title = "Issue36322",
+			Content = new VerticalStackLayout
+			{
+				Padding = 24,
+				Children =
+				{
+					new Label
+					{
+						Text = "Verify that the Shell TitleView is centered in the command bar on initial load.",
+						AutomationId = "Issue36322Instructions"
+					}
+				}
+			}
+		};
+
+		Shell.SetTitleView(page, new ImageButton
+		{
+			Source = "dotnet_bot.png",
+			HeightRequest = 25,
+			WidthRequest = 25,
+			HorizontalOptions = LayoutOptions.Center,
+			VerticalOptions = LayoutOptions.Center,
+			AutomationId = "Issue36322TitleView"
+		});
+
+		AddContentPage(page, "Home");
 	}
 }
