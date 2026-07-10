386e47e297dd52355f0799d87df87cf84c1d4ab8
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2026-07-10 12:16:24 +0530

    Address AI summary

--- src/Core/src/Animations/PlatformTicker.Windows.cs
+++ src/Core/src/Animations/PlatformTicker.Windows.cs
@@ -1,21 +1,32 @@
-﻿using Microsoft.UI.Xaml.Media;
+﻿using System;
+using Microsoft.UI.Xaml.Media;
 using ViewManagement = Windows.UI.ViewManagement;
 
 namespace Microsoft.Maui.Animations
 {
 	/// <inheritdoc/>
-	public class PlatformTicker : Ticker
+	public class PlatformTicker : Ticker, IDisposable
 	{
 		readonly ViewManagement.UISettings _uiSettings = new();
+		bool _disposed;
 
-		// Always read the live system value so AnimationManager.Add() and OnFire() both
-		// see the current "Show animations in Windows" (Ease of Access) setting without caching.
-		/// <inheritdoc/>
-		public override bool SystemEnabled => _uiSettings.AnimationsEnabled;
+		/// <summary>
+		/// Creates a new Windows <see cref="PlatformTicker"/> that respects the "Show animations in Windows" accessibility setting.
+		/// </summary>
+		public PlatformTicker()
+		{
+			SystemEnabled = _uiSettings.AnimationsEnabled;
+			_uiSettings.AnimationsEnabledChanged += OnAnimationsEnabledChanged;
+		}
 
 		/// <inheritdoc/>
 		public override void Start()
 		{
+			if (_disposed)
+			{
+				return;
+			}
+
 			CompositionTarget.Rendering += RenderingFrameEventHandler;
 		}
 
@@ -23,6 +34,35 @@ namespace Microsoft.Maui.Animations
 		public override void Stop()
 		{
 			CompositionTarget.Rendering -= RenderingFrameEventHandler;
+		}
+
+		/// <inheritdoc/>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (_disposed)
+			{
+				return;
+			}
+
+			_disposed = true;
+
+			if (disposing)
+			{
+				Stop();
+				_uiSettings.AnimationsEnabledChanged -= OnAnimationsEnabledChanged;
+			}
+		}
+
+		/// <inheritdoc/>
+		public void Dispose()
+		{
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+
+		void OnAnimationsEnabledChanged(ViewManagement.UISettings sender, ViewManagement.UISettingsAnimationsEnabledChangedEventArgs args)
+		{
+			SystemEnabled = sender.AnimationsEnabled;
 		}
 
 		void RenderingFrameEventHandler(object? sender, object? args)
