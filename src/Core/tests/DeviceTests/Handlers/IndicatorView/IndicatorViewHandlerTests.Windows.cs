51e1c2e1293ddb5959ff9c80ce4bb09c4056efa9
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2026-07-13 21:38:24 +0530

    Add test

--- src/Core/tests/DeviceTests/Handlers/IndicatorView/IndicatorViewHandlerTests.Windows.cs
+++ src/Core/tests/DeviceTests/Handlers/IndicatorView/IndicatorViewHandlerTests.Windows.cs
@@ -1,7 +1,45 @@
-﻿namespace Microsoft.Maui.DeviceTests
+﻿using Microsoft.Maui.DeviceTests.Stubs;
+using Microsoft.Maui.Graphics;
+using Xunit;
+
+namespace Microsoft.Maui.DeviceTests
 {
 	public partial class IndicatorViewHandlerTests
 	{
+		[Theory(DisplayName = "IndicatorView Background Color Initializes Correctly on Windows")]
+		[InlineData(0xFFFF0000)]
+		[InlineData(0xFF00FF00)]
+		[InlineData(0xFF0000FF)]
+		public async Task BackgroundColorInitializesCorrectly(uint color)
+		{
+			var expected = Color.FromUint(color);
 
+			var indicatorView = new IndicatorViewStub()
+			{
+				Count = 3,
+				Position = 0,
+				Background = new SolidPaintStub(expected),
+			};
+
+			await ValidateHasColor(indicatorView, expected);
+		}
+
+		[Theory(DisplayName = "IndicatorView Background Color Updates Correctly on Windows")]
+		[InlineData(0xFFFF0000)]
+		[InlineData(0xFF00FF00)]
+		[InlineData(0xFF0000FF)]
+		public async Task BackgroundColorUpdatesCorrectly(uint color)
+		{
+			var expected = Color.FromUint(color);
+
+			var indicatorView = new IndicatorViewStub()
+			{
+				Count = 3,
+				Position = 0,
+				Background = new SolidPaintStub(Colors.Grey),
+			};
+
+			await ValidateHasColor(indicatorView, expected, () => indicatorView.Background = new SolidPaintStub(expected), nameof(indicatorView.Background));
+		}
 	}
 }
