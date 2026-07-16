0a96f3daf07f8400df8a22bba26239abdcca5d9b
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2026-07-17 02:20:45 +0530

    [Windows] Fix Dynamic ShellContent Title Updates at Runtime (#35998)
    ### Root Cause
    Runtime scenarios for ShellContent were not properly handled. When
    ShellContent was added or removed dynamically, its property changes
    (such as title updates) were not tracked correctly.
    
    
    ### Description of Change
    
    Windows: Moved title update handling from ShellContentHandler.Windows to
    ShellSectionHandler.Windows. This modification ensures that title
    changes are handled properly for both initial and dynamically added
    ShellContent items.
    
    
    ### Issues Fixed
    
    <!-- Please make sure that there is a bug logged for the issue being
    fixed. The bug should describe the problem and how to reproduce it. -->
    
    Fixes #27494 
    
    ### Regarding test case
    Added a test case for runtime-added ShellContent title updates in PR
    #26062 . These tests are applicable to my changes as well
    
    
    **Tested the behavior in the following platforms.**
    - [ ] Android
    - [x] Windows
    - [ ] iOS
    - [ ] Mac
    
    ### Output Screenshot
    
    | Before  | After  |
    |---------|--------|
    | **Android**<br> <video
    src="https://github.com/user-attachments/assets/fda0a962-fdef-425e-9ec6-cfc263af4c53"
    width="260" height="500"> |**Android**<br> <video
    src="https://github.com/user-attachments/assets/cb3ed516-2955-46c6-b9aa-4c7ee22207ad"
    width="260" height="500"> |
    | **Windows**<br> <video
    src="https://github.com/user-attachments/assets/dc8db704-dc67-44ed-8752-0165ed124dd7"
    width="300" height="600"> | **Windows**<br> <video
    src="https://github.com/user-attachments/assets/0b14e6c2-b898-4c71-b464-80c33b5bab9e"
    width="300" height="600"> |
    
    ---------
    
    Co-authored-by: Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>

---
+++ src/Controls/src/Core/Platform/Windows/Extensions/ShellContentExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls.Handlers;
+
+namespace Microsoft.Maui.Controls.Platform
+{
+	internal static class ShellContentExtensions
+	{
+		internal static void UpdateParentShellItemTitle(this ShellContent shellContent)
+		{
+			if (shellContent.Parent is ShellSection shellSection && shellSection.Parent is ShellItem shellItem && shellItem.Handler is ShellItemHandler shellItemHandler)
+			{
+				shellItemHandler.UpdateTitle();
+			}
+		}
+	}
+}
