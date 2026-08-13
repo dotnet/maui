f4465064acaed216480b09dfcaa8ee9669eb1e10
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2026-08-13 17:58:18 +0530

    fix VoiceOver does not announce the role or interactivity

--- src/Controls/src/Core/Platform/iOS/Extensions/AcessibilityExtensions.cs
+++ src/Controls/src/Core/Platform/iOS/Extensions/AcessibilityExtensions.cs
@@ -4,6 +4,8 @@ namespace Microsoft.Maui.Controls.Platform;
 
 internal static class AcessibilityExtensions
 {
+	const int MaxAccessibilityElementDepth = 10;
+
 	internal static void UpdateAccessibilityTraits(this UICollectionView collectionView, SelectableItemsView itemsView)
 	{
 		foreach (var subview in collectionView.Subviews)
@@ -30,14 +32,45 @@ internal static class AcessibilityExtensions
 				return;
 			}
 
+			var accessibilityElement = FindAccessibilityElement(firstChild, 0) ?? firstChild;
+
 			if (selectionMode != SelectionMode.None)
 			{
-				firstChild.AccessibilityTraits |= UIAccessibilityTrait.Button;
+				accessibilityElement.AccessibilityTraits |= UIAccessibilityTrait.Button;
 			}
 			else
 			{
-				firstChild.AccessibilityTraits &= ~UIAccessibilityTrait.Button;
+				accessibilityElement.AccessibilityTraits &= ~UIAccessibilityTrait.Button;
 			}
 		}
+	}
+
+	static UIView? FindAccessibilityElement(UIView view, int depth)
+	{
+		if (view is UIControl)
+		{
+			return null;
+		}
+
+		if (view.IsAccessibilityElement)
+		{
+			return view;
+		}
+
+		if (depth >= MaxAccessibilityElementDepth)
+		{
+			return null;
+		}
+
+		var subviews = view.Subviews;
+		for (var index = 0; index < subviews.Length; index++)
+		{
+			if (FindAccessibilityElement(subviews[index], depth + 1) is UIView accessibilityElement)
+			{
+				return accessibilityElement;
+			}
+		}
+
+		return null;
 	}
 }
