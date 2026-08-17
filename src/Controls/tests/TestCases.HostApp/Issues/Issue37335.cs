ceb5b93328db232357b0b00ee8c2ce23bbff015d
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2026-08-17 15:43:21 +0530

    Added a test case

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue37335.cs
@@ -0,0 +1,33 @@
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 37335, "Orientation property as Horizontal is not working properly in ScrollView", PlatformAffected.Android)]
+public class Issue37335 : ContentPage
+{
+    public Issue37335()
+    {
+        Title = "Issue 37335";
+        // var text = string.Join(
+        //     Environment.NewLine,
+        //     Enumerable.Range(1, 60).Select(i =>
+        //         $"Row {i:D2} — Hello, World! This is a sample of a very long text that will require scrolling to view completely."));
+
+        var scrollView = new ScrollView
+        {
+            AutomationId = "HorizontalScrollView",
+            Orientation = ScrollOrientation.Horizontal,
+            VerticalScrollBarVisibility = ScrollBarVisibility.Always,
+            HorizontalScrollBarVisibility = ScrollBarVisibility.Always,
+            Content = new Label
+            {
+                AutomationId = "ScrollContent",
+                Text = "Hello, World! This is a sample of a very long text that will require scrolling to view completely. The purpose of this text is to demonstrate the scrolling capabilities of the ScrollView in .NET MAUI. You can add more content here to make it even longer and test the scrolling behavior further. Enjoy exploring the features of MAUI!",
+                TextColor = Colors.Black,
+                BackgroundColor = Colors.Red,
+                FontSize = 18,
+                WidthRequest = 1500,
+                HeightRequest = 2500
+            }
+        };
+        Content = scrollView;
+    }
+}
