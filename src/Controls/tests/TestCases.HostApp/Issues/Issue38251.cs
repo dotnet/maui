cf976a7ee1efc7dc477b6bb85ddfe2a1de1d07ba
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2026-09-08 15:26:38 +0530

    Update Issue38251.cs

--- src/Controls/tests/TestCases.HostApp/Issues/Issue38251.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue38251.cs
@@ -15,6 +15,10 @@ public class Issue38251 : ContentPage
                 new RowDefinition(GridLength.Auto),
                 new RowDefinition(GridLength.Auto),
                 new RowDefinition(GridLength.Auto),
+                new RowDefinition(GridLength.Auto),
+                new RowDefinition(GridLength.Auto),
+                new RowDefinition(GridLength.Auto),
+                new RowDefinition(GridLength.Auto),
                 new RowDefinition(GridLength.Star),
             },
             RowSpacing = 12,
@@ -22,7 +26,7 @@ public class Issue38251 : ContentPage
 
         grid.Add(new Label
         {
-            Text = "LTR reference",
+            Text = "LTR reference (image right)",
             FontAttributes = FontAttributes.Bold,
         }, 0, 1);
 
@@ -39,7 +43,7 @@ public class Issue38251 : ContentPage
         grid.Add(new Label
         {
             Margin = new Thickness(0, 12, 0, 0),
-            Text = "RTL reproduction",
+            Text = "RTL reproduction (image right)",
             FontAttributes = FontAttributes.Bold,
         }, 0, 3);
 
@@ -52,6 +56,40 @@ public class Issue38251 : ContentPage
             FlowDirection = FlowDirection.RightToLeft,
             HorizontalOptions = LayoutOptions.Fill,
         }, 0, 4);
+
+        grid.Add(new Label
+        {
+            Margin = new Thickness(0, 12, 0, 0),
+            Text = "LTR reference (image left)",
+            FontAttributes = FontAttributes.Bold,
+        }, 0, 5);
+
+        grid.Add(new Button
+        {
+            AutomationId = "LtrLeftImageButton",
+            Text = "Image1",
+            ImageSource = "coffee.png",
+            ContentLayout = new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Left, 50),
+            FlowDirection = FlowDirection.LeftToRight,
+            HorizontalOptions = LayoutOptions.Fill,
+        }, 0, 6);
+
+        grid.Add(new Label
+        {
+            Margin = new Thickness(0, 12, 0, 0),
+            Text = "RTL reproduction (image left)",
+            FontAttributes = FontAttributes.Bold,
+        }, 0, 7);
+
+        grid.Add(new Button
+        {
+            AutomationId = "RtlLeftImageButton",
+            Text = "Image1",
+            ImageSource = "coffee.png",
+            ContentLayout = new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Left, 50),
+            FlowDirection = FlowDirection.RightToLeft,
+            HorizontalOptions = LayoutOptions.Fill,
+        }, 0, 8);
 
         Content = grid;
     }
