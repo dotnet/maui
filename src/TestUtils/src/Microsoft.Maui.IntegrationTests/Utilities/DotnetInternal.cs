dotnet/maui

commit afa32ac2ba662fa2e3bb4775b30eb7518976f1ad

Update dotnet/dotnet dependency to the latest 10.0.1xx SDK. (#38318)
### Description of Change

This PR is the result of running:

```
$ darc update-dependencies --channel ".NET 10.0.1xx SDK"
```

NuGet.config
@@ -4,9 +4,15 @@
   <!-- `clear` ensures no additional sources are inherited from another config file. -->
   <packageSources>
     <clear />
+    <!--Begin: Package sources managed by Dependency Flow automation. Do not edit the sources below.-->
+    <!--  Begin: Package sources from dotnet-android -->
+    <add key="darc-pub-dotnet-android-ee72c6f" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-android-ee72c6ff/nuget/v3/index.json" />
+    <!--  End: Package sources from dotnet-android -->
     <!--  Begin: Package sources from dotnet-dotnet -->
+    <add key="darc-pub-dotnet-dotnet-11741c8" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-dotnet-11741c82/nuget/v3/index.json" />
     <add key="darc-pub-dotnet-dotnet-e17b0d0" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-dotnet-e17b0d08/nuget/v3/index.json" />
     <!--  End: Package sources from dotnet-dotnet -->
+    <!--End: Package sources managed by Dependency Flow automation. Do not edit the sources above.-->
     <add key="local" value="LOCAL_PLACEHOLDER" />
     <add key="nuget-only" value="NUGET_ONLY_PLACEHOLDER" />
     <add key="dotnet-public" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-public/nuget/v3/index.json" protocolVersion="3" />

eng/Version.Details.xml
@@ -1,16 +1,16 @@
 <Dependencies>
   <ProductDependencies>
-    <Dependency Name="Microsoft.NET.Sdk" Version="10.0.100-rtm.25523.113">
+    <Dependency Name="Microsoft.NET.Sdk" Version="10.0.113-servicing.26454.107">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.NETCore.App.Ref" Version="10.0.0">
+    <Dependency Name="Microsoft.NETCore.App.Ref" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Android.Sdk.Windows" Version="36.1.2">
+    <Dependency Name="Microsoft.Android.Sdk.Windows" Version="36.1.115">
       <Uri>https://github.com/dotnet/android</Uri>
-      <Sha>01024bb616e7b80417a2c6d320885bfdb956f20a</Sha>
+      <Sha>ee72c6ff89ce0b2609b21fc5b0897741cae8dc5c</Sha>
     </Dependency>
     <!-- Previous .NET Android version(s) -->
     <Dependency Name="Microsoft.NET.Sdk.Android.Manifest-9.0.100" Version="35.0.105" CoherentParentDependency="Microsoft.Android.Sdk.Windows">
@@ -54,109 +54,109 @@
     <Dependency Name="Microsoft.WindowsAppSDK" Version="0.0.1">
       <Uri>https://dev.azure.com/microsoft/ProjectReunion/_git/ProjectReunionInternal</Uri>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Authorization" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Authorization" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Authentication.Facebook" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Authentication.Facebook" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Authentication.Google" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Authentication.Google" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Authentication.MicrosoftAccount" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Authentication.MicrosoftAccount" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Components" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Components" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Components.Analyzers" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Components.Analyzers" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Components.Forms" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Components.Forms" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Components.WebAssembly" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Components.WebAssembly" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Components.WebAssembly.Server" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Components.WebAssembly.Server" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Components.WebView" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Components.WebView" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Components.Web" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Components.Web" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.Metadata" Version="10.0.0">
+    <Dependency Name="Microsoft.AspNetCore.Metadata" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.JSInterop" Version="10.0.0">
+    <Dependency Name="Microsoft.JSInterop" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
     <Dependency Name="Microsoft.TemplateEngine.Tasks" Version="7.0.100-preview.2.22102.8">
       <Uri>https://github.com/dotnet/templating</Uri>
       <Sha>3f4da9ced34942d83054e647f3b1d9d7dde281e8</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.Configuration" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.Configuration" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.Configuration.Abstractions" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.Configuration.Abstractions" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.Configuration.Json" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.Configuration.Json" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.DependencyInjection" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.DependencyInjection" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.DependencyInjection.Abstractions" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.DependencyInjection.Abstractions" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.FileProviders.Abstractions" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.FileProviders.Abstractions" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.Logging.Abstractions" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.Logging.Abstractions" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.Logging" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.Logging" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.Logging.Console" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.Logging.Console" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.Logging.Debug" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.Logging.Debug" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.Primitives" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.Primitives" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.Extensions.Hosting.Abstractions" Version="10.0.0">
+    <Dependency Name="Microsoft.Extensions.Hosting.Abstractions" Version="10.0.13">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
     <Dependency Name="Microsoft.DotNet.XHarness.TestRunners.Common" Version="11.0.0-prerelease.26451.1">
       <Uri>https://github.com/dotnet/xharness</Uri>
@@ -172,37 +172,37 @@
     </Dependency>
   </ProductDependencies>
   <ToolsetDependencies>
-    <Dependency Name="Microsoft.DotNet.Build.Tasks.Feed" Version="10.0.0-beta.25523.113">
+    <Dependency Name="Microsoft.DotNet.Build.Tasks.Feed" Version="10.0.0-beta.26454.107">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Arcade.Sdk" Version="10.0.0-beta.25523.113">
+    <Dependency Name="Microsoft.DotNet.Arcade.Sdk" Version="10.0.0-beta.26454.107">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Build.Tasks.Installers" Version="10.0.0-beta.25523.113">
+    <Dependency Name="Microsoft.DotNet.Build.Tasks.Installers" Version="10.0.0-beta.26454.107">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Build.Tasks.Workloads" Version="10.0.0-beta.25523.113">
+    <Dependency Name="Microsoft.DotNet.Build.Tasks.Workloads" Version="10.0.0-beta.26454.107">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Helix.Sdk" Version="10.0.0-beta.25523.113">
+    <Dependency Name="Microsoft.DotNet.Helix.Sdk" Version="10.0.0-beta.26454.107">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.SharedFramework.Sdk" Version="10.0.0-beta.25523.113">
+    <Dependency Name="Microsoft.DotNet.SharedFramework.Sdk" Version="10.0.0-beta.26454.107">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.RemoteExecutor" Version="10.0.0-beta.25523.113">
+    <Dependency Name="Microsoft.DotNet.RemoteExecutor" Version="10.0.0-beta.26454.107">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.XUnitExtensions" Version="10.0.0-beta.25523.113">
+    <Dependency Name="Microsoft.DotNet.XUnitExtensions" Version="10.0.0-beta.26454.107">
       <Uri>https://github.com/dotnet/dotnet</Uri>
-      <Sha>7b29526f2107416f68578bcb9deaca74fcfcf7f0</Sha>
+      <Sha>11741c82dbdf7acebca70db556911222226bee52</Sha>
     </Dependency>
   </ToolsetDependencies>
 </Dependencies>

commit b96aa036b89fe41fe1ce6cae63a3f2d1550e5682

Fix .NET 9 integration tests after the servicing SDK update (#38373)
<!-- Please let the below note in for people that find this PR -->
> [!NOTE]
> Are you waiting for the changes in this PR to be merged?
> It would be very helpful if you could [test the resulting
artifacts](https://github.com/dotnet/maui/wiki/Testing-PR-Builds) from
this PR and let us know in a comment if this change resolves your issue.
Thank you!

### Description of Change

Address both .NET 9 integration-test failures exposed by the servicing
SDK update in #38318, while preserving its SDK/dependency upgrades:

- Add the public ASP.NET Core, runtime, and Windows Desktop servicing
feeds containing the **9.0.21** targeting/runtime packs. Keep these
sources outside the Dependency Flow-managed block.
- In the integration-test build helper, retain the `NETSDK1202` workload
end-of-support diagnostic as a **warning**, rather than promoting it to
an error with `-warnaserror`. The compatibility matrix intentionally
exercises previous .NET workloads. Other warnings remain errors; no test
cases are skipped, and shipped templates/support policy are unchanged.

The installed SDK is now `10.0.113-servicing.26454.107`. When targeting
`net9.0`, it requests 9.0.21 packs, but the new .NET 10 coherent feed
supplies 10.0.13 packages and none of the previously configured sources
supplies the required 9.0.21 packs. Adding the general `dotnet9` feed
alone would not help: it does not contain these servicing versions.

The same 26 unique .NET 9 integration cases failed on [main build
1587498](https://dev.azure.com/dnceng-public/public/_build/results?buildId=1587498)
and [inflight/candidate build
1587610](https://dev.azure.com/dnceng-public/public/_build/results?buildId=1587610).
These builds also reported `NETSDK1202` for `net9.0-android`.

### Validation

**Final [maui-pr build
1589411](https://dev.azure.com/dnceng-public/public/_build/results?buildId=1589411)
succeeded on commit `d6ed823714`: all 26 jobs passed, with no failed,
canceled, pending, or skipped jobs.** Build Analysis and Build Insights
also passed.

The actual test logs confirm all four affected integration jobs passed,
with no missing-package or fatal EOL errors:

| Integration job | Passed | Failed | Skipped |
| --- | ---: | ---: | ---: |
| [Build
macOS](https://dev.azure.com/dnceng-public/public/_apis/build/builds/1589411/logs/965?api-version=7.1)
| 67 | 0 | 0 |
| [Build
windows](https://dev.azure.com/dnceng-public/public/_apis/build/builds/1589411/logs/956?api-version=7.1)
| 67 | 0 | 0 |
|
[WindowsTemplates](https://dev.azure.com/dnceng-public/public/_apis/build/builds/1589411/logs/909?api-version=7.1)
| 29 | 0 | 0 |
|
[RunOnAndroid](https://dev.azure.com/dnceng-public/public/_apis/build/builds/1589411/logs/900?api-version=7.1)
| 11 | 0 | 0 |

An isolated restore using the repository's NuGet.config reproduced
`NU1102` before the feed change for all six package IDs below at exactly
`9.0.21`. The same restore succeeded afterward; package metadata
confirms the intended public sources.

- `Microsoft.AspNetCore.App.Ref`
- `Microsoft.WindowsDesktop.App.Ref`
- `Microsoft.NETCore.App.Ref`
- `Microsoft.NETCore.App.Host.win-x64`
- `Microsoft.NETCore.App.Runtime.win-x64`
- `Microsoft.NETCore.App.Crossgen2.win-x64`

The first [PR CI build,
1589155](https://dev.azure.com/dnceng-public/public/_build/results?buildId=1589155),
confirmed the completed affected integration logs no longer contained
missing-package errors. RunOnAndroid passed all 11 cases. Build macOS
and WindowsTemplates still failed because they promoted `NETSDK1202` to
an error, motivating the test-helper follow-up.

For that follow-up, the real .NET SDK `_CheckForEolWorkloads` target
fails under `-warnaserror` and succeeds, with the warning still printed,
when adding `-warnnotaserror:NETSDK1202`. An unrelated control warning
still fails. Verified with installed .NET 10 and .NET 11 MSBuild; the
integration harness compiles successfully with .NET 10.

### Issues Fixed

Fixes the main and inflight/candidate integration regression introduced
by #38318. No separate issue was supplied. No other open PR referencing

NuGet.config
@@ -13,6 +13,10 @@
     <add key="darc-pub-dotnet-dotnet-e17b0d0" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-dotnet-e17b0d08/nuget/v3/index.json" />
     <!--  End: Package sources from dotnet-dotnet -->
     <!--End: Package sources managed by Dependency Flow automation. Do not edit the sources above.-->
+    <!-- .NET 9 integration tests need the 9.0.21 targeting/runtime packs requested by the .NET 10 servicing SDK. -->
+    <add key="darc-pub-dotnet-aspnetcore-58518050" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-aspnetcore-58518050/nuget/v3/index.json" />
+    <add key="darc-pub-dotnet-runtime-db534f81" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-runtime-db534f81/nuget/v3/index.json" />
+    <add key="darc-pub-dotnet-windowsdesktop-0bd7f472" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-windowsdesktop-0bd7f472/nuget/v3/index.json" />
     <add key="local" value="LOCAL_PLACEHOLDER" />
     <add key="nuget-only" value="NUGET_ONLY_PLACEHOLDER" />
     <add key="dotnet-public" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-public/nuget/v3/index.json" protocolVersion="3" />

src/TestUtils/src/Microsoft.Maui.IntegrationTests/Utilities/DotnetInternal.cs
@@ -58,6 +58,9 @@ namespace Microsoft.Maui.IntegrationTests
 				// affect only C# compiler warnings).
 				buildArgs += " -warnaserror";
 
+				// Previous-.NET compatibility tests intentionally exercise workloads after end of support.
+				buildArgs += " -warnnotaserror:NETSDK1202";
+
 				// However, we need to ignore specific MSBuild warnings that are acceptable in these tests:
 				var csWarningsToIgnore = new List<string>
 				{