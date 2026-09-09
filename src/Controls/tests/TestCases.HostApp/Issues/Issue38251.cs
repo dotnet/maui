50d5434bd735103e695add4ac58e20330b5672bf
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2026-09-09 15:35:30 +0530

    test sample modified

--- src/Controls/tests/TestCases.HostApp/Issues/Issue38251.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue38251.cs
@@ -24,73 +24,47 @@ public class Issue38251 : ContentPage
             RowSpacing = 12,
         };
 
-        grid.Add(new Label
-        {
-            Text = "LTR reference (image right)",
-            FontAttributes = FontAttributes.Bold,
-        }, 0, 1);
+        grid.Add(CreateLabel("LTR reference (image right)"), 0, 1);
 
-        grid.Add(new Button
-        {
-            AutomationId = "LtrReferenceButton",
-            Text = "Image1",
-            ImageSource = "coffee.png",
-            ContentLayout = new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Right, 50),
-            FlowDirection = FlowDirection.LeftToRight,
-            HorizontalOptions = LayoutOptions.Fill,
-        }, 0, 2);
+        grid.Add(CreateButton("LtrReferenceButton", new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Right, 50), FlowDirection.LeftToRight), 0, 2);
 
-        grid.Add(new Label
-        {
-            Margin = new Thickness(0, 12, 0, 0),
-            Text = "RTL reproduction (image right)",
-            FontAttributes = FontAttributes.Bold,
-        }, 0, 3);
+        grid.Add(CreateLabel("RTL reproduction (image right)"), 0, 3);
 
-        grid.Add(new Button
-        {
-            AutomationId = "RtlReferenceButton",
-            Text = "Image1",
-            ImageSource = "coffee.png",
-            ContentLayout = new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Right, 50),
-            FlowDirection = FlowDirection.RightToLeft,
-            HorizontalOptions = LayoutOptions.Fill,
-        }, 0, 4);
+        grid.Add(CreateButton("RtlReferenceButton", new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Right, 50), FlowDirection.RightToLeft), 0, 4);
 
-        grid.Add(new Label
-        {
-            Margin = new Thickness(0, 12, 0, 0),
-            Text = "LTR reference (image left)",
-            FontAttributes = FontAttributes.Bold,
-        }, 0, 5);
+        grid.Add(CreateLabel("LTR reference (image left)"), 0, 5);
 
-        grid.Add(new Button
-        {
-            AutomationId = "LtrLeftImageButton",
-            Text = "Image1",
-            ImageSource = "coffee.png",
-            ContentLayout = new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Left, 50),
-            FlowDirection = FlowDirection.LeftToRight,
-            HorizontalOptions = LayoutOptions.Fill,
-        }, 0, 6);
+        grid.Add(CreateButton("LtrLeftImageButton", new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Left, 50), FlowDirection.LeftToRight), 0, 6);
 
-        grid.Add(new Label
-        {
-            Margin = new Thickness(0, 12, 0, 0),
-            Text = "RTL reproduction (image left)",
-            FontAttributes = FontAttributes.Bold,
-        }, 0, 7);
+        grid.Add(CreateLabel("RTL reproduction (image left)"), 0, 7);
 
-        grid.Add(new Button
-        {
-            AutomationId = "RtlLeftImageButton",
-            Text = "Image1",
-            ImageSource = "coffee.png",
-            ContentLayout = new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Left, 50),
-            FlowDirection = FlowDirection.RightToLeft,
-            HorizontalOptions = LayoutOptions.Fill,
-        }, 0, 8);
+        grid.Add(CreateButton("RtlLeftImageButton", new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Left, 50), FlowDirection.RightToLeft), 0, 8);
 
         Content = grid;
+    }
+
+    Button CreateButton(string automationId, Button.ButtonContentLayout contentLayout, FlowDirection flowDirection)
+    {
+        Button button = new Button
+        {
+            AutomationId = automationId,
+            BackgroundColor = Colors.LightGray,
+            Text = "Image1",
+            ImageSource = "coffee.png",
+            ContentLayout = contentLayout,
+            FlowDirection = flowDirection,
+            HorizontalOptions = LayoutOptions.Fill,
+        };
+        return button;
+    }
+
+    Label CreateLabel(string text)
+    {
+        return new Label
+        {
+            Text = text,
+            FontAttributes = FontAttributes.Bold,
+            Margin = new Thickness(0, 12, 0, 0),
+        };
     }
 }
