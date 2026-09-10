3cc42eb971d5170f6313ea0dd009550d10918b53
KarthikRajaKalaimani <92777139+KarthikRajaKalaimani@users.noreply.github.com>
2026-09-10 12:42:48 +0530

    Test case improved

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue36422.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue36422.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		{
 			App.WaitForElement("IncreaseSpacingButton");
 			App.Tap("IncreaseSpacingButton");
-			VerifyScreenshot();
+			VerifyScreenshot(retryTimeout: TimeSpan.FromSeconds(2));
 		}
 	}
 }
