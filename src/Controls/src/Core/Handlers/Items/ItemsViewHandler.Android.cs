f346a6ff275bc546e2daa5685ef113ad1ffe9587
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2026-09-18 17:43:51 +0530

    Revert "Removed the unwanted changes"
    This reverts commit 54629392393f1b3877c6e45321d1b0aae13fdee3.

--- src/Controls/src/Core/Handlers/Items/ItemsViewHandler.Android.cs
+++ src/Controls/src/Core/Handlers/Items/ItemsViewHandler.Android.cs
@@ -136,6 +136,10 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 						// ViewHolder not created yet, defer layout request to next UI loop iteration
 						platformView.Post(() =>
 						{
+							if (!ReferenceEquals(((IElementHandler)this).PlatformView, platformView))
+							{
+								return;
+							}
 							var vh = platformView.FindViewHolderForAdapterPosition(emptyViewPosition);
 							vh?.ItemView.RequestLayout();
 						});
