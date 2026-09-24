3c4075a55c534786f297de031b7f61f89314c34e
Subhiksha Chandrasekaran <subhiksha.c@syncfusion.com>
2026-09-24 13:03:00 +0530

    Fix candidate failure test failure xArray Parsed

--- src/Controls/tests/DeviceTests/Xaml/XamlTests.cs
+++ src/Controls/tests/DeviceTests/Xaml/XamlTests.cs
@@ -40,6 +40,8 @@ namespace Microsoft.Maui.DeviceTests
 
 		[Fact("Parsed XAML can use x:Array")]
 		[RequiresUnreferencedCode("XAML parsing may require unreferenced code")]
+		[DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(TypeExtension))]
+		[DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(ArrayExtension))]
 		public void x_Array_Parsed()
 		{
 			var page = new ContentPage();
